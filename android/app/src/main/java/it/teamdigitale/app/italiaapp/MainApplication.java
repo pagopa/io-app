89b310d1901edf08d1a93f5312230efa81858028
Francesco Persico <francescopersico@gmail.com>
2017-12-13 16:45:00 +0100

    [#152927315] - Temporarily remove all native packages

--- android/app/src/main/java/it/teamdigitale/app/italiaapp/MainApplication.java
+++ android/app/src/main/java/it/teamdigitale/app/italiaapp/MainApplication.java
@@ -3,11 +3,6 @@ package it.teamdigitale.app.italiaapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.rnfs.RNFSPackage;
-import com.sha256lib.Sha256Package;
-import com.kevinejohn.RNMixpanel.RNMixpanel;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,12 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactNativeConfigPackage(),
-            new RNDeviceInfo(),
-            new RNFSPackage(),
-            new Sha256Package(),
-            new RNMixpanel()
+          new MainReactPackage()
       );
     }
 
