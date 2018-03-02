5a53c84e7d64077ca9a947854782b35cad747eeb
Federico Feroldi <cloudify@users.noreply.github.com>
2018-03-02 22:16:23 +0100

    Adds DangerJS rules (#61)

---
+++ Dangerfile.ts
@@ -0,0 +1,7 @@
+// import custom DangerJS rules
+// see http://danger.systems/js
+// see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
+// tslint:disable-next-line:prettier
+import checkDangers from 'danger-plugin-digitalcitizenship';
+
+checkDangers();
