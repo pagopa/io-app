714791537394e0d9c793983fbce56c040b75487f
Federico Feroldi <federico@teamdigitale.governo.it>
2018-04-02 17:29:42 +0200

    avoid using intermediate variable when generating next state

--- js/reducers/navigation.js
+++ js/reducers/navigation.js
@@ -6,24 +6,16 @@ import { type Action } from '../actions/types'
 import { USER_LOGGED_IN_ACTION, USER_LOGGED_OUT_ACTION } from '../actions/login'
 import { LOGIN_SUCCESS } from '../store/actions/constants'
 import AppNavigator from '../navigation/AppNavigator'
-const INITIAL_STATE = AppNavigator.router.getInitialState()
 
-const reducer = (
-  state: NavigationState = INITIAL_STATE,
-  action: Action
-): NavigationState => {
-  let nextState: ?NavigationState
+const INITIAL_STATE: NavigationState = AppNavigator.router.getInitialState()
+
+function nextState(state: NavigationState, action: Action): ?NavigationState {
   switch (action.type) {
+    // On user login or logout return to the IngressScreen
     case USER_LOGGED_IN_ACTION:
     case LOGIN_SUCCESS:
-      // On user login return to the IngressScreen
-      nextState = AppNavigator.router.getInitialState()
-      break
-
     case USER_LOGGED_OUT_ACTION:
-      // On user logout return to the IngressScreen
-      nextState = AppNavigator.router.getInitialState()
-      break
+      return INITIAL_STATE
 
     /**
      * The getStateForAction method only accepts NavigationActions so we need to
@@ -34,11 +26,18 @@ const reducer = (
     case NavigationActions.NAVIGATE:
     case NavigationActions.RESET:
     case NavigationActions.SET_PARAMS:
-      nextState = AppNavigator.router.getStateForAction(action, state)
-      break
-  }
+      return AppNavigator.router.getStateForAction(action, state)
 
-  return nextState || state
+    default:
+      return null
+  }
+}
+
+const reducer = (
+  state: NavigationState = INITIAL_STATE,
+  action: Action
+): NavigationState => {
+  return nextState(state, action) || state
 }
 
 export default reducer
