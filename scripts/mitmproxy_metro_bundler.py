8f80ee46a1cc55101160797be71ccea4e6bbfd71
Francesco Persico <francescopersico@gmail.com>
2018-04-23 17:00:12 +0200

    Fix imports

--- scripts/mitmproxy_metro_bundler.py
+++ scripts/mitmproxy_metro_bundler.py
@@ -1,8 +1,8 @@
 # This script is used during development to allow mitmproxy to intercept request on specific ports and make a proxy-pass on localhost
 # Usage: SIMULATOR_HOST_IP=XXXXX mitmweb --listen-port 9060 --web-port 9061 --ssl-insecure -s mitmproxy_metro_bundler.py
 import os
-from mitmproxy import ctx
+from mitmproxy import http
 
-def request(flow):
+def request(flow: http.HTTPFlow):
     if flow.request.host == os.environ.get("SIMULATOR_HOST_IP") and (flow.request.port == 8081 or flow.request.port == 8082 or flow.request.port == 8097):
       flow.request.host = "127.0.0.1"
