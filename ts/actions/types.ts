a4c8ba1799ecbb5c550af141bd92834b06e51575
Federico Feroldi <federico@teamdigitale.governo.it>
2018-04-24 15:26:43 +0200

    Fixes redux types

--- ts/actions/types.ts
+++ ts/actions/types.ts
@@ -4,7 +4,7 @@
 
 import {
   Store as ReduxStore,
-  DispatchAPI,
+  Dispatch as DispatchAPI,
   MiddlewareAPI as ReduxMiddlewareAPI,
   StoreEnhancer as ReduxStoreEnhancer
 } from 'redux'
@@ -20,27 +20,27 @@ import { APP_STATE_CHANGE_ACTION } from '../store/actions/constants'
 export type ApplicationState = 'background' | 'inactive' | 'active'
 
 export type ApplicationStateAction = {
-  type: typeof APP_STATE_CHANGE_ACTION,
+  type: typeof APP_STATE_CHANGE_ACTION
   payload: ApplicationState
 }
 
 export type Action =
   | { type: 'USER_WILL_LOGIN_ACTION' }
   | {
-      type: 'USER_SELECTED_SPID_PROVIDER_ACTION',
+      type: 'USER_SELECTED_SPID_PROVIDER_ACTION'
       data: { idp: IdentityProvider }
     }
-  | { type: 'USER_LOGGED_IN_ACTION', data: { token: string, idpId: string } }
-  | { type: 'USER_LOGIN_ERROR_ACTION', data: { error: string } }
+  | { type: 'USER_LOGGED_IN_ACTION'; data: { token: string; idpId: string } }
+  | { type: 'USER_LOGIN_ERROR_ACTION'; data: { error: string } }
   | { type: 'USER_LOGGED_OUT_ACTION' }
   | { type: 'REQUEST_USER_PROFILE_ACTION' }
   | {
-      type: 'RECEIVE_USER_PROFILE_ACTION',
-      profile: ApiUserProfile,
+      type: 'RECEIVE_USER_PROFILE_ACTION'
+      profile: ApiUserProfile
       receivedAt: number
     }
   | { type: 'UPDATE_USER_PROFILE_REQUEST_ACTION' }
-  | { type: 'UPDATE_USER_PROFILE_ERROR_ACTION', data: { error: string } }
+  | { type: 'UPDATE_USER_PROFILE_ERROR_ACTION'; data: { error: string } }
   | ApplicationStateAction
   | NavigationAction
   | SessionActions
@@ -57,11 +57,11 @@ export type AnyAction = Action | Thunk
 
 export type Dispatch = DispatchAPI<AnyAction>
 
-export type Store = ReduxStore<GlobalState, Action, Dispatch>
+export type Store = ReduxStore<GlobalState>
 
-export type StoreEnhancer = ReduxStoreEnhancer<GlobalState, Action, Dispatch>
+export type StoreEnhancer = ReduxStoreEnhancer<GlobalState>
 
-export type MiddlewareAPI = ReduxMiddlewareAPI<GlobalState, Action, Dispatch>
+export type MiddlewareAPI = ReduxMiddlewareAPI<GlobalState>
 
 // Props injected by react-redux connect() function
 export type ReduxProps = {
