4e19cee586e733b2d7925c1386cae64863271970
Francesco Persico <francescopersico@gmail.com>
2018-04-24 17:33:51 +0200

    Remove unused components

--- ts/screens/main/ProfileScreen.tsx
+++ ts/screens/main/ProfileScreen.tsx
@@ -5,36 +5,19 @@ import { Container } from 'native-base'
 import { GlobalState } from '../../reducers/types'
 import { ReduxProps } from '../../actions/types'
 import { UserState } from '../../reducers/user'
-import ProfileComponent from '../../components/ProfileComponent'
-type ReduxMappedProps = {
-  user: UserState
-}
-type OwnProps = {
+
+export type OwnProps = {
   navigation: NavigationScreenProp<NavigationState>
 }
-type Props = ReduxMappedProps & ReduxProps & OwnProps
+
+export type Props = OwnProps
 /**
  * This screen show the profile to the authenticated user.
  */
 class ProfileScreen extends React.Component<Props, never> {
   render() {
-    const { user, navigation, dispatch } = this.props
-    if (!user.isLoggedIn) {
-      return null
-    }
-    return (
-      <Container>
-        <ProfileComponent
-          user={user}
-          navigation={navigation}
-          dispatch={dispatch}
-        />
-      </Container>
-    )
+    return <Container />
   }
 }
-const mapStateToProps = (state: GlobalState): ReduxMappedProps => ({
-  user: state.user,
-  navigation: state.navigation
-})
-export default connect(mapStateToProps)(ProfileScreen)
+
+export default ProfileScreen
