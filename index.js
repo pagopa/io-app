cf85f03a0124413e210620ce89657970df22277c
Federico Feroldi <federico@teamdigitale.governo.it>
2018-04-26 09:20:50 +0200

    Updates sources for build

--- index.js
+++
@@ -1,6 +0,0 @@
-/**
- * @flow
- */
-'use strict'
-
-require('./js/index')
--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@
  */
 
 import { AppRegistry } from 'react-native'
-import App from './App'
+import App from './ts/App'
 
 AppRegistry.registerComponent('ItaliaApp', () => App)
