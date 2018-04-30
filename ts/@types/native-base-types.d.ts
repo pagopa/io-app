33c67a81bdff71792d043c7093ff776ec32e5c74
Federico Feroldi <federico@teamdigitale.governo.it>
2018-04-30 17:55:14 +0200

    Declare modules for native-base stuff that is not typed

---
+++ ts/@types/native-base-types.d.ts
@@ -0,0 +1,27 @@
+declare module "native-base-shoutem-theme" {
+  export interface IConnectStyleOptions {
+    withRef?: boolean;
+  }
+
+  export type MapPropsToStyleNames = (
+    styleNames: string[],
+    props: {}
+  ) => string[];
+
+  export function connectStyle(
+    componentStyleName: string,
+    componentStyle = {},
+    mapPropsToStyleNames: MapPropsToStyleNames,
+    options?: IConnectStyleOptions
+  );
+}
+
+declare module "native-base/src/Utils/mapPropsToStyleNames" {
+  export default function(styleNames: string[], props: {}): string[];
+}
+
+declare module "native-base/src/theme/components" {
+  type Variables = {};
+  type Theme = {};
+  export default function(variables: Variables): Theme;
+}
