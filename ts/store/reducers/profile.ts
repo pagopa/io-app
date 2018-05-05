e73b0d02698c7b01991849b53ea02325302ccfc8
Federico Feroldi <federico@teamdigitale.governo.it>
2018-05-05 17:50:47 +0200

    Fixes undefined state

--- ts/store/reducers/profile.ts
+++ ts/store/reducers/profile.ts
@@ -1,10 +1,13 @@
 /**
  * A reducer for the Profile.
- * It only manages SUCCESS actions because all UI state properties (like loading/error)
+ * It only manages SUCCESS actions because all UI state properties (like
+ * loading/error)
  * are managed by different global reducers.
  *
  * @flow
  */
+
+import { none, Option, some } from "fp-ts/lib/Option";
 
 import { Action } from "../../actions/types";
 import { ApiProfile } from "../../api";
@@ -13,9 +16,9 @@ import {
   PROFILE_UPDATE_SUCCESS
 } from "../actions/constants";
 
-export type ProfileState = ApiProfile | undefined;
+export type ProfileState = Option<ApiProfile>;
 
-export const INITIAL_STATE: ProfileState = undefined;
+export const INITIAL_STATE: ProfileState = none;
 
 const reducer = (
   state: ProfileState = INITIAL_STATE,
@@ -23,10 +26,10 @@ const reducer = (
 ): ProfileState => {
   switch (action.type) {
     case PROFILE_LOAD_SUCCESS:
-      return action.payload;
+      return some(action.payload);
 
     case PROFILE_UPDATE_SUCCESS:
-      return action.payload;
+      return some(action.payload);
 
     default:
       return state;
