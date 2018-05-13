58e08192b71ca262a70ca847b58fead613441870
Federico Feroldi <federico@teamdigitale.governo.it>
2018-05-13 17:38:27 +0200

    Adds typesafe definition of API requests.
    Also adds a method to generate a fetch based implementation

--- ts/utils/api.ts
+++ ts/utils/api.ts
@@ -2,38 +2,112 @@
  * Implements the APIs to interact with the backend.
  */
 
+import * as t from "io-ts";
+
 import { ExtendedProfile } from "../../definitions/backend/ExtendedProfile";
 import { Profile } from "../../definitions/backend/Profile";
 
-import { createApiCall } from "./api_request";
+import {
+  ApiHeaderJson,
+  composeHeaderProducers,
+  createFetchRequestForApi,
+  IGetApiRequestType,
+  IPostApiRequestType,
+  RequestHeaderProducer,
+  RequestHeaders,
+  ResponseType
+} from "./api_request";
+
+type BasicResponseType<R> =
+  | ResponseType<200, R>
+  | ResponseType<500 | 404, Error>;
+
+function basicResponseDecoder<R>(
+  type: t.Type<R>
+): (response: Response) => Promise<BasicResponseType<R> | undefined> {
+  return async (response: Response) => {
+    if (response.status === 200) {
+      const json = await response.json();
+      const validated = type.decode(json);
+      if (validated.isRight()) {
+        return { status: 200, value: validated.value };
+      }
+    } else if (response.status === 500 || response.status === 404) {
+      return { status: response.status, value: new Error(response.statusText) };
+    }
+    return undefined;
+  };
+}
 
 interface IBaseApiParams {
   apiUrlPrefix: string;
   token: string;
 }
 
-export const getUserProfile = createApiCall({
+class AuthorizationHeaderProducer<P extends { token: string }>
+  implements RequestHeaderProducer<P, "Authorization"> {
+  public apply(p: P): RequestHeaders<"Authorization"> {
+    return {
+      Authorization: `Bearer ${p.token}`
+    };
+  }
+}
+
+//
+// GetUserProfile
+//
+
+type GetUserProfileRequestType = IGetApiRequestType<
+  IBaseApiParams,
+  "Authorization",
+  BasicResponseType<Profile>
+>;
+
+const getUserProfileRequestType: GetUserProfileRequestType = {
   method: "get",
-  url: (p: IBaseApiParams) => `${p.apiUrlPrefix}/api/v1/profile`,
-  headers: (p: IBaseApiParams) => ({
-    Authorization: `Bearer ${p.token}`
-  }),
-  response_type: Profile
-});
+  url: p => `${p.apiUrlPrefix}/api/v1/profile`,
+  headers: new AuthorizationHeaderProducer<IBaseApiParams>(),
+  response_decoder: basicResponseDecoder(Profile)
+};
+
+export const getUserProfile = createFetchRequestForApi(
+  getUserProfileRequestType
+);
+
+//
+// SetUserProfile
+//
 
 interface ISetProfileParams extends IBaseApiParams {
   newProfile: ExtendedProfile;
 }
 
-export const setUserProfile = createApiCall({
+type SetUserProfileApiRequestType = IPostApiRequestType<
+  ISetProfileParams,
+  "Authorization",
+  BasicResponseType<Profile>
+>;
+
+const setUserProfileHeaders = composeHeaderProducers(
+  new AuthorizationHeaderProducer<ISetProfileParams>(),
+  ApiHeaderJson
+);
+
+const setUserProfileRequestType: SetUserProfileApiRequestType = {
   method: "post",
-  url: (p: ISetProfileParams) => `${p.apiUrlPrefix}/api/v1/profile`,
-  headers: (p: ISetProfileParams) => ({
-    Authorization: `Bearer ${p.token}`
-  }),
-  body: (p: ISetProfileParams) => JSON.stringify(p.newProfile),
-  response_type: Profile
-});
+  url: p => `${p.apiUrlPrefix}/api/v1/profile`,
+  headers: setUserProfileHeaders,
+  body: p => JSON.stringify(p.newProfile),
+  response_decoder: basicResponseDecoder(Profile)
+};
+
+export const setUserProfile = createFetchRequestForApi(
+  setUserProfileRequestType
+);
+
+//
+// ----
+//
 
 /**
  * Describes a SPID Identity Provider
--- ts/utils/api_request.ts
+++ ts/utils/api_request.ts
@@ -1,46 +1,148 @@
-import * as t from "io-ts";
-
-import { fromEither } from "fp-ts/lib/Option";
-
-export type ApiMethod = "get" | "post";
+/**
+ * Type safe wrapper around the fetch API
+ */
 
 /**
- * P = params type
- * R = response body type
+ * Describes the possible methods of a request
  */
-export interface IBaseApiCall<P, R> {
-  readonly method: ApiMethod;
-  readonly url: (params: P) => string;
-  readonly headers: (params: P) => { [key: string]: string };
-  readonly response_type: t.Type<R>;
+export type RequestMethod = "get" | "post";
+
+/**
+ * Describes the possible header keys of a request
+ */
+type RequestHeaderKey = "Authorization" | "Content-Type";
+
+/**
+ * Describes a set of headers whose keys are of type RequestHeaderKey
+ */
+export type RequestHeaders<HS extends RequestHeaderKey> = {
+  [key in HS]: string
+};
+
+/**
+ * Generates a set of headers with certain keys (KH) from a parameters object
+ * of type P.
+ */
+export interface RequestHeaderProducer<P, KH extends RequestHeaderKey> {
+  readonly apply: (params: P) => RequestHeaders<KH>;
 }
 
-export interface IGetApiCall<P, R> extends IBaseApiCall<P, R> {
+/**
+ * Composes two RequestHeaderProducer(s)
+ */
+export function composeHeaderProducers<
+  PI,
+  KI extends RequestHeaderKey,
+  PH,
+  KH extends RequestHeaderKey
+>(
+  p0: RequestHeaderProducer<PI, KI>,
+  p1: RequestHeaderProducer<PH, KH>
+): RequestHeaderProducer<PI & PH, KI | KH> {
+  return {
+    apply: params => {
+      const headers0 = p0.apply(params);
+      const headers1 = p1.apply(params);
+      return {
+        ...(headers0 as any),
+        ...(headers1 as any)
+      };
+    }
+  };
+}
+
+/**
+ * Describes a possible response type: when the status code is S, the response
+ * type is T.
+ */
+export interface ResponseType<S extends number, T> {
+  status: S;
+  value: T;
+}
+
+/**
+ * Fully describes an API request.
+ *
+ * @param M   The request method
+ * @param P   The type of input parameters
+ * @param H   The headers that must be defined for this request
+ * @param R   The possible response types for this request
+ */
+export interface IBaseApiRequestType<
+  M extends RequestMethod,
+  P,
+  H extends RequestHeaderKey,
+  R
+> {
+  readonly method: M;
+  readonly url: (params: P) => string;
+  readonly headers: RequestHeaderProducer<P, H>;
+  readonly response_decoder: (response: Response) => Promise<R | undefined>;
+}
+
+/**
+ * Fully describes a GET request.
+ */
+export interface IGetApiRequestType<P, KH extends RequestHeaderKey, R>
+  extends IBaseApiRequestType<"get", P, KH, R> {
   readonly method: "get";
 }
 
-export interface IPostApiCall<P, R> extends IBaseApiCall<P, R> {
+/**
+ * Fully describes a POST request.
+ *
+ * POST requests require to provide the "Content-Type" header.
+ */
+export interface IPostApiRequestType<P, KH extends RequestHeaderKey, R>
+  extends IBaseApiRequestType<"post", P, KH | "Content-Type", R> {
   readonly method: "post";
   readonly body: (params: P) => string;
 }
 
-type ApiCallType<P, R> = IGetApiCall<P, R> | IPostApiCall<P, R>;
+type ApiRequestType<P, KH extends RequestHeaderKey, R> =
+  | IGetApiRequestType<P, KH, R>
+  | IPostApiRequestType<P, KH, R>;
 
-// TODO handle unsuccessful fetch and HTTP errors
-// @see https://www.pivotaltracker.com/story/show/154661120
-export function createApiCall<P, R>(
-  requestType: ApiCallType<P, R>
+//
+// helpers
+//
+
+/**
+ * Returns an async method that implements the provided ApiRequestType backed
+ * by the "fetch" API.
+ */
+export function createFetchRequestForApi<P, KH extends RequestHeaderKey, R>(
+  requestType: ApiRequestType<P, KH, R>
 ): (params: P) => Promise<R | undefined> {
+  // TODO: handle unsuccessful fetch and HTTP errors
+  // @see https://www.pivotaltracker.com/story/show/154661120
   return async params => {
+    // get the URL from the params
     const url = requestType.url(params);
-    const response = await fetch(url, {
+
+    // get the headers from the params
+    const headers = requestType.headers.apply(params);
+
+    // build the request
+    const requestInit: RequestInit = {
       method: requestType.method,
-      headers: requestType.headers(params),
+      headers,
       body: requestType.method === "get" ? null : requestType.body(params)
-    });
-    const payloadOrError = requestType.response_type.decode(
-      await response.json()
-    );
-    return fromEither(payloadOrError).toUndefined();
+    };
+
+    // make the async call
+    const response = await fetch(url, requestInit);
+
+    // decode the response
+    return requestType.response_decoder(response);
   };
 }
+
+/**
+ * An header producer that sets the Content-Type to application/json
+ */
+export const ApiHeaderJson: RequestHeaderProducer<{}, "Content-Type"> = {
+  apply: () => ({
+    "Content-Type": "application/json"
+  })
+};
