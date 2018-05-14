22b94549b33851981597761b710306fc9f73258e
Federico Feroldi <federico@teamdigitale.governo.it>
2018-05-14 15:34:27 +0200

    Removes IdentityProvider from api.ts

--- ts/utils/api.ts
+++ ts/utils/api.ts
@@ -71,22 +71,3 @@ const setUserProfileRequestType: SetUserProfileApiRequestType = {
 export const setUserProfile = createFetchRequestForApi(
   setUserProfileRequestType
 );
-
-//
-// ----
-//
-
-/**
- * Describes a SPID Identity Provider
- */
-export type IdentityProvider = {
-  id: string;
-  logo: any;
-  name: string;
-  entityID: string;
-  profileUrl: string;
-};
-
-export function isDemoIdp(idp: IdentityProvider): boolean {
-  return idp.id === "demo";
-}
