25db9e9e05deb9fd976eb96941ec2f2f6e4aeaaa
Federico Feroldi <federico@teamdigitale.governo.it>
2018-05-16 19:20:10 +0200

    Working TS tests

--- ts/__e2e__/app.e2e.ts
+++ ts/__e2e__/app.e2e.ts
@@ -1,5 +1,16 @@
-const detox = require("detox");
-const config = require("../package.json").detox;
+// tslint:disable:no-commented-code
+
+// FIXME: putting these declarations in detox.d.ts makes them available in all
+// test modules clashing with Jest's expect
+declare const device: Detox.Device;
+declare const element: Detox.Element;
+// declare const waitFor: Detox.WaitFor;
+declare const expect: Detox.Expect<Detox.Expect<any>>;
+declare const by: Detox.Matchers;
+
+import * as detox from "detox";
+
+const config = require("package.json").detox;
 
 // Set the default test timeout of 120s
 jest.setTimeout(120000);
@@ -12,13 +23,14 @@ afterAll(async () => {
   await detox.cleanup();
 });
 
-describe("app", () => {
+describe("e2e app", () => {
   it("should display SPID login button", async () => {
     await device.launchApp({ permissions: { notifications: "YES" } });
     await device.reloadReactNative();
 
     const loginButtonId = "landing-button-login";
     await expect(element(by.id(loginButtonId))).toBeVisible();
+
     // await element(by.id(loginButtonId)).tap();
     // await expect(element(by.id("idps-grid"))).toBeVisible();
   });
