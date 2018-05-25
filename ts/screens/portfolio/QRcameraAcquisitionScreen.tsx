a899bc38983b73535d3f903a55dc4361de593329
VALENTINA Cala <valentina.cala@ibm.com>
2018-05-25 17:29:35 +0200

    [156913734] setting of the QRcode component - tested on Android only

--- ts/screens/portfolio/QRcameraAcquisitionScreen.tsx
+++ ts/screens/portfolio/QRcameraAcquisitionScreen.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import{
 	Container,
-	Content,
 	Body,
 	Left,
 	Button,
 	Icon,
 	Text,
-	View
+	View,
+	Grid,
+	Row,
+	Col
 } from "native-base";
 import AppHeader from "../../components/ui/AppHeader";
 import QRCodeScanner from 'react-native-qrcode-scanner'; 
+
+import {Dimensions, StyleSheet} from 'react-native';
 
 import I18n from "../../i18n";
 import ROUTES from "../../navigation/routes";
@@ -56,13 +60,45 @@ export default class QRcameraAcquisitionScreen extends React.Component<Props, ne
 						</Text>
 					</Body>
 				</AppHeader>
-				<Content>
-					<QRCodeScanner>
+				<Container>
+					<QRCodeScanner
+						containerStyle={styles.container}
+						showMarker={true}
+						cameraStyle={styles.camera}
+						customMarker={
+							<View style={styles.rectangleContainer}>
+									<View style={styles.rectangle}>
+										<Grid>
+											<Row>
+												<Col>
+												<View style={styles.rectangleTL}/>
+												</Col>
+												<Col>
+												<View style={styles.rectangleTR}/>
+												</Col>
+											</Row>
+											<Row>
+												<Col>
+													<View style={styles.rectangleBL}/>
+												</Col>
+												<Col>
+												<View style={styles.rectangleBR}/>
+												</Col>
+											</Row>
+										</Grid>
+									</View>
+          				</View>
+						}
+					>
 					</QRCodeScanner>
-					<Text>
-						{I18n.t("portfolio.QRtoPay.instructions")}
-					</Text>
-				</Content>
+					<View>
+						<View spacer={true} large={true}/>
+						<Text style={{textAlign:'center'}}>
+								{I18n.t("portfolio.QRtoPay.instructions")}
+						</Text>
+						<View spacer={true} extralarge={true}/>
+					</View>	
+				</Container>
 				<View footer={true}>
 					<Button
 						block={true}
@@ -88,6 +124,98 @@ export default class QRcameraAcquisitionScreen extends React.Component<Props, ne
 			</Container>
 		);
 	}
-	
-
 }
+
+
+const screenwidth = Dimensions.get("screen").width;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "flex-start",
+		justifyContent: "center",
+		backgroundColor: "transparent"
+	 },
+	camera: {
+		flex: 0,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'transparent',
+		height: screenwidth*4/3,
+		width: screenwidth,
+	 },
+  
+	 rectangleContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'transparent',
+		
+		borderColor: 'rgba(56,56,56,0.5)',
+		//borderWidth: screenwidth/4,
+	 },
+
+	 outRectangle: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderWidth: screenwidth/2,
+		borderColor: 'rgba(56,56,56,0.5)',
+		
+	 },
+	 rectangle: {
+		height: screenwidth/2,
+		width: screenwidth/2,
+		borderWidth: 0,
+		borderColor: 'transparent',
+		backgroundColor: 'transparent',
+	 },
+
+	 rectangleTR: {
+		height: screenwidth/6,
+		width: screenwidth/6,
+		borderTopWidth: 2,
+		borderRightWidth: 2,
+		borderColor: '#FFFFFF',
+		backgroundColor: 'transparent',
+		position:'absolute',
+		top:0,
+		right:0
+	 },
+
+	 rectangleTL: {
+		height: screenwidth/6,
+		width: screenwidth/6,
+		borderTopWidth: 2,
+		borderLeftWidth: 2,
+		borderColor: '#FFFFFF',
+		backgroundColor: 'transparent',
+		position:'absolute',
+		top:0,
+		left:0
+	 },
+
+	 rectangleBL: {
+		height: screenwidth/6,
+		width: screenwidth/6,
+		borderBottomWidth: 2,
+		borderLeftWidth: 2,
+		borderColor: '#FFFFFF',
+		backgroundColor: 'transparent',
+		position:'absolute',
+		bottom:0,
+		left:0
+	 },
+
+	 rectangleBR: {
+		height: screenwidth/6,
+		width: screenwidth/6,
+		borderBottomWidth: 2,
+		borderRightWidth: 2,
+		borderColor: '#FFFFFF',
+		backgroundColor: 'transparent',
+		position:'absolute',
+		bottom:0,
+		right:0
+	 },
+
+})
