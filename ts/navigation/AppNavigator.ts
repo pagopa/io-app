7f5fe9148957d333c456eaeb3a584f4dcb2403f5
Flavio Giobergia <flavio.giobergia@gmail.com>
2018-05-29 10:00:04 +0200

    aligning text to the right; adding padding between rows

--- ts/navigation/AppNavigator.ts
+++ ts/navigation/AppNavigator.ts
@@ -5,6 +5,7 @@ import AuthenticationNavigator from "./AuthenticationNavigator";
 import MainNavigator from "./MainNavigator";
 import OnboardingNavigator from "./OnboardingNavigator";
 import ROUTES from "./routes";
+import { WalletHomeScreen } from '../screens/wallet/WalletHomeScreen';
 
 /**
  * The main stack of screens of the application.
@@ -15,7 +16,7 @@ const navigator = SwitchNavigator({
   [ROUTES.INGRESS]: {
     // This screen check if the user is authenticated than perform a redirect to
     // MainNavigator (if authenticated) or AuthenticationNavigator (otherwise)
-    screen: IngressScreen
+    screen: WalletHomeScreen
   },
   [ROUTES.AUTHENTICATION]: {
     // The navigator used for unauthenticated users
