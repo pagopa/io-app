1b5ed07827d7296d7e2fe0df566df7d1b89ce64e
Francesco Persico <fpersico@wellnet.it>
2018-06-06 17:51:30 +0200

    Update templates

--- index.js
+++ index.js
@@ -5,4 +5,9 @@
 import { AppRegistry } from 'react-native'
 import App from './ts/App'
 
+// Temporary workaround for @https://github.com/facebook/react-native/issues/18868
+// TODO: Remove this as soon as it has been fixed @https://www.pivotaltracker.com/story/show/158144437 
+import { YellowBox } from 'react-native'
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated'])
+
 AppRegistry.registerComponent('ItaliaApp', () => App)
