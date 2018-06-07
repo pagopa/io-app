dfabe96c3d1cff409e89b5fc18c2ea1604c42ad5
Francesco Persico <fpersico@wellnet.it>
2018-06-07 13:56:10 +0200

    Use AsyncStorage with redux-persist

--- ts/boot/configureStoreAndPersistor.ts
+++ ts/boot/configureStoreAndPersistor.ts
@@ -1,3 +1,4 @@
+import { AsyncStorage } from "react-native";
 import { NavigationState } from "react-navigation";
 import { createReactNavigationReduxMiddleware } from "react-navigation-redux-helpers";
 import { applyMiddleware, compose, createStore, Reducer } from "redux";
@@ -8,7 +9,6 @@ import {
   Persistor,
   persistStore
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import createSagaMiddleware from "redux-saga";
 import thunk from "redux-thunk";
 
@@ -23,7 +23,7 @@ const isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent;
 
 const persistConfig: PersistConfig = {
   key: "root",
-  storage,
+  storage: AsyncStorage,
   /**
    * Sections of the store that must be persisted and rehydrated.
    */
