bc16ef535cca2f5c4a8934a15ebc0c60374fa278
Francesco Persico <fpersico@wellnet.it>
2018-06-18 15:16:58 +0200

    Move the session to a redux-persist keychain storage

---
+++ ts/store/storages/keychain.ts
@@ -0,0 +1,33 @@
+import * as Keychain from "react-native-keychain";
+import { Storage } from "redux-persist";
+
+/**
+ * A storage to save/restore values from/to the Keychain.
+ * The react-native-keychain library do not support storing multiple key/value so we
+ * use the service as key. Each value is stored as a new service and a constant username
+ * is set.
+ */
+export const USERNAME = "REDUX_PERSIST";
+
+export default function createSecureStorage(): Storage {
+  return {
+    getItem: async key => {
+      const result = await Keychain.getGenericPassword({ service: key });
+      if (typeof result === "boolean") {
+        return undefined;
+      } else {
+        return result.password;
+      }
+    },
+
+    setItem: async (key, value) => {
+      return await Keychain.setGenericPassword(USERNAME, value, {
+        service: key
+      });
+    },
+
+    removeItem: async key => {
+      return await Keychain.resetGenericPassword({ service: key });
+    }
+  };
+}
