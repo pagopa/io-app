799ca42569276a80ee78c23d8f0a6ceb45432ff9
Francesco Persico <fpersico@wellnet.it>
2018-06-19 17:24:53 +0200

    Add tests

---
+++ ts/__mocks__/react-native-keychain.ts
@@ -0,0 +1,44 @@
+/**
+ * A mocked version of the Keychain
+ */
+
+type KeychainDB = {
+  [key: string]: {
+    username: string;
+    password: string;
+    service: string;
+  };
+};
+
+type Options = {
+  service: string;
+};
+
+const keychainDB: KeychainDB = {};
+
+module.exports = {
+  getGenericPassword: jest.fn(
+    (options: Options) =>
+      keychainDB[options.service]
+        ? Promise.resolve(keychainDB[options.service])
+        : Promise.resolve(false)
+  ),
+
+  setGenericPassword: jest.fn(
+    (username: string, password: string, options: Options) => {
+      // tslint:disable-next-line no-object-mutation
+      keychainDB[options.service] = {
+        username,
+        password,
+        service: options.service
+      };
+      return Promise.resolve(true);
+    }
+  ),
+
+  resetGenericPassword: jest.fn((options: Options) => {
+    // tslint:disable-next-line no-object-mutation
+    delete keychainDB[options.service];
+    return Promise.resolve(true);
+  })
+};
