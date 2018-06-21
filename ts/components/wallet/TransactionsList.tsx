18e4b151fb94e6389351be87d4c1d369fe408070
Flavio Giobergia <flavio.giobergia@gmail.com>
2018-06-21 12:26:31 +0200

    fixing minor bug

--- ts/components/wallet/TransactionsList.tsx
+++ ts/components/wallet/TransactionsList.tsx
@@ -21,6 +21,7 @@ import { connect, Dispatch } from "react-redux";
 import I18n from "../../i18n";
 import ROUTES from "../../navigation/routes";
 
+import { selectCardForDetails } from "../../store/actions/wallet/cards";
 import { selectTransactionForDetails } from "../../store/actions/wallet/transactions";
 import { GlobalState } from "../../store/reducers/types";
 import {
@@ -36,6 +37,7 @@ type ReduxMappedStateProps = Readonly<{
 
 type ReduxMappedDispatchProps = Readonly<{
   selectTransaction: (i: WalletTransaction) => void;
+  selectCard: (item: number) => void;
 }>;
 
 /**
@@ -85,6 +87,7 @@ class TransactionsList extends React.Component<Props> {
     <ListItem
       onPress={() => {
         this.props.selectTransaction(item);
+        this.props.selectCard(item.cardId);
         this.props.navigation.navigate(ROUTES.WALLET_TRANSACTION_DETAILS);
       }}
     >
@@ -167,7 +170,8 @@ const mapStateToProps = (
 };
 
 const mapDispatchToProps = (dispatch: Dispatch): ReduxMappedDispatchProps => ({
-  selectTransaction: item => dispatch(selectTransactionForDetails(item))
+  selectTransaction: item => dispatch(selectTransactionForDetails(item)),
+  selectCard: item => dispatch(selectCardForDetails(item))
 });
 
 export default connect(
