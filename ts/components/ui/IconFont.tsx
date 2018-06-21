bed1c99589277316d84725eec6576f6413942c34
Claudio Conversa <claudio.conversa@wellnet.it>
2018-06-21 12:06:03 +0200

    Fix post review

--- ts/components/ui/IconFont.tsx
+++ ts/components/ui/IconFont.tsx
@@ -1,7 +1,6 @@
 import { connectStyle } from "native-base-shoutem-theme";
 import mapPropsToStyleNames from "native-base/src/utils/mapPropsToStyleNames";
 import * as React from "react";
-import { StyleSheet } from "react-native";
 import { IconProps } from "react-native-vector-icons/Icon";
 import Icon from "../../theme/font-icons/io-icon-font/index";
 
@@ -15,12 +14,9 @@ type Props = Readonly<IconProps>;
 class IconFont extends React.Component<Props> {
   public render() {
     const { color, size, style } = this.props;
-    const styleOverride = StyleSheet.flatten([
-      style,
-      color !== undefined ? { color } : {},
-      size !== undefined ? { fontSize: size } : {}
-    ]);
-    return <Icon {...this.props} style={styleOverride} />;
+    const colorStyle = color ? { color } : {};
+    const fontSizeStyle = size ? { fontSize: size } : {};
+    return <Icon {...this.props} style={[style, colorStyle, fontSizeStyle]} />;
   }
 }
 
