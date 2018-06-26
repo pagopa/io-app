097c658bf036730d2f03ba83f8ee5d1041e31bc9
valentinacala <38431762+valentinacala@users.noreply.github.com>
2018-06-26 15:44:36 +0200

    [#156913734] QR scanner implementation (#224)
    * camera component integration
    
    * add screen to acquire the QR code by scanner

---
+++ ts/@types/react-native-qrcode-scanner.d.ts
@@ -0,0 +1,26 @@
+/**
+ * Bare essential to get things building
+ */
+declare module "react-native-qrcode-scanner" {
+  export type Props = Readonly<{
+    onRead?: () => void;
+    reactivate?: boolean;
+    reactivateTimeout?: number;
+    fadeIn?: boolean;
+    showMarker?: boolean;
+    cameraType?: "front" | "back";
+    customMarker?: React.ReactElement;
+    containerStyle?: any;
+    cameraStyle?: any;
+    topViewStyle?: any;
+    bottomViewStyle?: any;
+    topContent?: React.ReactElement | string;
+    bottomContent?: React.ReactElement | string;
+    notAuthorizedView?: React.ReactElement;
+    permissionDialogTitle?: string;
+    permissionDialogMessage?: string;
+    checkAndroid6Permissions?: boolean;
+  }>;
+
+  export default class QRCodeScanner extends React.Component<Props> {}
+}
--- ts/navigation/WalletNavigator.ts
+++ ts/navigation/WalletNavigator.ts
@@ -7,7 +7,8 @@ import ConfirmToProceedTransactionScreen from "../screens/wallet/ConfirmToProcee
 import CreditCardsScreen from "../screens/wallet/CreditCardsScreen";
 import { FirstTransactionSummaryScreen } from "../screens/wallet/FirstTransactionSummaryScreen";
 import { ManuallyIdentifyTransactionScreen } from "../screens/wallet/ManuallyIdentifyTransactionScreen";
-import { TransactionDetailsScreen } from "../screens/wallet/TransactionDetailsScreen";
+import { QRcodeAcquisitionByScannerScreen } from "../screens/wallet/QRcodeAcquisitionByScannerScreen";
+import TransactionDetailsScreen from "../screens/wallet/TransactionDetailsScreen";
 import TransactionsScreen from "../screens/wallet/TransactionsScreen";
 import WalletHomeScreen from "../screens/wallet/WalletHomeScreen";
 import ROUTES from "./routes";
@@ -32,6 +33,9 @@ const WalletNavigator = StackNavigator(
     },
     [ROUTES.WALLET_CARD_TRANSACTIONS]: {
       screen: TransactionsScreen
+    },
+    [ROUTES.WALLET_QRCODE_ACQUISITION_BY_SCANNER]: {
+      screen: QRcodeAcquisitionByScannerScreen
     },
     [ROUTES.WALLET_ADD_MANAGER]: {
       screen: AddManagerToCardScreen
