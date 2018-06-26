02b7c46bd181d7f5f109bf44d78526871fe94248
Francesco Persico <fpersico@wellnet.it>
2018-06-26 11:02:41 +0200

    Fix scripts

--- scripts/json-key-file-setup.sh
+++ scripts/json-key-file-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo $ENCODED_ITALIAAPP_JSON_KEY_FILE | base64 --decode >> /tmp/json-key.json
+echo $ENCODED_ITALIAAPP_JSON_KEY_FILE | base64 --decode > /tmp/json-key.json
--- scripts/keystore-setup.sh
+++ scripts/keystore-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo $ENCODED_ITALIAAPP_RELEASE_KEYSTORE | base64 --decode >> /tmp/italiaapp-release.keystore
+echo $ENCODED_ITALIAAPP_RELEASE_KEYSTORE | base64 --decode > /tmp/italiaapp-release.keystore
