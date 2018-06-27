ad7ebd9d9e4ea404914e132db58e54c31210f7ed
Flavio Giobergia <flavio.giobergia@gmail.com>
2018-06-27 10:54:01 +0200

    restoring ingress screen

--- ts/navigation/AppNavigator.ts
+++ ts/navigation/AppNavigator.ts
@@ -14,7 +14,7 @@ const navigator = SwitchNavigator({
   [ROUTES.INGRESS]: {
     // This screen check if the user is authenticated than perform a redirect to
     // MainNavigator (if authenticated) or AuthenticationNavigator (otherwise)
-    screen: MainNavigator
+    screen: IngressScreen
   },
   [ROUTES.AUTHENTICATION]: {
     // The navigator used for unauthenticated users
