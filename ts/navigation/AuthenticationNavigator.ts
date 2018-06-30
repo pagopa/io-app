82580c652449fa8758ba114492ead9054c13cd3a
Francesco Persico <fpersico@wellnet.it>
2018-06-30 19:19:19 +0200

    Include MarkdownScreen only on development mode

--- ts/navigation/AuthenticationNavigator.ts
+++ ts/navigation/AuthenticationNavigator.ts
@@ -1,5 +1,9 @@
-import { createStackNavigator } from "react-navigation";
+import {
+  createStackNavigator,
+  NavigationRouteConfigMap
+} from "react-navigation";
 
+import { environment } from "../config";
 import IdpLoginScreen from "../screens/authentication/IdpLoginScreen";
 import IdpSelectionScreen from "../screens/authentication/IdpSelectionScreen";
 import LandingScreen from "../screens/authentication/LandingScreen";
@@ -7,27 +11,38 @@ import MarkdownScreen from "../screens/authentication/MarkdownScreen";
 import SpidInformationRequestScreen from "../screens/authentication/SpidInformationRequestScreen";
 import ROUTES from "./routes";
 
+// Routes loaded in production mode
+const productionRouteConfigMap: NavigationRouteConfigMap = {
+  [ROUTES.AUTHENTICATION_LANDING]: {
+    screen: LandingScreen
+  },
+  [ROUTES.AUTHENTICATION_IDP_SELECTION]: {
+    screen: IdpSelectionScreen
+  },
+  [ROUTES.AUTHENTICATION_IDP_LOGIN]: {
+    screen: IdpLoginScreen
+  },
+  [ROUTES.AUTHENTICATION_SPID_INFORMATION_REQUEST]: {
+    screen: SpidInformationRequestScreen
+  },
+  [ROUTES.MARKDOWN]: {
+    screen: MarkdownScreen
+  }
+};
+
+// Routes loaded on development mode
+const developmentRouteConfigMap: NavigationRouteConfigMap = {
+  ...productionRouteConfigMap,
+  [ROUTES.MARKDOWN]: {
+    screen: MarkdownScreen
+  }
+};
+
 /**
  * The authentication related stack of screens of the application.
  */
 const navigator = createStackNavigator(
-  {
-    [ROUTES.AUTHENTICATION_LANDING]: {
-      screen: LandingScreen
-    },
-    [ROUTES.AUTHENTICATION_IDP_SELECTION]: {
-      screen: IdpSelectionScreen
-    },
-    [ROUTES.AUTHENTICATION_IDP_LOGIN]: {
-      screen: IdpLoginScreen
-    },
-    [ROUTES.AUTHENTICATION_SPID_INFORMATION_REQUEST]: {
-      screen: SpidInformationRequestScreen
-    },
-    [ROUTES.MARKDOWN]: {
-      screen: MarkdownScreen
-    }
-  },
+  environment === "DEV" ? developmentRouteConfigMap : productionRouteConfigMap,
   {
     // Let each screen handle the header and navigation
     headerMode: "none"
