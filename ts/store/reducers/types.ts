61acb3c1d96bf0eef35f8dfb853ac6078c39a3be
DenisFrezzato <frezzato.denis@gmail.com>
2018-07-18 09:58:34 +0200

    Fixes from rebase.

--- ts/store/reducers/types.ts
+++ ts/store/reducers/types.ts
@@ -1,17 +1,18 @@
+import { NavigationState } from "react-navigation";
 import { FormStateMap } from "redux-form";
 import { PersistPartial } from "redux-persist";
 
 import { Action } from "../actions/types";
 import { AppState } from "./appState";
-import { BackendInfoState } from "./backendInfo";
 import { DeeplinkState } from "./deeplink";
+import { EntitiesState } from "./entities";
 import { ErrorState } from "./error";
 import { LoadingState } from "./loading";
-import { NavigationState } from "./navigation";
 import { NotificationsState } from "./notifications";
 import { OnboardingState } from "./onboarding";
 import { PinLoginState } from "./pinlogin";
 import { ProfileState } from "./profile";
+import { PersistedSessionState } from "./session";
 import { WalletState } from "./wallet";
 
 export type NetworkState = Readonly<{
@@ -21,18 +22,18 @@ export type NetworkState = Readonly<{
 
 export type GlobalState = Readonly<{
   appState: AppState;
-  backendInfo: BackendInfoState;
   deeplink: DeeplinkState;
   entities: EntitiesState;
   error: ErrorState;
   form: FormStateMap;
   loading: LoadingState;
-  nav: NavigationState;
+  navigation: NavigationState;
   network: NetworkState;
   notifications: NotificationsState;
   onboarding: OnboardingState;
   pinlogin: PinLoginState;
   profile: ProfileState;
+  session: PersistedSessionState;
   wallet: WalletState;
 }>;
 
