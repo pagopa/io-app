988654f14aa89882146dacc7272c7eed5bcd43d1
Flavio Giobergia <flavio.giobergia@gmail.com>
2018-07-31 12:18:26 +0200

    minor lint fix

--- ts/navigation/AppNavigator.ts
+++ ts/navigation/AppNavigator.ts
@@ -1,11 +1,11 @@
 import { createSwitchNavigator } from "react-navigation";
 
+import IngressScreen from "../screens/IngressScreen";
 import AuthenticationNavigator from "./AuthenticationNavigator";
 import MainNavigator from "./MainNavigator";
 import OnboardingNavigator from "./OnboardingNavigator";
 import PinNavigator from "./PinNavigator";
 import ROUTES from "./routes";
-import IngressScreen from "../screens/IngressScreen";
 
 /**
  * The main stack of screens of the application.
