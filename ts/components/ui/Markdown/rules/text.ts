f07a61702a080eb32c22d8f524746bfc1a944ec0
Francesco Persico <fpersico@wellnet.it>
2018-08-01 18:04:51 +0200

    Fix the rendering of the Markdown

--- ts/components/ui/Markdown/rules/text.ts
+++ ts/components/ui/Markdown/rules/text.ts
@@ -5,35 +5,21 @@ import { ReactOutput, SingleASTNode, State } from "simple-markdown";
 import { makeReactNativeRule } from ".";
 
 function rule() {
-  return (
-    node: SingleASTNode,
-    _: ReactOutput,
-    state: State
-  ): React.ReactNode => {
-    // If we are inside a heading just return the plain content
-    if (state.withinHeading) {
-      return node.content;
-    }
-    // If we are inside a list add proper indentation and numbers/bullets
-    if (state.withinList) {
-      const listLevel = state.listLevel || 0;
-      const symbol = state.listOrdered ? `${state.position}.` : "*";
-      const content = `${" ".repeat(listLevel * 2)}${symbol} ${node.content}`;
+  return (node: SingleASTNode, _: ReactOutput, __: State): React.ReactNode => {
+    const words = node.content.split(" ");
+
+    const texts = words.map((word: any, i: number) => {
+      const text = i !== words.length - 1 ? `${word} ` : word;
       return React.createElement(
         Text,
         {
-          key: state.key
+          key: i
         },
-        content
+        text
       );
-    }
-    return React.createElement(
-      Text,
-      {
-        key: state.key
-      },
-      node.content
-    );
+    });
+
+    return texts;
   };
 }
 
