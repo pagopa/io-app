0dd174d0dff2f8a5379e582169a5789ace2e8899
VALENTINA Cala <valentina.cala@ibm.com>
2018-08-03 16:15:37 +0200

    add cards horizonal scroll view

---
+++ ts/components/HorizontalScroll.tsx
@@ -0,0 +1,102 @@
+/**
+ * This component allows to display a carousel with rounded indicators at the bottom
+ */
+import { View } from "native-base";
+import * as React from "react";
+import { Animated, Dimensions, ScrollView, StyleSheet } from "react-native";
+import { landingCardType } from "../screens/authentication/LandingScreen";
+import variables from "../theme/variables";
+
+export type Props = {
+  cardsContent: ReadonlyArray<landingCardType> | ReadonlyArray<any>;
+  cardsArray: ReadonlyArray<JSX.Element>;
+};
+
+const itemWidth = 10; // Radius of the indicators
+const screenWidth = Dimensions.get("screen").width;
+
+const styles = StyleSheet.create({
+  track: {
+    backgroundColor: variables.brandLightGray,
+    overflow: "hidden",
+    width: itemWidth,
+    height: itemWidth,
+    borderRadius: itemWidth / 2
+  },
+  bar: {
+    backgroundColor: variables.brandPrimary,
+    width: itemWidth,
+    height: itemWidth
+  },
+  scrollView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  barContainer: {
+    zIndex: itemWidth,
+    flexDirection: "row"
+  }
+});
+
+export class HorizontalScroll extends React.PureComponent<Props> {
+  private animVal = new Animated.Value(0);
+
+  private getBarArray(cards: ReadonlyArray<any>) {
+    const barArray: ReadonlyArray<JSX.Element> = [];
+
+    cards.forEach((_, i) => {
+      const scrollBarVal = this.animVal.interpolate({
+        inputRange: [screenWidth * (i - 1), screenWidth * (i + 1)],
+        outputRange: [-itemWidth, itemWidth],
+        extrapolate: "clamp"
+      });
+
+      const thisBar = (
+        <View
+          key={`bar${i}`}
+          style={[
+            styles.track,
+            {
+              marginLeft: i === 0 ? 0 : itemWidth
+            }
+          ]}
+        >
+          <Animated.View
+            style={[
+              styles.bar,
+              {
+                transform: [{ translateX: scrollBarVal }]
+              }
+            ]}
+          />
+        </View>
+      );
+      barArray.push(thisBar);
+    });
+
+    return barArray;
+  }
+
+  public render() {
+    return (
+      <View style={styles.scrollView}>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={this.props.cardsContent.length}
+          pagingEnabled={true}
+          onScroll={Animated.event([
+            { nativeEvent: { contentOffset: { x: this.animVal } } }
+          ])}
+        >
+          {this.props.cardsArray}
+        </ScrollView>
+
+        <View style={styles.barContainer}>
+          {this.getBarArray(this.props.cardsContent)}
+        </View>
+      </View>
+    );
+  }
+}
--- ts/screens/authentication/LandingScreen.tsx
+++ ts/screens/authentication/LandingScreen.tsx
@@ -1,9 +1,17 @@
+/**
+ * A screen where the user can choose to login with SPID or get more informations.
+ * It includes a carousel with highlights on the app functionalities
+ */
+
 import { Body, Button, Container, Content, Text, View } from "native-base";
 import * as React from "react";
+import { Animated, Dimensions, Image, StyleSheet } from "react-native";
 import DeviceInfo from "react-native-device-info";
+import { Col } from "react-native-easy-grid";
+import { Grid } from "react-native-easy-grid";
 import { NavigationScreenProp, NavigationState } from "react-navigation";
 import { connect } from "react-redux";
-
+import { HorizontalScroll } from "../../components/HorizontalScroll";
 import AppHeader from "../../components/ui/AppHeader";
 import IconFont from "../../components/ui/IconFont";
 import { environment } from "../../config";
@@ -17,10 +25,62 @@ type OwnProps = {
   navigation: NavigationScreenProp<NavigationState>;
 };
 type Props = ReduxMappedProps & ReduxProps & OwnProps;
-/**
- * A screen where the user can choose to login with SPID or get more informations.
- */
+
+export type landingCardType = {
+  id: number;
+  image: NodeRequire;
+  title: string;
+  content: string;
+};
+
+const cards: ReadonlyArray<landingCardType> = [
+  {
+    id: 1,
+    image: require("../../../img/landing/01.png"),
+    title: I18n.t("authentication.landing.card1-title"),
+    content: I18n.t("authentication.landing.card1-content")
+  },
+  {
+    id: 2,
+    image: require("../../../img/landing/02.png"),
+    title: I18n.t("authentication.landing.card2-title"),
+    content: I18n.t("authentication.landing.card2-content")
+  },
+  {
+    id: 3,
+    image: require("../../../img/landing/03.png"),
+    title: I18n.t("authentication.landing.card3-title"),
+    content: I18n.t("authentication.landing.card3-content")
+  },
+  {
+    id: 4,
+    image: require("../../../img/landing/04.png"),
+    title: I18n.t("authentication.landing.card4-title"),
+    content: I18n.t("authentication.landing.card4-content")
+  }
+];
+
+const screenWidth = Dimensions.get("screen").width;
+const BAR_SPACE = 10;
+
+const styles = StyleSheet.create({
+  card: {
+    width: screenWidth,
+    alignItems: "center",
+    alignContent: "flex-start"
+  },
+  image: {
+    width: screenWidth / 2,
+    height: screenWidth / 2,
+    resizeMode: "contain"
+  }
+});
+
 class LandingScreen extends React.Component<Props, never> {
+  public numItems = cards.length;
+  public itemWidth = BAR_SPACE;
+  public animVal = new Animated.Value(0);
+
   private navigateToMarkdown() {
     this.props.navigation.navigate(ROUTES.MARKDOWN);
   }
@@ -32,6 +92,34 @@ class LandingScreen extends React.Component<Props, never> {
   private navigateToSpidInformationRequest() {
     this.props.navigation.navigate(ROUTES.AUTHENTICATION_SPID_INFORMATION);
   }
+
+  private getCardArray() {
+    const cardArray: ReadonlyArray<JSX.Element> = [];
+    cards.forEach((card, i) => {
+      const thisCard = (
+        <View key={`card${i}`} style={styles.card}>
+          <Image source={card.image} style={styles.image} />
+          <View spacer={true} />
+          <Grid>
+            <Col size={1} />
+            <Col size={7}>
+              <Text bold={true} alignCenter={true}>
+                {" "}
+                {card.title}{" "}
+              </Text>
+              <View spacer={true} />
+              <Text alignCenter={true}> {card.content} </Text>
+              <View spacer={true} />
+            </Col>
+            <Col size={1} />
+          </Grid>
+        </View>
+      );
+      cardArray.push(thisCard);
+    });
+    return cardArray;
+  }
+
   public render() {
     return (
       <Container>
@@ -40,12 +128,17 @@ class LandingScreen extends React.Component<Props, never> {
             <Text>{DeviceInfo.getApplicationName()}</Text>
           </Body>
         </AppHeader>
-        <Content>
+        <Content noPadded={true}>
           {environment === "DEV" && (
             <Text link={true} onPress={_ => this.navigateToMarkdown()}>
               Test Markdown
             </Text>
           )}
+
+          <HorizontalScroll
+            cardsContent={cards}
+            cardsArray={this.getCardArray()}
+          />
         </Content>
         <View footer={true}>
           <Button
@@ -59,6 +152,7 @@ class LandingScreen extends React.Component<Props, never> {
             <Text>{I18n.t("authentication.landing.login")}</Text>
           </Button>
           <View spacer={true} />
+
           <Button
             block={true}
             small={true}
