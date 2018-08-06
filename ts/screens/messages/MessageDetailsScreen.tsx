65af855a96df87c7b9eb70fbb3d466928c05e4e4
Federico Feroldi <cloudify@users.noreply.github.com>
2018-08-06 10:31:56 +0200

    Cleans up messages logic removing unnecessary types (#371) [skip ci]

--- ts/screens/messages/MessageDetailsScreen.tsx
+++ ts/screens/messages/MessageDetailsScreen.tsx
@@ -7,27 +7,21 @@ import {
 } from "react-navigation";
 import { connect } from "react-redux";
 
-import { PaymentData } from "../../../definitions/backend/PaymentData";
+import I18n from "../../i18n";
+
 import MessageDetailsComponent from "../../components/messages/MessageDetailsComponent";
 import AppHeader from "../../components/ui/AppHeader";
 import IconFont from "../../components/ui/IconFont";
-import I18n from "../../i18n";
-import { startPayment } from "../../store/actions/messages";
 import { ReduxProps } from "../../store/actions/types";
 
-interface ParamTypeObject {
-  markdown: string;
-  paymentData: PaymentData;
-  serviceDepartmentName: string;
-  serviceName: string;
-  createdAt: string;
-  serviceOrganizationName: string;
-  subject: string;
-}
+import { MessageWithContentPO } from "../../types/MessageWithContentPO";
 
-interface ParamType {
-  readonly details: ParamTypeObject;
-}
+import { ServicePublic } from "../../../definitions/backend/ServicePublic";
+
+type ParamType = Readonly<{
+  message: MessageWithContentPO;
+  senderService: ServicePublic | undefined;
+}>;
 
 interface StateParams extends NavigationState {
   readonly params: ParamType;
@@ -47,20 +41,7 @@ export class MessageDetailsScreen extends React.Component<Props, never> {
     this.props.navigation.goBack();
   }
 
-  private handlePaymentCTA = (paymentData: PaymentData) => {
-    this.props.dispatch(startPayment(paymentData));
-  };
-
   public render() {
-    const {
-      createdAt,
-      markdown,
-      paymentData,
-      serviceDepartmentName,
-      serviceName,
-      serviceOrganizationName,
-      subject
-    } = this.props.navigation.state.params.details;
     return (
       <Container>
         <AppHeader>
@@ -75,14 +56,8 @@ export class MessageDetailsScreen extends React.Component<Props, never> {
         </AppHeader>
         <Content noPadded={true}>
           <MessageDetailsComponent
-            createdAt={createdAt}
-            markdown={markdown}
-            paymentData={paymentData}
-            serviceDepartmentName={serviceDepartmentName}
-            serviceName={serviceName}
-            serviceOrganizationName={serviceOrganizationName}
-            subject={subject}
-            onPaymentCTAClick={this.handlePaymentCTA}
+            message={this.props.navigation.state.params.message}
+            senderService={this.props.navigation.state.params.senderService}
           />
         </Content>
       </Container>
