47db7bac1e5745097f667c2d179bf46e6c02e48e
Flavio Giobergia <flavio.giobergia@gmail.com>
2018-08-29 15:44:44 +0200

    mocking TextInputMask

---
+++ ts/__mocks__/react-native-text-input-mask.ts
@@ -0,0 +1,12 @@
+/**
+ * Mocked version of TextInputMask
+ */
+import React from "react";
+
+class TextInputMask extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+export default TextInputMask;
