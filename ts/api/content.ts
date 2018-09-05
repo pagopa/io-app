010001e00a999b34f490f95035b8165b13cd40b5
Federico Feroldi <federico@teamdigitale.governo.it>
2018-09-05 19:05:27 +0200

    WIP

--- ts/api/content.ts
+++ ts/api/content.ts
@@ -3,33 +3,19 @@
  * content published at https://github.com/teamdigitale/italia-services-metadata
  */
 
-import * as t from "io-ts";
-
 import {
   basicResponseDecoder,
   BasicResponseType,
   createFetchRequestForApi,
   IGetApiRequestType
 } from "italia-ts-commons/lib/requests";
-import {
-  NonEmptyString,
-  OrganizationFiscalCode
-} from "italia-ts-commons/lib/strings";
+
+import { contentRepoUrl } from "../config";
 
 import { defaultRetryingFetch } from "../utils/fetch";
 
 import { ServiceId } from "../../definitions/backend/ServiceId";
-
-//
-// Definition of Service metadata
-//
-// TODO: generate from specs https://www.pivotaltracker.com/story/show/159440146
-
-const ServiceMetadata = t.partial({
-  description: NonEmptyString
-});
-
-export type ServiceMetadata = t.TypeOf<typeof ServiceMetadata>;
+import { Service as ServiceMetadata } from "../../definitions/content/Service";
 
 export type GetServiceT = IGetApiRequestType<
   {
@@ -48,79 +34,16 @@ const getServiceT: GetServiceT = {
   response_decoder: basicResponseDecoder(ServiceMetadata)
 };
 
-//
-// Definition of Organization metadata
-//
-// TODO: generate from specs https://www.pivotaltracker.com/story/show/159440146
-
-const OrganizationMetadata = t.partial({
-  cod_amm: t.string,
-  des_amm: t.string,
-  Comune: t.string,
-  nome_resp: t.string,
-  cogn_resp: t.string,
-  Cap: t.string,
-  Provincia: t.string,
-  Regione: t.string,
-  sito_istituzionale: t.string,
-  Indirizzo: t.string,
-  titolo_resp: t.string,
-  tipologia_istat: t.string,
-  tipologia_amm: t.string,
-  acronimo: t.string,
-  cf_validato: t.string,
-  Cf: t.string,
-  mail1: t.string,
-  tipo_mail1: t.string,
-  mail2: t.string,
-  tipo_mail2: t.string,
-  mail3: t.string,
-  tipo_mail3: t.string,
-  mail4: t.string,
-  tipo_mail4: t.string,
-  mail5: t.string,
-  tipo_mail5: t.string,
-  url_facebook: t.string,
-  url_twitter: t.string,
-  url_googleplus: t.string,
-  url_youtube: t.string,
-  liv_accessibili: t.string
-});
-
-export type OrganizationMetadata = t.TypeOf<typeof OrganizationMetadata>;
-
-export type GetOrganizationT = IGetApiRequestType<
-  {
-    organizationFiscalCode: OrganizationFiscalCode;
-  },
-  never,
-  never,
-  BasicResponseType<OrganizationMetadata>
->;
-
-const getOrganizationT: GetOrganizationT = {
-  method: "get",
-  url: params =>
-    `/amministrazioni/${params.organizationFiscalCode[0]}${
-      params.organizationFiscalCode[1]
-    }/${params.organizationFiscalCode}.json`,
-  query: _ => ({}),
-  headers: _ => ({}),
-  response_decoder: basicResponseDecoder(OrganizationMetadata)
-};
-
 /**
  * A client for the static content
  */
 export function ContentClient(fetchApi: typeof fetch = defaultRetryingFetch()) {
   const options = {
-    baseUrl:
-      "https://raw.githubusercontent.com/teamdigitale/italia-services-metadata/services-cards",
+    baseUrl: contentRepoUrl,
     fetchApi
   };
 
   return {
-    getService: createFetchRequestForApi(getServiceT, options),
-    getOrganization: createFetchRequestForApi(getOrganizationT, options)
+    getService: createFetchRequestForApi(getServiceT, options)
   };
 }
