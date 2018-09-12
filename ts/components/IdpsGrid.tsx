6dcc8a944a65a9e54d9e58c75531e24638ce1d97
Federico Feroldi <federico@teamdigitale.governo.it>
2018-09-12 16:33:16 +0200

    Simplifies IdpsGrid

--- ts/components/IdpsGrid.tsx
+++ ts/components/IdpsGrid.tsx
@@ -1,4 +1,4 @@
-import { Button, View } from "native-base";
+import { Button } from "native-base";
 import * as React from "react";
 import {
   Dimensions,
@@ -11,7 +11,7 @@ import {
 import { IdentityProvider } from "../models/IdentityProvider";
 import variables from "../theme/variables";
 
-export type OwnProps = {
+type OwnProps = {
   // Array of Identity Provider to show in the grid.
   idps: ReadonlyArray<IdentityProvider>;
   // A callback function called when an Identity Provider is selected
@@ -19,7 +19,7 @@ export type OwnProps = {
   testID?: string;
 };
 
-export type Props = OwnProps;
+type Props = OwnProps;
 
 const { width: windowWidth } = Dimensions.get("window");
 
@@ -31,10 +31,10 @@ const GRID_GUTTER = variables.gridGutter;
  */
 const styles = StyleSheet.create({
   gridItem: {
-    padding: GRID_GUTTER / 2,
+    margin: GRID_GUTTER / 2,
     // Calculate the real width of each item
 
-    width: (windowWidth - (2 * variables.contentPadding - 2 * GRID_GUTTER)) / 2
+    width: (windowWidth - (2 * variables.contentPadding + 2 * GRID_GUTTER)) / 2
   },
   idpLogo: {
     width: 120,
@@ -43,47 +43,40 @@ const styles = StyleSheet.create({
   }
 });
 
+const keyExtractor = (idp: IdentityProvider): string => idp.id;
+
+const renderItem = (props: Props) => (
+  info: ListRenderItemInfo<IdentityProvider>
+): React.ReactElement<any> => {
+  const { onIdpSelected } = props;
+  const { item } = info;
+  const onPress = () => onIdpSelected(item);
+  return (
+    <Button
+      key={item.id}
+      style={styles.gridItem}
+      transparent={true}
+      block={true}
+      white={true}
+      onPress={onPress}
+      testID={`idp-${item.id}-button`}
+    >
+      <Image source={item.logo} style={styles.idpLogo} />
+    </Button>
+  );
+};
+
 /**
  * A component that show a Grid with every Identity Provider passed in the idps
  * array property. When an Identity Provider is selected a callback function is called.
  */
-class IdpsGrid extends React.Component<Props> {
-  public render() {
-    const { idps } = this.props;
-    return (
-      <FlatList
-        data={idps}
-        numColumns={2}
-        keyExtractor={this.keyExtractor}
-        renderItem={this.renderItem}
-      />
-    );
-  }
-
-  private keyExtractor = (idp: IdentityProvider): string => {
-    return idp.id;
-  };
-
-  private renderItem = (
-    info: ListRenderItemInfo<IdentityProvider>
-  ): React.ReactElement<any> => {
-    const { onIdpSelected } = this.props;
-    const { item } = info;
-    const onPress = () => onIdpSelected(item);
-    return (
-      <View key={item.id} style={styles.gridItem}>
-        <Button
-          transparent={true}
-          block={true}
-          white={true}
-          onPress={onPress}
-          testID={`idp-${item.id}-button`}
-        >
-          <Image source={item.logo} style={styles.idpLogo} />
-        </Button>
-      </View>
-    );
-  };
-}
+const IdpsGrid: React.SFC<Props> = props => (
+  <FlatList
+    data={props.idps}
+    numColumns={2}
+    keyExtractor={keyExtractor}
+    renderItem={renderItem(props)}
+  />
+);
 
 export default IdpsGrid;
