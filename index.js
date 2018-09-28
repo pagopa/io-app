68c75980ef39b97d02c69aa3fe31b4f1c70932e5
DenisFrezzato <frezzato.denis@gmail.com>
2018-09-28 09:24:18 +0200

    Remove react-native-exception-handler.

--- index.js
+++ index.js
@@ -4,44 +4,10 @@
 
 import { AppRegistry } from "react-native";
 import Config from "react-native-config";
-import {
-  setJSExceptionHandler,
-  setNativeExceptionHandler
-} from "react-native-exception-handler";
-import DeviceInfo from "react-native-device-info";
-import { mixpanel } from "./ts/mixpanel";
+import Mixpanel from "react-native-mixpanel";
 
-const errorHandler = (e, isFatal) => {
-  if (isFatal) {
-    if (mixpanel) {
-      mixpanel.track("APPLICATION_ERROR", {
-        TYPE: "js",
-        ERROR: JSON.stringify(e),
-        APP_VERSION: DeviceInfo.getReadableVersion()
-      });
-    }
-    Alert.alert(
-      "Unexpected error occurred",
-      `
-        Error: ${isFatal ? "Fatal:" : ""} ${e.name} ${e.message}
-        You will need to restart the app.
-        `
-    );
-  } else {
-    console.log(e); // So that we can see it in the ADB logs in case of Android if needed
-  }
-};
-
-setJSExceptionHandler(errorHandler);
-setNativeExceptionHandler(exceptionString => {
-  if (mixpanel) {
-    mixpanel.track("APPLICATION_ERROR", {
-      TYPE: "native",
-      ERROR: exceptionString,
-      APP_VERSION: DeviceInfo.getReadableVersion()
-    });
-  }
-});
+// Configure Mixpanel session
+Mixpanel.sharedInstanceWithToken(Config.MIXPANEL_TOKEN);
 
 import { App } from "./ts/App";
 
