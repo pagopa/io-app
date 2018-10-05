628c98f2cc89d0949ca8c52f91e868ad4b0646d0
Federico Feroldi <federico@teamdigitale.governo.it>
2018-10-05 09:00:21 +0200

    Extracts token functions to utils

--- ts/sagas/wallet.ts
+++ ts/sagas/wallet.ts
@@ -9,7 +9,6 @@ import { none, Option, some } from "fp-ts/lib/Option";
 import { RptIdFromString } from "italia-ts-commons/lib/pagopa";
 import { AmountInEuroCents, RptId } from "italia-ts-commons/lib/pagopa";
 import {
-  IResponseType,
   TypeofApiCall,
   TypeofApiResponse
 } from "italia-ts-commons/lib/requests";
@@ -41,7 +40,6 @@ import I18n from "../i18n";
 import ROUTES from "../navigation/routes";
 
 import { logoutSuccess } from "../store/actions/authentication";
-import { storePagoPaToken } from "../store/actions/wallet/pagopa";
 import {
   paymentCompleted,
   paymentConfirmPaymentMethod,
@@ -137,75 +135,14 @@ import { constantPollingFetch, pagopaFetch } from "../utils/fetch";
 import { loginWithPinSaga } from "./startup/pinLoginSaga";
 import { watchPinResetSaga } from "./startup/watchPinResetSaga";
 
-// allow refreshing token this number of times
-const MAX_TOKEN_REFRESHES = 2;
+import {
+  fetchAndStorePagoPaToken,
+  fetchWithTokenRefresh
+} from "./wallet/utils";
 
 const navigateTo = (routeName: string, params?: object) => {
   return NavigationActions.navigate({ routeName, params });
 };
-
-// this function tries to carry out the provided
-// request, and refreshes the pagoPA token if a 401
-// is returned. Upon refreshing, it tries to
-// re-fetch the contents again for a maximum of
-// MAX_TOKEN_REFRESHES times.
-// If the request is successful (i.e. it does not
-// return a 401), the successful token is returned
-// along with the response (the caller will then
-// decide what to do with it)
-function* fetchWithTokenRefresh<T>(
-  request: (
-    pagoPaToken: string
-  ) => Promise<IResponseType<401, undefined> | undefined | T>,
-  pagoPaClient: PagoPaClient,
-  retries: number = MAX_TOKEN_REFRESHES
-): Iterator<T | undefined | Effect> {
-  if (retries === 0) {
-    return undefined;
-  }
-  const pagoPaToken: Option<string> = yield select(getPagoPaToken);
-  const response: SagaCallReturnType<typeof request> = yield call(
-    request,
-    pagoPaToken.getOrElse("") // empty token -> pagoPA returns a 401 and the app fetches a new one
-  );
-  if (response !== undefined) {
-    // BEWARE: since there is not an easy way to restrict T to an arbitrary union
-    // of IResponseType(s), we kind of take a leap of faith here and assume that T
-    // is always a union of IResponseType(s) together with undefined.
-    if ((response as any).status !== 401) {
-      // return code is not 401, the token
-      // has been "accepted" (the caller will
-      // then handle other error codes)
-      return response;
-    } else {
-      yield call(fetchAndStorePagoPaToken, pagoPaClient);
-
-      // and retry fetching the result
-      return yield call(
-        fetchWithTokenRefresh,
-        request,
-        pagoPaClient,
-        retries - 1
-      );
-    }
-  }
-  return undefined;
-}
-
-function* fetchAndStorePagoPaToken(pagoPaClient: PagoPaClient) {
-  const refreshTokenResponse: SagaCallReturnType<
-    typeof pagoPaClient.getSession
-  > = yield call(pagoPaClient.getSession, pagoPaClient.walletToken);
-  if (
-    refreshTokenResponse !== undefined &&
-    refreshTokenResponse.status === 200
-  ) {
-    // token fetched successfully, store it
-    yield put(
-      storePagoPaToken(some(refreshTokenResponse.value.data.sessionToken))
-    );
-  }
-}
 
 function* fetchTransactions(pagoPaClient: PagoPaClient): Iterator<Effect> {
   const response:
---
+++ ts/sagas/wallet/utils.ts
@@ -0,0 +1,76 @@
+import { Option, some } from "fp-ts/lib/Option";
+import { IResponseType } from "italia-ts-commons/lib/requests";
+import { call, Effect, put, select } from "redux-saga/effects";
+
+import { PagoPaClient } from "../../api/pagopa";
+
+import { storePagoPaToken } from "../../store/actions/wallet/pagopa";
+import { getPagoPaToken } from "../../store/reducers/wallet/pagopa";
+
+import { SagaCallReturnType } from "../../types/utils";
+
+// allow refreshing token this number of times
+const MAX_TOKEN_REFRESHES = 2;
+
+export function* fetchAndStorePagoPaToken(pagoPaClient: PagoPaClient) {
+  const refreshTokenResponse: SagaCallReturnType<
+    typeof pagoPaClient.getSession
+  > = yield call(pagoPaClient.getSession, pagoPaClient.walletToken);
+  if (
+    refreshTokenResponse !== undefined &&
+    refreshTokenResponse.status === 200
+  ) {
+    // token fetched successfully, store it
+    yield put(
+      storePagoPaToken(some(refreshTokenResponse.value.data.sessionToken))
+    );
+  }
+}
+
+// this function tries to carry out the provided
+// request, and refreshes the pagoPA token if a 401
+// is returned. Upon refreshing, it tries to
+// re-fetch the contents again for a maximum of
+// MAX_TOKEN_REFRESHES times.
+// If the request is successful (i.e. it does not
+// return a 401), the successful token is returned
+// along with the response (the caller will then
+// decide what to do with it)
+export function* fetchWithTokenRefresh<T>(
+  request: (
+    pagoPaToken: string
+  ) => Promise<IResponseType<401, undefined> | undefined | T>,
+  pagoPaClient: PagoPaClient,
+  retries: number = MAX_TOKEN_REFRESHES
+): Iterator<T | undefined | Effect> {
+  if (retries === 0) {
+    return undefined;
+  }
+  const pagoPaToken: Option<string> = yield select(getPagoPaToken);
+  const response: SagaCallReturnType<typeof request> = yield call(
+    request,
+    pagoPaToken.getOrElse("") // empty token -> pagoPA returns a 401 and the app fetches a new one
+  );
+  if (response !== undefined) {
+    // BEWARE: since there is not an easy way to restrict T to an arbitrary union
+    // of IResponseType(s), we kind of take a leap of faith here and assume that T
+    // is always a union of IResponseType(s) together with undefined.
+    if ((response as any).status !== 401) {
+      // return code is not 401, the token
+      // has been "accepted" (the caller will
+      // then handle other error codes)
+      return response;
+    } else {
+      yield call(fetchAndStorePagoPaToken, pagoPaClient);
+
+      // and retry fetching the result
+      return yield call(
+        fetchWithTokenRefresh,
+        request,
+        pagoPaClient,
+        retries - 1
+      );
+    }
+  }
+  return undefined;
+}
