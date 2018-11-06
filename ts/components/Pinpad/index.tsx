13a016b381c17cd098c63859e60c75cdb8af74fc
Federico Feroldi <federico@teamdigitale.governo.it>
2018-11-06 07:14:33 +0100

    Do not update keypad when typing the PIN
    Rendering time is now 1/5 of before

--- ts/components/Pinpad/index.tsx
+++ ts/components/Pinpad/index.tsx
@@ -1,12 +1,14 @@
-import { ITuple3, Tuple3 } from "italia-ts-commons/lib/tuples";
-import { Button, Col, Grid, Row, Text, View } from "native-base";
+import { Tuple2 } from "italia-ts-commons/lib/tuples";
+import { View } from "native-base";
 import * as React from "react";
 
 import I18n from "../../i18n";
+import { ComponentProps } from "../../types/react";
 
 import { PinString } from "../../types/PinString";
 import { PIN_LENGTH } from "../../utils/constants";
 
+import { KeyPad } from "./KeyPad";
 import { styles } from "./Pinpad.style";
 import { Baseline, Bullet } from "./Placeholders";
 
@@ -15,7 +17,7 @@ interface Props {
   clearOnInvalid?: boolean;
   compareWithCode?: string;
   inactiveColor: string;
-  buttonType: "primary" | "light";
+  buttonType: ComponentProps<typeof KeyPad>["buttonType"];
   onFulfill: (code: PinString, isValid: boolean) => void;
   onCancel?: () => void;
 }
@@ -33,6 +35,42 @@ class Pinpad extends React.PureComponent<Props, State> {
   // Utility array of as many elements as how many digits the pin has.
   // Its map method will be used to render the pin's placeholders.
   private placeholderPositions: ReadonlyArray<undefined>;
+
+  private deleteLastDigit = () =>
+    this.setState(prev => ({
+      value:
+        prev.value.length > 0
+          ? prev.value.slice(0, prev.value.length - 1)
+          : prev.value
+    }));
+
+  private pinPadDigits: ComponentProps<typeof KeyPad>["digits"] = [
+    [
+      Tuple2("1", () => this.handlePinDigit("1")),
+      Tuple2("2", () => this.handlePinDigit("2")),
+      Tuple2("3", () => this.handlePinDigit("3"))
+    ],
+    [
+      Tuple2("4", () => this.handlePinDigit("4")),
+      Tuple2("5", () => this.handlePinDigit("5")),
+      Tuple2("6", () => this.handlePinDigit("6"))
+    ],
+    [
+      Tuple2("7", () => this.handlePinDigit("7")),
+      Tuple2("8", () => this.handlePinDigit("8")),
+      Tuple2("9", () => this.handlePinDigit("9"))
+    ],
+    [
+      this.props.onCancel
+        ? Tuple2(
+            I18n.t("global.buttons.cancel").toUpperCase(),
+            this.props.onCancel
+          )
+        : undefined,
+      Tuple2("0", () => this.handlePinDigit("0")),
+      Tuple2("<", this.deleteLastDigit)
+    ]
+  ];
 
   constructor(props: Props) {
     super(props);
@@ -70,16 +108,8 @@ class Pinpad extends React.PureComponent<Props, State> {
     }
   };
 
-  private handlePinDigit = (digit: number) =>
+  private handlePinDigit = (digit: string) =>
     this.handleChangeText(`${this.state.value}${digit}`);
-
-  private deleteLastDigit = () =>
-    this.setState(prev => ({
-      value:
-        prev.value.length > 0
-          ? prev.value.slice(0, prev.value.length - 1)
-          : prev.value
-    }));
 
   private renderPlaceholder = (_: undefined, i: number) => {
     const isPlaceholderPopulated = i <= this.state.value.length - 1;
@@ -92,64 +122,6 @@ class Pinpad extends React.PureComponent<Props, State> {
     );
   };
 
-  private renderPinCol = (
-    digit: number,
-    label: string,
-    handler: (digit: number) => void,
-    style: "digit" | "label"
-  ) => {
-    return (
-      <Col key={`pinpad-digit-${digit}-${label}`}>
-        <Button
-          onPress={() => handler(digit)}
-          style={style === "digit" ? styles.roundButton : {}}
-          transparent={style === "label"}
-          block={style === "label"}
-          primary={this.props.buttonType === "primary"}
-          light={this.props.buttonType === "light"}
-        >
-          <Text
-            style={[
-              styles.buttonTextBase,
-              style === "digit"
-                ? styles.buttonTextDigit
-                : styles.buttonTextLabel,
-              style === "label" && this.props.buttonType === "primary"
-                ? {
-                    color: "white"
-                  }
-                : {}
-            ]}
-          >
-            {label}
-          </Text>
-        </Button>
-      </Col>
-    );
-  };
-
-  private renderPinRow = (
-    digits: ReadonlyArray<
-      ITuple3<number, string, (digit: number) => void> | undefined
-    >
-  ) => (
-    <Row>
-      {digits.map(
-        (el, i) =>
-          el ? (
-            this.renderPinCol(
-              el.e1,
-              el.e2,
-              el.e3,
-              el.e2.length === 1 ? "digit" : "label"
-            )
-          ) : (
-            <Col key={`pinpad-empty-${i}`} />
-          )
-      )}
-    </Row>
-  );
-
   public clear = () => this.setState({ value: "" });
 
   public render() {
@@ -159,34 +131,7 @@ class Pinpad extends React.PureComponent<Props, State> {
           {this.placeholderPositions.map(this.renderPlaceholder)}
         </View>
         <View spacer={true} extralarge={true} />
-        <Grid>
-          {this.renderPinRow([
-            Tuple3(1, "1", this.handlePinDigit),
-            Tuple3(2, "2", this.handlePinDigit),
-            Tuple3(3, "3", this.handlePinDigit)
-          ])}
-          {this.renderPinRow([
-            Tuple3(4, "4", this.handlePinDigit),
-            Tuple3(5, "5", this.handlePinDigit),
-            Tuple3(6, "6", this.handlePinDigit)
-          ])}
-          {this.renderPinRow([
-            Tuple3(7, "7", this.handlePinDigit),
-            Tuple3(8, "8", this.handlePinDigit),
-            Tuple3(9, "9", this.handlePinDigit)
-          ])}
-          {this.renderPinRow([
-            this.props.onCancel
-              ? Tuple3(
-                  0,
-                  I18n.t("global.buttons.cancel").toUpperCase(),
-                  this.props.onCancel
-                )
-              : undefined,
-            Tuple3(10, "0", this.handlePinDigit),
-            Tuple3(11, "<", this.deleteLastDigit)
-          ])}
-        </Grid>
+        <KeyPad digits={this.pinPadDigits} buttonType={this.props.buttonType} />
       </React.Fragment>
     );
   }
