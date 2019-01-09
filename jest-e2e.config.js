9dd066258a421e372e7db8b0066fe3841cd687a3
Francesco Persico <fpersico@wellnet.it>
2019-01-09 10:49:02 +0100

    Attempt to fix e2e tests

---
+++ jest-e2e.config.js
@@ -0,0 +1,23 @@
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+module.exports = {
+  ...tsjPreset,
+  preset: 'react-native',
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!react-native|react-navigation|react-native-device-info|native-base|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui)"
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    }
+  },
+  testMatch: [
+    "**/__e2e__/*.e2e.ts?(x)"
+  ],
+  forceExit: true,
+  verbose: true
+};
