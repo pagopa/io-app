3602a938cefc372547d82223d730c0f3e85470c9
Francesco Persico <fpersico@wellnet.it>
2019-03-12 16:31:22 +0100

    Add bottom padding to Message detail (#872)

--- ts/components/messages/MessageDetailComponent.tsx
+++ ts/components/messages/MessageDetailComponent.tsx
@@ -26,6 +26,10 @@ type OwnProps = {
 type Props = OwnProps;
 
 const styles = StyleSheet.create({
+  mainWrapper: {
+    paddingBottom: 100
+  },
+
   headerContainer: {
     padding: variables.contentPadding
   },
@@ -61,7 +65,7 @@ export default class MessageDetailComponent extends React.PureComponent<Props> {
   public render() {
     const { message, paymentByRptId, service, onServiceLinkPress } = this.props;
     return (
-      <View>
+      <View style={styles.mainWrapper}>
         <View style={styles.headerContainer}>
           {/* Service */}
           {pot.isSome(service) && (
