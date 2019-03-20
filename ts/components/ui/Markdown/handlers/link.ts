4d58e14f0b51f32577e577a8e3e48172d687c593
Francesco Persico <fpersico@wellnet.it>
2019-03-20 18:56:29 +0100

    Add function documentation

--- ts/components/ui/Markdown/handlers/link.ts
+++ ts/components/ui/Markdown/handlers/link.ts
@@ -3,10 +3,16 @@ import { Linking } from "react-native";
 import { Dispatch } from "../../../../store/actions/types";
 import { handleInternalLink, IO_INTERNAL_LINK_PREFIX } from "./internalLink";
 
+/**
+ * Handles links clicked in the Markdown (webview) component.
+ * Internal links handling is demanded to the `handleInternalLink` function.
+ */
 export function handleLinkMessage(dispatch: Dispatch, href: string) {
   if (href.startsWith(IO_INTERNAL_LINK_PREFIX)) {
     handleInternalLink(dispatch, href);
   } else {
+    // External urls must be opened with the OS browser.
+    // FIXME: Whitelist allowed domains: https://www.pivotaltracker.com/story/show/158470128
     Linking.openURL(href).catch(() => 0);
   }
 }
