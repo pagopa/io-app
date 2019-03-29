5339d32548ca9e149469c014ea8316cb4918d199
Francesco Persico <fpersico@wellnet.it>
2019-03-29 18:07:57 +0100

    First commit

--- ts/components/messages/MessagesDeadlines.tsx
+++ ts/components/messages/MessagesDeadlines.tsx
@@ -1,17 +1,17 @@
-import { compareAsc, startOfDay } from "date-fns";
-import { none, Option, some } from "fp-ts/lib/Option";
-import * as pot from "italia-ts-commons/lib/pot";
-import { View } from "native-base";
-import React, { ComponentProps } from "react";
-import { StyleSheet } from "react-native";
+import { compareAsc, differenceInCalendarMonths, startOfDay } from 'date-fns';
+import { none, Option, some } from 'fp-ts/lib/Option';
+import * as pot from 'italia-ts-commons/lib/pot';
+import { View } from 'native-base';
+import React, { ComponentProps } from 'react';
+import { StyleSheet } from 'react-native';
 
-import { lexicallyOrderedMessagesStateSelector } from "../../store/reducers/entities/messages";
-import { MessageState } from "../../store/reducers/entities/messages/messagesById";
+import { lexicallyOrderedMessagesStateSelector } from '../../store/reducers/entities/messages';
+import { MessageState } from '../../store/reducers/entities/messages/messagesById';
 import {
   isMessageWithContentAndDueDatePO,
-  MessageWithContentAndDueDatePO
-} from "../../types/MessageWithContentAndDueDatePO";
-import MessageAgenda, { MessageAgendaSection } from "./MessageAgenda";
+  MessageWithContentAndDueDatePO,
+} from '../../types/MessageWithContentAndDueDatePO';
+import MessageAgenda, { MessageAgendaSection } from './MessageAgenda';
 
 const styles = StyleSheet.create({
   listWrapper: {
@@ -41,6 +41,15 @@ type OwnProps = {
 };
 
 type Props = Pick<ComponentProps<typeof MessageAgenda>, "onRefresh"> & OwnProps;
+
+type State = {
+  isProcessing: boolean;
+  // How many months back to consider when showing the messages with deadline
+  monthsBack: number;
+  sections: ReturnType<typeof generateSections>;
+  sectionsToRender: ReturnType<typeof generateSections>;
+  lastMessagesState?: pot.Pot<ReadonlyArray<MessageState>, string>;
+};
 
 /**
  * Filter only the messages with a due date and group them by due_date day.
@@ -124,30 +133,147 @@ const generateSections = (
     []
   );
 
+const calculateInitialSectionsToRender = (
+  sections: ReturnType<typeof generateSections>,
+  monthsBack: number
+) => {
+  // Get the current date
+  const currentDate = new Date();
+
+  // Find the first section with a date that is `monthsBack` back.
+  const initialSectionsIndex = sections.findIndex(section => {
+    const months = differenceInCalendarMonths(
+      currentDate,
+      new Date(section.title)
+    );
+    return months <= monthsBack;
+  });
+
+  return initialSectionsIndex < 0 ? [] : sections.slice(initialSectionsIndex);
+};
+
+const calculateNewSectionsToRenderWithNewMonthsBack = (
+  sections: ReturnType<typeof generateSections>,
+  initialMonthsBack: number
+) => {
+  const currentDate = new Date();
+
+  // tslint:disable-next-line: no-let
+  let newInitialSectionIndex = -1;
+  // tslint:disable-next-line: no-let
+  let newMonthsBack = initialMonthsBack;
+
+  if (
+    differenceInCalendarMonths(currentDate, new Date(sections[0].title)) <
+    newMonthsBack
+  ) {
+    return none;
+  }
+
+  while (newInitialSectionIndex < 0) {
+    // Find the first section with a date that is `monthsBack` back.
+    newInitialSectionIndex = sections.findIndex(section => {
+      const months = differenceInCalendarMonths(
+        currentDate,
+        new Date(section.title)
+      );
+      return months <= newMonthsBack;
+    });
+    newMonthsBack++;
+  }
+
+  return some({
+    newSectionsToRender: sections.slice(newInitialSectionIndex),
+    newMonthsBack: newMonthsBack + 1
+  });
+};
+
 /**
  * A component to show the messages with a due_date.
  */
-class MessagesDeadlines extends React.PureComponent<Props, never> {
-  public render() {
-    const { messagesState, onRefresh } = this.props;
-    const isLoading = pot.isLoading(messagesState);
+class MessagesDeadlines extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isProcessing: false,
+      monthsBack: 0,
+      sections: [],
+      sectionsToRender: []
+    };
+  }
 
-    const sections = generateSections(messagesState);
+  public static getDerivedStateFromProps(
+    nextProps: Props,
+    prevState: State
+  ): Partial<State> | null {
+    // Calculate sections only if messagesState is changed
+    const sections =
+      nextProps.messagesState !== prevState.lastMessagesState
+        ? generateSections(nextProps.messagesState)
+        : prevState.sections;
+
+    const sectionsToRender =
+      sections !== prevState.sections
+        ? calculateInitialSectionsToRender(sections, prevState.monthsBack)
+        : prevState.sectionsToRender;
+
+    return {
+      sections,
+      sectionsToRender,
+      lastMessagesState: nextProps.messagesState
+    };
+  }
+
+  public render() {
+    const { messagesState } = this.props;
+    const { isProcessing, sectionsToRender } = this.state;
+
+    const isLoading = pot.isLoading(messagesState) || isProcessing;
 
     return (
       <View style={styles.listWrapper}>
         <MessageAgenda
-          sections={sections}
           isRefreshing={isLoading}
-          onRefresh={onRefresh}
+          sections={sectionsToRender}
+          onRefresh={this.onRefresh}
           onPressItem={this.handleOnPressItem}
         />
       </View>
     );
   }
 
+  private onRefresh = () => {
+    this.setState({
+      isProcessing: true
+    });
+    this.loadPreviousData().then(
+      _ => {
+        this.setState({
+          isProcessing: false,
+          monthsBack: _.newMonthsBack,
+          sectionsToRender: _.newSectionsToRender
+        });
+      },
+      _ => this.setState({ isProcessing: false, monthsBack: _.newMonthsBack })
+    );
+  };
+
   private handleOnPressItem = (id: string) => {
     this.props.navigateToMessageDetail(id);
+  };
+
+  private loadPreviousData = (): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      const { monthsBack, sections } = this.state;
+      const maybeNewSectionsToRenderWithNewMonthsBack = calculateNewSectionsToRenderWithNewMonthsBack(
+        sections,
+        monthsBack + 1
+      );
+      if (maybeNewSectionsToRenderWithNewMonthsBack.isSome()) {
+        resolve(maybeNewSectionsToRenderWithNewMonthsBack.value);
+      }
+      reject({ newMonthsBack: monthsBack + 1 });
+    });
   };
 }
 
