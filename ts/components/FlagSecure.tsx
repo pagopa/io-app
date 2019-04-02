7d80b803bc1f369173e55e9521f38c44ed859dc7
Francesco Persico <fpersico@wellnet.it>
2019-04-02 12:25:33 +0200

    [#164630993] Enable FLAG_SECURE when debugmode is disabled (#917)
    * Enable FLAG_SECURE when debugmode is disabled
    
    * Add FlagSecureComponent component
    
    * Update comments
    
    * Add comment for FlagSecure component
    
    * Update comment for FlagSecure component
    
    * Add another comment for FlagSecure component
    
    * Render FlagSecure component only on Android

---
+++ ts/components/FlagSecure.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import FlagSecure from "react-native-flag-secure-android";
+
+type Props = {
+  isFlagSecureEnabled: boolean;
+};
+
+/**
+ * On Android, it enables or disables FLAG_SECURE based on the isFlagSecureEnabled prop.
+ * On unmount, FLAG_SECURE gets disabled if it was enabled.
+ */
+class FlagSecureComponent extends React.PureComponent<Props> {
+  public render() {
+    return null;
+  }
+
+  public componentDidMount() {
+    // Enable FLAG_SECURE when requested.
+    if (this.props.isFlagSecureEnabled) {
+      FlagSecure.activate();
+    }
+  }
+
+  public componentDidUpdate() {
+    // When isFlagSecureEnabled property changes we need update FLAG_SECURE.
+    this.props.isFlagSecureEnabled
+      ? FlagSecure.activate()
+      : FlagSecure.deactivate();
+  }
+}
+
+export default FlagSecureComponent;
