53ed70d2f16b8c278932e0cf44da16a7878b6430
Francesco Persico <fpersico@wellnet.it>
2019-04-11 11:16:06 +0200

    Remove contextual help from PreferencesScreen

--- ts/screens/preferences/PreferencesScreen.tsx
+++ ts/screens/preferences/PreferencesScreen.tsx
@@ -120,11 +120,6 @@ class PreferencesScreen extends React.Component<Props, State> {
   };
 
   public render() {
-    const contextualHelp = {
-      title: I18n.t("preferences.title"),
-      body: () => <Markdown>{I18n.t("preferences.preferencesHelp")}</Markdown>
-    };
-
     const { potProfile } = this.props;
     const { hasCalendarPermission, isFingerprintAvailable } = this.state;
 
@@ -148,7 +143,6 @@ class PreferencesScreen extends React.Component<Props, State> {
         title={I18n.t("preferences.title")}
         icon={require("../../../img/icons/gears.png")}
         subtitle={I18n.t("preferences.subtitle")}
-        contextualHelp={contextualHelp}
       >
         <Content noPadded={true}>
           <List withContentLateralPadding={true}>
