1bd45d6c8f3c6010391ede8f21fb3fa94e9d7cd2
Francesco Persico <francesco.persico@wellnet.it>
2019-04-17 10:09:28 +0200

    Add messagesIdsByServiceId

---
+++ ts/store/reducers/entities/messages/messagesIdsByServiceId.ts
@@ -0,0 +1,36 @@
+import { getType } from "typesafe-actions";
+
+import { loadMessage } from "../../../actions/messages";
+import { Action } from "../../../actions/types";
+
+export type MessagesIdsByServiceId = Readonly<{
+  [key: string]: ReadonlyArray<string> | undefined;
+}>;
+
+export const INITIAL_STATE: MessagesIdsByServiceId = {};
+
+const messagesIdsByServiceIdReducer = (
+  state: MessagesIdsByServiceId = INITIAL_STATE,
+  action: Action
+): MessagesIdsByServiceId => {
+  switch (action.type) {
+    case getType(loadMessage.success): {
+      const message = action.payload;
+      const serviceId = message.sender_service_id;
+      const messagesIds = state[serviceId];
+      const newMessagesIds =
+        messagesIds === undefined
+          ? [message.id]
+          : messagesIds.concat(message.id);
+      return {
+        ...state,
+        [serviceId]: newMessagesIds
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default messagesIdsByServiceIdReducer;
