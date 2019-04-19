0ddf275318844c1ed3c77779b421b4999d62e669
Francesco Persico <francescopersico@gmail.com>
2019-04-19 17:30:23 +0200

    Add component description

--- ts/components/screens/ScreenContent.tsx
+++ ts/components/screens/ScreenContent.tsx
@@ -3,6 +3,10 @@ import React, { ComponentPropsWithRef } from "react";
 
 type Props = ComponentPropsWithRef<typeof Content>;
 
+/**
+ * Wrapper for the native-base Content component that
+ * properly sets default content padding.
+ */
 class ScreenContent extends React.Component<Props> {
   public render() {
     const { noPadded } = this.props;
