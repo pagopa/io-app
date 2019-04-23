f9fcada4dd3938c5018b468880c515c1c7e490c6
Luca Morelli <luca.morelli@wellnet.it>
2019-04-23 17:18:28 +0200

    [#165410566] Add comment highlighting correlation between "checkAndroid6Permissions" and "notAuthorizedView" for Android systems

--- ts/screens/wallet/payment/ScanQrCodeScreen.tsx
+++ ts/screens/wallet/payment/ScanQrCodeScreen.tsx
@@ -201,6 +201,8 @@ class ScanQrCodeScreen extends React.Component<Props, State> {
                 </View>
               }
               cameraProps={{ ratio: "1:1" }}
+              // "checkAndroid6Permissions" property enables permission checking for
+              // Android versions greater than 6.0 (23+).
               checkAndroid6Permissions={true}
               permissionDialogTitle={I18n.t(
                 "wallet.QRtoPay.cameraUsagePerissionInfobox.title"
@@ -208,6 +210,12 @@ class ScanQrCodeScreen extends React.Component<Props, State> {
               permissionDialogMessage={I18n.t(
                 "wallet.QRtoPay.cameraUsagePerissionInfobox.message"
               )}
+              // "notAuthorizedView" is by defatult available on iOS systems ONLY.
+              // In order to make Android systems act the same as iOSs you MUST
+              // enable "checkAndroid6Permissions" property as well.
+              // On devices before SDK version 23, the permissions are automatically
+              // granted if they appear in the manifest, so message customization would
+              // be impossible.
               notAuthorizedView={
                 <View
                   style={{
