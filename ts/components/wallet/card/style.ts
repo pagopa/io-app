4affaa10d76a0635ba2f6555367fe5ab77ac8bd5
vcala <valentina.cala@ibm.com>
2019-05-08 15:32:43 +0200

    revert(3)

---
+++ ts/components/wallet/card/style.ts
@@ -0,0 +1,21 @@
+import { Platform, StyleSheet } from "react-native";
+import { makeFontStyleObject } from "../../../theme/fonts";
+import variables from "../../../theme/variables";
+
+export const CreditCardStyles = StyleSheet.create({
+  largeTextStyle: {
+    ...makeFontStyleObject(Platform.select, undefined, false, "RobotoMono"),
+    fontSize: variables.fontSizeBase * 1.125 // 18
+  },
+  rowStyle: {
+    alignItems: "center"
+  },
+  textStyle: {
+    fontFamily: variables.fontFamily,
+    color: variables.cardFontColor
+  },
+  smallTextStyle: {
+    fontSize: variables.fontSizeSmall,
+    color: variables.brandDarkGray
+  }
+});
