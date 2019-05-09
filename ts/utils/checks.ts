83575d4276ded3f531418d038d94cd09400250b8
Luca Melandri <lucamelandri0@gmail.com>
2019-05-09 10:43:32 +0200

    Added lint + prettify

--- ts/utils/checks.ts
+++ ts/utils/checks.ts
@@ -6,17 +6,17 @@ import { Wallet } from "../types/pagopa";
  */
 
 export const isExpiredCard = (w: Wallet) => {
-    const today: Date = new Date();
-    const cmpM: number = today.getMonth() + 1;
-    const cmpY: number = parseInt(
-      today
-        .getFullYear()
-        .toString()
-        .slice(2),
-      10
-    );
-    return (
-      parseInt(w.creditCard.expireMonth, 10) > cmpM &&
-      parseInt(w.creditCard.expireYear, 10) <= cmpY
-    );
-  };
+  const today: Date = new Date();
+  const cmpM: number = today.getMonth() + 1;
+  const cmpY: number = parseInt(
+    today
+      .getFullYear()
+      .toString()
+      .slice(2),
+    10
+  );
+  return (
+    parseInt(w.creditCard.expireMonth, 10) > cmpM &&
+    parseInt(w.creditCard.expireYear, 10) <= cmpY
+  );
+};
