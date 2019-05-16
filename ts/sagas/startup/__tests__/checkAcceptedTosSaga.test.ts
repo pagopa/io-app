49004b3a87ec15ded4a02d20c0d52d512bbab7dc
valentinacala <valentina.cala@ibm.com>
2019-05-16 17:15:32 +0200

    implement proper tests for tos

--- ts/sagas/startup/__tests__/checkAcceptedTosSaga.test.ts
+++ ts/sagas/startup/__tests__/checkAcceptedTosSaga.test.ts
@@ -1,30 +1,67 @@
-import { expectSaga, testSaga } from "redux-saga-test-plan";
+import { expectSaga } from "redux-saga-test-plan";
 
-import { getType } from "typesafe-actions";
+import {
+  NonNegativeInteger,
+  NonNegativeNumber
+} from "italia-ts-commons/lib/numbers";
+import {
+  EmailString,
+  FiscalCode,
+  NonEmptyString
+} from "italia-ts-commons/lib/strings";
+import { UserProfileUnion } from "../../../api/backend";
 import { tosVersion } from "../../../config";
 import { navigateToTosScreen } from "../../../store/actions/navigation";
-import { tosAccept } from "../../../store/actions/onboarding";
 import { profileUpsert } from "../../../store/actions/profile";
 import { checkAcceptedTosSaga } from "../checkAcceptedTosSaga";
 
 describe("checkAcceptedTosSaga", () => {
+  const initialProfile: UserProfileUnion = {
+    has_profile: true,
+    is_inbox_enabled: true,
+    is_webhook_enabled: true,
+    email: "test@example.com" as EmailString,
+    spid_email: "test@example.com" as EmailString,
+    family_name: "Connor",
+    name: "John",
+    fiscal_code: "XYZ" as FiscalCode,
+    spid_mobile_phone: "123" as NonEmptyString,
+    version: 1 as NonNegativeInteger
+  };
+
+  const notUpdatedProfile: UserProfileUnion = {
+    ...initialProfile,
+    accepted_tos_version: (tosVersion - 1) as NonNegativeNumber
+  };
+
+  const updatedProfile: UserProfileUnion = {
+    ...initialProfile,
+    accepted_tos_version: tosVersion
+  };
+
   describe("when user has already accepted the last version of ToS", () => {
     it("should should do nothing", () => {
-      return expectSaga(checkAcceptedTosSaga)
+      return expectSaga(checkAcceptedTosSaga, updatedProfile)
         .not.put(navigateToTosScreen)
         .run();
     });
   });
 
-  describe("when user has not already accepted the ToS or accepted an old version", () => {
+  describe("when user has not already accepted an old version of ToS", () => {
     it("should navigate to the terms of service screen and succeed when ToS get accepted", () => {
-      testSaga(checkAcceptedTosSaga)
-        .next()
+      return expectSaga(checkAcceptedTosSaga, notUpdatedProfile)
         .put(navigateToTosScreen)
-        .next()
-        .take(getType(tosAccept))
-        .next()
-        .put(profileUpsert.request({ accepted_tos_version: tosVersion }));
+        .take(profileUpsert.request)
+        .run();
+    });
+  });
+
+  describe("when user has not already accepted an old version of ToS", () => {
+    it("should navigate to the terms of service screen and succeed when ToS get accepted", () => {
+      return expectSaga(checkAcceptedTosSaga, initialProfile)
+        .put(navigateToTosScreen)
+        .take(profileUpsert.request)
+        .run();
     });
   });
 });
