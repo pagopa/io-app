7ddc8d59cc9433811af6b7c1be967805d93feca2
Matteo Boschi <mbmaken@gmail.com>
2019-05-23 18:58:37 +0200

    [#166151994] add deleted code

--- ts/utils/calendar.ts
+++ ts/utils/calendar.ts
@@ -38,3 +38,16 @@ export async function checkAndRequestPermission(): Promise<
     return notAuthorizedAndAsked;
   }
 }
+
+/**
+ * Checks whether the app is authorized to read/write to system calendars.
+ */
+export async function checkCalendarPermission() {
+  try {
+    const status = await RNCalendarEvents.authorizationStatus();
+    // If the permission is already granted return true
+    return status === "authorized";
+  } catch (error) {
+    return false;
+  }
+}
