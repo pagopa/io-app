806ff8bbd88cbb67181cfe7eed734cbc6adc3897
Matteo Boschi <mbmaken@gmail.com>
2019-06-25 10:48:23 +0200

    [#165800423] Check pagopa DEV and PROD specs alignment (#1116)
    * [#165800423] add script to check pagopa prod and dev spec alignment
    edit circle ci config to run a nightly job
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * test
    
    * test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] add slack notification
    
    * change settings
    
    * [#165800423] add webhook support to slack notification
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] add webhook support to slack notification
    
    * [#165800423] some improvements
    
    * [#165800423] remove docker node image
    
    * [#165800423] add docket image android api:28
    
    * [#165800423] test
    
    * [#165800423] remove test stuffs
    
    * [#165800423] test
    
    * [#165800423] add slack webhook from env variable
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] test
    
    * [#165800423] remove slack notification when the check goes well

---
+++ scripts/pagopa_api_check.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# read endpoint urls from .env.production files
+PAGOPA_API_URL_PREFIX=""
+PAGOPA_API_URL_PREFIX_TEST=""
+PAGOPA_API_URL_SUFFIX="/v2/api-docs"
+input=".env.production"
+while IFS= read -r line; do
+    if [[ $line =~ PAGOPA_API_URL_PREFIX=.* ]]; then
+        # take the value a strip the string
+        PAGOPA_API_URL_PREFIX=$(echo $line | cut -d'=' -f 2 | tr -d "\'" | tr -d '[:space:]')
+        PAGOPA_API_URL_PREFIX="$PAGOPA_API_URL_PREFIX$PAGOPA_API_URL_SUFFIX"
+    fi
+    if [[ $line =~ PAGOPA_API_URL_PREFIX_TEST=.* ]]; then
+        # take the value a strip the string
+        PAGOPA_API_URL_PREFIX_TEST=$(echo $line | cut -d'=' -f 2 | tr -d "\'" | tr -d '[:space:]')
+        PAGOPA_API_URL_PREFIX_TEST="$PAGOPA_API_URL_PREFIX_TEST$PAGOPA_API_URL_SUFFIX"
+    fi
+done < "$input"
+# regex pattern to handle only hosts url difference
+NO_CHANGES_REGEX='.*.*<.*"host": ".*",.*---.*>.*"host": ".*",'
+
+printf "### COMPARING SWAGGER DEFINITIONS ###\n\n"
+printf "%14s $PAGOPA_API_URL_PREFIX\n" "production:" 
+printf "%14s $PAGOPA_API_URL_PREFIX_TEST\n" "test:" 
+
+SEND_MSG="✅ pagopa production and test specifications have no differences"
+SEND_EXIT=0
+# mention matteo boschi slack account
+MB_SLACK="<@UGP1H4GLR>" 
+DIFF=$(diff -w -B <(curl -s $PAGOPA_API_URL_PREFIX | python -m json.tool) <(curl -s $PAGOPA_API_URL_PREFIX_TEST | python -m json.tool))
+printf "\n\n"
+if [ -n "$DIFF" ]; then
+    if [[ $DIFF =~ $NO_CHANGES_REGEX ]]; then
+        echo $SEND_MSG
+    else
+        KO_MSG="⚠ $MB_SLACK ko. It seems *PROD* and *DEV* pagoPa specifications are different"
+        echo "It seems PROD and DEV pagoPa specifications are different"
+        echo $DIFF
+        SEND_MSG=$KO_MSG
+        SEND_EXIT=1
+        #send slack notification
+        channel="#io-status"
+        res=$(curl -s -X POST -H 'Content-type: application/json' --data '{"text":"'"$SEND_MSG"'", "channel" : "'$channel'"}' ${ITALIAAPP_SLACK_TOKEN_PAGOPA_CHECK:-})
+    fi
+fi
+exit $SEND_EXIT
