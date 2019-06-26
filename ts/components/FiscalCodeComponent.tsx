f0d0812c3b9e125cbe404a58246c8e605dd4681d
Valentina Calà <valentina.cala@ibm.com@Valentinas-MacBook-Pro.local>
2019-06-26 18:15:17 +0200

    apply new design of fiscal code component

--- ts/components/FiscalCodeComponent.tsx
+++ ts/components/FiscalCodeComponent.tsx
@@ -2,10 +2,9 @@
  * A component to show a representation
  * of the fiscal code card of the user profile
  */
-import I18n from "i18n-js";
 import { Text, View } from "native-base";
 import * as React from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, Dimensions } from "react-native";
 
 import { UserProfile } from "../../definitions/backend/UserProfile";
 import customVariables from "../theme/variables";
@@ -14,112 +13,85 @@ type Props = {
   profile: UserProfile;
 };
 
+// fiscal card fac-simile dimensions: 546 x 870
+const cardWidth2 = Dimensions.get('screen').width - 2 * customVariables.contentPadding;
+const scaleFactor = cardWidth2 / 870;
+
+const cardHeigth2 =  546 * scaleFactor;
+const cardHeaderHeight = 155 * scaleFactor;
+const cardSpacer = 16 * scaleFactor;
+const cardLargeSpacer = 24 * scaleFactor;
+const cardLineHeight = 26 * scaleFactor;
+const textdLineHeight = 15;
+const textFontSize = 11;
+const textLeftMargin = 140 * scaleFactor + 6
+
+/*// risoluzione fac-simile carta: 608 x 932
+const cardWidth1 = Dimensions.get('screen').width - 2 * customVariables.contentPadding;
+const cardHeigth1 = cardWidth1 * 608 / 932;
+const rotatedFCstyle1= {
+  resizeMode: 'contain',
+  height: cardWidth1, 
+  width: cardHeigth1, 
+  transform: [
+    {rotateZ: '-90deg'},
+    {translateY: -(cardHeigth1-cardWidth1)/2 + customVariables.contentPadding}
+  ],
+  //marginTop: -56
+}*/
+
+
 const styles = StyleSheet.create({
-  mainContainer: {
-    backgroundColor: customVariables.brandGray,
-    borderRadius: 6,
-    padding: 6,
-    width: "100%"
+  cardBackground: {
+    resizeMode: 'contain',
+    height: cardHeigth2, 
+    width: cardWidth2
   },
-
-  topContainer: {
-    backgroundColor: "green",
-    borderTopLeftRadius: 6,
-    borderTopRightRadius: 6,
-    paddingVertical: 8
-  },
-
-  logo: {
-    resizeMode: "contain",
-    width: 70,
-    height: 70
-  },
-
-  imageContainer: {
-    justifyContent: "center",
-    alignContent: "center",
-    paddingHorizontal: 2
-  },
-
-  logoText: {
-    lineHeight: 12,
-    fontSize: 10
-  },
-
-  flexRow: {
-    flexDirection: "row"
-  },
-
-  labelText: {
-    lineHeight: 10,
-    fontSize: 8,
-    color: "green"
-  },
-
-  labelContainer: {
-    paddingRight: 6
-  },
-
-  contentContainer: {
-    padding: 6,
-    alignItems: "center",
-    justifyContent: "flex-start"
+  text: {
+    position:'absolute',
+    fontSize: textFontSize,
+    marginLeft: textLeftMargin,
   }
 });
 
 export default class FiscalCodeComponent extends React.Component<Props> {
-  private renderRow(label: string, value: string) {
-    return (
-      <View style={[styles.contentContainer, styles.flexRow]}>
-        <View style={styles.labelContainer}>
-          <Text style={styles.labelText}>{label.split(" ").join("\n")}</Text>
-        </View>
-        <View>
-          <Text>{value}</Text>
-        </View>
-      </View>
-    );
-  }
-
   public render(): React.ReactNode {
     return (
-      <View style={styles.mainContainer}>
-        <View style={[styles.topContainer, styles.flexRow]}>
-          <View style={styles.imageContainer}>
-            <Image
-              style={styles.logo}
-              source={require("../../img/repubblica-italiana-icon.png")}
-            />
-          </View>
+      <View>    
+        <Image
+          source={require("./../../img/fiscalCode/fiscalCodeFront.png")}
+          style={styles.cardBackground}
+        /> 
+        <View style={{
+          position: 'absolute', 
+          height: cardHeaderHeight + cardLargeSpacer + cardLineHeight *2 + cardSpacer + cardLineHeight + cardSpacer + cardLineHeight + cardSpacer, 
+          width: '100%', 
+          backgroundColor: 'yellow', 
+          opacity: 0.4}}/> 
+        <Text 
+          bold={true}
+          style={[styles.text,{
+            lineHeight: cardLineHeight *2,
+            marginTop: cardHeaderHeight + cardLargeSpacer
+        }]}>
+          {this.props.profile.fiscal_code.toUpperCase()}</Text>
 
-          <View>
-            <Text white={true} style={styles.logoText}>
-              {I18n.t("profile.fiscalCode.header1")
-                .split(" ")
-                .join("\n")}
-            </Text>
-            <View spacer={true} />
-            <Text white={true} style={styles.logoText}>
-              {I18n.t("profile.fiscalCode.header2")
-                .split(" ")
-                .join("\n")}
-            </Text>
-          </View>
-        </View>
-
-        {this.renderRow(
-          I18n.t("profile.fiscalCode.fiscalCode"),
-          this.props.profile.fiscal_code
-        )}
-        {this.renderRow(
-          I18n.t("profile.fiscalCode.name"),
-          this.props.profile.name
-        )}
-        {this.renderRow(
-          I18n.t("profile.fiscalCode.familyName"),
-          this.props.profile.family_name
-        )}
-      </View>
-    );
+        <Text 
+          bold={true}
+          style={[styles.text,{
+            lineHeight: textdLineHeight,
+            marginTop: cardHeaderHeight + cardLargeSpacer + cardLineHeight *2 + cardSpacer
+        }]}>
+          {this.props.profile.family_name.toUpperCase()}</Text>
+        
+        <Text 
+          bold={true}
+          style={[styles.text,{
+            lineHeight: textdLineHeight,
+            marginTop: cardHeaderHeight + cardLargeSpacer + cardLineHeight *2 + cardSpacer + cardLineHeight + cardSpacer
+        }]}>
+          {this.props.profile.name.toUpperCase()}</Text>
+    </View>
+      );
   }
 }
