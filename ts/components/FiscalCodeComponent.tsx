4fa0c81c5e90282d9a9bf1b4fe336deb62e4e679
valentinacala <valentina.cala@outlook.com>
2019-06-27 15:50:05 +0200

    new fiscal code fac-simile front sides

--- ts/components/FiscalCodeComponent.tsx
+++ ts/components/FiscalCodeComponent.tsx
@@ -1,10 +1,8 @@
 /**
  * A component to show a representation
  * of the fiscal code card of the user profile
- * 
- * TODO: 
- * - get rotated header only
- * - get landscape of card 
+ *
+ * TODO:
  * - add shadow to component
  * - extract missing data from fiscal code
  */
@@ -15,96 +13,212 @@ import { Dimensions, Image, StyleSheet } from "react-native";
 import { UserProfile } from "../../definitions/backend/UserProfile";
 import customVariables from "../theme/variables";
 
-type Props = {
+interface BaseProps {
   profile: UserProfile;
-};
+}
+
+interface PreviewProps {
+  type: "Preview";
+}
+
+interface FullProps extends BaseProps {
+  type: "Full";
+}
+
+interface LandscapeProps extends BaseProps {
+  type: "Landscape";
+}
+
+type Props = PreviewProps | FullProps | LandscapeProps;
 
 // fiscal card fac-simile dimensions: 546 x 870
-const cardWidth2 =
+const contentWidth =
   Dimensions.get("screen").width - 2 * customVariables.contentPadding;
-const scaleFactor = cardWidth2 / 870;
 
-const cardHeigth2 = 546 * scaleFactor;
-const cardHeaderHeight = 155 * scaleFactor;
-const cardSpacer = 16 * scaleFactor;
-const cardLargeSpacer = 24 * scaleFactor;
-const cardLineHeight = 26 * scaleFactor;
-const textdLineHeight = 15;
-const textFontSize = 11;
-const textLeftMargin = 140 * scaleFactor + 6;
+// Full (horizontal position)
+const fullScaleFactor = contentWidth / 870;
+const cardHeightF = 546 * fullScaleFactor;
+const cardHeaderHeightF = 154 * fullScaleFactor;
+const cardSpacerF = 16 * fullScaleFactor;
+const cardLargeSpacerF = 24 * fullScaleFactor;
+const cardLineHeightF = 26 * fullScaleFactor;
+const textdLineHeightF = 15;
+const textFontSizeF = 11;
+const textLeftMarginF = 140 * fullScaleFactor + 6;
 
-const fiscalCodeHeight = cardHeaderHeight + cardLargeSpacer;
-const lastNameHeight =
-  cardHeaderHeight + cardLargeSpacer + cardLineHeight * 2 + cardSpacer;
-const nameHeight =
-  cardHeaderHeight +
-  cardLargeSpacer +
-  cardLineHeight * 2 +
-  cardSpacer +
-  cardLineHeight +
-  cardSpacer;
+const fiscalCodeHeight = cardHeaderHeightF + cardLargeSpacerF;
+const lastNameHeightFull =
+  cardHeaderHeightF + cardLargeSpacerF + cardLineHeightF * 2 + cardSpacerF;
+const nameHeightFull = lastNameHeightFull + cardLineHeightF + cardSpacerF;
+
+// Landscape (vertical position)
+const landscapeScaleFactor = contentWidth / 546;
+const textLineHeightL = 28; // to solve misalignment on font, 28 is the fist value that seems to give text centered to line
+const textLeftMarginL = 140 * landscapeScaleFactor + 8;
+const cardHeaderHeightL = 154 * landscapeScaleFactor;
+const cardSpacerL = 16 * landscapeScaleFactor;
+const cardLargeSpacerL = 24 * landscapeScaleFactor;
+const cardLineHeightL = 26 * landscapeScaleFactor;
+
+const fiscalCodeHeightL =
+  cardHeaderHeightL +
+  cardLargeSpacerL -
+  (cardLineHeightL * 2 - textLineHeightL);
+const lastNameHeightL =
+  fiscalCodeHeightL +
+  cardSpacerL +
+  cardLineHeightL -
+  (cardLineHeightL - textLineHeightL);
+const nameHeightL = lastNameHeightL + cardSpacerL + cardLineHeightL;
 
 const styles = StyleSheet.create({
-  cardBackground: {
-    resizeMode: "contain",
-    height: cardHeigth2,
-    width: cardWidth2
+  previewCardBackground: {
+    marginBottom: -(cardHeightF - cardHeaderHeightF),
+    transform: [{ perspective: 700 }, { rotateX: "-20deg" }, { scaleX: 0.95 }]
   },
-  text: {
+
+  fullCardBackground: {
+    resizeMode: "contain",
+    height: cardHeightF,
+    width: contentWidth
+  },
+
+  landscapeCardBackground: {
+    resizeMode: "contain",
+    width: 870 * landscapeScaleFactor,
+    height: contentWidth
+  },
+
+  landscapeCardRotation: {
+    transform: [
+      { rotateZ: "90deg" },
+      { translateY: (870 * landscapeScaleFactor - contentWidth) / 2 }
+    ],
+    marginVertical: (870 * landscapeScaleFactor - contentWidth) / 2
+  },
+
+  fullText: {
     position: "absolute",
-    fontSize: textFontSize,
-    marginLeft: textLeftMargin,
+    fontSize: textFontSizeF,
+    marginLeft: textLeftMarginF,
     color: customVariables.brandDarkestGray
+  },
+
+  landscapeText: {
+    position: "absolute",
+    color: customVariables.brandDarkestGray,
+    fontSize: 18,
+    width: 870 * landscapeScaleFactor,
+    paddingLeft: textLeftMarginL,
+    lineHeight: textLineHeightL
+  },
+
+  fullFiscalCodeText: {
+    lineHeight: cardLineHeightF * 2,
+    marginTop: fiscalCodeHeight
+  },
+
+  landscapeFiscalCodeText: {
+    transform: [
+      { rotateZ: "90deg" },
+      { translateY: -(2 * customVariables.contentPadding) + fiscalCodeHeightL },
+      {
+        translateX:
+          (870 * landscapeScaleFactor - customVariables.contentPadding) / 2
+      }
+    ]
+  },
+
+  fullLastNameText: {
+    lineHeight: textdLineHeightF,
+    marginTop: lastNameHeightFull
+  },
+
+  landscapeLastNameText: {
+    transform: [
+      { rotateZ: "90deg" },
+      { translateY: -(2 * customVariables.contentPadding) + lastNameHeightL },
+      {
+        translateX:
+          (870 * landscapeScaleFactor - customVariables.contentPadding) / 2
+      }
+    ]
+  },
+
+  fulleNameText: {
+    lineHeight: textdLineHeightF,
+    marginTop: nameHeightFull
+  },
+
+  landscapeNameText: {
+    transform: [
+      { rotateZ: "90deg" },
+      { translateY: -(2 * customVariables.contentPadding) + nameHeightL },
+      {
+        translateX:
+          (870 * landscapeScaleFactor - customVariables.contentPadding) / 2
+      }
+    ]
   }
 });
 
 export default class FiscalCodeComponent extends React.Component<Props> {
+  private renderContent(profile: UserProfile, isLandscape: boolean) {
+    return (
+      <React.Fragment>
+        <Text
+          bold={true}
+          style={[
+            isLandscape
+              ? [styles.landscapeText, styles.landscapeFiscalCodeText]
+              : [styles.fullText, styles.fullFiscalCodeText]
+          ]}
+        >
+          {profile.fiscal_code.toUpperCase()}
+        </Text>
+
+        <Text
+          bold={true}
+          style={[
+            isLandscape
+              ? [styles.landscapeText, styles.landscapeLastNameText]
+              : [styles.fullText, styles.fullLastNameText]
+          ]}
+        >
+          {profile.family_name.toUpperCase()}
+        </Text>
+
+        <Text
+          bold={true}
+          style={[
+            isLandscape
+              ? [styles.landscapeText, styles.landscapeNameText]
+              : [styles.fullText, styles.fulleNameText]
+          ]}
+        >
+          {profile.name.toUpperCase()}
+        </Text>
+      </React.Fragment>
+    );
+  }
+
   public render(): React.ReactNode {
     return (
       <View>
         <Image
           source={require("./../../img/fiscalCode/fiscalCodeFront.png")}
-          style={styles.cardBackground}
+          style={[
+            this.props.type === "Preview" && styles.previewCardBackground,
+            this.props.type === "Landscape"
+              ? [styles.landscapeCardBackground, styles.landscapeCardRotation]
+              : styles.fullCardBackground
+          ]}
         />
-
-        <Text
-          bold={true}
-          style={[
-            styles.text,
-            {
-              lineHeight: cardLineHeight * 2,
-              marginTop: fiscalCodeHeight
-            }
-          ]}
-        >
-          {this.props.profile.fiscal_code.toUpperCase()}
-        </Text>
-
-        <Text
-          bold={true}
-          style={[
-            styles.text,
-            {
-              lineHeight: textdLineHeight,
-              marginTop: lastNameHeight
-            }
-          ]}
-        >
-          {this.props.profile.family_name.toUpperCase()}
-        </Text>
-
-        <Text
-          bold={true}
-          style={[
-            styles.text,
-            {
-              lineHeight: textdLineHeight,
-              marginTop: nameHeight
-            }
-          ]}
-        >
-          {this.props.profile.name.toUpperCase()}
-        </Text>
+        {this.props.type !== "Preview" &&
+          this.renderContent(
+            this.props.profile,
+            this.props.type === "Landscape"
+          )}
       </View>
     );
   }
