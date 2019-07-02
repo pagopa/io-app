8d83a1235170f23d80d3f7471e64c17242eac0c4
Alex193a <alessandro.paluzzi@progettopa.it>
2019-07-02 17:02:24 +0200

    [#163241620] Fix no connection warning
    This PR has the intent to try to fix the problem of the unavailable connection warning that turns out to be too "sensitive", appearing even when the connection is available.
    The react-native-offline library has been updated to the latest version (4.3.0), the ping timeout has been slightly increased while the ping interval has been slightly decreased and it seems to have improved the situation.

--- ts/@types/react-native-offline.d.ts
+++ ts/@types/react-native-offline.d.ts
@@ -1,14 +1,1 @@
-declare module "react-native-offline" {
-  export function reducer<T>(state: T | undefined, action: any): T;
-
-  interface NetworkEventsListenerSagaArguments {
-    timeout?: number;
-    pingServerUrl?: string;
-    withExtraHeadRequest?: boolean;
-    checkConnectionInterval?: number;
-  }
-
-  export function* networkEventsListenerSaga<A, B, C>(
-    args: NetworkEventsListenerSagaArguments
-  ): Generator<A, B, C>;
-}
+declare module "react-native-offline";
