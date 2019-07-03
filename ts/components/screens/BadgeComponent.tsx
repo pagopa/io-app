fcb08677d4f260ad9913007dc470940fd9db9d87
valentinacala <valentina.cala@outlook.com>
2019-07-03 12:10:14 +0200

    add story reference to TODO

--- ts/components/screens/BadgeComponent.tsx
+++ ts/components/screens/BadgeComponent.tsx
@@ -1,6 +1,6 @@
 /**
  * A component to render the circolar badge
- * TODO: use the same component on all lists (messages, services, transaction)
+ * TODO: use the same component on all lists (messages, services, transaction): https://www.pivotaltracker.com/story/show/167064275
  */
 import * as React from "react";
 import { Circle, Svg } from "react-native-svg";
