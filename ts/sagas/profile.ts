a641da6eb91452fad69935f747e684bca57dfd56
Davide Chiabrero <davide.chiabrero@it.ibm.com>
2019-07-04 08:47:23 +0200

    Update error handling

--- ts/sagas/profile.ts
+++ ts/sagas/profile.ts
@@ -115,15 +115,12 @@ function* createOrUpdateProfileSaga(
     return;
   }
 
-  if (response.value.status === 429) {
-    yield put(sessionExpired());
-    return;
-  }
-
   if (response.value.status !== 200) {
     // We got a error, send a SESSION_UPSERT_FAILURE action
     const error: Error = Error(
-      response.value.value.title || I18n.t("profile.errors.upsert")
+      response.value.value !== undefined
+        ? response.value.value.title
+        : "" || I18n.t("profile.errors.upsert")
     );
 
     yield put(profileUpsert.failure(error));
