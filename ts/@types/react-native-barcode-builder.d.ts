c545cd9e14a3db013d1596db81de72aae93f183d
valentinacala <valentina.cala@outlook.com>
2019-07-04 16:34:07 +0200

    update library declaration file

--- ts/@types/react-native-barcode-builder.d.ts
+++ ts/@types/react-native-barcode-builder.d.ts
@@ -1,1 +1,15 @@
-declare module 'react-native-barcode-builder';
+declare module 'react-native-barcode-builder' {
+    import { PureComponent } from "react";
+
+    export interface BarcodeProps {
+        value: string;
+        width?: number;
+        height?: number;
+        background?: string;
+        format: string;
+    }
+
+    export default class Barcode extends React.PureComponent<BarcodeProps>{
+        constructor(props);
+    }
+}
