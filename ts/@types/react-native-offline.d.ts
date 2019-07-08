02cb488926e5d7012f783683570999b4c164e8f4
Alex193a <alessandro.paluzzi@progettopa.it>
2019-07-08 16:17:26 +0200

    [#163241620] Fix no connection warning

--- ts/@types/react-native-offline.d.ts
+++ ts/@types/react-native-offline.d.ts
@@ -1,1 +1,14 @@
-declare module "react-native-offline";
+declare module "react-native-offline" {
+  export function reducer<T>(state: T | undefined, action: any): T;
+
+  interface NetworkEventsListenerSagaArguments {
+    pingTimeout?: number;
+    pingServerUrl?: string;
+    shouldPing?: boolean;
+    pingInterval?: number;
+  }
+
+  export function* networkSaga<A, B, C>(
+    args: NetworkEventsListenerSagaArguments
+  ): Generator<A, B, C>;
+}
