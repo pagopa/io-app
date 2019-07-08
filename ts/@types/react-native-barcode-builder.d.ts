36d1d0ae27f47b9b94e7271fea218c93396a6016
valentinacala <valentina.cala@outlook.com>
2019-07-08 12:47:43 +0200

    details barcode format type

--- ts/@types/react-native-barcode-builder.d.ts
+++ ts/@types/react-native-barcode-builder.d.ts
@@ -1,12 +1,36 @@
-declare module 'react-native-barcode-builder' {
+declare module "react-native-barcode-builder" {
     import { PureComponent } from "react";
+    import { oneOf } from "fp-ts/lib/Foldable2v";
+    import { api } from "jsbarcode/jsbarcode";
+
+    type barcodeTypes = "CODE39" |
+        "CODE128" | 
+        "CODE128A" | 
+        "CODE128B" | 
+        "CODE128C" |
+        "EAN13"| 
+        "EAN8" | 
+        "EAN5" | 
+        "EAN2" | 
+        "UPC" | 
+        "UPCE" |
+	    "ITF14" |
+	    "ITF" |
+        "MSI" | 
+        "MSI10" | 
+        "MSI11" | 
+        "MSI1010" | 
+        "MSI1110" |
+	    "pharmacode" |
+	    "codabar" |
+	    "GenericBarcode";
 
     export interface BarcodeProps {
         value: string;
         width?: number;
         height?: number;
         background?: string;
-        format: string;
+        format: barcodeTypes;
         text?: string;
         lineColor?: string;
         onError?: () => void;
