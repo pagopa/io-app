ae7cf47546626dd754a33caa7d6930b990c739e3
valentinacala <valentina.cala@outlook.com>
2019-07-09 15:44:51 +0200

    remove not used barcode format

--- ts/@types/react-native-barcode-builder.d.ts
+++ ts/@types/react-native-barcode-builder.d.ts
@@ -3,28 +3,12 @@ declare module "react-native-barcode-builder" {
     import { oneOf } from "fp-ts/lib/Foldable2v";
     import { api } from "jsbarcode/jsbarcode";
 
+    // For not included barcode format, if required,
+    // review rules on value to encode as barcode
     type barcodeTypes = "CODE39" |
         "CODE128" | 
         "CODE128A" | 
-        "CODE128B" | 
-        "CODE128C" |
-        "EAN13"| 
-        "EAN8" | 
-        "EAN5" | 
-        "EAN2" | 
-        "UPC" | 
-        "UPCE" |
-	    "ITF14" |
-	    "ITF" |
-        "MSI" | 
-        "MSI10" | 
-        "MSI11" | 
-        "MSI1010" | 
-        "MSI1110" |
-	    "pharmacode" |
-	    "codabar" |
-	    "GenericBarcode";
-
+        "CODE128B" ;
     export interface BarcodeProps {
         value: string;
         width?: number;
