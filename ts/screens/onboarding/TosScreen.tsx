d36dc33e33294046d64d7fc7c58d9db7f6830aea
valentinacala <valentina.cala@outlook.com>
2019-07-10 10:13:33 +0200

    new tos restyle

--- ts/screens/onboarding/TosScreen.tsx
+++ ts/screens/onboarding/TosScreen.tsx
@@ -1,12 +1,12 @@
 import * as pot from "italia-ts-commons/lib/pot";
-import { Button, Content, Text, View } from "native-base";
+import { Content, Text, View } from "native-base";
 import * as React from "react";
 import { NavigationScreenProp, NavigationState } from "react-navigation";
 import { connect } from "react-redux";
 
-import { StyleSheet } from "react-native";
-import AbortOnboardingModal from "../../components/AbortOnboardingModal";
+import { Alert, StyleSheet } from "react-native";
 import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
+import FooterWithButtons from "../../components/ui/FooterWithButtons";
 import H4 from "../../components/ui/H4";
 import Markdown from "../../components/ui/Markdown";
 import { tosVersion } from "../../config";
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
     alignContent: "flex-start"
   },
   boldH4: {
-    fontWeight: "700",
+    fontWeight: customVariables.textBoldWeight,
     paddingTop: customVariables.spacerLargeHeight
   },
   horizontalPadding: {
@@ -87,21 +87,21 @@ class TosScreen extends React.PureComponent<Props, State> {
           </View>
         </Content>
         {isProfile === false && (
-          <View footer={true}>
-            <Button
-              block={true}
-              primary={true}
-              onPress={() => dispatch(tosAccepted())}
-            >
-              <Text>{I18n.t("onboarding.tos.continue")}</Text>
-            </Button>
-          </View>
-        )}
-
-        {this.state.showAbortOnboardingModal && (
-          <AbortOnboardingModal
-            onClose={this.handleModalClose}
-            onConfirm={this.handleModalConfirm}
+          <FooterWithButtons
+            type={"TwoButtonsInlineThird"}
+            leftButton={{
+              block: true,
+              light: true,
+              bordered: true,
+              onPress: () => this.handleGoBack(),
+              title: I18n.t("global.buttons.exit")
+            }}
+            rightButton={{
+              block: true,
+              primary: true,
+              onPress: () => dispatch(tosAccepted()),
+              title: I18n.t("onboarding.tos.continue")
+            }}
           />
         )}
       </BaseScreenComponent>
@@ -109,15 +109,21 @@ class TosScreen extends React.PureComponent<Props, State> {
   }
 
   private handleGoBack = () =>
-    this.setState({ showAbortOnboardingModal: true });
-
-  private handleModalClose = () =>
-    this.setState({ showAbortOnboardingModal: false });
-
-  private handleModalConfirm = () => {
-    this.handleModalClose();
-    this.props.dispatch(abortOnboarding());
-  };
+    Alert.alert(
+      "Vuoi davvero uscire da IO?",
+      "Dovrai loggarti nuovamente per continuare ad usare l'app",
+      [
+        {
+          text: "Annulla",
+          style: "cancel"
+        },
+        {
+          text: "Esci",
+          style: "default",
+          onPress: () => this.props.dispatch(abortOnboarding()) // TODO: make it to de on right and bold
+        }
+      ]
+    );
 }
 
 // If the user had accepted a previous version of ToS
