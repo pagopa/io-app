0a58e4493e9c144bf441941f27d4db9595485f96
Alex193a <alessandro.paluzzi@progettopa.it>
2019-07-12 17:25:28 +0200

    [#167169752] Fixed navigation loop in Wallet Home
    Fixed navigation loop in Wallet Home that happens when you use the "Cancel" button on the QR Code screen (for example) and then you use the hardware back button in the Wallet Home screen.

--- ts/screens/wallet/WalletHomeScreen.tsx
+++ ts/screens/wallet/WalletHomeScreen.tsx
@@ -7,7 +7,7 @@ import { none } from "fp-ts/lib/Option";
 import * as pot from "italia-ts-commons/lib/pot";
 import { Button, Content, Text, View } from "native-base";
 import * as React from "react";
-import { Image, StyleSheet } from "react-native";
+import { BackHandler, Image, StyleSheet } from "react-native";
 import { Grid, Row } from "react-native-easy-grid";
 import { NavigationScreenProp, NavigationState } from "react-navigation";
 import { connect } from "react-redux";
@@ -116,7 +116,17 @@ class WalletHomeScreen extends React.Component<Props, never> {
     // WIP WIP create pivotal story
     this.props.loadWallets();
     this.props.loadTransactions();
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
   }
+
+  public componentWillUnmount(): void {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+  }
+
+  private onBackPress = () => {
+    this.props.navigation.goBack();
+    return true;
+  };
 
   private cardHeader(isError: boolean = false) {
     return (
