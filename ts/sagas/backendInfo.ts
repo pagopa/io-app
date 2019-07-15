ba56f1832ec3658a5cd80227ae469f48b41608d4
Davide Chiabrero <davide.chiabrero@it.ibm.com>
2019-07-15 15:31:44 +0200

    Update error definition

--- ts/sagas/backendInfo.ts
+++ ts/sagas/backendInfo.ts
@@ -1,4 +1,5 @@
 import { left } from "fp-ts/lib/Either";
+import { Errors } from "io-ts";
 import * as t from "io-ts";
 import { BasicResponseType } from "italia-ts-commons/lib/requests";
 import { call, Effect, fork, put } from "redux-saga/effects";
@@ -23,22 +24,20 @@ const BACKEND_INFO_RETRY_INTERVAL = 60 * 60 * 10 * 1000;
 function* backendInfoWatcher(): IterableIterator<Effect> {
   const backendPublicClient = BackendPublicClient(apiUrlPrefix);
 
+  const error: Errors = [
+    {
+      context: [],
+      value: "some error occurred"
+    }
+  ];
+
   function getServerInfo(): Promise<
     t.Validation<BasicResponseType<ServerInfo>>
   > {
     return new Promise((resolve, _) =>
       backendPublicClient
         .getServerInfo({})
-        .then(resolve, () =>
-          resolve(
-            left([
-              t.getValidationError(
-                "some error occurred",
-                t.getDefaultContext(t.null)
-              )
-            ])
-          )
-        )
+        .then(resolve, () => resolve(left(error)))
     );
   }
 
