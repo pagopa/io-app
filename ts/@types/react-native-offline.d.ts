7698a315a778d90ce9faf3d5375dffe8a4815298
valentinacala <valentina.cala@outlook.com>
2019-07-17 10:48:51 +0200

    properly revert master file

--- ts/@types/react-native-offline.d.ts
+++ ts/@types/react-native-offline.d.ts
@@ -1,15 +1,13 @@
-declare module "react-native-offline" {
-  export function reducer<T>(state: T | undefined, action: any): T;
+export function reducer<T>(state: T | undefined, action: any): T; 
 
-  interface NetworkEventsListenerSagaArguments {
-    pingTimeout?: number;
-    pingServerUrl?: string;
-    shouldPing?: boolean;
-    pingInterval?: number;
-  }
+interface NetworkEventsListenerSagaArguments { 
+  timeout?: number; 
+  pingServerUrl?: string; 
+  withExtraHeadRequest?: boolean; 
+  checkConnectionInterval?: number; 
+} 
 
-  export function* networkSaga<A, B, C>(
-    args: NetworkEventsListenerSagaArguments
-  ): Generator<A, B, C>;
+export function* networkEventsListenerSaga<A, B, C>( 
+  args: NetworkEventsListenerSagaArguments 
+  ): Generator<A, B, C>; 
 }
-
