6dfd3b043a2b24d0bf3866ce83a6d71b24e10574
laurapugliese <laura.pugliese@live.it>
2019-08-05 15:15:03 +0200

    [#167102335] Added selection in organization list component (#1240)
    * [#167102335] Introduced selection
    
    Introduced selection
    
    * [#167102335] Fixed lint and added locales
    
    Fixed lint and added locales
    
    * [#167102335] Replaced set with array in organizationsFiscalCodesSelected reducer
    
    Replaced set with array in organizationsFiscalCodesSelected reducer
    
    * [#167102335] Added comment
    
    Added comment
    
    * [#167102335] Fix KeyboardAvoidingView on android
    
    Fix KeyboardAvoidingView on android
    
    * Update ts/components/helpers/withItemsSelection.tsx
    
    Co-Authored-By: Matteo Boschi <mbmaken@gmail.com>
    
    * [#167102335] Changed name props
    
    Changed name props
    
    * [#167102335] Renamed constant
    
    Renamed constant
    
    * [#167102335] Use type Either for itemIconComponent prop
    
    Use type Either for itemIconComponent prop
    
    * [#167102335] Removed prop isSearchEnabled
    
    Removed prop isSearchEnabled

---
+++ ts/components/helpers/withItemsSelection.tsx
@@ -0,0 +1,83 @@
+import { none, Option, some } from "fp-ts/lib/Option";
+import hoistNonReactStatics from "hoist-non-react-statics";
+import { Omit } from "italia-ts-commons/lib/types";
+import React from "react";
+
+type State = {
+  selectedItemIds: Option<Set<string>>;
+};
+
+export type InjectedWithItemsSelectionProps = {
+  selectedItemIds: Option<Set<string>>;
+  toggleItemSelection: (id: string) => void;
+  resetSelection: () => void;
+  setSelectedItemIds: (newSelectedItemIds: Option<Set<string>>) => void;
+};
+
+/**
+ * An HOC to maintain and manipulate the items selection.
+ */
+export function withItemsSelection<P extends InjectedWithItemsSelectionProps>(
+  WrappedComponent: React.ComponentType<P>
+) {
+  class WithItemsSelection extends React.PureComponent<
+    Omit<P, keyof InjectedWithItemsSelectionProps>,
+    State
+  > {
+    constructor(props: Omit<P, keyof InjectedWithItemsSelectionProps>) {
+      super(props);
+      this.state = {
+        selectedItemIds: none
+      };
+    }
+
+    public render() {
+      const { selectedItemIds } = this.state;
+      return (
+        <WrappedComponent
+          {...this.props as P}
+          selectedItemIds={selectedItemIds}
+          toggleItemSelection={this.toggleItemSelection}
+          resetSelection={this.resetSelection}
+          setSelectedItemIds={this.setSelectedItemIds}
+        />
+      );
+    }
+
+    // A function to add/remove an id from the selectedItemIds Set.
+    private toggleItemSelection = (id: string) => {
+      this.setState(({ selectedItemIds }) => {
+        return selectedItemIds
+          .map(_ => {
+            const newSelectedItemIds = new Set(_);
+            newSelectedItemIds.has(id)
+              ? newSelectedItemIds.delete(id)
+              : newSelectedItemIds.add(id);
+
+            return {
+              selectedItemIds: some(newSelectedItemIds)
+            };
+          })
+          .getOrElse({
+            selectedItemIds: some(new Set<string>([id]))
+          });
+      });
+    };
+
+    private setSelectedItemIds = (newSelectedItemIds: Option<Set<string>>) => {
+      this.setState({
+        selectedItemIds: newSelectedItemIds
+      });
+    };
+
+    private resetSelection = () => {
+      this.setState({
+        selectedItemIds: none
+      });
+    };
+  }
+
+  hoistNonReactStatics(WithItemsSelection, WrappedComponent);
+
+  return WithItemsSelection;
+}
