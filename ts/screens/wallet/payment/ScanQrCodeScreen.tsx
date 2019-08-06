79bd5d0be1fa06a39813e6792b5f62422fe9a800
Matteo Gentili <43071536+matgentili@users.noreply.github.com>
2019-08-06 14:45:29 +0200

    [#167696716] remove microphone access (#1258)
    * [#167696716] Remove microphone access
    
    Remove microphone access
    
    * [#167696716] Fixed typo
    
    Fixed type

--- ts/screens/wallet/payment/ScanQrCodeScreen.tsx
+++ ts/screens/wallet/payment/ScanQrCodeScreen.tsx
@@ -242,17 +242,18 @@ class ScanQrCodeScreen extends React.Component<Props, State> {
                   <View spacer={true} extralarge={true} />
                 </View>
               }
-              cameraProps={{ ratio: "1:1" }}
+              // "captureAudio" enable/disable microphone permission
+              cameraProps={{ ratio: "1:1", captureAudio: false }}
               // "checkAndroid6Permissions" property enables permission checking for
               // Android versions greater than 6.0 (23+).
               checkAndroid6Permissions={true}
               permissionDialogTitle={I18n.t(
-                "wallet.QRtoPay.cameraUsagePerissionInfobox.title"
+                "wallet.QRtoPay.cameraUsagePermissionInfobox.title"
               )}
               permissionDialogMessage={I18n.t(
-                "wallet.QRtoPay.cameraUsagePerissionInfobox.message"
+                "wallet.QRtoPay.cameraUsagePermissionInfobox.message"
               )}
-              // "notAuthorizedView" is by defatult available on iOS systems ONLY.
+              // "notAuthorizedView" is by default available on iOS systems ONLY.
               // In order to make Android systems act the same as iOSs you MUST
               // enable "checkAndroid6Permissions" property as well.
               // On devices before SDK version 23, the permissions are automatically
