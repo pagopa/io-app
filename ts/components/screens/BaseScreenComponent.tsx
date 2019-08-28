1024f8c065181b07d0a0721d5b1297431574a17f
valentinacala <valentina.cala@outlook.com>
2019-08-28 16:49:55 +0200

    revert approach to contextual help

--- ts/components/screens/BaseScreenComponent.tsx
+++ ts/components/screens/BaseScreenComponent.tsx
@@ -3,7 +3,7 @@ import { connectStyle } from "native-base-shoutem-theme";
 import mapPropsToStyleNames from "native-base/src/utils/mapPropsToStyleNames";
 import * as React from "react";
 
-import { ContextualHelp } from "../ContextualHelp";
+import { ContextualHelpModal } from "../ContextualHelpModal";
 import { SearchType } from "../search/SearchButton";
 import { BaseHeader } from "./BaseHeader";
 
@@ -66,15 +66,7 @@ class BaseScreenComponent extends React.PureComponent<Props, State> {
       isSearchAvailable,
       searchType
     } = this.props;
-    if (contextualHelp && this.state.isHelpVisible) {
-      return (
-        <ContextualHelp
-          title={contextualHelp.title}
-          body={contextualHelp.body}
-          onClose={this.hideHelp}
-        />
-      );
-    }
+
     return (
       <Container>
         <BaseHeader
@@ -89,6 +81,14 @@ class BaseScreenComponent extends React.PureComponent<Props, State> {
           appLogo={appLogo}
         />
         {this.props.children}
+        {contextualHelp && (
+          <ContextualHelpModal
+            title={contextualHelp.title}
+            body={contextualHelp.body}
+            isVisible={this.state.isHelpVisible}
+            close={this.hideHelp}
+          />
+        )}
       </Container>
     );
   }
