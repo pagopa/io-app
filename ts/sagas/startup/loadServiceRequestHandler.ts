96c9f09ba64cd574a3723862f470a0d66a950f92
valentinacala <valentina.cala@outlook.com>
2019-09-23 10:52:02 +0200

    add check on organization name

--- ts/sagas/startup/loadServiceRequestHandler.ts
+++ ts/sagas/startup/loadServiceRequestHandler.ts
@@ -1,10 +1,21 @@
+import * as pot from "italia-ts-commons/lib/pot";
 import { readableReport } from "italia-ts-commons/lib/reporters";
-import { call, Effect, put } from "redux-saga/effects";
+import { call, Effect, put, select } from "redux-saga/effects";
 import { ActionType } from "typesafe-actions";
 import { BackendClient } from "../../api/backend";
 import { contentServiceLoad } from "../../store/actions/content";
+import { updateOrganizations } from "../../store/actions/organizations";
 import { loadService } from "../../store/actions/services";
+import {
+  organizationNamesByFiscalCodeSelector,
+  OrganizationNamesByFiscalCodeState
+} from "../../store/reducers/entities/organizations/organizationsByFiscalCodeReducer";
+import {
+  visibleServicesSelector,
+  VisibleServicesState
+} from "../../store/reducers/entities/services/visibleServices";
 import { SagaCallReturnType } from "../../types/utils";
+import { isVisibleService } from "../../utils/services";
 
 /**
  * A generator to load the service details from the Backend
@@ -28,6 +39,28 @@ export function* loadServiceRequestHandler(
     }
 
     if (response.value.status === 200) {
+      // If the organization fiscal code is associated to different organizazion names,
+      // it is considered valid the one declared for a visible service
+      const visibleServices: VisibleServicesState = yield select(
+        visibleServicesSelector
+      );
+      const service = pot.some(response.value.value);
+      const isVisible = isVisibleService(visibleServices, service) || false;
+
+      const organizations: OrganizationNamesByFiscalCodeState = yield select(
+        organizationNamesByFiscalCodeSelector
+      );
+      const fc = service.value.organization_fiscal_code;
+
+      if (organizations) {
+        const organization = organizations[fc];
+        // If the organization has been previously saved in the organization entity,
+        // the organization name  is updated only if the related service is visible
+        if (!organization || (organization && isVisible)) {
+          yield put(updateOrganizations(response.value.value));
+        }
+      }
+
       yield put(loadService.success(response.value.value));
 
       // Once the service content is loaded, the service metadata loading is requested.
