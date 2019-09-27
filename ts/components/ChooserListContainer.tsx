2ddc8711e3d8beadb8b45cb1228f21e568cb7df5
Alex193a <alessandro.paluzzi@progettopa.it>
2019-09-27 16:09:55 +0200

    [#168784014] Fixed the keyboard in SPID test login screen
    Fix for iOS.

--- ts/components/ChooserListContainer.tsx
+++ ts/components/ChooserListContainer.tsx
@@ -287,7 +287,7 @@ class ChooserListContainer<T> extends React.PureComponent<Props<T>, State> {
           </View>
         </Content>
         <KeyboardAvoidingView
-          behavior="height"
+          behavior={Platform.OS === "android" ? "height" : "padding"}
           keyboardVerticalOffset={Platform.select({
             ios: 0,
             android: customVariables.contentPadding
