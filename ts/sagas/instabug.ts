0c41af4bc5a7a51bd244ed23e17236c0bdba4a63
Matteo Gentili <matteo.gentili@progettopa.it>
2019-10-14 11:01:18 +0200

    [#168453533] Fix saga
    Fix saga

--- ts/sagas/instabug.ts
+++ ts/sagas/instabug.ts
@@ -1,37 +1,32 @@
+/* tslint:disable */
 import { Replies } from "instabug-reactnative";
 import { call, Effect, put } from "redux-saga/effects";
 import { responseInstabugInfoLoaded } from "../store/actions/instabug";
 import { startTimer } from "../utils/timer";
 
 // load instabug's number messages every five minutes
-// 60 * 5 * 1000;
-const INSTABUG_INFO_LOAD_INTERVAL = 3 * 1000;
-
+const INSTABUG_INFO_LOAD_INTERVAL = 60 * 5 * 1000;
+var repliesCount = 0;
 export function* watchInstabugSaga(
   retryPeriodically: boolean = false
 ): IterableIterator<Effect> {
   if (!retryPeriodically) {
-    // tslint:disable-next-line:no-commented-code
-    // console.warn("custom dispatch");
+
   }
   // tslint:disable-next-line:no-commented-code
   // console.warn("get repliesCount");
-  var repliesCount = 0;
+
   Replies.getUnreadRepliesCount(count => {
     repliesCount = count;
   });
   yield put(responseInstabugInfoLoaded(repliesCount));
   while (retryPeriodically) {
-    // tslint:disable-next-line:no-commented-code
-    // console.warn("start waiting");
+    // "start waiting"
     yield call(startTimer, INSTABUG_INFO_LOAD_INTERVAL);
-    // tslint:disable-next-line:no-commented-code
-    // console.warn("stop waiting");
-    // console.warn("get repliesCount");
-    var repliesCountPeriodically = 0;
+    // "stop waiting"
     Replies.getUnreadRepliesCount(count => {
-      repliesCountPeriodically = count;
+      repliesCount = count;
     });
-    yield put(responseInstabugInfoLoaded(repliesCountPeriodically));
+    yield put(responseInstabugInfoLoaded(repliesCount));
   }
 }
