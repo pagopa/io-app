fb256b0029f0b5e2daa3791c99bf5480598174f4
Matteo Gentili <matteo.gentili@progettopa.it>
2019-10-18 11:52:30 +0200

    [#168453533] Fix
    Fix

--- ts/store/actions/instabug.ts
+++ ts/store/actions/instabug.ts
@@ -4,7 +4,7 @@
 import { ActionType, createStandardAction } from "typesafe-actions";
 
 export const instabugUnreadMessagesLoaded = createStandardAction(
-  "RESPONSE_INSTABUG_INFO_LOADED"
+  "INSTABUG_UNREAD_MESSAGES_LOADED"
 )<number>();
 
 export type InstabugInfoActions = ActionType<
