84f7e797f28617925d33d3b1e18c28d13d37929e
Matteo Gentili <43071536+matgentili@users.noreply.github.com>
2019-10-18 11:59:28 +0200

    [#168453533] Add Instabug badge unread messages (#1369)
    * [#168453533] Implementation of IB badge
    
    Implementation of IB badge
    
    * Update config.yml
    
    * [#168453533] Fix
    
    Fix
    
    * [#168453533] Fix TabIconComponent
    
    Fix TabIconComponent
    
    * Update instabug-reactnative+8.4.1.patch
    
    * Update config.yml
    
    For invalidate cache
    
    * [#168453533] Fix after review
    
    Fix after review
    
    * [#168453533] Fix import
    
    Fix import
    
    * [#168453533] Fix
    
    Fix
    
    * [#168453533] Fix
    
    Fix

---
+++ ts/components/ui/CustomBadge.tsx
@@ -0,0 +1,29 @@
+import { Badge, Text } from "native-base";
+import React from "react";
+import { StyleProp, ViewStyle } from "react-native";
+
+type Props = {
+  badgeStyle: StyleProp<ViewStyle>;
+  textStyle: StyleProp<ViewStyle>;
+  badgeValue: number;
+};
+
+const MAX_BADGE_VALUE = 99;
+
+/**
+ * A simple badge used for show the number of messages to read
+ *
+ */
+export default class CustomBadge extends React.PureComponent<Props> {
+  public render() {
+    return (
+      this.props.badgeValue > 0 && (
+        <Badge style={this.props.badgeStyle}>
+          <Text badge={true} style={this.props.textStyle}>
+            {Math.min(this.props.badgeValue, MAX_BADGE_VALUE)}
+          </Text>
+        </Badge>
+      )
+    );
+  }
+}
