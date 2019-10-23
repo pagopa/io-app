dd4fdc9b5aa2e305fbd99d0224b2962e9fa76218
valentinacala <valentina.cala@outlook.com>
2019-10-23 12:29:55 +0200

    remove persistance of the navigation history

--- ts/sagas/startup/watchSessionExpiredSaga.ts
+++ ts/sagas/startup/watchSessionExpiredSaga.ts
@@ -1,19 +1,22 @@
 import { Effect } from "redux-saga";
-import { call, put, takeLatest } from "redux-saga/effects";
+import { put, takeLatest } from "redux-saga/effects";
 import { getType } from "typesafe-actions";
-
 import { startApplicationInitialization } from "../../store/actions/application";
 import { sessionExpired } from "../../store/actions/authentication";
-
-import { saveNavigationStateSaga } from "./saveNavigationStateSaga";
+import { resetToAuthenticationRoute } from "../../store/actions/navigation";
 
 /**
  * Handles the expiration of session while the user is using the app.
  */
 export function* watchSessionExpiredSaga(): IterableIterator<Effect> {
   yield takeLatest(getType(sessionExpired), function*() {
+    /**
+     * TODO: clarify what use case the saveNavigationStateSaga should cover
+     */
     // Save the navigation state
-    yield call(saveNavigationStateSaga);
+    // yield call(saveNavigationStateSaga);
+
+    yield put(resetToAuthenticationRoute);
 
     // Re-initialize the app
     // Since there was a SESSION_EXPIRED action, the user will be asked to
