9e277f57c7ad9b434aaa77348de0086c1a3cff86
valentinacala <valentina.cala@outlook.com>
2019-10-25 11:37:57 +0200

    add navigation constant

--- ts/store/actions/navigation.ts
+++ ts/store/actions/navigation.ts
@@ -6,15 +6,9 @@ import {
   StackActions
 } from "react-navigation";
 import { ActionType, createStandardAction } from "typesafe-actions";
-
-import { InferNavigationParams } from "../../types/react";
-
 import ROUTES from "../../navigation/routes";
-
-import { FingerprintScreen } from "../../screens/onboarding/FingerprintScreen";
-
 import { MessageDetailScreen } from "../../screens/messages/MessageDetailScreen";
-
+import { FingerprintScreen } from "../../screens/onboarding/FingerprintScreen";
 import ServiceDetailsScreen from "../../screens/services/ServiceDetailsScreen";
 import AddCardScreen from "../../screens/wallet/AddCardScreen";
 import AddPaymentMethodScreen from "../../screens/wallet/AddPaymentMethodScreen";
@@ -27,6 +21,7 @@ import TransactionErrorScreen from "../../screens/wallet/payment/TransactionErro
 import TransactionSummaryScreen from "../../screens/wallet/payment/TransactionSummaryScreen";
 import TransactionDetailsScreen from "../../screens/wallet/TransactionDetailsScreen";
 import TransactionsScreen from "../../screens/wallet/TransactionsScreen";
+import { InferNavigationParams } from "../../types/react";
 
 export const navigationRestore = createStandardAction("NAVIGATION_RESTORE")<
   NavigationState
@@ -56,6 +51,13 @@ export const navigateToMainNavigatorAction = StackActions.reset({
       routeName: ROUTES.MAIN
     })
   ]
+});
+
+export const navigateToIdpSelectionScreenAction = NavigationActions.navigate({
+  routeName: ROUTES.AUTHENTICATION,
+  action: NavigationActions.navigate({
+    routeName: ROUTES.AUTHENTICATION_IDP_SELECTION
+  })
 });
 
 export const navigateToOnboardingPinScreenAction = NavigationActions.navigate({
