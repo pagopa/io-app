a221655c593d97e50ac0e6e279f2572bbc6f7370
Matteo Gentili <matteo.gentili@progettopa.it>
2019-10-28 11:33:11 +0100

    [#169167649] Invalid NFC connection screen
    Invalid NFC connection screen

---
+++ locales/en/cie/noNfcConnection.md
@@ -0,0 +1,3 @@
+It appears that your smartphone has not detected a valid card.  
+Rest it on the back of the device and try again. When the phone recognizes the card, it will still need a few seconds to read the data.  
+If you don't know where the NFC antenna is on your device, try moving the card on the back until the app tells you to hold it in place.
---
+++ ts/screens/authentication/InvalidNfcConnectionScreen.tsx
@@ -0,0 +1,68 @@
+/**
+ * A screen where the user is warned of a problem with the NFC connection.
+ */
+import { Container, H2, View } from "native-base";
+import * as React from "react";
+import { NavigationScreenProp, NavigationState } from "react-navigation";
+
+import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
+import FooterWithButtons from "../../components/ui/FooterWithButtons";
+import Markdown from "../../components/ui/Markdown";
+import I18n from "../../i18n";
+
+type OwnProps = {
+  navigation: NavigationScreenProp<NavigationState>;
+};
+
+type Props = OwnProps;
+
+class InvalidNfcConnectionScreen extends React.Component<Props, never> {
+  /**
+   * Footer
+   */
+  private renderFooterButtons() {
+    const cancelButtonProps = {
+      cancel: true,
+      block: true,
+      onPress: (): boolean => this.props.navigation.goBack(),
+      title: I18n.t("global.buttons.cancel")
+    };
+    const retryButtonProps = {
+      block: true,
+      primary: true,
+      onPress: undefined,
+      title: I18n.t("global.buttons.retry")
+    };
+
+    return (
+      <FooterWithButtons
+        type="TwoButtonsInlineThird"
+        leftButton={cancelButtonProps}
+        rightButton={retryButtonProps}
+      />
+    );
+  }
+
+  public render() {
+    return (
+      <Container>
+        <BaseScreenComponent goBack={true}>
+          <Container>
+            <View style={{ margin: 24 }}>
+              <H2>
+                {I18n.t("authentication.landing.cie.noNfcConnectionTitle")}
+              </H2>
+              <Markdown>
+                {I18n.t("authentication.landing.cie.noNfcConnectionContent")}
+              </Markdown>
+            </View>
+          </Container>
+        </BaseScreenComponent>
+        {this.renderFooterButtons()}
+        <View />
+      </Container>
+    );
+  }
+}
+
+export default InvalidNfcConnectionScreen;
