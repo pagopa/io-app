25ae3845fd161bbd2bf169217f79e2ad925f2640
Alex193a <paluzzi.alessandro193a@gmail.com>
2019-11-08 15:00:31 +0100

    [#169626834] Added Markdown list in SPID/CIE Information screen
    Added the Markdown list in the SPID/CIE Information screen

--- ts/screens/authentication/SpidCIEInformationScreen.tsx
+++ ts/screens/authentication/SpidCIEInformationScreen.tsx
@@ -3,12 +3,9 @@
  */
 import {
   Button,
-  Col,
   Container,
   Content,
-  Grid,
   H2,
-  Row,
   Tab,
   Tabs,
   Text,
@@ -53,20 +50,6 @@ const styles = StyleSheet.create({
 });
 
 class SpidInformationScreen extends React.Component<Props, never> {
-  private getValueContent(value: string, content: string) {
-    return (
-      <Row style={styles.row}>
-        <Col size={1}>
-          <H2 style={styles.value}>{value}</H2>
-        </Col>
-        <Col size={5}>
-          <Text>{content}</Text>
-        </Col>
-        <Col size={2} />
-      </Row>
-    );
-  }
-
   private browseToLink(url: string) {
     Linking.openURL(url).catch(() => {
       Toast.show({ text: I18n.t("genericError") });
@@ -89,30 +72,9 @@ class SpidInformationScreen extends React.Component<Props, never> {
           >
             <Tab heading={I18n.t("authentication.spid")}>
               <Content>
-                <Markdown>
+                <Markdown useCustomSortedList={true}>
                   {I18n.t("authentication.spid_information.spid")}
                 </Markdown>
-                <Grid>
-                  {this.getValueContent(
-                    I18n.t("authentication.spid_information.point1-value"),
-                    I18n.t("authentication.spid_information.point1-content")
-                  )}
-
-                  {this.getValueContent(
-                    I18n.t("authentication.spid_information.point2-value"),
-                    I18n.t("authentication.spid_information.point2-content")
-                  )}
-
-                  {this.getValueContent(
-                    I18n.t("authentication.spid_information.point3-value"),
-                    I18n.t("authentication.spid_information.point3-content")
-                  )}
-
-                  {this.getValueContent(
-                    I18n.t("authentication.spid_information.point4-value"),
-                    I18n.t("authentication.spid_information.point4-content")
-                  )}
-                </Grid>
                 <View spacer={true} extralarge={true} />
               </Content>
               <View footer={true}>
