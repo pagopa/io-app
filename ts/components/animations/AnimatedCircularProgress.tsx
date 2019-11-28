9eb384c2f5770392ef800ef18f7ed0798751baeb
Edigio <egidio.filosa@gmail.com>
2019-11-28 11:52:00 +0100

    [#169167557] Fixed overflow circular progress anim
    Fixed overflow circular progress anim

--- ts/components/animations/AnimatedCircularProgress.tsx
+++ ts/components/animations/AnimatedCircularProgress.tsx
@@ -5,9 +5,13 @@ import themeVariables from "../../theme/variables";
 
 const styles = StyleSheet.create({
   ring: {
-    position: "absolute",
-    flexDirection: "column",
+    flexDirection: "column"
+  },
+  ringContainer: {
+    flexDirection: "row",
+    alignItems: "flex-start",
     overflow: "hidden",
+    position: "absolute",
     padding: -2
   }
 });
@@ -38,61 +42,41 @@ export default class AnimatedCircularProgress extends React.Component<Props> {
   }
 
   public render() {
+    const RotateData = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"]
+    });
     return (
-      <Animated.View
+      <View
         style={[
-          styles.ring,
           {
             width: this.props.dimension,
             height: this.props.dimension,
             borderRadius: this.props.dimension / 2
           },
-          {
-            transform: [
-              {
-                rotate: this.animatedValue.interpolate({
-                  inputRange: [0, 1],
-                  outputRange: ["0deg", "360deg"]
-                })
-              }
-            ]
-          }
+          styles.ringContainer
         ]}
       >
-        <View
-          style={{
-            flexDirection: "row",
-            backgroundColor: themeVariables.brandPrimary
-          }}
+        <Animated.View
+          style={[
+            styles.ring,
+            {
+              width: this.props.dimension,
+              height: this.props.dimension,
+              borderRadius: this.props.dimension / 2,
+              backgroundColor: themeVariables.brandPrimary,
+              flexDirection: "row",
+              alignItems: "flex-start"
+            },
+            {
+              transform: [
+                {
+                  rotate: RotateData
+                }
+              ]
+            }
+          ]}
         >
-          <View
-            style={{
-              width: this.props.dimension / 2,
-              height: this.props.dimension / 2,
-              backgroundColor: themeVariables.brandPrimary
-            }}
-          />
-          <View
-            style={{
-              width: this.props.dimension / 2,
-              height: this.props.dimension / 2,
-              backgroundColor: themeVariables.brandPrimary
-            }}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            backgroundColor: themeVariables.brandPrimary
-          }}
-        >
-          <View
-            style={{
-              width: this.props.dimension / 2,
-              height: this.props.dimension / 2,
-              backgroundColor: themeVariables.brandPrimary
-            }}
-          />
           <View
             style={{
               width: this.props.dimension / 2,
@@ -100,8 +84,8 @@ export default class AnimatedCircularProgress extends React.Component<Props> {
               backgroundColor: themeVariables.brandLightGray
             }}
           />
-        </View>
-      </Animated.View>
+        </Animated.View>
+      </View>
     );
   }
 }
