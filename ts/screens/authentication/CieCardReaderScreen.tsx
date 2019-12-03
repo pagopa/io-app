ef7472daa22c024a1fff6d04251628e9c688ee9e
Edigio <egidio.filosa@gmail.com>
2019-12-03 17:00:28 +0100

    [#169167557] Changed animation with a new module
    Changed animation with a new module

--- ts/screens/authentication/CieCardReaderScreen.tsx
+++ ts/screens/authentication/CieCardReaderScreen.tsx
@@ -1,9 +1,8 @@
-import { Millisecond } from "italia-ts-commons/lib/units";
 import { Button, Content, H2, Text, View } from "native-base";
 import * as React from "react";
 import { Image, StyleSheet } from "react-native";
+import ProgressCircle from "react-native-progress-circle";
 import { NavigationScreenProp, NavigationState } from "react-navigation";
-import AnimatedCircularProgress from "../../components/animations/AnimatedCircularProgress";
 import ScreenHeader from "../../components/ScreenHeader";
 import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
 import I18n from "../../i18n";
@@ -16,6 +15,12 @@ interface OwnProps {
 type Props = OwnProps;
 // Image dimension
 const imgDimension = 180;
+/** Every stepTimeProgress update circular progress, max value progress is 100, example
+ *  50: the animation ending in 16 seconds
+ *  60: the animation ending in 19 seconds
+ *  ecc ecc
+ */
+const stepTimeProgress = 50;
 
 const styles = StyleSheet.create({
   messageHeader: {
@@ -48,45 +53,83 @@ const styles = StyleSheet.create({
   }
 });
 
+type State = {
+  progressBarValue: number;
+};
+
 /**
  *  This screen shown while reading the card
  */
-const CieCardReaderScreen: React.SFC<Props> = props => {
+class CieCardReaderScreen extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      progressBarValue: 0
+    };
+    // Start an interval to increment progress bar
+    const interval = setInterval(() => {
+      // After 60% decrease velocity
+      if (this.state.progressBarValue < 60) {
+        this.setState({
+          progressBarValue: this.state.progressBarValue + 0.5
+        });
+      } else {
+        this.setState({
+          progressBarValue: this.state.progressBarValue + 0.2
+        });
+      }
+      // Stop interval
+      if (this.state.progressBarValue >= 100) {
+        clearInterval(interval);
+      }
+    }, stepTimeProgress);
+  }
   // With the AnimatedCircularProgress component we can represent a circular progress
-  return (
-    <BaseScreenComponent goBack={true}>
-      <Content noPadded={true} bounces={false}>
-        <ScreenHeader
-          heading={
-            <H2 style={styles.titleHeader}>
-              {I18n.t("authentication.cie.readerCardTitle")}
-            </H2>
-          }
-        />
-        <Text style={styles.messageHeader}>
-          {I18n.t("authentication.cie.readerCardHeader")}
-        </Text>
-        <View style={styles.imgContainer}>
-          <AnimatedCircularProgress
-            dimension={imgDimension}
-            duration={3000 as Millisecond}
+  public render(): React.ReactNode {
+    return (
+      <BaseScreenComponent goBack={true}>
+        <Content noPadded={true} bounces={false}>
+          <ScreenHeader
+            heading={
+              <H2 style={styles.titleHeader}>
+                {I18n.t("authentication.cie.readerCardTitle")}
+              </H2>
+            }
           />
-          <Image
-            source={require("../../../img/landing/place-card-illustration.png")}
-            style={styles.img}
-          />
+          <Text style={styles.messageHeader}>
+            {I18n.t("authentication.cie.readerCardHeader")}
+          </Text>
+          <View style={styles.imgContainer}>
+            <ProgressCircle
+              percent={this.state.progressBarValue}
+              radius={imgDimension / 2}
+              borderWidth={3}
+              color={customVariables.brandPrimary}
+              shadowColor={customVariables.brandLightGray}
+              bgColor={customVariables.brandLightGray}
+            >
+              <Image
+                source={require("../../../img/landing/place-card-illustration.png")}
+                style={styles.img}
+              />
+            </ProgressCircle>
+          </View>
+          <Text style={styles.messageFooter}>
+            {I18n.t("authentication.cie.readerCardFooter")}
+          </Text>
+        </Content>
+        <View footer={true}>
+          <Button
+            onPress={this.props.navigation.goBack}
+            cancel={true}
+            block={true}
+          >
+            <Text>{I18n.t("global.buttons.cancel")}</Text>
+          </Button>
         </View>
-        <Text style={styles.messageFooter}>
-          {I18n.t("authentication.cie.readerCardFooter")}
-        </Text>
-      </Content>
-      <View footer={true}>
-        <Button onPress={props.navigation.goBack} cancel={true} block={true}>
-          <Text>{I18n.t("global.buttons.cancel")}</Text>
-        </Button>
-      </View>
-    </BaseScreenComponent>
-  );
-};
+      </BaseScreenComponent>
+    );
+  }
+}
 
 export default CieCardReaderScreen;
