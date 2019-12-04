f046908b3a95596960a1608c75de75cbc65f3f18
Edigio <egidio.filosa@gmail.com>
2019-12-04 11:49:01 +0100

    [#169167557] Change animation when the reader is interrupted
    Change animation when the reader is interrupted

--- ts/screens/authentication/CieCardReaderScreen.tsx
+++ ts/screens/authentication/CieCardReaderScreen.tsx
@@ -1,8 +1,10 @@
+import { Millisecond } from "italia-ts-commons/lib/units";
 import { Button, Content, H2, Text, View } from "native-base";
 import * as React from "react";
 import { Image, StyleSheet } from "react-native";
 import ProgressCircle from "react-native-progress-circle";
 import { NavigationScreenProp, NavigationState } from "react-navigation";
+import AnimatedRing from "../../components/animations/AnimatedRing";
 import ScreenHeader from "../../components/ScreenHeader";
 import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
 import I18n from "../../i18n";
@@ -15,6 +17,7 @@ interface OwnProps {
 type Props = OwnProps;
 // Image dimension
 const imgDimension = 180;
+const boxDimension = 245;
 /** Every stepTimeProgress update circular progress, max value progress is 100, example
  *  50: the animation ending in 16 seconds
  *  60: the animation ending in 19 seconds
@@ -24,17 +27,16 @@ const stepTimeProgress = 50;
 
 const styles = StyleSheet.create({
   messageHeader: {
+    minHeight: 90,
     paddingRight: customVariables.contentPadding,
     paddingLeft: customVariables.contentPadding,
     paddingTop: customVariables.contentPadding,
-    paddingBottom: customVariables.contentPadding / 2,
     fontSize: customVariables.fontSizeBase
   },
   messageFooter: {
     paddingRight: customVariables.contentPadding,
     paddingLeft: customVariables.contentPadding,
     paddingBottom: customVariables.contentPadding,
-    paddingTop: customVariables.contentPadding / 2,
     fontSize: customVariables.fontSizeBase
   },
   titleHeader: {
@@ -42,7 +44,8 @@ const styles = StyleSheet.create({
   },
   imgContainer: {
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    height: boxDimension
   },
   img: {
     overflow: "hidden",
@@ -50,6 +53,14 @@ const styles = StyleSheet.create({
     height: imgDimension - 3,
     width: imgDimension - 3,
     borderRadius: imgDimension / 2
+  },
+  rings: {
+    height: boxDimension,
+    width: boxDimension,
+    position: "absolute",
+    overflow: "hidden",
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
 
@@ -86,27 +97,75 @@ class CieCardReaderScreen extends React.Component<Props, State> {
         });
       }
       // Stop interval
-      if (this.state.progressBarValue >= 100) {
+      if (this.state.progressBarValue >= 100 || this.state.isReadInterrupted) {
         clearInterval(interval);
       }
     }, stepTimeProgress);
+
+    // TODO: remove this!!
+    // Simulates user interruption
+    // tslint:disable-next-line: no-commented-code
+    /*  setTimeout(() => {
+      this.setState({
+        isReadInterrupted: true
+      });
+    }, 5000); */
   }
-  // With the AnimatedCircularProgress component we can represent a circular progress
+
   public render(): React.ReactNode {
+    // Setting for 'radar' animation
+    const ringSettings = {
+      dimension: imgDimension,
+      // Three different animation start delays (one is 0), one for each ring
+      delayX1: 700 as Millisecond,
+      delayX2: 1400 as Millisecond,
+      duration: 2100 as Millisecond
+    };
+
     return (
       <BaseScreenComponent goBack={true}>
         <Content noPadded={true} bounces={false}>
           <ScreenHeader
             heading={
               <H2 style={styles.titleHeader}>
-                {I18n.t("authentication.cie.readerCardTitle")}
+                {this.state.isReadInterrupted
+                  ? I18n.t("authentication.cie.readerCardLostTitle")
+                  : I18n.t("authentication.cie.readerCardTitle")}
               </H2>
             }
           />
           <Text style={styles.messageHeader}>
-            {I18n.t("authentication.cie.readerCardHeader")}
+            {this.state.isReadInterrupted
+              ? I18n.t("authentication.cie.readerCardLostHeader")
+              : I18n.t("authentication.cie.readerCardHeader")}
           </Text>
           <View style={styles.imgContainer}>
+            <View style={styles.rings}>
+              {this.state.isReadInterrupted && (
+                <AnimatedRing
+                  dimension={ringSettings.dimension}
+                  startAnimationAfter={0 as Millisecond}
+                  duration={ringSettings.duration}
+                  boxDimension={boxDimension}
+                />
+              )}
+              {this.state.isReadInterrupted && (
+                <AnimatedRing
+                  dimension={ringSettings.dimension}
+                  startAnimationAfter={ringSettings.delayX1}
+                  duration={ringSettings.duration}
+                  boxDimension={boxDimension}
+                />
+              )}
+              {this.state.isReadInterrupted && (
+                <AnimatedRing
+                  dimension={ringSettings.dimension}
+                  startAnimationAfter={ringSettings.delayX2}
+                  duration={ringSettings.duration}
+                  boxDimension={boxDimension}
+                />
+              )}
+            </View>
             <ProgressCircle
               percent={this.state.progressBarValue}
               radius={imgDimension / 2}
@@ -126,7 +185,9 @@ class CieCardReaderScreen extends React.Component<Props, State> {
             </ProgressCircle>
           </View>
           <Text style={styles.messageFooter}>
-            {I18n.t("authentication.cie.readerCardFooter")}
+            {this.state.isReadInterrupted
+              ? ""
+              : I18n.t("authentication.cie.readerCardFooter")}
           </Text>
         </Content>
         <View footer={true}>
