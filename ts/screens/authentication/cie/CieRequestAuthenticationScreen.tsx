c826d78db4332afe29424a71c9c2329ea270c228
Matteo Boschi <mbmaken@gmail.com>
2019-12-05 15:02:47 +0100

    [#170046248] wip

--- ts/screens/authentication/cie/CieRequestAuthenticationScreen.tsx
+++ ts/screens/authentication/cie/CieRequestAuthenticationScreen.tsx
@@ -1,18 +1,23 @@
 import { Container, H1, Text, View } from "native-base";
 import * as React from "react";
-import { StyleSheet, NavState } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationScreenProp, NavigationState } from "react-navigation";
+import { connect } from "react-redux";
 import ScreenHeader from "../../../components/ScreenHeader";
 import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
 import FooterWithButtons from "../../../components/ui/FooterWithButtons";
 import I18n from "../../../i18n";
 import ROUTES from "../../../navigation/routes";
+import { ReduxProps } from "../../../store/actions/types";
+import { GlobalState } from "../../../store/reducers/types";
 import variables from "../../../theme/variables";
 import { isNfcEnabled, openNFCSettings } from "../../../utils/cie";
 
 type Props = Readonly<{
   navigation: NavigationScreenProp<NavigationState>;
-}>;
+}> &
+  ReturnType<typeof mapStateToProps> &
+  ReduxProps;
 
 type State = {
   isNfcEnabled: boolean;
@@ -27,11 +32,46 @@ const styles = StyleSheet.create({
   }
 });
 
-export class CieNfcScreen extends React.Component<Props, State> {
+class CieRequestAuthenticationScreen extends React.Component<Props, State> {
   private idInterval?: number;
   constructor(props: Props) {
     super(props);
+    this.state = { isNfcEnabled: false };
   }
+
+  private updateNfcState = async () => {
+    const isNfcOn = await isNfcEnabled();
+    if (this.state.isNfcEnabled !== isNfcOn) {
+      this.setState({ isNfcEnabled: isNfcOn });
+    }
+  };
+
+  private checkNfcState = async () => {
+    // it is executed repeatedly to check the nfc state
+    // we avoid to listen app state change because in most case, when user opens
+    // the nfc settings and then comes back the app doesn't change state
+    // tslint:disable-next-line: no-object-mutation
+    this.idInterval = setTimeout(async () => {
+      await this.updateNfcState();
+      await this.checkNfcState();
+    }, 300);
+  };
+
+  public async componentDidMount() {
+    await this.checkNfcState();
+  }
+
+  public componentWillUnmount() {
+    clearTimeout(this.idInterval);
+  }
+
+  private handleOnPressActivateNFC = async () => {
+    await openNFCSettings();
+  };
+
+  private handleOnPressContinue = () => {
+    this.props.navigation.navigate(ROUTES.CIE_PIN_SCREEN);
+  };
 
   private renderFooter = () => {
     if (this.state.isNfcEnabled) {
@@ -64,7 +104,7 @@ export class CieNfcScreen extends React.Component<Props, State> {
           }}
           rightButton={{
             cancel: false,
-            onPress: undefined,
+            onPress: this.handleOnPressActivateNFC,
             title: I18n.t("authentication.cie.enableNfcTitle"),
             block: true
           }}
@@ -73,25 +113,6 @@ export class CieNfcScreen extends React.Component<Props, State> {
     }
   };
 
-  private handleNavigationStateChange = (event: NavState): void => {
-    if (event.url && event.url.indexOf("OpenApp") !== -1) {
-    }
-  };
-
-  private renderWebView() {
-    return (
-      <WebView
-        thirdPartyCookiesEnabled={true}
-        style={{ flex: 1 }}
-        javaScriptEnabled={true}
-        onError={this.handleError}
-        onNavigationStateChange={this.handleNavigationStateChange}
-        source={{
-          uri: this.state.webUrl
-        }}
-      />
-    );
-  }
   public render(): React.ReactNode {
     const heading = this.state.isNfcEnabled
       ? I18n.t("authentication.cie.nfcEnabledTitle")
@@ -103,10 +124,22 @@ export class CieNfcScreen extends React.Component<Props, State> {
 
     return (
       <Container>
-        <BaseScreenComponent goBack={true} />
+        <BaseScreenComponent goBack={true}>
+          <ScreenHeader
+            heading={<H1>{heading}</H1>}
+            icon={require("../../../../img/icons/nfc-icon.png")}
+          />
+          <View style={styles.contentContainerStyle}>
+            <Text style={styles.text}>{content}</Text>
+          </View>
+        </BaseScreenComponent>
         {this.renderFooter()}
-        {this.renderWebView()}
       </Container>
     );
   }
 }
+
+const mapStateToProps = (state: GlobalState) => ({
+  appState: state.appState.appState
+});
+export default connect(mapStateToProps)(CieRequestAuthenticationScreen);
