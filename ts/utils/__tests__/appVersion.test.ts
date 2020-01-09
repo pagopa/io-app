0a80205df70874fda60c11fb9afc2cfd853542db
Matteo Boschi <mbmaken@gmail.com>
2020-01-09 20:24:48 +0100

    [#167319140] add sem version 5.5.1

--- ts/utils/__tests__/appVersion.test.ts
+++ ts/utils/__tests__/appVersion.test.ts
@@ -6,18 +6,18 @@ describe("appVersion", () => {
   });
 
   it("is not supported", () => {
-    expect(isVersionAppSupported("1.4.5", "1.4.1")).toEqual(true);
+    expect(isVersionAppSupported("1.4.5", "1.4.1")).toEqual(false);
   });
 
   it("is not supported", () => {
-    expect(isVersionAppSupported("5", "1.4.1")).toEqual(true);
+    expect(isVersionAppSupported("5", "1.4.1")).toEqual(false);
   });
 
   it("is not supported", () => {
-    expect(isVersionAppSupported("3.0", "1.4.1")).toEqual(true);
+    expect(isVersionAppSupported("3.0", "1.4.1")).toEqual(false);
   });
 
   it("If the backend-info is not available (es. request http error) continue to use app", () => {
-    expect(isVersionAppSupported(undefined, "1.2")).toEqual(false);
+    expect(isVersionAppSupported(undefined, "1.2")).toEqual(true);
   });
 });
--- ts/utils/appVersion.ts
+++ ts/utils/appVersion.ts
@@ -1,10 +1,19 @@
+import semver from "semver";
 // Check min version app supported
 export const isVersionAppSupported = (
   minAppVersion: string | undefined,
   DeviceVersion: string
 ): boolean => {
-  if (minAppVersion && DeviceVersion) {
+  // If the backend-info is not available (es. request http error) continue to use app
+  if (minAppVersion !== undefined) {
+    const minVersion =
+      semver.valid(minAppVersion) === null
+        ? semver.coerce(minAppVersion)
+        : minAppVersion;
+    return minVersion !== null
+      ? semver.satisfies(minVersion, `<=${DeviceVersion}`)
+      : true;
+  } else {
     return true;
   }
-  return false;
 };
