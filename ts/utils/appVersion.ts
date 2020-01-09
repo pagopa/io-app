74215094e183d9065ac4960cdfc39780f91c4a83
Matteo Boschi <mbmaken@gmail.com>
2020-01-09 20:17:42 +0100

    [#167319140] rollback

--- ts/utils/appVersion.ts
+++ ts/utils/appVersion.ts
@@ -4,16 +4,8 @@ export const isVersionAppSupported = (
   minAppVersion: string | undefined,
   DeviceVersion: string
 ): boolean => {
-  // If the backend-info is not available (es. request http error) continue to use app
-  if (minAppVersion !== undefined) {
-    const minVersion =
-      semver.valid(minAppVersion) === null
-        ? semver.coerce(minAppVersion)
-        : minAppVersion;
-    return minVersion !== null
-      ? semver.satisfies(minVersion, `<=${DeviceVersion}`)
-      : true;
-  } else {
+  if (minAppVersion && DeviceVersion) {
     return true;
   }
+  return falseM;
 };
