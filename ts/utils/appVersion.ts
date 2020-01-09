e2a1252d5d0f37a52d727d6bb66f6986e1f0a6a3
Matteo Boschi <mbmaken@gmail.com>
2020-01-09 20:44:07 +0100

    [#167319140] remove semver

--- ts/utils/appVersion.ts
+++ ts/utils/appVersion.ts
@@ -1,19 +1,9 @@
-import semver from "semver";
-// Check min version app supported
 export const isVersionAppSupported = (
   minAppVersion: string | undefined,
   DeviceVersion: string
 ): boolean => {
-  // If the backend-info is not available (es. request http error) continue to use app
-  if (minAppVersion !== undefined) {
-    const minVersion =
-      semver.valid(minAppVersion) === null
-        ? semver.coerce(minAppVersion)
-        : minAppVersion;
-    return minVersion !== null
-      ? semver.satisfies(minVersion, `<=${DeviceVersion}`)
-      : true;
-  } else {
+  if (minAppVersion && DeviceVersion) {
     return true;
   }
+  return false;
 };
