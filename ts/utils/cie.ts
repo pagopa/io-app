91e5a5432055c4d5bd6f45680aaabf1f1968e3ab
Matteo Boschi <mbmaken@gmail.com>
2020-02-04 20:35:58 +0100

    add launch cieid app

--- ts/utils/cie.ts
+++ ts/utils/cie.ts
@@ -2,13 +2,16 @@ import { Platform } from "react-native";
 import cieSdk from "react-native-cie";
 
 const realIsCIEAuthenticationSupported = cieSdk.isCIEAuthenticationSupported;
-const mockIsCIEAuthenticationSupported = () => Promise.resolve(true);
+const mockIsCIEAuthenticationSupported = () => Promise.resolve(false);
 
 const realIsNfcEnabled = () => cieSdk.isNFCEnabled();
 const mockIsNfcEnabled = () => Promise.resolve(true);
 
 const realOpenNfcSettings = cieSdk.openNFCSettings;
 const mockOpenNfcSettings = () => Promise.resolve();
+
+const realLaunchCieID = cieSdk.launchCieID;
+const mockLaunchCieID = () => Promise.reject("ops");
 
 const test = Platform.OS !== "android";
 
@@ -17,5 +20,5 @@ export const isCIEAuthenticationSupported = test
   : realIsCIEAuthenticationSupported;
 
 export const isNfcEnabled = test ? mockIsNfcEnabled : realIsNfcEnabled;
-
 export const openNFCSettings = test ? mockOpenNfcSettings : realOpenNfcSettings;
+export const launchCieID = test ? mockLaunchCieID : realLaunchCieID;
