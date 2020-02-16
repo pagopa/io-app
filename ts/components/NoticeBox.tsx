b4d57a2b6ace684dd2fd3109e357b5c629534d8a
Matteo Boschi <mbmaken@gmail.com>
2020-02-16 12:43:38 +0100

    [#171228169] removed unused imports

--- ts/components/NoticeBox.tsx
+++ ts/components/NoticeBox.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
-import { Col, Grid } from "react-native-easy-grid";
 import { IconProps } from "react-native-vector-icons/Icon";
 
 import IconFont from "./ui/IconFont";
