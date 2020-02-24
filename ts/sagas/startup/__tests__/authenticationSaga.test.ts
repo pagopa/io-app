2f9ace72aadbdb387135d6d81233d76918832ba9
Matteo Boschi <mbmaken@gmail.com>
2020-02-24 20:20:56 +0100

    restore code (alpha line test)

--- ts/sagas/startup/__tests__/authenticationSaga.test.ts
+++ ts/sagas/startup/__tests__/authenticationSaga.test.ts
@@ -1,14 +1,31 @@
 import { testSaga } from "redux-saga-test-plan";
-
-import { analyticsAuthenticationStarted } from "../../../store/actions/analytics";
-
+import { getType } from "typesafe-actions";
+import { removeScheduledNotificationAccessSpid } from "../../../boot/scheduleLocalNotifications";
+import {
+  analyticsAuthenticationCompleted,
+  analyticsAuthenticationStarted
+} from "../../../store/actions/analytics";
+import { loginSuccess } from "../../../store/actions/authentication";
+import { resetToAuthenticationRoute } from "../../../store/actions/navigation";
+import { SessionToken } from "../../../types/SessionToken";
 import { authenticationSaga } from "../authenticationSaga";
+
+const aSessionToken = "a_session_token" as SessionToken;
 
 describe("authenticationSaga", () => {
   it("should always navigate to authentication screen and return the session token on login success", () => {
     testSaga(authenticationSaga)
       .next()
       .put(analyticsAuthenticationStarted())
-      .next();
+      .next()
+      .put(resetToAuthenticationRoute)
+      .next()
+      .take(getType(loginSuccess))
+      .next(loginSuccess(aSessionToken))
+      .call(removeScheduledNotificationAccessSpid)
+      .next()
+      .put(analyticsAuthenticationCompleted())
+      .next()
+      .returns(aSessionToken);
   });
 });
