2facbf2243ab8b5e17c839cb890c6586f4459efa
Edigio <egidio.filosa@gmail.com>
2020-02-28 14:11:11 +0100

    [#170818734] Set the goback function also on the screen title label (#1575)
    Set the goback function also on the screen title label
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

--- ts/components/screens/BaseHeader.tsx
+++ ts/components/screens/BaseHeader.tsx
@@ -1,6 +1,6 @@
 import { Body, Left, Right, Text, View } from "native-base";
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableWithoutFeedback } from "react-native";
 import { connect } from "react-redux";
 
 import IconFont from "../../components/ui/IconFont";
@@ -60,9 +60,11 @@ class BaseHeaderComponent extends React.PureComponent<Props> {
             {body
               ? body
               : headerTitle && (
-                  <Text white={this.props.primary} numberOfLines={1}>
-                    {headerTitle}
-                  </Text>
+                  <TouchableWithoutFeedback onPress={goBack}>
+                    <Text white={this.props.primary} numberOfLines={1}>
+                      {headerTitle}
+                    </Text>
+                  </TouchableWithoutFeedback>
                 )}
           </Body>
         )}
