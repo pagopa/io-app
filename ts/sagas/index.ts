eb45f09baddee41d6a6033f7a2d45e16aaec0416
Matteo Boschi <mbmaken@gmail.com>
2020-03-03 11:28:37 +0100

    disabled network saga

--- ts/sagas/index.ts
+++ ts/sagas/index.ts
@@ -1,7 +1,6 @@
 /**
  * The root saga that forks and includes all the other sagas.
  */
-import { networkSaga } from "react-native-offline";
 import { all, call, Effect } from "redux-saga/effects";
 
 import backendInfoSaga from "./backendInfo";
@@ -15,7 +14,6 @@ import { loadSystemPreferencesSaga } from "./preferences";
 import { startupSaga } from "./startup";
 import { watchNavigateToDeepLinkSaga } from "./watchNavigateToDeepLinkSaga";
 
-import { apiUrlPrefix } from "../config";
 import {
   watchBackToEntrypointPaymentSaga,
   watchPaymentInitializeSaga
@@ -24,6 +22,8 @@ import {
 // Parameters used by the withNetworkConnectivity HOC of react-native-offline.
 // We use `withRedux: true` to store the network status in the redux store.
 // More info at https://github.com/rauliyohmc/react-native-offline#withnetworkconnectivity
+/* 
+import { apiUrlPrefix } from "../config";
 const connectionMonitorParameters = {
   withRedux: true,
   pingTimeout: 3000,
@@ -31,11 +31,14 @@ const connectionMonitorParameters = {
   shouldPing: true,
   pingInterval: 30000
 };
+*/
 
 export default function* root(): Iterator<Effect> {
   yield all([
     call(startupSaga),
-    //call(networkSaga, connectionMonitorParameters), // ERROR ON ANDROID
+    // this saga is temporary removed since it seems to not work properly
+    // TODO https://www.pivotaltracker.com/story/show/171597422
+    // call(networkSaga, connectionMonitorParameters),
     call(backendInfoSaga),
     call(unreadInstabugMessagesSaga),
     call(watchNavigateToDeepLinkSaga),
