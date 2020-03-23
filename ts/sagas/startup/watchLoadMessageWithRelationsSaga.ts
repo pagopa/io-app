66d06f5798ac20a40408fe10aabbff521c035f37
valentinacala <valentina.cala@outlook.com>
2020-03-23 10:29:21 +0100

    update error management

--- ts/sagas/startup/watchLoadMessageWithRelationsSaga.ts
+++ ts/sagas/startup/watchLoadMessageWithRelationsSaga.ts
@@ -22,32 +22,36 @@ export function* loadMessageWithRelationsSaga(
   // Extract the message id from the action payload
   const messageId = messageWithRelationsLoadRequest.payload;
 
-  const messageOrError: SagaCallReturnType<typeof loadMessage> = yield call(
-    loadMessage,
-    getMessage,
-    messageId
-  );
+  try{
+    const messageOrError: SagaCallReturnType<typeof loadMessage> = yield call(
+      loadMessage,
+      getMessage,
+      messageId
+    );
+  
+    if (messageOrError.isLeft()) {
+      throw  new Error(messageOrError.value.message)
+    }
+  
+    const message = messageOrError.value;
+    yield put(loadMessageWithRelations.success());
+  
+    const serviceById = serviceByIdSelector(message.sender_service_id);
+  
+    const potService: ReturnType<typeof serviceById> = yield select<GlobalState>(
+      serviceById
+    );
+  
+    // We have the message try to load also the sender service only if there's
+    // no such service or if we are already loading it
+    if (
+      potService === undefined ||
+      (!pot.isSome(potService) && !pot.isLoading(potService))
+    ) {
+      yield put(loadServiceDetail.request(message.sender_service_id));
+    }
 
-  if (messageOrError.isLeft()) {
-    yield put(loadMessageWithRelations.failure(messageOrError.value));
-    return;
-  }
-
-  const message = messageOrError.value;
-  yield put(loadMessageWithRelations.success());
-
-  const serviceById = serviceByIdSelector(message.sender_service_id);
-
-  const potService: ReturnType<typeof serviceById> = yield select<GlobalState>(
-    serviceById
-  );
-
-  // We have the message try to load also the sender service only if there's
-  // no such service or if we are already loading it
-  if (
-    potService === undefined ||
-    (!pot.isSome(potService) && !pot.isLoading(potService))
-  ) {
-    yield put(loadServiceDetail.request(message.sender_service_id));
+  }catch(e){
+    yield put(loadMessageWithRelations.failure(e));
   }
 }
