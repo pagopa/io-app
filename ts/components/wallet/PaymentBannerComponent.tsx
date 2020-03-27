75fd066bbfdc4504be864ca365ba0e268303bfb4
valentinacala <valentina.cala@outlook.com>
2020-03-27 23:21:25 +0100

    update payment banner

--- ts/components/wallet/PaymentBannerComponent.tsx
+++ ts/components/wallet/PaymentBannerComponent.tsx
@@ -1,92 +1,70 @@
-/**
- * This component displays a summary on the transaction.
- * Used for the screens from the identification of the transaction to the end of the procedure.
- */
-
-import {
-  AmountInEuroCents,
-  AmountInEuroCentsFromNumber
-} from "italia-pagopa-commons/lib/pagopa";
-import { Right, Text, View } from "native-base";
+import { Text, View } from "native-base";
 import * as React from "react";
 import { StyleSheet } from "react-native";
-import { Col, Grid, Row } from "react-native-easy-grid";
-
-import { EnteBeneficiario } from "../../../definitions/backend/EnteBeneficiario";
+import { ImportoEuroCents } from "../../../definitions/backend/ImportoEuroCents";
 import I18n from "../../i18n";
 import variables from "../../theme/variables";
-import { UNKNOWN_RECIPIENT } from "../../types/unknown";
-import { formatNumberAmount } from "../../utils/stringBuilder";
-import ButtonDefaultOpacity from "../ButtonDefaultOpacity";
-
-const styles = StyleSheet.create({
-  rightButtonText: {
-    paddingRight: 0
-  },
-
-  topContainer: {
-    backgroundColor: variables.brandDarkGray
-  },
-
-  paddedLR: {
-    paddingLeft: variables.contentPadding,
-    paddingRight: variables.contentPadding
-  },
-
-  textRight: {
-    textAlign: "right"
-  }
-});
+import { centsToAmount, formatNumberAmount } from "../../utils/stringBuilder";
 
 type Props = Readonly<{
   paymentReason: string;
-  currentAmount: AmountInEuroCents;
-  recipient?: EnteBeneficiario;
-  onCancel: () => void;
+  currentAmount: ImportoEuroCents; // from verifica
+  fee?: ImportoEuroCents;
 }>;
 
-const PaymentBannerComponent: React.SFC<Props> = props => {
-  const currentAmount = formatNumberAmount(
-    AmountInEuroCentsFromNumber.encode(props.currentAmount)
-  );
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: variables.contentPadding,
+    paddingTop: 12,
+    paddingBottom: 16,
+    backgroundColor: variables.brandDarkGray
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  smallText: {
+    fontSize: 14
+  }
+});
 
-  const recipient = props.recipient || UNKNOWN_RECIPIENT;
+/**
+ * This component displays a summary on the transaction.
+ * Used for the screens from the identification of the transaction to the end of the procedure.
+ * Fee is shown only when a method screen is selected
+ */
+const PaymentBannerComponent: React.SFC<Props> = props => {
+  const totalAmount = props.fee
+    ? props.currentAmount + props.fee
+    : props.currentAmount;
 
   return (
-    <Grid style={[styles.topContainer, styles.paddedLR]}>
-      <Row>
-        <Col size={2}>
-          <View spacer={true} />
-          <Text bold={true} white={true}>
-            {props.paymentReason}
-          </Text>
-        </Col>
-        <Col size={1}>
-          <View spacer={true} />
-          <Text bold={true} white={true} style={styles.textRight}>
-            {currentAmount}
-          </Text>
-        </Col>
-      </Row>
-      <Row>
-        <Col size={2}>
-          <Text white={true}>{recipient.denominazioneBeneficiario}</Text>
-          <View spacer={true} />
-        </Col>
-        <Right>
-          <ButtonDefaultOpacity
-            small={true}
-            transparent={true}
-            onPress={props.onCancel}
-          >
-            <Text white={true} style={styles.rightButtonText}>
-              {I18n.t("global.buttons.cancel").toUpperCase()}
-            </Text>
-          </ButtonDefaultOpacity>
-          <View spacer={true} />
-        </Right>
-      </Row>
-    </Grid>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text white={true} bold={true} style={styles.smallText}>
+          {props.paymentReason}
+        </Text>
+        <Text white={true} bold={true} style={styles.smallText}>
+          {formatNumberAmount(centsToAmount(props.currentAmount))}
+        </Text>
+      </View>
+      <View style={styles.row}>
+        <Text white={true} style={styles.smallText}>
+          {I18n.t("wallet.ConfirmPayment.fee")}
+        </Text>
+        <Text white={true} style={styles.smallText}>
+          {props.fee ? formatNumberAmount(centsToAmount(props.fee)) : "-"}
+        </Text>
+      </View>
+      <View style={styles.row}>
+        <Text white={true} bold={true} style={styles.smallText}>
+          {I18n.t("wallet.total")}
+        </Text>
+        <Text white={true} bold={true}>
+          {formatNumberAmount(centsToAmount(totalAmount))}
+        </Text>
+      </View>
+    </View>
   );
 };
 
