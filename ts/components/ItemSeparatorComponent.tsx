16050df937f5f78d2122b4e1fa22936e3ebf5c5b
valentinacala <valentina.cala@outlook.com>
2020-03-28 13:13:13 +0100

    update ItemSeparator

--- ts/components/ItemSeparatorComponent.tsx
+++ ts/components/ItemSeparatorComponent.tsx
@@ -3,17 +3,29 @@ import * as React from "react";
 import { StyleSheet } from "react-native";
 import customVariables from "../theme/variables";
 
+type Props = Readonly<{
+  noPadded?: boolean;
+}>;
+
 const styles = StyleSheet.create({
   itemSeparator: {
     backgroundColor: customVariables.itemSeparator,
-    height: 1 / 3,
-    marginLeft: customVariables.contentPadding,
-    marginRight: customVariables.contentPadding
+    height: 0.33
+  },
+  padded: {
+    paddingHorizontal: customVariables.contentPadding
   }
 });
 
-export default class ItemSeparatorComponent extends React.PureComponent<{}> {
+export default class ItemSeparatorComponent extends React.PureComponent<Props> {
   public render() {
-    return <View style={styles.itemSeparator} />;
+    return (
+      <React.Fragment>
+        <View
+          style={[styles.itemSeparator, !this.props.noPadded && styles.padded]}
+        />
+        ;
+      </React.Fragment>
+    );
   }
 }
