6109ba4095c520da9cc2e46d41565fccc16d72e8
Matteo Boschi <mbmaken@gmail.com>
2020-04-05 18:35:00 +0200

    [#171082956] add tests add util function to export payment data as readable string

---
+++ ts/store/reducers/__tests__/paymentsHistory.test.ts
@@ -0,0 +1,82 @@
+import { RptId } from "italia-pagopa-commons/lib/pagopa";
+import { paymentVerifica } from "../../actions/wallet/payment";
+import reducer, {
+  PaymentsHistoryState,
+  HISTORY_SIZE
+} from "../payments/history";
+import { PaymentRequestsGetResponse } from "../../../../definitions/backend/PaymentRequestsGetResponse";
+import { OrganizationFiscalCode } from "italia-ts-commons/lib/strings";
+import { range } from "fp-ts/lib/Array";
+// tslint:disable-next-line: no-let
+let state: PaymentsHistoryState = [];
+
+describe("payments history", () => {
+  const anRptId = RptId.decode({
+    organizationFiscalCode: "00000123456",
+    paymentNoticeNumber: {
+      applicationCode: "02",
+      auxDigit: "0",
+      checkDigit: "78",
+      iuv13: "1600203993985"
+    }
+  }).value as RptId;
+
+  const successData = PaymentRequestsGetResponse.decode({
+    importoSingoloVersamento: 1,
+    codiceContestoPagamento: "54ea05a0773011ea9dc75dfcb08809dc",
+    ibanAccredito: "IT93I0503457280000000117101",
+    causaleVersamento: "99 TEST_PAGOPASPA_IO VAIANO CREMASCO",
+    enteBeneficiario: {
+      identificativoUnivocoBeneficiario: "00122230196",
+      denominazioneBeneficiario: "Comune di Vaiano Cremasco",
+      indirizzoBeneficiario: "Piazza Gloriosi Caduti, 2",
+      capBeneficiario: "26010",
+      localitaBeneficiario: "Vaiano Cremasco",
+      provinciaBeneficiario: "CR",
+      nazioneBeneficiario: "IT"
+    }
+  }).value as PaymentRequestsGetResponse;
+
+  it("should add a payment in the history", () => {
+    state = reducer(state, paymentVerifica.request(anRptId));
+    expect(state.length).toEqual(1);
+  });
+
+  it("should not add a payment in the history because it is the same", () => {
+    state = reducer(state, paymentVerifica.request(anRptId));
+    expect(state.length).toEqual(1);
+  });
+
+  it("should update the existing payment history with success values", () => {
+    state = reducer(state, paymentVerifica.success(successData));
+    expect(state.length).toEqual(1);
+    expect(state[0].verified_data).toEqual(successData);
+  });
+
+  it("should add a payment in the history", () => {
+    // change some attrs to make a new payment
+    state = reducer(
+      state,
+      paymentVerifica.request({
+        ...anRptId,
+        organizationFiscalCode: "123098" as OrganizationFiscalCode
+      })
+    );
+    expect(state.length).toEqual(2);
+    expect(state[1].verified_data).toEqual(undefined);
+  });
+
+  it("should limit the payment history insertions", () => {
+    // change some attrs to make a new payment
+    range(1, HISTORY_SIZE + 10).forEach(_ => {
+      state = reducer(
+        state,
+        paymentVerifica.request({
+          ...anRptId,
+          organizationFiscalCode: `123098${_}` as OrganizationFiscalCode
+        })
+      );
+    });
+    expect(state.length).toBeLessThanOrEqual(HISTORY_SIZE);
+  });
+});
--- ts/store/reducers/payments/history.ts
+++ ts/store/reducers/payments/history.ts
@@ -1,24 +1,27 @@
 /**
- * the goal of this reducer is store a fixed amount of payments
- * to allow the user pick one that could be problematic and forward it
+ * the goal of this reducer is store a fixed amount of payments (requested, done or failed)
+ * to allow the user to pick one that could be problematic and forward it
  * to the customer care assistance
  *
- * we store 3 info:
+ * to accomplish this scope we store:
+ * - started_at the time in ISO format when the payment started
  * - "data" coming from: a message, qr code, or manual insertion
  * - "verified_data" coming from the verification of the previous one (see paymentVerifica.request ACTION and related SAGA)
  * - "failure" coming from the failure of a verification (paymentVerifica.failure)
  */
 
 import { RptId } from "italia-pagopa-commons/lib/pagopa";
+import _ from "lodash";
 import { getType } from "typesafe-actions";
 import { DetailEnum } from "../../../../definitions/backend/PaymentProblemJson";
 import { PaymentRequestsGetResponse } from "../../../../definitions/backend/PaymentRequestsGetResponse";
+import { clearCache } from "../../actions/profile";
 import { Action } from "../../actions/types";
 import { paymentVerifica } from "../../actions/wallet/payment";
-import { RTron } from "../../../boot/configureStoreAndPersistor";
-import _ from "lodash";
+import { GlobalState } from "../types";
 
-type PaymentHistory = {
+export type PaymentHistory = {
+  started_at: string;
   data: RptId;
   verified_data?: PaymentRequestsGetResponse;
   failure?: keyof typeof DetailEnum;
@@ -26,12 +29,7 @@ type PaymentHistory = {
 
 export type PaymentsHistoryState = ReadonlyArray<PaymentHistory>;
 const INITIAL_STATE: ReadonlyArray<PaymentHistory> = [];
-const HISTORY_SIZE = 10;
-
-const findPayment = (
-  state: PaymentsHistoryState,
-  paymentRequest: RptId
-): number => state.findIndex(ph => _.isEqual(ph.data, paymentRequest));
+export const HISTORY_SIZE = 10;
 
 const reducer = (
   state: PaymentsHistoryState = INITIAL_STATE,
@@ -39,18 +37,21 @@ const reducer = (
 ): PaymentsHistoryState => {
   switch (action.type) {
     case getType(paymentVerifica.request):
-      // it already in, remove the previous one
-
-      // tslint:disable-next-line: readonly-array
-      const updateState = [...state];
-      const prevIndex = findPayment(state, action.payload);
-      if (prevIndex !== -1) {
-        updateState.splice(prevIndex, 1);
+      // if already in, remove the previous one
+      const updateState = [...state].filter(
+        ph => !_.isEqual(ph.data, action.payload)
+      );
+      // if size exceeded, remove the ones exceeding (here we consider the one we will add in it)
+      if (updateState.length + 1 >= HISTORY_SIZE) {
+        updateState.splice(
+          HISTORY_SIZE - 1,
+          updateState.length + 1 - HISTORY_SIZE
+        );
       }
-      if (updateState.length >= HISTORY_SIZE) {
-        updateState.splice(state.length - 1, 1);
-      }
-      return [...updateState, { data: { ...action.payload } }];
+      return [
+        ...updateState,
+        { data: { ...action.payload }, started_at: new Date().toISOString() }
+      ];
     case getType(paymentVerifica.success):
       // it shouldn't happen since success comes after request
       if (state.length === 0) {
@@ -63,6 +64,7 @@ const reducer = (
       };
       // tslint:disable-next-line: readonly-array
       const updateStateSuccess = [...state];
+      // replace the old vale with the new one
       updateStateSuccess.splice(state.length - 1, 1, updateHistorySuccess);
       return updateStateSuccess;
     case getType(paymentVerifica.failure):
@@ -77,10 +79,17 @@ const reducer = (
       };
       // tslint:disable-next-line: readonly-array
       const updateStateFailure = [...state];
+      // replace the old value with the new one
       updateStateFailure.splice(state.length - 1, 1, updateHistoryFailure);
       return updateStateFailure;
+    case getType(clearCache): {
+      return INITIAL_STATE;
+    }
   }
   return state;
 };
+
+export const paymentsHistorySelector = (state: GlobalState) =>
+  state.payments.history;
 
 export default reducer;
