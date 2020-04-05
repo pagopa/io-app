adcf890d6e5b46152427dfb1ad5244495a11773d
Matteo Boschi <mbmaken@gmail.com>
2020-04-05 18:51:31 +0200

    [#171082956] remove useless code

--- ts/store/reducers/wallet/payment.ts
+++ ts/store/reducers/wallet/payment.ts
@@ -20,7 +20,6 @@ import {
   runPollTransactionSaga
 } from "../../actions/wallet/transactions";
 import { GlobalState } from "../types";
-import { RTron } from "../../../boot/configureStoreAndPersistor";
 
 export type EntrypointRoute = Readonly<{
   name: string;
@@ -106,7 +105,6 @@ const reducer = (
     // verifica
     //
     case getType(paymentVerifica.request):
-      RTron.log("paymentVerifica.request", action.payload);
       return {
         // a verifica operation will generate a new codice contesto pagamento
         // effectively starting a new payment session, thus we also invalidate
@@ -116,13 +114,11 @@ const reducer = (
         verifica: pot.noneLoading
       };
     case getType(paymentVerifica.success):
-      RTron.log("paymentVerifica.success", action.payload);
       return {
         ...state,
         verifica: pot.some(action.payload)
       };
     case getType(paymentVerifica.failure):
-      RTron.log("paymentVerifica.failure", action.payload);
       return {
         ...state,
         verifica: pot.noneError(action.payload)
