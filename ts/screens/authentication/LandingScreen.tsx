acc1e78c11462e7177386494929d2dd70452c6ef
valentinacala <valentina.cala@outlook.com>
2020-04-07 10:37:33 +0200

    restore icon

--- ts/screens/authentication/LandingScreen.tsx
+++ ts/screens/authentication/LandingScreen.tsx
@@ -171,8 +171,7 @@ class LandingScreen extends React.PureComponent<Props> {
               testID={"landing-button-login-cie"}
             >
               {
-                // disabled due a bug see https://www.pivotaltracker.com/story/show/172196031
-                // <IconFont name={"io-cie"} color={variables.colorWhite} />
+                <IconFont name={"io-cie"} color={variables.colorWhite} />
               }
 
               <Text>{I18n.t("authentication.landing.loginCie")}</Text>
