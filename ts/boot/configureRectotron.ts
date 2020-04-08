226b16b2a24e4c78ae0de8f179e12e63d5e5cca0
Matteo Boschi <mbmaken@gmail.com>
2020-04-08 18:21:03 +0200

    [#172205784] Add ignored urls in reactotron config (#1659)

--- ts/boot/configureRectotron.ts
+++ ts/boot/configureRectotron.ts
@@ -3,11 +3,19 @@ import Reactotron from "reactotron-react-native";
 import { reactotronRedux } from "reactotron-redux";
 import sagaPlugin from "reactotron-redux-saga";
 
+// add a regex to avoid tracing specific urls in Reactotron timeline
+// use this regex to avoid tracing scheduled pollings
+// ignoredUrls = /backend.json|info/
+const ignoredUrls: RegExp | undefined = undefined;
 export const configureReactotron = () => {
   return Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
     .configure({ host: "127.0.0.1" }) // controls connection & communication settings
     .use(reactotronRedux())
     .use(sagaPlugin({ except: [] }))
-    .useReactNative() // add all built-in react native plugins
+    .useReactNative({
+      networking: {
+        ignoreUrls: ignoredUrls
+      }
+    }) // add all built-in react native plugins
     .connect(); // let's connect!
 };
