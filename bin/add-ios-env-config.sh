64b0785bf384763836bbb31ae293814c40c4534c
Edigio <egidio.filosa@gmail.com>
2020-04-09 12:57:33 +0200

    [#170822968] Changed the output dir
    Changed the output dir for ios generated file

--- bin/add-ios-env-config.sh
+++ bin/add-ios-env-config.sh
@@ -6,5 +6,5 @@ then
   export SYMROOT="$RNC_ROOT/ios/ReactNativeConfig"
   export BUILD_DIR="$RNC_ROOT/ios/ReactNativeConfig"
   ENV_ROOT="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")"
-  ruby "$RNC_ROOT/ios/ReactNativeConfig/BuildDotenvConfig.rb" "$ENV_ROOT" "$ENV_ROOT"
+  ruby "$RNC_ROOT/ios/ReactNativeConfig/BuildDotenvConfig.rb" "$ENV_ROOT" "${ENV_ROOT}ios"
 fi
