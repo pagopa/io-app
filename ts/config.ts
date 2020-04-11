df8eef51c348c608c8fc6e51c16ccff32d73af73
Matteo Boschi <mbmaken@gmail.com>
2020-04-11 18:07:36 +0200

    [#172275240] increase fetch timeout from 5s to 8s

--- ts/config.ts
+++ ts/config.ts
@@ -13,7 +13,7 @@ export const DEFAULT_APPLICATION_NAME = "io.italia.it";
 const DEFAULT_CONTENT_REPO_URL = "https://raw.githubusercontent.com/pagopa/io-services-metadata/master" as NonEmptyString;
 
 // default timeout of fetch (in ms)
-const DEFAULT_FETCH_TIMEOUT_MS = 5000;
+const DEFAULT_FETCH_TIMEOUT_MS = 8000;
 
 // default max retries for fetch
 const DEFAULT_FETCH_MAX_RETRIES = 3;
