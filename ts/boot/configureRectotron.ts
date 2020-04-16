8de02d9e0e1d289066a12a5846c1789efe6b7a86
Matteo Boschi <mbmaken@gmail.com>
2020-04-16 18:15:57 +0200

    [#172275240] restore code

--- ts/boot/configureRectotron.ts
+++ ts/boot/configureRectotron.ts
@@ -6,10 +6,10 @@ import sagaPlugin from "reactotron-redux-saga";
 // add a regex to avoid tracing specific urls in Reactotron timeline
 // use this regex to avoid tracing scheduled pollings
 // ignoredUrls = /backend.json|info/
-const ignoredUrls: RegExp | undefined = /backend.json|info/;
+const ignoredUrls: RegExp | undefined = undefined;
 export const configureReactotron = () => {
   return Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-    .configure({ host: "192.168.1.69" }) // controls connection & communication settings
+    .configure({ host: "127.0.0.1" }) // controls connection & communication settings
     .use(reactotronRedux())
     .use(sagaPlugin({ except: [] }))
     .useReactNative({
