65d00f0ebcf16ef3250682a7faaacd3511d4158d
Matteo Boschi <mbmaken@gmail.com>
2020-04-21 17:56:09 +0200

    [#169952327] add partial test of initializeApplicationSaga

--- ts/sagas/startup.ts
+++ ts/sagas/startup.ts
@@ -93,7 +93,7 @@ const WAIT_INITIALIZE_SAGA = 3000 as Millisecond;
  * Handles the application startup and the main application logic loop
  */
 // tslint:disable-next-line:cognitive-complexity no-big-function
-function* initializeApplicationSaga(): IterableIterator<Effect> {
+export function* initializeApplicationSaga(): IterableIterator<Effect> {
   // Remove explicitly previous session data. This is done as completion of two
   // use cases:
   // 1. Logout with data reset
