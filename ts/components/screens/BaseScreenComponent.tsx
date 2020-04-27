e6cdb7c903e8db562e806758a7d4d57f6b296ccb
Matteo Boschi <mbmaken@gmail.com>
2020-04-27 18:44:05 +0200

    [#172076948] support Android Modal

--- ts/components/screens/BaseScreenComponent.tsx
+++ ts/components/screens/BaseScreenComponent.tsx
@@ -11,6 +11,7 @@ import { Container } from "native-base";
 import { connectStyle } from "native-base-shoutem-theme";
 import mapPropsToStyleNames from "native-base/src/utils/mapPropsToStyleNames";
 import * as React from "react";
+import { ModalBaseProps, Platform } from "react-native";
 import { TranslationKeys } from "../../../locales/locales";
 import {
   openInstabugBugReport,
@@ -23,6 +24,7 @@ import { ContextualHelpModal } from "../ContextualHelpModal";
 import { SearchType } from "../search/SearchButton";
 import Markdown from "../ui/Markdown";
 import { BaseHeader } from "./BaseHeader";
+import { Millisecond } from "italia-ts-commons/lib/units";
 
 export interface ContextualHelpProps {
   title: string;
@@ -64,16 +66,20 @@ interface State {
   isHelpVisible: boolean;
   requestReport: Option<BugReporting.reportType>;
   markdownContentLoaded: Option<boolean>;
+  contextualHelpModalAnimation: ModalBaseProps["animationType"];
 }
 
 const maybeDark = fromPredicate(
   (isDark: boolean | undefined = undefined) => isDark === true
 );
 
+const ANDROID_REPORT_DELAY = 50 as Millisecond;
+
 class BaseScreenComponent extends React.PureComponent<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
+      contextualHelpModalAnimation: "slide",
       isHelpVisible: false,
       requestReport: none,
       // if the content is markdown we listen for load end event, otherwise the content is
@@ -85,23 +91,37 @@ class BaseScreenComponent extends React.PureComponent<Props, State> {
   }
 
   private handleOnRequestAssistance = (type: BugReporting.reportType) => {
-    this.setState({ isHelpVisible: false }, () => {
-      this.setState({ requestReport: some(type) });
+    this.setState({ contextualHelpModalAnimation: "none" }, () => {
+      this.setState({ isHelpVisible: false }, () => {
+        this.setState({ requestReport: some(type) }, () => {
+          // since in Android we have no way to handle Modal onDismiss event https://reactnative.dev/docs/modal#ondismiss
+          // we force handling here. The timeout is due to wait until the modal is completly hidden
+          // otherwise in the Instabug screeshoot we will se the contextual help content instead the screen below
+          if (Platform.OS === "android") {
+            setTimeout(
+              this.handleOnContextualHelpDismissed,
+              ANDROID_REPORT_DELAY
+            );
+          }
+          this.setState({ contextualHelpModalAnimation: "slide" });
+        });
+      });
     });
   };
 
   private handleOnContextualHelpDismissed = () => {
     const maybeReport = this.state.requestReport;
-    this.setState({ requestReport: none }, () => {});
-    maybeReport.map(type => {
-      switch (type) {
-        case BugReporting.reportType.bug:
-          openInstabugBugReport();
-          break;
-        case BugReporting.reportType.question:
-          openInstabugChat();
-          break;
-      }
+    this.setState({ requestReport: none }, () => {
+      maybeReport.map(type => {
+        switch (type) {
+          case BugReporting.reportType.bug:
+            openInstabugBugReport();
+            break;
+          case BugReporting.reportType.question:
+            openInstabugChat();
+            break;
+        }
+      });
     });
   };
 
@@ -109,7 +129,6 @@ class BaseScreenComponent extends React.PureComponent<Props, State> {
     maybeDark(this.props.dark).map(_ =>
       setStatusBarColorAndBackground("dark-content", customVariables.colorWhite)
     );
-
     this.setState({ isHelpVisible: true });
   };
 
@@ -180,7 +199,7 @@ class BaseScreenComponent extends React.PureComponent<Props, State> {
             title={ch.title}
             body={ch.body}
             isVisible={this.state.isHelpVisible}
-            modalAnimation={"slide"}
+            modalAnimation={this.state.contextualHelpModalAnimation}
             onRequestAssistance={this.handleOnRequestAssistance}
             close={this.hideHelp}
             contentLoaded={this.state.markdownContentLoaded.fold(true, s => s)}
