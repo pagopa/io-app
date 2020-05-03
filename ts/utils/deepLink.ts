5e55b2da538c94d1b9e828ef13070009bfdb118f
Fabio Bonelli <fb@fabiobonelli.it>
2020-05-03 14:38:12 +0200

    Remove android:host from Android deep links.
    Remove host from Android deep links as per documentation and to be
    uniform with iOS links.

--- ts/utils/deepLink.ts
+++ ts/utils/deepLink.ts
@@ -1,12 +1,8 @@
-import { Platform } from "react-native";
 import { NavigationNavigateActionPayload } from "react-navigation";
 
 export function getNavigateActionFromDeepLink(
   url: string,
-  // on Android, the URI prefix contains a host in addition to scheme
-  deepLinkPrefix: string = Platform.OS === "android"
-    ? "ioit://ioit/"
-    : "ioit://"
+  deepLinkPrefix: string = "ioit://"
 ): NavigationNavigateActionPayload {
   const route = url.slice(deepLinkPrefix.length);
   const routeParts = route.split("/");
