783a7ba3a62328306f74e03427b1b8f727bd743a
Matteo Boschi <mbmaken@gmail.com>
2020-05-13 19:02:28 +0200

    [#172553600,#172442428] hide payment button if message is paid

--- ts/components/messages/MessageDetailCTABar.tsx
+++ ts/components/messages/MessageDetailCTABar.tsx
@@ -67,6 +67,9 @@ class MessageDetailCTABar extends React.PureComponent<Props> {
 
   // Render abutton to display details of the payment related to the message
   private renderPaymentButton() {
+    if (this.paid) {
+      return null;
+    }
     // The button is displayed if the payment has an expiration date in the future
     return this.paymentExpirationInfo.fold(null, pei => {
       const { message, service } = this.props;
