6635be760beba326c38d99052eca9f1e8617f5f1
Cristiano Tofani <cri.tofani@gmail.com>
2020-05-19 14:42:32 +0200

    Fixes on first condition

--- ts/sagas/startup/checkProfileEnabledSaga.ts
+++ ts/sagas/startup/checkProfileEnabledSaga.ts
@@ -5,22 +5,17 @@ import { ActionType, getType } from "typesafe-actions";
 import { InitializedProfile } from "../../../definitions/backend/InitializedProfile";
 import { startApplicationInitialization } from "../../store/actions/application";
 import { profileFirstLogin, profileUpsert } from "../../store/actions/profile";
+import { RTron } from "../../boot/configureStoreAndPersistor";
 
 export function* checkProfileEnabledSaga(
   profile: InitializedProfile
 ): IterableIterator<Effect> {
   if (
-    !profile.has_profile ||
+    profile.version === 0 ||
     !profile.email ||
     !profile.is_inbox_enabled ||
     !profile.is_webhook_enabled
   ) {
-    // NOTE: `has_profile` is a boolean that is true if the profile is
-    // active in the API.
-    // FIXME: the `version` field has the same meaning at the `has_profile`
-    //        field, we should get rid of `has_profile`.
-    //        see https://github.com/pagopa/io-backend/blob/v0.0.48/src/types/profile.ts#L33
-
     // Upsert the user profile to enable inbox and webhook
     yield put(
       profileUpsert.request({
