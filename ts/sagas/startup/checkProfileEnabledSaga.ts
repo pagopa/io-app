4d8e0938b321451f3b03fb4bd2519236f9643acf
Cristiano Tofani <cri.tofani@gmail.com>
2020-05-20 11:20:36 +0200

    Replace condition with utility functions

--- ts/sagas/startup/checkProfileEnabledSaga.ts
+++ ts/sagas/startup/checkProfileEnabledSaga.ts
@@ -5,13 +5,19 @@ import { ActionType, getType } from "typesafe-actions";
 import { InitializedProfile } from "../../../definitions/backend/InitializedProfile";
 import { startApplicationInitialization } from "../../store/actions/application";
 import { profileFirstLogin, profileUpsert } from "../../store/actions/profile";
+import {
+  hasProfileEmail,
+  isProfileFirstOnBoarding
+} from "../../store/reducers/profile";
 
 export function* checkProfileEnabledSaga(
   profile: InitializedProfile
 ): IterableIterator<Effect> {
   if (
-    profile.version === 0 &&
-    (!profile.email || !profile.is_inbox_enabled || !profile.is_webhook_enabled)
+    isProfileFirstOnBoarding(profile) &&
+    (!hasProfileEmail(profile) ||
+      !profile.is_inbox_enabled ||
+      !profile.is_webhook_enabled)
   ) {
     // Upsert the user profile to enable inbox and webhook
     yield put(
@@ -33,7 +39,7 @@ export function* checkProfileEnabledSaga(
       yield put(startApplicationInitialization());
     } else {
       // First time login
-      if (profile.version === 0) {
+      if (isProfileFirstOnBoarding(profile)) {
         yield put(profileFirstLogin());
       }
     }
