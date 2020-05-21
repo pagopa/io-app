99440fc69b83229fac8997fcc87b66b49f3e8335
Fabrizio <fartux@gmail.com>
2020-05-21 11:31:13 +0200

    add missing ; for CURRENT_PROJECT_VERSION

--- scripts/changelog/pbxproj_updater.js
+++ scripts/changelog/pbxproj_updater.js
@@ -7,17 +7,18 @@ module.exports.readVersion = function(contents) {
  * @param match
  * @param p1: the key CURRENT_PROJECT_VERSION
  * @param p2: the value
+ * @param p3: the ";"
  * @param offset
  * @param string
  * @return {string}
  */
-function replacer(match, p1, p2, offset, string) {
+function replacer(match, p1, p2, p3, offset, string) {
   const currentProjectVersionValue = parseInt(p2, 10) + 1;
-  return [p1, currentProjectVersionValue].join("");
+  return [p1, currentProjectVersionValue, p3].join("");
 }
 
 module.exports.writeVersion = function(contents, version) {
-  const regex = /(CURRENT_PROJECT_VERSION\s?=\s?)(\d+);/gm;
+  const regex = /(CURRENT_PROJECT_VERSION\s?=\s?)(\d+)(;)/gm;
   contents = contents.replace(regex, replacer);
   return contents;
 };
