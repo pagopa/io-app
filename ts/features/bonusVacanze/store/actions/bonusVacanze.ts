ea224df079bf0ccf5c6f41b6f568bb3d2d61c7c5
Matteo Boschi <mbmaken@gmail.com>
2020-05-26 13:33:05 +0200

    [#172989345] refactoring

--- ts/features/bonusVacanze/store/actions/bonusVacanze.ts
+++ ts/features/bonusVacanze/store/actions/bonusVacanze.ts
@@ -2,7 +2,7 @@ import { ActionType, createAsyncAction } from "typesafe-actions";
 import { BonusList } from "../../types/bonusList";
 
 export const availableBonusListLoad = createAsyncAction(
-  "BONUS_AVAILABLE_LIST_REQUEST",
+  "BONUS_AVAILABLE_REQUEST",
   "BONUS_AVAILABLE_SUCCESS",
   "BONUS_AVAILABLE_FAILURE"
 )<void, BonusList, Error>();
