fda442519b8c630505ea3667a24943936117c4fc
Fabrizio <fartux@gmail.com>
2020-05-27 18:39:53 +0200

    add locales & ActivateBonusScreen refinements

--- ts/features/bonusVacanze/screens/ActivateBonus/ActivateBonusComponent.tsx
+++ ts/features/bonusVacanze/screens/ActivateBonus/ActivateBonusComponent.tsx
@@ -1,15 +1,26 @@
+import { string } from "io-ts";
+import * as t from "io-ts";
 import { List, View } from "native-base";
 import * as React from "react";
+import { StyleSheet, Text } from "react-native";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import ListItemComponent from "../../../../components/screens/ListItemComponent";
 import ScreenContent from "../../../../components/screens/ScreenContent";
-import { BlockButtonProps } from "../../../../components/ui/BlockButtons";
-import FooterWithButtons from "../../../../components/ui/FooterWithButtons";
-import { ScrollView, StyleSheet } from "react-native";
 import I18n from "../../../../i18n";
-import ROUTES from "../../../../navigation/routes";
+import themeVariables from "../../../../theme/variables";
+import { FooterTwoButtons } from "../../components/markdown/FooterTwoButtons";
+
+// TODO: temp definition, remove after merge with https://github.com/pagopa/io-app/pull/1849/
+const FamilyMember = t.interface({
+  name: string,
+  surname: string
+});
+export type FamilyMember = t.TypeOf<typeof FamilyMember>;
 
 type Props = {
+  familyMembers: ReadonlyArray<FamilyMember>;
+  bonusAmount: number;
+  onCancel: () => void;
   onRequestBonus: () => void;
 };
 
@@ -17,49 +28,68 @@ const styles = StyleSheet.create({
   body: {
     flex: 1
   },
-  footer: {
-    flex: 2
+  padding: {
+    paddingLeft: themeVariables.contentPadding,
+    paddingRight: themeVariables.contentPadding
   }
 });
 
-const screenTitle = "Attiva bonus";
-
-export const requestBonusButtonProps = (props: Props): BlockButtonProps => {
-  return {
-    primary: true,
-    title: "Richiedi Bonus",
-    onPress: props.onRequestBonus
-  };
-};
+const title = I18n.t("bonus.bonusVacanza.activateBonus.title");
+const activateBonusText = title;
 
 /**
+ * Transform an object of type {@link FamilyMember} to a string representation
+ * @param familyMember
+ */
+const familyMemberToDisplayName = (familyMember: FamilyMember) =>
+  `${familyMember.name} ${familyMember.surname}`;
+
+/**
+ * Transform a string to a {@link ListItemComponent}, displaying information about a family member
+ * @param displayName
+ */
+const familyMemberDisplayNameToRenderItem = (displayName: string) => (
+  <ListItemComponent key={displayName} title={displayName} hideIcon={true} />
+);
+
+/**
+ * Transform a list of object of type {@link FamilyMember} to a {@link List} containing foreach family member a
+ * {@link ListItemComponent}
+ * @param familyMembers
+ */
+const renderFamilyMembersList = (
+  familyMembers: ReadonlyArray<FamilyMember>
+) => (
+  <List withContentLateralPadding={true}>
+    {familyMembers
+      .map(familyMemberToDisplayName)
+      .map(familyMemberDisplayNameToRenderItem)}
+  </List>
+);
+
+/**
+ * This component displays generic information about the bonus, including the {@link Props.bonusAmount} and
+ * the {@link Props.familyMembers}.
  * @param props
  * @constructor
  */
 export const ActivateBonusComponent: React.FunctionComponent<Props> = props => {
+  const description = I18n.t("bonus.bonusVacanza.activateBonus.description", {
+    amount: props.bonusAmount
+  });
+
   return (
-    <BaseScreenComponent goBack={true} headerTitle={screenTitle}>
-      <ScreenContent title={screenTitle} bounces={false}>
-        <List withContentLateralPadding={true}>
-          {/* Preferences */}
-          <ListItemComponent
-            title={I18n.t("profile.main.preferences.title")}
-            hideIcon={true}
-          />
-          <ListItemComponent
-            title={I18n.t("profile.main.preferences.title")}
-            hideIcon={true}
-          />
-          <ListItemComponent
-            title={I18n.t("profile.main.preferences.title")}
-            hideIcon={true}
-          />
-        </List>
-        <FooterWithButtons
-          type={"SingleButton"}
-          leftButton={requestBonusButtonProps(props)}
-        />
+    <BaseScreenComponent goBack={true} headerTitle={title}>
+      <ScreenContent title={title} bounces={false}>
+        <Text style={styles.padding}>{description}</Text>
+        <View spacer={true} />
+        {renderFamilyMembersList(props.familyMembers)}
       </ScreenContent>
+      <FooterTwoButtons
+        onCancel={props.onCancel}
+        onRight={props.onRequestBonus}
+        title={activateBonusText}
+      />
     </BaseScreenComponent>
   );
 };
--- ts/features/bonusVacanze/screens/ActivateBonus/ActivateBonusScreen.tsx
+++ ts/features/bonusVacanze/screens/ActivateBonus/ActivateBonusScreen.tsx
@@ -2,22 +2,29 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { RTron } from "../../../../boot/configureStoreAndPersistor";
+import { GlobalState } from "../../../../store/reducers/types";
 import { ActivateBonusComponent } from "./ActivateBonusComponent";
 
-type Props = ReturnType<typeof mapDispatchToProps>;
+type Props = ReturnType<typeof mapDispatchToProps> &
+  ReturnType<typeof mapStateToProps>;
 
 /**
- * This screen display some additional information when the ISEE is not available for the user.
- * It provides two CTA:
- * - Cancel: exit from the bonus request workflow
- * - Go to INPS website
- * The screen is tied to the business logic and is composed using {@link MarkdownBaseScreen} and {@link FooterTwoButtons}
+ * This screen allows the user to visualize the amount of the bonus available, the family composition, and offers two CTA:
+ * - Cancel: Abort the bonus request
+ * - Activate the bonus: Start the procedure to activate the bonus
+ * The screen is tied to the business logic and is composed using {@link ActivateBonusComponent}
  * @param props
  * @constructor
  */
-
 const ActivateBonusScreen: React.FunctionComponent<Props> = props => {
-  return <ActivateBonusComponent />;
+  return (
+    <ActivateBonusComponent
+      onCancel={props.onCancel}
+      onRequestBonus={props.onActivateBonus}
+      bonusAmount={props.bonusAmount}
+      familyMembers={props.familyMembers}
+    />
+  );
 };
 
 const mapDispatchToProps = (_: Dispatch) => ({
@@ -26,9 +33,23 @@ const mapDispatchToProps = (_: Dispatch) => ({
     RTron.log("CANCEL");
   },
   // TODO: link with the right dispatch action
-  onGoToINPSWebsite: () => {
-    RTron.log("GOTO INPS");
+  onActivateBonus: () => {
+    RTron.log("BONUS REQUEST");
   }
 });
 
-export default connect(mapDispatchToProps)(ActivateBonusScreen);
+const mapStateToProps = (_: GlobalState) => ({
+  // TODO: link with the right reducer
+  bonusAmount: 500,
+  // TODO: link with the right reducer
+  familyMembers: [
+    { name: "Chiara", surname: "D'Anassimandro" },
+    { name: "Giovanni", surname: "Filippi" },
+    { name: "Piero", surname: "Filippi" }
+  ]
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ActivateBonusScreen);
