f4867a22dfb45ea253bc550ce9f0098ef8814ae9
Fabrizio <fartux@gmail.com>
2020-06-04 10:44:59 +0200

    refactoring IseeErrorComponent and graphical customization for IseeNotAvailableScreen

---
+++ ts/features/bonusVacanze/screens/eligibility/isee/BaseIseeErrorComponent.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { ImageSourcePropType, Linking } from "react-native";
+import I18n from "../../../../../i18n";
+import {
+  cancelButtonProps,
+  confirmButtonProps
+} from "../../../components/buttons/ButtonConfigurations";
+import { FooterStackButton } from "../../../components/buttons/FooterStackButtons";
+import { renderRasterImage } from "../../../components/infoScreen/imageRendering";
+import { InfoScreenComponent } from "../../../components/infoScreen/InfoScreenComponent";
+
+const inpsDsuHomeUrl =
+  "https://servizi2.inps.it/servizi/ISEEPrecompilato/WfSimHome.aspx";
+const inpsSimulationUrl =
+  "https://servizi2.inps.it/servizi/ISEEPrecompilato/WfSimOrdDati.aspx";
+
+type Props = {
+  image: ImageSourcePropType;
+  title: string;
+  body: string;
+  onCancel: () => void;
+};
+/**
+ * A generic component used to display the possible ISEE errors during the check eligibility phase.
+ * @param props
+ * @constructor
+ */
+export const BaseIseeErrorComponent: React.FunctionComponent<Props> = props => {
+  const goToDsu = I18n.t(
+    "bonus.bonusVacanza.eligibility.iseeNotEligible.goToNewDSU"
+  );
+  const goToSimulation = I18n.t(
+    "bonus.bonusVacanza.eligibility.iseeNotEligible.goToNewSimulation"
+  );
+  const cancelRequest = I18n.t("bonus.bonusVacanza.cta.cancelRequest");
+
+  return (
+    <>
+      <InfoScreenComponent
+        image={renderRasterImage(props.image)}
+        title={props.title}
+        body={props.body}
+      />
+      <FooterStackButton
+        buttons={[
+          confirmButtonProps(() => Linking.openURL(inpsDsuHomeUrl), goToDsu),
+          confirmButtonProps(
+            () => Linking.openURL(inpsSimulationUrl),
+            goToSimulation
+          ),
+          cancelButtonProps(props.onCancel, cancelRequest)
+        ]}
+      />
+    </>
+  );
+};
