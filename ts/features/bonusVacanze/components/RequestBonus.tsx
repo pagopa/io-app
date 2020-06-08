ef89d093f110ea3f4281303cb87380821f29cede
Cristiano Tofani <cri.tofani@gmail.com>
2020-06-08 17:31:46 +0200

    Minor improvements on add bonus card

--- ts/features/bonusVacanze/components/RequestBonus.tsx
+++ ts/features/bonusVacanze/components/RequestBonus.tsx
@@ -47,11 +47,11 @@ const styles = StyleSheet.create({
     color: customVariables.brandDarkGray
   },
   brandLightGray: {
-    color: customVariables.brandLightGray
+    color: customVariables.brandGray
   },
   badgeColor: {
     height: 18,
-    marginTop: 2,
+    marginTop: 5,
     backgroundColor: customVariables.brandHighLighter
   },
   headerText: {
@@ -61,6 +61,47 @@ const styles = StyleSheet.create({
   badgeText: {
     fontSize: customVariables.fontSizeSmaller,
     lineHeight: 16
+  },
+  cardInner: {
+    paddingBottom: 16,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 22
+  },
+  card: {
+    // iOS and Andorid card shadow
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1.5,
+    elevation: 3,
+
+    backgroundColor: customVariables.brandDarkGray,
+    borderRadius: 8,
+    marginBottom: -1,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 20
+  },
+  flatBottom: {
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0
+  },
+  rotateCard: {
+    marginBottom: -(58 / 2 + 1),
+    flex: 1,
+    shadowRadius: 10,
+    shadowOpacity: 0.15,
+    transform: [
+      { perspective: 700 },
+      { rotateX: "-20deg" },
+      { scaleX: 0.98 },
+      { translateY: -(58 / 2 + 20) * (1 - Math.cos(20)) }
+    ]
+  },
+  rotateText: {
+    flex: 1,
+    transform: [{ perspective: 700 }, { rotateX: "20deg" }, { scaleX: 0.98 }]
   }
 });
 
@@ -93,14 +134,40 @@ const RequestBonus: React.FunctionComponent<OwnProps> = (props: OwnProps) => {
 
   return (
     <React.Fragment>
-      <View style={styles.flexRow}>
+      <View style={styles.rotateCard}>
+        <View style={[styles.card, styles.flatBottom]}>
+          <View style={[styles.cardInner]}>
+            <View style={[styles.flexRow, styles.rotateText]}>
+              <View style={styles.flexRow2}>
+                <Text style={[styles.brandLightGray, styles.headerText]}>
+                  {I18n.t("bonus.requestLabel")}
+                </Text>
+                <Badge style={styles.badgeColor}>
+                  <Text style={styles.badgeText}>
+                    {/* Replace with I18n.t("wallet.methods.newCome") after PR #1875 */}
+                    Novità
+                  </Text>
+                </Badge>
+              </View>
+              <View>
+                <AddPaymentMethodButton
+                  onPress={onButtonPress}
+                  iconSize={customVariables.fontSize2}
+                  labelSize={customVariables.fontSizeSmall}
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+      {/* <View style={styles.flexRow}>
         <View style={styles.flexRow2}>
           <Text style={[styles.brandLightGray, styles.headerText]}>
             {I18n.t("bonus.requestLabel")}
           </Text>
           <Badge style={styles.badgeColor}>
             <Text style={styles.badgeText}>
-              {/* Replace with I18n.t("wallet.methods.newCome") after PR #1875 */}
+              {/* Replace with I18n.t("wallet.methods.newCome") after PR #1875
               Novità
             </Text>
           </Badge>
@@ -112,8 +179,7 @@ const RequestBonus: React.FunctionComponent<OwnProps> = (props: OwnProps) => {
             labelSize={customVariables.fontSizeSmall}
           />
         </View>
-      </View>
-      <View spacer={true} />
+      </View> */}
       {pot.isSome(activeBonus) &&
         activeBonus.value && (
           <ActiveBonus
