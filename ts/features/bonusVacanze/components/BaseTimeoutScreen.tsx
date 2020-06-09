cab4afbb2b2df67a90897606888821af091a22a7
Fabrizio <fartux@gmail.com>
2020-06-09 20:52:18 +0200

    remove useless code

--- ts/features/bonusVacanze/components/BaseTimeoutScreen.tsx
+++ ts/features/bonusVacanze/components/BaseTimeoutScreen.tsx
@@ -1,18 +1,15 @@
 import * as React from "react";
-import { NavigationScreenProps, withNavigation } from "react-navigation";
 import I18n from "../../../i18n";
 import { cancelButtonProps } from "./buttons/ButtonConfigurations";
 import { FooterStackButton } from "./buttons/FooterStackButtons";
 import { renderRasterImage } from "./infoScreen/imageRendering";
 import { InfoScreenComponent } from "./infoScreen/InfoScreenComponent";
 
-type MyProps = {
+type Props = {
   title: string;
   body: string;
   onExit: () => void;
 };
-
-type Props = NavigationScreenProps & MyProps;
 
 const image = require("../../../../img/wallet/errors/payment-expired-icon.png");
 
@@ -23,7 +20,7 @@ const image = require("../../../../img/wallet/errors/payment-expired-icon.png");
  * @constructor
  */
 
-const InnerBaseTimeoutScreen: React.FunctionComponent<Props> = props => {
+export const BaseTimeoutScreen: React.FunctionComponent<Props> = props => {
   const confirmText = I18n.t("global.buttons.exit");
   return (
     <>
@@ -38,5 +35,3 @@ const InnerBaseTimeoutScreen: React.FunctionComponent<Props> = props => {
     </>
   );
 };
-
-export const BaseTimeoutScreen = withNavigation(InnerBaseTimeoutScreen);
