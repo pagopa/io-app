46e72256839e4dbe4b3a810ef701a59edf11fe65
Cristiano Tofani <cri.tofani@gmail.com>
2020-06-12 17:58:34 +0200

    feat: [#173168958] UI refinement of bonus request component (#1897)
    * move the request bonus component in the wallet header and fixes minor ui
    
    * Minor improvements on add bonus card
    
    * Adds react-native-linear-gradient and refines the payment methods header
    
    * Clean styles object
    
    * Created section card component
    
    * Created section card component
    
    * Cleanup code
    
    * Cleanup code
    
    * Minor refactoring and links new package on android
    
    * Podfile
    
    * Improvements on card shadow
    
    * Minor improvements
    
    * [#173168958] refactor
    
    * [#173168958] add const and comment
    
    * [#173168958] revert previous behavior: render only credit cards
    
    * Fixes Wallets screen spacing
    
    * [#173168958] render only wallet of credit card type
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

--- ts/features/bonusVacanze/components/RequestBonus.tsx
+++ ts/features/bonusVacanze/components/RequestBonus.tsx
@@ -1,14 +1,10 @@
 import { fromNullable } from "fp-ts/lib/Option";
 import * as pot from "italia-ts-commons/lib/pot";
-import { Content, Text, View } from "native-base";
 import * as React from "react";
-import { StyleSheet } from "react-native";
 import { BonusActivationWithQrCode } from "../../../../definitions/bonus_vacanze/BonusActivationWithQrCode";
 import { BonusesAvailable } from "../../../../definitions/content/BonusesAvailable";
-import ButtonDefaultOpacity from "../../../components/ButtonDefaultOpacity";
-import H5 from "../../../components/ui/H5";
+import SectionCardComponent from "../../../components/wallet/card/SectionCardComponent";
 import I18n from "../../../i18n";
-import customVariables from "../../../theme/variables";
 import { ID_BONUS_VACANZE_TYPE } from "../utils/bonus";
 import ActiveBonus from "./ActiveBonus";
 
@@ -22,23 +18,6 @@ type OwnProps = {
   activeBonus: pot.Pot<BonusActivationWithQrCode, Error>;
   availableBonusesList: BonusesAvailable;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "flex-start",
-    justifyContent: "center",
-    backgroundColor: "transparent"
-  },
-  subHeaderContent: {
-    flexDirection: "row",
-    alignItems: "baseline",
-    justifyContent: "space-between"
-  },
-  brandDarkGray: {
-    color: customVariables.brandDarkGray
-  }
-});
 
 /**
  * Component to show the request Bonus section on the Wallet Home Screen:
@@ -58,41 +37,26 @@ const RequestBonus: React.FunctionComponent<OwnProps> = (props: OwnProps) => {
   );
 
   return (
-    <Content>
+    <React.Fragment>
+      <SectionCardComponent
+        label={I18n.t("bonus.requestLabel")}
+        onPress={onButtonPress}
+        isNew={true}
+      />
       {!pot.isLoading(activeBonus) &&
         pot.isSome(activeBonus) && (
-          <View>
-            <View style={styles.subHeaderContent}>
-              <H5 style={styles.brandDarkGray}>
-                {I18n.t("bonus.latestBonus")}
-              </H5>
-              <Text>{I18n.t("wallet.amount")}</Text>
-            </View>
-            <View spacer={true} />
-            <ActiveBonus
-              bonus={activeBonus.value}
-              onPress={onBonusPress}
-              validFrom={maybeBonusVacanzeCategory
-                .map(b => b.valid_from)
-                .toUndefined()}
-              validTo={maybeBonusVacanzeCategory
-                .map(b => b.valid_to)
-                .toUndefined()}
-            />
-          </View>
+          <ActiveBonus
+            bonus={activeBonus.value}
+            onPress={onBonusPress}
+            validFrom={maybeBonusVacanzeCategory
+              .map(b => b.valid_from)
+              .toUndefined()}
+            validTo={maybeBonusVacanzeCategory
+              .map(b => b.valid_to)
+              .toUndefined()}
+          />
         )}
-      <View spacer={true} />
-      <View style={styles.container}>
-        <ButtonDefaultOpacity
-          block={true}
-          bordered={true}
-          onPress={onButtonPress}
-          activeOpacity={1}
-        >
-          <Text bold={true}>{I18n.t("bonus.request")}</Text>
-        </ButtonDefaultOpacity>
-      </View>
-    </Content>
+    </React.Fragment>
   );
 };
 
