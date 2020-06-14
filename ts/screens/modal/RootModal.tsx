de716284c8d1c3ff5feb11f8aa7f90b99e473e22
Matteo Boschi <mbmaken@gmail.com>
2020-06-14 11:27:43 +0200

    [#173322606] update comment

--- ts/screens/modal/RootModal.tsx
+++ ts/screens/modal/RootModal.tsx
@@ -12,7 +12,7 @@ import UpdateAppModal from "./UpdateAppModal";
 type Props = ReturnType<typeof mapStateToProps>;
 
 /**
- * This is a wrapper of all possibile modals the app can show in an automatic way, in this order:
+ * This is a wrapper of all possibile modals the app can show (without user interaction), in this order:
  * - SystemOffModal -> when backend systems are off the app avoids its usage by showing a modal
  * - UpdateAppModal -> when the backend is not compliant anymore with the app, this modal is shown to force an update
  * - IdentificationModal -> the default case. It renders it self only if an identification action is required
