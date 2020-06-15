4867c12870e6b3ea8fc75ef351b144996130eee7
Matteo Boschi <mbmaken@gmail.com>
2020-06-15 17:31:52 +0200

    [#173157535] fix merge errors

--- ts/features/bonusVacanze/screens/eligibility/ActivateBonus/request/ActivateBonusRequestScreen.tsx
+++ ts/features/bonusVacanze/screens/eligibility/ActivateBonus/request/ActivateBonusRequestScreen.tsx
@@ -7,10 +7,7 @@ import { identificationRequest } from "../../../../../../store/actions/identific
 import { GlobalState } from "../../../../../../store/reducers/types";
 import { abortBonusRequest } from "../../../../components/AbortBonusRequest";
 import { familyMembers } from "../../../../mock/mockData";
-import {
-  cancelBonusEligibility,
-  completeBonusEligibility
-} from "../../../../store/actions/bonusVacanze";
+import { cancelBonusEligibility } from "../../../../store/actions/bonusVacanze";
 import { eligibilityEligibleSelector } from "../../../../store/reducers/eligibility";
 import { ActivateBonusRequestComponent } from "./ActivateBonusRequestComponent";
 
@@ -72,11 +69,8 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 const mapStateToProps = (state: GlobalState) => {
   const elc = eligibilityEligibleSelector(state);
   return {
-    bonusAmount: elc.fold(500 as MaxBonusAmount, e => e.dsu_request.max_amount),
-    taxBenefit: elc.fold(
-      100 as MaxBonusTaxBenefit,
-      e => e.dsu_request.max_tax_benefit
-    ),
+    bonusAmount: elc.fold(500, e => e.dsu_request.max_amount),
+    taxBenefit: elc.fold(100, e => e.dsu_request.max_tax_benefit),
     familyMembers: elc.fold(familyMembers, e => e.dsu_request.family_members),
     hasDiscrepancies: elc.fold(true, e => e.dsu_request.has_discrepancies)
   };
