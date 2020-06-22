c558c7902d6e9d51bc8d396c4e2c2839e16191df
Matteo Boschi <mbmaken@gmail.com>
2020-06-22 19:37:16 +0200

    [#173171577] add tests add type definition

---
+++ ts/types/MessageCTA.ts
@@ -0,0 +1,27 @@
+/**
+ * this type models 2 cta that could be nested inside the markdown content of a message
+ */
+import * as t from "io-ts";
+
+const CTA = t.interface({
+  text: t.string,
+  action: t.string
+});
+export type CTA = t.TypeOf<typeof CTA>;
+const CTASR = t.interface({
+  cta_1: CTA
+});
+
+const CTASO = t.partial({
+  cta_2: CTA
+});
+
+export const CTAS = t.intersection([CTASR, CTASO], "CTAS");
+
+const MessageCTA = t.partial({
+  it: CTAS,
+  en: CTAS
+});
+export type CTAS = t.TypeOf<typeof CTAS>;
+
+export type MessageCTA = t.TypeOf<typeof MessageCTA>;
