5858b851762ff0e97027b531287fa54f1bc3bd22
Cristiano Tofani <cri.tofani@gmail.com>
2020-07-07 16:56:12 +0200

    [#173686901] Restore accessibility for app header

--- ts/components/ui/AppHeader.tsx
+++ ts/components/ui/AppHeader.tsx
@@ -4,7 +4,6 @@
 import { Header, NativeBase, View } from "native-base";
 import * as React from "react";
 import { ViewProps } from "react-native";
-import I18n from "../../i18n";
 import variables from "../../theme/variables";
 import ConnectionBar from "../ConnectionBar";
 
@@ -12,10 +11,7 @@ type Props = NativeBase.Header & ViewProps;
 
 const AppHeader = (props: React.PropsWithChildren<Props>) => {
   return (
-    <View
-      accessible={true}
-      accessibilityLabel={I18n.t("global.accessibility.header.label")}
-    >
+    <View>
       <Header
         androidStatusBarColor={variables.androidStatusBarColor}
         iosBarStyle={"dark-content"}
