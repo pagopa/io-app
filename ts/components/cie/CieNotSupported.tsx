f36fb214c29f891f39e99d32349b19ac9207311c
Matteo Boschi <mbmaken@gmail.com>
2020-07-14 18:00:00 +0200

    [#173638823] use new icons change texts some refactoring & improvements

--- ts/components/cie/CieNotSupported.tsx
+++ ts/components/cie/CieNotSupported.tsx
@@ -1,5 +1,6 @@
 import { Body, List, ListItem, Text, View } from "native-base";
 import * as React from "react";
+import { useState } from "react";
 import { Platform } from "react-native";
 import I18n from "../../i18n";
 import customVariables from "../../theme/variables";
@@ -11,59 +12,60 @@ type Props = {
   hasCieNFCFeature: boolean;
 };
 
-const ICON_SIZE = 16;
-
+const ICON_SIZE = 24;
+const okColor = customVariables.brandSuccess;
+const koColor = customVariables.brandDanger;
+const okIcon = "io-complete";
+const koIcon = "io-error";
+const markDownElements = 2;
 const CieNotSupported: React.FunctionComponent<Props> = props => {
+  const [markdownLoaded, setMarkdownLoaded] = useState(0);
+  const handleMarkdownLoaded = () => setMarkdownLoaded(s => s + 1);
   return (
     <React.Fragment>
-      <Markdown>
+      <Markdown onLoadEnd={handleMarkdownLoaded}>
         {I18n.t("authentication.landing.cie_unsupported.body")}
       </Markdown>
-      {Platform.OS === "ios" && (
+      {Platform.OS === "android" && (
         <React.Fragment>
           <View spacer={true} />
-          <Markdown>
+          <Markdown onLoadEnd={handleMarkdownLoaded}>
             {I18n.t("authentication.landing.cie_unsupported.android_desc")}
           </Markdown>
           <View spacer={true} extralarge={true} />
-          <List>
-            <ListItem>
-              <IconFont
-                name={props.hasCieApiLevelSupport ? "io-complete" : "io-error"}
-                size={ICON_SIZE}
-                color={
-                  props.hasCieApiLevelSupport
-                    ? customVariables.brandSuccess
-                    : customVariables.brandDanger
-                }
-              />
-              <Body>
-                <Text>
-                  {I18n.t(
-                    "authentication.landing.cie_unsupported.os_version_unsupported"
-                  )}
-                </Text>
-              </Body>
-            </ListItem>
-            <ListItem>
-              <IconFont
-                name={props.hasCieNFCFeature ? "io-complete" : "io-error"}
-                size={ICON_SIZE}
-                color={
-                  props.hasCieNFCFeature
-                    ? customVariables.brandSuccess
-                    : customVariables.brandDanger
-                }
-              />
-              <Body>
-                <Text>
-                  {I18n.t(
-                    "authentication.landing.cie_unsupported.nfc_incompatible"
-                  )}
-                </Text>
-              </Body>
-            </ListItem>
-          </List>
+          {markdownLoaded === markDownElements && (
+            <List>
+              <ListItem>
+                <IconFont
+                  name={props.hasCieApiLevelSupport ? okIcon : koIcon}
+                  size={ICON_SIZE}
+                  color={props.hasCieApiLevelSupport ? okColor : koColor}
+                />
+                <Body>
+                  <Text>
+                    {I18n.t(
+                      "authentication.landing.cie_unsupported.os_version_unsupported"
+                    )}
+                  </Text>
+                </Body>
+              </ListItem>
+              <ListItem>
+                <IconFont
+                  style={{ alignItems: "flex-start" }}
+                  name={props.hasCieNFCFeature ? okIcon : koIcon}
+                  size={ICON_SIZE}
+                  color={props.hasCieNFCFeature ? okColor : koColor}
+                />
+                <Body>
+                  <Text>
+                    {I18n.t(
+                      "authentication.landing.cie_unsupported.nfc_incompatible"
+                    )}
+                  </Text>
+                </Body>
+              </ListItem>
+            </List>
+          )}
         </React.Fragment>
       )}
     </React.Fragment>
