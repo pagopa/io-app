628af905b9563be3f90baad6939540d4b66502b9
Fabrizio <fartux@gmail.com>
2020-07-22 15:19:09 +0200

    generate pivotal url in changelog

--- scripts/changelog/add_pivotal_stories.js
+++ scripts/changelog/add_pivotal_stories.js
@@ -1,1 +1,87 @@
-console.log("PIVOTAL STORIESSSSS");
+const fs = require("fs-extra");
+const Pivotal = require("pivotaljs");
+const pivotal = new Pivotal();
+
+/**
+ * Skip the use of API to find the story url if the url is already retrieved (contains pivotaltracker.com)
+ * or the id length is < 9 (not a pivotal id)
+ * @param id
+ * @param url
+ * @return {*|boolean}
+ */
+function skipStoryCheck(id, url) {
+  return url.includes("pivotaltracker.com") || id.length < 9;
+}
+
+async function replacePivotalUrl(match, p1, p2, p3) {
+  try {
+    // avoid to use the api if story is already linked to pivotal
+    if (skipStoryCheck(p1, p2)) {
+      return `[${p1}](${p2})`;
+    }
+    // try to get the story with the specified id
+    const story = await getStory(p1.substr(1));
+
+    // if the story doesn't exists (aka is a gitlab pull request id) use the same link
+    // else use the story url
+    const url = story.url !== undefined ? story.url : p2;
+    return `[${p1}](${url})`;
+  } catch (err) {
+    return `[${p1}](${p2})`;
+  }
+}
+
+async function replacePivotalStories() {
+  // read changelog
+  const content = fs.readFileSync("CHANGELOG.md").toString("utf8");
+  // identify the pattern [#XXXXX](url)
+  const pivotalTagRegex = /\[(#\d+)\]\(([a-zA-z:\/\.\d-@:%._\+~#=]+)\)/g;
+
+  // check for all the matches if is a pivotal story and update the url
+  const updatedChangelog = await replaceAsync(
+    content,
+    pivotalTagRegex,
+    replacePivotalUrl
+  );
+
+  // write the new modified changelog
+  fs.writeFileSync("CHANGELOG.md", updatedChangelog);
+}
+
+/**
+ * A custom method created to allows the replace in a string using an async function
+ * @param str
+ * @param regex
+ * @param asyncFn
+ * @return {Promise<*>}
+ */
+async function replaceAsync(str, regex, asyncFn) {
+  const promises = [];
+  str.replace(regex, (match, ...args) => {
+    const promise = asyncFn(match, ...args);
+    promises.push(promise);
+  });
+  const data = await Promise.all(promises);
+  return str.replace(regex, () => data.shift());
+}
+
+/**
+ * Wrap the getStory callback with a promise
+ * @param storyId
+ * @return {Promise<Pivotal.Story>}
+ */
+getStory = storyId => {
+  return new Promise((resolve, reject) => {
+    pivotal.getStory(storyId, (err, story) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(story);
+    });
+  });
+};
+
+// Execute the script to find the pivotal stories id in order to associate the right url
+replacePivotalStories()
+  .then(() => console.log("done"))
+  .catch(ex => console.log(ex));
