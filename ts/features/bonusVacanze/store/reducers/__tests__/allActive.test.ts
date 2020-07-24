28962510b4145c1dd1ffa7d344afc14e5cbea8d5
Matteo Boschi <mbmaken@gmail.com>
2020-07-24 13:22:14 +0200

    [#173965657] update tests

--- ts/features/bonusVacanze/store/reducers/__tests__/allActive.test.ts
+++ ts/features/bonusVacanze/store/reducers/__tests__/allActive.test.ts
@@ -39,6 +39,12 @@ const bonusActive = pot.some({
   applicant_fiscal_code: fiscalCode
 });
 
+const bonusRedeemed = pot.some({
+  ...mockedBonus,
+  status: BonusActivationStatusEnum.REDEEMED,
+  applicant_fiscal_code: fiscalCode
+});
+
 describe("ownedActiveOrRedeemedBonus", () => {
   it("should return an empty array", () => {
     expect(ownedActiveOrRedeemedBonus.resultFunc([], potProfile)).toStrictEqual(
@@ -64,15 +70,39 @@ describe("ownedActiveOrRedeemedBonus", () => {
     ).toStrictEqual([]);
   });
 
-  it("should return an empty array (status !== ACTIVE)", () => {
-    const bonusRedeemed = pot.some({
-      ...mockedBonus,
-      status: BonusActivationStatusEnum.REDEEMED,
-      applicant_fiscal_code: fiscalCode
-    });
+  it("should return the bonus redeemed", () => {
     expect(
       ownedActiveOrRedeemedBonus.resultFunc([bonusRedeemed], potProfile)
     ).toStrictEqual([bonusRedeemed.value]);
+  });
+
+  it("should return the bonus redeemed and active", () => {
+    expect(
+      ownedActiveOrRedeemedBonus.resultFunc(
+        [bonusRedeemed, bonusActive],
+        potProfile
+      )
+    ).toStrictEqual([bonusRedeemed.value, bonusActive.value]);
+  });
+
+  it("should return the bonus redeemed and active", () => {
+    expect(
+      ownedActiveOrRedeemedBonus.resultFunc(
+        [bonusRedeemed, bonusActive, pot.none, bonusDifferentApplicant],
+        potProfile
+      )
+    ).toStrictEqual([bonusRedeemed.value, bonusActive.value]);
+  });
+
+  it("should return an empty array (status !== ACTIVE|REDEEMED)", () => {
+    const bonusProcessing = pot.some({
+      ...mockedBonus,
+      status: BonusActivationStatusEnum.PROCESSING,
+      applicant_fiscal_code: fiscalCode
+    });
+    expect(
+      ownedActiveOrRedeemedBonus.resultFunc([bonusProcessing], potProfile)
+    ).toStrictEqual([]);
   });
 
   it("should return the active bonus", () => {
