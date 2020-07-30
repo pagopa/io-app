8fe90e2ce04ccd8a4c803c1221150faa3449d3cb
Matteo Boschi <mbmaken@gmail.com>
2020-07-30 17:15:35 +0200

    [#174089908] add comment

--- ts/utils/accessibility.ts
+++ ts/utils/accessibility.ts
@@ -62,6 +62,7 @@ export const isScreenReaderEnabled = async (): Promise<boolean> => {
   return maybeReaderEnabled.getOrElse(false);
 };
 
+// return the state of the screen reader when the caller component is mounted
 export const useScreenReaderEnabled = () => {
   const [screenReaderEnabled, setIscreenReaderEnabled] = useState(false);
 
