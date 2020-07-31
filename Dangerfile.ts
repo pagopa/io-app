921a3ab5267246b3fb88f3788a9d4630e02ae74f
Fabrizio <fartux@gmail.com>
2020-07-31 17:51:30 +0200

    test

--- Dangerfile.ts
+++ Dangerfile.ts
@@ -2,15 +2,29 @@
 // See http://danger.systems/js
 // See https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
 import checkDangers from "danger-plugin-digitalcitizenship";
+import {
+  getPivotalStories,
+  getPivotalStoryIDs
+} from "danger-plugin-digitalcitizenship/dist/utils";
 
 import { DangerDSLType } from "danger/distribution/dsl/DangerDSL";
 declare var danger: DangerDSLType;
 
-checkDangers();
+const updatePrTitleForChangelog = async () => {
+  const storyIDs = getPivotalStoryIDs(danger.github.pr.title);
+  console.log("story_IDs " + storyIDs[0]);
+  const stories = await getPivotalStories(storyIDs);
+  console.log("story_IDs " + stories[0]);
 
-danger.github.api.pulls.update({
-  owner: danger.github.thisPR.owner,
-  repo: danger.github.thisPR.repo,
-  pull_number: danger.github.thisPR.number,
-  title: "danger test!"
-});
+  // danger.github.api.pulls.update({
+  //   owner: danger.github.thisPR.owner,
+  //   repo: danger.github.thisPR.repo,
+  //   pull_number: danger.github.thisPR.number,
+  //   title: "danger test!"
+  // });
+};
+
+checkDangers();
+updatePrTitleForChangelog()
+  .then()
+  .catch();
