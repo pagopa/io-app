a4b99f2e855ce77c21d926e62a68b72c5722bd16
Fabrizio <fartux@gmail.com>
2020-07-31 15:16:12 +0200

    test change title

--- Dangerfile.ts
+++ Dangerfile.ts
@@ -3,4 +3,12 @@
 // See https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
 import checkDangers from "danger-plugin-digitalcitizenship";
 
+import { DangerDSLType } from "danger/distribution/dsl/DangerDSL";
+declare var danger: DangerDSLType;
+
 checkDangers();
+
+danger.github.api.pullRequests.update({
+  ...danger.github.thisPR,
+  title: "danger test!"
+});
