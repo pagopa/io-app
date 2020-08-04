4a0d2ce42b3c54a0f2070573d647d3e47667b50d
CrisTofani <cri.tofani@gmail.com>
2020-08-04 17:15:32 +0200

    Adds validation on fields

--- ts/screens/authentication/TestAuthenticationScreen.tsx
+++ ts/screens/authentication/TestAuthenticationScreen.tsx
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { PasswordLogin } from "../../../definitions/backend/PasswordLogin";
 import { LabelledItem } from "../../components/LabelledItem";
 import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
+import { BlockButtonProps } from "../../components/ui/BlockButtons";
 import FooterWithButtons from "../../components/ui/FooterWithButtons";
 import I18n from "../../i18n";
 import { testLoginRequest } from "../../store/actions/authentication";
@@ -12,15 +13,22 @@ import { Dispatch } from "../../store/actions/types";
 
 type Props = ReturnType<typeof mapDispatchToProps>;
 
+const FC_REGEXP = /(^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})$|^([0-9]{11})$)/g;
+
+const checkUsernameValid = (username: string): boolean => {
+  return username.length > 0 && username.match(FC_REGEXP) !== null;
+};
+
 const TestAuthenticationScreen: React.FunctionComponent<Props> = (
   props: Props
 ) => {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-  const confirmButton = {
+  const confirmButton: BlockButtonProps = {
     block: true,
     primary: true,
+    disabled: password.length === 0 || !checkUsernameValid(username),
     onPress: () => {
       props.requestLogin({ username, password } as PasswordLogin);
     },
@@ -35,6 +43,7 @@ const TestAuthenticationScreen: React.FunctionComponent<Props> = (
             type={"text"}
             label={I18n.t("profile.fiscalCode.fiscalCode")}
             icon="io-titolare"
+            isValid={checkUsernameValid(username) ? true : undefined}
             inputProps={{
               value: username,
               placeholder: I18n.t("profile.fiscalCode.fiscalCode"),
@@ -49,6 +58,7 @@ const TestAuthenticationScreen: React.FunctionComponent<Props> = (
             type={"text"}
             label={I18n.t("global.password")}
             icon="io-lucchetto"
+            isValid={password.length > 0 ? true : undefined}
             inputProps={{
               value: password,
               placeholder: I18n.t("global.password"),
