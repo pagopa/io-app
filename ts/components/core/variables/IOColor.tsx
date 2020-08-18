501119615ffb3f19712f09871c2824f426916c65
Fabrizio <fartux@gmail.com>
2020-08-18 14:41:08 +0200

    Add ColorSection, IOStyles and IOStyleVariables

--- ts/components/core/variables/IOColor.tsx
+++ ts/components/core/variables/IOColor.tsx
@@ -1,20 +1,25 @@
 import { ColorValue } from "react-native";
 
+// Ensure the Type for IOColor without losing the inferred types
+function asIOColor<T extends { [key: string]: ColorValue }>(arg: T): T {
+  return arg;
+}
+
 /**
  * The palette of color used, created from
  * https://pagopa.invisionapp.com/console/IO-app---Library-ckcdil0710mt1014buxuo4u34/ckcdilqwl032d01xbx6db5zwz/play
  */
-export const IOColor: Record<string, ColorValue> = {
+export const IOColor = asIOColor({
   black: "#000000",
   white: "#FFFFFF",
-  "bluegrey-dark": "#17324D",
+  bluegreyDark: "#17324D",
   bluegrey: "#5C6F82",
-  "bluegrey-light": "#CCD4DC",
-  "grey-light": "#E6E9F2",
-  "grey-ultralight": "#FCFDFF",
+  bluegreyLight: "#CCD4DC",
+  greyLight: "#E6E9F2",
+  greyUltralight: "#FCFDFF",
   blue: "#0073E6",
   aqua: "#00C5CA",
   red: "#C02927",
-  "grey-gradient-top": "#5C6F82",
-  "grey-gradient-bottom": "#42484F"
-};
+  greyGradientTop: "#5C6F82",
+  greyGradientBottom: "#42484F"
+});
