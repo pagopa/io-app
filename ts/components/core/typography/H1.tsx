2f4719beb8b45869fc2ae6f455a8b99a78596bf0
Fabrizio <fartux@gmail.com>
2020-08-19 16:59:49 +0200

    rework h1 and h2

--- ts/components/core/typography/H1.tsx
+++ ts/components/core/typography/H1.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
-import { useMemo } from "react";
 import { AccessibilityProps } from "react-native";
 import { IOFontFamily, IOFontWeight } from "../fonts";
 import { IOColorType } from "../variables/IOColors";
-import { BaseTypography } from "./BaseTypography";
-import { calculateWeightColor, TypographyProps } from "./common";
+import { typographyFactory, TypographyProps } from "./common";
 
 type H1AllowedColors = Extract<IOColorType, "bluegreyDark" | "white">;
 type H1AllowedWeight = Extract<IOFontWeight, "Bold">;
@@ -17,24 +15,11 @@ const H1FontName: IOFontFamily = "TitilliumWeb";
 const H1FontSize = 28;
 
 export const H1: React.FunctionComponent<OwnProps> = props => {
-  const { weight, color } = useMemo(
-    () =>
-      calculateWeightColor<H1AllowedWeight, H1AllowedColors>(
-        "Bold",
-        "bluegreyDark",
-        props.weight,
-        props.color
-      ),
-    [props.weight, props.color]
-  );
-
-  return (
-    <BaseTypography
-      font={H1FontName}
-      weight={weight}
-      color={color}
-      fontSize={H1FontSize}
-      {...props}
-    />
-  );
+  return typographyFactory<H1AllowedWeight, H1AllowedColors>({
+    ...props,
+    defaultWeight: "Bold",
+    defaultColor: "bluegreyDark",
+    font: H1FontName,
+    fontSize: H1FontSize
+  });
 };
--- ts/components/core/typography/H2.tsx
+++ ts/components/core/typography/H2.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
-import { useMemo } from "react";
 import { AccessibilityProps } from "react-native";
 import { IOFontFamily, IOFontWeight } from "../fonts";
 import { IOColorType } from "../variables/IOColors";
-import { BaseTypography } from "./BaseTypography";
-import { calculateWeightColor, TypographyProps } from "./common";
+import { typographyFactory, TypographyProps } from "./common";
 
 type H2AllowedColors = Extract<IOColorType, "bluegreyDark">;
 type H2AllowedWeight = Extract<IOFontWeight, "Bold">;
@@ -17,24 +15,11 @@ const H2FontName: IOFontFamily = "TitilliumWeb";
 const H2FontSize = 20;
 
 export const H2: React.FunctionComponent<OwnProps> = props => {
-  const { weight, color } = useMemo(
-    () =>
-      calculateWeightColor<H2AllowedWeight, H2AllowedColors>(
-        "Bold",
-        "bluegreyDark",
-        props.weight,
-        props.color
-      ),
-    [props.weight, props.color]
-  );
-
-  return (
-    <BaseTypography
-      font={H2FontName}
-      weight={weight}
-      color={color}
-      fontSize={H2FontSize}
-      {...props}
-    />
-  );
+  return typographyFactory<H2AllowedWeight, H2AllowedColors>({
+    ...props,
+    defaultWeight: "Bold",
+    defaultColor: "bluegreyDark",
+    font: H2FontName,
+    fontSize: H2FontSize
+  });
 };
