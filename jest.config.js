7acb8ad52651f68ccd481940496423c022479bf1
Matteo Boschi <mbmaken@gmail.com>
2020-08-31 10:55:11 +0200

    feat: [#174321085] Save Bonus Vacanze screenshot (#2156)
    * [#174321085] first attempt
    
    * [#174321085] refactoring
    
    * [#174321085] remove useless imports
    
    * [#174321085] add camera oll library
    save on gallery the screen short
    add the time when the screenshot is captured
    
    * [#174321085] improve screenshot layout
    
    * [#174321085] fix
    
    * [#174321085] fix
    
    * [#174321085] refactoring
    
    * [#174321085] fix tests
    
    * [#174321085] restore qrcode handler
    
    * [#174321085] ignore ts lint warning
    
    Co-authored-by: fabriziofff <fartux@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|react-navigation|@react-navigation|react-navigation-redux-helpers|react-native-device-info|native-base|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|rn-placeholder|jsbarcode|@pagopa/react-native-cie|react-native-share|jail-monkey|@react-native-community/art|@react-native-community/push-notification-ios)"
+    "node_modules/(?!(jest-)?react-native|react-navigation|@react-navigation|react-navigation-redux-helpers|react-native-device-info|native-base|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|rn-placeholder|jsbarcode|@pagopa/react-native-cie|react-native-share|jail-monkey|@react-native-community/art|@react-native-community/push-notification-ios|@react-native-community/cameraroll)"
   ],
   globals: {
     "ts-jest": {
