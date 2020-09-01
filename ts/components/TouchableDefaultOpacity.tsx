3656486a1be14448ab9a51bb27774d08a754a35d
Matteo Boschi <mbmaken@gmail.com>
2020-09-01 19:54:41 +0200

    [#174333382] fix type

--- ts/components/TouchableDefaultOpacity.tsx
+++ ts/components/TouchableDefaultOpacity.tsx
@@ -7,17 +7,17 @@ const defaultActiveOpacity = 1.0;
  * return TouchableOpacity component where the activeOpacity is 1.0 by default
  * instead of 0.2 https://github.com/facebook/react-native/blob/3042407f43b69994abc00350681f1f0a79683bfd/Libraries/Components/Touchable/TouchableOpacity.js#L149
  */
-const TouchableDefaultOpacity = (
-  props: TouchableOpacityProps & React.Props<TouchableOpacity>
-) => (
-    <TouchableOpacity
-      {...{
-        ...props,
-        activeOpacity: props.activeOpacity || defaultActiveOpacity
-      }}
-    >
-      {props.children}
-    </TouchableOpacity>
-  );
+const TouchableDefaultOpacity: React.FunctionComponent<
+  TouchableOpacity & TouchableOpacityProps
+> = props => (
+  <TouchableOpacity
+    {...{
+      ...props,
+      activeOpacity: props.activeOpacity || defaultActiveOpacity
+    }}
+  >
+    {props.children}
+  </TouchableOpacity>
+);
 
 export default TouchableDefaultOpacity;
