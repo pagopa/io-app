62be22fcd01574d3c19723bc3d944070b30efcc7
CrisTofani <cri.tofani@gmail.com>
2020-09-01 16:27:14 +0200

    Adds android support

--- ts/screens/modal/RegionServiceModal.tsx
+++ ts/screens/modal/RegionServiceModal.tsx
@@ -13,20 +13,25 @@ type Props = {
 const RegionServiceModal: React.FunctionComponent<Props> = (props: Props) => {
   React.useEffect(() => {
     const cookie: Cookie = {
-      name: "test cookie",
+      name: "test",
       value: "test",
-      domain: "localhost",
+      domain: "192.168.1.20", //the domain on which you need to add the cookie, in this case the IP on your machine
       path: "/",
       version: "1",
       expires: "2021-05-30T12:30:00.00-05:00"
     };
-    CookieManager.set("http://localhost:3000", cookie);
+    /**
+     * the domain on which you need to add the cookie, in this case the IP on your machine
+     * this value and domain value on cookie MUST match
+     * this parameter is considered only by Android.
+     */
+    CookieManager.set("http://192.168.1.20", cookie);
   });
   return (
     <Modal visible={props.modalVisible} onRequestClose={props.onModalClose}>
       <BaseScreenComponent goBack={() => props.onModalClose()}>
         <WebView
-          source={{ uri: "http://localhost:3000/test-html" }}
+          source={{ uri: "http://192.168.1.20:3000/test-html" }}
           textZoom={100}
           injectedJavaScript={closeInjectedScript(AVOID_ZOOM_JS)}
           // onMessage={(event: WebViewMessageEvent) => {
