4f861e454edb51ef65ada25b14ecf7ff66f82582
Cristiano Tofani <cri.tofani@gmail.com>
2020-09-21 17:37:56 +0200

    feat(MyPortal): [#174801172] Screen component for region service external webview (#2204)
    * [#174801172] First implementation of webview screen for services
    
    * [#174801172] Sets cookie on navigation event and store selector
    
    * [#174801172] Code cleanup
    
    * [#174801172] Fixes and refactoring
    
    * [#174801172] Improvements
    
    * [#174801172] refactoring
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

--- ts/store/reducers/authentication.ts
+++ ts/store/reducers/authentication.ts
@@ -1,5 +1,6 @@
 import { none, Option, some } from "fp-ts/lib/Option";
 import { PersistPartial } from "redux-persist";
+import { createSelector } from "reselect";
 import { isActionOf } from "typesafe-actions";
 import { PublicSession } from "../../../definitions/backend/PublicSession";
 import { IdentityProvider } from "../../models/IdentityProvider";
@@ -22,6 +23,9 @@ import { GlobalState } from "./types";
 
 // reason for the user to be in the unauthenticated state
 type LoggedOutReason = "NOT_LOGGED_IN" | "SESSION_EXPIRED";
+
+// PublicSession attributes
+export type TokenName = keyof Omit<PublicSession, "spidLevel">;
 
 // The user is logged out and hasn't selected an IDP
 type LoggedOutWithoutIdp = Readonly<{
@@ -123,7 +127,8 @@ export const isSessionExpiredSelector = (state: GlobalState) =>
 
 export const sessionTokenSelector = (
   state: GlobalState
-): SessionToken | undefined => isLoggedIn(state.authentication)
+): SessionToken | undefined =>
+  isLoggedIn(state.authentication)
     ? state.authentication.sessionToken
     : undefined;
 
@@ -131,6 +136,14 @@ export const sessionInfoSelector = (state: GlobalState) =>
   isLoggedInWithSessionInfo(state.authentication)
     ? some(state.authentication.sessionInfo)
     : none;
+
+export const tokenFromNameSelector = (
+  tokenName: TokenName
+): ((state: GlobalState) => Option<string>) =>
+  createSelector<GlobalState, Option<PublicSession>, Option<string>>(
+    sessionInfoSelector,
+    maybeSessionInfo => maybeSessionInfo.map(si => si[tokenName])
+  );
 
 export const selectedIdentityProviderSelector = (state: GlobalState) =>
   isLoggedOutWithIdp(state.authentication)
