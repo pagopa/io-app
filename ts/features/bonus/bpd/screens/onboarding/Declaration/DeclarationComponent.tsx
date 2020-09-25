1e1fc622d5f635457e1fd091c2c68ad884a6d177
Fabrizio <fartux@gmail.com>
2020-09-25 17:25:39 +0200

    add locales and state

--- ts/features/bonus/bpd/screens/onboarding/Declaration/DeclarationComponent.tsx
+++ ts/features/bonus/bpd/screens/onboarding/Declaration/DeclarationComponent.tsx
@@ -3,10 +3,12 @@ import * as React from "react";
 import { SafeAreaView, ScrollView } from "react-native";
 import { InfoBox } from "../../../../../../components/box/InfoBox";
 import { H1 } from "../../../../../../components/core/typography/H1";
+import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
+import I18n from "../../../../../../i18n";
 import { FooterTwoButtons } from "../../../../bonusVacanze/components/markdown/FooterTwoButtons";
-import { bonusVacanzeStyle } from "../../../../bonusVacanze/components/Styles";
 import { Body } from "../../../../../../components/core/typography/Body";
+import { Link } from "../../../../../../components/core/typography/Link";
 import { DeclarationEntry } from "./DeclarationEntry";
 
 type OwnProps = {
@@ -14,41 +16,95 @@ type OwnProps = {
   onConfirm: () => void;
 };
 
+type PersonalUse = {
+  normal: string;
+  bold: string;
+};
+
+const loadLocales = () => ({
+  title: I18n.t("bonus.bpd.title"),
+  header: I18n.t("bonus.bpd.onboarding.declaration.header"),
+  age: I18n.t("bonus.bpd.onboarding.declaration.conditions.age"),
+  resident: I18n.t("bonus.bpd.onboarding.declaration.conditions.resident"),
+  personal_use: {
+    normal: I18n.t(
+      "bonus.bpd.onboarding.declaration.conditions.personal_use.normal"
+    ),
+    bold: I18n.t(
+      "bonus.bpd.onboarding.declaration.conditions.personal_use.bold"
+    )
+  },
+  disclaimer: {
+    normal: I18n.t("bonus.bpd.onboarding.declaration.disclaimer.normal"),
+    link: I18n.t("bonus.bpd.onboarding.declaration.disclaimer.link")
+  }
+});
+
+const personalUseText = (personalUse: PersonalUse) => (
+  <Body>
+    {personalUse.normal}
+    <Body>{personalUse.normal}</Body>
+  </Body>
+);
+
 /**
  * This screen allows the user to declare the required conditions
  */
-export const DeclarationComponent: React.FunctionComponent<OwnProps> = props => (
-  <BaseScreenComponent
-    goBack={props.onCancel}
-    headerTitle={"Cashback Pagamenti Digitali"}
-  >
-    <SafeAreaView style={bonusVacanzeStyle.flex}>
-      <ScrollView>
-        <View style={bonusVacanzeStyle.horizontalPadding}>
-          <View spacer={true} large={true} />
-          <H1>Per poter attivare il cashback devi dichiarare</H1>
-          <View spacer={true} extralarge={true} />
-          <DeclarationEntry text={"di essere maggiorenne"} />
-          <DeclarationEntry text={"di risiedere in Italia"} />
-          <DeclarationEntry
-            text={
-              "che userai gli strumenti di pagamento su cui è attivo il cashback solo per acquisti effettuati fuori dall'esercizio di attività di Impresa, Arte o Professione "
-            }
-          />
-          <View spacer={true} small={true} />
-          <InfoBox>
-            <Body>
-              Questa autodichirazione è resa ai sensi del dpr 28 dicembre 2000
-              n. 445 art 46 e 47
-            </Body>
-          </InfoBox>
-        </View>
-      </ScrollView>
-      <FooterTwoButtons
-        onCancel={props.onCancel}
-        onRight={props.onConfirm}
-        title={"Continua"}
-      />
-    </SafeAreaView>
-  </BaseScreenComponent>
-);
+export const DeclarationComponent: React.FunctionComponent<OwnProps> = props => {
+  const {
+    title,
+    header,
+    age,
+    resident,
+    personal_use,
+    disclaimer
+  } = loadLocales();
+
+  // tracks the condition accepted, used to enabled the "continue" button
+  const [conditionsAccepted, setConditionAccepted] = React.useState(0);
+
+  // transform the required textual conditions to graphical objects with checkbox
+  const requiredConditions = [
+    age,
+    resident,
+    personalUseText(personal_use)
+  ].map((condition, index) => (
+    <DeclarationEntry
+      text={condition}
+      key={index}
+      onValueChange={newValue =>
+        newValue
+          ? setConditionAccepted(conditionsAccepted + 1)
+          : setConditionAccepted(conditionsAccepted - 1)
+      }
+    />
+  ));
+
+  return (
+    <BaseScreenComponent goBack={props.onCancel} headerTitle={title}>
+      <SafeAreaView style={IOStyles.flex}>
+        <ScrollView>
+          <View style={IOStyles.horizontalContentPadding}>
+            <View spacer={true} large={true} />
+            <H1>{header}</H1>
+            <View spacer={true} extralarge={true} />
+            {requiredConditions}
+            <View spacer={true} small={true} />
+            <InfoBox>
+              <Body>
+                {disclaimer.normal}
+                <Link>{disclaimer.link}</Link>
+              </Body>
+            </InfoBox>
+          </View>
+        </ScrollView>
+        <FooterTwoButtons
+          rightDisabled={conditionsAccepted !== 3}
+          onCancel={props.onCancel}
+          onRight={props.onConfirm}
+          title={I18n.t("global.buttons.continue")}
+        />
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
