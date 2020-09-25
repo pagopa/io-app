676be0145a02f7220dc49f24a0e4e9952e4d3424
Fabrizio <fartux@gmail.com>
2020-09-25 17:29:54 +0200

    edit locales

--- ts/features/bonus/bpd/screens/onboarding/Declaration/DeclarationComponent.tsx
+++ ts/features/bonus/bpd/screens/onboarding/Declaration/DeclarationComponent.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { SafeAreaView, ScrollView } from "react-native";
 import { InfoBox } from "../../../../../../components/box/InfoBox";
 import { H1 } from "../../../../../../components/core/typography/H1";
+import { Label } from "../../../../../../components/core/typography/Label";
 import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
 import I18n from "../../../../../../i18n";
@@ -40,10 +41,14 @@ const loadLocales = () => ({
   }
 });
 
+/**
+ * Need a specific rendering of this component because have some bold parts
+ * @param personalUse
+ */
 const personalUseText = (personalUse: PersonalUse) => (
   <Body>
     {personalUse.normal}
-    <Body>{personalUse.normal}</Body>
+    <Label color={"bluegrey"}>{personalUse.bold}</Label>
   </Body>
 );
 
