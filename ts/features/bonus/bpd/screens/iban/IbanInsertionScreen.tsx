38c280870110e83b10f47927f7626ebd2fa154c1
Fabrizio <fartux@gmail.com>
2020-09-29 17:35:32 +0200

    add screens

---
+++ ts/features/bonus/bpd/screens/iban/IbanInsertionScreen.tsx
@@ -0,0 +1,16 @@
+import { View } from "native-base";
+import * as React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { GlobalState } from "../../../../../store/reducers/types";
+
+export const IbanInsertionScreen: React.FunctionComponent = () => <View />;
+
+const mapDispatchToProps = (_: Dispatch) => ({});
+
+const mapStateToProps = (_: GlobalState) => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(IbanInsertionScreen);
---
+++ ts/features/bonus/bpd/screens/iban/IbanKONotOwned.tsx
@@ -0,0 +1,13 @@
+import { View } from "native-base";
+import * as React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { GlobalState } from "../../../../../store/reducers/types";
+
+export const IbanKoNotOwned: React.FunctionComponent = () => <View />;
+
+const mapDispatchToProps = (_: Dispatch) => ({});
+
+const mapStateToProps = (_: GlobalState) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(IbanKoNotOwned);
