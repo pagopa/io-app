07bf628512edbfbd8ef385674a73cccb6226b3b8
Simone <simone.biffi.91@gmail.com>
2020-10-01 11:52:33 +0200

    [#175060613] remove screen in white list

--- ts/store/reducers/allowedSnapshotScreens.ts
+++ ts/store/reducers/allowedSnapshotScreens.ts
@@ -1,19 +1,11 @@
 import { createSelector } from "reselect";
-import { bonusVacanzeEnabled } from "../../config";
-import BONUSVACANZE_ROUTES from "../../features/bonus/bonusVacanze/navigation/routes";
 import ROUTES from "../../navigation/routes";
 import { isDebugModeEnabledSelector } from "./debug";
 import { plainNavigationCurrentRouteSelector } from "./navigation";
 
 const defaultScreenBlackList: ReadonlyArray<string> = [ROUTES.WALLET_ADD_CARD];
 
-const screenBonusVacanzaWhiteList: ReadonlyArray<string> = [
-  BONUSVACANZE_ROUTES.BONUS_ACTIVE_DETAIL_SCREEN
-];
-
-export const screenBlackList = bonusVacanzeEnabled
-  ? new Set([...defaultScreenBlackList, ...screenBonusVacanzaWhiteList])
-  : new Set(defaultScreenBlackList);
+export const screenBlackList = new Set(defaultScreenBlackList);
 
 /**
  * Return {true} if the current screen can be snapshotted (android only).
