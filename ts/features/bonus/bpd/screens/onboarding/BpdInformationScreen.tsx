526c64bb0c1d2097a8da88cc25dbd060c96754d1
fabriziofff <fartux@gmail.com>
2020-10-01 10:55:17 +0200

    feat(Bonus Pagamenti Digitali): [#174837969] bpd information & tos screen (#2231)
    * [#174858345] Add screen placeholder for bpd onboarding
    
    * [#174840617] add navigator and routes
    
    * wip action
    
    * add missing actions
    
    * [#174897872] State, actions and reducers stub for bpd onboarding
    
    * add comments
    
    * Update with the latest design changes
    
    * fix navigation
    
    * wip onboarding saga
    
    * add enrollToBpdSaga
    
    * revert wallethomescreen
    
    * update saga
    
    * add checkbox
    
    * remove comment
    
    * wip integration with networking
    
    * Integrate with networking
    
    * integrate bpdEnroll
    
    * remove all bdp from code
    
    * rename variables
    
    * typo
    
    * wip declarationscreen
    
    * update showroom
    
    * update showroom
    
    * add locales and state
    
    * edit locales
    
    * adjust layout
    
    * fix
    
    * Update ts/features/bonus/bpd/screens/onboarding/DeclarationScreen.tsx
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * Update ts/features/bonus/bpd/screens/onboarding/BpdInformationScreen.tsx
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * change enroll to bpd
    
    * unused import
    
    * simplify code
    
    * [#174860317] refinement for LoadBpdActivationStatus
    
    * add load activate bpd screen and fix some saga bugs
    
    * fixes
    
    * add comment
    
    * fix renaming
    
    * [#174837969] Extract generic BonusInformationComponent and add BpdInformationScreen
    
    * Create RawCheckBox and change CheckBox
    
    * Update ts/features/bonus/bpd/screens/onboarding/declaration/DeclarationComponent.tsx
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * fix disabled forwarding
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

--- ts/features/bonus/bpd/screens/onboarding/BpdInformationScreen.tsx
+++ ts/features/bonus/bpd/screens/onboarding/BpdInformationScreen.tsx
@@ -1,31 +1,45 @@
-import { Button } from "native-base";
 import * as React from "react";
-import { SafeAreaView } from "react-native";
 import { connect } from "react-redux";
-import { H1 } from "../../../../../components/core/typography/H1";
-import { Label } from "../../../../../components/core/typography/Label";
 import { Dispatch } from "../../../../../store/actions/types";
-import { bpdUserActivate } from "../../store/actions/onboarding";
+import { GlobalState } from "../../../../../store/reducers/types";
+import { availableBonusTypesSelectorFromId } from "../../../bonusVacanze/store/reducers/availableBonusesTypes";
+import { ID_BPD_TYPE } from "../../../bonusVacanze/utils/bonus";
+import BonusInformationComponent from "../../../common/components/BonusInformationComponent";
+import {
+  bpdOnboardingCancel,
+  bpdUserActivate
+} from "../../store/actions/onboarding";
 
-export type Props = ReturnType<typeof mapDispatchToProps>;
+export type Props = ReturnType<typeof mapDispatchToProps> &
+  ReturnType<typeof mapStateToProps>;
 
 /**
  * This Screen shows all the information about the bpd program, with the rules and t&c.
  */
 
 const BpdInformationScreen: React.FunctionComponent<Props> = props => (
-  <SafeAreaView style={{ flex: 1 }}>
-    <H1>Information & tos</H1>
-    <Button>
-      <Label color={"white"} onPress={props.userActivateBpd}>
-        Attiva
-      </Label>
-    </Button>
-  </SafeAreaView>
+  <>
+    {props.bonus.isSome() ? (
+      <BonusInformationComponent
+        bonus={props.bonus.value}
+        onConfirm={props.userActivateBpd}
+        onCancel={props.onCancel}
+      />
+    ) : null}
+  </>
 );
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  userActivateBpd: () => dispatch(bpdUserActivate())
+const mapStateToProps = (state: GlobalState) => ({
+  // display the error with the retry only in case of networking errors
+  bonus: availableBonusTypesSelectorFromId(ID_BPD_TYPE)(state)
 });
 
-export default connect(undefined, mapDispatchToProps)(BpdInformationScreen);
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  userActivateBpd: () => dispatch(bpdUserActivate()),
+  onCancel: () => dispatch(bpdOnboardingCancel())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BpdInformationScreen);
