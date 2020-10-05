a49991aa7fc0987d2ba75eb3294b20870d290d91
CrisTofani <cri.tofani@gmail.com>
2020-10-05 12:30:01 +0200

    [#175011879] Renaming item component

---
+++ ts/features/wallet/onboarding/bancomat/components/BankPreviewItem.tsx
@@ -0,0 +1,98 @@
+import { fromNullable } from "fp-ts/lib/Option";
+import { ListItem, View } from "native-base";
+import * as React from "react";
+import { Image, ImageStyle, StyleProp, StyleSheet } from "react-native";
+import { Abi } from "../../../../../../definitions/pagopa/bancomat/Abi";
+import ButtonDefaultOpacity from "../../../../../components/ButtonDefaultOpacity";
+import { LabelSmall } from "../../../../../components/core/typography/LabelSmall";
+import { IOColors } from "../../../../../components/core/variables/IOColors";
+import IconFont from "../../../../../components/ui/IconFont";
+
+type Props = {
+  bank: Abi;
+  inList: boolean;
+  onPress: () => void;
+};
+
+const BASE_IMG_H = 40;
+const BASE_IMG_W = 160;
+
+const styles = StyleSheet.create({
+  flexRow: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 105
+  },
+  listItem: {
+    flexDirection: "column",
+    alignItems: "flex-start"
+  },
+  gridItem: {
+    margin: 16,
+    padding: 30,
+    height: 72,
+    width: 156,
+    flexDirection: "column",
+    flex: 1
+  }
+});
+
+export const BankPreviewItem: React.FunctionComponent<Props> = (
+  props: Props
+) => {
+  const [imageW, setImageW] = React.useState(BASE_IMG_W);
+  const [calculatedDim, setCalculatedDim] = React.useState(false);
+
+  const handleImageDimensionSuccess = (width: number, height: number) => {
+    if (width > 0 && height > 0) {
+      const newWidth = (width / height) * BASE_IMG_H;
+      setImageW(newWidth > BASE_IMG_W ? BASE_IMG_W : newWidth);
+      setCalculatedDim(true);
+    }
+  };
+
+  React.useEffect(() => {
+    fromNullable(props.bank.logoUrl).map(url =>
+      Image.getSize(url, handleImageDimensionSuccess)
+    );
+  }, []);
+
+  const imageStyle: StyleProp<ImageStyle> = {
+    width: imageW,
+    height: BASE_IMG_H,
+    resizeMode: "contain"
+  };
+
+  return props.inList ? (
+    <ListItem style={styles.flexRow} onPress={props.onPress}>
+      <View style={styles.listItem}>
+        <View spacer={true} />
+        {calculatedDim && props.bank.logoUrl && (
+          <Image style={imageStyle} source={{ uri: props.bank.logoUrl }} />
+        )}
+        <View spacer={true} small={true} />
+        <LabelSmall color={"bluegrey"} weight={"Bold"}>
+          {props.bank.name ? props.bank.name : "Nome non trovato"}
+        </LabelSmall>
+        <View spacer={true} />
+      </View>
+      <IconFont name={"io-right"} color={IOColors.blue} />
+    </ListItem>
+  ) : (
+    <ButtonDefaultOpacity
+      white={true}
+      style={styles.gridItem}
+      onPress={props.onPress}
+    >
+      {calculatedDim && props.bank.logoUrl && (
+        <Image style={imageStyle} source={{ uri: props.bank.logoUrl }} />
+      )}
+      <View spacer={true} />
+      <LabelSmall color={"bluegrey"} weight={"Bold"}>
+        {props.bank.name ? props.bank.name : "Nome non trovato"}
+      </LabelSmall>
+    </ButtonDefaultOpacity>
+  );
+};
