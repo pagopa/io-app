a1546d5c1435d36911600e16a3d10ba9cfe138f9
fabriziofff <fartux@gmail.com>
2020-10-05 11:31:40 +0200

    feat(Bonus Pagamenti Digitali): [#175013470,#174847416,#174847513] Onboarding Iban business logic workflow, actions and store (#2252)
    * add screens
    
    * add comments
    
    * Add navigation stack
    
    * add screen events layout
    
    * wip
    
    * wip
    
    * wip
    
    * rework ibaninsertion
    
    * clean code
    
    * file mssing
    
    * cleaning
    
    * reorder store
    
    * clean code
    
    * add comments
    
    * comments
    
    * Update ts/features/bonus/bpd/saga/orchestration/onboarding/enrollToBpd.ts
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * Update ts/features/bonus/bpd/screens/iban/IbanLoadingUpsert.tsx
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

---
+++ ts/features/bonus/bpd/screens/iban/IbanLoadingUpsert.tsx
@@ -0,0 +1,56 @@
+import { fromNullable } from "fp-ts/lib/Option";
+import * as React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { Iban } from "../../../../../../definitions/backend/Iban";
+import { GlobalState } from "../../../../../store/reducers/types";
+import { useHardwareBackButton } from "../../../bonusVacanze/components/hooks/useHardwareBackButton";
+import { LoadingErrorComponent } from "../../../bonusVacanze/components/loadingErrorScreen/LoadingErrorComponent";
+import {
+  bpdIbanInsertionCancel,
+  bpdUpsertIban
+} from "../../store/actions/iban";
+import {
+  bpdUpsertIbanIsError,
+  bpdUpsertIbanSelector
+} from "../../store/reducers/details/activation/payoffInstrument";
+
+export type Props = ReturnType<typeof mapDispatchToProps> &
+  ReturnType<typeof mapStateToProps>;
+
+/**
+ * displays to the user a loading screen while sending the new iban or
+ * possibly an error screen in case of unmanaged errors.
+ * @param props
+ * @constructor
+ */
+const IbanLoadingUpsert: React.FunctionComponent<Props> = props => {
+  useHardwareBackButton(() => {
+    if (!props.isLoading) {
+      props.onAbort();
+    }
+    return true;
+  });
+  return (
+    <LoadingErrorComponent
+      {...props}
+      loadingCaption={"insert new iban loading..."}
+      onAbort={props.onAbort}
+      onRetry={() =>
+        fromNullable(props.ibanValue.value).map(props.onRetry)
+      }
+    />
+  );
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  onAbort: () => dispatch(bpdIbanInsertionCancel()),
+  onRetry: (iban: Iban) => dispatch(bpdUpsertIban.request(iban))
+});
+
+const mapStateToProps = (state: GlobalState) => ({
+  isLoading: !bpdUpsertIbanIsError(state),
+  ibanValue: bpdUpsertIbanSelector(state)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(IbanLoadingUpsert);
--- ts/features/bonus/bpd/store/actions/details.ts
+++ ts/features/bonus/bpd/store/actions/details.ts
@@ -1,9 +1,7 @@
 import { ActionType, createAsyncAction } from "typesafe-actions";
-import { Iban } from "../../../../../../definitions/backend/Iban";
-import { IbanStatus } from "../../saga/networking/patchCitizenIban";
 
 /**
- * This file contains all the action related to the bpd details like the activation status, iban, value, etc.
+ * This file contains all the action related to the bpd details like the activation status, value, etc.
  */
 
 // TODO change payload for loadBpdActivationStatus with this one
@@ -21,17 +19,4 @@ export const bpdLoadActivationStatus = createAsyncAction(
   "BPD_LOAD_ACTIVATION_STATUS_FAILURE"
 )<void, BpdActivationPayload, Error>();
 
-// represent the outcome of iban upsert
-export type IbanUpsertResult = { payoffInstr?: Iban; status: IbanStatus };
-/**
- * Request the upsert of the citizen iban
- */
-export const bpdUpsertIban = createAsyncAction(
-  "BPD_UPSERT_IBAN_REQUEST",
-  "BPD_UPSERT_IBAN_SUCCESS",
-  "BPD_UPSERT_IBAN_FAILURE"
-)<Iban, IbanUpsertResult, Error>();
-
-export type BpdDetailsActions =
-  | ActionType<typeof bpdLoadActivationStatus>
-  | ActionType<typeof bpdUpsertIban>;
+export type BpdDetailsActions = ActionType<typeof bpdLoadActivationStatus>;
