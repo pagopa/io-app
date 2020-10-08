84f65a6bd2b40df4a7360f97717faed8ef57cba3
Matteo Boschi <mbmaken@gmail.com>
2020-10-08 10:00:01 +0200

    [#175165785] update spec remove api key header

--- ts/config.ts
+++ ts/config.ts
@@ -47,9 +47,7 @@ export const bonusVacanzeEnabled: boolean =
 
 export const myPortalEnabled: boolean = Config.MYPORTAL_ENABLED === "YES";
 
-export const bpdEnabled: boolean = Config.BPD_ENABLED === "YES";
-
-export const bpdApiKey: string = Config.BPD_API_KEY;
+export const bpdEnabled: boolean = true;
 
 export const bpdApiUrlPrefix: string = Config.BPD_API_URL_PREFIX;
 
