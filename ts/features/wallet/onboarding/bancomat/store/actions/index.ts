cd92644e197ae80ad54eb2b443fe7fb7f46720b1
Matteo Boschi <mbmaken@gmail.com>
2020-10-08 11:03:45 +0200

    feat(Bonus Pagamenti Digitali): [#175115845] Add networking and store logic to support pans search (#2263)
    * [#175115845] wip
    
    * [#175115845] wip
    
    * [#175115845] fixes
    
    * [#175115845] fixes
    
    * [#175115845] update comment
    
    * [#175115845] reshape store
    
    * [#175115845] add utility getValueOrElse in RemoteValue
    
    Co-authored-by: fabriziofff <fartux@gmail.com>

--- ts/features/wallet/onboarding/bancomat/store/actions/index.ts
+++ ts/features/wallet/onboarding/bancomat/store/actions/index.ts
@@ -1,5 +1,6 @@
 import { ActionType, createAsyncAction } from "typesafe-actions";
 import { AbiResponse } from "../../../../../../../definitions/pagopa/bancomat/AbiResponse";
+import { PatchedCard } from "../../../../../bonus/bpd/api/patchedTypes";
 
 /**
  * Request the list of all abi
@@ -10,4 +11,15 @@ export const loadAbi = createAsyncAction(
   "WALLET_LOAD_ABI_FAILURE"
 )<void, AbiResponse, Error>();
 
-export type AbiActions = ActionType<typeof loadAbi>;
+/**
+ * search for user's pans
+ */
+export const loadPans = createAsyncAction(
+  "WALLET_LOAD_PANS_REQUEST",
+  "WALLET_LOAD_PANS_SUCCESS",
+  "WALLET_LOAD_PANS_FAILURE"
+)<string | undefined, ReadonlyArray<PatchedCard>, Error>();
+
+export type AbiActions =
+  | ActionType<typeof loadAbi>
+  | ActionType<typeof loadPans>;
