787b00967c3911ca7a4940bb99c55541ff155537
Fabrizio <fartux@gmail.com>
2020-10-09 17:05:27 +0200

    wip

--- ts/features/bonus/bpd/store/reducers/details/paymentMethods.ts
+++ ts/features/bonus/bpd/store/reducers/details/paymentMethods.ts
@@ -1,8 +1,12 @@
+import { fromNullable } from "fp-ts/lib/Option";
 import { getType } from "typesafe-actions";
 import { Action } from "../../../../../../store/actions/types";
 import { IndexedById } from "../../../../../../store/helpers/indexer";
-import { RemoteValue } from "../../../model/RemoteValue";
-import { bpdUpsertIban } from "../../actions/iban";
+import {
+  remoteLoading,
+  remoteUndefined,
+  RemoteValue
+} from "../../../model/RemoteValue";
 import { bpdPaymentMethodActivation } from "../../actions/paymentMethods";
 
 enum EBpdActivationStatus {
@@ -22,20 +26,35 @@ type BpdActivationStatus = {
 
 export type BpdPaymentMethods = {
   // the value used as key to group the information related to the payment method
-  hpan: string;
+  hPan: string;
   // represent the effective remote value of bpd activation status on the payment method
   actualValue: RemoteValue<Error, BpdActivationStatus>;
   // represent the remote request to update the activation status on the payment method
   upsertValue: RemoteValue<Error, void>;
 };
 
+const readBpdPaymentMethodEntry = (
+  hPan: string,
+  data: IndexedById<BpdPaymentMethods>
+): BpdPaymentMethods =>
+  fromNullable(data[hPan]).getOrElse({
+    hPan,
+    actualValue: remoteUndefined,
+    upsertValue: remoteUndefined
+  });
+
 export const bpdPaymentMethodsReducer = (
   state: IndexedById<BpdPaymentMethods> = {},
   action: Action
 ): IndexedById<BpdPaymentMethods> => {
   switch (action.type) {
-    case getType(bpdPaymentMethodActivation.request):
-      return { ...state };
+    case getType(bpdPaymentMethodActivation.request): {
+      const entry = {
+        ...readBpdPaymentMethodEntry(action.payload, state),
+        actualValue: remoteLoading
+      };
+      return { ...state, [action.payload]: entry };
+    }
   }
 
   return state;
