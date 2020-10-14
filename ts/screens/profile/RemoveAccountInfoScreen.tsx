047a1235c8b6b25ee96ccb35e5ae5c94adc83cbc
Simone <simone.biffi.91@gmail.com>
2020-10-14 17:12:03 +0200

    [#175060536] add navigation to the RemoveAccountDetailsScreen

--- ts/screens/profile/RemoveAccountInfoScreen.tsx
+++ ts/screens/profile/RemoveAccountInfoScreen.tsx
@@ -1,52 +1,28 @@
-import * as pot from "italia-ts-commons/lib/pot";
-import { Content, View } from "native-base";
+import { Content } from "native-base";
 import * as React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 
-import { NavigationScreenProp, NavigationState } from "react-navigation";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
+import { NavigationScreenProps } from "react-navigation";
 import { IOStyles } from "../../components/core/variables/IOStyles";
-import { UserDataProcessingChoiceEnum } from "../../../definitions/backend/UserDataProcessingChoice";
-import { withLoadingSpinner } from "../../components/helpers/withLoadingSpinner";
 import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
-import { EdgeBorderComponent } from "../../components/screens/EdgeBorderComponent";
-import ScreenContent from "../../components/screens/ScreenContent";
 import FooterWithButtons from "../../components/ui/FooterWithButtons";
-import Markdown from "../../components/ui/Markdown";
 import I18n from "../../i18n";
-import ROUTES from "../../navigation/routes";
-import { ReduxProps } from "../../store/actions/types";
-import {
-  resetUserDataProcessingRequest,
-  upsertUserDataProcessing
-} from "../../store/actions/userDataProcessing";
-import { GlobalState } from "../../store/reducers/types";
-import { userDataProcessingSelector } from "../../store/reducers/userDataProcessing";
-import themeVariables from "../../theme/variables";
-import { showToast } from "../../utils/showToast";
-import TopScreenComponent from "../../components/screens/TopScreenComponent";
 import { H1 } from "../../components/core/typography/H1";
 import { H4 } from "../../components/core/typography/H4";
+import ROUTES from "../../navigation/routes";
 
-type OwnProps = {
-  navigation: NavigationScreenProp<NavigationState>;
-};
-
-type Props = ReduxProps &
-  OwnProps &
-  ReturnType<typeof mapStateToProps> &
-  ReturnType<typeof mapDispatchToProps>;
+type Props = NavigationScreenProps;
 
 /**
  * A screen to explain how the account removal works.
  * Here user can ask to delete his account
  */
-const RemoveAccountInfo: React.FunctionComponent<Props> = () => {
+const RemoveAccountInfo: React.FunctionComponent<Props> = props => {
   const continueButtonProps = {
     block: true,
     primary: true,
-    onPress: () => none,
+    onPress: () =>
+      props.navigation.navigate(ROUTES.PROFILE_REMOVE_ACCOUNT_DETAILS),
     title: I18n.t("profile.main.privacy.removeAccount.info.cta")
   };
 
