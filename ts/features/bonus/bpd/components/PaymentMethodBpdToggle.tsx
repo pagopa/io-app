ca451ca018f17def1ece139cb66efd5a3ba43d18
Fabrizio <fartux@gmail.com>
2020-10-15 12:00:30 +0200

    [#175010541] draft layout

--- ts/features/bonus/bpd/components/PaymentMethodBpdToggle.tsx
+++ ts/features/bonus/bpd/components/PaymentMethodBpdToggle.tsx
@@ -1,14 +1,31 @@
-import { View } from "native-base";
+import { Switch, View } from "native-base";
 import * as React from "react";
+import { Image, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { Body } from "../../../../components/core/typography/Body";
+import { H4 } from "../../../../components/core/typography/H4";
 import { GlobalState } from "../../../../store/reducers/types";
+import image from "../../../../../img/wallet/cards-icons/pagobancomat.png";
 
 type OwnProps = {};
 
 export type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps> &
   OwnProps;
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    height: 48
+  },
+  cardIcon: {
+    width: 40,
+    height: 25
+  }
+});
 
 /**
  * This component represents the activation state of bpd on a payment method.
@@ -17,7 +34,18 @@ export type Props = ReturnType<typeof mapDispatchToProps> &
  * - Sync the remote communication with the graphical states
  * @constructor
  */
-const PaymentMethodBpdToggle: React.FunctionComponent<Props> = () => <View />;
+const PaymentMethodBpdToggle: React.FunctionComponent<Props> = () => (
+  <>
+    <View style={styles.row}>
+      <View style={{ flexDirection: "row", flex: 1 }}>
+        <Image source={image} style={styles.cardIcon} />
+        <View hspacer={true} />
+        <Body>Intesa San Paolo </Body>
+      </View>
+      <Switch />
+    </View>
+  </>
+);
 
 const mapDispatchToProps = (_: Dispatch) => ({});
 
