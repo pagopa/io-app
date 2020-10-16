1be15c836040f500b893d3324408965fe864520c
Fabrizio <fartux@gmail.com>
2020-10-16 19:54:03 +0200

    link to business logic

--- ts/features/bonus/bpd/screens/onboarding/EnrollPaymentMethodsScreen.tsx
+++ ts/features/bonus/bpd/screens/onboarding/EnrollPaymentMethodsScreen.tsx
@@ -1,6 +1,7 @@
+import * as pot from "italia-ts-commons/lib/pot";
 import { View } from "native-base";
 import * as React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { Body } from "../../../../../components/core/typography/Body";
@@ -9,8 +10,12 @@ import { IOStyles } from "../../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../../components/screens/BaseScreenComponent";
 import I18n from "../../../../../i18n";
 import { navigateToWalletHome } from "../../../../../store/actions/navigation";
+import { navigationHistoryPop } from "../../../../../store/actions/navigationHistory";
 import { GlobalState } from "../../../../../store/reducers/types";
+import { walletsSelector } from "../../../../../store/reducers/wallet/wallets";
+import { Wallet } from "../../../../../types/pagopa";
 import { FooterTwoButtons } from "../../../bonusVacanze/components/markdown/FooterTwoButtons";
+import { PaymentMethodBpdList } from "../../components/PaymentMethodBpdList";
 
 const loadLocales = () => ({
   headerTitle: I18n.t("bonus.bpd.title"),
@@ -24,6 +29,21 @@ const loadLocales = () => ({
 export type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
 
+const renderPaymentMethod = (
+  potWallets: pot.Pot<ReadonlyArray<Wallet>, Error>
+) =>
+  pot.fold(
+    potWallets,
+    () => null,
+    () => null,
+    _ => null,
+    _ => null,
+    value => <PaymentMethodBpdList paymentList={value} />,
+    value => <PaymentMethodBpdList paymentList={value} />,
+    value => <PaymentMethodBpdList paymentList={value} />,
+    value => <PaymentMethodBpdList paymentList={value} />
+  );
+
 /**
  * This screen allows the user to activate bpd on the payment methods already in the wallet
  */
@@ -32,13 +52,18 @@ const EnrollPaymentMethodsScreen: React.FunctionComponent<Props> = props => {
   return (
     <BaseScreenComponent goBack={false} headerTitle={headerTitle}>
       <SafeAreaView style={IOStyles.flex}>
-        <View style={[IOStyles.horizontalContentPadding, IOStyles.flex]}>
-          <View spacer={true} large={true} />
-          <H1>{title}</H1>
-          <View spacer={true} extralarge={true} />
-          <Body>{body1}</Body>
-          <Body>{body2}</Body>
-        </View>
+        <ScrollView>
+          <View style={[IOStyles.horizontalContentPadding, IOStyles.flex]}>
+            <View spacer={true} large={true} />
+            <H1>{title}</H1>
+            <View spacer={true} extralarge={true} />
+            <Body>{body1}</Body>
+            <View spacer={true} large={true} />
+            {renderPaymentMethod(props.potWallets)}
+            <View spacer={true} large={true} />
+            <Body>{body2}</Body>
+          </View>
+        </ScrollView>
         <FooterTwoButtons
           onRight={props.skip}
           onCancel={props.skip}
@@ -51,10 +76,15 @@ const EnrollPaymentMethodsScreen: React.FunctionComponent<Props> = props => {
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  skip: () => dispatch(navigateToWalletHome())
+  skip: () => {
+    dispatch(navigateToWalletHome());
+    dispatch(navigationHistoryPop(1));
+  }
 });
 
-const mapStateToProps = (_: GlobalState) => ({});
+const mapStateToProps = (state: GlobalState) => ({
+  potWallets: walletsSelector(state)
+});
 
 export default connect(
   mapStateToProps,
