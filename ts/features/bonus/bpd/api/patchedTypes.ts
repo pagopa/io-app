abd1906bb3c51920833cf9c8ec950f861d008beb
Matteo Boschi <mbmaken@gmail.com>
2020-10-16 17:39:06 +0200

    feat(Bonus Pagamenti Digitali): [#175185611] Add pans to wallet (#2286)
    * [#175185611] removed useless type patch
    
    * [#175185611] support add pans API
    
    * [#175185611] update comment
    
    * [#175185611] refactoring
    
    * [#175185611] remove patchedCard
    
    * [#175185611] remove patchedCard
    
    * [#175185611] add pans a single array item
    
    * [#175185611] renaming
    
    * [#175185611] add abi length ref
    
    * [#175185611] refactoring
    
    * [#175185611] change comment
    
    * [#175185611] renaming
    
    * [#175185611] support WalletV2Response
    
    * [#175185611] merging
    
    * [#175185611] fix lint errors
    
    Co-authored-by: fabriziofff <fartux@gmail.com>

--- ts/features/bonus/bpd/api/patchedTypes.ts
+++ ts/features/bonus/bpd/api/patchedTypes.ts
@@ -1,5 +1,4 @@
 import * as t from "io-ts";
-import { enumType } from "italia-ts-commons/lib/types";
 
 /**
  * patched version of CitizenResource
@@ -25,50 +24,3 @@ export const PatchedCitizenResource = t.intersection(
 );
 
 export type PatchedCitizenResource = t.TypeOf<typeof PatchedCitizenResource>;
-
-/**
- * patched version of Card / Cards
- * - expiringDate is PatterString('yyyy-MM-dd') instead of string
- */
-export enum ProductTypeEnum {
-  "PP" = "PP",
-
-  "DEB" = "DEB"
-}
-
-export enum ValidityStateEnum {
-  "V" = "V",
-
-  "BR" = "BR"
-}
-
-// required attributes
-const CardR = t.interface({});
-
-// optional attributes
-const CardO = t.partial({
-  abi: t.string,
-
-  cardNumber: t.string,
-
-  cardPartialNumber: t.string,
-
-  expiringDate: t.string,
-
-  hpan: t.string,
-
-  productType: enumType<ProductTypeEnum>(ProductTypeEnum, "productType"),
-
-  tokens: t.readonlyArray(t.string, "array of string"),
-
-  validityState: enumType<ValidityStateEnum>(ValidityStateEnum, "validityState")
-});
-
-export const PatchedCard = t.intersection([CardR, CardO], "Card");
-
-export type PatchedCard = t.TypeOf<typeof PatchedCard>;
-export const PatchedCards = t.partial({
-  data: t.readonlyArray(PatchedCard, "array of Card")
-});
-
-export type PatchedCards = t.TypeOf<typeof PatchedCards>;
