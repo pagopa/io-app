60f30ceec9441a26727318ee2f3e7d13530f0763
CrisTofani <cri.tofani@gmail.com>
2020-10-20 14:15:23 +0200

    Fixes test mock configuration

--- jestSetup.js
+++ jestSetup.js
@@ -18,6 +18,13 @@ jest.mock("@react-native-community/push-notification-ios", jest.fn());
 jest.mock("react-native-permissions", jest.fn());
 jest.mock("@react-native-community/cookies", jest.fn());
 
+/**
+ * adds as for documentation suggestion
+ * https://docs.swmansion.com/react-native-reanimated/docs/1.x.x/getting_started/#testing
+ */
+jest.mock("react-native-reanimated", () =>
+  require("react-native-reanimated/mock")
+);
 NativeModules.PlatformConstants = NativeModules.PlatformConstants || {
   forceTouchAvailable: false
 };
