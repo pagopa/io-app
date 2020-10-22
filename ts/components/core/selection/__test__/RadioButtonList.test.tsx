7e0a103ef4ec388b970a9e8a3db32ff074ae32f8
Simone <simone.biffi.91@gmail.com>
2020-10-22 16:23:46 +0200

    [#175060536] wip add snapshot testing to RadioButtonList

--- ts/components/core/selection/__test__/RadioButtonList.test.tsx
+++ ts/components/core/selection/__test__/RadioButtonList.test.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import TestRenderer from "react-test-renderer";
 
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import { RadioButtonList } from "../RadioButtonList";
 
 const items = [
   {
     label: "foo",
+    id: 0
+  },
+  {
+    label: "bar",
     id: 1
   }
 ];
-const onPress = selected => {};
-const radioButtonListRendered = TestRenderer.create(
-  <RadioButtonList items={items} />
-);
+const onPressHandler = () => 1;
+
+describe("Test RadioButtonList componet", () => {
+  it("Expected to be rendered", () => {
+    render(<RadioButtonList items={items} onPress={onPressHandler} />);
+
+    screen.
+  });
+});
