ba08a9755349e907c321fabfb20f6214fa3a5a90
Fabrizio <fartux@gmail.com>
2020-10-27 18:16:23 +0100

    [#175349901] periods action & reducers

--- ts/features/bonus/bpd/store/actions/index.ts
+++ ts/features/bonus/bpd/store/actions/index.ts
@@ -2,9 +2,11 @@ import { BpdDetailsActions } from "./details";
 import { BpdIbanActions } from "./iban";
 import { BpdOnboardingActions } from "./onboarding";
 import { BpdPaymentMethod } from "./paymentMethods";
+import { BpdPeriodsAction } from "./periods";
 
 export type BpdActions =
   | BpdOnboardingActions
   | BpdDetailsActions
   | BpdIbanActions
-  | BpdPaymentMethod;
+  | BpdPaymentMethod
+  | BpdPeriodsAction;
--- ts/features/bonus/bpd/store/reducers/details/index.ts
+++ ts/features/bonus/bpd/store/reducers/details/index.ts
@@ -1,20 +1,26 @@
+import * as pot from "italia-ts-commons/lib/pot";
 import { Action, combineReducers } from "redux";
 import { IndexedById } from "../../../../../../store/helpers/indexer";
+import { RemoteValue } from "../../../model/RemoteValue";
+import { BpdPeriod } from "../../actions/periods";
 import bpdActivationReducer, { BpdActivation } from "./activation";
 import {
   bpdPaymentMethodsReducer,
   BpdPotPaymentMethodActivation
 } from "./paymentMethods";
+import { bpdPeriodsReducer } from "./periods";
 
 export type BpdDetailsState = {
   activation: BpdActivation;
   paymentMethods: IndexedById<BpdPotPaymentMethodActivation>;
+  periods: pot.Pot<IndexedById<BpdPeriod>, Error>;
   // IBAN, value, points, other info...
 };
 
 const bpdDetailsReducer = combineReducers<BpdDetailsState, Action>({
   activation: bpdActivationReducer,
-  paymentMethods: bpdPaymentMethodsReducer
+  paymentMethods: bpdPaymentMethodsReducer,
+  periods: bpdPeriodsReducer
 });
 
 export default bpdDetailsReducer;
