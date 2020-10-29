873ee37c2f351d2f02a421b2c37520e5b585b7c5
Matteo Boschi <mbmaken@gmail.com>
2020-10-29 11:00:26 +0100

    [#175263873] handle periods response

--- ts/features/bonus/bpd/saga/networking/periods.ts
+++ ts/features/bonus/bpd/saga/networking/periods.ts
@@ -1,12 +1,41 @@
-import { call } from "redux-saga/effects";
+import { call, put } from "redux-saga/effects";
+import { fromNullable } from "fp-ts/lib/Option";
 import { BackendBpdClient } from "../../api/backendBpdClient";
 import { SagaCallReturnType } from "../../../../../types/utils";
-import { RTron } from "../../../../../boot/configureStoreAndPersistor";
+import {
+  AwardPeriodId,
+  BpdPeriod,
+  bpdPeriodsLoad,
+  BpdPeriodStatus
+} from "../../store/actions/periods";
+import { AwardPeriodResource } from "../../../../../../definitions/bpd/award_periods/AwardPeriodResource";
+import { getError } from "../../../../../utils/errors";
+import { readableReport } from "italia-ts-commons/lib/reporters";
+
+const periodStatusMap: Map<string, BpdPeriodStatus> = new Map<
+  string,
+  BpdPeriodStatus
+>([
+  ["ACTIVE", "Active"],
+  ["INACTIVE", "Inactive"],
+  ["CLOSED", "Closed"]
+]);
+
+const convertPeriod = (
+  networkPeriod: AwardPeriodResource,
+  statusFallback: BpdPeriodStatus = "Inactive"
+): BpdPeriod => ({
+  ...networkPeriod,
+  awardPeriodId: networkPeriod.awardPeriodId.toString() as AwardPeriodId,
+  superCashbackAmount: 0, // this field isn't yet present in AwardPeriodResource
+  status: fromNullable(periodStatusMap.get(networkPeriod.status)).getOrElse(
+    statusFallback
+  )
+});
 
 /**
  * Networking logic to request the periods list
- * TODO: replace with real code
- * @param _
+ * @param awardPeriods
  */
 export function* bpdLoadPeriodsSaga(
   awardPeriods: ReturnType<typeof BackendBpdClient>["awardPeriods"]
@@ -16,6 +45,22 @@ export function* bpdLoadPeriodsSaga(
       awardPeriods,
       {} as any
     );
-    RTron.log(awardPeriodsResult);
-  } catch (e) {}
+    if (awardPeriodsResult.isRight()) {
+      if (awardPeriodsResult.value.status === 200) {
+        const periods = awardPeriodsResult.value.value;
+        // convert data into app domain model
+        yield put(
+          bpdPeriodsLoad.success(
+            periods.map<BpdPeriod>(p => convertPeriod(p))
+          )
+        );
+      } else {
+        throw new Error(`response status ${awardPeriodsResult.value.status}`);
+      }
+    } else {
+      throw new Error(readableReport(awardPeriodsResult.value));
+    }
+  } catch (e) {
+    yield put(bpdPeriodsLoad.failure(getError(e)));
+  }
 }
