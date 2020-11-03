2ca138e38c5e1e85a9ab37d25a823dc9e021955d
Fabrizio <fartux@gmail.com>
2020-11-03 19:01:22 +0100

    [#175266102] prefetch activation status e abi list

--- ts/features/bonus/bpd/saga/networking/prefetctBpdDetails.ts
+++ ts/features/bonus/bpd/saga/networking/prefetctBpdDetails.ts
@@ -1,17 +1,23 @@
 import { put, take, all } from "redux-saga/effects";
 import { ActionType, getType } from "typesafe-actions";
+import { loadAbi } from "../../../../wallet/onboarding/bancomat/store/actions";
 import { bpdAmountLoad } from "../../store/actions/amount";
+import { bpdLoadActivationStatus } from "../../store/actions/details";
 import { bpdPeriodsLoad } from "../../store/actions/periods";
 import { bpdTransactionsLoad } from "../../store/actions/transactions";
 
 /**
  * Prefetch all the BPD details data:
+ * - Activation Status
+ * - Abi list
  * - Periods
  * - Amount foreach periods
  * - Transactions foreach period
  */
 export function* prefetchBpdData() {
+  yield put(bpdLoadActivationStatus.request());
   yield put(bpdPeriodsLoad.request());
+  yield put(loadAbi.request());
 
   const result: ActionType<
     typeof bpdPeriodsLoad.success | typeof bpdPeriodsLoad.failure
