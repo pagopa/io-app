fdefcfa46bc08073d0d48e57288d497af36951ed
Fabrizio <fartux@gmail.com>
2020-11-05 19:23:37 +0100

    [#175268877] wip

--- ts/features/bonus/bpd/components/transactionItem/BpdTransactionItem.tsx
+++ ts/features/bonus/bpd/components/transactionItem/BpdTransactionItem.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
+import { ImageSourcePropType } from "react-native";
 import { format } from "../../../../../utils/dates";
 import { formatNumberAmount } from "../../../../../utils/stringBuilder";
 import { BpdTransaction } from "../../store/actions/transactions";
 import { BaseBpdTransactionItem } from "./BaseBpdTransactionItem";
+
+type EnhancedBpdTransaction = {
+  image: ImageSourcePropType;
+  title: string;
+} & BpdTransaction;
 
 type Props = {
   transaction: any;
