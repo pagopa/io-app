9a08690f8f7a22a98975bdda6a300392d9e3dd7f
Fabrizio <fartux@gmail.com>
2020-11-09 09:51:14 +0100

    [#175636284] test for bpdsummary

---
+++ ts/features/bonus/bpd/screens/details/components/summary/__test__/bpdSummaryComponent.test.tsx
@@ -0,0 +1,173 @@
+import "@testing-library/jest-native/extend-expect";
+import { render, RenderAPI } from "@testing-library/react-native";
+import * as pot from "italia-ts-commons/lib/pot";
+import MockDate from "mockdate";
+import * as React from "react";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+import { dateToAccessibilityReadableFormat } from "../../../../../../../../utils/accessibility";
+import { BpdAmount } from "../../../../../store/actions/amount";
+import { BpdPeriod } from "../../../../../store/actions/periods";
+import {
+  eligibleAmount,
+  notEligibleAmount,
+  zeroAmount
+} from "../../../../../store/reducers/__mock__/amount";
+import {
+  activePeriod,
+  closedPeriod,
+  inactivePeriod
+} from "../../../../../store/reducers/__mock__/periods";
+import BpdSummaryComponent from "../BpdSummaryComponent";
+
+describe("Bpd Summary Component graphical test for different states", () => {
+  const mockStore = configureMockStore();
+  MockDate.set("2020-11-04");
+
+  it("Render Inactive period", () => {
+    const store = mockStore(mockBpdState(inactivePeriod, zeroAmount));
+    const component = render(
+      <Provider store={store}>
+        <BpdSummaryComponent />
+      </Provider>
+    );
+
+    expect(component).not.toBeNull();
+
+    // When the period is "Inactive", the TransactionsGraphicalSummary should be null
+    expect(component.queryByTestId("progressBar")).toBeNull();
+    expect(
+      component.queryByTestId("textualTransaction.transactions")
+    ).toBeNull();
+    // When the period is "Inactive", the TextualSummary should be in "inactive" mode
+    const textualSummary = component.queryByTestId("inactivePeriod");
+    expect(textualSummary).toBeEnabled();
+    // The TextualSummary text should include the start date of the inactive period
+    expect(textualSummary).toHaveTextContent(
+      dateToAccessibilityReadableFormat(inactivePeriod.startDate)
+    );
+  });
+
+  it("Render Active period, transactionNumber<minTransactionNumber, totalCashback = 0", () => {
+    const store = mockStore(mockBpdState(activePeriod, zeroAmount));
+    const component = render(
+      <Provider store={store}>
+        <BpdSummaryComponent />
+      </Provider>
+    );
+
+    activePeriodNotEnoughTransaction(component, zeroAmount);
+
+    // When the period is "Active" and transactionNumber<minTransactionNumber,
+    // TextualSummary should be null if totalCashback == 0
+    expect(component.queryByTestId("currentPeriodWarning")).toBeNull();
+  });
+
+  it("Render Active period, transactionNumber < minTransactionNumber, totalCashback > 0", () => {
+    const store = mockStore(mockBpdState(activePeriod, notEligibleAmount));
+    const component = render(
+      <Provider store={store}>
+        <BpdSummaryComponent />
+      </Provider>
+    );
+
+    activePeriodNotEnoughTransaction(component, notEligibleAmount);
+
+    // When the period is "Active" and transactionNumber<minTransactionNumber,
+    // TextualSummary should be null if totalCashback > 0
+    const textualSummary = component.queryByTestId("currentPeriodWarning");
+    expect(textualSummary).toBeEnabled();
+    // The text description should indicate the minimum transaction required to acquire the cashback
+    expect(textualSummary).toHaveTextContent(
+      activePeriod.minTransactionNumber.toString()
+    );
+  });
+
+  it("Render Active period, transactionNumber >= minTransactionNumber", () => {
+    const store = mockStore(mockBpdState(activePeriod, eligibleAmount));
+    const component = render(
+      <Provider store={store}>
+        <BpdSummaryComponent />
+      </Provider>
+    );
+
+    // When the period is "Active" and transactionNumber >= minTransactionNumber,
+    // the TransactionsGraphicalSummary should be null
+    expect(component.queryByTestId("progressBar")).toBeNull();
+    // TextualTransaction should be enabled
+    const textualTransactionTransactions = component.queryByTestId(
+      "textualTransaction.transactions"
+    );
+    expect(textualTransactionTransactions).toBeEnabled();
+
+    expect(textualTransactionTransactions).toHaveTextContent(
+      eligibleAmount.transactionNumber.toString()
+    );
+
+    // When the period is "Active" and transactionNumber>=minTransactionNumber,
+    // no TextualSummary should be rendered.
+    expect(component.queryByTestId("currentPeriodWarning")).toBeNull();
+  });
+
+  it("Render Closed period, grace period", () => {
+    MockDate.set("2020-11-04");
+    const store = mockStore(mockBpdState(closedPeriod, zeroAmount));
+    const component = render(
+      <Provider store={store}>
+        <BpdSummaryComponent />
+      </Provider>
+    );
+
+    // When the period is "Active" and transactionNumber>=minTransactionNumber,
+    // no TextualSummary should be rendered.
+    const textualSummary = component.queryByTestId("gracePeriod");
+    expect(textualSummary).toBeEnabled();
+    expect(textualSummary).toHaveTextContent(
+      dateToAccessibilityReadableFormat(closedPeriod.endDate)
+    );
+    MockDate.reset();
+  });
+
+  MockDate.reset();
+  MockDate.set("2020-11-09");
+
+  it("Render Closed period, not enough transactions", () => {});
+
+  it("Render Closed period, cashback earned", () => {});
+
+  it("Render Closed period, max cashback earned", () => {});
+
+  MockDate.reset();
+});
+
+/**
+ * Generate a mocked state with the fields required for these tests
+ * @param period
+ * @param amount
+ */
+const mockBpdState = (period: BpdPeriod, amount: BpdAmount) => ({
+  bonus: {
+    bpd: {
+      details: {
+        selectedPeriod: period,
+        amounts: { [period.awardPeriodId]: pot.some(amount) }
+      }
+    }
+  },
+  profile: pot.none
+});
+
+const activePeriodNotEnoughTransaction = (
+  component: RenderAPI,
+  amount: BpdAmount
+) => {
+  expect(component).not.toBeNull();
+
+  // When the period is "Active" and transactionNumber<minTransactionNumber,
+  // GraphicalSummary should be enabled
+  expect(component.queryByTestId("progressBar")).toBeEnabled();
+  expect(component.queryByTestId("percentageTransactions.title")).toBeEnabled();
+  expect(
+    component.queryByTestId("percentageTransactions.transactions")
+  ).toHaveTextContent(amount.transactionNumber.toString());
+};
---
+++ ts/features/bonus/bpd/store/reducers/__mock__/amount.ts
@@ -0,0 +1,20 @@
+import { BpdAmount } from "../../actions/amount";
+import { AwardPeriodId } from "../../actions/periods";
+
+export const zeroAmount: BpdAmount = {
+  awardPeriodId: 2 as AwardPeriodId,
+  totalCashback: 0,
+  transactionNumber: 0
+};
+
+export const notEligibleAmount: BpdAmount = {
+  awardPeriodId: 2 as AwardPeriodId,
+  totalCashback: 10.25,
+  transactionNumber: 3
+};
+
+export const eligibleAmount: BpdAmount = {
+  awardPeriodId: 2 as AwardPeriodId,
+  totalCashback: 83.52,
+  transactionNumber: 50
+};
