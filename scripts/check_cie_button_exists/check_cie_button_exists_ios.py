d665b9f9451c2007c7d8cfd9b44583ad6f5d1398
Giovanni Mancini <g.mancini.0gmail.com>
2020-11-12 16:53:05 +0100

    [#175420010] Refactored to improve testability. Added unit tests

--- scripts/check_cie_button_exists/check_cie_button_exists_ios.py
+++ scripts/check_cie_button_exists/check_cie_button_exists_ios.py
@@ -1,72 +1,99 @@
 import requests
 import re
 import os
+import sys
+
+cieHeaders = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+    'Accept-Encoding': 'identity, deflate, compress, gzip, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1'
+}
+
+# As requests doesn't support JS, redirects stop, so a POST with the following payload is required
+ciePayload = {
+    "shib_idp_ls_exception.shib_idp_session_ss": "",
+    "shib_idp_ls_success.shib_idp_session_ss": "false",
+    "shib_idp_ls_value.shib_idp_session_ss": "",
+    "shib_idp_ls_exception.shib_idp_persistent_ss":	"",
+    "shib_idp_ls_success.shib_idp_persistent_ss":	"false",
+    "shib_idp_ls_value.shib_idp_persistent_ss": "",
+    "shib_idp_ls_supported": "",
+    "_eventId_proceed": ""
+}
 
 
-class CieButton:
-    def __init__(self, uri="https://app-backend.io.italia.it/login?entityID=xx_servizicie&authLevel=SpidL2"):
-        # Starting uri. For ease of maintenace point to our API
-        self._uri = uri
-        self._nAttempts = 0
-        # Server response
-        self._response = None
-        # As requests doesn't support JS, redirects stop. Then a post with the following payload is required
-        self._payload = {
-            "shib_idp_ls_exception.shib_idp_session_ss": "",
-            "shib_idp_ls_success.shib_idp_session_ss": "false",
-            "shib_idp_ls_value.shib_idp_session_ss": "",
-            "shib_idp_ls_exception.shib_idp_persistent_ss":	"",
-            "shib_idp_ls_success.shib_idp_persistent_ss":	"false",
-            "shib_idp_ls_value.shib_idp_persistent_ss": "",
-            "shib_idp_ls_supported": "",
-            "_eventId_proceed": ""
-        }
+def requestCieAuthPage(
+        uri,
+        headers,
+        payload,
+        maxAttempts):
 
-        # Headers for requests
-        self._headers = {
-            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-            'Accept-Encoding': 'identity, deflate, compress, gzip, br',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Language': 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1'
-        }
+    statusFlag = False
+    nAttempts = 0
+    response = None
 
-    def requestAuthPage(self, maxAttempts=5):
-        statusFlag = False
-        while not (self._nAttempts >= 5 or statusFlag):
-            self._response = requests.get(
-                self._uri, headers=self._headers, allow_redirects=True)
-            self._response = requests.post(
-                self._response.url, headers=self._headers, data=self._payload, allow_redirects=True)
-            self._nAttempts += 1
-            statusFlag = (self._response.status_code == 200)
+    # Do the required attempts
+    while not (nAttempts >= maxAttempts or statusFlag):
+        # do the requests
+        response = requests.get(
+            uri, headers=headers, allow_redirects=True)
+        response = requests.post(
+            response.url, headers=headers, data=payload, allow_redirects=True)
+        # set conditions for next step
+        nAttempts += 1
+        statusFlag = (response.status_code == 200)
 
-    def checkExists(self):
-        # Check if the fetched page has the required JS function
-        match = re.search("apriIosUL", self._response.text)
-        return match is not None
+    return response
 
-    def postSlack(self):
-        self._resetHeaders()
-        slackToken = os.environ.get("IO_APP_SLACK_TOKEN_CHECK_CIE_BTN", None)
-        self._headers["Content-type"] = "application/json"
-        self._headers["Authorization"] = "Bearer " + slackToken
 
-        self._payload = {
-            "text": "Funziona", "channel": '#io_status'}
-        print(self._headers)
-        self._response = requests.post("https://slack.com/api/chat.postMessage",
-                                       headers=self._headers, data=self._payload, allow_redirects=True)
-        print(self._response.status_code)
+def postSlack(
+        token,
+        message="Page containig CIE button unreachable",
+        uri="https://slack.com/api/chat.postMessage",
+        channel='#io_status'):
 
-    def _resetHeaders(self):
-        self._headers = {}
+    slackHeaders = {
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + token
+    }
+
+    slackPayload = {
+        "text": message,
+        "channel": channel
+    }
+
+    return requests.post(
+        uri, headers=slackHeaders, json=slackPayload, allow_redirects=True)
+
+
+def main(uri="https://app-backend.io.italia.it/login?entityID=xx_servizicie&authLevel=SpidL2",
+         headers=cieHeaders,
+         payload=ciePayload,
+         maxAttempts=5,
+         pattern="apriIosUL"):
+    cieAuthPageResponse = None
+    try:
+        cieAuthPageResponse = requestCieAuthPage(
+            uri, headers, payload, maxAttempts)
+        cieAuthPageResponse.raise_for_status()
+    except requests.exceptions.RequestException:
+        print("[Fatal Error] Page containing CIE button unrechable", file=sys.stderr)
+        postSlack(os.environ.get("IO_APP_SLACK_TOKEN_CHECK_CIE_BTN", None))
+        sys.exit(os.EX_UNAVAILABLE)
+
+    if (not re.search(pattern, cieAuthPageResponse.text)):
+        print("[Fatal Error] Can't find CIE button JS script", file=sys.stderr)
+        postSlack(os.environ.get("IO_APP_SLACK_TOKEN_CHECK_CIE_BTN",
+                                 None), "Can't find CIE button JS script")
+        sys.exit(os.EX_UNAVAILABLE)
 
 
 if __name__ == "__main__":
-    myCieButton = CieButton()
-    myCieButton.requestAuthPage(3)
-    if (myCieButton.checkExists()):
-        print("OK")
-    myCieButton.postSlack()
+    main(uri="https://app-backend.io.italia.it/login?entityID=xx_servizicie&authLevel=SpidL2",
+         headers=cieHeaders,
+         payload=ciePayload,
+         maxAttempts=5,
+         pattern="apriIosUL")
