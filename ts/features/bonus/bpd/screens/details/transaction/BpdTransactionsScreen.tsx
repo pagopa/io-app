88e87bb248814c9169768ae417df6cb7ffecea5e
CrisTofani <cri.tofani@gmail.com>
2020-11-12 15:00:41 +0100

    [#175269135] fixes logic on missing transactions for update date

--- ts/features/bonus/bpd/screens/details/transaction/BpdTransactionsScreen.tsx
+++ ts/features/bonus/bpd/screens/details/transaction/BpdTransactionsScreen.tsx
@@ -40,10 +40,7 @@ const BpdTransactionsScreen: React.FunctionComponent<Props> = props => {
     compareDesc(trx1.trxDate, trx2.trxDate)
   );
 
-  const lastUpdateDate = format(
-    index(0, trxSortByDate).fold(new Date(), t => t.trxDate),
-    "DD MMMM YYYY"
-  );
+  const maybeLastUpdateDate = index(0, trxSortByDate).map(t => t.trxDate);
 
   return (
     <BaseScreenComponent goBack={true} headerTitle={I18n.t("bonus.bpd.title")}>
@@ -52,13 +49,18 @@ const BpdTransactionsScreen: React.FunctionComponent<Props> = props => {
           <View spacer={true} large={true} />
           <H1>{I18n.t("bonus.bpd.details.transaction.title")}</H1>
           <View spacer={true} />
-          {pot.isSome(props.selectedAmount) && props.selectedPeriod && (
-            <BPDTransactionSummaryComponent
-              lastUpdateDate={lastUpdateDate}
-              period={props.selectedPeriod}
-              totalAmount={props.selectedAmount.value}
-            />
-          )}
+          {pot.isSome(props.selectedAmount) &&
+            props.selectedPeriod &&
+            maybeLastUpdateDate.isSome() && (
+              <BPDTransactionSummaryComponent
+                lastUpdateDate={format(
+                  maybeLastUpdateDate.value,
+                  "DD MMMM YYYY"
+                )}
+                period={props.selectedPeriod}
+                totalAmount={props.selectedAmount.value}
+              />
+            )}
           <FlatList
             data={transactions}
             renderItem={transaction => (
