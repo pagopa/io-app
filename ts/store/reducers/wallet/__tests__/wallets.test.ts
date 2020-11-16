e8f30aaf2b1eec89fa5cf58ac1df57b46b94abe6
Matteo Boschi <mbmaken@gmail.com>
2020-11-16 16:08:07 +0100

    [#175553856] enhance tests

--- ts/store/reducers/wallet/__tests__/wallets.test.ts
+++ ts/store/reducers/wallet/__tests__/wallets.test.ts
@@ -41,14 +41,24 @@ describe("walletV2 selectors", () => {
     expect(pot.isSome(maybeCC)).toBeTruthy();
     if (pot.isSome(maybeCC)) {
       expect(maybeCC.value.length).toEqual(1);
+      expect(maybeCC.value[0].v2!.info.hashPan).toEqual(
+        "853afb770973eb48d5d275778bd124b28f60a684c20bcdf05dc8f0014c7ce871"
+      );
     }
   });
 
   it("should return bancomat", () => {
     const maybeBancomat = bancomatSelector(globalState);
     expect(pot.isSome(maybeBancomat)).toBeTruthy();
+    const hpans = [
+      "a591ab131bd9492e6df0357f1ac52785a96ddc8e772baddbb02e2169af9474f4",
+      "e105a87731025d54181d8e4c4c04ff344ce82e57d6a3d6c6911e8eadb0348d7b"
+    ];
     if (pot.isSome(maybeBancomat)) {
       expect(maybeBancomat.value.length).toEqual(2);
+      maybeBancomat.value.forEach(w => {
+        expect(hpans.find(h => h === w.info.hashPan)).toBeDefined();
+      });
     }
   });
 
@@ -57,6 +67,9 @@ describe("walletV2 selectors", () => {
     expect(pot.isSome(maybePagoPaCC)).toBeTruthy();
     if (pot.isSome(maybePagoPaCC)) {
       expect(maybePagoPaCC.value.length).toEqual(1);
+      expect(maybePagoPaCC.value[0].v2!.info.hashPan).toEqual(
+        "853afb770973eb48d5d275778bd124b28f60a684c20bcdf05dc8f0014c7ce871"
+      );
     }
   });
 
