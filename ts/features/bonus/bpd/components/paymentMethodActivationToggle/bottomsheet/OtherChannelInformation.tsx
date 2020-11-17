94f3ea2f7a70d4de071549f22f5b07d30d0b3099
Matteo Boschi <mbmaken@gmail.com>
2020-11-17 11:07:56 +0100

    [#175271334] use blockButton instead of button refactoring

--- ts/features/bonus/bpd/components/paymentMethodActivationToggle/bottomsheet/OtherChannelInformation.tsx
+++ ts/features/bonus/bpd/components/paymentMethodActivationToggle/bottomsheet/OtherChannelInformation.tsx
@@ -1,16 +1,14 @@
-import { useBottomSheetModal, TouchableOpacity } from "@gorhom/bottom-sheet";
+import { useBottomSheetModal } from "@gorhom/bottom-sheet";
 import { none } from "fp-ts/lib/Option";
-import { Button, View } from "native-base";
+import { View } from "native-base";
 import * as React from "react";
 import { useContext } from "react";
-import { StyleSheet } from "react-native";
 import { NavigationContext } from "react-navigation";
-import { Label } from "../../../../../../components/core/typography/Label";
 import Markdown from "../../../../../../components/ui/Markdown";
 import I18n from "../../../../../../i18n";
 import { navigateToWalletAddPaymentMethod } from "../../../../../../store/actions/navigation";
 import { bottomSheetContent } from "../../../../../../utils/bottomSheet";
-import { isIos } from "../../../../../../utils/platform";
+import BlockButtons from "../../../../../../components/ui/BlockButtons";
 
 // NotActivable: already activated by someone else
 // NotCompatible: missing bpd capability
@@ -18,30 +16,20 @@ export type NotActivableType = "NotActivable" | "NotCompatible";
 
 type Props = { onAddPayment: () => void };
 
-const styles = StyleSheet.create({
-  button: {
-    width: "100%"
-  }
-});
-
-// A very quick fix for android
-const addPaymentMethodButton = (onPress: () => void) => {
-  const button = (
-    <Button style={styles.button} onPress={isIos ? onPress : undefined}>
-      <Label color={"white"}>
-        {I18n.t(
-          "bonus.bpd.details.paymentMethods.activateOnOthersChannel.addWallet"
-        )}
-      </Label>
-    </Button>
-  );
-
-  return !isIos ? (
-    <TouchableOpacity onPress={onPress}>{button}</TouchableOpacity>
-  ) : (
-    button
-  );
-};
+const addPaymentMethodButton = (onPress: () => void) => (
+  <BlockButtons
+    type={"SingleButton"}
+    leftButton={{
+      onPressWithGestureHandler: true,
+      block: true,
+      primary: true,
+      onPress,
+      title: I18n.t(
+        "bonus.bpd.details.paymentMethods.activateOnOthersChannel.addWallet"
+      )
+    }}
+  />
+);
 
 export const OtherChannelInformation: React.FunctionComponent<Props> = props => (
   <View>
