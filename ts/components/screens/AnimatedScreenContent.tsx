a0c48c004730feb265539e23a6e84c6998cc88f0
CrisTofani <cri.tofani@gmail.com>
2020-11-18 14:42:51 +0100

    [#175741977] reduces offset for IOS

--- ts/components/screens/AnimatedScreenContent.tsx
+++ ts/components/screens/AnimatedScreenContent.tsx
@@ -88,8 +88,8 @@ export default class AnimatedScreenContent extends React.Component<
   private headerHeight: number =
     Platform.OS === "ios"
       ? isIphoneX()
-        ? customVariables.appHeaderHeight + 65
-        : customVariables.appHeaderHeight + 37
+        ? customVariables.appHeaderHeight + 62
+        : customVariables.appHeaderHeight + 35
       : customVariables.appHeaderHeight;
 
   public render(): React.ReactNode {
