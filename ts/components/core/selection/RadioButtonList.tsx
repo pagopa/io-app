e740b3c00b3978df0fa6251b18679dd6b101b8a3
Matteo Boschi <mbmaken@gmail.com>
2020-11-20 17:31:13 +0100

    [#175060536] refactoring

--- ts/components/core/selection/RadioButtonList.tsx
+++ ts/components/core/selection/RadioButtonList.tsx
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import * as React from "react";
 import { H4 } from "../typography/H4";
+import { IOStyles } from "../variables/IOStyles";
 import IconFont from "./../../ui/IconFont";
 import themeVariables from "./../../../theme/variables";
 
@@ -16,58 +17,56 @@ type Props<T> = {
   onPress: (selected: T) => void;
 };
 
+const styles = StyleSheet.create({
+  head: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  item: {
+    flexDirection: "row",
+    flex: 1,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  itemsContainer: {
+    flex: 1,
+    flexDirection: "column"
+  },
+  icon: {
+    paddingBottom: 15,
+    paddingRight: 20
+  }
+});
+
 /**
  * A list of radio button with an optional heading.
  * The management of the selection is demanded and drived by the `selectedItem` prop.
  * The item with the `id` equal to the `selectedItem` is the active one.
  */
-export const RadioButtonList = <T,>(props: Props<T>) => {
-  const styles = {
-    head: {
-      paddingTop: 10,
-      paddingBottom: 10
-    },
-    item: {
-      flexDirection: "row" as const,
-      flex: 1,
-      paddingTop: 10,
-      paddingBottom: 10
-    },
-    itemsContainer: {
-      flex: 1,
-      flexDirection: "column" as const
-    },
-    icon: {
-      paddingBottom: 15,
-      paddingRight: 20
-    }
-  };
-
-  return (
-    <View>
-      {props.head && (
-        <H4 color={"bluegreyDark"} weight={"Regular"} style={styles.head}>
-          {props.head}
-        </H4>
-      )}
-      <View style={styles.itemsContainer}>
-        {props.items.map((item, key) => (
-          <View key={`radio_item_${key}`} style={styles.item}>
-            <IconFont
-              name={
-                props.selectedItem === item.id ? "io-radio-on" : "io-radio-off"
-              }
-              size={24}
-              color={themeVariables.contentPrimaryBackground}
-              onPress={() => props.onPress(item.id)}
-              style={styles.icon}
-            />
-            <H4 color={"bluegreyDark"} weight={"Regular"} style={{ flex: 1 }}>
-              {item.label}
-            </H4>
-          </View>
-        ))}
-      </View>
+export const RadioButtonList = <T,>(props: Props<T>) => (
+  <View>
+    {props.head && (
+      <H4 color={"bluegreyDark"} weight={"Regular"} style={styles.head}>
+        {props.head}
+      </H4>
+    )}
+    <View style={styles.itemsContainer}>
+      {props.items.map((item, key) => (
+        <View key={`radio_item_${key}`} style={styles.item}>
+          <IconFont
+            name={
+              props.selectedItem === item.id ? "io-radio-on" : "io-radio-off"
+            }
+            size={24}
+            color={themeVariables.contentPrimaryBackground}
+            onPress={() => props.onPress(item.id)}
+            style={styles.icon}
+          />
+          <H4 color={"bluegreyDark"} weight={"Regular"} style={IOStyles.flex}>
+            {item.label}
+          </H4>
+        </View>
+      ))}
     </View>
-  );
-};
+  </View>
+);
