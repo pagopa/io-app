c5a64bb33bd3f87bad0d8c000d33d78ee9d7acb1
Simone <simone.biffi.91@gmail.com>
2020-11-24 18:49:09 +0100

    [#174926807] refactor component, update text show chat list button only if there are chats

--- ts/components/InstabugAssistanceComponent.tsx
+++ ts/components/InstabugAssistanceComponent.tsx
@@ -1,9 +1,10 @@
-import { BugReporting } from "instabug-reactnative";
-import { H3, Text, View } from "native-base";
+import { BugReporting, Replies } from "instabug-reactnative";
+import { H3, View } from "native-base";
 import * as React from "react";
 import I18n from "../i18n";
 import AdviceComponent from "./AdviceComponent";
 import ButtonWithImage from "./ButtonWithImage";
+import Markdown from "./ui/Markdown";
 
 type Props = Readonly<{
   requestAssistance: (type: BugReporting.reportType) => void;
@@ -13,44 +14,55 @@ type Props = Readonly<{
  * A component to display the buttons to access a new chat with the assistance via Instabug.
  * The logic to manage the Instabug APIs is inerithed from the withInstabugHelper HOC.
  */
-class InstabugAssistanceComponent extends React.PureComponent<Props> {
-  public render() {
-    return (
-      <React.Fragment>
-        <H3>{I18n.t("instabug.contextualHelp.title1")}</H3>
-        <View spacer={true} />
-        <Text>{I18n.t("instabug.contextualHelp.description")}</Text>
-        <View spacer={true} />
-        <Text>{I18n.t("instabug.contextualHelp.descriptionChat")}</Text>
-        <View spacer={true} large={true} />
-        <AdviceComponent
-          text={I18n.t("instabug.contextualHelp.assistanceWorkHours")}
-        />
-        <View spacer={true} />
-        <ButtonWithImage
-          icon={"io-bug"}
-          onPress={() =>
-            this.props.requestAssistance(BugReporting.reportType.bug)
-          }
-          text={I18n.t("instabug.contextualHelp.buttonBug")}
-          disabled={false}
-          light={true}
-        />
-        <View spacer={true} />
-        <Text>{I18n.t("instabug.contextualHelp.descriptionBug")}</Text>
-        <View spacer={true} />
-        <ButtonWithImage
-          icon={"io-send-message"}
-          onPress={() =>
-            this.props.requestAssistance(BugReporting.reportType.question)
-          }
-          text={I18n.t("instabug.contextualHelp.buttonChat")}
-          disabled={false}
-          light={true}
-        />
-      </React.Fragment>
-    );
-  }
-}
+
+const InstabugAssistanceComponent: React.FunctionComponent<Props> = ({
+  requestAssistance
+}) => {
+  const [hasPreviousChats, setHasPreviousChats] = React.useState<boolean>(
+    false
+  );
+
+  React.useEffect(() => {
+    Replies.hasChats(hasChats => {
+      setHasPreviousChats(hasChats);
+    });
+  });
+
+  return (
+    <>
+      <H3>{I18n.t("instabug.contextualHelp.title1")}</H3>
+      <View spacer={true} />
+      <Markdown>{I18n.t("instabug.contextualHelp.descriptionBug")}</Markdown>
+      <View spacer={true} large={true} />
+      <AdviceComponent
+        text={I18n.t("instabug.contextualHelp.assistanceWorkHours")}
+      />
+      <View spacer={true} />
+      <ButtonWithImage
+        icon={"io-bug"}
+        onPress={() => requestAssistance(BugReporting.reportType.bug)}
+        text={I18n.t("instabug.contextualHelp.buttonBug")}
+        disabled={false}
+        light={true}
+      />
+      {hasPreviousChats && (
+        <>
+          <View spacer={true} />
+          <Markdown>
+            {I18n.t("instabug.contextualHelp.descriptionChat")}
+          </Markdown>
+          <View spacer={true} />
+          <ButtonWithImage
+            icon={"io-bug"}
+            onPress={() => requestAssistance(BugReporting.reportType.question)}
+            text={I18n.t("instabug.contextualHelp.buttonChat")}
+            disabled={false}
+            light={true}
+          />
+        </>
+      )}
+    </>
+  );
+};
 
 export default InstabugAssistanceComponent;
