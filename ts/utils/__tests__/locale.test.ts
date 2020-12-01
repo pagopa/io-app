8714ba0b369a9f5919e24e664fd1103d5633a96d
Matteo Boschi <mbmaken@gmail.com>
2020-12-01 18:45:21 +0100

    [#175931000] refactoring

--- ts/utils/__tests__/locale.test.ts
+++ ts/utils/__tests__/locale.test.ts
@@ -1,6 +1,6 @@
 import { some } from "fp-ts/lib/Option";
 import { Locales } from "../../../locales/locales";
-import { setLocale } from "../../i18n";
+import I18n, { setLocale } from "../../i18n";
 import {
   getLocalePrimary,
   getLocalePrimaryWithFallback,
@@ -43,32 +43,50 @@ describe("localeDateFormat on locale", () => {
   const date = new Date("May 17, 1995 03:24:00");
   const invalidDate = new Date("?");
 
-  [
-    ["%d/%m/%Y", "17/05/1995", "17/05/1995"],
-    ["%d %b %Y", "17 Mag 1995", "17 May 1995"],
-    ["%d %b %Y, %H:%M", "17 Mag 1995, 03:24", "17 May 1995, 03:24"],
-    ["%d %B %Y", "17 Maggio 1995", "17 May 1995"],
-    ["%B", "Maggio", "May"],
-    ["%B %Y", "Maggio 1995", "May 1995"],
-    ["%d %b, %H:%M", "17 Mag, 03:24", "17 May, 03:24"],
-    ["%d %B", "17 Maggio", "17 May"],
-    ["%m/%Y", "05/1995", "05/1995"]
-  ].forEach(elem => {
+  const data: ReadonlyArray<[() => string, string, string]> = [
+    [() => I18n.t("date.formats.default"), "17/05/1995", "17/05/1995"],
+    [
+      () => I18n.t("global.dateFormats.fullFormatShortMonthLiteral"),
+      "17 Mag 1995",
+      "17 May 1995"
+    ],
+    [
+      () => I18n.t("global.dateFormats.fullFormatShortMonthLiteralWithTime"),
+      "17 Mag 1995, 03:24",
+      "17 May 1995, 03:24"
+    ],
+    [
+      () => I18n.t("global.dateFormats.fullFormatFullMonthLiteral"),
+      "17 Maggio 1995",
+      "17 May 1995"
+    ],
+    [() => I18n.t("global.dateFormats.fullMonthLiteral"), "Maggio", "May"],
+    [
+      () => I18n.t("global.dateFormats.fullMonthLiteralWithYear"),
+      "Maggio 1995",
+      "May 1995"
+    ],
+    [
+      () => I18n.t("global.dateFormats.dayMonthWithTime"),
+      "17 Mag, 03:24",
+      "17 May, 03:24"
+    ],
+    [() => I18n.t("global.dateFormats.dayFullMonth"), "17 Maggio", "17 May"],
+    [() => I18n.t("global.dateFormats.numericMonthYear"), "05/1995", "05/1995"]
+  ];
+  data.forEach(elem => {
     const [dateFormat, expectedIT, expectedEN] = elem;
     it("convert given date to string format on IT locale", () => {
       setLocale("it" as Locales);
-      expect(localeDateFormat(date, dateFormat)).toStrictEqual(expectedIT);
+      expect(localeDateFormat(date, dateFormat())).toStrictEqual(expectedIT);
       setLocale("en" as Locales);
-      expect(localeDateFormat(date, dateFormat)).toStrictEqual(expectedEN);
+      expect(localeDateFormat(date, dateFormat())).toStrictEqual(expectedEN);
     });
   });
 
   it("invalid date converts in common date error string", () => {
-    setLocale("it" as Locales);
     expect(localeDateFormat(invalidDate, "%d/%m/%Y")).toStrictEqual(
-      "data non valida"
+      I18n.t("global.date.invalid")
     );
-    setLocale("en" as Locales);
-    expect(localeDateFormat(invalidDate, "%B")).toStrictEqual("invalid date");
   });
 });
