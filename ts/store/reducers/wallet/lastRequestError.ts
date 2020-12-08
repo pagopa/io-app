b1dde0efd3e71604d11522fca37fe318db060f2a
Matteo Boschi <mbmaken@gmail.com>
2020-12-08 20:48:46 +0100

    [#176058079] revert

--- ts/store/reducers/wallet/lastRequestError.ts
+++ ts/store/reducers/wallet/lastRequestError.ts
@@ -4,12 +4,10 @@ import { getType } from "typesafe-actions";
 import { Action } from "../../actions/types";
 import {
   fetchTransactionsFailure,
-  fetchTransactionsRequestWithExpBackoff,
   fetchTransactionsSuccess
 } from "../../actions/wallet/transactions";
 import {
   fetchWalletsFailure,
-  fetchWalletsRequestWithExpBackoff,
   fetchWalletsSuccess
 } from "../../actions/wallet/wallets";
 import { GlobalState } from "../types";
@@ -21,23 +19,12 @@ export type LastRequestErrorState = {
 
 const defaultState: LastRequestErrorState = { attempts: 0 };
 const backOffExpLimitAttempts = 4;
-const maxBackOff = Math.pow(2, backOffExpLimitAttempts) * 1000;
+const backOffBase = 1.6;
 const reducer = (
   state: LastRequestErrorState = defaultState,
   action: Action
 ): LastRequestErrorState => {
   switch (action.type) {
-    case getType(fetchTransactionsRequestWithExpBackoff):
-    case getType(fetchWalletsRequestWithExpBackoff): {
-      const elapsed = fromNullable(state.lastUpdate).fold(
-        0,
-        lu => new Date().getTime() - lu.getTime()
-      );
-      if (elapsed > maxBackOff) {
-        return defaultState;
-      }
-      return state;
-    }
     case getType(fetchTransactionsFailure):
     case getType(fetchWalletsFailure):
       return {
@@ -56,7 +43,8 @@ export const backOffWaitingTime = (state: GlobalState): Millisecond =>
   fromNullable(state.wallet.lastRequestError.lastUpdate).fold(
     0 as Millisecond,
     lu => {
-      const wait = Math.pow(2, state.wallet.lastRequestError.attempts) * 1000;
+      const wait =
+        Math.pow(backOffBase, state.wallet.lastRequestError.attempts) * 1000;
       return (new Date().getTime() - lu.getTime() < wait
         ? wait
         : 0) as Millisecond;
