148b4c056ed36210452a80b4bbfc2d9ec079c989
Matteo Boschi <mbmaken@gmail.com>
2020-12-15 11:46:58 +0100

    [#176129361] change label review footer button enabling login

--- ts/features/wallet/onboarding/common/screens/bpd/ActivateBpdOnNewPaymentMethodScreen.tsx
+++ ts/features/wallet/onboarding/common/screens/bpd/ActivateBpdOnNewPaymentMethodScreen.tsx
@@ -4,14 +4,19 @@ import { SafeAreaView, ScrollView } from "react-native";
 import { NavigationActions } from "react-navigation";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import * as pot from "italia-ts-commons/lib/pot";
+import { fromNullable } from "fp-ts/lib/Option";
 import { Body } from "../../../../../../components/core/typography/Body";
 import { H1 } from "../../../../../../components/core/typography/H1";
 import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
 import I18n from "../../../../../../i18n";
 import { PaymentMethod } from "../../../../../../types/pagopa";
-import { FooterTwoButtons } from "../../../../../bonus/bonusVacanze/components/markdown/FooterTwoButtons";
 import { PaymentMethodRawList } from "../../../../../bonus/bpd/components/paymentMethodActivationToggle/list/PaymentMethodRawList";
+import { GlobalState } from "../../../../../../store/reducers/types";
+import { bpdPaymentMethodActivationSelector } from "../../../../../bonus/bpd/store/reducers/details/paymentMethods";
+import { getPaymentMethodHash } from "../../../../../../utils/paymentMethod";
+import FooterWithButtons from "../../../../../../components/ui/FooterWithButtons";
 
 type OwnProps = {
   paymentMethods: ReadonlyArray<PaymentMethod>;
@@ -19,6 +24,7 @@ type OwnProps = {
 };
 
 export type Props = ReturnType<typeof mapDispatchToProps> &
+  ReturnType<typeof mapStateToProps> &
   OwnProps &
   Pick<React.ComponentProps<typeof BaseScreenComponent>, "contextualHelp">;
 
@@ -30,8 +36,53 @@ const loadLocales = () => ({
   continueStr: I18n.t("global.buttons.continue")
 });
 
+/**
+ * return a two button footer
+ * left button is enabled when no method are active
+ * right button button is enabled when at least one method is active
+ * @param props
+ */
+const getFooter = (props: Props) => {
+  const { continueStr, skip } = loadLocales();
+  const { paymentMethods, bpdPaymentMethodsActivation } = props;
+  const paymentMethodsHash = paymentMethods.map(getPaymentMethodHash);
+  const atLeastOneActive = paymentMethodsHash.some(pmh =>
+    fromNullable(pmh)
+      .mapNullable(h => bpdPaymentMethodsActivation[h])
+      .map(potActivation =>
+        pot.getOrElse(
+          pot.map(potActivation, p => p.activationStatus === "active"),
+          false
+        )
+      )
+      .getOrElse(false)
+  );
+  const notNowButtonProps = {
+    primary: false,
+    bordered: true,
+    disabled: atLeastOneActive,
+    onPress: props.skip,
+    title: skip
+  };
+  const continueButtonProps = {
+    block: true,
+    primary: true,
+    disabled: !atLeastOneActive,
+    onPress: props.skip,
+    title: continueStr
+  };
+  return (
+    <FooterWithButtons
+      type={"TwoButtonsInlineHalf"}
+      leftButton={notNowButtonProps}
+      rightButton={continueButtonProps}
+    />
+  );
+};
+
 const ActivateBpdOnNewPaymentMethodScreen: React.FunctionComponent<Props> = props => {
-  const { title, body1, body2, skip, continueStr } = loadLocales();
+  const { title, body1, body2 } = loadLocales();
+
   return (
     <BaseScreenComponent
       headerTitle={props.title}
@@ -50,14 +101,7 @@ const ActivateBpdOnNewPaymentMethodScreen: React.FunctionComponent<Props> = prop
             <Body>{body2}</Body>
           </View>
         </ScrollView>
-
-        <FooterTwoButtons
-          type={"TwoButtonsInlineHalf"}
-          onCancel={props.skip}
-          onRight={props.skip}
-          rightText={continueStr}
-          leftText={skip}
-        />
+        {getFooter(props)}
       </SafeAreaView>
     </BaseScreenComponent>
   );
@@ -67,7 +111,11 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
   skip: () => dispatch(NavigationActions.back())
 });
 
+const mapStateToProps = (state: GlobalState) => ({
+  bpdPaymentMethodsActivation: bpdPaymentMethodActivationSelector(state)
+});
+
 export default connect(
-  undefined,
+  mapStateToProps,
   mapDispatchToProps
 )(ActivateBpdOnNewPaymentMethodScreen);
