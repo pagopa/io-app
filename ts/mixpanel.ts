5630858cef83caf2c1b429e3ab034377440d3eb0
Matteo Boschi <mbmaken@gmail.com>
2020-12-15 18:11:52 +0100

    fixes

--- ts/mixpanel.ts
+++ ts/mixpanel.ts
@@ -1,5 +1,6 @@
 import DeviceInfo from "react-native-device-info";
 import { MixpanelInstance } from "react-native-mixpanel";
+import { Platform } from "react-native";
 import { mixpanelToken } from "./config";
 import { isScreenReaderEnabled } from "./utils/accessibility";
 import { getAppVersion } from "./utils/appVersion";
@@ -36,7 +37,11 @@ const setupMixpanel = async (mp: MixpanelInstance) => {
 
 export const setMixpanelPushNotificationToken = (token: string) => {
   if (mixpanel) {
-    return mixpanel.setPushRegistrationId(token);
+    return Platform.select({
+      android: mixpanel.setPushRegistrationId(token),
+      ios: mixpanel.addPushDeviceToken(token),
+      default: Promise.resolve()
+    });
   }
   return Promise.resolve();
 };
