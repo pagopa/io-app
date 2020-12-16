1d5ca3c42e04308683af122219f81de073bf760e
Simone <simone.biffi.91@gmail.com>
2020-12-16 19:30:19 +0100

    [#176143533] refactor in functional way

--- ts/features/wallet/component/PagoPaPaymentCapability.tsx
+++ ts/features/wallet/component/PagoPaPaymentCapability.tsx
@@ -1,3 +1,4 @@
+import { fromNullable, isNone } from "fp-ts/lib/Option";
 import { Badge, View } from "native-base";
 import * as React from "react";
 import { StyleSheet } from "react-native";
@@ -6,11 +7,7 @@ import { H5 } from "../../../components/core/typography/H5";
 import { IOColors } from "../../../components/core/variables/IOColors";
 import { IOStyles } from "../../../components/core/variables/IOStyles";
 import I18n from "../../../i18n";
-import {
-  CreditCardType,
-  isCreditCard,
-  PaymentMethod
-} from "../../../types/pagopa";
+import { CreditCardType, PaymentMethod } from "../../../types/pagopa";
 
 const styles = StyleSheet.create({
   row: {
@@ -48,35 +45,52 @@ type Props = { paymentMethod: PaymentMethod };
 
 type badgeType = "available" | "arriving" | "not_available";
 
+const getLocales = () => ({
+  available: I18n.t("wallet.methods.card.pagoPaCapability.active"),
+  arriving: I18n.t("wallet.methods.card.pagoPaCapability.arriving"),
+  incompatible: I18n.t("wallet.methods.card.pagoPaCapability.incompatible")
+});
+
 const availabilityBadge = (badgeType: badgeType) => {
+  const { available, arriving, incompatible } = getLocales();
   switch (badgeType) {
     case "available":
       return (
         <Badge style={[styles.badgeInfo, styles.badgeAvailable]}>
-          <H5 color="white">
-            {I18n.t("wallet.methods.card.pagoPaCapability.active")}
-          </H5>
+          <H5 color="white">{available}</H5>
         </Badge>
       );
     case "arriving":
       return (
         <Badge style={[styles.badgeInfo, styles.badgeArriving]}>
-          <H5 color="blue">
-            {I18n.t("wallet.methods.card.pagoPaCapability.arriving")}
-          </H5>
+          <H5 color="blue">{arriving}</H5>
         </Badge>
       );
     case "not_available":
       return (
         <Badge style={[styles.badgeNotAvailable, styles.badgeInfo]}>
-          <H5 color="blue">
-            {I18n.t("wallet.methods.card.pagoPaCapability.incompatible")}
-          </H5>
+          <H5 color="blue">{incompatible}</H5>
         </Badge>
       );
   }
 };
 
+const extractBadgeType = (paymentMethod: PaymentMethod): badgeType => {
+  const badgeType = fromNullable(paymentMethod).map<badgeType>(pM => {
+    switch (pM.kind) {
+      case "CreditCard":
+        return CreditCardType.decode(pM.info.brand).fold(
+          () => "not_available",
+          b => (b === "MAESTRO" ? "arriving" : "available")
+        );
+      case "Satispay":
+        return "arriving";
+      default:
+        return "not_available";
+    }
+  });
+  return isNone(badgeType) ? "not_available" : badgeType.value;
+};
 /**
  * Represent the capability to pay in PagoPa of a payment method.
  *
@@ -96,13 +110,7 @@ const PagoPaPaymentCapability: React.FunctionComponent<Props> = props => (
         {I18n.t("wallet.methods.card.pagoPaCapability.description")}
       </H5>
     </View>
-    {props.paymentMethod.pagoPA &&
-    isCreditCard(props.paymentMethod) &&
-    CreditCardType.decode(props.paymentMethod.info.brand).value !== "MAESTRO"
-      ? availabilityBadge("available")
-      : props.paymentMethod.kind === "Bancomat"
-      ? availabilityBadge("not_available")
-      : availabilityBadge("arriving")}
+    {availabilityBadge(extractBadgeType(props.paymentMethod))}
   </View>
 );
 
