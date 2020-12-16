83688b0798c4d5d65957703ced6185d13ce9f261
Simone <simone.biffi.91@gmail.com>
2020-12-16 11:19:41 +0100

    [#176167278] fix typo

--- ts/sagas/startup.ts
+++ ts/sagas/startup.ts
@@ -264,12 +264,12 @@ export function* initializeApplicationSaga(): Generator<Effect, void, any> {
     backendClient.getSupportToken
   );
 
-  // Sart watching for request of remove profile
+  // Start watching for request bonus before remove profile
   yield takeEvery(
     loadBonusBeforeRemoveAccount,
     handleLoadBonusBeforeRemoveAccount
   );
-  // Sart watching for request of remove profile
+  // Start watching for request of remove profile
   yield takeEvery(removeAccountMotivation, handleRemoveAccount);
   // Start watching for requests of abort the onboarding
   const watchAbortOnboardingSagaTask = yield fork(watchAbortOnboardingSaga);
