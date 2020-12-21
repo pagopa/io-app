6479b9170ae011f2871543cd08367ff158e3a34a
Matteo Boschi <mbmaken@gmail.com>
2020-12-21 13:48:56 +0100

    [#175263933] support BPD/citizen/ranking API

--- ts/features/bonus/bpd/saga/networking/ranking.ts
+++ ts/features/bonus/bpd/saga/networking/ranking.ts
@@ -1,15 +1,41 @@
-import { Either, right } from "fp-ts/lib/Either";
-import { Effect } from "redux-saga/effects";
-import { readyRanking } from "../../store/reducers/__mock__/ranking";
+import { Either, left, right } from "fp-ts/lib/Either";
+import { call, Effect } from "redux-saga/effects";
+import { readableReport } from "italia-ts-commons/lib/reporters";
 import { BpdRankingReady } from "../../store/reducers/details/periods";
+import { BackendBpdClient } from "../../api/backendBpdClient";
+import { SagaCallReturnType } from "../../../../../types/utils";
+import { AwardPeriodId } from "../../store/actions/periods";
+import { getError } from "../../../../../utils/errors";
 
 // Load the ranking for all the periods
-// TODO: replace with networking logic and remove the eslint-disable
-// eslint-disable-next-line require-yield
-export function* bpdLoadRaking(): Generator<
-  Effect,
-  Either<Error, ReadonlyArray<BpdRankingReady>>,
-  any
-> {
-  return right<Error, ReadonlyArray<BpdRankingReady>>([readyRanking]);
+export function* bpdLoadRaking(
+  getRanking: ReturnType<typeof BackendBpdClient>["getRanking"]
+): Generator<Effect, Either<Error, ReadonlyArray<BpdRankingReady>>, any> {
+  try {
+    const getRankingResult: SagaCallReturnType<typeof getRanking> = yield call(
+      getRanking,
+      {} as any
+    );
+    if (getRankingResult.isRight()) {
+      if (getRankingResult.value.status === 200) {
+        return right<Error, ReadonlyArray<BpdRankingReady>>(
+          getRankingResult.value.value.map<BpdRankingReady>(rr => ({
+            ...rr,
+            awardPeriodId: rr.awardPeriodId as AwardPeriodId,
+            kind: "ready"
+          }))
+        );
+      } else {
+        return left<Error, ReadonlyArray<BpdRankingReady>>(
+          new Error(`response status ${getRankingResult.value.status}`)
+        );
+      }
+    } else {
+      return left<Error, ReadonlyArray<BpdRankingReady>>(
+        new Error(readableReport(getRankingResult.value))
+      );
+    }
+  } catch (e) {
+    return left<Error, ReadonlyArray<BpdRankingReady>>(getError(e));
+  }
 }
