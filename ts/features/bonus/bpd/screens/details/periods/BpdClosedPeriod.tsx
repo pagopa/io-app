f1b72930731bc4fa28e1cf32c29e1acfe2a2803d
Fabrizio <fartux@gmail.com>
2020-12-23 20:52:04 +0100

    [#176269197] display IbanComponent only with some conditions

--- ts/features/bonus/bpd/screens/details/periods/BpdClosedPeriod.tsx
+++ ts/features/bonus/bpd/screens/details/periods/BpdClosedPeriod.tsx
@@ -4,6 +4,10 @@ import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
 import { GlobalState } from "../../../../../../store/reducers/types";
+import {
+  BpdPeriodWithInfo,
+  isGracePeriod
+} from "../../../store/reducers/details/periods";
 import { bpdSelectedPeriodSelector } from "../../../store/reducers/details/selectedPeriod";
 import IbanInformationComponent from "../components/iban/IbanInformationComponent";
 import BpdSummaryComponent from "../components/summary/BpdSummaryComponent";
@@ -11,16 +15,23 @@ import BpdSummaryComponent from "../components/summary/BpdSummaryComponent";
 export type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
 
+const shouldRenderIbanComponent = (period: BpdPeriodWithInfo) =>
+  isGracePeriod(period) ||
+  (period.status === "Closed" &&
+    period.amount.transactionNumber >= period.minTransactionNumber);
+
 /**
  * Render the details for a completed and closed cashback periods
  * @constructor
  */
-const BpdClosedPeriod: React.FunctionComponent<Props> = () => (
+const BpdClosedPeriod = (props: Props): React.ReactElement => (
   <View style={IOStyles.horizontalContentPadding}>
     <View spacer={true} />
     <BpdSummaryComponent />
     <View spacer={true} extralarge={true} />
-    <IbanInformationComponent />
+    {props.currentPeriod && shouldRenderIbanComponent(props.currentPeriod) && (
+      <IbanInformationComponent />
+    )}
   </View>
 );
 
