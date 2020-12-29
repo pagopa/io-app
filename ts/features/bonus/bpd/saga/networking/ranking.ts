42fe66c40ac94679c00094c1d2e07c3ea2227090
Fabrizio <fartux@gmail.com>
2020-12-29 12:11:23 +0100

    [#176291649] track ranking api on mixpanel

--- ts/features/bonus/bpd/saga/networking/ranking.ts
+++ ts/features/bonus/bpd/saga/networking/ranking.ts
@@ -1,12 +1,17 @@
 import { Either, left, right } from "fp-ts/lib/Either";
 import { call, Effect } from "redux-saga/effects";
 import { readableReport } from "italia-ts-commons/lib/reporters";
+import { mixpanelTrack } from "../../../../../mixpanel";
 import { BpdRankingReady } from "../../store/reducers/details/periods";
 import { BackendBpdClient } from "../../api/backendBpdClient";
 import { SagaCallReturnType } from "../../../../../types/utils";
 import { AwardPeriodId } from "../../store/actions/periods";
 import { getError } from "../../../../../utils/errors";
 import { CitizenRankingResourceArray } from "../../../../../../definitions/bpd/citizen/CitizenRankingResourceArray";
+
+const mixpanelActionRequest = "BPD_RANKING_REQUEST";
+const mixpanelActionSuccess = "BPD_RANKING_SUCCESS";
+const mixpanelActionFailure = "BPD_RANKING_FAILURE";
 
 // convert the network payload ranking into the relative app domain model
 const convertRankingArray = (
@@ -23,12 +28,16 @@ export function* bpdLoadRaking(
   getRanking: ReturnType<typeof BackendBpdClient>["getRanking"]
 ): Generator<Effect, Either<Error, ReadonlyArray<BpdRankingReady>>, any> {
   try {
+    void mixpanelTrack(mixpanelActionRequest);
     const getRankingResult: SagaCallReturnType<typeof getRanking> = yield call(
       getRanking,
       {} as any
     );
     if (getRankingResult.isRight()) {
       if (getRankingResult.value.status === 200) {
+        void mixpanelTrack(mixpanelActionSuccess, {
+          count: getRankingResult.value.value?.length
+        });
         return right<Error, ReadonlyArray<BpdRankingReady>>(
           convertRankingArray(getRankingResult.value.value)
         );
@@ -43,6 +52,9 @@ export function* bpdLoadRaking(
       );
     }
   } catch (e) {
+    void mixpanelTrack(mixpanelActionFailure, {
+      reason: e.message
+    });
     return left<Error, ReadonlyArray<BpdRankingReady>>(getError(e));
   }
 }
