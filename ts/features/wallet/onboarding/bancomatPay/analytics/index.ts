c4fbbaafbfcb4ca128739344e9d310c38357ed1e
Matteo Boschi <mbmaken@gmail.com>
2021-01-07 18:34:32 +0100

    [#175889262] track bpay add to wallet actions

--- ts/features/wallet/onboarding/bancomatPay/analytics/index.ts
+++ ts/features/wallet/onboarding/bancomatPay/analytics/index.ts
@@ -2,18 +2,21 @@ import { getType } from "typesafe-actions";
 import { mixpanel } from "../../../../../mixpanel";
 import { Action } from "../../../../../store/actions/types";
 import { getNetworkErrorMessage } from "../../../../../utils/errors";
-import { searchUserBPay } from "../store/actions";
+import { searchUserBPay, addBPayToWallet } from "../store/actions";
 
 const trackAction = (mp: NonNullable<typeof mixpanel>) => (
   action: Action
 ): Promise<void> => {
   switch (action.type) {
+    case getType(addBPayToWallet.success):
+    case getType(addBPayToWallet.request):
     case getType(searchUserBPay.request):
       return mp.track(action.type);
     case getType(searchUserBPay.success):
       return mp.track(action.type, {
         count: action.payload.length
       });
+    case getType(addBPayToWallet.failure):
     case getType(searchUserBPay.failure):
       return mp.track(action.type, {
         reason: getNetworkErrorMessage(action.payload)
