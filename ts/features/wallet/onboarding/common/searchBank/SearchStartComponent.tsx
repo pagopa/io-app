debcbf960006633d4bb22bca2760af730477b086
CrisTofani <cri.tofani@gmail.com>
2021-01-08 16:45:32 +0100

    [#175890863] Fixes

--- ts/features/wallet/onboarding/common/searchBank/SearchStartComponent.tsx
+++ ts/features/wallet/onboarding/common/searchBank/SearchStartComponent.tsx
@@ -1,6 +1,5 @@
 import { View } from "native-base";
 import * as React from "react";
-import { fromNullable } from "fp-ts/lib/Option";
 import I18n from "../../../../../i18n";
 import { Body } from "../../../../../components/core/typography/Body";
 import { H1 } from "../../../../../components/core/typography/H1";
@@ -10,45 +9,31 @@ import { Link } from "../../../../../components/core/typography/Link";
 type Props = {
   openTosModal: () => void;
   onSearch: () => void;
-  methodType: "bancomatpay" | "pagobancomat";
+  methodType: "bancomatPay" | "bancomat";
 };
 
-type LocalesText = {
-  title: string;
-  text1: string;
-  text2: string;
-  text3: string;
-  text4: string;
-};
-
-const bancomatLocales = {
+const bancomatLocales = () => ({
   title: I18n.t("wallet.searchAbi.bancomat.title"),
   text1: I18n.t("wallet.searchAbi.bancomat.description.text1"),
   text2: I18n.t("wallet.searchAbi.bancomat.description.text2"),
   text3: I18n.t("wallet.searchAbi.bancomat.description.text3"),
   text4: I18n.t("wallet.searchAbi.bancomat.description.text4")
-};
+});
+const bancomatPayLocales = () => ({
+  title: I18n.t("wallet.searchAbi.bpay.title"),
+  text1: I18n.t("wallet.searchAbi.bpay.description.text1"),
+  text2: I18n.t("wallet.searchAbi.bpay.description.text2"),
+  text3: I18n.t("wallet.searchAbi.bpay.description.text3"),
+  text4: I18n.t("wallet.searchAbi.bpay.description.text4")
+});
 
-const mapLocalesFromMethod = new Map<string, LocalesText>([
-  [
-    "bancomatpay",
-    {
-      title: I18n.t("wallet.searchAbi.bpay.title"),
-      text1: I18n.t("wallet.searchAbi.bpay.description.text1"),
-      text2: I18n.t("wallet.searchAbi.bpay.description.text2"),
-      text3: I18n.t("wallet.searchAbi.bpay.description.text3"),
-      text4: I18n.t("wallet.searchAbi.bpay.description.text4")
-    }
-  ],
-  ["bancomat", bancomatLocales]
-]);
+const loadLocales = (methodType: "bancomatPay" | "bancomat") =>
+  methodType === "bancomatPay" ? bancomatPayLocales() : bancomatLocales();
 
 export const SearchStartComponent: React.FunctionComponent<Props> = (
   props: Props
 ) => {
-  const locales = fromNullable(
-    mapLocalesFromMethod.get(props.methodType)
-  ).getOrElse(bancomatLocales);
+  const locales = loadLocales(props.methodType);
 
   return (
     <>
