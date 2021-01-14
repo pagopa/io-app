174209f2c1d3d31a7beb6d8d8851c0860b8fd82e
Matteo Boschi <mbmaken@gmail.com>
2021-01-14 15:57:22 +0100

    [#176498731] update

--- ts/types/backendStatus.ts
+++ ts/types/backendStatus.ts
@@ -53,7 +53,7 @@ export type Sections = t.TypeOf<typeof Sections>;
 
 // it represents a remote config to switch on/off a specific section,feature,module,etc
 const Config = t.interface({
-  // this flag is legacy, don't use it anymore see https://www.pivotaltracker.com/story/show/176498731
+  // bpd_ranking is legacy, don't use it anymore see https://www.pivotaltracker.com/story/show/176498731
   bpd_ranking: t.boolean,
   bpd_ranking_v2: t.boolean
 });
