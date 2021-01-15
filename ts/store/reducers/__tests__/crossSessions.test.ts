a6448d30fd5def0c14ed5adb1b1c193c3821aacd
Matteo Boschi <mbmaken@gmail.com>
2021-01-15 15:20:13 +0100

    [#176497587] add reducer / selector tests

--- ts/store/reducers/__tests__/crossSessions.test.ts
+++ ts/store/reducers/__tests__/crossSessions.test.ts
@@ -1,10 +1,59 @@
-import { areSystemsDeadReducer } from "../backendStatus";
+import { FiscalCode } from "italia-ts-commons/lib/strings";
+import sha from "sha.js";
+import { appReducer } from "../index";
+import { GlobalState } from "../types";
+import { setProfileHashedFiscalCode } from "../../actions/crossSessions";
+import {
+  hashedProfileFiscalCodeSelector,
+  isDifferentFiscalCode
+} from "../crossSessions";
 
-describe("backend service status reducer", () => {
-  it("should decode the backend status", () => {
-    const newState = areSystemsDeadReducer(currentState, responseON);
-    expect(newState.areSystemsDead).toBeFalsy();
-    expect(newState.deadsCounter).toEqual(0);
-    expect(newState.status.isSome()).toBeTruthy();
+jest.mock("@react-native-community/async-storage", () => ({
+  AsyncStorage: jest.fn()
+}));
+
+jest.mock("react-native-share", () => ({
+  open: jest.fn()
+}));
+
+const hash = (value: string): string =>
+  sha("sha256").update(value).digest("hex");
+const fiscalCode = "TAMMRA80A41H501Y" as FiscalCode;
+const hashedFiscalCode = hash(fiscalCode);
+
+describe("cross sessions status reducer/selectors", () => {
+  it("should be the hash value of fiscal code", () => {
+    const globalState: GlobalState = appReducer(
+      undefined,
+      setProfileHashedFiscalCode(fiscalCode)
+    );
+    expect(hashedProfileFiscalCodeSelector(globalState)).toEqual(
+      hashedFiscalCode
+    );
+  });
+
+  it("should be different from hash value of fiscal code", () => {
+    const globalState: GlobalState = appReducer(
+      undefined,
+      setProfileHashedFiscalCode("TAMMRA80A41H501X" as FiscalCode)
+    );
+    expect(hashedProfileFiscalCodeSelector(globalState)).not.toEqual(
+      hashedFiscalCode
+    );
+  });
+
+  it("should not be different", () => {
+    const globalState: GlobalState = appReducer(
+      undefined,
+      setProfileHashedFiscalCode(fiscalCode)
+    );
+    expect(isDifferentFiscalCode(fiscalCode)(globalState)).toBeFalsy();
+    // empty state
+    expect(
+      isDifferentFiscalCode(fiscalCode)({
+        ...globalState,
+        crossSessions: { hashedFiscalCode: undefined }
+      })
+    ).toBeFalsy();
   });
 });
