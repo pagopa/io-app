5bc6f7b3a95863d42cdc2d3e636513b6c0880513
Cristiano Tofani <cri.tofani@gmail.com>
2021-01-19 13:16:34 +0100

    feat(Carta Giovani Nazionale): [#176443044] Adds directories skeleton for CGN implementation (#2718)
    * [#176443044] Adds some skaffolding for CGN store, reducer and navigation
    
    * [#176443044] Minor fixes
    
    * Update ts/features/bonus/cgn/screens/onboarding/CgnInformationScreen.tsx
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * Update ts/features/bonus/cgn/navigation/routes.ts
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * Update ts/features/bonus/cgn/navigation/actions.ts
    
    Co-authored-by: fabriziofff <fartux@gmail.com>
    
    * Update ts/features/bonus/cgn/navigation/actions.ts
    
    Co-authored-by: fabriziofff <fartux@gmail.com>
    
    * Update ts/features/bonus/cgn/navigation/actions.ts
    
    Co-authored-by: fabriziofff <fartux@gmail.com>
    
    * Update ts/features/bonus/cgn/navigation/actions.ts
    
    Co-authored-by: fabriziofff <fartux@gmail.com>
    
    * [#176443044] Adds a locale for activate CTA
    
    * [#176443044] removes comment
    
    * Update locales/en/index.yml
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    Co-authored-by: fabriziofff <fartux@gmail.com>

--- ts/store/actions/types.ts
+++ ts/store/actions/types.ts
@@ -14,6 +14,7 @@ import { AbiActions } from "../../features/wallet/onboarding/bancomat/store/acti
 import { BPayActions } from "../../features/wallet/onboarding/bancomatPay/store/actions";
 import { SatispayActions } from "../../features/wallet/onboarding/satispay/store/actions";
 import { GlobalState } from "../reducers/types";
+import { CgnActions } from "../../features/bonus/cgn/store/actions";
 import { AnalyticsActions } from "./analytics";
 import { ApplicationActions } from "./application";
 import { AuthenticationActions } from "./authentication";
@@ -82,7 +83,8 @@ export type Action =
   | BpdActions
   | AbiActions
   | BPayActions
-  | SatispayActions;
+  | SatispayActions
+  | CgnActions;
 
 export type Dispatch = DispatchAPI<Action>;
 
