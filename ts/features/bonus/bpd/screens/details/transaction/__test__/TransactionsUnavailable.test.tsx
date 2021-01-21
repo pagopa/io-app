c44dc51ea18f9ccad03f0398975944f1b4058b3e
Simone <simone.biffi.91@gmail.com>
2021-01-21 14:30:11 +0100

    [#176455937] add test files

---
+++ ts/features/bonus/bpd/screens/details/transaction/__test__/TransactionsUnavailable.test.tsx
@@ -0,0 +1,55 @@
+import { render } from "@testing-library/react-native";
+import configureMockStore from "redux-mock-store";
+import * as React from "react";
+import { Provider } from "react-redux";
+import { Store } from "redux";
+import TransactionsUnavailable from "../TransactionsUnavailable";
+
+jest.mock("react-navigation", () => ({
+  NavigationEvents: "mockNavigationEvents",
+  StackActions: {
+    push: jest
+      .fn()
+      .mockImplementation(x => ({ ...x, type: "Navigation/PUSH" })),
+    replace: jest
+      .fn()
+      .mockImplementation(x => ({ ...x, type: "Navigation/REPLACE" })),
+    reset: jest.fn()
+  },
+  NavigationActions: {
+    navigate: jest.fn().mockImplementation(x => x)
+  },
+  createStackNavigator: jest.fn(),
+  withNavigation: (component: any) => component
+}));
+describe("TransactionsUnavailable component", () => {
+  const mockStore = configureMockStore();
+  // eslint-disable-next-line functional/no-let
+  let store: ReturnType<typeof mockStore>;
+
+  beforeEach(() => {
+    store = mockStore({
+      search: { isSearchEnabled: false },
+      persistedPreferences: { isPagoPATestEnabled: false },
+      network: { isConnected: true },
+      instabug: { unreadMessages: 0 }
+    });
+  });
+
+  it("should show the payment-unavailable-icon.png", () => {
+    const component = getComponent(store);
+    const rasterImageComponent = component.queryByTestId("rasterImage");
+    const paymentUnavailableIconPath =
+      "../../../img/wallet/errors/payment-unavailable-icon.png";
+
+    expect(rasterImageComponent).toHaveProp("source", {
+      testUri: paymentUnavailableIconPath
+    });
+  });
+});
+const getComponent = (store: Store<unknown>) =>
+  render(
+    <Provider store={store}>
+      <TransactionsUnavailable />
+    </Provider>
+  );
