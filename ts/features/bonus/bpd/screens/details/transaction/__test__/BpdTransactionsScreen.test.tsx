2d50d6e190bc2884e7985c269116e52452c5263b
Simone <simone.biffi.91@gmail.com>
2021-01-22 10:11:32 +0100

    [#176455937] add bpdTransactionsScreen tests

---
+++ ts/features/bonus/bpd/screens/details/transaction/__test__/BpdTransactionsScreen.test.tsx
@@ -0,0 +1,99 @@
+import { render } from "@testing-library/react-native";
+import * as React from "react";
+import * as pot from "italia-ts-commons/lib/pot";
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { Store } from "redux";
+import BpdTransactionsScreen from "../BpdTransactionsScreen";
+import * as transactionsReducer from "../../../../store/reducers/details/combiner";
+import * as lastUpdateReducer from "../../../../store/reducers/details/lastUpdate";
+import { EnhancedBpdTransaction } from "../../../../components/transactionItem/BpdTransactionItem";
+import * as LoadTransactions from "../LoadTransactions";
+import * as TransactionsUnavailable from "../TransactionsUnavailable";
+jest.mock("react-navigation", () => ({
+  NavigationEvents: "mockNavigationEvents",
+  StackActions: {
+    push: jest
+      .fn()
+      .mockImplementation(x => ({ ...x, type: "Navigation/PUSH" })),
+    replace: jest
+      .fn()
+      .mockImplementation(x => ({ ...x, type: "Navigation/REPLACE" })),
+    reset: jest.fn()
+  },
+  NavigationActions: {
+    navigate: jest.fn().mockImplementation(x => x)
+  },
+  createStackNavigator: jest.fn(),
+  withNavigation: (component: any) => component
+}));
+
+describe("BpdTransactionsScreen", () => {
+  const mockStore = configureMockStore();
+  // eslint-disable-next-line functional/no-let
+  let store: ReturnType<typeof mockStore>;
+
+  beforeEach(() => {
+    store = mockStore({
+      bonus: { bpd: { details: { lastUpdate: pot.none } } },
+      wallet: {
+        wallets: { walletById: pot.some({} as EnhancedBpdTransaction) }
+      },
+      search: { isSearchEnabled: false },
+      persistedPreferences: { isPagoPATestEnabled: false },
+      network: { isConnected: true },
+      instabug: { unreadMessages: 0 }
+    });
+  });
+
+  it.each`
+    bpdLastUpdate
+    ${pot.noneLoading}
+    ${pot.noneUpdating({} as Date)}
+    ${pot.someLoading({} as Date)}
+    ${pot.someUpdating({} as Date, {} as Date)}
+  `(
+    "should show loading screen if $bpdLastUpdate is pot.loading or pot.updating ",
+    ({ bpdLastUpdate }) => {
+      jest
+        .spyOn(lastUpdateReducer, "bpdLastUpdateSelector")
+        .mockReturnValue(bpdLastUpdate);
+      jest
+        .spyOn(transactionsReducer, "bpdDisplayTransactionsSelector")
+        .mockReturnValue(pot.none);
+      const myspy = jest.spyOn(LoadTransactions, "default");
+
+      getComponent(store);
+      expect(myspy).toHaveBeenCalled();
+    }
+  );
+  it.each`
+    bpdLastUpdate
+    ${pot.none}
+    ${pot.noneError({})}
+    ${pot.someError({} as Date, {} as Error)}
+  `(
+    "should show unavailable screen if $bpdLastUpdate is pot.Error",
+    ({ bpdLastUpdate }) => {
+      jest
+        .spyOn(lastUpdateReducer, "bpdLastUpdateSelector")
+        .mockReturnValue(bpdLastUpdate);
+      jest
+        .spyOn(transactionsReducer, "bpdDisplayTransactionsSelector")
+        .mockReturnValue(pot.none);
+
+      const myspy = jest.spyOn(TransactionsUnavailable, "default");
+
+      getComponent(store);
+
+      expect(myspy).toHaveBeenCalled();
+    }
+  );
+});
+
+const getComponent = (store: Store<unknown>) =>
+  render(
+    <Provider store={store}>
+      <BpdTransactionsScreen />
+    </Provider>
+  );
