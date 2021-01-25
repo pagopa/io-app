556c728bebd11e9fb0379de0c549393acabcda2e
Simone <simone.biffi.91@gmail.com>
2021-01-25 09:38:13 +0100

    [#176578587] add dark layout

--- ts/features/wallet/cobadge/screen/CobadgeDetailScreen.tsx
+++ ts/features/wallet/cobadge/screen/CobadgeDetailScreen.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from "native-base";
 import * as React from "react";
+import { StyleSheet } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import I18n from "../../../../i18n";
+import { IOColors } from "../../../../components/core/variables/IOColors";
+import DarkLayout from "../../../../components/screens/DarkLayout";
 import { GlobalState } from "../../../../store/reducers/types";
 import { CreditCardPaymentMethod } from "../../../../types/pagopa";
+import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
 import { useRemovePaymentMethodBottomSheet } from "../../component/RemovePaymentMethod";
 
 type NavigationParams = Readonly<{
@@ -15,6 +20,24 @@ type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps> &
   NavigationInjectedProps<NavigationParams>;
 
+const styles = StyleSheet.create({
+  cardContainer: {
+    height: 235,
+    width: "100%",
+    position: "absolute",
+    top: 16,
+    zIndex: 7,
+    elevation: 7,
+    alignItems: "center"
+  },
+  headerSpacer: {
+    height: 172
+  },
+  cancelButton: {
+    borderColor: IOColors.red,
+    width: "100%"
+  }
+});
 /**
  * Detail screen for a cobadge card
  * @constructor
@@ -23,9 +46,16 @@ const CobadgeDetailScreen: React.FunctionComponent<Props> = props => {
   const cobadge: CreditCardPaymentMethod = props.navigation.getParam("cobadge");
 
   return (
-    <View>
-      <Text>{"Entra"}</Text>
-    </View>
+    <DarkLayout
+      bounces={false}
+      contextualHelp={emptyContextualHelp}
+      title={I18n.t("wallet.methods.bancomatPay.name")}
+      faqCategories={["wallet_methods"]}
+      allowGoBack={true}
+      topContent={<View style={styles.headerSpacer} />}
+      gradientHeader={true}
+      hideHeader={true}
+    ></DarkLayout>
   );
 };
 const mapDispatchToProps = (_: Dispatch) => ({});
