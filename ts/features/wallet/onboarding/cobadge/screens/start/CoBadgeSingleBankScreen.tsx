d7d73e5030387c2be8e63f46a3f92b3ce2a2b818
Simone <simone.biffi.91@gmail.com>
2021-01-28 17:45:43 +0100

    [#176575663] Add CobadgeSingleBankScreen

--- ts/features/wallet/onboarding/cobadge/screens/start/CoBadgeSingleBankScreen.tsx
+++ ts/features/wallet/onboarding/cobadge/screens/start/CoBadgeSingleBankScreen.tsx
@@ -1,41 +1,72 @@
-import { Content } from "native-base";
 import * as React from "react";
-import { SafeAreaView } from "react-native";
+import { useContext } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { H1 } from "../../../../../../components/core/typography/H1";
-import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
-import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
-import I18n from "../../../../../../i18n";
+import { Abi } from "../../../../../../../definitions/pagopa/walletv2/Abi";
+import { LightModalContext } from "../../../../../../components/ui/LightModal";
 import { GlobalState } from "../../../../../../store/reducers/types";
-import { emptyContextualHelp } from "../../../../../../utils/emptyContextualHelp";
+import TosBonusComponent from "../../../../../bonus/bonusVacanze/components/TosBonusComponent";
+import SearchStartScreen from "../../../common/searchBank/SearchStartScreen";
+import { abiListSelector } from "../../../store/abi";
+import { navigateToOnboardingCoBadgeSearchAvailable } from "../../navigation/action";
+import { searchUserCoBadge, walletAddCoBadgeCancel } from "../../store/actions";
 
-type Props = ReturnType<typeof mapDispatchToProps> &
+type OwnProps = {
+  abi: string;
+};
+type Props = OwnProps &
+  ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
 
+const tos_url =
+  "https://io.italia.it/app-content/privacy_circuiti_internazionali.html";
+const partecipatingBank_url =
+  "https://io.italia.it/app-content/privacy_circuiti_internazionali.html";
 /**
  * The initial screen of the co-badge workflow (starting with a specific ABI, eg. from BANCOMAT screen)
  * The user can see the selected bank and can start the search for all the co-badge for the specific bank.
  * @param _
  * @constructor
  */
-const CoBadgeSingleBankScreen = (_: Props): React.ReactElement => (
-  <BaseScreenComponent
-    goBack={true}
-    headerTitle={I18n.t("wallet.onboarding.coBadge.headerTitle")}
-    contextualHelp={emptyContextualHelp}
-  >
-    <SafeAreaView style={IOStyles.flex}>
-      <Content style={IOStyles.flex}>
-        <H1>CoBadgeAllBanksScreen</H1>
-      </Content>
-    </SafeAreaView>
-  </BaseScreenComponent>
-);
+const CoBadgeSingleBankScreen = (props: Props): React.ReactElement => {
+  const { showModal, hideModal } = useContext(LightModalContext);
 
-const mapDispatchToProps = (_: Dispatch) => ({});
+  const openTosModal = () => {
+    showModal(<TosBonusComponent tos_url={tos_url} onClose={hideModal} />);
+  };
 
-const mapStateToProps = (_: GlobalState) => ({});
+  const openPartecipatingBankModal = () => {
+    showModal(
+      <TosBonusComponent tos_url={partecipatingBank_url} onClose={hideModal} />
+    );
+  };
+
+  const abiInfo: Abi | undefined = props.abiList.find(
+    aI => aI.abi === props.abi
+  );
+  return (
+    <SearchStartScreen
+      methodType={"cobadge"}
+      onCancel={props.onCancel}
+      onSearch={props.searchAccounts}
+      handlePartecipatingBanksModal={openPartecipatingBankModal}
+      handleTosModal={openTosModal}
+      bankName={abiInfo?.name}
+    />
+  );
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  onCancel: () => dispatch(walletAddCoBadgeCancel()),
+  searchAccounts: (abi?: string) => {
+    dispatch(searchUserCoBadge.request(abi));
+    dispatch(navigateToOnboardingCoBadgeSearchAvailable());
+  }
+});
+
+const mapStateToProps = (state: GlobalState) => ({
+  abiList: abiListSelector(state)
+});
 
 export default connect(
   mapStateToProps,
