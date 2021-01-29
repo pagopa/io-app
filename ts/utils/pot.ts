bfc659feeec44920e4141f622c5c6779f6ee5830
Matteo Boschi <mbmaken@gmail.com>
2021-01-29 12:36:35 +0100

    [#176575256] restore

--- ts/utils/pot.ts
+++ ts/utils/pot.ts
@@ -1,5 +1,4 @@
 import * as pot from "italia-ts-commons/lib/pot";
 
 // return true if pot is some and not someError and not someLoading
-export const isStrictSome = <T, E>(p: pot.Pot<T, E>): p is Some<T> =>
-  p.kind === "PotSome";
+export const isStrictSome = <T, E>(p: pot.Pot<T, E>) => p.kind === "PotSome";
