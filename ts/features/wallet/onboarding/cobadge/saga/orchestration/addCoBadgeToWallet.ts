3d2878d84cbae2377c738dba183d37ae13b3006a
Matteo Boschi <mbmaken@gmail.com>
2021-01-29 12:34:50 +0100

    Update ts/features/wallet/onboarding/cobadge/saga/orchestration/addCoBadgeToWallet.ts

--- ts/features/wallet/onboarding/cobadge/saga/orchestration/addCoBadgeToWallet.ts
+++ ts/features/wallet/onboarding/cobadge/saga/orchestration/addCoBadgeToWallet.ts
@@ -24,7 +24,7 @@ import { onboardingCoBadgeAddedSelector } from "../../store/reducers/addedCoBadg
 /**
  * Define the workflow that allows the user to add a co-badge card to the wallet.
  * The workflow ends when:
- * - The user add at least one owned co-badge card to the wallet {@link walletAddCoBadgeCompleted}
+ * - The user adds at least one owned co-badge card to the wallet {@link walletAddCoBadgeCompleted}
  * - The user aborts the insertion of a co-badge {@link walletAddCoBadgeCancel}
  * - The user chooses back from the first screen {@link walletAddCoBadgeBack}
  */
