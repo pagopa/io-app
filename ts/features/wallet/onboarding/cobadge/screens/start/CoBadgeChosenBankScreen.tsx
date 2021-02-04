4f1144b4ff15fb828a9cade80877b43917e6f37a
Fabrizio <fartux@gmail.com>
2021-02-04 13:47:01 +0100

    [#176786909] fix typescript

--- ts/features/wallet/onboarding/cobadge/screens/start/CoBadgeChosenBankScreen.tsx
+++ ts/features/wallet/onboarding/cobadge/screens/start/CoBadgeChosenBankScreen.tsx
@@ -9,7 +9,7 @@ import TosBonusComponent from "../../../../../bonus/bonusVacanze/components/TosB
 import SearchStartScreen from "../../../common/searchBank/SearchStartScreen";
 import { abiListSelector } from "../../../store/abi";
 import { navigateToOnboardingCoBadgeSearchAvailable } from "../../navigation/action";
-import { searchUserCoBadge, walletAddCoBadgeCancel } from "../../store/actions";
+import { walletAddCoBadgeCancel } from "../../store/actions";
 
 type OwnProps = {
   abi?: string;
