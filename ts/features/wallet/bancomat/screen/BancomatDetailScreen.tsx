fad574047612f82ce0aac2de11e72aca3a57849c
fabriziofff <fartux@gmail.com>
2021-02-05 14:55:26 +0100

    chore(Bonus Pagamenti Digitali): [#176816895] Add feature flag for cobadge workflow (#2786)
    * [#176738620] add choosetype screen
    
    * [#176738620] rename action
    
    * [#176738620] remove back from KO screens
    
    * [#176738620] add legacy support to navigation
    
    * [#176738620] add choosetype screen
    
    * [#176738620] rename action
    
    * [#176738620] remove back from KO screens
    
    * [#176738620] add legacy support to navigation
    
    * [#176738620] fix eslint
    
    * [#176816895] add ff for cobadge
    
    * Update ts/features/wallet/component/NewMethodAddedNotifier.tsx
    
    * [#176816895] remove useless object
    
    Co-authored-by: Simone <simone.biffi.91@gmail.com>
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

--- ts/features/wallet/bancomat/screen/BancomatDetailScreen.tsx
+++ ts/features/wallet/bancomat/screen/BancomatDetailScreen.tsx
@@ -1,3 +1,4 @@
+import { none } from "fp-ts/lib/Option";
 import { Button, View } from "native-base";
 import * as React from "react";
 import { StyleSheet } from "react-native";
@@ -9,8 +10,10 @@ import { IOColors } from "../../../../components/core/variables/IOColors";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import ItemSeparatorComponent from "../../../../components/ItemSeparatorComponent";
 import DarkLayout from "../../../../components/screens/DarkLayout";
+import { cobadgeEnabled } from "../../../../config";
 import I18n from "../../../../i18n";
 import { mixpanelTrack } from "../../../../mixpanel";
+import { navigateToWalletAddPaymentMethod } from "../../../../store/actions/navigation";
 import { deleteWalletRequest } from "../../../../store/actions/wallet/wallets";
 import { GlobalState } from "../../../../store/reducers/types";
 import { BancomatPaymentMethod } from "../../../../types/pagopa";
@@ -133,10 +136,12 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
     ),
   addCoBadge: (abi: string) =>
     dispatch(
-      navigateToOnboardingCoBadgeChooseTypeStartScreen({
-        abi,
-        legacyAddCreditCardBack: 1
-      })
+      cobadgeEnabled
+        ? navigateToOnboardingCoBadgeChooseTypeStartScreen({
+            abi,
+            legacyAddCreditCardBack: 1
+          })
+        : navigateToWalletAddPaymentMethod({ inPayment: none })
     )
 });
 
