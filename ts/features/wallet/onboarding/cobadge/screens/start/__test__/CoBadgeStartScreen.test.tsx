e1d970f22293cb60a36f6370eda08d55bdac1657
Fabrizio <fartux@gmail.com>
2021-02-10 18:07:22 +0100

    [#176842043] add tests

--- ts/features/wallet/onboarding/cobadge/screens/start/__test__/CoBadgeStartScreen.test.tsx
+++ ts/features/wallet/onboarding/cobadge/screens/start/__test__/CoBadgeStartScreen.test.tsx
@@ -3,6 +3,8 @@ import * as pot from "italia-ts-commons/lib/pot";
 import * as React from "react";
 import { NavigationParams } from "react-navigation";
 import { Action, createStore, Store } from "redux";
+import { StatusEnum } from "../../../../../../../../definitions/pagopa/cobadge/configuration/CoBadgeService";
+import { CoBadgeServices } from "../../../../../../../../definitions/pagopa/cobadge/configuration/CoBadgeServices";
 import I18n from "../../../../../../../i18n";
 import { applicationChangeState } from "../../../../../../../store/actions/application";
 import { appReducer } from "../../../../../../../store/reducers";
@@ -20,6 +22,24 @@ jest.mock("react-native-share", () => jest.fn());
 
 const configurationFailure = getGenericError(new Error("generic Error"));
 const abiTestId = "9876";
+const abiConfigurationWithoutBankMock: CoBadgeServices = {
+  ServiceName: {
+    status: StatusEnum.enabled,
+    issuers: [{ abi: "1", name: "bankName" }]
+  }
+};
+const abiConfigurationDisabled: CoBadgeServices = {
+  ServiceName: {
+    status: StatusEnum.disabled,
+    issuers: [{ abi: abiTestId, name: "bankName" }]
+  }
+};
+const abiConfigurationUnavailable: CoBadgeServices = {
+  ServiceName: {
+    status: StatusEnum.unavailable,
+    issuers: [{ abi: abiTestId, name: "bankName" }]
+  }
+};
 
 describe("Test behaviour of the CoBadgeStartScreen", () => {
   jest.useFakeTimers();
@@ -103,27 +123,7 @@ describe("Test behaviour of the CoBadgeStartScreen", () => {
     ).toBeTruthy();
   });
   it("When receive an error during the loading, the screen should render LoadAbiConfiguration (error state)", () => {
-    const globalState = appReducer(undefined, applicationChangeState("active"));
-    const store = createStore(appReducer, globalState as any);
-    store.dispatch(walletAddCoBadgeStart(abiTestId));
-
-    // check default initial state
-    expect(store.getState().wallet.onboarding.coBadge.abiSelected).toBe(
-      abiTestId
-    );
-    const testComponent = renderCoBadgeScreen(store);
-
-    // When the component is mounted with coBadgeConfiguration pot.none, the component should try to reload the state
-    expect(
-      store.getState().wallet.onboarding.coBadge.abiConfiguration
-    ).toStrictEqual(pot.noneLoading);
-
-    expect(isLoadingScreen(testComponent)).toBe(true);
-
-    // check the loading state for the loading error component
-    expect(
-      testComponent.queryByTestId("LoadingErrorComponentLoading")
-    ).toBeTruthy();
+    const { store, testComponent } = getInitCoBadgeStartScreen(abiTestId);
 
     store.dispatch(loadCoBadgeAbiConfiguration.failure(configurationFailure));
     // After an error, the component switch to "error" mode
@@ -133,7 +133,46 @@ describe("Test behaviour of the CoBadgeStartScreen", () => {
       testComponent.queryByTestId("LoadingErrorComponentError")
     ).toBeTruthy();
   });
+  it("When receive a configuration without the selected abi, the screen should render CoBadgeStartKoDisabled", () => {
+    const { store, testComponent } = getInitCoBadgeStartScreen(abiTestId);
+
+    store.dispatch(
+      loadCoBadgeAbiConfiguration.success(abiConfigurationWithoutBankMock)
+    );
+    // The user should see the disabled screen when the selected abi is not in the remote configuration
+    expect(isDisabledScreen(testComponent)).toBe(true);
+  });
+  it("When receive a configuration without an abi disabled, the screen should render CoBadgeStartKoDisabled", () => {
+    const { store, testComponent } = getInitCoBadgeStartScreen(abiTestId);
+
+    store.dispatch(
+      loadCoBadgeAbiConfiguration.success(abiConfigurationDisabled)
+    );
+    // The user should see the disabled screen
+    expect(isDisabledScreen(testComponent)).toBe(true);
+  });
+  it("When receive a configuration with an abi unavailable, the screen should render CoBadgeStartKoUnavailable", () => {
+    const { store, testComponent } = getInitCoBadgeStartScreen(abiTestId);
+
+    store.dispatch(
+      loadCoBadgeAbiConfiguration.success(abiConfigurationUnavailable)
+    );
+    // The user should see the unavailable screen
+    expect(isUnavailableScreen(testComponent)).toBe(true);
+  });
 });
+
+/**
+ * Initialize a CoBadgeStartScreen, that pass in loading state after the mount
+ * @param abi
+ */
+const getInitCoBadgeStartScreen = (abi: string) => {
+  const globalState = appReducer(undefined, applicationChangeState("active"));
+  const store = createStore(appReducer, globalState as any);
+  store.dispatch(walletAddCoBadgeStart(abi));
+  const testComponent = renderCoBadgeScreen(store);
+  return { store, testComponent };
+};
 
 const renderCoBadgeScreen = (store: Store<GlobalState, Action>) =>
   renderScreenFakeNavRedux<GlobalState, NavigationParams>(
@@ -151,3 +190,9 @@ const isAllBankScreen = (component: RenderAPI) =>
 
 const isLoadingScreen = (component: RenderAPI) =>
   component.queryByTestId("LoadAbiConfiguration") !== null;
+
+const isDisabledScreen = (component: RenderAPI) =>
+  component.queryByTestId("CoBadgeStartKoDisabled") !== null;
+
+const isUnavailableScreen = (component: RenderAPI) =>
+  component.queryByTestId("CoBadgeStartKoUnavailable") !== null;
