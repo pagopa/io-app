29f1c703e1eecf18e2b1d794134c2aca803add7d
Fabrizio <fartux@gmail.com>
2021-02-10 15:39:21 +0100

    [#176842043] add error -> loading case

--- ts/features/wallet/onboarding/cobadge/screens/start/__test__/CoBadgeStartScreen.test.tsx
+++ ts/features/wallet/onboarding/cobadge/screens/start/__test__/CoBadgeStartScreen.test.tsx
@@ -1,3 +1,4 @@
+import { RenderAPI } from "@testing-library/react-native";
 import * as pot from "italia-ts-commons/lib/pot";
 import * as React from "react";
 import { NavigationParams } from "react-navigation";
@@ -6,6 +7,7 @@ import I18n from "../../../../../../../i18n";
 import { applicationChangeState } from "../../../../../../../store/actions/application";
 import { appReducer } from "../../../../../../../store/reducers";
 import { GlobalState } from "../../../../../../../store/reducers/types";
+import { getGenericError } from "../../../../../../../utils/errors";
 import { renderScreenFakeNavRedux } from "../../../../../../../utils/testWrapper";
 import WALLET_ONBOARDING_COBADGE_ROUTES from "../../../navigation/routes";
 import {
@@ -15,6 +17,9 @@ import {
 import CoBadgeStartScreen from "../CoBadgeStartScreen";
 
 jest.mock("react-native-share", () => jest.fn());
+
+const configurationFailure = getGenericError(new Error("generic Error"));
+const abiTestId = "9876";
 
 describe("Test behaviour of the CoBadgeStartScreen", () => {
   jest.useFakeTimers();
@@ -30,31 +35,72 @@ describe("Test behaviour of the CoBadgeStartScreen", () => {
 
     const testComponent = renderCoBadgeScreen(store);
 
-    // render the choose bank screen for all abi
-    expect(
-      testComponent.queryByTestId("CoBadgeChosenBankScreenAll")
-    ).toBeTruthy();
-
-    const participatingBank = testComponent.queryByText(
-      I18n.t("wallet.searchAbi.cobadge.description.text2")
-    );
-    expect(participatingBank).toBeTruthy();
+    expect(isAllBankScreen(testComponent)).toBe(true);
   });
-  it("With the configuration loading, the screen should render LoadAbiConfiguration without Abi", () => {
+  it("With the configuration loading and a specific abi, the screen should render LoadAbiConfiguration (loading state)", () => {
     const globalState = appReducer(undefined, applicationChangeState("active"));
     const store = createStore(appReducer, globalState as any);
-    const abi = "100000";
-    store.dispatch(walletAddCoBadgeStart(abi));
+    store.dispatch(walletAddCoBadgeStart(abiTestId));
     store.dispatch(loadCoBadgeAbiConfiguration.request());
 
     // check default initial state
     expect(store.getState().wallet.onboarding.coBadge.abiConfiguration).toBe(
       pot.noneLoading
     );
-    expect(store.getState().wallet.onboarding.coBadge.abiSelected).toBe(abi);
+    expect(store.getState().wallet.onboarding.coBadge.abiSelected).toBe(
+      abiTestId
+    );
 
     const testComponent = renderCoBadgeScreen(store);
-    expect(testComponent.queryByTestId("LoadAbiConfiguration")).toBeTruthy();
+    expect(isLoadingScreen(testComponent)).toBe(true);
+
+    // check the loading state for the loading error component
+    expect(
+      testComponent.queryByTestId("LoadingErrorComponentLoading")
+    ).toBeTruthy();
+    expect(
+      testComponent.queryByText(
+        I18n.t("wallet.onboarding.coBadge.start.loading")
+      )
+    ).toBeTruthy();
+
+    store.dispatch(
+      loadCoBadgeAbiConfiguration.failure({
+        kind: "generic",
+        value: new Error()
+      })
+    );
+    expect(
+      testComponent.queryByTestId("LoadingErrorComponentError")
+    ).toBeTruthy();
+  });
+  it("With the configuration error and a specific abi, the screen should render LoadAbiConfiguration (loading state)", () => {
+    const globalState = appReducer(undefined, applicationChangeState("active"));
+    const store = createStore(appReducer, globalState as any);
+    store.dispatch(walletAddCoBadgeStart(abiTestId));
+    store.dispatch(loadCoBadgeAbiConfiguration.failure(configurationFailure));
+
+    // check default initial state
+    expect(
+      store.getState().wallet.onboarding.coBadge.abiConfiguration
+    ).toStrictEqual(pot.noneError(configurationFailure));
+    expect(store.getState().wallet.onboarding.coBadge.abiSelected).toBe(
+      abiTestId
+    );
+
+    const testComponent = renderCoBadgeScreen(store);
+
+    // When the component is mounted with coBadgeConfiguration pot.Error, the component try to reload the state
+    expect(
+      store.getState().wallet.onboarding.coBadge.abiConfiguration
+    ).toStrictEqual(pot.noneLoading);
+
+    expect(isLoadingScreen(testComponent)).toBe(true);
+
+    // check the error state for the loading error component
+    expect(
+      testComponent.queryByTestId("LoadingErrorComponentLoading")
+    ).toBeTruthy();
   });
 });
 
@@ -65,3 +111,12 @@ const renderCoBadgeScreen = (store: Store<GlobalState, Action>) =>
     {},
     store
   );
+
+const isAllBankScreen = (component: RenderAPI) =>
+  component.queryByTestId("CoBadgeChosenBankScreenAll") !== null &&
+  component.queryByText(
+    I18n.t("wallet.searchAbi.cobadge.description.text2")
+  ) !== null;
+
+const isLoadingScreen = (component: RenderAPI) =>
+  component.queryByTestId("LoadAbiConfiguration") !== null;
