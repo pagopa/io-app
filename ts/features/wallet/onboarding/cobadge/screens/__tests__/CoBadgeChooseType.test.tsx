969610048694f617ccab0fc28bf62cf3fbf3d18d
Simone <simone.biffi.91@gmail.com>
2021-02-15 11:54:48 +0100

    [#176842973] add CoBadgeChooseType tests

--- ts/features/wallet/onboarding/cobadge/screens/__tests__/CoBadgeChooseType.test.tsx
+++ ts/features/wallet/onboarding/cobadge/screens/__tests__/CoBadgeChooseType.test.tsx
@@ -1,16 +1,89 @@
-import { render } from "@testing-library/react-native";
+import { fireEvent } from "@testing-library/react-native";
+import { none } from "fp-ts/lib/Option";
 import * as React from "react";
-import { Provider } from "react-redux";
+import { NavigationActions, NavigationParams } from "react-navigation";
 import configureMockStore from "redux-mock-store";
+import ROUTES from "../../../../../../navigation/routes";
+import { applicationChangeState } from "../../../../../../store/actions/application";
+import { appReducer } from "../../../../../../store/reducers";
+import { GlobalState } from "../../../../../../store/reducers/types";
+import { renderScreenFakeNavRedux } from "../../../../../../utils/testWrapper";
+import WALLET_ONBOARDING_COBADGE_ROUTES from "../../navigation/routes";
 import CoBadgeChooseType from "../CoBadgeChooseType";
 
-const getComponent = (onAddPaymentMethod: () => void) => {
-  const mockStore = configureMockStore();
+jest.mock("react-native-share", () => ({
+  open: jest.fn()
+}));
+describe("CoBadgeChooseType component", () => {
+  beforeEach(() => jest.useFakeTimers());
+  it("should dispatch navigateToWalletAddCreditCard action if press the enabled item", () => {
+    const { component, store } = getComponent(undefined, 1);
+    const enabledItem = component.queryByTestId("enabledItem");
 
-  const store: ReturnType<typeof mockStore> = mockStore();
-  return render(
-    <Provider store={store}>
-      <CoBadgeChooseType />
-    </Provider>
+    expect(component).not.toBeNull();
+    expect(enabledItem).not.toBeNull();
+
+    const expectedPayload = [
+      {
+        type: NavigationActions.BACK,
+        key: undefined
+      },
+      {
+        type: NavigationActions.NAVIGATE,
+        routeName: ROUTES.WALLET_ADD_CARD,
+        params: { inPayment: none }
+      }
+    ];
+
+    if (enabledItem) {
+      fireEvent.press(enabledItem);
+      expect(store.getActions()).toEqual(expectedPayload);
+    }
+  });
+  it("should dispatch walletAddCoBadgeStart action if press disabled or unknown item", () => {
+    const anAbi = "1234";
+    const { component, store } = getComponent(anAbi, 1);
+    const disabledItem = component.queryByTestId("disabledItem");
+    const unknownItem = component.queryByTestId("unknownItem");
+
+    expect(disabledItem).not.toBeNull();
+    expect(unknownItem).not.toBeNull();
+
+    const expectedPayload = {
+      type: WALLET_ONBOARDING_COBADGE_ROUTES.START,
+      payload: anAbi
+    };
+    if (disabledItem) {
+      fireEvent.press(disabledItem);
+      expect(store.getActions()).toEqual([expectedPayload]);
+    }
+    if (unknownItem) {
+      fireEvent.press(unknownItem);
+      expect(store.getActions()).toEqual([expectedPayload, expectedPayload]);
+    }
+  });
+});
+
+const getComponent = (abi?: string, legacyAddCreditCardBack?: number) => {
+  const globalState = appReducer(undefined, applicationChangeState("active"));
+
+  const ToBeTested: React.FunctionComponent<React.ComponentProps<
+    typeof CoBadgeChooseType
+  >> = (props: React.ComponentProps<typeof CoBadgeChooseType>) => (
+    <CoBadgeChooseType {...props} />
   );
+  const mockStore = configureMockStore<GlobalState>();
+  const store: ReturnType<typeof mockStore> = mockStore({
+    ...globalState
+  } as GlobalState);
+
+  return {
+    component: renderScreenFakeNavRedux<GlobalState, NavigationParams>(
+      ToBeTested,
+      ROUTES.WALLET_BPAY_DETAIL,
+      { abi, legacyAddCreditCardBack },
+      store
+    ),
+    store
+  };
 };
