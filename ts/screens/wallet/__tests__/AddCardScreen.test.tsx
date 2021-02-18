623d6e1e3bef8b91906da3fd247b0f5188a5ab7b
Simone <simone.biffi.91@gmail.com>
2021-02-18 15:11:23 +0100

    [#176980827] add AddCardScreen tests for cardHolder

--- ts/screens/wallet/__tests__/AddCardScreen.test.tsx
+++ ts/screens/wallet/__tests__/AddCardScreen.test.tsx
@@ -1,6 +1,7 @@
-import { none } from "fp-ts/lib/Option";
+import { none, some } from "fp-ts/lib/Option";
 import * as React from "react";
 import { createStore } from "redux";
+import { fireEvent } from "@testing-library/react-native";
 import I18n from "../../../i18n";
 import ROUTES from "../../../navigation/routes";
 import { applicationChangeState } from "../../../store/actions/application";
@@ -8,6 +9,7 @@ import { appReducer } from "../../../store/reducers";
 import { GlobalState } from "../../../store/reducers/types";
 import { renderScreenFakeNavRedux } from "../../../utils/testWrapper";
 import AddCardScreen, { NavigationParams } from "../AddCardScreen";
+import { isValidCardHolder } from "../../../utils/input";
 
 const mockPresentFn = jest.fn();
 jest.mock("../../../utils/bottomSheet", () => ({
@@ -20,6 +22,12 @@ jest.mock("react-native-share", () => ({
   open: jest.fn()
 }));
 
+const aValidCardHolder = "Mario Rossi";
+const aValidPan = "1234 5678 9123 4568";
+const aValidExpirationDate = "12/99";
+const aValidSecurityCode = "123";
+
+const anInvalidCardHolder = "Màriò Ròssì";
 describe("AddCardScreen", () => {
   beforeEach(() => jest.useFakeTimers());
   it("should show the continue button blocked if the aren't data", () => {
@@ -28,6 +36,70 @@ describe("AddCardScreen", () => {
       I18n.t("global.buttons.continue")
     );
     expect(continueButton).not.toBeNull();
+    expect(continueButton).toBeDisabled();
+  });
+
+  it("should show the continue button active if all the field are filled correctly", () => {
+    const component = getComponent();
+    const cardHolderInput = component.queryByTestId("cardHolderInput");
+    const panInputMask = component.queryByTestId("panInputMask");
+    const expirationDateInput = component.queryByTestId(
+      "expirationDateInputMask"
+    );
+    const securityCodeInput = component.queryByTestId("securityCodeInputMask");
+    const continueButton = component.queryByText(
+      I18n.t("global.buttons.continue")
+    );
+
+    expect(cardHolderInput).not.toBeNull();
+    expect(panInputMask).not.toBeNull();
+    expect(expirationDateInput).not.toBeNull();
+    expect(securityCodeInput).not.toBeNull();
+
+    if (
+      cardHolderInput &&
+      panInputMask &&
+      expirationDateInput &&
+      securityCodeInput
+    ) {
+      fireEvent.changeText(panInputMask, aValidPan);
+      fireEvent.changeText(expirationDateInput, aValidExpirationDate);
+      fireEvent.changeText(securityCodeInput, aValidSecurityCode);
+      fireEvent.changeText(cardHolderInput, aValidCardHolder);
+    }
+
+    expect(continueButton).not.toBeDisabled();
+  });
+  it("should show the continue button blocked if all the cardHolder is invalid", () => {
+    const component = getComponent();
+    const cardHolderInput = component.queryByTestId("cardHolderInput");
+    const panInputMask = component.queryByTestId("panInputMask");
+    const expirationDateInput = component.queryByTestId(
+      "expirationDateInputMask"
+    );
+    const securityCodeInput = component.queryByTestId("securityCodeInputMask");
+    const continueButton = component.queryByText(
+      I18n.t("global.buttons.continue")
+    );
+
+    if (
+      cardHolderInput &&
+      panInputMask &&
+      expirationDateInput &&
+      securityCodeInput
+    ) {
+      fireEvent.changeText(panInputMask, aValidPan);
+      fireEvent.changeText(expirationDateInput, aValidExpirationDate);
+      fireEvent.changeText(securityCodeInput, aValidSecurityCode);
+      fireEvent.changeText(cardHolderInput, anInvalidCardHolder);
+    }
+
+    const errorMessage = component.queryByText(
+      I18n.t("wallet.dummyCard.labels.holder.description.error")
+    );
+
+    expect(isValidCardHolder(some(anInvalidCardHolder))).toBeFalsy();
+    expect(errorMessage).not.toBeNull();
     expect(continueButton).toBeDisabled();
   });
 });
