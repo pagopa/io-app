e0472120f2ba1dfe3affea2ef5e5e985ad6f75d1
Simone <simone.biffi.91@gmail.com>
2021-02-22 22:17:43 +0100

    [#176981284] move success components outside OutcomeCodeMessageComponent

--- ts/components/wallet/OutcomeCodeMessageComponent.tsx
+++ ts/components/wallet/OutcomeCodeMessageComponent.tsx
@@ -11,58 +11,39 @@ import FooterWithButtons from "../ui/FooterWithButtons";
 import { cancelButtonProps } from "../../features/bonus/bonusVacanze/components/buttons/ButtonConfigurations";
 import { OutcomeCode } from "../../types/outcomeCode";
 
-type PossibleFlow = "addCreditCard" | "payment";
 type OwnProp = {
   outcomeCode: OutcomeCode;
+  successComponent: React.FC;
+  successFooter: React.FC;
   onClose: () => void;
-  onSuccess: () => void;
-  flow: PossibleFlow;
 };
 type Props = OwnProp;
 
 const blockingFooterWithButton = (onClose: () => void) => (
   <FooterWithButtons
     type={"SingleButton"}
-    leftButton={cancelButtonProps(onClose, "Chiudi")}
+    leftButton={cancelButtonProps(
+      onClose,
+      I18n.t("wallet.outcomeMessage.cta.close")
+    )}
   />
 );
 
-const successFooterWithButton = (onSuccess: () => void) => (
-  <FooterWithButtons
-    type={"SingleButton"}
-    leftButton={cancelButtonProps(onSuccess, "Visualizza carta")}
-  />
-);
-
-const loadSuccessLocales = (flow: PossibleFlow) => {
-  switch (flow) {
-    case "addCreditCard":
-      return {
-        title: "pippo",
-        description: "",
-        image: ""
-      };
-    case "payment":
-      return {
-        title: "",
-        description: "",
-        image: ""
-      };
-  }
-};
 /**
  * This component renders the message associated to the outcome code of a payment.
- * This component renders an image, a title and a description.
+ * This component renders an image, a title and a description or a success component.
  *
  * Since there are 3 possible case for the outcomeCode.status and the footer buttons are
  * related to every case the choice of the footer is based on the outcomeCode.status props.
  *
- * This component manages differently the success message for the addCreditCard or Payment state.
  * @param props
  */
 const OutcomeCodeMessageComponent: React.FC<Props> = (props: Props) => {
   const locale = I18n.currentLocale() === "en" ? "en-EN" : "it-IT";
-  const successLocales = loadSuccessLocales(props.flow);
+  const title = props.outcomeCode.title ? props.outcomeCode.title[locale] : "";
+  const description = props.outcomeCode.description
+    ? props.outcomeCode.description[locale]
+    : "";
   return (
     <BaseScreenComponent
       goBack={false}
@@ -71,28 +52,23 @@ const OutcomeCodeMessageComponent: React.FC<Props> = (props: Props) => {
     >
       <SafeAreaView style={IOStyles.flex} testID={"OutcomeCode"}>
         {props.outcomeCode.status === "success" ? (
-          <InfoScreenComponent
-            image={renderInfoRasterImage(
-              successLocales.image as ImageSourcePropType
-            )}
-            title={successLocales.title}
-            body={successLocales.description}
-          />
+          <>
+            <props.successComponent />
+            <props.successFooter />
+          </>
         ) : (
-          <InfoScreenComponent
-            image={renderInfoRasterImage(
-              props.outcomeCode.icon as ImageSourcePropType
-            )}
-            title={props.outcomeCode.title[locale]}
-            body={props.outcomeCode.description[locale]}
-          />
+          <>
+            <InfoScreenComponent
+              image={renderInfoRasterImage(
+                props.outcomeCode.icon as ImageSourcePropType
+              )}
+              title={title}
+              body={description}
+            />
+            {props.outcomeCode.status === "errorBlocking" &&
+              blockingFooterWithButton(props.onClose)}
+          </>
         )}
-        {props.onClose &&
-          props.outcomeCode.status === "errorBlocking" &&
-          blockingFooterWithButton(props.onClose)}
-        {props.onSuccess &&
-          props.outcomeCode.status === "success" &&
-          successFooterWithButton(props.onSuccess)}
       </SafeAreaView>
     </BaseScreenComponent>
   );
