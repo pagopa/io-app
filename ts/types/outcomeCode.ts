804af8d1fa9da28cb89da90afda394411ba6d24f
Simone <simone.biffi.91@gmail.com>
2021-02-22 15:52:03 +0100

    [#176981284] add types comments

--- ts/types/outcomeCode.ts
+++ ts/types/outcomeCode.ts
@@ -13,18 +13,25 @@ const Status = t.keyof({
 const OutcomeCode = t.type({
   title: LocalizedMessage,
   description: LocalizedMessage,
-  icon: t.string,
+  icon: t.unknown,
   status: Status
 });
 
+// This are the errors that we want to map specifically at the moment.
+// All the other errors will fall back in the generic error category.
 const OutcomeCodes = {
   "0": OutcomeCode,
   "1": OutcomeCode,
-  "2": OutcomeCode
+  "2": OutcomeCode,
+  "4": OutcomeCode,
+  "7": OutcomeCode,
+  "8": OutcomeCode,
+  "10": OutcomeCode
 };
 
 const OutcomeCodesCodec = t.type(OutcomeCodes);
 
+export type OutcomeStatus = t.TypeOf<typeof Status>;
 export type OutcomeCode = t.TypeOf<typeof OutcomeCode>;
 export type OutcomeCodes = t.TypeOf<typeof OutcomeCodesCodec>;
 export const OutcomeCodesKey = t.keyof(OutcomeCodes);
