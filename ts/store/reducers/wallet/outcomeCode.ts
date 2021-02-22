4532b655200dc463c7ad7da71b49ca45fc79e5e2
Simone <simone.biffi.91@gmail.com>
2021-02-22 15:52:33 +0100

    [#176981284] add outcome message data structure

--- ts/store/reducers/wallet/outcomeCode.ts
+++ ts/store/reducers/wallet/outcomeCode.ts
@@ -1,5 +1,5 @@
 import { Option } from "fp-ts/lib/Option";
-import { pot } from "italia-ts-commons";
+import * as pot from "italia-ts-commons/lib/pot";
 import { getType } from "typesafe-actions";
 import {
   OutcomeCode,
@@ -8,6 +8,10 @@ import {
 } from "../../../types/outcomeCode";
 import { Action } from "../../actions/types";
 import { outcomeCodeRetrieved } from "../../actions/wallet/outcomeCode";
+import authorizationDenied from "../../../../img/servicesStatus/error-detail-icon.png";
+import genericError from "../../../../img/wallet/errors/generic-error-icon.png";
+import sessionExpired from "../../../../img/wallet/errors/payment-expired-icon.png";
+import cardProblemOrOperationCanceled from "../../../../img/wallet/errors/payment-unknown-icon.png";
 import { GlobalState } from "../types";
 
 export type OutcomeCodeState = {
@@ -17,7 +21,8 @@ const initialOutcomeCodeState: OutcomeCodeState = {
   outcomeCode: pot.none
 };
 
-// TODO: make the outcomeCodes list remote
+// TODO: As refinement make the outcomeCodes list remote, like backend status.
+// This data structure replicates the future remote data structure.
 const OutcomeCodesPrintable: OutcomeCodes = {
   "0": {
     title: {
@@ -34,38 +39,92 @@ const OutcomeCodesPrintable: OutcomeCodes = {
   "1": {
     title: {
       "en-EN": "",
-      "it-IT": ""
+      "it-IT": "Spiacenti, si è verificato un errore imprevisto"
     },
     description: {
       "en-EN": "",
-      "it-IT": ""
+      "it-IT": "Non è stato addebitato alcun importo."
     },
-    icon: "",
+    icon: genericError,
     status: "errorBlocking"
   },
   "2": {
     title: {
       "en-EN": "",
-      "it-IT": ""
+      "it-IT": "Autorizzazione negata"
+    },
+    description: {
+      "en-EN": "",
+      "it-IT":
+        "La tua banca non ha autorizzato l’operazione. Controlla di aver inserito correttamente i vari codici richiesti dalla tua banca."
+    },
+    icon: authorizationDenied,
+    status: "errorBlocking"
+  },
+  "4": {
+    title: {
+      "en-EN": "",
+      "it-IT": "Spiacenti, la sessione è scaduta"
+    },
+    description: {
+      "en-EN": "",
+      "it-IT":
+        "Non è stato addebitato alcun importo. Per la tua sicurezza, hai a disposizione 5 minuti per completare l’operazione."
+    },
+    icon: sessionExpired,
+    status: "errorBlocking"
+  },
+  "7": {
+    title: {
+      "en-EN": "",
+      "it-IT": "C’è un problema con la tua carta"
+    },
+    description: {
+      "en-EN": "",
+      "it-IT":
+        "Non è stato addebitato alcun importo. Per maggiori informazioni, contatta la tua banca."
+    },
+    icon: cardProblemOrOperationCanceled,
+    status: "errorBlocking"
+  },
+  "8": {
+    title: {
+      "en-EN": "",
+      "it-IT": "L’operazione è stata annullata"
     },
     description: {
       "en-EN": "",
       "it-IT": ""
     },
-    icon: "",
+    icon: cardProblemOrOperationCanceled,
+    status: "errorBlocking"
+  },
+  "10": {
+    title: {
+      "en-EN": "",
+      "it-IT": "Autorizzazione negata"
+    },
+    description: {
+      "en-EN": "",
+      "it-IT":
+        "Probabilmente hai sforato il massimale della tua carta. Verifica con la tua banca prima di riprovare."
+    },
+    icon: authorizationDenied,
     status: "errorTryAgain"
   }
 };
+
+// This fallback is used both for unexpected code and for code that we don't want to map specifically.
 const fallbackOutcomeCodes: OutcomeCode = {
   title: {
     "en-EN": "fallback title",
-    "it-IT": "fallback title"
+    "it-IT": "Spiacenti, si è verificato un errore imprevisto"
   },
   description: {
     "en-EN": "fallback description",
-    "it-IT": "fallback description"
+    "it-IT": "Non è stato addebitato alcun importo."
   },
-  icon: "",
+  icon: genericError,
   status: "errorBlocking"
 };
 
