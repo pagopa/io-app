e36999ce0dd922bdce4e396baa0b74ab2efbe6e3
Simone <simone.biffi.91@gmail.com>
2021-02-22 10:37:42 +0100

    [#176981284] add outcome code message component

--- ts/components/wallet/OutcomeCodeMessageComponent.tsx
+++ ts/components/wallet/OutcomeCodeMessageComponent.tsx
@@ -1,17 +1,44 @@
-import { Option } from "fp-ts/lib/Option";
+import { pot } from "italia-ts-commons";
 import React from "react";
+import { ImageSourcePropType, SafeAreaView } from "react-native";
 import { connect } from "react-redux";
+import I18n from "../../i18n";
+import View from "../ui/TextWithIcon";
 import { GlobalState } from "../../store/reducers/types";
-import { OutcomeCodesKey } from "../../types/outcomeCode";
+import { lastPaymentOutcomeCodeSelector } from "../../store/reducers/wallet/outcomeCode";
+import { emptyContextualHelp } from "../../utils/emptyContextualHelp";
+import { IOStyles } from "../core/variables/IOStyles";
+import BaseScreenComponent from "../screens/BaseScreenComponent";
+import { InfoScreenComponent } from "../infoScreen/InfoScreenComponent";
+import { renderInfoRasterImage } from "../infoScreen/imageRendering";
 
-type OwnProps = {
-  outcomeCodeKey: Option<OutcomeCodesKey>;
+type Props = ReturnType<typeof mapStateToProps>;
+const OutcomeCodeMessageComponent: React.FC<Props> = (props: Props) => {
+  const locale = I18n.currentLocale() === "en" ? "en-EN" : "it-IT";
+  return (
+    <>
+      {pot.map(props.outcomeCode.outcomeCode, oC => {
+        return (
+          <BaseScreenComponent
+            goBack={false}
+            customGoBack={<View />}
+            contextualHelp={emptyContextualHelp}
+          >
+            <SafeAreaView style={IOStyles.flex} testID={"OutcomeCode"}>
+              <InfoScreenComponent
+                image={renderInfoRasterImage(oC.icon as ImageSourcePropType)}
+                title={oC.title[locale]}
+                body={oC.description[locale]}
+              />
+            </SafeAreaView>
+          </BaseScreenComponent>
+        );
+      })}
+    </>
+  );
 };
 
-type Props = OwnProps & ReturnType<typeof mapStateToProps>;
-const OutcomeCodeMessageComponent: React.FC<Props> = (props: Props) => <></>;
-
-const mapStateToProps = (state: GlobalState, props: OwnProps) => ({
-  sectionStatus: sectionStatusSelector(props.outcomeCodeKey)(state)
+const mapStateToProps = (state: GlobalState) => ({
+  outcomeCode: lastPaymentOutcomeCodeSelector(state)
 });
 export default connect(mapStateToProps)(OutcomeCodeMessageComponent);
