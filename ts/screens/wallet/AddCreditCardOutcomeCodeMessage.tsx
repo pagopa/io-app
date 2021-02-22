4edb5dccd068f96f08fd55edb6e13b701cf9fd03
Simone <simone.biffi.91@gmail.com>
2021-02-22 23:14:05 +0100

    [#176981284] refine AddCreditCardOutcomeCodeMessage

--- ts/screens/wallet/AddCreditCardOutcomeCodeMessage.tsx
+++ ts/screens/wallet/AddCreditCardOutcomeCodeMessage.tsx
@@ -2,14 +2,57 @@ import React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import * as pot from "italia-ts-commons/lib/pot";
+import { NavigationInjectedProps } from "react-navigation";
+import I18n from "../../i18n";
 import { GlobalState } from "../../store/reducers/types";
 import { lastPaymentOutcomeCodeSelector } from "../../store/reducers/wallet/outcomeCode";
-import { navigateToWalletHome } from "../../store/actions/navigation";
+import {
+  navigateToWalletHome,
+  navigateToWalletTransactionsScreen
+} from "../../store/actions/navigation";
 import OutcomeCodeMessageComponent from "../../components/wallet/OutcomeCodeMessageComponent";
+import { InfoScreenComponent } from "../../components/infoScreen/InfoScreenComponent";
+import { renderInfoRasterImage } from "../../components/infoScreen/imageRendering";
+import paymentCompleted from "../../../img/pictograms/payment-completed.png";
+import { FooterTwoButtons } from "../../features/bonus/bonusVacanze/components/markdown/FooterTwoButtons";
+import { Wallet } from "../../types/pagopa";
 
-type Props = ReturnType<typeof mapStateToProps> &
+type NavigationParams = Readonly<{
+  selectedWallet: Wallet;
+}>;
+
+type OwnProps = NavigationInjectedProps<NavigationParams>;
+type Props = OwnProps &
+  ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
+
+const successComponent = () => (
+  <InfoScreenComponent
+    image={renderInfoRasterImage(paymentCompleted)}
+    title={I18n.t("wallet.outcomeMessage.addCreditCard.success.title")}
+  />
+);
+
+const successFooter = (onVisualize: () => void, onClose: () => void) => (
+  <FooterTwoButtons
+    onCancel={onClose}
+    onRight={onVisualize}
+    leftText={I18n.t("wallet.outcomeMessage.cta.close")}
+    rightText={I18n.t("wallet.outcomeMessage.cta.visualizeCard")}
+  />
+);
+
+/**
+ * This is the wrapper component which takes care of showing the outcome message after that
+ * a user try to add a card.
+ * The component expects the action outcomeCodeRetrieved to be dispatched before being rendered,
+ * so the pot.none case is not taken into account.
+ *
+ * If the outcome code is of type success the render a two buttons footer that allow the user to go to the wallet home
+ * or to go to the added card details.
+ */
 const AddCreditCardOutcomeCodeMessage: React.FC<Props> = (props: Props) => {
+  const selectedWallet = props.navigation.getParam("selectedWallet");
   const outcomeCode = pot.isSome(props.outcomeCode.outcomeCode)
     ? props.outcomeCode.outcomeCode.value
     : false;
@@ -18,14 +61,21 @@ const AddCreditCardOutcomeCodeMessage: React.FC<Props> = (props: Props) => {
     <OutcomeCodeMessageComponent
       outcomeCode={outcomeCode}
       onClose={props.navigateToWalletHome}
-      onSuccess={() => true}
-      flow="addCreditCard"
+      successComponent={successComponent}
+      successFooter={() =>
+        successFooter(
+          () => props.navigateToCreditCardDetails(selectedWallet),
+          props.navigateToWalletHome
+        )
+      }
     />
   ) : null;
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  navigateToWalletHome: () => dispatch(navigateToWalletHome())
+  navigateToWalletHome: () => dispatch(navigateToWalletHome()),
+  navigateToCreditCardDetails: (selectedWallet: Wallet) =>
+    dispatch(navigateToWalletTransactionsScreen({ selectedWallet }))
 });
 
 const mapStateToProps = (state: GlobalState) => ({
