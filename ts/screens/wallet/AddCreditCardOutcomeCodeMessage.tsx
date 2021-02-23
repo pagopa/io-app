05694d9c6f4ab8d247027df59548ab24b369b39c
Simone <simone.biffi.91@gmail.com>
2021-02-23 16:31:28 +0100

    [#176981284] fix lint error

--- ts/screens/wallet/AddCreditCardOutcomeCodeMessage.tsx
+++ ts/screens/wallet/AddCreditCardOutcomeCodeMessage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import * as pot from "italia-ts-commons/lib/pot";
 import { NavigationInjectedProps } from "react-navigation";
 import I18n from "../../i18n";
 import { GlobalState } from "../../store/reducers/types";
