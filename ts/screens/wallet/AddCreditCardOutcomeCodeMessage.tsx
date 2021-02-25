47f768b6d4defc4f8df704f93496dd907e895c1c
Simone <simone.biffi.91@gmail.com>
2021-02-25 09:53:16 +0100

    [#176981159] remove footer buttons for the addCreditCardOutcomeCodeMessage component

--- ts/screens/wallet/AddCreditCardOutcomeCodeMessage.tsx
+++ ts/screens/wallet/AddCreditCardOutcomeCodeMessage.tsx
@@ -13,7 +13,6 @@ import OutcomeCodeMessageComponent from "../../components/wallet/OutcomeCodeMess
 import { InfoScreenComponent } from "../../components/infoScreen/InfoScreenComponent";
 import { renderInfoRasterImage } from "../../components/infoScreen/imageRendering";
 import paymentCompleted from "../../../img/pictograms/payment-completed.png";
-import { FooterTwoButtons } from "../../features/bonus/bonusVacanze/components/markdown/FooterTwoButtons";
 import { Wallet } from "../../types/pagopa";
 
 type NavigationParams = Readonly<{
@@ -32,26 +31,14 @@ const successComponent = () => (
   />
 );
 
-const successFooter = (onVisualize: () => void, onClose: () => void) => (
-  <FooterTwoButtons
-    onCancel={onClose}
-    onRight={onVisualize}
-    leftText={I18n.t("wallet.outcomeMessage.cta.close")}
-    rightText={I18n.t("wallet.outcomeMessage.cta.visualizeCard")}
-  />
-);
-
 /**
  * This is the wrapper component which takes care of showing the outcome message after that
  * a user tries to add a card to his/her wallet.
  * The component expects the action outcomeCodeRetrieved to be dispatched before being rendered,
  * so the pot.none case is not taken into account.
  *
- * If the outcome code is of type success the render a two buttons footer that allow the user to go to the wallet home
- * or to go to the added card details.
  */
 const AddCreditCardOutcomeCodeMessage: React.FC<Props> = (props: Props) => {
-  const selectedWallet = props.navigation.getParam("selectedWallet");
   const outcomeCode = props.outcomeCode.outcomeCode.fold(undefined, oC => oC);
 
   return outcomeCode ? (
@@ -59,12 +46,7 @@ const AddCreditCardOutcomeCodeMessage: React.FC<Props> = (props: Props) => {
       outcomeCode={outcomeCode}
       onClose={props.navigateToWalletHome}
       successComponent={successComponent}
-      successFooter={() =>
-        successFooter(
-          () => props.navigateToCreditCardDetails(selectedWallet),
-          props.navigateToWalletHome
-        )
-      }
+      successFooter={() => <></>}
     />
   ) : null;
 };
