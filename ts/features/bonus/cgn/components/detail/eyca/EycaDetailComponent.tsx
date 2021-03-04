c59721a9be1bf1f4d5c78ac5e300d8a7ef47d7c3
CrisTofani <cri.tofani@gmail.com>
2021-03-04 18:12:16 +0100

    [#177163732] Implements EYCA informations bottom sheet

--- ts/features/bonus/cgn/components/detail/eyca/EycaDetailComponent.tsx
+++ ts/features/bonus/cgn/components/detail/eyca/EycaDetailComponent.tsx
@@ -9,26 +9,41 @@ import { EycaCard } from "../../../../../../../definitions/cgn/EycaCard";
 import EycaStatusDetailsComponent from "./EycaStatusDetailsComponent";
 import EycaPendingComponent from "./EycaPendingComponent";
 import EycaErrorComponent from "./EycaErrorComponent";
+import { useEycaInformationBottomSheet } from "./EycaInformationComponent";
 
 type Props = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
 
 const EycaDetailComponent: React.FunctionComponent<Props> = (props: Props) => {
+  const { present } = useEycaInformationBottomSheet();
+
+  const openEycaBottomSheet = async () => {
+    await present();
+  };
+
   const renderComponentEycaStatus = (eyca: EycaCard) => {
     switch (eyca.status) {
       case "ACTIVATED":
       case "REVOKED":
       case "EXPIRED":
-        return <EycaStatusDetailsComponent eycaCard={eyca} />;
+        return (
+          <EycaStatusDetailsComponent
+            eycaCard={eyca}
+            openBottomSheet={openEycaBottomSheet}
+          />
+        );
       case "PENDING":
-        return <EycaPendingComponent />;
+        return <EycaPendingComponent openBottomSheet={openEycaBottomSheet} />;
       default:
         return <></>;
     }
   };
 
   return fromNullable(props.eyca).fold(
-    <EycaErrorComponent onRetry={props.requestEycaActivation} />,
+    <EycaErrorComponent
+      onRetry={props.requestEycaActivation}
+      openBottomSheet={openEycaBottomSheet}
+    />,
     renderComponentEycaStatus
   );
 };
---
+++ ts/features/bonus/cgn/components/detail/eyca/EycaInformationComponent.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { View } from "native-base";
+import Markdown from "../../../../../../components/ui/Markdown";
+import I18n from "../../../../../../i18n";
+import { useIOBottomSheet } from "../../../../../../utils/bottomSheet";
+import { openWebUrl } from "../../../../../../utils/url";
+import ButtonDefaultOpacity from "../../../../../../components/ButtonDefaultOpacity";
+import { Label } from "../../../../../../components/core/typography/Label";
+
+const EYCA_DISCOUNTS_URL = "https://www.eyca.org/discounts/it";
+
+const EycaInformationComponent: React.FunctionComponent = () => (
+  <View>
+    <View spacer />
+    <View style={{ flex: 1 }}>
+      <Markdown avoidTextSelection>
+        {I18n.t("bonus.cgn.detail.status.eycaDescription")}
+      </Markdown>
+      <View spacer large />
+      <ButtonDefaultOpacity
+        style={{ width: "100%" }}
+        bordered
+        onPress={() => openWebUrl(EYCA_DISCOUNTS_URL)}
+      >
+        <Label color={"blue"}>
+          {I18n.t("bonus.cgn.detail.cta.eyca.bottomSheet")}
+        </Label>
+      </ButtonDefaultOpacity>
+    </View>
+  </View>
+);
+
+export const useEycaInformationBottomSheet = () =>
+  useIOBottomSheet(
+    <EycaInformationComponent />,
+    I18n.t("bonus.cgn.detail.status.eycaBottomSheetTitle"),
+    420
+  );
