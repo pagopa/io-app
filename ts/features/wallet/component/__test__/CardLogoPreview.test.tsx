0b74b0f039ee4c69891776c4046b7bbaf13b5419
Simone <simone.biffi.91@gmail.com>
2021-03-08 22:28:36 +0100

    [#177145461] add CardLogoPreview tests

---
+++ ts/features/wallet/component/__test__/CardLogoPreview.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render } from "@testing-library/react-native";
+import { View } from "native-base";
+import * as React from "react";
+import { CardLogoPreview } from "../CardLogoPreview";
+import anImage from "../../../../../img/wallet/cards-icons/bPay.png";
+
+describe("CardLogoPreview", () => {
+  it("should show the left component, the image and when press should run the onPress", () => {
+    const left = () => <View testID="leftComponent"></View>;
+    const onPress = jest.fn();
+    const component = render(
+      <CardLogoPreview left={left()} image={anImage} onPress={onPress} />
+    );
+    const cardImage = component.queryByTestId("cardImage");
+    const cardPreview = component.queryByTestId("cardPreview");
+
+    expect(component.queryByTestId("leftComponent")).not.toBeNull();
+    expect(cardImage).not.toBeNull();
+    expect(cardImage).toHaveProp("source", anImage);
+
+    if (cardPreview !== null) {
+      fireEvent.press(cardPreview);
+      expect(onPress).toHaveBeenCalledTimes(1);
+    }
+  });
+});
