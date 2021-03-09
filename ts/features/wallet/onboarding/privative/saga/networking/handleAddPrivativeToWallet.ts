d9b0e08cde97a944216adeadc704e24aaab33a40
Fabrizio <fartux@gmail.com>
2021-03-09 15:29:49 +0100

    [#177142050] add privative saga handling

--- ts/features/wallet/onboarding/privative/saga/networking/handleAddPrivativeToWallet.ts
+++ ts/features/wallet/onboarding/privative/saga/networking/handleAddPrivativeToWallet.ts
@@ -1,13 +1,29 @@
+import { call, put } from "redux-saga/effects";
 import { ActionType } from "typesafe-actions";
 import { PaymentManagerClient } from "../../../../../../api/pagopa";
 import { PaymentManagerToken } from "../../../../../../types/pagopa";
 import { SessionManager } from "../../../../../../utils/SessionManager";
+import { addCobadgeToWallet } from "../../../cobadge/saga/networking/addCobadgeToWallet";
 import { addPrivativeToWallet } from "../../store/actions";
 
 export function* handleAddPrivativeToWallet(
-  addCobadgeToWallet: ReturnType<
+  addCobadgeToWalletClient: ReturnType<
     typeof PaymentManagerClient
   >["addCobadgeToWallet"],
   sessionManager: SessionManager<PaymentManagerToken>,
   addAction: ActionType<typeof addPrivativeToWallet.request>
-) {}
+) {
+  // get the results
+  const result = yield call(
+    addCobadgeToWallet,
+    addCobadgeToWalletClient,
+    sessionManager,
+    addAction.payload
+  );
+  // dispatch the related action
+  if (result.isRight()) {
+    yield put(addPrivativeToWallet.success(result.value));
+  } else {
+    yield put(addPrivativeToWallet.failure(result.value));
+  }
+}
