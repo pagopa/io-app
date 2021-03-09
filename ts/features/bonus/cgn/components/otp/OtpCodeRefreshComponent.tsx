e02ae8ab14a374f2a1e923bb8c960b7416163e93
Matteo Boschi <mbmaken@gmail.com>
2021-03-09 19:18:44 +0100

    [#176959374] wip

--- ts/features/bonus/cgn/components/otp/OtpCodeRefreshComponent.tsx
+++ ts/features/bonus/cgn/components/otp/OtpCodeRefreshComponent.tsx
@@ -1,31 +1,85 @@
-import React from "react";
-import { Platform, StyleSheet, View } from "react-native";
-import { Text } from "native-base";
+import React, { useEffect, useState } from "react";
+import { Animated, Easing, StyleSheet, View } from "react-native";
 import { Otp } from "../../../../../../definitions/cgn/Otp";
 import { IOColors } from "../../../../../components/core/variables/IOColors";
-import { Monospace } from "../../../../../components/core/typography/Monospace";
 import { addEvery } from "../../../../../utils/strings";
-import { makeFontStyleObject } from "../../../../../theme/fonts";
+import { BaseTypography } from "../../../../../components/core/typography/BaseTypography";
+import CopyButtonComponent from "../../../../../components/CopyButtonComponent";
 
 type Props = {
   otp: Otp;
+  onEnd: () => void;
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: IOColors.white
+    backgroundColor: IOColors.white,
+    flex: 1
   },
-  optCode: {
-    fontSize: 20,
-    padding: 4,
-    textAlign: "center",
-    ...makeFontStyleObject(Platform.select, "600", undefined, "RobotoMono")
-  }
+  otpContainer: {
+    justifyContent: "center",
+    flexDirection: "row"
+  },
+  optCode: { fontSize: 20, padding: 4, marginRight: 4, textAlign: "center" }
 });
 
-export const OtpCodeRefreshComponent = (props: Props) => (
-  <View style={styles.container}>
-    <Text style={styles.optCode}>{addEvery(props.otp.code, " ", 3)}</Text>
-  </View>
+const OtpCodeComponent = (code: string) => (
+  <BaseTypography color={"blue"} weight={"Regular"} fontStyle={styles.optCode}>
+    {code}
+  </BaseTypography>
 );
+
+export const OtpCodeRefreshComponent = (props: Props) => {
+  const formattedCode = addEvery(props.otp.code, " ", 3);
+  const [translateX] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(translateX, {
+      useNativeDriver: false,
+      toValue: 100,
+      duration: 3000,
+      easing: Easing.linear
+    }).start(() => {
+      translateX.setValue(0);
+      props.onEnd();
+    });
+  }, [props.otp.code]);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.otpContainer}>
+        {OtpCodeComponent(formattedCode)}
+        <CopyButtonComponent textToCopy={formattedCode} />
+      </View>
+      <View
+        style={{
+          alignSelf: "center",
+          marginBottom: 10,
+          backgroundColor: "#d9d9d9",
+          width: "90%",
+          height: 6,
+          margin: 4,
+          borderRadius: 4
+        }}
+      >
+        <Animated.View
+          style={[
+            {
+              alignSelf: "flex-start",
+              backgroundColor: IOColors.blue,
+              width: "50%",
+              height: 6,
+              borderRadius: 4
+            },
+            {
+              width: translateX.interpolate({
+                inputRange: [0, 100],
+                outputRange: ["0%", "100%"]
+              })
+            }
+          ]}
+        />
+      </View>
+    </View>
+  );
+};
