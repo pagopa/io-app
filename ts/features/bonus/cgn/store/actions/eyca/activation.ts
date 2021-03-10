3220a261d3860d9a8ffb662105e9032ec9a004d6
CrisTofani <cri.tofani@gmail.com>
2021-03-10 12:28:50 +0100

    [#177063605] Fixes on activation action file

--- ts/features/bonus/cgn/store/actions/eyca/activation.ts
+++ ts/features/bonus/cgn/store/actions/eyca/activation.ts
@@ -3,7 +3,6 @@ import {
   createAsyncAction,
   createStandardAction
 } from "typesafe-actions";
-import { NetworkError } from "../../../../../../utils/errors";
 import { EycaActivationDetail } from "../../../../../../../definitions/cgn/EycaActivationDetail";
 import { CgnEycaActivationProgressEnum } from "../../reducers/eyca/activation";
 
