00bc6c8aa764b1e4b68a31647fa93df24a5aad71
Simone <simone.biffi.91@gmail.com>
2021-03-12 11:25:33 +0100

    [#177243713] add privative kotimeout

--- ts/features/wallet/onboarding/privative/screens/search/ko/PrivativeKoTimeout.tsx
+++ ts/features/wallet/onboarding/privative/screens/search/ko/PrivativeKoTimeout.tsx
@@ -1,13 +1,42 @@
 import { View } from "native-base";
 import * as React from "react";
+import { SafeAreaView } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { isNone } from "fp-ts/lib/Option";
+import I18n from "../../../../../../../i18n";
+import { InfoScreenComponent } from "../../../../../../../components/infoScreen/InfoScreenComponent";
 import { GlobalState } from "../../../../../../../store/reducers/types";
+import { FooterTwoButtons } from "../../../../../../bonus/bonusVacanze/components/markdown/FooterTwoButtons";
 import BaseScreenComponent from "../../../../../../../components/screens/BaseScreenComponent";
+import { useHardwareBackButton } from "../../../../../../bonus/bonusVacanze/components/hooks/useHardwareBackButton";
+import { emptyContextualHelp } from "../../../../../../../utils/emptyContextualHelp";
+import { IOStyles } from "../../../../../../../components/core/variables/IOStyles";
+import image from "../../../../../../../../img/servicesStatus/error-detail-icon.png";
+import { renderInfoRasterImage } from "../../../../../../../components/infoScreen/imageRendering";
+import {
+  searchUserPrivative,
+  walletAddPrivativeCancel
+} from "../../../store/actions";
+import {
+  onboardingSearchedPrivativeSelector,
+  SearchedPrivativeData
+} from "../../../store/reducers/searchedPrivative";
+import { toPrivativeQuery } from "../SearchPrivativeCardScreen";
+import { showToast } from "../../../../../../../utils/showToast";
+import { mixpanelTrack } from "../../../../../../../mixpanel";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps> &
   Pick<React.ComponentProps<typeof BaseScreenComponent>, "contextualHelp">;
+
+const loadLocales = () => ({
+  headerTitle: I18n.t("wallet.onboarding.privative.headerTitle"),
+  title: I18n.t("wallet.onboarding.privative.search.koTimeout.title"),
+  body: I18n.t("wallet.onboarding.privative.search.koTimeout.body"),
+  cancel: I18n.t("global.buttons.cancel"),
+  retry: I18n.t("global.buttons.retry")
+});
 
 /**
  * This screen informs the user that a timeout is occurred while searching the indicated privative card.
@@ -17,10 +46,55 @@ type Props = ReturnType<typeof mapDispatchToProps> &
  * @param props
  * @constructor
  */
-const PrivativeKoTimeout = (_: Props): React.ReactElement => <View />;
+const PrivativeKoTimeout = (props: Props): React.ReactElement | null => {
+  const { headerTitle, title, body, cancel, retry } = loadLocales();
 
-const mapDispatchToProps = (_: Dispatch) => ({});
+  useHardwareBackButton(() => {
+    props.cancel();
+    return true;
+  });
 
-const mapStateToProps = (_: GlobalState) => ({});
+  const privativeQueryParam = toPrivativeQuery(props.privativeSelected);
+  if (isNone(privativeQueryParam)) {
+    showToast(I18n.t("global.genericError"), "danger");
+    void mixpanelTrack("PRIVATIVE_NO_QUERY_PARAMS_ERROR");
+    props.cancel();
+    return null;
+  } else {
+    return (
+      <BaseScreenComponent
+        goBack={false}
+        customGoBack={<View />}
+        headerTitle={headerTitle}
+        contextualHelp={emptyContextualHelp}
+      >
+        <SafeAreaView style={IOStyles.flex} testID={"CoBadgeKoTimeout"}>
+          <InfoScreenComponent
+            image={renderInfoRasterImage(image)}
+            title={title}
+            body={body}
+          />
+          <FooterTwoButtons
+            type={"TwoButtonsInlineThird"}
+            onRight={() => props.retry(privativeQueryParam.value)}
+            onCancel={props.cancel}
+            rightText={retry}
+            leftText={cancel}
+          />
+        </SafeAreaView>
+      </BaseScreenComponent>
+    );
+  }
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  cancel: () => dispatch(walletAddPrivativeCancel()),
+  retry: (searchedPrivativeData: Required<SearchedPrivativeData>) =>
+    dispatch(searchUserPrivative.request(searchedPrivativeData))
+});
+
+const mapStateToProps = (state: GlobalState) => ({
+  privativeSelected: onboardingSearchedPrivativeSelector(state)
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PrivativeKoTimeout);
