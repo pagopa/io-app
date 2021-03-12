065ee09b52e3aeb965cd02ac44cf2e0746336b54
Simone <simone.biffi.91@gmail.com>
2021-03-12 15:42:20 +0100

    feat(Bonus Pagamenti Digitali): [#177243713,#177144863,#177144964] Manages privative search loading and ko. (#2894)
    * [#177243713] add search prop
    
    * [#177243713] add data from selectors
    
    * [#177243713] wip
    
    * [#177243713] add SearchPrivativeCardScreen logic
    
    * [#177243713] add cancel prop
    
    * [#177243713] add missing contextual help props
    
    * [#177243713] add label for load message
    
    * [#177243713] add ko labels
    
    * [#177243713] add privative kotimeout
    
    * [#177243713] add cta ko not found label
    
    * [#177243713] add ko not found screen
    
    * [#177243713] fix lint error
    
    Co-authored-by: fabriziofff <fartux@gmail.com>

--- ts/features/wallet/onboarding/privative/screens/add/AddPrivativeCardScreen.tsx
+++ ts/features/wallet/onboarding/privative/screens/add/AddPrivativeCardScreen.tsx
@@ -3,9 +3,11 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { GlobalState } from "../../../../../../store/reducers/types";
+import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
-  ReturnType<typeof mapStateToProps>;
+  ReturnType<typeof mapStateToProps> &
+  Pick<React.ComponentProps<typeof BaseScreenComponent>, "contextualHelp">;;
 
 /**
  * This screen shows the user the private card found and allows him to add it to the wallet
