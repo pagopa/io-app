58b109825f9ec9c3a0f6c46f69153d8d177978eb
Simone <simone.biffi.91@gmail.com>
2021-03-15 11:25:18 +0100

    feat(Bonus Pagamenti Digitali): [#177145461] Show privative cards in wallet home (#2878)
    * [#177145461] add privative type
    
    * [#177145461] add enhancement for privative cards
    
    * [#177145461] wip privative
    
    * [#177145461] add privative type
    
    * [#177145461] enhance privative card
    
    * [#177145461] update CardInfo definition in Logo file
    
    * [#177145461] modify privative card logo return type
    
    * [#177145461] modify privative selector
    
    * [#177145461] Add the card layout preview component
    
    * [#177145461] use new CardLogoPreview component to favor composition
    
    * [#177145461] add PrivativeWalletPreview component
    
    * [#177145461] add unknown-gdo logo
    
    * [#177145461] add the privative cards to the wallet home
    
    * [#177145461] add placeholder PrivativeDetailScreen
    
    * [#177145461] add routes and action to go to PrivativeDetailScreen
    
    * [#177145461] use fallback image if there isn't the gdo logo
    
    * [#177145461] add CardLayoutPreview test
    
    * [#177145461] add CardLogoPreview tests
    
    * [#177145461] add PrivativeWalletPreview tests
    
    * [#177145461] add key to avoid image cache
    
    * [#177145461] update method type guards
    
    * [#177145461] add RawPrivativePaymentMethod type
    
    * [#177145461] modify gdo logo prop name and type guard
    
    * [#177145461] modify privative selector
    
    * [#177145461] modify privative paymentMethod utils
    
    * [#177145461] fix wrong naming
    
    * [#177145461] fix typo in comments
    
    * [#177145461] use shorter syntax for declare component in tests
    
    * [#177145461] modify tests description
    
    * [#177145461] add Privative kind in fromPatchedWalletV2ToRawPaymentMethod
    
    * [#177145461] return getPrivativeLoyaltyLogoUrl as ImageSourcePropType
    
    * [#177145461] update PrivativeWalletPreview
    
    * [#177145461] update tests
    
    * [#177145461] remove inline style
    
    * [#177145461] update naming
    
    * Update ts/features/wallet/privative/screen/PrivativeDetailScreen.tsx
    
    Co-authored-by: fabriziofff <fartux@gmail.com>
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

---
+++ ts/features/wallet/privative/component/PrivativeWalletPreview.tsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { Option } from "fp-ts/lib/Option";
+import {
+  Image,
+  ImageSourcePropType,
+  ImageStyle,
+  ImageURISource,
+  StyleProp,
+  StyleSheet
+} from "react-native";
+import { Body } from "../../../../components/core/typography/Body";
+import { navigateToPrivativeDetailScreen } from "../../../../store/actions/navigation";
+import { GlobalState } from "../../../../store/reducers/types";
+import { PrivativePaymentMethod } from "../../../../types/pagopa";
+import { IOStyles } from "../../../../components/core/variables/IOStyles";
+import { useImageResize } from "../../onboarding/bancomat/screens/hooks/useImageResize";
+import { CardLayoutPreview } from "../../component/CardLayoutPreview";
+import unknownGdo from "../../../../../img/wallet/unknown-gdo.png";
+
+type OwnProps = {
+  privative: PrivativePaymentMethod;
+};
+
+type Props = ReturnType<typeof mapDispatchToProps> &
+  ReturnType<typeof mapStateToProps> &
+  OwnProps;
+
+const BASE_IMG_W = 160;
+const BASE_IMG_H = 20;
+
+const styles = StyleSheet.create({
+  unknownLoyaltyLogo: {
+    width: 40,
+    height: 40,
+    resizeMode: "contain"
+  }
+});
+const fallbackLoyaltyLogo: React.ReactElement = (
+  <Image
+    source={unknownGdo}
+    style={styles.unknownLoyaltyLogo}
+    key={"unknownLoyaltyLogo"}
+    testID={"unknownLoyaltyLogo"}
+  />
+);
+
+const renderRight = (props: Props, size: Option<[number, number]>) =>
+  size.fold(fallbackLoyaltyLogo, imgDim => {
+    const imageUrl = props.privative.icon;
+
+    const imageStyle: StyleProp<ImageStyle> = {
+      width: imgDim[0],
+      height: imgDim[1],
+      resizeMode: "contain"
+    };
+    return (
+      <Image
+        source={imageUrl}
+        style={imageStyle}
+        key={"loyaltyLogo"}
+        testID={"loyaltyLogo"}
+      />
+    );
+  });
+
+/**
+ * Typeguard for handle backward compatibility of icon type
+ * @param image
+ */
+const isImageURISource = (
+  image: ImageSourcePropType
+): image is ImageURISource =>
+  typeof image !== "number" && (image as ImageURISource).uri !== undefined;
+
+/**
+ * A card preview for a privative card
+ * @param props
+ * @constructor
+ */
+const PrivativeWalletPreview: React.FunctionComponent<Props> = props => {
+  const rightElement = isImageURISource(props.privative.icon)
+    ? renderRight(
+        props,
+        useImageResize(BASE_IMG_W, BASE_IMG_H, props.privative.icon.uri)
+      )
+    : fallbackLoyaltyLogo;
+
+  return (
+    <CardLayoutPreview
+      left={
+        <Body style={IOStyles.flex} numberOfLines={1} testID={"caption"}>
+          {props.privative.caption}
+        </Body>
+      }
+      right={rightElement}
+      onPress={() => props.navigateToPrivativeDetails(props.privative)}
+    />
+  );
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  navigateToPrivativeDetails: (privative: PrivativePaymentMethod) =>
+    dispatch(navigateToPrivativeDetailScreen(privative))
+});
+
+const mapStateToProps = (_: GlobalState) => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PrivativeWalletPreview);
