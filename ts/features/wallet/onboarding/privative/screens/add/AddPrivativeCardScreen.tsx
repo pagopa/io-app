8e622cabf16934cb0844b487c78e92a21515d9d4
Simone <simone.biffi.91@gmail.com>
2021-03-17 17:12:39 +0100

    [#177144784] add AddPrivativeCardComponent

--- ts/features/wallet/onboarding/privative/screens/add/AddPrivativeCardScreen.tsx
+++ ts/features/wallet/onboarding/privative/screens/add/AddPrivativeCardScreen.tsx
@@ -1,27 +1,16 @@
-import { fromNullable } from "fp-ts/lib/Option";
-import { View } from "native-base";
+import { fromNullable, isSome } from "fp-ts/lib/Option";
 import * as React from "react";
-import { useEffect, useState } from "react";
-import { SafeAreaView } from "react-native";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { PaymentInstrument } from "../../../../../../../definitions/pagopa/walletv2/PaymentInstrument";
-import { Body } from "../../../../../../components/core/typography/Body";
-import { H1 } from "../../../../../../components/core/typography/H1";
-import { H3 } from "../../../../../../components/core/typography/H3";
-import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
-import FooterWithButtons from "../../../../../../components/ui/FooterWithButtons";
-import I18n from "../../../../../../i18n";
 import { GlobalState } from "../../../../../../store/reducers/types";
 import { emptyContextualHelp } from "../../../../../../utils/emptyContextualHelp";
 import {
-  cancelButtonProps,
-  confirmButtonProps
-} from "../../../../../bonus/bonusVacanze/components/buttons/ButtonConfigurations";
-import {
   fold,
-  getValueOrElse
+  getValueOrElse,
+  isReady
 } from "../../../../../bonus/bpd/model/RemoteValue";
 import {
   addPrivativeToWallet,
@@ -30,66 +19,41 @@ import {
 } from "../../store/actions";
 import { onboardingPrivativeAddingResultSelector } from "../../store/reducers/addingPrivative";
 import { onboardingPrivativeFoundSelector } from "../../store/reducers/foundPrivative";
+import LoadAddPrivativeCard from "./LoadAddPrivativeCard";
+import AddPrivativeCardComponent from "./AddPrivativeCardComponent";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps> &
   Pick<React.ComponentProps<typeof BaseScreenComponent>, "contextualHelp">;
-
-const AddPrivativeBody = (props: Props) => (
-  <>
-    <H1>TMP Privative card display screen</H1>
-    <Body>
-      Card Number:
-      {props.foundPrivative.fold("No card number", pm => pm.panPartialNumber)}
-    </Body>
-  </>
-);
 
 /**
  * This screen shows the user the private card found and allows him to add it to the wallet
  * @param props
  * @constructor
  */
-const AddPrivativeCardScreen = (props: Props): React.ReactElement => {
-  const [stateLabel, setStateLabel] = useState("undefined");
+const AddPrivativeCardScreen = (props: Props): React.ReactElement | null => {
   useEffect(() => {
-    fold(
-      props.addingResult,
-      () => setStateLabel(""),
-      () => setStateLabel("Adding to wallet, Loading..."),
-      _ => props.complete(),
-      error => setStateLabel(`Error! ${JSON.stringify(error)}`)
-    );
+    if (isReady(props.addingResult)) {
+      props.complete();
+    }
   }, [props.addingResult]);
 
-  return (
-    <BaseScreenComponent
-      goBack={true}
-      headerTitle={I18n.t("wallet.onboarding.privative.headerTitle")}
-      contextualHelp={emptyContextualHelp}
-    >
-      <SafeAreaView
-        style={IOStyles.flex}
-        testID={"ChoosePrivativeIssuerComponent"}
-      >
-        {/* TODO: Complete the component, this is a draft version for test purpose only */}
-        <View style={[IOStyles.horizontalContentPadding, IOStyles.flex]}>
-          <AddPrivativeBody {...props} />
-          <H3>StateLabel: {stateLabel}</H3>
-        </View>
-        <FooterWithButtons
-          type={"TwoButtonsInlineThird"}
-          leftButton={cancelButtonProps(
-            props.cancel,
-            I18n.t("global.buttons.skip")
-          )}
-          rightButton={confirmButtonProps(
-            () => props.foundPrivative.map(p => props.addToWallet(p)),
-            I18n.t("global.buttons.add")
-          )}
+  return fold(
+    props.addingResult,
+    () =>
+      isSome(props.foundPrivative) ? (
+        <AddPrivativeCardComponent
+          pan={props.foundPrivative.value}
+          handleSkip={props.cancel}
+          handleContinue={() =>
+            props.foundPrivative.map(p => props.addToWallet(p))
+          }
+          contextualHelp={emptyContextualHelp}
         />
-      </SafeAreaView>
-    </BaseScreenComponent>
+      ) : null,
+    () => <LoadAddPrivativeCard />,
+    _ => null,
+    _ => <LoadAddPrivativeCard />
   );
 };
 
