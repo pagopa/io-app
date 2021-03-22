ca17eb8202a3bab34dc08290528b89da4f85cb77
Simone <simone.biffi.91@gmail.com>
2021-03-22 18:02:55 +0100

    [#177434959] add add card button inside bottomsheet

--- ts/screens/wallet/AddCardScreen.tsx
+++ ts/screens/wallet/AddCardScreen.tsx
@@ -52,6 +52,10 @@ import { useIOBottomSheet } from "../../utils/bottomSheet";
 import { Body } from "../../components/core/typography/Body";
 import { CreditCard } from "../../types/pagopa";
 import { BlockButtonProps } from "../../components/ui/BlockButtons";
+import ButtonDefaultOpacity from "../../components/ButtonDefaultOpacity";
+import { walletAddCoBadgeStart } from "../../features/wallet/onboarding/cobadge/store/actions";
+import { Label } from "../../components/core/typography/Label";
+import { IOColors } from "../../components/core/variables/IOColors";
 
 type NavigationParams = Readonly<{
   inPayment: Option<{
@@ -90,7 +94,10 @@ const styles = StyleSheet.create({
     width: 16,
     flex: 0
   },
-
+  button: {
+    width: "100%",
+    borderColor: IOColors.blue
+  },
   whiteBg: {
     backgroundColor: variables.colorWhite
   }
@@ -141,10 +148,24 @@ const AddCardScreen: React.FC<Props> = props => {
     INITIAL_CARD_FORM_STATE
   );
 
-  const { present } = useIOBottomSheet(
-    <Body>{I18n.t("wallet.missingDataText")}</Body>,
+  const { present, dismiss } = useIOBottomSheet(
+    <>
+      <Body>{I18n.t("wallet.missingDataText.body")}</Body>
+      <View spacer={true} large />
+      <ButtonDefaultOpacity
+        style={styles.button}
+        bordered={true}
+        onPress={() => {
+          dismiss();
+          props.startAddCobadgeWorkflow();
+        }}
+        onPressWithGestureHandler={true}
+      >
+        <Label>{I18n.t("wallet.missingDataText.cta")}</Label>
+      </ButtonDefaultOpacity>
+    </>,
     I18n.t("wallet.missingDataCTA"),
-    260
+    300
   );
 
   const detectedBrand: SupportedBrand = CreditCardDetector.validate(
@@ -313,6 +334,7 @@ const AddCardScreen: React.FC<Props> = props => {
 const mapStateToProps = (_: GlobalState) => ({});
 
 const mapDispatchToProps = (dispatch: Dispatch, props: OwnProps) => ({
+  startAddCobadgeWorkflow: () => dispatch(walletAddCoBadgeStart(undefined)),
   addWalletCreditCardInit: () => dispatch(addWalletCreditCardInit()),
   navigateBack: () => dispatch(navigateBack()),
   navigateToConfirmCardDetailsScreen: (creditCard: CreditCard) =>
