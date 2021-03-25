ba4f76d08ce9ed686189979ef1490d36a7c601ab
Fabrizio <fartux@gmail.com>
2021-03-25 19:49:48 +0100

    [#177458092] add italic

--- scripts/changelog/ts/checkDanger.ts
+++ scripts/changelog/ts/checkDanger.ts
@@ -41,7 +41,7 @@ ${ticketList
   .map(
     s =>
       `  * ${renderTicket(s)}${
-        s.parent ? ` subtask of\n     * ${renderTicket(s.parent)}` : ""
+        s.parent ? ` _subtask of_\n     * ${renderTicket(s.parent)}` : ""
       }`
   )
   .join("\n")}\n`);
