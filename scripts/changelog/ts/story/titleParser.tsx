33f3b56d19980e0050b82b78cd4455c5d786cf43
Fabrizio <fartux@gmail.com>
2021-03-25 15:30:31 +0100

    [#177458092] fix

--- scripts/changelog/ts/story/titleParser.tsx
+++ scripts/changelog/ts/story/titleParser.tsx
@@ -6,34 +6,13 @@ import { Either, left, right } from "fp-ts/lib/Either";
 import { fromNullable, Option } from "fp-ts/lib/Option";
 import { Errors } from "io-ts";
 import { getJiraTickets } from "./jira";
-import { JiraKey } from "./jira/types";
-import { PivotalId } from "./pivotal/types";
 import {
   fromJiraToGenericTicket,
   fromPivotalToGenericTicket,
   GenericTicket
 } from "./types";
 
-const pivotalRegex = /\[(#\d+(,#\d+)*)]\s.+/;
 const jiraRegex = /\[([a-zA-Z]+-\d+(,[a-zA-Z]+-\d+)*)]\s.+/;
-
-/**
- * From a PR title extract the Pivotal IDs or Jira IDs (if any).
- * The title can have multiple IDs but they must all be of the same type
- * @param title
- */
-export const parsePrTitle = (
-  title: string
-): Option<ReadonlyArray<JiraKey> | ReadonlyArray<PivotalId>> => {
-  const maybePivotal = fromNullable(title.match(pivotalRegex)?.pop()).map(
-    p => p.replace("#", "").split(",") as Array<PivotalId>
-  );
-  const maybeJira = fromNullable(title.match(jiraRegex)?.pop()).map(
-    j => j.split(",") as Array<JiraKey>
-  );
-
-  return maybePivotal.isNone() ? maybeJira : maybePivotal;
-};
 
 export const prTitleContainsJiraIds = (title: string) =>
   title.match(jiraRegex) !== null;
