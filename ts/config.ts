1d9a34b5bff50897e8df86d857d64705925a16f8
Fabrizio <fartux@gmail.com>
2021-03-30 15:58:56 +0200

    [#177543860] add bpd transaction paging feature flag

--- ts/config.ts
+++ ts/config.ts
@@ -53,6 +53,9 @@ export const myPortalEnabled: boolean = Config.MYPORTAL_ENABLED === "YES";
 export const bpdEnabled: boolean = Config.BPD_ENABLED === "YES";
 export const bpdTestOverlay: boolean = Config.BPD_TEST_OVERLAY === "YES";
 
+export const bpdTransactionPaging: boolean =
+  Config.BPD_TRANSACTION_PAGING === "YES";
+
 export const bpdApiUrlPrefix: string = Config.BPD_API_URL_PREFIX;
 
 export const bpdApiSitUrlPrefix: string = Config.BPD_API_SIT;
