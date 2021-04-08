81c1374a2cbddc936403f91f525a664474ca7827
Andrea Favarò <andrea@bitrocket.dev>
2021-04-08 17:07:45 +0200

    [#177590245] Removed useless code

--- ts/components/AssistanceFormWebView.tsx
+++ ts/components/AssistanceFormWebView.tsx
@@ -1,16 +1,7 @@
 import { StyleSheet, View } from "react-native";
 import React, { createRef, FC, useState } from "react";
-import * as pot from "italia-ts-commons/lib/pot";
-import WebView, { WebViewMessageEvent } from "react-native-webview";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
-import { fromNullable } from "fp-ts/lib/Option";
-import { ServiceId } from "../../definitions/backend/ServiceId";
+import WebView from "react-native-webview";
 import { AVOID_ZOOM_JS, closeInjectedScript } from "../utils/webview";
-import { loadServiceDetail } from "../store/actions/services";
-import { GlobalState } from "../store/reducers/types";
-import { ServicePublic } from "../../definitions/backend/ServicePublic";
-import { servicesByIdSelector } from "../store/reducers/entities/services/servicesById";
 import { RefreshIndicator } from "./ui/RefreshIndicator";
 import GenericErrorComponent from "./screens/GenericErrorComponent";
 import { withLightModalContext } from "./helpers/withLightModalContext";
@@ -39,22 +30,8 @@ const styles = StyleSheet.create({
   }
 });
 
-type Props = {
-  onServiceSelect: (service: ServicePublic) => void;
-} & ReturnType<typeof mapDispatchToProps> &
-  ReturnType<typeof mapStateToProps>;
-
-/**
- * This component is basically a webview that loads an url showing local services
- * It intercepts the request of loading a service and it does:
- * - block that request from loading
- * - extract from the request url the service id
- * - load the selected service starting from the service id (load and error are handled)
- */
-const AssistanceForm: FC<Props> = ({ loadService, servicesById }) => {
-  const [serviceIdToLoad, setServiceIdToLoad] = useState<string | undefined>(
-    undefined
-  );
+/** This component is basically a webview that loads an url showing assistance form */
+const AssistanceForm: FC = () => {
   const [webViewError, setWebViewError] = useState(false);
   const webViewRef = createRef<WebView>();
 
@@ -73,26 +50,8 @@ const AssistanceForm: FC<Props> = ({ loadService, servicesById }) => {
 
   const onError = () => setWebViewError(true);
 
-  /**
-   * 'listen' on web message
-   * if a serviceId is sent: dispatch service loading request
-   * @param event
-   */
-  const handleWebviewMessage = (event: WebViewMessageEvent) => {
-    ServiceId.decode(event.nativeEvent.data).map(sId => {
-      setServiceIdToLoad(sId);
-      // request loading service
-      loadService(sId);
-    });
-  };
-
-  const isLoadingService = fromNullable(serviceIdToLoad)
-    .mapNullable(sid => servicesById[sid])
-    .fold(false, pot.isLoading);
-
   return (
     <>
-      {isLoadingService && renderLoading()}
       <WebView
         ref={webViewRef}
         injectedJavaScript={closeInjectedScript(AVOID_ZOOM_JS)}
@@ -100,7 +59,6 @@ const AssistanceForm: FC<Props> = ({ loadService, servicesById }) => {
         textZoom={100}
         source={{ uri: URI }}
         onError={onError}
-        onMessage={handleWebviewMessage}
         renderLoading={renderLoading}
         startInLoadingState
         javaScriptEnabled
@@ -114,16 +72,4 @@ const AssistanceForm: FC<Props> = ({ loadService, servicesById }) => {
   );
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  loadService: (serviceId: string) =>
-    dispatch(loadServiceDetail.request(serviceId))
-});
-
-const mapStateToProps = (state: GlobalState) => ({
-  servicesById: servicesByIdSelector(state)
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withLightModalContext(AssistanceForm));
+export default withLightModalContext(AssistanceForm);
