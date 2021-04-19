25cb7e2e942c11c35f1113ab93a77304ba12508a
Andrea Favarò <andrea@bitrocket.dev>
2021-04-19 17:25:04 +0200

    [#177560087] Added loading error component

--- ts/screens/wallet/payment/PickPspScreen.tsx
+++ ts/screens/wallet/payment/PickPspScreen.tsx
@@ -9,7 +9,6 @@ import { PaymentRequestsGetResponse } from "../../../../definitions/backend/Paym
 import { H5 } from "../../../components/core/typography/H5";
 import { H4 } from "../../../components/core/typography/H4";
 import { withLightModalContext } from "../../../components/helpers/withLightModalContext";
-import { withLoadingSpinner } from "../../../components/helpers/withLoadingSpinner";
 import ItemSeparatorComponent from "../../../components/ItemSeparatorComponent";
 import BaseScreenComponent, {
   ContextualHelpPropsMarkdown
@@ -29,6 +28,7 @@ import { navigateBack } from "../../../store/actions/navigation";
 import { IOStyles } from "../../../components/core/variables/IOStyles";
 import { PspComponent } from "../../../components/wallet/payment/PspComponent";
 import { cancelButtonProps } from "../../../features/bonus/bonusVacanze/components/buttons/ButtonConfigurations";
+import { LoadingErrorComponent } from "../../../features/bonus/bonusVacanze/components/loadingErrorScreen/LoadingErrorComponent";
 import { dispatchUpdatePspForWalletAndConfirm } from "./common";
 
 type NavigationParams = Readonly<{
@@ -100,45 +100,61 @@ class PickPspScreen extends React.Component<Props> {
         contextualHelpMarkdown={contextualHelpMarkdown}
         faqCategories={["payment"]}
       >
-        <SafeAreaView style={IOStyles.flex} testID="PickPspScreen">
-          <View spacer />
-          <View style={styles.padded}>
-            <H3>{I18n.t("wallet.pickPsp.title")}</H3>
-            <View spacer small />
-            <H4 weight="Regular" color="bluegreyDark">
-              {I18n.t("wallet.pickPsp.info")}
-            </H4>
-            <H4 weight="Regular" color="bluegreyDark">
-              {I18n.t("wallet.pickPsp.info2")}
-              <H4 color="bluegreyDark">{` ${I18n.t(
-                "wallet.pickPsp.info2Bold"
-              )}`}</H4>
-            </H4>
-          </View>
-          <View spacer />
-          <FlatList
-            testID="pspList"
-            ItemSeparatorComponent={() => <ItemSeparatorComponent />}
-            removeClippedSubviews={false}
-            data={availablePsps}
-            keyExtractor={item => item.id.toString()}
-            renderItem={({ item }) => (
-              <PspComponent
-                psp={item}
-                onPress={() => this.props.pickPsp(item.id, this.props.allPsps)}
-              />
-            )}
-            ListHeaderComponent={this.headerItem}
-            ListFooterComponent={() => <ItemSeparatorComponent />}
+        {this.props.isLoading || this.props.hasError ? (
+          <LoadingErrorComponent
+            isLoading={this.props.isLoading}
+            onRetry={() => {
+              const idWallet = this.props.navigation
+                .getParam("wallet")
+                .idWallet.toString();
+              const idPayment = this.props.navigation.getParam("idPayment");
+              this.props.loadAllPsp(idWallet, idPayment);
+            }}
+            loadingCaption={I18n.t("wallet.pickPsp.loadingPsps")}
           />
-          <FooterWithButtons
-            type="SingleButton"
-            leftButton={cancelButtonProps(
-              this.props.navigateBack,
-              I18n.t("global.buttons.back")
-            )}
-          />
-        </SafeAreaView>
+        ) : (
+          <SafeAreaView style={IOStyles.flex} testID="PickPspScreen">
+            <View spacer />
+            <View style={styles.padded}>
+              <H3>{I18n.t("wallet.pickPsp.title")}</H3>
+              <View spacer small />
+              <H4 weight="Regular" color="bluegreyDark">
+                {I18n.t("wallet.pickPsp.info")}
+              </H4>
+              <H4 weight="Regular" color="bluegreyDark">
+                {I18n.t("wallet.pickPsp.info2")}
+                <H4 color="bluegreyDark">{` ${I18n.t(
+                  "wallet.pickPsp.info2Bold"
+                )}`}</H4>
+              </H4>
+            </View>
+            <View spacer />
+            <FlatList
+              testID="pspList"
+              ItemSeparatorComponent={() => <ItemSeparatorComponent />}
+              removeClippedSubviews={false}
+              data={availablePsps}
+              keyExtractor={item => item.id.toString()}
+              renderItem={({ item }) => (
+                <PspComponent
+                  psp={item}
+                  onPress={() =>
+                    this.props.pickPsp(item.id, this.props.allPsps)
+                  }
+                />
+              )}
+              ListHeaderComponent={this.headerItem}
+              ListFooterComponent={() => <ItemSeparatorComponent />}
+            />
+            <FooterWithButtons
+              type="SingleButton"
+              leftButton={cancelButtonProps(
+                this.props.navigateBack,
+                I18n.t("global.buttons.back")
+              )}
+            />
+          </SafeAreaView>
+        )}
       </BaseScreenComponent>
     );
   }
@@ -149,6 +165,7 @@ const mapStateToProps = (state: GlobalState) => {
   return {
     isLoading:
       pot.isLoading(state.wallet.wallets.walletById) || pot.isLoading(psps),
+    hasError: pot.isError(state.wallet.wallets.walletById) || pot.isError(psps),
     allPsps: pot.getOrElse(psps, [])
   };
 };
@@ -180,4 +197,4 @@ const mapDispatchToProps = (dispatch: Dispatch, props: OwnProps) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(withLightModalContext(withLoadingSpinner(PickPspScreen)));
+)(withLightModalContext(PickPspScreen));
