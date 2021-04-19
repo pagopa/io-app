c6560ec8cb06ac540ec8295d637f86bc6b60e0c6
Fabrizio <fartux@gmail.com>
2021-04-19 17:44:30 +0200

    [#177543882] add paginated winning-transactions api

--- ts/features/bonus/bpd/api/winning-transactions/v2.ts
+++ ts/features/bonus/bpd/api/winning-transactions/v2.ts
@@ -1,14 +1,30 @@
+import { fromNullable } from "fp-ts/lib/Option";
 import {
+  findWinningTransactionsUsingGETDefaultDecoder,
+  FindWinningTransactionsUsingGETT,
   getCountByDayGETDefaultDecoder,
   GetCountByDayGETT
 } from "../../../../../../definitions/bpd/winning_transactions_v2/requestTypes";
 import { bpdHeadersProducers } from "../common";
 
-export const winningTransactionsV2: GetCountByDayGETT = {
+export const winningTransactionsV2CountByDayGET: GetCountByDayGETT = {
   method: "get",
   url: ({ awardPeriodId }) =>
     `/bpd/io/winning-transactions/v2/countbyday?awardPeriodId=${awardPeriodId}`,
   query: _ => ({}),
   headers: bpdHeadersProducers(),
   response_decoder: getCountByDayGETDefaultDecoder()
+};
+
+export const winningTransactionsV2GET: FindWinningTransactionsUsingGETT = {
+  method: "get",
+  url: ({ awardPeriodId, nextCursor }) =>
+    `/bpd/io/winning-transactions/v2?awardPeriodId=${awardPeriodId}${fromNullable(
+      nextCursor
+    )
+      .map(c => `&nextCursor=${c}`)
+      .getOrElse("")}`,
+  query: _ => ({}),
+  headers: bpdHeadersProducers(),
+  response_decoder: findWinningTransactionsUsingGETDefaultDecoder()
 };
