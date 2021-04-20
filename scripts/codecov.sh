c21b0d14984d96d3f57529c17d2be573a27c9533
Matteo Boschi <mbmaken@gmail.com>
2021-04-20 18:47:00 +0200

    [#177795910] bash script improvements

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -1,22 +1,22 @@
-#/bin/bash
+#!/bin/bash
 FILENAME=codecov
 curl -s https://codecov.io/bash > $FILENAME
 CODECOV_VERSION=$(grep 'VERSION=\".*\"' codecov | cut -d'"' -f2);
 VALIDATION_FAILURE=0
-for i in 1 256 512
-do
-  IS_DIFF=$(diff <(shasum -a $i $FILENAME) <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/$CODECOV_VERSION/SHA${i}SUM))
-  if [ -z "${IS_DIFF}" ]; then
-    echo "Sha:" $i "passes validation."
-  else
-    VALIDATION_FAILURE=1
-  fi
-done
+
+echo "codecov version: $CODECOV_VERSION"
+CODECOV_SHA=$(head -1 <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/$CODECOV_VERSION/SHA256SUM))
+COMPUTED_SHA=$(shasum -a 256 $FILENAME)
+if [ "$COMPUTED_SHA" = "$CODECOV_SHA" ]; then
+  echo "sha256 passes validation."
+else
+  VALIDATION_FAILURE=1
+fi
 if [ "${VALIDATION_FAILURE}" == 1 ]; then
-  echo "Invalid Checksum Detected From Codecov. Quitting."
+  echo "Invalid checksum detected from codecov. Aborting."
   exit 1
 else
-  echo "Starting Codecov."
+  echo "Starting codecov."
   chmod +x codecov
   ./codecov
 fi
