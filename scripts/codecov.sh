1390ab88c26ae19e1115ae80fcaf176f7323fad5
pp-ps <pietro.stroia@pagopa.it>
2021-04-20 19:21:21 +0200

    [#177795910] consistent style

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 FILENAME=codecov
-curl -s https://codecov.io/bash > $FILENAME
+curl -s https://codecov.io/bash > "${FILENAME}"
 CODECOV_VERSION=$(grep 'VERSION=\".*\"' codecov | cut -d'"' -f2);
 VALIDATION_FAILURE=0
 
-echo "codecov version: $CODECOV_VERSION"
-CODECOV_SHA=$(head -1 <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/$CODECOV_VERSION/SHA256SUM))
-COMPUTED_SHA=$(shasum -a 256 $FILENAME)
-if [ "$COMPUTED_SHA" = "$CODECOV_SHA" ]; then
+echo "codecov version: ${CODECOV_VERSION}"
+CODECOV_SHA=$(head -1 <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/"$CODECOV_VERSION"/SHA256SUM))
+COMPUTED_SHA=$(shasum -a 256 "${FILENAME}")
+if [ "${COMPUTED_SHA}" == "${CODECOV_SHA}" ]; then
   echo "sha256 checksum OK"
 else
   VALIDATION_FAILURE=1
@@ -20,4 +20,4 @@ else
   chmod +x codecov
   ./codecov
 fi
-rm -rf $FILENAME
+rm -rf "${FILENAME}"
