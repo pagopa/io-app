b2cf240fe9de731614e4633398eee4709b2ae4b3
Fabrizio <fartux@gmail.com>
2021-04-27 12:44:20 +0200

    [#177544049] move empty and header in sectionList

--- ts/features/bonus/bpd/screens/details/transaction/v2/BpdAvailableTransactionsScreenV2.tsx
+++ ts/features/bonus/bpd/screens/details/transaction/v2/BpdAvailableTransactionsScreenV2.tsx
@@ -1,7 +1,6 @@
-import * as pot from "italia-ts-commons/lib/pot";
 import { View } from "native-base";
 import * as React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { H1 } from "../../../../../../../components/core/typography/H1";
@@ -10,22 +9,12 @@ import BaseScreenComponent from "../../../../../../../components/screens/BaseScr
 import I18n from "../../../../../../../i18n";
 import { GlobalState } from "../../../../../../../store/reducers/types";
 import { emptyContextualHelp } from "../../../../../../../utils/emptyContextualHelp";
-import { localeDateFormat } from "../../../../../../../utils/locale";
-import BpdTransactionSummaryComponent from "../../../../components/BpdTransactionSummaryComponent";
-import {
-  atLeastOnePaymentMethodHasBpdEnabledSelector,
-  paymentMethodsWithActivationStatusSelector
-} from "../../../../store/reducers/details/combiner";
-import { bpdSelectedPeriodSelector } from "../../../../store/reducers/details/selectedPeriod";
-import { bpdLastTransactionUpdateSelector } from "../../../../store/reducers/details/transactionsv2/ui";
-import { NoPaymentMethodAreActiveWarning } from "../BpdAvailableTransactionsScreen";
-import BpdEmptyTransactionsList from "../BpdEmptyTransactionsList";
 import TransactionsSectionList from "./TransactionsSectionList";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
 
-const BpdAvailableTransactionsScreenV2 = (props: Props): React.ReactElement => {
+const BpdAvailableTransactionsScreenV2 = (): React.ReactElement => {
   const a = "";
 
   return (
@@ -42,40 +31,7 @@ const BpdAvailableTransactionsScreenV2 = (props: Props): React.ReactElement => {
         <View style={IOStyles.horizontalContentPadding}>
           <H1>{I18n.t("bonus.bpd.details.transaction.title")}</H1>
         </View>
-        <ScrollView style={[IOStyles.flex]}>
-          <View style={IOStyles.horizontalContentPadding}>
-            <View spacer={true} />
-            {props.selectedPeriod && props.maybeLastUpdateDate.isSome() && (
-              <>
-                <BpdTransactionSummaryComponent
-                  lastUpdateDate={localeDateFormat(
-                    props.maybeLastUpdateDate.value,
-                    I18n.t("global.dateFormats.fullFormatFullMonthLiteral")
-                  )}
-                  period={props.selectedPeriod}
-                  totalAmount={props.selectedPeriod.amount}
-                />
-                <View spacer={true} />
-              </>
-            )}
-          </View>
-          {props.selectedPeriod &&
-            (props.selectedPeriod.amount.transactionNumber > 0 ? (
-              <>
-                <TransactionsSectionList />
-              </>
-            ) : !props.atLeastOnePaymentMethodActive &&
-              pot.isSome(props.potWallets) &&
-              props.potWallets.value.length > 0 ? (
-              <View style={IOStyles.horizontalContentPadding}>
-                <NoPaymentMethodAreActiveWarning />
-              </View>
-            ) : (
-              <View style={IOStyles.horizontalContentPadding}>
-                <BpdEmptyTransactionsList />
-              </View>
-            ))}
-        </ScrollView>
+        <TransactionsSectionList />
       </SafeAreaView>
     </BaseScreenComponent>
   );
@@ -83,14 +39,7 @@ const BpdAvailableTransactionsScreenV2 = (props: Props): React.ReactElement => {
 
 const mapDispatchToProps = (_: Dispatch) => ({});
 
-const mapStateToProps = (state: GlobalState) => ({
-  selectedPeriod: bpdSelectedPeriodSelector(state),
-  maybeLastUpdateDate: bpdLastTransactionUpdateSelector(state),
-  potWallets: paymentMethodsWithActivationStatusSelector(state),
-  atLeastOnePaymentMethodActive: atLeastOnePaymentMethodHasBpdEnabledSelector(
-    state
-  )
-});
+const mapStateToProps = (_: GlobalState) => ({});
 
 export default connect(
   mapStateToProps,
--- ts/features/bonus/bpd/screens/details/transaction/v2/TransactionsSectionList.tsx
+++ ts/features/bonus/bpd/screens/details/transaction/v2/TransactionsSectionList.tsx
@@ -1,23 +1,33 @@
 import * as pot from "italia-ts-commons/lib/pot";
+import { View } from "native-base";
 import * as React from "react";
-import { ScrollView, View } from "react-native";
 import { SectionList, SectionListRenderItemInfo } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { H1 } from "../../../../../../../components/core/typography/H1";
 import { IOStyles } from "../../../../../../../components/core/variables/IOStyles";
+import I18n from "../../../../../../../i18n";
 import { GlobalState } from "../../../../../../../store/reducers/types";
+import { localeDateFormat } from "../../../../../../../utils/locale";
+import BpdTransactionSummaryComponent from "../../../../components/BpdTransactionSummaryComponent";
 import { BpdTransactionItem } from "../../../../components/transactionItem/BpdTransactionItem";
 import { AwardPeriodId } from "../../../../store/actions/periods";
 import {
   BpdTransactionId,
   bpdTransactionsLoadPage
 } from "../../../../store/actions/transactions";
+import {
+  atLeastOnePaymentMethodHasBpdEnabledSelector,
+  paymentMethodsWithActivationStatusSelector
+} from "../../../../store/reducers/details/combiner";
+import { BpdPeriodWithInfo } from "../../../../store/reducers/details/periods";
 import { bpdSelectedPeriodSelector } from "../../../../store/reducers/details/selectedPeriod";
 import {
+  bpdLastTransactionUpdateSelector,
   bpdTransactionByIdSelector,
   bpdTransactionsSelector
 } from "../../../../store/reducers/details/transactionsv2/ui";
+import { NoPaymentMethodAreActiveWarning } from "../BpdAvailableTransactionsScreen";
+import BpdEmptyTransactionsList from "../BpdEmptyTransactionsList";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
@@ -30,28 +40,58 @@ const renderItem = (
     () => <></>,
     trx => <BpdTransactionItem transaction={trx} />
   );
+
+const TransactionsHeader = (
+  props: Pick<Props, "selectedPeriod" | "maybeLastUpdateDate">
+) => (
+  <View style={IOStyles.horizontalContentPadding}>
+    <View spacer={true} />
+    {props.selectedPeriod && props.maybeLastUpdateDate.isSome() && (
+      <>
+        <BpdTransactionSummaryComponent
+          lastUpdateDate={localeDateFormat(
+            props.maybeLastUpdateDate.value,
+            I18n.t("global.dateFormats.fullFormatFullMonthLiteral")
+          )}
+          period={props.selectedPeriod}
+          totalAmount={props.selectedPeriod.amount}
+        />
+        <View spacer={true} />
+      </>
+    )}
+  </View>
+);
+
+const TransactionsEmpty = (
+  props: Pick<Props, "potWallets" | "atLeastOnePaymentMethodActive">
+) =>
+  !props.atLeastOnePaymentMethodActive &&
+  pot.isSome(props.potWallets) &&
+  props.potWallets.value.length > 0 ? (
+    <View style={IOStyles.horizontalContentPadding}>
+      <NoPaymentMethodAreActiveWarning />
+    </View>
+  ) : (
+    <View style={IOStyles.horizontalContentPadding}>
+      <BpdEmptyTransactionsList />
+    </View>
+  );
+
 const TransactionsSectionList = (props: Props): React.ReactElement => {
   const isError = pot.isError(props.potTransactions);
   const isLoading = pot.isLoading(props.potTransactions);
   const transactions = pot.getOrElse(props.potTransactions, []);
 
-  let onEndReachedCalledDuringMomentum = false;
-
   return (
     <SectionList
       // renderSectionHeader={renderSectionHeader}
-      // onEndReached={() => props.loadNextPage(2 as AwardPeriodId, 1)}
-      onEndReachedThreshold={0.1}
-      onMomentumScrollBegin={() => {
-        onEndReachedCalledDuringMomentum = false;
-      }}
+      // onEndReached={() => props.loadNextPage(2 as AwardPeriodId, 1)},
+      ListHeaderComponent={<TransactionsHeader {...props} />}
+      ListEmptyComponent={<TransactionsEmpty {...props} />}
       onEndReached={() => {
         console.log("LOAD");
-        // if (!onEndReachedCalledDuringMomentum) {
-        //   console.log("LOAD");
-        //   onEndReachedCalledDuringMomentum = true;
-        // }
       }}
+      onEndReachedThreshold={0.1}
       scrollEnabled={true}
       stickySectionHeadersEnabled={true}
       sections={transactions}
@@ -68,6 +108,11 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 
 const mapStateToProps = (state: GlobalState) => ({
   selectedPeriod: bpdSelectedPeriodSelector(state),
+  maybeLastUpdateDate: bpdLastTransactionUpdateSelector(state),
+  potWallets: paymentMethodsWithActivationStatusSelector(state),
+  atLeastOnePaymentMethodActive: atLeastOnePaymentMethodHasBpdEnabledSelector(
+    state
+  ),
   potTransactions: bpdTransactionsSelector(state),
   bpdTransactionByIdSelector: (trxId: BpdTransactionId) =>
     bpdTransactionByIdSelector(state, trxId)
