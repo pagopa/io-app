95673da5bf191486c987395016429450ea426ef1
Andrea Favarò <andrea@bitrocket.dev>
2021-04-28 11:32:19 +0200

    [#177534705] Fixed header with close button that causes press problems

--- ts/components/FiscalCodeLandscapeOverlay.tsx
+++ ts/components/FiscalCodeLandscapeOverlay.tsx
@@ -2,16 +2,9 @@
  * A component to show the fiscal code fac-simile in Landscape
  */
 import * as pot from "italia-ts-commons/lib/pot";
-import { Body, Button, Container, View } from "native-base";
+import { Body, Button, Container, Content, Right, View } from "native-base";
 import * as React from "react";
-import {
-  BackHandler,
-  Platform,
-  ScrollView,
-  StatusBar,
-  StyleSheet
-} from "react-native";
-import { isIphoneX } from "react-native-iphone-x-helper";
+import { BackHandler, ScrollView, StatusBar, StyleSheet } from "react-native";
 import { InitializedProfile } from "../../definitions/backend/InitializedProfile";
 import { Municipality } from "../../definitions/content/Municipality";
 import IconFont from "../components/ui/IconFont";
@@ -28,28 +21,14 @@ export type Props = Readonly<{
   showBackSide?: boolean;
 }>;
 
-const globalHeaderHeight: number = Platform.select({
-  ios: customVariables.appHeaderHeight + (isIphoneX() ? 42 : 18),
-  android: customVariables.appHeaderHeight,
-  default: -1
-});
-
 const styles = StyleSheet.create({
   content: {
     backgroundColor: customVariables.brandDarkGray,
     paddingHorizontal: customVariables.contentPadding,
-    marginTop: -customVariables.appHeaderHeight,
     paddingTop: 0
   },
-
   headerSpacer: {
     height: customVariables.appHeaderHeight
-  },
-
-  closeButton: {
-    position: "absolute",
-    right: customVariables.contentPadding,
-    top: globalHeaderHeight - customVariables.appHeaderHeight
   }
 });
 
@@ -61,7 +40,7 @@ const FiscalCodeLandscapeOverlay: React.FunctionComponent<Props> = (
   // eslint-disable-next-line functional/no-let
   let scrollTimeout: number | undefined;
 
-  const ScrollViewRef = React.createRef<ScrollView>();
+  const ContentRef = React.createRef<ScrollView>();
 
   const handleBackPress = () => {
     // On backpress the component gets unmounted, so the brightness is restored by the
@@ -71,12 +50,12 @@ const FiscalCodeLandscapeOverlay: React.FunctionComponent<Props> = (
   };
 
   const scrollToEnd = () => {
-    if (props.showBackSide && ScrollViewRef.current) {
+    if (props.showBackSide && ContentRef.current) {
       // dalay the scroll to end command to wait until the ingress animation is completed
       // eslint-disable-next-line
       scrollTimeout = setTimeout(() => {
-        if (ScrollViewRef.current) {
-          ScrollViewRef.current.scrollToEnd({ animated: true });
+        if (ContentRef.current) {
+          ContentRef.current.scrollToEnd({ animated: true });
         }
       }, 300);
     }
@@ -138,16 +117,23 @@ const FiscalCodeLandscapeOverlay: React.FunctionComponent<Props> = (
     <Container style={{ backgroundColor: customVariables.brandDarkGray }}>
       <AppHeader noLeft={true} dark={true}>
         <Body />
+        <Right>
+          <Button
+            transparent={true}
+            onPress={props.onCancel}
+            accessible={true}
+            accessibilityRole={"button"}
+            accessibilityLabel={I18n.t("global.buttons.close")}
+          >
+            <IconFont name="io-close" color={customVariables.colorWhite} />
+          </Button>
+        </Right>
       </AppHeader>
       <StatusBar
         backgroundColor={customVariables.brandDarkGray}
         barStyle={"light-content"}
       />
-      <ScrollView
-        style={styles.content}
-        ref={ScrollViewRef}
-        onLayout={scrollToEnd}
-      >
+      <Content onLayout={scrollToEnd} ref={ContentRef} style={styles.content}>
         <View style={styles.headerSpacer} />
         <View spacer={true} />
         <View>
@@ -170,18 +156,7 @@ const FiscalCodeLandscapeOverlay: React.FunctionComponent<Props> = (
 
         <View spacer={true} large={true} />
         <View spacer={true} large={true} />
-      </ScrollView>
-      <View style={styles.closeButton}>
-        <Button
-          transparent={true}
-          onPress={props.onCancel}
-          accessible={true}
-          accessibilityRole={"button"}
-          accessibilityLabel={I18n.t("global.buttons.close")}
-        >
-          <IconFont name="io-close" color={customVariables.colorWhite} />
-        </Button>
-      </View>
+      </Content>
     </Container>
   );
 };
