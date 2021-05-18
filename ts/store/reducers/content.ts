2309ee6e7806280eee1ec6e620b2ab26defe1545
CrisTofani <cri.tofani@gmail.com>
2021-05-18 16:20:56 +0200

    [#177558088] suggestion from code-review

--- ts/store/reducers/content.ts
+++ ts/store/reducers/content.ts
@@ -1,30 +1,25 @@
 /**
  * Implements the reducers for static content.
  */
-import { fromNullable, none, Option } from "fp-ts/lib/Option";
+import {fromNullable, none, Option} from "fp-ts/lib/Option";
 import * as pot from "italia-ts-commons/lib/pot";
-import { ITuple2 } from "italia-ts-commons/lib/tuples";
-import { NavigationState } from "react-navigation";
-import { createSelector } from "reselect";
-import { getType } from "typesafe-actions";
-import { ContextualHelp } from "../../../definitions/content/ContextualHelp";
-import { Idp } from "../../../definitions/content/Idp";
-import { Municipality as MunicipalityMetadata } from "../../../definitions/content/Municipality";
-import { ScreenCHData } from "../../../definitions/content/ScreenCHData";
-import { Service as ServiceMetadata } from "../../../definitions/content/Service";
-import { IdentityProviderId } from "../../models/IdentityProvider";
-import { CodiceCatastale } from "../../types/MunicipalityCodiceCatastale";
-import { getLocalePrimaryWithFallback } from "../../utils/locale";
-import { getCurrentRouteName } from "../../utils/navigation";
-import {
-  contentMunicipalityLoad,
-  loadContextualHelpData,
-  loadIdps,
-  loadServiceMetadata
-} from "../actions/content";
-import { clearCache } from "../actions/profile";
-import { removeServiceTuples } from "../actions/services";
-import { Action } from "../actions/types";
+import {ITuple2} from "italia-ts-commons/lib/tuples";
+import {NavigationState} from "react-navigation";
+import {createSelector} from "reselect";
+import {getType} from "typesafe-actions";
+import {ContextualHelp} from "../../../definitions/content/ContextualHelp";
+import {Idp} from "../../../definitions/content/Idp";
+import {Municipality as MunicipalityMetadata} from "../../../definitions/content/Municipality";
+import {ScreenCHData} from "../../../definitions/content/ScreenCHData";
+import {Service as ServiceMetadata} from "../../../definitions/content/Service";
+import {IdentityProviderId} from "../../models/IdentityProvider";
+import {CodiceCatastale} from "../../types/MunicipalityCodiceCatastale";
+import {getLocalePrimaryWithFallback} from "../../utils/locale";
+import {getCurrentRouteName} from "../../utils/navigation";
+import {contentMunicipalityLoad, loadContextualHelpData, loadIdps, loadServiceMetadata} from "../actions/content";
+import {clearCache} from "../actions/profile";
+import {removeServiceTuples} from "../actions/services";
+import {Action} from "../actions/types";
 import {
   isReady,
   remoteError,
@@ -33,11 +28,11 @@ import {
   remoteUndefined,
   RemoteValue
 } from "../../features/bonus/bpd/model/RemoteValue";
-import { SpidIdps } from "../../../definitions/content/SpidIdps";
-import { SpidIdp } from "../../../definitions/content/SpidIdp";
-import { navSelector } from "./navigationHistory";
-import { GlobalState } from "./types";
-import { idps } from "./__mock__/idps";
+import {SpidIdps} from "../../../definitions/content/SpidIdps";
+import {SpidIdp} from "../../../definitions/content/SpidIdp";
+import {navSelector} from "./navigationHistory";
+import {GlobalState} from "./types";
+import {idps} from "./__mock__/idps";
 
 /**
  * Stores useful content such as services and organizations metadata,
@@ -113,7 +108,7 @@ export const idpContextualHelpDataFromIdSelector = (id: SpidIdp["id"]) =>
         pot.map(contextualHelpData, data => {
           const locale = getLocalePrimaryWithFallback();
 
-          return fromNullable(data[locale]).fold(none, l =>
+          return fromNullable(data[locale]).chain(l =>
             fromNullable(l.idps[id as IdentityProviderId])
           );
         }),
