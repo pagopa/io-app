37525863f8bd5b668f0db220a5c070093f519ad6
Cristiano Tofani <cri.tofani@gmail.com>
2021-05-26 12:21:50 +0200

    feat: [#177558088] Idps data for selection grid is now loaded from CDN (#3008)
    * [#177558088] WIP Loading idps data from CDN
    
    * [#177558088] Adds test and completes migration to remote idps data
    
    * [#177558088] fixes types
    
    * [#177558088] Fixed type missmatch
    
    * [#177558088] Renames Spid Type
    
    * [#177558088] Revert content specs url
    
    * [#177558088] Adds content actions to analytics report
    
    * [#177558088] Minor
    
    * [#177558088] suggestion from code-review
    
    * [#177558088] refactoring and fixes
    
    * [#177558088] Minor
    
    * [#177558088] fixes compile errors
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    Co-authored-by: fabriziofff <fartux@gmail.com>

--- ts/components/IdpsGrid.tsx
+++ ts/components/IdpsGrid.tsx
@@ -6,15 +6,15 @@ import * as React from "react";
 import { FlatList, Image, ListRenderItemInfo, StyleSheet } from "react-native";
 
 import { Button } from "native-base";
-import { IdentityProvider } from "../models/IdentityProvider";
 import variables from "../theme/variables";
+import { SpidIdp } from "../../definitions/content/SpidIdp";
 import ButtonDefaultOpacity from "./ButtonDefaultOpacity";
 
 type OwnProps = {
   // Array of Identity Provider to show in the grid.
-  idps: ReadonlyArray<IdentityProvider>;
+  idps: ReadonlyArray<SpidIdp>;
   // A callback function called when an Identity Provider is selected
-  onIdpSelected: (_: IdentityProvider) => void;
+  onIdpSelected: (_: SpidIdp) => void;
   testID?: string;
 };
 
@@ -39,10 +39,10 @@ const styles = StyleSheet.create({
   }
 });
 
-const keyExtractor = (idp: IdentityProvider): string => idp.id;
+const keyExtractor = (idp: SpidIdp): string => idp.id;
 
 const renderItem = (props: Props) => (
-  info: ListRenderItemInfo<IdentityProvider>
+  info: ListRenderItemInfo<SpidIdp>
 ): React.ReactElement => {
   const { onIdpSelected } = props;
   const { item } = info;
@@ -70,7 +70,7 @@ const renderItem = (props: Props) => (
       onPress={onPress}
       testID={`idp-${item.id}-button`}
     >
-      <Image source={item.logo} style={styles.idpLogo} />
+      <Image source={{ uri: item.logo }} style={styles.idpLogo} />
     </ButtonDefaultOpacity>
   );
 };
