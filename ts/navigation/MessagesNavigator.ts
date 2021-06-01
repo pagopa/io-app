1ea88b68e5fdc9b4045d429cf05581c922f631c5
Fabrizio <fartux@gmail.com>
2021-06-01 14:34:29 +0200

    integrate eu covid certificate navigator

--- ts/navigation/MessagesNavigator.ts
+++ ts/navigation/MessagesNavigator.ts
@@ -1,24 +1,42 @@
-import { createStackNavigator } from "react-navigation";
+import {
+  createStackNavigator,
+  NavigationRouteConfigMap
+} from "react-navigation";
+import { euCovidCertificateEnabled } from "../config";
+import EuCovidCertNavigator from "../features/euCovidCert/navigation/navigator";
+import EUCOVIDCERT_ROUTES from "../features/euCovidCert/navigation/routes";
 import MessageDetailScreen from "../screens/messages/MessageDetailScreen";
 import MessagesHomeScreen from "../screens/messages/MessagesHomeScreen";
 import ROUTES from "./routes";
 
-const MessagesNavigator = createStackNavigator(
-  {
-    [ROUTES.MESSAGES_HOME]: {
-      screen: MessagesHomeScreen
-    },
-    [ROUTES.MESSAGE_DETAIL]: {
-      screen: MessageDetailScreen
-    }
+const baseMessageRouteConfig: NavigationRouteConfigMap = {
+  [ROUTES.MESSAGES_HOME]: {
+    screen: MessagesHomeScreen
   },
-  {
-    // Let each screen handle the header and navigation
-    headerMode: "none",
-    defaultNavigationOptions: {
-      gesturesEnabled: false
-    }
+  [ROUTES.MESSAGE_DETAIL]: {
+    screen: MessageDetailScreen
   }
-);
+};
+
+const euCovidCertificateRouteConfig: NavigationRouteConfigMap = euCovidCertificateEnabled
+  ? {
+      [EUCOVIDCERT_ROUTES.MAIN]: {
+        screen: EuCovidCertNavigator
+      }
+    }
+  : {};
+
+const messageRouteConfig: NavigationRouteConfigMap = {
+  ...baseMessageRouteConfig,
+  ...euCovidCertificateRouteConfig
+};
+
+const MessagesNavigator = createStackNavigator(messageRouteConfig, {
+  // Let each screen handle the header and navigation
+  headerMode: "none",
+  defaultNavigationOptions: {
+    gesturesEnabled: false
+  }
+});
 
 export default MessagesNavigator;
