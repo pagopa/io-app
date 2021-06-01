ea58847a2a7c57742c32a0c78e6bb52031dab210
Fabrizio <fartux@gmail.com>
2021-06-01 14:20:24 +0200

    add file

---
+++ ts/features/euCovidCert/store/reducers/index.ts
@@ -0,0 +1,15 @@
+import * as pot from "italia-ts-commons/lib/pot";
+import { combineReducers } from "redux";
+import { Action } from "../../../../store/actions/types";
+import { IndexedById } from "../../../../store/helpers/indexer";
+import { EUCovidCertificateResponse } from "../../types/EUCovidCertificateResponse";
+import { euCovidCertByAuthCodeReducer } from "./byAuthCode";
+
+export type EuCovidCertState = {
+  byAuthCode: IndexedById<pot.Pot<EUCovidCertificateResponse, Error>>;
+};
+
+export const euCovidCertReducer = combineReducers<EuCovidCertState, Action>({
+  // save, using the AuthCode as key the the pot.Pot<EUCovidCertificateResponse, Error> response
+  byAuthCode: euCovidCertByAuthCodeReducer
+});
