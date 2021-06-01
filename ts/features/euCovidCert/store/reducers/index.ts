2ca896d7169347467ca8895c10f4994f1a44dae6
Fabrizio <fartux@gmail.com>
2021-06-01 20:29:47 +0200

    add currentAuthCode to the store information

--- ts/features/euCovidCert/store/reducers/index.ts
+++ ts/features/euCovidCert/store/reducers/index.ts
@@ -2,14 +2,19 @@ import * as pot from "italia-ts-commons/lib/pot";
 import { combineReducers } from "redux";
 import { Action } from "../../../../store/actions/types";
 import { IndexedById } from "../../../../store/helpers/indexer";
+import { EUCovidCertificateAuthCode } from "../../types/EUCovidCertificate";
 import { EUCovidCertificateResponse } from "../../types/EUCovidCertificateResponse";
 import { euCovidCertByAuthCodeReducer } from "./byAuthCode";
+import { currentAuthCodeReducer } from "./currentAuthCode";
 
 export type EuCovidCertState = {
   byAuthCode: IndexedById<pot.Pot<EUCovidCertificateResponse, Error>>;
+  currentAuthCode: EUCovidCertificateAuthCode | null;
 };
 
 export const euCovidCertReducer = combineReducers<EuCovidCertState, Action>({
   // save, using the AuthCode as key the the pot.Pot<EUCovidCertificateResponse, Error> response
-  byAuthCode: euCovidCertByAuthCodeReducer
+  byAuthCode: euCovidCertByAuthCodeReducer,
+  // The current auth code that the user is viewing
+  currentAuthCode: currentAuthCodeReducer
 });
