4a040706a8bea52d37185a89ed99548a70293922
fabriziofff <fartux@gmail.com>
2021-06-01 09:18:06 +0200

    chore(EU Covid Certificate): [IAGP-5] Types, actions & store for EU Covid Certificate (#3076)
    * add placeholders for foldering
    
    * wip datamodel
    
    * add actions & store
    
    * add networking handlers
    
    * renaming
    
    * fix wrong import
    
    * add test
    
    * revert
    
    * add test
    
    * add selector
    
    * update certificate types
    
    * change selector
    
    * add generator
    
    * Update ts/features/euCovidCert/saga/index.ts
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

---
+++ ts/features/euCovidCert/store/reducers/byAuthCode.ts
@@ -0,0 +1,51 @@
+import { pot } from "italia-ts-commons";
+import { createSelector } from "reselect";
+import { getType } from "typesafe-actions";
+import { Action } from "../../../../store/actions/types";
+import { IndexedById } from "../../../../store/helpers/indexer";
+import {
+  toError,
+  toLoading,
+  toSome
+} from "../../../../store/reducers/IndexedByIdPot";
+import { GlobalState } from "../../../../store/reducers/types";
+import { EUCovidCertificateAuthCode } from "../../types/EUCovidCertificate";
+import { EUCovidCertificateResponse } from "../../types/EUCovidCertificateResponse";
+import { euCovidCertificateGet } from "../actions";
+
+type EuCovidCertByIdState = IndexedById<
+  pot.Pot<EUCovidCertificateResponse, Error>
+>;
+
+/**
+ * Store the EU Certificate response status based on the AuthCode used to issue the request
+ * @param state
+ * @param action
+ */
+export const euCovidCertByAuthCodeReducer = (
+  state: EuCovidCertByIdState = {},
+  action: Action
+): EuCovidCertByIdState => {
+  switch (action.type) {
+    case getType(euCovidCertificateGet.request):
+      return toLoading(action.payload, state);
+    case getType(euCovidCertificateGet.success):
+      return toSome(action.payload.authCode, state, action.payload);
+    case getType(euCovidCertificateGet.failure):
+      return toError(action.payload.authCode, state, action.payload.value);
+  }
+
+  return state;
+};
+
+/**
+ * From authCode to EUCovidCertificateResponse
+ */
+export const euCovidCertificateFromAuthCodeSelector = createSelector(
+  [
+    (state: GlobalState) => state.features.euCovidCert.byAuthCode,
+    (_: GlobalState, authCode: EUCovidCertificateAuthCode) => authCode
+  ],
+  (byAuthCode, authCode): pot.Pot<EUCovidCertificateResponse, Error> =>
+    byAuthCode[authCode] ?? pot.none
+);
---
+++ ts/features/euCovidCert/types/EUCovidCertificate.ts
@@ -0,0 +1,44 @@
+import { IUnitTag } from "italia-ts-commons/lib/units";
+
+/**
+ * The unique ID of a EU Covid Certificate
+ */
+type EUCovidCertificateId = string & IUnitTag<"EUCovidCertificateId">;
+
+/**
+ * The auth code used to request the EU Covid Certificate, received via message
+ */
+export type EUCovidCertificateAuthCode = string &
+  IUnitTag<"EUCovidCertificateAuthCode">;
+
+type WithEUCovidCertificateId<T> = T & {
+  id: EUCovidCertificateId;
+};
+
+type QRCode = {
+  mimeType: "image/png" | "image/svg";
+  content: string;
+};
+
+type ValidCertificate = {
+  kind: "valid";
+  qrCode: QRCode;
+  markdownPreview: string;
+  markdownDetails: string;
+};
+
+type ExpiredCertificate = {
+  kind: "expired";
+};
+
+type RevokedCertificate = {
+  kind: "revoked";
+  revokedOn: Date;
+};
+
+/**
+ * This type represents the EU Covid Certificate with the different states & data
+ */
+export type EUCovidCertificate = WithEUCovidCertificateId<
+  ValidCertificate | ExpiredCertificate | RevokedCertificate
+>;
