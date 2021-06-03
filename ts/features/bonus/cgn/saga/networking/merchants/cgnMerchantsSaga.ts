e6022d48464f0d2602d6b6eece76b5bb0a096701
CrisTofani <cri.tofani@gmail.com>
2021-06-03 08:54:15 +0200

    [#178358407] Merchant saga temporary implementation

--- ts/features/bonus/cgn/saga/networking/merchants/cgnMerchantsSaga.ts
+++ ts/features/bonus/cgn/saga/networking/merchants/cgnMerchantsSaga.ts
@@ -1,14 +1,19 @@
-import {call, put} from "redux-saga/effects";
-import {readableReport} from "italia-ts-commons/lib/reporters";
-import {BackendCgnMerchants} from "../../../api/backendCgnMerchants";
-import {SagaCallReturnType} from "../../../../../../types/utils";
-import {cgnOfflineMerchants, cgnOnlineMerchants} from "../../../store/actions/merchants";
-import {OnlineMerchants} from "../../../../../../../definitions/cgn/merchants/OnlineMerchants";
-import {getNetworkError} from "../../../../../../utils/errors";
-import {OfflineMerchants} from "../../../../../../../definitions/cgn/merchants/OfflineMerchants";
+import { call, put } from "redux-saga/effects";
+import { readableReport } from "italia-ts-commons/lib/reporters";
+import { BackendCgnMerchants } from "../../../api/backendCgnMerchants";
+import { SagaCallReturnType } from "../../../../../../types/utils";
+import {
+  cgnOfflineMerchants,
+  cgnOnlineMerchants
+} from "../../../store/actions/merchants";
+import { OnlineMerchants } from "../../../../../../../definitions/cgn/merchants/OnlineMerchants";
+import { getNetworkError } from "../../../../../../utils/errors";
+import { OfflineMerchants } from "../../../../../../../definitions/cgn/merchants/OfflineMerchants";
 
 export function* cgnOnlineMerchantsSaga(
-  getOnlineMerchants: ReturnType<typeof BackendCgnMerchants>["getOnlineMerchants"]
+  getOnlineMerchants: ReturnType<
+    typeof BackendCgnMerchants
+  >["getOnlineMerchants"]
 ) {
   try {
     const onlineMerchantsResult: SagaCallReturnType<typeof getOnlineMerchants> = yield call(
@@ -39,16 +44,31 @@ export function* cgnOnlineMerchantsSaga(
   }
 }
 
+const OFFLINE_TEMP_BOUNDINGBOX = {
+  userCoordinates: {
+    latitude: 41.827701462326985,
+    longitude: 12.66444625336996
+  },
+  boundingBox: {
+    coordinates: {
+      latitude: 34.845459548,
+      longitude: 6.5232427904
+    },
+    deltaLatitude: 6.9822419143,
+    deltaLongitude: 6.141203463
+  }
+};
+
 export function* cgnOfflineMerchantsSaga(
-  getOfflineMerchants: ReturnType<typeof BackendCgnMerchants>["getOfflineMerchants"]
+  getOfflineMerchants: ReturnType<
+    typeof BackendCgnMerchants
+  >["getOfflineMerchants"]
 ) {
   try {
     const offlineMerchantsResult: SagaCallReturnType<typeof getOfflineMerchants> = yield call(
       getOfflineMerchants,
       {
-        offlineMerchantSearchRequest: {
-          boundingBox:
-        }
+        offlineMerchantSearchRequest: OFFLINE_TEMP_BOUNDINGBOX
       }
     );
 
