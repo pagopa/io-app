f2db603a3b6839acb0cd878688ab85e82e986364
Fabrizio <fartux@gmail.com>
2021-06-03 20:30:31 +0200

    add combined loading state

--- ts/screens/messages/MessageRouterScreen.tsx
+++ ts/screens/messages/MessageRouterScreen.tsx
@@ -1,70 +1,109 @@
-import { useEffect, useState } from "react";
 import * as React from "react";
-import { ActivityIndicator, SafeAreaView } from "react-native";
+import { useEffect, useState } from "react";
 import { NavigationActions, NavigationInjectedProps } from "react-navigation";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { IOStyles } from "../../components/core/variables/IOStyles";
-import { InfoScreenComponent } from "../../components/infoScreen/InfoScreenComponent";
+import * as pot from "italia-ts-commons/lib/pot";
+import { CreatedMessageWithContentAndAttachments } from "../../../definitions/backend/CreatedMessageWithContentAndAttachments";
 import { LoadingErrorComponent } from "../../features/bonus/bonusVacanze/components/loadingErrorScreen/LoadingErrorComponent";
 import { navigateToEuCovidCertificateDetailScreen } from "../../features/euCovidCert/navigation/actions";
 import { EUCovidCertificateAuthCode } from "../../features/euCovidCert/types/EUCovidCertificate";
 import I18n from "../../i18n";
+import { loadMessages } from "../../store/actions/messages";
 import { navigateToMessageDetailScreenAction } from "../../store/actions/navigation";
+import { messagesAllIdsSelector } from "../../store/reducers/entities/messages/messagesAllIds";
 import { messageStateByIdSelector } from "../../store/reducers/entities/messages/messagesById";
 import { GlobalState } from "../../store/reducers/types";
 import { InferNavigationParams } from "../../types/react";
+import { isStrictSome } from "../../utils/pot";
 import { MessageDetailScreen } from "./MessageDetailScreen";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps> &
   NavigationInjectedProps<InferNavigationParams<typeof MessageDetailScreen>>;
 
-const LoadingMessageDetails = () => (
-  <SafeAreaView
-    accessible={true}
-    style={IOStyles.flex}
-    testID={"LoadMessageDetails"}
-  >
-    <InfoScreenComponent
-      image={
-        <ActivityIndicator
-          color={"black"}
-          accessible={false}
-          importantForAccessibility={"no-hide-descendants"}
-          accessibilityElementsHidden={true}
-          testID={"activityIndicator"}
-        />
-      }
-      title={I18n.t("messageDetails.loadingText")}
-    />
-  </SafeAreaView>
-);
+const getLoadingState = (
+  props: Props
+): pot.Pot<
+  CreatedMessageWithContentAndAttachments | undefined,
+  string | undefined
+> => {
+  const messageId = props.navigation.getParam("messageId");
+  // const messageId = "wrong";
+  if (!isStrictSome(props.allMessages)) {
+    return pot.map(props.allMessages, _ => undefined);
+  }
+  if (!props.allMessages.value.some(id => id === messageId)) {
+    return pot.noneError(`Cannot found ${messageId} in messages list`);
+  }
+  const messageState = props.messageState(messageId);
+  if (messageState === undefined) {
+    return pot.noneError("MessageState is undefined");
+  }
+  return messageState.message;
+};
+
+const navigateToScreenHandler = (
+  message: CreatedMessageWithContentAndAttachments,
+  props: Props
+) => {
+  if (message.content.eu_covid_cert !== undefined) {
+    props.navigateToEuCovidCertificate(
+      message.content.eu_covid_cert.auth_code as EUCovidCertificateAuthCode,
+      message.id
+    );
+  } else {
+    props.navigateToDetails(message.id);
+  }
+};
 
 const MessageRouterScreen = (props: Props): React.ReactElement => {
   const [isLoading, setIsLoading] = useState(true);
-  const messageId = props.navigation.getParam("messageId");
-  const messageState = props.messageState(messageId);
+  const loadingState = getLoadingState(props);
 
   useEffect(() => {
-    props.navigateToEuCovidCertificate(
-      "asd" as EUCovidCertificateAuthCode,
-      messageId
-    );
+    if (isStrictSome(loadingState) && loadingState.value !== undefined) {
+      navigateToScreenHandler(loadingState.value, props);
+    }
+    // first component mount, if not some, refresh the data
+    props.loadMessages();
+    setIsLoading(true);
   }, []);
+
+  useEffect(() => {
+    // all the messages data are ready, no need to reload
+    if (isStrictSome(loadingState) && loadingState.value !== undefined) {
+      navigateToScreenHandler(loadingState.value, props);
+    }
+    pot.fold(
+      loadingState,
+      () => setIsLoading(true),
+      () => setIsLoading(true),
+      _ => setIsLoading(true),
+      _ => setIsLoading(false),
+      _ => setIsLoading(false),
+      _ => setIsLoading(true),
+      (_, __) => setIsLoading(true),
+      _ => setIsLoading(false)
+    );
+  }, [loadingState]);
 
   return (
     <LoadingErrorComponent
       isLoading={isLoading}
       loadingCaption={I18n.t("messageDetails.loadingText")}
       onAbort={props.cancel}
-      onRetry={() => console.log("retry")}
+      onRetry={() => {
+        setIsLoading(true);
+        props.loadMessages();
+      }}
     />
   );
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   cancel: () => dispatch(NavigationActions.back()),
+  loadMessages: () => dispatch(loadMessages.request()),
   navigateToDetails: (messageId: string) => {
     dispatch(NavigationActions.back());
     dispatch(navigateToMessageDetailScreenAction({ messageId }));
@@ -79,7 +118,8 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 });
 const mapStateToProps = (state: GlobalState) => ({
   messageState: (messageId: string) =>
-    messageStateByIdSelector(messageId)(state)
+    messageStateByIdSelector(messageId)(state),
+  allMessages: messagesAllIdsSelector(state)
 });
 
 export default connect(
