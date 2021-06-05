d499d3f47d11ee263065ff60596ba8c6c997142e
Fabrizio <fartux@gmail.com>
2021-06-05 15:41:47 +0200

    add cta save details

--- ts/features/euCovidCert/screens/valid/EuCovidCertMarkdownDetailsScreen.tsx
+++ ts/features/euCovidCert/screens/valid/EuCovidCertMarkdownDetailsScreen.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import * as React from "react";
 import { SafeAreaView, ScrollView } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
+import { StyleSheet } from "react-native";
+import ButtonDefaultOpacity from "../../../../components/ButtonDefaultOpacity";
+import { Label } from "../../../../components/core/typography/Label";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import FooterWithButtons from "../../../../components/ui/FooterWithButtons";
@@ -13,29 +17,52 @@ type NavigationParams = Readonly<{
   markdownDetails: string;
 }>;
 
+const styles = StyleSheet.create({
+  save: {
+    width: "100%"
+  }
+});
+
 export const EuCovidCertMarkdownDetailsScreen = (
   props: NavigationInjectedProps<NavigationParams>
-): React.ReactElement => (
-  <BaseScreenComponent
-    goBack={true}
-    headerTitle={I18n.t(
-      "features.euCovidCertificate.valid.markdownDetails.headerTitle"
-    )}
-    contextualHelp={emptyContextualHelp}
-  >
-    <SafeAreaView style={IOStyles.flex} testID={"EuCovidCertQrCodeFullScreen"}>
-      <ScrollView style={IOStyles.horizontalContentPadding}>
-        <MarkdownHandleCustomLink>
-          {props.navigation.getParam("markdownDetails")}
-        </MarkdownHandleCustomLink>
-      </ScrollView>
-      <FooterWithButtons
-        type={"SingleButton"}
-        leftButton={cancelButtonProps(
-          () => props.navigation.goBack(),
-          I18n.t("global.buttons.close")
-        )}
-      />
-    </SafeAreaView>
-  </BaseScreenComponent>
-);
+): React.ReactElement => {
+  const [loadComplete, setLoadComplete] = useState(false);
+  const activateCTA = () => setLoadComplete(true);
+
+  return (
+    <BaseScreenComponent
+      goBack={true}
+      headerTitle={I18n.t(
+        "features.euCovidCertificate.valid.markdownDetails.headerTitle"
+      )}
+      contextualHelp={emptyContextualHelp}
+    >
+      <SafeAreaView
+        style={IOStyles.flex}
+        testID={"EuCovidCertQrCodeFullScreen"}
+      >
+        <ScrollView style={IOStyles.horizontalContentPadding}>
+          <MarkdownHandleCustomLink onLoadEnd={activateCTA}>
+            {props.navigation.getParam("markdownDetails")}
+          </MarkdownHandleCustomLink>
+          {loadComplete && (
+            <ButtonDefaultOpacity style={styles.save}>
+              <Label color={"white"}>
+                {I18n.t(
+                  "features.euCovidCertificate.valid.markdownDetails.save"
+                )}
+              </Label>
+            </ButtonDefaultOpacity>
+          )}
+        </ScrollView>
+        <FooterWithButtons
+          type={"SingleButton"}
+          leftButton={cancelButtonProps(
+            () => props.navigation.goBack(),
+            I18n.t("global.buttons.close")
+          )}
+        />
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
