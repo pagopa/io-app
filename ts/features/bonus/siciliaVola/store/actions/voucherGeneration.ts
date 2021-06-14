d9e8e99ef5a31948799a10168605ab4113d5da14
Simone <simone.biffi.91@gmail.com>
2021-06-14 09:35:08 +0200

    chore(Sicilia Vola): [IASV-5] Add sicilia vola actions, store & reducer placeholders (#3074)
    * add analytics placeholder
    
    * add saga placeholder
    
    * add navigation placeholder
    
    * add screens placeholder
    
    * add voucher generation workunit actions
    
    * add SiciliaVola actions to Action type
    
    * add voucher generation reducer
    
    * add siciliaVola reducer
    
    * add siciliaVola state to bonusState
    
    * fix lint errors
    
    Co-authored-by: fabriziofff <fartux@gmail.com>

---
+++ ts/features/bonus/siciliaVola/store/actions/voucherGeneration.ts
@@ -0,0 +1,35 @@
+import { ActionType, createStandardAction } from "typesafe-actions";
+
+/**
+ * The user chooses to start the workflow to generate a new SiciliaVola voucher
+ */
+export const svGenerateVoucherStart = createStandardAction(
+  "SV_GENERATE_VOUCHER_START"
+)<void>();
+/**
+ * The user completes the workflow to generate a new SiciliaVola voucher
+ * (the request is sent and we receive an OK or a timeout response)
+ */
+export const svGenerateVoucherCompleted = createStandardAction(
+  "SV_GENERATE_VOUCHER_COMPLETED"
+)<void>();
+
+/**
+ * The user chooses to cancel the generation of a SiciliaVola voucher (no voucher request has been sent)
+ */
+export const svGenerateVoucherCancel = createStandardAction(
+  "SV_GENERATE_VOUCHER_CANCEL"
+)<void>();
+
+/**
+ * The user chooses `back` from the first screen
+ */
+export const svGenerateVoucherBack = createStandardAction(
+  "SV_GENERATE_VOUCHER_BACK"
+)<void>();
+
+export type SvVoucherGenerationActions =
+  | ActionType<typeof svGenerateVoucherStart>
+  | ActionType<typeof svGenerateVoucherCompleted>
+  | ActionType<typeof svGenerateVoucherBack>
+  | ActionType<typeof svGenerateVoucherCancel>;
