0518e7bdc78e4c214f62081e53f270c4d993a871
Matteo Boschi <mbmaken@gmail.com>
2021-06-15 15:45:01 +0200

    add tests

---
+++ ts/store/reducers/__tests__/persistedPreferences.test.ts
@@ -0,0 +1,56 @@
+import { GlobalState } from "../types";
+import { appReducer } from "../index";
+import { isMixpanelEnabled } from "../persistedPreferences";
+import { applicationChangeState } from "../../actions/application";
+import { setMixpanelEnabled } from "../../actions/mixpanel";
+import {
+  logoutRequest,
+  logoutSuccess,
+  sessionExpired,
+  sessionInvalid
+} from "../../actions/authentication";
+import { differentProfileLoggedIn } from "../../actions/crossSessions";
+
+describe("persistedPreferences reducer/selectors", () => {
+  it("should be reset mixpanel preference only on differentProfileLoggedIn action ", () => {
+    const initialState: GlobalState = appReducer(
+      undefined,
+      applicationChangeState("active")
+    );
+    expect(isMixpanelEnabled(initialState)).toBeNull();
+
+    const enabledState: GlobalState = appReducer(
+      initialState,
+      setMixpanelEnabled(true)
+    );
+    expect(isMixpanelEnabled(enabledState)).toBeTruthy();
+
+    const notEnabledState: GlobalState = appReducer(
+      enabledState,
+      setMixpanelEnabled(false)
+    );
+    expect(isMixpanelEnabled(notEnabledState)).toBeFalsy();
+
+    // eslint-disable-next-line functional/no-let
+    let noChangesState: GlobalState = notEnabledState;
+    // for these actions isMixpanelEnabled must not change
+    [
+      logoutRequest({ keepUserData: true }),
+      logoutRequest({ keepUserData: false }),
+      logoutSuccess({ keepUserData: true }),
+      logoutSuccess({ keepUserData: false }),
+      sessionExpired(),
+      sessionInvalid()
+    ].forEach(action => {
+      noChangesState = appReducer(noChangesState, action);
+      // it has to match the last state
+      expect(isMixpanelEnabled(notEnabledState)).toBeFalsy();
+    });
+
+    const resetState: GlobalState = appReducer(
+      noChangesState,
+      differentProfileLoggedIn()
+    );
+    expect(isMixpanelEnabled(resetState)).toBeNull();
+  });
+});
