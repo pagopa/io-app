27ba5de3c520ff23e36488288604f2259d0f90e1
Matteo Boschi <mbmaken@gmail.com>
2021-06-25 10:50:35 +0200

    update comment

--- ts/store/middlewares/analytics.ts
+++ ts/store/middlewares/analytics.ts
@@ -458,7 +458,7 @@ export function screenTracking(
       if (nextScreen) {
         setInstabugUserAttribute("activeScreen", nextScreen);
       }
-      // track only those event that are not included in the blacklist
+      // track only those events that are not included in the blacklist
       if (nextScreen && !noAnalyticsRoutes.has(nextScreen)) {
         void mixpanel.track("SCREEN_CHANGE_V2", {
           SCREEN_NAME: nextScreen
