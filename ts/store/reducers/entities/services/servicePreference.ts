d4d1b5b9ee76f7919bdc7021bec5ea5d16d13ea2
CrisTofani <cri.tofani@gmail.com>
2021-07-01 09:22:41 +0200

    Preference are stored only for current service

--- ts/store/reducers/entities/services/servicePreference.ts
+++ ts/store/reducers/entities/services/servicePreference.ts
@@ -1,6 +1,5 @@
 import * as pot from "italia-ts-commons/lib/pot";
 import { getType } from "typesafe-actions";
-import { createSelector } from "reselect";
 import { Action } from "../../../actions/types";
 import {
   loadServicePreference,
@@ -8,37 +7,32 @@ import {
 } from "../../../actions/services/servicePreference";
 import { ServicePreferenceResponse } from "../../../../types/services/ServicePreferenceResponse";
 import { getNetworkErrorMessage } from "../../../../utils/errors";
-import { IndexedById } from "../../../helpers/indexer";
-import { toError, toLoading, toSome, toUpdating } from "../../IndexedByIdPot";
 import { GlobalState } from "../../types";
 
-export type ServicePreferenceState = IndexedById<
-  pot.Pot<ServicePreferenceResponse, Error>
->;
+export type ServicePreferenceState = pot.Pot<ServicePreferenceResponse, Error>;
 
 // Reducer to handle specific service contact preferences (inbox, push, emails)
 const servicePreferenceReducer = (
-  state: ServicePreferenceState = {},
+  state: ServicePreferenceState = pot.none,
   action: Action
 ): ServicePreferenceState => {
   switch (action.type) {
     case getType(loadServicePreference.request):
-      return toLoading(action.payload, state);
+      return pot.toLoading(state);
     case getType(upsertServicePreference.request):
       const { id, ...payload } = action.payload;
 
-      return toUpdating(id, state, {
+      return pot.toUpdating(state, {
         id,
         kind: "success",
         value: payload
       });
     case getType(loadServicePreference.success):
     case getType(upsertServicePreference.success):
-      return toSome(action.payload.id, state, action.payload);
+      return pot.some(action.payload);
     case getType(loadServicePreference.failure):
     case getType(upsertServicePreference.failure):
-      return toError(
-        action.payload.id,
+      return pot.toError(
         state,
         new Error(getNetworkErrorMessage(action.payload))
       );
@@ -48,13 +42,7 @@ const servicePreferenceReducer = (
 
 export default servicePreferenceReducer;
 
+// Selectors
 export const servicePreferenceSelector = (
   state: GlobalState
 ): ServicePreferenceState => state.entities.services.servicePreference;
-
-export const servicePreferenceByIDSelector = (id: string) =>
-  createSelector(
-    servicePreferenceSelector,
-    (spById): pot.Pot<ServicePreferenceResponse, Error> =>
-      spById[id] ?? pot.none
-  );
