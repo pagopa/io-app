8534b6dacef79e33d83a346c84d27fc287cb5bcb
Simone <simone.biffi.91@gmail.com>
2021-07-01 18:03:32 +0200

    modify pot in remotevalues

--- ts/features/bonus/siciliaVola/store/reducers/activation.ts
+++ ts/features/bonus/siciliaVola/store/reducers/activation.ts
@@ -1,19 +1,25 @@
 import { Action } from "../../../../../store/actions/types";
-import * as pot from "italia-ts-commons/lib/pot";
 import { NetworkError } from "../../../../../utils/errors";
 import { getType } from "typesafe-actions";
 import { svServiceAlive, svTosAccepted } from "../actions/activation";
 import { createSelector } from "reselect";
 import { GlobalState } from "../../../../../store/reducers/types";
+import {
+  remoteError,
+  remoteLoading,
+  remoteReady,
+  remoteUndefined,
+  RemoteValue
+} from "../../../bpd/model/RemoteValue";
 
 export type ActivationState = {
-  isAlive: pot.Pot<boolean, NetworkError>;
-  tosAccepted: pot.Pot<boolean, NetworkError>;
+  isAlive: RemoteValue<boolean, NetworkError>;
+  tosAccepted: RemoteValue<boolean, NetworkError>;
 };
 
 const INITIAL_STATE: ActivationState = {
-  isAlive: pot.none,
-  tosAccepted: pot.none
+  isAlive: remoteUndefined,
+  tosAccepted: remoteUndefined
 };
 
 const reducer = (
@@ -24,32 +30,32 @@ const reducer = (
     case getType(svServiceAlive.request):
       return {
         ...state,
-        isAlive: pot.toLoading(state.isAlive)
+        isAlive: remoteLoading
       };
     case getType(svServiceAlive.success):
       return {
         ...state,
-        isAlive: pot.some(action.payload)
+        isAlive: remoteReady(action.payload)
       };
     case getType(svServiceAlive.failure):
       return {
         ...state,
-        isAlive: pot.toError(state.isAlive, action.payload)
+        isAlive: remoteError(action.payload)
       };
     case getType(svTosAccepted.request):
       return {
         ...state,
-        tosAccepted: pot.toLoading(state.tosAccepted)
+        tosAccepted: remoteLoading
       };
     case getType(svTosAccepted.success):
       return {
         ...state,
-        tosAccepted: pot.some(action.payload)
+        tosAccepted: remoteReady(action.payload)
       };
     case getType(svTosAccepted.failure):
       return {
         ...state,
-        tosAccepted: pot.toError(state.tosAccepted, action.payload)
+        tosAccepted: remoteError(action.payload)
       };
   }
   return state;
@@ -59,13 +65,14 @@ export default reducer;
 
 export const isAliveSelector = createSelector(
   [(state: GlobalState) => state.bonus.sv.activation.isAlive],
-  (isAlive: pot.Pot<boolean, NetworkError>): pot.Pot<boolean, NetworkError> =>
-    isAlive
+  (
+    isAlive: RemoteValue<boolean, NetworkError>
+  ): RemoteValue<boolean, NetworkError> => isAlive
 );
 
 export const tosAcceptedSelector = createSelector(
   [(state: GlobalState) => state.bonus.sv.activation.tosAccepted],
   (
-    tosAccepted: pot.Pot<boolean, NetworkError>
-  ): pot.Pot<boolean, NetworkError> => tosAccepted
+    tosAccepted: RemoteValue<boolean, NetworkError>
+  ): RemoteValue<boolean, NetworkError> => tosAccepted
 );
