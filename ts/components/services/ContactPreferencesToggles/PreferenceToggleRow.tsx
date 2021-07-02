3aebc294298954b55c3b3751209cf3550c5e66ba
CrisTofani <cri.tofani@gmail.com>
2021-07-02 13:43:55 +0200

    Accessibility implementation for PreferenceToggle component

--- ts/components/services/ContactPreferencesToggles/PreferenceToggleRow.tsx
+++ ts/components/services/ContactPreferencesToggles/PreferenceToggleRow.tsx
@@ -6,6 +6,7 @@ import { IOStyles } from "../../core/variables/IOStyles";
 import IconFont from "../../ui/IconFont";
 import { IOColors } from "../../core/variables/IOColors";
 import TouchableDefaultOpacity from "../../TouchableDefaultOpacity";
+import I18n from "../../../i18n";
 
 type Props = {
   label: string;
@@ -39,7 +40,12 @@ const PreferenceToggleRow = ({
 }: Props): React.ReactElement => (
   <View style={[styles.row]}>
     <View style={IOStyles.flex}>
-      <H4 weight={"Regular"} color={"bluegreyDark"} testID={`${testID}-label`}>
+      <H4
+        weight={"Regular"}
+        color={"bluegreyDark"}
+        testID={`${testID}-label`}
+        accessibilityRole={"text"}
+      >
         {label}
       </H4>
     </View>
@@ -49,13 +55,23 @@ const PreferenceToggleRow = ({
         onValueChange={onPress}
         testID={testID}
         disabled={disabled}
+        accessibilityRole={"switch"}
+        accessibilityState={{ checked: value, disabled }}
       />
     )}
     {isLoading && !isError && (
-      <ActivityIndicator testID={`${testID}-loading`} />
+      <ActivityIndicator
+        testID={`${testID}-loading`}
+        accessibilityLabel={I18n.t("global.remoteStates.loading")}
+      />
     )}
     {!isLoading && isError && (
-      <TouchableDefaultOpacity onPress={onReload} testID={`${testID}-reload`}>
+      <TouchableDefaultOpacity
+        onPress={onReload}
+        testID={`${testID}-reload`}
+        accessibilityRole={"button"}
+        accessibilityLabel={I18n.t("global.accessibility.reload")}
+      >
         <IconFont name={"io-reload"} size={20} color={IOColors.blue} />
       </TouchableDefaultOpacity>
     )}
