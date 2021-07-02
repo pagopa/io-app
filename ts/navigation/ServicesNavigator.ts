0723fbe4e28c5fdc33b577b32e1156c44b0d1522
Simone <simone.biffi.91@gmail.com>
2021-07-02 11:53:30 +0200

    Move navigator to service section

--- ts/navigation/ServicesNavigator.ts
+++ ts/navigation/ServicesNavigator.ts
@@ -1,10 +1,12 @@
 import { createStackNavigator } from "react-navigation";
-import { myPortalEnabled } from "../config";
+import { myPortalEnabled, svEnabled } from "../config";
 
 import ServiceDetailsScreen from "../screens/services/ServiceDetailsScreen";
 import ServicesHomeScreen from "../screens/services/ServicesHomeScreen";
 import ServicesWebviewScreen from "../screens/services/ServicesWebviewScreen";
 import ROUTES from "./routes";
+import SV_ROUTES from "../features/bonus/siciliaVola/navigation/routes";
+import SvNavigator from "../features/bonus/siciliaVola/navigation/navigator";
 
 const servicesRoutes = {
   [ROUTES.SERVICES_HOME]: {
@@ -15,21 +17,30 @@ const servicesRoutes = {
   }
 };
 
-const myPortalRoutes = {
-  [ROUTES.SERVICE_WEBVIEW]: {
-    screen: ServicesWebviewScreen
-  }
-};
-
-const ServicesNavigator = createStackNavigator(
-  myPortalEnabled ? { ...servicesRoutes, ...myPortalRoutes } : servicesRoutes,
-  {
-    // Let each screen handle the header and navigation
-    headerMode: "none",
-    defaultNavigationOptions: {
-      gesturesEnabled: false
+const myPortalRoutes = myPortalEnabled
+  ? {
+      [ROUTES.SERVICE_WEBVIEW]: {
+        screen: ServicesWebviewScreen
+      }
     }
+  : {};
+
+const svConfigMap = svEnabled
+  ? {
+      [SV_ROUTES.MAIN]: {
+        screen: SvNavigator
+      }
+    }
+  : {};
+
+const routeConfig = { ...servicesRoutes, ...myPortalRoutes, ...svConfigMap };
+
+const ServicesNavigator = createStackNavigator(routeConfig, {
+  // Let each screen handle the header and navigation
+  headerMode: "none",
+  defaultNavigationOptions: {
+    gesturesEnabled: false
   }
-);
+});
 
 export default ServicesNavigator;
