b2ecb9538abb261513348bc9b45ac6db47e61e5f
CrisTofani <cri.tofani@gmail.com>
2021-07-06 10:03:45 +0200

    adds mixpanel track for service preference selected

--- ts/screens/onboarding/OnboardingServicesPreferenceScreen.tsx
+++ ts/screens/onboarding/OnboardingServicesPreferenceScreen.tsx
@@ -28,6 +28,7 @@ import { profileUpsert } from "../../store/actions/profile";
 import { withLoadingSpinner } from "../../components/helpers/withLoadingSpinner";
 import { showToast } from "../../utils/showToast";
 import { servicesOptinCompleted } from "../../store/actions/onboarding";
+import { mixpanelTrack } from "../../mixpanel";
 
 type NavigationProps = {
   isFirstOnboarding: boolean;
@@ -55,6 +56,9 @@ const OnboardingServicesPreferenceScreen = (
   React.useEffect(() => {
     // when the user made a choice (the profile is right updated), navigate to thank-you page
     if (isServicesPreferenceModeSet(props.profileServicePreferenceMode)) {
+      void mixpanelTrack("SERVICE_CONTACT_MODE_SET", {
+        mode: props.profileServicePreferenceMode
+      });
       props.onContinue(isFirstOnboarding);
       return;
     }
