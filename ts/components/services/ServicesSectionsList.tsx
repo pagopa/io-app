482c0311dc1bf86406283ab60b6e8b2f9faa3e5f
Cristiano Tofani <cri.tofani@gmail.com>
2021-07-13 15:40:32 +0200

    chore(Redesign Servizi): [IARS-28] Removes services redesign FF and cleanup dead code (#3220)
    * [IARS-28] Removes services redesign FF and cleanup dead code
    
    * refactoring after review
    
    * remove dead code
    
    * remove dead code
    
    * remove dead code
    
    * Removes further dead code about local services and service search
    
    * update
    
    * refactoring
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    Co-authored-by: pietro909 <2094604+pietro909@users.noreply.github.com>

--- ts/components/services/ServicesSectionsList.tsx
+++ ts/components/services/ServicesSectionsList.tsx
@@ -8,11 +8,7 @@ import { StyleSheet } from "react-native";
 import { ServicePublic } from "../../../definitions/backend/ServicePublic";
 import I18n from "../../i18n";
 import { ServicesSectionState } from "../../store/reducers/entities/services";
-import { ReadStateByServicesId } from "../../store/reducers/entities/services/readStateByServiceId";
-import { ProfileState } from "../../store/reducers/profile";
 import customVariables from "../../theme/variables";
-import ButtonDefaultOpacity from "../ButtonDefaultOpacity";
-import IconFont from "../ui/IconFont";
 import ServiceList from "./ServiceList";
 
 type AnimatedProps = {
@@ -24,23 +20,9 @@ type AnimatedProps = {
 
 type OwnProps = {
   sections: ReadonlyArray<ServicesSectionState>;
-  profile: ProfileState;
-  onChooserAreasOfInterestPress?: () => void;
-  selectedOrganizationsFiscalCodes?: Set<string>;
-  isLocal?: boolean;
-  isAll: boolean;
-  onLongPressItem?: () => void;
-  isLongPressEnabled: boolean;
-  onItemSwitchValueChanged?: (
-    service: ReadonlyArray<ServicePublic>,
-    value: boolean
-  ) => void;
-  renderRightIcon?: (section: ServicesSectionState) => React.ReactNode;
   isRefreshing: boolean;
   onRefresh: () => void;
   onSelect: (service: ServicePublic) => void;
-  readServices: ReadStateByServicesId;
-  isSelectableOrgsEmpty?: boolean;
 };
 
 type Props = AnimatedProps & OwnProps;
@@ -55,24 +37,6 @@ const styles = StyleSheet.create({
     paddingTop: customVariables.contentPadding / 2,
     paddingBottom: customVariables.contentPadding / 2,
     alignItems: "center"
-  },
-  message: {
-    fontSize: customVariables.fontSizeBase,
-    textAlign: "left"
-  },
-  button: {
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: 0,
-    paddingBottom: 0
-  },
-  textButton: {
-    paddingLeft: 0,
-    paddingRight: 0
-  },
-  icon: {
-    color: customVariables.brandPrimaryInverted,
-    lineHeight: 24
   },
   emptyListContentTitle: {
     paddingTop: customVariables.contentPadding,
@@ -93,93 +57,17 @@ const emptyListComponent = () => (
   </View>
 );
 
-class ServicesSectionsList extends React.PureComponent<Props> {
-  private localListEmptyComponent() {
-    return (
-      this.props.isLocal && (
-        <View style={styles.headerContentWrapper}>
-          <Text style={styles.message}>
-            {this.props.isSelectableOrgsEmpty
-              ? I18n.t("services.areasOfInterest.selectMessageEmptyOrgs")
-              : I18n.t("services.areasOfInterest.selectMessage")}
-          </Text>
-          <View spacer={true} large={true} />
-          <ButtonDefaultOpacity
-            small={true}
-            primary={true}
-            style={styles.button}
-            block={true}
-            onPress={this.props.onChooserAreasOfInterestPress}
-            disabled={this.props.isSelectableOrgsEmpty}
-          >
-            <IconFont name="io-plus" style={styles.icon} />
-            <Text style={styles.textButton}>
-              {I18n.t("services.areasOfInterest.addButton")}
-            </Text>
-          </ButtonDefaultOpacity>
-          <View spacer={true} extralarge={true} />
-          <Image source={require("../../../img/services/icon-places.png")} />
-        </View>
-      )
-    );
-  }
-
-  private renderEditButton = () =>
-    this.props.isLocal &&
-    this.props.selectedOrganizationsFiscalCodes &&
-    this.props.selectedOrganizationsFiscalCodes.size > 0 && (
-      <View style={styles.headerContentWrapper}>
-        <ButtonDefaultOpacity
-          small={true}
-          primary={!this.props.isLongPressEnabled}
-          style={styles.button}
-          block={true}
-          onPress={this.props.onChooserAreasOfInterestPress}
-          disabled={
-            this.props.isRefreshing ||
-            this.props.isLongPressEnabled ||
-            this.props.isSelectableOrgsEmpty
-          }
-        >
-          <Text style={styles.textButton}>
-            {I18n.t("services.areasOfInterest.editButton")}
-          </Text>
-        </ButtonDefaultOpacity>
-      </View>
-    );
-
-  private renderList = () => {
-    // empty component is different from local and others (national and all)
-    const emptyComponent = this.props.isLocal
-      ? this.localListEmptyComponent()
-      : emptyListComponent();
-    return (
-      <ServiceList
-        renderUnreadState={!this.props.isAll}
-        animated={this.props.animated}
-        sections={this.props.sections}
-        profile={this.props.profile}
-        isRefreshing={this.props.isRefreshing}
-        onRefresh={this.props.onRefresh}
-        onSelect={this.props.onSelect}
-        readServices={this.props.readServices}
-        ListEmptyComponent={emptyComponent}
-        onLongPressItem={this.props.onLongPressItem}
-        isLongPressEnabled={this.props.isLongPressEnabled}
-        onItemSwitchValueChanged={this.props.onItemSwitchValueChanged}
-        renderRightIcon={this.props.renderRightIcon}
-      />
-    );
-  };
-
-  public render() {
-    return (
-      <View style={styles.contentWrapper}>
-        {this.renderEditButton()}
-        {this.renderList()}
-      </View>
-    );
-  }
-}
+const ServicesSectionsList = (props: Props) => (
+  <View style={styles.contentWrapper}>
+    <ServiceList
+      animated={props.animated}
+      sections={props.sections}
+      isRefreshing={props.isRefreshing}
+      onRefresh={props.onRefresh}
+      onSelect={props.onSelect}
+      ListEmptyComponent={emptyListComponent()}
+    />
+  </View>
+);
 
 export default ServicesSectionsList;
