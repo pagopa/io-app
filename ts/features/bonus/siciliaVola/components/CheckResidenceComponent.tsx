b21c5bbb0d0735fdaf946edb098f3f9d25b821e5
Simone <simone.biffi.91@gmail.com>
2021-07-14 13:32:11 +0200

    manage KoCheckResidenceScreen

--- ts/features/bonus/siciliaVola/components/CheckResidenceComponent.tsx
+++ ts/features/bonus/siciliaVola/components/CheckResidenceComponent.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useRef } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, Text } from "react-native";
 import { NavigationEvents } from "react-navigation";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
@@ -12,13 +12,21 @@ import { H1 } from "../../../../components/core/typography/H1";
 import { GlobalState } from "../../../../store/reducers/types";
 import { svGenerateVoucherBack } from "../store/actions/voucherGeneration";
 import FooterWithButtons from "../../../../components/ui/FooterWithButtons";
-import { navigateToSvSelectBeneficiaryCategoryScreen } from "../navigation/actions";
+import {
+  navigateToSvKoCheckResidenceScreen,
+  navigateToSvSelectBeneficiaryCategoryScreen
+} from "../navigation/actions";
 import I18n from "../../../../i18n";
+import ButtonDefaultOpacity from "../../../../components/ButtonDefaultOpacity";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
 
 const CheckResidenceComponent = (props: Props): React.ReactElement => {
+  const [isResidentInSicily, setisResidentInSicily] = React.useState<
+    boolean | undefined
+  >();
+
   const elementRef = useRef(null);
   const backButtonProps = {
     primary: false,
@@ -29,9 +37,14 @@ const CheckResidenceComponent = (props: Props): React.ReactElement => {
   const cancelButtonProps = {
     primary: false,
     bordered: true,
-    onPress: props.cancel,
-    title: "Continue"
+    onPress:
+      isResidentInSicily === true
+        ? props.navigateToSvSelectBeneficiaryCategory
+        : props.navigateToSvKoCheckResidence,
+    title: "Continue",
+    disabled: isResidentInSicily === undefined
   };
+
   return (
     <BaseScreenComponent goBack={true} contextualHelp={emptyContextualHelp}>
       <NavigationEvents onDidFocus={() => setAccessibilityFocus(elementRef)} />
@@ -41,6 +54,13 @@ const CheckResidenceComponent = (props: Props): React.ReactElement => {
         ref={elementRef}
       >
         <H1>{I18n.t("bonus.sv.voucherGeneration.checkResidence.title")}</H1>
+
+        <ButtonDefaultOpacity onPress={() => setisResidentInSicily(true)}>
+          <Text> {"Resident In Sicily"} </Text>
+        </ButtonDefaultOpacity>
+        <ButtonDefaultOpacity onPress={() => setisResidentInSicily(false)}>
+          <Text> {"Not ResidentInSicily"} </Text>
+        </ButtonDefaultOpacity>
       </SafeAreaView>
       <FooterWithButtons
         type={"TwoButtonsInlineHalf"}
@@ -52,7 +72,10 @@ const CheckResidenceComponent = (props: Props): React.ReactElement => {
 };
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   back: () => dispatch(svGenerateVoucherBack()),
-  cancel: () => dispatch(navigateToSvSelectBeneficiaryCategoryScreen())
+  navigateToSvSelectBeneficiaryCategory: () =>
+    dispatch(navigateToSvSelectBeneficiaryCategoryScreen()),
+  navigateToSvKoCheckResidence: () =>
+    dispatch(navigateToSvKoCheckResidenceScreen())
 });
 const mapStateToProps = (_: GlobalState) => ({});
 
