40a5c09e33f558e787f777940602d5dba8fe9db7
Pietro Grandi <pietro.grandi.3d@gmail.com>
2021-07-15 15:13:12 +0200

    Remove tautological tests, cover new logic, mock platform module

--- ts/components/services/__tests__/ServiceMetadata.test.tsx
+++ ts/components/services/__tests__/ServiceMetadata.test.tsx
@@ -1,38 +1,54 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
+import { OrganizationFiscalCode } from "italia-ts-commons/lib/strings";
 
 import { TranslationKeys } from "../../../../locales/locales";
+import { Service, ScopeEnum } from "../../../../definitions/content/Service";
+import { ServiceId } from "../../../../definitions/backend/ServiceId";
 import { capitalize } from "../../../utils/strings";
+import * as utilsUrl from "../../../utils/url";
 import I18n from "../../../i18n";
 import ServiceMetadata from "../ServiceMetadata";
 
-const mockOpenWebUrl = jest.fn();
+jest.mock("../../../utils/platform");
 
-jest.mock("../../../utils/url", () => ({
-  openWebUrl: (...args: any) => mockOpenWebUrl(...args)
-}));
+const spyOpenWebUrl = jest.spyOn(utilsUrl, "openWebUrl");
+
+const defaultServiceMetadata: Service = {
+  scope: ScopeEnum.NATIONAL
+};
+
+const defaultProps = {
+  getItemOnPress: jest.fn(),
+  isDebugModeEnabled: false,
+  organizationFiscalCode: "01234567891" as OrganizationFiscalCode,
+  serviceId: "ABC123" as ServiceId,
+  servicesMetadata: defaultServiceMetadata
+};
 
 describe("ServiceMetadata component", () => {
   beforeEach(() => {
-    mockOpenWebUrl.mockReset();
+    defaultProps.getItemOnPress.mockReset();
+    spyOpenWebUrl.mockReset();
+  });
+  afterEach(() => {
+    jest.dontMock("../../../utils/url");
   });
 
   it("should render the section header", () => {
     expect(
-      renderComponent({ ...options }).getByText(
+      renderComponent({ ...defaultProps }).getByText(
         I18n.t("services.contactsAndInfo")
       )
     ).toBeDefined();
   });
 
-  describe("given a serviceId", () => {
+  describe("when debug mode is enabled", () => {
     const currentOptions = {
-      ...options,
-      serviceId: "abcdDEF123___",
-      getItemOnPress: jest.fn()
+      ...defaultProps,
+      isDebugModeEnabled: true
     };
-    beforeEach(() => currentOptions.getItemOnPress.mockReset());
-    it("should render its label", () => {
+    it("should render the serviceId label", () => {
       expect(
         renderComponent(currentOptions).getByText(
           capitalize(I18n.t("global.id"))
@@ -40,7 +56,7 @@ describe("ServiceMetadata component", () => {
       ).toBeDefined();
     });
 
-    it("should render service its value", () => {
+    it("should render the serviceId value", () => {
       expect(
         renderComponent(currentOptions).getByText(currentOptions.serviceId)
       ).toBeDefined();
@@ -55,41 +71,64 @@ describe("ServiceMetadata component", () => {
     });
   });
 
-  [
-    ["address", "via genova", "services.contactAddress", "MAP"],
-    ["fiscalCode", "via genova", "profile.fiscalCode.fiscalCode", "COPY"]
-  ].forEach(([name, value, label, action]) => {
-    describe(`when ${name} is defined`, () => {
-      const currentOptions = {
-        ...options,
-        [name]: value,
-        getItemOnPress: jest.fn()
-      };
-      beforeEach(() => currentOptions.getItemOnPress.mockReset());
-      it(`should render its label "${label}"`, () => {
-        expect(
-          renderComponent(currentOptions).getByText(
-            capitalize(I18n.t(label as TranslationKeys))
-          )
-        ).toBeDefined();
-      });
-      it(`should render its value "${value}"`, () => {
-        expect(renderComponent(currentOptions).getByText(value)).toBeDefined();
-      });
-      it(`should call "getItemOnPress" with ("${value}", ${action})`, () => {
-        renderComponent(currentOptions);
-        expect(currentOptions.getItemOnPress).toHaveBeenCalledWith(
-          value,
-          action
-        );
-      });
+  describe("given an organizationFiscalCode", () => {
+    it("should render the organizationFiscalCode label", () => {
+      expect(
+        renderComponent(defaultProps).getByText(
+          capitalize(I18n.t("serviceDetail.fiscalCode"))
+        )
+      ).toBeDefined();
     });
-    describe(`when ${name} is not defined`, () => {
-      it(`should not render it`, () => {
-        expect(renderComponent(options).queryByText(value)).toBeNull();
-      });
+
+    it("should render the organizationFiscalCode value", () => {
+      expect(
+        renderComponent(defaultProps).getByText(
+          defaultProps.organizationFiscalCode
+        )
+      ).toBeDefined();
+    });
+
+    it(`should call "getItemOnPress" with (${defaultProps.organizationFiscalCode}, "COPY")`, () => {
+      renderComponent(defaultProps);
+      expect(defaultProps.getItemOnPress).toHaveBeenCalledWith(
+        defaultProps.organizationFiscalCode,
+        "COPY"
+      );
     });
   });
+
+  [["address", "via genova", "services.contactAddress", "MAP"]].forEach(
+    ([name, value, label, action]) => {
+      describe(`when ${name} is defined`, () => {
+        const currentOptions = {
+          ...defaultProps,
+          servicesMetadata: {
+            ...defaultServiceMetadata,
+            [name]: value
+          }
+        };
+        it(`should render its label "${label}"`, () => {
+          expect(
+            renderComponent(currentOptions).getByText(
+              capitalize(I18n.t(label as TranslationKeys))
+            )
+          ).toBeDefined();
+        });
+        it(`should render its value "${value}"`, () => {
+          expect(
+            renderComponent(currentOptions).getByText(value)
+          ).toBeDefined();
+        });
+        it(`should call "getItemOnPress" with ("${value}", ${action})`, () => {
+          renderComponent(currentOptions);
+          expect(currentOptions.getItemOnPress).toHaveBeenCalledWith(
+            value,
+            action
+          );
+        });
+      });
+    }
+  );
 
   [
     ["email", "jest@test.com", "global.media.email", "mailto:"],
@@ -98,11 +137,9 @@ describe("ServiceMetadata component", () => {
   ].forEach(([name, value, label, prefix]) => {
     describe(`when ${name} is defined`, () => {
       const currentOptions = {
-        ...options,
-        [name]: value,
-        getItemOnPress: jest.fn()
+        ...defaultProps,
+        servicesMetadata: { ...defaultServiceMetadata, [name]: value }
       };
-      beforeEach(() => currentOptions.getItemOnPress.mockReset());
       // eslint-disable-next-line sonarjs/no-identical-functions
       it(`should render its label "${label}"`, () => {
         expect(
@@ -121,26 +158,19 @@ describe("ServiceMetadata component", () => {
         );
       });
     });
-    // eslint-disable-next-line sonarjs/no-identical-functions
-    describe(`when ${name} is not defined`, () => {
-      it(`should not render it`, () => {
-        expect(renderComponent(options).queryByText(value)).toBeNull();
-      });
-    });
   });
 
   [
-    ["supportUrl", "www.support.it", "services.askForAssistance"],
-    ["webUrl", "www.product.it", "services.visitWebsite"],
-    ["iosStoreUrl", "www.ios.it", "services.otherAppIos"],
-    ["androidStoreUrl", "www.android.it", "services.otherAppAndroid"]
+    ["support_url", "www.support.it", "services.askForAssistance"],
+    ["web_url", "www.product.it", "services.visitWebsite"]
   ].forEach(([name, value, label]) => {
     const currentOptions = {
-      ...options,
-      [name]: value,
-      getItemOnPress: jest.fn()
+      ...defaultProps,
+      servicesMetadata: {
+        ...defaultServiceMetadata,
+        [name]: value
+      }
     };
-    beforeEach(() => currentOptions.getItemOnPress.mockReset());
     describe(`when ${name} is defined`, () => {
       it(`should render a link with "${label}"`, () => {
         const component = renderComponent(currentOptions);
@@ -155,25 +185,109 @@ describe("ServiceMetadata component", () => {
           const component = renderComponent(currentOptions);
           const link = component.getByRole("link");
           fireEvent(link, "onPress");
-          expect(mockOpenWebUrl).toHaveBeenCalledWith(
+          expect(spyOpenWebUrl).toHaveBeenCalledWith(
             value,
             expect.any(Function)
           );
         });
       });
     });
-    describe(`when ${name} is not defined`, () => {
+  });
+
+  describe("when the platform is Android", () => {
+    beforeAll(() => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("../../../utils/platform").test_setPlatform("android");
+    });
+
+    describe(`and servicesMetadata.app_android is defined`, () => {
+      const androidUrl = "http://www.android.google";
+      const currentOptions = {
+        ...defaultProps,
+        servicesMetadata: {
+          ...defaultServiceMetadata,
+          app_android: androidUrl
+        }
+      };
+      it(`should render the Android link`, () => {
+        const component = renderComponent(currentOptions);
+        const link = component.getByRole("link");
+        expect(link.children.toString()).toMatch(
+          I18n.t("services.otherAppAndroid")
+        );
+      });
+      it(`the link should open ${androidUrl}`, () => {
+        const component = renderComponent(currentOptions);
+        const link = component.getByRole("link");
+        fireEvent(link, "onPress");
+        expect(spyOpenWebUrl).toHaveBeenCalledWith(
+          androidUrl,
+          expect.any(Function)
+        );
+      });
+    });
+
+    describe(`and servicesMetadata.app_ios is defined`, () => {
+      const currentOptions = {
+        ...defaultProps,
+        servicesMetadata: {
+          ...defaultServiceMetadata,
+          app_ios: "dummy"
+        }
+      };
       it(`should not render it`, () => {
-        expect(renderComponent(options).queryByRole("link")).toBeNull();
+        expect(renderComponent(currentOptions).queryByRole("link")).toBeNull();
+      });
+    });
+  });
+
+  describe("when the platform is iOS", () => {
+    beforeAll(() => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("../../../utils/platform").test_setPlatform("ios");
+    });
+
+    describe(`and servicesMetadata.app_ios is defined`, () => {
+      const iosUrl = "http://www.ios.apple";
+      const currentOptions = {
+        ...defaultProps,
+        servicesMetadata: {
+          ...defaultServiceMetadata,
+          app_ios: iosUrl
+        }
+      };
+      it(`should render the iOS link`, () => {
+        const component = renderComponent(currentOptions);
+        const link = component.getByRole("link");
+        expect(link.children.toString()).toMatch(
+          I18n.t("services.otherAppIos")
+        );
+      });
+      it(`the link should open ${iosUrl}`, () => {
+        const component = renderComponent(currentOptions);
+        const link = component.getByRole("link");
+        fireEvent(link, "onPress");
+        expect(spyOpenWebUrl).toHaveBeenCalledWith(
+          iosUrl,
+          expect.any(Function)
+        );
+      });
+    });
+
+    describe(`and servicesMetadata.app_android is defined`, () => {
+      const currentOptions = {
+        ...defaultProps,
+        servicesMetadata: {
+          ...defaultServiceMetadata,
+          app_android: "dummy"
+        }
+      };
+      it(`should not render it`, () => {
+        expect(renderComponent(currentOptions).queryByRole("link")).toBeNull();
       });
     });
   });
 });
-
-const options = {
-  getItemOnPress: jest.fn(),
-  serviceId: "ABC123"
-};
 
 function renderComponent(props: Parameters<typeof ServiceMetadata>[0]) {
   return render(<ServiceMetadata {...props} />);
