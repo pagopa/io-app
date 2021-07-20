c5ee982d59568c36234ef4af66cd4e9757b0e1f5
Simone <simone.biffi.91@gmail.com>
2021-07-20 17:10:53 +0200

    add siciliaVola client

---
+++ ts/features/bonus/siciliaVola/api/backendSiciliaVola.ts
@@ -0,0 +1,41 @@
+import {
+  getMitVoucherTokenDefaultDecoder,
+  GetMitVoucherTokenT
+} from "../../../../../definitions/io_sicilia_vola_token/requestTypes";
+import {
+  ApiHeaderJson,
+  composeHeaderProducers,
+  createFetchRequestForApi
+} from "italia-ts-commons/lib/requests";
+import {
+  tokenHeaderProducer,
+  withBearerToken as withToken
+} from "../../../../utils/api";
+import { SessionToken } from "../../../../types/SessionToken";
+import { defaultRetryingFetch } from "../../../../utils/fetch";
+
+const GetMitVoucherToken: GetMitVoucherTokenT = {
+  method: "get",
+  url: () => "/api/v1/mitvoucher/auth/token",
+  query: _ => ({}),
+  headers: composeHeaderProducers(tokenHeaderProducer, ApiHeaderJson),
+  response_decoder: getMitVoucherTokenDefaultDecoder()
+};
+
+// client for SiciliaVola to handle API communications
+export const BackendSiciliaVolaClient = (
+  baseUrl: string,
+  token: SessionToken,
+  fetchApi: typeof fetch = defaultRetryingFetch()
+) => {
+  const options = {
+    baseUrl,
+    fetchApi
+  };
+  const withBearerToken = withToken(token);
+  return {
+    GetMitVoucherToken: withBearerToken(
+      createFetchRequestForApi(GetMitVoucherToken, options)
+    )
+  };
+};
