b5f54d571e5c041fb54938fd8eeb45974cdb6694
Simone <simone.biffi.91@gmail.com>
2021-07-29 11:39:39 +0200

    add voucherRevocation handler

--- ts/features/bonus/siciliaVola/store/actions/voucherList.ts
+++ ts/features/bonus/siciliaVola/store/actions/voucherList.ts
@@ -1,6 +1,6 @@
 import { ActionType, createAsyncAction } from "typesafe-actions";
 import { NetworkError } from "../../../../../utils/errors";
-import { SvVoucher } from "../../types/SvVoucher";
+import { SvVoucher, SvVoucherId } from "../../types/SvVoucher";
 import { VoucherBeneficiarioInputBean } from "../../../../../../definitions/api_sicilia_vola/VoucherBeneficiarioInputBean";
 import { SvVoucherListResponse } from "../../types/SvVoucherResponse";
 
@@ -22,6 +22,16 @@ export const svVoucherDetailGet = createAsyncAction(
   "SV_VOUCHER_DETAIL_GET_FAILURE"
 )<number, SvVoucher, NetworkError>();
 
+/**
+ * handle the voucher revocation
+ */
+export const svVoucherRevocation = createAsyncAction(
+  "SV_VOUCHER_REVOCATION_REQUEST",
+  "SV_VOUCHER_REVOCATION_GET_SUCCESS",
+  "SV_VOUCHER_REVOCATION_GET_FAILURE"
+)<SvVoucherId, void, NetworkError>();
+
 export type SvVoucherListActions =
   | ActionType<typeof svVoucherListGet>
-  | ActionType<typeof svVoucherDetailGet>;
+  | ActionType<typeof svVoucherDetailGet>
+  | ActionType<typeof svVoucherRevocation>;
