ef42c6a919a5811dfd25f8ed0f1acd9f98c2e0f2
Simone <simone.biffi.91@gmail.com>
2021-08-02 17:48:01 +0200

    fix lint error

--- ts/features/bonus/siciliaVola/screens/voucherGeneration/SelectBeneficiaryCategoryScreen.tsx
+++ ts/features/bonus/siciliaVola/screens/voucherGeneration/SelectBeneficiaryCategoryScreen.tsx
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { SafeAreaView, ScrollView } from "react-native";
+import { View } from "native-base";
 import BaseScreenComponent from "../../../../../components/screens/BaseScreenComponent";
 import { emptyContextualHelp } from "../../../../../utils/emptyContextualHelp";
 import { IOStyles } from "../../../../../components/core/variables/IOStyles";
@@ -25,7 +26,6 @@ import {
 } from "../../navigation/actions";
 import I18n from "../../../../../i18n";
 import { RadioButtonList } from "../../../../../components/core/selection/RadioButtonList";
-import { View } from "native-base";
 
 type BeneficiaryCategory = SvBeneficiaryCategory | "other";
 
