dccdbadc8442142471ea5bd2241a82e0840f7fe8
Fabrizio <fartux@gmail.com>
2021-08-03 19:02:50 +0200

    add expirationDate

--- ts/features/wallet/creditCard/component/CreditCardComponent.tsx
+++ ts/features/wallet/creditCard/component/CreditCardComponent.tsx
@@ -1,11 +1,16 @@
+import { View } from "native-base";
 import * as React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { Body } from "../../../../components/core/typography/Body";
+import { H5 } from "../../../../components/core/typography/H5";
 import { getCardIconFromBrandLogo } from "../../../../components/wallet/card/Logo";
+import I18n from "../../../../i18n";
 import { profileNameSurnameSelector } from "../../../../store/reducers/profile";
 import { GlobalState } from "../../../../store/reducers/types";
 import { CreditCardPaymentMethod } from "../../../../types/pagopa";
+import { isPaymentMethodExpired } from "../../../../utils/paymentMethod";
+import { buildExpirationDate } from "../../../../utils/stringBuilder";
 import BaseCardComponent from "../../component/card/BaseCardComponent";
 import { BlurredPan } from "../../component/card/BlurredPan";
 import { BrandImage } from "../../component/card/BrandImage";
@@ -18,9 +23,24 @@ type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps> &
   OwnProps;
 
+const topLeft = (creditCard: CreditCardPaymentMethod) => {
+  const expirationDate = buildExpirationDate(creditCard.info);
+  const isCardExpired = isPaymentMethodExpired(creditCard).getOrElse(false);
+
+  return (
+    <>
+      <BlurredPan>{creditCard.caption}</BlurredPan>
+      <View spacer={true} small={true} />
+      <H5 color={isCardExpired ? "red" : "bluegreyDark"} weight={"Regular"}>
+        {`${I18n.t("cardComponent.validUntil")} ${expirationDate}`}
+      </H5>
+    </>
+  );
+};
+
 const CreditCardComponent = (props: Props): React.ReactElement => (
   <BaseCardComponent
-    topLeftCorner={<BlurredPan>{props.creditCard.caption}</BlurredPan>}
+    topLeftCorner={topLeft(props.creditCard)}
     bottomLeftCorner={<Body>{props.nameSurname?.toLocaleUpperCase()}</Body>}
     bottomRightCorner={
       <BrandImage image={getCardIconFromBrandLogo(props.creditCard.info)} />
