69f62e87312d148cf5fa34342d2490f119c36c8b
Simone <simone.biffi.91@gmail.com>
2021-08-09 14:24:52 +0200

    wip picker

--- ts/features/bonus/siciliaVola/screens/voucherGeneration/StudentSelectDestinationScreen.tsx
+++ ts/features/bonus/siciliaVola/screens/voucherGeneration/StudentSelectDestinationScreen.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import { isSome } from "fp-ts/lib/Option";
 import BaseScreenComponent from "../../../../../components/screens/BaseScreenComponent";
 import { emptyContextualHelp } from "../../../../../utils/emptyContextualHelp";
@@ -20,9 +20,27 @@ import { University } from "../../types/SvVoucherRequest";
 import { selectedBeneficiaryCategorySelector } from "../../store/reducers/voucherRequest";
 import { navigateToSvSelectFlightsDateScreen } from "../../navigation/actions";
 import I18n from "../../../../../i18n";
+import { Picker } from "native-base";
+import IconFont from "../../../../../components/ui/IconFont";
+import variables from "../../../../../theme/variables";
+import { IOColors } from "../../../../../components/core/variables/IOColors";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
+
+const styles = StyleSheet.create({
+  container: { borderBottomWidth: 1, borderColor: IOColors.bluegreyLight },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  icon: {
+    height: 24,
+    width: 24,
+    marginBottom: 5
+  }
+});
 
 const StudentSelectDestinationScreen = (
   props: Props
@@ -48,7 +66,7 @@ const StudentSelectDestinationScreen = (
     props.failure("The selected category is not Student");
     return null;
   }
-
+  const [selectedValue, setSelectedValue] = useState();
   return (
     <BaseScreenComponent
       goBack={true}
@@ -66,7 +84,50 @@ const StudentSelectDestinationScreen = (
               "bonus.sv.voucherGeneration.student.selectDestination.title"
             )}
           </H1>
+          <Picker
+            /*style={{ position: "absolute", bottom: 0, left: 0, right: 0 }}*/
+            enabled={true}
+            mode={"dropdown"}
+            collapsable={true}
+            selectedValue={selectedValue}
+            onValueChange={(itemValue, _) => setSelectedValue(itemValue)}
+            headerTitleStyle={{
+              fontFamily: "Titillium Web",
+              color: "black",
+              textAlign: "auto",
+              paddingLeft: 15
+            }}
+            headerBackButtonTextStyle={{
+              fontFamily: "Titillium Web",
+              color: "black",
+              fontSize: 16,
+              textAlign: "auto",
+              lineHeight: undefined,
+              paddingLeft: 0,
+              fontWeight: "300"
+            }}
+            placeholder={"seleziona una scelta"}
+            iosIcon={
+              <IconFont
+                size={variables.iconSize3}
+                color={IOColors.blue}
+                name={"io-calendario"}
+                selectionColor={IOColors.blue}
+                style={styles.icon}
+              />
+            }
+            style={{
+              flex: 1,
+              justifyContent: "space-between",
+              alignSelf: "auto"
+            }}
+          >
+            <Picker.Item label="Office eg: VC, DSA" value="" color="#c1c1c1" />
+            <Picker.Item label={"pippo"} value={"pippo"} />
+            <Picker.Item label={"pluto"} value={"pluto"} />
+          </Picker>
         </ScrollView>
+
         <FooterWithButtons
           type={"TwoButtonsInlineHalf"}
           leftButton={backButtonProps}
