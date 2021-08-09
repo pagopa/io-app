0317b1c4034b48678ae4d36f55bd43cd6eaa6319
fabriziofff <fartux@gmail.com>
2021-08-09 10:15:15 +0200

    feat: [IA-165] Rework payment method details screen & add functionality to change payment method status (#3274)
    * renaming
    
    * refactoring
    
    * change copy and add initiative icon
    
    * complete refactoring without feature flag
    
    * refactoring
    
    * refactoring
    
    * doesn't render initiative block if no initiative are enabled
    
    * move favorite logic inside the component
    
    * include FavoirtePaymentMethodSwitch inside the PaymentSettings
    
    * clean import
    
    * move pagopacapability
    
    * change payment supported
    
    * add feature flag
    
    * add RemoteSwitch
    
    * add missing files
    
    * add paymentStatusSwitch
    
    * changelayout
    
    * link all together!
    
    * fix
    
    * fix eslint
    
    * fix key
    
    * fix test
    
    * refactoring walletv2 preview card
    
    * change sorting and add CreditCardWalletPreview
    
    * add credit card details
    
    * add brandlogo
    
    * add expirationDate
    
    * add payment method details screen
    
    * clean locales
    
    * add TODO
    
    * remove unused screen
    
    * fix test
    
    * fix test
    
    * fix test
    
    * avoid rendering error at first render
    
    * change
    
    * add highlevel test for CreditCardDetailScreen
    
    * update copy
    
    * Update ts/features/wallet/common/BasePaymentMethodScreen.tsx
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * change in DeletButtono
    
    * Update ts/features/wallet/component/card/WalletV2PreviewCards.tsx
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>
    
    * add feature flag
    
    * remove unused config
    
    * fix typo
    
    * update pot
    
    * update routes
    
    * refactoring
    
    * refactoring back
    
    * refactoring cobadge
    
    Co-authored-by: Matteo Boschi <mbmaken@gmail.com>

--- ts/features/wallet/component/features/PagoPaPaymentCapability.tsx
+++ ts/features/wallet/component/features/PagoPaPaymentCapability.tsx
@@ -1,32 +1,24 @@
 import { Badge, View } from "native-base";
 import * as React from "react";
 import { StyleSheet } from "react-native";
-import ButtonDefaultOpacity from "../../../components/ButtonDefaultOpacity";
-import { H4 } from "../../../components/core/typography/H4";
-import { H5 } from "../../../components/core/typography/H5";
-import { Link } from "../../../components/core/typography/Link";
-import { IOColors } from "../../../components/core/variables/IOColors";
-import { IOStyles } from "../../../components/core/variables/IOStyles";
-import TouchableDefaultOpacity from "../../../components/TouchableDefaultOpacity";
-import Markdown from "../../../components/ui/Markdown";
-import Switch from "../../../components/ui/Switch";
-import I18n from "../../../i18n";
-import { PaymentMethod } from "../../../types/pagopa";
-import { PaymentSupportStatus } from "../../../types/paymentMethodCapabilities";
-import { useIOBottomSheet } from "../../../utils/bottomSheet";
-import { isPaymentMethodSupported } from "../../../utils/paymentMethodCapabilities";
-import { openWebUrl } from "../../../utils/url";
+import ButtonDefaultOpacity from "../../../../components/ButtonDefaultOpacity";
+import { H5 } from "../../../../components/core/typography/H5";
+import { Link } from "../../../../components/core/typography/Link";
+import { IOColors } from "../../../../components/core/variables/IOColors";
+import TouchableDefaultOpacity from "../../../../components/TouchableDefaultOpacity";
+import Markdown from "../../../../components/ui/Markdown";
+import Switch from "../../../../components/ui/Switch";
+import { pmActivatePaymentEnabled } from "../../../../config";
+import I18n from "../../../../i18n";
+import { PaymentMethod } from "../../../../types/pagopa";
+import { PaymentSupportStatus } from "../../../../types/paymentMethodCapabilities";
+import { useIOBottomSheet } from "../../../../utils/bottomSheet";
+import { isPaymentSupported } from "../../../../utils/paymentMethodCapabilities";
+import { openWebUrl } from "../../../../utils/url";
+import { BasePaymentFeatureListItem } from "./BasePaymentFeatureListItem";
+import PaymentStatusSwitch from "./PaymentStatusSwitch";
 
 const styles = StyleSheet.create({
-  row: {
-    paddingVertical: 16,
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
-  left: {
-    ...IOStyles.flex,
-    paddingRight: 8
-  },
   badgeInfo: {
     borderWidth: 1,
     borderStyle: "solid",
@@ -66,11 +58,16 @@ const getLocales = () => ({
   incompatible: I18n.t("wallet.methods.card.pagoPaCapability.incompatible")
 });
 
-const availabilityBadge = (badgeType: PaymentSupportStatus) => {
+const availabilityBadge = (
+  badgeType: PaymentSupportStatus,
+  paymentMethod: PaymentMethod
+) => {
   const { available, arriving, incompatible } = getLocales();
   switch (badgeType) {
     case "available":
-      return (
+      return pmActivatePaymentEnabled ? (
+        <PaymentStatusSwitch paymentMethod={paymentMethod} />
+      ) : (
         <Badge style={[styles.badgeInfo, styles.badgeAvailable]}>
           <H5 color="white">{available}</H5>
         </Badge>
@@ -81,7 +78,7 @@ const availabilityBadge = (badgeType: PaymentSupportStatus) => {
           <H5 color="blue">{arriving}</H5>
         </Badge>
       );
-    case "not_available":
+    case "notAvailable":
       return (
         <Badge style={[styles.badgeNotAvailable, styles.badgeInfo]}>
           <H5 color="blue">{incompatible}</H5>
@@ -96,15 +93,16 @@ const availabilityBadge = (badgeType: PaymentSupportStatus) => {
  * Represent the capability to pay in PagoPa of a payment method.
  *
  * We have 4 possible different cases:
- *   - The card can pay on IO -> pagoPa === true
- *   - The card in the future can pay on IO -> Satispay, BPay
- *   - The card is not abilitated to pay on IO, pagoPa === false and type === PRV or Bancomat
- *   - The card can onboard another card that can pay on IO -> co-badge credit card pagoPa === false and type !== PRV
+ *   - The card can pay on IO -> has capability pagoPa
+ *   - The card will be able to pay in the future on IO -> Satispay, BPay
+ *   - The card is not able to pay on IO, (no pagoPa capability) and type === PRV or Bancomat
+ *   - The card can onboard another card that can pay on IO -> co-badge credit card (no pagoPa capability) and type !== PRV
  * @param props
  */
 const PagoPaPaymentCapability: React.FC<Props> = props => {
   const onOpenLearnMoreAboutInAppPayments = () =>
     openWebUrl(IN_APP_PAYMENTS_LEARN_MORE_VIDEO_URL);
+  const paymentSupported = isPaymentSupported(props.paymentMethod);
 
   const { present } = useIOBottomSheet(
     <View>
@@ -126,26 +124,19 @@ const PagoPaPaymentCapability: React.FC<Props> = props => {
   );
 
   return (
-    <TouchableDefaultOpacity onPress={present}>
-      <View style={styles.row}>
-        <View style={styles.left}>
-          <H4
-            weight={"SemiBold"}
-            color={"bluegreyDark"}
-            testID={"PagoPaPaymentCapabilityTitle"}
-          >
-            {I18n.t("wallet.methods.card.pagoPaCapability.title")}
-          </H4>
-          <H5
-            weight={"Regular"}
-            color={"bluegrey"}
-            testID={"PagoPaPaymentCapabilityDescription"}
-          >
-            {I18n.t("wallet.methods.card.pagoPaCapability.description")}
-          </H5>
-        </View>
-        {availabilityBadge(isPaymentMethodSupported(props.paymentMethod))}
-      </View>
+    <TouchableDefaultOpacity
+      onPress={
+        paymentSupported === "available" && pmActivatePaymentEnabled
+          ? undefined
+          : present
+      }
+    >
+      <BasePaymentFeatureListItem
+        testID={"PagoPaPaymentCapability"}
+        title={I18n.t("wallet.methods.card.pagoPaCapability.title")}
+        description={I18n.t("wallet.methods.card.pagoPaCapability.description")}
+        rightElement={availabilityBadge(paymentSupported, props.paymentMethod)}
+      />
     </TouchableDefaultOpacity>
   );
 };
