e8f1adf3ee005997a8795152846ef51582dee060
Fabrizio <fartux@gmail.com>
2021-08-10 10:02:01 +0200

    missing directory

--- scripts/ios-release-build.sh
+++ scripts/ios-release-build.sh
@@ -3,5 +3,6 @@
 cp .env.production .env
 yarn postinstall
 yarn cie-ios:ci
+mkdir ./fastlane
 touch ./fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
 echo -e "$APP_STORE_API_PRIVATE_KEY" > ./fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
