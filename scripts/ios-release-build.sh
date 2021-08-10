85c605658f14fd158d47370c96f017b5d228a977
Fabrizio <fartux@gmail.com>
2021-08-10 11:55:11 +0200

    fix path

--- scripts/ios-release-build.sh
+++ scripts/ios-release-build.sh
@@ -3,6 +3,5 @@
 cp .env.production .env
 yarn postinstall
 yarn cie-ios:ci
-mkdir ./fastlane
-touch ./fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
-echo -e "$APP_STORE_API_PRIVATE_KEY" > ./fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
+touch ./ios/fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
+echo -e "$APP_STORE_API_PRIVATE_KEY" > ./ios/fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
