ba56060bcf5c0620bbfe9511366419a05de5f19f
Fabrizio <fartux@gmail.com>
2021-08-10 09:56:28 +0200

    add steps

--- scripts/ios-release-build.sh
+++ scripts/ios-release-build.sh
@@ -3,3 +3,5 @@
 cp .env.production .env
 yarn postinstall
 yarn cie-ios:ci
+touch ./fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
+echo -e "$APP_STORE_API_PRIVATE_KEY" > ./fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
