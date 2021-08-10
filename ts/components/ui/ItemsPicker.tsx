4a9df1d153177767a19478b9ba39a0533366c408
Simone <simone.biffi.91@gmail.com>
2021-08-10 14:58:00 +0200

    wip picker

--- ts/components/ui/ItemsPicker.tsx
+++ ts/components/ui/ItemsPicker.tsx
@@ -1,19 +1,21 @@
 import * as React from "react";
 import { useState } from "react";
 import { Picker } from "native-base";
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import ArrowDown from "../../../img/features/siciliaVola/arrowDown.svg";
 import variables from "../../theme/variables";
 import { isAndroid } from "../../utils/platform";
+import { IOColors } from "../core/variables/IOColors";
+import { fromNullable } from "fp-ts/lib/Option";
 
 type PickerItem = {
   label: string;
-  value: string;
+  value?: string;
 };
 
 type Props = {
   items: ReadonlyArray<PickerItem>;
-  // onValueChange: (value: string, key: number) => void;
+  onValueChange: (value: string) => void;
   placeholder?: string;
   selectedValue?: string;
 };
@@ -41,6 +43,36 @@ const styles = StyleSheet.create({
   }
 });
 
+const addAndroidPlaceholder = (
+  placeholder: string,
+  items: ReadonlyArray<PickerItem>
+): ReadonlyArray<PickerItem & { disabled?: boolean }> => {
+  const placeholderItem: PickerItem & { disabled?: boolean } = {
+    label: placeholder,
+    value: undefined,
+    disabled: true
+  };
+
+  return [placeholderItem].concat(
+    ...items.map(i => ({ ...i, disabled: false }))
+  );
+};
+
+const generateItems = (
+  items: ReadonlyArray<PickerItem>,
+  placeholder?: string
+) => {
+  if (isAndroid && placeholder) {
+    return addAndroidPlaceholder(placeholder, items).map(i => (
+      <Picker.Item
+        label={i.label}
+        value={undefined}
+        color={i.disabled ? IOColors.bluegreyLight : IOColors.bluegreyDark}
+      />
+    ));
+  }
+  return items.map(i => <Picker.Item label={i.label} value={i.value} />);
+};
 /**
  * Wrapper around the native-base Picker component.
  * in addition to the functionality of the native-base component it adds:
@@ -51,12 +83,16 @@ const styles = StyleSheet.create({
  */
 const ItemsPicker: React.FunctionComponent<Props> = (props: Props) => {
   const [selectedValue, setSelectedValue] = useState();
+
+  const handleValueChange = (value: string, _: number) =>
+    fromNullable(value).map(v => props.onValueChange(v));
+
   return (
     <Picker
       enabled={true}
       collapsable={true}
       selectedValue={selectedValue}
-      onValueChange={(itemValue, _) => setSelectedValue(itemValue)}
+      onValueChange={handleValueChange}
       headerTitleStyle={styles.headerTitle}
       headerBackButtonTextStyle={styles.headerBackButton}
       placeholder={props.placeholder}
@@ -65,11 +101,13 @@ const ItemsPicker: React.FunctionComponent<Props> = (props: Props) => {
       }
       style={styles.picker}
     >
-      {props.placeholder && Platform.OS === "android" && (
-        <Picker.Item label={props.placeholder} value="" color="#c1c1c1" />
+      {generateItems(
+        [
+          { value: "pippo", label: "pippo" },
+          { value: "pluto", label: "pluto" }
+        ],
+        props.placeholder
       )}
-      <Picker.Item label={"pippo"} value={"pippo"} />
-      <Picker.Item label={"pluto"} value={"pluto"} />
     </Picker>
   );
 };
