32c21570054adb5c13be5b837089bdd000683fbc
pietro909 <2094604+pietro909@users.noreply.github.com>
2021-08-11 17:55:26 +0200

    feat(Sicilia Vola): [IASV-34] Select region screen (#3288)
    * Region screen
    
    * Add tests and fix
    
    * Add accessibility attributes to radio buttons
    
    * Address review's comment
    
    * renaming
    
    * set continue button color
    
    Co-authored-by: Simone <simone.biffi.91@gmail.com>

---
+++ ts/features/bonus/siciliaVola/components/__tests__/CheckResidenceComponent.test.tsx
@@ -0,0 +1,104 @@
+import { NavigationParams } from "react-navigation";
+import configureMockStore from "redux-mock-store";
+import { fireEvent } from "@testing-library/react-native";
+import { applicationChangeState } from "../../../../../store/actions/application";
+import { GlobalState } from "../../../../../store/reducers/types";
+import { renderScreenFakeNavRedux } from "../../../../../utils/testWrapper";
+import { appReducer } from "../../../../../store/reducers";
+import ROUTES from "../../../../../navigation/routes";
+import I18n from "../../../../../i18n";
+import { svGenerateVoucherCancel } from "../../store/actions/voucherGeneration";
+import {
+  navigateToSvKoCheckResidenceScreen,
+  navigateToSvSelectBeneficiaryCategoryScreen
+} from "../../navigation/actions";
+
+import CheckResidenceComponent from "../CheckResidenceComponent";
+
+jest.useFakeTimers();
+
+describe("the CheckResidenceComponent", () => {
+  it("should render the title", () => {
+    const { component } = renderComponent();
+    expect(
+      component.getByText(
+        I18n.t("bonus.sv.voucherGeneration.checkResidence.title")
+      )
+    ).toBeDefined();
+  });
+
+  it("should render two radio buttons", () => {
+    const { component } = renderComponent();
+    const itemYes = component.getByText(
+      I18n.t("bonus.sv.voucherGeneration.checkResidence.items.inSicily")
+    );
+    expect(itemYes).toBeDefined();
+    const itemNo = component.getByText(
+      I18n.t("bonus.sv.voucherGeneration.checkResidence.items.notInSicily")
+    );
+    expect(itemNo).toBeDefined();
+  });
+
+  it("should render the information text", () => {
+    const { component } = renderComponent();
+    expect(
+      component.getByText(
+        I18n.t("bonus.sv.voucherGeneration.checkResidence.info")
+      )
+    ).toBeDefined();
+  });
+
+  describe("when 'I reside in Sicily' is checked", () => {
+    it("the continue button should navigate to SelectBeneficiaryCategory screen", () => {
+      const { component, store } = renderComponent();
+      const itemYes = component.getByText(
+        I18n.t("bonus.sv.voucherGeneration.checkResidence.items.inSicily")
+      );
+      fireEvent(itemYes, "onPress");
+      const continueButton = component.getByText(
+        I18n.t("global.buttons.continue")
+      );
+      fireEvent(continueButton, "onPress");
+      expect(store.getActions()).toEqual([
+        navigateToSvSelectBeneficiaryCategoryScreen()
+      ]);
+    });
+  });
+
+  describe("when 'I reside in another region' is checked", () => {
+    const { component, store } = renderComponent();
+    const itemYes = component.getByText(
+      I18n.t("bonus.sv.voucherGeneration.checkResidence.items.notInSicily")
+    );
+    fireEvent(itemYes, "onPress");
+    const continueButton = component.getByText(
+      I18n.t("global.buttons.continue")
+    );
+    fireEvent(continueButton, "onPress");
+    expect(store.getActions()).toEqual([navigateToSvKoCheckResidenceScreen()]);
+  });
+
+  describe("when the cancel button is pressed", () => {
+    it("should dispatch a `svGenerateVoucherCancel` action", () => {
+      const { component, store } = renderComponent();
+      const cancelButton = component.getByText(I18n.t("global.buttons.cancel"));
+      fireEvent(cancelButton, "onPress");
+      expect(store.getActions()).toEqual([svGenerateVoucherCancel()]);
+    });
+  });
+});
+
+function renderComponent() {
+  const globalState = appReducer(undefined, applicationChangeState("active"));
+  const mockStore = configureMockStore<GlobalState>();
+  const store = mockStore(globalState);
+  return {
+    component: renderScreenFakeNavRedux<GlobalState, NavigationParams>(
+      CheckResidenceComponent,
+      ROUTES.MAIN,
+      {},
+      store
+    ),
+    store
+  };
+}
