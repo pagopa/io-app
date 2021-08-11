d39adf86c2aa7fad5e18b9f25258a75ce930ad3c
Fabrizio <fartux@gmail.com>
2021-08-11 10:00:15 +0200

    clean

--- scripts/ios-release-build.sh
+++ scripts/ios-release-build.sh
@@ -5,9 +5,3 @@ yarn postinstall
 yarn cie-ios:ci
 touch ./ios/fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
 echo -e "$APP_STORE_API_PRIVATE_KEY" > ./ios/fastlane/AuthKey_$APP_STORE_API_KEY_ID.p8
-
-# read the previous tag in order to generate the Test changelog
-# PREVIOUS_TAG=$(git tag --sort=-taggerdate | grep \\-rc\\. | sed -n '2 p')
-# echo $PREVIOUS_TAG
-# touch ./ios/fastlane/previous_tag
-# echo -e "$PREVIOUS_TAG" > ./ios/fastlane/previous_tag
