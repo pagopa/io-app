657d82ec374425d7353dc6163cdac6a304e41333
Simone <simone.biffi.91@gmail.com>
2021-08-11 14:58:10 +0200

    modify municipalities reducer and add selectors

--- ts/features/bonus/siciliaVola/store/reducers/availableMunicipalities.ts
+++ ts/features/bonus/siciliaVola/store/reducers/availableMunicipalities.ts
@@ -1,6 +1,9 @@
-import * as pot from "italia-ts-commons/lib/pot";
 import { getType } from "typesafe-actions";
-import { IndexedById, toIndexed } from "../../../../../store/helpers/indexer";
+import {
+  IndexedById,
+  toArray,
+  toIndexed
+} from "../../../../../store/helpers/indexer";
 import { Municipality } from "../../types/SvVoucherRequest";
 import { NetworkError } from "../../../../../utils/errors";
 import { Action } from "../../../../../store/actions/types";
@@ -11,12 +14,22 @@ import {
   svGenerateVoucherAvailableState,
   svGenerateVoucherStart
 } from "../actions/voucherGeneration";
+import {
+  remoteError,
+  remoteLoading,
+  remoteReady,
+  remoteUndefined,
+  RemoteValue
+} from "../../../bpd/model/RemoteValue";
+import { createSelector } from "reselect";
+import { GlobalState } from "../../../../../store/reducers/types";
+import { PickerItems } from "../../../../../components/ui/ItemsPicker";
 
-export type AvailableMunicipalitiesState = pot.Pot<
+export type AvailableMunicipalitiesState = RemoteValue<
   IndexedById<Municipality>,
   NetworkError
 >;
-const INITIAL_STATE: AvailableMunicipalitiesState = pot.none;
+const INITIAL_STATE: AvailableMunicipalitiesState = remoteUndefined;
 
 const reducer = (
   state: AvailableMunicipalitiesState = INITIAL_STATE,
@@ -24,21 +37,45 @@ const reducer = (
 ): AvailableMunicipalitiesState => {
   switch (action.type) {
     case getType(svGenerateVoucherStart):
-      return INITIAL_STATE;
     case getType(svGenerateVoucherAvailableState.request):
-      return pot.none;
     case getType(svGenerateVoucherAvailableRegion.request):
-      return pot.none;
     case getType(svGenerateVoucherAvailableProvince.request):
-      return pot.none;
+      return INITIAL_STATE;
     case getType(svGenerateVoucherAvailableMunicipality.request):
-      return pot.toLoading(state);
+      return remoteLoading;
     case getType(svGenerateVoucherAvailableMunicipality.success):
-      return pot.some(toIndexed(action.payload, m => m.id));
+      return remoteReady(toIndexed(action.payload, m => m.id));
     case getType(svGenerateVoucherAvailableMunicipality.failure):
-      return pot.toError(state, action.payload);
+      return remoteError(action.payload);
   }
   return state;
 };
 
 export default reducer;
+
+export const availableMunicipalitiesSelector = createSelector(
+  [
+    (state: GlobalState) =>
+      state.bonus.sv.voucherGeneration.availableMunicipalities
+  ],
+  (
+    availableMunicipalities: AvailableMunicipalitiesState
+  ): AvailableMunicipalitiesState => availableMunicipalities
+);
+
+export const availableMunicipalitiesItemsSelector = createSelector(
+  [availableMunicipalitiesSelector],
+  (availableMunicipalities: AvailableMunicipalitiesState): PickerItems => {
+    switch (availableMunicipalities.kind) {
+      case "ready":
+        return toArray(availableMunicipalities.value).map(r => ({
+          value: r.id,
+          label: r.name
+        }));
+      case "error":
+      case "loading":
+      case "undefined":
+        return [];
+    }
+  }
+);
