e3fcdc572d5b8443100bea483dbc7585cd618514
Simone <simone.biffi.91@gmail.com>
2021-09-07 17:59:35 +0200

    update voucher list screen

--- ts/features/bonus/siciliaVola/screens/VoucherListScreen.tsx
+++ ts/features/bonus/siciliaVola/screens/VoucherListScreen.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useContext, useEffect } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { SafeAreaView, ScrollView } from "react-native";
+import { FlatList, SafeAreaView } from "react-native";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
@@ -15,26 +15,38 @@ import {
   LightModalContext
 } from "../../../../components/ui/LightModal";
 import SvVoucherListFilters from "../components/SvVoucherListFilters";
-import { svPossibleVoucherStateGet } from "../store/actions/voucherList";
+import {
+  svPossibleVoucherStateGet,
+  svRequestVoucherPage,
+  svResetFilter
+} from "../store/actions/voucherList";
+import ListItemComponent from "../../../../components/screens/ListItemComponent";
+import ItemSeparatorComponent from "../../../../components/ItemSeparatorComponent";
+import { svVouchersSelector } from "../store/reducers/voucherList/vouchers";
+import { toArray } from "../../../../store/helpers/indexer";
+import { formatDateAsLocal } from "../../../../utils/dates";
+import { View } from "native-base";
+import {
+  FilterState,
+  svFiltersSelector
+} from "../store/reducers/voucherList/filters";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
-
+let foo = 1;
 const VoucherListScreen = (props: Props): React.ReactElement => {
   const { showAnimatedModal, hideModal } = useContext(LightModalContext);
 
+  const vouchers = toArray(props.indexedVouchers);
   useEffect(() => {
     props.requestVoucherState();
+    props.resetFilter();
   }, []);
 
   const openFiltersModal = () =>
     showAnimatedModal(
       // TODO replace onConfirm function when the search functionalities are defined
-      <SvVoucherListFilters
-        onClose={hideModal}
-        onConfirm={constNull}
-        isLocal={false}
-      />,
+      <SvVoucherListFilters onClose={hideModal} onConfirm={constNull} />,
       BottomTopAnimation
     );
 
@@ -46,16 +58,45 @@ const VoucherListScreen = (props: Props): React.ReactElement => {
       isSearchAvailable={{ enabled: true, onSearchTap: openFiltersModal }}
     >
       <SafeAreaView style={IOStyles.flex} testID={"VoucherListScreen"}>
-        <ScrollView style={[IOStyles.horizontalContentPadding]}>
-          <H1>{I18n.t("bonus.sv.voucherList.title")}</H1>
-        </ScrollView>
+        <H1>{I18n.t("bonus.sv.voucherList.title")}</H1>
+        <View spacer />
+        <FlatList
+          style={[IOStyles.horizontalContentPadding]}
+          data={vouchers}
+          keyExtractor={v => v.idVoucher.toString()}
+          ItemSeparatorComponent={() => (
+            <ItemSeparatorComponent noPadded={true} />
+          )}
+          onEndReached={() => {
+            if (foo < 5) {
+              props.requestVoucherPage({});
+              foo++;
+            }
+          }}
+          onEndReachedThreshold={0}
+          renderItem={v => (
+            <ListItemComponent
+              title={v.item.destination}
+              subTitle={formatDateAsLocal(v.item.departureDate, true, true)}
+              onPress={() => true}
+            />
+          )}
+          scrollEnabled={true}
+          keyboardShouldPersistTaps={"handled"}
+        />
       </SafeAreaView>
     </BaseScreenComponent>
   );
 };
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  requestVoucherState: () => dispatch(svPossibleVoucherStateGet.request())
+  requestVoucherState: () => dispatch(svPossibleVoucherStateGet.request()),
+  resetFilter: () => dispatch(svResetFilter({})),
+  requestVoucherPage: (filters: FilterState) =>
+    dispatch(svRequestVoucherPage(filters))
 });
-const mapStateToProps = (_: GlobalState) => ({});
+const mapStateToProps = (state: GlobalState) => ({
+  indexedVouchers: svVouchersSelector(state),
+  filters: svFiltersSelector(state)
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(VoucherListScreen);
