9c2ff704d0e1ebddbc6141ef43d5894f8e0bb4ca
Simone <simone.biffi.91@gmail.com>
2021-09-07 17:58:10 +0200

    add ui reducer

---
+++ ts/features/bonus/siciliaVola/store/reducers/voucherList/ui.ts
@@ -0,0 +1,59 @@
+import { getType } from "typesafe-actions";
+import { Action } from "../../../../../../store/actions/types";
+import {
+  svResetFilter,
+  svSetFilter,
+  svVoucherListGet
+} from "../../actions/voucherList";
+import {
+  remoteError,
+  remoteLoading,
+  remoteReady,
+  remoteUndefined,
+  RemoteValue
+} from "../../../../bpd/model/RemoteValue";
+import { svGenerateVoucherStart } from "../../actions/voucherGeneration";
+import { NetworkError } from "../../../../../../utils/errors";
+import { createSelector } from "reselect";
+import { GlobalState } from "../../../../../../store/reducers/types";
+
+export type VouchersListUiState = {
+  nextPage?: number;
+  pagination: true;
+  elementNumber: number;
+  requiredDataLoaded: RemoteValue<true, NetworkError>;
+};
+
+const INITIAL_STATE: VouchersListUiState = {
+  nextPage: 1,
+  elementNumber: 5,
+  pagination: true,
+  requiredDataLoaded: remoteUndefined
+};
+
+const reducer = (
+  state: VouchersListUiState = INITIAL_STATE,
+  action: Action
+): VouchersListUiState => {
+  switch (action.type) {
+    case getType(svGenerateVoucherStart):
+    case getType(svSetFilter):
+    case getType(svResetFilter):
+      return INITIAL_STATE;
+    case getType(svVoucherListGet.request):
+      return { ...state, requiredDataLoaded: remoteLoading };
+    case getType(svVoucherListGet.success):
+      return { ...state, requiredDataLoaded: remoteReady(true) };
+    case getType(svVoucherListGet.failure):
+      return { ...state, requiredDataLoaded: remoteError(action.payload) };
+  }
+
+  return state;
+};
+
+export const svVouchersListUiSelector = createSelector(
+  [(state: GlobalState) => state.bonus.sv.voucherList.ui],
+  (uiParameters: VouchersListUiState): VouchersListUiState => uiParameters
+);
+
+export default reducer;
--- ts/features/bonus/siciliaVola/store/reducers/voucherList/voucherList.ts
+++ ts/features/bonus/siciliaVola/store/reducers/voucherList/voucherList.ts
@@ -6,17 +6,20 @@ import svPossibleVoucherStateReducer, {
 import { combineReducers } from "redux";
 import svVouchersStateReducer, { VouchersState } from "./vouchers";
 import svFilterStateReducer, { FilterState } from "./filters";
+import svVouchersListUiReducer, { VouchersListUiState } from "./ui";
 
 export type VoucherListState = {
   possibleVoucherState: PossibleVoucherStateState;
   vouchers: VouchersState;
   filters: FilterState;
+  ui: VouchersListUiState;
 };
 
 const svVoucherListReducer = combineReducers<VoucherListState, Action>({
   possibleVoucherState: svPossibleVoucherStateReducer,
   vouchers: svVouchersStateReducer,
-  filters: svFilterStateReducer
+  filters: svFilterStateReducer,
+  ui: svVouchersListUiReducer
 });
 
 export default svVoucherListReducer;
