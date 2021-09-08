724cdbe6600ba547ca2de9e11805137a9bc2b41a
Pietro Grandi <pietro.grandi.3d@gmail.com>
2021-09-08 12:11:42 +0200

    Only remove unused deps

--- ts/utils/markdown.ts
+++ ts/utils/markdown.ts
@@ -1,9 +1,9 @@
-import { remark } from "remark";
+import remark from "remark";
 import remarkCustomBlocks from "remark-custom-blocks";
 import remarkHtml from "remark-html";
 
 // Configuration fo remark-custom-blocks
-const REMARK_CUSTOM_BLOCKS_CONFIG: any = {
+const REMARK_CUSTOM_BLOCKS_CONFIG = {
   "IO-DEMO": {
     classes: "io-demo-block"
   }
