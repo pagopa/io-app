cb4afee5d9af87dee0db9b0600cede7374466eef
Pietro Grandi <pietro.grandi.3d@gmail.com>
2021-09-08 12:11:42 +0200

    Only remove unused deps

--- ts/@types/remark-custom-blocks.d.ts
+++ ts/@types/remark-custom-blocks.d.ts
@@ -1,16 +1,3 @@
 declare module "remark-custom-blocks" {
-  type AvailableBlock = {
-    title: string;
-    details: string;
-    containerElement: string;
-    contentsElement: string;
-    classes: string;
-    titleElement: string;
-  };
-
-  type AvailableBlocks = { [index: string]: AvailableBlock };
-
-  export default function blockPlugin(
-    availableBlocks: AvailableBlocks = {}
-  ): void;
+  export default function (): any;
 }
