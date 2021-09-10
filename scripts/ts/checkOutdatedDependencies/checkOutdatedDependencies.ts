b4ae006efb6d2b68ed3d5142e507165d4ea8ff6a
Fabrizio <fartux@gmail.com>
2021-09-10 09:59:54 +0200

    better be safe!

--- scripts/ts/checkOutdatedDependencies/checkOutdatedDependencies.ts
+++ scripts/ts/checkOutdatedDependencies/checkOutdatedDependencies.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 import { exec } from "child_process";
 import * as util from "util";
+import { Either, left, right } from "fp-ts/lib/Either";
+import { Errors } from "io-ts";
 import { readableReport } from "italia-ts-commons/lib/reporters";
 import * as semver from "semver";
 import { slackPostMessage } from "../common/slack/postMessage";
@@ -16,22 +18,34 @@ const execAsync = util.promisify(exec);
 const majorVersionDeltaWarning = 3;
 
 /**
- * Execute the command "yarn outdated --json" and return the results as JSON any
+ * Execute the command "yarn outdated --json" and return the results as string
  */
-const readOutdatedJson = async (): Promise<any> => {
+const readOutdatedJson = async (): Promise<string> => {
   try {
     const std = await execAsync("yarn outdated --json");
-    return JSON.parse(std.stdout.split("\n")[1]);
+    return std.stdout;
   } catch (error) {
     // When the command finds outdated packages, return with exit code 1
     if (error.code === 1) {
-      return JSON.parse(error.stdout.split("\n")[1]);
+      return error.stdout;
     } else {
       throw new Error(
         `Error ${error.code} while executing 'yarn outdated --json': ${error.message}`
       );
     }
   }
+};
+
+/**
+ * Try to extract the second section of the stdout. The first section describe the table header, the second section the content.
+ * @param stdout
+ */
+const extractStdoutSection = (stdout: string): Either<Error | Errors, any> => {
+  const splittedErrorStdout = stdout.split("\n");
+  if (splittedErrorStdout.length > 1) {
+    return right(JSON.parse(splittedErrorStdout[1]));
+  }
+  return left(new Error("stdout splitting has the wrong size"));
 };
 
 /**
@@ -88,9 +102,10 @@ const main = async () => {
     console.log("Executing yarn outdated --json");
     const rawJSON = await readOutdatedJson();
     console.log("Convert the json to OutdatedStats");
-    const outdatedPackages = DependenciesTable.decode(rawJSON).map(
-      extractGroupByType
-    );
+
+    const outdatedPackages = extractStdoutSection(rawJSON)
+      .chain(DependenciesTable.decode)
+      .map(extractGroupByType);
 
     if (outdatedPackages.isRight()) {
       console.log(`Send slack message to ${destinationChannel}`);
@@ -101,7 +116,11 @@ const main = async () => {
       );
     } else {
       console.log("Error while decoding the command output:");
-      console.log(readableReport(outdatedPackages.value));
+      const report =
+        outdatedPackages.value instanceof Error
+          ? outdatedPackages.value.message
+          : readableReport(outdatedPackages.value);
+      console.log(report);
     }
   } catch (e) {
     console.log("Generic error while executing the script:");
