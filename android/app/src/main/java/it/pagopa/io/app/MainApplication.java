0368239e069a8e0e3cf8ee8e80eb6551240145e2
Fabrizio <fartux@gmail.com>
2021-09-10 15:20:40 +0200

    changed reflection class

--- android/app/src/main/java/it/pagopa/io/app/MainApplication.java
+++ android/app/src/main/java/it/pagopa/io/app/MainApplication.java
@@ -76,7 +76,7 @@ public class MainApplication extends Application implements ReactApplication {
           We use reflection here to pick up the class that initializes Flipper,
          since Flipper library is not available in release mode
          */
-         Class<?> aClass = Class.forName("it.pagopa.io.app");
+         Class<?> aClass = Class.forName("it.pagopa.io.app.ReactNativeFlipper");
          aClass
            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
            .invoke(null, context, reactInstanceManager);
