ced2e17342f0d5371ce60afdc771470d6826a8c5
Fabrizio <fartux@gmail.com>
2021-09-16 15:47:59 +0200

    add rationale for WRITE_EXTERNAL_STORAGE permission

---
+++ ts/utils/asyncAlert.ts
@@ -0,0 +1,51 @@
+import { Alert, AlertButton, AlertOptions } from "react-native";
+
+/**
+ * The result of the Alert.
+ * - onPress: one button was pressed
+ * - onDismiss: The user tapped on empty space, dismissing the alert
+ */
+type AlertResult =
+  | { kind: "onPress"; text: AlertButton["text"]; style: AlertButton["style"] }
+  | { kind: "onDismiss" };
+
+/**
+ * Wraps the {@link Alert.alert} using promises instead of callback.
+ * If there are no buttons, the default button "OK" will be used, like the default implementation.
+ * @param title
+ * @param message
+ * @param buttons
+ * @param options
+ * @constructor
+ */
+export const AsyncAlert = (
+  title: string,
+  message?: string,
+  buttons?: Array<AlertButton>,
+  options?: AlertOptions
+): Promise<AlertResult> => {
+  const newButtons: Array<AlertButton> = buttons ?? [
+    { text: "OK", style: "default" }
+  ];
+
+  return new Promise(resolve => {
+    Alert.alert(
+      title,
+      message,
+      newButtons?.map(x => ({
+        ...x,
+        onPress: () => {
+          x.onPress?.();
+          resolve({ kind: "onPress", text: x.text, style: x.style });
+        }
+      })),
+      {
+        ...options,
+        onDismiss: () => {
+          options?.onDismiss?.();
+          resolve({ kind: "onDismiss" });
+        }
+      }
+    );
+  });
+};
--- ts/utils/permission.ts
+++ ts/utils/permission.ts
@@ -1,12 +1,36 @@
-import { Permission, PermissionsAndroid, Platform } from "react-native";
-// check if the given permission is granted. If not, a prompt is shown to request permission
-export const hasAndroidPermission = async (permission: Permission) => {
+import {
+  Permission,
+  PermissionsAndroid,
+  Platform,
+  Rationale
+} from "react-native";
+import { AsyncAlert } from "./asyncAlert";
+
+/**
+ * This function enhance and wraps the react-native function {@link PermissionsAndroid.request}, in order to give to the user
+ * a prominent disclosure why that permission must be requested.
+ * The default behaviour of {@link PermissionsAndroid.request} displays the alert only in for
+ * https://developer.android.com/training/permissions/requesting.html#explain (the user refused to give permissions )
+ * but we should always explain the reason, also the first time (the user did not make any choices)
+ * @param permission
+ * @param rationale
+ */
+export const requestIOAndroidPermission = async (
+  permission: Permission,
+  rationale: Rationale
+): Promise<boolean> => {
   if (Platform.OS !== "android") {
     return true;
   }
   const hasPermission = await PermissionsAndroid.check(permission);
   if (hasPermission) {
     return true;
+  }
+
+  if (rationale !== undefined) {
+    await AsyncAlert(rationale.title, rationale.message, [
+      { text: rationale.buttonPositive, style: "default" }
+    ]);
   }
 
   const status = await PermissionsAndroid.request(permission);
