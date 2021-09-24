eb02d86360de9039c4227717cb4d4e7ffda8aa56
Simone <simone.biffi.91@gmail.com>
2021-09-24 09:52:14 +0200

    add logic to voucherGeneratedScreen

--- ts/features/bonus/siciliaVola/screens/voucherGeneration/VoucherGeneratedScreen.tsx
+++ ts/features/bonus/siciliaVola/screens/voucherGeneration/VoucherGeneratedScreen.tsx
@@ -10,6 +10,7 @@ import { GlobalState } from "../../../../../store/reducers/types";
 import {
   svGenerateVoucherBack,
   svGenerateVoucherCompleted,
+  svGenerateVoucherFailure,
   svGenerateVoucherGeneratedVoucher
 } from "../../store/actions/voucherGeneration";
 import FooterWithButtons from "../../../../../components/ui/FooterWithButtons";
@@ -19,20 +20,31 @@ import { View } from "native-base";
 import { H5 } from "../../../../../components/core/typography/H5";
 import VoucherInformationComponent from "../../components/VoucherInformationComponent";
 import { useEffect } from "react";
-import { voucherRequestSelector } from "../../store/reducers/voucherRequest";
 import { isVoucherRequest } from "../../utils";
+import { voucherRequestSelector } from "../../store/reducers/voucherGeneration/voucherRequest";
+import { voucherGeneratedSelector } from "../../store/reducers/voucherGeneration/voucherGenerated";
+import { isLoading, isReady } from "../../../bpd/model/RemoteValue";
+import { LoadingErrorComponent } from "../../../bonusVacanze/components/loadingErrorScreen/LoadingErrorComponent";
+import { isNone } from "fp-ts/lib/Option";
+import SvGeneratedVoucherTimeoutScreen from "./ko/SvGeneratedVoucherTimeoutScreen";
 
 type Props = ReturnType<typeof mapDispatchToProps> &
   ReturnType<typeof mapStateToProps>;
 
-const VoucherGeneratedScreen = (props: Props): React.ReactElement => {
+const VoucherGeneratedScreen = (props: Props): React.ReactElement | null => {
+  const {
+    remoteVoucherGenerated,
+    maybeVoucherRequest,
+    generateVoucherRequest
+  } = props;
+
   useEffect(() => {
-    props.voucherRequest.map(vR => {
+    maybeVoucherRequest.map(vR => {
       if (isVoucherRequest(vR)) {
-        props.generateVoucherRequest(vR);
+        generateVoucherRequest(vR);
       }
     });
-  }, []);
+  }, [maybeVoucherRequest, generateVoucherRequest]);
 
   const backButtonProps = {
     primary: false,
@@ -48,44 +60,81 @@ const VoucherGeneratedScreen = (props: Props): React.ReactElement => {
     title: I18n.t("global.genericSave")
   };
 
-  // TODO: manage loading/error/timeout(SvGeneratedVoucherTimeoutScreen) state on generateVoucherRequest selector
-  return (
-    <BaseScreenComponent
-      goBack={true}
-      contextualHelp={emptyContextualHelp}
-      headerTitle={I18n.t("bonus.sv.headerTitle")}
-    >
-      <SafeAreaView style={IOStyles.flex} testID={"VoucherGeneratedScreen"}>
-        <ScrollView style={[IOStyles.horizontalContentPadding]}>
-          <H1>{I18n.t("bonus.sv.voucherGeneration.voucherGenerated.title")}</H1>
-          <View spacer />
-          <H5 weight={"Regular"}>
-            {I18n.t("bonus.sv.voucherGeneration.voucherGenerated.subtitle")}
-          </H5>
-          <View spacer />
-          <VoucherInformationComponent
-            voucherCode={"123456"}
-            qrCode={"abc"}
-            barCode={"abc"}
-          />
-        </ScrollView>
-        <FooterWithButtons
-          type={"TwoButtonsInlineHalf"}
-          leftButton={backButtonProps}
-          rightButton={continueButtonProps}
+  if (isNone(maybeVoucherRequest)) {
+    props.failure("Voucher request is None");
+    return null;
+  }
+  const voucherRequest = maybeVoucherRequest.value;
+  if (!isVoucherRequest(voucherRequest)) {
+    props.failure("Partial voucher request is not a complete voucher request");
+    return null;
+  }
+
+  if (!isReady(remoteVoucherGenerated)) {
+    return (
+      <BaseScreenComponent
+        goBack={true}
+        contextualHelp={emptyContextualHelp}
+        headerTitle={I18n.t("bonus.sv.headerTitle")}
+      >
+        <LoadingErrorComponent
+          isLoading={isLoading(remoteVoucherGenerated)}
+          loadingCaption={I18n.t("global.remoteStates.loading")}
+          onRetry={() => props.generateVoucherRequest(voucherRequest)}
         />
-      </SafeAreaView>
-    </BaseScreenComponent>
-  );
+      </BaseScreenComponent>
+    );
+  }
+
+  const voucherGenerated = remoteVoucherGenerated.value;
+
+  switch (voucherGenerated.kind) {
+    // TODO: manage conflict and other state when the final swagger is available
+    case "success":
+      return (
+        <BaseScreenComponent
+          goBack={true}
+          contextualHelp={emptyContextualHelp}
+          headerTitle={I18n.t("bonus.sv.headerTitle")}
+        >
+          <SafeAreaView style={IOStyles.flex} testID={"VoucherGeneratedScreen"}>
+            <ScrollView style={[IOStyles.horizontalContentPadding]}>
+              <H1>
+                {I18n.t("bonus.sv.voucherGeneration.voucherGenerated.title")}
+              </H1>
+              <View spacer />
+              <H5 weight={"Regular"}>
+                {I18n.t("bonus.sv.voucherGeneration.voucherGenerated.subtitle")}
+              </H5>
+              <View spacer />
+              <VoucherInformationComponent
+                voucherCode={voucherGenerated.value.id.toString()}
+                qrCode={voucherGenerated.value.qrCode}
+                barCode={voucherGenerated.value.barCode}
+              />
+            </ScrollView>
+            <FooterWithButtons
+              type={"TwoButtonsInlineHalf"}
+              leftButton={backButtonProps}
+              rightButton={continueButtonProps}
+            />
+          </SafeAreaView>
+        </BaseScreenComponent>
+      );
+    case "timeout":
+      return <SvGeneratedVoucherTimeoutScreen />;
+  }
 };
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   back: () => dispatch(svGenerateVoucherBack()),
   completed: () => dispatch(svGenerateVoucherCompleted()),
+  failure: (reason: string) => dispatch(svGenerateVoucherFailure(reason)),
   generateVoucherRequest: (voucherRequest: VoucherRequest) =>
     dispatch(svGenerateVoucherGeneratedVoucher.request(voucherRequest))
 });
 const mapStateToProps = (state: GlobalState) => ({
-  voucherRequest: voucherRequestSelector(state)
+  maybeVoucherRequest: voucherRequestSelector(state),
+  remoteVoucherGenerated: voucherGeneratedSelector(state)
 });
 
 export default connect(
