d5e989f183a427613818106d2aa0541561aa2c2f
Pietro Grandi <pietro.grandi.3d@gmail.com>
2021-10-04 18:48:09 +0200

    Add test for messages based on message ID

--- ts/__e2e__/app.e2e.ts
+++ ts/__e2e__/app.e2e.ts
@@ -1,13 +1,5 @@
 /* eslint-disable */
 
-// FIXME: putting these declarations in detox.d.ts makes them available in all
-// test modules clashing with Jest's expect
-declare const device: Detox.Device;
-declare const element: Detox.Element;
-declare const waitFor: Detox.WaitFor;
-declare const expect: Detox.Expect<Detox.Expect<any>>;
-declare const by: Detox.Matchers;
-
 import * as detox from "detox";
 import adapter from "detox/runners/jest/adapter";
 
@@ -57,6 +49,75 @@ describe("e2e app", () => {
 
       await waitFor(element(by.text("Your usage data")))
         .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+    });
+  });
+
+  describe("when the user is already logged in", () => {
+    beforeAll(async () => {
+      await waitFor(element(by.id(loginButtonId)))
+        .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+
+      await element(by.id(loginButtonId)).tap();
+
+      await waitFor(element(by.id(posteIdpButtonId)))
+        .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+
+      await element(by.id(posteIdpButtonId)).tap();
+
+      await waitFor(element(by.text("Your usage data")))
+        .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+
+      await element(by.text("Share data")).tap();
+
+      await waitFor(element(by.text("Choose an unlock code")))
+        .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+
+      await waitFor(element(by.text("Insert your code again")))
+        .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+      await element(by.text("2")).tap();
+
+      await element(by.text("Continue")).tap();
+    });
+
+    it("should load the user's messages", async () => {
+      await waitFor(element(by.text("Messages")))
+        .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+
+      // The webserver is sending us exactly 20 messages with consecutive IDs
+      // in reverse order.
+      // We test that the first one is visibile on the UI and that the last one
+      // exists (but is not visible)
+
+      await waitFor(
+        element(by.id(`MessageListItem_00000000000000000000000020`))
+      )
+        .toBeVisible()
+        .withTimeout(WAIT_TIMEOUT_MS);
+
+      await waitFor(
+        element(by.id(`MessageListItem_00000000000000000000000001`))
+      )
+        .toExist()
         .withTimeout(WAIT_TIMEOUT_MS);
     });
   });
