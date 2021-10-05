b9d3397a128cbc51fd03bb4ea5d4d2958a3f43bc
Simone <simone.biffi.91@gmail.com>
2021-10-05 16:13:33 +0200

    finalize component

--- ts/components/ui/TextboxAutocomplete.tsx
+++ ts/components/ui/TextboxAutocomplete.tsx
@@ -1,30 +1,32 @@
 import * as React from "react";
 import { LabelledItem } from "../LabelledItem";
-import { useContext } from "react";
-import SelectCalendarModal from "../SelectCalendarModal";
+import { useContext, useState } from "react";
 import { LightModalContext } from "./LightModal";
 import { H4 } from "../core/typography/H4";
 import { IOColors } from "../core/variables/IOColors";
-import { Container, View } from "native-base";
+import {
+  Body,
+  Container,
+  Content,
+  Left,
+  ListItem,
+  Right,
+  View
+} from "native-base";
 import { H5 } from "../core/typography/H5";
 import TouchableDefaultOpacity from "../TouchableDefaultOpacity";
-import { formatDateAsLocal } from "../../utils/dates";
-import I18n from "../../i18n";
-import IconFont from "./IconFont";
-import variables from "../../theme/variables";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
-import { ScrollView, StyleSheet } from "react-native";
-import { emptyContextualHelp } from "../../utils/emptyContextualHelp";
-import BaseScreenComponent from "../screens/BaseScreenComponent";
+import {
+  ActivityIndicator,
+  FlatList,
+  ListRenderItemInfo,
+  StyleSheet
+} from "react-native";
 import { IOStyles } from "../core/variables/IOStyles";
 import { SafeAreaView } from "react-native";
-import { BaseHeader } from "../screens/BaseHeader";
+import AppHeader from "./AppHeader";
+import ButtonDefaultOpacity from "../ButtonDefaultOpacity";
+import IconFont from "./IconFont";
 
-type Props = {
-  label?: string;
-  minimumDate?: Date;
-  blocked?: boolean;
-};
 const styles = StyleSheet.create({
   container: {
     borderBottomWidth: 1,
@@ -37,80 +39,148 @@ const styles = StyleSheet.create({
     justifyContent: "space-between"
   }
 });
-
-type ModalProps = {
-  onClose: () => void;
+type Props<T> = {
+  data: ReadonlyArray<T>;
+  keyExtractor: (item: T, index: number) => string;
+  onChangeText: (value: string) => void;
+  onSelectValue: (value: T) => string;
+  renderItem: (item: ListRenderItemInfo<T>) => React.ReactElement;
+  title: string;
+  label: string;
+  placeholder: string;
+  isLoading: boolean;
 };
+type ModalProps<T> = {
+  data: ReadonlyArray<T>;
+  keyExtractor: (item: T, index: number) => string;
+  renderItem: (item: ListRenderItemInfo<T>) => React.ReactElement;
+  title: string;
+  label: string;
+  placeholder: string;
+  onChangeText: (value: string) => void;
+  onClose: () => void;
+  isLoading: boolean;
+};
+const FooterLoading = () => (
+  <>
+    <View spacer={true} />
+    <ActivityIndicator
+      color={"black"}
+      accessible={false}
+      importantForAccessibility={"no-hide-descendants"}
+      accessibilityElementsHidden={true}
+      testID={"activityIndicator"}
+    />
+  </>
+);
 
-const TextboxAutocompleteModal: React.FunctionComponent<ModalProps> = (
-  props: ModalProps
-) => {
+const TextboxAutocompleteModal = <T extends any>(props: ModalProps<T>) => {
+  const [inputValue, setInputValue] = useState<string>("");
   return (
     <Container>
-      <BaseHeader
-        accessibilityEvents={{
-          avoidNavigationEventsUsage: true
-        }}
-        headerTitle={"Inserisci un comune"}
-        customRightIcon={{
-          iconName: "io-close",
-          onPress: props.onClose,
-          accessibilityLabel: I18n.t(
-            "global.accessibility.contextualHelp.close"
-          )
-        }}
-      />
-      <SafeAreaView
-        style={IOStyles.flex}
-        testID={"SickSelectDestinationScreen"}
-      >
-        <ScrollView style={[IOStyles.horizontalContentPadding]}>
+      <AppHeader>
+        <Left>
+          <ButtonDefaultOpacity onPress={props.onClose} transparent={true}>
+            <IconFont name="io-close" />
+          </ButtonDefaultOpacity>
+        </Left>
+        <Body style={{ alignItems: "center" }}>
+          <H5 weight={"SemiBold"} color={"bluegrey"}>
+            {props.title}
+          </H5>
+        </Body>
+        <Right />
+      </AppHeader>
+      <SafeAreaView style={IOStyles.flex}>
+        <Content style={IOStyles.flex}>
           <LabelledItem
-            label={"Comune"}
+            iconPosition={"right"}
+            label={props.label}
             inputProps={{
-              value: "",
-              onChangeText: () => true,
-              placeholder: "Inserisci un comune"
+              value: inputValue,
+              onChangeText: v => {
+                setInputValue(v);
+                props.onChangeText(v);
+              },
+              placeholder: props.placeholder
             }}
           />
-        </ScrollView>
+          <View spacer large />
+
+          <FlatList
+            data={props.data}
+            ListFooterComponent={props.isLoading && <FooterLoading />}
+            renderItem={props.renderItem}
+            keyExtractor={props.keyExtractor}
+            keyboardShouldPersistTaps={"handled"}
+          />
+        </Content>
       </SafeAreaView>
     </Container>
   );
 };
 
-const TextboxAutocomplete: React.FunctionComponent<Props> = (props: Props) => {
+/**
+ * This component is a wrapper around a modal that contains:
+ * - a LabelledItem component
+ * - a Flatlist that shows a list of results from which the user has to choose
+ * Two callbacks are available:
+ * - onChangeText -> executed when the user inserts or cancels some character from the LabelledItem
+ * - onSelectValue -> executed when the user selects an item before the modal is hide
+ * @param props
+ * @constructor
+ */
+const TextboxAutocomplete = <T extends any>(props: Props<T>) => {
   const { showModal, hideModal } = useContext(LightModalContext);
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    undefined
+  );
+
   return (
-    <View style={styles.container}>
-      {props.label && <H5 color={"bluegreyDark"}>{props.label}</H5>}
-      <View spacer={true} />
-      <TouchableDefaultOpacity
-        style={styles.inputContainer}
-        onPress={() =>
-          showModal(<TextboxAutocompleteModal onClose={hideModal} />)
-        }
-      >
-        <H4 weight={"Regular"} color={"bluegreyLight"}>
-          {"Inserisci un comune"}
-        </H4>
-      </TouchableDefaultOpacity>
-    </View>
-    // <LabelledItem
-    //   label={props.label}
-    //   inputProps={{
-    //     onFocus: _ =>
-    //       showModal(
-    //         <SelectCalendarModal
-    //           onCancel={hideModal}
-    //           onCalendarSelected={() => true}
-    //         />
-    //       ),
-    //     value: "",
-    //     onChangeText: () => true,
-    //     placeholder: "Inserisci un comune"
-    //   }}
-    // />
+    <>
+      <View style={styles.container}>
+        <H5 color={"bluegreyDark"}>{props.label}</H5>
+        <View spacer={true} />
+        <TouchableDefaultOpacity
+          style={styles.inputContainer}
+          onPress={() =>
+            showModal(
+              <TextboxAutocompleteModal<T>
+                data={props.data}
+                keyExtractor={props.keyExtractor}
+                renderItem={i => (
+                  <ListItem
+                    icon={false}
+                    onPress={() => {
+                      setSelectedValue(props.onSelectValue(i.item));
+                      hideModal();
+                    }}
+                  >
+                    {props.renderItem(i)}
+                  </ListItem>
+                )}
+                title={props.title}
+                label={props.label}
+                placeholder={props.placeholder}
+                onClose={hideModal}
+                onChangeText={props.onChangeText}
+                isLoading={props.isLoading}
+              />
+            )
+          }
+        >
+          {selectedValue ? (
+            <H4 weight={"Regular"} color={"bluegrey"}>
+              {selectedValue}
+            </H4>
+          ) : (
+            <H4 weight={"Regular"} color={"bluegreyLight"}>
+              {props.placeholder}
+            </H4>
+          )}
+        </TouchableDefaultOpacity>
+      </View>
+    </>
   );
 };
 
