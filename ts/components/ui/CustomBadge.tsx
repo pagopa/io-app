e6109b5db1bf15b13e7b6b9b5140d392c53df74d
Matteo Boschi <mbmaken@gmail.com>
2021-10-11 12:32:33 +0200

    from class to functional component memo component computer width depending on digits

--- ts/components/ui/CustomBadge.tsx
+++ ts/components/ui/CustomBadge.tsx
@@ -1,34 +1,79 @@
 import { Badge, Text } from "native-base";
-import React from "react";
-import { StyleProp, ViewStyle } from "react-native";
+import React, { memo } from "react";
+import { StyleSheet } from "react-native";
+import variables from "../../theme/variables";
 
 type Props = {
-  badgeStyle: StyleProp<ViewStyle>;
-  textStyle: StyleProp<ViewStyle>;
-  badgeValue: number;
+  badgeValue?: number;
 };
 
+const styles = StyleSheet.create({
+  textStyle: {
+    paddingLeft: 0,
+    paddingRight: 0
+  },
+  badgeStyle: {
+    backgroundColor: variables.brandPrimary,
+    borderColor: "white",
+    borderWidth: 2,
+    position: "absolute",
+    elevation: 0.1,
+    shadowColor: "white",
+    width: 20,
+    height: 20,
+    left: 12,
+    bottom: 10,
+    paddingLeft: 0,
+    paddingRight: 0,
+    justifyContent: "center",
+    alignContent: "center"
+  }
+});
+
 const MAX_BADGE_VALUE = 99;
+const getWidthMultiplier = (text: string) => {
+  const digits = text.length;
+  if (digits <= 1) {
+    return 1;
+  }
+  if (digits <= 2) {
+    return 1.1;
+  }
+  return 1.4;
+};
 
 /**
  * A simple badge used for show the number of messages to read
  *
  */
-export default class CustomBadge extends React.PureComponent<Props> {
-  public render() {
-    return (
-      this.props.badgeValue > 0 && (
-        <Badge style={this.props.badgeStyle}>
-          <Text
-            badge={true}
-            style={this.props.textStyle}
-            accessible={false}
-            importantForAccessibility={"no-hide-descendants"}
-          >
-            {Math.min(this.props.badgeValue, MAX_BADGE_VALUE)}
-          </Text>
-        </Badge>
-      )
-    );
+const CustomBadge = (props: Props) => {
+  const badgeValue = props.badgeValue ?? 0;
+  if (badgeValue === 0) {
+    return null;
   }
-}
+  const badge = `${Math.min(badgeValue, MAX_BADGE_VALUE)}${
+    badgeValue > MAX_BADGE_VALUE ? "+" : ""
+  }`;
+  return (
+    <Badge
+      style={[
+        styles.badgeStyle,
+        { width: styles.badgeStyle.width * getWidthMultiplier(badge) }
+      ]}
+    >
+      <Text
+        badge={true}
+        style={styles.textStyle}
+        accessible={false}
+        importantForAccessibility={"no-hide-descendants"}
+      >
+        {badge}
+      </Text>
+    </Badge>
+  );
+};
+
+export default memo(
+  CustomBadge,
+  (prev, next) => prev.badgeValue === next.badgeValue
+);
--- ts/components/ui/TabIconComponent.tsx
+++ ts/components/ui/TabIconComponent.tsx
@@ -1,6 +1,5 @@
 import { View } from "native-base";
 import React from "react";
-import { StyleSheet } from "react-native";
 import variables from "../../theme/variables";
 import CustomBadge from "./CustomBadge";
 
@@ -12,29 +11,6 @@ type Props = {
   badgeValue?: number;
 };
 
-const styles = StyleSheet.create({
-  textStyle: {
-    paddingLeft: 0,
-    paddingRight: 0
-  },
-  badgeStyle: {
-    backgroundColor: variables.brandPrimary,
-    borderColor: "white",
-    borderWidth: 2,
-    position: "absolute",
-    elevation: 0.1,
-    shadowColor: "white",
-    height: 19,
-    width: 19,
-    left: 12,
-    bottom: 10,
-    paddingLeft: 0,
-    paddingRight: 0,
-    justifyContent: "center",
-    alignContent: "center"
-  }
-});
-
 /**
  *  Generic tab icon with badge indicator
  */
@@ -45,13 +21,7 @@ class TabIconComponent extends React.PureComponent<Props> {
       // accessibilityLabel={""} in order to read the font icon, without modify the library element
       <View accessibilityLabel={""}>
         <IconFont name={iconName} size={variables.iconSize3} color={color} />
-        {badgeValue && badgeValue > 0 ? (
-          <CustomBadge
-            badgeStyle={styles.badgeStyle}
-            textStyle={styles.textStyle}
-            badgeValue={badgeValue}
-          />
-        ) : null}
+        <CustomBadge badgeValue={badgeValue} />
       </View>
     );
   }
