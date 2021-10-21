5d4894b9f216fce18716f3f86aa37b571ed457f6
Matteo Boschi <mbmaken@gmail.com>
2021-10-21 13:25:56 +0200

    add tests

--- ts/features/wallet/paypal/screen/__tests__/PspInfoBottomSheetContent.test.tsx
+++ ts/features/wallet/paypal/screen/__tests__/PspInfoBottomSheetContent.test.tsx
@@ -1,7 +1,6 @@
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 import React from "react";
 import { NonNegativeNumber } from "@pagopa/ts-commons/lib/numbers";
-import { constNull } from "fp-ts/lib/function";
 import { PspInfoBottomSheetContent } from "../../components/PspInfoBottomSheet";
 
 jest.mock("@gorhom/bottom-sheet", () => {
@@ -14,16 +13,35 @@ jest.mock("@gorhom/bottom-sheet", () => {
   };
 });
 
+const mockOnPress = jest.fn();
+const props = {
+  pspName: "pspNameTest",
+  pspFee: 123 as NonNegativeNumber,
+  pspPrivacyUrl: "privacyUrlTest",
+  onButtonPress: mockOnPress
+};
+
 describe("PspInfoBottomSheetContent", () => {
   jest.useFakeTimers();
   it(`component should be defined`, () => {
-    const props = {
-      pspName: "pspNameTest",
-      pspFee: 123 as NonNegativeNumber,
-      pspPrivacyUrl: "privacyUrlTest",
-      onButtonPress: constNull
-    };
+    const renderComponent = render(<PspInfoBottomSheetContent {...props} />);
+    expect(
+      renderComponent.queryByTestId("PspInfoBottomSheetContentTestID")
+    ).not.toBeNull();
+  });
+
+  it(`footer button should be defined`, () => {
     const renderComponent = render(<PspInfoBottomSheetContent {...props} />);
     expect(renderComponent.queryByTestId("continueButtonId")).not.toBeNull();
+  });
+
+  it(`button handler should be called when the button is pressed`, () => {
+    const renderComponent = render(<PspInfoBottomSheetContent {...props} />);
+    const continueButton = renderComponent.queryByTestId("continueButtonId");
+    expect(renderComponent.queryByTestId("continueButtonId")).not.toBeNull();
+    if (continueButton) {
+      fireEvent.press(continueButton);
+      expect(mockOnPress).toBeCalledTimes(1);
+    }
   });
 });
