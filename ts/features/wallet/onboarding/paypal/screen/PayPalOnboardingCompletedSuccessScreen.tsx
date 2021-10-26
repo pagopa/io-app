be581c924009781c3caa84a1cc81b08ab7c0f105
Matteo Boschi <mbmaken@gmail.com>
2021-10-26 15:47:58 +0200

    refactoring

--- ts/features/wallet/onboarding/paypal/screen/PayPalOnboardingCompletedSuccessScreen.tsx
+++ ts/features/wallet/onboarding/paypal/screen/PayPalOnboardingCompletedSuccessScreen.tsx
@@ -9,10 +9,10 @@ import BaseScreenComponent from "../../../../../components/screens/BaseScreenCom
 import { InfoScreenComponent } from "../../../../../components/infoScreen/InfoScreenComponent";
 import { renderInfoRasterImage } from "../../../../../components/infoScreen/imageRendering";
 import successImage from "../../../../../../img/pictograms/payment-completed.png";
-import FooterWithButtons from "../../../../../components/ui/FooterWithButtons";
 import { cancelButtonProps } from "../../../../bonus/bonusVacanze/components/buttons/ButtonConfigurations";
 import { GlobalState } from "../../../../../store/reducers/types";
 import I18n from "../../../../../i18n";
+import { FooterStackButton } from "../../../../bonus/bonusVacanze/components/buttons/FooterStackButtons";
 type Props = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
 
@@ -28,21 +28,24 @@ const PayPalOnboardingCompletedSuccessScreen = (props: Props) => (
         title={I18n.t("wallet.onboarding.paypal.onBoardingCompleted.title")}
         body={I18n.t("wallet.onboarding.paypal.onBoardingCompleted.body")}
       />
-      <FooterWithButtons
-        type={"TwoButtonsInlineThird"}
-        rightButton={{
-          testID: "continueButtonId",
-          bordered: false,
-          // TODO replace with the effective handler
-          onPress: undefined,
-          title: I18n.t(
-            "wallet.onboarding.paypal.onBoardingCompleted.primaryButton"
+      <FooterStackButton
+        buttons={[
+          {
+            testID: "continueButtonId",
+            bordered: false,
+            // TODO replace with the effective handler
+            onPress: undefined,
+            title: I18n.t(
+              "wallet.onboarding.paypal.onBoardingCompleted.primaryButton"
+            )
+          },
+          cancelButtonProps(
+            props.onClose,
+            I18n.t(
+              "wallet.onboarding.paypal.onBoardingCompleted.secondaryButton"
+            )
           )
-        }}
-        leftButton={cancelButtonProps(
-          props.onClose,
-          I18n.t("wallet.onboarding.paypal.onBoardingCompleted.secondaryButton")
-        )}
+        ]}
       />
     </SafeAreaView>
   </BaseScreenComponent>
