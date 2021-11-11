08b0461239d320f794c99ce56c359d397c45b4cb
Simone <simone.biffi.91@gmail.com>
2021-11-11 11:59:26 +0100

    modify key

--- ts/utils/zendesk.ts
+++ ts/utils/zendesk.ts
@@ -9,14 +9,14 @@ export type ZendeskConfig = {
 };
 
 export const zendeskDefaultJwtConfig: ZendeskConfig = {
-  key: "6e2c01f7-cebc-4c77-b878-3ed3c749a835",
+  key: "mp9agCp6LWusBxvHIGbeBmfI0wMeLIJM",
   appId: "7f23d5b0eadc5b4f2cc83df3898c6f607bad769fe053a186",
   clientId: "mobile_sdk_client_4bf774ed28b085195cc5",
   url: "https://appio.zendesk.com",
   token: ""
 };
 export const zendeskDefaultAnonymousConfig: ZendeskConfig = {
-  key: "6e2c01f7-cebc-4c77-b878-3ed3c749a835",
+  key: "mp9agCp6LWusBxvHIGbeBmfI0wMeLIJM",
   appId: "8547479b47fdacd0e8f74a4fb076a41014dee620d1d890b3",
   clientId: "mobile_sdk_client_518ee6a8160220698f97",
   url: "https://appio.zendesk.com",
