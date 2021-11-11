0ce7d2a2f13df311befa95be49e9ab427a5592d6
Fabrizio <fartux@gmail.com>
2021-11-11 11:30:14 +0100

    remove unnecessary beforeAll

--- ts/__e2e__/login.e2e.ts
+++ ts/__e2e__/login.e2e.ts
@@ -1,14 +1,9 @@
 import adapter from "detox/runners/jest/adapter";
-import { launchAppConfig } from "./config";
 import { loginWithSPID } from "./utils";
 
 describe("User Login using SPID", () => {
   beforeEach(async () => {
     await adapter.beforeEach();
-  });
-
-  beforeAll(async () => {
-    await device.launchApp(launchAppConfig);
   });
 
   describe("when the user never logged in before", () => {
