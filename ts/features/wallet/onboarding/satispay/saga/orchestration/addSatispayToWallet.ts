92b53de6d88893415cb8fcf81de74a33eccc2dec
CrisTofani <cri.tofani@gmail.com>
2021-11-11 10:48:30 +0100

    Fixes compile errors

--- ts/features/wallet/onboarding/satispay/saga/orchestration/addSatispayToWallet.ts
+++ ts/features/wallet/onboarding/satispay/saga/orchestration/addSatispayToWallet.ts
@@ -1,18 +1,12 @@
 import { call, put, select } from "redux-saga/effects";
 import NavigationService from "../../../../../../navigation/NavigationService";
 import ROUTES from "../../../../../../navigation/routes";
-import {
-  executeWorkUnit,
-  withResetNavigationStack
-} from "../../../../../../sagas/workUnit";
+import { executeWorkUnit, withResetNavigationStack } from "../../../../../../sagas/workUnit";
 import { navigateToWalletHome } from "../../../../../../store/actions/navigation";
 import { fetchWalletsRequest } from "../../../../../../store/actions/wallet/wallets";
 import { SagaCallReturnType } from "../../../../../../types/utils";
 import { activateBpdOnNewPaymentMethods } from "../../../../../bonus/bpd/saga/orchestration/activateBpdOnNewAddedPaymentMethods";
-import {
-  navigateToActivateBpdOnNewSatispay,
-  navigateToOnboardingSatispayStart
-} from "../../navigation/action";
+import { navigateToActivateBpdOnNewSatispay, navigateToOnboardingSatispayStart } from "../../navigation/action";
 import WALLET_ONBOARDING_SATISPAY_ROUTES from "../../navigation/routes";
 import {
   walletAddSatispayBack,
@@ -53,7 +47,7 @@ export function* addSatispayToWalletAndActivateBpd() {
   );
 
   const isInitialScreenDigitalPayment =
-    initialScreenName === ROUTES.WALLET_ADD_DIGITAL_PAYMENT_METHOD;
+    initialScreenName === ROUTES.WALLET_ADD_PAYMENT_METHOD;
 
   if (res !== "back" && isInitialScreenDigitalPayment) {
     // integration with the legacy "Add a payment"
