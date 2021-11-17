138e2045e03586c113e72e8a54e67b0fb84e8401
Simone <simone.biffi.91@gmail.com>
2021-11-17 15:40:45 +0100

    modify tenant address

--- ts/utils/zendesk.ts
+++ ts/utils/zendesk.ts
@@ -12,14 +12,14 @@ export const zendeskDefaultJwtConfig: ZendeskConfig = {
   key: "mp9agCp6LWusBxvHIGbeBmfI0wMeLIJM",
   appId: "7f23d5b0eadc5b4f2cc83df3898c6f607bad769fe053a186",
   clientId: "mobile_sdk_client_4bf774ed28b085195cc5",
-  url: "https://appio.zendesk.com",
+  url: "https://appiotest.zendesk.com",
   token: ""
 };
 export const zendeskDefaultAnonymousConfig: ZendeskConfig = {
   key: "mp9agCp6LWusBxvHIGbeBmfI0wMeLIJM",
   appId: "8547479b47fdacd0e8f74a4fb076a41014dee620d1d890b3",
   clientId: "mobile_sdk_client_518ee6a8160220698f97",
-  url: "https://appio.zendesk.com",
+  url: "https://appiotest.zendesk.com",
   token: ""
 };
 
