8daf14257a151949580a09bc806a9afd35fcc23f
Pietro Grandi <pietro.grandi.3d@gmail.com>
2021-11-24 19:16:52 +0100

    Update the UI types and mocks

--- ts/store/reducers/entities/messages/types.ts
+++ ts/store/reducers/entities/messages/types.ts
@@ -5,6 +5,8 @@ import { PaymentAmount } from "../../../../../definitions/backend/PaymentAmount"
 import { MessageBodyMarkdown } from "../../../../../definitions/backend/MessageBodyMarkdown";
 import { PaymentNoticeNumber } from "../../../../../definitions/backend/PaymentNoticeNumber";
 import { OrganizationFiscalCode } from "../../../../../definitions/backend/OrganizationFiscalCode";
+import { CreatedMessageWithContentAndAttachments } from "../../../../../definitions/backend/CreatedMessageWithContentAndAttachments";
+import { PublicMessage } from "../../../../../definitions/backend/PublicMessage";
 
 // just a placeholder for now
 export type MessageCategory = null;
@@ -15,15 +17,20 @@ export type MessageCategory = null;
 export type UIMessage = {
   id: string;
   fiscalCode: FiscalCode;
+
   // TODO:  https://pagopa.atlassian.net/browse/IA-417
   //        and https://pagopa.atlassian.net/browse/IA-418
   category: MessageCategory;
+
   createdAt: Date;
   serviceId: ServiceId;
-  serviceName?: string;
-  organizationName?: string;
+  serviceName: string;
+  organizationName: string;
   title: string;
   timeToLive?: TimeToLiveSeconds;
+
+  // @deprecated please use it only for backward compatibility
+  raw: PublicMessage;
 };
 
 /**
@@ -31,17 +38,24 @@ export type UIMessage = {
  */
 export type UIMessageDetails = {
   id: string;
-  prescriptionData?: {
-    nre: string;
-    iup?: string;
-    prescriberFiscalCode: FiscalCode;
-  };
+  prescriptionData?: PrescriptionData;
   attachments?: ReadonlyArray<Attachment>;
   markdown: MessageBodyMarkdown;
   dueDate?: Date;
   paymentData?: PaymentData;
-  greenPass?: { authCode: string };
+  greenPass?: GreenPass;
+
+  // @deprecated please use it only for backward compatibility
+  raw: CreatedMessageWithContentAndAttachments;
 };
+
+export type PrescriptionData = {
+  nre: string;
+  iup?: string;
+  prescriberFiscalCode?: FiscalCode;
+};
+
+export type GreenPass = { authCode: string };
 
 export type PaymentData = {
   payee: {
