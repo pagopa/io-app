f988046bd401fc2373b69981878ed26cabfe9eb3
Matteo Boschi <mbmaken@gmail.com>
2021-11-26 16:38:40 +0100

    update comment

--- ts/utils/paypal.ts
+++ ts/utils/paypal.ts
@@ -1,6 +1,8 @@
 import { PayPalInfo } from "../../definitions/pagopa/PayPalInfo";
 import I18n from "../i18n";
 
+// return the email used in the paypal account. It it can't be found "n/a" will be returned
+// it is inside the default psp ¯\_(ツ)_/¯
 export const getPaypalAccountEmail = (paypal: PayPalInfo): string =>
   paypal.pspInfo.find(p => p.default)?.email ??
   I18n.t("wallet.onboarding.paypal.emailNotAvailable");
