d15a910bcbace83c5f58eea5501a545b2e08278b
Matteo Boschi <mbmaken@gmail.com>
2021-11-29 17:44:46 +0100

    fix

--- ts/features/wallet/paypal/screen/PaypalDetailScreen.tsx
+++ ts/features/wallet/paypal/screen/PaypalDetailScreen.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as pot from "italia-ts-commons/lib/pot";
-import { NavigationInjectedProps } from "react-navigation";
 import { connect } from "react-redux";
 import BasePaymentMethodScreen from "../../common/BasePaymentMethodScreen";
 import PaymentMethodFeatures from "../../component/features/PaymentMethodFeatures";
@@ -10,12 +9,9 @@ import { PayPalPaymentMethod } from "../../../../types/pagopa";
 import WorkunitGenericFailure from "../../../../components/error/WorkunitGenericFailure";
 import { paypalSelector } from "../../../../store/reducers/wallet/wallets";
 
-type NavigationParams = Readonly<{
-  paypal: PayPalPaymentMethod;
-}>;
-
-type Props = NavigationInjectedProps<NavigationParams> &
-  ReturnType<typeof mapStateToProps>;
+type Props = { paypal: PayPalPaymentMethod } & ReturnType<
+  typeof mapStateToProps
+>;
 
 /**
  * Detail screen for a paypal payment method
