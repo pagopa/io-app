48d4fb9e0cd3f6aa6e5dfec7ab9e71435c7d9aaa
Simone <simone.biffi.91@gmail.com>
2021-12-02 17:21:40 +0100

    fix lint error

--- ts/features/zendesk/navigation/navigator.tsx
+++ ts/features/zendesk/navigation/navigator.tsx
@@ -2,8 +2,8 @@ import { createStackNavigator } from "react-navigation-stack";
 import ZendeskSupportHelpCenter from "../screens/ZendeskSupportHelpCenter";
 import ZendeskAskPermissions from "../screens/ZendeskAskPermissions";
 import ZendeskChooseCategory from "../screens/ZendeskChooseCategory";
-import ZENDESK_ROUTES from "./routes";
 import { zendeskEnabled } from "../../../config";
+import ZENDESK_ROUTES from "./routes";
 
 const zendeskConfigMap = zendeskEnabled
   ? {
