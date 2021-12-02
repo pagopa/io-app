4104e1e7183ff9e91c15d352a061ccbd4c7d2649
Simone <simone.biffi.91@gmail.com>
2021-12-02 12:30:56 +0100

    update navigator

--- ts/features/zendesk/navigation/navigator.tsx
+++ ts/features/zendesk/navigation/navigator.tsx
@@ -1,10 +1,14 @@
 import { createStackNavigator } from "react-navigation-stack";
 import ZENDESK_ROUTES from "./routes";
 import ZendeskSupportHelpCenter from "../screens/ZendeskSupportHelpCenter";
+import ZendeskAskPermissions from "../screens/ZendeskAskPermissions";
+import ZendeskChooseCategory from "../screens/ZendeskChooseCategory";
 
 export const zendeskSupportNavigator = createStackNavigator(
   {
-    [ZENDESK_ROUTES.HELP_CENTER]: { screen: ZendeskSupportHelpCenter }
+    [ZENDESK_ROUTES.HELP_CENTER]: { screen: ZendeskSupportHelpCenter },
+    [ZENDESK_ROUTES.ASK_PERMISSIONS]: { screen: ZendeskAskPermissions },
+    [ZENDESK_ROUTES.CHOOSE_CATEGORY]: { screen: ZendeskChooseCategory }
   },
   {
     // Let each screen handles the header and navigation
