07d12fc4577856c577730fad5be8e35b8c25c40b
Simone <simone.biffi.91@gmail.com>
2021-12-02 12:29:35 +0100

    add ZendeskChooseCategory screen

---
+++ ts/features/zendesk/screens/ZendeskChooseCategory.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
+import { IOStyles } from "../../../components/core/variables/IOStyles";
+import { SafeAreaView, ScrollView } from "react-native";
+import { H1 } from "../../../components/core/typography/H1";
+import View from "../../../components/ui/TextWithIcon";
+import I18n from "../../../i18n";
+
+const ZendeskChooseCategory = () => {
+  return (
+    <BaseScreenComponent
+      showInstabugChat={false}
+      goBack={true}
+      customRightIcon={{
+        iconName: "",
+        onPress: () => true
+      }}
+      headerTitle={I18n.t("support.chooseCategory.header")}
+    >
+      <SafeAreaView style={IOStyles.flex} testID={"ZendeskChooseCategory"}>
+        <ScrollView style={[IOStyles.horizontalContentPadding]}>
+          <H1>{I18n.t("support.chooseCategory.title.category")}</H1>
+          <View spacer />
+        </ScrollView>
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
+
+export default ZendeskChooseCategory;
