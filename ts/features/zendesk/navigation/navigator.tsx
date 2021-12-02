51f6383303aa5c25d656b9df10a89967a94cf997
Simone <simone.biffi.91@gmail.com>
2021-12-02 16:50:17 +0100

    protect screen navigation with FF

--- ts/features/zendesk/navigation/navigator.tsx
+++ ts/features/zendesk/navigation/navigator.tsx
@@ -3,18 +3,20 @@ import ZendeskSupportHelpCenter from "../screens/ZendeskSupportHelpCenter";
 import ZendeskAskPermissions from "../screens/ZendeskAskPermissions";
 import ZendeskChooseCategory from "../screens/ZendeskChooseCategory";
 import ZENDESK_ROUTES from "./routes";
+import { zendeskEnabled } from "../../../config";
 
-export const zendeskSupportNavigator = createStackNavigator(
-  {
-    [ZENDESK_ROUTES.HELP_CENTER]: { screen: ZendeskSupportHelpCenter },
-    [ZENDESK_ROUTES.ASK_PERMISSIONS]: { screen: ZendeskAskPermissions },
-    [ZENDESK_ROUTES.CHOOSE_CATEGORY]: { screen: ZendeskChooseCategory }
-  },
-  {
-    // Let each screen handles the header and navigation
-    headerMode: "none",
-    defaultNavigationOptions: {
-      gesturesEnabled: false
+const zendeskConfigMap = zendeskEnabled
+  ? {
+      [ZENDESK_ROUTES.HELP_CENTER]: { screen: ZendeskSupportHelpCenter },
+      [ZENDESK_ROUTES.ASK_PERMISSIONS]: { screen: ZendeskAskPermissions },
+      [ZENDESK_ROUTES.CHOOSE_CATEGORY]: { screen: ZendeskChooseCategory }
     }
+  : {};
+
+export const zendeskSupportNavigator = createStackNavigator(zendeskConfigMap, {
+  // Let each screen handles the header and navigation
+  headerMode: "none",
+  defaultNavigationOptions: {
+    gesturesEnabled: false
   }
-);
+});
