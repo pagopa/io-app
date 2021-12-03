fdd09ee4f271861948d6d2789e4fe50111e0c619
Fabrizio <fartux@gmail.com>
2021-12-03 11:39:30 +0100

    add screen stubs

---
+++ ts/features/mvl/screens/MVLRouterScreen.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { H1 } from "../../../components/core/typography/H1";
+import { IOStyles } from "../../../components/core/variables/IOStyles";
+import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
+import { emptyContextualHelp } from "../../../utils/emptyContextualHelp";
+
+type Props = unknown;
+
+/**
+ * Entrypoint for the MVL, handle the loading, error, success and future business logic ko, routing the screen rendering
+ * @param _
+ * @constructor
+ */
+export const MVLRouterScreen = (_: Props): React.ReactElement => (
+  <BaseScreenComponent goBack={true} contextualHelp={emptyContextualHelp}>
+    <SafeAreaView style={IOStyles.flex} testID={"MVLRouterScreen"}>
+      <ScrollView style={[IOStyles.horizontalContentPadding]}>
+        <H1>MVLRouterScreen</H1>
+      </ScrollView>
+    </SafeAreaView>
+  </BaseScreenComponent>
+);
---
+++ ts/features/mvl/screens/metadata/MVLSignatureScreen.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { H1 } from "../../../../components/core/typography/H1";
+import { IOStyles } from "../../../../components/core/variables/IOStyles";
+import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
+import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
+
+type Props = unknown;
+
+/**
+ * This screen displays the signature details for a MVL
+ * @param _
+ * @constructor
+ */
+export const MVLSignatureScreen = (_: Props): React.ReactElement => (
+  <BaseScreenComponent goBack={true} contextualHelp={emptyContextualHelp}>
+    <SafeAreaView style={IOStyles.flex} testID={"MVLSignatureScreen"}>
+      <ScrollView style={[IOStyles.horizontalContentPadding]}>
+        <H1>MVLSignatureScreen</H1>
+      </ScrollView>
+    </SafeAreaView>
+  </BaseScreenComponent>
+);
