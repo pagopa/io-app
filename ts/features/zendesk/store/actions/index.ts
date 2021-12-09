c37818975c1918b0f547ea57f66982e06448c42e
Simone <simone.biffi.91@gmail.com>
2021-12-09 10:10:37 +0100

    add zendesk actions

--- ts/features/zendesk/store/actions/index.ts
+++ ts/features/zendesk/store/actions/index.ts
@@ -1,4 +1,11 @@
-import { ActionType, createStandardAction } from "typesafe-actions";
+import {
+  ActionType,
+  createAsyncAction,
+  createStandardAction
+} from "typesafe-actions";
+import { Zendesk } from "../../../../../definitions/content/Zendesk";
+import { NetworkError } from "../../../../utils/errors";
+import { ZendeskCategory } from "../../../../../definitions/content/ZendeskCategory";
 
 /**
  * The user chooses to start the workflow to open a support request
@@ -34,9 +41,27 @@ export const zendeskSupportFailure = createStandardAction(
   "ZENDESK_SUPPORT_FAILURE"
 )<string>();
 
+/**
+ * Request the zendesk config
+ */
+export const getZendeskConfig = createAsyncAction(
+  "GET_ZENDESK_CONFIG_REQUEST",
+  "GET_ZENDESK_CONFIG_SUCCESS",
+  "GET_ZENDESK_CONFIG_FAILURE"
+)<void, Zendesk, NetworkError>();
+
+/**
+ * The workflow fails
+ */
+export const zendeskSelectedCategory = createStandardAction(
+  "ZENDESK_SELECTED_CATEGORY"
+)<ZendeskCategory>();
+
 export type ZendeskSupportActions =
   | ActionType<typeof zendeskSupportStart>
   | ActionType<typeof zendeskSupportCompleted>
   | ActionType<typeof zendeskSupportCancel>
   | ActionType<typeof zendeskSupportBack>
-  | ActionType<typeof zendeskSupportFailure>;
+  | ActionType<typeof zendeskSupportFailure>
+  | ActionType<typeof getZendeskConfig>
+  | ActionType<typeof zendeskSelectedCategory>;
