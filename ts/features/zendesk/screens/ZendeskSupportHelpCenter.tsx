bea6b9744696597396e3ecc5d55b063e936c57c8
Simone <simone.biffi.91@gmail.com>
2021-12-11 16:12:03 +0100

    update ZendeskSupportHelpCenter screen

--- ts/features/zendesk/screens/ZendeskSupportHelpCenter.tsx
+++ ts/features/zendesk/screens/ZendeskSupportHelpCenter.tsx
@@ -1,21 +1,162 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, ScrollView } from "react-native";
 import { useDispatch } from "react-redux";
 import I18n from "../../../i18n";
 import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
 import { IOStyles } from "../../../components/core/variables/IOStyles";
 import View from "../../../components/ui/TextWithIcon";
-import { getZendeskConfig, zendeskSupportBack } from "../store/actions";
-import ZendeskSupportComponent from "../../../components/ZendeskSupportComponent";
+import {
+  getZendeskConfig,
+  ZendeskStartPayload,
+  zendeskSupportBack,
+  zendeskSupportCompleted
+} from "../store/actions";
+import ZendeskSupportComponent from "../components/ZendeskSupportComponent";
+import { none } from "fp-ts/lib/Option";
+import { ContextualHelpData } from "../../../components/ContextualHelp/ContextualHelpComponent";
+import { getContextualHelpDataFromRouteSelector } from "../../../store/reducers/content";
+import * as pot from "italia-ts-commons/lib/pot";
+import { loadContextualHelpData } from "../../../store/actions/content";
+import { useIOSelector } from "../../../store/hooks";
+import { getFAQsFromCategories } from "../../../utils/faq";
+import { NavigationInjectedProps } from "react-navigation";
+import ActivityIndicator from "../../../components/ui/ActivityIndicator";
+import themeVariables from "../../../theme/variables";
+import { isStringNullyOrEmpty } from "../../../utils/strings";
+import { H3 } from "../../../components/core/typography/H3";
+import FAQComponent from "../../../components/FAQComponent";
+import {
+  getContextualHelpConfig,
+  getContextualHelpData,
+  handleOnLinkClicked,
+  reloadContextualHelpDataThreshold
+} from "../../../components/screens/BaseScreenComponent/utils";
+import { constNull } from "fp-ts/lib/function";
 
+type FaqManagerProps = Pick<
+  ZendeskStartPayload,
+  "contentLoaded" | "faqCategories" | "startingRoute"
+> & { title: string; body: () => React.ReactNode };
+
+/**
+ * This component must be used only here.
+ * Make the {@link ZendeskSupportHelpCenter} compatible with {@link BaseScreenComponent} and substitute the {@link ContextualHelp}
+ * It show the title and the FAQ of the contextual help.
+ * @constructor
+ */
+const FaqManager = (props: FaqManagerProps) => {
+  const dispatch = useDispatch();
+  const potContextualData = useIOSelector(
+    getContextualHelpDataFromRouteSelector(props.startingRoute)
+  );
+  const maybeContextualData = pot.getOrElse(potContextualData, none);
+  const loadContextualHelp = () => dispatch(loadContextualHelpData.request());
+
+  const [contentHasLoaded, setContentHasLoaded] = useState<boolean | undefined>(
+    undefined
+  );
+  const [lastContextualDataUpdate, setLastContextualDataUpdate] =
+    useState<Date>(new Date());
+  const { title, body, faqCategories, contentLoaded } = props;
+  useEffect(() => {
+    const now = new Date();
+    // if the contextual data is empty or is in error and last reload was done before the threshold -> try to reload
+    if (
+      now.getTime() - lastContextualDataUpdate.getTime() >
+        reloadContextualHelpDataThreshold &&
+      !pot.isLoading(potContextualData) &&
+      pot.isNone(potContextualData)
+    ) {
+      setLastContextualDataUpdate(now);
+      loadContextualHelp();
+    }
+  }, [lastContextualDataUpdate, potContextualData, loadContextualHelpData]);
+
+  const contextualHelpData: ContextualHelpData = getContextualHelpData(
+    maybeContextualData,
+    {
+      title: title,
+      faqs: getFAQsFromCategories(faqCategories ?? []),
+      content: body()
+    },
+    () => setContentHasLoaded(true)
+  );
+
+  /**
+   content is loaded when:
+   - provided one from props is loaded or
+   - when the remote one is loaded
+   */
+  const isContentLoaded = maybeContextualData.fold(
+    contentLoaded,
+    _ => contentHasLoaded
+  );
+
+  const isContentReady = contextualHelpData.content === undefined;
+
+  return (
+    <>
+      {isContentReady && (
+        <View centerJustified={true}>
+          <ActivityIndicator color={themeVariables.brandPrimaryLight} />
+        </View>
+      )}
+      {!isContentReady && (
+        <>
+          {!isStringNullyOrEmpty(contextualHelpData.title) && (
+            <>
+              <H3 accessible={true}>{contextualHelpData.title}</H3>
+              <View spacer={true} />
+            </>
+          )}
+          {contextualHelpData.content && (
+            <>
+              {contextualHelpData.content}
+              <View spacer={true} />
+            </>
+          )}
+          {contextualHelpData.faqs && isContentLoaded && (
+            <FAQComponent
+              onLinkClicked={() => true}
+              faqs={contextualHelpData.faqs}
+            />
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+type Props = NavigationInjectedProps<ZendeskStartPayload>;
 /**
  * Ingress screen to access the Zendesk assistance tool
  * the user can choose to open a new ticket, follow previous conversations or read the faqs
  * @constructor
  */
-const ZendeskSupportHelpCenter = () => {
+const ZendeskSupportHelpCenter = (props: Props) => {
   const dispatch = useDispatch();
   const workUnitBack = () => dispatch(zendeskSupportBack());
+  const workUnitComplete = () => dispatch(zendeskSupportCompleted());
+
+  // Navigation prop
+  const faqCategories = props.navigation.getParam("faqCategories");
+  const contextualHelp = props.navigation.getParam("contextualHelp");
+  const contextualHelpMarkdown = props.navigation.getParam(
+    "contextualHelpMarkdown"
+  );
+  const startingRoute = props.navigation.getParam("startingRoute");
+
+  const [markdownContentLoaded, setMarkdownContentLoaded] = useState<boolean>(
+    !contextualHelpMarkdown
+  );
+
+  const contextualHelpConfig = getContextualHelpConfig(
+    contextualHelp,
+    contextualHelpMarkdown,
+    () => setMarkdownContentLoaded(true),
+    handleOnLinkClicked(() => workUnitComplete())
+  );
+
   /**
    * as first step request the config (categories + panicmode) that could
      be used in the next steps (possible network error are handled in {@link ZendeskAskPermissions})
@@ -40,6 +181,13 @@ const ZendeskSupportHelpCenter = () => {
         testID={"ZendeskSupportHelpCenterScreen"}
       >
         <ScrollView style={[IOStyles.horizontalContentPadding]}>
+          <FaqManager
+            title={contextualHelpConfig?.title ?? ""}
+            body={contextualHelpConfig?.body ?? constNull}
+            faqCategories={faqCategories}
+            contentLoaded={markdownContentLoaded}
+            startingRoute={startingRoute}
+          />
           <View spacer />
           <ZendeskSupportComponent />
         </ScrollView>
