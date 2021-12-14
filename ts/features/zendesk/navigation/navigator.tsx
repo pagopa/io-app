1276110a9e7d957cf3a263fa977a387290a194f7
Simone <simone.biffi.91@gmail.com>
2021-12-14 14:46:11 +0100

    chore: [IA-532] Complete ZendeskSupportHelpCenter screen (#3571)
    * remove placeholder
    
    * add zendesk actions
    
    * add zendeskConfig client
    
    * add api saga handler
    
    * add zendesk reducer
    
    * add zendesk to action type
    
    * add assistance tools reducer
    
    * add zendesk config request when start the zendesk flow
    
    * fix useEffect
    
    * add zendesk config selector
    
    * modify reducer value when the action is a request
    
    * update labels
    
    * add subcategory route
    
    * add subcategory screen in the navigator
    
    * add navigate to subCategory action
    
    * add selectedCategory selector
    
    * complete choose category screen
    
    * add chooseSubCategory screen
    
    * fix lint errors
    
    * Update locales/en/index.yml
    
    * fix lint errors
    
    * move zendesk component and test in components dir
    
    * add duplicated utils functions
    
    * use utils functions
    
    * modify imports after move file
    
    * add navigationParams to zendeskSupportStart action
    
    * replicate selector using passed route instead of  currentRouteSelector
    
    * update labels
    
    * add isPanicMode util
    
    * update ZendeskSupportHelpCenter screen
    
    * add navigation to panic mode screen
    
    * complete panic mode screen
    
    * fix lint errors
    
    * New copy
    
    * remove panicMode image
    
    * fix link hook warning
    
    * add getContextualHelpConfig comment
    
    * add ZendeskSupportComponent comment
    
    * add ZendeskPanicMode comment
    
    * renaming
    
    * fix placeholder
    
    Co-authored-by: Alice Azzolini <93777761+AliceAzzolini@users.noreply.github.com>
    Co-authored-by: Jacopo Pompilii <Jacopo.pompilii@pagopa.it>
    Co-authored-by: fabriziofff <fartux@gmail.com>

--- ts/features/zendesk/navigation/navigator.tsx
+++ ts/features/zendesk/navigation/navigator.tsx
@@ -3,11 +3,13 @@ import ZendeskSupportHelpCenter from "../screens/ZendeskSupportHelpCenter";
 import ZendeskAskPermissions from "../screens/ZendeskAskPermissions";
 import ZendeskChooseCategory from "../screens/ZendeskChooseCategory";
 import ZendeskChooseSubCategory from "../screens/ZendeskChooseSubCategory";
+import ZendeskPanicMode from "../screens/ZendeskPanicMode";
 import ZENDESK_ROUTES from "./routes";
 
 export const zendeskSupportNavigator = createStackNavigator(
   {
     [ZENDESK_ROUTES.HELP_CENTER]: { screen: ZendeskSupportHelpCenter },
+    [ZENDESK_ROUTES.PANIC_MODE]: { screen: ZendeskPanicMode },
     [ZENDESK_ROUTES.ASK_PERMISSIONS]: { screen: ZendeskAskPermissions },
     [ZENDESK_ROUTES.CHOOSE_CATEGORY]: { screen: ZendeskChooseCategory },
     [ZENDESK_ROUTES.CHOOSE_SUB_CATEGORY]: { screen: ZendeskChooseSubCategory }
