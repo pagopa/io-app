86810b7b26edb8a8ca53837e972c06455c47d35c
fabriziofff <fartux@gmail.com>
2021-12-15 15:56:51 +0100

    chore(Messaggi a valore legale): [IAMVL-23] Display plain and html message body (#3575)
    * wip
    
    * add legalmessageheader
    
    * simplify CtaBar and HeaderDueDateBar
    
    * fix
    
    * fix
    
    * rework CTABar
    
    * refactoring
    
    * add placeholders
    
    * fix tests
    
    * add comments
    
    * addd tests
    
    * align images to center
    
    * refactoring
    
    * fix mock due date
    
    * delete style
    
    * fix merge
    
    * add react-native-render-html and first MvlBody
    
    * refactoring
    
    * renaming
    
    * change mock text
    
    * graphical refinement
    
    * add tests
    
    * remove comment
    
    * add comments
    
    Co-authored-by: pietro909 <2094604+pietro909@users.noreply.github.com>

--- ts/features/mvl/screens/details/components/MvlBody.tsx
+++ ts/features/mvl/screens/details/components/MvlBody.tsx
@@ -1,26 +1,109 @@
 import { View } from "native-base";
 import * as React from "react";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { IORenderHtml } from "../../../../../components/core/IORenderHtml";
 import { Body } from "../../../../../components/core/typography/Body";
-import { Label } from "../../../../../components/core/typography/Label";
+import { H4 } from "../../../../../components/core/typography/H4";
+import { Link } from "../../../../../components/core/typography/Link";
+import I18n from "../../../../../i18n";
+import { isStringNullyOrEmpty } from "../../../../../utils/strings";
 import { MvlData } from "../../../types/mvlData";
 
 type Props = {
   body: MvlData["body"];
 };
+const styles = StyleSheet.create({
+  row: { flexDirection: "row", flexWrap: "wrap" }
+});
+
+type RenderMode = "plain" | "html";
+
+const PlainBody = (props: { text: string }): React.ReactElement | null =>
+  isStringNullyOrEmpty(props.text) ? null : <Body>{props.text}</Body>;
+
+const HtmlBody = (props: { html: string }): React.ReactElement | null => {
+  if (isStringNullyOrEmpty(props.html)) {
+    return null;
+  }
+  return <IORenderHtml source={{ html: props.html }} />;
+};
 
 /**
- * Render the body of a legal message, allows the user to choose between plain text or html representation
- * TODO: this is a placeholder, will be implemented in https://pagopa.atlassian.net/browse/IAMVL-23
- * @param _
+ * Switch between different body representation
+ * @param props
  * @constructor
  */
-export const MvlBody = (_: Props): React.ReactElement => (
-  <View>
-    <Body>
-      <Label>{"Body Placeholder\n"}</Label>
-      {
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
-      }
-    </Body>
+const Content = (props: {
+  mode: RenderMode;
+  body: MvlData["body"];
+}): React.ReactElement => {
+  switch (props.mode) {
+    case "plain":
+      return <PlainBody text={props.body.plain} />;
+    case "html":
+      return <HtmlBody html={props.body.html} />;
+  }
+};
+
+/**
+ * The single element of the {@link Selector}, handling the enable and disable state
+ * @param props
+ * @constructor
+ */
+const SelectorItem = (props: {
+  currentSelected: boolean;
+  text: string;
+  onPress: () => void;
+}) =>
+  props.currentSelected ? (
+    <H4 weight={"SemiBold"} color={"bluegreyLight"}>
+      {props.text}
+    </H4>
+  ) : (
+    <Link onPress={() => props.onPress()}>{props.text}</Link>
+  );
+
+/**
+ * A textual selector that allows the user to change representation, switching from html to plain text
+ * @param props
+ * @constructor
+ */
+const Selector = (props: {
+  currentValue: RenderMode;
+  onValueChanged: (mode: RenderMode) => void;
+}) => (
+  <View style={styles.row}>
+    <SelectorItem
+      currentSelected={props.currentValue === "plain"}
+      text={I18n.t("features.mvl.details.body.plain")}
+      onPress={() => props.onValueChanged("plain")}
+    />
+    <H4 weight={"Regular"} accessible={false}>
+      {" | "}
+    </H4>
+    <SelectorItem
+      currentSelected={props.currentValue === "html"}
+      text={I18n.t("features.mvl.details.body.html")}
+      onPress={() => props.onValueChanged("html")}
+    />
   </View>
 );
+
+/**
+ * Render the body of a legal message, allows the user to choose between plain text or html representation.
+ * The default representation is the plain text
+ * @constructor
+ * @param props
+ */
+export const MvlBody = (props: Props): React.ReactElement => {
+  const [mode, setMode] = useState<RenderMode>("plain");
+
+  return (
+    <>
+      <Content mode={mode} body={props.body} />
+      <View spacer={true} small={true} />
+      <Selector currentValue={mode} onValueChanged={setMode} />
+    </>
+  );
+};
--- ts/features/mvl/types/__mock__/mvlMock.ts
+++ ts/features/mvl/types/__mock__/mvlMock.ts
@@ -9,8 +9,21 @@ import { Mvl, MvlAttachment, MvlBody, MvlData, MvlMetadata } from "../mvlData";
 export const mvlMockId = "mockId" as UIMessageId;
 
 export const mvlMockBody: MvlBody = {
-  html: "This is an html <b>text</b>",
-  plain: "This is a plain text"
+  html:
+    "<h1>This is a legal message</h1> " +
+    "<h2>It contains information with legal value</h2>" +
+    "<img " +
+    'width="200" height="100" ' +
+    'alt="IO Cat" ' +
+    'style="align-self: center;" ' +
+    'src="https://placekitten.com/g/200/100"' +
+    "/>" +
+    "This is the text of the <i>message</i> that could include some <b>additional information</b> or a <a href='https://io.italia.it/'>link</a>!\n" +
+    "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  plain:
+    "This is a legal message\nIt contains information with legal value\n" +
+    "This is the text of the message that could include some additional information or a link! " +
+    "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
 };
 
 export const mvlMockPdfAttachment: MvlAttachment = {
