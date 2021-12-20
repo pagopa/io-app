8a972197be031be35f6dd7cae363b39646f619d2
Simone <simone.biffi.91@gmail.com>
2021-12-20 17:49:41 +0100

    add zendesk analytics events

---
+++ ts/features/zendesk/analytics/index.ts
@@ -0,0 +1,46 @@
+import { getType } from "typesafe-actions";
+import { mixpanel } from "../../../mixpanel";
+import { Action } from "../../../store/actions/types";
+
+import {
+  getZendeskConfig,
+  zendeskSelectedCategory,
+  zendeskSupportBack,
+  zendeskSupportCancel,
+  zendeskSupportCompleted,
+  zendeskSupportFailure,
+  zendeskSupportStart
+} from "../store/actions";
+import { getNetworkErrorMessage } from "../../../utils/errors";
+
+const trackZendesk =
+  (mp: NonNullable<typeof mixpanel>) =>
+  (action: Action): Promise<void> => {
+    switch (action.type) {
+      case getType(zendeskSupportCompleted):
+      case getType(zendeskSupportCancel):
+      case getType(zendeskSupportBack):
+      case getType(zendeskSupportFailure):
+      case getType(getZendeskConfig.request):
+      case getType(getZendeskConfig.success):
+        return mp.track(action.type);
+      case getType(zendeskSupportStart):
+        return mp.track(action.type, {
+          isAssistanceForPayment: action.payload.assistanceForPayment
+        });
+      case getType(zendeskSelectedCategory):
+        return mp.track(action.type, {
+          category: action.payload.value
+        });
+      case getType(getZendeskConfig.failure):
+        return mp.track(action.type, {
+          reason: getNetworkErrorMessage(action.payload)
+        });
+    }
+    return Promise.resolve();
+  };
+
+const emptyTracking = (_: NonNullable<typeof mixpanel>) => (__: Action) =>
+  Promise.resolve();
+
+export default false ? trackZendesk : emptyTracking;
