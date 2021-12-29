d4cb3be74f5249c0e2131ee8c23c9dc1a5bcfbf5
Matteo Boschi <mbmaken@gmail.com>
2021-12-29 21:40:27 +0100

    Revert "improve log"
    This reverts commit 8e6c9b2e5abf60a6eeaa9f818746bb7ee87f1e33.

--- ts/sagas/startup/watchSessionExpiredSaga.ts
+++ ts/sagas/startup/watchSessionExpiredSaga.ts
@@ -11,10 +11,7 @@ import { OPERISSUES_10_track } from "../startup";
  */
 export function* watchSessionExpiredSaga(): IterableIterator<Effect> {
   yield takeLatest(getType(sessionExpired), function* () {
-    yield call(OPERISSUES_10_track, "watchSessionExpiredSaga_1", {
-      isResetAssistanceDataDefined:
-        resetAssistanceData !== null && resetAssistanceData !== undefined
-    });
+    yield call(OPERISSUES_10_track, "watchSessionExpiredSaga_1");
     // clean up any assistance data
     resetAssistanceData();
     yield call(OPERISSUES_10_track, "watchSessionExpiredSaga_2");
