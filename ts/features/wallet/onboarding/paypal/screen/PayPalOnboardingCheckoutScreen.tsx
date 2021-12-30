1a4ddfcd6dc99699f9a854183ad79784cd7f91b3
Matteo Boschi <mbmaken@gmail.com>
2021-12-30 17:51:28 +0100

    add comment

--- ts/features/wallet/onboarding/paypal/screen/PayPalOnboardingCheckoutScreen.tsx
+++ ts/features/wallet/onboarding/paypal/screen/PayPalOnboardingCheckoutScreen.tsx
@@ -116,6 +116,7 @@ const PayPalOnboardingCheckoutScreen = (props: Props) => {
     navigation.dispatch(navigateToPayPalCheckoutCompleted());
   };
 
+  // notify the user that the current onboarding operation could be interrupted
   const handleBack = () => {
     Alert.alert(I18n.t("wallet.abortWebView.title"), "", [
       {
