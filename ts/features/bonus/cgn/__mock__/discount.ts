b521b455b731dae98d55d407c0ecbb7ebd4ab2bf
CrisTofani <cri.tofani@gmail.com>
2022-01-11 09:15:49 +0100

    Updates mock and snapshot

--- ts/features/bonus/cgn/__mock__/discount.ts
+++ ts/features/bonus/cgn/__mock__/discount.ts
@@ -2,7 +2,7 @@ import { Discount } from "../../../../../definitions/cgn/merchants/Discount";
 
 export const mockDiscount: Discount = {
   name: "a name" as any,
-  startDate: new Date(),
-  endDate: new Date(),
+  startDate: new Date("2022-02-07T08:43:38.000Z"),
+  endDate: new Date("2022-02-20T08:43:38.000Z"),
   productCategories: []
 };
