37fb5d6b4a088ab33754191efb4eca1c83f038ec
Matteo Boschi <mbmaken@gmail.com>
2022-02-01 15:01:26 +0100

    Update ts/utils/dates.ts
    Co-authored-by: fabriziofff <26501317+fabriziofff@users.noreply.github.com>

--- ts/utils/dates.ts
+++ ts/utils/dates.ts
@@ -165,8 +165,8 @@ export const isExpired = (
     .chain(year =>
       maybeMonth.map(month => {
         const now = new Date();
-        const nowYearMonth = new Date(now.getFullYear(), now.getMonth());
-        const cardExpirationDate = new Date(year, month - 1);
+        const nowYearMonth = new Date(now.getFullYear(), now.getMonth()+1);
+        const cardExpirationDate = new Date(year, month);
         return nowYearMonth > cardExpirationDate;
       })
     )
