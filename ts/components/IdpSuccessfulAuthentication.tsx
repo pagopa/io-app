2772d8164c9fa318856dd78241a66f47ac6b2ba6
Emilio Pavia <emilio.pavia@gmail.com>
2022-02-02 11:20:13 +0100

    Add StatusBar component to IdpSuccessfulAuthentication

--- ts/components/IdpSuccessfulAuthentication.tsx
+++ ts/components/IdpSuccessfulAuthentication.tsx
@@ -3,7 +3,7 @@
  */
 import { View } from "native-base";
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
 
 import variables from "../theme/variables";
 import IconFont from "./ui/IconFont";
@@ -21,6 +21,10 @@ const tickSize = variables.iconSize6 * 2;
 
 export const IdpSuccessfulAuthentication = () => (
   <View style={styles.container}>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor={styles.container.backgroundColor}
+    />
     <IconFont name="io-tick-big" color={variables.colorWhite} size={tickSize} />
   </View>
 );
