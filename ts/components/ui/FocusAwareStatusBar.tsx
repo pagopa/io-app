e714d2525da71dee2c4890a3c14dcfea866f24e1
Emilio Pavia <emilio.pavia@gmail.com>
2022-02-03 10:47:05 +0100

    Add a comment to FocusAwareStatusBar

--- ts/components/ui/FocusAwareStatusBar.tsx
+++ ts/components/ui/FocusAwareStatusBar.tsx
@@ -2,6 +2,15 @@ import * as React from "react";
 import { StatusBar, StatusBarProps } from "react-native";
 import { withNavigationFocus } from "react-navigation";
 
+/**
+ * FocusAwareStatusBar makes the status bar component aware of
+ * screen focus and renders it only when the screen is focused.
+ * This is needed if you're using a tab or drawer navigator,
+ * because all the screens in the navigator might be rendered
+ * at once and kept rendered - that means that the last StatusBar
+ * config you set will be used (likely on the final tab of your
+ * tab navigator, not what the user is seeing).
+ */
 const FocusAwareStatusBar = withNavigationFocus<StatusBarProps>(
   ({ isFocused, ...rest }) => (isFocused ? <StatusBar {...rest} /> : null)
 );
