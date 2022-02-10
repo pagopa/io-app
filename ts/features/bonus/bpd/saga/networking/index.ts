465b339963413da4f3311158b2953d6a4ad6a057
Simone <simone.biffi.91@gmail.com>
2022-02-10 18:06:05 +0100

    fix action not dispatched

--- ts/features/bonus/bpd/saga/networking/index.ts
+++ ts/features/bonus/bpd/saga/networking/index.ts
@@ -87,6 +87,7 @@ export function* putOptInStatusCitizenV2(
         // (note the required header will be injected automatically)
         { citizenOptInStatus: action.payload } as any
       );
+
     if (updateCitizenIOResult.isRight()) {
       if (updateCitizenIOResult.value.status === 200) {
         const { optInStatus } = updateCitizenIOResult.value.value;
@@ -98,8 +99,10 @@ export function* putOptInStatusCitizenV2(
         );
       }
     } else {
-      bpdUpdateOptInStatusMethod.failure(
-        new Error(readableReport(updateCitizenIOResult.value))
+      yield put(
+        bpdUpdateOptInStatusMethod.failure(
+          new Error(readableReport(updateCitizenIOResult.value))
+        )
       );
     }
   } catch (e) {
