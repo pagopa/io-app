eb4fd39653d522083085d8f3ffb4c081f561b286
Simone <simone.biffi.91@gmail.com>
2022-02-10 18:06:48 +0100

    manage api status

--- ts/features/bonus/bpd/screens/optInPaymentMethods/OptInPaymentMethodsThankYouDeleteMethodsScreen.tsx
+++ ts/features/bonus/bpd/screens/optInPaymentMethods/OptInPaymentMethodsThankYouDeleteMethodsScreen.tsx
@@ -7,22 +7,44 @@ import FooterWithButtons from "../../../../../components/ui/FooterWithButtons";
 import { cancelButtonProps } from "../../../bonusVacanze/components/buttons/ButtonConfigurations";
 import { useIOSelector } from "../../../../../store/hooks";
 import { deleteAllPaymentMethodsByFunctionSelector } from "../../../../../store/reducers/wallet/wallets";
-import { isError } from "../../model/RemoteValue";
+import { isError, isLoading, isUndefined } from "../../model/RemoteValue";
 import Completed from "../../../../../../img/pictograms/payment-completed.svg";
 import { InfoScreenComponent } from "../../../../../components/infoScreen/InfoScreenComponent";
-import { optInPaymentMethodsDeletionChoice } from "../../store/actions/optInPaymentMethods";
+import {
+  optInPaymentMethodsCompleted,
+  optInPaymentMethodsDeletionChoice
+} from "../../store/actions/optInPaymentMethods";
+import { showToast } from "../../../../../utils/showToast";
+import I18n from "../../../../../i18n";
+import { optInStatusSelector } from "../../store/reducers/details/activation";
+import * as pot from "italia-ts-commons/lib/pot";
+import { CitizenOptInStatusEnum } from "../../../../../../definitions/bpd/citizen_v2/CitizenOptInStatus";
+import { LoadingErrorComponent } from "../../../bonusVacanze/components/loadingErrorScreen/LoadingErrorComponent";
+import { RTron } from "../../../../../boot/configureStoreAndPersistor";
 
 const OptInPaymentMethodsThankYouDeleteMethodsScreen = () => {
   const dispatch = useDispatch();
   const deleteAllPaymentMethodsByFunctionStatus = useIOSelector(
     deleteAllPaymentMethodsByFunctionSelector
   );
+  const optInStatus = useIOSelector(optInStatusSelector);
+
   useEffect(() => {
     // dispatch saga that delete payment methods and update opt-in field
     dispatch(optInPaymentMethodsDeletionChoice());
   }, [dispatch]);
 
-  // if one of the
+  const isOptInStatusLoading = pot.fold(
+    optInStatus,
+    () => true,
+    () => true,
+    _ => true,
+    _ => false,
+    _ => false,
+    _ => true,
+    _ => true,
+    _ => false
+  );
 
   // if the payment methods deletion fails show the retry component
   if (isError(deleteAllPaymentMethodsByFunctionStatus)) {
@@ -35,7 +57,27 @@ const OptInPaymentMethodsThankYouDeleteMethodsScreen = () => {
     );
   }
 
+  // if one between deleteAllPaymentMethodsByFunctionStatus and optInStatus is on loading state show loading component
+  if (
+    isUndefined(deleteAllPaymentMethodsByFunctionStatus) ||
+    isLoading(deleteAllPaymentMethodsByFunctionStatus) ||
+    isOptInStatusLoading
+  ) {
+    return (
+      <LoadingErrorComponent
+        isLoading={true}
+        loadingCaption={"sto loadanod"}
+        onRetry={() => true}
+      />
+    );
+  }
+
   // if the opt-in choice fails complete the workunit and show an error toast to the user
+  if (pot.isError(optInStatus)) {
+    showToast(I18n.t("msgErrorUpdateApp"));
+    dispatch(optInPaymentMethodsCompleted());
+    return null;
+  }
 
   // both the payment methods deletion and the opt-in update succeed, show the thank you page
   return (
@@ -46,7 +88,10 @@ const OptInPaymentMethodsThankYouDeleteMethodsScreen = () => {
       <ThankYouComponent />
       <FooterWithButtons
         type={"SingleButton"}
-        leftButton={cancelButtonProps(() => true, "Test")}
+        leftButton={cancelButtonProps(
+          () => dispatch(optInPaymentMethodsCompleted()),
+          "Vai al Portafoglio"
+        )}
       />
     </SafeAreaView>
   );
