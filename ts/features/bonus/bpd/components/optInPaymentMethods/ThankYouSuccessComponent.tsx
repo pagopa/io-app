21958403cf7cab49464a7432357d5069b476cadb
Simone <simone.biffi.91@gmail.com>
2022-02-14 10:55:57 +0100

    update labels

--- ts/features/bonus/bpd/components/optInPaymentMethods/ThankYouSuccessComponent.tsx
+++ ts/features/bonus/bpd/components/optInPaymentMethods/ThankYouSuccessComponent.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { SafeAreaView } from "react-native";
 import { InfoScreenComponent } from "../../../../../components/infoScreen/InfoScreenComponent";
 import Completed from "../../../../../../img/pictograms/payment-completed.svg";
 import { IOStyles } from "../../../../../components/core/variables/IOStyles";
-import { SafeAreaView } from "react-native";
 import { cancelButtonProps } from "../../../bonusVacanze/components/buttons/ButtonConfigurations";
 import { optInPaymentMethodsCompleted } from "../../store/actions/optInPaymentMethods";
 import FooterWithButtons from "../../../../../components/ui/FooterWithButtons";
+import I18n from "../../../../../i18n";
 
 const ThankYouSuccessComponent = () => {
   const dispatch = useDispatch();
@@ -17,14 +18,16 @@ const ThankYouSuccessComponent = () => {
     >
       <InfoScreenComponent
         image={<Completed width={80} height={80} />}
-        title={"Fatto!"}
-        body={"Abbiamo salvato la tua scelta."}
+        title={I18n.t(
+          "bonus.bpd.optInPaymentMethods.thankYouPage.success.title"
+        )}
+        body={I18n.t("bonus.bpd.optInPaymentMethods.thankYouPage.success.body")}
       />
       <FooterWithButtons
         type={"SingleButton"}
         leftButton={cancelButtonProps(
           () => dispatch(optInPaymentMethodsCompleted()),
-          "Vai al Portafoglio"
+          I18n.t("bonus.bpd.optInPaymentMethods.thankYouPage.cta.goToWallet")
         )}
       />
     </SafeAreaView>
