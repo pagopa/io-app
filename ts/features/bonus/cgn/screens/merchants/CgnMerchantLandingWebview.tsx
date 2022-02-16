105257ac5b76e7139dda50d82e6c41754c18f5c7
CrisTofani <cri.tofani@gmail.com>
2022-02-16 17:18:44 +0100

    Change LandingPage Webview header with close button

--- ts/features/bonus/cgn/screens/merchants/CgnMerchantLandingWebview.tsx
+++ ts/features/bonus/cgn/screens/merchants/CgnMerchantLandingWebview.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { SafeAreaView } from "react-native";
 import { NavigationInjectedProps } from "react-navigation";
-import BaseScreenComponent from "../../../../../components/screens/BaseScreenComponent";
+import { Body, Container, Right } from "native-base";
 import { IOStyles } from "../../../../../components/core/variables/IOStyles";
-import { emptyContextualHelp } from "../../../../../utils/emptyContextualHelp";
 import WebviewComponent from "../../../../../components/WebviewComponent";
+import AppHeader from "../../../../../components/ui/AppHeader";
+import ButtonDefaultOpacity from "../../../../../components/ButtonDefaultOpacity";
+import IconFont from "../../../../../components/ui/IconFont";
+import { useNavigationContext } from "../../../../../utils/hooks/useOnFocus";
 
 type NavigationParams = Readonly<{
   landingPageUrl: string;
@@ -16,11 +19,23 @@ type Props = NavigationInjectedProps<NavigationParams>;
 const CgnMerchantLandingWebview: React.FunctionComponent<Props> = (
   props: Props
 ) => {
+  const navigation = useNavigationContext();
   const landingPageUrl = props.navigation.getParam("landingPageUrl");
   const landingPageReferrer = props.navigation.getParam("landingPageReferrer");
 
   return (
-    <BaseScreenComponent goBack={true} contextualHelp={emptyContextualHelp}>
+    <Container>
+      <AppHeader noLeft={true}>
+        <Body />
+        <Right>
+          <ButtonDefaultOpacity
+            onPress={() => navigation.goBack()}
+            transparent={true}
+          >
+            <IconFont name="io-close" />
+          </ButtonDefaultOpacity>
+        </Right>
+      </AppHeader>
       <SafeAreaView style={IOStyles.flex}>
         <WebviewComponent
           source={{
@@ -31,7 +46,7 @@ const CgnMerchantLandingWebview: React.FunctionComponent<Props> = (
           }}
         />
       </SafeAreaView>
-    </BaseScreenComponent>
+    </Container>
   );
 };
 
