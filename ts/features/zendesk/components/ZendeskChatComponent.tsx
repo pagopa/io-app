df7bb72b7c5ece59d5b82f0f5ca077db8b20caf7
Simone <simone.biffi.91@gmail.com>
2022-02-16 14:47:42 +0100

    added the ZendeskChatComponent

---
+++ ts/features/zendesk/components/ZendeskChatComponent.tsx
@@ -0,0 +1,45 @@
+import { View } from "react-native";
+import * as React from "react";
+import { useIOSelector } from "../../../store/hooks";
+import {
+  zendeskTicketNumberSelector,
+  zendeskTotalNewResponseSelector
+} from "../store/reducers";
+import { getValueOrElse } from "../../bonus/bpd/model/RemoteValue";
+import ButtonDefaultOpacity from "../../../components/ButtonDefaultOpacity";
+import { mixpanelTrack } from "../../../mixpanel";
+import { showSupportTickets } from "../../../utils/supportAssistance";
+import I18n from "../../../i18n";
+import IconFont from "../../../components/ui/IconFont";
+import CustomBadge from "../../../components/ui/CustomBadge";
+
+const ZendeskChatComponent = () => {
+  const ticketsNumber = useIOSelector(zendeskTicketNumberSelector);
+  const maybeTotalNewResponse = useIOSelector(zendeskTotalNewResponseSelector);
+
+  const totalNewResponse = getValueOrElse(maybeTotalNewResponse, 0);
+
+  if (getValueOrElse(ticketsNumber, 0) <= 0) {
+    return null;
+  }
+  return (
+    <ButtonDefaultOpacity
+      onPress={() => {
+        void mixpanelTrack("ZENDESK_SHOW_TICKETS_FROM_CHAT");
+        showSupportTickets();
+      }}
+      transparent={true}
+      accessibilityLabel={I18n.t("global.accessibility.chat.description")}
+      accessibilityHint={"accessibilityHint"}
+    >
+      <IconFont name="io-chat" />
+      {totalNewResponse > 0 && (
+        <View style={{ position: "absolute", left: 6, bottom: 10 }}>
+          <CustomBadge badgeValue={totalNewResponse} />
+        </View>
+      )}
+    </ButtonDefaultOpacity>
+  );
+};
+
+export default ZendeskChatComponent;
