6ed7b63d1c8c5080230a27cc64990d4981943db0
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-02-17 16:06:25 +0100

    Created bottom button logic

--- ts/features/bonus/bpd/screens/optInPaymentMethods/OptInPaymentMethodsChoiceScreen.tsx
+++ ts/features/bonus/bpd/screens/optInPaymentMethods/OptInPaymentMethodsChoiceScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, ScrollView } from "react-native";
 import { View, Text } from "native-base";
 import I18n from "../../../../../i18n";
@@ -6,32 +6,74 @@ import BaseScreenComponent from "../../../../../components/screens/BaseScreenCom
 import { IOStyles } from "../../../../../components/core/variables/IOStyles";
 import { H1 } from "../../../../../components/core/typography/H1";
 import ButtonDefaultOpacity from "../../../../../components/ButtonDefaultOpacity";
+import FooterWithButtons from "../../../../../components/ui/FooterWithButtons";
+import {
+  confirmButtonProps,
+  errorButtonProps,
+  disablePrimaryButtonProps
+} from "../../../bonusVacanze/components/buttons/ButtonConfigurations";
+import { BlockButtonProps } from "../../../../../components/ui/BlockButtons";
 
-const OptInPaymentMethodsChoiceScreen = () => (
-  // The void customRightIcon and customGoBack are needed to have a centered header title
-  <BaseScreenComponent
-    showInstabugChat={false}
-    goBack={false}
-    headerTitle={I18n.t("bonus.bpd.optInPaymentMethods.choice.header")}
-    customRightIcon={{
-      iconName: "",
-      onPress: () => true
-    }}
-    customGoBack={
-      <ButtonDefaultOpacity onPress={() => true} transparent={true} />
-    }
-  >
-    <SafeAreaView
-      style={IOStyles.flex}
-      testID={"OptInPaymentMethodsCashbackUpdate"}
+type PaymentMethodsChoiceOptions =
+  | "keepPaymentMethods"
+  | "deletePaymentMethods";
+
+const bottomButtons: {
+  [key in PaymentMethodsChoiceOptions]: BlockButtonProps;
+} = {
+  keepPaymentMethods: confirmButtonProps(
+    () => null,
+    I18n.t("global.buttons.continue"),
+    undefined,
+    "continueButton"
+  ),
+
+  deletePaymentMethods: errorButtonProps(
+    () => null,
+    I18n.t("bonus.bpd.optInPaymentMethods.choice.deleteAllButton"),
+    undefined
+  )
+};
+
+const OptInPaymentMethodsChoiceScreen = () => {
+  const [selectedMethod] = useState<PaymentMethodsChoiceOptions | null>(null);
+
+  const computedBottomButtonProps =
+    selectedMethod === null
+      ? disablePrimaryButtonProps(I18n.t("global.buttons.continue"))
+      : bottomButtons[selectedMethod];
+
+  return (
+    // The void customRightIcon and customGoBack are needed to have a centered header title
+    <BaseScreenComponent
+      showInstabugChat={false}
+      goBack={false}
+      headerTitle={I18n.t("bonus.bpd.optInPaymentMethods.choice.header")}
+      customRightIcon={{
+        iconName: "",
+        onPress: () => true
+      }}
+      customGoBack={
+        <ButtonDefaultOpacity onPress={() => true} transparent={true} />
+      }
     >
-      <ScrollView style={[IOStyles.horizontalContentPadding]}>
-        <H1>{I18n.t("bonus.bpd.optInPaymentMethods.choice.title")}</H1>
-        <Text>{I18n.t("bonus.bpd.optInPaymentMethods.choice.subtitle")}</Text>
-        <View spacer />
-      </ScrollView>
-    </SafeAreaView>
-  </BaseScreenComponent>
-);
+      <SafeAreaView
+        style={IOStyles.flex}
+        testID={"OptInPaymentMethodsCashbackUpdate"}
+      >
+        <ScrollView style={[IOStyles.horizontalContentPadding]}>
+          <H1>{I18n.t("bonus.bpd.optInPaymentMethods.choice.title")}</H1>
+          <View spacer small />
+          <Text>{I18n.t("bonus.bpd.optInPaymentMethods.choice.subtitle")}</Text>
+          <View spacer />
+        </ScrollView>
+        <FooterWithButtons
+          type={"SingleButton"}
+          leftButton={computedBottomButtonProps}
+        />
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
 
 export default OptInPaymentMethodsChoiceScreen;
