f3cd208892715e8d933667b852f929021e9ec963
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-02-18 16:15:01 +0100

    Updated tests

--- ts/features/bonus/bpd/screens/optInPaymentMethods/__tests__/OptInPaymentMethodsChoiceScreen.test.tsx
+++ ts/features/bonus/bpd/screens/optInPaymentMethods/__tests__/OptInPaymentMethodsChoiceScreen.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationParams } from "react-navigation";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { createStore } from "redux";
-import { fireEvent } from "@testing-library/react-native";
+import { fireEvent, waitFor } from "@testing-library/react-native";
 import { appReducer } from "../../../../../../store/reducers";
 import { renderScreenFakeNavRedux } from "../../../../../../utils/testWrapper";
 import OptInPaymentMethodsChoiceScreen from "../OptInPaymentMethodsChoiceScreen";
@@ -10,54 +10,61 @@ import { GlobalState } from "../../../../../../store/reducers/types";
 import BPD_ROUTES from "../../../navigation/routes";
 import I18n from "../../../../../../i18n";
 
+jest.useFakeTimers();
+
 describe("the OptInPaymentMethodsChoiceScreen screen", () => {
-  it("should first render with a disabled button", async () => {
+  it("should first render with a disabled button", () => {
     const component = renderComponent();
-    const disabledButton = await component.findByTestId(
-      "disabledContinueButton"
-    );
+    const disabledButton = component.getByTestId("disabledContinueButton");
 
     expect(disabledButton).toBeTruthy();
   });
 
-  it("should show the activated primary button after selecting the `Keep` option", async () => {
+  it("should show the activated primary button after selecting the `Keep` option", () => {
     const component = renderComponent();
 
-    const firstOptionTitle = await component.findByText(
+    const firstOptionTitle = component.getByText(
       I18n.t("bonus.bpd.optInPaymentMethods.choice.options.keepAll.title")
     );
 
-    await fireEvent.press(firstOptionTitle);
+    fireEvent.press(firstOptionTitle);
 
-    const activeButton = await component.findByTestId("continueButton");
+    const activeButton = component.getByTestId("continueButton");
 
     expect(activeButton).toBeTruthy();
   });
 
-  it("should show the red button after selecting the `Delete all` option and a bottom sheet after pressing it", async () => {
+  it("should show the red button after selecting the `Delete all` option and a bottom sheet after pressing it", () => {
     const component = renderComponent();
 
-    const secondOptionTitle = await component.findByText(
+    const secondOptionTitle = component.getByText(
       I18n.t("bonus.bpd.optInPaymentMethods.choice.options.deleteAll.title")
     );
 
-    await fireEvent.press(secondOptionTitle);
+    fireEvent.press(secondOptionTitle);
 
-    const redButton = await component.findByText(
+    const redButton = component.getByText(
       I18n.t("bonus.bpd.optInPaymentMethods.choice.deleteAllButton")
     );
 
     expect(redButton).toBeTruthy();
 
-    await fireEvent.press(redButton);
+    fireEvent.press(redButton);
 
-    const bottomSheet = await component.findByText(
-      I18n.t(
-        "bonus.bpd.optInPaymentMethods.deletePaymentMethodsBottomSheet.title"
-      )
-    );
+    // This is an hack to use asynchronous code inside
+    // jest while using `jest.useFakeTimers()`.
+    // See: https://stackoverflow.com/a/51132058/9825478
+    void Promise.resolve().then(() => jest.advanceTimersByTime(100));
 
-    expect(bottomSheet).toBeTruthy();
+    return waitFor(() => {
+      const bottomSheet = component.getByText(
+        I18n.t(
+          "bonus.bpd.optInPaymentMethods.deletePaymentMethodsBottomSheet.title"
+        )
+      );
+
+      expect(bottomSheet).toBeTruthy();
+    });
   });
 });
 
