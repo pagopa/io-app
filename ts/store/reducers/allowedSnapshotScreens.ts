79b1989d030390fb8e00ff993a9c86d123b57411
fabriziofff <26501317+fabriziofff@users.noreply.github.com>
2022-02-21 17:47:56 +0100

    Update ts/store/reducers/allowedSnapshotScreens.ts

--- ts/store/reducers/allowedSnapshotScreens.ts
+++ ts/store/reducers/allowedSnapshotScreens.ts
@@ -6,7 +6,7 @@ import { currentRouteSelector } from "./navigation";
 export const screenBlackList = new Set([ROUTES.WALLET_ADD_CARD as string]);
 
 /**
- * Return {true} if a snapshot can be made in the current screen (android only).
+ * Return {true} if a snapshot can be taken in the current screen (android only).
  * If the app is in debug mode, the snapshot is always possible.
  */
 export const isAllowedSnapshotCurrentScreen = createSelector(
