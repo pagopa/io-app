08ea5df0ef8af1e3d532bbf24f89eb78ee24c499
Simone <simone.biffi.91@gmail.com>
2022-03-01 09:47:46 +0100

    update saga typing

--- ts/features/bonus/bpd/saga/orchestration/optInPaymentMethods/optInShouldShowChoiceHandler.ts
+++ ts/features/bonus/bpd/saga/orchestration/optInPaymentMethods/optInShouldShowChoiceHandler.ts
@@ -1,4 +1,4 @@
-import { Effect, put, select, take } from "redux-saga/effects";
+import { put, select, take } from "typed-redux-saga/macro";
 import { ActionType, getType, isActionOf } from "typesafe-actions";
 import * as pot from "italia-ts-commons/lib/pot";
 import {
@@ -16,6 +16,7 @@ import {
   optInStatusSelector
 } from "../../../store/reducers/details/activation";
 import { CitizenOptInStatusEnum } from "../../../../../../../definitions/bpd/citizen_v2/CitizenOptInStatus";
+import { ReduxSagaEffect } from "../../../../../../types/utils";
 
 /**
  * This saga manage the flow that checks if a user has already take a choice about the opt-in of the payment methods.
@@ -28,26 +29,30 @@ import { CitizenOptInStatusEnum } from "../../../../../../../definitions/bpd/cit
  * - check if the loading of the payment method succeed
  * - if succeed start the saga
  */
-export function* optInShouldShowChoiceHandler(): Generator<Effect, void, any> {
+export function* optInShouldShowChoiceHandler(): Generator<
+  ReduxSagaEffect,
+  void,
+  any
+> {
   // Load the information about the participation of the user to the bpd program
-  yield put(bpdAllData.request());
-  const bpdAllDataResponse: ActionType<
-    typeof bpdAllData.success | typeof bpdAllData.failure
-  > = yield take([getType(bpdAllData.success), getType(bpdAllData.failure)]);
+  yield* put(bpdAllData.request());
+  const bpdAllDataResponse = yield* take<
+    ActionType<typeof bpdAllData.success | typeof bpdAllData.failure>
+  >([getType(bpdAllData.success), getType(bpdAllData.failure)]);
 
   // If the bpdAllData request fail report the error
   if (isActionOf(bpdAllData.failure, bpdAllDataResponse)) {
-    yield put(
+    yield* put(
       optInPaymentMethodsShowChoice.failure(bpdAllDataResponse.payload)
     );
     return;
   }
 
-  const bpdEnabled: pot.Pot<boolean, Error> = yield select(bpdEnabledSelector);
+  const bpdEnabled: pot.Pot<boolean, Error> = yield* select(bpdEnabledSelector);
 
   // Safety check on field returned in @link{bpdEnabled} and managed by @{enabledReducer}
   if (bpdEnabled.kind !== "PotSome") {
-    yield put(
+    yield* put(
       optInPaymentMethodsShowChoice.failure(
         new Error("The bpdEnabled value is not potSome")
       )
@@ -57,17 +62,17 @@ export function* optInShouldShowChoiceHandler(): Generator<Effect, void, any> {
 
   // If the user is not enrolled in the bpd program returns with value false
   if (!bpdEnabled.value) {
-    yield put(optInPaymentMethodsShowChoice.success(bpdEnabled.value));
+    yield* put(optInPaymentMethodsShowChoice.success(bpdEnabled.value));
     return;
   }
 
-  const optInStatus: pot.Pot<CitizenOptInStatusEnum, Error> = yield select(
+  const optInStatus: pot.Pot<CitizenOptInStatusEnum, Error> = yield* select(
     optInStatusSelector
   );
 
   // Safety check on field returned in @link{optInStatus} and managed by @{optInStatusReducer}
   if (optInStatus.kind !== "PotSome") {
-    yield put(
+    yield* put(
       optInPaymentMethodsShowChoice.failure(
         new Error("The optInStatus value is not potSome")
       )
@@ -77,22 +82,22 @@ export function* optInShouldShowChoiceHandler(): Generator<Effect, void, any> {
 
   // If the user already take a choice returns with value false
   if (optInStatus.value !== CitizenOptInStatusEnum.NOREQ) {
-    yield put(optInPaymentMethodsShowChoice.success(false));
+    yield* put(optInPaymentMethodsShowChoice.success(false));
     return;
   }
 
   // Load the user payment methods
-  yield put(fetchWalletsRequestWithExpBackoff());
-  const fetchWalletsResultAction: ActionType<
-    typeof fetchWalletsSuccess | typeof fetchWalletsFailure
-  > = yield take([getType(fetchWalletsSuccess), getType(fetchWalletsFailure)]);
+  yield* put(fetchWalletsRequestWithExpBackoff());
+  const fetchWalletsResultAction = yield* take<
+    ActionType<typeof fetchWalletsSuccess | typeof fetchWalletsFailure>
+  >([getType(fetchWalletsSuccess), getType(fetchWalletsFailure)]);
 
   // If the loading work successfully starts the OptInPaymentMethods saga
   if (isActionOf(fetchWalletsSuccess, fetchWalletsResultAction)) {
-    yield put(optInPaymentMethodsShowChoice.success(true));
-    yield put(optInPaymentMethodsStart());
+    yield* put(optInPaymentMethodsShowChoice.success(true));
+    yield* put(optInPaymentMethodsStart());
   } else {
-    yield put(
+    yield* put(
       optInPaymentMethodsShowChoice.failure(fetchWalletsResultAction.payload)
     );
   }
