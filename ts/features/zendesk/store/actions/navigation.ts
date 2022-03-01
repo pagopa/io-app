5dbded12fe9af6c2401b5723eab09f6bb9adfe74
Simone <simone.biffi.91@gmail.com>
2022-03-01 17:21:00 +0100

    manage routing after user press contact support button

--- ts/features/zendesk/store/actions/navigation.ts
+++ ts/features/zendesk/store/actions/navigation.ts
@@ -2,6 +2,7 @@ import { NavigationActions } from "react-navigation";
 import ZENDESK_ROUTES from "../../navigation/routes";
 import { ZendeskAskPermissionsNavigationParams } from "../../screens/ZendeskAskPermissions";
 import { ZendeskChooseSubCategoryNavigationParams } from "../../screens/ZendeskChooseSubCategory";
+import { ZendeskChooseCategoryNavigationParams } from "../../screens/ZendeskChooseCategory";
 
 export const navigateToZendeskAskPermissions = (
   params: ZendeskAskPermissionsNavigationParams
@@ -14,9 +15,12 @@ export const navigateToZendeskPanicMode = () =>
   NavigationActions.navigate({
     routeName: ZENDESK_ROUTES.PANIC_MODE
   });
-export const navigateToZendeskChooseCategory = () =>
+export const navigateToZendeskChooseCategory = (
+  params: ZendeskChooseCategoryNavigationParams
+) =>
   NavigationActions.navigate({
-    routeName: ZENDESK_ROUTES.CHOOSE_CATEGORY
+    routeName: ZENDESK_ROUTES.CHOOSE_CATEGORY,
+    params
   });
 export const navigateToZendeskChooseSubCategory = (
   params: ZendeskChooseSubCategoryNavigationParams
