b37b62dc4a91036bd17012f6f73a3132971947b4
Simone <simone.biffi.91@gmail.com>
2022-03-07 16:38:47 +0100

    chore: [IA-698] Donation config selectors (#3812)
    * temporary modify io_content_specs url
    
    * add donationConfigSelector selector
    
    * refactor
    
    * update test
    
    * update io-services-metadata version
    
    * update naming
    
    * add test
    
    Co-authored-by: fabriziofff <26501317+fabriziofff@users.noreply.github.com>

--- ts/store/reducers/backendStatus.ts
+++ ts/store/reducers/backendStatus.ts
@@ -13,6 +13,7 @@ import { Sections } from "../../../definitions/content/Sections";
 import { SectionStatus } from "../../../definitions/content/SectionStatus";
 import { ToolEnum } from "../../../definitions/content/AssistanceToolConfig";
 import { cgnMerchantsV2Enabled } from "../../config";
+import { UaDonationsBanner } from "../../../definitions/content/UaDonationsBanner";
 import { GlobalState } from "./types";
 
 export type SectionStatusKey = keyof Sections;
@@ -73,6 +74,26 @@ export const assistanceToolConfigSelector = createSelector(
   backendStatusSelector,
   (backendStatus): ToolEnum | undefined =>
     backendStatus.map(bs => bs.config.assistanceTool.tool).toUndefined()
+);
+
+/**
+ * return the remote config about Ukrainian donations enabled/disabled
+ * if there is no data, false is the default value -> (donation disabled)
+ */
+export const isUaDonationsEnabledSelector = createSelector(
+  backendStatusSelector,
+  (backendStatus): boolean =>
+    backendStatus.map(bs => bs.config.uaDonations.enabled).toUndefined() ??
+    false
+);
+
+/**
+ * return the remote config about Ukrainian donations banner if available
+ */
+export const uaDonationsBannerConfigSelector = createSelector(
+  backendStatusSelector,
+  (backendStatus): UaDonationsBanner | undefined =>
+    backendStatus.map(bs => bs.config.uaDonations.banner).toUndefined()
 );
 
 /**
