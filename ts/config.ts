cede28ddf027956f2c4a69aafbb56aacb0151e45
Fabrizio <26501317+fabriziofff@users.noreply.github.com>
2022-03-07 09:59:18 +0100

    rename

--- ts/config.ts
+++ ts/config.ts
@@ -91,7 +91,7 @@ export const bpdOptInPaymentMethodsEnabled =
   Config.BPD_OPT_IN_PAYMENT_METHODS === "YES";
 
 // Ukraine donation
-export const uaDonationEnabled = Config.UA_DONATION_ENABLED === "YES";
+export const uaDonationsEnabled = Config.UA_DONATION_ENABLED === "YES";
 
 // version of ToS
 export const tosVersion: NonNegativeNumber = 2.4 as NonNegativeNumber;
