4ec7b19c5d3110b5237f1ad003e9a2290c04c4e5
Fabrizio <26501317+fabriziofff@users.noreply.github.com>
2022-03-14 15:10:34 +0100

    fix test

--- ts/features/zendesk/components/__tests__/ZendeskSupportComponent.test.tsx
+++ ts/features/zendesk/components/__tests__/ZendeskSupportComponent.test.tsx
@@ -1,28 +1,27 @@
-import { createStore, Store } from "redux";
 import { fireEvent } from "@testing-library/react-native";
-import { appReducer } from "../../../../store/reducers";
-import { applicationChangeState } from "../../../../store/actions/application";
-import { GlobalState } from "../../../../store/reducers/types";
-import { renderScreenFakeNavRedux } from "../../../../utils/testWrapper";
+import { createStore, Store } from "redux";
+import { PublicSession } from "../../../../../definitions/backend/PublicSession";
+import { SpidLevelEnum } from "../../../../../definitions/backend/SpidLevel";
+import { SpidIdp } from "../../../../../definitions/content/SpidIdp";
+import { Zendesk } from "../../../../../definitions/content/Zendesk";
+import MockZendesk from "../../../../__mocks__/io-react-native-zendesk";
 import ROUTES from "../../../../navigation/routes";
-import ZendeskSupportComponent from "../ZendeskSupportComponent";
+import { applicationChangeState } from "../../../../store/actions/application";
 import {
   idpSelected,
   loginSuccess,
   sessionInformationLoadSuccess
 } from "../../../../store/actions/authentication";
+import { appReducer } from "../../../../store/reducers";
+import { GlobalState } from "../../../../store/reducers/types";
 import { SessionToken } from "../../../../types/SessionToken";
-import { PublicSession } from "../../../../../definitions/backend/PublicSession";
-import { SpidLevelEnum } from "../../../../../definitions/backend/SpidLevel";
-import MockZendesk from "../../../../__mocks__/io-react-native-zendesk";
-import { SpidIdp } from "../../../../../definitions/content/SpidIdp";
+import { getNetworkError } from "../../../../utils/errors";
+import { renderScreenFakeNavRedux } from "../../../../utils/testWrapper";
 import {
   getZendeskConfig,
   zendeskRequestTicketNumber
 } from "../../store/actions";
-import * as zendeskNavigation from "../../store/actions/navigation";
-import { Zendesk } from "../../../../../definitions/content/Zendesk";
-import { getNetworkError } from "../../../../utils/errors";
+import ZendeskSupportComponent from "../ZendeskSupportComponent";
 
 const mockPublicSession: PublicSession = {
   bpdToken: "bpdToken",
@@ -37,6 +36,20 @@ const mockZendeskConfig: Zendesk = {
 const mockZendeskPanicModeConfig: Zendesk = {
   panicMode: true
 };
+
+const mockedNavigation = jest.fn();
+
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigation,
+      dispatch: jest.fn()
+    })
+  };
+});
+
 jest.useFakeTimers();
 
 describe("the ZendeskSupportComponent", () => {
@@ -73,20 +86,21 @@ describe("the ZendeskSupportComponent", () => {
   });
 
   describe("when the user press the zendesk open ticket button", () => {
-    const navigateToZendeskAskPermissionsSpy = jest.spyOn(
-      zendeskNavigation,
-      "navigateToZendeskAskPermissions"
-    );
-    const navigateToZendeskPanicModeSpy = jest.spyOn(
-      zendeskNavigation,
-      "navigateToZendeskPanicMode"
-    );
-    const navigateToZendeskChooseCategorySpy = jest.spyOn(
-      zendeskNavigation,
-      "navigateToZendeskChooseCategory"
-    );
-    afterEach(() => {
-      jest.clearAllMocks();
+    // const navigateToZendeskAskPermissionsSpy = jest.spyOn(
+    //   zendeskNavigation,
+    //   "navigateToZendeskAskPermissions"
+    // );
+    // const navigateToZendeskPanicModeSpy = jest.spyOn(
+    //   zendeskNavigation,
+    //   "navigateToZendeskPanicMode"
+    // );
+    // const navigateToZendeskChooseCategorySpy = jest.spyOn(
+    //   zendeskNavigation,
+    //   "navigateToZendeskChooseCategory"
+    // );
+    beforeEach(() => {
+      // Alternatively, set "clearMocks" in your Jest config to "true"
+      mockedNavigation.mockClear();
     });
     describe("if panic mode is false", () => {
       it("if the assistanceForPayment is true should navigate to the ZendeskAskPermissions screen", () => {
@@ -94,30 +108,38 @@ describe("the ZendeskSupportComponent", () => {
         const component = renderComponent(store, true);
         const zendeskButton = component.getByTestId("contactSupportButton");
         fireEvent(zendeskButton, "onPress");
-        expect(navigateToZendeskAskPermissionsSpy).toBeCalled();
-        expect(navigateToZendeskPanicModeSpy).not.toBeCalled();
-        expect(navigateToZendeskChooseCategorySpy).not.toBeCalled();
+        expect(mockedNavigation).toHaveBeenCalledTimes(1);
+        expect(mockedNavigation).toHaveBeenCalledWith("ZENDESK_MAIN", {
+          params: { assistanceForPayment: undefined },
+          screen: "ZENDESK_ASK_PERMISSIONS"
+        });
       });
       it("if the zendeskRemoteConfig is not remoteReady should navigate to the ZendeskAskPermissions screen", () => {
         const store = createStore(appReducer, globalState as any);
         const component = renderComponent(store, false);
         const zendeskButton = component.getByTestId("contactSupportButton");
         fireEvent(zendeskButton, "onPress");
-        expect(navigateToZendeskAskPermissionsSpy).toBeCalled();
-        expect(navigateToZendeskPanicModeSpy).not.toBeCalled();
-        expect(navigateToZendeskChooseCategorySpy).not.toBeCalled();
+        expect(mockedNavigation).toHaveBeenCalledTimes(1);
+        expect(mockedNavigation).toHaveBeenCalledWith("ZENDESK_MAIN", {
+          params: { assistanceForPayment: undefined },
+          screen: "ZENDESK_ASK_PERMISSIONS"
+        });
         store.dispatch(getZendeskConfig.request());
         fireEvent(zendeskButton, "onPress");
-        expect(navigateToZendeskAskPermissionsSpy).toBeCalledTimes(2);
-        expect(navigateToZendeskPanicModeSpy).not.toBeCalled();
-        expect(navigateToZendeskChooseCategorySpy).not.toBeCalled();
+        expect(mockedNavigation).toHaveBeenCalledTimes(2);
+        expect(mockedNavigation).toHaveBeenCalledWith("ZENDESK_MAIN", {
+          params: { assistanceForPayment: undefined },
+          screen: "ZENDESK_ASK_PERMISSIONS"
+        });
         store.dispatch(
           getZendeskConfig.failure(getNetworkError("mockedError"))
         );
         fireEvent(zendeskButton, "onPress");
-        expect(navigateToZendeskAskPermissionsSpy).toBeCalledTimes(3);
-        expect(navigateToZendeskPanicModeSpy).not.toBeCalled();
-        expect(navigateToZendeskChooseCategorySpy).not.toBeCalled();
+        expect(mockedNavigation).toHaveBeenCalledTimes(3);
+        expect(mockedNavigation).toHaveBeenCalledWith("ZENDESK_MAIN", {
+          params: { assistanceForPayment: undefined },
+          screen: "ZENDESK_ASK_PERMISSIONS"
+        });
       });
       it("if the assistanceForPayment is false and the zendeskRemoteConfig is remoteReady should navigate to the navigateToZendeskChooseCategory screen", () => {
         const store = createStore(appReducer, globalState as any);
@@ -125,29 +147,24 @@ describe("the ZendeskSupportComponent", () => {
         store.dispatch(getZendeskConfig.success(mockZendeskConfig));
         const zendeskButton = component.getByTestId("contactSupportButton");
         fireEvent(zendeskButton, "onPress");
-        expect(navigateToZendeskChooseCategorySpy).toBeCalled();
-        expect(navigateToZendeskAskPermissionsSpy).not.toBeCalled();
-        expect(navigateToZendeskPanicModeSpy).not.toBeCalled();
+        expect(mockedNavigation).toHaveBeenCalledTimes(1);
+        expect(mockedNavigation).toHaveBeenCalledWith("ZENDESK_MAIN", {
+          params: { assistanceForPayment: undefined },
+          screen: "ZENDESK_CHOOSE_CATEGORY"
+        });
       });
     });
 
     it("if panic mode is true, should navigate to the ZendeskAskPermissions screen", () => {
-      const navigateToZendeskAskPermissionsSpy = jest.spyOn(
-        zendeskNavigation,
-        "navigateToZendeskAskPermissions"
-      );
-      const navigateToZendeskPanicModeSpy = jest.spyOn(
-        zendeskNavigation,
-        "navigateToZendeskPanicMode"
-      );
       const store = createStore(appReducer, globalState as any);
       const component = renderComponent(store, false);
       store.dispatch(getZendeskConfig.success(mockZendeskPanicModeConfig));
       const zendeskButton = component.getByTestId("contactSupportButton");
       fireEvent(zendeskButton, "onPress");
-      expect(navigateToZendeskPanicModeSpy).toBeCalled();
-      expect(navigateToZendeskAskPermissionsSpy).not.toBeCalled();
-      expect(navigateToZendeskChooseCategorySpy).not.toBeCalled();
+      expect(mockedNavigation).toHaveBeenCalledTimes(1);
+      expect(mockedNavigation).toHaveBeenCalledWith("ZENDESK_MAIN", {
+        screen: "ZENDESK_PANIC_MODE"
+      });
     });
   });
   describe("when the user press the zendesk show tickets button", () => {
