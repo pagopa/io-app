8bc71828cd0fb67e3ecb660e23cc12e45ca40f8f
Emilio Pavia <emilio.pavia@gmail.com>
2022-03-14 11:21:15 +0100

    Change module name

--- android/app/src/main/java/it/pagopa/io/app/MainActivity.java
+++ android/app/src/main/java/it/pagopa/io/app/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ItaliaApp";
+        return "IO";
     }
 
     // see
