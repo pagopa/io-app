7e071e4bc194a25cd55f886069faa70e7071b0e6
Simone <simone.biffi.91@gmail.com>
2022-03-15 17:33:59 +0100

    refactor handleGetTotalNewResponses

--- ts/features/zendesk/saga/networking/handleGetTotalNewResponses.ts
+++ ts/features/zendesk/saga/networking/handleGetTotalNewResponses.ts
@@ -1,18 +1,20 @@
-import { call, put, race, select, take } from "redux-saga/effects";
+import { call, fork, put, race, select, take } from "typed-redux-saga/macro";
 import { delay, SagaReturnType } from "@redux-saga/core/effects";
+import { isActionOf } from "typesafe-actions";
 import { getError } from "../../../../utils/errors";
 
 import {
   AnonymousIdentity,
   getIdentityByToken,
   getTotalNewResponses,
-  getTotalNewResponsesRefreshRate,
+  unreadTicketsCountRefreshRate,
   initSupportAssistance,
   JwtIdentity,
   setUserIdentity,
   ZendeskAppConfig,
   zendeskDefaultAnonymousConfig,
-  zendeskDefaultJwtConfig
+  zendeskDefaultJwtConfig,
+  unreadTicketsCountRefreshRateWhileSupportIsOpen
 } from "../../../../utils/supportAssistance";
 import { zendeskTokenSelector } from "../../../../store/reducers/authentication";
 import {
@@ -23,53 +25,113 @@ import {
 } from "../../../../store/actions/authentication";
 import {
   zendeskGetTotalNewResponses,
-  zendeskRequestTicketNumber
+  zendeskRequestTicketNumber,
+  zendeskSupportOpened
 } from "../../store/actions";
 import { isTestEnv } from "../../../../utils/environment";
+import {
+  versionInfoLoadFailure,
+  versionInfoLoadSuccess
+} from "../../../../common/versionInfo/store/actions/versionInfo";
+import { backendStatusLoadSuccess } from "../../../../store/actions/backendStatus";
+import { Action } from "../../../../store/actions/types";
 
-function* totalNewResponsesFunction() {
-  const zendeskToken: string | undefined = yield select(zendeskTokenSelector);
+function* setupZendesk() {
+  const zendeskToken: string | undefined = yield* select(zendeskTokenSelector);
 
   const zendeskConfig: ZendeskAppConfig = zendeskToken
     ? { ...zendeskDefaultJwtConfig, token: zendeskToken }
     : zendeskDefaultAnonymousConfig;
 
-  yield call(initSupportAssistance, zendeskConfig);
+  yield* call(initSupportAssistance, zendeskConfig);
   const zendeskIdentity: JwtIdentity | AnonymousIdentity =
     getIdentityByToken(zendeskToken);
 
-  yield call(setUserIdentity, zendeskIdentity);
+  yield* call(setUserIdentity, zendeskIdentity);
+}
 
+function* getTicketsCount() {
+  yield* call(setupZendesk);
   // Try to get the total messages of the user
-  yield put(zendeskRequestTicketNumber.request());
+  yield* put(zendeskRequestTicketNumber.request());
+}
 
+function* getUnreadTicketsCount() {
+  yield* call(setupZendesk);
   // Try to get the new messages of the user
   try {
-    const response: SagaReturnType<typeof getTotalNewResponses> = yield call(
+    const response: SagaReturnType<typeof getTotalNewResponses> = yield* call(
       getTotalNewResponses
     );
-    yield put(zendeskGetTotalNewResponses.success(response));
+    yield* put(zendeskGetTotalNewResponses.success(response));
   } catch (e) {
-    yield put(zendeskGetTotalNewResponses.failure(getError(e)));
+    yield* put(zendeskGetTotalNewResponses.failure(getError(e)));
   }
-  yield race({
-    wait: delay(getTotalNewResponsesRefreshRate),
-    signals: take([
-      sessionInvalid,
-      sessionExpired,
-      logoutRequest,
-      sessionInformationLoadSuccess
-    ])
-  });
+}
+
+/**
+ * when the zendesk is opened it starts to check the number of unread tickets cyclically
+ * it stops when the user makes some changes inside the app (ex. SCREEN_CHANGE)
+ */
+function* refreshUnreadTicketsCountWhileSupportIsOpen() {
+  // a predicate that return true if the given action is different from all
+  // these ones defined in the array
+  const stoppersPredicate = (action: Action) =>
+    [
+      zendeskGetTotalNewResponses.request,
+      zendeskGetTotalNewResponses.success,
+      zendeskGetTotalNewResponses.failure,
+      zendeskRequestTicketNumber.request,
+      zendeskRequestTicketNumber.failure,
+      zendeskRequestTicketNumber.success,
+      backendStatusLoadSuccess,
+      versionInfoLoadFailure,
+      versionInfoLoadSuccess
+    ].every(skipAction => !isActionOf(skipAction, action));
+  while (true) {
+    yield* take(zendeskSupportOpened);
+    while (true) {
+      yield* call(getUnreadTicketsCount);
+      const { stoppers } = yield* race({
+        wait: delay(unreadTicketsCountRefreshRateWhileSupportIsOpen),
+        stoppers: take(stoppersPredicate)
+      });
+
+      if (stoppers) {
+        break;
+      }
+    }
+  }
+}
+
+/**
+ * check cyclically the number of unread tickets
+ * the check is done when
+ * - a waiting timer elapses
+ * - when the user session changes (login/logout/session)
+ */
+function* refreshUnreadTicketsCount() {
+  while (true) {
+    yield* call(getUnreadTicketsCount);
+    yield* call(getTicketsCount);
+    yield* race({
+      wait: delay(unreadTicketsCountRefreshRate),
+      signals: take([
+        sessionInvalid,
+        sessionExpired,
+        logoutRequest,
+        sessionInformationLoadSuccess
+      ])
+    });
+  }
 }
 
 // retrieve the number of ticket opened by the user from the Zendesk SDK
 export function* handleGetTotalNewResponses() {
-  while (true) {
-    yield call(totalNewResponsesFunction);
-  }
+  yield* fork(refreshUnreadTicketsCount);
+  yield* fork(refreshUnreadTicketsCountWhileSupportIsOpen);
 }
 
 export const testTotalNewResponsesFunction = isTestEnv
-  ? totalNewResponsesFunction
+  ? refreshUnreadTicketsCount
   : undefined;
