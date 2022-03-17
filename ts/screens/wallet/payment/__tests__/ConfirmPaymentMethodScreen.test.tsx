6d3fa2b0fc7a90240294d27b661cf91e295e5330
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-03-17 14:37:10 +0100

    Remove the tests relative to the "Why a fee" modal

--- ts/screens/wallet/payment/__tests__/ConfirmPaymentMethodScreen.test.tsx
+++ ts/screens/wallet/payment/__tests__/ConfirmPaymentMethodScreen.test.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { createStore, Store } from "redux";
-import { fireEvent } from "@testing-library/react-native";
-
 import ConfirmPaymentMethodScreen, {
   ConfirmPaymentMethodScreenNavigationParams
 } from "../ConfirmPaymentMethodScreen";
@@ -42,44 +40,6 @@ describe("Integration Tests With Actual Store and Simplified Navigation", () => 
 
   // Store with the true appReducer
   const myStore: Store<GlobalState> = createStore(appReducer, initState as any);
-
-  it("Should fire showModal by pressing the Why-A-Fee Touchable", async () => {
-    // Spy on showModa
-    const mySpy = jest.fn();
-
-    const ToBeTested: React.FunctionComponent<
-      React.ComponentProps<typeof ConfirmPaymentMethodScreen>
-    > = (props: React.ComponentProps<typeof ConfirmPaymentMethodScreen>) => (
-      <LightModalContext.Provider
-        value={{
-          component: null,
-          showModal: mySpy,
-          showAnimatedModal: jest.fn(),
-          showModalFadeInAnimation: jest.fn(),
-          hideModal: jest.fn(),
-          onHiddenModal: jest.fn(),
-          setOnHiddenModal: jest.fn()
-        }}
-      >
-        <ConfirmPaymentMethodScreen {...props} />
-      </LightModalContext.Provider>
-    );
-
-    // Render with simplified but true navigation. Just two screen: the
-    // screen under test and a fake screen to navigate without mocking
-    const MyObj = renderScreenFakeNavRedux<
-      GlobalState,
-      ConfirmPaymentMethodScreenNavigationParams
-    >(ToBeTested, ROUTES.PAYMENT_CONFIRM_PAYMENT_METHOD, params, myStore);
-
-    // Link Why A Fee? Must be present
-    const whyAFeeTouch = MyObj.getByTestId(/why-a-fee/i);
-    expect(whyAFeeTouch).toBeDefined();
-
-    // showModal must be called when pressing touchable why-a-fee
-    fireEvent.press(whyAFeeTouch);
-    expect(mySpy).toHaveBeenCalledTimes(1);
-  });
 
   it("should NOT display the transactions costs contextual help since they are zero", () => {
     const paramsWithoutFees: ConfirmPaymentMethodScreenNavigationParams = {
