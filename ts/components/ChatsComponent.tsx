61eeea5bbc8275f87654f24bd5f4155907b05a9b
Simone <simone.biffi.91@gmail.com>
2022-03-24 10:36:19 +0100

    remove InstabugChatsComponent

--- ts/components/ChatsComponent.tsx
+++ ts/components/ChatsComponent.tsx
@@ -4,7 +4,6 @@ import { assistanceToolConfigSelector } from "../store/reducers/backendStatus";
 import { useIOSelector } from "../store/hooks";
 import { ToolEnum } from "../../definitions/content/AssistanceToolConfig";
 import ZendeskChatComponent from "../features/zendesk/components/ZendeskChatComponent";
-import InstabugChatsComponent from "./InstabugChatsComponent";
 
 /**
  * This component renders the right Chat tool based on the remote configured assistance tool
@@ -15,10 +14,9 @@ const ChatsComponent = () => {
   const chosenTool = assistanceToolRemoteConfig(assistanceToolConfig);
 
   switch (chosenTool) {
-    case ToolEnum.instabug:
-      return <InstabugChatsComponent />;
     case ToolEnum.zendesk:
       return <ZendeskChatComponent />;
+    case ToolEnum.instabug:
     case ToolEnum.none:
     case ToolEnum.web:
       return null;
