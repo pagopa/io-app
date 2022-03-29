f31cddbd6df9cc967ddf7e23839e43ebebda543a
Simone <simone.biffi.91@gmail.com>
2022-03-29 18:01:53 +0200

    add reset custom field in BaseScreenComponent

--- ts/components/screens/BaseScreenComponent/index.tsx
+++ ts/components/screens/BaseScreenComponent/index.tsx
@@ -27,7 +27,10 @@ import { AccessibilityEvents, BaseHeader } from "../BaseHeader";
 import { zendeskSupportStart } from "../../../features/zendesk/store/actions";
 import { useIOSelector } from "../../../store/hooks";
 import { assistanceToolConfigSelector } from "../../../store/reducers/backendStatus";
-import { assistanceToolRemoteConfig } from "../../../utils/supportAssistance";
+import {
+  assistanceToolRemoteConfig,
+  resetCustomFields
+} from "../../../utils/supportAssistance";
 import { ToolEnum } from "../../../../definitions/content/AssistanceToolConfig";
 import { canShowHelpSelector } from "../../../store/reducers/assistanceTools";
 import {
@@ -189,6 +192,7 @@ const BaseScreenComponentFC = React.forwardRef<ReactNode, Props>(
           // TODO: remove local feature flag
           // The navigation param assistanceForPayment is fixed to false because in this entry point we don't know the category yet.
           return () => {
+            resetCustomFields();
             dispatch(
               zendeskSupportStart({
                 faqCategories,
