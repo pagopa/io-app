7a1b987808d535c11fe6f8ccc2494ae4576aef4a
Simone <simone.biffi.91@gmail.com>
2022-04-12 10:16:16 +0200

    reorder input

--- ts/navigation/params/AppParamsList.ts
+++ ts/navigation/params/AppParamsList.ts
@@ -14,6 +14,8 @@ import UADONATION_ROUTES from "../../features/uaDonations/navigation/routes";
 import { ZendeskParamsList } from "../../features/zendesk/navigation/params";
 import ZENDESK_ROUTES from "../../features/zendesk/navigation/routes";
 import ROUTES from "../routes";
+import { CDC_ROUTES } from "../../features/bonus/cdc/navigation/routes";
+import { CdcBonusRequestParamsList } from "../../features/bonus/cdc/navigation/params";
 import { AuthenticationParamsList } from "./AuthenticationParamsList";
 import { MainTabParamsList } from "./MainTabParamsList";
 import { MessagesParamsList } from "./MessagesParamsList";
@@ -21,8 +23,6 @@ import { OnboardingParamsList } from "./OnboardingParamsList";
 import { ProfileParamsList } from "./ProfileParamsList";
 import { ServicesParamsList } from "./ServicesParamsList";
 import { WalletParamsList } from "./WalletParamsList";
-import { CDC_ROUTES } from "../../features/bonus/cdc/navigation/routes";
-import { CdcBonusRequestParamsList } from "../../features/bonus/cdc/navigation/params";
 
 export type AppParamsList = {
   [ROUTES.INGRESS]: undefined;
