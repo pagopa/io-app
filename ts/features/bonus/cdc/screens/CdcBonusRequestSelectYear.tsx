dde40ab5f9c38d4ac11519052a8a684f3cf9c637
Simone <simone.biffi.91@gmail.com>
2022-04-12 10:16:48 +0200

    add cancel button

--- ts/features/bonus/cdc/screens/CdcBonusRequestSelectYear.tsx
+++ ts/features/bonus/cdc/screens/CdcBonusRequestSelectYear.tsx
@@ -4,19 +4,47 @@ import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import { H1 } from "../../../../components/core/typography/H1";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
+import { useNavigation } from "@react-navigation/native";
+import { IOStackNavigationProp } from "../../../../navigation/params/AppParamsList";
+import { CdcBonusRequestParamsList } from "../navigation/params";
+import { CDC_ROUTES } from "../navigation/routes";
+import ButtonDefaultOpacity from "../../../../components/ButtonDefaultOpacity";
+import { RTron } from "../../../../boot/configureStoreAndPersistor";
+import ROUTES from "../../../../navigation/routes";
 
-const CdcBonusRequestSelectYear = () => (
-  <BaseScreenComponent
-    goBack={false}
-    contextualHelp={emptyContextualHelp}
-    headerTitle={"Cdc bonus"}
-  >
-    <SafeAreaView style={IOStyles.flex}>
-      <ScrollView style={[IOStyles.horizontalContentPadding]}>
-        <H1>{"CdcBonusRequestSelectYear"}</H1>
-      </ScrollView>
-    </SafeAreaView>
-  </BaseScreenComponent>
-);
+const CdcBonusRequestSelectYear = () => {
+  const navigation =
+    useNavigation<
+      IOStackNavigationProp<CdcBonusRequestParamsList, "CDC_SELECT_BONUS_YEAR">
+    >();
+  return (
+    <BaseScreenComponent
+      goBack={true}
+      contextualHelp={emptyContextualHelp}
+      headerTitle={"Cdc bonus"}
+    >
+      <SafeAreaView style={IOStyles.flex}>
+        <ScrollView style={[IOStyles.horizontalContentPadding]}>
+          <H1>{"CdcBonusRequestSelectYear"}</H1>
+          <ButtonDefaultOpacity
+            onPress={() => {
+              // navigation.popToTop();
+              // navigation.goBack();
+              navigation.getParent()?.goBack();
+              navigation.navigate(ROUTES.MAIN, {
+                screen: ROUTES.PROFILE_MAIN
+              });
+              navigation.navigate(ROUTES.PROFILE_NAVIGATOR, {
+                screen: ROUTES.PROFILE_DATA
+              });
+            }}
+          >
+            <H1>{"Annulla"}</H1>
+          </ButtonDefaultOpacity>
+        </ScrollView>
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
 
 export default CdcBonusRequestSelectYear;
