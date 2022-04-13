b5a7cf15588bca7b8319ba73f0db3a7797ba44ea
Matteo Boschi <mbmaken@gmail.com>
2022-04-13 10:51:23 +0200

    update mock models

--- ts/features/wallet/onboarding/paypal/screen/__tests__/PayPalPspUpdateScreen.test.tsx
+++ ts/features/wallet/onboarding/paypal/screen/__tests__/PayPalPspUpdateScreen.test.tsx
@@ -12,6 +12,7 @@ import { pspForPaymentV2 } from "../../../../../../store/actions/wallet/payment"
 
 const pspList: ReadonlyArray<PspData> = [
   {
+    id: 1,
     codiceAbi: "0001",
     defaultPsp: true,
     fee: 100,
@@ -21,6 +22,7 @@ const pspList: ReadonlyArray<PspData> = [
     ragioneSociale: "PayTipper"
   },
   {
+    id: 2,
     codiceAbi: "0002",
     defaultPsp: true,
     fee: 120,
