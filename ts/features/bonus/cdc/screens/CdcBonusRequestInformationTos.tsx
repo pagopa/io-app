138c0d40612029176c6b24c9b3dfe8458f943f61
Simone <simone.biffi.91@gmail.com>
2022-04-13 18:02:56 +0200

    add buttons in CdcBonusRequestInformationTos

--- ts/features/bonus/cdc/screens/CdcBonusRequestInformationTos.tsx
+++ ts/features/bonus/cdc/screens/CdcBonusRequestInformationTos.tsx
@@ -4,11 +4,12 @@ import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import { H1 } from "../../../../components/core/typography/H1";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
-import ButtonDefaultOpacity from "../../../../components/ButtonDefaultOpacity";
 import { useNavigation } from "@react-navigation/native";
 import { IOStackNavigationProp } from "../../../../navigation/params/AppParamsList";
 import { CdcBonusRequestParamsList } from "../navigation/params";
 import { CDC_ROUTES } from "../navigation/routes";
+import I18n from "../../../../i18n";
+import FooterWithButtons from "../../../../components/ui/FooterWithButtons";
 
 const CdcBonusRequestInformationTos = () => {
   const navigation =
@@ -20,20 +21,31 @@ const CdcBonusRequestInformationTos = () => {
     <BaseScreenComponent
       goBack={true}
       contextualHelp={emptyContextualHelp}
-      headerTitle={"Cdc bonus"}
+      headerTitle={I18n.t("bonus.cdc.title")}
     >
       <SafeAreaView style={IOStyles.flex}>
         <ScrollView style={[IOStyles.horizontalContentPadding]}>
           <H1>{"CdcBonusRequestInformationTos"}</H1>
-
-          <ButtonDefaultOpacity
-            onPress={() => {
-              navigation.navigate(CDC_ROUTES.SELECT_BONUS_YEAR);
-            }}
-          >
-            <H1>{"Continua"}</H1>
-          </ButtonDefaultOpacity>
         </ScrollView>
+        <FooterWithButtons
+          type={"TwoButtonsInlineThird"}
+          leftButton={{
+            block: true,
+            bordered: true,
+            onPress: () => {
+              navigation.getParent()?.goBack();
+            },
+            title: I18n.t("global.buttons.cancel")
+          }}
+          rightButton={{
+            block: true,
+            primary: true,
+            onPress: () => {
+              navigation.navigate(CDC_ROUTES.SELECT_BONUS_YEAR);
+            },
+            title: I18n.t("global.buttons.continue")
+          }}
+        />
       </SafeAreaView>
     </BaseScreenComponent>
   );
