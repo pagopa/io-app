5b6519e7d478b64596f002b11dee35275fa7d6e9
CrisTofani <cri.tofani@gmail.com>
2022-04-14 15:27:29 +0200

    This PR implements some UI Changes on CGN Merchant detail screen

--- ts/features/bonus/cgn/components/merchants/CgnMerchantsDiscountItem.tsx
+++ ts/features/bonus/cgn/components/merchants/CgnMerchantsDiscountItem.tsx
@@ -1,23 +1,18 @@
 import * as React from "react";
 import { View } from "native-base";
 import { StyleSheet } from "react-native";
-import { lookup } from "fp-ts/lib/Array";
 import { connect } from "react-redux";
-import { IOColors } from "../../../../../components/core/variables/IOColors";
-import { H5 } from "../../../../../components/core/typography/H5";
-import { H4 } from "../../../../../components/core/typography/H4";
-import { ShadowBox } from "../../../bpd/screens/details/components/summary/base/ShadowBox";
 import { IOStyles } from "../../../../../components/core/variables/IOStyles";
-import TouchableDefaultOpacity from "../../../../../components/TouchableDefaultOpacity";
 import { Discount } from "../../../../../../definitions/cgn/merchants/Discount";
-import I18n from "../../../../../i18n";
 import { navigateToCgnMerchantLandingWebview } from "../../navigation/actions";
 import { Dispatch } from "../../../../../store/actions/types";
 import { GlobalState } from "../../../../../store/reducers/types";
 import { DiscountCodeType } from "../../../../../../definitions/cgn/merchants/DiscountCodeType";
 import { useCgnDiscountDetailBottomSheet } from "../../hooks/useCgnDiscountDetailBottomSheet";
-import CgnDiscountValueBox from "./CgnDiscountValueBox";
-import { renderCategoryElement } from "./CgnMerchantListItem";
+import { Label } from "../../../../../components/core/typography/Label";
+import { IOColors } from "../../../../../components/core/variables/IOColors";
+import IconFont from "../../../../../components/ui/IconFont";
+import TouchableDefaultOpacity from "../../../../../components/TouchableDefaultOpacity";
 
 type Props = {
   discount: Discount;
@@ -27,13 +22,15 @@ type Props = {
 
 const styles = StyleSheet.create({
   container: { justifyContent: "space-between", alignItems: "center" },
-  categories: { display: "flex", flexDirection: "row", flexWrap: "wrap" },
-  row: {
-    flexDirection: "row"
-  },
-  verticalPadding: {
+  listItem: {
+    paddingHorizontal: 16,
     flex: 1,
-    paddingVertical: 7
+    paddingVertical: 20,
+    marginBottom: 16,
+    borderRadius: 8,
+    borderColor: "#CCD4DC",
+    borderStyle: "solid",
+    borderWidth: 1
   }
 });
 
@@ -48,69 +45,15 @@ const CgnMerchantDiscountItem: React.FunctionComponent<Props> = ({
     navigateToLandingWebview
   );
   return (
-    <TouchableDefaultOpacity style={styles.verticalPadding} onPress={present}>
-      <ShadowBox>
-        <View style={[styles.row, styles.container]}>
-          <View style={IOStyles.flex}>
-            <View style={IOStyles.flex}>
-              <H4 weight={"Bold"} color={"blue"}>
-                {discount.name}
-              </H4>
-            </View>
-            <View spacer xsmall />
-            <View style={[styles.categories, IOStyles.flex]}>
-              {discount.productCategories.length > 2
-                ? lookup(0, [...discount.productCategories]).fold(
-                    undefined,
-                    c => (
-                      <>
-                        {renderCategoryElement(c)}
-                        <View
-                          hspacer
-                          small
-                          style={{
-                            borderRightColor: IOColors.bluegrey,
-                            borderRightWidth: 1
-                          }}
-                        />
-                        <View hspacer small />
-                        <H5 color={"bluegrey"} weight={"SemiBold"}>
-                          {I18n.t(
-                            "bonus.cgn.merchantDetail.categories.counting",
-                            {
-                              count: discount.productCategories.length - 1
-                            }
-                          ).toLocaleUpperCase()}
-                        </H5>
-                      </>
-                    )
-                  )
-                : discount.productCategories.map((category, i) => (
-                    <View key={i} style={IOStyles.row}>
-                      {renderCategoryElement(category)}
-                      {discount.productCategories.indexOf(category) !==
-                        discount.productCategories.length - 1 && (
-                        <>
-                          <View
-                            hspacer
-                            small
-                            style={{
-                              borderRightColor: IOColors.bluegrey,
-                              borderRightWidth: 1
-                            }}
-                          />
-                          <View hspacer small />
-                        </>
-                      )}
-                    </View>
-                  ))}
-            </View>
-          </View>
-          {discount.discount && (
-            <CgnDiscountValueBox value={discount.discount} small={true} />
-          )}
+    <TouchableDefaultOpacity style={[styles.listItem]} onPress={present}>
+      <View style={[IOStyles.row, styles.container]}>
+        <View style={IOStyles.flex}>
+          <Label weight={"SemiBold"} color={"bluegreyDark"}>
+            {discount.name}
+          </Label>
         </View>
-      </ShadowBox>
+        <IconFont name={"io-right"} color={IOColors.blue} size={24} />
+      </View>
     </TouchableDefaultOpacity>
   );
 };
