17c94582b72a75b64b0999ce5466327e562a5ffa
Emilio Pavia <emilio.pavia@gmail.com>
2022-04-20 18:14:49 +0200

    Add downloads to the MVL store

---
+++ ts/features/mvl/store/reducers/downloads.ts
@@ -0,0 +1,17 @@
+import * as pot from "italia-ts-commons/lib/pot";
+import {IndexedById} from "../../../../store/helpers/indexer";
+import {Action} from "../../../../store/actions/types";
+
+export type MvlDownloads = IndexedById<pot.Pot<string, Error>>;
+
+export const initialState: MvlDownloads = {};
+
+/**
+ * Store download info for MVL attachments
+ * @param state
+ * @param _
+ */
+export const mvlDownloadsReducer = (
+  state: MvlDownloads = initialState,
+  _: Action
+): MvlDownloads => state;
