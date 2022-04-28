c258017992ba55bb0cff85a038f454ca20246014
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-04-28 11:34:11 +0200

    Improve `BarcodeCamera` component to handle a scan callback

--- ts/components/BarcodeCamera.tsx
+++ ts/components/BarcodeCamera.tsx
@@ -1,10 +1,52 @@
 import "react-native-reanimated";
-import React, { useMemo } from "react";
+import React, { useEffect } from "react";
 import { Camera, useCameraDevices } from "react-native-vision-camera";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { useScanBarcodes, BarcodeFormat } from "vision-camera-code-scanner";
 
-export const BarcodeCamera = () => {
+/**
+ * Type describing the supported barcodes in IO.
+ */
+type IOBarcodeFormat = "DATA_MATRIX" | "QRCODE";
+
+/**
+ * The message sent through the `onBarcodeScanned`
+ * callback.
+ */
+type ScannedBarcode = {
+  format: IOBarcodeFormat;
+  value: string;
+};
+
+/**
+ * Map the barcode scanned through the underneath
+ * library to one of the supported ones in IO, or `null`.
+ */
+function barcodeFormatToIOFormat(
+  format: BarcodeFormat
+): IOBarcodeFormat | null {
+  switch (format) {
+    case BarcodeFormat.QR_CODE:
+      return "QRCODE";
+
+    case BarcodeFormat.DATA_MATRIX:
+      return "DATA_MATRIX";
+
+    default:
+      return null;
+  }
+}
+
+type Props = {
+  onBarcodeScanned: (barcode: ScannedBarcode) => void;
+};
+
+/**
+ * A Camera view which can scan different types of
+ * barcodes (QRCodes, Data Matrix, ...).
+ */
+export const BarcodeCamera = (props: Props) => {
+  const { onBarcodeScanned } = props;
   const devices = useCameraDevices();
   const device = devices.back;
 
@@ -15,29 +57,40 @@ export const BarcodeCamera = () => {
     }
   );
 
-  const serializedBarcodes = useMemo(
-    () =>
-      barcodes[0] ? `${barcodes[0].content.data}`.slice(0, 20) + "..." : "",
-    [barcodes]
-  );
+  useEffect(() => {
+    // This is going to take only the first scanned
+    // barcode. This could be improved or changed in relation
+    // to the business decisions.
+    const firstBarcode = barcodes[0];
+
+    if (!firstBarcode) {
+      return;
+    }
+
+    const barcodeValue = firstBarcode.content.data.toString();
+    const barcodeFormat = barcodeFormatToIOFormat(firstBarcode.format);
+
+    if (barcodeFormat === null) {
+      return;
+    }
+
+    onBarcodeScanned({
+      format: barcodeFormat,
+      value: barcodeValue
+    });
+  }, [barcodes, onBarcodeScanned]);
 
   return (
-    <>
-      <View style={{ width: "100%", height: 400, flexDirection: "column" }}>
-        {device && (
-          <Camera
-            style={{ width: "100%", height: "100%" }}
-            device={device}
-            isActive={true}
-            frameProcessor={frameProcessor}
-            frameProcessorFps={5}
-          />
-        )}
-      </View>
-
-      <View style={{ marginTop: 15 }}>
-        <Text>Recognized code: {serializedBarcodes || "none"}</Text>
-      </View>
-    </>
+    <View style={{ width: "100%", height: 400, flexDirection: "column" }}>
+      {device && (
+        <Camera
+          style={{ width: "100%", height: "100%" }}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+          frameProcessorFps={5}
+        />
+      )}
+    </View>
   );
 };
