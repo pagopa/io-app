a018f56945e9f11646215fd0ae6060252b4f07fd
Simone <simone.biffi.91@gmail.com>
2022-05-16 11:58:41 +0200

    add request response type

--- ts/features/bonus/cdc/types/CdcBonusRequest.ts
+++ ts/features/bonus/cdc/types/CdcBonusRequest.ts
@@ -1,6 +1,7 @@
 import { StatoBeneficiarioEnum } from "../../../../../definitions/cdc/StatoBeneficiario";
 import { Anno } from "../../../../../definitions/cdc/Anno";
 import { EsitoRichiesta } from "../../../../../definitions/cdc/EsitoRichiesta";
+import { RichiestaCartaErrataMotivoEnum } from "../../../../../definitions/cdc/RichiestaCartaErrataMotivo";
 
 export type CdcBonusRequest = {
   year: Anno;
@@ -31,3 +32,46 @@ export type CdcSelectedBonus = {
 };
 
 export type CdcSelectedBonusList = ReadonlyArray<CdcSelectedBonus>;
+
+/**
+ * All the years for which the bonus is requested returns ok in the body (200)
+ */
+type CdcBonusRequestResponseCompleteSuccess = {
+  kind: "success";
+  value: CdcBonusEnrollmentOutcomeList;
+};
+
+/**
+ * Not all the years for which the bonus is requested returns ok in the body (200)
+ */
+type CdcBonusRequestResponsePartialSuccess = {
+  kind: "partialSuccess";
+  value: CdcBonusEnrollmentOutcomeList;
+};
+
+/**
+ * Validations not passed (400)
+ */
+type CdcBonusRequestResponseWrongFormat = {
+  kind: "wrongFormat";
+  reason?: RichiestaCartaErrataMotivoEnum;
+};
+
+/**
+ * Union of all the success case
+ */
+export type CdcBonusRequestResponseSuccess =
+  | CdcBonusRequestResponseCompleteSuccess
+  | CdcBonusRequestResponsePartialSuccess;
+
+/**
+ * Union of all the error case
+ */
+export type CdcBonusRequestResponseFailure = CdcBonusRequestResponseWrongFormat;
+
+/**
+ * This type represents all the possible remote responses
+ */
+export type CdcBonusRequestResponse =
+  | CdcBonusRequestResponseSuccess
+  | CdcBonusRequestResponseWrongFormat;
