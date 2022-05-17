813d10531cd5c5326127340e1a8d59c52fd49aa2
Emilio Pavia <emilio.pavia@gmail.com>
2022-05-17 14:35:00 +0200

    Add the PotTransform in mvlPersistConfig

--- ts/features/mvl/index.ts
+++ ts/features/mvl/index.ts
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-community/async-storage";
 import { PersistConfig, PersistPartial, persistReducer } from "redux-persist";
 import { Action } from "../../store/actions/types";
+import { PotTransform } from "../../store/transforms/potTransform";
 import { MvlState, mvlReducer as rootReducer } from "./store/reducers";
 
 const CURRENT_REDUX_MVL_STORE_VERSION = 1;
@@ -11,7 +12,8 @@ export const mvlPersistConfig: PersistConfig = {
   key: "mvl",
   storage: AsyncStorage,
   version: CURRENT_REDUX_MVL_STORE_VERSION,
-  whitelist: ["preferences", "downloads"]
+  whitelist: ["preferences", "downloads"],
+  transforms: [PotTransform]
 };
 
 export const mvlPersistor = persistReducer<MvlState, Action>(
