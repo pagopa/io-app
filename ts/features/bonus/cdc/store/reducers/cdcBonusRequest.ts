b8aef842a16dd91c3ade1c4836ed09449c4ade57
Simone <simone.biffi.91@gmail.com>
2022-05-17 10:29:34 +0200

    remove unused import

--- ts/features/bonus/cdc/store/reducers/cdcBonusRequest.ts
+++ ts/features/bonus/cdc/store/reducers/cdcBonusRequest.ts
@@ -1,8 +1,5 @@
 import { getType } from "typesafe-actions";
-import { createSelector } from "reselect";
-import { constUndefined } from "fp-ts/lib/function";
 import {
-  fold,
   remoteError,
   remoteLoading,
   remoteReady,
@@ -22,7 +19,6 @@ import {
   cdcSelectedBonus
 } from "../actions/cdcBonusRequest";
 import { GlobalState } from "../../../../../store/reducers/types";
-import { StatoBeneficiarioEnum } from "../../../../../../definitions/cdc/StatoBeneficiario";
 
 export type CdcBonusRequestState = {
   bonusList: RemoteValue<CdcBonusRequestList, NetworkError>;
