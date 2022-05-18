60238f19539ebb5081bcdc7b601f0a83fbab5c5d
Simone <simone.biffi.91@gmail.com>
2022-05-18 14:32:26 +0200

    add cdc mixpanel events

---
+++ ts/features/bonus/cdc/analytics/index.ts
@@ -0,0 +1,39 @@
+import { getType } from "typesafe-actions";
+import { cdcEnabled } from "../../../../config";
+import { mixpanel } from "../../../../mixpanel";
+import { Action } from "../../../../store/actions/types";
+import {
+  cdcEnrollUserToBonus,
+  cdcRequestBonusList
+} from "../store/actions/cdcBonusRequest";
+
+/**
+ * numero richieste passate a sogei
+ numero richieste con residenza estera
+ numero richieste per anno
+ * @param mp
+ */
+const trackCdc =
+  (mp: NonNullable<typeof mixpanel>) =>
+  (action: Action): Promise<void> => {
+    switch (action.type) {
+      case getType(cdcRequestBonusList.request):
+      case getType(cdcRequestBonusList.success):
+        return mp.track(action.type);
+      case getType(cdcEnrollUserToBonus.failure):
+      case getType(cdcRequestBonusList.failure):
+        return mp.track(action.type, {
+          reason: action.payload
+        });
+      case getType(cdcEnrollUserToBonus.request):
+        return mp.track(action.type, { bonusYear: action.payload });
+      case getType(cdcEnrollUserToBonus.success):
+        return mp.track(action.type);
+    }
+    return Promise.resolve();
+  };
+
+const emptyTracking = (_: NonNullable<typeof mixpanel>) => (__: Action) =>
+  Promise.resolve();
+
+export default cdcEnabled ? trackCdc : emptyTracking;
