421df80bb74047dff7335f6064dbadbd577b3216
CrisTofani <cri.tofani@gmail.com>
2022-05-19 16:35:03 +0200

    fixes internalLink tests

--- ts/utils/__tests__/internalLink.test.ts
+++ ts/utils/__tests__/internalLink.test.ts
@@ -1,67 +1,33 @@
-import { none, some } from "fp-ts/lib/Option";
 import { Tuple2 } from "italia-ts-commons/lib/tuples";
 import {
   getInternalRoute,
   testableALLOWED_ROUTE_NAMES
 } from "../../components/ui/Markdown/handlers/internalLink";
-import ROUTES from "../../navigation/routes";
 import { IO_INTERNAL_LINK_PREFIX } from "../navigation";
 
 describe("getInternalRoute", () => {
   const allowedRoutes = Object.entries(testableALLOWED_ROUTE_NAMES!).map(
-    ([r, v]) =>
-      Tuple2(
-        `${IO_INTERNAL_LINK_PREFIX}${r}`,
-        some({
-          navigationAction: v,
-          routeName: r
-        })
-      )
+    ([r, v]) => Tuple2(`${IO_INTERNAL_LINK_PREFIX}${r}`, v)
   );
   const validRoute = Object.keys(testableALLOWED_ROUTE_NAMES!)[0];
   it("should recognize a valid internal route", () => {
     [
       ...allowedRoutes,
-      Tuple2("", none),
-      Tuple2("  some noise  ", none),
-      Tuple2(allowedRoutes[0].e1 + "suffix", none),
+      Tuple2("", ""),
+      Tuple2("  some noise  ", "  some noise  "),
+      Tuple2(
+        allowedRoutes[0].e1 + "suffix",
+        allowedRoutes[0].e1.replace(IO_INTERNAL_LINK_PREFIX, "/") + "suffix"
+      ),
       Tuple2(
         IO_INTERNAL_LINK_PREFIX + validRoute + "?param1=value1&param2=value2",
-        some({
-          routeName: validRoute,
-          navigationAction: {
-            payload: {
-              name: ROUTES.MAIN,
-              params: {
-                screen: ROUTES.MESSAGES_HOME
-              }
-            },
-            type: "NAVIGATE"
-          },
-          params: {
-            param1: "value1",
-            param2: "value2"
-          }
-        })
+        `${
+          testableALLOWED_ROUTE_NAMES![validRoute]
+        }?param1=value1&param2=value2`
       ),
       Tuple2(
         IO_INTERNAL_LINK_PREFIX + validRoute + "?param1=&param2=value2",
-        some({
-          routeName: validRoute,
-          navigationAction: {
-            payload: {
-              name: ROUTES.MAIN,
-              params: {
-                screen: ROUTES.MESSAGES_HOME
-              }
-            },
-            type: "NAVIGATE"
-          },
-          params: {
-            param1: "",
-            param2: "value2"
-          }
-        })
+        `${testableALLOWED_ROUTE_NAMES![validRoute]}?param1=&param2=value2`
       )
     ].forEach(tuple => {
       expect(getInternalRoute(tuple.e1)).toEqual(tuple.e2);
