74b2e7c4f51e789cd053257210c50cb87a7fcaf3
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-06-07 19:32:21 +0200

    Handle error logs

--- jestSetupDevServer.js
+++ jestSetupDevServer.js
@@ -1,10 +1,14 @@
 import { spawn, execSync } from "child_process";
 import cwd from "cwd";
 
+const logPrefix = "[DEV SERVER]";
 let server = null;
 
 beforeAll(done => {
-  console.log("Starting development server in a child process...");
+  console.log(
+    logPrefix,
+    "🚀 Starting the development server in a child process..."
+  );
 
   execSync(
     "cp ./scripts/api-config.json ./node_modules/io-dev-api-server/config/config.json",
@@ -30,10 +34,15 @@ beforeAll(done => {
   server.stdout.on("data", data => {
     done();
   });
+
+  server.stderr.on("data", data => {
+    console.error(logPrefix, data.toString());
+  });
 });
 
-afterAll(done => {
+afterAll(() => {
   if (server) {
+    console.log(logPrefix, "🔪 Killing the development server...");
     server.kill();
   }
 });
