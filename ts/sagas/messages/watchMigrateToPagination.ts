807a089fc7d0ea7677becfba415fcbeaa10cb20e
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-06-10 16:50:46 +0200

    Fix linting error in `watchMigrateToPagination`

--- ts/sagas/messages/watchMigrateToPagination.ts
+++ ts/sagas/messages/watchMigrateToPagination.ts
@@ -1,6 +1,6 @@
 import { call, put, takeLatest } from "typed-redux-saga/macro";
 import { ActionType, getType } from "typesafe-actions";
-
+import { ValidationError } from "io-ts";
 import { MessageStatusBulkChange } from "../../../definitions/backend/MessageStatusBulkChange";
 import { MessageStatusArchivingChange } from "../../../definitions/backend/MessageStatusArchivingChange";
 import { BackendClient } from "../../api/backend";
@@ -11,10 +11,8 @@ import {
 import { ReduxSagaEffect, SagaCallReturnType } from "../../types/utils";
 import { isTestEnv } from "../../utils/environment";
 import migrateToPagination from "../../boot/migrateToPagination";
-
 import { MessageStatus } from "../../store/reducers/entities/messages/messagesStatus";
 import { readablePrivacyReport } from "../../utils/reporters";
-import { ValidationError } from "io-ts";
 
 type LocalActionType = ActionType<typeof migrateToPaginatedMessages["request"]>;
 type LocalBeClient = ReturnType<
