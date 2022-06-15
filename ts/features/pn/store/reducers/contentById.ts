64c5d1fe9d57f5096b7a26137bd12bf1f2bf6aa1
Emilio Pavia <emilio.pavia@gmail.com>
2022-06-15 15:56:08 +0200

    Implement reducer

--- ts/features/pn/store/reducers/contentById.ts
+++ ts/features/pn/store/reducers/contentById.ts
@@ -1,10 +1,17 @@
 import * as pot from "italia-ts-commons/lib/pot";
 import { createSelector } from "reselect";
+import { getType } from "typesafe-actions";
 import { ThirdPartyMessageWithContent } from "../../../../../definitions/backend/ThirdPartyMessageWithContent";
 import { Action } from "../../../../store/actions/types";
 import { IndexedById } from "../../../../store/helpers/indexer";
 import { UIMessageId } from "../../../../store/reducers/entities/messages/types";
+import {
+  toError,
+  toLoading,
+  toSome
+} from "../../../../store/reducers/IndexedByIdPot";
 import { GlobalState } from "../../../../store/reducers/types";
+import { loadPnContent } from "../actions";
 
 export type PnContentByIdState = IndexedById<
   pot.Pot<ThirdPartyMessageWithContent, Error>
@@ -20,7 +27,17 @@ export const initialState: PnContentByIdState = {};
 export const pnContentByIdReducer = (
   state: PnContentByIdState = initialState,
   action: Action
-): PnContentByIdState => state;
+): PnContentByIdState => {
+  switch (action.type) {
+    case getType(loadPnContent.request):
+      return toLoading(action.payload, state);
+    case getType(loadPnContent.success):
+      return toSome(action.payload.id, state, action.payload.content);
+    case getType(loadPnContent.failure):
+      return toError(action.payload.id, state, action.payload.error);
+  }
+  return state;
+};
 
 /**
  * From UIMessageId to the PN content pot
