b7d63e532761b8082af1b5832126eb85d78b69d5
Emilio Pavia <emilio.pavia@gmail.com>
2022-06-15 15:37:54 +0200

    Fix import

--- ts/features/pn/store/reducers/index.ts
+++ ts/features/pn/store/reducers/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import { Action } from "../../../../store/actions/types";
-import { pnContentByIdReducer, PnContentByIdState } from "./byId";
+import { pnContentByIdReducer, PnContentByIdState } from "./contentById";
 import { PnPreferences, pnPreferencesReducer } from "./preferences";
 
 export type PnState = {
