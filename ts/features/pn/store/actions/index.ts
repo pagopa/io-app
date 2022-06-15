2e8ae00c4d7ec414e28415a2087d811c97c70f7c
Emilio Pavia <emilio.pavia@gmail.com>
2022-06-15 15:34:01 +0200

    Add async action

--- ts/features/pn/store/actions/index.ts
+++ ts/features/pn/store/actions/index.ts
@@ -1,4 +1,10 @@
-import { ActionType, createStandardAction } from "typesafe-actions";
+import {
+  ActionType,
+  createAsyncAction,
+  createStandardAction
+} from "typesafe-actions";
+import { ThirdPartyMessageWithContent } from "../../../../../definitions/backend/ThirdPartyMessageWithContent";
+import { UIMessageId } from "../../../../store/reducers/entities/messages/types";
 
 /**
  * Set whether to show a warning message when opening a PN message.
@@ -8,6 +14,19 @@ export const pnPreferencesSetWarningForMessageOpening = createStandardAction(
   "PN_PREFERENCES_SET_WARNING_FOR_MESSAGE_OPENING"
 )<boolean>();
 
+/**
+ * The user requests the message content from PN.
+ */
+export const loadPnContent = createAsyncAction(
+  "PN_CONTENT_LOAD_REQUEST",
+  "PN_CONTENT_LOAD_SUCCESS",
+  "PN_CONTENT_LOAD_FAILURE"
+)<
+  UIMessageId,
+  { id: UIMessageId; content: ThirdPartyMessageWithContent },
+  { id: UIMessageId; error: Error }
+>();
+
 export type PnActions = ActionType<
-  typeof pnPreferencesSetWarningForMessageOpening
+  typeof pnPreferencesSetWarningForMessageOpening | typeof loadPnContent
 >;
