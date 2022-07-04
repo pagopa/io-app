2dc3f2adc1af482b5823f174b9f99c62482fa2af
Simone <simone.biffi.91@gmail.com>
2022-07-04 17:21:59 +0200

    add ZENDESK_SEE_REPORTS_ROUTERS routes and navigator

--- ts/features/zendesk/navigation/navigator.tsx
+++ ts/features/zendesk/navigation/navigator.tsx
@@ -6,6 +6,7 @@ import ZendeskChooseCategory from "../screens/ZendeskChooseCategory";
 import ZendeskChooseSubCategory from "../screens/ZendeskChooseSubCategory";
 import ZendeskPanicMode from "../screens/ZendeskPanicMode";
 import ZendeskAskSeeReportsPermissions from "../screens/ZendeskAskSeeReportsPermissions";
+import ZendeskSeeReportsRouters from "../screens/ZendeskSeeReportsRouters";
 import ZENDESK_ROUTES from "./routes";
 
 export const zendeskSupportNavigator = createCompatNavigatorFactory(
@@ -17,6 +18,9 @@ export const zendeskSupportNavigator = createCompatNavigatorFactory(
     [ZENDESK_ROUTES.ASK_PERMISSIONS]: { screen: ZendeskAskPermissions },
     [ZENDESK_ROUTES.ASK_SEE_REPORTS_PERMISSIONS]: {
       screen: ZendeskAskSeeReportsPermissions
+    },
+    [ZENDESK_ROUTES.SEE_REPORTS_ROUTERS]: {
+      screen: ZendeskSeeReportsRouters
     },
     [ZENDESK_ROUTES.CHOOSE_CATEGORY]: { screen: ZendeskChooseCategory },
     [ZENDESK_ROUTES.CHOOSE_SUB_CATEGORY]: { screen: ZendeskChooseSubCategory }
--- ts/features/zendesk/navigation/params.ts
+++ ts/features/zendesk/navigation/params.ts
@@ -3,6 +3,7 @@ import { ZendeskChooseCategoryNavigationParams } from "../screens/ZendeskChooseC
 import { ZendeskChooseSubCategoryNavigationParams } from "../screens/ZendeskChooseSubCategory";
 import { ZendeskSupportHelpCenterNavigationParams } from "../screens/ZendeskSupportHelpCenter";
 import { ZendeskAskSeeReportsPermissionsNavigationParams } from "../screens/ZendeskAskSeeReportsPermissions";
+import { ZendeskSeeReportsRoutersNavigationParams } from "../screens/ZendeskSeeReportsRouters";
 import ZENDESK_ROUTES from "./routes";
 
 export type ZendeskParamsList = {
@@ -10,6 +11,7 @@ export type ZendeskParamsList = {
   [ZENDESK_ROUTES.PANIC_MODE]: undefined;
   [ZENDESK_ROUTES.ASK_PERMISSIONS]: ZendeskAskPermissionsNavigationParams;
   [ZENDESK_ROUTES.ASK_SEE_REPORTS_PERMISSIONS]: ZendeskAskSeeReportsPermissionsNavigationParams;
+  [ZENDESK_ROUTES.SEE_REPORTS_ROUTERS]: ZendeskSeeReportsRoutersNavigationParams;
   [ZENDESK_ROUTES.CHOOSE_CATEGORY]: ZendeskChooseCategoryNavigationParams;
   [ZENDESK_ROUTES.CHOOSE_SUB_CATEGORY]: ZendeskChooseSubCategoryNavigationParams;
 };
