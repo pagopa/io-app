a0dffe340090f74e93b19c5f83ecbb1cf91f570b
CrisTofani <cri.tofani@gmail.com>
2022-07-19 17:51:25 +0200

    add new navigator

---
+++ ts/navigation/ServicesNavigator.tsx
@@ -0,0 +1,47 @@
+import { createCompatNavigatorFactory } from "@react-navigation/compat";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { myPortalEnabled, svEnabled } from "../config";
+import svNavigationConfig from "../features/bonus/siciliaVola/navigation/navigator";
+
+import SV_ROUTES from "../features/bonus/siciliaVola/navigation/routes";
+import ServiceDetailsScreen from "../screens/services/ServiceDetailsScreen";
+import ServicesWebviewScreen from "../screens/services/ServicesWebviewScreen";
+import ROUTES from "./routes";
+
+const SvNavigator = createCompatNavigatorFactory(createStackNavigator)(
+  svNavigationConfig,
+  {
+    // Let each screen handle the header and navigation
+    headerMode: "none",
+    defaultNavigationOptions: {
+      gestureEnabled: false
+    }
+  }
+);
+
+const Stack = createStackNavigator();
+
+const ServicesNavigator = () => (
+  <Stack.Navigator
+    initialRouteName={ROUTES.SERVICE_DETAIL}
+    headerMode={"none"}
+    screenOptions={{ gestureEnabled: true }}
+  >
+    <Stack.Screen
+      name={ROUTES.SERVICE_DETAIL}
+      component={ServiceDetailsScreen}
+    />
+    {myPortalEnabled && (
+      <Stack.Screen
+        name={ROUTES.SERVICE_WEBVIEW}
+        component={ServicesWebviewScreen}
+      />
+    )}
+    {svEnabled && (
+      <Stack.Screen name={SV_ROUTES.MAIN} component={SvNavigator} />
+    )}
+  </Stack.Navigator>
+);
+
+export default ServicesNavigator;
