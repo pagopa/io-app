f4a0b28f628cc8beabd529a9d27d477b660e020f
Damiano Plebani <damiano.plebani@pagopa.it>
2022-07-22 11:00:26 +0200

    Remove deprecated yellow gradient

--- ts/components/core/variables/IOColors.ts
+++ ts/components/core/variables/IOColors.ts
@@ -57,16 +57,13 @@ export const IOColors = asIOColors({
   green: "#005C3C",
   /* ↳ ALIAS TOKEN: colorSuccess → green */
   greyGradientTop: "#475A6D",
-  greyGradientBottom: "#42484F",
-  yellowGradientTop: "#FEC709",
-  yellowGradientBottom: "#FAA01F"
+  greyGradientBottom: "#42484F"
 });
 
 export const IOColorGradients = asIOColorGradients({
   appLaunch: ["#0C00D3", "#0073E6"],
   appIcon: ["#1D51DF", "#1723D5"],
   grey: ["#475A6D", "#42484F"],
-  yellow: ["#FEC709", "#FAA01F"],
   cgn: ["#9184B7", "#5C488F"],
   cgnCulture: ["#C51C82", "#E28DC0"],
   cgnHealth: ["#F1901A", "#EE898A"],
