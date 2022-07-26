e6f16eac91c75e2d9c3704e161ff8357f4da7d46
Cristiano <cri.tofani@gmail.com>
2022-07-26 17:41:53 +0200

    fixes

--- ts/navigation/ServicesNavigator.tsx
+++ ts/navigation/ServicesNavigator.tsx
@@ -1,27 +1,14 @@
-import { createCompatNavigatorFactory } from "@react-navigation/compat";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as React from "react";
 import { myPortalEnabled, svEnabled } from "../config";
-import svNavigationConfig, {
+import {
   SvVoucherGenerationNavigator,
   SvVoucherListNavigator
 } from "../features/bonus/siciliaVola/navigation/navigator";
-
 import SV_ROUTES from "../features/bonus/siciliaVola/navigation/routes";
 import ServiceDetailsScreen from "../screens/services/ServiceDetailsScreen";
 import ServicesWebviewScreen from "../screens/services/ServicesWebviewScreen";
 import ROUTES from "./routes";
-
-const SvNavigator = createCompatNavigatorFactory(createStackNavigator)(
-  svNavigationConfig,
-  {
-    // Let each screen handle the header and navigation
-    headerMode: "none",
-    defaultNavigationOptions: {
-      gestureEnabled: false
-    }
-  }
-);
 
 const Stack = createStackNavigator();
 
