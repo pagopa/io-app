46fdc3723a41a70edfac36a45ec3601fdb6d4522
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2022-10-21 18:07:41 +0200

    update notification preferences

--- ts/screens/profile/NotificationsPreferencesScreen.tsx
+++ ts/screens/profile/NotificationsPreferencesScreen.tsx
@@ -1,6 +1,7 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import React, { useEffect, useState } from "react";
 import { List } from "native-base";
+import { StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { PreferencesListItem } from "../../components/PreferencesListItem";
 import ScreenContent from "../../components/screens/ScreenContent";
@@ -12,6 +13,18 @@ import { useIODispatch } from "../../store/hooks";
 import { profileUpsert } from "../../store/actions/profile";
 import { ReminderStatusEnum } from "../../../definitions/backend/ReminderStatus";
 import { showToast } from "../../utils/showToast";
+import { Link } from "../../components/core/typography/Link";
+import customVariables from "../../theme/variables";
+import { useIOBottomSheetModal } from "../../utils/hooks/bottomSheet";
+import FooterWithButtons from "../../components/ui/FooterWithButtons";
+import { Body } from "../../components/core/typography/Body";
+
+const styles = StyleSheet.create({
+  mediumText: {
+    fontSize: customVariables.fontSizeSmall,
+    lineHeight: customVariables.h5LineHeight
+  }
+});
 
 export const NotificationsPreferencesScreen = () => {
   const dispatch = useIODispatch();
@@ -22,6 +35,25 @@ export const NotificationsPreferencesScreen = () => {
   const isError = pot.isError(remindersPreference);
   const isUpdating = pot.isUpdating(remindersPreference);
 
+  const { present, bottomSheet, dismiss } = useIOBottomSheetModal(
+    <Body>
+      {I18n.t("profile.preferences.notifications.preview.bottomSheet.content")}
+    </Body>,
+    I18n.t("profile.preferences.notifications.preview.bottomSheet.title"),
+    400,
+    <FooterWithButtons
+      type="SingleButton"
+      leftButton={{
+        block: true,
+        primary: true,
+        onPress: () => dismiss(),
+        title: I18n.t(
+          "profile.preferences.notifications.preview.bottomSheet.cta"
+        )
+      }}
+    />
+  );
+
   useEffect(() => {
     if (isError && isUpserting) {
       showToast(I18n.t("profile.preferences.notifications.error"));
@@ -31,15 +63,9 @@ export const NotificationsPreferencesScreen = () => {
     }
   }, [isError, isUpdating, isUpserting]);
 
-  const toggleRemindersPreference = (isEnabled: boolean) => {
+  const togglePreference = <T,>(type: string, value: T) => {
     setIsUpserting(true);
-    dispatch(
-      profileUpsert.request({
-        reminder_status: isEnabled
-          ? ReminderStatusEnum.ENABLED
-          : ReminderStatusEnum.DISABLED
-      })
-    );
+    dispatch(profileUpsert.request({ [type]: value }));
   };
 
   return (
@@ -53,6 +79,33 @@ export const NotificationsPreferencesScreen = () => {
       >
         <List withContentLateralPadding={true}>
           <PreferencesListItem
+            title={I18n.t("profile.preferences.notifications.preview.title")}
+            description={
+              <>
+                {`${I18n.t(
+                  "profile.preferences.notifications.preview.description"
+                )} `}
+                <Link style={styles.mediumText} onPress={present}>
+                  {I18n.t("profile.preferences.notifications.preview.link")}
+                </Link>
+              </>
+            }
+            rightElement={
+              <RemoteSwitch
+                value={remindersPreference}
+                onValueChange={(value: boolean) =>
+                  togglePreference<ReminderStatusEnum>(
+                    "reminder_status",
+                    value
+                      ? ReminderStatusEnum.ENABLED
+                      : ReminderStatusEnum.DISABLED
+                  )
+                }
+                testID="previewPreferenceSwitch"
+              />
+            }
+          />
+          <PreferencesListItem
             title={I18n.t("profile.preferences.notifications.reminders.title")}
             description={I18n.t(
               "profile.preferences.notifications.reminders.description"
@@ -60,12 +113,20 @@ export const NotificationsPreferencesScreen = () => {
             rightElement={
               <RemoteSwitch
                 value={remindersPreference}
-                onValueChange={toggleRemindersPreference}
+                onValueChange={(value: boolean) =>
+                  togglePreference<ReminderStatusEnum>(
+                    "reminder_status",
+                    value
+                      ? ReminderStatusEnum.ENABLED
+                      : ReminderStatusEnum.DISABLED
+                  )
+                }
                 testID="remindersPreferenceSwitch"
-              ></RemoteSwitch>
+              />
             }
           />
         </List>
+        {bottomSheet}
       </ScreenContent>
     </TopScreenComponent>
   );
