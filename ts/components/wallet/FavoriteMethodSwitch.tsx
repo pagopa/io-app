4703efefc9a208b191f311e46cbd517984a97c51
Emilio Pavia <emilio.pavia@gmail.com>
2022-10-24 15:40:45 +0200

    feat: [IA-945] Add a screen to preferences to opt-in/out reminders notifications (#4128)
    * Add feature flag
    
    * Update backend specs to v9.1.0
    
    * Add item to preferences list
    
    * Add navigation to NotificationsPreferenceScreen
    
    * Add content to NotificationsPreferenceScreen
    
    * Add logic to the remote switch
    
    * Rename PreferenceListItem to PreferencesListItem
    
    * Rename NotificationsPreferenceScreen to NotificationsPreferencesScreen
    
    * Fix import
    
    * Fix imports
    
    * Show an error if the upser fails
    
    * Add tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Alice Azzolini <93777761+AliceAzzolini@users.noreply.github.com>
    
    * Update locales/en/index.yml
    
    Co-authored-by: Alice Azzolini <93777761+AliceAzzolini@users.noreply.github.com>
    
    * Update locales/en/index.yml
    
    Co-authored-by: Alice Azzolini <93777761+AliceAzzolini@users.noreply.github.com>
    
    Co-authored-by: Jacopo Pompilii <Jacopo.pompilii@pagopa.it>
    Co-authored-by: Alice Azzolini <93777761+AliceAzzolini@users.noreply.github.com>
    Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>

--- ts/components/wallet/FavoriteMethodSwitch.tsx
+++ ts/components/wallet/FavoriteMethodSwitch.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import { ActivityIndicator } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
-import { BasePaymentFeatureListItem } from "../../features/wallet/component/features/BasePaymentFeatureListItem";
 import I18n from "../../i18n";
 import { setFavouriteWalletRequest } from "../../store/actions/wallet/wallets";
 import { GlobalState } from "../../store/reducers/types";
@@ -16,6 +15,7 @@ import { PaymentMethod } from "../../types/pagopa";
 import { isAndroid } from "../../utils/platform";
 import { handleSetFavourite } from "../../utils/wallet";
 import { IOStyleVariables } from "../core/variables/IOStyleVariables";
+import { PreferencesListItem } from "../PreferencesListItem";
 import Switch from "../ui/Switch";
 
 type OwnProps = {
@@ -75,7 +75,7 @@ const FavoritePaymentMethodSwitch = (props: Props) => {
     />
   );
   return (
-    <BasePaymentFeatureListItem
+    <PreferencesListItem
       testID={"FavoritePaymentMethodSwitch"}
       title={I18n.t("wallet.favourite.setFavoriteTitle")}
       description={I18n.t("wallet.favourite.setFavoriteSubtitle")}
