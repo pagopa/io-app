6536db0afa78756b44830a58bf8ffd463fe4a559
Francesco Persico <francescopersico@gmail.com>
2022-10-30 09:58:04 +0100

    Add xstate types generation

--- ts/features/idpay/onboarding/xstate/machine.ts
+++ ts/features/idpay/onboarding/xstate/machine.ts
@@ -62,7 +62,6 @@ const createIDPayOnboardingMachine = () =>
   /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOgHUBBASQBUqA5AcQH0HaqK6A1AUWYGUeAGR4BhOgHl6AYkEjxreu05VeAbQAMAXUSgADgHtYuAC64D+XSAAeiAMx2ATCQCsLhwA4A7F4CcvjwBGRy8XABoQAE9EQO8SAI07ABYk7wCvR0cAXyyItCw8QlIhCQoAEQYWNjoVXmYyzgppCAswEgIANwMAazaAGwN0CCp8U1x0Mw6wMon0TR0kEENjMwsrWwQPDySSLw8ANhd9jS8HR3dHCOiEWK94j0SUtO9MnLyMHAJiEgr+AAUhBQAJqVRTKbh8Bo0JoUUSiHh-GjMGgSfjzKzLMZrRYbQJ2Xz7EhJFwElwPE4uLxJLxXRCOQKBEj7Jz7B4hLYaI4uN4gfKfIokWHwxGglH8Zqtdr4Lq9EhYTBgPQmGhGdGLTGrSw4mIeTIkQL7Xx2FwaAleQJuGlROkMpkstl7B5cnl8wrfErlUF-ABKPF9AEUAKpUfi0HjilqEKUy-qDCB-ABOYCTAEcAK64FZwNX6IxYrWgDaOOzM-UafZU0K+E6+cLWm5xBLJVJ+F7ZXK8j5u0g8LgUISBlRMZg+v08IMhsPinNLPOa9Z07yE-zG-YVpwaE60hD0nbMk5JRwPXzE9vvApfHt9gdDlijgPB0M0cPSNSBBa5lbmAs2RdeZdGkc66OJuVrXAcRI+MEPgHEaGjEi6XaXiQvb9oOdDDve46PlOr6OB+s5ftihZ-gBq7AaB252EERJ2H4GQeC4KS1uWiEXgKvwAsCXplPQZQjr6D6Ts+4pCgiSJ-Lx-FYROT7hjOGrfguCDJMa+rUcEDIlh4Th1tcIE7ASvghIcxaBKEIRsfy3ycYCIKYVJAljrJuFiYiI6OTJOEiW+BGKcRv4qUkal4kE9J4qyunbucy5JEZ1ZHLWoReFZ3Y-CGXH2SwcgAGJOUJcmiXC4kCMIeVecJ8naBic5KdqCBJIEBK7I4a5Nd4iUeNu1bxMlwWBI1DwRalyFuRhd6CdhlURpKnQ9G08qKiYibJmA6aZqY2bVeqtUBRsqRrq4jhxbW-iBBomTbudhLUuWTVMfiDgITy+AGBAcBWK6yGUOww7VBwEKlfIkj0DVRE-vtlz1vSzh4q1Docs6HZfQKHoVH9Sg1IDUIUGD+bKe4Gj6i450nsadhNRaV1bOpjFHqEqSmhWI0cRldmgv9tSQo0ePzvVDJHq4dibiBXJbAS0XBOp8MXY6nKHCz3xjaKqK83VJE3G4dhMiZ+wWkajj+F19axB4tMuPTTHHilyNIajpToxNzneeGat7XSm6+PqjVGnsFtrtTZuhRbHWM2aitXmht75VNhVuxDdLHISFP7K11Gtb4-6S7D9qy4jCu2+xNls9xDl8THLkifHykU-StElvSGixI1DjZ9LrJ506BfntZpC2aX2VlRXLv8NX9XUR4XutUxx1+IbdjdYy1KUv1g1Nw4EeCsVIqYZNleuzt4PKU8RMGtR-jUkka5gYguq9fsV9DSuoSb-3WXMFIABCpTeo7zCiBIAAsgCHgz4yhjw1iTECJBdTNx8MWA6V0BrqQpiWQ85ljongjhAwKABafY258E5ByEAA */
   createMachine(
     {
-      context: {},
       tsTypes: {} as import("./machine.typegen").Typegen0,
       schema: {
         context: {} as Context,
@@ -70,7 +69,8 @@ const createIDPayOnboardingMachine = () =>
         services: {} as Services
       },
       predictableActionArguments: true,
-      id: "(machine)",
+      id: "IDPAY_ONBOARDING",
+      context: {},
       initial: "WAITING_INITIATIVE_SELECTION",
       states: {
         WAITING_INITIATIVE_SELECTION: {
