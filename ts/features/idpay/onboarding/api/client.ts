ee86a8024c3d69c622649eb238443e15b6a180a9
Francesco Persico <francescopersico@gmail.com>
2022-11-02 17:30:09 +0100

    Update client to support bearer token

--- ts/features/idpay/onboarding/api/client.ts
+++ ts/features/idpay/onboarding/api/client.ts
@@ -1,10 +1,20 @@
 import { createClient } from "../../../../../definitions/idpay/onboarding/client";
 import { defaultRetryingFetch } from "../../../../utils/fetch";
 
-const createOnboardingClient = (baseUrl: string, _token: string) =>
-  createClient({
+const createOnboardingClient = (baseUrl: string, token: string) =>
+  createClient<"bearerAuth">({
     baseUrl,
-    fetchApi: defaultRetryingFetch()
+    fetchApi: defaultRetryingFetch(),
+    withDefaults: op => params => {
+      const paramsWithDefaults = {
+        ...params,
+        bearerAuth: `Bearer ${token}`
+      } as Parameters<typeof op>[0];
+
+      return op(paramsWithDefaults);
+    }
   });
+
+export type OnboardingClient = ReturnType<typeof createOnboardingClient>;
 
 export { createOnboardingClient };
