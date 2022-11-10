215ea59c1ad20d14c9f3a334c5bd61e6f84a2af4
Andrea Piai <andrea.piai@pagopa.it>
2022-11-10 12:11:11 +0100

    Refactored onboarding code on startup saga

--- ts/sagas/startup.ts
+++ ts/sagas/startup.ts
@@ -308,68 +308,59 @@ export function* initializeApplicationSaga(): Generator<
   // Start watching for requests of abort the onboarding
   const watchAbortOnboardingSagaTask = yield* fork(watchAbortOnboardingSaga);
 
-  if (!previousSessionToken || O.isNone(maybeStoredPin)) {
-    // The user wasn't logged in when the application started or, for some
-    // reason, he was logged in but there is no unlock code set, thus we need
-    // to pass through the onboarding process.
+  // This block has been refactored to avoid a code duplication on an if-else branching.
+  // Some of the conditions could have been simplified further (i.e., the firstOnBoarding
+  // check is done here but also inside some sagas) but, in order to keep the refactoring
+  // as simple as possible, only this part has been edited
+  const hasSessionAndPin = previousSessionToken && O.isSome(maybeStoredPin);
+  if (hasSessionAndPin) {
+    storedPin = maybeStoredPin.value;
 
-    // Ask to accept ToS if it is the first access on IO or if there is a new available version of ToS
-    yield* call(checkAcceptedTosSaga, userProfile);
+    // we ask the user to identify using the unlock code.
+    // FIXME: This is an unsafe cast caused by a wrongly described type.
+    const identificationResult: SagaCallReturnType<
+      typeof startAndReturnIdentificationResult
+    > = yield* call(startAndReturnIdentificationResult, storedPin);
 
-    // check if the user expressed preference about mixpanel, if not ask for it
-    yield* call(askMixpanelOptIn);
+    if (identificationResult === IdentificationResult.pinreset) {
+      // If we are here the user had chosen to reset the unlock code
+      yield* put(startApplicationInitialization());
+      return;
+    }
+  }
 
+  // Ask to accept ToS if there is a new available version
+  yield* call(checkAcceptedTosSaga, userProfile);
+
+  // check if the user expressed preference about mixpanel, if not ask for it
+  yield* call(askMixpanelOptIn);
+
+  // This condition comes from the refactoring. By reading it, it may seem that
+  // this code is handling a non-logged in user (due to not having a session) but
+  // actually, if the session was not valid, the code would have stopped before
+  // reaching this point. Consider refactoring even more by removing the check
+  // on the session
+  if (!hasSessionAndPin) {
     storedPin = yield* call(checkConfiguredPinSaga);
 
     yield* call(checkAcknowledgedFingerprintSaga);
 
     yield* call(checkAcknowledgedEmailSaga, userProfile);
-
-    const isFirstOnboarding = isProfileFirstOnBoarding(userProfile);
-
-    // check if the user must set preferences for push notifications (e.g. reminders)
-    yield* call(checkNotificationsPreferencesSaga, userProfile);
-
-    yield* call(askServicesPreferencesModeOptin, isFirstOnboarding);
-
-    // Show the thank-you screen for the onboarding
-    if (isFirstOnboarding) {
-      yield* call(completeOnboardingSaga);
-    }
-
-    // Stop the watchAbortOnboardingSaga
-    yield* cancel(watchAbortOnboardingSagaTask);
-  } else {
-    storedPin = maybeStoredPin.value;
-    if (!isSessionRefreshed) {
-      // The user was previously logged in, so no onboarding is needed
-      // The session was valid so the user didn't event had to do a full login,
-      // in this case we ask the user to identify using the unlock code.
-      // FIXME: This is an unsafe cast caused by a wrongly described type.
-      const identificationResult: SagaCallReturnType<
-        typeof startAndReturnIdentificationResult
-      > = yield* call(startAndReturnIdentificationResult, storedPin);
-
-      if (identificationResult === IdentificationResult.pinreset) {
-        // If we are here the user had chosen to reset the unlock code
-        yield* put(startApplicationInitialization());
-        return;
-      }
-      // Ask to accept ToS if there is a new available version
-      yield* call(checkAcceptedTosSaga, userProfile);
-
-      // check if the user expressed preference about mixpanel, if not ask for it
-      yield* call(askMixpanelOptIn);
-
-      // check if the user must set preferences for push notifications (e.g. reminders)
-      yield* call(checkNotificationsPreferencesSaga, userProfile);
-
-      yield* call(askServicesPreferencesModeOptin, false);
-
-      // Stop the watchAbortOnboardingSaga
-      yield* cancel(watchAbortOnboardingSagaTask);
-    }
   }
+
+  // check if the user must set preferences for push notifications (e.g. reminders)
+  yield* call(checkNotificationsPreferencesSaga, userProfile);
+
+  const isFirstOnboarding = isProfileFirstOnBoarding(userProfile);
+  yield* call(askServicesPreferencesModeOptin, isFirstOnboarding);
+
+  if (isFirstOnboarding) {
+    // Show the thank-you screen for the onboarding
+    yield* call(completeOnboardingSaga);
+  }
+
+  // Stop the watchAbortOnboardingSaga
+  yield* cancel(watchAbortOnboardingSagaTask);
 
   // Start the notification installation update as early as
   // possible to begin receiving push notifications
