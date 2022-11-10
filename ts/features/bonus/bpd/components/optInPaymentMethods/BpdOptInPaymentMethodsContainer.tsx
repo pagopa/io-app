45bf5fbcf95cd20552b398f1a1016e0d728223dd
Cristiano Tofani <cri.tofani@gmail.com>
2022-11-10 10:14:03 +0100

    feat: [IOAPPPEB-4] Handle new opt-in FF and adds the support to new linking (#4164)
    * handle new opt-in FF and adds the support to new linking
    
    * update specs tag on io-services-metadata
    
    * fix wrong FF in navigator
    
    * Fix choice was not sent to API
    
    * fix wrong FF on AppStackNavigator
    
    * fixes from review
    
    * Update ts/navigation/AppStackNavigator.tsx
    
    * fix danger

--- ts/features/bonus/bpd/components/optInPaymentMethods/BpdOptInPaymentMethodsContainer.tsx
+++ ts/features/bonus/bpd/components/optInPaymentMethods/BpdOptInPaymentMethodsContainer.tsx
@@ -1,15 +1,15 @@
+import * as React from "react";
 import { useContext, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import * as React from "react";
 import LoadingSpinnerOverlay from "../../../../../components/LoadingSpinnerOverlay";
-import I18n from "../../../../../i18n";
-import { bpdOptInPaymentMethodsEnabled } from "../../../../../config";
-import { bpdRemoteConfigSelector } from "../../../../../store/reducers/backendStatus";
-import { useIOSelector } from "../../../../../store/hooks";
-import { optInPaymentMethodsShowChoice } from "../../store/actions/optInPaymentMethods";
 import { LightModalContext } from "../../../../../components/ui/LightModal";
-import { showOptInChoiceSelector } from "../../store/reducers/details/activation/ui";
+import { bpdOptInPaymentMethodsEnabled } from "../../../../../config";
+import I18n from "../../../../../i18n";
+import { useIOSelector } from "../../../../../store/hooks";
+import { bpdRemoteConfigSelector } from "../../../../../store/reducers/backendStatus";
 import { isError, isReady } from "../../model/RemoteValue";
+import { optInPaymentMethodsShowChoice } from "../../store/actions/optInPaymentMethods";
+import { showOptInChoiceSelector } from "../../store/reducers/details/activation/ui";
 import { bpdLastUpdateSelector } from "../../store/reducers/details/lastUpdate";
 
 const BpdOptInPaymentMethodsContainer = () => {
@@ -20,7 +20,7 @@ const BpdOptInPaymentMethodsContainer = () => {
   const showOptInChoice = useIOSelector(showOptInChoiceSelector);
   const bpdLastUpdate = useIOSelector(bpdLastUpdateSelector);
   const isOptInPaymentMethodsEnabled =
-    bpdRemoteConfig?.opt_in_payment_methods && bpdOptInPaymentMethodsEnabled;
+    bpdRemoteConfig?.opt_in_payment_methods_v2 && bpdOptInPaymentMethodsEnabled;
 
   useEffect(() => {
     if (isOptInPaymentMethodsEnabled && !showOptInChecked) {
