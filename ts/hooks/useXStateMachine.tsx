6c2be3d62dab7c65b555bff1244ab66daed241ab
Francesco Persico <francescopersico@gmail.com>
2022-11-14 13:03:59 +0100

    Avoid xstate machine recreation on provider rerendering

---
+++ ts/hooks/useXStateMachine.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type MachineCreatorFn<T> = () => T;
+
+const useXStateMachine = <T,>(fn: MachineCreatorFn<T>): [T] => {
+  const machine = React.useRef<T | undefined>(undefined);
+
+  if (machine.current === undefined) {
+    // eslint-disable-next-line functional/immutable-data
+    machine.current = fn();
+    // eslint-disable-next-line no-console
+    console.log("Created new xstate machine");
+  }
+
+  return [machine.current];
+};
+
+export { useXStateMachine };
