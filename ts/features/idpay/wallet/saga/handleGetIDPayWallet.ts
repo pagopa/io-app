fc6abdbe6d97e64f8e8283bc3d7b008dc1092608
Federico Mastrini <federicomastrini93@gmail.com>
2022-11-16 12:11:01 +0100

    Added mocked data to work on the feature (waiting for APIs)

--- ts/features/idpay/wallet/saga/handleGetIDPayWallet.ts
+++ ts/features/idpay/wallet/saga/handleGetIDPayWallet.ts
@@ -5,6 +5,53 @@ import { IDPayWalletClient } from "../api/client";
 import { idPayWalletGet } from "../store/actions";
 import { readablePrivacyReport } from "../../../../utils/reporters";
 import { getGenericError, getNetworkError } from "../../../../utils/errors";
+import { WalletDTO } from "../../../../../definitions/idpay/wallet/WalletDTO";
+import { StatusEnum } from "../../../../../definitions/idpay/wallet/InitiativeDTO";
+
+const mockedData: WalletDTO = {
+  initiativeList: [
+    {
+      initiativeId: "63492bace71cdb164d377853",
+      initiativeName: "ANTONIO",
+      status: StatusEnum.NOT_REFUNDABLE,
+      endDate: new Date("2022-10-31T00:00:00"),
+      available: 100,
+      accrued: 0.0,
+      refunded: 0.0,
+      nInstr: "0"
+    },
+    {
+      initiativeId: "6349320ba1bbcf7cb9d98f6c",
+      initiativeName: "LUCE",
+      status: StatusEnum.NOT_REFUNDABLE_ONLY_INSTRUMENT,
+      endDate: new Date("2022-10-29T00:00:00"),
+      available: 10.0,
+      accrued: 0.0,
+      refunded: 0.0,
+      nInstr: "1"
+    },
+    {
+      initiativeId: "63495094a1bbcf7cb9d98f6d",
+      initiativeName: "Stefano",
+      status: StatusEnum.NOT_REFUNDABLE_ONLY_INSTRUMENT,
+      endDate: new Date("2022-10-31T00:00:00"),
+      available: 10,
+      accrued: 0.0,
+      refunded: 0.0,
+      nInstr: "2"
+    },
+    {
+      initiativeId: "6364fce370fc881452fdaa2c",
+      initiativeName: "BonusIO Test Criteri manuali",
+      status: StatusEnum.NOT_REFUNDABLE,
+      endDate: new Date("2022-12-31T00:00:00"),
+      available: 10.0,
+      accrued: 0.0,
+      refunded: 0.0,
+      nInstr: "0"
+    }
+  ]
+};
 
 /**
  * Handle the remote call to retrieve the IDPay wallet
@@ -14,11 +61,15 @@ import { getGenericError, getNetworkError } from "../../../../utils/errors";
 export function* handleGetIDPayWallet(
   getWallet: IDPayWalletClient["getWallet"]
 ) {
+  yield* put(idPayWalletGet.success(mockedData));
+  return;
+
   try {
     const getWalletResult: SagaCallReturnType<typeof getWallet> = yield* call(
       getWallet,
       {}
     );
+
     if (E.isRight(getWalletResult)) {
       if (getWalletResult.right.status === 200) {
         // handled success
