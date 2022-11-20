9cd4b1d250f4948a2e8d2636f4b4be71f9d4e2f8
Francesco Persico <francescopersico@gmail.com>
2022-11-20 16:21:01 +0100

    Initiative configuration state machine

---
+++ ts/features/idpay/wallet/initiative/configuration/xstate/machine.ts
@@ -0,0 +1,78 @@
+import { createMachine } from "xstate";
+import {
+  LOADING_TAG,
+  WAITING_USER_INPUT_TAG
+} from "../../../../../../utils/xstate";
+
+type Context = {
+  initiativeId?: string;
+};
+
+type E_SELECT_INITIATIVE = {
+  type: "SELECT_INITIATIVE";
+};
+
+type Events = E_SELECT_INITIATIVE;
+
+type Services = {};
+
+const createIDPayInitiativeConfigurationMachine = () =>
+  createMachine({
+    predictableActionArguments: true,
+    tsTypes: {} as import("./machine.typegen").Typegen0,
+    schema: {
+      context: {} as Context,
+      events: {} as Events,
+      services: {} as Services
+    },
+    id: "IDPAY_INITIATIVE_CONFIGURATION",
+    initial: "WAITING_INITIATIVE_SELECTION",
+    states: {
+      WAITING_INITIATIVE_SELECTION: {
+        tags: [LOADING_TAG],
+        on: {
+          SELECT_INITIATIVE: {
+            target: "LOADING_INITIATIVE",
+            actions: "selectInitiative"
+          }
+        }
+      },
+      LOADING_INITIATIVE: {
+        tags: [LOADING_TAG],
+        invoke: {
+          src: "loadInitiative",
+          id: "loadInitiative",
+          onDone: [
+            {
+              target: "EVALUTING_INITIATIVE_CONFIGURATION",
+              actions: "loadInitiativeSuccess"
+            }
+          ]
+        }
+      },
+      EVALUTING_INITIATIVE_CONFIGURATION: {
+        tags: [LOADING_TAG],
+        type: "final",
+        always: [
+          {
+            cond: "isIBANConfigurationNeeded",
+            target: "CONFIGURING_IBAN"
+          },
+          {
+            cond: "isInstrumentsConfigurationNeeded",
+            target: "CONFIGURING_INSTRUMENTS"
+          }
+        ]
+      },
+      CONFIGURING_IBAN: {
+        tags: [WAITING_USER_INPUT_TAG],
+        type: "final"
+      },
+      CONFIGURING_INSTRUMENTS: {
+        tags: [WAITING_USER_INPUT_TAG],
+        type: "final"
+      }
+    }
+  });
+
+export { createIDPayInitiativeConfigurationMachine };
