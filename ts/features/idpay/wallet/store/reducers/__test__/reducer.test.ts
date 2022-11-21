34d693b18e87718a9826a3865fe233052e165b8a
Federico Mastrini <federicomastrini93@gmail.com>
2022-11-21 10:41:03 +0100

    Fixed typo in tests

--- ts/features/idpay/wallet/store/reducers/__test__/reducer.test.ts
+++ ts/features/idpay/wallet/store/reducers/__test__/reducer.test.ts
@@ -1,7 +1,7 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import { createStore } from "redux";
 import { idPayWalletInitiativeListSelector, idPayWalletSelector } from "..";
-import { WalletDTO } from "../../../../../../../definitions/idpay/wallet/WalletDTO";
+import { WalletDTO } from "../../../../../../../definitions/idPay/wallet/WalletDTO";
 import { applicationChangeState } from "../../../../../../store/actions/application";
 import { appReducer } from "../../../../../../store/reducers";
 import { NetworkError } from "../../../../../../utils/errors";
@@ -19,7 +19,7 @@ const mockFailure: NetworkError = {
 describe("Test IDPay wallet reducers and selectors", () => {
   it("should be pot.none before the first loading action", () => {
     const globalState = appReducer(undefined, applicationChangeState("active"));
-    expect(globalState.features.idpay.wallet).toStrictEqual(pot.none);
+    expect(globalState.features.idPay.wallet).toStrictEqual(pot.none);
     expect(idPayWalletSelector(globalState)).toStrictEqual(pot.none);
     expect(idPayWalletInitiativeListSelector(globalState)).toStrictEqual(
       pot.none
@@ -29,7 +29,7 @@ describe("Test IDPay wallet reducers and selectors", () => {
     const globalState = appReducer(undefined, applicationChangeState("active"));
     const store = createStore(appReducer, globalState as any);
     store.dispatch(idPayWalletGet.request());
-    expect(store.getState().features.idpay.wallet).toStrictEqual(
+    expect(store.getState().features.idPay.wallet).toStrictEqual(
       pot.noneLoading
     );
     expect(idPayWalletSelector(store.getState())).toStrictEqual(
@@ -44,7 +44,7 @@ describe("Test IDPay wallet reducers and selectors", () => {
     const store = createStore(appReducer, globalState as any);
     store.dispatch(idPayWalletGet.request());
     store.dispatch(idPayWalletGet.success(mockResponseSuccess));
-    expect(store.getState().features.idpay.wallet).toStrictEqual(
+    expect(store.getState().features.idPay.wallet).toStrictEqual(
       pot.some(mockResponseSuccess)
     );
     expect(idPayWalletSelector(store.getState())).toStrictEqual(
@@ -59,7 +59,7 @@ describe("Test IDPay wallet reducers and selectors", () => {
     const store = createStore(appReducer, globalState as any);
     store.dispatch(idPayWalletGet.request());
     store.dispatch(idPayWalletGet.failure(mockFailure));
-    expect(store.getState().features.idpay.wallet).toStrictEqual(
+    expect(store.getState().features.idPay.wallet).toStrictEqual(
       pot.noneError(mockFailure)
     );
     expect(idPayWalletSelector(store.getState())).toStrictEqual(
