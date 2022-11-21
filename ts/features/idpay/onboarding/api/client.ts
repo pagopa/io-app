307ac4a89997b6756bf0057816624a750045163a
forrest57 <tomba808@gmail.com>
2022-11-21 14:27:28 +0100

    first commit

--- ts/features/idpay/onboarding/api/client.ts
+++ ts/features/idpay/onboarding/api/client.ts
@@ -1,18 +1,26 @@
 import { createClient } from "../../../../../definitions/idpay/onboarding/client";
+
+import { PreferredLanguageEnum } from "../../../../../definitions/backend/PreferredLanguage";
 import { defaultRetryingFetch } from "../../../../utils/fetch";
 
-const createOnboardingClient = (baseUrl: string, token: string) =>
-  createClient<"bearerAuth">({
+const createOnboardingClient = (
+  baseUrl: string,
+  defaultsFunc: () => RequestInit
+  token: string,
+  language: PreferredLanguageEnum
+) =>
+  createClient<"bearerAuth" | "Accept-Language">({
     baseUrl,
     fetchApi: defaultRetryingFetch(),
-    withDefaults: op => params => {
-      const paramsWithDefaults = {
-        ...params,
-        bearerAuth: token
-      } as Parameters<typeof op>[0];
+    withDefaults:defaultsFunc
+    // withDefaults: op => params => {
+    //   const paramsWithDefaults = {
+    //     ...params,
+    //     bearerAuth: token
+    //   };
 
-      return op(paramsWithDefaults);
-    }
+    //   return op(paramsWithDefaults);
+    // }
   });
 
 export type OnboardingClient = ReturnType<typeof createOnboardingClient>;
