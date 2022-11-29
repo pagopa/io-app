b46eb0123eb690963be6aaafbfda56e6aca14552
Federico Mastrini <federicomastrini93@gmail.com>
2022-11-29 10:18:52 +0100

    Data from machine and navigation back to details

--- ts/features/idpay/initiative/configuration/screens/AssociationSuccessScreen.tsx
+++ ts/features/idpay/initiative/configuration/screens/AssociationSuccessScreen.tsx
@@ -1,4 +1,8 @@
+import * as pot from "@pagopa/ts-commons/lib/pot";
 import { useNavigation } from "@react-navigation/native";
+import { useActor } from "@xstate/react";
+import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/lib/Option";
 import { Text, View } from "native-base";
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
@@ -7,17 +11,34 @@ import ButtonDefaultOpacity from "../../../../../components/ButtonDefaultOpacity
 import { H3 } from "../../../../../components/core/typography/H3";
 import { IOStyles } from "../../../../../components/core/variables/IOStyles";
 import I18n from "../../../../../i18n";
+import {
+  AppParamsList,
+  IOStackNavigationProp
+} from "../../../../../navigation/params/AppParamsList";
+import ROUTES from "../../../../../navigation/routes";
 import themeVariables from "../../../../../theme/variables";
+import { useConfigurationMachineService } from "../xstate/provider";
 
 const AssociationSuccessScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<IOStackNavigationProp<AppParamsList>>();
 
-  // TODO get initative name and ID
-  const initiativeName = "l'Iniziativa 1";
+  const configurationMachine = useConfigurationMachineService();
+  const [state] = useActor(configurationMachine);
+
+  const initiativeDetails = pipe(
+    state.context.initiative,
+    pot.toOption,
+    O.toUndefined
+  );
+
+  if (initiativeDetails === undefined) {
+    return null;
+  }
 
   const handleNavigateToInitiativePress = () => {
-    // TODO handle navigation to initiative details
-    navigation.goBack();
+    navigation.navigate(ROUTES.IDPAY_INITIATIVE_DETAILS, {
+      initiativeId: initiativeDetails.initiativeId
+    });
   };
 
   return (
@@ -30,7 +51,7 @@ const AssociationSuccessScreen = () => {
           <View spacer={true} />
           <Text style={styles.body}>
             {I18n.t("idpay.configuration.associationSuccess.body", {
-              initiativeName
+              initiativeName: initiativeDetails.initiativeName
             })}
           </Text>
         </View>
