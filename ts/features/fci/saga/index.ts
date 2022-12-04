c3d95978b7a9605209cad275924f9ec1e6869c66
Mario Perrotta <mario.perrotta@pagopa.it>
2022-12-04 23:56:19 +0100

    refactor: use takelatest

--- ts/features/fci/saga/index.ts
+++ ts/features/fci/saga/index.ts
@@ -1,16 +1,8 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import { SagaIterator } from "redux-saga";
-import { ActionType } from "typesafe-actions";
-import {
-  call,
-  fork,
-  take,
-  takeLatest,
-  put,
-  select
-} from "typed-redux-saga/macro";
+import { ActionType, getType } from "typesafe-actions";
+import { call, takeLatest, put, select } from "typed-redux-saga/macro";
 import { CommonActions } from "@react-navigation/native";
-import { ReduxSagaEffect } from "../../../types/utils";
 import NavigationService from "../../../navigation/NavigationService";
 import { FCI_ROUTES } from "../navigation/routes";
 import ROUTES from "../../../navigation/routes";
@@ -41,7 +33,7 @@ export function* watchFciSaga(bearerToken: SessionToken): SagaIterator {
 
   // handle the request of getting FCI signatureRequestDetails
   yield* takeLatest(
-    fciSignatureRequestFromId.request,
+    getType(fciSignatureRequestFromId.request),
     function* (action: ActionType<typeof fciSignatureRequestFromId.request>) {
       yield* call(
         handleGetSignatureRequestById,
@@ -52,71 +44,66 @@ export function* watchFciSaga(bearerToken: SessionToken): SagaIterator {
   );
 
   // handle the request of getting QTSP metadata
-  yield* takeLatest(fciLoadQtspClauses.request, function* () {
-    yield* call(handleGetQtspMetadata, fciClient.getQtspClausesMetadata);
-  });
-
   yield* takeLatest(
-    fciLoadQtspFilledDocument.request,
-    function* (action: ActionType<typeof fciLoadQtspFilledDocument.request>) {
-      yield* call(
-        handleCreateFilledDocument,
-        fciClient.postQtspFilledBody,
-        action
-      );
-    }
+    getType(fciLoadQtspClauses.request),
+    handleGetQtspMetadata,
+    fciClient.getQtspClausesMetadata
   );
 
-  yield* fork(watchFciQtspClausesSaga);
+  yield* takeLatest(
+    getType(fciLoadQtspFilledDocument.request),
+    handleCreateFilledDocument,
+    fciClient.postQtspFilledBody
+  );
 
-  yield* fork(watchFciStartSaga);
-
-  yield* fork(watchFciAbortSaga);
+  yield* takeLatest(getType(fciStartRequest), watchFciStartSaga);
+  yield* takeLatest(
+    getType(fciLoadQtspClauses.success),
+    watchFciQtspClausesSaga
+  );
+  yield* takeLatest(getType(fciAbortRequest), watchFciAbortSaga);
 }
 
-function* watchFciQtspClausesSaga(): Iterator<ReduxSagaEffect> {
-  while (true) {
-    yield* take(fciLoadQtspClauses.success);
-    const potQtspClauses: FciQtspClausesState = yield* select(
-      fciQtspClausesSelector
-    );
+/**
+ * Handle the FCI requests to get the QTSP filled_document
+ */
+function* watchFciQtspClausesSaga(): SagaIterator {
+  const potQtspClauses: FciQtspClausesState = yield* select(
+    fciQtspClausesSelector
+  );
 
-    if (pot.isSome(potQtspClauses)) {
-      const documentUrl = potQtspClauses.value.document_url;
-      yield* put(
-        fciLoadQtspFilledDocument.request({ document_url: documentUrl })
-      );
-    }
+  if (pot.isSome(potQtspClauses)) {
+    const documentUrl = potQtspClauses.value.document_url;
+    yield* put(
+      fciLoadQtspFilledDocument.request({ document_url: documentUrl })
+    );
   }
 }
 
 /**
  * Handle the FCI abort requests
  */
-function* watchFciAbortSaga(): Iterator<ReduxSagaEffect> {
-  while (true) {
-    yield* take(fciAbortRequest);
-    NavigationService.dispatchNavigationAction(
-      CommonActions.navigate(ROUTES.MAIN)
-    );
-  }
+function* watchFciAbortSaga(): SagaIterator {
+  yield* call(
+    NavigationService.dispatchNavigationAction,
+    CommonActions.navigate(ROUTES.MAIN)
+  );
 }
 
 /**
  * Handle the FCI start requests
  */
-function* watchFciStartSaga(): Iterator<ReduxSagaEffect> {
-  while (true) {
-    yield* take(fciStartRequest);
-    NavigationService.dispatchNavigationAction(
-      CommonActions.navigate(FCI_ROUTES.MAIN, {
-        screen: FCI_ROUTES.DOCUMENTS
-      })
-    );
-    // when the user start signing flow
-    // start a request to get the QTSP metadata
-    // this is needed to get the document_url
-    // that will be used to create the filled document
-    yield* put(fciLoadQtspClauses.request());
-  }
+function* watchFciStartSaga(): SagaIterator {
+  yield* call(
+    NavigationService.dispatchNavigationAction,
+    CommonActions.navigate(FCI_ROUTES.MAIN, {
+      screen: FCI_ROUTES.DOCUMENTS
+    })
+  );
+  // when the user start signing flow
+  // start a request to get the QTSP metadata
+  // this is needed to get the document_url
+  // that will be used to create the filled document
+  yield* put(fciLoadQtspClauses.request());
+  // }
 }
