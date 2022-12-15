d16af59b7bc28474ad1fd23f997568f53ce0d05a
Federico Mastrini <federicomastrini93@gmail.com>
2022-12-15 17:04:23 +0100

    Fix state transitions

--- ts/features/idpay/initiative/configuration/xstate/machine.ts
+++ ts/features/idpay/initiative/configuration/xstate/machine.ts
@@ -93,6 +93,7 @@ type Services = {
 };
 
 const createIDPayInitiativeConfigurationMachine = () =>
+  /** @xstate-layout N4IgpgJg5mDOIC5QEkAiAFAggTQPrIDlkAVZTUgNQFFcBhAeQIDFkBxAVQCVzlGA6AOqYShVviKke1XAGUqAGSq1SjAMRzFy8SKlUA2gAYAuolAAHAPawAlgBdrFgHamQAD0QBmDwHY+AFgAmAEYADiCDPyCATgMAVm8QgBoQAE9EML5YgwMg8ICQ7Kio2I8AX1LktCw8Qh1KGgZmNi4efnl6TFRRbUl61QgnMD5rRwA3CwBrIYAbCwBDCGRHO2s5+1GwQxMkEEsbeycXdwQQjyj-EMjTqO94vw9Y5LSTjz5snICzoIA2CPzvcqVDA4Hpkep0RgsDjcFQEPhUCiYeTsUgEMS1XrIaSNKEtWGqLYuPYrQ47Y6RAJ8Dz3bwPIIeELeb7eCJPRB+Yp8H5BWLxb6BDm-b6AkBVEEYsFYhqQ5ow3hwhFIlHdCW6CFNaGtAgEoLbcxWEnOMnsoKU6k+OkMpksvxshCRIJ8KK5b6M2JRfLRekisU1CSS7EyzWwvg42Uq-26dTETCcYjq3FyxiEnbEg5G0DHcJBPx8AJ8j1+X6XMJ2rIhPghfIBGthGJFgEVUXAv11KUJ2Va0NBrgqgBCmG1A0cQ1gtjWQ19oLVYeD8u7Gt7aPwA4IKf1+wcGbciACRQr3kiMQKgRZAW+dtdFYdRYMzO+UQZUR9Len4NnePnH84-cHfHanS-gQuDyMgMjEP0gzDGMkwzPMiwAEZzI48jWGO667Aa6ZHIg3LfFyjLUvE2S-H4jypIgsR+AYmRnAYRSRIEBgeMKTZTqq749kmcLfkBfBdDI6DyDgQEgWBEGsPQuADrQADSGFpluOEINmub5kyhbFn4pYUQgT5vAk+axDytyxFWrFAtUb7tt+Xa8cuyCrvxYFCSJDmrmJ4GqJ0qAroOClYUpxoIAEES5lWHzaSEITfAEtJlh4lKxZ8h6-DW3zfGUbGvhxNlcXZXF8T5QGQSO0HjFMfALIhyEBZupKZrugTnHeNahTWHq-N4CW5qanzRf1oVRBZzZWblgaLtx-4dF0DkEOBnDsAAslQBDEDIpVDCMFVwQsSxjgATgArgAtmAji2LAdWGspNJct4w2ZRlURXJldq5KFVKhb80RhZcAQvmNkacZNXYCa52ARgty2retqiSdJmBydd2HBbEATvXuvi-MU3gJNpUQhMNgPisDeWgyG4PCZDc3Qyta0bT54h07DKNBY1CCPrEtH0jkJSxA+2nvaE3NxTFPgGCE6M3CTraYhNiZgy51NQ8Qi303DYacEtzNqzDDNsw1O6cw8PMeHzDyC0kum8zR1Gml8+b1lllmk22CudiGxW03rGubeVsFVRAiyOIdp3nbYhvbscHrcyUvM8pbL3W884TOnwLJxNEfgPd4+ay9ZHtzvwVNuWItmwrI7C0LQVAyBtDBLUJVDENKFPylHyk8sZTo+LyqXfGZ3hBO95t2x8jvuhEWVNo4FgQHALjsWTRefowRKBUbxwALQXrpu8F+NbeKyGQgiHN7s0BoSiwhv9XR+yGM23EbxnMEWTxNSESNq7csBsfnt5wAVmuiFeVA743TRtRPgrpaQ1hzPmHI3VdIZBKLHAwNZaQYLxofMBHZi4KkRMiVEoDL74LXgQCBqMObxDtNpfCDEWTcjij8QeuCyEVy-IVC+8twGpk3g-BALJXgDVpPSEoNZbS6SLOcZ03IGT5h+D-UabteHkKmvZdEq4qHs2Ng7CsUsczWiZETKIdCGSZASJLDB2R7gC3YWozh-BNF+ThMA0SoFwI6K3rhfI+FDE52ZCYooZZzwGTrBEAo-MggOP-uogqk0+KlxplowcnliDeMEaEH490fgxT8GRc2ZjdIC0pPEAoAQixE2yLcWJM58ohhcY5P83tUmUP4ffZSwQpYZ2iqcKiDwx5SOeBImB3weQ5ioi9QWdSQYny4Yk9yf5mnAScas+gTdFCt1QJkrpvxuY5jCCWc8fNyIjKok6XIWRk75AKbEWZ5N5ltBmqrdWsMZC7OCsyYWL8pY3FuPWVqI1l4cIafOZJrz9brU+RzDKNE0EGGHnje4MRkGpz3DRDBsVeR7mMj9B5q8pqtN1m8taMK9H0nOPyS4sVDyRASMLYeTpCi5GHl4LwANspA1Be3Euysy7xMrjIautd67kqzCLPMacMFFi-sxTGeN-A5HCP9R8tICUAIIQuJ56zNktyoDsjpkCaFotwmZV4MRebGTIhSYFOU8FrO1YAxguACD0HjAQKgBqDXisooq3kRZMqIvoTpZ41EKxyNpIySW+TiblFKEAA */
   createMachine(
     {
       context: INITIAL_CONTEXT,
@@ -145,47 +146,56 @@ const createIDPayInitiativeConfigurationMachine = () =>
           entry: "navigateToConfigurationEntry",
           on: {
             START_CONFIGURATION: {
-              target: "LOADING_IBAN_LIST"
+              target: "CONFIGURING_IBAN"
             }
           }
         },
-        LOADING_IBAN_LIST: {
-          tags: [LOADING_TAG],
-          entry: "navigateToIbanAssociationScreen",
-          invoke: {
-            src: "loadIbanList",
-            id: "loadIbanList",
-            onDone: [
-              {
-                target: "DISPLAYING_IBAN_LIST",
-                actions: "loadIbanListSuccess"
+        CONFIGURING_IBAN: {
+          id: "IBAN",
+          initial: "LOADING_IBAN_LIST",
+          states: {
+            LOADING_IBAN_LIST: {
+              tags: [LOADING_TAG],
+              entry: "navigateToIbanAssociationScreen",
+              invoke: {
+                src: "loadIbanList",
+                id: "loadIbanList",
+                onDone: {
+                  target: "DISPLAYING_IBAN_LIST",
+                  actions: "loadIbanListSuccess"
+                }
               }
-            ]
-          }
-        },
-        DISPLAYING_IBAN_LIST: {
-          tags: [WAITING_USER_INPUT_TAG],
-          on: {
-            GO_BACK: {
-              target: "CONFIGURING_INITIATIVE"
             },
-            ADD_IBAN: {
-              target: "ADDING_IBAN",
-              actions: "selectIban"
-            }
-          }
-        },
-        ADDING_IBAN: {
-          tags: [LOADING_TAG],
-          invoke: {
-            src: "addIban",
-            id: "addIban",
-            onDone: [
-              {
-                target: "DISPLAYING_INSTRUMENTS",
-                actions: "addIbanSuccess"
+            DISPLAYING_IBAN_LIST: {
+              tags: [WAITING_USER_INPUT_TAG],
+              on: {
+                GO_BACK: {
+                  target:
+                    "#IDPAY_INITIATIVE_CONFIGURATION.CONFIGURING_INITIATIVE"
+                },
+                ADD_IBAN: {
+                  target: "ADDING_IBAN",
+                  actions: "selectIban"
+                }
               }
-            ]
+            },
+            ADDING_IBAN: {
+              tags: [LOADING_TAG],
+              invoke: {
+                src: "addIban",
+                id: "addIban",
+                onDone: {
+                  target: "IBAN_CONFIGURATION_COMPLETED",
+                  actions: "addIbanSuccess"
+                }
+              }
+            },
+            IBAN_CONFIGURATION_COMPLETED: {
+              type: "final"
+            }
+          },
+          onDone: {
+            target: "#IDPAY_INITIATIVE_CONFIGURATION.LOADING_INSTRUMENTS"
           }
         },
         LOADING_INSTRUMENTS: {
@@ -206,7 +216,7 @@ const createIDPayInitiativeConfigurationMachine = () =>
           tags: [WAITING_USER_INPUT_TAG],
           on: {
             GO_BACK: {
-              target: "LOADING_IBAN_LIST"
+              target: "CONFIGURING_IBAN"
             },
             ADD_INSTRUMENT: {
               target: "ADDING_INSTRUMENT",
