71489d43f18cb0b832f2f59ef6dfd0d6615d4402
Damiano Plebani <damiano.plebani@pagopa.it>
2022-12-19 17:21:14 +0100

    Add neutral toast notification using straight `Toast` NB component

--- ts/screens/showroom/core/ToastNotificationsShowroom.tsx
+++ ts/screens/showroom/core/ToastNotificationsShowroom.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Text as NBText, View as NBView } from "native-base";
+import { Text as NBText, View as NBView, Toast as NBToast } from "native-base";
 
 import { ShowroomSection } from "../components/ShowroomSection";
 import ButtonDefaultOpacity from "../../../components/ButtonDefaultOpacity";
@@ -11,14 +11,29 @@ export const ToastNotificationsShowroom = () => (
     <H2 color={"bluegrey"} weight={"SemiBold"} style={{ marginBottom: 16 }}>
       Type
     </H2>
+
     <ButtonDefaultOpacity
       bordered={true}
       block={true}
       onPress={() => {
-        showToast("Here's the default behavior");
+        NBToast.show({
+          text: "Here's the default behavior"
+        });
       }}
     >
       <NBText>Default behavior</NBText>
+    </ButtonDefaultOpacity>
+
+    <NBView spacer={true} />
+
+    <ButtonDefaultOpacity
+      bordered={true}
+      block={true}
+      onPress={() => {
+        showToast("Example of a danger message");
+      }}
+    >
+      <NBText>Danger</NBText>
     </ButtonDefaultOpacity>
 
     <NBView spacer={true} />
@@ -84,6 +99,26 @@ export const ToastNotificationsShowroom = () => (
       }}
     >
       <NBText>Default · Bottom</NBText>
+    </ButtonDefaultOpacity>
+
+    <H2
+      color={"bluegrey"}
+      weight={"SemiBold"}
+      style={{ marginTop: 24, marginBottom: 16 }}
+    >
+      Misc
+    </H2>
+
+    <ButtonDefaultOpacity
+      bordered={true}
+      block={true}
+      onPress={() => {
+        NBToast.show({
+          text: "Here's the default behavior with multi-line loooong loooooong text"
+        });
+      }}
+    >
+      <NBText>Multi-line text</NBText>
     </ButtonDefaultOpacity>
   </ShowroomSection>
 );
