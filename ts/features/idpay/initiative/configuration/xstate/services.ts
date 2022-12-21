f39753836ed1134b90b4417378ec46e246293ce1
Federico Mastrini <federicomastrini93@gmail.com>
2022-12-21 15:54:39 +0100

    Removed unused imports

--- ts/features/idpay/initiative/configuration/xstate/services.ts
+++ ts/features/idpay/initiative/configuration/xstate/services.ts
@@ -2,7 +2,6 @@ import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
 import { PreferredLanguageEnum } from "../../../../../../definitions/backend/PreferredLanguage";
-import { IbanDTO } from "../../../../../../definitions/idpay/iban/IbanDTO";
 import { IbanListDTO } from "../../../../../../definitions/idpay/iban/IbanListDTO";
 import { InitiativeDTO } from "../../../../../../definitions/idpay/wallet/InitiativeDTO";
 import { PaymentManagerClient } from "../../../../../api/pagopa";
