9091d26a528ba9b593b8a38684811f4092c5f5e4
Mario Perrotta <mario.perrotta@pagopa.it>
2022-12-23 13:30:05 +0100

    test: update handle saga

--- ts/features/fci/saga/networking/__tests__/handleCreateFilledDocument.test.ts
+++ ts/features/fci/saga/networking/__tests__/handleCreateFilledDocument.test.ts
@@ -4,7 +4,10 @@ import { left, right } from "fp-ts/lib/Either";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { getNetworkError } from "../../../../../utils/errors";
 import { qtspFilledDocument } from "../../../types/__mocks__/CreateFilledDocumentBody.mock";
-import { fciLoadQtspFilledDocument } from "../../../store/actions";
+import {
+  fciLoadQtspFilledDocument,
+  fciPollFilledDocument
+} from "../../../store/actions";
 import { CreateFilledDocument } from "../../../../../../definitions/fci/CreateFilledDocument";
 import { handleCreateFilledDocument } from "../handleCreateFilledDocument";
 import { FilledDocumentDetailView } from "../../../../../../definitions/fci/FilledDocumentDetailView";
@@ -36,6 +39,8 @@ describe("handleCreateFilledDocument", () => {
       })
       .next(right(successResponse))
       .put(fciLoadQtspFilledDocument.success(successResponse.value))
+      .next()
+      .put(fciPollFilledDocument.request())
       .next()
       .isDone();
   });
