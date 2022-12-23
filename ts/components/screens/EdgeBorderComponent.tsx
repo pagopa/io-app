3b3c0286da70013da9a13b07611af8ba30e0cf86
Damiano Plebani <damiano.plebani@pagopa.it>
2022-12-23 10:52:25 +0100

    Add comment, remove useless style properties

--- ts/components/screens/EdgeBorderComponent.tsx
+++ ts/components/screens/EdgeBorderComponent.tsx
@@ -3,14 +3,27 @@ import { StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
   emptyListWrapper: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignSelf: "center",
     marginTop: 40,
     marginBottom: 40,
-    height: 32
+    height: 40
   }
 });
+
+/*
+Dimensions of this component should be set
+without magic numbers. Instead, we should:
+- Calculate the height of the footer
+- Add the page margin to the previous value
+- Assign the obtained value to this component.
+
+Component height = Dynamic footer height + Page margin
+
+The following solution with a fixed height and vertical margins
+is inherited by the previous developments (there was
+a smile image instead of a blank space).
+It should be considered a temporary trade-off
+and not a definitive solution.
+*/
 
 export class EdgeBorderComponent extends React.PureComponent {
   public render() {
