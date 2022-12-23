80c8bef22ad42147b49097d11dd2da9472a74b9a
forrest57 <tomba808@gmail.com>
2022-12-23 12:48:58 +0100

    WIP: styled the onboarding landing

--- ts/features/idpay/initiative/configuration/iban/screens/IbanConfigurationLandingScreen.tsx
+++ ts/features/idpay/initiative/configuration/iban/screens/IbanConfigurationLandingScreen.tsx
@@ -1,12 +1,33 @@
 import { useActor } from "@xstate/react";
-import { Text } from "native-base";
+import { Text, View } from "native-base";
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
 import FooterWithButtons from "../../../../../../components/ui/FooterWithButtons";
 import I18n from "../../../../../../i18n";
 import { emptyContextualHelp } from "../../../../../../utils/emptyContextualHelp";
 import { useConfigurationMachineService } from "../../xstate/provider";
+import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
+import { H3 } from "../../../../../../components/core/typography/H3";
+import { Body } from "../../../../../../components/core/typography/Body";
+import { IOColors } from "../../../../../../components/core/variables/IOColors";
+import { Link } from "../../../../../../components/core/typography/Link";
+
+const styles = StyleSheet.create({
+  greyCircle: {
+    aspectRatio: 1,
+    width: 206,
+    height: 206,
+    backgroundColor: IOColors.greyUltraLight,
+    borderRadius: 100
+  },
+  justifyCenter: { justifyContent: "center" },
+  adjustCenterHeight: {
+    marginTop: -103, // 206 / 2 , the height of the circle; this allows to center the content vertically
+    alignItems: "center"
+  },
+  textCenter: { textAlign: "center" }
+});
 
 const IbanConfigurationLanding = () => {
   const configurationMachine = useConfigurationMachineService();
@@ -18,7 +39,30 @@ const IbanConfigurationLanding = () => {
       headerTitle={I18n.t("idpay.configuration.headerTitle")}
       contextualHelp={emptyContextualHelp}
     >
-      <Text style={{ flex: 1 }}>IBAN CONFIGURATION LANDING</Text>
+      <View
+        style={[
+          IOStyles.flex,
+          styles.justifyCenter,
+          IOStyles.horizontalContentPadding
+        ]}
+      >
+        <View
+          style={[IOStyles.horizontalContentPadding, styles.adjustCenterHeight]}
+        >
+          <View style={styles.greyCircle}></View>
+          <View spacer large />
+          <H3>Aggiungi un IBAN al tuo Profilo</H3>
+          <View spacer />
+          <Body style={styles.textCenter}>
+            Salva i dati del tuo conto e ottieni più facilmente i rimborsi a te
+            previsti.
+          </Body>
+          <Body color="blue" weight="SemiBold">
+            Scopri di più
+          </Body>
+        </View>
+      </View>
+
       <SafeAreaView>
         <FooterWithButtons
           type="SingleButton"
