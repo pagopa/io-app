29dc1aa6e37194385372f63a3a6697e4a9731169
Francesco Persico <francescopersico@gmail.com>
2023-01-02 14:49:16 +0100

    Fix png import

--- ts/features/bonus/common/BaseTimeoutScreen.tsx
+++ ts/features/bonus/common/BaseTimeoutScreen.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { SafeAreaView } from "react-native";
-import image from "../../../../../img/wallet/errors/payment-expired-icon.png";
+import image from "../../../../img/wallet/errors/payment-expired-icon.png";
 import { renderInfoRasterImage } from "../../../components/infoScreen/imageRendering";
 import { InfoScreenComponent } from "../../../components/infoScreen/InfoScreenComponent";
 import I18n from "../../../i18n";
