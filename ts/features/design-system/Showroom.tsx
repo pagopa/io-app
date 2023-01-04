ba129db8259ba934cd0acfeb50ff1918759e905e
Damiano Plebani <damiano.plebani@pagopa.it>
2023-01-04 12:13:47 +0100

    Resolve TS error

--- ts/features/design-system/Showroom.tsx
+++ ts/features/design-system/Showroom.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, SectionList, Text, View } from "react-native";
+import { SafeAreaView, SectionList, View } from "react-native";
 import { View as NBView } from "native-base";
 import * as React from "react";
 import { IOStyles } from "../../components/core/variables/IOStyles";
