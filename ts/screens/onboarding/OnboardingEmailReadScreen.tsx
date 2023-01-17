740abc04006c559862b5dfca18321c7b15c5ff39
Cristiano <cri.tofani@gmail.com>
2023-01-17 18:09:15 +0100

    refactor screen component

--- ts/screens/onboarding/OnboardingEmailReadScreen.tsx
+++ ts/screens/onboarding/OnboardingEmailReadScreen.tsx
@@ -7,20 +7,12 @@
  */
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import { StackActions } from "@react-navigation/native";
-import * as O from "fp-ts/lib/Option";
-import { Text as NBText, View } from "native-base";
 import * as React from "react";
-import { Alert, Platform, SafeAreaView, StyleSheet } from "react-native";
-import { H3 } from "../../components/core/typography/H3";
+import { Alert } from "react-native";
+import EmailReadScreenComponent from "../../components/EmailReadScreenComponent";
 import { withValidatedEmail } from "../../components/helpers/withValidatedEmail";
 import LoadingSpinnerOverlay from "../../components/LoadingSpinnerOverlay";
-import { ContextualHelpPropsMarkdown } from "../../components/screens/BaseScreenComponent";
-import ScreenContent from "../../components/screens/ScreenContent";
-import TopScreenComponent from "../../components/screens/TopScreenComponent";
-import SectionStatusComponent from "../../components/SectionStatus";
 import { TwoButtonsInlineHalf } from "../../components/ui/BlockButtons";
-import FooterWithButtons from "../../components/ui/FooterWithButtons";
-import IconFont from "../../components/ui/IconFont";
 import I18n from "../../i18n";
 import { IOStackNavigationRouteProps } from "../../navigation/params/AppParamsList";
 import { OnboardingParamsList } from "../../navigation/params/OnboardingParamsList";
@@ -30,45 +22,15 @@ import {
   emailAcknowledged
 } from "../../store/actions/onboarding";
 import { useIODispatch, useIOSelector } from "../../store/hooks";
-import { profileEmailSelector } from "../../store/reducers/profile";
 import { userMetadataSelector } from "../../store/reducers/userMetadata";
-import customVariables from "../../theme/variables";
 
 type Props = IOStackNavigationRouteProps<
   OnboardingParamsList,
   "READ_EMAIL_SCREEN"
 >;
 
-const styles = StyleSheet.create({
-  flex: {
-    flex: 1
-  },
-  emailWithIcon: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center"
-  },
-  content: {
-    paddingHorizontal: customVariables.contentPadding,
-    backgroundColor: customVariables.contentBackground,
-    flex: 1
-  },
-  spacerSmall: { height: 12 },
-  spacerLarge: { height: 24 },
-  icon: {
-    marginTop: Platform.OS === "android" ? 3 : 0, // correct icon position to align it with baseline of email text
-    marginRight: 8
-  }
-});
-
-const contextualHelpMarkdown: ContextualHelpPropsMarkdown = {
-  title: "profile.data.email.contextualHelpTitle",
-  body: "profile.data.email.contextualHelpContent"
-};
-
 const OnboardingEmailReadScreen = (props: Props) => {
   const dispatch = useIODispatch();
-  const optionEmail = useIOSelector(profileEmailSelector);
   const potUserMetadata = useIOSelector(userMetadataSelector);
 
   const isLoading = pot.isLoading(potUserMetadata);
@@ -115,37 +77,10 @@ const OnboardingEmailReadScreen = (props: Props) => {
 
   return (
     <LoadingSpinnerOverlay isLoading={isLoading}>
-      <SafeAreaView style={styles.flex}>
-        <TopScreenComponent
-          goBack={handleGoBack}
-          headerTitle={I18n.t("profile.data.list.email")}
-          contextualHelpMarkdown={contextualHelpMarkdown}
-        >
-          <ScreenContent
-            title={I18n.t("email.read.title")}
-            subtitle={I18n.t("email.insert.subtitle")}
-          >
-            <View style={styles.content}>
-              <NBText>{I18n.t("email.insert.label")}</NBText>
-              <View style={styles.spacerSmall} />
-              <View style={styles.emailWithIcon}>
-                <IconFont
-                  name="io-envelope"
-                  accessible={true}
-                  accessibilityLabel={I18n.t("email.read.title")}
-                  size={24}
-                  style={styles.icon}
-                />
-                {O.isSome(optionEmail) && <H3>{optionEmail.value}</H3>}
-              </View>
-              <View style={styles.spacerLarge} />
-              <NBText>{I18n.t("email.read.info")}</NBText>
-            </View>
-          </ScreenContent>
-          <SectionStatusComponent sectionKey={"email_validation"} />
-          <FooterWithButtons {...footerProps} />
-        </TopScreenComponent>
-      </SafeAreaView>
+      <EmailReadScreenComponent
+        handleGoBack={handleGoBack}
+        footerProps={footerProps}
+      />
     </LoadingSpinnerOverlay>
   );
 };
--- ts/screens/profile/EmailReadScreen.tsx
+++ ts/screens/profile/EmailReadScreen.tsx
@@ -5,61 +5,21 @@
  * - it is displayed during the user onboarding
  * - it is displayed after the onboarding (navigation from the profile section)
  */
-import * as O from "fp-ts/lib/Option";
-import { Text as NBText, View } from "native-base";
 import * as React from "react";
-import { Platform, SafeAreaView, StyleSheet } from "react-native";
-import { H3 } from "../../components/core/typography/H3";
+import EmailReadScreenComponent from "../../components/EmailReadScreenComponent";
 import { withValidatedEmail } from "../../components/helpers/withValidatedEmail";
-import { ContextualHelpPropsMarkdown } from "../../components/screens/BaseScreenComponent";
-import ScreenContent from "../../components/screens/ScreenContent";
-import TopScreenComponent from "../../components/screens/TopScreenComponent";
-import SectionStatusComponent from "../../components/SectionStatus";
 import { SingleButton } from "../../components/ui/BlockButtons";
-import FooterWithButtons from "../../components/ui/FooterWithButtons";
-import IconFont from "../../components/ui/IconFont";
 import I18n from "../../i18n";
 import { IOStackNavigationRouteProps } from "../../navigation/params/AppParamsList";
 import { OnboardingParamsList } from "../../navigation/params/OnboardingParamsList";
 import { navigateToEmailInsertScreen } from "../../store/actions/navigation";
-import { ReduxProps } from "../../store/actions/types";
-import { useIOSelector } from "../../store/hooks";
-import { profileEmailSelector } from "../../store/reducers/profile";
-import customVariables from "../../theme/variables";
 
-type Props = ReduxProps &
-  IOStackNavigationRouteProps<OnboardingParamsList, "READ_EMAIL_SCREEN">;
-
-const styles = StyleSheet.create({
-  flex: {
-    flex: 1
-  },
-  emailWithIcon: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center"
-  },
-  content: {
-    paddingHorizontal: customVariables.contentPadding,
-    backgroundColor: customVariables.contentBackground,
-    flex: 1
-  },
-  spacerSmall: { height: 12 },
-  spacerLarge: { height: 24 },
-  icon: {
-    marginTop: Platform.OS === "android" ? 3 : 0, // correct icon position to align it with baseline of email text
-    marginRight: 8
-  }
-});
-
-const contextualHelpMarkdown: ContextualHelpPropsMarkdown = {
-  title: "profile.data.email.contextualHelpTitle",
-  body: "profile.data.email.contextualHelpContent"
-};
+type Props = IOStackNavigationRouteProps<
+  OnboardingParamsList,
+  "READ_EMAIL_SCREEN"
+>;
 
 const EmailReadScreen = (props: Props) => {
-  const optionEmail = useIOSelector(profileEmailSelector);
-
   const handleGoBack = () => {
     props.navigation.goBack();
   };
@@ -75,34 +35,10 @@ const EmailReadScreen = (props: Props) => {
   };
 
   return (
-    <SafeAreaView style={styles.flex}>
-      <TopScreenComponent
-        goBack={handleGoBack}
-        headerTitle={I18n.t("profile.data.list.email")}
-        contextualHelpMarkdown={contextualHelpMarkdown}
-      >
-        <ScreenContent title={I18n.t("email.read.title")}>
-          <View style={styles.content}>
-            <NBText>{I18n.t("email.insert.label")}</NBText>
-            <View style={styles.spacerSmall} />
-            <View style={styles.emailWithIcon}>
-              <IconFont
-                name="io-envelope"
-                accessible={true}
-                accessibilityLabel={I18n.t("email.read.title")}
-                size={24}
-                style={styles.icon}
-              />
-              {O.isSome(optionEmail) && <H3>{optionEmail.value}</H3>}
-            </View>
-            <View style={styles.spacerLarge} />
-            <NBText>{`${I18n.t("email.read.details")}`}</NBText>
-          </View>
-        </ScreenContent>
-        <SectionStatusComponent sectionKey={"email_validation"} />
-        <FooterWithButtons {...footerProps} />
-      </TopScreenComponent>
-    </SafeAreaView>
+    <EmailReadScreenComponent
+      handleGoBack={handleGoBack}
+      footerProps={footerProps}
+    />
   );
 };
 
