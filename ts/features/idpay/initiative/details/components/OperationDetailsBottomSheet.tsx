7ac49d7fdb9e30e3fed1a1db47528a5d90dff847
Federico Mastrini <federicomastrini93@gmail.com>
2023-01-17 17:09:35 +0100

    Add `useOperationDetailsBottomSheet`

--- ts/features/idpay/initiative/details/components/OperationDetailsBottomSheet.tsx
+++ ts/features/idpay/initiative/details/components/OperationDetailsBottomSheet.tsx
@@ -1,42 +1,129 @@
-import { Text as NBText } from "native-base";
+import { Text as NBText, View as NBView } from "native-base";
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
+import { OperationTypeEnum as IbanOperationType } from "../../../../../../definitions/idpay/timeline/IbanOperationDTO";
+import { OperationTypeEnum as InstrumentOperationType } from "../../../../../../definitions/idpay/timeline/InstrumentOperationDTO";
+import { OperationTypeEnum as OnboardingOperationType } from "../../../../../../definitions/idpay/timeline/OnboardingOperationDTO";
 import { OperationListDTO } from "../../../../../../definitions/idpay/timeline/OperationListDTO";
+import { OperationTypeEnum as TransactionOperationType } from "../../../../../../definitions/idpay/timeline/TransactionOperationDTO";
 import ButtonDefaultOpacity from "../../../../../components/ButtonDefaultOpacity";
+import CopyButtonComponent from "../../../../../components/CopyButtonComponent";
+import { Body } from "../../../../../components/core/typography/Body";
+import { H4 } from "../../../../../components/core/typography/H4";
 import { IOStyles } from "../../../../../components/core/variables/IOStyles";
+import ItemSeparatorComponent from "../../../../../components/ItemSeparatorComponent";
 import I18n from "../../../../../i18n";
+import themeVariables from "../../../../../theme/variables";
+import { formatDateAsLocal } from "../../../../../utils/dates";
 import { useIOBottomSheetModal } from "../../../../../utils/hooks/bottomSheet";
 
-type Props = {
-  operation?: OperationListDTO;
-  onDismiss: () => void;
+// TODO: this mapping should be done on the backend
+const operationCircuitTypeMap: Record<string, string> = {
+  "00": "Bancomat",
+  "01": "Visa",
+  "02": "Mastercard",
+  "03": "Amex",
+  "04": "JCB",
+  "05": "UnionPay",
+  "06": "Diners",
+  "07": "PostePay",
+  "08": "BancomatPay",
+  "09": "Satispay",
+  "10": "PrivateCircuit"
 };
 
-const OperationDetailsBottomSheet = (props: Props) => {
-  const { operation, onDismiss } = props;
+type OperationDetailsProps = {
+  operation?: OperationListDTO;
+};
 
-  return (
-    <View style={IOStyles.flex}>
-      <NBText>{operation?.operationId}</NBText>
-      <ButtonDefaultOpacity block={true} bordered={true} onPress={onDismiss}>
+const OperationDetails = (props: OperationDetailsProps) => {
+  const { operation } = props;
+
+  switch (operation?.operationType) {
+    case TransactionOperationType.TRANSACTION:
+      return (
+        <View style={styles.container}>
+          <View style={styles.detailRow}>
+            <Body>Metodo di pagamento</Body>
+            <View style={styles.centerRow}>
+              <Image
+                style={styles.brandLogo}
+                source={{ uri: operation.brandLogo }}
+              />
+              <NBView hspacer={true} small={true} />
+              <Body weight="SemiBold">{operation.maskedPan}</Body>
+            </View>
+          </View>
+          <View style={styles.detailRow}>
+            <Body>Importo transazione</Body>
+            <Body weight="SemiBold">{operation.amount}</Body>
+          </View>
+          <View style={styles.detailRow}>
+            <Body>Importo rimborsabile</Body>
+            <Body weight="SemiBold">{operation.amount}</Body>
+          </View>
+          <ItemSeparatorComponent noPadded={true} />
+          <NBView spacer={true} large={true} />
+          <H4>Informazioni sulla transazione</H4>
+          <View style={styles.detailRow}>
+            <Body>Data</Body>
+            <Body weight="SemiBold">
+              {formatDateAsLocal(operation.operationDate)}
+            </Body>
+          </View>
+          <View style={styles.detailRow}>
+            <Body>Circuito di pagamento</Body>
+            <Body weight="SemiBold">
+              {operationCircuitTypeMap[operation.circuitType]}
+            </Body>
+          </View>
+          <View style={styles.detailRow}>
+            <Body>ID transazione Acquirer</Body>
+            <View style={IOStyles.row}>
+              <Body weight="SemiBold">{operation.operationId}</Body>
+              <NBView hspacer={true} small={true} />
+              <CopyButtonComponent textToCopy={operation.operationId} />
+            </View>
+          </View>
+          <View style={styles.detailRow}>
+            <Body>ID transazione Issuer</Body>
+            <View style={IOStyles.row}>
+              <Body weight="SemiBold">{operation.operationId}</Body>
+              <NBView hspacer={true} small={true} />
+              <CopyButtonComponent textToCopy={operation.operationId} />
+            </View>
+          </View>
+        </View>
+      );
+    case IbanOperationType.ADD_IBAN:
+    case InstrumentOperationType.ADD_INSTRUMENT:
+    case OnboardingOperationType.ONBOARDING:
+      return null;
+    default:
+      return null;
+  }
+};
+
+export const useOperationDetailsBottomSheet = () => {
+  const [operation, setOperation] = React.useState<OperationListDTO>();
+
+  const footer = (
+    <View style={styles.footer}>
+      <ButtonDefaultOpacity
+        block={true}
+        bordered={true}
+        onPress={() => dismiss()}
+      >
         <NBText>{I18n.t("global.buttons.close")}</NBText>
       </ButtonDefaultOpacity>
     </View>
   );
-};
-
-export const useOperationDetailsBottomSheet = () => {
-  const [operation, setOperation] = React.useState<
-    OperationListDTO | undefined
-  >(undefined);
 
   const modal = useIOBottomSheetModal(
-    <OperationDetailsBottomSheet
-      operation={operation}
-      onDismiss={() => modal.dismiss()}
-    />,
-    "Dettaglio dell'operazione",
-    200
+    <OperationDetails operation={operation} />,
+    I18n.t("idpay.initiative.operationDetails.title"),
+    530,
+    footer
   );
 
   const present = (operation: OperationListDTO) => {
@@ -44,5 +131,35 @@ export const useOperationDetailsBottomSheet = () => {
     modal.present();
   };
 
-  return { bottomSheet: modal.bottomSheet, present, dismiss: modal.dismiss };
+  const dismiss = () => {
+    modal.dismiss();
+    setOperation(undefined);
+  };
+
+  return { bottomSheet: modal.bottomSheet, present, dismiss };
 };
+
+const styles = StyleSheet.create({
+  footer: {
+    padding: themeVariables.contentPadding
+  },
+  container: {
+    flex: 1,
+    flexGrow: 1,
+    paddingVertical: themeVariables.spacerHeight
+  },
+  detailRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: themeVariables.spacerSmallHeight,
+    paddingBottom: themeVariables.spacerSmallHeight
+  },
+  centerRow: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  brandLogo: {
+    width: 24,
+    height: 16
+  }
+});
--- ts/features/idpay/initiative/details/saga/handleGetTimeline.ts
+++ ts/features/idpay/initiative/details/saga/handleGetTimeline.ts
@@ -2,6 +2,11 @@ import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 import { call, put } from "typed-redux-saga/macro";
 import { PreferredLanguageEnum } from "../../../../../../definitions/backend/PreferredLanguage";
+import { OperationTypeEnum as IbanOperationType } from "../../../../../../definitions/idpay/timeline/IbanOperationDTO";
+import { OperationTypeEnum as InstrumentOperationType } from "../../../../../../definitions/idpay/timeline/InstrumentOperationDTO";
+import { OperationTypeEnum as OnboardingOperationType } from "../../../../../../definitions/idpay/timeline/OnboardingOperationDTO";
+import { TimelineDTO } from "../../../../../../definitions/idpay/timeline/TimelineDTO";
+import { OperationTypeEnum as TransactionOperationType } from "../../../../../../definitions/idpay/timeline/TransactionOperationDTO";
 import { SagaCallReturnType } from "../../../../../types/utils";
 import { getGenericError, getNetworkError } from "../../../../../utils/errors";
 import { readablePrivacyReport } from "../../../../../utils/reporters";
@@ -11,13 +16,50 @@ import {
   IdPayInitiativeGetPayloadType
 } from "../store/actions";
 
+// TODO remove mocked data
+const mockedData: TimelineDTO = {
+  lastUpdate: new Date(),
+  operationList: [
+    {
+      operationType: TransactionOperationType.TRANSACTION,
+      operationDate: new Date(),
+      amount: 100,
+      brandLogo:
+        "https://uat.wisp2.pagopa.gov.it/wallet/assets/img/creditcard/carta_visa.png",
+      circuitType: "01",
+      maskedPan: "1234",
+      operationId: "1"
+    },
+    {
+      operationType: InstrumentOperationType.ADD_INSTRUMENT,
+      channel: "APP",
+      operationDate: new Date(),
+      brandLogo:
+        "https://uat.wisp2.pagopa.gov.it/wallet/assets/img/creditcard/carta_visa.png",
+      maskedPan: "1234",
+      operationId: "2"
+    },
+    {
+      operationType: IbanOperationType.ADD_IBAN,
+      channel: "APP",
+      operationDate: new Date(),
+      iban: "IT60X0542811101000000123456",
+      operationId: "3"
+    },
+    {
+      operationType: OnboardingOperationType.ONBOARDING,
+      operationDate: new Date(),
+      operationId: "4"
+    }
+  ]
+};
+
 /**
  * Handle the remote call to retrieve the IDPay initiative details
  * @param getTimeline
  * @param action
  * @param initiativeId
  */
-
 export function* handleGetTimeline(
   getTimeline: IDPayTimelineClient["getTimeline"],
   token: string,
@@ -43,7 +85,7 @@ export function* handleGetTimeline(
         response =>
           put(
             response.status === 200
-              ? idpayTimelineGet.success(response.value)
+              ? idpayTimelineGet.success(mockedData)
               : idpayTimelineGet.failure({
                   ...getGenericError(
                     new Error(`response status code ${response.status}`)
