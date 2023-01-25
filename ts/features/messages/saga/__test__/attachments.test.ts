3598d8cfc0da0dfd1141af21246286eaeccb81c9
Andrea Piai <andrea.piai@pagopa.it>
2023-01-25 11:21:20 +0100

    English translations and working attachment download saga tests

--- ts/features/messages/saga/__test__/attachments.test.ts
+++ ts/features/messages/saga/__test__/attachments.test.ts
@@ -1,6 +1,7 @@
 import ReactNativeBlobUtil from "react-native-blob-util";
 import { expectSaga } from "redux-saga-test-plan";
 import { cancel, fork } from "typed-redux-saga/macro";
+import I18n from "i18n-js";
 import { downloadAttachmentSaga } from "../networking/downloadAttachment";
 import { SessionToken } from "../../../../types/SessionToken";
 import { downloadAttachment } from "../../../../store/actions/messages";
@@ -38,7 +39,7 @@ describe("downloadAttachment", () => {
           .run());
     });
 
-    describe("when a download request fails", () => {
+    describe("when a download request generically fails", () => {
       const status = 404;
 
       beforeAll(() => {
@@ -50,7 +51,7 @@ describe("downloadAttachment", () => {
         }));
       });
 
-      it("then it puts a failure action with the error", () =>
+      it("then it puts a failure action with the generic error message", () =>
         expectSaga(
           downloadAttachmentSaga,
           "token" as SessionToken,
@@ -60,8 +61,36 @@ describe("downloadAttachment", () => {
             downloadAttachment.failure({
               attachment,
               error: new Error(
-                `error ${status} fetching ${attachment.resourceUrl.href}`
+                I18n.t("messageDetails.attachments.downloadFailed")
               )
+            })
+          )
+          .run());
+    });
+
+    describe("when a download request fails for a bad file format", () => {
+      const status = 415;
+
+      // eslint-disable-next-line sonarjs/no-identical-functions
+      beforeAll(() => {
+        // eslint-disable-next-line functional/immutable-data, sonarjs/no-identical-functions
+        (ReactNativeBlobUtil as any).config = jest.fn(() => ({
+          fetch: jest.fn().mockReturnValue({
+            info: jest.fn().mockReturnValue({ status })
+          })
+        }));
+      });
+
+      it("then it puts a failure action with the file format error", () =>
+        expectSaga(
+          downloadAttachmentSaga,
+          "token" as SessionToken,
+          downloadAttachment.request(attachment)
+        )
+          .put(
+            downloadAttachment.failure({
+              attachment,
+              error: new Error(I18n.t("messageDetails.attachments.badFormat"))
             })
           )
           .run());
