993a9e4566d46c80f1a8a6b65d59258c7f85087f
Damiano Plebani <damiano.plebani@pagopa.it>
2023-01-27 16:16:17 +0100

    Add label to the `CustomBadge` component in the DS

--- ts/features/design-system/core/DSBadges.tsx
+++ ts/features/design-system/core/DSBadges.tsx
@@ -3,6 +3,7 @@ import { View, StyleSheet } from "react-native";
 import { IOBadge } from "../../../components/core/IOBadge";
 import { VSpacer, HSpacer } from "../../../components/core/spacer/Spacer";
 import { H2 } from "../../../components/core/typography/H2";
+import { H4 } from "../../../components/core/typography/H4";
 import { IOColors } from "../../../components/core/variables/IOColors";
 import { IOStyles } from "../../../components/core/variables/IOStyles";
 import CustomBadge from "../../../components/ui/CustomBadge";
@@ -43,6 +44,10 @@ export const DSBadges = () => (
     <VSpacer size={40} />
 
     <H2>Notifications</H2>
+    <VSpacer size={16} />
+    <H4 weight="SemiBold" color="bluegreyDark">
+      CustomBadge
+    </H4>
     <VSpacer size={16} />
     <View style={IOStyles.row}>
       <View style={styles.fakeNavItem}>
