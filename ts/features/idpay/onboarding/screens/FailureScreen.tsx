6399663821261d2eda956fc8b8b9d212be87c270
Federico Mastrini <federicomastrini93@gmail.com>
2023-02-01 18:40:24 +0100

    Use selecor in failure screen

--- ts/features/idpay/onboarding/screens/FailureScreen.tsx
+++ ts/features/idpay/onboarding/screens/FailureScreen.tsx
@@ -1,27 +1,61 @@
-import { useActor } from "@xstate/react";
+import * as O from "fp-ts/lib/Option";
+import { useSelector } from "@xstate/react";
 import { Text, View } from "native-base";
-import React from "react";
+import * as React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import ButtonDefaultOpacity from "../../../../components/ButtonDefaultOpacity";
+import {
+  IOPictogramType,
+  Pictogram
+} from "../../../../components/core/pictograms";
+import { VSpacer } from "../../../../components/core/spacer/Spacer";
+import { H3 } from "../../../../components/core/typography/H3";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
+import I18n from "../../../../i18n";
 import themeVariables from "../../../../theme/variables";
-import FailureMessage from "../components/FailureMessage";
+import { OnboardingFailureType } from "../xstate/failure";
 import { useOnboardingMachineService } from "../xstate/provider";
+import { failureSelector } from "../xstate/selectors";
+
+const failurePictures: Record<OnboardingFailureType, IOPictogramType> = {
+  [OnboardingFailureType.GENERIC]: "umbrella",
+  [OnboardingFailureType.NO_PERMISSION]: "question",
+  [OnboardingFailureType.ALREADY_COMPLETED]: "inProgress",
+  [OnboardingFailureType.ONBOARDING_KO]: "question"
+};
+
+type FailureMessageProps = {
+  failure: OnboardingFailureType;
+};
+
+const FailureMessageComponent = (props: FailureMessageProps) => (
+  <>
+    <Pictogram name={failurePictures[props.failure]} size={80} />
+    <VSpacer size={16} />
+    <H3 style={styles.title}>
+      {I18n.t(`idpay.onboarding.failure.${props.failure}.title`)}
+    </H3>
+    <VSpacer size={16} />
+    <Text alignCenter={true}>
+      {I18n.t(`idpay.onboarding.failure.${props.failure}.subtitle`)}
+    </Text>
+  </>
+);
 
 const FailureScreen = () => {
-  const onboardingMachineService = useOnboardingMachineService();
-  const [state, send] = useActor(onboardingMachineService);
+  const machine = useOnboardingMachineService();
+  const failure = useSelector(machine, failureSelector);
 
   const handleClosePress = () => {
-    send({ type: "QUIT_ONBOARDING" });
+    machine.send({ type: "QUIT_ONBOARDING" });
   };
-
-  const failureType = state.context.failure;
 
   return (
     <SafeAreaView style={IOStyles.flex}>
       <View style={styles.errorContainer}>
-        {failureType && <FailureMessage type={failureType} />}
+        {O.isSome(failure) && (
+          <FailureMessageComponent failure={failure.value} />
+        )}
       </View>
       <View style={styles.buttonContainer}>
         <ButtonDefaultOpacity
@@ -47,6 +81,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: themeVariables.contentPadding
+  },
+  title: {
+    textAlign: "center"
   }
 });
 
--- ts/features/idpay/onboarding/xstate/machine.ts
+++ ts/features/idpay/onboarding/xstate/machine.ts
@@ -16,7 +16,11 @@ export type Context = {
   initiative?: InitiativeDto;
   initiativeStatus?: StatusEnum;
   requiredCriteria?: O.Option<RequiredCriteriaDTO>;
-  failure?: OnboardingFailureType;
+  failure: O.Option<OnboardingFailureType>;
+};
+
+const INITIAL_CONTEXT: Context = {
+  failure: O.none
 };
 
 // Events types
@@ -94,7 +98,7 @@ const createIDPayOnboardingMachine = () =>
   /** @xstate-layout N4IgpgJg5mDOIC5QEkAiAFAggTQPoHkA5AIX0wCVVlCBxAOgHVNkAVam3a15TNgNQCiuAMoCAMgIDCbIgGJRE6Z0LdeyQQG0ADAF1EoAA4B7WAEsALqaMA7fSACeiAEwAWLQBoQAD2cB2AMx0vgCs-v6+TgCMAJxO-gAcTvEAvsmeaFh4RKQUVLR0YmR5HFxsaoKyEDZgdKbWAG5GANY1ADZGAIYQyNYWph2W9WDaekggxmaWNnaOCP7B8UGR8aFaAGzBLtFaEZ4+CE6+i+Errkdah-4uLqnpGDgEJGSU7HRUwuhiOOzKqvwCskwkkkAnQLFwLHwwhGdgmfWmY32kSOdC0LiOLic22iwV8WminlmuN8dEiLmROM2WmC0RCtxAGQe2WexToQJBYJ+kOElWqtQazRqHQAxsKwAZzCwTDCxnCprZEYgwotljF4kdIsFlmjCYg8cdMS55gtIpEtP5IvTGVknrlXoVMMVcOQBABFACqyBdqFwknIrAE-swvOsNTqjRadHaXXIYAAjgBXUwAJ0gkmTFjAGY6MsMJnhCtAs3VWjoawCvhcazN2y08Xie0QLmCTjoLfi0X86zWbjWTmCqTSIGsRggcDs1seORe+SY3FovzK-xE4ikMkIsPz8rs+w8Dmcbit9xt09ZDqdpR4-03kyshe8iD3s3mi18y1WGy2OycR8yU5ZrzvJ83wLpe5QCDeBY7o+uoIMEaJ0OqazRJEaz1qEvhrL+TK2jO9DsqCbALtykHboqCBPogyFrIhmEoWhKzhFhQ6Tsydr5OePwuh6XoCD6foBkGpF3tBFGwSWZYVlWNZaHWKQsce-7sfQAh8JgYjumoC7cZ63q+v6LCBjwwkIqAu6wWaayBOaVYrJEVz1r4vjYSeAG0CZ977AAtC4Db7ggmqRKiUTqniWj2aEcSDskQA */
   createMachine(
     {
-      context: {},
+      context: INITIAL_CONTEXT,
       tsTypes: {} as import("./machine.typegen").Typegen0,
       schema: {
         context: {} as Context,
@@ -293,7 +297,7 @@ const createIDPayOnboardingMachine = () =>
           requiredCriteria: event.data
         })),
         setFailure: assign((_, event) => ({
-          failure: event.data as OnboardingFailureType
+          failure: O.some(event.data as OnboardingFailureType)
         }))
       },
       guards: {
