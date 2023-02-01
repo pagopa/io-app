32056a6bb58ed508382d79cac02e1860b0ba3477
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-02-01 13:57:30 +0100

    Add android setup
    WIP: currently not working

--- ts/__owl__/login.owl.ts
+++ ts/__owl__/login.owl.ts
@@ -1,9 +1,15 @@
+import { exec } from "child_process";
+import { Platform } from "react-native";
 import { takeScreenshot } from "react-native-owl";
 import { waitForLoginScreen } from "./utils";
 
 describe("Login screen", () => {
   it("Check screen UI", async () => {
     await waitForLoginScreen();
+    // https://github.com/FormidableLabs/react-native-owl/issues/129
+    if (Platform.OS === "android") {
+      exec('adb shell input text "mock\\ text"');
+    }
     const screen = await takeScreenshot("login-screen");
     expect(screen).toMatchBaseline();
   });
