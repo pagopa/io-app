76abcdf2fca776eadccf05388e8b24e3cb672ac1
Andrea <andrea.piai@pagopa.it>
2023-02-02 10:13:37 +0100

    feat: [IOAPPCOM-44, IOAPPCOM-47, IOAPPCOM-49, IOAPPCOM-52] Support for attachments on standard messages (#4320)
    ## Short description
    This PR adds support for PDF attachments on standard messages (which are
    not medical prescriptions).
    
    ## List of changes proposed in this pull request
    In order not to break the existing PN flow (since it slightly differs
    from generic attachments one), the following changes have been made:
    - In-app model of the attachment has a category, to distinguish it
    between generic, PN and MVL attachment (even though the last should be
    removed in the future)
    - Standard message detail component retrieves the third-party message
    list of attachments (if there is one) upon first loading, while
    rendering the markdown. It then makes sure to render the list only after
    the markdown has been loaded.
    - Reusable PN hook for showing the bottom sheet has been edited in order
    not to start the attachment download automatically, but just notifying
    the calling component of a single attachment request from the user
    (after having shown the alert on the bottom sheet - if the preference
    allows it)
    - The Attachment preview component downloads the attachment if it is a
    generic one (and has to be downloaded) and handles the proper loading or
    errors
    
    This PR does not include mixpanel events, which will be added in a later
    one
    
    ## How to test
    You can either use the dev-server (`withRemoteAttachments` setting in
    the `config`) or use the test credentials to try it out with the
    `io-backend` (contact me or the red unit to retrieve them).
    While medical prescription cannot be tested, you can check that standard
    (non third-party messages) still work (they do not have attachments),
    that PN messages with and without attachments still work and that
    third-party messages with attachment are now correctly supported.
    Make also sure to test the three main download errors (a failing
    download, an invalid PDF and a corrupted one) as well as cancelling a
    (generic third-party attachment) download ([reference FIGMA
    here](https://www.figma.com/file/cXzK7qaiagRil5vZy6EqQS/Messaggi-con-allegati_Errori?node-id=0%3A1&t=RB2fB8pcBZAdxofm-0)).

--- ts/features/messages/saga/__test__/attachments.test.ts
+++ ts/features/messages/saga/__test__/attachments.test.ts
@@ -1,12 +1,18 @@
 import ReactNativeBlobUtil from "react-native-blob-util";
 import { expectSaga } from "redux-saga-test-plan";
-import { cancel, fork } from "typed-redux-saga/macro";
+import I18n from "../../../../i18n";
 import { downloadAttachmentSaga } from "../networking/downloadAttachment";
 import { SessionToken } from "../../../../types/SessionToken";
 import { downloadAttachment } from "../../../../store/actions/messages";
 import { mockPdfAttachment } from "../../../../__mocks__/attachment";
+import { ServiceId } from "../../../../../definitions/backend/ServiceId";
 
 const savePath = "/tmp/attachment.pdf";
+const serviceId = "service0000001" as ServiceId;
+
+jest.mock("../../../../store/reducers/entities/messages/paginatedById", () => ({
+  getServiceByMessageId: jest.fn().mockReturnValue(serviceId)
+}));
 
 describe("downloadAttachment", () => {
   describe("given an attachment", () => {
@@ -38,7 +44,7 @@ describe("downloadAttachment", () => {
           .run());
     });
 
-    describe("when a download request fails", () => {
+    describe("when a download request generically fails", () => {
       const status = 404;
 
       beforeAll(() => {
@@ -50,7 +56,7 @@ describe("downloadAttachment", () => {
         }));
       });
 
-      it("then it puts a failure action with the error", () =>
+      it("then it puts a failure action with the generic error message", () =>
         expectSaga(
           downloadAttachmentSaga,
           "token" as SessionToken,
@@ -60,36 +66,39 @@ describe("downloadAttachment", () => {
             downloadAttachment.failure({
               attachment,
               error: new Error(
-                `error ${status} fetching ${attachment.resourceUrl.href}`
+                I18n.t("messageDetails.attachments.downloadFailed")
               )
             })
           )
           .run());
     });
 
-    describe("when the saga gets cancelled", () => {
+    describe("when a download request fails for a bad file format", () => {
+      const status = 415;
+
       // eslint-disable-next-line sonarjs/no-identical-functions
       beforeAll(() => {
-        // eslint-disable-next-line functional/immutable-data,sonarjs/no-identical-functions
+        // eslint-disable-next-line functional/immutable-data, sonarjs/no-identical-functions
         (ReactNativeBlobUtil as any).config = jest.fn(() => ({
           fetch: jest.fn().mockReturnValue({
-            info: jest.fn().mockReturnValue({ status: 200 }),
-            path: jest.fn().mockReturnValue(savePath)
+            info: jest.fn().mockReturnValue({ status })
           })
         }));
       });
 
-      function* saga() {
-        const task = yield* fork(
+      it("then it puts a failure action with the file format error", () =>
+        expectSaga(
           downloadAttachmentSaga,
           "token" as SessionToken,
           downloadAttachment.request(attachment)
-        );
-        yield* cancel(task);
-      }
-
-      it("then it puts a cancel action", () =>
-        expectSaga(saga).put(downloadAttachment.cancel(attachment)).run());
+        )
+          .put(
+            downloadAttachment.failure({
+              attachment,
+              error: new Error(I18n.t("messageDetails.attachments.badFormat"))
+            })
+          )
+          .run());
     });
   });
 });
