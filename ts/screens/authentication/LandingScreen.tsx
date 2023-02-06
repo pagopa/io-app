923f8cc1a8e8d043e36bd52c0726848feddad7b2
SabPicariello <114305019+SabPicariello@users.noreply.github.com>
2023-02-06 15:00:06 +0100

    feat: [IA-362,IA-363,IA-364,IA-319] -- Carousel accessibility (#4345)
    ## Short description
    Fixed carousel accessibility. Voice over now reads the slides.
    
    ## List of changes proposed in this pull request
    - [locales/en/index.yml,locales/it/index.yml]: added an indication to
    the correct place of the help button, for the first carousel slide.
    - ts/components/LandingCardComponent.tsx: added accessibility support to
    LandingCardComponent.
    - ts/screens/authentication/LandingScreen.tsx: added accessibility
    support to every carousel slide. The first one, describes the component
    as well.
    - ts/components/HorizontalScroll.tsx: removed specific carousel
    accessibility description in this generic component.
    
    ## How to test
    iOS:
    Run app, VoiceOver, and verify if every slide of the carousel is read.
    Android:
    Run app, TalkBack, and verify if every slide of the carousel is read.
    
    ---------
    
    Co-authored-by: Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>

--- ts/screens/authentication/LandingScreen.tsx
+++ ts/screens/authentication/LandingScreen.tsx
@@ -77,25 +77,42 @@ const getCards = (
     id: 5,
     image: require("../../../img/landing/05.png"),
     title: I18n.t("authentication.landing.card5-title"),
-    content: I18n.t("authentication.landing.card5-content")
+    content: I18n.t("authentication.landing.card5-content"),
+    accessibilityLabel: `${I18n.t(
+      "authentication.landing.accessibility.carousel.label"
+    )}. ${I18n.t("authentication.landing.card5-title")}. ${I18n.t(
+      "authentication.landing.card5-content-accessibility"
+    )}`,
+    accessibilityHint: I18n.t(
+      "authentication.landing.accessibility.carousel.hint"
+    )
   },
   {
     id: 1,
     image: require("../../../img/landing/01.png"),
     title: I18n.t("authentication.landing.card1-title"),
-    content: I18n.t("authentication.landing.card1-content")
+    content: I18n.t("authentication.landing.card1-content"),
+    accessibilityLabel: `${I18n.t(
+      "authentication.landing.card1-title"
+    )}. ${I18n.t("authentication.landing.card1-content")}`
   },
   {
     id: 2,
     image: require("../../../img/landing/02.png"),
     title: I18n.t("authentication.landing.card2-title"),
-    content: I18n.t("authentication.landing.card2-content")
+    content: I18n.t("authentication.landing.card2-content"),
+    accessibilityLabel: `${I18n.t(
+      "authentication.landing.card2-title"
+    )}. ${I18n.t("authentication.landing.card2-content")}`
   },
   {
     id: 3,
     image: require("../../../img/landing/03.png"),
     title: I18n.t("authentication.landing.card3-title"),
-    content: I18n.t("authentication.landing.card3-content")
+    content: I18n.t("authentication.landing.card3-content"),
+    accessibilityLabel: `${I18n.t(
+      "authentication.landing.card3-title"
+    )}. ${I18n.t("authentication.landing.card3-content")}`
   },
   {
     id: 4,
@@ -107,7 +124,10 @@ const getCards = (
       : I18n.t("authentication.landing.card4-title"),
     content: isCIEAvailable
       ? I18n.t("authentication.landing.loginSpidCieContent")
-      : I18n.t("authentication.landing.card4-content")
+      : I18n.t("authentication.landing.card4-content"),
+    accessibilityLabel: `${I18n.t(
+      "authentication.landing.card4-title"
+    )}. ${I18n.t("authentication.landing.card4-content")}`
   }
 ];
 
