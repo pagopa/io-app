e5918b57d6ca29afc5b477feb78ee089715e5f89
forrest57 <tomba808@gmail.com>
2023-02-06 10:53:56 +0100

    updated` xstate ` and ` @xstate/cli `

--- ts/features/idpay/onboarding/xstate/__tests__/machine.test.ts
+++ ts/features/idpay/onboarding/xstate/__tests__/machine.test.ts
@@ -205,7 +205,8 @@ describe("machine", () => {
     await waitFor(() =>
       expect(mockNavigateToMultiSelfDeclarationsScreen).toHaveBeenCalled()
     );
-    onboardingService.send("SELECT_MULTI_CONSENT", {
+    onboardingService.send({
+      type: "SELECT_MULTI_CONSENT",
       data: T_SELF_CONSENT_MULTI
     });
 
