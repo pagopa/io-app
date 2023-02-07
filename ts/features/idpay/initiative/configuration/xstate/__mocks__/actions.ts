13956e5e1af43e1a1d46299cdfa65376ef25be56
Federico Mastrini <federicomastrini93@gmail.com>
2023-02-07 12:30:49 +0100

    Simplify mocked actions

--- ts/features/idpay/initiative/configuration/xstate/__mocks__/actions.ts
+++ ts/features/idpay/initiative/configuration/xstate/__mocks__/actions.ts
@@ -1,38 +1,12 @@
-import { createActionsImplementation } from "../actions";
-
-export const mockNavigateToConfigurationIntro = jest.fn();
-
-export const mockNavigateToIbanEnrollmentScreen = jest.fn();
-
-export const mockNavigateToIbanLandingScreen = jest.fn();
-
-export const mockNavigateToIbanOnboardingScreen = jest.fn();
-
-export const mockNavigateToInstrumentsEnrollmentScreen = jest.fn();
-
-export const mockNavigateToConfigurationSuccessScreen = jest.fn();
-
-export const mockNavigateToInitiativeDetailScreen = jest.fn();
-
-export const mockExitConfiguration = jest.fn();
-
-export const mockNavigateToAddPaymentMethodScreen = jest.fn();
-
-export const mockShowFailureToast = jest.fn();
-
-export type MockActionsType = ReturnType<typeof createActionsImplementation>;
-
-export const mockActions: MockActionsType = {
-  navigateToConfigurationIntro: mockNavigateToConfigurationIntro,
-  navigateToIbanOnboardingScreen: mockNavigateToIbanOnboardingScreen,
-  exitConfiguration: mockExitConfiguration,
-  navigateToConfigurationSuccessScreen:
-    mockNavigateToConfigurationSuccessScreen,
-  navigateToIbanEnrollmentScreen: mockNavigateToIbanEnrollmentScreen,
-  navigateToIbanLandingScreen: mockNavigateToIbanLandingScreen,
-  navigateToInitiativeDetailScreen: mockNavigateToInitiativeDetailScreen,
-  navigateToInstrumentsEnrollmentScreen:
-    mockNavigateToInstrumentsEnrollmentScreen,
-  navigateToAddPaymentMethodScreen: mockNavigateToAddPaymentMethodScreen,
-  showFailureToast: mockShowFailureToast
+export const mockActions = {
+  navigateToConfigurationIntro: jest.fn(),
+  navigateToIbanOnboardingScreen: jest.fn(),
+  exitConfiguration: jest.fn(),
+  navigateToConfigurationSuccessScreen: jest.fn(),
+  navigateToIbanEnrollmentScreen: jest.fn(),
+  navigateToIbanLandingScreen: jest.fn(),
+  navigateToInitiativeDetailScreen: jest.fn(),
+  navigateToInstrumentsEnrollmentScreen: jest.fn(),
+  navigateToAddPaymentMethodScreen: jest.fn(),
+  showFailureToast: jest.fn()
 };
