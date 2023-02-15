739a09ccc4cd06e4c5f1ffdd4c194c36568db0e1
Federico Mastrini <federicomastrini93@gmail.com>
2023-02-15 09:12:48 +0100

    fix: snap point for refund operations

--- ts/features/idpay/initiative/details/components/TimelineDetailsBottomSheet.tsx
+++ ts/features/idpay/initiative/details/components/TimelineDetailsBottomSheet.tsx
@@ -82,7 +82,7 @@ const bottomSheetConfigurations: Record<
   TimelineDetailsBottomSheetConfiguration
 > = {
   PAID_REFUND: {
-    snapPoint: 390,
+    snapPoint: 420,
     title: I18n.t("idpay.initiative.operationDetails.title.refund")
   },
   REJECTED_REFUND: {
