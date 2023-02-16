5d854cba60592f6223e1e6f7b2695af60662adfc
Mario Perrotta <mario.perrotta@pagopa.it>
2023-02-16 16:29:22 +0100

    refactor: add retry prop and update copy

--- ts/features/fci/components/GenericErrorComponent.tsx
+++ ts/features/fci/components/GenericErrorComponent.tsx
@@ -1,19 +1,23 @@
 import * as React from "react";
 import genericError from "../../../../img/wallet/errors/generic-error-icon.png";
-import I18n from "../../../i18n";
+import { WithTestID } from "../../../types/WithTestID";
 import ErrorComponent from "./ErrorComponent";
 
-type Props = {
+type Props = WithTestID<{
   onPress: () => void;
-};
+  title: string;
+  subTitle: string;
+  retry?: boolean;
+}>;
 
 const GenericErrorComponent = (props: Props) => (
   <ErrorComponent
-    title={I18n.t("features.fci.errors.generic.title")}
-    subTitle={I18n.t("features.fci.errors.generic.subTitle")}
+    title={props.title}
+    subTitle={props.subTitle}
     image={genericError}
-    testID={"GenericErrorComponentTestID"}
+    testID={props.testID}
     onPress={props.onPress}
+    retry={props.retry}
   />
 );
 
--- ts/features/fci/components/__tests__/ErrorComponent.test.tsx
+++ ts/features/fci/components/__tests__/ErrorComponent.test.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import configureMockStore from "redux-mock-store";
 import { constNull } from "fp-ts/lib/function";
 import ErrorComponent from "../ErrorComponent";
+import I18n from "../../../../i18n";
 import { appReducer } from "../../../../store/reducers";
 import { applicationChangeState } from "../../../../store/actions/application";
 import { GlobalState } from "../../../../store/reducers/types";
@@ -42,7 +43,7 @@ describe("Test ErrorComponent", () => {
     expect(component.queryByText(props.title)).toBeTruthy();
     expect(component.queryByText(props.subTitle)).toBeTruthy();
   });
-  it("with all props should render a FootWithButtons", () => {
+  it("with all props should render a FootWithButtons with close button", () => {
     const props = {
       title: "title",
       subTitle: "subTitle",
@@ -50,8 +51,24 @@ describe("Test ErrorComponent", () => {
     };
     const component = renderComponent({ ...props });
     expect(component.queryByTestId(fakeTestID)).toBeTruthy();
-    const leftCloseButton = component.queryByTestId("closeButton");
+    const leftCloseButton = component.queryByText(
+      I18n.t("global.buttons.close")
+    );
     expect(leftCloseButton).not.toBeNull();
+  });
+  it("with all props should render a FootWithButtons with retry button", () => {
+    const props = {
+      title: "title",
+      subTitle: "subTitle",
+      image: 1,
+      retry: true
+    };
+    const component = renderComponent({ ...props });
+    expect(component.queryByTestId(fakeTestID)).toBeTruthy();
+    const leftRetryButton = component.queryByText(
+      I18n.t("global.buttons.retry")
+    );
+    expect(leftRetryButton).not.toBeNull();
   });
 });
 
