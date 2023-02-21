29e8af7f1eb17d2255f317bcc980ca5e7dee982c
Damiano Plebani <damiano.plebani@pagopa.it>
2023-02-21 18:00:04 +0100

    Update status-related icons used in the `Alert` component

--- ts/components/core/icons/svg/IconInfoFilled.tsx
+++ ts/components/core/icons/svg/IconInfoFilled.tsx
@@ -5,7 +5,9 @@ import { SVGIconProps } from "../Icon";
 const IconInfoFilled = ({ size, style }: SVGIconProps) => (
   <Svg width={size} height={size} viewBox="0 0 24 24" style={style}>
     <Path
-      d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0Zm1.028 17.96a1.028 1.028 0 1 1-2.056 0V9.954a1.028 1.028 0 1 1 2.056 0v8.004ZM12 7.07a1.028 1.028 0 1 1 0-2.057 1.028 1.028 0 0 1 0 2.056Z"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0Zm.028 7.556a1.528 1.528 0 1 0 0-3.056 1.528 1.528 0 0 0 0 3.056Zm0 1.944c.568 0 1.029.46 1.029 1.028v8.005a1.028 1.028 0 0 1-2.057 0v-8.005c0-.568.46-1.028 1.029-1.028Z"
       fill="currentColor"
     />
   </Svg>
