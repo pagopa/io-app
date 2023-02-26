d43298a67374473714d2ea16f51833332257eea6
Mario Perrotta <mario.perrotta@pagopa.it>
2023-02-26 20:35:04 +0100

    chore: remove resolver module

--- babel.config.js
+++ babel.config.js
@@ -6,17 +6,7 @@ module.exports = {
       {
         globals: ["__scanCodes"]
       }
-    ],
-    [
-           'module-resolver',
-           {
-             alias: {
-               'crypto': 'react-native-quick-crypto',
-               'stream': 'stream-browserify',
-               'buffer': '@craftzdog/react-native-buffer',
-             },
-           },
-         ]
+    ]
   ],
   presets: ["module:metro-react-native-babel-preset"]
 };
