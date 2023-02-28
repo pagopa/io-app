45694070df154012d3ab52e5b9ff39c1a4f96098
SabPicariello <114305019+SabPicariello@users.noreply.github.com>
2023-02-28 10:02:27 +0100

    fix: [IABT-1433] Unwanted tab-bar shadow Android (#4387)
    ## Short description
    Messages and services tab: 
    
    - Set elevation to 0 for the top tab-bar component style. 
    - Added a top border to each component shown by a top tab-bar section.
    
    ## List of changes proposed in this pull request
    - [ts/navigation/MessagesHomeTabNavigator.tsx,
    ts/navigation/ServicesHomeTabNavigator.tsx]: Set elevation to 0
    - [ts/components/messages/MessagesArchive.tsx,
    ts/components/messages/MessagesInbox.tsx,
    ts/components/services/ServicesSectionsList.tsx,
    ts/screens/services/ServicesLocalScreen.tsx]: Added a top border to
    display a divider.
    
    ## How to test
    Android: Verify that the unwanted shadow has disappeared.
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>
    Co-authored-by: Andrea <andrea.piai@pagopa.it>
    Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>

--- ts/components/messages/MessagesArchive.tsx
+++ ts/components/messages/MessagesArchive.tsx
@@ -9,6 +9,7 @@ import { UIMessage } from "../../store/reducers/entities/messages/types";
 
 import { useItemsSelection } from "../../utils/hooks/useItemsSelection";
 import ListSelectionBar from "../ListSelectionBar";
+import { IOStyles } from "../core/variables/IOStyles";
 import { EmptyListComponent } from "./EmptyListComponent";
 import MessageList from "./MessageList";
 
@@ -72,7 +73,7 @@ const MessagesArchive = ({
   );
 
   return (
-    <View style={styles.listWrapper}>
+    <View style={[styles.listWrapper, IOStyles.topListBorderBelowTabsStyle]}>
       <View style={styles.listContainer}>
         <MessageList
           filter={{ getArchived: true }}
