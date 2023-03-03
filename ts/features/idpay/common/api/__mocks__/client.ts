5c4b9e17c7eccdd5e8355b26151f1fec6b9dcafe
Federico Mastrini <federicomastrini93@gmail.com>
2023-03-03 10:01:55 +0100

    fix: tests mocks

---
+++ ts/features/idpay/common/api/__mocks__/client.ts
@@ -0,0 +1,19 @@
+export const mockIDPayClient = {
+  getInitiativeData: jest.fn(),
+  checkPrerequisites: jest.fn(),
+  consentOnboarding: jest.fn(),
+  onboardingCitizen: jest.fn(),
+  onboardingStatus: jest.fn(),
+  deleteInstrument: jest.fn(),
+  enrollIban: jest.fn(),
+  enrollInstrument: jest.fn(),
+  getIban: jest.fn(),
+  getIbanList: jest.fn(),
+  getInstrumentList: jest.fn(),
+  getTimeline: jest.fn(),
+  getTimelineDetail: jest.fn(),
+  getWallet: jest.fn(),
+  getWalletDetail: jest.fn(),
+  getWalletStatus: jest.fn(),
+  unsubscribe: jest.fn()
+};
