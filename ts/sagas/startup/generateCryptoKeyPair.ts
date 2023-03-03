287e730913a3c15ceb4bdbbc6946e5b9c549c920
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-03-03 15:43:59 +0100

    Add missing yield

--- ts/sagas/startup/generateCryptoKeyPair.ts
+++ ts/sagas/startup/generateCryptoKeyPair.ts
@@ -77,7 +77,7 @@ function* deleteCryptoKeyPair(keyTag: string) {
 function* generateCryptoKeyPair(keyTag: string) {
   try {
     // Remove an already existing key with the same tag.
-    call(deleteCryptoKeyPair, keyTag);
+    yield* call(deleteCryptoKeyPair, keyTag);
 
     const key = yield* call(generate, keyTag);
     const keyGenerationInfo: KeyGenerationInfo = {
