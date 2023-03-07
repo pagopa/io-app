6a8865988cc02a2c4ecd65d6c85854db616683db
Mario Perrotta <mario.perrotta@pagopa.it>
2023-03-07 17:27:35 +0100

    chore(Firma con IO): [SFEQS-1452] Update signing saga to use Lollipop (#4410)
    This PR adds utils to generate tos and signature challenges and updates the handleCreateSignature to use fetchLollipop.
    ---------
    Co-authored-by: Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
    Co-authored-by: Sabino Picariello <sabino.picariello@pagopa.it>

--- ts/features/fci/saga/networking/handleCreateSignature.ts
+++ ts/features/fci/saga/networking/handleCreateSignature.ts
@@ -1,11 +1,16 @@
 import { SagaIterator } from "redux-saga";
-import { call, put } from "typed-redux-saga/macro";
+import { call, put, select } from "typed-redux-saga/macro";
 import { ActionType } from "typesafe-actions";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as E from "fp-ts/lib/Either";
 import { readablePrivacyReport } from "../../../../utils/reporters";
 import { BackendFciClient } from "../../api/backendFci";
 import { fciSigningRequest } from "../../store/actions";
 import { getNetworkError } from "../../../../utils/errors";
+import { LollipopConfig } from "../../../lollipop";
+import { getCustomSignature, getTosSignature } from "../../utils/signature";
+import { fciDocumentsWithUrlSelector } from "../../store/reducers/fciSignatureRequest";
+import { fciQtspFilledDocumentUrlSelector } from "../../store/reducers/fciQtspFilledDocument";
 
 /*
  * A saga to post signature data.
@@ -15,7 +20,45 @@ export function* handleCreateSignature(
   action: ActionType<typeof fciSigningRequest["request"]>
 ): SagaIterator {
   try {
-    const postSignatureResponse = yield* call(postSignature, {
+    const qtspFilledDocumentUrl = yield* select(
+      fciQtspFilledDocumentUrlSelector
+    );
+    const qtspClauses = {
+      ...action.payload.qtsp_clauses,
+      filled_document_url: qtspFilledDocumentUrl as NonEmptyString
+    };
+
+    const tosChallange = yield* call(getTosSignature(qtspClauses));
+
+    if (E.isLeft(tosChallange)) {
+      throw Error(`${tosChallange.left}`);
+    }
+
+    const tosChallengeHashHex = E.isRight(tosChallange)
+      ? tosChallange.right
+      : "";
+
+    const documentSignatures = yield* select(
+      fciDocumentsWithUrlSelector(action.payload.documents_to_sign)
+    );
+
+    const signChallenge = yield* call(getCustomSignature(documentSignatures));
+    if (E.isLeft(signChallenge)) {
+      throw Error(`${signChallenge.left}`);
+    }
+    const signChallengeHashHex = E.isRight(signChallenge)
+      ? signChallenge.right
+      : "";
+
+    const lollipopConfig: LollipopConfig = {
+      nonce: action.payload.qtsp_clauses.nonce,
+      customContentToSign: {
+        "tos-challenge": tosChallengeHashHex,
+        "sign-challenge": signChallengeHashHex
+      }
+    };
+
+    const postSignatureResponse = yield* call(postSignature(lollipopConfig), {
       signatureToCreate: action.payload
     });
 
