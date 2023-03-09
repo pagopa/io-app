be00b2feb987fb8a5261349fdeb974f679a946de
Damiano Plebani <damiano.plebani@pagopa.it>
2023-03-09 17:17:38 +0100

    Update H1 typographic style

--- ts/components/core/typography/H1.tsx
+++ ts/components/core/typography/H1.tsx
@@ -1,25 +1,34 @@
 import * as React from "react";
+import { isDesignSystemEnabledSelector } from "../../../store/reducers/persistedPreferences";
 import { IOFontFamily, IOFontWeight } from "../fonts";
 import { IOColors, IOColorsStatusForeground } from "../variables/IOColors";
+import { useIOSelector } from "../../../store/hooks";
 import { ExternalTypographyProps, TypographyProps } from "./common";
 import { useTypographyFactory } from "./Factory";
 
 type PartialAllowedColors = Extract<
   IOColors,
-  "bluegreyDark" | "white" | "blue"
+  "bluegreyDark" | "white" | "blue" | "black"
 >;
 type AllowedColors = PartialAllowedColors | IOColorsStatusForeground;
-type AllowedWeight = Extract<IOFontWeight, "Bold">;
+type AllowedWeight = Extract<IOFontWeight, "Bold" | "Regular">;
 
 type OwnProps = ExternalTypographyProps<
   TypographyProps<AllowedWeight, AllowedColors>
 >;
 
+/* Legacy typograhic style */
+const legacyFontName: IOFontFamily = "TitilliumWeb";
+export const h1LegacyFontSize = 28;
+export const h1LegacyLineHeight = 32;
+export const h1LegacyDefaultColor: AllowedColors = "bluegreyDark";
+export const h1LegacyDefaultWeight: AllowedWeight = "Bold";
+/* New typograhic style */
 const fontName: IOFontFamily = "ReadexPro";
 export const h1FontSize = 28;
 export const h1LineHeight = 32;
-export const h1DefaultColor: AllowedColors = "bluegreyDark";
-export const h1DefaultWeight: AllowedWeight = "Bold";
+export const h1DefaultColor: AllowedColors = "black";
+export const h1DefaultWeight: AllowedWeight = "Regular";
 
 /**
  * Typography component to render H1 text with font size {@link fontSize} and fontFamily {@link fontName}.
@@ -27,11 +36,27 @@ export const h1DefaultWeight: AllowedWeight = "Bold";
  * @param props
  * @constructor
  */
-export const H1: React.FunctionComponent<OwnProps> = props =>
-  useTypographyFactory<AllowedWeight, AllowedColors>({
-    ...props,
-    defaultWeight: h1DefaultWeight,
-    defaultColor: h1DefaultColor,
-    font: fontName,
-    fontStyle: { fontSize: h1FontSize, lineHeight: h1LineHeight }
-  });
+export const H1: React.FunctionComponent<OwnProps> = props => {
+  const isDesignSystemEnabled = useIOSelector(isDesignSystemEnabledSelector);
+
+  return useTypographyFactory<AllowedWeight, AllowedColors>(
+    isDesignSystemEnabled
+      ? {
+          ...props,
+          defaultWeight: h1DefaultWeight,
+          defaultColor: h1DefaultColor,
+          font: fontName,
+          fontStyle: { fontSize: h1FontSize, lineHeight: h1LineHeight }
+        }
+      : {
+          ...props,
+          defaultWeight: h1LegacyDefaultWeight,
+          defaultColor: h1LegacyDefaultColor,
+          font: legacyFontName,
+          fontStyle: {
+            fontSize: h1LegacyFontSize,
+            lineHeight: h1LegacyLineHeight
+          }
+        }
+  );
+};
