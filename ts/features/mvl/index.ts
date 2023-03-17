e857663f5a4057167caed5a804dee2cd5ea67476
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-03-17 17:03:00 +0100

    removed disclaimer

--- ts/features/mvl/index.ts
+++ ts/features/mvl/index.ts
@@ -13,7 +13,7 @@ import { PotTransform } from "../../store/transforms/potTransform";
 import { isDevEnv } from "../../utils/environment";
 import { mvlReducer as rootReducer, MvlState } from "./store/reducers";
 
-const CURRENT_REDUX_MVL_STORE_VERSION = 3;
+const CURRENT_REDUX_MVL_STORE_VERSION = 4;
 
 const migrations: MigrationManifest = {
   // version 2
@@ -32,6 +32,13 @@ const migrations: MigrationManifest = {
     return {
       ..._.omit(mvl, "downloads")
     };
+  },
+  // version 4
+  "4": (state: PersistedState): PersistedMvlState => {
+    const mvl = state as PersistedMvlState;
+    return {
+      ..._.omit(mvl, "preferences")
+    };
   }
 };
 export type PersistedMvlState = MvlState & PersistPartial;
@@ -40,7 +47,6 @@ export const mvlPersistConfig: PersistConfig = {
   key: "mvl",
   storage: AsyncStorage,
   version: CURRENT_REDUX_MVL_STORE_VERSION,
-  whitelist: ["preferences"],
   migrate: createMigrate(migrations, { debug: isDevEnv }),
   transforms: [PotTransform]
 };
