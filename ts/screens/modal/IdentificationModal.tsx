f50245c7d17acf858821e1da2a9a0c5faad52af2
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2023-03-19 13:27:58 +0100

    chore: run Prettier.

--- ts/screens/modal/IdentificationModal.tsx
+++ ts/screens/modal/IdentificationModal.tsx
@@ -3,7 +3,14 @@ import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
 import { Content, Text as NBText } from "native-base";
 import * as React from "react";
-import { View, Alert, Modal, StatusBar, StyleSheet, BackHandler } from "react-native";
+import {
+  View,
+  Alert,
+  Modal,
+  StatusBar,
+  StyleSheet,
+  BackHandler
+} from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { Link } from "../../components/core/typography/Link";
