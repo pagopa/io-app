af2ab445b577282b895410c9acad18c8428e519f
Sabino Picariello <sabino.picariello@pagopa.it>
2023-03-20 16:54:23 +0100

    Removed unused action. Updated package.json

--- ts/features/lollipop/store/actions/lollipop.ts
+++ ts/features/lollipop/store/actions/lollipop.ts
@@ -25,13 +25,8 @@ export const lollipopRemovePublicKey = createStandardAction(
   "LOLLIPOP_REMOVE_PUBLIC_KEY"
 )();
 
-export const lollipopAbortAppInitialization = createStandardAction(
-  "LOLLIPOP_ABORT_APP_INITIALIZATION"
-)();
-
 export type LollipopActions = ActionType<
   | typeof lollipopKeyTagSave
   | typeof lollipopSetPublicKey
   | typeof lollipopRemovePublicKey
-  | typeof lollipopAbortAppInitialization
 >;
