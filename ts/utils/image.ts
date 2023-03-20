f7e323946245d47b93baf63318e0b4d39c894e0f
LazyAfternoons <lazyafternoons@outlook.it>
2023-03-20 18:51:02 +0100

    chore: lint and prettier

--- ts/utils/image.ts
+++ ts/utils/image.ts
@@ -12,7 +12,7 @@ export const withBase64Uri = (
 
 /**
  * Adds a locale timestamp to the image URI to invalidate cache on the following day if the current platform is Android.
- * Useful to bypass React Native Image component aggressive cache on Android. 
+ * Useful to bypass React Native Image component aggressive cache on Android.
  * @param source - the source of the image.
  * @returns a new source with a modified URI which includes the actual timestamp in the locale format without slashes
  * if the platform is Android and the source contains an URI. The same source otherwise.
