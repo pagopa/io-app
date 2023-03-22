8ef5ee9d77b2cd3939fb0e8b112f8398e393b335
Martino Cesari Tomba <60693085+forrest57@users.noreply.github.com>
2023-03-22 15:28:20 +0100

    feat(IDPay): [IODPAY-129] Enable initiatives from instrument detail (#4352)
    ## Short description
    inclusion of screen skeleton and navigation for idpay's initiatives
    pairing from a payment method's details
    <table>
      <tr>
          <td>
    <img
    src="https://user-images.githubusercontent.com/60693085/220355356-72eec2a9-8935-4219-a46e-08ec9946fa3c.png"width=200/>
          </td>
          <td>
    <img
    src="https://user-images.githubusercontent.com/60693085/220355375-df956ef1-ed13-414a-8275-c97c0b2b5a76.png"
    width=200/>
          </td>
      </tr>
    </table>
    
    ## List of changes proposed in this pull request
    - introduction of the initiatives list screen's skeleton
    - introduction of a "show all pairable initiatives" button in the
    payment method page
    - the payment method page now renders a max of 3 pairable initiatives
    - added the necessary navigation logic
    - removed legacy component `CapabilityItem` along with its logic and
    generators
    
    ## How to test
    Having at least one initiative onboarded in your current account,
    navigate to any payment method's details from the wallet section, there
    a maximum of 3 pairable IDPay initiatives should show up, and in the
    "show all" section those plus any more in case the user has more than 3
    onboarded initiatives will aswell
    
    ---------
    
    Co-authored-by: Federico Mastrini <federicomastrini93@gmail.com>
    Co-authored-by: Jacopo Pompilii <Jacopo.pompilii@pagopa.it>
    Co-authored-by: Francesco Persico <francescopersico@gmail.com>

--- ts/features/idpay/wallet/saga/index.ts
+++ ts/features/idpay/wallet/saga/index.ts
@@ -1,10 +1,23 @@
 import { SagaIterator } from "redux-saga";
-import { call, takeLatest } from "typed-redux-saga/macro";
+import { call, takeEvery, takeLatest } from "typed-redux-saga/macro";
 import { PreferredLanguageEnum } from "../../../../../definitions/backend/PreferredLanguage";
 import { waitBackoffError } from "../../../../utils/backoffError";
 import { IDPayClient } from "../../common/api/client";
-import { idPayWalletGet } from "../store/actions";
+import {
+  IdPayInitiativesFromInstrumentPayloadType,
+  IdpayInitiativesInstrumentDeletePayloadType,
+  IdpayInitiativesInstrumentEnrollPayloadType,
+  idPayInitiativesFromInstrumentGet,
+  idPayWalletGet,
+  idpayInitiativesInstrumentDelete,
+  idpayInitiativesInstrumentEnroll
+} from "../store/actions";
+import { handleGetIDPayInitiativesFromInstrument } from "./handleGetIDPayInitiativesFromInstrument";
 import { handleGetIDPayWallet } from "./handleGetIDPayWallet";
+import {
+  handleInitiativeInstrumentDelete,
+  handleInitiativeInstrumentEnrollment
+} from "./handleInitiativeInstrumentEnrollment";
 
 /**
  * Handle the IDPay Wallet requests
@@ -26,4 +39,51 @@ export function* watchIDPayWalletSaga(
       preferredLanguage
     );
   });
+
+  yield* takeLatest(
+    idPayInitiativesFromInstrumentGet.request,
+    function* (action: { payload: IdPayInitiativesFromInstrumentPayloadType }) {
+      // wait backoff time if there were previous errors
+      yield* call(waitBackoffError, idPayInitiativesFromInstrumentGet.failure);
+      yield* call(
+        handleGetIDPayInitiativesFromInstrument,
+        idPayClient.getInitiativesWithInstrument,
+        token,
+        preferredLanguage,
+        action.payload
+      );
+    }
+  );
+  yield* takeEvery(
+    idpayInitiativesInstrumentEnroll.request,
+    function* (action: {
+      payload: IdpayInitiativesInstrumentEnrollPayloadType;
+    }) {
+      // wait backoff time if there were previous errors
+      yield* call(waitBackoffError, idpayInitiativesInstrumentEnroll.failure);
+      yield* call(
+        handleInitiativeInstrumentEnrollment,
+        idPayClient.enrollInstrument,
+        token,
+        preferredLanguage,
+        action.payload
+      );
+    }
+  );
+  yield* takeEvery(
+    idpayInitiativesInstrumentDelete.request,
+    function* (action: {
+      payload: IdpayInitiativesInstrumentDeletePayloadType;
+    }) {
+      // wait backoff time if there were previous errors
+      yield* call(waitBackoffError, idpayInitiativesInstrumentEnroll.failure);
+      yield* call(
+        handleInitiativeInstrumentDelete,
+        idPayClient.deleteInstrument,
+        token,
+        preferredLanguage,
+        action.payload
+      );
+    }
+  );
 }
