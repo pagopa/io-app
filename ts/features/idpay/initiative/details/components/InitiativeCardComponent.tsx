a29c6dcd2c86c3528096fa36dff6f8d3cf1fd1b8
forrest57 <tomba808@gmail.com>
2023-03-22 11:37:07 +0100

    still trying to get around circleci failing

--- ts/features/idpay/initiative/details/components/InitiativeCardComponent.tsx
+++ ts/features/idpay/initiative/details/components/InitiativeCardComponent.tsx
@@ -106,8 +106,8 @@ const InitiativeCardComponent = (props: Props) => {
   const isInitiativeConfigured = status === InitiativeStatusEnum.REFUNDABLE;
   const toBeRepaidAmount = (accrued || 0) - (refunded || 0);
   const remainingAmount = (amount || 0) - (accrued || 0);
-  const notNullyAmount = amount || 0;
-  const notNullyAccrued = accrued || 0;
+  const notNullyAmount = amount ?? 0;
+  const notNullyAccrued = accrued ?? 0;
   const totalAmount = notNullyAmount + notNullyAccrued;
 
   const dateString = formatDateAsLocal(endDate, true);
