071d4404de2f3723aceaac3795a6676af8b3d529
Mario Perrotta <mario.perrotta@pagopa.it>
2023-03-28 14:46:32 +0200

    test: update metadata saga

--- ts/features/fci/saga/networking/__tests__/handleGetMetadata.test.ts
+++ ts/features/fci/saga/networking/__tests__/handleGetMetadata.test.ts
@@ -5,6 +5,7 @@ import { mockFciMetadata } from "../../../types/__mocks__/Metadata.mock";
 import { Metadata } from "../../../../../../definitions/fci/Metadata";
 import { handleGetMetadata } from "../handleGetMetadata";
 import { fciMetadataRequest } from "../../../store/actions";
+import { SessionToken } from "../../../../../types/SessionToken";
 
 const successResponse = {
   status: 200,
@@ -18,18 +19,26 @@ const failureResponse = {
 describe("handleGetMetadata", () => {
   const mockBackendFciClient = jest.fn();
   it("it should dispatch fciMetadataRequest.success with the response payload if the response is right and the status code is 200", () => {
-    testSaga(handleGetMetadata, mockBackendFciClient)
+    testSaga(
+      handleGetMetadata,
+      mockBackendFciClient,
+      "mockedToken" as SessionToken
+    )
       .next()
-      .call(mockBackendFciClient, {})
+      .call(mockBackendFciClient, { Bearer: "mockedToken" })
       .next(right(successResponse))
       .put(fciMetadataRequest.success(successResponse.value))
       .next()
       .isDone();
   });
   it("it should dispatch fciMetadataRequest.failure with the response status code as payload if the response is right and the status code is different from 200", () => {
-    testSaga(handleGetMetadata, mockBackendFciClient)
+    testSaga(
+      handleGetMetadata,
+      mockBackendFciClient,
+      "mockedToken" as SessionToken
+    )
       .next()
-      .call(mockBackendFciClient, {})
+      .call(mockBackendFciClient, { Bearer: "mockedToken" })
       .next(right(failureResponse))
       .next(
         fciMetadataRequest.failure(
@@ -40,9 +49,13 @@ describe("handleGetMetadata", () => {
       .isDone();
   });
   it("it should dispatch fciMetadataRequest.failure with a fixed message as payload if the response left", () => {
-    testSaga(handleGetMetadata, mockBackendFciClient)
+    testSaga(
+      handleGetMetadata,
+      mockBackendFciClient,
+      "mockedToken" as SessionToken
+    )
       .next()
-      .call(mockBackendFciClient, {})
+      .call(mockBackendFciClient, { Bearer: "mockedToken" })
       .next(left(new Error()))
       .next(
         fciMetadataRequest.failure(
@@ -54,9 +67,13 @@ describe("handleGetMetadata", () => {
   });
   it("it should dispatch fciMetadataRequest.failure with the error message as payload if an exception is raised", () => {
     const mockedError = new Error("mockedErrorMessage");
-    testSaga(handleGetMetadata, mockBackendFciClient)
+    testSaga(
+      handleGetMetadata,
+      mockBackendFciClient,
+      "mockedToken" as SessionToken
+    )
       .next()
-      .call(mockBackendFciClient, {})
+      .call(mockBackendFciClient, { Bearer: "mockedToken" })
       .throw(mockedError)
       .next(fciMetadataRequest.failure(getNetworkError(mockedError)))
       .next()
