8e8cd78e35bfa9732939699882d73bc9f3a69d2a
Mario Perrotta <mario.perrotta@pagopa.it>
2023-04-04 10:33:35 +0200

    feat(Firma con IO): [SFEQS-1044] Add bottom sheet to check/activate service preferences (#4508)
    ## Short description
    This PR adds a BS and a check on the inbox status of the service
    preference giving the serviceId of FCI.
    
    
    https://user-images.githubusercontent.com/49342144/229453496-9757b75c-b873-4ce6-966b-724c3f199a64.mp4
    
    ## List of changes proposed in this pull request
    - Add check service bs
    - Update `FciQtspClausesScreen`
    
    ## How to test
    
    1.  start `io-dev-api-server`
    2. go to the service panel -> Firma con IO and disable "Contattarti in
    app"
    3. tap on a `WAIT_FOR_SIGNATURE` message and start a signing flow
    4. on the QTSP clauses screen try to tap to continue and should appear a
    BS
    
    NOTE: tapping on "Attiva ora" or "Non ora" are a non blocking CTA. The
    signing flow should continue in any case. The BS gives to the user the
    availability to activate "Contattarti in app" without go in the service
    preference screen.

---
+++ ts/features/fci/hooks/useFciCheckService.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import { useIOBottomSheetModal } from "../../../utils/hooks/bottomSheet";
+import { IOStyles } from "../../../components/core/variables/IOStyles";
+import { H3 } from "../../../components/core/typography/H3";
+import FooterWithButtons from "../../../components/ui/FooterWithButtons";
+import I18n from "../../../i18n";
+import customVariables from "../../../theme/variables";
+import { confirmButtonProps } from "../../bonus/bonusVacanze/components/buttons/ButtonConfigurations";
+import { H4 } from "../../../components/core/typography/H4";
+import { useIODispatch, useIOSelector } from "../../../store/hooks";
+import { fciStartSigningRequest } from "../store/actions";
+import { upsertServicePreference } from "../../../store/actions/services/servicePreference";
+import { ServiceId } from "../../../../definitions/backend/ServiceId";
+import { isServicePreferenceResponseSuccess } from "../../../types/services/ServicePreferenceResponse";
+import { servicePreferenceSelector } from "../../../store/reducers/entities/services/servicePreference";
+import { fciMetadataServiceIdSelector } from "../store/reducers/fciMetadata";
+
+const styles = StyleSheet.create({
+  verticalPad: {
+    paddingVertical: customVariables.spacerHeight
+  }
+});
+
+/**
+ * A hook that returns a function to present the abort signature flow bottom sheet
+ */
+export const useFciCheckService = () => {
+  const dispatch = useIODispatch();
+  const fciServiceId = useIOSelector(fciMetadataServiceIdSelector);
+  const servicePreference = useIOSelector(servicePreferenceSelector);
+  const servicePreferenceValue = pot.getOrElse(servicePreference, undefined);
+  const { present, bottomSheet, dismiss } = useIOBottomSheetModal(
+    <View style={styles.verticalPad}>
+      <H4 weight={"Regular"}>{I18n.t("features.fci.checkService.content")}</H4>
+    </View>,
+    <View style={IOStyles.flex}>
+      <H3 color={"bluegreyDark"} weight={"SemiBold"}>
+        {I18n.t("features.fci.checkService.title")}
+      </H3>
+    </View>,
+    280,
+    <FooterWithButtons
+      type={"TwoButtonsInlineThird"}
+      leftButton={{
+        onPressWithGestureHandler: true,
+        bordered: true,
+        onPress: () => {
+          dispatch(fciStartSigningRequest());
+          dismiss();
+        },
+        title: I18n.t("features.fci.checkService.cancel")
+      }}
+      rightButton={{
+        ...confirmButtonProps(() => {
+          if (
+            fciServiceId &&
+            servicePreferenceValue &&
+            isServicePreferenceResponseSuccess(servicePreferenceValue)
+          ) {
+            const sp = { ...servicePreferenceValue.value, inbox: true };
+            dispatch(
+              upsertServicePreference.request({
+                id: fciServiceId as ServiceId,
+                ...sp
+              })
+            );
+          }
+          dispatch(fciStartSigningRequest());
+          dismiss();
+        }, I18n.t("features.fci.checkService.confirm")),
+        onPressWithGestureHandler: true
+      }}
+    />
+  );
+
+  return {
+    dismiss,
+    present,
+    bottomSheet
+  };
+};
