pagopa/io-app

commit 17b2dbd182342508e168c647c7166499be78f3e8

chore: [IAI-217] Replace legacy `Text` components with the new ones (#4321)
### ⚠️ This PR depends on #4306 

## Short description
This PR removes all instances of the NativeBase `Text` component from
the codebase, except those involved in _buttons_ and _badges_. A number
of redundant style properties have been removed in the process.

## List of changes proposed in this pull request
* Replace the old `Text` components with the new ones (`Body`, `Label`,
`H1...H5`, etc...).
* Replace custom `Badge` styles with the `IOBadge` component
* Rename `NBText` (NativeBase Text) to `NBButtonText` when text is used
for buttons. This is a temporary solution until we add a new `Button`
component.
* Rename `NBText` to `NBBadgeText`when text is used for custom badges.
This is a temporary solution until we replace all the instances with a
new variant of the `IOBadge` component. Here's the specific PR:
(https://github.com/pagopa/io-app/pull/4337).
* Refactor the `CustomBadge` with native Text component (without
NativeBase `Badge` or `Text` components)
* Add the new `DSBadges` section to the design system

### Preview
<img
src="https://user-images.githubusercontent.com/1255491/215122932-6bdaff8f-a2f2-4805-b2ac-a167aa91aab2.png"
width="300" />

## How to test
Open the app and check literally every page/modal/component you come
across.

---------

Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>

ts/components/screens/BaseHeader.tsx
@@ -2,7 +2,7 @@ import { Millisecond } from "@pagopa/ts-commons/lib/units";
 import { NavigationEvents } from "@react-navigation/compat";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
-import { Body as NBBody, Left, Right, Text as NBText } from "native-base";
+import { Body as NBBody, Left, Right } from "native-base";
 import * as React from "react";
 import { FC, Ref } from "react";
 import {
@@ -108,7 +108,7 @@ const setAccessibilityTimeout = 0 as Millisecond;
 const noReferenceTimeout = 150 as Millisecond;
 /** A component representing the properties common to all the screens (and the most of modal/overlay displayed) */
 class BaseHeaderComponent extends React.PureComponent<Props, State> {
-  private firstElementRef = React.createRef<NBText | View>();
+  private firstElementRef = React.createRef<View>();
 
   public constructor(props: Props) {
     super(props);
@@ -231,20 +231,23 @@ class BaseHeaderComponent extends React.PureComponent<Props, State> {
         */}
         {!isSearchEnabled && (
           <NBBody style={goBack || customGoBack ? styles.body : styles.noLeft}>
-            {this.state.isScreenReaderActive &&
-            O.isSome(maybeAccessibilityLabel) ? (
-              this.renderBodyLabel(
-                maybeAccessibilityLabel.value,
-                this.firstElementRef as React.RefObject<Text>
-              )
-            ) : (
-              <View
-                ref={this.firstElementRef as React.RefObject<View>}
-                accessible={true}
-              >
-                {body ? body : headerTitle && this.renderBodyLabel(headerTitle)}
-              </View>
-            )}
+            <NBBody
+              style={goBack || customGoBack ? styles.body : styles.noLeft}
+            >
+              {this.state.isScreenReaderActive &&
+              O.isSome(maybeAccessibilityLabel) ? (
+                this.renderBodyLabel(
+                  maybeAccessibilityLabel.value,
+                  this.firstElementRef
+                )
+              ) : (
+                <View ref={this.firstElementRef} accessible={true}>
+                  {body
+                    ? body
+                    : headerTitle && this.renderBodyLabel(headerTitle)}
+                </View>
+              )}
+            </NBBody>
           </NBBody>
         )}
 

commit 80c34697e0eb5b85f68820434a39681b4294d725

fix: [IAI-275] Fix `BaseHeader` UI regression (#4519)
This PR fixes the UI regression caused by #4321
## List of changes proposed in this pull request
- Remove double `NBBody` container added by mistake

ts/components/screens/BaseHeader.tsx
@@ -231,23 +231,17 @@ class BaseHeaderComponent extends React.PureComponent<Props, State> {
         */}
         {!isSearchEnabled && (
           <NBBody style={goBack || customGoBack ? styles.body : styles.noLeft}>
-            <NBBody
-              style={goBack || customGoBack ? styles.body : styles.noLeft}
-            >
-              {this.state.isScreenReaderActive &&
-              O.isSome(maybeAccessibilityLabel) ? (
-                this.renderBodyLabel(
-                  maybeAccessibilityLabel.value,
-                  this.firstElementRef
-                )
-              ) : (
-                <View ref={this.firstElementRef} accessible={true}>
-                  {body
-                    ? body
-                    : headerTitle && this.renderBodyLabel(headerTitle)}
-                </View>
-              )}
-            </NBBody>
+            {this.state.isScreenReaderActive &&
+            O.isSome(maybeAccessibilityLabel) ? (
+              this.renderBodyLabel(
+                maybeAccessibilityLabel.value,
+                this.firstElementRef
+              )
+            ) : (
+              <View ref={this.firstElementRef} accessible={true}>
+                {body ? body : headerTitle && this.renderBodyLabel(headerTitle)}
+              </View>
+            )}
           </NBBody>
         )}
 