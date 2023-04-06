pagopa/io-app

commit 24d2d1b7f01e2041f8d9c9ceb8d243debe2b10e9

chore: [IOAPPFD0-45] Add next iteration of the current buttons (#4435)
## Short description
This PR adds the new variants of the `ButtonSolid`, `ButtonOutline`,
`IconButton` and `IconButtonSolid`. All the variants use the new
typeface, instead of the previously implemented `Titillium`.

### Preview


https://user-images.githubusercontent.com/1255491/223750691-47eb5904-d62f-431c-a5ae-a7bb090e8318.mp4


## List of changes proposed in this pull request
- Add the new variant of `ButtonSolid` and `ButtonOutline` using the
boolean variable `isDesignSystemEnabled`
- Duplicate all the style objects to make the future replacement easier

### Notes
To make the replacement easier, all the code blocks related to the
legacy button are marked in this way:
```js
/* ◀ REMOVE_LEGACY_COMPONENT: Start */
[…]
/* REMOVE_LEGACY_COMPONENT: End ▶ */
```
If you find `REMOVE_LEGACY_COMPONENT` in the codebase, you should check
all code blocks that can be easily removed.

## How to test
Go to the `Profile → Design System → Buttons`. Try to enable/disable
`Experimental design system` in the Profile page to play with the
different styles.

---------

Co-authored-by: Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

ts/components/ui/ButtonOutline.tsx
@@ -17,9 +17,14 @@ import Animated, {
 } from "react-native-reanimated";
 import { hexToRgba, IOColors } from "../core/variables/IOColors";
 import { IOSpringValues, IOScaleValues } from "../core/variables/IOAnimations";
-import { IOButtonStyles } from "../core/variables/IOStyles";
+import {
+  IOButtonStyles,
+  IOButtonLegacyStyles
+} from "../core/variables/IOStyles";
 import { makeFontStyleObject } from "../core/fonts";
 import { WithTestID } from "../../types/WithTestID";
+import { useIOSelector } from "../../store/hooks";
+import { isDesignSystemEnabledSelector } from "../../store/reducers/persistedPreferences";
 
 export type ButtonOutline = WithTestID<{
   color?: "primary" | "neutral" | "contrast" | "danger";
@@ -50,9 +55,16 @@ type ColorStates = {
   };
 };
 
-// COMPONENT CONFIGURATION
+/*
+░░░ COMPONENT CONFIGURATION ░░░
+*/
 
-const mapColorStates: Record<
+/* Delete the following block if you want to
+get rid of legacy variant */
+
+/* ◀ REMOVE_LEGACY_COMPONENT: Start */
+
+const mapLegacyColorStates: Record<
   NonNullable<ButtonOutline["color"]>,
   ColorStates
 > = {
@@ -130,12 +142,101 @@ const mapColorStates: Record<
   }
 };
 
-const IOButtonStylesLocal = StyleSheet.create({
+const IOButtonLegacyStylesLocal = StyleSheet.create({
   label: {
     ...makeFontStyleObject("Bold")
   },
   buttonWithBorder: {
     borderWidth: 1
+  }
+});
+
+/* REMOVE_LEGACY_COMPONENT: End ▶ */
+
+const mapColorStates: Record<
+  NonNullable<ButtonOutline["color"]>,
+  ColorStates
+> = {
+  // Primary button
+  primary: {
+    border: {
+      default: IOColors["blueIO-500"],
+      pressed: IOColors["blueIO-600"],
+      disabled: IOColors["grey-200"]
+    },
+    background: {
+      default: hexToRgba(IOColors["blueIO-50"], 0),
+      pressed: hexToRgba(IOColors["blueIO-50"], 1),
+      disabled: "transparent"
+    },
+    label: {
+      default: IOColors["blueIO-500"],
+      pressed: IOColors["blueIO-600"],
+      disabled: IOColors["grey-700"]
+    }
+  },
+  // Neutral button
+  neutral: {
+    border: {
+      default: IOColors.grey,
+      pressed: IOColors.bluegrey,
+      disabled: IOColors.bluegreyLight
+    },
+    background: {
+      default: IOColors.white,
+      pressed: IOColors.greyUltraLight,
+      disabled: "transparent"
+    },
+    label: {
+      default: IOColors.bluegrey,
+      pressed: IOColors.bluegreyDark,
+      disabled: IOColors.grey
+    }
+  },
+  // Contrast button
+  contrast: {
+    border: {
+      default: IOColors.white,
+      pressed: IOColors.white,
+      disabled: IOColors["blueIO-200"]
+    },
+    background: {
+      default: hexToRgba(IOColors["blueIO-600"], 0),
+      pressed: IOColors["blueIO-600"],
+      disabled: "transparent"
+    },
+    label: {
+      default: IOColors.white,
+      pressed: IOColors.white,
+      disabled: IOColors["blueIO-200"]
+    }
+  },
+  // Danger button
+  danger: {
+    border: {
+      default: IOColors.red,
+      pressed: IOColors.red,
+      disabled: IOColors.bluegreyLight
+    },
+    background: {
+      default: hexToRgba(IOColors.red, 0),
+      pressed: hexToRgba(IOColors.red, 0.15),
+      disabled: "transparent"
+    },
+    label: {
+      default: IOColors.red,
+      pressed: IOColors.red,
+      disabled: IOColors.grey
+    }
+  }
+};
+
+const IOButtonStylesLocal = StyleSheet.create({
+  label: {
+    ...makeFontStyleObject("Regular", false, "ReadexPro")
+  },
+  buttonWithBorder: {
+    borderWidth: 2
   }
 });
 
@@ -149,8 +250,10 @@ export const ButtonOutline = ({
   accessibilityLabel,
   accessibilityHint,
   testID
-}: ButtonOutline) => {
+}: // eslint-disable-next-line sonarjs/cognitive-complexity
+ButtonOutline) => {
   const isPressed: Animated.SharedValue<number> = useSharedValue(0);
+  const isDesignSystemEnabled = useIOSelector(isDesignSystemEnabledSelector);
 
   // Scaling transformation applied when the button is pressed
   const animationScaleValue = IOScaleValues?.basicButton?.pressedState;
@@ -163,23 +266,44 @@ export const ButtonOutline = ({
   // Interpolate animation values from `isPressed` values
   const pressedAnimationStyle = useAnimatedStyle(() => {
     // Link color states to the pressed states
-    const backgroundColor = interpolateColor(
-      progressPressed.value,
-      [0, 1],
-      [
-        mapColorStates[color].background.default,
-        mapColorStates[color].background.pressed
-      ]
-    );
 
-    const borderColor = interpolateColor(
-      progressPressed.value,
-      [0, 1],
-      [
-        mapColorStates[color].border.default,
-        mapColorStates[color].border.pressed
-      ]
-    );
+    /* ◀ REMOVE_LEGACY_COMPONENT: Remove the following condition */
+    const backgroundColor = isDesignSystemEnabled
+      ? interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [
+            mapColorStates[color].background.default,
+            mapColorStates[color].background.pressed
+          ]
+        )
+      : interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [
+            mapLegacyColorStates[color].background.default,
+            mapLegacyColorStates[color].background.pressed
+          ]
+        );
+
+    const borderColor = isDesignSystemEnabled
+      ? interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [
+            mapColorStates[color].border.default,
+            mapColorStates[color].border.pressed
+          ]
+        )
+      : interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [
+            mapLegacyColorStates[color].border.default,
+            mapLegacyColorStates[color].border.pressed
+          ]
+        );
+    /* REMOVE_LEGACY_COMPONENT: End ▶ */
 
     // Scale down button slightly when pressed
     const scale = interpolate(
@@ -198,11 +322,26 @@ export const ButtonOutline = ({
 
   const pressedColorLabelAnimationStyle = useAnimatedStyle(() => {
     // Link color states to the pressed states
-    const labelColor = interpolateColor(
-      progressPressed.value,
-      [0, 1],
-      [mapColorStates[color].label.default, mapColorStates[color].label.pressed]
-    );
+
+    /* ◀ REMOVE_LEGACY_COMPONENT: Remove the following condition */
+    const labelColor = isDesignSystemEnabled
+      ? interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [
+            mapColorStates[color].border.default,
+            mapColorStates[color].border.pressed
+          ]
+        )
+      : interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [
+            mapLegacyColorStates[color].label.default,
+            mapLegacyColorStates[color].label.pressed
+          ]
+        );
+    /* REMOVE_LEGACY_COMPONENT: End ▶ */
 
     return {
       color: labelColor
@@ -218,7 +357,74 @@ export const ButtonOutline = ({
     isPressed.value = 0;
   }, [isPressed]);
 
-  return (
+  /* ◀ REMOVE_LEGACY_COMPONENT: Start */
+  const LegacyButton = () => (
+    <Pressable
+      accessibilityLabel={accessibilityLabel}
+      accessibilityHint={accessibilityHint}
+      accessibilityRole={"button"}
+      testID={testID}
+      onPress={onPress}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      accessible={true}
+      disabled={disabled}
+      style={
+        fullWidth
+          ? IOButtonLegacyStyles.dimensionsFullWidth
+          : IOButtonLegacyStyles.dimensionsDefault
+      }
+    >
+      <Animated.View
+        style={[
+          IOButtonLegacyStyles.button,
+          IOButtonLegacyStylesLocal.buttonWithBorder,
+          small
+            ? IOButtonLegacyStyles.buttonSizeSmall
+            : IOButtonLegacyStyles.buttonSizeDefault,
+          disabled
+            ? {
+                backgroundColor:
+                  mapLegacyColorStates[color]?.background?.disabled,
+                borderColor: mapLegacyColorStates[color]?.border?.disabled
+              }
+            : {
+                backgroundColor:
+                  mapLegacyColorStates[color]?.background?.default,
+                borderColor: mapLegacyColorStates[color]?.border.default
+              },
+          /* Prevent Reanimated from overriding background colors
+          if button is disabled */
+          !disabled && pressedAnimationStyle
+        ]}
+      >
+        <Animated.Text
+          style={[
+            IOButtonLegacyStylesLocal.label,
+            IOButtonLegacyStyles.label,
+            small
+              ? IOButtonLegacyStyles.labelSizeSmall
+              : IOButtonLegacyStyles.labelSizeDefault,
+            disabled
+              ? { color: mapLegacyColorStates[color]?.label?.disabled }
+              : { color: mapLegacyColorStates[color]?.label?.default },
+            !disabled && pressedColorLabelAnimationStyle
+          ]}
+          numberOfLines={1}
+          ellipsizeMode="tail"
+          /* A11y-related props:
+          DON'T UNCOMMENT THEM */
+          /* allowFontScaling
+          maxFontSizeMultiplier={1.3} */
+        >
+          {label}
+        </Animated.Text>
+      </Animated.View>
+    </Pressable>
+  );
+  /* REMOVE_LEGACY_COMPONENT: End ▶ */
+
+  const NewButton = () => (
     <Pressable
       accessibilityLabel={accessibilityLabel}
       accessibilityHint={accessibilityHint}
@@ -276,6 +482,10 @@ export const ButtonOutline = ({
       </Animated.View>
     </Pressable>
   );
+
+  /* ◀ REMOVE_LEGACY_COMPONENT: Move the entire <NewButton /> here,
+  without the following condition */
+  return isDesignSystemEnabled ? <NewButton /> : <LegacyButton />;
 };
 
 export default ButtonOutline;

ts/components/ui/ButtonSolid.tsx
@@ -18,8 +18,13 @@ import Animated, {
 import { IOColors } from "../core/variables/IOColors";
 import { IOSpringValues, IOScaleValues } from "../core/variables/IOAnimations";
 import { BaseTypography } from "../core/typography/BaseTypography";
-import { IOButtonStyles } from "../core/variables/IOStyles";
+import {
+  IOButtonStyles,
+  IOButtonLegacyStyles
+} from "../core/variables/IOStyles";
 import { WithTestID } from "../../types/WithTestID";
+import { useIOSelector } from "../../store/hooks";
+import { isDesignSystemEnabledSelector } from "../../store/reducers/persistedPreferences";
 
 export type ButtonSolid = WithTestID<{
   color?: "primary" | "danger" | "contrast";
@@ -41,9 +46,19 @@ type ColorStates = {
   };
 };
 
-// COMPONENT CONFIGURATION
+/*
+░░░ COMPONENT CONFIGURATION ░░░
+*/
 
-const mapColorStates: Record<NonNullable<ButtonSolid["color"]>, ColorStates> = {
+/* Delete the following block if you want to
+get rid of legacy variant */
+
+/* ◀ REMOVE_LEGACY_COMPONENT: Start */
+
+const mapLegacyColorStates: Record<
+  NonNullable<ButtonSolid["color"]>,
+  ColorStates
+> = {
   // Primary button
   primary: {
     default: IOColors.blue,
@@ -74,13 +89,53 @@ const mapColorStates: Record<NonNullable<ButtonSolid["color"]>, ColorStates> = {
 };
 
 // Disabled state
-const colorPrimaryButtonDisabled: IOColors = "bluegreyLight";
+const colorPrimaryLegacyButtonDisabled: IOColors = "bluegreyLight";
+const legacyStyles = StyleSheet.create({
+  backgroundDisabled: {
+    backgroundColor: IOColors[colorPrimaryLegacyButtonDisabled]
+  }
+});
+
+/* REMOVE_LEGACY_COMPONENT: End ▶ */
+
+// Disabled state
+const colorPrimaryButtonDisabled: IOColors = "grey-200";
 
 const styles = StyleSheet.create({
   backgroundDisabled: {
     backgroundColor: IOColors[colorPrimaryButtonDisabled]
   }
 });
+
+const mapColorStates: Record<NonNullable<ButtonSolid["color"]>, ColorStates> = {
+  // Primary button
+  primary: {
+    default: IOColors["blueIO-500"],
+    pressed: IOColors["blueIO-600"],
+    label: {
+      default: "white",
+      disabled: "grey-700"
+    }
+  },
+  // Danger button
+  danger: {
+    default: IOColors["error-850"],
+    pressed: IOColors["error-600"],
+    label: {
+      default: "white",
+      disabled: "grey-700"
+    }
+  },
+  // Contrast button
+  contrast: {
+    default: IOColors.white,
+    pressed: IOColors["blueIO-50"],
+    label: {
+      default: "blueIO-500",
+      disabled: "grey-700"
+    }
+  }
+};
 
 export const ButtonSolid = ({
   color = "primary",
@@ -92,8 +147,10 @@ export const ButtonSolid = ({
   accessibilityLabel,
   accessibilityHint,
   testID
-}: ButtonSolid) => {
+}: // eslint-disable-next-line sonarjs/cognitive-complexity
+ButtonSolid) => {
   const isPressed: Animated.SharedValue<number> = useSharedValue(0);
+  const isDesignSystemEnabled = useIOSelector(isDesignSystemEnabledSelector);
 
   // Scaling transformation applied when the button is pressed
   const animationScaleValue = IOScaleValues?.basicButton?.pressedState;
@@ -106,11 +163,23 @@ export const ButtonSolid = ({
   // Interpolate animation values from `isPressed` values
   const pressedAnimationStyle = useAnimatedStyle(() => {
     // Link color states to the pressed states
-    const bgColor = interpolateColor(
-      progressPressed.value,
-      [0, 1],
-      [mapColorStates[color].default, mapColorStates[color].pressed]
-    );
+
+    /* ◀ REMOVE_LEGACY_COMPONENT: Remove the following condition */
+    const bgColor = isDesignSystemEnabled
+      ? interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [mapColorStates[color].default, mapColorStates[color].pressed]
+        )
+      : interpolateColor(
+          progressPressed.value,
+          [0, 1],
+          [
+            mapLegacyColorStates[color].default,
+            mapLegacyColorStates[color].pressed
+          ]
+        );
+    /* REMOVE_LEGACY_COMPONENT: End ▶ */
 
     // Scale down button slightly when pressed
     const scale = interpolate(
@@ -135,7 +204,66 @@ export const ButtonSolid = ({
     isPressed.value = 0;
   }, [isPressed]);
 
-  return (
+  /* ◀ REMOVE_LEGACY_COMPONENT: Start */
+  const LegacyButton = () => (
+    <Pressable
+      accessibilityLabel={accessibilityLabel}
+      accessibilityHint={accessibilityHint}
+      accessibilityRole={"button"}
+      testID={testID}
+      onPress={onPress}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      accessible={true}
+      disabled={disabled}
+      style={
+        fullWidth
+          ? IOButtonLegacyStyles.dimensionsFullWidth
+          : IOButtonLegacyStyles.dimensionsDefault
+      }
+    >
+      <Animated.View
+        style={[
+          IOButtonLegacyStyles.button,
+          small
+            ? IOButtonLegacyStyles.buttonSizeSmall
+            : IOButtonLegacyStyles.buttonSizeDefault,
+          disabled
+            ? legacyStyles.backgroundDisabled
+            : { backgroundColor: mapLegacyColorStates[color]?.default },
+          /* Prevent Reanimated from overriding background colors
+          if button is disabled */
+          !disabled && pressedAnimationStyle
+        ]}
+      >
+        <BaseTypography
+          weight={"Bold"}
+          color={
+            disabled
+              ? mapLegacyColorStates[color]?.label?.disabled
+              : mapLegacyColorStates[color]?.label?.default
+          }
+          style={[
+            IOButtonLegacyStyles.label,
+            small
+              ? IOButtonLegacyStyles.labelSizeSmall
+              : IOButtonLegacyStyles.labelSizeDefault
+          ]}
+          numberOfLines={1}
+          ellipsizeMode="tail"
+          /* A11y-related props:
+          DON'T UNCOMMENT THEM */
+          /* allowFontScaling
+          maxFontSizeMultiplier={1.3} */
+        >
+          {label}
+        </BaseTypography>
+      </Animated.View>
+    </Pressable>
+  );
+  /* REMOVE_LEGACY_COMPONENT: End ▶ */
+
+  const NewButton = () => (
     <Pressable
       accessibilityLabel={accessibilityLabel}
       accessibilityHint={accessibilityHint}
@@ -163,7 +291,8 @@ export const ButtonSolid = ({
         ]}
       >
         <BaseTypography
-          weight={"Bold"}
+          font="ReadexPro"
+          weight={"Regular"}
           color={
             disabled
               ? mapColorStates[color]?.label?.disabled
@@ -187,6 +316,10 @@ export const ButtonSolid = ({
       </Animated.View>
     </Pressable>
   );
+
+  /* ◀ REMOVE_LEGACY_COMPONENT: Move the entire <NewButton /> here,
+  without the following condition */
+  return isDesignSystemEnabled ? <NewButton /> : <LegacyButton />;
 };
 
 export default ButtonSolid;

ios/ItaliaApp.xcodeproj/project.pbxproj
@@ -19,6 +19,7 @@
 		3A3CA4696BA44782A02FE37E /* TitilliumWeb-SemiBold.ttf in Resources */ = {isa = PBXBuildFile; fileRef = B021D4CC17ED4D54B7944FED /* TitilliumWeb-SemiBold.ttf */; };
 		4D331CF68DAC4800A6D6297A /* TitilliumWeb-Regular.ttf in Resources */ = {isa = PBXBuildFile; fileRef = 11C2483C363E432BB4D0A583 /* TitilliumWeb-Regular.ttf */; };
 		570BBC8FC70C4A76ABF035AC /* TitilliumWeb-Light.ttf in Resources */ = {isa = PBXBuildFile; fileRef = 163BC670599B42509D4D138E /* TitilliumWeb-Light.ttf */; };
+		69A5E1839C924EE5B8CD9470 /* ReadexPro-Regular.ttf in Resources */ = {isa = PBXBuildFile; fileRef = 2C584AD92CB24628B1AA76DE /* ReadexPro-Regular.ttf */; };
 		6B44A74498354CC19B90FE9C /* TitilliumWeb-ExtraLightItalic.ttf in Resources */ = {isa = PBXBuildFile; fileRef = 6754714BBEF54AA59833C0EB /* TitilliumWeb-ExtraLightItalic.ttf */; };
 		72E4B1EFF7D4414483079F91 /* TitilliumWeb-SemiBoldItalic.ttf in Resources */ = {isa = PBXBuildFile; fileRef = CED8B1FF1E254583BB3F285F /* TitilliumWeb-SemiBoldItalic.ttf */; };
 		859781B477BB45579FB9A9F2 /* TitilliumWeb-Italic.ttf in Resources */ = {isa = PBXBuildFile; fileRef = F709D4AE20E44EFAAB255A8E /* TitilliumWeb-Italic.ttf */; };
@@ -82,6 +83,7 @@
 		2A1DC2C4323E417BBBAE8817 /* RobotoMono-BoldItalic.ttf */ = {isa = PBXFileReference; explicitFileType = undefined; fileEncoding = 9; includeInIndex = 0; lastKnownFileType = unknown; name = "RobotoMono-BoldItalic.ttf"; path = "../assets/fonts/RobotoMono/RobotoMono-BoldItalic.ttf"; sourceTree = "<group>"; };
 		2AD40B9824CB049500E4124A /* LaunchScreen.storyboard */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = file.storyboard; name = LaunchScreen.storyboard; path = ItaliaApp/LaunchScreen.storyboard; sourceTree = "<group>"; };
 		2AFAC65324C9B7C300E85199 /* ItaliaApp-Bridging-Header.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = "ItaliaApp-Bridging-Header.h"; sourceTree = "<group>"; };
+		2C584AD92CB24628B1AA76DE /* ReadexPro-Regular.ttf */ = {isa = PBXFileReference; explicitFileType = undefined; fileEncoding = undefined; includeInIndex = 0; lastKnownFileType = unknown; name = "ReadexPro-Regular.ttf"; path = "../assets/fonts/ReadexPro/ReadexPro-Regular.ttf"; sourceTree = "<group>"; };
 		2D52800996AA471A87A7F975 /* LICENSE.txt */ = {isa = PBXFileReference; explicitFileType = undefined; fileEncoding = 9; includeInIndex = 0; lastKnownFileType = unknown; name = LICENSE.txt; path = ../assets/fonts/RobotoMono/LICENSE.txt; sourceTree = "<group>"; };
 		2FC1D966F75DABE66246CB1C /* Pods-ItaliaApp.release.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-ItaliaApp.release.xcconfig"; path = "Target Support Files/Pods-ItaliaApp/Pods-ItaliaApp.release.xcconfig"; sourceTree = "<group>"; };
 		309BA0207AA24FE9A0EBE61C /* TitilliumWeb-Bold.ttf */ = {isa = PBXFileReference; explicitFileType = undefined; fileEncoding = 9; includeInIndex = 0; lastKnownFileType = unknown; name = "TitilliumWeb-Bold.ttf"; path = "../assets/fonts/TitilliumWeb/TitilliumWeb-Bold.ttf"; sourceTree = "<group>"; };
@@ -200,6 +202,7 @@
 				76E4CEA78E3F4060ABB59808 /* RobotoMono-Regular.ttf */,
 				5389C36762A04AE69D762289 /* RobotoMono-RegularItalic.ttf */,
 				FB6C250A30844C039ED2913E /* io-icon-font.ttf */,
+				2C584AD92CB24628B1AA76DE /* ReadexPro-Regular.ttf */,
 			);
 			name = Resources;
 			sourceTree = "<group>";
@@ -391,6 +394,7 @@
 				16E38CBD19D4437A9C020B21 /* RobotoMono-Regular.ttf in Resources */,
 				28E4866232804051B865FC10 /* RobotoMono-RegularItalic.ttf in Resources */,
 				9AA4918716C04270861750C9 /* io-icon-font.ttf in Resources */,
+				69A5E1839C924EE5B8CD9470 /* ReadexPro-Regular.ttf in Resources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};

ios/ItaliaApp/Info.plist
@@ -109,6 +109,7 @@
       <string>RobotoMono-LightItalic.ttf</string>
       <string>RobotoMono-Regular.ttf</string>
       <string>RobotoMono-RegularItalic.ttf</string>
+      <string>ReadexPro-Regular.ttf</string>
       <string>io-icon-font.ttf</string>
     </array>
     <key>UIBackgroundModes</key>

react-native.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   assets: [
     "./assets/fonts/TitilliumWeb",
+    "./assets/fonts/ReadexPro",
     "./assets/fonts/io-icon-font",
     "./assets/fonts/Ionicons",
     "./assets/fonts/RobotoMono"

ts/components/Alert/index.tsx
@@ -19,9 +19,6 @@ import { H2 } from "../core/typography/H2";
 import { IOStyles } from "../core/variables/IOStyles";
 import { IOAlertRadius } from "../core/variables/IOShapes";
 import { IOAlertSpacing } from "../core/variables/IOSpacing";
-import { H1 } from "../core/typography/H1";
-import { useIOSelector } from "../../store/hooks";
-import { isDesignSystemEnabledSelector } from "../../store/reducers/persistedPreferences";
 
 const iconSize = 24;
 const [spacingDefault, spacingFullWidth] = IOAlertSpacing;
@@ -99,62 +96,50 @@ export const Alert = ({
   accessibilityLabel,
   accessibilityRole,
   testID
-}: Props) => {
-  const isDesignSystemEnabled = useIOSelector(isDesignSystemEnabledSelector);
-
-  return (
-    <View
-      ref={viewRef}
-      style={[
-        styles.container,
-        fullWidth ? styles.spacingFullWidth : styles.spacingDefault,
-        { backgroundColor: IOColors[mapVariantStates[variant].background] }
-      ]}
-      testID={testID}
-      accessibilityHint={accessibilityHint}
-      accessibilityLabel={accessibilityLabel}
-      accessibilityRole={accessibilityRole}
-      accessible={accessible ?? true}
-    >
-      <Icon
-        name={mapVariantStates[variant].icon}
-        size={iconSize}
-        color={mapVariantStates[variant].foreground}
-      />
-      <HSpacer />
-      <View style={IOStyles.flex}>
-        {title && (
-          <>
-            {isDesignSystemEnabled ? (
-              <H1 color={mapVariantStates[variant].foreground}>{title}</H1>
-            ) : (
-              <H2
-                weight="SemiBold"
-                color={mapVariantStates[variant].foreground}
-              >
-                {title}
-              </H2>
-            )}
-
-            <VSpacer size={4} />
-          </>
-        )}
-        <Label color={mapVariantStates[variant].foreground} weight={"Regular"}>
-          {content}
-        </Label>
-        {action && (
-          <>
-            <VSpacer size={4} />
-            <Link
-              color={mapVariantStates[variant].foreground}
-              onPress={onPress}
-              style={{ alignSelf: "flex-start" }}
-            >
-              {action}
-            </Link>
-          </>
-        )}
-      </View>
+}: Props) => (
+  <View
+    ref={viewRef}
+    style={[
+      styles.container,
+      fullWidth ? styles.spacingFullWidth : styles.spacingDefault,
+      { backgroundColor: IOColors[mapVariantStates[variant].background] }
+    ]}
+    testID={testID}
+    accessibilityHint={accessibilityHint}
+    accessibilityLabel={accessibilityLabel}
+    accessibilityRole={accessibilityRole}
+    accessible={accessible ?? true}
+  >
+    <Icon
+      name={mapVariantStates[variant].icon}
+      size={iconSize}
+      color={mapVariantStates[variant].foreground}
+    />
+    <HSpacer />
+    <View style={IOStyles.flex}>
+      {title && (
+        <>
+          <H2 weight="SemiBold" color={mapVariantStates[variant].foreground}>
+            {title}
+          </H2>
+          <VSpacer size={4} />
+        </>
+      )}
+      <Label color={mapVariantStates[variant].foreground} weight={"Regular"}>
+        {content}
+      </Label>
+      {action && (
+        <>
+          <VSpacer size={4} />
+          <Link
+            color={mapVariantStates[variant].foreground}
+            onPress={onPress}
+            style={{ alignSelf: "flex-start" }}
+          >
+            {action}
+          </Link>
+        </>
+      )}
     </View>
-  );
-};
+  </View>
+);

commit 70e8719f9d67aaeb774b49be3adacb923965dac2

fix: fixed regression made with #4435

ts/components/ui/ButtonOutline.tsx
@@ -369,11 +369,7 @@ ButtonOutline) => {
       onPressOut={onPressOut}
       accessible={true}
       disabled={disabled}
-      style={
-        fullWidth
-          ? IOButtonLegacyStyles.dimensionsFullWidth
-          : IOButtonLegacyStyles.dimensionsDefault
-      }
+      style={!fullWidth ? IOButtonLegacyStyles.dimensionsDefault : {}}
     >
       <Animated.View
         style={[

ts/components/ui/ButtonSolid.tsx
@@ -216,11 +216,7 @@ ButtonSolid) => {
       onPressOut={onPressOut}
       accessible={true}
       disabled={disabled}
-      style={
-        fullWidth
-          ? IOButtonLegacyStyles.dimensionsFullWidth
-          : IOButtonLegacyStyles.dimensionsDefault
-      }
+      style={!fullWidth ? IOButtonStyles.dimensionsDefault : {}}
     >
       <Animated.View
         style={[
70e8719f9d67aaeb774b49be3adacb923965dac2
Federico Mastrini <federicomastrini93@gmail.com>
2023-04-06 12:07:46 +0200

    fix: fixed regression made with #4435

--- ts/components/ui/ButtonSolid.tsx
+++ ts/components/ui/ButtonSolid.tsx
@@ -216,11 +216,7 @@ ButtonSolid) => {
       onPressOut={onPressOut}
       accessible={true}
       disabled={disabled}
-      style={
-        fullWidth
-          ? IOButtonLegacyStyles.dimensionsFullWidth
-          : IOButtonLegacyStyles.dimensionsDefault
-      }
+      style={!fullWidth ? IOButtonStyles.dimensionsDefault : {}}
     >
       <Animated.View
         style={[
