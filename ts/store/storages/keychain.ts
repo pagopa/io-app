83e950bf12faa93d29208da2fce8ca44b90a96fc
Andrea Piai <andrea.piai@pagopa.it>
2023-04-06 11:18:25 +0200

    Reverted bad changes

--- ts/store/storages/keychain.ts
+++ ts/store/storages/keychain.ts
@@ -11,14 +11,24 @@ import { setGenericPasswordWithDefaultAccessibleOption } from "../../utils/keych
  */
 const USERNAME = "REDUX_PERSIST";
 
+// eslint-disable-next-line
+export let keychainError: string | undefined;
+
 export default function createSecureStorage(): Storage {
   return {
     getItem: async key => {
-      const result = await Keychain.getGenericPassword({ service: key });
-      if (typeof result === "boolean") {
+      try {
+        const result = await Keychain.getGenericPassword({ service: key });
+        if (typeof result === "boolean") {
+          return undefined;
+        } else {
+          return result.password;
+        }
+      } catch (err) {
+        // workaround to send keychainError for Pixel devices
+        // TODO: REMOVE AFTER FIXING https://pagopa.atlassian.net/jira/software/c/projects/IABT/boards/92?modal=detail&selectedIssue=IABT-1441
+        keychainError = JSON.stringify(err);
         return undefined;
-      } else {
-        return result.password;
       }
     },
 
@@ -31,3 +41,7 @@ export default function createSecureStorage(): Storage {
       await Keychain.resetGenericPassword({ service: key })
   };
 }
+
+export const clearKeychainError = () => {
+  keychainError = undefined;
+};
