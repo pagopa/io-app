de5f192f35613d0eaa704db996432f64b2e0e5ff
Mario Perrotta <mario.perrotta@pagopa.it>
2023-04-14 12:29:50 +0200

    chore: add zendesk supporto

--- ts/features/fci/screens/valid/FciSignatureRequestsScreen.tsx
+++ ts/features/fci/screens/valid/FciSignatureRequestsScreen.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { SafeAreaView, View, SectionList } from "react-native";
-import { constNull } from "fp-ts/lib/function";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import ScreenContent from "../../../../components/screens/ScreenContent";
@@ -10,10 +9,55 @@ import { fciSignaturesListSelector } from "../../store/reducers/fciSignaturesLis
 import { fciSignaturesListRequest } from "../../store/actions";
 import LoadingSpinnerOverlay from "../../../../components/LoadingSpinnerOverlay";
 import I18n from "../../../../i18n";
+import { assistanceToolConfigSelector } from "../../../../store/reducers/backendStatus";
+import {
+  addTicketCustomField,
+  appendLog,
+  assistanceToolRemoteConfig,
+  resetCustomFields,
+  zendeskCategoryId,
+  zendeskFCICategory
+} from "../../../../utils/supportAssistance";
+import {
+  zendeskSelectedCategory,
+  zendeskSupportStart
+} from "../../../zendesk/store/actions";
+import { ToolEnum } from "../../../../../definitions/content/AssistanceToolConfig";
+import { SignatureRequestListView } from "../../../../../definitions/fci/SignatureRequestListView";
 
 const FciSignatureRequestsScreen = () => {
   const dispatch = useIODispatch();
   const dataItems = useIOSelector(fciSignaturesListSelector);
+  const assistanceToolConfig = useIOSelector(assistanceToolConfigSelector);
+  const choosenTool = assistanceToolRemoteConfig(assistanceToolConfig);
+
+  const zendeskAssistanceLogAndStart = (
+    signatureRequestId: SignatureRequestListView["id"]
+  ) => {
+    resetCustomFields();
+    addTicketCustomField(zendeskCategoryId, zendeskFCICategory.value);
+    // Append the signatureRequestID in the log
+    appendLog(JSON.stringify(signatureRequestId));
+    dispatch(
+      zendeskSupportStart({
+        startingRoute: "n/a",
+        assistanceForPayment: false,
+        assistanceForCard: false,
+        assistanceForFci: true
+      })
+    );
+    dispatch(zendeskSelectedCategory(zendeskFCICategory));
+  };
+
+  const handleAskAssistance = (
+    signatureRequestId: SignatureRequestListView["id"]
+  ) => {
+    switch (choosenTool) {
+      case ToolEnum.zendesk:
+        zendeskAssistanceLogAndStart(signatureRequestId);
+        break;
+    }
+  };
 
   React.useEffect(() => {
     dispatch(fciSignaturesListRequest.request());
@@ -29,7 +73,10 @@ const FciSignatureRequestsScreen = () => {
       keyExtractor={(_, index) => `${index}`}
       testID={"FciSignatureRequestsListTestID"}
       renderItem={({ item }) => (
-        <SignatureRequestItem item={item} onPress={constNull} />
+        <SignatureRequestItem
+          item={item}
+          onPress={() => handleAskAssistance(item.id)}
+        />
       )}
     />
   );
