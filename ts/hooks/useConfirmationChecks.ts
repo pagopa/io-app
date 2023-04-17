b17f36a69a4b1f09e957d97e2338a3cbe5ccee18
Federico Mastrini <federicomastrini93@gmail.com>
2023-04-17 10:28:37 +0200

    chore: removed braces

--- ts/hooks/useConfirmationChecks.ts
+++ ts/hooks/useConfirmationChecks.ts
@@ -27,13 +27,12 @@ export const useConfirmationChecks = (length: number): ConfirmationChecks => {
    * Function that updates the `values` state using the current state and the boolean value that's being toggled at the given index.
    * @param atIndex - The zero-based index in the `values` array that needs to be toggled.
    */
-  const toggle = (atIndex: number): void => {
+  const toggle = (atIndex: number): void =>
     setValues(current => [
       ...current.slice(0, atIndex),
       !current[atIndex],
       ...current.slice((atIndex as number) + 1)
     ]);
-  };
 
   return { values, toggle, areFulfilled };
 };
