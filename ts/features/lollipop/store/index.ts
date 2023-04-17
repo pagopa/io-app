7e0db5b9e485741b003382777cd83af1f7cf33c7
Mario Perrotta <mario.perrotta@pagopa.it>
2023-04-17 22:37:38 +0200

    refactor: add fp-ts version

--- ts/features/lollipop/store/index.ts
+++ ts/features/lollipop/store/index.ts
@@ -8,11 +8,61 @@ import {
   PersistPartial,
   persistReducer
 } from "redux-persist";
+import { pipe } from "fp-ts/lib/function";
 import { Action } from "../../../store/actions/types";
 import { isDevEnv } from "../../../utils/environment";
 import lollipopReducer, { LollipopState } from "./reducers/lollipop";
 
 export const CURRENT_REDUX_LOLLIPOP_STORE_VERSION = 0;
+
+export const migrationKeyTag = (
+  state: PersistedState
+): PersistedLollipopState => {
+  const persistedLS = state as PersistedLollipopState;
+  const keyTagOption = persistedLS.keyTag;
+  if (O.isSome(keyTagOption) && typeof keyTagOption.value !== "string") {
+    const innerOption = keyTagOption.value as O.Option<string>;
+    if (O.isSome(innerOption)) {
+      return {
+        ...state,
+        keyTag: O.some(innerOption.value)
+      } as PersistedLollipopState;
+    } else {
+      return {
+        ...state,
+        keyTag: O.none
+      } as PersistedLollipopState;
+    }
+  }
+  return state as PersistedLollipopState;
+};
+
+export const migrationKeyTagFunctional = (
+  state: PersistedState
+): PersistedLollipopState =>
+  pipe(
+    (state as PersistedLollipopState).keyTag as O.Option<O.Option<string>>,
+    O.filter(keyTag => typeof keyTag !== "string"),
+    O.fold(
+      () => state as PersistedLollipopState,
+      optionKeyTag =>
+        pipe(
+          optionKeyTag,
+          O.fold(
+            () =>
+              ({
+                ...state,
+                keyTag: O.none
+              } as PersistedLollipopState),
+            keyTg =>
+              ({
+                ...state,
+                keyTag: O.some(keyTg)
+              } as PersistedLollipopState)
+          )
+        )
+    )
+  );
 
 const migrations: MigrationManifest = {
   // Version 0
@@ -30,25 +80,8 @@ const migrations: MigrationManifest = {
       publicKey: O.none
     };
   },
-  "1": (state: PersistedState): PersistedLollipopState => {
-    const persistedLS = state as PersistedLollipopState;
-    const keyTagOption = persistedLS.keyTag;
-    if (O.isSome(keyTagOption) && typeof keyTagOption.value !== "string") {
-      const innerOption = keyTagOption.value as O.Option<string>;
-      if (O.isSome(innerOption)) {
-        return {
-          ...state,
-          keyTag: O.some(innerOption.value)
-        } as PersistedLollipopState;
-      } else {
-        return {
-          ...state,
-          keyTag: O.none
-        } as PersistedLollipopState;
-      }
-    }
-    return state as PersistedLollipopState;
-  }
+  "1": (state: PersistedState): PersistedLollipopState =>
+    migrationKeyTagFunctional(state)
 };
 
 export type PersistedLollipopState = LollipopState & PersistPartial;
