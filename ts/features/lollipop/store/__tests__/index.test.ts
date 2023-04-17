e76fffdeb78ca136dd0410497c8a3d06c1c5786f
Mario Perrotta <mario.perrotta@pagopa.it>
2023-04-17 22:38:15 +0200

    test: add unit for keytag

---
+++ ts/features/lollipop/store/__tests__/index.test.ts
@@ -0,0 +1,128 @@
+import * as O from "fp-ts/lib/Option";
+import { migrationKeyTag, migrationKeyTagFunctional } from "..";
+
+describe("Test migration KeyTag", () => {
+  describe("Test migrationKeyTag()", () => {
+    it("should return a keyTag as O.some(string) when keyTagOption is O.some(string)", () => {
+      const persistedState: any = {
+        keyTag: O.some("tag"),
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTag(persistedState).keyTag).toEqual(O.some("tag"));
+    });
+
+    it("should return a keyTag O.some(string) when keyTagOption is an Option of Option and is a string", () => {
+      const persistedState = {
+        keyTag: O.some(O.some("tag")),
+        publicKey: O.none
+      } as any;
+
+      expect(migrationKeyTag(persistedState).keyTag).toEqual(O.some("tag"));
+    });
+
+    it("should return a keyTag equal to O.none when keyTagOption is not Some", () => {
+      const persistedState: any = {
+        keyTag: O.some(O.none),
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTag(persistedState).keyTag).toEqual(O.none);
+    });
+
+    it("should return the original state when keyTagOption is None", () => {
+      const persistedState: any = {
+        keyTag: O.none,
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTag(persistedState).keyTag).toEqual(O.none);
+    });
+  });
+  describe("Test migrationKeyTagFunctional", () => {
+    it("should return a keyTag as O.some(string) when keyTagOption is O.some(string)", () => {
+      const persistedState: any = {
+        keyTag: O.some("tag"),
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
+        O.some("tag")
+      );
+    });
+
+    it("should return a keyTag equal to O.none when keyTagOption is not Some", () => {
+      const persistedState = {
+        keyTag: O.some(O.some("tag")),
+        publicKey: O.none
+      } as any;
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
+        O.some("tag")
+      );
+    });
+
+    it("should return a keyTag equal to O.none when keyTagOption is not Some", () => {
+      const persistedState: any = {
+        keyTag: O.some(O.none),
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(O.none);
+    });
+
+    it("should return the original state when keyTagOption is None", () => {
+      const persistedState: any = {
+        keyTag: O.none,
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(O.none);
+    });
+  });
+  describe("Test migrationKeyTag* method comparison", () => {
+    it("should return the same result when keyTagOption is O.some(string) for all methods", () => {
+      const persistedState: any = {
+        keyTag: O.some("tag"),
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
+        migrationKeyTag(persistedState).keyTag
+      );
+    });
+
+    it("should return the same result when keyTagOption is Some of Some and is a string for all methods", () => {
+      const persistedState = {
+        keyTag: O.some(O.some("tag")),
+        publicKey: O.none
+      } as any;
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
+        migrationKeyTag(persistedState).keyTag
+      );
+    });
+
+    it("should return the same result when keyTagOption is O.some(O.none) for all methods", () => {
+      const persistedState: any = {
+        keyTag: O.some(O.none),
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
+        migrationKeyTag(persistedState).keyTag
+      );
+    });
+
+    it("should return the original state when keyTagOption is None for all methods", () => {
+      const persistedState: any = {
+        keyTag: O.none,
+        publicKey: O.none
+      };
+
+      expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
+        migrationKeyTag(persistedState).keyTag
+      );
+    });
+  });
+});
