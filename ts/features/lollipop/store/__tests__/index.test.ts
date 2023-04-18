1ccdd9a181b623a4b13e83dee993f06445563002
Mario Perrotta <mario.perrotta@pagopa.it>
2023-04-18 07:16:46 +0200

    chore: update

--- ts/features/lollipop/store/__tests__/index.test.ts
+++ ts/features/lollipop/store/__tests__/index.test.ts
@@ -1,13 +1,17 @@
 import * as O from "fp-ts/lib/Option";
-import { migrationKeyTag, migrationKeyTagFunctional } from "..";
+import {
+  PersistedLollipopState,
+  migrationKeyTag,
+  migrationKeyTagFunctional
+} from "..";
 
 describe("Test migration KeyTag", () => {
   describe("Test migrationKeyTag()", () => {
     it("should return a keyTag as O.some(string) when keyTagOption is O.some(string)", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.some("tag"),
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTag(persistedState).keyTag).toEqual(O.some("tag"));
     });
@@ -16,35 +20,35 @@ describe("Test migration KeyTag", () => {
       const persistedState = {
         keyTag: O.some(O.some("tag")),
         publicKey: O.none
-      } as any;
+      } as PersistedLollipopState;
 
       expect(migrationKeyTag(persistedState).keyTag).toEqual(O.some("tag"));
     });
 
     it("should return a keyTag equal to O.none when keyTagOption is not Some", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.some(O.none),
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTag(persistedState).keyTag).toEqual(O.none);
     });
 
     it("should return the original state when keyTagOption is None", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.none,
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTag(persistedState).keyTag).toEqual(O.none);
     });
   });
   describe("Test migrationKeyTagFunctional", () => {
     it("should return a keyTag as O.some(string) when keyTagOption is O.some(string)", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.some("tag"),
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
         O.some("tag")
@@ -55,7 +59,7 @@ describe("Test migration KeyTag", () => {
       const persistedState = {
         keyTag: O.some(O.some("tag")),
         publicKey: O.none
-      } as any;
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
         O.some("tag")
@@ -63,29 +67,29 @@ describe("Test migration KeyTag", () => {
     });
 
     it("should return a keyTag equal to O.none when keyTagOption is not Some", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.some(O.none),
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(O.none);
     });
 
     it("should return the original state when keyTagOption is None", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.none,
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(O.none);
     });
   });
   describe("Test migrationKeyTag* method comparison", () => {
     it("should return the same result when keyTagOption is O.some(string) for all methods", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.some("tag"),
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
         migrationKeyTag(persistedState).keyTag
@@ -96,7 +100,7 @@ describe("Test migration KeyTag", () => {
       const persistedState = {
         keyTag: O.some(O.some("tag")),
         publicKey: O.none
-      } as any;
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
         migrationKeyTag(persistedState).keyTag
@@ -104,10 +108,10 @@ describe("Test migration KeyTag", () => {
     });
 
     it("should return the same result when keyTagOption is O.some(O.none) for all methods", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.some(O.none),
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
         migrationKeyTag(persistedState).keyTag
@@ -115,10 +119,10 @@ describe("Test migration KeyTag", () => {
     });
 
     it("should return the original state when keyTagOption is None for all methods", () => {
-      const persistedState: any = {
+      const persistedState = {
         keyTag: O.none,
         publicKey: O.none
-      };
+      } as PersistedLollipopState;
 
       expect(migrationKeyTagFunctional(persistedState).keyTag).toEqual(
         migrationKeyTag(persistedState).keyTag
