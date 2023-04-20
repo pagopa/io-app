75109800dcb42af56f5f315452cbd5fc7c78e6fc
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-04-20 17:17:06 +0200

    Refactoring

--- ts/features/lollipop/playgrounds/LollipopPlayground.tsx
+++ ts/features/lollipop/playgrounds/LollipopPlayground.tsx
@@ -1,20 +1,10 @@
 import * as O from "fp-ts/lib/Option";
 import * as E from "fp-ts/lib/Either";
 import React, { useCallback } from "react";
-import {
-  View,
-  SafeAreaView,
-  StyleSheet,
-  TextInput,
-  ScrollView
-} from "react-native";
-import { HSpacer, VSpacer } from "../../../components/core/spacer/Spacer";
+import { SafeAreaView, ScrollView } from "react-native";
 import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
-import { maybeNotNullyString } from "../../../utils/strings";
 import { IOStyles } from "../../../components/core/variables/IOStyles";
 import { ContentWrapper } from "../../../components/core/ContentWrapper";
-import ButtonSolid from "../../../components/ui/ButtonSolid";
-import ButtonOutline from "../../../components/ui/ButtonOutline";
 import { sessionTokenSelector } from "../../../store/reducers/authentication";
 import { LollipopClient, createLollipopClient } from "../api/backend";
 import { useIOSelector } from "../../../store/hooks";
@@ -25,9 +15,6 @@ import {
 import { toThumbprint } from "../utils/crypto";
 import { DEFAULT_LOLLIPOP_HASH_ALGORITHM_SERVER } from "../utils/login";
 import { apiUrlPrefix } from "../../../config";
-import { CheckBox } from "../../../components/core/selection/checkbox/CheckBox";
-import { Label } from "../../../components/core/typography/Label";
-import { Alert } from "../../../components/Alert";
 import { SignMessageResponse } from "../../../../definitions/lollipop/SignMessageResponse";
 import { LollipopOriginalURL } from "../../../../definitions/lollipop/LollipopOriginalURL";
 import { LollipopContentDigest } from "../../../../definitions/lollipop/LollipopContentDigest";
@@ -35,31 +22,7 @@ import { LollipopSignatureInput } from "../../../../definitions/lollipop/Lollipo
 import { LollipopSignature } from "../../../../definitions/lollipop/LollipopSignature";
 import { LollipopMethodEnum } from "../../../../definitions/lollipop/LollipopMethod";
 import { ProblemJson } from "../../../../definitions/lollipop/ProblemJson";
-
-const styles = StyleSheet.create({
-  textInput: {
-    textAlignVertical: "top",
-    padding: 10,
-    borderWidth: 1,
-    height: 120,
-    borderRadius: 4
-  },
-  column: {
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "stretch"
-  },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "stretch"
-  },
-  rowStart: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "stretch"
-  }
-});
+import LollipopPlaygroundContent from "./LollipopPlaygroundContent";
 
 type LollipopSignRequestBody = {
   message: string;
@@ -79,15 +42,20 @@ const signMessage = async (
     signature: "" as LollipopSignature
   });
 
+export type LollipopPlaygroundState = {
+  doSignBody: boolean;
+  isVerificationSuccess: boolean | undefined;
+  signResponse: string | undefined;
+};
+const INITIAL_STATE = {
+  doSignBody: true,
+  isVerificationSuccess: undefined,
+  signResponse: undefined
+};
+
 const LollipopPlayground = () => {
-  const viewRef = React.createRef<View>();
-  const [httpRequestBodyText, setHttpRequestBodyText] =
-    React.useState<string>("");
-  const [doSignBody, setDoSignBody] = React.useState<boolean>(true);
-  const [isVerificationSuccess, setIsVerificationSuccess] = React.useState<
-    boolean | undefined
-  >(undefined);
-  const [signResponse, setSignResponse] = React.useState<string>();
+  const [state, setState] =
+    React.useState<LollipopPlaygroundState>(INITIAL_STATE);
 
   const keyTag = useIOSelector(lollipopKeyTagSelector);
   const maybePublicKey = useIOSelector(lollipopPublicKeySelector);
@@ -104,7 +72,7 @@ const LollipopPlayground = () => {
               maybePublicKey
             )}`
           },
-          { nonce: "aNonce", signBody: doSignBody }
+          { nonce: "aNonce", signBody: state.doSignBody }
         )
       : undefined;
 
@@ -124,78 +92,58 @@ const LollipopPlayground = () => {
             const status = signResponse.right.status;
             if (status !== 200) {
               const response = signResponse.right.value as ProblemJson;
-              setIsVerificationSuccess(false);
-              setSignResponse(
-                `${status} - ${response.title}${
+              setState({
+                ...state,
+                isVerificationSuccess: false,
+                signResponse: `${status} - ${response.title}${
                   response.detail ? "\n" + response.detail : ""
                 }`
-              );
+              });
             } else {
               const response = signResponse.right.value as SignMessageResponse;
-              setIsVerificationSuccess(true);
-              setSignResponse(response.response);
+              setState({
+                ...state,
+                isVerificationSuccess: true,
+                signResponse: response.response
+              });
             }
           } else {
-            setIsVerificationSuccess(false);
-            setSignResponse(JSON.stringify(signResponse.left));
+            setState({
+              ...state,
+              isVerificationSuccess: false,
+              signResponse: JSON.stringify(signResponse.left)
+            });
           }
         } catch (e) {
-          setIsVerificationSuccess(false);
-          setSignResponse(JSON.stringify(e));
+          setState({
+            ...state,
+            isVerificationSuccess: false,
+            signResponse: JSON.stringify(e)
+          });
         }
       }
     },
-    [lollipopClient, maybeSessionToken]
+    [lollipopClient, maybeSessionToken, state]
   );
 
-  const isMessageBodySet = O.isSome(maybeNotNullyString(httpRequestBodyText));
   return (
     <BaseScreenComponent goBack={true} headerTitle={"Lollipop Playground"}>
       <SafeAreaView style={IOStyles.flex}>
         <ScrollView>
           <ContentWrapper>
-            <View style={styles.column}>
-              <TextInput
-                multiline={true}
-                placeholder={"paste here your body message"}
-                style={styles.textInput}
-                onChangeText={setHttpRequestBodyText}
-                value={httpRequestBodyText}
-              />
-              <VSpacer size={16} />
-              <View style={styles.rowStart}>
-                <CheckBox checked={doSignBody} onValueChange={setDoSignBody} />
-                <HSpacer />
-                <Label>{"Sign body"}</Label>
-              </View>
-              <VSpacer size={16} />
-              <View style={styles.row}>
-                <ButtonSolid
-                  accessibilityLabel="Sign body message"
-                  label={`Sign message${doSignBody ? " with body" : ""}`}
-                  disabled={!isMessageBodySet}
-                  onPress={() => onSignButtonPress(httpRequestBodyText)}
-                />
-                <HSpacer size={16} />
-                <ButtonOutline
-                  accessibilityLabel="Clear"
-                  label={"Clear"}
-                  disabled={!isMessageBodySet}
-                  onPress={() => {
-                    setIsVerificationSuccess(undefined);
-                    setHttpRequestBodyText("");
-                  }}
-                />
-              </View>
-              <VSpacer size={16} />
-              {isVerificationSuccess !== undefined && (
-                <Alert
-                  viewRef={viewRef}
-                  variant={isVerificationSuccess ? "success" : "error"}
-                  content={signResponse ?? ""}
-                />
-              )}
-            </View>
+            <LollipopPlaygroundContent
+              onSignButtonPress={onSignButtonPress}
+              onCheckBoxPress={v => {
+                setState({
+                  ...state,
+                  doSignBody: v
+                });
+              }}
+              onClearButtonPress={() => {
+                setState(INITIAL_STATE);
+              }}
+              playgroundState={state}
+            />
           </ContentWrapper>
         </ScrollView>
       </SafeAreaView>
