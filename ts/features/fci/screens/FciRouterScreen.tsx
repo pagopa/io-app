7c3be697236c0cb59dbf3bae0303b7fe38f832f9
Mario Perrotta <mario.perrotta@pagopa.it>
2023-04-21 10:50:03 +0200

    chore: add status error

--- ts/features/fci/screens/FciRouterScreen.tsx
+++ ts/features/fci/screens/FciRouterScreen.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import { constNull } from "fp-ts/lib/function";
+import { constNull, pipe } from "fp-ts/lib/function";
 import * as pot from "@pagopa/ts-commons/lib/pot";
+import * as E from "fp-ts/lib/Either";
+import * as O from "fp-ts/lib/Option";
 import I18n from "../../../i18n";
+import doubt from "../../../../img/pictograms/doubt.png";
 import { SignatureRequestDetailView } from "../../../../definitions/fci/SignatureRequestDetailView";
 import { IOStackNavigationRouteProps } from "../../../navigation/params/AppParamsList";
 import { useIODispatch, useIOSelector } from "../../../store/hooks";
@@ -12,6 +15,9 @@ import { LoadingErrorComponent } from "../../bonus/bonusVacanze/components/loadi
 import SuccessComponent from "../components/SuccessComponent";
 import GenericErrorComponent from "../components/GenericErrorComponent";
 import { withValidatedEmail } from "../../../components/helpers/withValidatedEmail";
+import { GenericError, NetworkError } from "../../../utils/errors";
+import { ProblemJson } from "../../../../definitions/fci/ProblemJson";
+import ErrorComponent from "../components/ErrorComponent";
 
 export type FciRouterScreenNavigationParams = Readonly<{
   signatureRequestId: SignatureRequestDetailView["id"];
@@ -37,25 +43,55 @@ const FciSignatureScreen = (
     />
   );
 
-  const ErrorComponent = () => (
-    <GenericErrorComponent
-      title={I18n.t("features.fci.errors.generic.default.title")}
-      subTitle={I18n.t("features.fci.errors.generic.default.subTitle")}
-      onPress={() => dispatch(fciEndRequest())}
-      testID="GenericErrorComponentTestID"
-    />
-  );
+  const GenericError = (status?: ProblemJson["status"]) => {
+    if (status === 404) {
+      return (
+        <ErrorComponent
+          title={I18n.t("features.fci.errors.generic.wrongUser.title")}
+          subTitle={I18n.t("features.fci.errors.generic.wrongUser.subTitle")}
+          image={doubt}
+          onPress={() => dispatch(fciEndRequest())}
+          testID="WrongUserErrorComponentTestID"
+        />
+      );
+    }
+
+    return (
+      <GenericErrorComponent
+        title={I18n.t("features.fci.errors.generic.default.title")}
+        subTitle={I18n.t("features.fci.errors.generic.default.subTitle")}
+        onPress={() => dispatch(fciEndRequest())}
+        testID="GenericErrorComponentTestID"
+      />
+    );
+  };
+
+  const renderErrorComponent = (error?: NetworkError) =>
+    pipe(
+      error,
+      O.fromNullable,
+      O.filter(e => e.kind === "generic"),
+      O.map(e => JSON.parse((e as GenericError).value.message)),
+      O.map(ProblemJson.decode),
+      O.map(
+        E.fold(
+          () => GenericError(),
+          problemJson => GenericError(problemJson.status)
+        )
+      ),
+      O.getOrElse(() => GenericError())
+    );
 
   return pot.fold(
     fciSignatureRequest,
     () => <LoadingComponent />,
     () => <LoadingComponent />,
     () => <LoadingComponent />,
-    _ => <ErrorComponent />,
+    renderErrorComponent,
     b => <SuccessComponent signatureRequest={b} />,
     () => <LoadingComponent />,
     () => <LoadingComponent />,
-    _ => <ErrorComponent />
+    () => renderErrorComponent()
   );
 };
 
