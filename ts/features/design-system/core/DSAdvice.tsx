b38a67acb110171428685baff0dee1a26f8a7854
Damiano Plebani <damiano.plebani@pagopa.it>
2023-04-28 15:17:00 +0200

    Move some legacy components from `DSAlert` to `DSAdvice`

--- ts/features/design-system/core/DSAdvice.tsx
+++ ts/features/design-system/core/DSAdvice.tsx
@@ -13,6 +13,12 @@ import { Label } from "../../../components/core/typography/Label";
 import { ActivateBonusReminder } from "../../bonus/bonusVacanze/screens/activation/request/ActivateBonusReminder";
 import { DesignSystemScreen } from "../components/DesignSystemScreen";
 import { VSpacer } from "../../../components/core/spacer/Spacer";
+import AdviceComponent from "../../../components/AdviceComponent";
+import { Body } from "../../../components/core/typography/Body";
+import { H2 } from "../../../components/core/typography/H2";
+import { InfoScreenComponent } from "../../../components/infoScreen/InfoScreenComponent";
+import { Icon } from "../../../components/core/icons";
+import { H5 } from "../../../components/core/typography/H5";
 
 const styles = StyleSheet.create({
   content: {
@@ -27,6 +33,47 @@ const styles = StyleSheet.create({
 
 export const DSAdvice = () => (
   <DesignSystemScreen title={"Advice & Banners"}>
+    <VSpacer size={16} />
+    <H2 color={"bluegrey"} weight={"SemiBold"} style={{ marginBottom: 16 }}>
+      Legacy components
+    </H2>
+    <AdviceComponent
+      text={
+        "Dopo questo passaggio non sarà più possibile annullare il pagamento."
+      }
+    />
+    <VSpacer size={16} />
+    <View style={[styles.content, IOStyles.horizontalContentPadding]}>
+      <InfoBox>
+        <Body>
+          Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut
+          aliquid ex ea commodi consequatur.
+        </Body>
+      </InfoBox>
+    </View>
+    <InfoBox
+      alignedCentral={true}
+      iconSize={24}
+      iconColor={IOColors.bluegreyDark}
+    >
+      <H5 weight={"Regular"}>
+        {
+          "Per verificare la tua carta, tratteniamo € 0.02. Non preoccuparti: ti restituiremo l'importo al più presto."
+        }
+      </H5>
+    </InfoBox>
+    <InfoScreenComponent
+      image={<Icon name="info" />}
+      title={"Title"}
+      body={
+        <Body style={{ textAlign: "center" }}>
+          Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod
+          tempor incidunt ut labore et dolore magna aliqua.
+        </Body>
+      }
+    />
     <VSpacer size={16} />
     <DSFullWidthComponent>
       <PaymentBannerComponent
