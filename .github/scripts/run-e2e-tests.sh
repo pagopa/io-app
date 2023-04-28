90fdca4c48032dcf2e1f0a91625e4c5b4b90f076
Cristiano Tofani <cri.tofani@gmail.com>
2023-04-28 18:27:01 +0200

    migrate e2e to ga

---
+++ .github/scripts/run-e2e-tests.sh
@@ -0,0 +1,21 @@
+git clone git@github.com:pagopa/io-dev-api-server.git --branch master --single-branch _io-dev-api-server_
+cd _io-dev-api-server_
+cp ../scripts/api-config.json config/config.json
+yarn
+yarn generate
+yarn start &> /tmp/io-dev-api-server.log &
+# wait for the server to be up and running
+sleep 10
+cd ..
+yarn detox clean-framework-cache
+yarn detox build-framework-cache
+yarn detox test \
+  --loglevel verbose \
+  -c ios.sim.release \
+  --cleanup \
+  --artifacts-location /tmp/e2e_artifacts/detox/ \
+  --record-logs all \
+  --take-screenshots all \
+  --record-videos failing \
+  --debug-synchronization 1000 \
+  --retries 3
