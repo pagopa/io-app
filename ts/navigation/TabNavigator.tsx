b97e6f1e9381ec5c1df9339af0704ec676cc2f37
Damiano Plebani <damiano.plebani@pagopa.it>
2023-05-02 09:47:31 +0200

    Remove `soon` from comment because it could raise expectations

--- ts/navigation/TabNavigator.tsx
+++ ts/navigation/TabNavigator.tsx
@@ -94,7 +94,9 @@ export const MainTabNavigator = () => {
       {/* This item refers to the new "Scan" feature that allows
       citizens to quickly manage a payment or signature request
       from a printed paper and/or a third-party website.
-      It will be activated soon with a specific screen layout. */}
+      It will be activated with a specific screen layout.
+      It differs from the existing "Scan QR Code" because
+      it's not limited to pagoPA payments. */}
       {/* <Tab.Screen
         name={ROUTES.PAYMENT_SCAN_QR_CODE}
         component={ScanQrCodeScreen}
