2cbdc7990249e6d84acda81df77db2f289de194d
Federico Mastrini <federicomastrini93@gmail.com>
2023-05-03 18:17:26 +0200

    chore: criteria mapping

--- ts/features/idpay/onboarding/components/PDNDCriteriaItem.tsx
+++ ts/features/idpay/onboarding/components/PDNDCriteriaItem.tsx
@@ -4,6 +4,7 @@ import { pipe } from "fp-ts/lib/function";
 import React from "react";
 import {
   CodeEnum,
+  OperatorEnum,
   PDNDCriteriaDTO
 } from "../../../../../definitions/idpay/PDNDCriteriaDTO";
 import ButtonExtendedOutline from "../../../../components/ui/ButtonExtendedOutline";
@@ -15,36 +16,45 @@ type Props = {
   onPress: () => void;
 };
 
+const getValueString = (code: CodeEnum, value?: string) => {
+  switch (code) {
+    case CodeEnum.ISEE:
+      return pipe(
+        NumberFromString.decode(value),
+        O.fromEither,
+        O.map(value => formatNumberAmount(value)),
+        O.map(valueString => `${valueString} €`),
+        O.getOrElse(() => "-")
+      );
+    default:
+      return value;
+  }
+};
+
 const PDNDCriteriaItem = (props: Props) => {
   const { criteria, onPress } = props;
-  const { code, operator, value } = criteria;
+  const { code, operator, value, value2 } = criteria;
 
-  const operatorString = pipe(
+  const firstValue = getValueString(code, value);
+  const secondValue = getValueString(code, value2);
+
+  const description = pipe(
     operator,
     O.fromNullable,
-    O.map(op => I18n.t(`idpay.onboarding.PDNDPrerequisites.operator.${op}`)),
-    O.getOrElse(() => "")
+    O.alt(() => O.some(OperatorEnum.EQ)),
+    O.map(op =>
+      I18n.t(`idpay.onboarding.PDNDPrerequisites.operator.${op}`, {
+        value: firstValue,
+        value2: secondValue
+      })
+    ),
+    O.getOrElse(() => "-")
   );
-
-  const getValueString = () => {
-    switch (code) {
-      case CodeEnum.ISEE:
-        return pipe(
-          NumberFromString.decode(value),
-          O.fromEither,
-          O.map(value => formatNumberAmount(value)),
-          O.map(valueString => `${operatorString} ${valueString} €`),
-          O.getOrElse(() => "-")
-        );
-      default:
-        return value;
-    }
-  };
 
   return (
     <ButtonExtendedOutline
       label={I18n.t(`idpay.onboarding.PDNDPrerequisites.code.${code}`)}
-      description={getValueString()}
+      description={description}
       onPress={onPress}
     />
   );
