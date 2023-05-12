d9d63c0e39406b95fa4c8c3ee8e5910dd53ec66e
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-05-12 15:19:44 +0200

    updated comment

--- ts/sagas/removePersistedStates.ts
+++ ts/sagas/removePersistedStates.ts
@@ -5,10 +5,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 // because it is no longer necessary, the states
 // must be manually removed from AsyncStorage
 // because Rexud-persist keeps a copy of
-// state in storage engine.
+// the state in the storage engine.
 // If an error occurs while deleting, because the key is
-// invalid or not present, it is not necessary
-// to handle the exception
+// invalid or not present, it is not necessary to handle
+// the exception because Rexud-persist doesn't read files
+// from storage engine since the persistedReducer
+// has been removed
 export function* removePersistedStatesSaga() {
   const keys = ["persist:mvl", "persist:pn"];
 
