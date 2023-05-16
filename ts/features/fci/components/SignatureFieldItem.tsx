0f0d26f0f5a62e9f971027d1718b99d3262f9fe3
Damiano Plebani <damiano.plebani@pagopa.it>
2023-05-16 11:42:35 +0200

    Update jest snapshots

--- ts/features/fci/components/SignatureFieldItem.tsx
+++ ts/features/fci/components/SignatureFieldItem.tsx
@@ -70,7 +70,7 @@ const SignatureFieldItem = (props: Props) => {
                 ? "grey"
                 : "bluegreyDark"
             }
-            size={20}
+            size={24}
           />
         </TouchableDefaultOpacity>
       </View>
