51ec8b85d12aab145115f28a36285cfc4d264c07
Damiano Plebani <damiano.plebani@pagopa.it>
2023-05-23 09:27:39 +0200

    Add SVG checkmark, improve easing

--- ts/components/core/selection/checkbox/AnimatedCheckBox.tsx
+++ ts/components/core/selection/checkbox/AnimatedCheckBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View, Pressable, PressableProps } from "react-native";
 import Animated, {
   useSharedValue,
@@ -7,9 +7,9 @@ import Animated, {
   Easing,
   interpolate
 } from "react-native-reanimated";
+import Svg, { Path } from "react-native-svg";
 import { calculateSlop } from "../../accessibility";
 import { IOColors } from "../../variables/IOColors";
-import { Icon } from "../../icons/Icon";
 
 type Props = {
   // the value of the checkbox
@@ -21,10 +21,12 @@ type OwnProps = Props & Pick<PressableProps, "disabled" | "onPress">;
 const SIZE: number = 24;
 const BORDER_WIDTH: number = 2;
 
-const offColor: IOColors = "grey-450";
+const offColor: IOColors = "grey-650";
 const onColor: IOColors = "blueIO-500";
 const slop = calculateSlop(SIZE);
-const tickSize = SIZE;
+
+const checkMarkPath =
+  "M18.7071 8.29289c.3905.39053.3905 1.02369 0 1.41422L12.4142 16c-.781.781-2.0474.7811-2.82841 0l-3.2929-3.2929c-.39052-.3905-.39052-1.0237 0-1.4142.39053-.3905 1.02369-.3905 1.41422 0L11 14.5858l6.2929-6.29291c.3905-.39052 1.0237-.39052 1.4142 0Z";
 
 const styles = StyleSheet.create({
   checkBox: {
@@ -37,7 +39,7 @@ const styles = StyleSheet.create({
     top: 0,
     width: SIZE,
     height: SIZE,
-    borderColor: IOColors.blue,
+    borderColor: IOColors[offColor],
     borderWidth: BORDER_WIDTH,
     borderRadius: 4
   },
@@ -50,9 +52,6 @@ const styles = StyleSheet.create({
     borderRadius: 4
   }
 });
-
-const getBorderColor = (value: boolean) =>
-  value ? IOColors[onColor] : IOColors[offColor];
 
 /**
  * A raw checkbox that follows the style guidelines without any state logic.
@@ -69,13 +68,13 @@ export const AnimatedCheckbox = ({ checked, onPress, disabled }: OwnProps) => {
   useEffect(() => {
     // eslint-disable-next-line functional/immutable-data
     animationProgress.value = withTiming(checked ? 1 : 0, {
-      duration: 300,
-      easing: Easing.elastic(1.5)
+      duration: 350,
+      easing: Easing.elastic(1.25)
     });
   }, [checked, animationProgress]);
 
   const boxStyle = useAnimatedStyle(() => {
-    const scale = interpolate(animationProgress.value, [0, 1], [0.8, 1]);
+    const scale = interpolate(animationProgress.value, [0, 1], [0.6, 1]);
     const opacity = animationProgress.value;
 
     return {
@@ -92,14 +91,7 @@ export const AnimatedCheckbox = ({ checked, onPress, disabled }: OwnProps) => {
       hitSlop={{ bottom: slop, left: slop, right: slop, top: slop }}
       style={styles.checkBox}
     >
-      <View
-        style={[
-          styles.checkboxBorder,
-          {
-            borderColor: getBorderColor(isChecked)
-          }
-        ]}
-      />
+      <View style={styles.checkboxBorder} />
       <Animated.View
         style={[
           styles.checkBoxInner,
@@ -109,7 +101,9 @@ export const AnimatedCheckbox = ({ checked, onPress, disabled }: OwnProps) => {
       />
       {isChecked && (
         <View>
-          <Icon name="completed" size={tickSize} color="white" />
+          <Svg viewBox="0 0 24 24">
+            <Path d={checkMarkPath} fill={IOColors.white} />
+          </Svg>
         </View>
       )}
     </Pressable>
