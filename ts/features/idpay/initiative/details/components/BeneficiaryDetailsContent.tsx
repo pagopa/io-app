43f2b0d6c9931acde01015e2da42c3b748798a62
Federico Mastrini <federicomastrini93@gmail.com>
2023-05-23 12:51:57 +0200

    refactor: moved components in its own files (w/ @forrest57)

---
+++ ts/features/idpay/initiative/details/components/BeneficiaryDetailsContent.tsx
@@ -0,0 +1,212 @@
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/function";
+import React from "react";
+import { View } from "react-native";
+import Placeholder from "rn-placeholder";
+import { InitiativeDTO } from "../../../../../../definitions/idpay/InitiativeDTO";
+import { InitiativeDetailDTO } from "../../../../../../definitions/idpay/InitiativeDetailDTO";
+import { VSpacer } from "../../../../../components/core/spacer/Spacer";
+import { LabelSmall } from "../../../../../components/core/typography/LabelSmall";
+import { IOStyles } from "../../../../../components/core/variables/IOStyles";
+import I18n from "../../../../../i18n";
+import { format } from "../../../../../utils/dates";
+import { formatNumberAmount } from "../../../../../utils/stringBuilder";
+import { Table } from "../../../common/components/Table";
+import {
+  InitiativeRulesInfoBox,
+  InitiativeRulesInfoBoxSkeleton
+} from "./InitiativeRulesInfoBox";
+
+type Props = {
+  initiativeDetails: InitiativeDTO;
+  beneficiaryDetails: InitiativeDetailDTO;
+};
+
+const formatNumberCurrency = (amount: number) =>
+  `${formatNumberAmount(amount, false)} €`;
+
+const formatDate = (fmt: string) => (date: Date) => format(date, fmt);
+
+const BeneficiaryDetailsContent = (props: Props) => {
+  const { initiativeDetails, beneficiaryDetails } = props;
+
+  const ruleInfoBox = pipe(
+    beneficiaryDetails.ruleDescription,
+    O.fromNullable,
+    O.fold(
+      () => undefined,
+      info => <InitiativeRulesInfoBox content={info} />
+    )
+  );
+
+  const statusString = pipe(
+    initiativeDetails.status,
+    O.fromNullable,
+    O.map(status =>
+      I18n.t(`idpay.initiative.details.initiativeCard.statusLabels.${status}`)
+    ),
+    O.getOrElse(() => "-")
+  );
+
+  const endDateString = pipe(
+    beneficiaryDetails.endDate,
+    O.fromNullable,
+    O.map(formatDate("DD/MM/YYYY")),
+    O.getOrElse(() => "-")
+  );
+
+  const availableAmountString = pipe(
+    initiativeDetails.amount,
+    O.fromNullable,
+    O.map(formatNumberCurrency),
+    O.getOrElse(() => "-")
+  );
+
+  const accuredAmountString = pipe(
+    initiativeDetails.accrued,
+    O.fromNullable,
+    O.map(formatNumberCurrency),
+    O.getOrElse(() => "-")
+  );
+
+  const refundedAmountString = pipe(
+    initiativeDetails.refunded,
+    O.fromNullable,
+    O.map(formatNumberCurrency),
+    O.getOrElse(() => "-")
+  );
+
+  const rankingStartDateString = pipe(
+    beneficiaryDetails.rankingStartDate,
+    O.fromNullable,
+    O.map(formatDate("DD MMM YYYY")),
+    O.getOrElse(() => "-")
+  );
+
+  const rankingEndDateString = pipe(
+    beneficiaryDetails.rankingEndDate,
+    O.fromNullable,
+    O.map(formatDate("DD MMM YYYY")),
+    O.getOrElse(() => "-")
+  );
+
+  const rewardPercentageString = pipe(
+    beneficiaryDetails.refundRule?.accumulatedAmount?.refundThreshold,
+    O.fromNullable,
+    O.map(percentage => `${percentage}%`),
+    O.getOrElse(() => "-")
+  );
+
+  const lastUpdateString = pipe(
+    beneficiaryDetails.updateDate,
+    O.fromNullable,
+    O.map(formatDate("DD MMMM YYYY, hh:mm")),
+    O.map(dateString =>
+      I18n.t("idpay.initiative.beneficiaryDetails.lastUpdate", { dateString })
+    ),
+    O.toUndefined
+  );
+
+  return (
+    <>
+      {ruleInfoBox}
+      <Table
+        title={I18n.t("idpay.initiative.beneficiaryDetails.summary")}
+        rows={[
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.status"),
+            value: statusString
+          },
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.endDate"),
+            value: endDateString
+          },
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.amount"),
+            value: availableAmountString
+          },
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.toBeRefunded"),
+            value: accuredAmountString
+          },
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.refunded"),
+            value: refundedAmountString
+          }
+        ]}
+      />
+      <VSpacer size={8} />
+      <LabelSmall weight="Regular" color="bluegrey">
+        {lastUpdateString}
+      </LabelSmall>
+      <VSpacer size={8} />
+      <Table
+        title={I18n.t("idpay.initiative.beneficiaryDetails.spendingRules")}
+        rows={[
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.spendFrom"),
+            value: rankingStartDateString
+          },
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.spendTo"),
+            value: rankingEndDateString
+          },
+          {
+            label: I18n.t(
+              "idpay.initiative.beneficiaryDetails.spendPercentage"
+            ),
+            value: rewardPercentageString
+          }
+        ]}
+      />
+      <VSpacer size={8} />
+      <Table
+        title={I18n.t("idpay.initiative.beneficiaryDetails.enrollmentDetails")}
+        rows={[
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.enrollmentDate"),
+            value: "-"
+          },
+          {
+            label: I18n.t("idpay.initiative.beneficiaryDetails.protocolNumber"),
+            value: "-"
+          }
+        ]}
+      />
+    </>
+  );
+};
+
+const BeneficiaryDetailsContentSkeleton = () => (
+  <>
+    <InitiativeRulesInfoBoxSkeleton />
+    {Array.from({ length: 3 }).map((_, i) => (
+      <View key={i}>
+        <VSpacer size={32} />
+        <Placeholder.Box animate="fade" height={24} width={"40%"} radius={4} />
+        <VSpacer size={8} />
+        {Array.from({ length: 5 }).map((_, j) => (
+          <View key={j}>
+            <View style={IOStyles.rowSpaceBetween}>
+              <Placeholder.Box
+                animate="fade"
+                height={24}
+                width={100}
+                radius={4}
+              />
+              <Placeholder.Box
+                animate="fade"
+                height={24}
+                width={150}
+                radius={4}
+              />
+            </View>
+            <VSpacer size={8} />
+          </View>
+        ))}
+      </View>
+    ))}
+  </>
+);
+
+export { BeneficiaryDetailsContent, BeneficiaryDetailsContentSkeleton };
