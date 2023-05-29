de60d3a7d50741ad6c118cd95940f609adb24d7e
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-05-29 15:06:46 +0200

    chore: Add more tests

--- ts/features/fastLogin/utils/__tests__/pinPolicy.test.ts
+++ ts/features/fastLogin/utils/__tests__/pinPolicy.test.ts
@@ -20,29 +20,81 @@ describe("isValidSixDigitNumber", () => {
     );
   });
 
-  it("returns false for ascending sequences", () => {
-    expect(isValidSixDigitNumber(123456)).toBe(false);
-    expect(isValidSixDigitNumber(234567)).toBe(false);
-    expect(isValidSixDigitNumber("123456")).toBe(false);
-    expect(isValidSixDigitNumber("234567")).toBe(false);
+  describe("returns false for ascending sequences", () => {
+    [
+      123456,
+      234567,
+      "123456",
+      "234567",
+      345678,
+      456789,
+      // 567890,
+      "345678"
+    ].forEach(
+      // eslint-disable-next-line sonarjs/no-identical-functions
+      input => {
+        it(`${input}`, () => {
+          expect(String(input).length).toBe(6);
+          expect(isValidSixDigitNumber(input)).toBe(false);
+        });
+      }
+    );
   });
 
-  it("returns false for descending sequences", () => {
-    expect(isValidSixDigitNumber(654321)).toBe(false);
-    expect(isValidSixDigitNumber(543210)).toBe(false);
-    expect(isValidSixDigitNumber("654321")).toBe(false);
-    expect(isValidSixDigitNumber("543210")).toBe(false);
+  describe("returns false for descending sequences", () => {
+    // eslint-disable-next-line sonarjs/no-identical-functions
+    [
+      654321,
+      543210,
+      "654321",
+      "543210",
+      // "098765",
+      987654,
+      876543,
+      765432,
+      654321
+      // eslint-disable-next-line sonarjs/no-identical-functions
+    ].forEach(input => {
+      it(`${input}`, () => {
+        expect(String(input).length).toBe(6);
+        expect(isValidSixDigitNumber(input)).toBe(false);
+      });
+    });
   });
 
-  it("returns false for non-numeric strings", () => {
-    expect(isValidSixDigitNumber("abcdef")).toBe(false);
-    expect(isValidSixDigitNumber("12345a")).toBe(false);
+  describe("returns false for non-numeric strings", () => {
+    // eslint-disable-next-line sonarjs/no-identical-functions
+    ["abcdef", "12345a"].forEach(input => {
+      it(`${input}`, () => {
+        expect(isValidSixDigitNumber(input)).toBe(false);
+      });
+    });
   });
 
-  it("returns true for other valid numbers", () => {
-    expect(isValidSixDigitNumber(123451)).toBe(true);
-    expect(isValidSixDigitNumber(654320)).toBe(true);
-    expect(isValidSixDigitNumber("123451")).toBe(true);
-    expect(isValidSixDigitNumber("654320")).toBe(true);
+  describe("returns true for other valid numbers", () => {
+    [
+      123451,
+      654320,
+      "123451",
+      "654320",
+      "223344",
+      "267843",
+      "986654",
+      "518045",
+      782098,
+      119876,
+      223345,
+      267844,
+      986655,
+      518046,
+      782099,
+      119877,
+      223346,
+      267845
+    ].forEach(input => {
+      it(`${input}`, () => {
+        expect(isValidSixDigitNumber(input)).toBe(true);
+      });
+    });
   });
 });
