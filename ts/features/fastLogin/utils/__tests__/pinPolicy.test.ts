837051ffd3d3fda64fcefcf3bab823221e219d90
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-05-29 14:40:34 +0200

    chore: Refactoring

--- ts/features/fastLogin/utils/__tests__/pinPolicy.test.ts
+++ ts/features/fastLogin/utils/__tests__/pinPolicy.test.ts
@@ -1,21 +1,26 @@
 import { isValidSixDigitNumber } from "../pinPolicy";
 
 describe("isValidSixDigitNumber", () => {
-  it("returns false for numbers of incorrect length", () => {
-    expect(isValidSixDigitNumber(12345)).toBe(false);
-    expect(isValidSixDigitNumber(1234567)).toBe(false);
-    expect(isValidSixDigitNumber("12345")).toBe(false);
-    expect(isValidSixDigitNumber("1234567")).toBe(false);
+  describe("returns false for numbers of incorrect length", () => {
+    [13452, 1345672, "13452", "1345672"].forEach(input => {
+      it(`${input}`, () => {
+        expect(isValidSixDigitNumber(input)).toBe(false);
+      });
+    });
   });
 
-  it("returns false for numbers with all identical digits", () => {
-    expect(isValidSixDigitNumber(111111)).toBe(false);
-    expect(isValidSixDigitNumber(333333)).toBe(false);
-    expect(isValidSixDigitNumber("111111")).toBe(false);
-    expect(isValidSixDigitNumber("333333")).toBe(false);
+  describe("returns false for numbers with all identical digits", () => {
+    Array.from({ length: 10 }, (_, k) => k.toString().repeat(6)).forEach(
+      str => {
+        it(`${str}`, () => {
+          expect(str.length).toBe(6);
+          expect(isValidSixDigitNumber(str)).toBe(false);
+        });
+      }
+    );
   });
 
-  it("returns false for ascending sequences", () => {
+  describe("returns false for ascending sequences", () => {
     expect(isValidSixDigitNumber(123456)).toBe(false);
     expect(isValidSixDigitNumber(234567)).toBe(false);
     expect(isValidSixDigitNumber("123456")).toBe(false);
@@ -29,15 +34,15 @@ describe("isValidSixDigitNumber", () => {
     expect(isValidSixDigitNumber("543210")).toBe(false);
   });
 
+  it("returns false for non-numeric strings", () => {
+    expect(isValidSixDigitNumber("abcdef")).toBe(false);
+    expect(isValidSixDigitNumber("12345a")).toBe(false);
+  });
+
   it("returns true for other valid numbers", () => {
     expect(isValidSixDigitNumber(123451)).toBe(true);
     expect(isValidSixDigitNumber(654320)).toBe(true);
     expect(isValidSixDigitNumber("123451")).toBe(true);
     expect(isValidSixDigitNumber("654320")).toBe(true);
-  });
-
-  it("returns false for non-numeric strings", () => {
-    expect(isValidSixDigitNumber("abcdef")).toBe(false);
-    expect(isValidSixDigitNumber("12345a")).toBe(false);
   });
 });
