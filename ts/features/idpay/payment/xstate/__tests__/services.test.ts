7b88316ca56d7d184bb9378b3082f9f30f6d83d8
Federico Mastrini <federicomastrini93@gmail.com>
2023-05-31 14:19:08 +0200

    chore: added tests

--- ts/features/idpay/payment/xstate/__tests__/services.test.ts
+++ ts/features/idpay/payment/xstate/__tests__/services.test.ts
@@ -1,30 +1,27 @@
 import * as E from "fp-ts/lib/Either";
-import { Errors } from "io-ts";
+import * as O from "fp-ts/lib/Option";
 import {
   AuthPaymentResponseDTO,
   StatusEnum
 } from "../../../../../../definitions/idpay/AuthPaymentResponseDTO";
 import { ErrorDTO } from "../../../../../../definitions/idpay/ErrorDTO";
 import { mockIDPayClient } from "../../../common/api/__mocks__/client";
-import { Context } from "../context";
+import { Context, INITIAL_CONTEXT } from "../context";
 import { PaymentFailureEnum } from "../failure";
 import { createServicesImplementation } from "../services";
 
 const T_AUTH_TOKEN = "abc123";
-const T_TRX_CODE = "123456";
-const T_RESPONSE_DTO: AuthPaymentResponseDTO = {
+const T_TRX_CODE = "ABCD1234";
+const T_TRANSACTION_DATA_DTO: AuthPaymentResponseDTO = {
   amountCents: 100,
   id: "",
   initiativeId: "",
   rejectionReasons: [],
   status: StatusEnum.AUTHORIZED,
-  trxCode: ""
+  trxCode: T_TRX_CODE
 };
 
-const T_CONTEXT: Context = {
-  trxCode: undefined,
-  transaction: undefined
-};
+const T_CONTEXT: Context = INITIAL_CONTEXT;
 
 describe("IDPay Payment machine services", () => {
   const services = createServicesImplementation(mockIDPayClient, T_AUTH_TOKEN);
@@ -42,24 +39,6 @@ describe("IDPay Payment machine services", () => {
       expect(mockIDPayClient.putPreAuthPayment).not.toHaveBeenCalled();
     });
 
-    it("should get a UNAUTHORIZED failure if status code is 403", async () => {
-      const response: E.Either<Errors, { status: number; value?: ErrorDTO }> =
-        E.right({ status: 403, value: { code: "403", message: "" } });
-
-      mockIDPayClient.putPreAuthPayment.mockImplementation(() => response);
-
-      await expect(
-        services.preAuthorizePayment({ ...T_CONTEXT, trxCode: T_TRX_CODE })
-      ).rejects.toStrictEqual(PaymentFailureEnum.UNAUTHORIZED);
-
-      expect(mockIDPayClient.putPreAuthPayment).toHaveBeenCalledWith(
-        expect.objectContaining({
-          bearerAuth: T_AUTH_TOKEN,
-          trxCode: T_TRX_CODE
-        })
-      );
-    });
-
     it("should get a GENERIC failure if status codeis not 200", async () => {
       const response: E.Either<Error, { status: number; value?: ErrorDTO }> =
         E.right({ status: 404, value: { code: "404", message: "" } });
@@ -67,7 +46,10 @@ describe("IDPay Payment machine services", () => {
       mockIDPayClient.putPreAuthPayment.mockImplementation(() => response);
 
       await expect(
-        services.preAuthorizePayment({ ...T_CONTEXT, trxCode: T_TRX_CODE })
+        services.preAuthorizePayment({
+          ...T_CONTEXT,
+          trxCode: O.some(T_TRX_CODE)
+        })
       ).rejects.toStrictEqual(PaymentFailureEnum.GENERIC);
 
       expect(mockIDPayClient.putPreAuthPayment).toHaveBeenCalledWith(
@@ -82,13 +64,16 @@ describe("IDPay Payment machine services", () => {
       const response: E.Either<
         Error,
         { status: number; value?: AuthPaymentResponseDTO }
-      > = E.right({ status: 200, value: T_RESPONSE_DTO });
+      > = E.right({ status: 200, value: T_TRANSACTION_DATA_DTO });
 
       mockIDPayClient.putPreAuthPayment.mockImplementation(() => response);
 
       await expect(
-        services.preAuthorizePayment({ ...T_CONTEXT, trxCode: T_TRX_CODE })
-      ).resolves.toStrictEqual(T_RESPONSE_DTO);
+        services.preAuthorizePayment({
+          ...T_CONTEXT,
+          trxCode: O.some(T_TRX_CODE)
+        })
+      ).resolves.toStrictEqual(T_TRANSACTION_DATA_DTO);
 
       expect(mockIDPayClient.putPreAuthPayment).toHaveBeenCalledWith(
         expect.objectContaining({
@@ -100,7 +85,7 @@ describe("IDPay Payment machine services", () => {
   });
 
   describe("authorizePayment", () => {
-    it("should fail if trxCode is not in context", async () => {
+    it("should fail if transactionData is not in context", async () => {
       await expect(services.authorizePayment(T_CONTEXT)).rejects.toStrictEqual(
         PaymentFailureEnum.GENERIC
       );
@@ -108,33 +93,18 @@ describe("IDPay Payment machine services", () => {
       expect(mockIDPayClient.putAuthPayment).not.toHaveBeenCalled();
     });
 
-    it("should get a UNAUTHORIZED failure if status code is 403", async () => {
-      const response: E.Either<Error, { status: number; value?: ErrorDTO }> =
-        E.right({ status: 403, value: { code: "403", message: "" } });
-
-      mockIDPayClient.putAuthPayment.mockImplementation(() => response);
-
-      await expect(
-        services.authorizePayment({ ...T_CONTEXT, trxCode: T_TRX_CODE })
-      ).rejects.toStrictEqual(PaymentFailureEnum.UNAUTHORIZED);
-
-      expect(mockIDPayClient.putAuthPayment).toHaveBeenCalledWith(
-        expect.objectContaining({
-          bearerAuth: T_AUTH_TOKEN,
-          trxCode: T_TRX_CODE
-        })
-      );
-    });
-
-    it("should get a TIMEOUT failure if status code is 400", async () => {
+    it("should fail if status code is not 200", async () => {
       const response: E.Either<Error, { status: number; value?: ErrorDTO }> =
         E.right({ status: 400, value: { code: "400", message: "" } });
 
       mockIDPayClient.putAuthPayment.mockImplementation(() => response);
 
       await expect(
-        services.authorizePayment({ ...T_CONTEXT, trxCode: T_TRX_CODE })
-      ).rejects.toStrictEqual(PaymentFailureEnum.TIMEOUT);
+        services.authorizePayment({
+          ...T_CONTEXT,
+          transactionData: O.some(T_TRANSACTION_DATA_DTO)
+        })
+      ).rejects.toStrictEqual(PaymentFailureEnum.GENERIC);
 
       expect(mockIDPayClient.putAuthPayment).toHaveBeenCalledWith(
         expect.objectContaining({
@@ -144,14 +114,20 @@ describe("IDPay Payment machine services", () => {
       );
     });
 
-    it("should get a GENERIC failure if status codeis not 200", async () => {
+    it("should fail if status code is 200 and transaction is not AUTHORIZED", async () => {
       const response: E.Either<Error, { status: number; value?: ErrorDTO }> =
-        E.right({ status: 404, value: { code: "404", message: "" } });
+        E.right({ status: 400, value: { code: "400", message: "" } });
 
       mockIDPayClient.putAuthPayment.mockImplementation(() => response);
 
       await expect(
-        services.authorizePayment({ ...T_CONTEXT, trxCode: T_TRX_CODE })
+        services.authorizePayment({
+          ...T_CONTEXT,
+          transactionData: O.some({
+            ...T_TRANSACTION_DATA_DTO,
+            status: StatusEnum.REJECTED
+          })
+        })
       ).rejects.toStrictEqual(PaymentFailureEnum.GENERIC);
 
       expect(mockIDPayClient.putAuthPayment).toHaveBeenCalledWith(
@@ -166,13 +142,16 @@ describe("IDPay Payment machine services", () => {
       const response: E.Either<
         Error,
         { status: number; value?: AuthPaymentResponseDTO }
-      > = E.right({ status: 200, value: T_RESPONSE_DTO });
+      > = E.right({ status: 200, value: T_TRANSACTION_DATA_DTO });
 
       mockIDPayClient.putAuthPayment.mockImplementation(() => response);
 
       await expect(
-        services.authorizePayment({ ...T_CONTEXT, trxCode: T_TRX_CODE })
-      ).resolves.toStrictEqual(T_RESPONSE_DTO);
+        services.authorizePayment({
+          ...T_CONTEXT,
+          transactionData: O.some(T_TRANSACTION_DATA_DTO)
+        })
+      ).resolves.toStrictEqual(T_TRANSACTION_DATA_DTO);
 
       expect(mockIDPayClient.putAuthPayment).toHaveBeenCalledWith(
         expect.objectContaining({
