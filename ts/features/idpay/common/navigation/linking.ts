e7744adcdfb3874f22c2dcc21988fc45cdc66d68
Federico Mastrini <federicomastrini93@gmail.com>
2023-05-31 15:48:09 +0200

    chore: deep-link update

--- ts/features/idpay/common/navigation/linking.ts
+++ ts/features/idpay/common/navigation/linking.ts
@@ -4,22 +4,40 @@ import { IDPayOnboardingRoutes } from "../../onboarding/navigation/navigator";
 import { IDPayPaymentRoutes } from "../../payment/navigation/navigator";
 
 export const idPayLinkingOptions: PathConfigMap = {
+  /**
+   * IDPay initiative onboarding
+   */
   [IDPayOnboardingRoutes.IDPAY_ONBOARDING_MAIN]: {
     path: "idpay/onboarding",
     screens: {
+      /**
+       * Handles ioit://idpay/onboarding/{initiativeId}
+       */
       [IDPayOnboardingRoutes.IDPAY_ONBOARDING_INITIATIVE_DETAILS]: "/:serviceId"
     }
   },
+  /**
+   * IDPay initiative details
+   */
   [IDPayDetailsRoutes.IDPAY_DETAILS_MAIN]: {
     path: "idpay/initiative",
     screens: {
+      /**
+       * Handles ioit://idpay/initiative/{initiativeId}
+       */
       [IDPayDetailsRoutes.IDPAY_DETAILS_MONITORING]: "/:initiativeId"
     }
   },
+  /**
+   * IDPay payment authorization
+   */
   [IDPayPaymentRoutes.IDPAY_PAYMENT_MAIN]: {
     path: "idpay/auth",
     screens: {
-      [IDPayPaymentRoutes.IDPAY_PAYMENT_AUTHORIZATION]: "/:trxCode"
+      /**
+       * Handles ioit://idpay/auth?trxCode={transactionCode}
+       */
+      [IDPayPaymentRoutes.IDPAY_PAYMENT_AUTHORIZATION]: ""
     }
   }
 };
