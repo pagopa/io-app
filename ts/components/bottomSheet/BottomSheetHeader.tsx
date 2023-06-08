ab3046811188e112b50e949d97114a09e0d9c45c
Damiano Plebani <damiano.plebani@pagopa.it>
2023-06-08 10:25:24 +0200

    Replace old button with the new `IconButton`

--- ts/components/bottomSheet/BottomSheetHeader.tsx
+++ ts/components/bottomSheet/BottomSheetHeader.tsx
@@ -3,12 +3,13 @@ import { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { Millisecond } from "@pagopa/ts-commons/lib/units";
 import I18n from "../../i18n";
-import ButtonDefaultOpacity from "../ButtonDefaultOpacity";
 import { IOLayoutCostants, IOStyles } from "../core/variables/IOStyles";
 import { IOColors } from "../core/variables/IOColors";
 import { setAccessibilityFocus } from "../../utils/accessibility";
-import { Icon } from "../core/icons";
 import { NewH4 } from "../core/typography/NewH4";
+import IconButton from "../ui/IconButton";
+import { useIOSelector } from "../../store/hooks";
+import { isDesignSystemEnabledSelector } from "../../store/reducers/persistedPreferences";
 
 const styles = StyleSheet.create({
   bottomSheetHeader: {
@@ -17,12 +18,8 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: IOLayoutCostants.appMarginDefault,
     paddingTop: IOLayoutCostants.appMarginDefault,
+    paddingBottom: IOLayoutCostants.appMarginDefault,
     backgroundColor: IOColors.white
-  },
-  modalClose: {
-    paddingRight: 0,
-    paddingVertical: 0,
-    justifyContent: "flex-end"
   }
 });
 
@@ -35,6 +32,7 @@ export const BottomSheetHeader: React.FunctionComponent<Props> = ({
   title,
   onClose
 }: Props) => {
+  const isDesignSystemEnabled = useIOSelector(isDesignSystemEnabledSelector);
   const headerRef = React.createRef<View>();
 
   useEffect(() => {
@@ -55,17 +53,24 @@ export const BottomSheetHeader: React.FunctionComponent<Props> = ({
           <NewH4>{title}</NewH4>
         </View>
       )}
-      <ButtonDefaultOpacity
-        onPressWithGestureHandler={true}
-        style={styles.modalClose}
-        onPress={onClose}
-        transparent={true}
-        accessible={true}
-        accessibilityRole={"button"}
-        accessibilityLabel={I18n.t("global.buttons.close")}
-      >
-        <Icon name="close" color="grey" size={24} />
-      </ButtonDefaultOpacity>
+      {/* ◀ REMOVE_LEGACY_COMPONENT: Remove the following condition */}
+      {isDesignSystemEnabled ? (
+        <IconButton
+          color="neutral"
+          onPress={onClose}
+          icon="closeMedium"
+          accessibilityLabel={I18n.t("global.buttons.close")}
+        />
+      ) : (
+        <View style={{ opacity: 0.5 }}>
+          <IconButton
+            color="neutral"
+            onPress={onClose}
+            icon="closeMedium"
+            accessibilityLabel={I18n.t("global.buttons.close")}
+          />
+        </View>
+      )}
     </View>
   );
 };
