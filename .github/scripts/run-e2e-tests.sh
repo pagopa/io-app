9dde4661a5fd6f6732acdb96ef7030d2a6b9e707
Cristiano Tofani <cri.tofani@gmail.com>
2023-06-08 14:29:07 +0200

    storing dev-server artifacts in the same folder

--- .github/scripts/run-e2e-tests.sh
+++ .github/scripts/run-e2e-tests.sh
@@ -2,7 +2,7 @@ cd _io-dev-api-server_
 cp ../scripts/api-config.json config/config.json
 yarn
 yarn generate
-yarn start &> /tmp/io-dev-api-server.log &
+yarn start &> /tmp/e2e_artifacts/io-dev-api-server.log &
 # wait for the server to be up and running
 sleep 10
 cd ..
