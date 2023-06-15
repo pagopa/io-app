0521d9eca4bf4f046a0a1588397b625341f0aae3
Damiano Plebani <damiano.plebani@pagopa.it>
2023-06-15 15:53:46 +0200

    chore: [IOAPPFD0-111] Visual improvements to the bottom sheet (#4676)
    ## Short description
    This PR updates the visual appearance of the Bottom sheet, according to
    the new design guidelines
    
    ## List of changes proposed in this pull request
    - Change the border radius' value to the header (top left and top right)
    - Change the typographic style used for the bottom sheet title
    - Restore the default Backdrop component with fade transition on
    enter/exit
    - Change opacity value to the backdrop component
    - Add the new `closeMedium` icon to the header
    - Replace the legacy close button with the new `IconButton` with
    improved touchable areas
    
    ### Preview
    | Before | After |
    |--------|--------|
    | <video
    src="https://github.com/pagopa/io-app/assets/1255491/18812a12-5270-479d-8fc6-5f2969585346"
    /> | <video
    src="https://github.com/pagopa/io-app/assets/1255491/3e5a053e-6e2e-4a84-a695-243184729c02"
    /> |
    #### Touchable areas
    | Before | After |
    |--------|--------|
    | <img width="341"
    src="https://github.com/pagopa/io-app/assets/1255491/2d9c1b4f-2eb3-4a11-98d9-6d4b0d190cca">
    | <img width="341"
    src="https://github.com/pagopa/io-app/assets/1255491/758af36b-16be-42d2-a2c6-19c98fe5c957">
    |
    
    
    
    
    
    
    
    
    ## How to test
    Go to the **Profile → Design System → Bottom Sheet (Components)**
    
    ---------
    
    Co-authored-by: Federico Mastrini <federicomastrini93@gmail.com>
    Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>

--- ts/features/design-system/core/DSBottomSheet.tsx
+++ ts/features/design-system/core/DSBottomSheet.tsx
@@ -12,6 +12,7 @@ import {
 import { IOThemeContext } from "../../../components/core/variables/IOColors";
 import ListItemNav from "../../../components/ui/ListItemNav";
 import { Body } from "../../../components/core/typography/Body";
+import ButtonSolid from "../../../components/ui/ButtonSolid";
 
 export const DSBottomSheet = () => {
   const handlePressDismiss = () => {
@@ -20,12 +21,16 @@ export const DSBottomSheet = () => {
   };
 
   const DimissBottomSheetItem = () => (
-    <ListItemNav
-      value="Dismiss"
-      description="This is a item that will close the bottom sheet"
-      accessibilityLabel="Dismiss"
-      onPress={handlePressDismiss}
-    />
+    <>
+      <VSpacer size={24} />
+      <ButtonSolid
+        fullWidth
+        label="Dismiss"
+        accessibilityLabel="Dismiss"
+        onPress={handlePressDismiss}
+        color="primary"
+      />
+    </>
   );
 
   const BottomSheetContentBody = () => (
