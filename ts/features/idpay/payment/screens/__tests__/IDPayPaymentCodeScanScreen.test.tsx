d828a18b3d669156a7d79323210dbea86b2d6a26
Federico Mastrini <federicomastrini93@gmail.com>
2023-06-19 16:06:42 +0200

    chore: `IDPayPaymentCodeScanScreen` tests

--- ts/features/idpay/payment/screens/__tests__/IDPayPaymentCodeScanScreen.test.tsx
+++ ts/features/idpay/payment/screens/__tests__/IDPayPaymentCodeScanScreen.test.tsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { View } from "react-native";
 import configureMockStore from "redux-mock-store";
+import { fireEvent } from "@testing-library/react-native";
 import { applicationChangeState } from "../../../../../store/actions/application";
 import { appReducer } from "../../../../../store/reducers";
 import { GlobalState } from "../../../../../store/reducers/types";
 import { renderScreenFakeNavRedux } from "../../../../../utils/testWrapper";
-import { IOBarcodeScanner } from "../../components/BarcodeScanner";
 import { IDPayPaymentRoutes } from "../../navigation/navigator";
 import { IDPayPaymentCodeScanScreen } from "../IDPayPaymentCodeScanScreen";
+import I18n from "../../../../../i18n";
+
+jest.mock("../../components/BarcodeScanner", () => ({
+  useIOBarcodeScanner: jest.fn()
+}));
+
+import {
+  IOBarcodeScanner,
+  useIOBarcodeScanner
+} from "../../components/BarcodeScanner";
 
 const mockCameraComponent = <View testID="cameraComponentTestID" />;
 
-const mockUseIOBarcodeScanner = jest.fn<IOBarcodeScanner, any>(() => ({
-  cameraComponent: mockCameraComponent,
-  cameraPermissionStatus: "authorized",
-  requestCameraPermission: jest.fn(),
-  openCameraSettings: jest.fn()
-}));
-
-jest.mock("../../components/BarcodeScanner", () => ({
-  useIOBarcodeScanner: mockUseIOBarcodeScanner
-}));
+(useIOBarcodeScanner as jest.Mock).mockImplementation(
+  (): IOBarcodeScanner => ({
+    cameraComponent: mockCameraComponent,
+    cameraPermissionStatus: "authorized",
+    requestCameraPermission: jest.fn(),
+    openCameraSettings: jest.fn()
+  })
+);
 
 describe("Test IDPayPaymentCodeScanScreen", () => {
   beforeEach(() => {
@@ -31,6 +39,94 @@ describe("Test IDPayPaymentCodeScanScreen", () => {
     const { component } = renderComponent();
 
     expect(component).toBeTruthy();
+  });
+
+  it("should display the camera component", () => {
+    const { component } = renderComponent();
+
+    expect(component.queryByTestId("cameraComponentTestID")).toBeTruthy();
+  });
+
+  it("should display the undefined camera permissions view and let user grant permissions", () => {
+    const mockRequestCameraPermission = jest.fn();
+    const mockOpenCameraSettings = jest.fn();
+
+    (useIOBarcodeScanner as jest.Mock).mockImplementationOnce(
+      (): IOBarcodeScanner => ({
+        cameraComponent: <View testID="cameraComponentTestID" />,
+        cameraPermissionStatus: "not-determined",
+        requestCameraPermission: mockRequestCameraPermission,
+        openCameraSettings: mockOpenCameraSettings
+      })
+    );
+
+    const { component } = renderComponent();
+
+    expect(component.queryByTestId("cameraComponentTestID")).toBeFalsy();
+
+    expect(
+      component.queryByText(
+        I18n.t("idpay.payment.qrCode.scan.permissions.undefined.title")
+      )
+    ).toBeTruthy();
+
+    expect(
+      component.queryByText(
+        I18n.t("idpay.payment.qrCode.scan.permissions.undefined.label")
+      )
+    ).toBeTruthy();
+
+    const buttonComponent = component.getByText(
+      I18n.t("idpay.payment.qrCode.scan.permissions.undefined.action")
+    );
+
+    expect(buttonComponent).toBeTruthy();
+
+    fireEvent(buttonComponent, "onPress");
+
+    expect(mockRequestCameraPermission).toHaveBeenCalledTimes(0);
+    expect(mockOpenCameraSettings).toHaveBeenCalledTimes(1);
+  });
+
+  it("should display the denied camera permissions view and let user grant permissions", () => {
+    const mockRequestCameraPermission = jest.fn();
+    const mockOpenCameraSettings = jest.fn();
+
+    (useIOBarcodeScanner as jest.Mock).mockImplementationOnce(
+      (): IOBarcodeScanner => ({
+        cameraComponent: <View testID="cameraComponentTestID" />,
+        cameraPermissionStatus: "restricted",
+        requestCameraPermission: mockRequestCameraPermission,
+        openCameraSettings: mockOpenCameraSettings
+      })
+    );
+
+    const { component } = renderComponent();
+
+    expect(component.queryByTestId("cameraComponentTestID")).toBeFalsy();
+
+    expect(
+      component.queryByText(
+        I18n.t("idpay.payment.qrCode.scan.permissions.denied.title")
+      )
+    ).toBeTruthy();
+
+    expect(
+      component.queryByText(
+        I18n.t("idpay.payment.qrCode.scan.permissions.denied.label")
+      )
+    ).toBeTruthy();
+
+    const buttonComponent = component.getByText(
+      I18n.t("idpay.payment.qrCode.scan.permissions.denied.action")
+    );
+
+    expect(buttonComponent).toBeTruthy();
+
+    fireEvent(buttonComponent, "onPress");
+
+    expect(mockRequestCameraPermission).toHaveBeenCalledTimes(0);
+    expect(mockOpenCameraSettings).toHaveBeenCalledTimes(1);
   });
 });
 
