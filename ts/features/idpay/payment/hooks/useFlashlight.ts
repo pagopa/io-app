106f29c794cece741a7d44fcfa4a49b4c8caab91
Federico Mastrini <federicomastrini93@gmail.com>
2023-06-28 17:50:20 +0200

    refactor: camera permissions

--- ts/features/idpay/payment/hooks/useFlashlight.ts
+++ ts/features/idpay/payment/hooks/useFlashlight.ts
@@ -5,26 +5,28 @@ import Torch from "react-native-torch";
 export const useFlashlight = () => {
   const [isFlashlightOn, setFlashlightOn] = React.useState(false);
 
+  // If Android, we need camera permission to toggle the torch
+  const isCameraAllowed = React.useCallback(
+    async () =>
+      Platform.OS === "ios"
+        ? true
+        : await Torch.requestCameraPermission(
+            "Camera Permissions", // dialog title
+            "We require camera permissions to use the torch on the back of your phone." // dialog body
+          ),
+    []
+  );
+
   /**
    * Toggles the flash light
    */
   const toggleFlashLight = React.useCallback(async () => {
-    // If Android, we need camera permission to toggle the torch
-    if (Platform.OS === "ios") {
+    const cameraAllowed = await isCameraAllowed();
+    if (cameraAllowed) {
       Torch.switchState(!isFlashlightOn);
       setFlashlightOn(!isFlashlightOn);
-    } else {
-      const cameraAllowed = await Torch.requestCameraPermission(
-        "Camera Permissions", // dialog title
-        "We require camera permissions to use the torch on the back of your phone." // dialog body
-      );
-
-      if (cameraAllowed) {
-        Torch.switchState(!isFlashlightOn);
-        setFlashlightOn(!isFlashlightOn);
-      }
     }
-  }, [isFlashlightOn, setFlashlightOn]);
+  }, [isFlashlightOn, setFlashlightOn, isCameraAllowed]);
 
   return {
     isFlashlightOn,
