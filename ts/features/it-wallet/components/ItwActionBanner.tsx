f6f00214a03e66212bdc50b55494c914141d9829
Mario Perrotta <mario.perrotta@pagopa.it>
2023-06-30 17:14:03 +0200

    feat(IT Wallet): [SIW-218] Add action banner sticky component (#4721)
    ## Short description
    This PR adds an ITW action banner sticky component. It also adds the
    banner in message home screen.

---
+++ ts/features/it-wallet/components/ItwActionBanner.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Banner } from "../../../components/Banner";
+import { VSpacer } from "../../../components/core/spacer/Spacer";
+import I18n from "../../../i18n";
+
+/**
+ * The base graphical component, take a text as input and dispatch onPress when pressed
+ * include also a close button
+ */
+export const ItwActionBanner = (): React.ReactElement => {
+  const viewRef = React.createRef<View>();
+  return (
+    <>
+      <VSpacer size={24} />
+      <Banner
+        testID={"ItwBannerTestID"}
+        viewRef={viewRef}
+        color={"neutral"}
+        variant="big"
+        title={I18n.t("features.itWallet.actionBanner.title")}
+        content={I18n.t("features.itWallet.actionBanner.description")}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        pictogramName={"setup"}
+        action={I18n.t("features.itWallet.actionBanner.action")}
+        labelClose={I18n.t("features.itWallet.actionBanner.hideLabel")}
+        onPress={() => null}
+        onClose={() => null}
+      />
+    </>
+  );
+};
