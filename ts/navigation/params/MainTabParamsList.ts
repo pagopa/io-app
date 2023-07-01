c346840e957f8e0c3b9d48f84c65fb1224a28503
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-07-01 13:27:52 +0200

    feat(IT Wallet): [SIW-206,SIW-217] Add new tab navigator (#4724)
    This PR adds a new tab navigator while keeping the original one if the
    IT-Wallet FF is disabled. It also moves the profile screen to the base
    header.
    
    - `locales/en/index.yml` & `locales/it/index.yml`: Adds a new tab
    navigator labels and profile button accessibility labeles.
    - `ts/components/core/icons/Icon.tsx`: Adds the `psp` icon as a
    navigation icon;
    - `ts/components/core/icons/svg/IconMultiCoggles.tsx`: Adds a multi
    coggles icon;
    - `ts/components/screens/BaseHeader.tsx`: Adds a new multi coggles
    button to access profile and a `isProfileAvailable` prop to enable it
    and define its callback;
    - `ts/components/screens/TopScreenComponent.tsx`: Props drilling to
    `BaseHeader.tsx`;
    - `ts/navigation/routes.ts`: Adds new routes;
    - `ts/navigation/params/ProfileParamsList.ts`: Adds the
    `ROUTE.PROFILE_MAIN` to the params list, see above;
    - `ts/navigation/params/MainTabParamsList.ts`: Adds new routes to the
    params list;
    - `ts/screens/messages/MessagesHomeScreen.tsx`: Adds the navigation
    context and uses the `isProfileAvailable` to render the new profile
    icon;
    - `ts/screens/profile/ProfileMainScreen.tsx`: Adds a back button prop to
    dynamically disable it when rendered through the tab navigator and
    enable it when rendered through the new profile icon;
    - `ts/navigation/ProfileNavigator.tsx`: Adds the `ROUTE.PROFILE_MAIN` to
    the navigator as initial screen in order to able to navigate to it from
    `BaseHeader` when the screen is not part of the tab navigator due to the
    FF;
    - `ts/features/it-wallet/screens/NotAvailableScreen.tsx`: Adds an empty
    screen for WIP features;
    - `ts/navigation/TabNavigator.tsx`: Adds conditional rendering based on
    the FF. New screens are currently rendered as `NotAvailableScreen.tsx`
    as placeholder;
    
    Just start the app while connected to the dev server and test the new
    profile button along with the new tab sections.
    Everything should come back to normal when disabling the FF.
    **Remember to comment line 65 to 71 in `ts/screens/modal/RootModal.tsx`
    before disabling the FF.**
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

--- ts/navigation/params/MainTabParamsList.ts
+++ ts/navigation/params/MainTabParamsList.ts
@@ -1,10 +1,13 @@
+import { ProfileMainNavigationParams } from "../../screens/profile/ProfileMainScreen";
 import { WalletHomeNavigationParams } from "../../screens/wallet/WalletHomeScreen";
 import ROUTES from "../routes";
 
 export type MainTabParamsList = {
+  [ROUTES.ITWALLET_HOME]: undefined;
+  [ROUTES.QR_CODE_SCAN]: undefined;
   [ROUTES.MESSAGES_HOME]: undefined;
   [ROUTES.WALLET_HOME]: WalletHomeNavigationParams;
   [ROUTES.PAYMENT_SCAN_QR_CODE]: undefined;
   [ROUTES.SERVICES_HOME]: undefined;
-  [ROUTES.PROFILE_MAIN]: undefined;
+  [ROUTES.PROFILE_MAIN]: ProfileMainNavigationParams;
 };
