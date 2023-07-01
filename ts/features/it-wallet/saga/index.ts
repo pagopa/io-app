40d0d312d56c6e89d69fb690842df7fd337ebde0
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-01 18:00:42 +0200

    feat(IT Wallet): [SIW-251] Init itw main saga and activation base screen (#4735)
    ## Short description
    This PR initialize ITW saga and a base activation screen. Also adds
    navigation action to sticky component to start activation flow.

---
+++ ts/features/it-wallet/saga/index.ts
@@ -0,0 +1,19 @@
+import { takeLatest, call } from "typed-redux-saga/macro";
+import { SagaIterator } from "redux-saga";
+import { CommonActions } from "@react-navigation/native";
+import { itwActivationStart } from "../store/actions";
+import NavigationService from "../../../navigation/NavigationService";
+import { ITW_ROUTES } from "../navigation/routes";
+
+export function* watchItwSaga(): SagaIterator {
+  yield* takeLatest(itwActivationStart, watchItwActivationStart);
+}
+
+function* watchItwActivationStart(): SagaIterator {
+  yield* call(
+    NavigationService.dispatchNavigationAction,
+    CommonActions.navigate(ITW_ROUTES.MAIN, {
+      screen: ITW_ROUTES.ACTIVATION.DETAILS
+    })
+  );
+}
