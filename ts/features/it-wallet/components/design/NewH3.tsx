54cdb5032b71cfc359b730286e1349421f7effbc
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-07-03 09:09:54 +0200

    feat(IT Wallet): [SIW-206,SIW-248] Refactor base header (#4727)
    ### This PR depends on https://github.com/pagopa/io-app/pull/4724
    
    
    ## Short description
    This PR refactors `BaseHeader.tsx` with new icons and a section title.
    
    ## List of changes proposed in this pull request
    - `ts/components/core/icons/svg/IconMultiCoggles.tsx`: Removes multi
    coggles icon;
    - `ts/components/core/icons/Icon.tsx`: Removes multi coggles icon
    reference;
    - `ts/components/screens/BaseHeader.tsx`: Adds new base header icons and
    section title props to display a section title instead of the IO logo;
    - `ts/components/screens/TopScreenComponent.tsx`: props drilling to
    `BaseHeader.tsx`;
    - `ts/components/search/SearchButton.tsx`: Adds new search button;
    - `ts/features/it-wallet/components/design/NewH3.tsx`: Adds a new H3
    component with the new font to display the `BaseHeader` section title;
    - `ts/screens/messages/MessagesHomeScreen.tsx`: Hides the
    `ScreenContentHeader.tsx` and displays the new section title;
    
    ## How to test
    Just start the app while connected to the dev server and test the new
    base header in the messages section.
    Remember to comment line 65 to 71 in ts/screens/modal/RootModal.tsx
    before disabling the FF.
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

---
+++ ts/features/it-wallet/components/design/NewH3.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import {
+  IOColorsStatusForeground,
+  IOTheme
+} from "../../../../components/core/variables/IOColors";
+import { IOFontFamily, IOFontWeight } from "../../../../components/core/fonts";
+import {
+  ExternalTypographyProps,
+  TypographyProps
+} from "../../../../components/core/typography/common";
+import { useTypographyFactory } from "../../../../components/core/typography/Factory";
+
+type AllowedColors = IOColorsStatusForeground | IOTheme["textHeading-default"];
+type AllowedWeight = Extract<IOFontWeight, "Bold" | "Regular">;
+
+type OwnProps = ExternalTypographyProps<
+  TypographyProps<AllowedWeight, AllowedColors>
+>;
+
+/* Common typographic styles */
+export const h3FontSize = 22;
+export const h3LineHeight = 33;
+/* New typographic styles */
+const h3FontName: IOFontFamily = "ReadexPro";
+const h3DefaultColor: AllowedColors = "black";
+const h3DefaultWeight: AllowedWeight = "Regular";
+
+/**
+ * Typography component to render `H3` text with font size {@link fontSize} and fontFamily {@link fontName}.
+ * default values(if not defined) are weight: `Bold`, color: `bluegreyDark`
+ * @param props
+ * @constructor
+ */
+export const NewH3: React.FunctionComponent<OwnProps> = props =>
+  useTypographyFactory<AllowedWeight, AllowedColors>({
+    ...props,
+    defaultWeight: h3DefaultWeight,
+    defaultColor: h3DefaultColor,
+    font: h3FontName,
+    fontStyle: { fontSize: h3FontSize, lineHeight: h3LineHeight }
+  });
