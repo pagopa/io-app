6b20fdb4d52cc34052f1967a8621d0bb057e5806
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-04 13:04:01 +0200

    feat(IT Wallet): [SIW-252] Add activation screen (#4741)
    ## Short description
    This PR adds the activation screen.

--- ts/features/it-wallet/screens/ItwActivationDetailsScreen.tsx
+++ ts/features/it-wallet/screens/ItwActivationDetailsScreen.tsx
@@ -1,16 +1,74 @@
 import * as React from "react";
+import { Image, SafeAreaView, View } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import walletCards from "../../../../img/features/it-wallet/wallet-cards.png";
 import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
 import { emptyContextualHelp } from "../../../utils/emptyContextualHelp";
 import I18n from "../../../i18n";
+import ScreenContent from "../../../components/screens/ScreenContent";
+import { IOStyles } from "../../../components/core/variables/IOStyles";
+import FooterWithButtons from "../../../components/ui/FooterWithButtons";
+import { H4 } from "../../../components/core/typography/H4";
+import { VSpacer } from "../../../components/core/spacer/Spacer";
+import ItwTos from "../components/ItwTos";
 
-const ItwActivationDetailsScreen = () => (
-  <BaseScreenComponent
-    goBack={true}
-    headerTitle={I18n.t("features.itWallet.title")}
-    contextualHelp={emptyContextualHelp}
-  >
-    {/* TODO: SIW-252 */}
-  </BaseScreenComponent>
-);
+const ItwActivationDetailsScreen = () => {
+  const navigation = useNavigation();
+  const cancelButtonProps = {
+    block: true,
+    light: false,
+    bordered: true,
+    onPress: navigation.goBack,
+    title: I18n.t("features.itWallet.activationScreen.cancel")
+  };
+
+  const continueButtonProps = {
+    block: true,
+    primary: true,
+    onPress: () => undefined,
+    title: I18n.t("features.itWallet.activationScreen.confirm")
+  };
+
+  return (
+    <BaseScreenComponent
+      goBack={true}
+      headerTitle={I18n.t("features.itWallet.title")}
+      contextualHelp={emptyContextualHelp}
+    >
+      <SafeAreaView style={IOStyles.flex}>
+        <ScreenContent
+          title={I18n.t("features.itWallet.activationScreen.title")}
+          subtitle={I18n.t("features.itWallet.activationScreen.subTitle")}
+        >
+          {/* Wallet cards image */}
+          <Image
+            source={walletCards}
+            resizeMode={"contain"}
+            style={{ width: "100%", height: 250 }}
+          />
+          {/* Info activation */}
+          <View style={IOStyles.horizontalContentPadding}>
+            <H4 weight={"SemiBold"} color={"bluegreyDark"}>
+              {I18n.t("features.itWallet.activationScreen.howActivate")}
+            </H4>
+            <VSpacer />
+            <H4 weight={"Regular"} color={"bluegrey"}>
+              {I18n.t(
+                "features.itWallet.activationScreen.howActivateDescription"
+              )}
+            </H4>
+          </View>
+          {/* Footer ToS and privacy link */}
+          <ItwTos />
+        </ScreenContent>
+        <FooterWithButtons
+          type={"TwoButtonsInlineThird"}
+          leftButton={cancelButtonProps}
+          rightButton={continueButtonProps}
+        />
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
 
 export default ItwActivationDetailsScreen;
