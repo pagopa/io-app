a3be5d0e3125dd711472fa90551dbedd8a738386
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-05 16:00:12 +0200

    feat(IT Wallet): [SIW-261] Add authentication info screen (#4745)
    This PR adds an info screen with some details about authentication with
    CIE before start authentication flow.

--- ts/features/it-wallet/navigation/routes.ts
+++ ts/features/it-wallet/navigation/routes.ts
@@ -1,6 +1,7 @@
 export const ITW_ROUTES = {
   MAIN: "ITW_MAIN",
   ACTIVATION: {
-    DETAILS: "ITW_ACTIVATION_DETAILS"
+    DETAILS: "ITW_ACTIVATION_DETAILS",
+    INFO: "ITW_ACTIVATION_INFO"
   } as const
 } as const;
