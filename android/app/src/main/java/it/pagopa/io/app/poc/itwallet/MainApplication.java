ca59973834a45e1458aa98c108116b6199856ae5
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-07-06 09:15:03 +0200

    feat(IT Wallet): [SIW-268] Update applicationId to fix Android deployment (#4750)
    ## Short description
    This PR updates the Android `applicationId` in order to, hopefully, fix
    the Android deployment via FastLane.
    In particular, `android/fastlane/Appfile` still had the old
    `applicationId`.
    
    ## How to test
    Delete `android/build` or run `cd android && ./gradlew clean`.
    Delete any IO app instance from the simulator/device.
    Run `yarn generate` then start building with `yarn run-android`.

--- android/app/src/main/java/it/pagopa/io/app/poc/itwallet/MainApplication.java
+++ android/app/src/main/java/it/pagopa/io/app/poc/itwallet/MainApplication.java
@@ -86,7 +86,7 @@ public class MainApplication extends Application implements ReactApplication {
           We use reflection here to pick up the class that initializes Flipper,
          since Flipper library is not available in release mode
          */
-         Class<?> aClass = Class.forName("it.pagopa.io.app.ReactNativeFlipper");
+         Class<?> aClass = Class.forName("it.pagopa.io.app.poc.itwallet.ReactNativeFlipper");
          aClass
            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
            .invoke(null, context, reactInstanceManager);
