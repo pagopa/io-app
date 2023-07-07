59d0a118e488b33633e88f287cdce0eb92b22b21
Damiano Plebani <damiano.plebani@pagopa.it>
2023-07-07 13:26:25 +0200

    Improve spacing of internal DS screens

--- ts/features/design-system/components/DesignSystemScreen.tsx
+++ ts/features/design-system/components/DesignSystemScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { ScrollView, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ContentWrapper } from "../../../components/core/ContentWrapper";
 import { IOThemeContext } from "../../../components/core/variables/IOColors";
 import { IOVisualCostants } from "../../../components/core/variables/IOStyles";
@@ -10,21 +11,26 @@ type Props = {
   noMargin?: boolean;
 };
 
-export const DesignSystemScreen = ({ children, noMargin = false }: Props) => (
-  <IOThemeContext.Consumer>
-    {theme => (
-      <ScrollView
-        contentContainerStyle={{
-          backgroundColor: theme["appBackground-primary"],
-          paddingTop: IOVisualCostants.appMarginDefault
-        }}
-      >
-        {noMargin ? (
-          <View>{children}</View>
-        ) : (
-          <ContentWrapper>{children}</ContentWrapper>
-        )}
-      </ScrollView>
-    )}
-  </IOThemeContext.Consumer>
-);
+export const DesignSystemScreen = ({ children, noMargin = false }: Props) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <IOThemeContext.Consumer>
+      {theme => (
+        <ScrollView
+          contentContainerStyle={{
+            backgroundColor: theme["appBackground-primary"],
+            paddingTop: IOVisualCostants.appMarginDefault,
+            paddingBottom: insets.bottom + IOVisualCostants.appMarginDefault
+          }}
+        >
+          {noMargin ? (
+            <View>{children}</View>
+          ) : (
+            <ContentWrapper>{children}</ContentWrapper>
+          )}
+        </ScrollView>
+      )}
+    </IOThemeContext.Consumer>
+  );
+};
