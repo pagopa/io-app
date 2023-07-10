b3b3144bf2c09f75c9fcebcd11f061ab331604d7
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-07-10 16:38:42 +0200

    feat(IT Wallet): [SIW-226] Add NFC check in issuing flow (#4760)
    ## Short description
    Include a summary of the changes.
    
    ## List of changes proposed in this pull request
    - `locales/en/index.yml` &  `locales/it/index.yml`: Adds locales;
    - `ts/features/common/store/reducers/index.ts`: Adds state and reducers
    to the list;
    - `ts/features/it-wallet/navigation/ItwStackNavigator.tsx`: Fixes
    imports after folders refactor;
    - `ts/features/it-wallet/saga/__tests__/handleRequirementsCheck.ts`:
    Adds saga tests;
    - `ts/features/it-wallet/saga/handleRequirementsCheck.ts`: Adds a saga
    which handles the requirements check, NFC in this case;
    - `ts/features/it-wallet/saga/index.ts`: Adds the
    `handleRequirementsCheck` saga;
    -
    `ts/features/it-wallet/screens/discovery/ItwActivationDetailsScreen.tsx`:
    FIxes import after folders refactor;
    -
    `ts/features/it-wallet/screens/issuing/ItwActivationInfoAuthScreen.tsx`:
    Adds the requirements check login to the existing screen and moves it
    under `issuing` folder;
    - `ts/features/it-wallet/store/actions/index.ts`: Adds actions for the
    `handleRequirementsCheck` saga;
    - `ts/features/it-wallet/store/reducers/index.ts`: Adds the reducer
    entry point;
    - `ts/features/it-wallet/store/reducers/itwRequirements.ts`: Adds the
    reducer for the requirements check;
    - `ts/features/it-wallet/utils/errors/itwErrors.ts`: Exports two types,
    the first one being `ItWalletError` which will be used for any saga
    related error in the it-wallet flow. It consists of a code and an
    optional message. The code is a superset of HTTP errors extended with
    some custom errors. The second one is `ItWalletMappedError` which is
    returned by our mapping functions and consists of a title and body to be
    used in UI components;
    - `ts/features/it-wallet/utils/errors/itwErrorsMapping.ts`: Contains our
    mapping functions;
    - `ts/store/actions/types.ts`: Adds actions to the global exported type.
    
    ## How to test
    Test the it wallet activation flow on a pyshical device with NFC for the
    happy path and on the simulator for the error path.
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

---
+++ ts/features/it-wallet/saga/handleRequirementsCheck.ts
@@ -0,0 +1,39 @@
+import { SagaIterator } from "redux-saga";
+import { put, select } from "typed-redux-saga/macro";
+import { isSome } from "fp-ts/lib/Option";
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import {
+  hasApiLevelSupportSelector,
+  hasNFCFeatureSelector
+} from "../../../store/reducers/cie";
+import { idpSelector } from "../../../store/reducers/authentication";
+import { itwRequirementsRequest } from "../store/actions";
+import { ItWalletErrorTypes } from "../utils/errors/itwErrors";
+
+/*
+ * This saga handles the requirements check for the IT Wallet activation.
+ * Currently it checks if the user logged in with CIE or if the device has NFC support.
+ */
+export function* handleRequirementsRequest(): SagaIterator {
+  const idp = yield* select(idpSelector);
+  if (isSome(idp) && idp.value.name === "cie") {
+    yield* put(itwRequirementsRequest.success(true));
+  } else {
+    const hasNFCFeature = yield* select(hasNFCFeatureSelector);
+    const hasApiLevelSupport = yield* select(hasApiLevelSupportSelector);
+    if (
+      pot.isSome(hasNFCFeature) &&
+      pot.isSome(hasApiLevelSupport) &&
+      hasNFCFeature.value &&
+      hasApiLevelSupport.value
+    ) {
+      yield* put(itwRequirementsRequest.success(true));
+    } else {
+      yield* put(
+        itwRequirementsRequest.failure({
+          code: ItWalletErrorTypes.NFC_NOT_SUPPORTED
+        })
+      );
+    }
+  }
+}
---
+++ ts/features/it-wallet/store/reducers/itwRequirements.ts
@@ -0,0 +1,43 @@
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import { getType } from "typesafe-actions";
+import { Action } from "../../../../store/actions/types";
+import { GlobalState } from "../../../../store/reducers/types";
+import { itwRequirementsRequest } from "../actions";
+import { ItWalletError } from "../../utils/errors/itwErrors";
+
+export type ItwRequirementsState = pot.Pot<true, ItWalletError>;
+
+const emptyState: ItwRequirementsState = pot.none;
+
+/**
+ * This reducer handles the requirements check for the IT Wallet activation.
+ * It manipulates a pot which maps to an error if the requirements are not met or to true if they are.
+ * A saga is attached to the request action to check the requirements.
+ * @param state the current state
+ * @param action the dispatched action
+ * @returns the result state
+ */
+const reducer = (
+  state: ItwRequirementsState = emptyState,
+  action: Action
+): ItwRequirementsState => {
+  switch (action.type) {
+    case getType(itwRequirementsRequest.request):
+      return pot.toLoading(state);
+    case getType(itwRequirementsRequest.success):
+      return pot.some(action.payload);
+    case getType(itwRequirementsRequest.failure):
+      return pot.toError(state, action.payload);
+  }
+  return state;
+};
+
+/**
+ * Selects the requirements state.
+ * @param state the global state
+ * @returns the requirements state
+ */
+export const itwRequirementsSelector = (state: GlobalState) =>
+  state.features.itWallet.requirements;
+
+export default reducer;
