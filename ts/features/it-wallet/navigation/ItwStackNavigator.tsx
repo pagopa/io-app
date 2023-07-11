d2917def1c4f56560127f8d664388166402561c6
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-11 17:10:56 +0200

    feat(IT Wallet): [SIW-209,SIW-255] Cie pid issuing (#4764)
    This PR adds CIE flow to read NFC card required to get the PID to
    related user. The last screen is `CieConsentDataUsageScreen`. After that
    in another PR will be add a loading component and a biometric/pin
    unlocking path screen.

--- ts/features/it-wallet/navigation/ItwStackNavigator.tsx
+++ ts/features/it-wallet/navigation/ItwStackNavigator.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { isGestureEnabled } from "../../../utils/navigation";
+import CiePinScreen from "../screens/issuing/cie/CiePinScreen";
+import CieCardReaderScreen from "../screens/issuing/cie/CieCardReaderScreen";
+import CieConsentDataUsageScreen from "../screens/issuing/cie/CieConsentDataUsageScreen";
+import CieExpiredOrInvalidScreen from "../screens/issuing/cie/CieExpiredOrInvalidScreen";
+import CieWrongCiePinScreen from "../screens/issuing/cie/CieWrongCiePinScreen";
 import ItwActivationDetailsScreen from "../screens/discovery/ItwActivationDetailsScreen";
 import ItwActivationInfoAuthScreen from "../screens/issuing/ItwActivationInfoAuthScreen";
 import { ItwParamsList } from "./params";
@@ -20,6 +25,26 @@ export const ItwStackNavigator = () => (
     <Stack.Screen
       name={ITW_ROUTES.ACTIVATION.INFO}
       component={ItwActivationInfoAuthScreen}
+    />
+    <Stack.Screen
+      name={ITW_ROUTES.ACTIVATION.CIE_PIN_SCREEN}
+      component={CiePinScreen}
+    />
+    <Stack.Screen
+      name={ITW_ROUTES.ACTIVATION.CIE_CONSENT_DATA_USAGE}
+      component={CieConsentDataUsageScreen}
+    />
+    <Stack.Screen
+      name={ITW_ROUTES.ACTIVATION.CIE_CARD_READER_SCREEN}
+      component={CieCardReaderScreen}
+    />
+    <Stack.Screen
+      name={ITW_ROUTES.ACTIVATION.CIE_EXPIRED_SCREEN}
+      component={CieExpiredOrInvalidScreen}
+    />
+    <Stack.Screen
+      name={ITW_ROUTES.ACTIVATION.CIE_WRONG_PIN_SCREEN}
+      component={CieWrongCiePinScreen}
     />
   </Stack.Navigator>
 );
