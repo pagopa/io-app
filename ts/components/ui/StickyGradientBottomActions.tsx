294700f58dbbd79ab1f4c2d9e1cb2badb46b599b
Damiano Plebani <damiano.plebani@pagopa.it>
2023-07-12 16:55:44 +0200

    Add a method to calculate precisely the component height

--- ts/components/ui/StickyGradientBottomActions.tsx
+++ ts/components/ui/StickyGradientBottomActions.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { View, StyleSheet, Alert } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useMemo } from "react";
 import { IOColors, hexToRgba } from "../core/variables/IOColors";
 import { WithTestID } from "../../types/WithTestID";
 import { IOVisualCostants } from "../core/variables/IOStyles";
@@ -14,7 +15,7 @@ export type StickyGradientBottomActions = WithTestID<{
 }>;
 
 const HEADER_BG_COLOR: IOColors = "white";
-const GRADIENT_AREA_HEIGHT: number = 144;
+const GRADIENT_AREA_HEIGHT: number = 132;
 
 const styles = StyleSheet.create({
   buttonContainer: {
@@ -32,27 +33,30 @@ export const StickyGradientBottomActions = ({
 }: StickyGradientBottomActions) => {
   const insets = useSafeAreaInsets();
 
+  // Check if iPhone bottom handle is present. If not, add a
+  // default margin to avoid Button attached to the
+  // bottom without margin
+  const bottomMargin: number = useMemo(
+    () =>
+      insets.bottom === 0 ? IOVisualCostants.appMarginDefault : insets.bottom,
+    [insets]
+  );
+
   return (
     <View
       style={{
         width: "100%",
         position: "absolute",
         bottom: 0,
-        height: GRADIENT_AREA_HEIGHT,
-        // Check if iPhone bottom handle is present. If not, add a
-        // default margin to avoid Button attached to the
-        // bottom without margin
-        paddingBottom:
-          insets.bottom === 0
-            ? IOVisualCostants.appMarginDefault
-            : insets.bottom
+        height: GRADIENT_AREA_HEIGHT + bottomMargin,
+        paddingBottom: bottomMargin
       }}
       testID={testID}
       pointerEvents="box-none"
     >
       <View
         style={{
-          height: GRADIENT_AREA_HEIGHT,
+          height: GRADIENT_AREA_HEIGHT + bottomMargin,
           ...StyleSheet.absoluteFillObject,
           backgroundColor: hexToRgba(IOColors[HEADER_BG_COLOR], 0.7),
           borderTopColor: IOColors["error-500"],
