2dbeb0c39a4c4d8241740d43959ba4c81cde00f3
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-12 16:40:36 +0200

    fix: remove unused ref

--- ts/features/it-wallet/components/ItwActionCompleted.tsx
+++ ts/features/it-wallet/components/ItwActionCompleted.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import {
   IOPictogramSizeScale,
   Pictogram
