80fcaa2838e59473e62d2c8b358446339ba86564
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-14 00:04:42 +0200

    feat(IT Wallet): [SIW-281] Animated loading spinner overlay (#4773)
    ## Short description
    This PR introduces an animated loading spinner overlay

---
+++ ts/features/it-wallet/components/ItwLoadingSpinnerOverlay.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import customVariables from "../../../theme/variables";
+import { Overlay } from "../../../components/ui/Overlay";
+import {
+  IOColors,
+  hexToRgba
+} from "../../../components/core/variables/IOColors";
+import ItwLoadingSpinner from "./ItwLoadingSpinner";
+
+const styles = StyleSheet.create({
+  main: {
+    padding: customVariables.contentPadding,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+type Props = Readonly<{
+  isLoading: boolean;
+  captionTitle?: string;
+  captionSubtitle?: string;
+  loadingOpacity?: number;
+  children: React.ReactNode;
+  onCancel?: () => void;
+}>;
+
+/**
+ * A Component to display and overlay animated spinner conditionally
+ */
+const ItwLoadingSpinnerOverlay: React.FunctionComponent<Props> = (
+  props: Props
+) => {
+  const {
+    children,
+    isLoading,
+    loadingOpacity = 1,
+    captionTitle,
+    captionSubtitle
+  } = props;
+
+  return (
+    <Overlay
+      backgroundColor={hexToRgba(IOColors.white, loadingOpacity)}
+      foreground={
+        isLoading && (
+          <View style={styles.main}>
+            <ItwLoadingSpinner
+              color={IOColors.blue}
+              captionTitle={captionTitle}
+              captionSubtitle={captionSubtitle}
+            />
+          </View>
+        )
+      }
+    >
+      {children}
+    </Overlay>
+  );
+};
+
+export default ItwLoadingSpinnerOverlay;
