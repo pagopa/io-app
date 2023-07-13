1beb061b096805def618977d488e895bf8a0e3ca
lazyafternoons <lazyafternoons@outlook.it>
2023-07-14 00:39:27 +0200

    feat: add itw state persistance

--- ts/features/it-wallet/store/reducers/index.ts
+++ ts/features/it-wallet/store/reducers/index.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
+import { PersistConfig, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Action } from "../../../../store/actions/types";
 import itwCieReducer, { ItwCieState } from "./cie";
 import itwRequirements, { ItwRequirementsState } from "./itwRequirements";
 import itwCredentials, { ItwCredentialsState } from "./itwCredentials";
+
+const CURRENT_REDUX_ITW_STORE_VERSION = 1;
 
 export type ItWalletState = {
   requirements: ItwRequirementsState;
@@ -10,10 +14,22 @@ export type ItWalletState = {
   activation: ItwCieState;
 };
 
-const itwReducer = combineReducers<ItWalletState, Action>({
+const persistConfig: PersistConfig = {
+  key: "itWallet",
+  storage: AsyncStorage,
+  version: CURRENT_REDUX_ITW_STORE_VERSION,
+  whitelist: ["credentials"]
+};
+
+const reducers = combineReducers<ItWalletState, Action>({
   requirements: itwRequirements,
   credentials: itwCredentials,
   activation: itwCieReducer
 });
+
+const itwReducer = persistReducer<ItWalletState, Action>(
+  persistConfig,
+  reducers
+);
 
 export default itwReducer;
