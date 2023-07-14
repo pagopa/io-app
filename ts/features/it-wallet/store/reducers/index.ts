9d348951fee8ee6a2866bd528dadec190308af6a
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-07-14 15:09:14 +0200

    feat(IT Wallet): [SIW-211] Add PID preview and addition (#4786)
    This PR adds the PID preview and issuing flow.
    The preview flow is connected to the CIE authentication flow and
    consists of a loading screen and a preview screen with a visual
    representation of the PID and its claims.
    Then the PID is added to the wallet.
    
    - `ts/boot/configureStoreAndPersistor.ts`: Adds a new persistor version
    to persist itWallet storage;
    - `ts/components/messages/MessagesInbox.tsx`: Checks if the wallet is
    already activated to hide the activation banner;
    - `ts/features/common/store/reducers/index.ts`: Fixes itWallet reducer
    typing for persistance;
    - `ts/features/it-wallet/assets/img/pidCard/pidCardFront.png`: Adds
    assets for the pid front card;
    - `ts/features/it-wallet/components/ClaimsList.tsx`: Adds a component
    which renders a list of claims, currenly only PID claims are supported.
    In the future schemes will be validated to enable dynamic rendering;
    - `ts/features/it-wallet/components/PidCredential.tsx`: Adds a component
    which renders a card for the PID;
    - `ts/features/it-wallet/hooks/useItwAbortFlow.tsx`: Hooks for rendering
    a bottom sheet to abort the wallet activation flow;
    - `ts/features/it-wallet/navigation/ItwStackNavigator.tsx`: Adds screens
    to the stack navigator;
    - `ts/features/it-wallet/navigation/params.ts`: Adds new screen params;
    - `ts/features/it-wallet/navigation/routes.ts`: Adds new routes;
    - `ts/features/it-wallet/saga/handleCredentials.ts`: Adds a saga which
    handles adding new credentials to the wallet;
    - `ts/features/it-wallet/saga/index.ts`: Adds the aforementioned saga;
    - `ts/features/it-wallet/screens/ItwHomeScreen.tsx`: Checks if the
    wallet is already activated to hide the activation banner;
    -
    `ts/features/it-wallet/screens/issuing/ItwActivationInfoAuthScreen.tsx`:
    Fixes locale with new structure;
    - `ts/features/it-wallet/screens/issuing/ItwActivationTypScreen.tsx`:
    Removes this screen as it's being moved directly into the
    `PidPreviewScreen.tsx`;
    - `ts/features/it-wallet/screens/issuing/PidActivationScreen.tsx`: Adds
    a new screen for the activation flow;
    -
    `ts/features/it-wallet/screens/issuing/cie/CieConsentDataUsageScreen.tsx`:
    Adds the navigation to the preview screen after the CIE login flow;
    - `ts/features/it-wallet/store/actions/index.ts`: Adds the necessary
    actions for adding the PID;
    - `ts/features/it-wallet/store/reducers/index.ts`: Adds persitance to
    the `itWallet` state;
    - `ts/features/it-wallet/store/reducers/itwCredentials.ts`: Adds a
    reducer for adding a new credential to the wallet;
    - `ts/features/it-wallet/utils/mocks.ts`: Adds PID mocks and temporary
    utility functions;
    
    On a physical device test the whole activation flow. It should match
    what's shown in the FIGMA, beside a few UI changes made to better fit
    the already implemented DS components.
    The activation banner should disappear both from `Messages` and `Wallet`
    sections after activating the wallet.
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

--- ts/features/it-wallet/store/reducers/index.ts
+++ ts/features/it-wallet/store/reducers/index.ts
@@ -1,16 +1,37 @@
 import { combineReducers } from "redux";
+import { PersistConfig, PersistPartial, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Action } from "../../../../store/actions/types";
 import itwCieReducer, { ItwCieState } from "./cie";
 import itwRequirements, { ItwRequirementsState } from "./itwRequirements";
+import itwCredentials, { ItwWalletState } from "./itwCredentials";
+
+const CURRENT_REDUX_ITW_STORE_VERSION = 1;
 
 export type ItWalletState = {
   requirements: ItwRequirementsState;
+  wallet: ItwWalletState;
   activation: ItwCieState;
 };
 
-const itwReducer = combineReducers<ItWalletState, Action>({
+export type PersistedItWalletState = ItWalletState & PersistPartial;
+
+const persistConfig: PersistConfig = {
+  key: "itWallet",
+  storage: AsyncStorage,
+  version: CURRENT_REDUX_ITW_STORE_VERSION,
+  whitelist: ["wallet"]
+};
+
+const reducers = combineReducers<ItWalletState, Action>({
   requirements: itwRequirements,
+  wallet: itwCredentials,
   activation: itwCieReducer
 });
+
+const itwReducer = persistReducer<ItWalletState, Action>(
+  persistConfig,
+  reducers
+);
 
 export default itwReducer;
---
+++ ts/features/it-wallet/store/reducers/itwCredentials.ts
@@ -0,0 +1,53 @@
+import { getType } from "typesafe-actions";
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import { Action } from "../../../../store/actions/types";
+import { itwCredentialsAddPid } from "../actions";
+import { PidMockType } from "../../utils/mocks";
+import { ItWalletError } from "../../utils/errors/itwErrors";
+import { GlobalState } from "../../../../store/reducers/types";
+
+type ItwWalletType = {
+  activated: boolean;
+  vcs: Array<PidMockType>;
+};
+
+export type ItwWalletState = pot.Pot<ItwWalletType, ItWalletError>;
+
+const emptyState: ItwWalletState = pot.none;
+
+/**
+ * This reducer handles the requirements check for the IT Wallet activation.
+ * It manipulates a pot which maps to an error if the requirements are not met or to true if they are.
+ * A saga is attached to the request action to check the requirements.
+ * @param state the current state
+ * @param action the dispatched action
+ * @returns the result state
+ */
+const reducer = (
+  state: ItwWalletState = emptyState,
+  action: Action
+): ItwWalletState => {
+  switch (action.type) {
+    case getType(itwCredentialsAddPid.request):
+      return pot.toLoading(state);
+    case getType(itwCredentialsAddPid.success):
+      return pot.some({
+        activated: true,
+        vcs: [action.payload]
+      });
+    case getType(itwCredentialsAddPid.failure):
+      return pot.toError(state, action.payload);
+  }
+  return state;
+};
+
+export const ItwWalletSelector = (state: GlobalState) =>
+  state.features.itWallet.wallet;
+
+export const ItwWalletActivatedSelector = (state: GlobalState) =>
+  pot.getOrElse(
+    pot.map(state.features.itWallet.wallet, w => w.activated),
+    false
+  );
+
+export default reducer;
