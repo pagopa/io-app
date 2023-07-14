0e5bf40d670153725747d81e9fe3e59c0d772d2f
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-14 10:10:09 +0200

    refactor: add loading spinner with a simulated timeout

--- ts/features/it-wallet/screens/issuing/ItwActivationTypScreen.tsx
+++ ts/features/it-wallet/screens/issuing/ItwActivationTypScreen.tsx
@@ -7,29 +7,46 @@ import FooterWithButtons from "../../../../components/ui/FooterWithButtons";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import ROUTES from "../../../../navigation/routes";
 import ItwActionCompleted from "../../components/ItwActionCompleted";
+import ItwLoadingSpinnerOverlay from "../../components/ItwLoadingSpinnerOverlay";
+import { useOnFirstRender } from "../../../../utils/hooks/useOnFirstRender";
 
 const ItwActivationTypScreen = (): React.ReactElement => {
   const navigation = useNavigation();
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+  // Simulate a loading time for
+  // a backgroud process
+  // in order to show the loading spinner
+  // In production this will be removed
+  useOnFirstRender(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  });
+
   return (
-    <SafeAreaView style={IOStyles.flex}>
-      <BaseScreenComponent
-        headerTitle="Attivazione IT Wallet"
-        goBack={() => navigation.navigate(ROUTES.MAIN)}
-      >
-        <ItwActionCompleted
-          title={I18n.t("features.itWallet.issuing.typ.title")}
-          content={I18n.t("features.itWallet.issuing.typ.content")}
-        />
-        <FooterWithButtons
-          type={"SingleButton"}
-          leftButton={{
-            onPress: () => navigation.navigate(ROUTES.ITWALLET_HOME),
-            title: I18n.t("features.itWallet.issuing.typ.button"),
-            block: true
-          }}
-        />
-      </BaseScreenComponent>
-    </SafeAreaView>
+    <ItwLoadingSpinnerOverlay
+      isLoading={isLoading}
+      captionTitle={I18n.t("features.itWallet.issuing.loading.title")}
+      captionSubtitle={I18n.t("features.itWallet.issuing.loading.subtitle")}
+    >
+      <SafeAreaView style={IOStyles.flex}>
+        <BaseScreenComponent goBack={false}>
+          <ItwActionCompleted
+            title={I18n.t("features.itWallet.issuing.typ.title")}
+            content={I18n.t("features.itWallet.issuing.typ.content")}
+          />
+          <FooterWithButtons
+            type={"SingleButton"}
+            leftButton={{
+              onPress: () => navigation.navigate(ROUTES.ITWALLET_HOME),
+              title: I18n.t("features.itWallet.issuing.typ.button"),
+              block: true
+            }}
+          />
+        </BaseScreenComponent>
+      </SafeAreaView>
+    </ItwLoadingSpinnerOverlay>
   );
 };
 
