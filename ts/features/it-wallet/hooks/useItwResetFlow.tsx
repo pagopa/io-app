c47b2c5130550739ee5425262bbf2924f164d81e
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-07-19 19:11:20 +0200

    feat: add wallet reset button

---
+++ ts/features/it-wallet/hooks/useItwResetFlow.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { View } from "react-native";
+import { useDispatch } from "react-redux";
+import { useIOBottomSheetModal } from "../../../utils/hooks/bottomSheet";
+import { IOStyles } from "../../../components/core/variables/IOStyles";
+import FooterWithButtons from "../../../components/ui/FooterWithButtons";
+import I18n from "../../../i18n";
+import { H4 } from "../../../components/core/typography/H4";
+import { itwCredentialsReset } from "../store/actions";
+import { IOColors } from "../../../components/core/variables/IOColors";
+
+/**
+ * A hook that returns a function to present the reset wallet bottom sheet in the wallet home screen.
+ */
+export const useItwResetFlow = () => {
+  const dispatch = useDispatch();
+  const BottomSheetBody = () => (
+    <View style={IOStyles.flex}>
+      <H4 color={"bluegreyDark"} weight={"Regular"}>
+        {I18n.t("features.itWallet.homeScreen.reset.bottomSheet.body")}
+      </H4>
+    </View>
+  );
+  const Footer = () => (
+    <FooterWithButtons
+      type={"TwoButtonsInlineHalf"}
+      leftButton={{
+        onPressWithGestureHandler: true,
+        bordered: true,
+        onPress: () => dismiss(),
+        title: I18n.t("features.itWallet.homeScreen.reset.bottomSheet.cancel")
+      }}
+      rightButton={{
+        onPress: () => {
+          dispatch(itwCredentialsReset());
+          dismiss();
+        },
+        primary: true,
+        labelColor: IOColors.white,
+        title: I18n.t("features.itWallet.homeScreen.reset.bottomSheet.confirm")
+      }}
+    />
+  );
+  const { present, bottomSheet, dismiss } = useIOBottomSheetModal({
+    title: I18n.t("features.itWallet.homeScreen.reset.bottomSheet.title"),
+    component: <BottomSheetBody />,
+    snapPoint: [300],
+    footer: <Footer />
+  });
+
+  return {
+    dismiss,
+    present,
+    bottomSheet
+  };
+};
