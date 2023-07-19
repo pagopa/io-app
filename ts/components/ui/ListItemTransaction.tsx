8e6841b802000da131653349fb5844c3a950184d
Damiano Plebani <damiano.plebani@pagopa.it>
2023-07-19 16:49:45 +0200

    chore: [IOAPPFD0-129] Remove extra space from `ListItem…` components (#4809)
    ## Short description
    This PR removes the extra space between label and value from all
    `ListItem...` components, according to the design guidelines.
    
    ## List of changes proposed in this pull request
    - Remove extra space from the components
    
    ### Preview
    | Before | After |
    |--------|--------|
    | ![Simulator Screen Shot - iPhone 13 - 2023-07-19 at 15 33
    40](https://github.com/pagopa/io-app/assets/1255491/35d2422c-3237-4550-b062-9f2338bfa3c4)
    | ![Simulator Screen Shot - iPhone 13 - 2023-07-19 at 15 33
    23](https://github.com/pagopa/io-app/assets/1255491/8df42697-ef64-4a51-8599-4f04b1c45d97)
    |
    
    
    ## How to test
    Go to the **Profile → Design System → List Items** (under Components)
    
    ---------
    
    Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>

--- ts/components/ui/ListItemTransaction.tsx
+++ ts/components/ui/ListItemTransaction.tsx
@@ -13,7 +13,7 @@ import { IOLogoPaymentType } from "../core/logos";
 import { VSpacer } from "../core/spacer/Spacer";
 import { LabelSmall } from "../core/typography/LabelSmall";
 import { NewH6 } from "../core/typography/NewH6";
-import { useIOTheme } from "../core/variables/IOColors";
+import { IOColors, useIOTheme } from "../core/variables/IOColors";
 import { IOListItemLogoMargin } from "../core/variables/IOSpacing";
 import { getCardLogoComponent } from "../../features/idpay/common/components/CardLogo";
 import {
@@ -97,13 +97,14 @@ export const ListItemTransaction = ({
   transactionStatus = "success"
 }: ListItemTransaction) => {
   const theme = useIOTheme();
-
   const isDSEnabled = useIOSelector(isDesignSystemEnabledSelector);
+
   if (isLoading) {
     return <SkeletonComponent />;
   }
 
-  const designSystemBlue = isDSEnabled ? "blue" : "blueIO-500";
+  const designSystemBlue: IOColors = isDSEnabled ? "blue" : "blueIO-500";
+
   const ListItemTransactionContent = () => {
     const TransactionAmountOrBadgeComponent = () => {
       switch (transactionStatus) {
@@ -158,7 +159,6 @@ export const ListItemTransaction = ({
           <LabelSmall weight="Regular" color={theme["textBody-tertiary"]}>
             {subtitle}
           </LabelSmall>
-          <VSpacer size={4} />
         </View>
         <View style={Styles.rightSection}>
           <TransactionAmountOrBadgeComponent />
