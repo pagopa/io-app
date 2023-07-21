f9a20402f0f096591036651d9855ac61aa3f56cf
forrest57 <tomba808@gmail.com>
2023-07-21 11:33:03 +0200

    moved to DS ListItems

--- ts/features/idpay/initiative/details/components/InitiativeSettingsComponent.tsx
+++ ts/features/idpay/initiative/details/components/InitiativeSettingsComponent.tsx
@@ -1,84 +1,25 @@
 import { useNavigation } from "@react-navigation/core";
 import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/function";
-import { ListItem as NBListItem } from "native-base";
 import React from "react";
 import { View } from "react-native";
-import Placeholder from "rn-placeholder";
 import {
   InitiativeDTO,
   StatusEnum as InitiativeStatusEnum
 } from "../../../../../../definitions/idpay/InitiativeDTO";
-import { HSpacer, VSpacer } from "../../../../../components/core/spacer/Spacer";
+import { VSpacer } from "../../../../../components/core/spacer/Spacer";
 import { H3 } from "../../../../../components/core/typography/H3";
-import { H4 } from "../../../../../components/core/typography/H4";
-import { LabelSmall } from "../../../../../components/core/typography/LabelSmall";
-import { IOStyles } from "../../../../../components/core/variables/IOStyles";
+import ListItemNav from "../../../../../components/ui/ListItemNav";
+import ListItemNavAlert from "../../../../../components/ui/ListItemNavAlert";
 import I18n from "../../../../../i18n";
 import {
   AppParamsList,
   IOStackNavigationProp
 } from "../../../../../navigation/params/AppParamsList";
 import { IDPayConfigurationRoutes } from "../../configuration/navigation/navigator";
-import { Icon } from "../../../../../components/core/icons/Icon";
 
 type Props = {
   initiative?: InitiativeDTO;
-};
-
-type SettingsButtonProps = {
-  title: string;
-  subtitle?: string;
-  onPress?: () => void;
-  hasWarnings?: boolean;
-  isLoading?: boolean;
-};
-
-const SettingsButtonComponent = (props: SettingsButtonProps) => {
-  const { title, subtitle, onPress, hasWarnings, isLoading } = props;
-
-  const getSubtitleComponent = () => {
-    if (isLoading) {
-      return (
-        <>
-          <VSpacer size={4} />
-          <Placeholder.Box animate="fade" height={16} width={120} radius={4} />
-        </>
-      );
-    }
-
-    if (hasWarnings) {
-      return (
-        <LabelSmall weight="SemiBold" color="red">
-          {I18n.t(
-            "idpay.initiative.details.initiativeDetailsScreen.configured.settings.actionsRequired"
-          )}
-        </LabelSmall>
-      );
-    }
-
-    return (
-      <LabelSmall weight="Regular" color="bluegrey">
-        {subtitle}
-      </LabelSmall>
-    );
-  };
-
-  return (
-    <NBListItem onPress={onPress} style={{ paddingEnd: 0 }}>
-      {hasWarnings && (
-        <>
-          <Icon name="notice" color="red" />
-          <HSpacer size={16} />
-        </>
-      )}
-      <View style={IOStyles.flex}>
-        <H4>{title}</H4>
-        {getSubtitleComponent()}
-      </View>
-      <Icon name="chevronRightListItem" color="blue" />
-    </NBListItem>
-  );
 };
 
 const InitiativeSettingsComponent = (props: Props) => {
@@ -110,19 +51,21 @@ const InitiativeSettingsComponent = (props: Props) => {
     O.fromNullable,
     O.fold(
       () => (
-        <SettingsButtonComponent
-          title={I18n.t(
+        <ListItemNav
+          value={I18n.t(
             "idpay.initiative.details.initiativeDetailsScreen.configured.settings.associatedPaymentMethods"
           )}
-          isLoading={true}
+          accessibilityLabel=""
+          onPress={() => null}
         />
       ),
-      ({ initiativeId, nInstr, status }) => (
-        <SettingsButtonComponent
-          title={I18n.t(
+      ({ initiativeId, nInstr, status }) => {
+        // between ListItemNav and ListItemNavAlert, ListItemNavAlert is the least inclusive one
+        const listItemOptions: ListItemNavAlert = {
+          value: I18n.t(
             "idpay.initiative.details.initiativeDetailsScreen.configured.settings.associatedPaymentMethods"
-          )}
-          subtitle={I18n.t(
+          ),
+          description: I18n.t(
             `idpay.initiative.details.initiativeDetailsScreen.configured.settings.methods`,
             {
               defaultValue: I18n.t(
@@ -131,11 +74,17 @@ const InitiativeSettingsComponent = (props: Props) => {
               ),
               count: nInstr
             }
-          )}
-          onPress={() => navigateToInstrumentsConfiguration(initiativeId)}
-          hasWarnings={status === InitiativeStatusEnum.NOT_REFUNDABLE_ONLY_IBAN}
-        />
-      )
+          ),
+
+          onPress: () => navigateToInstrumentsConfiguration(initiativeId),
+          accessibilityLabel: ""
+        };
+        return status === InitiativeStatusEnum.NOT_REFUNDABLE_ONLY_IBAN ? (
+          <ListItemNavAlert {...listItemOptions} />
+        ) : (
+          <ListItemNav {...listItemOptions} />
+        );
+      }
     )
   );
 
@@ -144,25 +93,30 @@ const InitiativeSettingsComponent = (props: Props) => {
     O.fromNullable,
     O.fold(
       () => (
-        <SettingsButtonComponent
-          title={I18n.t(
+        <ListItemNav
+          value={I18n.t(
             "idpay.initiative.details.initiativeDetailsScreen.configured.settings.selectedIBAN"
           )}
-          isLoading={true}
+          accessibilityLabel=""
+          onPress={() => null}
         />
       ),
-      ({ initiativeId, iban, status }) => (
-        <SettingsButtonComponent
-          title={I18n.t(
+      ({ initiativeId, iban, status }) => {
+        const listItemOptions: ListItemNavAlert = {
+          value: I18n.t(
             "idpay.initiative.details.initiativeDetailsScreen.configured.settings.selectedIBAN"
-          )}
-          subtitle={iban}
-          onPress={() => navigateToIbanConfiguration(initiativeId)}
-          hasWarnings={
-            status === InitiativeStatusEnum.NOT_REFUNDABLE_ONLY_INSTRUMENT
-          }
-        />
-      )
+          ),
+          description: iban,
+          onPress: () => navigateToIbanConfiguration(initiativeId),
+          accessibilityLabel: ""
+        };
+        return status ===
+          InitiativeStatusEnum.NOT_REFUNDABLE_ONLY_INSTRUMENT ? (
+          <ListItemNavAlert {...listItemOptions} />
+        ) : (
+          <ListItemNav {...listItemOptions} />
+        );
+      }
     )
   );
 
