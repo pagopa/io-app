281aac961f58e1f595206709e43466ead6cfc972
Damiano Plebani <damiano.plebani@pagopa.it>
2023-07-21 12:42:58 +0200

    Add basic border transition

--- ts/features/design-system/core/DSHeaderSecondLevel.tsx
+++ ts/features/design-system/core/DSHeaderSecondLevel.tsx
@@ -1,34 +1,79 @@
 import * as React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 import {
   SafeAreaView,
   useSafeAreaInsets
 } from "react-native-safe-area-context";
-import { IOColors } from "../../../components/core/variables/IOColors";
-import { H2 } from "../../../components/core/typography/H2";
+import Animated, {
+  interpolateColor,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue
+} from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
 import { Body } from "../../../components/core/typography/Body";
-import { IOVisualCostants } from "../../../components/core/variables/IOStyles";
+import {
+  IOColors,
+  hexToRgba
+} from "../../../components/core/variables/IOColors";
+
+const borderColorDisabled = hexToRgba(IOColors["grey-100"], 0);
 
 export const DSHeaderSecondLevel = () => {
   const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
+  const translationY = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler(event => {
+    // eslint-disable-next-line functional/immutable-data
+    translationY.value = event.contentOffset.y;
+  });
+
+  const aStyle = useAnimatedStyle(() => ({
+    // backgroundColor: interpolateColor(
+    //   translationY.value,
+    //   [0, 50],
+    //   ["white", "skyblue"],
+    //   "RGB"
+    // ),
+    borderColor: interpolateColor(
+      translationY.value,
+      [0, 50],
+      [borderColorDisabled, IOColors["grey-100"]]
+    )
+  }));
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      header: () => (
+        <Animated.View
+          style={[
+            // eslint-disable-next-line react-native/no-color-literals
+            {
+              paddingTop: 50,
+              padding: 15,
+              borderWidth: 1
+            },
+            aStyle
+          ]}
+        >
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Testing</Text>
+        </Animated.View>
+      )
+    });
+  }, [aStyle, navigation]);
 
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={["left", "right"]}>
-      <ScrollView
-        contentContainerStyle={{
-          backgroundColor: IOColors.white,
-          paddingTop: IOVisualCostants.appMarginDefault,
-          paddingBottom: insets.bottom
-        }}
+    <View style={{}}>
+      <Animated.ScrollView
+        contentContainerStyle={{ paddingBottom: insets.bottom }}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
       >
-        <View style={{ paddingHorizontal: IOVisualCostants.appMarginDefault }}>
-          <H2>Start</H2>
-          {[...Array(50)].map((_el, i) => (
-            <Body key={`body-${i}`}>Repeated text</Body>
-          ))}
-          <H2>End</H2>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        {[...Array(50)].map((_el, i) => (
+          <Body key={`body-${i}`}>Repeated text</Body>
+        ))}
+      </Animated.ScrollView>
+    </View>
   );
 };
