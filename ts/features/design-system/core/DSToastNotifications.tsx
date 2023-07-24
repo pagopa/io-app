74bca2f1fcfa93f286a347900bd1ef821f75f918
Federico Mastrini <federicomastrini93@gmail.com>
2023-07-24 09:56:12 +0200

    chore: removed NB button from DS showroom screen

--- ts/features/design-system/core/DSToastNotifications.tsx
+++ ts/features/design-system/core/DSToastNotifications.tsx
@@ -1,9 +1,9 @@
-import { Text as NBButtonText, Toast as NBToast } from "native-base";
+import { Toast as NBToast } from "native-base";
 import * as React from "react";
-import ButtonDefaultOpacity from "../../../components/ButtonDefaultOpacity";
 import { VSpacer } from "../../../components/core/spacer/Spacer";
 import { H2 } from "../../../components/core/typography/H2";
 import { H3 } from "../../../components/core/typography/H3";
+import ButtonOutline from "../../../components/ui/ButtonOutline";
 import {
   IOToast,
   ToastNotification
@@ -17,65 +17,48 @@ export const DSToastNotifications = () => (
       Events
     </H3>
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        IOToast.show("Hello!", {
-          icon: "checkTick"
-        });
-      }}
-    >
-      <NBButtonText>Neutral</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Neutral"
+      accessibilityLabel="Neutral"
+      onPress={() => IOToast.show("Hello!")}
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        IOToast.error("Error");
-      }}
-    >
-      <NBButtonText>Error</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Error"
+      accessibilityLabel="Error"
+      onPress={() => IOToast.error("Error")}
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        IOToast.info("Info");
-      }}
-    >
-      <NBButtonText>Info</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Info"
+      accessibilityLabel="Info"
+      onPress={() => IOToast.info("Info")}
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        IOToast.success("Success");
-      }}
-    >
-      <NBButtonText>Success</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Success"
+      accessibilityLabel="Success"
+      onPress={() => IOToast.success("Success")}
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        IOToast.warning("Warning");
-      }}
-    >
-      <NBButtonText>Warning</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Warning"
+      accessibilityLabel="Warning"
+      onPress={() => IOToast.warning("Warning")}
+    />
 
     <H3
       color={"bluegrey"}
@@ -109,53 +92,43 @@ export const DSToastNotifications = () => (
       Type
     </H3>
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
+    <ButtonOutline
+      fullWidth={true}
+      label="Default behavior"
+      accessibilityLabel="Default behavior"
+      onPress={() =>
         NBToast.show({
           text: "Here's the default behavior"
-        });
-      }}
-    >
-      <NBButtonText>Default behavior</NBButtonText>
-    </ButtonDefaultOpacity>
+        })
+      }
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        showToast("Example of a danger message");
-      }}
-    >
-      <NBButtonText>Danger</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Danger"
+      accessibilityLabel="Danger"
+      onPress={() => showToast("Example of a danger message")}
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        showToast("Example of a success message", "success");
-      }}
-    >
-      <NBButtonText>Success</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Success"
+      accessibilityLabel="Success"
+      onPress={() => showToast("Example of a success message", "success")}
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        showToast("Example of a warning message", "warning");
-      }}
-    >
-      <NBButtonText>Warning</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Warning"
+      accessibilityLabel="Warning"
+      onPress={() => showToast("Example of a warning message", "warning")}
+    />
 
     <H3
       color={"bluegrey"}
@@ -164,39 +137,37 @@ export const DSToastNotifications = () => (
     >
       Position
     </H3>
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        showToast("Here's the notification at the top", "danger", "top");
-      }}
-    >
-      <NBButtonText>Default · Top</NBButtonText>
-    </ButtonDefaultOpacity>
+
+    <ButtonOutline
+      fullWidth={true}
+      label="Default · Top"
+      accessibilityLabel="Default · Top"
+      onPress={() =>
+        showToast("Here's the notification at the top", "danger", "top")
+      }
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        showToast("Here's the notification at the center", "danger", "center");
-      }}
-    >
-      <NBButtonText>Default · Center</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Default · Center"
+      accessibilityLabel="Default · Center"
+      onPress={() =>
+        showToast("Here's the notification at the top", "danger", "top")
+      }
+    />
 
     <VSpacer size={16} />
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
-        showToast("Here's the notification at the bottom", "danger", "bottom");
-      }}
-    >
-      <NBButtonText>Default · Bottom</NBButtonText>
-    </ButtonDefaultOpacity>
+    <ButtonOutline
+      fullWidth={true}
+      label="Default · Bottom"
+      accessibilityLabel="Default · Bottom"
+      onPress={() =>
+        showToast("Here's the notification at the bottom", "danger", "bottom")
+      }
+    />
 
     <H3
       color={"bluegrey"}
@@ -206,17 +177,16 @@ export const DSToastNotifications = () => (
       Misc
     </H3>
 
-    <ButtonDefaultOpacity
-      bordered={true}
-      block={true}
-      onPress={() => {
+    <ButtonOutline
+      fullWidth={true}
+      label="Multi-line text"
+      accessibilityLabel="Multi-line text"
+      onPress={() =>
         NBToast.show({
           text: "Here's the default behavior with multi-line loooong loooooong text"
-        });
-      }}
-    >
-      <NBButtonText>Multi-line text</NBButtonText>
-    </ButtonDefaultOpacity>
+        })
+      }
+    />
 
     <VSpacer size={40} />
   </DesignSystemScreen>
