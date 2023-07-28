2b9151105d60278efd22315ee223fecdc0cd328e
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-07-28 10:00:45 +0200

    feat: add pid issuing flow

--- ts/features/it-wallet/utils/wia.ts
+++ ts/features/it-wallet/utils/wia.ts
@@ -1,34 +1,12 @@
-import {
-  CryptoError,
-  generate,
-  getPublicKey,
-  sign
-} from "@pagopa/io-react-native-crypto";
+import { sign } from "@pagopa/io-react-native-crypto";
 import { WalletInstanceAttestation } from "@pagopa/io-react-native-wallet";
 import { walletProviderUrl } from "../../../config";
+import { generateCryptoKey } from "./keychain";
 
 /**
  * The key alias used to store the WIA crypto key in the keychain.
  */
-export const ITW_WIA_CRYPTO_KEY = "ITW_WIA_CRYTPO";
-
-/**
- * Getter for the public key used to sign the WIA attestation.
- * This method tries to generate a new crypto key pair but if it already exists it returns already existing one.
- * @returns the public key used to sign the WIA attestation.
- */
-export const getWiaPublicKey = async () => {
-  try {
-    return await generate(ITW_WIA_CRYPTO_KEY);
-  } catch (e) {
-    const { message } = e as CryptoError;
-    if (message === "KEY_ALREADY_EXISTS") {
-      return await getPublicKey(ITW_WIA_CRYPTO_KEY);
-    } else {
-      throw e;
-    }
-  }
-};
+export const ITW_WIA_KEY_TAG = "ITW_WIA_CRYTPO";
 
 /**
  * Getter for the Wallet Instance Attestation.
@@ -36,12 +14,12 @@ export const getWiaPublicKey = async () => {
  * @returns the WIA attestation
  */
 export const getWia = async () => {
-  const publicKey = await getWiaPublicKey();
+  const publicKey = await generateCryptoKey(ITW_WIA_KEY_TAG);
   const issuing = new WalletInstanceAttestation.Issuing(walletProviderUrl);
   const attestationRequest = await issuing.getAttestationRequestToSign(
     publicKey
   );
-  const signature = await sign(attestationRequest, ITW_WIA_CRYPTO_KEY);
+  const signature = await sign(attestationRequest, ITW_WIA_KEY_TAG);
   return (
     await issuing.getAttestation(attestationRequest, signature)
   ).toString();
