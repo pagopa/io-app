8a759e9f8d30a15535877e370f246547bc702c50
Mario Perrotta <mario.perrotta@pagopa.it>
2023-07-28 10:26:32 +0200

    chore: add identification screen pid issuing

--- ts/features/it-wallet/saga/handleCredentials.ts
+++ ts/features/it-wallet/saga/handleCredentials.ts
@@ -1,7 +1,14 @@
 import { SagaIterator } from "redux-saga";
-import { delay, put } from "typed-redux-saga/macro";
-import { ActionType } from "typesafe-actions";
+import { delay, put, take } from "typed-redux-saga/macro";
+import { ActionType, isActionOf } from "typesafe-actions";
+import { CommonActions } from "@react-navigation/native";
 import { itwCredentialsAddPid } from "../store/actions";
+import {
+  identificationRequest,
+  identificationSuccess
+} from "../../../store/actions/identification";
+import I18n from "../../../i18n";
+import NavigationService from "../../../navigation/NavigationService";
 
 /*
  * This saga handles adding new credentials to the wallet.
@@ -10,6 +17,17 @@ import { itwCredentialsAddPid } from "../store/actions";
 export function* handleCredentialsAddPid(
   action: ActionType<typeof itwCredentialsAddPid.request>
 ): SagaIterator {
-  yield* delay(2000);
-  yield* put(itwCredentialsAddPid.success(action.payload));
+  yield* put(
+    identificationRequest(false, true, undefined, {
+      label: I18n.t("global.buttons.cancel"),
+      onCancel: () =>
+        NavigationService.dispatchNavigationAction(CommonActions.goBack())
+    })
+  );
+  const res = yield* take(identificationSuccess);
+
+  if (isActionOf(identificationSuccess, res)) {
+    yield* delay(2000);
+    yield* put(itwCredentialsAddPid.success(action.payload));
+  }
 }
