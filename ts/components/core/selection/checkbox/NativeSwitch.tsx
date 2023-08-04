eaba26fe8969c844a25f3f800621753b9893a29b
Davide Marro <131177412+drmarro@users.noreply.github.com>
2023-08-04 16:54:40 +0200

    feat: [IOPLT-121] Migrating Radio, Switch and Checkbox components from Design System (#4878)
    ## Short description
    Migrating Radio, Switch and Checkbox components from
    @pagopa/io-app-design-system library.
    ## List of changes proposed in this pull request
    - AnimatedTick
    - AnimatedCheckbox
    - AnimatedMessageCheckbox
    - RadioGroup
    - AnimatedRadio
    - AnimatedSwitch
    - CheckboxLabel
    - NativeSwitch
    - SwitchLabel
    
    ---------
    
    Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>

--- ts/components/core/selection/checkbox/NativeSwitch.tsx
+++ ts/components/core/selection/checkbox/NativeSwitch.tsx
@@ -1,34 +1,41 @@
+import { NativeSwitch as DSNativeSwitch } from "@pagopa/io-app-design-system";
 import React from "react";
 import { Platform, Switch, SwitchProps } from "react-native";
-import { IOColors } from "../../variables/IOColors";
-import { IOSwitchVisualParams } from "../../variables/IOStyles";
 import { useIOSelector } from "../../../../store/hooks";
 import { isDesignSystemEnabledSelector } from "../../../../store/reducers/persistedPreferences";
+import { IOColors } from "../../variables/IOColors";
+import { IOSwitchVisualParams } from "../../variables/IOStyles";
 
 type OwnProps = Pick<SwitchProps, "onValueChange" | "value">;
 
 const bgLegacyTrackColorAndroid =
   Platform.OS === "android" ? IOColors["grey-300"] : IOColors.greyUltraLight;
 
+/**
+ * This component provides a switch toggle that uses the native platform switch component.
+ * It can be integrated into other composite components.
+ * Currently if the Design System is enabled, the component returns the NativeSwitch of the @pagopa/io-app-design-system library
+ * otherwise it returns the legacy component.
+ *
+ * @param {boolean} value - The current value of the switch toggle (true for "on", false for "off").
+ * @param {function} onValueChange - The function to be called when the switch value changes.
+ *
+ * @deprecated The usage of this component is discouraged as it is being replaced by the NativeSwitch of the @pagopa/io-app-design-system library.
+ *
+ */
 export const NativeSwitch = ({ onValueChange, value }: OwnProps) => {
   const isDesignSystemEnabled = useIOSelector(isDesignSystemEnabledSelector);
 
-  return (
+  return isDesignSystemEnabled ? (
+    <DSNativeSwitch onValueChange={onValueChange} value={value} />
+  ) : (
     <Switch
       trackColor={{
-        false: isDesignSystemEnabled
-          ? IOColors[IOSwitchVisualParams.bgColorOffState]
-          : bgLegacyTrackColorAndroid,
-        true: isDesignSystemEnabled
-          ? IOColors[IOSwitchVisualParams.bgColorOnState]
-          : IOColors.blue
+        false: bgLegacyTrackColorAndroid,
+        true: IOColors.blue
       }}
       thumbColor={IOColors[IOSwitchVisualParams.bgCircle]}
-      ios_backgroundColor={
-        isDesignSystemEnabled
-          ? IOColors[IOSwitchVisualParams.bgColorOffState]
-          : bgLegacyTrackColorAndroid
-      }
+      ios_backgroundColor={bgLegacyTrackColorAndroid}
       onValueChange={onValueChange}
       value={value}
     />
