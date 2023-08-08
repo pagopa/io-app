b1b49110ade91ba321d44473b5d6b80dd817fc61
Martino Cesari Tomba <60693085+forrest57@users.noreply.github.com>
2023-08-08 12:38:34 +0200

    chore: [IOBP-160] New design for credit card detail screen (#4867)
    ## Short description
    applied new design to credit card details screen, also refactored
    screens and components as they came to hand, with the aim to remove
    legacy things like class components and `connect` syntax
    <img width="250" alt="Screenshot 2023-08-01 at 15 43 56"
    src="https://github.com/pagopa/io-app/assets/60693085/b41da2e7-32ce-4ef1-95ef-d3640425a836">
    
    ## List of changes proposed in this pull request
    - took`IOLogoPaymentType` case mapping from idpay's scope and added it
    to IOLogoPayment in order to avoid casing problems that stem from
    formatting by different data sources
    - added new `keysDecoderFromObject` util function, which takes an object
    and the `keyof typeof obj` type of it, and returns an io-ts decoder for
    a union of its keys as literals
    - small `PaymentCardBig` refactor to best adress new problems that
    emerged in the component's first real-world usage
    - updated its test snapsots
    - updated `LogoPaymentOrDefaultIcon` to use the new map and a decoder
    generated by the new util func
    - updated `IdpayInitiativesListComponent` to now handle the `isLast`
    case for the list
    - huge refactor of `basePaymentMethodScreen`
    - updated `PaymentMethodFeatures` to follow the design
    - same for `PaymentMethodInitiatives`, `PaymentMethodsSettings` and
    `CreditCardDetailScreen`
    
    
    ## How to test
    tests should solve correctly, also navigate to a credit card's details
    screen and make sure everything works correctly.
    also other kinds of payment methods should render effortlessly and with
    no visual glitches, although they dont completely follow design yet
    
    ---------
    
    Co-authored-by: Alessandro Izzo <34343582+Hantex9@users.noreply.github.com>

--- ts/features/wallet/component/features/PaymentMethodInitiatives.tsx
+++ ts/features/wallet/component/features/PaymentMethodInitiatives.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 
+import { Body, H6, IOStyles, VSpacer } from "@pagopa/io-app-design-system";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
-import { Body } from "../../../../components/core/typography/Body";
 import I18n from "../../../../i18n";
 import { IOStackNavigationProp } from "../../../../navigation/params/AppParamsList";
 import { WalletParamsList } from "../../../../navigation/params/WalletParamsList";
@@ -17,14 +15,10 @@ import {
   idPayInitiativesFromInstrumentRefreshStop
 } from "../../../idpay/wallet/store/actions";
 import { idPayEnabledInitiativesFromInstrumentSelector } from "../../../idpay/wallet/store/reducers";
-import { IOStyles } from "../../../../components/core/variables/IOStyles";
-import SectionHeader from "../../../../components/services/SectionHeader";
 
-type OwnProps = {
+type Props = {
   paymentMethod: PaymentMethod;
 } & Pick<React.ComponentProps<typeof View>, "style">;
-
-type Props = ReturnType<typeof mapDispatchToProps> & OwnProps;
 
 /**
  * This component enlists the different initiatives active on the payment methods
@@ -61,7 +55,7 @@ const PaymentMethodInitiatives = (props: Props): React.ReactElement | null => {
   return initiativesList.length > 0 ? (
     <View testID="idPayInitiativesList" style={props.style}>
       <View style={[IOStyles.rowSpaceBetween, IOStyles.alignCenter]}>
-        <SectionHeader iconName="initiatives" title="wallet.capability.title" />
+        <H6 color={"grey-700"}>{I18n.t("wallet.capability.title")}</H6>
         <Body
           weight="SemiBold"
           color="blue"
@@ -70,6 +64,7 @@ const PaymentMethodInitiatives = (props: Props): React.ReactElement | null => {
           {I18n.t("idpay.wallet.preview.showAll")}
         </Body>
       </View>
+      <VSpacer size={16} />
       <IDPayInitiativesList
         idWallet={idWalletString}
         initiatives={initiativesList.slice(0, 3)}
@@ -78,6 +73,4 @@ const PaymentMethodInitiatives = (props: Props): React.ReactElement | null => {
   ) : null;
 };
 
-const mapDispatchToProps = (_: Dispatch) => ({});
-
-export default connect(mapDispatchToProps)(PaymentMethodInitiatives);
+export default PaymentMethodInitiatives;
