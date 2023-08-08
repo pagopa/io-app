a086c406b10fd44b5a6f1c584ec89e4ee7859ad2
forrest57 <tomba808@gmail.com>
2023-08-08 10:53:01 +0200

    reverted change

--- ts/components/PreferencesListItem.tsx
+++ ts/components/PreferencesListItem.tsx
@@ -1,4 +1,3 @@
-import { H6 } from "@pagopa/io-app-design-system";
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
 import I18n from "../i18n";
@@ -6,6 +5,7 @@ import { TestID } from "../types/WithTestID";
 import { H5 } from "./core/typography/H5";
 import { LabelSmall } from "./core/typography/LabelSmall";
 import { IOStyles } from "./core/variables/IOStyles";
+import { H4 } from "./core/typography/H4";
 
 type MoreInfoProps = {
   moreInfoText: string | React.ReactNode;
@@ -40,9 +40,9 @@ const styles = StyleSheet.create({
 export const PreferencesListItem = (props: Props): React.ReactElement => (
   <View style={styles.row} testID={props.testID}>
     <View style={styles.left}>
-      <H6 weight={"SemiBold"} color={"bluegreyDark"}>
+      <H4 weight={"SemiBold"} color={"bluegreyDark"}>
         {props.title}
-      </H6>
+      </H4>
       <H5 weight={"Regular"} color={"bluegrey"}>
         {props.description}
       </H5>
