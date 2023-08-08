2a5d9b947ef1d06c0f2b01d8885d859efa4c3654
LazyAfternoons <LazyAfternoons@users.noreply.github.com>
2023-08-08 10:54:36 +0200

    feat(IT Wallet): [SIW-364] Add generic error and loading screens (#4889)
    ## Short description
    This PR introduces a generic error view tailored for ITW flows. It
    includes the error mapping within this view, eliminating the need for
    redundant component definitions across various screens. Furthermore, it
    also addresses the issue of duplicated declarations of the LoadingView
    local component by directly using the underlying `LoadingSpinnerOverlay`
    component.
    
    ## List of changes proposed in this pull request
    - Removes any `LoadingView` which only wraps the `LoadingSpinnerOverlay`
    with the `isLoading` parameter;
    - Adds the `ItwErrorViewSingleBtn` component, responsible for error
    rendering and mapping. This component also maps to a generic error when
    the error props is not available;
    - Replaces any locally defined `ErrorView` with `ItwErrorViewSingleBtn`.
    
    ## How to test 
    Test both regular and error flows. Simulate error scenarios by inducing
    errors in sagas during the flow.

--- ts/features/it-wallet/utils/errors/itwErrorsMapping.ts
+++ ts/features/it-wallet/utils/errors/itwErrorsMapping.ts
@@ -2,13 +2,20 @@ import I18n from "../../../../i18n";
 import { ItWalletError, ItWalletMappedError } from "./itwErrors";
 
 /**
- * Maps an ItWalletError to an ItWalletMappedError to display an error in the requirements screen.
+ * Getter for a generic error title and body.
+ * @returns a generic ItWalletMappedError.
+ */
+export const getItwGenericError = (): ItWalletMappedError => ({
+  title: I18n.t("features.itWallet.generic.error.title"),
+  body: I18n.t("features.itWallet.generic.error.body")
+});
+
+/**
+ * Maps an ItWalletError to an ItWalletMappedError to display an error message to the user.
  * @param error an instance of ItWalletError containg the code to be translated.
  * @returns a title and a body messages to be displayed to the user if the error is known, otherwise a generic error title and body.
  */
-export const mapRequirementsError = (
-  error: ItWalletError
-): ItWalletMappedError => {
+export const mapItwError = (error: ItWalletError): ItWalletMappedError => {
   switch (error.code) {
     case "NFC_NOT_SUPPORTED":
       return {
@@ -16,9 +23,6 @@ export const mapRequirementsError = (
         body: I18n.t("features.itWallet.infoAuthScreen.errors.nfc.body")
       };
     default:
-      return {
-        title: I18n.t("features.itWallet.generic.error.title"),
-        body: I18n.t("features.itWallet.generic.error.body")
-      };
+      return getItwGenericError();
   }
 };
