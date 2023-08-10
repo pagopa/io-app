2e4aa6769442068604e7238951cdf5c2608a68b2
Mario Perrotta <mario.perrotta@pagopa.it>
2023-08-10 20:07:34 +0200

    feat(IT Wallet): [SIW-381] Add Relying Party saga (#4912)

---
+++ ts/features/it-wallet/saga/itwRpSaga.ts
@@ -0,0 +1,77 @@
+import { SagaIterator } from "redux-saga";
+import { call, put, take, takeLatest } from "typed-redux-saga/macro";
+import { ActionType, getType } from "typesafe-actions";
+import { RelyingPartySolution } from "@pagopa/io-react-native-wallet";
+import {
+  itwRpCompleted,
+  itwRpInitialization,
+  itwRpPresentation,
+  itwRpStart,
+  itwRpStop,
+  itwRpUserConfirmed,
+  itwRpUserRejected
+} from "../store/actions/itwRpActions";
+import { itwWiaRequest } from "../store/actions/itwWiaActions";
+import { handleItwRpInitializationSaga } from "./itwRpInitialization";
+import { handleItwRpPresentationSaga } from "./itwRpPresentation";
+
+/**
+ * Watcher for the IT wallet Relying Party related sagas.
+ */
+export function* watchItwRpSaga(): SagaIterator {
+  /**
+   * Handles the ITW RP start.
+   */
+  yield* takeLatest(itwRpStart, handleRpStart);
+
+  /**
+   * Handles the ITW RP initialization flow.
+   */
+  yield* takeLatest(itwRpInitialization.request, handleItwRpInitializationSaga);
+
+  /**
+   * Handles the ITW RP presentation flow.
+   */
+  yield* takeLatest(itwRpPresentation.request, handleItwRpPresentationSaga);
+
+  /**
+   * Handles the ITW RP stop.
+   */
+  yield* takeLatest(itwRpStop, handleRpStop);
+
+  /**
+   * Handles the ITW RP completed.
+   */
+  yield* takeLatest(itwRpCompleted, handleRpCompleted);
+}
+
+export function* handleRpStart(
+  action: ActionType<typeof itwRpStart>
+): SagaIterator {
+  const authReqUrl = action.payload;
+
+  // Get WIA
+  yield* call(itwWiaRequest.request);
+  const wia = yield* take(itwWiaRequest.success);
+
+  // The RP solution is initialized using the authReqUrl
+  // of the qrcode payload
+  const RP = new RelyingPartySolution(authReqUrl, wia.payload);
+
+  yield* put(itwRpInitialization.request({ RP, authReqUrl }));
+
+  const result = yield* take([itwRpUserConfirmed, itwRpUserRejected]);
+  if (result.type === getType(itwRpUserConfirmed)) {
+    yield* put(itwRpPresentation.request(RP));
+  } else {
+    yield* put(itwRpStop());
+  }
+}
+
+export function* handleRpStop(): SagaIterator {
+  // TODO: (ex. go back or reset stack navigation)
+}
+
+export function* handleRpCompleted(): SagaIterator {
+  // TODO: (ex. show a typ)
+}
