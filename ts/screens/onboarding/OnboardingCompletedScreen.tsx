1cd2f88d0d64cb4e708b9bbaeb2b1c3f9b4bd213
Alice Di Rico <alice.dirico@outlook.it>
2023-08-10 12:07:18 +0200

    fix pictogram import and usage

--- ts/screens/onboarding/OnboardingCompletedScreen.tsx
+++ ts/screens/onboarding/OnboardingCompletedScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { SafeAreaView } from "react-native";
 import { useDispatch } from "react-redux";
+import { Pictogram } from "@pagopa/io-app-design-system";
 import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
 import { IOStyles } from "../../components/core/variables/IOStyles";
 import { InfoScreenComponent } from "../../components/infoScreen/InfoScreenComponent";
@@ -8,8 +9,6 @@ import FooterWithButtons from "../../components/ui/FooterWithButtons";
 import { BlockButtonProps } from "../../components/ui/BlockButtons";
 import I18n from "../../i18n";
 import { completeOnboarding } from "../../store/actions/onboarding";
-import PictogramFireworks from "../../components/core/pictograms/svg/PictogramFireworks";
-
 const OnboardingCompletedScreen = () => {
   const dispatch = useDispatch();
 
@@ -23,7 +22,7 @@ const OnboardingCompletedScreen = () => {
     <BaseScreenComponent>
       <SafeAreaView style={IOStyles.flex}>
         <InfoScreenComponent
-          image={<PictogramFireworks size={120} color="#00C5CA" />}
+          image={<Pictogram name="fireworks" />}
           title={I18n.t("onboarding.thankYouPage.title")}
         />
 
