c78296b0db8b172a33d6ea8b94af696a0b9de2e6
LazyAfternoons <lazyafternoons@outlook.it>
2023-09-01 16:46:02 +0200

    refactor: rp initialization and presentation sagas

--- ts/features/it-wallet/saga/itwRpInitialization.ts
+++ ts/features/it-wallet/saga/itwRpInitialization.ts
@@ -1,11 +1,14 @@
 import { SagaIterator } from "redux-saga";
-import { call, put } from "typed-redux-saga/macro";
-import { getPublicKey, sign } from "@pagopa/io-react-native-crypto";
+import { call, put, select } from "typed-redux-saga/macro";
+import { sign } from "@pagopa/io-react-native-crypto";
 import { SignJWT } from "@pagopa/io-react-native-jwt";
 import { ActionType } from "typesafe-actions";
+import * as O from "fp-ts/lib/Option";
+import { WalletInstanceAttestation } from "@pagopa/io-react-native-wallet";
 import { itwRpInitialization } from "../store/actions/itwRpActions";
 import { ItWalletErrorTypes } from "../utils/errors/itwErrors";
 import { ITW_WIA_KEY_TAG } from "../utils/wia";
+import { itwWiaSelector } from "../store/reducers/itwWiaReducer";
 
 /*
  * This saga handles the RP initialization.
@@ -14,34 +17,36 @@ import { ITW_WIA_KEY_TAG } from "../utils/wia";
 export function* handleItwRpInitializationSaga(
   action: ActionType<typeof itwRpInitialization.request>
 ): SagaIterator {
-  const { RP, authReqUrl } = action.payload;
-
-  const pk = yield* call(getPublicKey, ITW_WIA_KEY_TAG);
-
   try {
-    const unsignedDPoP = yield* call(
-      RP.getUnsignedWalletInstanceDPoP,
-      pk,
-      authReqUrl
-    );
-
-    const signedDPoP = yield* call(sign, unsignedDPoP, ITW_WIA_KEY_TAG);
-
-    const signedPayload = yield* call(
-      SignJWT.appendSignature,
-      unsignedDPoP,
-      signedDPoP
-    );
-
-    const entity = yield* call(RP.getEntityConfiguration);
-
-    const requestObject = yield* call(
-      RP.getRequestObject,
-      signedPayload,
-      entity
-    );
-
-    yield* put(itwRpInitialization.success({ requestObject, entity }));
+    const { RP, authReqUrl } = action.payload;
+    const wia = yield* select(itwWiaSelector);
+    if (O.isNone(wia)) {
+      throw new Error(); // TODO: Better error mapping
+    } else {
+      const decodedWIA = yield* call(
+        WalletInstanceAttestation.decode,
+        wia.value
+      );
+      const unsignedDPoP = yield* call(
+        RP.getUnsignedWalletInstanceDPoP,
+        decodedWIA.payload.cnf.jwk,
+        authReqUrl
+      );
+      const signedDPoP = yield* call(sign, unsignedDPoP, ITW_WIA_KEY_TAG);
+      const entity = yield* call(RP.getEntityConfiguration);
+      const signedPayload = yield* call(
+        SignJWT.appendSignature,
+        unsignedDPoP,
+        signedDPoP
+      );
+      const requestObject = yield* call(
+        RP.getRequestObject,
+        signedPayload,
+        authReqUrl,
+        entity
+      );
+      yield* put(itwRpInitialization.success({ requestObject, entity }));
+    }
   } catch (e) {
     yield* put(
       itwRpInitialization.failure({
