7cd1de1ff86c6203b721388a52f90dbc6c84433a
Alessandro Izzo <alessandro.izzo@pagopa.it>
2023-09-05 16:58:58 +0200

    chore: Replace `FooterWithButtons` with `ButtonSolid` component

--- ts/features/idpay/onboarding/screens/PDNDPrerequisitesScreen.tsx
+++ ts/features/idpay/onboarding/screens/PDNDPrerequisitesScreen.tsx
@@ -2,14 +2,13 @@ import * as pot from "@pagopa/ts-commons/lib/pot";
 import { useSelector } from "@xstate/react";
 import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/function";
-import React, { useState } from "react";
+import React from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ServiceId } from "../../../../../definitions/backend/ServiceId";
 import { VSpacer } from "../../../../components/core/spacer/Spacer";
 import { Body } from "../../../../components/core/typography/Body";
 import { H1 } from "../../../../components/core/typography/H1";
-import { IOColors } from "../../../../components/core/variables/IOColors";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import ButtonExtendedOutline from "../../../../components/ui/ButtonExtendedOutline";
@@ -24,6 +23,8 @@ import { useIOBottomSheetAutoresizableModal } from "../../../../utils/hooks/bott
 import { getPDNDCriteriaDescription } from "../utils/strings";
 import { useOnboardingMachineService } from "../xstate/provider";
 import { pdndCriteriaSelector, selectServiceId } from "../xstate/selectors";
+import ButtonSolid from "../../../../components/ui/ButtonSolid";
+import { ContentWrapper } from "../../../../components/core/ContentWrapper";
 
 const secondaryButtonProps = {
   block: true,
@@ -46,7 +47,6 @@ export const PDNDPrerequisitesScreen = () => {
   const machine = useOnboardingMachineService();
   const [authority, setAuthority] = React.useState<string | undefined>();
   const serviceId = useSelector(machine, selectServiceId);
-  const [paddingBottomSheet, setPaddingBottomSheet] = useState(0);
 
   const serviceName = pipe(
     useIOSelector(serviceByIdSelector(serviceId as ServiceId)) || pot.none,
@@ -77,23 +77,23 @@ export const PDNDPrerequisitesScreen = () => {
         </Markdown>
       ),
       footer: (
-        <FooterWithButtons
-          type="SingleButton"
-          leftButton={{
-            onLayout: evt =>
-              setPaddingBottomSheet(evt.nativeEvent.layout.height),
-            onPress: () => dismiss(),
-            block: true,
-            bordered: false,
-            labelColor: IOColors.white,
-            title: I18n.t(
+        <ContentWrapper>
+          <VSpacer size={16} />
+          <ButtonSolid
+            fullWidth
+            label={I18n.t(
               "idpay.onboarding.PDNDPrerequisites.prerequisites.info.understoodCTA"
-            )
-          }}
-        />
+            )}
+            accessibilityLabel={I18n.t(
+              "idpay.onboarding.PDNDPrerequisites.prerequisites.info.understoodCTA"
+            )}
+            onPress={() => dismiss()}
+          />
+          <VSpacer size={16} />
+        </ContentWrapper>
       )
     },
-    130 + paddingBottomSheet
+    162
   );
 
   const pdndCriteria = useSelector(machine, pdndCriteriaSelector);
