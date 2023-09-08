pagopa/io-app

commit 6886562d2d6a554ffebec19902e467e209ef428b

fix: [IOBP-211] Infinite loader if list of initiatives is an error (#4954)
## Short description
This PR fixes a recursive loader showed into a detail page of a credit
card. The bug occurs when the API request to get all the available
initiatives fails, it sets to `pot.toError` but since there is a polling
request to get the available initiatives every 3 seconds, it sets
`pot.toLoading` every time even if the previous request was in error;

## List of changes proposed in this pull request
- Into IDPay wallet reducer, when getting the list of available
initiatives, it sets `pot.toLoading` only if the previous value was not
a `pot.Error`

## How to test
Go into credit card detail page and inside the `io-dev-api-server`, try
to return a 404 response inside the `routers/features/idpay/wallet.ts`
file at the handler `/wallet/instrument/:walletId/initiatives`.

## Preview
|Before|After|
|-|-|
|<video
src="https://github.com/pagopa/io-app/assets/34343582/52b1b5e7-60c3-44ef-b21e-6dd6d8ae199d"
/>|<video
src="https://github.com/pagopa/io-app/assets/34343582/665a0eaa-dcbc-42e5-946c-ee1b334c6dde"
/>|

ts/features/idpay/wallet/store/reducers/index.ts
@@ -51,7 +51,10 @@ const reducer = (
       };
     // Initiatives with instrument
     case getType(idPayInitiativesFromInstrumentGet.request):
-      if (!action.payload.isRefreshing) {
+      if (
+        !action.payload.isRefreshing &&
+        !pot.isSome(state.initiativesWithInstrument)
+      ) {
         return {
           ...state,
           initiativesWithInstrument: pot.noneLoading,
@@ -69,11 +72,17 @@ const reducer = (
         };
       }
 
+      if (!pot.isError(state.initiativesWithInstrument)) {
+        return {
+          ...state,
+          initiativesWithInstrument: pot.toLoading(
+            state.initiativesWithInstrument
+          )
+        };
+      }
       return {
         ...state,
-        initiativesWithInstrument: pot.toLoading(
-          state.initiativesWithInstrument
-        )
+        initiativesWithInstrument: state.initiativesWithInstrument
       };
     case getType(idPayInitiativesFromInstrumentGet.success):
       const initiativesToKeepInLoadingState = pipe(

commit db88916093a0dc0a379da9cdc563fee00563deb8

fix: [IOBP-219] Initiative list into card details (#4960)
## Short description
This PR fixes a regression introduced with the PR #4954 about the
updated list for each payment method

## List of changes proposed in this pull request
- Updated reducer to refresh initiative list for a new payment method

## How to test
Open two or more payment method and check that the status of each
initiative is different from each other.

## Acknowledgement
@forrest57

ts/features/idpay/wallet/store/reducers/index.ts
@@ -51,10 +51,7 @@ const reducer = (
       };
     // Initiatives with instrument
     case getType(idPayInitiativesFromInstrumentGet.request):
-      if (
-        !action.payload.isRefreshing &&
-        !pot.isSome(state.initiativesWithInstrument)
-      ) {
+      if (!action.payload.isRefreshing) {
         return {
           ...state,
           initiativesWithInstrument: pot.noneLoading,