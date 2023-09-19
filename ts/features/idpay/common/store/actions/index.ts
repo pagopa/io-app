1304c7fd9a9e4ff274a3f093befbdc0195653605
Federico Mastrini <federicomastrini93@gmail.com>
2023-09-19 17:21:49 +0200

    fix: imports

--- ts/features/idpay/common/store/actions/index.ts
+++ ts/features/idpay/common/store/actions/index.ts
@@ -1,6 +1,6 @@
 import { IdPayCodeActions } from "../../../code/store/actions";
-import { IdPayInitiativeActions } from "../../../initiative/details/store/actions";
-import { IdPayTimelineActions } from "../../../initiative/timeline/store/actions";
+import { IdPayInitiativeActions } from "../../../details/store/actions";
+import { IdPayTimelineActions } from "../../../timeline/store/actions";
 import { IdPayWalletActions } from "../../../wallet/store/actions";
 
 export type IdPayActions =
