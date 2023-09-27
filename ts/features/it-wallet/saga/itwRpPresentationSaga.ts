59a420b7612829effaddff336ae659bf3516afc3
Mario Perrotta <mario.perrotta@pagopa.it>
2023-09-27 15:21:35 +0200

    feat(IT Wallet): [SIW-482] Update `io-react-native-wallet` version (#5031)
    ## Short description
    This PR update the package `io-react-native-wallet` version and related
    dependencies. This update resulted in a refactor on some redux-saga.
    
    ## List of changes proposed in this pull request
    - Update pid, rp and wia saga
    
    ## How to test
    Start the app with prod env and try all the use cases
    
    Co-authored-by: LazyAfternoons <LazyAfternoons@users.noreply.github.com>

--- ts/features/it-wallet/saga/itwRpPresentationSaga.ts
+++ ts/features/it-wallet/saga/itwRpPresentationSaga.ts
@@ -1,12 +1,10 @@
 import { SagaIterator } from "redux-saga";
-import { apply, call, put, select, take } from "typed-redux-saga/macro";
-import { sign } from "@pagopa/io-react-native-crypto";
-import { SignJWT } from "@pagopa/io-react-native-jwt";
+import { call, put, select } from "typed-redux-saga/macro";
 import * as O from "fp-ts/lib/Option";
 import { ActionType } from "typesafe-actions";
 import {
   RelyingPartySolution,
-  WalletInstanceAttestation
+  createCryptoContextFor
 } from "@pagopa/io-react-native-wallet";
 
 import {
@@ -15,21 +13,17 @@ import {
 } from "../store/reducers/itwRpInitializationReducer";
 import { itwRpPresentation } from "../store/actions/itwRpActions";
 import { ItWalletErrorTypes } from "../utils/errors/itwErrors";
-import { ITW_WIA_KEY_TAG } from "../utils/wia";
 import { ItwCredentialsPidSelector } from "../store/reducers/itwCredentialsReducer";
-import { itwWiaRequest } from "../store/actions/itwWiaActions";
+import { ITW_PID_KEY_TAG } from "../utils/pid";
 
 /*
  * This saga handles the RP presentation.
  * It calls the sendAuthorizationResponse method of the RP solution
- * to send the VP token to the RP
  */
 export function* handleItwRpPresentationSaga(
-  action: ActionType<typeof itwRpPresentation.request>
+  _: ActionType<typeof itwRpPresentation.request>
 ): SagaIterator {
   try {
-    const { authReqUrl } = action.payload;
-
     // TODO: this claims should be selected by user
     const claims = [
       "unique_id",
@@ -41,56 +35,38 @@ export function* handleItwRpPresentationSaga(
       "evidence"
     ];
 
-    const requestObject = yield* select(
+    const requestObjectValue = yield* select(
       itwRpInitializationRequestObjectValueSelector
     );
     const pidToken = yield* select(ItwCredentialsPidSelector);
 
-    // Get WIA
-    yield* put(itwWiaRequest.request());
-    const wia = yield* take(itwWiaRequest.success);
-
-    if (O.isNone(requestObject) || O.isNone(pidToken)) {
+    if (O.isNone(requestObjectValue) || O.isNone(pidToken)) {
       throw new Error("Request object is not defined");
     } else {
-      const RP = new RelyingPartySolution(authReqUrl, wia.payload);
-      const decodedWia = yield* call(
-        WalletInstanceAttestation.decode,
-        wia.payload
-      );
-
-      const walletInstanceId = new URL(
-        "instance/" + decodedWia.payload.sub,
-        decodedWia.payload.iss
-      ).href;
-
-      const { vp_token: unsignedVpToken, presentation_submission } =
-        yield* apply(RP, RP.prepareVpToken, [
-          requestObject.value,
-          walletInstanceId,
-          [pidToken.value.credential, claims],
-          decodedWia.payload.cnf.jwk.kid
-        ]);
-
-      const signature = yield* call(sign, unsignedVpToken, ITW_WIA_KEY_TAG);
-      const vpToken = yield* call(
-        SignJWT.appendSignature,
-        unsignedVpToken,
-        signature
-      );
-
+      // Create PID crypto context
+      const pidCryptoContext = createCryptoContextFor(ITW_PID_KEY_TAG);
+      // Retrieve entity configuration for RP
       const entity = yield* select(itwRpInitializationEntityValueSelector);
 
       if (O.isNone(entity)) {
         throw new Error("Entity is not defined");
       } else {
+        const {
+          requestObject,
+          rpEntityConfiguration,
+          walletInstanceAttestation
+        } = requestObjectValue.value;
+
         // Submit authorization response
-        const result = yield* apply(RP, RP.sendAuthorizationResponse, [
-          requestObject.value,
-          vpToken,
-          presentation_submission,
-          entity.value
-        ]);
+        const result = yield* call(
+          RelyingPartySolution.sendAuthorizationResponse({ pidCryptoContext }),
+          {
+            requestObject,
+            rpEntityConfiguration,
+            walletInstanceAttestation
+          },
+          [pidToken.value.credential, claims]
+        );
 
         yield* put(itwRpPresentation.success(result));
       }
--- ts/features/it-wallet/store/actions/itwCredentialsActions.ts
+++ ts/features/it-wallet/store/actions/itwCredentialsActions.ts
@@ -1,8 +1,8 @@
-import { PidResponse } from "@pagopa/io-react-native-wallet/lib/typescript/pid/issuing";
 import { ActionType, createAsyncAction } from "typesafe-actions";
 import { PidWithToken } from "@pagopa/io-react-native-wallet/lib/typescript/pid/sd-jwt";
 import { PidData } from "@pagopa/io-react-native-cie-pid";
 import * as O from "fp-ts/lib/Option";
+import { PidResponse } from "@pagopa/io-react-native-wallet/lib/typescript/pid/issuing";
 import { ItWalletError } from "../../utils/errors/itwErrors";
 
 /**
