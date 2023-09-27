59ca34aa13dae86cf430e8710fbe18fcc503dac5
LazyAfternoons <lazyafternoons@outlook.it>
2023-09-27 11:11:54 +0200

    refactor: use flatlist for rendering catalog items

--- ts/features/it-wallet/screens/ItwCredentialsCatalogScreen.tsx
+++ ts/features/it-wallet/screens/ItwCredentialsCatalogScreen.tsx
@@ -1,82 +1,83 @@
 import React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
-import { Badge, IOStyles, Icon, VSpacer } from "@pagopa/io-app-design-system";
+import { FlatList, SafeAreaView, View } from "react-native";
+import {
+  Badge,
+  H2,
+  IOStyles,
+  Icon,
+  VSpacer
+} from "@pagopa/io-app-design-system";
 import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
 import { emptyContextualHelp } from "../../../utils/emptyContextualHelp";
 import { ListItemItw } from "../components/ListItemItw";
-import ScreenContent from "../../../components/screens/ScreenContent";
 import ItwSearchBar from "../components/ItwSearchBar";
 import I18n from "../../../i18n";
+import { CREDENTIALS_CATALOG, CredentialCatalogItem } from "../utils/mocks";
 
 /**
  * Renders a preview screen which displays a visual representation and the claims contained in the PID.
  */
 const ItwCredentialsCatalogScreen = () => {
   /**
+   * Renders a single credential catalog item in a FlatList.
+   * @param catalogItem: the catalog item to render.
+   */
+  const RenderItem = ({
+    catalogItem
+  }: {
+    catalogItem: CredentialCatalogItem;
+  }) => (
+    <>
+      <ListItemItw
+        onPress={() => null}
+        accessibilityLabel={catalogItem.title}
+        title={catalogItem.title}
+        icon={catalogItem.icon}
+        rightNode={
+          catalogItem.incoming ? (
+            <Badge
+              text={I18n.t(
+                "features.itWallet.issuing.credentialsCatalogScreen.incomingFeature"
+              )}
+              variant="success"
+            />
+          ) : (
+            <Icon name="chevronRight" />
+          )
+        }
+        disabled={catalogItem.incoming}
+      />
+    </>
+  );
+  /**
    * Renders the content of the screen if the PID is decoded.
    * @param decodedPip - the decoded PID
    */
   const ContentView = () => (
-    <ScrollView contentContainerStyle={IOStyles.horizontalContentPadding}>
+    <View style={IOStyles.flex}>
       <VSpacer />
       <ItwSearchBar />
       <VSpacer />
-      <ListItemItw
-        onPress={() => null}
-        accessibilityLabel={I18n.t(
-          "features.itWallet.verifiableCredentials.type.disabilityCard"
-        )}
-        title={I18n.t(
-          "features.itWallet.verifiableCredentials.type.disabilityCard"
-        )}
-        icon="disabilityCard"
-        rightNode={<Icon name="chevronRight" />}
+      <FlatList
+        data={CREDENTIALS_CATALOG}
+        renderItem={({ item }) => <RenderItem catalogItem={item} />}
+        keyExtractor={(item, index) => `${index}_${item.title}`}
+        ItemSeparatorComponent={() => <VSpacer size={8} />}
       />
-      <VSpacer size={8} />
-      <ListItemItw
-        onPress={() => null}
-        accessibilityLabel={I18n.t(
-          "features.itWallet.verifiableCredentials.type.healthCard"
-        )}
-        title={I18n.t(
-          "features.itWallet.verifiableCredentials.type.healthCard"
-        )}
-        icon="healthCard"
-        rightNode={<Icon name="chevronRight" />}
-      />
-      <VSpacer size={8} />
-      <ListItemItw
-        onPress={() => null}
-        accessibilityLabel={I18n.t(
-          "features.itWallet.verifiableCredentials.type.drivingLicense"
-        )}
-        title={I18n.t(
-          "features.itWallet.verifiableCredentials.type.drivingLicense"
-        )}
-        icon="driverLicense"
-        rightNode={
-          <Badge
-            text={I18n.t(
-              "features.itWallet.issuing.credentialsCatalogScreen.incomingFeature"
-            )}
-            variant="success"
-          />
-        }
-        disabled
-      />
-    </ScrollView>
+    </View>
   );
 
   return (
     <BaseScreenComponent goBack={true} contextualHelp={emptyContextualHelp}>
-      <SafeAreaView style={IOStyles.flex}>
-        <ScreenContent
-          title={I18n.t(
-            "features.itWallet.issuing.credentialsCatalogScreen.title"
-          )}
+      <SafeAreaView style={{ ...IOStyles.flex }}>
+        <View
+          style={{ ...IOStyles.flex, ...IOStyles.horizontalContentPadding }}
         >
+          <H2>
+            {I18n.t("features.itWallet.issuing.credentialsCatalogScreen.title")}
+          </H2>
           <ContentView />
-        </ScreenContent>
+        </View>
       </SafeAreaView>
     </BaseScreenComponent>
   );
