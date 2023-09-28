06884e503510be6a3fcd7e3433a93f5a200ef3c5
LazyAfternoons <lazyafternoons@outlook.it>
2023-09-28 09:47:29 +0200

    feat: add missing feature screen

---
+++ ts/features/it-wallet/screens/ItwMissingFeatureScreen.tsx
@@ -0,0 +1,79 @@
+import {
+  FooterWithButtons,
+  IOStyles,
+  useIOExperimentalDesign
+} from "@pagopa/io-app-design-system";
+import * as React from "react";
+import { BlockButtonProps } from "@pagopa/io-app-design-system/lib/typescript/components/layout/BlockButtons";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+import BaseScreenComponent from "../../../components/screens/BaseScreenComponent";
+import I18n from "../../../i18n";
+import ItwResultScreen from "./ItwResultScreen";
+
+const ItwMissingFeatureScreen = () => {
+  const { isExperimental } = useIOExperimentalDesign();
+  const navigation = useNavigation();
+
+  /**
+   * Content view rendered when the experimental design is enabled.
+   * Includes a centered button to go back.
+   */
+  const ExperimentalContentView = () => (
+    <ItwResultScreen
+      title={I18n.t("features.itWallet.missingFeatureScreen.title")}
+      subtitle={I18n.t("features.itWallet.missingFeatureScreen.subtitle")}
+      pictogram="empty"
+      action={{
+        label: I18n.t("features.itWallet.missingFeatureScreen.button.label"),
+        accessibilityLabel: I18n.t(
+          "features.itWallet.missingFeatureScreen.button.label"
+        ),
+        onPress: () => navigation.goBack()
+      }}
+    />
+  );
+
+  /**
+   * Content view rendered when the experimental design is disabled.
+   * Doesn't include a button to go back.
+   */
+  const LegacyContentView = () => (
+    <ItwResultScreen
+      title={I18n.t("features.itWallet.missingFeatureScreen.title")}
+      subtitle={I18n.t("features.itWallet.missingFeatureScreen.subtitle")}
+      pictogram="empty"
+    />
+  );
+
+  /**
+   * Buttons props for the FooterWithButtons component which goes back to the previous screen.
+   */
+  const ButtonProps: BlockButtonProps = {
+    type: "Solid",
+    buttonProps: {
+      label: I18n.t("features.itWallet.missingFeatureScreen.button.label"),
+      accessibilityLabel: I18n.t(
+        "features.itWallet.missingFeatureScreen.button.label"
+      ),
+      fullWidth: true,
+      onPress: () => navigation.goBack()
+    }
+  };
+
+  return isExperimental ? (
+    <ExperimentalContentView />
+  ) : (
+    <BaseScreenComponent goBack={true} headerTitle="Test">
+      <SafeAreaView edges={["bottom"]} style={IOStyles.flex}>
+        <View style={{ flexGrow: 1 }}>
+          <LegacyContentView />
+          <FooterWithButtons primary={ButtonProps} type="SingleButton" />
+        </View>
+      </SafeAreaView>
+    </BaseScreenComponent>
+  );
+};
+
+export default ItwMissingFeatureScreen;
