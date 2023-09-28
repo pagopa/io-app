92c6529c5648edc9c762f91e9fd46f3436a2bea3
Sabino Picariello <sabino.picariello@pagopa.it>
2023-09-28 16:25:34 +0200

    logout state bug fix

--- ts/features/fastLogin/store/reducers/optInReducer.ts
+++ ts/features/fastLogin/store/reducers/optInReducer.ts
@@ -3,6 +3,10 @@ import { PersistConfig, persistReducer } from "redux-persist";
 import { getType } from "typesafe-actions";
 import { setFastLoginOptIn } from "../actions/optInActions";
 import { Action } from "../../../../store/actions/types";
+import {
+  logoutFailure,
+  logoutSuccess
+} from "../../../../store/actions/authentication";
 
 export type FastLoginOptInState = {
   fastLoginEnabled: boolean | undefined;
@@ -17,6 +21,9 @@ const fastLoginOptInReducer = (
   action: Action
 ): FastLoginOptInState => {
   switch (action.type) {
+    case getType(logoutSuccess):
+    case getType(logoutFailure):
+      return fastLoginOptInInitialState;
     case getType(setFastLoginOptIn):
       return {
         ...state,
