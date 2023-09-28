7b7218a39dc42e1d2d757b11d74091f775d1d92e
LazyAfternoons <lazyafternoons@outlook.it>
2023-09-28 10:33:52 +0200

    fix: key deletion when generating

--- ts/features/it-wallet/utils/keychain.ts
+++ ts/features/it-wallet/utils/keychain.ts
@@ -1,22 +1,13 @@
-import {
-  CryptoError,
-  deleteKey,
-  generate
-} from "@pagopa/io-react-native-crypto";
+import { generate, getPublicKey } from "@pagopa/io-react-native-crypto";
 
 /**
  * Getter for the public key used to sign the WIA attestation.
- * This method tries to generate a new crypto key pair but if it already exists it returns already existing one.
+ * This method tries to generate a new crypto key pair but if it already exists it returns the already existing one.
+ * Note that at the moment the generate function rejects the promise without a CryptoError if the key already exists.
+ * It does rejects the promise with a CryptoError if the key generation fails.
+ * In the future we should change the generate function to reject with a CryptoError if the key already exists, thus allowing us
+ * to better handle the error case and call getPublicKey only if generate fails due to an already existing key.
  * @returns the public key used to sign the WIA attestation.
  */
-export const generateCryptoKey = async (keyTag: string) => {
-  try {
-    await deleteKey(keyTag);
-  } catch (e) {
-    const { message } = e as CryptoError;
-    if (message !== "PUBLIC_KEY_NOT_FOUND") {
-      throw e;
-    }
-  }
-  return await generate(keyTag);
-};
+export const generateCryptoKey = async (keyTag: string) =>
+  generate(keyTag).catch(_ => getPublicKey(keyTag));
