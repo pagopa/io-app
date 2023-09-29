fa923270eda2d8246ca38fbe18151cee5dcc9408
Federico Mastrini <federicomastrini93@gmail.com>
2023-09-29 17:32:10 +0200

    chore: [IOBP-185,IOBP-271] Use `ListItemSwitch` in IdPay components (#5047)
    ## Short description
    This PR removes old solutions to display list item with a switch, a
    badge or a loading spinner in favor of the new `ListItemSwitch`
    component.
    
    
    https://github.com/pagopa/io-app/assets/6160324/820d1233-dac3-4002-8388-6e0030fe38e7
    
    ## List of changes proposed in this pull request
    - Renamed `IDPayInitiativesListComponents.tsx` to
    `IdPayInstrumentInitiativesList.tsx`
    - Refactored activation/deactivation to align to the new component props
    - Removed unused locales 
    
    ## How to test
    Using the `io-dev-server`, open the details screen of a payment method
    and enable/disabled the switches under the "Available initiatives"
    section.
    
    Co-authored-by: Alessandro Izzo <34343582+Hantex9@users.noreply.github.com>

--- ts/features/wallet/component/features/PaymentMethodInitiatives.tsx
+++ ts/features/wallet/component/features/PaymentMethodInitiatives.tsx
@@ -1,7 +1,6 @@
-import * as React from "react";
-
 import { Body, IOStyles, VSpacer } from "@pagopa/io-app-design-system";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import * as React from "react";
 import { View } from "react-native";
 import { NewH6 } from "../../../../components/core/typography/NewH6";
 import I18n from "../../../../i18n";
@@ -10,7 +9,7 @@ import { WalletParamsList } from "../../../../navigation/params/WalletParamsList
 import ROUTES from "../../../../navigation/routes";
 import { useIODispatch, useIOSelector } from "../../../../store/hooks";
 import { PaymentMethod } from "../../../../types/pagopa";
-import { IDPayInitiativesList } from "../../../idpay/wallet/components/IDPayInitiativesListComponents";
+import { IdPayInstrumentInitiativesList } from "../../../idpay/wallet/components/IdPayInstrumentInitiativesList";
 import {
   idPayInitiativesFromInstrumentRefreshStart,
   idPayInitiativesFromInstrumentRefreshStop
@@ -42,6 +41,7 @@ const PaymentMethodInitiatives = (props: Props): React.ReactElement | null => {
       dispatch(idPayInitiativesFromInstrumentRefreshStop());
     };
   }, [idWalletString, dispatch]);
+
   useFocusEffect(refresh);
 
   const initiativesList = useIOSelector(
@@ -66,7 +66,7 @@ const PaymentMethodInitiatives = (props: Props): React.ReactElement | null => {
         </Body>
       </View>
       <VSpacer size={16} />
-      <IDPayInitiativesList
+      <IdPayInstrumentInitiativesList
         idWallet={idWalletString}
         initiatives={initiativesList.slice(0, 3)}
       />
