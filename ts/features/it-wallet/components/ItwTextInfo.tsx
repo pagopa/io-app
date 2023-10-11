e7d1c18870f153457ecb0ed165fcfaa94c00f7ad
Mario Perrotta <mario.perrotta@pagopa.it>
2023-10-11 15:30:05 +0200

    chore: add text info component

---
+++ ts/features/it-wallet/components/ItwTextInfo.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { Text } from "react-native";
+import { Body, LabelLink } from "@pagopa/io-app-design-system";
+import { pipe } from "fp-ts/lib/function";
+import * as R from "fp-ts/lib/ReadonlyArray";
+import * as B from "fp-ts/lib/boolean";
+import { openWebUrl } from "../../../utils/url";
+import { showToast } from "../../../utils/showToast";
+import I18n from "../../../i18n";
+
+interface MarkdownParserProps {
+  markdownText: string;
+}
+
+const renderRegularText = (text: string, index: number) => (
+  <Text key={`text-${index}`}>{text}</Text>
+);
+
+const renderBoldText = (text: string, index: number) => (
+  <Text key={`bold-${index}`} style={{ fontWeight: "bold" }}>
+    {text}
+  </Text>
+);
+
+const renderLink = (text: string, url: string, index: number) => (
+  <LabelLink
+    key={`link-${index}`}
+    onPress={() =>
+      openWebUrl(url, () => showToast(I18n.t("global.jserror.title")))
+    }
+    numberOfLines={1}
+  >
+    {text}
+  </LabelLink>
+);
+
+/**
+ * This component renders a markdown text.
+ * It parses the text and renders bold text, links and headers.
+ * NOTE: the headers are not rendered yet.
+ * @param markdownText - contains the text to be parsed.
+ */
+const ItwTextInfo: React.FC<MarkdownParserProps> = ({ markdownText }) => {
+  const renderElements = () => {
+    // eslint-disable-next-line functional/no-let
+    let elements: ReadonlyArray<React.ReactElement> = [];
+
+    // A regex to match bold text, links and headers
+    // Note: the order of the groups is important
+    const regex =
+      /\*\*(.*?)\*\*|\[(.*?)\]\((.*?)\)|^#\s(.+)|^##\s(.+)|^###\s(.+)/gm;
+
+    // eslint-disable-next-line functional/no-let
+    let match: RegExpExecArray | null;
+
+    // eslint-disable-next-line functional/no-let
+    let lastIndex = 0;
+
+    while ((match = regex.exec(markdownText)) !== null) {
+      // Note: headers not already added to the elements array
+      const [boldText, linkText, url] = match.slice(1);
+
+      elements = pipe(
+        // Add any regular text before the match
+        elements,
+        R.append(
+          renderRegularText(
+            markdownText.slice(lastIndex, match.index),
+            lastIndex
+          )
+        ),
+        // Add bold text if present
+        elements =>
+          pipe(
+            boldText !== undefined,
+            B.fold(
+              () => elements,
+              () =>
+                pipe(
+                  elements,
+                  R.append(
+                    renderBoldText(boldText, match !== null ? match.index : 0)
+                  )
+                )
+            )
+          ),
+        // Add link if present
+        elements =>
+          pipe(
+            linkText !== undefined && url !== undefined,
+            B.fold(
+              () => elements,
+              () =>
+                pipe(
+                  elements,
+                  R.append(
+                    renderLink(linkText, url, match !== null ? match.index : 0)
+                  )
+                )
+            )
+          )
+      );
+
+      lastIndex = match.index + match[0].length;
+    }
+
+    // Add any remaining regular text
+    if (lastIndex < markdownText.length) {
+      elements = pipe(
+        elements,
+        R.append(renderRegularText(markdownText.slice(lastIndex), lastIndex))
+      );
+    }
+
+    return elements;
+  };
+
+  return <Body>{renderElements()}</Body>;
+};
+
+export default ItwTextInfo;
