3d706d914f92857bb1944f6bdbe8af41ac90fb2f
Cristiano Tofani <cri.tofani@gmail.com>
2023-10-11 14:33:59 +0200

    extraction of the header props

--- ts/components/ui/RNavScreenWithLargeHeader.tsx
+++ ts/components/ui/RNavScreenWithLargeHeader.tsx
@@ -1,76 +1,25 @@
 import {
-  ActionProp,
   H3,
   HeaderSecondLevel,
-  IOIcons,
   IOVisualCostants
 } from "@pagopa/io-app-design-system";
 import { useNavigation } from "@react-navigation/native";
-import React, {
-  ComponentProps,
-  useLayoutEffect,
-  useMemo,
-  useState
-} from "react";
+import React, { ComponentProps, useLayoutEffect, useState } from "react";
 import { LayoutChangeEvent, View } from "react-native";
 import Animated, {
   useAnimatedScrollHandler,
   useSharedValue
 } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import {
-  SupportRequestParams,
-  useStartSupportRequest
-} from "../../hooks/useStartSupportRequest";
+import { SupportRequestParams } from "../../hooks/useStartSupportRequest";
 import I18n from "../../i18n";
-
-type HeaderBaseProps = {
-  headerType?: Exclude<
-    ComponentProps<typeof HeaderSecondLevel>["type"],
-    "base" | "singleAction"
-  >;
-  showHelp?: boolean;
-};
-
-interface HeaderNoActionProps extends HeaderBaseProps {
-  showHelp?: false;
-  headerType?: never;
-  secondAction?: never;
-  thirdAction?: never;
-}
-
-interface HeaderHelpActionProps extends HeaderBaseProps {
-  showHelp: true;
-  headerType?: never;
-  secondAction?: never;
-  thirdAction?: never;
-}
-
-interface HeaderTwoActionsProps extends HeaderBaseProps {
-  showHelp: true;
-  headerType: "twoActions";
-  secondAction: ActionProp;
-  thirdAction?: never;
-}
-
-interface HeaderThreeActionsProps extends HeaderBaseProps {
-  showHelp: true;
-  headerType: "threeActions";
-  secondAction: ActionProp;
-  thirdAction: ActionProp;
-}
-
-type HeaderProps =
-  | HeaderNoActionProps
-  | HeaderHelpActionProps
-  | HeaderTwoActionsProps
-  | HeaderThreeActionsProps;
+import { HeaderActionProps, useHeaderProps } from "../../hooks/useHeaderProps";
 
 type Props = {
   children: React.ReactNode;
   title: string;
-} & SupportRequestParams &
-  HeaderProps;
+  headerActionsProp?: HeaderActionProps;
+} & SupportRequestParams;
 
 /**
  * A screen with a large header that can be used as a template for other screens.
@@ -89,21 +38,13 @@ export const RNavScreenWithLargeHeader = ({
   contextualHelp,
   contextualHelpMarkdown,
   faqCategories,
-  showHelp = true,
-  headerType,
-  secondAction,
-  thirdAction
+  headerActionsProp = {}
 }: Props) => {
   const [titleHeight, setTitleHeight] = useState(0);
   const translationY = useSharedValue(0);
 
   const insets = useSafeAreaInsets();
   const navigation = useNavigation();
-  const startSupportRequest = useStartSupportRequest({
-    contextualHelp,
-    contextualHelpMarkdown,
-    faqCategories
-  });
   const getTitleHeight = (event: LayoutChangeEvent) => {
     const { height } = event.nativeEvent.layout;
     setTitleHeight(height);
@@ -114,77 +55,25 @@ export const RNavScreenWithLargeHeader = ({
     translationY.value = event.contentOffset.y;
   });
 
-  const derivedHeaderProps: ComponentProps<typeof HeaderSecondLevel> =
-    useMemo(() => {
-      const baseHeaderProps = {
-        goBack: navigation.goBack,
-        scrollValues: {
-          contentOffsetY: translationY,
-          triggerOffset: titleHeight
-        },
-        title,
-        backAccessibilityLabel: I18n.t("global.buttons.back")
-      };
-
-      const baseHelpAction = {
-        icon: "help" as IOIcons,
-        accessibilityLabel: I18n.t(
-          "global.accessibility.contextualHelp.open.label"
-        ),
-        onPress: startSupportRequest
-      };
-
-      if (!showHelp) {
-        return {
-          ...baseHeaderProps,
-          type: "base"
-        };
-      }
-      if (headerType === "twoActions") {
-        return {
-          ...baseHeaderProps,
-          type: "twoActions",
-          firstAction: baseHelpAction,
-          secondAction
-        };
-      } else if (headerType === "threeActions") {
-        return {
-          ...baseHeaderProps,
-          type: "threeActions",
-          firstAction: baseHelpAction,
-          secondAction,
-          thirdAction
-        };
-      }
-      return {
-        ...baseHeaderProps,
-        type: "singleAction",
-        firstAction: baseHelpAction
-      };
-    }, [
-      headerType,
-      navigation.goBack,
-      secondAction,
-      showHelp,
-      startSupportRequest,
-      thirdAction,
-      title,
-      titleHeight,
-      translationY
-    ]);
+  const headerProps: ComponentProps<typeof HeaderSecondLevel> = useHeaderProps({
+    backAccessibilityLabel: I18n.t("global.buttons.back"),
+    goBack: navigation.goBack,
+    title,
+    scrollValues: {
+      contentOffsetY: translationY,
+      triggerOffset: titleHeight
+    },
+    contextualHelp,
+    contextualHelpMarkdown,
+    faqCategories,
+    ...headerActionsProp
+  });
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      header: () => <HeaderSecondLevel {...derivedHeaderProps} />
+      header: () => <HeaderSecondLevel {...headerProps} />
     });
-  }, [
-    derivedHeaderProps,
-    navigation,
-    startSupportRequest,
-    title,
-    titleHeight,
-    translationY
-  ]);
+  }, [headerProps, navigation]);
 
   return (
     <Animated.ScrollView
