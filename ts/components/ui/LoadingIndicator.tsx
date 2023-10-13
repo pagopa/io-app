9955cc87512e2385c45cf4a825001f20b5ea5452
Damiano Plebani <damiano.plebani@pagopa.it>
2023-10-13 14:31:39 +0200

    Uncomment the a11y related props

--- ts/components/ui/LoadingIndicator.tsx
+++ ts/components/ui/LoadingIndicator.tsx
@@ -3,27 +3,30 @@ import {
   LoadingSpinner,
   useIOExperimentalDesign
 } from "@pagopa/io-app-design-system";
-// import I18n from "i18n-js";
+import I18n from "i18n-js";
+import { WithTestID } from "../../types/WithTestID";
 
-export type LoadingIndicator = Exclude<
-  React.ComponentProps<typeof LoadingSpinner>,
-  "size" | "color" | "duration"
+export type LoadingIndicator = WithTestID<
+  Exclude<
+    React.ComponentProps<typeof LoadingSpinner>,
+    "size" | "color" | "duration"
+  >
 >;
 
-// export const LoadingIndicator = ({
-//   accessibilityHint = I18n.t("global.accessibility.activityIndicator.hint"),
-//   accessibilityLabel = I18n.t("global.accessibility.activityIndicator.label")
-// }: LoadingIndicator) => {
-export const LoadingIndicator = () => {
+export const LoadingIndicator = ({
+  accessibilityHint = I18n.t("global.accessibility.activityIndicator.hint"),
+  accessibilityLabel = I18n.t("global.accessibility.activityIndicator.label"),
+  testID = "LoadingIndicator"
+}: LoadingIndicator) => {
   const { isExperimental } = useIOExperimentalDesign();
 
   return (
     <LoadingSpinner
       size={48}
-      // accessibilityHint={accessibilityHint}
-      // accessibilityLabel={accessibilityLabel}
+      accessibilityHint={accessibilityHint}
+      accessibilityLabel={accessibilityLabel}
       color={isExperimental ? "blueIO-500" : "blue"}
-      testID="LoadingIndicator"
+      testID={testID}
     />
   );
 };
