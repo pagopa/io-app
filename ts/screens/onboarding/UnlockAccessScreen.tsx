cea04c2a8898efff25aa81266b229fecec22a9bf
Alice Di Rico <alice.dirico@outlook.it>
2023-10-17 15:50:40 +0200

    integrate new page to unlock access, miss tests and logic

---
+++ ts/screens/onboarding/UnlockAccessScreen.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import {
+  Body,
+  ButtonLink,
+  ButtonSolid,
+  ContentWrapper,
+  FeatureInfo,
+  GradientScrollView,
+  H3,
+  IOStyles,
+  Label,
+  LabelSmall,
+  Pictogram,
+  VSpacer
+} from "@pagopa/io-app-design-system";
+import { constVoid } from "fp-ts/lib/function";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
+import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
+import I18n from "../../i18n";
+import { useIOBottomSheetAutoresizableModal } from "../../utils/hooks/bottomSheet";
+import { openWebUrl } from "../../utils/url";
+
+const UnlockAccessScreen = () => {
+  const ModalContent = () => (
+    <View>
+      <Body weight="Regular" color="grey-700">
+        {I18n.t("authentication.unlockmodal.description1_1")}
+        {"\n"}
+        {I18n.t("authentication.unlockmodal.description1_2")}{" "}
+        <Label weight="Bold">
+          {I18n.t("authentication.unlockmodal.description1_3")}{" "}
+        </Label>
+        {I18n.t("authentication.unlockmodal.description1_4")}{" "}
+      </Body>
+      <VSpacer size={24} />
+      <H3 weight="SemiBold">{I18n.t("authentication.unlockmodal.title2")}</H3>
+      <VSpacer size={24} />
+      <FeatureInfo
+        iconName="security"
+        body={I18n.t("authentication.unlockmodal.listitem1")}
+      />
+      <VSpacer size={16} />
+      <FeatureInfo
+        iconName="security"
+        body={I18n.t("authentication.unlockmodal.listitem2_1")}
+        actionLabel={I18n.t("authentication.unlockmodal.listitem2_2")}
+        actionOnPress={() => openWebUrl("https://ioapp.it/")}
+      />
+      <VSpacer size={16} />
+      <FeatureInfo
+        iconName="security"
+        body={
+          <LabelSmall weight="Regular" color="grey-700">
+            {I18n.t("authentication.unlockmodal.listitem3_1")}{" "}
+            <Text style={{ fontStyle: "italic" }}>
+              {I18n.t("authentication.unlockmodal.listitem3_2")}{" "}
+            </Text>
+            {I18n.t("authentication.unlockmodal.listitem3_3")}{" "}
+            <Text style={{ fontWeight: "bold" }}>
+              {I18n.t("authentication.unlockmodal.listitem3_4")}{" "}
+            </Text>
+            {I18n.t("authentication.unlockmodal.listitem3_5")}
+          </LabelSmall>
+        }
+      />
+    </View>
+  );
+
+  const {
+    present: presentVeryLongAutoresizableBottomSheetWithFooter,
+    bottomSheet: veryLongAutoResizableBottomSheetWithFooter
+  } = useIOBottomSheetAutoresizableModal(
+    {
+      title: I18n.t("authentication.unlockmodal.title"),
+      component: <ModalContent />,
+      fullScreen: true
+    },
+    120
+  );
+
+  return (
+    <BaseScreenComponent
+      goBack={false}
+      accessibilityEvents={{ avoidNavigationEventsUsage: true }}
+    >
+      <GradientScrollView
+        testID="container-test"
+        primaryAction={
+          <ButtonSolid
+            fullWidth
+            label={I18n.t("authentication.unlock.title")}
+            accessibilityLabel=""
+            onPress={constVoid}
+          />
+        }
+        secondaryAction={
+          <ButtonLink
+            label={I18n.t("authentication.unlock.loginIO")}
+            accessibilityLabel=""
+            onPress={constVoid}
+          />
+        }
+      >
+        <SafeAreaView style={IOStyles.flex}>
+          <VSpacer size={40} />
+          <VSpacer size={40} />
+          <ContentWrapper>
+            <View style={IOStyles.selfCenter}>
+              <Pictogram name="accessDenied" size={120} color="aqua" />
+            </View>
+            <VSpacer size={16} />
+            <View style={IOStyles.alignCenter}>
+              <H3 weight="SemiBold">{I18n.t("authentication.unlock.title")}</H3>
+            </View>
+            <VSpacer size={16} />
+            <View>
+              <Body
+                weight="Regular"
+                style={{ textAlign: "center" }}
+                testID="subtitle-test"
+                color="grey-650"
+              >
+                {I18n.t("authentication.unlock.subtitle")}
+              </Body>
+            </View>
+            <VSpacer size={16} />
+            <View style={IOStyles.selfCenter}>
+              <ButtonLink
+                label={I18n.t("authentication.unlock.learnmore")}
+                onPress={presentVeryLongAutoresizableBottomSheetWithFooter}
+                testID="link-test"
+              />
+            </View>
+          </ContentWrapper>
+          {veryLongAutoResizableBottomSheetWithFooter}
+        </SafeAreaView>
+      </GradientScrollView>
+    </BaseScreenComponent>
+  );
+};
+
+export default UnlockAccessScreen;
