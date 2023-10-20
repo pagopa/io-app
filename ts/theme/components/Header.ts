9bba118540c1f52745bb87c9c0088eff6fc2728e
Damiano Plebani <damiano.plebani@pagopa.it>
2023-10-20 12:49:39 +0200

    chore: [IOAPPFD0-178] Partially remove local properties from `IOStyles` (#5140)

--- ts/theme/components/Header.ts
+++ ts/theme/components/Header.ts
@@ -1,10 +1,9 @@
 import { Platform } from "react-native";
 
-import { IOColors } from "@pagopa/io-app-design-system";
+import { IOColors, IOVisualCostants } from "@pagopa/io-app-design-system";
 import { makeFontStyleObject } from "../fonts";
 import { Theme } from "../types";
 import variables from "../variables";
-import { IOVisualCostants } from "../../components/core/variables/IOStyles";
 
 declare module "native-base" {
   // eslint-disable-next-line @typescript-eslint/no-namespace
