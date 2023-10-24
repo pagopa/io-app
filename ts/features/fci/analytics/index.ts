15ec02728a849ea10bd293f068e5759c6fc668fe
LazyAfternoons <lazyafternoons@outlook.it>
2023-10-24 17:25:16 +0200

    feat: add env to events

--- ts/features/fci/analytics/index.ts
+++ ts/features/fci/analytics/index.ts
@@ -13,99 +13,108 @@ import {
 } from "../store/actions";
 import { getNetworkErrorMessage } from "../../../utils/errors";
 import { SignatureRequestDetailView } from "../../../../definitions/fci/SignatureRequestDetailView";
-
-// This is the list of events that we want to track in mixpanel
-// The list is not exhaustive, it's just a starting point
-// for FCI feature but it can be extended in the future
-// to track other initiative
-enum FciUxEventCategory {
-  UX = "UX",
-  TECH = "TECH",
-  KO = "KO"
-}
-
-enum FciUxEventType {
-  ACTION = "action",
-  CONTROL = "control",
-  EXIT = "exit",
-  MICRO_ACTION = "micro_action",
-  SCREEN_VIEW = "screen_view"
-}
+import { EnvironmentEnum } from "../../../../definitions/fci/Environment";
+import { buildEventProperties } from "./../../../utils/analytics";
 
 export const trackFciDocOpening = (
   expire_date: SignatureRequestDetailView["expires_at"],
-  total_doc_count: number
+  total_doc_count: number,
+  env: EnvironmentEnum
 ) =>
-  void mixpanelTrack("FCI_DOC_OPENING", {
-    expire_date,
-    total_doc_count,
-    event_type: FciUxEventType.ACTION,
-    event_category: FciUxEventCategory.UX
-  });
+  void mixpanelTrack(
+    "NOTIFICATIONS_OPTIN_PREVIEW_STATUS",
+    buildEventProperties("UX", "action", {
+      expire_date,
+      total_doc_count,
+      env
+    })
+  );
 
-export const trackFciUserExit = (screen_name: string, cta_id?: string) =>
-  void mixpanelTrack("FCI_USER_EXIT", {
-    screen_name,
-    cta_id,
-    event_type: FciUxEventType.EXIT,
-    event_category: FciUxEventCategory.UX
-  });
+export const trackFciUserExit = (
+  screen_name: string,
+  env: EnvironmentEnum,
+  cta_id?: string
+) =>
+  void mixpanelTrack(
+    "FCI_USER_EXIT",
+    buildEventProperties("UX", "exit", {
+      screen_name,
+      cta_id,
+      env
+    })
+  );
 
-export const trackFciUxConversion = () =>
-  void mixpanelTrack("FCI_UX_CONVERSION", {
-    event_type: FciUxEventType.ACTION,
-    event_category: FciUxEventCategory.UX
-  });
+export const trackFciUxConversion = (env: EnvironmentEnum) =>
+  void mixpanelTrack(
+    "FCI_UX_CONVERSION",
+    buildEventProperties("UX", "action", {
+      env
+    })
+  );
 
-export const trackFciUserDataConfirmed = () =>
-  void mixpanelTrack("FCI_USER_DATA_CONFIRMED", {
-    event_type: FciUxEventType.ACTION,
-    event_category: FciUxEventCategory.UX
-  });
+export const trackFciUserDataConfirmed = (env: EnvironmentEnum) =>
+  void mixpanelTrack(
+    "FCI_USER_DATA_CONFIRMED",
+    buildEventProperties("UX", "action", {
+      env
+    })
+  );
 
 export const trackFciDocOpeningSuccess = (
   doc_count: number,
   sign_count: number,
-  optional_sign_count: number
+  optional_sign_count: number,
+  env: EnvironmentEnum
 ) =>
-  void mixpanelTrack("FCI_DOC_OPENING_SUCCESS", {
-    doc_count,
-    sign_count,
-    optional_sign_count,
-    event_type: FciUxEventType.CONTROL,
-    event_category: FciUxEventCategory.UX
-  });
+  void mixpanelTrack(
+    "FCI_DOC_OPENING_SUCCESS",
+    buildEventProperties("UX", "control", {
+      doc_count,
+      sign_count,
+      optional_sign_count,
+      env
+    })
+  );
 
-export const trackFciSigningDoc = () =>
-  void mixpanelTrack("FCI_SIGNING_DOC", {
-    event_type: FciUxEventType.ACTION,
-    event_category: FciUxEventCategory.UX
-  });
+export const trackFciSigningDoc = (env: EnvironmentEnum) =>
+  void mixpanelTrack(
+    "FCI_SIGNING_DOC",
+    buildEventProperties("UX", "action", {
+      env
+    })
+  );
 
-export const trackFciShowSignatureFields = () =>
-  void mixpanelTrack("FCI_SHOW_SIGNATURE_FIELDS", {
-    event_type: FciUxEventType.MICRO_ACTION,
-    event_category: FciUxEventCategory.UX
-  });
+export const trackFciShowSignatureFields = (env: EnvironmentEnum) =>
+  void mixpanelTrack(
+    "FCI_SHOW_SIGNATURE_FIELDS",
+    buildEventProperties("UX", "micro_action", {
+      env
+    })
+  );
 
 export const trackFciUxSuccess = (
   doc_signed_count: number,
   signed_count: number,
-  optional_signed_count: number
+  optional_signed_count: number,
+  env: EnvironmentEnum
 ) =>
-  void mixpanelTrack("FCI_UX_SUCCESS", {
-    doc_signed_count,
-    signed_count,
-    optional_signed_count,
-    event_type: FciUxEventType.SCREEN_VIEW,
-    event_category: FciUxEventCategory.UX
-  });
+  void mixpanelTrack(
+    "FCI_UX_SUCCESS",
+    buildEventProperties("UX", "screen_view", {
+      doc_signed_count,
+      signed_count,
+      optional_signed_count,
+      env
+    })
+  );
 
-export const trackFciStartSignature = () =>
-  void mixpanelTrack("FCI_START_SIGNATURE", {
-    event_type: FciUxEventType.ACTION,
-    event_category: FciUxEventCategory.UX
-  });
+export const trackFciStartSignature = (env: EnvironmentEnum) =>
+  void mixpanelTrack(
+    "FCI_START_SIGNATURE",
+    buildEventProperties("UX", "action", {
+      env
+    })
+  );
 
 const trackFciAction =
   (mp: NonNullable<typeof mixpanel>) =>
@@ -124,23 +133,20 @@ const trackFciAction =
       case getType(fciPollFilledDocument.request):
       case getType(fciPollFilledDocument.success):
       case getType(fciPollFilledDocument.cancel):
-        return mp.track(action.type, {
-          event_category: FciUxEventCategory.TECH
-        });
+        return mp.track(action.type, buildEventProperties("TECH", undefined));
       case getType(fciSigningRequest.success):
-        return mp.track(action.type, {
-          event_category: FciUxEventCategory.TECH,
-          event_type: FciUxEventType.CONTROL
-        });
+        return mp.track(action.type, buildEventProperties("TECH", "control"));
       case getType(fciSignatureRequestFromId.failure):
       case getType(fciLoadQtspClauses.failure):
       case getType(fciLoadQtspFilledDocument.failure):
       case getType(fciSigningRequest.failure):
       case getType(fciPollFilledDocument.failure):
-        return mp.track(action.type, {
-          reason: getNetworkErrorMessage(action.payload),
-          event_category: FciUxEventCategory.KO
-        });
+        return mp.track(
+          action.type,
+          buildEventProperties("KO", undefined, {
+            reason: getNetworkErrorMessage(action.payload)
+          })
+        );
     }
     return Promise.resolve();
   };
