cb12168c205f1c251068b20d888116ed993c7920
Mario Perrotta <mario.perrotta@pagopa.it>
2023-10-25 22:53:09 +0200

    chore: add test

---
+++ ts/features/fci/store/reducers/__tests__/fciEnvironment.test.ts
@@ -0,0 +1,37 @@
+import { createStore } from "redux";
+import { applicationChangeState } from "../../../../../store/actions/application";
+import { appReducer } from "../../../../../store/reducers";
+import { fciEnvironmentSet } from "../../actions";
+import { EnvironmentEnum } from "../../../../../../definitions/fci/Environment";
+
+describe("FciEnvironmentSet", () => {
+  it("it should have an initialState equal to prod", () => {
+    const globalState = appReducer(undefined, applicationChangeState("active"));
+    expect(globalState.features.fci.environment).toStrictEqual(
+      EnvironmentEnum.prod
+    );
+  });
+  it("it should be test if the fciEnvironmentSet is dispatched with EnvironmentEnum.test as payload", () => {
+    const globalState = appReducer(undefined, applicationChangeState("active"));
+    const store = createStore(appReducer, globalState as any);
+    store.dispatch(fciEnvironmentSet(EnvironmentEnum.test));
+    expect(store.getState().features.fci.environment).toStrictEqual(
+      EnvironmentEnum.test
+    );
+    expect(store.getState().features.fci.environment).not.toStrictEqual(
+      EnvironmentEnum.prod
+    );
+  });
+  it("it should be prod if the initialState is test and fciEnvironmentSet is dispatched with EnvironmentEnum.prod as payload", () => {
+    const globalState = appReducer(undefined, applicationChangeState("active"));
+    const store = createStore(appReducer, globalState as any);
+    store.dispatch(fciEnvironmentSet(EnvironmentEnum.test));
+    expect(store.getState().features.fci.environment).toStrictEqual(
+      EnvironmentEnum.test
+    );
+    store.dispatch(fciEnvironmentSet(EnvironmentEnum.prod));
+    expect(store.getState().features.fci.environment).toStrictEqual(
+      EnvironmentEnum.prod
+    );
+  });
+});
