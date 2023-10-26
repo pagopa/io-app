91b6fdd3392ca585ac026ae10e0d8256d8357943
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-10-26 17:32:05 +0200

    applied suggestions

--- ts/features/pn/components/MessageF24.tsx
+++ ts/features/pn/components/MessageF24.tsx
@@ -11,45 +11,45 @@ type Props = {
   attachments: ReadonlyArray<UIAttachment>;
 };
 
-export const MessageF24 = ({ attachments }: Props) => {
-  const renderContent = () => {
-    if (attachments.length === 1) {
-      return (
-        <MessageAttachments
-          testID="f24-list-container"
-          attachments={attachments.slice(0, 1)}
-          downloadAttachmentBeforePreview={true}
-          // TODO: navigate to preview
-          // https://pagopa.atlassian.net/browse/IOCOM-457
-          openPreview={constNull}
-        />
-      );
-    }
+const showAllLabel = I18n.t("features.pn.details.f24Section.showAll");
 
+const MessageF24Content = ({ attachments }: Props) => {
+  if (attachments.length === 1) {
     return (
-      <View style={{ alignSelf: "center" }}>
-        <ButtonLink
-          accessibilityLabel={I18n.t("features.pn.details.f24Section.showAll")}
-          label={I18n.t("features.pn.details.f24Section.showAll")}
-          // TODO: open bottom-sheet
-          // https://pagopa.atlassian.net/browse/IOCOM-455
-          onPress={constNull}
-        />
-      </View>
+      <MessageAttachments
+        testID="f24-list-container"
+        attachments={attachments.slice(0, 1)}
+        downloadAttachmentBeforePreview={true}
+        // TODO: navigate to preview
+        // https://pagopa.atlassian.net/browse/IOCOM-457
+        openPreview={constNull}
+      />
     );
-  };
+  }
 
   return (
-    <PnMessageDetailsSection
-      title={I18n.t("features.pn.details.f24Section.title")}
-      testID={"pn-f24-section"}
-    >
-      <VSpacer />
-      <Body color="bluegreyDark">
-        {I18n.t("features.pn.details.f24Section.description")}
-      </Body>
-      <VSpacer />
-      {renderContent()}
-    </PnMessageDetailsSection>
+    <View style={{ alignSelf: "center" }}>
+      <ButtonLink
+        accessibilityLabel={showAllLabel}
+        label={showAllLabel}
+        // TODO: open bottom-sheet
+        // https://pagopa.atlassian.net/browse/IOCOM-455
+        onPress={constNull}
+      />
+    </View>
   );
 };
+
+export const MessageF24 = (props: Props) => (
+  <PnMessageDetailsSection
+    title={I18n.t("features.pn.details.f24Section.title")}
+    testID={"pn-f24-section"}
+  >
+    <VSpacer />
+    <Body color="bluegreyDark">
+      {I18n.t("features.pn.details.f24Section.description")}
+    </Body>
+    <VSpacer />
+    <MessageF24Content {...props} />
+  </PnMessageDetailsSection>
+);
