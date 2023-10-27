e926b60257d1a3eac74d71a5781f0dddb057fc0f
LazyAfternoons <lazyafternoons@outlook.it>
2023-10-27 10:11:35 +0200

    docs: add deprecated methods

--- ts/features/it-wallet/utils/errors/itwErrorsMapping.ts
+++ ts/features/it-wallet/utils/errors/itwErrorsMapping.ts
@@ -9,6 +9,7 @@ import {
 /**
  * Getter for a generic error title and body.
  * @returns a generic ItWalletMappedError.
+ * @deprecated use {@link getItwGenerciMappedError} instead.
  */
 export const getItwGenericError = (): ItWalletMappedError => ({
   title: I18n.t("features.itWallet.generic.error.title"),
@@ -32,6 +33,7 @@ export const getItwGenerciMappedError = (
  * Maps an ItWalletError to an ItWalletMappedError to display an error message to the user.
  * @param error an instance of ItWalletError containg the code to be translated.
  * @returns a title and a body messages to be displayed to the user if the error is known, otherwise a generic error title and body.
+ * @deprecated use {@link ItwErrorMapping} prototype and implemend your own mapping function directly in the component.
  */
 export const mapItwError = (error: ItWalletError): ItWalletMappedError => {
   switch (error.code) {
