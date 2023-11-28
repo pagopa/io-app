44a927cf28e06e4c40aa73f2badefa94e6c79f14
forrest57 <tomba808@gmail.com>
2023-11-28 12:10:53 +0100

    removed double header, also changed navigation animation for screen

--- ts/features/idpay/barcode/navigation/navigator.tsx
+++ ts/features/idpay/barcode/navigation/navigator.tsx
@@ -1,5 +1,10 @@
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  TransitionPresets,
+  createStackNavigator
+} from "@react-navigation/stack";
 import * as React from "react";
+import { Platform } from "react-native";
+import { isGestureEnabled } from "../../../../utils/navigation";
 import { IdPayBarcodeResultScreen } from "../screens/IdPayBarcodeResultScreen";
 import { IdPayBarcodeParamsList } from "./params";
 import { IdPayBarcodeRoutes } from "./routes";
@@ -9,7 +14,16 @@ export const IdPayBarcodeNavigator = () => (
   <Stack.Navigator
     initialRouteName={IdPayBarcodeRoutes.IDPAY_BARCODE_RESULT}
     headerMode={"none"}
-    screenOptions={{ gestureEnabled: true }}
+    mode="modal"
+    screenOptions={
+      Platform.OS === "ios"
+        ? {
+            gestureEnabled: isGestureEnabled,
+            cardOverlayEnabled: true,
+            ...TransitionPresets.ModalPresentationIOS
+          }
+        : {}
+    }
   >
     <Stack.Screen
       name={IdPayBarcodeRoutes.IDPAY_BARCODE_RESULT}
--- ts/navigation/AuthenticatedStackNavigator.tsx
+++ ts/navigation/AuthenticatedStackNavigator.tsx
@@ -255,6 +255,11 @@ const AuthenticatedStackNavigator = () => {
           />
           <Stack.Screen
             name={IdPayBarcodeRoutes.IDPAY_BARCODE_MAIN}
+            options={{
+              ...TransitionPresets.ModalSlideFromBottomIOS,
+              gestureEnabled: isGestureEnabled,
+              ...hideHeaderOptions
+            }}
             component={IdPayBarcodeNavigator}
           />
         </>
