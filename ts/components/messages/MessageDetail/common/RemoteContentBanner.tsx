187172215256fc17006c7f711d76e510cbde453a
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-12-05 11:55:45 +0100

    Add bottom sheet to RemoteContentBanner component

--- ts/components/messages/MessageDetail/common/RemoteContentBanner.tsx
+++ ts/components/messages/MessageDetail/common/RemoteContentBanner.tsx
@@ -1,26 +1,39 @@
 import React from "react";
-import { constNull } from "fp-ts/lib/function";
+import { Body } from "@pagopa/io-app-design-system";
 import I18n from "../../../../i18n";
 import StatusContent from "../../../SectionStatus/StatusContent";
 import { Link } from "../../../core/typography/Link";
 import { Label } from "../../../core/typography/Label";
+import { useIOBottomSheetAutoresizableModal } from "../../../../utils/hooks/bottomSheet";
 
-export const RemoteContentBanner = () => (
-  <StatusContent
-    backgroundColor={"white"}
-    foregroundColor={"bluegrey"}
-    iconName={"notice"}
-    labelPaddingVertical={6}
-  >
-    {`${I18n.t("messageDetails.banner.content1")} `}
-    <Label weight="Bold" color="bluegrey">
-      {`${I18n.t("messageDetails.banner.content2")} `}
-    </Label>
-    {I18n.t("messageDetails.banner.content3")}
-    {"\n"}
-    {/* TODO IOCOM-698 */}
-    <Link onPress={constNull} weight={"SemiBold"}>
-      {I18n.t("messageDetails.banner.action")}
-    </Link>
-  </StatusContent>
-);
+export const RemoteContentBanner = () => {
+  const { present, bottomSheet } = useIOBottomSheetAutoresizableModal(
+    {
+      component: <Body>{I18n.t("messageDetails.bottomSheet.body")}</Body>,
+      title: I18n.t("messageDetails.bottomSheet.title")
+    },
+    100
+  );
+
+  return (
+    <>
+      <StatusContent
+        backgroundColor={"white"}
+        foregroundColor={"bluegrey"}
+        iconName={"notice"}
+        labelPaddingVertical={6}
+      >
+        {`${I18n.t("messageDetails.banner.content1")} `}
+        <Label weight="Bold" color="bluegrey">
+          {`${I18n.t("messageDetails.banner.content2")} `}
+        </Label>
+        {I18n.t("messageDetails.banner.content3")}
+        {"\n"}
+        <Link onPress={present} weight={"SemiBold"}>
+          {I18n.t("messageDetails.banner.action")}
+        </Link>
+      </StatusContent>
+      {bottomSheet}
+    </>
+  );
+};
