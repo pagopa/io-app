331dd43a45d0e8ce89b18336e49e912bcbe3e85f
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-12-06 12:35:01 +0100

    Add trackRemoteContentInfo event

--- ts/components/messages/MessageDetail/common/RemoteContentBanner.tsx
+++ ts/components/messages/MessageDetail/common/RemoteContentBanner.tsx
@@ -5,6 +5,7 @@ import StatusContent from "../../../SectionStatus/StatusContent";
 import { Link } from "../../../core/typography/Link";
 import { Label } from "../../../core/typography/Label";
 import { useIOBottomSheetAutoresizableModal } from "../../../../utils/hooks/bottomSheet";
+import { trackRemoteContentInfo } from "../../../../features/messages/analytics";
 
 export const RemoteContentBanner = () => {
   const { present, bottomSheet } = useIOBottomSheetAutoresizableModal(
@@ -29,7 +30,13 @@ export const RemoteContentBanner = () => {
         </Label>
         {I18n.t("messageDetails.banner.content3")}
         {"\n"}
-        <Link onPress={present} weight={"SemiBold"}>
+        <Link
+          onPress={() => {
+            trackRemoteContentInfo();
+            present();
+          }}
+          weight={"SemiBold"}
+        >
           {I18n.t("messageDetails.banner.action")}
         </Link>
       </StatusContent>
