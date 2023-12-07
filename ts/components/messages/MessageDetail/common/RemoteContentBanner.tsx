6f002dc61cc10dd3dc0ee3ed63599a8a19a06992
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-12-07 15:02:52 +0100

    feat: [IOCOM-698] Add bottom sheet to `RemoteContentBanner` component (#5298)
    This PR depends on https://github.com/pagopa/io-app/pull/5294
    
    ## Short description
    This PR adds bottom sheets to `RemoteContentBanner` component.
    
    ## List of changes proposed in this pull request
    - updated the component `RemoteContentBanner` to display the bottom
    sheet
    - updated locales
    
    ## How to test
    Using `io-dev-api-server` generate a message with remote content. Open
    the message detail, tap on the link in the banner and check that the
    bottom sheet is displayed.

--- ts/components/messages/MessageDetail/common/RemoteContentBanner.tsx
+++ ts/components/messages/MessageDetail/common/RemoteContentBanner.tsx
@@ -1,26 +1,38 @@
 import React from "react";
-import { LabelLink, LabelSmall } from "@pagopa/io-app-design-system";
-import { constNull } from "fp-ts/lib/function";
+import { Body, LabelLink, LabelSmall } from "@pagopa/io-app-design-system";
 import I18n from "../../../../i18n";
 import StatusContent from "../../../SectionStatus/StatusContent";
+import { useIOBottomSheetAutoresizableModal } from "../../../../utils/hooks/bottomSheet";
 
-export const RemoteContentBanner = () => (
-  <StatusContent
-    backgroundColor={"white"}
-    fontSize="small"
-    foregroundColor={"bluegrey"}
-    iconName={"notice"}
-    labelPaddingVertical={6}
-  >
-    {`${I18n.t("messageDetails.banner.content1")} `}
-    <LabelSmall weight="Bold" color="bluegrey">
-      {`${I18n.t("messageDetails.banner.content2")} `}
-    </LabelSmall>
-    {I18n.t("messageDetails.banner.content3")}
-    {"\n"}
-    {/* TODO IOCOM-698 */}
-    <LabelLink fontSize="small" onPress={constNull}>
-      {I18n.t("messageDetails.banner.action")}
-    </LabelLink>
-  </StatusContent>
-);
+export const RemoteContentBanner = () => {
+  const { present, bottomSheet } = useIOBottomSheetAutoresizableModal(
+    {
+      component: <Body>{I18n.t("messageDetails.bottomSheet.body")}</Body>,
+      title: I18n.t("messageDetails.bottomSheet.title")
+    },
+    100
+  );
+
+  return (
+    <>
+      <StatusContent
+        backgroundColor={"white"}
+        fontSize="small"
+        foregroundColor={"bluegrey"}
+        iconName={"notice"}
+        labelPaddingVertical={6}
+      >
+        {`${I18n.t("messageDetails.banner.content1")} `}
+        <LabelSmall weight="Bold" color="bluegrey">
+          {`${I18n.t("messageDetails.banner.content2")} `}
+        </LabelSmall>
+        {I18n.t("messageDetails.banner.content3")}
+        {"\n"}
+        <LabelLink fontSize="small" onPress={present}>
+          {I18n.t("messageDetails.banner.action")}
+        </LabelLink>
+      </StatusContent>
+      {bottomSheet}
+    </>
+  );
+};
