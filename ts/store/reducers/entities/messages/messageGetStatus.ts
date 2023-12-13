9c7706240e58e9c01936305d35673caa85963339
Andrea Piai <andrea.piai@pagopa.it>
2023-12-13 14:22:38 +0100

    WiP fix for stuck message router screen in case of a fast login expiration

--- ts/store/reducers/entities/messages/messageGetStatus.ts
+++ ts/store/reducers/entities/messages/messageGetStatus.ts
@@ -7,13 +7,16 @@ import {
 import { Action } from "../../../actions/types";
 import { GlobalState } from "../../types";
 import { reloadAllMessages } from "../../../actions/messages";
+import { startApplicationInitialization } from "../../../actions/application";
+import { UIMessageId } from "./types";
 
 export type MessageGetStatusType =
   | "idle"
   | "loading"
   | "blocked"
   | "error"
-  | "success";
+  | "success"
+  | "retry";
 export type MessageGetStatusFailurePhaseType =
   | "none"
   | "paginatedMessage"
@@ -22,16 +25,44 @@ export type MessageGetStatusFailurePhaseType =
   | "thirdPartyMessageDetails"
   | "readStatusUpdate";
 
-export type MessageGetStatus = {
-  failurePhase: MessageGetStatusFailurePhaseType;
-  status: MessageGetStatusType;
-  successData: SuccessGetMessageDataActionType | undefined;
+type IdleState = {
+  status: "idle";
 };
 
+type BlockedOrErrorState = {
+  status: "blocked" | "error";
+  failurePhase: MessageGetStatusFailurePhaseType;
+};
+
+type LoadingOrRetryState = {
+  status: "loading" | "retry";
+  data: {
+    messageId: UIMessageId;
+    fromPushNotification: boolean;
+  };
+};
+
+type SuccessState = {
+  status: "success";
+  successData: SuccessGetMessageDataActionType;
+};
+
+export type MessageGetStatus =
+  | IdleState
+  | BlockedOrErrorState
+  | LoadingOrRetryState
+  | SuccessState;
+
+const isRetryType = (
+  messageGetStatus: MessageGetStatus
+): messageGetStatus is LoadingOrRetryState =>
+  messageGetStatus.status === "retry";
+const isSuccessType = (
+  messageGetStatus: MessageGetStatus
+): messageGetStatus is SuccessState => messageGetStatus.status === "success";
+
 export const INITIAL_STATE: MessageGetStatus = {
-  failurePhase: "none",
-  status: "idle",
-  successData: undefined
+  status: "idle"
 };
 
 export const messageGetStatusReducer = (
@@ -42,21 +73,31 @@ export const messageGetStatusReducer = (
     case getType(getMessageDataAction.request):
       return {
         status: "loading",
-        failurePhase: "none",
-        successData: undefined
+        data: {
+          messageId: action.payload.messageId,
+          fromPushNotification: action.payload.fromPushNotification
+        }
       };
+    case getType(startApplicationInitialization):
+      const fastLoginSessionExpired =
+        !!(action.payload && action.payload.handleSessionExpiration) &&
+        isRetryType(state);
+      return fastLoginSessionExpired
+        ? {
+            ...state,
+            status: "retry"
+          }
+        : state;
     case getType(getMessageDataAction.failure):
       return {
         status: action.payload.blockedFromPushNotificationOpt
           ? "blocked"
           : "error",
-        failurePhase: action.payload.phase,
-        successData: undefined
+        failurePhase: action.payload.phase
       };
     case getType(getMessageDataAction.success):
       return {
         status: "success",
-        failurePhase: "none",
         successData: action.payload
       };
     case getType(resetGetMessageDataAction):
@@ -75,7 +116,17 @@ export const thirdPartyMessageDetailsErrorSelector = (state: GlobalState) =>
     "thirdPartyMessageDetails";
 
 export const messageSuccessDataSelector = (state: GlobalState) =>
-  state.entities.messages.messageGetStatus.successData;
+  isSuccessType(state.entities.messages.messageGetStatus)
+    ? state.entities.messages.messageGetStatus.successData
+    : undefined;
 
 export const blockedFromPushNotificationSelector = (state: GlobalState) =>
   state.entities.messages.messageGetStatus.status === "blocked";
+
+export const retryDataAfterFastLoginSessionExpirationSelector = (
+  state: GlobalState
+) =>
+  isRetryType(state.entities.messages.messageGetStatus) &&
+  state.entities.messages.messageGetStatus.status === "retry"
+    ? state.entities.messages.messageGetStatus.data
+    : undefined;
