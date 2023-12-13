7fa0442b17200643e7375880bde7b14f32e28054
Alice Di Rico <83651704+Ladirico@users.noreply.github.com>
2023-12-13 10:22:11 +0100

    fix tests

--- ts/screens/authentication/__tests__/NewOptInScreen.test.tsx
+++ ts/screens/authentication/__tests__/NewOptInScreen.test.tsx
@@ -17,9 +17,9 @@ describe("NewOptInScreen", () => {
     const declineButton = component.getByTestId("decline-button-test");
     expect(declineButton).toBeDefined();
     expect(component.getByTestId("badge-test")).toBeDefined();
-    const suggestButton = component.getAllByText(
+    const suggestButton = component.getByText(
       I18n.t("authentication.opt_in.security_suggests")
-    )[1];
+    );
     expect(suggestButton).toBeDefined();
 
     if (acceptButton) {
