7fbd71f56d1ad44f29871caf91756789604b4370
Mario Perrotta <mario.perrotta@pagopa.it>
2024-01-04 16:39:47 +0100

    chore: no signature fields hook

--- ts/features/fci/hooks/useFciNoSignatureFields.tsx
+++ ts/features/fci/hooks/useFciNoSignatureFields.tsx
@@ -1,29 +1,21 @@
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
 import { StackActions, useNavigation } from "@react-navigation/native";
 import { increment } from "fp-ts/lib/function";
-import { useLegacyIOBottomSheetModal } from "../../../utils/hooks/bottomSheet";
-import { IOStyles } from "../../../components/core/variables/IOStyles";
-import { H3 } from "../../../components/core/typography/H3";
-import FooterWithButtons from "../../../components/ui/FooterWithButtons";
+import {
+  Body,
+  ButtonSolidProps,
+  FooterWithButtons
+} from "@pagopa/io-app-design-system";
 import I18n from "../../../i18n";
-import customVariables from "../../../theme/variables";
-import { confirmButtonProps } from "../../bonus/bonusVacanze/components/buttons/ButtonConfigurations";
-import { H4 } from "../../../components/core/typography/H4";
 import { FCI_ROUTES } from "../navigation/routes";
 import { fciSignatureDetailDocumentsSelector } from "../store/reducers/fciSignatureRequest";
 import { useIOSelector } from "../../../store/hooks";
 import { trackFciStartSignature } from "../analytics";
+import { useIOBottomSheetModal } from "../../../utils/hooks/bottomSheet";
 
 type Props = {
   currentDoc: number;
 };
-
-const styles = StyleSheet.create({
-  verticalPad: {
-    paddingVertical: customVariables.spacerHeight
-  }
-});
 
 /**
  * A hook that returns a function to present the abort signature flow bottom sheet
@@ -32,47 +24,53 @@ export const useFciNoSignatureFields = (props: Props) => {
   const navigation = useNavigation();
   const documents = useIOSelector(fciSignatureDetailDocumentsSelector);
   const { currentDoc } = props;
-  const { present, bottomSheet, dismiss } = useLegacyIOBottomSheetModal(
-    <View style={styles.verticalPad}>
-      <H4 weight={"Regular"}>{I18n.t("features.fci.noFields.content")}</H4>
-    </View>,
-    <View style={IOStyles.flex}>
-      <H3 color={"bluegreyDark"} weight={"SemiBold"}>
-        {I18n.t("features.fci.noFields.title")}
-      </H3>
-    </View>,
-    280,
-    <FooterWithButtons
-      type={"TwoButtonsInlineThird"}
-      leftButton={{
-        onPressWithGestureHandler: true,
-        bordered: true,
-        onPress: () => {
-          dismiss();
-        },
-        title: I18n.t("features.fci.noFields.leftButton")
-      }}
-      rightButton={{
-        ...confirmButtonProps(() => {
-          dismiss();
-          if (currentDoc < documents.length - 1) {
-            navigation.dispatch(
-              StackActions.push(FCI_ROUTES.DOCUMENTS, {
-                attrs: undefined,
-                currentDoc: increment(currentDoc)
-              })
-            );
-          } else {
-            trackFciStartSignature();
-            navigation.navigate(FCI_ROUTES.MAIN, {
-              screen: FCI_ROUTES.USER_DATA_SHARE
-            });
-          }
-        }, I18n.t("features.fci.noFields.rightButton")),
-        onPressWithGestureHandler: true
-      }}
-    />
-  );
+  const readButtonProps: ButtonSolidProps = {
+    onPress: () => {
+      dismiss();
+    },
+    label: I18n.t("features.fci.noFields.leftButton"),
+    accessibilityLabel: I18n.t("features.fci.noFields.leftButton")
+  };
+  const confirmButtonProps: ButtonSolidProps = {
+    onPress: () => {
+      dismiss();
+      if (currentDoc < documents.length - 1) {
+        navigation.dispatch(
+          StackActions.push(FCI_ROUTES.DOCUMENTS, {
+            attrs: undefined,
+            currentDoc: increment(currentDoc)
+          })
+        );
+      } else {
+        trackFciStartSignature();
+        navigation.navigate(FCI_ROUTES.MAIN, {
+          screen: FCI_ROUTES.USER_DATA_SHARE
+        });
+      }
+    },
+    label: I18n.t("features.fci.noFields.rightButton"),
+    accessibilityLabel: I18n.t("features.fci.noFields.rightButton")
+  };
+  const { present, bottomSheet, dismiss } = useIOBottomSheetModal({
+    component: (
+      <Body weight={"Regular"}>{I18n.t("features.fci.noFields.content")}</Body>
+    ),
+    title: I18n.t("features.fci.noFields.title"),
+    snapPoint: [280],
+    footer: (
+      <FooterWithButtons
+        type={"TwoButtonsInlineThird"}
+        primary={{
+          type: "Outline",
+          buttonProps: readButtonProps
+        }}
+        secondary={{
+          type: "Solid",
+          buttonProps: confirmButtonProps
+        }}
+      />
+    )
+  });
 
   return {
     dismiss,
