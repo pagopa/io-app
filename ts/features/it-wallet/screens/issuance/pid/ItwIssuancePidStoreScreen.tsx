8d590881d84774c6b62f6368468a8c223404aa3d
LazyAfternoons <lazyafternoons@outlook.it>
2024-01-10 15:48:33 +0100

    chore: replace continue with ko view in pid store screen

--- ts/features/it-wallet/screens/issuance/pid/ItwIssuancePidStoreScreen.tsx
+++ ts/features/it-wallet/screens/issuance/pid/ItwIssuancePidStoreScreen.tsx
@@ -3,12 +3,12 @@ import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { IOStyles } from "@pagopa/io-app-design-system";
 import I18n from "../../../../../i18n";
-import ItwContinueView from "../../../components/ItwContinueView";
 import {
   AppParamsList,
   IOStackNavigationProp
 } from "../../../../../navigation/params/AppParamsList";
 import ROUTES from "../../../../../navigation/routes";
+import ItwKoView from "../../../components/ItwKoView";
 
 /**
  * Renders an activation screen which displays a loading screen while the PID is being added and a success screen when the PID is added.
@@ -18,7 +18,7 @@ const ItwIssuancePidStoreScreen = () => {
 
   return (
     <SafeAreaView style={IOStyles.flex}>
-      <ItwContinueView
+      <ItwKoView
         title={I18n.t(
           "features.itWallet.issuing.pidActivationScreen.typ.title"
         )}
