32742d345efe4bcc1773d5698635900dae69c6d1
LazyAfternoons <lazyafternoons@outlook.it>
2024-01-22 21:43:27 +0100

    chore: remove legacy scopes

--- scripts/ts/danger/utils/changelog.ts
+++ scripts/ts/danger/utils/changelog.ts
@@ -22,34 +22,17 @@ const storyOrder = new Map<GenericTicketType, number>([
 const allowedScope = new Map<string, string>([
   ["android", "Android"],
   ["ios", "iOS"],
-  ["bonus_vacanze", "Bonus Vacanze"],
   ["messages", "Messages"],
   ["payments", "Payments"],
   ["services", "Services"],
   ["profile", "Profile"],
   ["privacy", "Privacy"],
   ["security", "Security"],
-  ["accessibility", "Accessibility"],
-  ["bpd", "Bonus Pagamenti Digitali"],
-  ["cgn", "Carta Giovani Nazionale"],
-  ["fims", "Federated Identity Management System"],
-  ["myportal", "MyPortal"]
+  ["accessibility", "Accessibility"]
 ]);
 
 // a list of project ids associated with a specific scope
 const projectToScope = new Map<string, string>([
-  ["2449547", "Bonus Vacanze"],
-  ["2463683", "My Portal"],
-  ["2477137", "Bonus Pagamenti Digitali"],
-  ["IAC", "Bonus Pagamenti Digitali"],
-  ["IOACGN", "Carta Giovani Nazionale"],
-  ["IASV", "Sicilia Vola"],
-  ["IAGP", "EU Covid Certificate"],
-  ["IARS", "Redesign Servizi"],
-  ["ASZ", "Zendesk"],
-  ["IAMVL", "Piattaforma Notifiche"],
-  ["IAFIMS", "Federated Identity Management System"],
-  ["AP", "Carta della cultura"],
   ["SFEQS", "Firma con IO"],
   ["IODPAY", "IDPay"]
 ]);
