761919d57d8bd9c143981852e45623636c024b6f
Federico Mastrini <federicomastrini93@gmail.com>
2024-01-24 17:00:09 +0100

    fix: permissions

--- ts/utils/permission.ts
+++ ts/utils/permission.ts
@@ -11,18 +11,12 @@ export const requestIOPermission = async (
   permission: RNPermissions.Permission,
   rationale?: RNPermissions.Rationale
 ): Promise<boolean> => {
-  // Be aware that some permissions may return "unavailable" event if the library
-  // documents them as supported. One notorious case is the iOS PHOTO_LIBRARY_ADD_ONLY
-  // permission. If such permission is automatically handled by the system upon request
-  // (such as PHOTO_LIBRARY_ADD_ONLY is), then you should not use this function to
-  // check nor to request such permission
-  const checkResult = await RNPermissions.check(permission);
-  if (checkResult === "granted") {
+  if (await checkIOPermission(permission)) {
     return true;
   }
 
   const requestStatus = await RNPermissions.request(permission, rationale);
-  return requestStatus === "granted";
+  return requestStatus === "granted" || requestStatus === "limited";
 };
 
 /**
@@ -33,8 +27,13 @@ export const requestIOPermission = async (
 export const checkIOPermission = async (
   permission: RNPermissions.Permission
 ): Promise<boolean> => {
+  // Be aware that some permissions may return "unavailable" event if the library
+  // documents them as supported. One notorious case is the iOS PHOTO_LIBRARY_ADD_ONLY
+  // permission. If such permission is automatically handled by the system upon request
+  // (such as PHOTO_LIBRARY_ADD_ONLY is), then you should not use this function to
+  // check nor to request such permission
   const checkResult = await RNPermissions.check(permission);
-  return checkResult === "granted";
+  return checkResult === "granted" || checkResult === "limited";
 };
 
 /**
