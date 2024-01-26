7e9d2cda89a0728fe284636c4dcbf971c89c4f65
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2024-01-26 14:40:46 +0100

    update snaps

--- ts/features/pn/components/__test__/Timeline.test.tsx
+++ ts/features/pn/components/__test__/Timeline.test.tsx
@@ -2,40 +2,47 @@ import React from "react";
 import { render } from "@testing-library/react-native";
 import { Timeline, TimelineProps } from "../Timeline";
 
-const data: TimelineProps["data"] = [
-  {
-    day: "25",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    month: "03",
-    time: "10:00",
-    status: "viewed"
-  },
-  {
-    day: "25",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    month: "03",
-    time: "10:00",
-    status: "default"
-  },
-  {
-    day: "25",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    month: "03",
-    time: "10:00",
-    status: "default"
-  },
-  {
-    day: "25",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    month: "03",
-    time: "10:00",
-    status: "default"
-  }
-];
+const defaultProps: TimelineProps = {
+  data: [
+    {
+      day: "25",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      month: "03",
+      time: "10:00",
+      status: "viewed"
+    },
+    {
+      day: "25",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      month: "03",
+      time: "10:00",
+      status: "default"
+    },
+    {
+      day: "25",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      month: "03",
+      time: "10:00",
+      status: "default"
+    },
+    {
+      day: "25",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      month: "03",
+      time: "10:00",
+      status: "default"
+    }
+  ]
+};
 
 describe("Timeline component", () => {
-  it("should match the snapshot", () => {
-    const component = render(<Timeline data={data} />);
+  it("should match the snapshot with empty data", () => {
+    const component = render(<Timeline data={[]} />);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("should match the snapshot with default props", () => {
+    const component = render(<Timeline {...defaultProps} />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
