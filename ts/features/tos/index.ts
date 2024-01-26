8f68e2c11aa67e82e06ea50080df5a59622891d3
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2024-01-26 16:04:38 +0100

    add utility and selector functions

---
+++ ts/features/tos/index.ts
@@ -0,0 +1,5 @@
+import { NonNegativeNumber } from "@pagopa/ts-commons/lib/numbers";
+import { TosConfig } from "../../../definitions/content/TosConfig";
+
+export const getTosVersion = (tosData: TosConfig): NonNegativeNumber =>
+  tosData.tos_version as NonNegativeNumber;
