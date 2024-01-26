868be80cc8b5ddf4e9351a4741820e2530a388f0
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2024-01-26 17:05:00 +0100

    add snap

--- ts/features/messages/components/MessageDetail/__tests__/MessageDetailHeader.test.tsx
+++ ts/features/messages/components/MessageDetail/__tests__/MessageDetailHeader.test.tsx
@@ -1,6 +1,15 @@
 import React, { ComponentProps } from "react";
 import { render } from "@testing-library/react-native";
 import { MessageDetailHeader } from "../MessageDetailHeader";
+import { ServicePublic } from "../../../../../../definitions/backend/ServicePublic";
+
+const service = {
+  service_id: "serviceId",
+  service_name: "health",
+  organization_name: "Organization foo",
+  department_name: "Department one",
+  organization_fiscal_code: "OFSAAAAAA"
+} as ServicePublic;
 
 const defaultProps: ComponentProps<typeof MessageDetailHeader> = {
   subject: "Subject",
@@ -10,6 +19,17 @@ const defaultProps: ComponentProps<typeof MessageDetailHeader> = {
 describe("MessageDetailHeader component", () => {
   it("should match the snapshot with default props", () => {
     const component = render(<MessageDetailHeader {...defaultProps} />);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it("should match the snapshot with all props", () => {
+    const component = render(
+      <MessageDetailHeader
+        {...defaultProps}
+        sender="Sender"
+        service={service}
+      />
+    );
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
