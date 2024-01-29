5f5f644b32faa6737af480aa119fa94f22f760b8
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2024-01-29 15:56:36 +0100

    feat: [IOCOM-868] Update SEND message header and content to the new design system (#5431)
    ## Short description
    This PR updates the header and content of a SEND message.
    
    _NOTE: Other PRs will follow to update the rest of the message._
    
    <details open><summary>Details</summary>
    <p>
    
    | message details |
    | - |
    | <img
    src="https://github.com/pagopa/io-app/assets/29163287/728a25f9-a597-4543-b3d2-02ef46695c05"
    width="300" /> |
    
    </p>
    </details> 
    
    ## List of changes proposed in this pull request
    - added `MessageDetailHeader`
    - updated `MessageDetailsContent`
    - added tests
    
    ## How to test
    Using the `io-dev-api-server`, generate a SEND message. Enable the
    design system and navigate to the message detail. Check that the content
    is displayed correctly.
    
    ---------
    
    Co-authored-by: Andrea <andrea.piai@pagopa.it>

--- ts/features/pn/components/__test__/MessageDetails.test.tsx
+++ ts/features/pn/components/__test__/MessageDetails.test.tsx
@@ -1,171 +1,38 @@
 import React from "react";
-import * as pot from "@pagopa/ts-commons/lib/pot";
-import { act, fireEvent } from "@testing-library/react-native";
-import { createStore } from "redux";
+import configureMockStore from "redux-mock-store";
+import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/lib/Option";
 import { applicationChangeState } from "../../../../store/actions/application";
 import { appReducer } from "../../../../store/reducers";
 import { MessageDetails } from "../MessageDetails";
 import { GlobalState } from "../../../../store/reducers/types";
 import { renderScreenWithNavigationStoreContext } from "../../../../utils/testWrapper";
-import PN_ROUTES from "../../navigation/routes";
-import { UIAttachment, UIMessageId } from "../../../messages/types";
 import { PNMessage } from "../../store/types/types";
-import { Download } from "../../../messages/store/reducers/downloads";
-import { NotificationRecipient } from "../../../../../definitions/pn/NotificationRecipient";
-import { ATTACHMENT_CATEGORY } from "../../../messages/types/attachmentCategory";
+import { thirdPartyMessage } from "../../__mocks__/message";
+import { toPNMessage } from "../../store/types/transformers";
+import { UIMessageId } from "../../../messages/types";
 
-const mockedOnAttachmentSelect = jest.fn();
-
-jest.mock("../../../messages/hooks/useAttachmentDownload", () => ({
-  useAttachmentDownload: (
-    _attachment: UIAttachment,
-    _openPreview: (attachment: UIAttachment) => void
-  ) => ({
-    onAttachmentSelect: mockedOnAttachmentSelect,
-    downloadPot: { kind: "PotNone" } as pot.Pot<Download, Error>
-  })
-}));
+const pnMessage = pipe(thirdPartyMessage, toPNMessage, O.toUndefined);
 
 describe("MessageDetails component", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("should not show the cancelled banner when the PN message is not cancelled", () => {
+  it("should match the snapshot", () => {
     const { component } = renderComponent(
-      generateComponentProperties(generatePnMessage())
+      generateComponentProperties(
+        thirdPartyMessage.id as UIMessageId,
+        pnMessage!
+      )
     );
-    expect(component.queryByTestId("PnCancelledMessageBanner")).toBeFalsy();
-  });
-
-  it("every attachment item should have a full opaque style when the PN message is not cancelled", () => {
-    const { component } = renderComponent(
-      generateComponentProperties(generatePnMessage())
-    );
-    const messageAttachmentComponents = component.queryAllByTestId(
-      "MessageAttachmentTouchable"
-    );
-    expect(messageAttachmentComponents.length).toBe(2);
-
-    messageAttachmentComponents.forEach(messageAttachmentComponent => {
-      const opacity = messageAttachmentComponent?.props.style.opacity;
-      expect(opacity).toBe(1.0);
-    });
-  });
-
-  it("should show the cancelled banner when the PN message is cancelled", () => {
-    const { component } = renderComponent(
-      generateComponentProperties({
-        ...generatePnMessage(),
-        isCancelled: true
-      })
-    );
-    expect(component.queryByTestId("PnCancelledMessageBanner")).toBeDefined();
-  });
-
-  it("every attachment item should have a semi-transparent style when the PN message is cancelled", () => {
-    const { component } = renderComponent(
-      generateComponentProperties({
-        ...generatePnMessage(),
-        isCancelled: true
-      })
-    );
-    const messageAttachmentComponents = component.queryAllByTestId(
-      "MessageAttachmentTouchable"
-    );
-    expect(messageAttachmentComponents.length).toBe(2);
-
-    messageAttachmentComponents.forEach(messageAttachmentComponent => {
-      const opacity = messageAttachmentComponent?.props.style.opacity;
-      expect(opacity).toBe(0.5);
-    });
-  });
-
-  it("every attachment item should handle input when the PN message not is cancelled", async () => {
-    const { component } = renderComponent(
-      generateComponentProperties(generatePnMessage())
-    );
-    const messageAttachmentComponents = component.queryAllByTestId(
-      "MessageAttachmentTouchable"
-    );
-    expect(messageAttachmentComponents.length).toBe(2);
-    await act(() => {
-      messageAttachmentComponents.forEach(messageAttachmentComponent =>
-        fireEvent.press(messageAttachmentComponent)
-      );
-    });
-    expect(mockedOnAttachmentSelect).toHaveBeenCalledTimes(
-      messageAttachmentComponents.length
-    );
-  });
-
-  it("every attachment item should not handle input when the PN message is cancelled", async () => {
-    const { component } = renderComponent(
-      generateComponentProperties({ ...generatePnMessage(), isCancelled: true })
-    );
-    const messageAttachmentComponents = component.queryAllByTestId(
-      "MessageAttachmentTouchable"
-    );
-    expect(messageAttachmentComponents.length).toBe(2);
-    // eslint-disable-next-line sonarjs/no-identical-functions
-    await act(() => {
-      messageAttachmentComponents.forEach(messageAttachmentComponent =>
-        fireEvent.press(messageAttachmentComponent)
-      );
-    });
-    expect(mockedOnAttachmentSelect).toHaveBeenCalledTimes(0);
-  });
-
-  it("should NOT render the F24 section if there are no multiple F24", () => {
-    const { component } = renderComponent(
-      generateComponentProperties(generatePnMessage())
-    );
-    expect(component.queryByTestId("pn-message-f24-section")).toBeFalsy();
+    expect(component).toMatchSnapshot();
   });
 });
 
-const generateTestMessageId = () => "00000000000000000000000004" as UIMessageId;
-const generateTestFiscalCode = () => "AAABBB00A00A000A";
-const generatePnMessage = (): PNMessage => ({
-  iun: "731143-7-0317-8200-0",
-  subject: "This is the message subject",
-  senderDenomination: "Sender denomination",
-  abstract: "Message abstract",
-  notificationStatusHistory: [],
-  recipients: [
-    {
-      recipientType: "-",
-      taxId: generateTestFiscalCode(),
-      denomination: "AaAaAa BbBbBb",
-      payment: {
-        noticeCode: "026773337463073118",
-        creditorTaxId: "00000000009"
-      }
-    }
-  ] as Array<NotificationRecipient>,
-  attachments: [
-    {
-      messageId: generateTestMessageId(),
-      id: "1",
-      displayName: "A First Attachment",
-      contentType: "application/pdf",
-      category: ATTACHMENT_CATEGORY.DOCUMENT,
-      resourceUrl: { href: "/resource/attachment1.pdf" }
-    },
-    {
-      messageId: generateTestMessageId(),
-      id: "2",
-      displayName: "A Second Attachment",
-      contentType: "application/pdf",
-      category: ATTACHMENT_CATEGORY.DOCUMENT,
-      resourceUrl: { href: "/resource/attachment2.pdf" }
-    }
-  ] as Array<UIAttachment>
-});
-const generateComponentProperties = (pnMessage: PNMessage) => ({
+const generateComponentProperties = (
+  messageId: UIMessageId,
+  message: PNMessage
+) => ({
+  messageId,
+  message,
   payments: undefined,
-  messageId: generateTestMessageId(),
-  message: pnMessage,
   service: undefined
 });
 
@@ -173,12 +40,13 @@ const renderComponent = (
   props: React.ComponentProps<typeof MessageDetails>
 ) => {
   const globalState = appReducer(undefined, applicationChangeState("active"));
-  const store = createStore(appReducer, globalState as any);
+  const mockStore = configureMockStore<GlobalState>();
+  const store: ReturnType<typeof mockStore> = mockStore(globalState);
 
   return {
     component: renderScreenWithNavigationStoreContext<GlobalState>(
       () => <MessageDetails {...props} />,
-      PN_ROUTES.MESSAGE_DETAILS,
+      "DUMMY_ROUTE",
       {},
       store
     ),
