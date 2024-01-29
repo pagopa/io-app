48773e3de4ac99a9b1c00e43580c2f6681361838
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2024-01-29 15:16:42 +0100

    update tests

--- ts/features/pn/__mocks__/message.ts
+++ ts/features/pn/__mocks__/message.ts
@@ -1,44 +1,46 @@
-import { UIAttachment, UIMessageId } from "../../messages/types";
-import { PNMessage } from "../store/types/types";
+import { ThirdPartyMessageWithContent } from "../../../../definitions/backend/ThirdPartyMessageWithContent";
+import { message_1 } from "../../messages/__mocks__/message";
 import { ATTACHMENT_CATEGORY } from "../../messages/types/attachmentCategory";
-import { NotificationRecipient } from "../../../../definitions/pn/NotificationRecipient";
 
-export const messageId = "00000000000000000000000004" as UIMessageId;
-
-export const pnMessage: PNMessage = {
+export const thirdPartyMessage: ThirdPartyMessageWithContent = {
+  ...message_1,
   created_at: new Date("2020-01-01T00:00:00.000Z"),
-  iun: "731143-7-0317-8200-0",
-  subject: "This is the message subject",
-  senderDenomination: "Sender denomination",
-  abstract: "Message abstract",
-  notificationStatusHistory: [],
-  recipients: [
-    {
-      recipientType: "-",
-      taxId: "AAABBB00A00A000A",
-      denomination: "AaAaAa BbBbBb",
-      payment: {
-        noticeCode: "026773337463073118",
-        creditorTaxId: "00000000009"
-      }
+  third_party_message: {
+    details: {
+      abstract: "######## abstract ########",
+      attachments: [
+        {
+          messageId: message_1.id,
+          id: "1",
+          displayName: "A First Attachment",
+          contentType: "application/pdf",
+          category: ATTACHMENT_CATEGORY.DOCUMENT,
+          resourceUrl: { href: "/resource/attachment1.pdf" }
+        },
+        {
+          messageId: message_1.id,
+          id: "2",
+          displayName: "A Second Attachment",
+          contentType: "application/pdf",
+          category: ATTACHMENT_CATEGORY.DOCUMENT,
+          resourceUrl: { href: "/resource/attachment2.pdf" }
+        }
+      ],
+      iun: "731143-7-0317-8200-0",
+      subject: "######## subject ########",
+      recipients: [
+        {
+          recipientType: "-",
+          taxId: "AAABBB00A00A000A",
+          denomination: "AaAaAa BbBbBb",
+          payment: {
+            noticeCode: "026773337463073118",
+            creditorTaxId: "00000000009"
+          }
+        }
+      ],
+      notificationStatusHistory: [],
+      senderDenomination: "Sender denomination"
     }
-  ] as Array<NotificationRecipient>,
-  attachments: [
-    {
-      messageId,
-      id: "1",
-      displayName: "A First Attachment",
-      contentType: "application/pdf",
-      category: ATTACHMENT_CATEGORY.DOCUMENT,
-      resourceUrl: { href: "/resource/attachment1.pdf" }
-    },
-    {
-      messageId,
-      id: "2",
-      displayName: "A Second Attachment",
-      contentType: "application/pdf",
-      category: ATTACHMENT_CATEGORY.DOCUMENT,
-      resourceUrl: { href: "/resource/attachment2.pdf" }
-    }
-  ] as Array<UIAttachment>
+  }
 };
--- ts/features/pn/components/__test__/MessageDetails.test.tsx
+++ ts/features/pn/components/__test__/MessageDetails.test.tsx
@@ -1,23 +1,35 @@
 import React from "react";
 import configureMockStore from "redux-mock-store";
+import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/lib/Option";
 import { applicationChangeState } from "../../../../store/actions/application";
 import { appReducer } from "../../../../store/reducers";
 import { MessageDetails } from "../MessageDetails";
 import { GlobalState } from "../../../../store/reducers/types";
 import { renderScreenWithNavigationStoreContext } from "../../../../utils/testWrapper";
 import { PNMessage } from "../../store/types/types";
-import { messageId, pnMessage } from "../../__mocks__/message";
+import { thirdPartyMessage } from "../../__mocks__/message";
+import { toPNMessage } from "../../store/types/transformers";
+import { UIMessageId } from "../../../messages/types";
+
+const pnMessage = pipe(thirdPartyMessage, toPNMessage, O.toUndefined);
 
 describe("MessageDetails component", () => {
   it("should match the snapshot", () => {
     const { component } = renderComponent(
-      generateComponentProperties(pnMessage)
+      generateComponentProperties(
+        thirdPartyMessage.id as UIMessageId,
+        pnMessage!
+      )
     );
     expect(component).toMatchSnapshot();
   });
 });
 
-const generateComponentProperties = (message: PNMessage) => ({
+const generateComponentProperties = (
+  messageId: UIMessageId,
+  message: PNMessage
+) => ({
   messageId,
   message,
   payments: undefined,
