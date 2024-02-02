1a956000d094face3f47d7cb6ce7b45e67ac0bfe
Damiano Plebani <damiano.plebani@pagopa.it>
2024-02-02 11:27:42 +0100

    Refactor `DevScreenButton` to not use `native-base` anymore

--- ts/components/DevScreenButton.tsx
+++ ts/components/DevScreenButton.tsx
@@ -1,30 +1,34 @@
+import { IOColors, IOVisualCostants } from "@pagopa/io-app-design-system";
 import * as React from "react";
-import { StyleSheet } from "react-native";
-import { Text as NBButtonText } from "native-base";
-import ButtonDefaultOpacity from "./ButtonDefaultOpacity";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-type Props = Readonly<{
+type DevScreenButtonProps = Readonly<{
   onPress: () => void;
 }>;
 
 const styles = StyleSheet.create({
   devButton: {
     position: "absolute",
-    top: 20,
-    left: 3,
-    zIndex: 1000
+    right: IOVisualCostants.appMarginDefault,
+    zIndex: 1000,
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderRadius: 8,
+    borderColor: IOColors["grey-450"]
   }
 });
 
-export const DevScreenButton: React.SFC<Props> = props => (
-  <ButtonDefaultOpacity
-    dark={true}
-    onPress={props.onPress}
-    small={true}
-    bordered={true}
-    transparent={true}
-    style={styles.devButton}
-  >
-    <NBButtonText>Dev</NBButtonText>
-  </ButtonDefaultOpacity>
-);
+export const DevScreenButton = ({ onPress }: DevScreenButtonProps) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <Pressable
+      onPress={onPress}
+      style={[styles.devButton, { top: insets.top + 56 }]}
+    >
+      <Text style={{ color: IOColors["grey-700"] }}>Dev</Text>
+    </Pressable>
+  );
+};
