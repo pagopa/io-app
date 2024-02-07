b050ee23e02760191ac634e88f92363da5fb31cd
Andrea Piai <andrea.piai@pagopa.it>
2024-02-07 14:42:43 +0100

    Suggested changes

--- ts/features/messages/__mocks__/attachment.ts
+++ ts/features/messages/__mocks__/attachment.ts
@@ -1,19 +1,19 @@
-import { INonEmptyStringTag } from "@pagopa/ts-commons/lib/strings";
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { ThirdPartyAttachment } from "../../../../definitions/backend/ThirdPartyAttachment";
 import { ATTACHMENT_CATEGORY } from "../types/attachmentCategory";
 
 export const mockPdfAttachment: ThirdPartyAttachment = {
-  id: "1" as string & INonEmptyStringTag,
-  name: "invoice.pdf" as string & INonEmptyStringTag,
-  content_type: "application/pdf" as string & INonEmptyStringTag,
-  url: "https://www.invoicepdf.com/invoice.pdf" as string & INonEmptyStringTag,
+  id: "1" as NonEmptyString,
+  name: "invoice.pdf" as NonEmptyString,
+  content_type: "application/pdf" as NonEmptyString,
+  url: "https://www.invoicepdf.com/invoice.pdf" as NonEmptyString,
   category: ATTACHMENT_CATEGORY.DOCUMENT
 };
 
 export const mockOtherAttachment: ThirdPartyAttachment = {
-  id: "2" as string & INonEmptyStringTag,
-  name: "image.png" as string & INonEmptyStringTag,
-  content_type: "other" as string & INonEmptyStringTag,
-  url: "htts://www.randomImage.com/image.png" as string & INonEmptyStringTag,
+  id: "2" as NonEmptyString,
+  name: "image.png" as NonEmptyString,
+  content_type: "other" as NonEmptyString,
+  url: "htts://www.randomImage.com/image.png" as NonEmptyString,
   category: ATTACHMENT_CATEGORY.F24
 };
