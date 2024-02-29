e103b64c823d24dccfc2a97e12983070ee4d98e7
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2024-02-29 17:49:58 +0100

    fix: iOS a11y

--- ts/screens/ingress/IngressScreen.tsx
+++ ts/screens/ingress/IngressScreen.tsx
@@ -2,7 +2,7 @@
  * An ingress screen to choose the real first screen the user must navigate to.
  */
 import * as React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import {
   ContentWrapper,
   H3,
@@ -11,10 +11,12 @@ import {
   VSpacer
 } from "@pagopa/io-app-design-system";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useFocusEffect } from "@react-navigation/native";
 import I18n from "../../i18n";
 import { useOnFirstRender } from "../../utils/hooks/useOnFirstRender";
 import { mixpanelTrack } from "../../mixpanel";
 import { buildEventProperties } from "../../utils/analytics";
+import { setAccessibilityFocus } from "../../utils/accessibility";
 
 const styles = StyleSheet.create({
   container: {
@@ -28,22 +30,48 @@ const SPINNER_SIZE = 48;
 const SPACE_BETWEEN_SPINNER_AND_TEXT = 24;
 
 export const IngressScreen = () => {
+  const accessibilityFocusFirstElementRef = React.useRef<View>(null);
   useOnFirstRender(() => {
     void mixpanelTrack(
       "INITIALIZATION_LOADING",
       buildEventProperties("UX", "screen_view")
     );
   });
+
+  useFocusEffect(() => {
+    if (Platform.OS === "ios") {
+      setAccessibilityFocus(accessibilityFocusFirstElementRef);
+    }
+  });
+
   const contentTitle = I18n.t("startup.title");
   return (
     <SafeAreaView style={styles.container}>
       <ContentWrapper>
         <View style={{ alignItems: "center" }}>
-          <LoadingSpinner size={SPINNER_SIZE} />
+          <View accessible={false}>
+            <LoadingSpinner size={SPINNER_SIZE} />
+          </View>
           <VSpacer size={SPACE_BETWEEN_SPINNER_AND_TEXT} />
-          <H3 style={{ textAlign: "center" }} accessibilityLabel={contentTitle}>
-            {contentTitle}
-          </H3>
+          {/**
+           * TalkBack reads the H3 title when the screen is mounted.
+           * While VoiceOver does not read the H3 title when the screen is mounted,
+           * unless the H3 title is wrapped in a View with the ref and accessible props.
+           */}
+          <View
+            ref={
+              Platform.OS === "ios" ? accessibilityFocusFirstElementRef : null
+            }
+            accessible={true}
+          >
+            <H3
+              ref={accessibilityFocusFirstElementRef}
+              style={{ textAlign: "center" }}
+              accessibilityLabel={contentTitle}
+            >
+              {contentTitle}
+            </H3>
+          </View>
         </View>
       </ContentWrapper>
     </SafeAreaView>
