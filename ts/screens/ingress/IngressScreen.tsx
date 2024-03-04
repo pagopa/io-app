059913233ab170cfc667a76e3dcac70937edd423
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2024-03-04 11:29:22 +0100

    chore: prefere a11y announcement instead of focus

--- ts/screens/ingress/IngressScreen.tsx
+++ ts/screens/ingress/IngressScreen.tsx
@@ -2,7 +2,7 @@
  * An ingress screen to choose the real first screen the user must navigate to.
  */
 import * as React from "react";
-import { View, StyleSheet, Platform } from "react-native";
+import { View, StyleSheet, AccessibilityInfo } from "react-native";
 import {
   ContentWrapper,
   H3,
@@ -11,10 +11,8 @@ import {
   VSpacer
 } from "@pagopa/io-app-design-system";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useFocusEffect } from "@react-navigation/native";
 import I18n from "../../i18n";
 import { useOnFirstRender } from "../../utils/hooks/useOnFirstRender";
-import { setAccessibilityFocus } from "../../utils/accessibility";
 import { trackIngressScreen } from "../profile/analytics";
 
 const styles = StyleSheet.create({
@@ -29,20 +27,16 @@ const SPINNER_SIZE = 48;
 const SPACE_BETWEEN_SPINNER_AND_TEXT = 24;
 
 export const IngressScreen = () => {
+  const contentTitle = I18n.t("startup.title");
   const accessibilityFocusFirstElementRef = React.useRef<View>(null);
   useOnFirstRender(() => {
     trackIngressScreen();
+    // Since the screen is shown for a very short time,
+    // we prefer to announce the content to the screen reader,
+    // instead of focusing the first element.
+    AccessibilityInfo.announceForAccessibility(contentTitle);
   });
 
-  useFocusEffect(() => {
-    // We need to set the accessibility focus on the first element of the screen
-    // only on iOS, because on Android TalkBack reads the H3 title when the screen is mounted.
-    if (Platform.OS === "ios") {
-      setAccessibilityFocus(accessibilityFocusFirstElementRef);
-    }
-  });
-
-  const contentTitle = I18n.t("startup.title");
   return (
     <SafeAreaView style={styles.container}>
       <ContentWrapper>
@@ -55,25 +49,13 @@ export const IngressScreen = () => {
             <LoadingSpinner size={SPINNER_SIZE} />
           </View>
           <VSpacer size={SPACE_BETWEEN_SPINNER_AND_TEXT} />
-          {/**
-           * TalkBack reads the H3 title when the screen is mounted.
-           * While VoiceOver does not read the H3 title when the screen is mounted,
-           * unless the H3 title is wrapped in a View with the ref and accessible props.
-           */}
-          <View
-            ref={
-              Platform.OS === "ios" ? accessibilityFocusFirstElementRef : null
-            }
-            accessible={true}
+          <H3
+            ref={accessibilityFocusFirstElementRef}
+            style={{ textAlign: "center" }}
+            accessibilityLabel={contentTitle}
           >
-            <H3
-              ref={accessibilityFocusFirstElementRef}
-              style={{ textAlign: "center" }}
-              accessibilityLabel={contentTitle}
-            >
-              {contentTitle}
-            </H3>
-          </View>
+            {contentTitle}
+          </H3>
         </View>
       </ContentWrapper>
     </SafeAreaView>
