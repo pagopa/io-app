90764e027b056c19f70a0340c0e8d4efdf6331ac
Damiano Plebani <damiano.plebani@pagopa.it>
2024-03-04 14:53:28 +0100

    Remove legacy `FooterWithButtons` from `OnboardingCompletedScreen`

--- ts/screens/onboarding/OnboardingCompletedScreen.tsx
+++ ts/screens/onboarding/OnboardingCompletedScreen.tsx
@@ -1,23 +1,21 @@
+import { FooterWithButtons, Pictogram } from "@pagopa/io-app-design-system";
+import { useRoute } from "@react-navigation/native";
 import * as O from "fp-ts/lib/Option";
 import * as React from "react";
 import { SafeAreaView } from "react-native";
 import { useDispatch } from "react-redux";
-import { Pictogram } from "@pagopa/io-app-design-system";
-import { useRoute } from "@react-navigation/native";
-import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
 import { IOStyles } from "../../components/core/variables/IOStyles";
 import { InfoScreenComponent } from "../../components/infoScreen/InfoScreenComponent";
-import FooterWithButtons from "../../components/ui/FooterWithButtons";
-import { BlockButtonProps } from "../../components/ui/BlockButtons";
+import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
+import { isFastLoginEnabledSelector } from "../../features/fastLogin/store/selectors";
 import I18n from "../../i18n";
 import { completeOnboarding } from "../../store/actions/onboarding";
-import { useOnFirstRender } from "../../utils/hooks/useOnFirstRender";
-import { trackThankYouPageScreen } from "../profile/analytics";
 import { useIOSelector } from "../../store/hooks";
-import { isFastLoginEnabledSelector } from "../../features/fastLogin/store/selectors";
 import { idpSelector } from "../../store/reducers/authentication";
-import { trackLoginEnded } from "../authentication/analytics";
 import { getFlowType } from "../../utils/analytics";
+import { useOnFirstRender } from "../../utils/hooks/useOnFirstRender";
+import { trackLoginEnded } from "../authentication/analytics";
+import { trackThankYouPageScreen } from "../profile/analytics";
 
 const OnboardingCompletedScreen = () => {
   const dispatch = useDispatch();
@@ -27,20 +25,6 @@ const OnboardingCompletedScreen = () => {
 
   const idp = O.isSome(idpSelected) ? idpSelected.value.name : "";
   const route = useRoute();
-
-  const continueButtonProps: BlockButtonProps = {
-    bordered: false,
-    title: I18n.t("global.buttons.continue"),
-    onPress: () => {
-      trackLoginEnded(
-        isFastLoginEnabled,
-        idp,
-        getFlowType(false, true),
-        route.name
-      );
-      dispatch(completeOnboarding());
-    }
-  };
 
   useOnFirstRender(() => {
     trackThankYouPageScreen();
@@ -53,12 +37,26 @@ const OnboardingCompletedScreen = () => {
           image={<Pictogram name="fireworks" />}
           title={I18n.t("onboarding.thankYouPage.title")}
         />
-
-        <FooterWithButtons
-          type={"SingleButton"}
-          leftButton={continueButtonProps}
-        />
       </SafeAreaView>
+      <FooterWithButtons
+        type="SingleButton"
+        primary={{
+          type: "Solid",
+          buttonProps: {
+            label: I18n.t("global.buttons.continue"),
+            accessibilityLabel: I18n.t("global.buttons.continue"),
+            onPress: () => {
+              trackLoginEnded(
+                isFastLoginEnabled,
+                idp,
+                getFlowType(false, true),
+                route.name
+              );
+              dispatch(completeOnboarding());
+            }
+          }
+        }}
+      />
     </BaseScreenComponent>
   );
 };
