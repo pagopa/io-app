9c1c7d51f0c439fbefc926644a2c3a4d3bc1c663
Andrea Piai <andrea.piai@pagopa.it>
2024-03-06 15:55:50 +0100

    getRptIdStringFromPaymentData tests

---
+++ ts/features/messages/utils/__tests__/index.test.ts
@@ -0,0 +1,17 @@
+import { getRptIdStringFromPaymentData } from "..";
+import { PaymentData } from "../../types";
+
+describe("getRptIdStringFromPaymentData", () => {
+  it("should properly format the RptID", () => {
+    const fiscalCode = "01234567890";
+    const noticeNumber = "012345678912345610";
+    const paymentData = {
+      noticeNumber: "",
+      payee: {
+        fiscalCode
+      }
+    } as PaymentData;
+    const rptId = getRptIdStringFromPaymentData(paymentData);
+    expect(rptId).toBe(`${fiscalCode}${noticeNumber}`);
+  });
+});
