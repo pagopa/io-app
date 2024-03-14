3ae055f2863316077829ba757c392af4ce8af007
Andrea <andrea.piai@pagopa.it>
2024-03-14 18:04:36 +0100

    feat: [IOCOM-859] New DS payment button on standard message details (#5583)
    ## Short description
    This PR adds the payment button to a message details, using the new DS.
    ![Simulator Screenshot - iPhone 15 - 2024-03-13 at 17 07
    57](https://github.com/pagopa/io-app/assets/5150343/7af14df3-bd7c-4274-a238-4c1f572d3cd4)
    
    ## List of changes proposed in this pull request
    - New `isPaymentsButtonVisibleSelector` selector to get the payment
    button visibility
    - New `MessageDetailsStickyFooter` component that has both CTAs and
    payment button, set as a sibling of the (message) scroll view with
    absolute positioning to the bottom
    - New `MessageDetailsScrollViewAdditionalSpace` component that adds
    additional space to the bottom of the scroll view in order for the
    content to scroll below the sticky footer (without causing unnecessary
    re-renderings)
    - `MesageDetailsScreen` edited to include both
    `MessageDetailsStickyFooter` and
    `MessageDetailsScrollViewAdditionalSpace`
    - `ExtractedCTAs ` renamed to `CTAsBar` and included into
    `MessageDetailsStickyFooter`
    - `FooterCTAs` replaced by `MessageDetailsStickyFooter`
    - Some PN reducers and hooks have been moved into the appropriate PN
    folder
    
    ## How to test
    Using the io-dev-api-server, check that the button appears on messages
    with payment and that its state reflects the payment status (hidden if
    there's no payment or if the payment is paid or has an error, loading
    while the payment is updating and enabled when the payment is still
    payable)
    
    ---------
    
    Co-authored-by: Alessandro Dell'Oste <alessandro.delloste1@gmail.com>

--- ts/features/pn/hooks/usePNOptInMessage.ts
+++ ts/features/pn/hooks/usePNOptInMessage.ts
@@ -1,17 +1,20 @@
-import { isPNOptInMessage } from "../../pn/utils";
+import { isPNOptInMessage } from "../utils";
 import { useOnFirstRender } from "../../../utils/hooks/useOnFirstRender";
 import {
   trackPNOptInMessageCTADisplaySuccess,
   trackPNOptInMessageOpened
-} from "../../pn/analytics";
+} from "../analytics";
 import { ServiceId } from "../../../../definitions/backend/ServiceId";
 import { useIOStore } from "../../../store/hooks";
-import { CTAS } from "../types/MessageCTA";
+import { CTAS } from "../../messages/types/MessageCTA";
 
 export const usePNOptInMessage = (
   ctas: CTAS | undefined,
   serviceId: ServiceId
 ) => {
+  // Use the store since `isPNOptInMessage` is not a selector but an utility
+  // that uses a backend status configuration that is normally updated every
+  // minute. We do not want to cause a re-rendering or recompute the value
   const store = useIOStore();
   const state = store.getState();
   const pnOptInMessageInfo = isPNOptInMessage(ctas, serviceId, state);
