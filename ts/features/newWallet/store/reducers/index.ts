1bd55ef5cd5f22573d8c6ee8e6e15078cfe1c3af
Federico Mastrini <federicomastrini93@gmail.com>
2024-03-14 16:27:21 +0100

    Revert "chore: remove `PersistPartial`"
    This reverts commit bd530735f327050f8c30b6776a82976f9e329523.

--- ts/features/newWallet/store/reducers/index.ts
+++ ts/features/newWallet/store/reducers/index.ts
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
+import { PersistPartial } from "redux-persist";
 import preferencesPersistor, { WalletPreferencesState } from "./preferences";
 
 export type WalletState = {
-  preferences: WalletPreferencesState;
+  preferences: WalletPreferencesState & PersistPartial;
 };
 
 const walletReducer = combineReducers({
