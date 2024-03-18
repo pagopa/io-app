3d26edae0f58471257bfa9cad0edaf49b72e47c9
Federico Mastrini <federicomastrini93@gmail.com>
2024-03-18 18:26:16 +0100

    chore: add selectors

--- ts/features/newWallet/store/selectors/index.ts
+++ ts/features/newWallet/store/selectors/index.ts
@@ -1,5 +1,7 @@
 import { pipe } from "fp-ts/lib/function";
+import { createSelector } from "reselect";
 import { GlobalState } from "../../../../store/reducers/types";
+import { WalletCardCategory } from "../../types";
 
 const selectWalletFeature = (state: GlobalState) => state.features.wallet;
 
@@ -11,3 +13,9 @@ export const isWalletPaymentsRedirectBannerVisibleSelector = (
     selectWalletFeature,
     wallet => wallet.preferences.shouldShowPaymentsRedirectBanner
   );
+
+export const selectWalletCards = (state: GlobalState) =>
+  pipe(state, selectWalletFeature, wallet => Object.values(wallet.cards));
+
+export const selectWalletCardsByCategory = (category: WalletCardCategory) =>
+  createSelector(selectWalletFeature, wallet => wallet.cards[category]);
