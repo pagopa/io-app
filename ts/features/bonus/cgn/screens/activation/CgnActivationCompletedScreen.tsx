de6a9f75f04501a3b2e34d83d664d3a9fbf0a5a4
Cristiano Tofani <cri.tofani@gmail.com>
2024-03-26 13:04:31 +0100

    chore: [PE-564] CGN Activation KO pages (#5629)
    ## Short description
    This PR applies the new `OperationResultScreenComponent` to all CGN
    result screen for the activation process
    
    ## List of changes proposed in this pull request
    - Changes the UI of result screen components
    
    ## How to test
    Change the return response on the activation API or navigate directly on
    the result page from anywhere in the codebase
    
    | Result | Screen |
    |--------|--------|
    | **Already Active** | <img
    src="https://github.com/pagopa/io-app/assets/3959405/08a13d35-186e-4196-bb1c-2d19f62a822c"
    height="500"/> |
    | **Pending** | <img
    src="https://github.com/pagopa/io-app/assets/3959405/9928ce00-af6e-4cf8-89c7-028400c6a718"
    height="500"/> |
    | **Ineligible** | <img
    src="https://github.com/pagopa/io-app/assets/3959405/eaf7b600-0262-4992-ac1e-51e23365f4a9"
    height="500"/> |
    | **Timeout** | <img
    src="https://github.com/pagopa/io-app/assets/3959405/0d3b64ba-20be-44a9-8343-e662b261de01"
    height="500"/> |

--- ts/features/bonus/cgn/screens/activation/CgnActivationCompletedScreen.tsx
+++ ts/features/bonus/cgn/screens/activation/CgnActivationCompletedScreen.tsx
@@ -1,57 +1,32 @@
 import * as React from "react";
-import { connect } from "react-redux";
-import { SafeAreaView, View } from "react-native";
-import { FooterWithButtons } from "@pagopa/io-app-design-system";
-
-import { GlobalState } from "../../../../../store/reducers/types";
-import { Dispatch } from "../../../../../store/actions/types";
-import { InfoScreenComponent } from "../../../../../components/infoScreen/InfoScreenComponent";
-import { IOStyles } from "../../../../../components/core/variables/IOStyles";
 import { cgnActivationComplete } from "../../store/actions/activation";
 import I18n from "../../../../../i18n";
-import paymentCompleted from "../../../../../../img/pictograms/payment-completed.png";
-import { renderInfoRasterImage } from "../../../../../components/infoScreen/imageRendering";
-
-type Props = ReturnType<typeof mapStateToProps> &
-  ReturnType<typeof mapDispatchToProps>;
+import { useIODispatch } from "../../../../../store/hooks";
+import { OperationResultScreenContent } from "../../../../../components/screens/OperationResultScreenContent";
 
 /**
  * Screen which is displayed when a user requested a CGN activation
  * and it has been correctly activated
  */
-const CgnActivationCompletedScreen = (props: Props): React.ReactElement => (
-  <SafeAreaView style={IOStyles.flex}>
-    <InfoScreenComponent
-      image={renderInfoRasterImage(paymentCompleted)}
-      title={I18n.t("bonus.cgn.activation.success.title")}
-      body={I18n.t("bonus.cgn.activation.success.body")}
-    />
-    <View>
-      <FooterWithButtons
-        type="SingleButton"
-        primary={{
-          type: "Solid",
-          buttonProps: {
-            label: I18n.t("bonus.cgn.cta.goToDetail"),
-            onPress: props.onConfirm,
-            accessibilityLabel: I18n.t("bonus.cgn.cta.goToDetail"),
-            testID: "cgnConfirmButtonTestId"
-          }
-        }}
-      />
-    </View>
-  </SafeAreaView>
-);
-
-const mapStateToProps = (_: GlobalState) => ({});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  onConfirm: () => {
+const CgnActivationCompletedScreen = () => {
+  const dispatch = useIODispatch();
+  const onConfirm = React.useCallback(() => {
     dispatch(cgnActivationComplete());
-  }
-});
+  }, [dispatch]);
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CgnActivationCompletedScreen);
+  return (
+    <OperationResultScreenContent
+      pictogram="success"
+      title={I18n.t("bonus.cgn.activation.success.title")}
+      subtitle={I18n.t("bonus.cgn.activation.success.body")}
+      action={{
+        label: I18n.t("bonus.cgn.cta.goToDetail"),
+        accessibilityLabel: I18n.t("bonus.cgn.cta.goToDetail"),
+        testID: "cgnConfirmButtonTestId",
+        onPress: onConfirm
+      }}
+    />
+  );
+};
+
+export default CgnActivationCompletedScreen;
