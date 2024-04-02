218007f960cdc9615dd3459b7091ff487b332cc5
Federico Mastrini <federicomastrini93@gmail.com>
2024-04-02 11:26:05 +0200

    fix: add missing dispatch

--- ts/features/payments/home/components/PaymentsHomeTransactionList.tsx
+++ ts/features/payments/home/components/PaymentsHomeTransactionList.tsx
@@ -3,45 +3,58 @@ import {
   ListItemTransaction
 } from "@pagopa/io-app-design-system";
 import * as pot from "@pagopa/ts-commons/lib/pot";
+import { useFocusEffect } from "@react-navigation/native";
 import * as _ from "lodash";
 import * as React from "react";
 import { default as I18n } from "../../../../i18n";
-import { useIOSelector } from "../../../../store/hooks";
+import { fetchTransactionsRequestWithExpBackoff } from "../../../../store/actions/wallet/transactions";
+import { useIODispatch, useIOSelector } from "../../../../store/hooks";
 import { GlobalState } from "../../../../store/reducers/types";
 import { Transaction } from "../../../../types/pagopa";
 import { PaymentsHomeListItemTransaction } from "./PaymentsHomeListItemTransaction";
 
 const PaymentsHomeTransactionList = () => {
+  const dispatch = useIODispatch();
+
   const transactionsPot = useIOSelector(
     (state: GlobalState) => state.wallet.transactions.transactions
   );
 
+  const isLoading = pot.isLoading(transactionsPot);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      dispatch(fetchTransactionsRequestWithExpBackoff({ start: 0 }));
+    }, [dispatch])
+  );
+
   const renderItems = () => {
-    if (pot.isLoading(transactionsPot)) {
-      return Array.from({ length: 5 }).map((_, index) => (
-        <ListItemTransaction
-          isLoading={true}
-          key={index}
-          transactionStatus="success"
-          transactionAmount=""
-          title=""
-          subtitle=""
-        />
-      ));
+    if (!isLoading && pot.isSome(transactionsPot)) {
+      const toArray = _.values(transactionsPot.value);
+      const sortedByCreationDate = _.orderBy(toArray, item => item?.created, [
+        "desc"
+      ]);
+
+      return sortedByCreationDate
+        .filter((item): item is Transaction => item !== undefined)
+        .map(transaction => (
+          <PaymentsHomeListItemTransaction
+            key={`transaction_${transaction.id}`}
+            transaction={transaction}
+          />
+        ));
     }
 
-    const toArray = _.values(history);
-    const sortedByCreationDate = _.orderBy(toArray, item => item?.created, [
-      "desc"
-    ]);
-    return sortedByCreationDate
-      .filter((item): item is Transaction => item !== undefined)
-      .map(transaction => (
-        <PaymentsHomeListItemTransaction
-          key={`transaction_${transaction.id}`}
-          transaction={transaction}
-        />
-      ));
+    return Array.from({ length: 5 }).map((_, index) => (
+      <ListItemTransaction
+        isLoading={true}
+        key={index}
+        transactionStatus="success"
+        transactionAmount=""
+        title=""
+        subtitle=""
+      />
+    ));
   };
 
   return (
