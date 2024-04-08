7b3fe2e0e885db1eecb82e573a9762892fac93f2
Federico Mastrini <federicomastrini93@gmail.com>
2024-04-08 15:32:49 +0200

    feat: add `WalletCategoryFilterTabs` component

---
+++ ts/features/newWallet/components/WalletCategoryFilterTabs.tsx
@@ -0,0 +1,67 @@
+import {
+  IOVisualCostants,
+  TabItem,
+  TabNavigation,
+  VSpacer
+} from "@pagopa/io-app-design-system";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import I18n from "../../../i18n";
+import { useIODispatch, useIOSelector } from "../../../store/hooks";
+import { walletSetCategoryFilter } from "../store/actions/preferences";
+import { getWalletCardsByCategorySelector } from "../store/selectors";
+import { WalletCardCategory, walletCardCategoryIcons } from "../types";
+
+const WalletCategoryFilterTabs = () => {
+  const dispatch = useIODispatch();
+
+  const cardsByCategory = useIOSelector(getWalletCardsByCategorySelector);
+  const categories = Object.keys(
+    cardsByCategory
+  ) as ReadonlyArray<WalletCardCategory>;
+
+  const handleFilterSelected = (index: number) => {
+    dispatch(
+      walletSetCategoryFilter(index === 0 ? undefined : categories[index - 1])
+    );
+  };
+
+  if (categories.length < 2) {
+    // There is no need to show category filter if we have less than 2 categories
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <TabNavigation tabAlignment="start" onItemPress={handleFilterSelected}>
+        {[
+          <TabItem
+            key={`category_tab_all`}
+            label={I18n.t(`features.wallet.cards.categories.all`)}
+            accessibilityLabel={I18n.t(`features.wallet.cards.categories.all`)}
+          />,
+          ...categories.map(category => (
+            <TabItem
+              key={`category_tab_${category}`}
+              label={I18n.t(`features.wallet.cards.categories.${category}`)}
+              accessibilityLabel={I18n.t(
+                `features.wallet.cards.categories.${category}`
+              )}
+              icon={walletCardCategoryIcons[category]}
+            />
+          ))
+        ]}
+      </TabNavigation>
+      <VSpacer size={16} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: -IOVisualCostants.appMarginDefault * 2,
+    paddingHorizontal: IOVisualCostants.appMarginDefault
+  }
+});
+
+export { WalletCategoryFilterTabs };
