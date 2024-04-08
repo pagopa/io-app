e277a372ecbfb446103d8c65d32d13f9c7b40506
Federico Mastrini <federicomastrini93@gmail.com>
2024-04-08 14:49:59 +0200

    chore: remove category grouping if only 1 card

--- ts/features/newWallet/components/WalletCardsContainer.tsx
+++ ts/features/newWallet/components/WalletCardsContainer.tsx
@@ -2,12 +2,31 @@ import * as React from "react";
 import { View } from "react-native";
 import I18n from "../../../i18n";
 import { useIOSelector } from "../../../store/hooks";
-import { getWalletCardsByCategorySelector } from "../store/selectors";
-import { WalletCardCategory, walletCardCategoryIcons } from "../types";
+import {
+  getWalletCardsByCategorySelector,
+  selectWalletCards
+} from "../store/selectors";
+import {
+  WalletCardCategory,
+  walletCardCategoryIcons,
+  walletCardComponentMapper
+} from "../types";
 import { WalletCardsCategoryContainer } from "./WalletCardsCategoryContainer";
 
 const WalletCardsContainer = () => {
+  const cards = useIOSelector(selectWalletCards);
   const cardsByCategory = useIOSelector(getWalletCardsByCategorySelector);
+
+  if (cards.length === 1) {
+    // Single card does not need grouping
+    const card = cards[0];
+    const Component = walletCardComponentMapper[card.type];
+    return (
+      Component && (
+        <Component testID={`walletCardTestID_${card.key}`} cardProps={card} />
+      )
+    );
+  }
 
   return (
     <View testID="walletCardsContainerTestID">
