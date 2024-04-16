0e66c95cc296d117f644b3a64e132f3450005b34
Mario Perrotta <mario.perrotta@pagopa.it>
2024-04-16 15:55:51 +0200

    test: add test for ItwDiscoveryInfoScreen

---
+++ ts/features/itwallet/screens/discovery/__tests__/ItwDiscoveryInfoScreen.test.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { createStore } from "redux";
+import ItwDiscoveryInfoScreen from "../ItwDiscoveryInfoScreen";
+import { ITW_ROUTES } from "../../../navigation/routes";
+import { appReducer } from "../../../../../store/reducers";
+import { applicationChangeState } from "../../../../../store/actions/application";
+import { renderScreenWithNavigationStoreContext } from "../../../../../utils/testWrapper";
+import { GlobalState } from "../../../../../store/reducers/types";
+
+describe("Test ItwDiscoveryInfo screen", () => {
+  it("should render the ItwDiscoveryInfo screen", () => {
+    const component = renderComponent();
+    expect(component).toBeTruthy();
+  });
+  it("should match the snapshot", () => {
+    const component = renderComponent();
+    expect(component).toMatchSnapshot();
+  });
+});
+
+const renderComponent = () => {
+  const globalState = appReducer(undefined, applicationChangeState("active"));
+  return renderScreenWithNavigationStoreContext<GlobalState>(
+    () => <ItwDiscoveryInfoScreen />,
+    ITW_ROUTES.DISCOVERY.INFO,
+    {},
+    createStore(appReducer, globalState as any)
+  );
+};
