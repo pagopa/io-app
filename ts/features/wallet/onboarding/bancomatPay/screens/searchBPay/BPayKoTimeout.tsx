a633a2faeb9aaef71f3df0756b5b9d62d43c24d0
Damiano Plebani <damiano.plebani@pagopa.it>
2024-04-16 12:33:17 +0200

    Remove legacy `FooterTwoButtons` and `FooterSingleButton`

--- ts/features/wallet/onboarding/bancomatPay/screens/searchBPay/BPayKoTimeout.tsx
+++ ts/features/wallet/onboarding/bancomatPay/screens/searchBPay/BPayKoTimeout.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { SafeAreaView } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { FooterWithButtons } from "@pagopa/io-app-design-system";
 import image from "../../../../../../../img/servicesStatus/error-detail-icon.png";
 import { IOStyles } from "../../../../../../components/core/variables/IOStyles";
 import { renderInfoRasterImage } from "../../../../../../components/infoScreen/imageRendering";
@@ -9,7 +10,6 @@ import { InfoScreenComponent } from "../../../../../../components/infoScreen/Inf
 import BaseScreenComponent from "../../../../../../components/screens/BaseScreenComponent";
 import I18n from "../../../../../../i18n";
 import { GlobalState } from "../../../../../../store/reducers/types";
-import { FooterTwoButtons } from "../../../../../../components/markdown/FooterTwoButtons";
 import { searchUserBPay, walletAddBPayCancel } from "../../store/actions";
 import { onboardingBPayAbiSelectedSelector } from "../../store/reducers/abiSelected";
 
@@ -43,14 +43,24 @@ const BPayKoTimeout = (props: Props): React.ReactElement => {
           title={title}
           body={body}
         />
-        <FooterTwoButtons
-          type={"TwoButtonsInlineThird"}
-          onRight={() => props.retry(props.abiSelected)}
-          onCancel={props.cancel}
-          rightText={retry}
-          leftText={cancel}
-        />
       </SafeAreaView>
+      <FooterWithButtons
+        type="TwoButtonsInlineThird"
+        primary={{
+          type: "Outline",
+          buttonProps: {
+            label: cancel,
+            onPress: props.cancel
+          }
+        }}
+        secondary={{
+          type: "Solid",
+          buttonProps: {
+            label: retry,
+            onPress: () => props.retry(props.abiSelected)
+          }
+        }}
+      />
     </BaseScreenComponent>
   );
 };
