f663a1bacdc3c45399be09ec6cc1407197ba38bf
Mario Perrotta <mario.perrotta@pagopa.it>
2024-04-16 09:08:52 +0200

    chore: update styles

--- ts/features/itwallet/components/ItwMarkdown.tsx
+++ ts/features/itwallet/components/ItwMarkdown.tsx
@@ -2,11 +2,15 @@ import React, { ReactNode } from "react";
 import {
   Body,
   H1,
+  h1FontSize,
   H2,
   h2FontSize,
   H3,
+  h3FontSize,
   H4,
+  h4FontSize,
   H5,
+  h5FontSize,
   H6,
   h6FontSize,
   LabelLink,
@@ -62,16 +66,28 @@ const rules = {
   )
 };
 
+/* eslint-disable react-native/no-unused-styles */
 const styles = StyleSheet.create({
-  // eslint-disable-next-line react-native/no-unused-styles
+  heading1: {
+    fontSize: h1FontSize
+  },
   heading2: {
     fontSize: h2FontSize
   },
-  // eslint-disable-next-line react-native/no-unused-styles
+  heading3: {
+    fontSize: h3FontSize
+  },
+  heading4: {
+    fontSize: h4FontSize
+  },
+  heading5: {
+    fontSize: h5FontSize
+  },
   heading6: {
     fontSize: h6FontSize
   }
 });
+/* eslint-enable react-native/no-unused-styles */
 
 /**
  * This component renders a markdown text.
