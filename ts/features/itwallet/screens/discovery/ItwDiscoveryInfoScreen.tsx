6e27fae388c29509ff78c04a1a9249b18b50aad1
Mario Perrotta <mario.perrotta@pagopa.it>
2024-04-16 14:22:39 +0200

    chore: update

--- ts/features/itwallet/screens/discovery/ItwDiscoveryInfoScreen.tsx
+++ ts/features/itwallet/screens/discovery/ItwDiscoveryInfoScreen.tsx
@@ -4,7 +4,14 @@ import I18n from "../../../../i18n";
 import { useHeaderSecondLevel } from "../../../../hooks/useHeaderSecondLevel";
 import { emptyContextualHelp } from "../../../../utils/emptyContextualHelp";
 import ItwMarkdown from "../../components/ItwMarkdown";
+import { RNavScreenWithLargeHeader } from "../../../../components/ui/RNavScreenWithLargeHeader";
 
+/**
+ * This is the screen that shows the information about the discovery process
+ * about the activation of the DIW. It uses a markdown component to render
+ * the content of the screen. The screen is wrapped in a GradientScrollView
+ * with a primary and secondary action.
+ */
 const ItwDiscoveryInfoScreen = () => {
   useHeaderSecondLevel({
     title: I18n.t("features.itWallet.discovery.title"),
@@ -13,21 +20,25 @@ const ItwDiscoveryInfoScreen = () => {
   });
 
   return (
-    <GradientScrollView
-      primaryActionProps={{
-        label: "Continua",
-        accessibilityLabel: "Continue",
-        onPress: () => undefined
-      }}
-      secondaryActionProps={{
-        label: "Annulla",
-        accessibilityLabel: "RptIt casuale",
-        onPress: () => undefined
-      }}
+    <RNavScreenWithLargeHeader
+      title={{ label: I18n.t("features.itWallet.discovery.title") }}
     >
-      {/* Info activation */}
-      <ItwMarkdown content={I18n.t("features.itWallet.discovery.content")} />
-    </GradientScrollView>
+      <GradientScrollView
+        primaryActionProps={{
+          label: I18n.t("global.buttons.continue"),
+          accessibilityLabel: I18n.t("global.buttons.continue"),
+          onPress: () => undefined
+        }}
+        secondaryActionProps={{
+          label: I18n.t("global.buttons.cancel"),
+          accessibilityLabel: I18n.t("global.buttons.cancel"),
+          onPress: () => undefined
+        }}
+      >
+        {/* Info activation */}
+        <ItwMarkdown content={I18n.t("features.itWallet.discovery.content")} />
+      </GradientScrollView>
+    </RNavScreenWithLargeHeader>
   );
 };
 
