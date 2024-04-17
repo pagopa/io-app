7dd5ed294cdaeb42b00c3592b90e7f98de2d27f7
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2024-04-17 15:07:55 +0200

    chore: adopt DS to RefreshTokenLoadingScreen

--- ts/features/fastLogin/screens/RefreshTokenLoadingScreen.tsx
+++ ts/features/fastLogin/screens/RefreshTokenLoadingScreen.tsx
@@ -1,17 +1,56 @@
 import * as React from "react";
-import { Modal } from "react-native";
+import { Modal, View, StyleSheet } from "react-native";
+import {
+  ContentWrapper,
+  H3,
+  IOStyles,
+  VSpacer
+} from "@pagopa/io-app-design-system";
+import { SafeAreaView } from "react-native-safe-area-context";
+import I18n from "../../../i18n";
 import { useAvoidHardwareBackButton } from "../../../utils/useAvoidHardwareBackButton";
-import LoadingSpinnerOverlay from "../../../components/LoadingSpinnerOverlay";
-import { isDevEnv } from "../../../utils/environment";
-import { Body } from "../../../components/core/typography/Body";
+import { LoadingIndicator } from "../../../components/ui/LoadingIndicator";
+
+const styles = StyleSheet.create({
+  container: {
+    ...IOStyles.bgWhite,
+    ...IOStyles.centerJustified,
+    ...IOStyles.flex
+  },
+  contentTitle: {
+    textAlign: "center"
+  },
+  content: {
+    alignItems: "center"
+  }
+});
+
+const SPACE_BETWEEN_SPINNER_AND_TEXT = 24;
 
 const RefreshTokenLoadingScreen = () => {
   useAvoidHardwareBackButton();
 
+  const contentTitle = I18n.t("fastLogin.loadingScreen.title");
+
   return (
     <Modal>
-      <LoadingSpinnerOverlay isLoading={true} loadingOpacity={1} />
-      {isDevEnv && <Body>This is the RefreshTokenLoadingScreen</Body>}
+      <SafeAreaView style={styles.container}>
+        <ContentWrapper>
+          <View style={styles.content}>
+            <View
+              accessible={false}
+              accessibilityElementsHidden={true}
+              importantForAccessibility={"no-hide-descendants"}
+            >
+              <LoadingIndicator />
+            </View>
+            <VSpacer size={SPACE_BETWEEN_SPINNER_AND_TEXT} />
+            <H3 style={styles.contentTitle} accessibilityLabel={contentTitle}>
+              {contentTitle}
+            </H3>
+          </View>
+        </ContentWrapper>
+      </SafeAreaView>
     </Modal>
   );
 };
