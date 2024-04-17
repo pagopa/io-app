c18ca68e72cf7c640fb83699df3ce0a9e2f97c60
Cristiano Tofani <cri.tofani@gmail.com>
2024-04-17 10:23:28 +0200

    fixes tests

--- ts/components/__tests__/TosWebviewComponent.test.tsx
+++ ts/components/__tests__/TosWebviewComponent.test.tsx
@@ -27,7 +27,22 @@ describe("TosWebviewComponent", () => {
     it("Should render correctly with bottom footer and a basic placeholding HTML", () => {
       const tree = renderer
         .create(
-          <SafeAreaProvider>
+          <SafeAreaProvider
+            initialMetrics={{
+              frame: {
+                width: 320,
+                height: 640,
+                x: 0,
+                y: 0
+              },
+              insets: {
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: 0
+              }
+            }}
+          >
             <TosWebviewComponent
               shouldRenderFooter={true}
               webViewSource={{
@@ -235,7 +250,22 @@ const commonSetup = ({
   onLoaded = () => undefined
 }: CurrentTestConfiguration = {}) =>
   render(
-    <SafeAreaProvider>
+    <SafeAreaProvider
+      initialMetrics={{
+        frame: {
+          width: 320,
+          height: 640,
+          x: 0,
+          y: 0
+        },
+        insets: {
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: 0
+        }
+      }}
+    >
       <TosWebviewComponent
         shouldRenderFooter={shouldRenderFooter}
         webViewSource={{ html: "<html><head></head><body></body></html>" }}
