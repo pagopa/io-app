5293f5a63d8e34c8cd8c4cf0089a858e7acd6506
Cristiano Tofani <cri.tofani@gmail.com>
2024-04-18 11:26:09 +0200

    remove theme files no more used

--- ts/theme/components/Input.ts
+++
@@ -1,9 +0,0 @@
-import { Platform } from "react-native";
-
-import { makeFontStyleObject } from "../fonts";
-import { Theme } from "../types";
-import variables from "../variables";
-
-export default (): Theme => ({
-  ...makeFontStyleObject(Platform.select, variables.inputNormalWeight)
-});
