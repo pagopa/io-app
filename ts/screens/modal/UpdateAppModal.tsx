bef5a45ae90277c5285deb705bfd53c886ec6950
ChrisMattew <cristian.matteu@gmail.com>
2024-04-19 17:14:37 +0200

    refactor(UpdateAppModal): integrate new DS and toast message on error

--- ts/screens/modal/UpdateAppModal.tsx
+++ ts/screens/modal/UpdateAppModal.tsx
@@ -3,156 +3,51 @@
  *
  */
 
-import { Millisecond } from "@pagopa/ts-commons/lib/units";
-import { Button, Container, Text as NBButtonText } from "native-base";
-import React, { FC, useCallback, useEffect, useState } from "react";
-import {
-  View,
-  BackHandler,
-  Image,
-  Linking,
-  Modal,
-  Platform,
-  StyleSheet
-} from "react-native";
-import { VSpacer } from "@pagopa/io-app-design-system";
-import updateIcon from "../../../img/icons/update-icon.png";
-import { Body } from "../../components/core/typography/Body";
-import { H1 } from "../../components/core/typography/H1";
-import { Label } from "../../components/core/typography/Label";
-import { IOStyles } from "../../components/core/variables/IOStyles";
-
-import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
-import SectionStatusComponent from "../../components/SectionStatus";
-import FooterWithButtons from "../../components/ui/FooterWithButtons";
+import React, { useCallback } from "react";
+import { Linking } from "react-native";
+import { IOToast } from "@pagopa/io-app-design-system";
 import { useHardwareBackButton } from "../../hooks/useHardwareBackButton";
 import I18n from "../../i18n";
-import customVariables from "../../theme/variables";
 import { storeUrl, webStoreURL } from "../../utils/appVersion";
-import { emptyContextualHelp } from "../../utils/emptyContextualHelp";
 import { openWebUrl } from "../../utils/url";
+import { OperationResultScreenContent } from "../../components/screens/OperationResultScreenContent";
+import withModalAndToastProvider from "../../hocs/withModalAndToastProvider";
 import { trackForcedUpdateScreen, trackUpdateAppButton } from "./analytics";
 
-const ERROR_MESSAGE_TIMEOUT: Millisecond = 5000 as Millisecond;
-
-const styles = StyleSheet.create({
-  container: {
-    margin: customVariables.contentPadding,
-    flex: 1,
-    alignItems: "flex-start"
-  },
-  img: {
-    marginTop: customVariables.contentPaddingLarge,
-    alignSelf: "center"
-  }
-});
-
-type FooterProps = { onOpenAppStore: () => void };
-
-const IOSFooter: FC<FooterProps> = ({ onOpenAppStore }: FooterProps) => (
-  <View style={IOStyles.footer}>
-    <>
-      <Button
-        block={true}
-        primary={true}
-        onPress={onOpenAppStore}
-        accessibilityRole={"button"}
-      >
-        <NBButtonText>{I18n.t("btnUpdateApp")}</NBButtonText>
-      </Button>
-      <VSpacer size={16} />
-    </>
-  </View>
-);
-
-const AndroidFooter: FC<FooterProps> = ({ onOpenAppStore }: FooterProps) => {
-  const cancelButtonProps = {
-    cancel: true,
-    block: true,
-    onPress: () => BackHandler.exitApp(),
-    title: I18n.t("global.buttons.close")
-  };
-
-  const updateButtonProps = {
-    block: true,
-    primary: true,
-    onPress: onOpenAppStore,
-    title: I18n.t("btnUpdateApp")
-  };
-
-  return (
-    <FooterWithButtons
-      type="TwoButtonsInlineThird"
-      leftButton={cancelButtonProps}
-      rightButton={updateButtonProps}
-    />
-  );
-};
-
 const UpdateAppModal: React.FC = () => {
+  const title = I18n.t("titleUpdateApp");
+  const subtitle = I18n.t("messageUpdateApp");
+  const actionLabel = I18n.t("btnUpdateApp");
   // Disable Android back button
   useHardwareBackButton(() => true);
 
   trackForcedUpdateScreen();
 
-  // Reset the error state after a given timeout
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    if (error) {
-      const timeoutHandle = setTimeout(
-        () => setError(false),
-        ERROR_MESSAGE_TIMEOUT
-      );
-
-      return () => clearTimeout(timeoutHandle);
-    }
-
-    return undefined;
-  }, [error]);
-
   // Tries to open the native app store, falling to browser web store
   const openAppStore = useCallback(async () => {
     trackUpdateAppButton();
+
     try {
       await Linking.openURL(storeUrl);
     } catch (e) {
-      openWebUrl(webStoreURL, () => setError(true));
+      openWebUrl(webStoreURL, () => {
+        IOToast.error(I18n.t("msgErrorUpdateApp"));
+      });
     }
   }, []);
 
   return (
-    <Modal>
-      <BaseScreenComponent
-        appLogo={true}
-        goBack={false}
-        accessibilityEvents={{ avoidNavigationEventsUsage: true }}
-        contextualHelp={emptyContextualHelp}
-      >
-        <Container>
-          <View style={styles.container}>
-            <H1>{I18n.t("titleUpdateApp")}</H1>
-            <VSpacer size={24} />
-            <Body>{I18n.t("messageUpdateApp")}</Body>
-            <Image style={styles.img} source={updateIcon} />
-            {error && (
-              <View style={IOStyles.alignCenter}>
-                <VSpacer size={24} />
-                <Label color="red" weight="SemiBold">
-                  {I18n.t("msgErrorUpdateApp")}
-                </Label>
-              </View>
-            )}
-          </View>
-        </Container>
-        <SectionStatusComponent sectionKey={"app_update_required"} />
-      </BaseScreenComponent>
-      {Platform.select({
-        default: <AndroidFooter onOpenAppStore={openAppStore} />,
-        ios: <IOSFooter onOpenAppStore={openAppStore} />
-      })}
-    </Modal>
+    <OperationResultScreenContent
+      pictogram="updateOS"
+      title={title}
+      subtitle={subtitle}
+      action={{
+        label: actionLabel,
+        accessibilityLabel: actionLabel,
+        onPress: openAppStore
+      }}
+    />
   );
 };
 
-export default UpdateAppModal;
+export default withModalAndToastProvider(UpdateAppModal);
