215c6b3828ab217f6c9ff881308f36cc6fc4fc78
Federico Mastrini <federicomastrini93@gmail.com>
2024-04-22 11:52:15 +0200

    chore: added insightful comment

--- ts/features/newWallet/saga/handleWalletLoadingStateSaga.ts
+++ ts/features/newWallet/saga/handleWalletLoadingStateSaga.ts
@@ -11,7 +11,10 @@ export function* handleWalletLoadingStateSaga(
   action: ActionType<typeof walletToggleLoadingState>
 ) {
   const cards = yield* select(selectWalletCards);
+  // The loading state should be enabled only if there aren't already cards in the wallet
   if (action.payload && cards.length === 0) {
+    // To disable the loading state we wait for at least on `walletAddCards` action
+    // Disable the loading state after LOADING_STATE_TIMEOUT ms in case of no actions received
     yield* race({
       take: take(walletAddCards),
       timeout: delay(LOADING_STATE_TIMEOUT)
