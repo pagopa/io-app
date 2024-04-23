b55ef6baa86bede8a86b001425a1f66d5bdd21f8
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2024-04-23 09:28:09 +0200

    chore: remove CiePinpad

--- ts/features/cieLogin/components/screens/CieLoginConfigScreen.tsx
+++ ts/features/cieLogin/components/screens/CieLoginConfigScreen.tsx
@@ -1,52 +1,22 @@
-import React, { useCallback } from "react";
-import {
-  Button,
-  InputAccessoryView,
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  View
-} from "react-native";
-import { IOColors, ContentWrapper } from "@pagopa/io-app-design-system";
+import React, { useEffect } from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import { ContentWrapper, OTPInput } from "@pagopa/io-app-design-system";
 import BaseScreenComponent from "../../../../components/screens/BaseScreenComponent";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
-import CiePinpad from "../../../../components/CiePinpad";
 import CieLoginConfigScreenContent from "./CieLoginConfigScreenContent";
 
 type PinViewProps = {
   pin: string;
   setPin: (pin: string) => void;
-  onSubmit: (pin: string) => void;
 };
 const PinView = (props: PinViewProps) => (
   <>
-    <CiePinpad
-      pin={props.pin}
-      pinLength={6}
-      onPinChanged={props.setPin}
-      InputAccessoryViewID="pinInputId"
-      onSubmit={props.onSubmit}
+    <OTPInput
+      secret
+      value={props.pin}
+      length={6}
+      onValueChange={props.setPin}
     />
-    {Platform.OS === "ios" && (
-      <InputAccessoryView
-        backgroundColor={IOColors.greyUltraLight}
-        nativeID={"pinInputId"}
-      >
-        <View
-          style={[
-            IOStyles.flex,
-            IOStyles.horizontalContentPadding,
-            { justifyContent: "flex-end", flexDirection: "row" }
-          ]}
-        >
-          <Button
-            onPress={() => props.onSubmit(props.pin)}
-            title="Sblocca"
-            accessibilityLabel="Premi qui per sbloccare"
-          />
-        </View>
-      </InputAccessoryView>
-    )}
   </>
 );
 
@@ -54,22 +24,22 @@ const CieLoginConfigScreen = () => {
   const [locked, setLocked] = React.useState(true);
   const [pin, setPin] = React.useState("");
 
-  const onSubmit = useCallback((pin: string) => {
-    // constant day containig the current day in the format YYMMDD
+  // constant day containig the current day in the format YYMMDD
+  useEffect(() => {
     const day = new Date().toISOString().slice(2, 10).replace(/-/g, "");
     if (pin === day) {
       setLocked(false);
     } else {
       setPin("");
     }
-  }, []);
+  }, [pin]);
 
   return (
     <BaseScreenComponent goBack={true} headerTitle={"CIE Login Settings"}>
       <SafeAreaView style={IOStyles.flex}>
         <ContentWrapper>
           {locked ? (
-            <PinView pin={pin} setPin={setPin} onSubmit={onSubmit} />
+            <PinView pin={pin} setPin={setPin} />
           ) : (
             <ScrollView>
               <CieLoginConfigScreenContent />
