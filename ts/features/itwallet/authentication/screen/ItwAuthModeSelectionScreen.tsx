ed8fba10063c797f07e979e2914930c26a0862f4
Federico Mastrini <federicomastrini93@gmail.com>
2024-04-30 12:01:00 +0200

    chore: add CIE support check

--- ts/features/itwallet/authentication/screen/ItwAuthModeSelectionScreen.tsx
+++ ts/features/itwallet/authentication/screen/ItwAuthModeSelectionScreen.tsx
@@ -4,12 +4,25 @@ import {
   ModuleCredential,
   VSpacer
 } from "@pagopa/io-app-design-system";
+import * as pot from "@pagopa/ts-commons/lib/pot";
 import React from "react";
 import { Alert } from "react-native";
 import { RNavScreenWithLargeHeader } from "../../../../components/ui/RNavScreenWithLargeHeader";
 import I18n from "../../../../i18n";
+import { useIOSelector } from "../../../../store/hooks";
+import { isCieSupportedSelector } from "../../../../store/reducers/cie";
+import { cieFlowForDevServerEnabled } from "../../../cieLogin/utils";
 
 export const ItwAuthModeSelectionScreen = () => {
+  const isCIEAuthenticationSupported = useIOSelector(isCieSupportedSelector);
+
+  const isCieSupported = React.useMemo(
+    () =>
+      cieFlowForDevServerEnabled ||
+      pot.getOrElse(isCIEAuthenticationSupported, false),
+    [isCIEAuthenticationSupported]
+  );
+
   const handleSpidPress = () => {
     Alert.alert("Not implemented");
   };
@@ -37,13 +50,19 @@ export const ItwAuthModeSelectionScreen = () => {
           onPress={handleSpidPress}
         />
         <VSpacer size={8} />
-        <ModuleCredential
-          label={I18n.t("features.itWallet.authentication.method.ciePin.title")}
-          // description="Usa Carta d’Identità Elettronica e PIN"
-          icon="fiscalCodeIndividual"
-          onPress={handleCiePinPress}
-        />
-        <VSpacer size={8} />
+        {isCieSupported && (
+          <>
+            <ModuleCredential
+              label={I18n.t(
+                "features.itWallet.authentication.method.ciePin.title"
+              )}
+              // description="Usa Carta d’Identità Elettronica e PIN"
+              icon="fiscalCodeIndividual"
+              onPress={handleCiePinPress}
+            />
+            <VSpacer size={8} />
+          </>
+        )}
         <ModuleCredential
           label={I18n.t("features.itWallet.authentication.method.cieId.title")}
           // description="Usa credenziali e app CieID"
