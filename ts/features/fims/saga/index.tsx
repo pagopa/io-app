a83063a1285c024774523fc0af42b7bf9881a651
forrest57 <tomba808@gmail.com>
2024-05-14 17:25:29 +0200

    removed second part of the saga for future PR

--- ts/features/fims/saga/index.tsx
+++ ts/features/fims/saga/index.tsx
@@ -1,26 +1,18 @@
 import { SagaIterator } from "redux-saga";
 import { call, put, select, takeLatest } from "typed-redux-saga";
-import { ActionType } from "typesafe-actions";
 import { fimsTokenSelector } from "../../../store/reducers/authentication";
 import { fimsDomainSelector } from "../../../store/reducers/backendStatus";
 import {
   mockHttpNativeCall,
   mockSetNativeCookie
 } from "../__mocks__/mockFIMSCallbacks";
-import {
-  fimsGetConsentsListAction,
-  fimsGetRedirectUrlAndOpenIABAction
-} from "../store/actions";
+import { fimsGetConsentsListAction } from "../store/actions";
 import { fimsCTAUrlSelector } from "../store/selectors";
 
 export function* watchFimsSaga(): SagaIterator {
   yield* takeLatest(
     fimsGetConsentsListAction.request,
     handleFimsGetConsentsList
-  );
-  yield* takeLatest(
-    fimsGetRedirectUrlAndOpenIABAction.request,
-    handleFimsGetRedirectUrlAndOpenIAB
   );
 }
 
@@ -56,29 +48,3 @@ function* handleFimsGetConsentsList() {
 
   yield* put(fimsGetConsentsListAction.success(getConsentsResult));
 }
-
-// note: IAB => InAppBrowser
-function* handleFimsGetRedirectUrlAndOpenIAB(
-  action: ActionType<(typeof fimsGetRedirectUrlAndOpenIABAction)["request"]>
-) {
-  // TODO:: get auth code and nonce, then sign with lollipop, HTTP GET and open IAB
-  const rpUrl = yield* call(recurseUntilRPUrl, action.payload.acceptUrl);
-
-  yield rpUrl;
-}
-
-const isRedirect = (statusCode: number) => statusCode === 302;
-const isOIDCProviderBaseUrl = (url: string) => url.includes("oidc-provider");
-const recurseUntilRPUrl = async (url: string) => {
-  const res = await mockHttpNativeCall({
-    followRedirects: false,
-    verb: "get",
-    url
-  });
-
-  if (!isRedirect(res.status) || !isOIDCProviderBaseUrl(res.headers.Location)) {
-    return res;
-  } else {
-    return; // TODO recursion
-  }
-};
