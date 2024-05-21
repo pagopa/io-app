87198e2fa66b154d8b8064c9e94490eb1b23bcb9
Federico Mastrini <federicomastrini93@gmail.com>
2024-05-21 16:19:36 +0200

    chore: login wip

--- ts/features/itwallet/identification/hooks/useItwIdpIdentificationWebView.ts
+++ ts/features/itwallet/identification/hooks/useItwIdpIdentificationWebView.ts
@@ -1,16 +1,66 @@
-import { openAuthenticationSession } from "@pagopa/io-react-native-login-utils";
+import {
+  LoginUtilsError,
+  openAuthenticationSession
+} from "@pagopa/io-react-native-login-utils";
+import * as O from "fp-ts/lib/Option";
+import * as T from "fp-ts/lib/Task";
+import * as TE from "fp-ts/lib/TaskEither";
+import { pipe } from "fp-ts/lib/function";
 import React from "react";
+import { useIODispatch, useIOSelector } from "../../../../store/hooks";
+import { isMixpanelEnabled } from "../../../../store/reducers/persistedPreferences";
 import { LocalIdpsFallback } from "../../../../utils/idps";
+import { getIdpLoginUri } from "../../../../utils/login";
+import { isFastLoginEnabledSelector } from "../../../fastLogin/store/selectors";
+import { lollipopKeyTagSelector } from "../../../lollipop/store/reducers/lollipop";
+import { regenerateKeyGetRedirectsAndVerifySaml } from "../../../lollipop/utils/login";
 
 export const useItwIdpIdentificationWebView = () => {
+  const dispatch = useIODispatch();
+
+  const maybeKeyTag = useIOSelector(lollipopKeyTagSelector);
+  const isFastLogin = useIOSelector(isFastLoginEnabledSelector);
+  const mixpanelEnabled = useIOSelector(isMixpanelEnabled);
+
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-  const startIdentification = (idp: LocalIdpsFallback) => {
-    if (isLoading) {
+  const idpAuthSession = (
+    loginUri: string
+  ): TE.TaskEither<LoginUtilsError, string> =>
+    pipe(loginUri, () =>
+      TE.tryCatch(
+        () => openAuthenticationSession(loginUri, "iologin"),
+        error => error as LoginUtilsError
+      )
+    );
+
+  const startIdentification = async (idp: LocalIdpsFallback) => {
+    if (isLoading || O.isNone(maybeKeyTag)) {
       return;
     }
 
-    void openAuthenticationSession("", "");
+    void pipe(
+      () =>
+        regenerateKeyGetRedirectsAndVerifySaml(
+          getIdpLoginUri(idp.id, 2),
+          maybeKeyTag.value,
+          mixpanelEnabled,
+          isFastLogin,
+          dispatch
+        ),
+      TE.fold(
+        () => TE.of(undefined),
+        url =>
+          pipe(
+            url,
+            () => idpAuthSession(url),
+            TE.fold(
+              error => T.of(undefined),
+              response => T.of(undefined)
+            )
+          )
+      )
+    )();
   };
 
   return {
