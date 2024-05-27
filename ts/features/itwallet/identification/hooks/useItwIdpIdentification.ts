d2c35d3ad86c62a9f02f16f9a8b4cf0f6c65f1e2
Federico Mastrini <federicomastrini93@gmail.com>
2024-05-27 17:33:21 +0200

    chore: remove `regenerateKeyGetRedirectsAndVerifySaml` func

--- ts/features/itwallet/identification/hooks/useItwIdpIdentification.ts
+++ ts/features/itwallet/identification/hooks/useItwIdpIdentification.ts
@@ -2,17 +2,11 @@ import {
   LoginUtilsError,
   openAuthenticationSession
 } from "@pagopa/io-react-native-login-utils";
-import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/function";
 import React from "react";
-import { useIODispatch, useIOSelector } from "../../../../store/hooks";
-import { isMixpanelEnabled } from "../../../../store/reducers/persistedPreferences";
 import { LocalIdpsFallback } from "../../../../utils/idps";
 import { getIdpLoginUri } from "../../../../utils/login";
-import { isFastLoginEnabledSelector } from "../../../fastLogin/store/selectors";
-import { lollipopKeyTagSelector } from "../../../lollipop/store/reducers/lollipop";
-import { regenerateKeyGetRedirectsAndVerifySaml } from "../../../lollipop/utils/login";
 import {
   ItWalletError,
   ItWalletErrorTypes
@@ -21,45 +15,28 @@ import {
 const URL_LOGIN_SCHEME = "iologin";
 
 export const useItwIdpIdentification = () => {
-  const dispatch = useIODispatch();
-
-  const maybeKeyTag = useIOSelector(lollipopKeyTagSelector);
-  const isFastLogin = useIOSelector(isFastLoginEnabledSelector);
-  const mixpanelEnabled = useIOSelector(isMixpanelEnabled);
-
   const [isPending, setIsPending] = React.useState<boolean>(false);
   const [result, setResult] = React.useState<string>();
   const [error, setError] = React.useState<ItWalletError>();
 
-  const idpAuthSession = (
-    loginUri: string
-  ): TE.TaskEither<Error | LoginUtilsError, string> =>
-    TE.tryCatch(
-      () => {
-        setIsPending(true);
-        return openAuthenticationSession(loginUri, URL_LOGIN_SCHEME);
-      },
-      error => {
-        setError({ code: ItWalletErrorTypes.IDENTIFICATION_ERROR });
-        return error as LoginUtilsError;
-      }
-    );
-
   const startIdentification = async (idp: LocalIdpsFallback) => {
-    if (isPending || O.isNone(maybeKeyTag)) {
+    if (isPending) {
       return;
     }
+    setIsPending(true);
 
     void pipe(
-      () =>
-        regenerateKeyGetRedirectsAndVerifySaml(
-          getIdpLoginUri(idp.id, 2),
-          maybeKeyTag.value,
-          mixpanelEnabled,
-          isFastLogin,
-          dispatch
-        ),
-      TE.chain(idpAuthSession),
+      TE.tryCatch(
+        () =>
+          openAuthenticationSession(
+            getIdpLoginUri(idp.id, 2),
+            URL_LOGIN_SCHEME
+          ),
+        error => {
+          setError({ code: ItWalletErrorTypes.IDENTIFICATION_ERROR });
+          return error as LoginUtilsError;
+        }
+      ),
       TE.map(setResult)
     )();
   };
