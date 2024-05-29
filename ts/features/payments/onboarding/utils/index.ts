00c23f30f391ff2c97b9c9dc71d9c0e1744ec168
Alessandro Izzo <alessandro.izzo998@gmail.com>
2024-05-29 11:13:12 +0200

    chore: code review suggestions applied

--- ts/features/payments/onboarding/utils/index.ts
+++ ts/features/payments/onboarding/utils/index.ts
@@ -6,10 +6,12 @@ export const ONBOARDING_FAQ_ENABLE_3DS = "https://io.italia.it/faq/#n3_3";
 export const ONBOARDING_CALLBACK_URL_SCHEMA = "iowallet";
 export const ONBOARDING_OUTCOME_PATH = "/wallets/outcomes";
 
-export const isMethodOnboardable = (method: PaymentMethodResponse) =>
-  (method.methodManagement === PaymentMethodManagementTypeEnum.ONBOARDABLE ||
-    method.methodManagement ===
-      PaymentMethodManagementTypeEnum.ONBOARDABLE_ONLY ||
-    method.methodManagement ===
-      PaymentMethodManagementTypeEnum.ONBOARDABLE_WITH_PAYMENT) &&
-  method.status === PaymentMethodStatusEnum.ENABLED;
+export const isMethodOnboardable = ({
+  methodManagement,
+  status
+}: PaymentMethodResponse) =>
+  [
+    PaymentMethodManagementTypeEnum.ONBOARDABLE,
+    PaymentMethodManagementTypeEnum.ONBOARDABLE_ONLY,
+    PaymentMethodManagementTypeEnum.ONBOARDABLE_WITH_PAYMENT
+  ].includes(methodManagement) && status === PaymentMethodStatusEnum.ENABLED;
