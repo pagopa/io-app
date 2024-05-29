7214c2a9d9562b1952ea36fbe4feb27d9c486939
ChrisMattew <cristian.matteu@gmail.com>
2024-05-29 16:35:42 +0200

    docs(Carousel): add JSDoc for this component

--- ts/components/Carousel.tsx
+++ ts/components/Carousel.tsx
@@ -20,10 +20,21 @@ const WINDOW_WIDTH = Dimensions.get("window").width;
 type Nullable<T> = T | null;
 
 type Props<T> = {
+  /**
+   * The array used to build the carousel items.
+   */
   data: Array<T>;
+  /**
+   * The component used to render the single item.
+   */
   Component: JSXElementConstructor<T>;
+  /**
+   * The horizontal space between carousel items.
+   */
   itemsGap?: IOAppMargin | 0;
-  screenActivePadding?: number;
+  /**
+   * The number of items displayed simultaneously.
+   */
   itemsPerTime?: number;
 } & Pick<
   FlatListProps<T>,
@@ -39,6 +50,10 @@ type Props<T> = {
   | "style"
 >;
 
+/**
+ * This component renders a carousel of elements from a given `data` entry and a `Component` of your choice.
+ * It allows you to define how many items to display at a time, how many space between the rendered elements and other important features inherited from the `FlatList` component.
+ */
 function CarouselComponent<T extends Record<string, unknown>>(
   {
     data,
