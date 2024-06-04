956abfdc37324d53e70cc59067e695de9790d62c
Mario Perrotta <mario.perrotta@pagopa.it>
2024-06-04 15:21:40 +0200

    Revert "feat: add status banner ios 14 version"
    This reverts commit 14d05fc3c42116d5f77b4f11d0650a5464550e06.

--- ts/features/messages/screens/legacy/MessageListScreen.tsx
+++ ts/features/messages/screens/legacy/MessageListScreen.tsx
@@ -3,8 +3,6 @@ import { RouteProp, useRoute } from "@react-navigation/native";
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
-import { Platform } from "react-native";
-import { Millisecond } from "@pagopa/ts-commons/lib/units";
 import { useMessageOpening } from "../../hooks/useMessageOpening";
 import MessagesInbox from "../../components/Home/legacy/MessagesInbox";
 import { upsertMessageStatusAttributes } from "../../store/actions";
@@ -19,16 +17,6 @@ import MessagesArchive from "../../components/Home/legacy/MessagesArchive";
 import messageListData, {
   MessageListCategory
 } from "../../types/messageListCategory";
-import SectionStatusComponent, {
-  InnerSectionStatus
-} from "../../../../components/SectionStatus";
-import { LevelEnum } from "../../../../../definitions/content/SectionStatus";
-import { unsupportedDeviceMoreInfoUrl } from "../../../../config";
-import I18n from "../../../../i18n";
-import {
-  setAccessibilityFocus,
-  useScreenReaderEnabled
-} from "../../../../utils/accessibility";
 
 export type MessagesHomeTabNavigationParams = Readonly<{
   category: MessageListCategory;
@@ -38,9 +26,6 @@ type MessageListScreenRouteProps = RouteProp<
   MessagesHomeTabParamsList,
   "MESSAGES_INBOX" | "MESSAGES_ARCHIVE"
 >;
-
-const isIOSVersionLessThan14 =
-  Platform.OS === "ios" && parseInt(Platform.Version as string, 10) < 14;
 
 // it's not managed in the selector
 // because pot.map generates a new instance
@@ -59,8 +44,6 @@ const MessageListScreen = () => {
   );
   const messages = getMessages(messagePagePot);
   const { present, bottomSheet } = useMessageOpening();
-
-  const isScreenReaderEnabled = useScreenReaderEnabled();
 
   const setArchived = (
     isArchived: boolean,
@@ -94,38 +77,9 @@ const MessageListScreen = () => {
       )
     );
 
-  const unsupportedDevicesStatusComponent = isIOSVersionLessThan14 && (
-    <InnerSectionStatus
-      sectionKey={"messages"}
-      sectionStatus={{
-        is_visible: true,
-        level: LevelEnum.warning,
-        web_url: {
-          "it-IT": unsupportedDeviceMoreInfoUrl,
-          "en-EN": unsupportedDeviceMoreInfoUrl
-        },
-        message: {
-          "it-IT": I18n.t("unsupportedDevice.warning"),
-          "en-EN": I18n.t("unsupportedDevice.warning")
-        }
-      }}
-    />
-  );
-
-  const statusComponent = (
-    <SectionStatusComponent
-      sectionKey={"messages"}
-      onSectionRef={v => {
-        setAccessibilityFocus(v, 100 as Millisecond);
-      }}
-    />
-  );
-
   return (
     <>
       {pipe(route.params.category, O.of, O.map(getContent), O.toUndefined)}
-      {isScreenReaderEnabled && statusComponent}
-      {!isScreenReaderEnabled && unsupportedDevicesStatusComponent}
       {bottomSheet}
     </>
   );
