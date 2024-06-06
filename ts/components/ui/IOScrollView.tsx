8500df000b45e4487257094a0c256cc2a9f002c8
Damiano Plebani <damiano.plebani@pagopa.it>
2024-06-06 16:08:55 +0200

    Remove some useless `useMemo` from `IOScrollView` too

--- ts/components/ui/IOScrollView.tsx
+++ ts/components/ui/IOScrollView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable functional/immutable-data */
 import {
   ButtonLink,
   ButtonOutline,
@@ -128,11 +129,6 @@ export const IOScrollView = ({
 }: IOScrollView) => {
   const theme = useIOTheme();
 
-  const type = actions?.type;
-  const primaryAction = actions?.primary;
-  const secondaryAction = actions?.secondary;
-  const tertiaryAction = actions?.tertiary;
-
   /* Navigation */
   const navigation = useNavigation();
 
@@ -151,20 +147,17 @@ export const IOScrollView = ({
   };
 
   const insets = useSafeAreaInsets();
-  const needSafeAreaMargin = useMemo(() => insets.bottom !== 0, [insets]);
-  const safeAreaMargin = useMemo(() => insets.bottom, [insets]);
+  const needSafeAreaMargin: boolean = insets.bottom !== 0;
+  const safeAreaMargin = insets.bottom;
 
   /* Check if the iPhone bottom handle is present.
      If not, or if you don't need safe area insets,
      add a default margin to prevent the button
      from sticking to the bottom. */
-  const bottomMargin: number = useMemo(
-    () =>
-      !needSafeAreaMargin || excludeSafeAreaMargins
-        ? IOVisualCostants.appMarginDefault
-        : safeAreaMargin,
-    [needSafeAreaMargin, excludeSafeAreaMargins, safeAreaMargin]
-  );
+  const bottomMargin: number =
+    !needSafeAreaMargin || excludeSafeAreaMargins
+      ? IOVisualCostants.appMarginDefault
+      : safeAreaMargin;
 
   /* GENERATE EASING GRADIENT
      Background color should be app main background
@@ -182,10 +175,8 @@ export const IOScrollView = ({
 
   /* When the secondary action is visible, add extra margin
      to avoid little space from iPhone bottom handle */
-  const extraBottomMargin: number = useMemo(
-    () => (secondaryAction && needSafeAreaMargin ? extraSafeAreaMargin : 0),
-    [needSafeAreaMargin, secondaryAction]
-  );
+  const extraBottomMargin: number =
+    actions?.secondary && needSafeAreaMargin ? extraSafeAreaMargin : 0;
 
   /* Safe background block. Cover at least 85% of the space
      to avoid glitchy elements underneath */
@@ -213,9 +204,7 @@ export const IOScrollView = ({
       const maxScrollHeight = contentSize.height - layoutMeasurement.height;
       const scrollPercentage = scrollPosition / maxScrollHeight;
 
-      // eslint-disable-next-line functional/immutable-data
       scrollPositionAbsolute.value = scrollPosition;
-      // eslint-disable-next-line functional/immutable-data
       scrollPositionPercentage.value = scrollPercentage;
     }
   );
@@ -314,8 +303,8 @@ export const IOScrollView = ({
             </Animated.View>
 
             {/* Safe background block. It's added because when you swipe up
-          quickly, the content below is visible for about 100ms. Without this
-          block, the content appears glitchy. */}
+                quickly, the content below is visible for about 100ms. Without this
+                block, the content appears glitchy. */}
             <View
               style={{
                 bottom: 0,
@@ -324,60 +313,65 @@ export const IOScrollView = ({
               }}
             />
           </Animated.View>
-
           <View
             style={styles.buttonContainer}
             onLayout={getActionBlockHeight}
             pointerEvents="box-none"
           >
-            {primaryAction && <ButtonSolid fullWidth {...primaryAction} />}
-
-            {type === "TwoButtons" && (
-              <View
-                style={{
-                  alignSelf: "center",
-                  marginBottom: extraBottomMargin
-                }}
-              >
-                <VSpacer size={spaceBetweenActionAndLink} />
-                {secondaryAction && (
-                  <ButtonLink
-                    color="primary"
-                    {...(secondaryAction as ComponentProps<typeof ButtonLink>)}
-                  />
-                )}
-              </View>
-            )}
-
-            {type === "ThreeButtons" && (
-              <Fragment>
-                {secondaryAction && (
-                  <Fragment>
-                    <VSpacer size={spaceBetweenActions} />
-                    <ButtonOutline
-                      fullWidth
-                      color="primary"
-                      {...secondaryAction}
-                    />
-                  </Fragment>
-                )}
-
-                {tertiaryAction && (
-                  <View
-                    style={{
-                      alignSelf: "center",
-                      marginBottom: extraBottomMargin
-                    }}
-                  >
-                    <VSpacer size={spaceBetweenActionAndLink} />
-                    <ButtonLink color="primary" {...tertiaryAction} />
-                  </View>
-                )}
-              </Fragment>
-            )}
+            {renderActionButtons(actions, extraBottomMargin)}
           </View>
         </View>
       )}
     </Fragment>
+  );
+};
+
+const renderActionButtons = (
+  actions: IOScrollViewActions,
+  extraBottomMargin: number
+) => {
+  const {
+    type,
+    primary: primaryAction,
+    secondary: secondaryAction,
+    tertiary: tertiaryAction
+  } = actions;
+
+  return (
+    <>
+      {primaryAction && <ButtonSolid fullWidth {...primaryAction} />}
+
+      {type === "TwoButtons" && (
+        <View
+          style={{
+            alignSelf: "center",
+            marginBottom: extraBottomMargin
+          }}
+        >
+          <VSpacer size={spaceBetweenActionAndLink} />
+          <ButtonLink
+            color="primary"
+            {...(secondaryAction as ComponentProps<typeof ButtonLink>)}
+          />
+        </View>
+      )}
+
+      {type === "ThreeButtons" && (
+        <Fragment>
+          <VSpacer size={spaceBetweenActions} />
+          <ButtonOutline fullWidth color="primary" {...secondaryAction} />
+
+          <View
+            style={{
+              alignSelf: "center",
+              marginBottom: extraBottomMargin
+            }}
+          >
+            <VSpacer size={spaceBetweenActionAndLink} />
+            <ButtonLink color="primary" {...tertiaryAction} />
+          </View>
+        </Fragment>
+      )}
+    </>
   );
 };
