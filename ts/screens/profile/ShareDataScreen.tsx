32f680c4405fdaf400989c23a81edbc95a3fba15
ChrisMattew <cristian.matteu@spindox.it>
2024-06-20 20:05:11 +0200

    feat(ShareDataScreen): integrate new DS

--- ts/screens/profile/ShareDataScreen.tsx
+++ ts/screens/profile/ShareDataScreen.tsx
@@ -1,21 +1,15 @@
-import {
-  BlockButtonProps,
-  IOToast,
-  FooterWithButtons
-} from "@pagopa/io-app-design-system";
-import * as React from "react";
+import { useIOToast } from "@pagopa/io-app-design-system";
+import React, { useCallback } from "react";
 import { SafeAreaView, View } from "react-native";
-import { connect } from "react-redux";
-import { Dispatch } from "redux";
 import { IOStyles } from "../../components/core/variables/IOStyles";
-import { RNavScreenWithLargeHeader } from "../../components/ui/RNavScreenWithLargeHeader";
 import I18n from "../../i18n";
 import { setMixpanelEnabled } from "../../store/actions/mixpanel";
-import { isMixpanelEnabled } from "../../store/reducers/persistedPreferences";
-import { GlobalState } from "../../store/reducers/types";
+import { isMixpanelEnabled as isMixpanelEnabledSelector } from "../../store/reducers/persistedPreferences";
 import { getFlowType } from "../../utils/analytics";
 import { useOnFirstRender } from "../../utils/hooks/useOnFirstRender";
-import { useIOStore } from "../../store/hooks";
+import { useIODispatch, useIOSelector, useIOStore } from "../../store/hooks";
+import { IOScrollViewWithLargeHeader } from "../../components/ui/IOScrollViewWithLargeHeader";
+import { IOScrollViewActions } from "../../components/ui/IOScrollView";
 import { trackMixpanelScreen } from "./analytics";
 import {
   TrackingInfo,
@@ -26,57 +20,53 @@ import {
 import { useConfirmOptOutBottomSheet } from "./components/OptOutBottomSheet";
 import { ShareDataComponent } from "./components/ShareDataComponent";
 
-type Props = ReturnType<typeof mapDispatchToProps> &
-  ReturnType<typeof mapStateToProps>;
-
-const ShareDataScreen = (props: Props): React.ReactElement => {
+const ShareDataScreen = () => {
   const store = useIOStore();
+  const dispatch = useIODispatch();
+  const isMixpanelEnabled = useIOSelector(isMixpanelEnabledSelector) ?? true;
+  const toast = useIOToast();
+
   const { present, bottomSheet } = useConfirmOptOutBottomSheet(() => {
     const flow = getFlowType(false, false);
     trackMixpanelDeclined(flow);
     trackMixpanelSetEnabled(false, flow, store.getState()).finally(() => {
-      props.setMixpanelEnabled(false);
+      dispatch(setMixpanelEnabled(false));
     });
 
-    IOToast.success(
-      I18n.t("profile.main.privacy.shareData.screen.confirmToast")
-    );
+    toast.success(I18n.t("profile.main.privacy.shareData.screen.confirmToast"));
   });
-  const isMixpanelEnabled = props.isMixpanelEnabled ?? true;
+
+  const dontShareDataLabel = I18n.t(
+    "profile.main.privacy.shareData.screen.cta.dontShareData"
+  );
+  const shareDataLabel = I18n.t(
+    "profile.main.privacy.shareData.screen.cta.shareData"
+  );
 
   useOnFirstRender(() => {
     trackMixpanelScreen(getFlowType(false, false));
   });
 
-  const handleTrackingAction = React.useCallback((info: TrackingInfo) => {
+  const handleTrackingAction = useCallback((info: TrackingInfo) => {
     const flow = getFlowType(false, false);
     trackMixPanelTrackingInfo(flow, info);
   }, []);
 
-  const buttonProps: BlockButtonProps = isMixpanelEnabled
-    ? {
-        type: "Outline",
-        buttonProps: {
-          color: "primary",
-          accessibilityLabel: I18n.t(
-            "profile.main.privacy.shareData.screen.cta.dontShareData"
-          ),
+  const buttonProps: IOScrollViewActions = {
+    type: "SingleButton",
+    primary: isMixpanelEnabled
+      ? {
+          accessibilityLabel: dontShareDataLabel,
           onPress: present,
-          label: I18n.t(
-            "profile.main.privacy.shareData.screen.cta.dontShareData"
-          )
+          label: dontShareDataLabel
         }
-      }
-    : {
-        type: "Solid",
-        buttonProps: {
-          color: "primary",
-          accessibilityLabel: I18n.t(
-            "profile.main.privacy.shareData.screen.cta.dontShareData"
-          ),
+      : {
+          label: shareDataLabel,
+          accessibilityLabel: shareDataLabel,
+          testID: "share-data-confirm-button",
           onPress: () => {
             // Before tracking any event, we need to enable mixpanel
-            props.setMixpanelEnabled(true);
+            dispatch(setMixpanelEnabled(true));
             // We wait some time to allow mixpanel to be enabled
             // before tracking the event
             setTimeout(() => {
@@ -86,25 +76,21 @@ const ShareDataScreen = (props: Props): React.ReactElement => {
                 store.getState()
               );
             }, 1000);
-            IOToast.success(
+            toast.success(
               I18n.t("profile.main.privacy.shareData.screen.confirmToast")
             );
-          },
-          label: I18n.t("profile.main.privacy.shareData.screen.cta.shareData"),
-          testID: "share-data-confirm-button"
+          }
         }
-      };
+  };
 
   return (
-    <RNavScreenWithLargeHeader
+    <IOScrollViewWithLargeHeader
       title={{
         label: I18n.t("profile.main.privacy.shareData.screen.title"),
         testID: "share-data-component-title"
       }}
       description={I18n.t("profile.main.privacy.shareData.screen.description")}
-      fixedBottomSlot={
-        <FooterWithButtons type="SingleButton" primary={buttonProps} />
-      }
+      actions={buttonProps}
     >
       <SafeAreaView style={IOStyles.flex}>
         <View style={[IOStyles.horizontalContentPadding, { flexGrow: 1 }]}>
@@ -113,16 +99,8 @@ const ShareDataScreen = (props: Props): React.ReactElement => {
 
         {bottomSheet}
       </SafeAreaView>
-    </RNavScreenWithLargeHeader>
+    </IOScrollViewWithLargeHeader>
   );
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  setMixpanelEnabled: (newValue: boolean) =>
-    dispatch(setMixpanelEnabled(newValue))
-});
-const mapStateToProps = (state: GlobalState) => ({
-  isMixpanelEnabled: isMixpanelEnabled(state)
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ShareDataScreen);
+export default ShareDataScreen;
