46c88f22f8ae60834a3d78aa0680af83bc6e1b1e
ChrisMattew <cristian.matteu@spindox.it>
2024-06-24 17:03:01 +0200

    feat(ShareDataFeaturesInfos): replace deprecated useLegacyIOBottomSheetModal with useIOBottomSheetAutoresizableModal

--- ts/screens/profile/components/ShareDataComponent/ShareDataFeatureInfos.tsx
+++ ts/screens/profile/components/ShareDataComponent/ShareDataFeatureInfos.tsx
@@ -7,9 +7,10 @@ import {
 import React, { useCallback, useMemo, useRef } from "react";
 import { View } from "react-native";
 import { Millisecond } from "@pagopa/ts-commons/lib/units";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import I18n from "../../../../i18n";
 import LegacyMarkdown from "../../../../components/ui/Markdown/LegacyMarkdown";
-import { useLegacyIOBottomSheetModal } from "../../../../utils/hooks/bottomSheet";
+import { useIOBottomSheetAutoresizableModal } from "../../../../utils/hooks/bottomSheet";
 import { openWebUrl } from "../../../../utils/url";
 import { ioSuppliersUrl } from "../../../../urls";
 import { TrackingInfo } from "../../analytics/mixpanel/mixpanelAnalytics";
@@ -22,44 +23,47 @@ export type FeatureProps = {
 const shareDataSecurityMoreLink =
   "https://www.pagopa.it/it/politiche-sulla-sicurezza-delle-informazioni-e-sulla-qualita/";
 
-const MarkdownBody = () => (
-  <View
-    accessible
-    // Necessary because `LegacyMarkdown` component truncates the content into separate blocks.
-    accessibilityLabel={
-      I18n.t("profile.main.privacy.shareData.whyBottomSheet.body").replace(
-        /\*/g,
-        ""
-      ) // It removes all '*' characters associated with Markdown syntax.
-    }
-  >
-    <VSpacer size={16} />
+const MarkdownBody = () => {
+  const { bottom } = useSafeAreaInsets();
+
+  return (
     <View
-      accessible={false}
-      accessibilityElementsHidden
-      importantForAccessibility="no-hide-descendants"
+      accessible
+      // Necessary because `LegacyMarkdown` component truncates the content into separate blocks.
+      accessibilityLabel={
+        I18n.t("profile.main.privacy.shareData.whyBottomSheet.body").replace(
+          /\*/g,
+          ""
+        ) // It removes all '*' characters associated with Markdown syntax.
+      }
     >
-      <LegacyMarkdown>
-        {I18n.t("profile.main.privacy.shareData.whyBottomSheet.body")}
-      </LegacyMarkdown>
+      <VSpacer size={16} />
+      <View
+        accessible={false}
+        accessibilityElementsHidden
+        importantForAccessibility="no-hide-descendants"
+      >
+        <LegacyMarkdown>
+          {I18n.t("profile.main.privacy.shareData.whyBottomSheet.body")}
+        </LegacyMarkdown>
+      </View>
+      {bottom === 0 && <VSpacer size={16} />}
     </View>
-  </View>
-);
+  );
+};
 
 const AnalyticsFeatureInfo = ({ trackAction }: FeatureProps) => {
   const bodyRef = useRef<View>(null);
-  const { present, bottomSheet } = useLegacyIOBottomSheetModal(
-    <MarkdownBody />,
-    I18n.t("profile.main.privacy.shareData.whyBottomSheet.title"),
-    250,
-    undefined,
-    () => {
+  const { present, bottomSheet } = useIOBottomSheetAutoresizableModal({
+    title: I18n.t("profile.main.privacy.shareData.whyBottomSheet.title"),
+    component: <MarkdownBody />,
+    onDismiss: () => {
       // When the bottom sheet is dismissed, the accessibility focus shifts to the body component of `FeatureInfo`.
       // This workaround is implemented to maintain semantic order and prevent disruptions.
       // Ideally, the accessibility focus should return to the element that triggered the bottom sheet's display. However, implementing this solution currently isn't feasible.
       setAccessibilityFocus(bodyRef, 300 as Millisecond);
     }
-  );
+  });
 
   const analyticsBody = useMemo(
     () => (
