5538ed076b58cc7234f150aed6a190b43d206cc6
Cristiano Tofani <cri.tofani@gmail.com>
2024-06-27 15:10:30 +0200

    chore: [IOPLT-515] Adds the saga to handle api requests on trial system (#5777)
    > [!warning]
    > This PR depends on #5776
    
    ## Short description
    This PR adds the saga to handle the requests on Trial System workflow
    
    ## List of changes proposed in this pull request
    - Create `watchTrialSystemSaga`
    - Fork the new saga to the startupSaga
    
    ## How to test
    Check if the API call are properly dispatched
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

---
+++ ts/features/trialSystem/store/sagas/watchTrialSystemSaga.ts
@@ -0,0 +1,101 @@
+import * as E from "fp-ts/lib/Either";
+import { SagaIterator } from "redux-saga";
+import { call, put, takeLatest } from "typed-redux-saga/macro";
+import { ActionType } from "typesafe-actions";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import { SessionToken } from "../../../../types/SessionToken";
+import { TrialSystemClient, createTrialSystemClient } from "../../api/client";
+import { apiUrlPrefix } from "../../../../config";
+import {
+  trialSystemActivationStatus,
+  trialSystemActivationStatusUpsert
+} from "../actions";
+import { getError } from "../../../../utils/errors";
+
+function* handleTrialSystemActivationStatusUpsert(
+  upsertTrialSystemActivationStatus: TrialSystemClient["createSubscription"],
+  action: ActionType<typeof trialSystemActivationStatusUpsert.request>
+) {
+  try {
+    const result = yield* call(upsertTrialSystemActivationStatus, {
+      trialId: action.payload
+    });
+
+    if (E.isLeft(result)) {
+      yield* put(
+        trialSystemActivationStatusUpsert.failure({
+          trialId: action.payload,
+          error: new Error(readableReport(result.left))
+        })
+      );
+    } else if (result.right.status === 201) {
+      yield* put(trialSystemActivationStatusUpsert.success(result.right.value));
+    } else {
+      yield* put(
+        trialSystemActivationStatusUpsert.failure({
+          trialId: action.payload,
+          error: new Error(`response status ${result.right.status}`)
+        })
+      );
+    }
+  } catch (e) {
+    yield* put(
+      trialSystemActivationStatusUpsert.failure({
+        trialId: action.payload,
+        error: getError(e)
+      })
+    );
+  }
+}
+
+function* handleTrialSystemActivationStatus(
+  trialSystemActivationStatusRequest: TrialSystemClient["getSubscription"],
+  action: ActionType<typeof trialSystemActivationStatus.request>
+) {
+  try {
+    const result = yield* call(trialSystemActivationStatusRequest, {
+      trialId: action.payload
+    });
+
+    if (E.isLeft(result)) {
+      yield* put(
+        trialSystemActivationStatus.failure({
+          trialId: action.payload,
+          error: new Error(readableReport(result.left))
+        })
+      );
+    } else if (result.right.status === 200) {
+      yield* put(trialSystemActivationStatus.success(result.right.value));
+    } else {
+      yield* put(
+        trialSystemActivationStatus.failure({
+          trialId: action.payload,
+          error: new Error(`response status ${result.right.status}`)
+        })
+      );
+    }
+  } catch (e) {
+    yield* put(
+      trialSystemActivationStatus.failure({
+        trialId: action.payload,
+        error: getError(e)
+      })
+    );
+  }
+}
+
+export function* watchTrialSystemSaga(bearerToken: SessionToken): SagaIterator {
+  const trialSystemClient = createTrialSystemClient(apiUrlPrefix, bearerToken);
+
+  yield* takeLatest(
+    trialSystemActivationStatusUpsert.request,
+    handleTrialSystemActivationStatusUpsert,
+    trialSystemClient.createSubscription
+  );
+
+  yield* takeLatest(
+    trialSystemActivationStatus.request,
+    handleTrialSystemActivationStatus,
+    trialSystemClient.getSubscription
+  );
+}
