469e8729af2ba7a3fd09bfcf86d7ffd7584a465b
Cristiano Tofani <cri.tofani@gmail.com>
2024-06-27 15:45:39 +0200

    chore: [IOPLT-516] Add CTA to enroll to a trial on Profile section (#5794)
    > [!warning]
    > This PR depends on #5777 
    
    ## Short description
    This PR aims to add a new screen with CTA to handle the loading status
    of a specific trial under profile section
    
    ## List of changes proposed in this pull request
    - Adds the TrialSystemPlayground under Playground section of
    Profile/DeveloperSection
    
    ## How to test
    Try to start the subscription by using this PR code on dev-server
    pagopa/io-dev-api-server#378
    
    ---------
    
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>

--- ts/features/trialSystem/store/reducers/index.ts
+++ ts/features/trialSystem/store/reducers/index.ts
@@ -1,5 +1,6 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import { getType } from "typesafe-actions";
+import { pipe } from "fp-ts/lib/function";
 import { TrialId } from "../../../../../definitions/trial_systwem/TrialId";
 import {
   SubscriptionState,
@@ -8,8 +9,10 @@ import {
 import { Action } from "../../../../store/actions/types";
 import {
   trialSystemActivationStatus,
+  trialSystemActivationStatusReset,
   trialSystemActivationStatusUpsert
 } from "../actions";
+import { GlobalState } from "../../../../store/reducers/types";
 
 export type TrialSystemState = Record<
   TrialId,
@@ -39,6 +42,11 @@ export const trialSystemActivationStatusReducer = (
         ...state,
         [action.payload.trialId]: pot.some(action.payload.state)
       };
+    case getType(trialSystemActivationStatusReset):
+      return {
+        ...state,
+        [action.payload]: pot.none
+      };
     case getType(trialSystemActivationStatusUpsert.failure):
     case getType(trialSystemActivationStatus.failure):
       return {
@@ -63,7 +71,38 @@ export const trialSystemActivationStatusReducer = (
   }
 };
 
-const isStateActive = (status: pot.Pot<SubscriptionState, Error>) =>
+const isStateActive = (status: pot.Pot<SubscriptionState, Error> | undefined) =>
+  status &&
   pot.isSome(status) &&
   (status.value === SubscriptionStateEnum.ACTIVE ||
     status.value === SubscriptionStateEnum.SUBSCRIBED);
+
+export const trialSystemActivationStatusSelector = (
+  state: GlobalState
+): TrialSystemState => state.trialSystem;
+
+export const trialStatusSelector = (id: TrialId) => (state: GlobalState) =>
+  pipe(
+    state,
+    trialSystemActivationStatusSelector,
+    status => status[id] ?? pot.none,
+    pot.toUndefined
+  );
+
+export const isLoadingTrialStatusSelector =
+  (id: TrialId) => (state: GlobalState) =>
+    pipe(
+      state,
+      trialSystemActivationStatusSelector,
+      status => status[id] ?? pot.none,
+      pot.isLoading
+    );
+
+export const isUpdatingTrialStatusSelector =
+  (id: TrialId) => (state: GlobalState) =>
+    pipe(
+      state,
+      trialSystemActivationStatusSelector,
+      status => status[id] ?? pot.none,
+      pot.isUpdating
+    );
--- ts/features/trialSystem/store/sagas/watchTrialSystemSaga.ts
+++ ts/features/trialSystem/store/sagas/watchTrialSystemSaga.ts
@@ -3,14 +3,17 @@ import { SagaIterator } from "redux-saga";
 import { call, put, takeLatest } from "typed-redux-saga/macro";
 import { ActionType } from "typesafe-actions";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import { IOToast } from "@pagopa/io-app-design-system";
 import { SessionToken } from "../../../../types/SessionToken";
 import { TrialSystemClient, createTrialSystemClient } from "../../api/client";
 import { apiUrlPrefix } from "../../../../config";
 import {
   trialSystemActivationStatus,
+  trialSystemActivationStatusReset,
   trialSystemActivationStatusUpsert
 } from "../actions";
 import { getError } from "../../../../utils/errors";
+import I18n from "../../../../i18n";
 
 function* handleTrialSystemActivationStatusUpsert(
   upsertTrialSystemActivationStatus: TrialSystemClient["createSubscription"],
@@ -30,6 +33,7 @@ function* handleTrialSystemActivationStatusUpsert(
       );
     } else if (result.right.status === 201) {
       yield* put(trialSystemActivationStatusUpsert.success(result.right.value));
+      IOToast.success(I18n.t("features.trialSystem.toast.subscribed"));
     } else {
       yield* put(
         trialSystemActivationStatusUpsert.failure({
@@ -37,6 +41,7 @@ function* handleTrialSystemActivationStatusUpsert(
           error: new Error(`response status ${result.right.status}`)
         })
       );
+      IOToast.error(I18n.t("global.genericError"));
     }
   } catch (e) {
     yield* put(
@@ -45,6 +50,7 @@ function* handleTrialSystemActivationStatusUpsert(
         error: getError(e)
       })
     );
+    IOToast.error(I18n.t("global.genericError"));
   }
 }
 
@@ -64,8 +70,17 @@ function* handleTrialSystemActivationStatus(
           error: new Error(readableReport(result.left))
         })
       );
-    } else if (result.right.status === 200) {
+      return;
+    }
+
+    if (result.right.status === 200) {
       yield* put(trialSystemActivationStatus.success(result.right.value));
+      return;
+    }
+
+    if (result.right.status === 404) {
+      yield* put(trialSystemActivationStatusReset(action.payload));
+      return;
     } else {
       yield* put(
         trialSystemActivationStatus.failure({
