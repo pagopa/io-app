3018924c12abbc64ea55f4d3a44b439be2bb1643
Alessandro Izzo <alessandro.izzo998@gmail.com>
2024-06-28 11:39:02 +0200

    chore: removed unused payment history attributes

--- ts/features/payments/history/types/index.ts
+++ ts/features/payments/history/types/index.ts
@@ -1,33 +1,19 @@
 import { NewTransactionResponse } from "../../../../../definitions/pagopa/ecommerce/NewTransactionResponse";
 import { PaymentRequestsGetResponse } from "../../../../../definitions/pagopa/ecommerce/PaymentRequestsGetResponse";
 import { RptId } from "../../../../../definitions/pagopa/ecommerce/RptId";
-import { WalletInfo } from "../../../../../definitions/pagopa/walletv3/WalletInfo";
-import { WalletInfo as EcommerceWalletInfo } from "../../../../../definitions/pagopa/ecommerce/WalletInfo";
 import { PaymentStartOrigin } from "../../checkout/types";
 import { WalletPaymentOutcomeEnum } from "../../checkout/types/PaymentOutcomeEnum";
 import { WalletPaymentFailure } from "../../checkout/types/WalletPaymentFailure";
-import { PaymentAnalyticsSelectedPspFlag } from "../../checkout/types/PaymentAnalytics";
-import { Bundle } from "../../../../../definitions/pagopa/ecommerce/Bundle";
 
 export type PaymentHistory = {
   startOrigin?: PaymentStartOrigin;
   rptId?: RptId;
   startedAt?: Date;
   lookupId?: string;
-  savedPaymentMethods?: ReadonlyArray<WalletInfo | EcommerceWalletInfo>;
-  savedPaymentMethodsUnavailable?: ReadonlyArray<
-    WalletInfo | EcommerceWalletInfo
-  >;
-  pspList?: ReadonlyArray<Bundle>;
-  selectedPsp?: Bundle;
-  selectedPspFlag?: PaymentAnalyticsSelectedPspFlag;
-  selectedPaymentMethod?: string;
   verifiedData?: PaymentRequestsGetResponse;
-  formattedAmount?: string;
   transaction?: NewTransactionResponse;
   outcome?: WalletPaymentOutcomeEnum;
   failure?: WalletPaymentFailure;
   success?: true;
   attempt?: number;
-  serviceName?: string;
 };
