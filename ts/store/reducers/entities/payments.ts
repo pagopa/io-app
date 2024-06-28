56bc826cd741523a436b2ab2429c28527af48a0d
Alessandro Izzo <alessandro.izzo998@gmail.com>
2024-06-28 15:29:59 +0200

    chore: [IOBP-704] Add payed rptid into state entities on payment success (#5914)
    ## Short description
    This PR adds the rptId when a payment is successfully made within the
    state.entities to display the "Paid" badge in the message list.
    
    ## List of changes proposed in this pull request
    - Added an additional orchestration action `paymentCompletedSuccess` to
    the new payment flow checkout;
    - Dispatched the above action when showing the successful outcome;
    - Added the action handler into `ts/store/reducers/entities/payments.ts`
    file
    
    ## How to test
    - Enable the new wallet landing page FF;
    - Start a payment from the new payment landing;
    - When you reach the success outcome, you should be able to see the
    dispatched action with the rptId paid.
    
    ## Preview
    
    https://github.com/pagopa/io-app/assets/34343582/ebf23085-8549-49b1-9987-5c7de3a04c75
    
    ---------
    
    Co-authored-by: Andrea <andrea.piai@pagopa.it>

--- ts/store/reducers/entities/payments.ts
+++ ts/store/reducers/entities/payments.ts
@@ -6,7 +6,8 @@
 import { getType } from "typesafe-actions";
 import { RptIdFromString } from "@pagopa/io-pagopa-commons/lib/pagopa";
 import { Action } from "../../actions/types";
-import { paymentCompletedSuccess } from "../../actions/wallet/payment";
+import { paymentCompletedSuccess as legacyPaymentCompletedSuccess } from "../../actions/wallet/payment";
+import { paymentCompletedSuccess } from "../../../features/payments/checkout/store/actions/orchestration";
 import { GlobalState } from "../types";
 import { differentProfileLoggedIn } from "../../actions/crossSessions";
 
@@ -36,7 +37,7 @@ export const paymentByRptIdReducer = (
   action: Action
 ): PaymentByRptIdState => {
   switch (action.type) {
-    case getType(paymentCompletedSuccess):
+    case getType(legacyPaymentCompletedSuccess):
       // Use the ID as object key
       const rptIdString: string = RptIdFromString.encode(action.payload.rptId);
       return {
@@ -50,6 +51,16 @@ export const paymentByRptIdReducer = (
             : {
                 kind: "DUPLICATED"
               }
+      };
+    // New payment flow completed
+    case getType(paymentCompletedSuccess):
+      return {
+        ...state,
+        [action.payload.rptId]: {
+          kind: action.payload.kind,
+          // The transaction ID is not available with the PM, it will be added when migrated to the NPG that will support it
+          transactionId: undefined
+        }
       };
     // clear state if the current profile is different from the previous one
     case getType(differentProfileLoggedIn):
