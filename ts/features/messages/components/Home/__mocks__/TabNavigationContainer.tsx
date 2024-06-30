b54fd72d13d3ababf69ffa40d16563c042e5596f
Andrea Piai <andrea.piai@pagopa.it>
2024-06-30 23:25:27 +0200

    Tests

---
+++ ts/features/messages/components/Home/__mocks__/TabNavigationContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+export const TabNavigationContainer = () => (
+  <View testID="mock_tab_navigation_container">
+    This is a mock for TabNavigationContainer
+  </View>
+);
