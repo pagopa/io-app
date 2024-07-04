6487de3a94da168d7ffb571e78446e3f89df7a8d
Federico Mastrini <federicomastrini93@gmail.com>
2024-07-04 18:10:29 +0200

    chore: remove hook

--- ts/features/itwallet/playgrounds/components/ItwTrialSystemSection.tsx
+++ ts/features/itwallet/playgrounds/components/ItwTrialSystemSection.tsx
@@ -1,23 +1,49 @@
 import {
   ButtonSolid,
   ListItemHeader,
-  ListItemInfo
+  ListItemInfo,
+  VSpacer
 } from "@pagopa/io-app-design-system";
 import { constNull } from "fp-ts/lib/function";
 import * as React from "react";
 import { SubscriptionStateEnum } from "../../../../../definitions/trial_systwem/SubscriptionState";
 import I18n from "../../../../i18n";
-import { useTrialSystemStatus } from "../../../trialSystem/hooks/useTrialSystemStatus";
+import { useIODispatch, useIOSelector } from "../../../../store/hooks";
+import {
+  trialSystemActivationStatus,
+  trialSystemActivationStatusReset,
+  trialSystemActivationStatusUpsert
+} from "../../../trialSystem/store/actions";
+import {
+  isLoadingTrialStatusSelector,
+  isUpdatingTrialStatusSelector,
+  trialStatusSelector
+} from "../../../trialSystem/store/reducers";
 import { ITW_TRIAL_ID } from "../../common/utils/itwTrialUtils";
 
 export const ItwTrialSystemSection = () => {
-  const { status, isLoading, isUpdating, subscribe } =
-    useTrialSystemStatus(ITW_TRIAL_ID);
+  const dispatch = useIODispatch();
+  const status = useIOSelector(trialStatusSelector(ITW_TRIAL_ID));
+  const isLoading = useIOSelector(isLoadingTrialStatusSelector(ITW_TRIAL_ID));
+  const isUpdating = useIOSelector(isUpdatingTrialStatusSelector(ITW_TRIAL_ID));
+
+  const subscribe = () => {
+    dispatch(trialSystemActivationStatusUpsert.request(ITW_TRIAL_ID));
+  };
+
+  const refresh = () => {
+    dispatch(trialSystemActivationStatus.request(ITW_TRIAL_ID));
+  };
+
+  const reset = () => {
+    dispatch(trialSystemActivationStatusReset(ITW_TRIAL_ID));
+  };
 
   return (
     <>
-      <ListItemHeader label="F&F Trial" />
+      <ListItemHeader label="Trial" />
       <ListItemInfo label="Current status" value={status ?? "Not present"} />
+      <VSpacer size={8} />
       <>
         {status === undefined ||
         status === SubscriptionStateEnum.UNSUBSCRIBED ? (
@@ -38,6 +64,22 @@ export const ItwTrialSystemSection = () => {
           />
         )}
       </>
+      <VSpacer size={8} />
+      <ButtonSolid
+        loading={isUpdating}
+        disabled={isLoading}
+        fullWidth={true}
+        label={"Refresh"}
+        onPress={refresh}
+      />
+      <VSpacer size={8} />
+      <ButtonSolid
+        loading={isUpdating}
+        disabled={isLoading}
+        fullWidth={true}
+        label={"Reset"}
+        onPress={reset}
+      />
     </>
   );
 };
