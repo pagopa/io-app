5b79f536eb56350f24641252286addbbffa3bdf7
Cristiano Tofani <cri.tofani@gmail.com>
2024-07-09 16:58:19 +0200

    Set API level to 34 and upgrade vision-camera

--- ts/features/barcode/hooks/useWindeAngleCameraDevice.tsx
+++ ts/features/barcode/hooks/useWindeAngleCameraDevice.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { Camera, CameraDevice } from "react-native-vision-camera";
+import {
+  Camera,
+  CameraDevice,
+  useCameraDevice
+} from "react-native-vision-camera";
 
 /**
  * This function returns the best match of back camera, excuding the ultra-wide-camera
@@ -12,7 +16,8 @@ const getWideAngleCameraOrDefault = (
   const defaultBackDevice = devices.find(d => d.position === "back");
   const wideAngleDevice = devices.find(
     d =>
-      d.position !== "front" && !d.devices.includes("ultra-wide-angle-camera")
+      d.position !== "front" &&
+      !d.physicalDevices.includes("ultra-wide-angle-camera")
   );
   return wideAngleDevice || defaultBackDevice;
 };
@@ -22,19 +27,16 @@ const getWideAngleCameraOrDefault = (
  * wide-angle-camera format. If not present, returns the default one as fallback
  * FIXME: remove this when react-native-vision-camera is updated to 3.x
  */
-const useWideAngleCameraDevice = (): CameraDevice | undefined => {
-  const [device, setDevice] = React.useState<CameraDevice>();
+const useWideAngleCameraDevice = (): CameraDevice | undefined =>
+  useCameraDevice("back", {
+    physicalDevices: ["ultra-wide-angle-camera"]
+  });
+// const [device, setDevice] = React.useState<CameraDevice>();
 
-  React.useEffect(() => {
-    Camera.getAvailableCameraDevices()
-      .then(devices => {
-        const bestMatchDevice = getWideAngleCameraOrDefault(devices);
-        setDevice(bestMatchDevice);
-      })
-      .catch(() => setDevice(undefined));
-  }, []);
-
-  return device;
-};
+// React.useEffect(() => {
+//   const devices = Camera.getAvailableCameraDevices();
+//   const bestMatchDevice = getWideAngleCameraOrDefault(devices);
+//   setDevice(bestMatchDevice);
+// }, []);
 
 export { useWideAngleCameraDevice };
