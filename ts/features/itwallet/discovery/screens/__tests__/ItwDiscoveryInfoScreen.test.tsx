afdfedb3d364a5bb0c906546cbfaf60135cacc95
Federico Mastrini <federicomastrini93@gmail.com>
2024-07-10 12:56:05 +0200

    chore: fix tests

--- ts/features/itwallet/discovery/screens/__tests__/ItwDiscoveryInfoScreen.test.tsx
+++ ts/features/itwallet/discovery/screens/__tests__/ItwDiscoveryInfoScreen.test.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 import { createStore } from "redux";
-import { ItwDiscoveryInfoScreen } from "../ItwDiscoveryInfoScreen";
-import { ITW_ROUTES } from "../../../navigation/routes";
-import { appReducer } from "../../../../../store/reducers";
 import { applicationChangeState } from "../../../../../store/actions/application";
-import { renderScreenWithNavigationStoreContext } from "../../../../../utils/testWrapper";
+import { appReducer } from "../../../../../store/reducers";
 import { GlobalState } from "../../../../../store/reducers/types";
-import {
-  ItwEidIssuanceMachineContext,
-  ItwCredentialIssuanceMachineContext
-} from "../../../machine/provider";
+import { renderScreenWithNavigationStoreContext } from "../../../../../utils/testWrapper";
+import { itwEidIssuanceMachine } from "../../../machine/eid/machine";
+import { ItwEidIssuanceMachineContext } from "../../../machine/provider";
+import { ITW_ROUTES } from "../../../navigation/routes";
+import { ItwDiscoveryInfoScreen } from "../ItwDiscoveryInfoScreen";
 
 describe("Test ItwDiscoveryInfo screen", () => {
   it("it should render the screen correctly", () => {
@@ -21,12 +19,16 @@ describe("Test ItwDiscoveryInfo screen", () => {
 const renderComponent = () => {
   const globalState = appReducer(undefined, applicationChangeState("active"));
 
+  const logic = itwEidIssuanceMachine.provide({
+    actions: {
+      navigateToTosScreen: () => undefined
+    }
+  });
+
   return renderScreenWithNavigationStoreContext<GlobalState>(
     () => (
-      <ItwEidIssuanceMachineContext.Provider>
-        <ItwCredentialIssuanceMachineContext.Provider>
-          <ItwDiscoveryInfoScreen />
-        </ItwCredentialIssuanceMachineContext.Provider>
+      <ItwEidIssuanceMachineContext.Provider logic={logic}>
+        <ItwDiscoveryInfoScreen />
       </ItwEidIssuanceMachineContext.Provider>
     ),
     ITW_ROUTES.DISCOVERY.INFO,
