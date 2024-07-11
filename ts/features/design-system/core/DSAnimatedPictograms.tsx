f333207e7c3140336c73bb408bae31fbbff0777d
Damiano Plebani <damiano.plebani@pagopa.it>
2024-07-11 15:28:23 +0200

    Add all the new light mode animated pictograms

--- ts/features/design-system/core/DSAnimatedPictograms.tsx
+++ ts/features/design-system/core/DSAnimatedPictograms.tsx
@@ -1,52 +1,127 @@
+import {
+  ContentWrapper,
+  IOColors,
+  IOVisualCostants,
+  RadioGroup,
+  hexToRgba,
+  useIOTheme
+} from "@pagopa/io-app-design-system";
 import * as React from "react";
-
+import { useCallback, useState } from "react";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import LinearGradient from "react-native-linear-gradient";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Skottie } from "react-native-skottie";
 
 /* Animated Pictograms */
-import ScanCardiOS from "../../../../assets/animated-pictograms/ScanCardiOS.json";
+import empty from "../../../../assets/animated-pictograms/Empty.json";
+import error from "../../../../assets/animated-pictograms/Error.json";
+import fatalError from "../../../../assets/animated-pictograms/FatalError.json";
+import lock from "../../../../assets/animated-pictograms/Lock.json";
+import scanCardAndroid from "../../../../assets/animated-pictograms/ScanCardAndroid.json";
+import scanCardiOS from "../../../../assets/animated-pictograms/ScanCardiOS.json";
+import search from "../../../../assets/animated-pictograms/Search.json";
+import success from "../../../../assets/animated-pictograms/Success.json";
+import umbrella from "../../../../assets/animated-pictograms/Umbrella.json";
+import warning from "../../../../assets/animated-pictograms/Warning.json";
+import welcome from "../../../../assets/animated-pictograms/Welcome.json";
 
 export const DSAnimatedPictograms = () => {
   const insets = useSafeAreaInsets();
-  // const theme = useIOTheme();
+  const theme = useIOTheme();
 
-  const heroHeight: number = 350 + insets.top;
+  const scrollGradientHeight: number = 32;
+  const pictogramSize: number = 200;
+
+  const pictogramsRefs = [
+    { name: welcome, label: "Welcome", loop: false },
+    { name: empty, label: "Empty", loop: true },
+    { name: scanCardiOS, label: "Scan Card (iOS)", loop: true },
+    { name: scanCardAndroid, label: "Scan Card (Android)", loop: true },
+    { name: umbrella, label: "Umbrella", loop: true },
+    { name: error, label: "Error", loop: true },
+    { name: fatalError, label: "Fatal Error", loop: true },
+    { name: lock, label: "Lock", loop: true },
+    { name: search, label: "Search", loop: true },
+    { name: success, label: "Success", loop: true },
+    { name: warning, label: "Warning", loop: true }
+  ];
+
+  const renderedPictogramsRefs: Array<{
+    value: string;
+    id: string;
+  }> = pictogramsRefs.map(item => ({
+    value: item.label,
+    id: item.label
+  }));
+
+  const [selectedPictogram, setSelectedPictogram] = useState(
+    pictogramsRefs[0].label
+  );
+
+  const onPictogramSelected = useCallback(
+    (item: string) => {
+      setSelectedPictogram(item);
+    },
+    [setSelectedPictogram]
+  );
+
+  const getCurrentPictogram = () =>
+    pictogramsRefs.find(ref => ref.label === selectedPictogram);
 
   return (
     <>
       <View
         style={{
           width: "100%",
-          minHeight: heroHeight,
+          minHeight: pictogramSize,
+          backgroundColor: IOColors[theme["appBackground-primary"]],
           position: "absolute",
           alignItems: "center",
           top: 0
         }}
       >
         <Skottie
-          style={{ width: 200, height: 200 }}
-          source={ScanCardiOS}
           autoPlay={true}
+          style={{
+            width: pictogramSize,
+            height: pictogramSize,
+            marginVertical: IOVisualCostants.appMarginDefault
+          }}
+          source={getCurrentPictogram()?.name || pictogramsRefs[0].name}
+          loop={getCurrentPictogram()?.loop || pictogramsRefs[0].loop}
+        />
+        <LinearGradient
+          style={{
+            height: scrollGradientHeight,
+            position: "absolute",
+            left: -IOVisualCostants.appMarginDefault,
+            right: -IOVisualCostants.appMarginDefault,
+            bottom: -scrollGradientHeight
+          }}
+          colors={[
+            IOColors[theme["appBackground-primary"]],
+            hexToRgba(IOColors[theme["appBackground-primary"]], 0)
+          ]}
         />
       </View>
 
-      {/* <ScrollView
+      <ScrollView
         contentContainerStyle={{
-          paddingTop: scrollGradientHeight,
+          paddingTop: pictogramSize,
           paddingBottom: 32 + insets.bottom
         }}
       >
         <ContentWrapper>
           <RadioGroup<string>
             type="radioListItem"
-            items={renderedOrganizationsURIs}
-            selectedItem={selectedItem}
-            onPress={onEntitySelected}
+            items={renderedPictogramsRefs}
+            selectedItem={selectedPictogram}
+            onPress={onPictogramSelected}
           />
         </ContentWrapper>
-      </ScrollView> */}
+      </ScrollView>
     </>
   );
 };
