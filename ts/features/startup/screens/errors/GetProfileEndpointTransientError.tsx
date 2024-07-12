515b99a40a81d4846e178eeb2aef405af10ab996
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2024-07-12 17:22:29 +0200

    wip: add todo comment

--- ts/features/startup/screens/errors/GetProfileEndpointTransientError.tsx
+++ ts/features/startup/screens/errors/GetProfileEndpointTransientError.tsx
@@ -2,17 +2,30 @@ import React from "react";
 import { Modal } from "react-native";
 import I18n from "../../../../i18n";
 import { OperationResultScreenContent } from "../../../../components/screens/OperationResultScreenContent";
+import {
+  isStartupLoaded,
+  StartupStatusEnum
+} from "../../../../store/reducers/startup";
+import { useIOSelector } from "../../../../store/hooks";
 
-export const GetProfileEndpointTransientError = () => (
-  <Modal>
-    <OperationResultScreenContent
-      pictogram="umbrellaNew"
-      title={I18n.t(
-        "fastLogin.userInteraction.sessionExpired.transientError.title"
-      )}
-      subtitle={I18n.t(
-        "fastLogin.userInteraction.sessionExpired.transientError.subtitle"
-      )}
-    />
-  </Modal>
-);
+export const GetProfileEndpointTransientError = () => {
+  const startupStatus = useIOSelector(isStartupLoaded);
+  // TODO: use this constant in MP event
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const afterIdentificationWithIdp =
+    startupStatus === StartupStatusEnum.NOT_AUTHENTICATED;
+  return (
+    <Modal>
+      <OperationResultScreenContent
+        pictogram="umbrellaNew"
+        title={I18n.t(
+          "fastLogin.userInteraction.sessionExpired.transientError.title"
+        )}
+        subtitle={I18n.t(
+          "fastLogin.userInteraction.sessionExpired.transientError.subtitle"
+        )}
+      />
+    </Modal>
+  );
+};
--- ts/features/startup/screens/errors/GetSessionEndpointTransientError.tsx
+++ ts/features/startup/screens/errors/GetSessionEndpointTransientError.tsx
@@ -2,17 +2,30 @@ import React from "react";
 import { Modal } from "react-native";
 import I18n from "../../../../i18n";
 import { OperationResultScreenContent } from "../../../../components/screens/OperationResultScreenContent";
+import { useIOSelector } from "../../../../store/hooks";
+import {
+  isStartupLoaded,
+  StartupStatusEnum
+} from "../../../../store/reducers/startup";
 
-export const GetSessionEndpointTransientError = () => (
-  <Modal>
-    <OperationResultScreenContent
-      pictogram="umbrellaNew"
-      title={I18n.t(
-        "fastLogin.userInteraction.sessionExpired.transientError.title"
-      )}
-      subtitle={I18n.t(
-        "fastLogin.userInteraction.sessionExpired.transientError.subtitle"
-      )}
-    />
-  </Modal>
-);
+export const GetSessionEndpointTransientError = () => {
+  const startupStatus = useIOSelector(isStartupLoaded);
+  // TODO: use this constant in MP event
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const afterIdentificationWithIdp =
+    startupStatus === StartupStatusEnum.NOT_AUTHENTICATED;
+  return (
+    <Modal>
+      <OperationResultScreenContent
+        pictogram="umbrellaNew"
+        title={I18n.t(
+          "fastLogin.userInteraction.sessionExpired.transientError.title"
+        )}
+        subtitle={I18n.t(
+          "fastLogin.userInteraction.sessionExpired.transientError.subtitle"
+        )}
+      />
+    </Modal>
+  );
+};
