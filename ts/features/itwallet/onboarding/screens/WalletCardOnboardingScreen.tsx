5eea0ab72a9611094c3de88f7a3b654baf1bba21
Federico Mastrini <federicomastrini93@gmail.com>
2024-07-15 12:59:17 +0200

    chore: update credential catalog screen

--- ts/features/itwallet/onboarding/screens/WalletCardOnboardingScreen.tsx
+++ ts/features/itwallet/onboarding/screens/WalletCardOnboardingScreen.tsx
@@ -5,9 +5,9 @@ import {
   VSpacer
 } from "@pagopa/io-app-design-system";
 import React from "react";
+import cgnLogo from "../../../../../img/bonus/cgn/cgn_logo.png";
 import { IOScrollViewWithLargeHeader } from "../../../../components/ui/IOScrollViewWithLargeHeader";
 import I18n from "../../../../i18n";
-import cgnLogo from "../../../../../img/bonus/cgn/cgn_logo.png";
 import { useIONavigation } from "../../../../navigation/params/AppParamsList";
 import { useIODispatch, useIOSelector } from "../../../../store/hooks";
 import { isIdPayEnabledSelector } from "../../../../store/reducers/backendStatus";
@@ -16,8 +16,12 @@ import { cgnActivationStart } from "../../../bonus/cgn/store/actions/activation"
 import { isCgnInformationAvailableSelector } from "../../../bonus/cgn/store/reducers/details";
 import { loadAvailableBonuses } from "../../../bonus/common/store/actions/availableBonusesTypes";
 import { PaymentsOnboardingRoutes } from "../../../payments/onboarding/navigation/routes";
+import { trialStatusSelector } from "../../../trialSystem/store/reducers";
 import { CredentialType } from "../../common/utils/itwMocksUtils";
+import { ITW_TRIAL_ID } from "../../common/utils/itwTrialUtils";
 import { ItwCredentialIssuanceMachineContext } from "../../machine/provider";
+import { SubscriptionStateEnum } from "../../../../../definitions/trial_system/SubscriptionState";
+import { ItwTags } from "../../machine/tags";
 
 const WalletCardOnboardingScreen = () => {
   const dispatch = useIODispatch();
@@ -26,8 +30,25 @@ const WalletCardOnboardingScreen = () => {
 
   const isIdPayEnabled = useIOSelector(isIdPayEnabledSelector);
   const isCgnAlreadyActive = useIOSelector(isCgnInformationAvailableSelector);
+  const itwTrialStatus = useIOSelector(trialStatusSelector(ITW_TRIAL_ID));
+  const isItwActive = React.useMemo(
+    // TODO add itw activation check
+    () => itwTrialStatus === SubscriptionStateEnum.SUBSCRIBED,
+    [itwTrialStatus]
+  );
+
+  const isCredentialLoading = ItwCredentialIssuanceMachineContext.useSelector(
+    snapshot => snapshot.hasTag(ItwTags.Loading)
+  );
+  const selectedCredential = ItwCredentialIssuanceMachineContext.useSelector(
+    snapshot => snapshot.context.credentialType
+  );
 
   const startCgnActiviation = () => {
+    if (isCredentialLoading) {
+      return;
+    }
+
     dispatch(loadAvailableBonuses.request());
     dispatch(cgnActivationStart());
   };
@@ -37,12 +58,20 @@ const WalletCardOnboardingScreen = () => {
   };
 
   const navigateToPaymentMethodOnboarding = () => {
+    if (isCredentialLoading) {
+      return;
+    }
+
     navigation.navigate(PaymentsOnboardingRoutes.PAYMENT_ONBOARDING_NAVIGATOR, {
       screen: PaymentsOnboardingRoutes.PAYMENT_ONBOARDING_SELECT_METHOD
     });
   };
 
   const beginCredentialIssuance = (type: CredentialType) => () => {
+    if (isCredentialLoading) {
+      return;
+    }
+
     machineRef.send({ type: "select-credential", credentialType: type });
   };
 
@@ -56,37 +85,36 @@ const WalletCardOnboardingScreen = () => {
       headerActionsProp={{ showHelp: true }}
     >
       <ContentWrapper>
-        <ListItemHeader label="IT Wallet" />
-        <ModuleCredential
-          icon="fingerprint"
-          label={"Identità Digitale"}
-          onPress={beginCredentialIssuance(CredentialType.PID)}
-        />
-        <VSpacer size={8} />
-        <ModuleCredential
-          icon="categTravel"
-          label={"Patente di guida"}
-          onPress={beginCredentialIssuance(CredentialType.DRIVING_LICENSE)}
-        />
+        <VSpacer size={16} />
+        {isItwActive ? (
+          <>
+            <ListItemHeader label="IT Wallet" />
+            <ModuleCredential
+              icon="driverLicense"
+              label={"Patente di guida"}
+              onPress={beginCredentialIssuance(CredentialType.DRIVING_LICENSE)}
+              isFetching={
+                isCredentialLoading &&
+                selectedCredential === CredentialType.DRIVING_LICENSE
+              }
+            />
 
-        <VSpacer size={8} />
-        <ModuleCredential
-          icon="archiveFilled"
-          label={"Carta Europea della Disabilità"}
-          onPress={beginCredentialIssuance(
-            CredentialType.EUROPEAN_DISABILITY_CARD
-          )}
-        />
-        <VSpacer size={8} />
-        <ModuleCredential
-          icon="healthCard"
-          label={"Tessera Sanitaria"}
-          onPress={beginCredentialIssuance(
-            CredentialType.EUROPEAN_HEALTH_INSURANCE_CARD
-          )}
-        />
-        <VSpacer size={24} />
-        <ListItemHeader label="Altro" />
+            <VSpacer size={8} />
+            <ModuleCredential
+              icon="disabilityCard"
+              label={"Carta Europea della Disabilità"}
+              onPress={beginCredentialIssuance(
+                CredentialType.EUROPEAN_DISABILITY_CARD
+              )}
+              isFetching={
+                isCredentialLoading &&
+                selectedCredential === CredentialType.EUROPEAN_DISABILITY_CARD
+              }
+            />
+            <VSpacer size={16} />
+            <ListItemHeader label="Altro" />
+          </>
+        ) : null}
         <ModuleCredential
           image={cgnLogo}
           label={I18n.t("features.wallet.onboarding.options.cgn")}
