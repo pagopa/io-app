d518e9ed600cb78ae28981b8c25876087e8ff6db
Gianluca Spada <gianluca.spada@spindox.it>
2024-07-16 18:00:16 +0200

    chore: add comments to the wallet instance state enum and selectors

--- ts/features/itwallet/lifecycle/store/selectors/index.ts
+++ ts/features/itwallet/lifecycle/store/selectors/index.ts
@@ -5,11 +5,19 @@ import { ItwLifecycleState } from "../reducers";
 export const itwLifecycleSelector = (state: GlobalState) =>
   state.features.itWallet.lifecycle;
 
+/**
+ * The wallet instance is installed. In this state
+ * the integrity key tag should not exist.
+ */
 export const itwLifecycleIsInstalledSelector = (state: GlobalState) =>
   state.features.itWallet.lifecycle ===
     ItwLifecycleState.ITW_LIFECYCLE_INSTALLED &&
   O.isNone(state.features.itWallet.issuance.integrityKeyTag);
 
+/**
+ * The wallet instance is operational. In this state
+ * the integrity key tag must exist.
+ */
 export const itwLifecycleIsOperationalSelector = (state: GlobalState) =>
   state.features.itWallet.lifecycle ===
     ItwLifecycleState.ITW_LIFECYCLE_OPERATIONAL &&
