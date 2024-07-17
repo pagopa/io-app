6e6c4b74285e5239c6f3b810ffd00c79d0cae359
Alice Di Rico <83651704+Ladirico@users.noreply.github.com>
2024-07-17 12:01:45 +0200

    add comment

--- ts/screens/profile/useContentWithFF.tsx
+++ ts/screens/profile/useContentWithFF.tsx
@@ -5,6 +5,15 @@ import I18n from "../../i18n";
 import { useIOSelector } from "../../store/hooks";
 import { isSettingsVisibleAndHideProfileSelector } from "../../store/reducers/backendStatus";
 
+/**
+ *
+ * @param i18nString is the translation string that contains a placeholder (pageName)
+ * @returns a string in which the placeholder (pageName) is replaced according to the value of the selector
+ * @notes when the selector isSettingsVisibleAndHideProfileSelector will be deleted, this custom hook
+ * will also be deleted and the placeholder pageName in the translation files will have to be replaced
+ * with the value of the transation string "global.buttons.settings"
+ */
+
 const useContentWithFF = (i18nString: TranslationKeys) => {
   const isSettingsVisibleAndHideProfile = useIOSelector(
     isSettingsVisibleAndHideProfileSelector
