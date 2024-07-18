7265b18c4d5ae08cfa35af9e09717f9b122274d5
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2024-07-18 11:51:52 +0200

    chore: refactoring

--- ts/features/startup/sagas/index.ts
+++ ts/features/startup/sagas/index.ts
@@ -25,44 +25,60 @@ export function* handleApplicationStartupTransientError(
     startupTransientErrorSelector
   );
 
-  if (
-    transientError.getSessionRetries < STARTUP_TRANSIENT_ERROR_MAX_RETRIES &&
-    transientError.getProfileRetries < STARTUP_TRANSIENT_ERROR_MAX_RETRIES
-  ) {
-    if (kind === "GET_SESSION_DOWN") {
-      yield* put(
-        startupTransientError({
-          kind,
-          getSessionRetries: transientError.getSessionRetries + 1,
-          getProfileRetries: 0,
-          showError: false
-        })
-      );
-    } else if (kind === "GET_PROFILE_DOWN") {
-      yield* put(
-        startupTransientError({
-          kind,
-          getSessionRetries: 0,
-          getProfileRetries: transientError.getProfileRetries + 1,
-          showError: false
-        })
-      );
-    }
-    yield* delay(WAIT_INITIALIZE_SAGA);
-    yield* put(startApplicationInitialization());
-  } else {
-    if (kind === "GET_SESSION_DOWN") {
-      void trackGetSessionEndpointTransientError();
-    } else if (kind === "GET_PROFILE_DOWN") {
-      void trackGetProfileEndpointTransientError();
-    }
-    yield* put(
-      startupTransientError({
-        kind,
-        getSessionRetries: 0,
-        getProfileRetries: 0,
-        showError: true
-      })
-    );
+  const isSessionRetriesWithinLimit =
+    transientError.getSessionRetries < STARTUP_TRANSIENT_ERROR_MAX_RETRIES;
+  const isProfileRetriesWithinLimit =
+    transientError.getProfileRetries < STARTUP_TRANSIENT_ERROR_MAX_RETRIES;
+
+  if (!isSessionRetriesWithinLimit || !isProfileRetriesWithinLimit) {
+    yield* handleMaxRetriesReached(kind);
+    return;
   }
+
+  yield* handleRetry(kind, transientError);
+}
+
+function* handleRetry(
+  kind: StartupTransientErrorKind,
+  transientError: StartupTransientError
+) {
+  const updateErrorAction = getUpdateErrorAction(kind, transientError);
+
+  yield* put(updateErrorAction);
+  yield* delay(WAIT_INITIALIZE_SAGA);
+  yield* put(startApplicationInitialization());
+}
+
+function getUpdateErrorAction(
+  kind: StartupTransientErrorKind,
+  transientError: StartupTransientError
+) {
+  const getSessionRetries =
+    kind === "GET_SESSION_DOWN" ? transientError.getSessionRetries + 1 : 0;
+  const getProfileRetries =
+    kind === "GET_PROFILE_DOWN" ? transientError.getProfileRetries + 1 : 0;
+
+  return startupTransientError({
+    kind,
+    getSessionRetries,
+    getProfileRetries,
+    showError: false
+  });
+}
+
+function* handleMaxRetriesReached(kind: StartupTransientErrorKind) {
+  if (kind === "GET_SESSION_DOWN") {
+    void trackGetSessionEndpointTransientError();
+  } else if (kind === "GET_PROFILE_DOWN") {
+    void trackGetProfileEndpointTransientError();
+  }
+
+  yield* put(
+    startupTransientError({
+      kind,
+      getSessionRetries: 0,
+      getProfileRetries: 0,
+      showError: true
+    })
+  );
 }
