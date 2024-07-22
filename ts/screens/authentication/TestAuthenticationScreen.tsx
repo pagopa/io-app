3ef7d238319eee457a6678a831ebc2f1bee14e36
Alice Di Rico <83651704+Ladirico@users.noreply.github.com>
2024-07-22 12:59:09 +0200

    replace deprecated component and delete mapStateToProps and mapDispatchToProps (now use hooks)

--- ts/screens/authentication/TestAuthenticationScreen.tsx
+++ ts/screens/authentication/TestAuthenticationScreen.tsx
@@ -1,7 +1,5 @@
 import {
   Alert,
-  ContentWrapper,
-  FooterWithButtons,
   IOColors,
   TextInputPassword,
   TextInputValidation,
@@ -15,34 +13,28 @@ import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
 import { pipe } from "fp-ts/lib/function";
 import React, { useCallback, useState } from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import { connect } from "react-redux";
+import { StyleSheet, View } from "react-native";
 import { PasswordLogin } from "../../../definitions/backend/PasswordLogin";
 import { Body } from "../../components/core/typography/Body";
 import { IOStyles } from "../../components/core/variables/IOStyles";
-import BaseScreenComponent from "../../components/screens/BaseScreenComponent";
 import ActivityIndicator from "../../components/ui/ActivityIndicator";
 import I18n from "../../i18n";
 import {
   testLoginCleanUp,
   testLoginRequest
 } from "../../store/actions/authentication";
-import { Dispatch } from "../../store/actions/types";
-import { useIOSelector } from "../../store/hooks";
+import { useIODispatch, useIOSelector } from "../../store/hooks";
 import { testLoginSelector } from "../../store/reducers/testLogin";
 import { getAppVersion } from "../../utils/appVersion";
 import { handleRegenerateKey } from "../../features/lollipop";
-import { GlobalState } from "../../store/reducers/types";
 import { lollipopKeyTagSelector } from "../../features/lollipop/store/reducers/lollipop";
 import { isMixpanelEnabled } from "../../store/reducers/persistedPreferences";
+import { useHeaderSecondLevel } from "../../hooks/useHeaderSecondLevel";
+import { IOScrollView } from "../../components/ui/IOScrollView";
 
 const styles = StyleSheet.create({
   appVersion: { ...IOStyles.flex, ...IOStyles.rowSpaceBetween }
 });
-
-type Props = ReturnType<typeof mapDispatchToProps> &
-  ReturnType<typeof mapStateToProps>;
 
 const checkUsernameValid = (username: string): boolean =>
   E.isRight(FiscalCode.decode(username));
@@ -100,16 +92,28 @@ const isConfirmButtonDisabled = (
 const isUsernameFieldValid = (username: string) =>
   username.length > 0 ? checkUsernameValid(username) : false;
 
-const TestAuthenticationScreen = (props: Props) => {
+const TestAuthenticationScreen = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const loginState = useIOSelector(testLoginSelector);
+  const dispatch = useIODispatch();
   const isLoading = loginState.kind === "requested";
   const isError = loginState.kind === "failed";
   const isSuccessful = loginState.kind === "succedeed";
+  const maybeKeyTag = useIOSelector(lollipopKeyTagSelector);
+  const mixpanelEnabled = useIOSelector(isMixpanelEnabled);
 
-  useFocusEffect(useCallback(() => props.cleanUpLogin, [props.cleanUpLogin]));
+  const requestLogin = useCallback(
+    (passwordLogin: PasswordLogin) => dispatch(testLoginRequest(passwordLogin)),
+    [dispatch]
+  );
+  const cleanUpLogin = useCallback(
+    () => dispatch(testLoginCleanUp()),
+    [dispatch]
+  );
+
+  useFocusEffect(useCallback(() => cleanUpLogin, [cleanUpLogin]));
 
   const handlePressLoginButton = useCallback(
     () =>
@@ -117,15 +121,10 @@ const TestAuthenticationScreen = (props: Props) => {
         // First, map over maybeKeyTag to get the TaskEither if it's Some
         O.map((keyTag: string) =>
           TE.tryCatch(
-            () =>
-              handleRegenerateKey(
-                keyTag,
-                props.mixpanelEnabled,
-                props.dispatch
-              ),
+            () => handleRegenerateKey(keyTag, mixpanelEnabled, dispatch),
             E.toError
           )
-        )(props.maybeKeyTag),
+        )(maybeKeyTag),
         // If maybeKeyTag is None, create a TaskEither that immediately fails
         O.getOrElse(() => TE.left(new Error("Missing keyTag"))),
         // Continue with the login flow if key regeneration is successful
@@ -133,7 +132,7 @@ const TestAuthenticationScreen = (props: Props) => {
           pipe(
             PasswordLogin.decode({ username, password }),
             TE.fromEither,
-            TE.map(props.requestLogin)
+            TE.map(requestLogin)
           )
         ),
         TE.fold(
@@ -145,77 +144,59 @@ const TestAuthenticationScreen = (props: Props) => {
           result => T.of(result)
         )
       )(),
-    [username, password, props]
+    [maybeKeyTag, mixpanelEnabled, requestLogin, dispatch, username, password]
   );
 
+  useHeaderSecondLevel({
+    title: "Test login",
+    canGoBack: true
+  });
+
   return (
-    <BaseScreenComponent goBack={true} headerTitle={"Test login"}>
-      <SafeAreaView style={IOStyles.flex}>
-        <ScrollView>
-          <ContentWrapper>
-            <TextInputValidation
-              placeholder={I18n.t("global.username")}
-              value={username}
-              onChangeText={setUsername}
-              testID={"usernameInput"}
-              icon="profile"
-              onValidate={isUsernameFieldValid}
-              disabled={isLoading}
-              textInputProps={{
-                inputMode: "text",
-                returnKeyType: "done"
-              }}
-            />
-            <VSpacer size={16} />
-            <TextInputPassword
-              placeholder={I18n.t("global.password")}
-              value={password}
-              onChangeText={setPassword}
-              testID={"passwordInput"}
-              icon="locked"
-              disabled={isLoading}
-              textInputProps={{
-                returnKeyType: "done"
-              }}
-            />
-            <VSpacer size={16} />
-            <VersionView />
-            {isLoading && <LoadingView />}
-            {isError && ErrorView("Error", loginState.errorMessage)}
-            {isSuccessful && SuccessfulView()}
-          </ContentWrapper>
-        </ScrollView>
-      </SafeAreaView>
-      <FooterWithButtons
-        type="SingleButton"
-        primary={{
-          type: "Solid",
-          buttonProps: {
-            label: I18n.t("global.buttons.confirm"),
-            accessibilityLabel: I18n.t("global.buttons.confirm"),
-            disabled: isConfirmButtonDisabled(username, password, isLoading),
-            onPress: handlePressLoginButton,
-            testID: "confirmButton"
-          }
+    <IOScrollView
+      actions={{
+        type: "SingleButton",
+        primary: {
+          label: I18n.t("global.buttons.confirm"),
+          accessibilityLabel: I18n.t("global.buttons.confirm"),
+          disabled: isConfirmButtonDisabled(username, password, isLoading),
+          onPress: handlePressLoginButton,
+          testID: "confirmButton"
+        }
+      }}
+    >
+      <TextInputValidation
+        placeholder={I18n.t("global.username")}
+        value={username}
+        onChangeText={setUsername}
+        testID={"usernameInput"}
+        icon="profile"
+        onValidate={isUsernameFieldValid}
+        disabled={isLoading}
+        textInputProps={{
+          inputMode: "text",
+          returnKeyType: "done"
         }}
       />
-    </BaseScreenComponent>
+      <VSpacer size={16} />
+      <TextInputPassword
+        placeholder={I18n.t("global.password")}
+        value={password}
+        onChangeText={setPassword}
+        testID={"passwordInput"}
+        icon="locked"
+        disabled={isLoading}
+        textInputProps={{
+          returnKeyType: "done"
+        }}
+      />
+      <VSpacer size={16} />
+      <VersionView />
+      {isLoading && <LoadingView />}
+      {isError && ErrorView("Error", loginState.errorMessage)}
+      {isSuccessful && SuccessfulView()}
+    </IOScrollView>
   );
 };
 
-const mapStateToProps = (state: GlobalState) => ({
-  maybeKeyTag: lollipopKeyTagSelector(state),
-  mixpanelEnabled: isMixpanelEnabled(state)
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  requestLogin: (passwordLogin: PasswordLogin) =>
-    dispatch(testLoginRequest(passwordLogin)),
-  cleanUpLogin: () => dispatch(testLoginCleanUp()),
-  dispatch
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TestAuthenticationScreen);
+export default TestAuthenticationScreen;
