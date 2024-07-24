287555afb1f1e611ef599f69d23087988f236f67
Federico Mastrini <federicomastrini93@gmail.com>
2024-07-24 12:32:11 +0200

    chore(IT Wallet): [SIW-1379] Add informational alerts in credential details screens (#6023)
    ## Short description
    This PR adds the credential card status and informational alerts in the
    credential details screen
    
    ## List of changes proposed in this pull request
    - Added `getCredentialExpireDate` utility function to get the credential
    expiration date from its claims, if present
    - Added `getCredentialExpireDays` utility function to get the credential
    remaining days until its expiration
    - Added `getCredentialExpireStatus` utility function to get the
    credential expiration status
    - Added alerts in credential details screen component
    - Added credential card status visualization in credential details
    screen component
    - Removed grouping for credential claims in credential details screen
    
    ## How to test
    Open the MDL credential screen details, check that the alert is
    correctly displayed and the bottom sheet is presented by tapping on it.
    Additionally, you should be able to get other alerts (expired, expiring)
    by mocking the expiration date of the credential.
    
    ## Previews
    
    <img
    src="https://github.com/user-attachments/assets/d5f5c3ae-d8c2-4992-bfde-3d1d28424c05"
    width="200" />
    <img
    src="https://github.com/user-attachments/assets/cd8adf2f-8534-4e98-9bf0-0f531b5a5524"
    width="200" />
    <img
    src="https://github.com/user-attachments/assets/c1570617-8851-423d-8ece-39160c40cb29"
    width="200" />
    
    ---------
    
    Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>
    Co-authored-by: Andrea Piai <andrea.piai@pagopa.it>
    Co-authored-by: LazyAfternoons <LazyAfternoons@users.noreply.github.com>
    Co-authored-by: Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
    Co-authored-by: Mario Perrotta <mario.perrotta@pagopa.it>
    Co-authored-by: Gianluca Spada <gianluca.spada@spindox.it>
    Co-authored-by: LazyAfternoons <lazyafternoons@outlook.it>

--- ts/features/itwallet/presentation/components/ItwPresentationDetailFooter.tsx
+++ ts/features/itwallet/presentation/components/ItwPresentationDetailFooter.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Alert, View } from "react-native";
 import {
-  ListItemAction,
-  VSpacer,
   Chip,
-  IOStyles
+  IOStyles,
+  ListItemAction,
+  VSpacer
 } from "@pagopa/io-app-design-system";
-import { format } from "../../../../utils/dates";
+import React from "react";
+import { Alert, Linking, View } from "react-native";
 import I18n from "../../../../i18n";
+import { format } from "../../../../utils/dates";
 import { IssuerConfiguration } from "../../common/utils/itwTypesUtils";
 
 type Props = {
@@ -31,7 +31,9 @@ export const ItwPresentationDetailFooter = ({
         "features.itWallet.presentation.credentialDetails.actions.requestAssistance",
         { authSource: issuerConf.federation_entity.organization_name }
       )}
-      onPress={() => Alert.alert("Assistance")}
+      onPress={() =>
+        Linking.openURL(`mailto:${issuerConf.federation_entity.contacts?.[0]}`)
+      }
     />
     <ListItemAction
       variant="danger"
