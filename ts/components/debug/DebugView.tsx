cb5b5a765faf94c56e5d72e2bf8fa4b998762ade
Federico Mastrini <federicomastrini93@gmail.com>
2024-07-27 12:04:20 +0200

    chore: add ignore margins prop

--- ts/components/debug/DebugView.tsx
+++ ts/components/debug/DebugView.tsx
@@ -5,19 +5,25 @@ import { withDebugEnabled } from "./withDebugEnabled";
 
 export type DebugViewProps = {
   title?: string;
+  ignoreHorizontalMargins?: boolean;
 };
 
 /**
  * This component renders its content only if the debug mode is enabled, otherwise return null (nothing)
  */
 export const DebugView = withDebugEnabled(
-  ({ children, title = "Debug" }: React.PropsWithChildren<DebugViewProps>) => (
+  ({
+    children,
+    title = "Debug",
+    ignoreHorizontalMargins = false
+  }: React.PropsWithChildren<DebugViewProps>) => (
     <View
       testID="DebugViewTestID"
       style={{
         paddingHorizontal: 24,
         paddingBottom: 12,
-        backgroundColor: IOColors["grey-100"]
+        backgroundColor: IOColors["grey-100"],
+        marginHorizontal: ignoreHorizontalMargins ? -24 : 0
       }}
     >
       <ListItemHeader label={title} iconName="ladybug" />
