1bc9cec01d7b71244cce6fae917c26dac4426344
Damiano Plebani <damiano.plebani@pagopa.it>
2024-07-29 17:35:52 +0200

    chore(Cross): [IOAPPX-345] Refactor the entire DS section using `Stack` components (#5974)
    > [!important]
    > This PR depends on:
    > * https://github.com/pagopa/io-app/pull/5944
    
    ## Short description
    This PR refactors the entire **Design System** section using the new
    `Stack` components. This is a kind of exercise to test the functionality
    of the new components in real use cases.
    
    ## List of changes proposed in this pull request
    - Update almost all DS-related screens
    - Add the new MD-related typographic styles to the **Typography** page
    - Add the new `DSLegacyTypography` screen
    
    ## How to test
    1. Launch the app in the local environment
    2. Go to the **Design System** section and check every page
    
    ---------
    
    Co-authored-by: Jacopo Pompilii <Jacopo.pompilii@pagopa.it>

--- ts/features/design-system/core/DSLayout.tsx
+++ ts/features/design-system/core/DSLayout.tsx
@@ -1,27 +1,43 @@
-import * as React from "react";
-import { View } from "react-native";
 import {
-  IOColors,
-  Divider,
-  HSpacer,
-  VSpacer,
-  IOAppMargin,
-  IOSpacer,
   ContentWrapper,
+  Divider,
+  HStack,
+  IOAppMargin,
+  IOColors,
+  IOSpacer,
+  VStack,
   useIOTheme
 } from "@pagopa/io-app-design-system";
-import { DesignSystemScreen } from "../components/DesignSystemScreen";
-import { DSSpacerViewerBox } from "../components/DSSpacerViewerBox";
+import * as React from "react";
+import { View } from "react-native";
 import { Body } from "../../../components/core/typography/Body";
-import { LabelSmall } from "../../../components/core/typography/LabelSmall";
-import { H3 } from "../../../components/core/typography/H3";
 import { H1 } from "../../../components/core/typography/H1";
+import { H3 } from "../../../components/core/typography/H3";
+import { LabelSmall } from "../../../components/core/typography/LabelSmall";
+import { DSComponentViewerBox } from "../components/DSComponentViewerBox";
+import { DSSpacerViewerBox } from "../components/DSSpacerViewerBox";
+import { DesignSystemScreen } from "../components/DesignSystemScreen";
 
 export const DSLayout = () => {
-  const theme = useIOTheme();
+  const sectionMargin = 40;
 
   return (
     <DesignSystemScreen title={"Layout"} noMargin>
+      <VStack space={sectionMargin}>
+        <Grid />
+        <Spacing />
+        <Stack />
+        <DividerRow />
+      </VStack>
+    </DesignSystemScreen>
+  );
+};
+
+const Grid = () => {
+  const theme = useIOTheme();
+
+  return (
+    <View>
       <ContentWrapper>
         <H1
           color={theme["textHeading-default"]}
@@ -38,9 +54,11 @@ export const DSLayout = () => {
           ContentWrapper
         </H3>
       </ContentWrapper>
-      {IOAppMargin.map((value, i, arr) => (
-        <React.Fragment key={`${value}-${i}`}>
+
+      <VStack space={16}>
+        {IOAppMargin.map((value, i) => (
           <View
+            key={`${value}-${i}`}
             style={{
               backgroundColor: IOColors[theme["appBackground-tertiary"]]
             }}
@@ -64,63 +82,229 @@ export const DSLayout = () => {
               </View>
             </ContentWrapper>
           </View>
-          {i !== arr.length - 1 && <VSpacer size={16} />}
-        </React.Fragment>
-      ))}
-
-      <VSpacer size={40} />
-
-      <ContentWrapper>
-        <H1
-          color={theme["textHeading-default"]}
-          weight={"Bold"}
-          style={{ marginBottom: 16 }}
-        >
-          Spacing
-        </H1>
-
-        <H3
-          color={theme["textHeading-default"]}
-          weight={"Semibold"}
-          style={{ marginBottom: 16 }}
-        >
-          VSpacer
-        </H3>
-
-        {/* Vertical */}
-        {IOSpacer.map((spacerEntry, i, arr) => (
-          <React.Fragment key={`${spacerEntry}-${i}-vertical`}>
-            <DSSpacerViewerBox orientation="vertical" size={spacerEntry} />
-            {/* Don't add spacer to the last item. Quick and dirty
-        alternative to the Stack component.
-        https://stackoverflow.com/a/60975451 */}
-            {i !== arr.length - 1 && <VSpacer size={16} />}
-          </React.Fragment>
         ))}
+      </VStack>
+    </View>
+  );
+};
 
-        <VSpacer size={24} />
+const Spacing = () => {
+  const theme = useIOTheme();
 
-        <H3
-          color={theme["textHeading-default"]}
-          weight={"Semibold"}
-          style={{ marginBottom: 16 }}
+  return (
+    <ContentWrapper>
+      <H1
+        color={theme["textHeading-default"]}
+        weight={"Bold"}
+        style={{ marginBottom: 16 }}
+      >
+        Spacing
+      </H1>
+
+      <VStack space={24}>
+        {/* VSPACER */}
+        <VStack space={16}>
+          <H3 color={theme["textHeading-default"]} weight={"Semibold"}>
+            VSpacer
+          </H3>
+
+          {/* Vertical */}
+          <VStack space={16}>
+            {IOSpacer.map((spacerEntry, i) => (
+              <DSSpacerViewerBox
+                key={`${spacerEntry}-${i}-vertical`}
+                orientation="vertical"
+                size={spacerEntry}
+              />
+            ))}
+          </VStack>
+        </VStack>
+
+        {/* HSPACER */}
+        <VStack space={16}>
+          <H3 color={theme["textHeading-default"]} weight={"Semibold"}>
+            HSpacer
+          </H3>
+
+          {/* Horizontal */}
+          <HStack space={8}>
+            {IOSpacer.map((spacerEntry, i) => (
+              <DSSpacerViewerBox
+                key={`${spacerEntry}-${i}-horizontal`}
+                orientation="horizontal"
+                size={spacerEntry}
+              />
+            ))}
+          </HStack>
+        </VStack>
+      </VStack>
+    </ContentWrapper>
+  );
+};
+
+const Stack = () => {
+  const theme = useIOTheme();
+
+  return (
+    <ContentWrapper>
+      <H1
+        color={theme["textHeading-default"]}
+        weight={"Bold"}
+        style={{ marginBottom: 16 }}
+      >
+        Stack
+      </H1>
+
+      <VStack space={24}>
+        <VStack space={16}>
+          <H3 color={theme["textHeading-default"]} weight={"Semibold"}>
+            VStack
+          </H3>
+
+          <View>
+            <DSComponentViewerBox name="VStack, space 16">
+              <View
+                style={{
+                  backgroundColor: IOColors[theme["appBackground-secondary"]]
+                }}
+              >
+                <VStack space={16}>
+                  <VStackBlocks />
+                </VStack>
+              </View>
+            </DSComponentViewerBox>
+
+            {/* <DSComponentViewerBox name="VStack, space 16, centered">
+          <View
+            style={{
+              alignSelf: "flex-start",
+              backgroundColor: IOColors[theme["appBackground-secondary"]]
+            }}
+          >
+            <VStack space={16} style={{ alignItems: "center" }}>
+              <VStackBlocks />
+            </VStack>
+          </View>
+        </DSComponentViewerBox> */}
+          </View>
+        </VStack>
+
+        <VStack space={16}>
+          <H3 color={theme["textHeading-default"]} weight={"Semibold"}>
+            HStack
+          </H3>
+
+          <DSComponentViewerBox name="HStack, space 16">
+            <View
+              style={{
+                backgroundColor: IOColors[theme["appBackground-secondary"]]
+              }}
+            >
+              <HStack space={16}>
+                <HStackBlocks />
+              </HStack>
+            </View>
+          </DSComponentViewerBox>
+
+          {/* <DSComponentViewerBox name="HStack, space 16, centered">
+          <View
+            style={{
+              backgroundColor: IOColors[theme["appBackground-secondary"]]
+            }}
+          >
+            <HStack space={16} style={{ alignItems: "center" }}>
+              <HStackBlocks />
+            </HStack>
+          </View>
+        </DSComponentViewerBox> */}
+        </VStack>
+      </VStack>
+    </ContentWrapper>
+  );
+};
+
+const VStackBlocks = () => {
+  const theme = useIOTheme();
+
+  return (
+    <>
+      {[...Array(3)].map((_el, i) => (
+        <View
+          key={`block-${i}`}
+          style={{
+            height: 32,
+            paddingHorizontal: 8,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: IOColors[theme["appBackground-tertiary"]]
+          }}
         >
-          HSpacer
-        </H3>
-
-        {/* Horizontal */}
-        <View style={{ flexDirection: "row" }}>
-          {IOSpacer.map((spacerEntry, i, arr) => (
-            <React.Fragment key={`${spacerEntry}-${i}-horizontal`}>
-              <DSSpacerViewerBox orientation="horizontal" size={spacerEntry} />
-              {i !== arr.length - 1 && <HSpacer size={8} />}
-            </React.Fragment>
-          ))}
+          <LabelSmall
+            weight="Regular"
+            color={theme["textBody-tertiary"]}
+          >{`Block n.${i + 1}`}</LabelSmall>
         </View>
+      ))}
+      <View
+        style={{
+          height: 72,
+          paddingHorizontal: 16,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: IOColors[theme["appBackground-tertiary"]]
+        }}
+      >
+        <LabelSmall weight="Regular" color={theme["textBody-tertiary"]}>
+          Different height
+        </LabelSmall>
+      </View>
+    </>
+  );
+};
 
-        <VSpacer size={48} />
-      </ContentWrapper>
+const HStackBlocks = () => {
+  const theme = useIOTheme();
 
+  return (
+    <>
+      {[...Array(3)].map((_el, i) => (
+        <View
+          key={`block-${i}`}
+          style={{
+            width: 48,
+            paddingVertical: 8,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: IOColors[theme["appBackground-tertiary"]]
+          }}
+        >
+          <LabelSmall weight="Regular" color={theme["textBody-tertiary"]}>{`${
+            i + 1
+          }`}</LabelSmall>
+        </View>
+      ))}
+      <View
+        style={{
+          flexGrow: 1,
+          paddingVertical: 16,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: IOColors[theme["appBackground-tertiary"]]
+        }}
+      >
+        <LabelSmall weight="Regular" color={theme["textBody-tertiary"]}>
+          Growing block
+        </LabelSmall>
+      </View>
+    </>
+  );
+};
+
+const DividerRow = () => {
+  const theme = useIOTheme();
+
+  return (
+    <>
       <ContentWrapper>
         <H1
           color={theme["textHeading-default"]}
@@ -139,10 +323,8 @@ export const DSLayout = () => {
         </H3>
 
         <Divider />
-        <VSpacer size={48} />
       </ContentWrapper>
       <Divider />
-      <VSpacer size={48} />
-    </DesignSystemScreen>
+    </>
   );
 };
