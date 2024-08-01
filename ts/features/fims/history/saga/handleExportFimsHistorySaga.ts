e7f67f51aace22ce9a84aa0f7c7b98429ea4ab3e
forrest57 <tomba808@gmail.com>
2024-08-01 12:05:55 +0200

    still WIP

--- ts/features/fims/history/saga/handleExportFimsHistorySaga.ts
+++ ts/features/fims/history/saga/handleExportFimsHistorySaga.ts
@@ -2,34 +2,47 @@ import { IOToast } from "@pagopa/io-app-design-system";
 import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/function";
 import { call, put } from "typed-redux-saga/macro";
+import { ActionType } from "typesafe-actions";
 import { withRefreshApiCall } from "../../../fastLogin/saga/utils";
 import { FimsHistoryClient } from "../api/client";
 import { fimsHistoryExport } from "../store/actions";
+import { SagaCallReturnType } from "../../../../types/utils";
 
 export function* handleExportFimsHistorySaga(
   exportHistory: FimsHistoryClient["exports"],
-  bearerToken: string
-  // _action: ActionType<typeof fimsHistoryExport.request>
+  bearerToken: string,
+  action: ActionType<typeof fimsHistoryExport.request>
 ) {
   const exportHistoryRequest = exportHistory({
     Bearer: bearerToken
   });
 
   try {
-    const exportHistoryResult = yield* call(
+    const exportHistoryResult = (yield* call(
       withRefreshApiCall,
-      exportHistoryRequest
-    );
+      exportHistoryRequest,
+      action
+    )) as SagaCallReturnType<typeof exportHistory>;
 
     const actionToPut = pipe(
       exportHistoryResult,
       E.foldW(
         _failure => {
-          IOToast.error("failure");
+          IOToast.error(`failure: ${_failure.toLocaleString()}`);
           return fimsHistoryExport.failure("FAIL");
         },
-        _success => {
-          IOToast.show("success");
+        success => {
+          switch (success.status) {
+            case 202:
+              IOToast.success(`success: ${success.status}`);
+              break;
+            case 409:
+              IOToast.info(`exporting: ${success.status}`);
+              break;
+            default:
+              IOToast.error(`unknown status: ${success.status}`);
+              break;
+          }
           return fimsHistoryExport.success();
         }
       )
