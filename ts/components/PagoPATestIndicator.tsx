53d17f8be44c361d3a59cb606d5e8bfc58b11677
Damiano Plebani <damiano.plebani@pagopa.it>
2024-08-01 11:57:44 +0200

    Use `Text` for `pagoPATestIndicator`, instead of `IOText`

--- ts/components/PagoPATestIndicator.tsx
+++ ts/components/PagoPATestIndicator.tsx
@@ -1,11 +1,6 @@
-import {
-  IOColors,
-  IOText,
-  Icon,
-  hexToRgba
-} from "@pagopa/io-app-design-system";
+import { IOColors, Icon, hexToRgba } from "@pagopa/io-app-design-system";
 import * as React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const debugItemBgColor = hexToRgba(IOColors.white, 0.4);
 const debugItemBorderColor = hexToRgba(IOColors.black, 0.1);
@@ -27,14 +22,16 @@ const styles = StyleSheet.create({
 const PagoPATestIndicator = () => (
   <View style={styles.indicatoWrapper}>
     <Icon name="productPagoPA" color="grey-850" size={20} />
-    <IOText
-      weight="Semibold"
-      color="grey-850"
-      size={9}
-      style={{ marginLeft: 4, textTransform: "uppercase", letterSpacing: 0.2 }}
+    <Text
+      style={{
+        fontSize: 8,
+        color: IOColors["grey-850"],
+        marginLeft: 4,
+        textTransform: "uppercase"
+      }}
     >
       Test
-    </IOText>
+    </Text>
   </View>
 );
 
