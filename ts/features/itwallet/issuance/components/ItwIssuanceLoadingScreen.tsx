18b074e435a4253391f3c1f7296eda85036334fa
Gianluca Spada <gianluca.spada@spindox.it>
2024-08-09 15:24:06 +0200

    chore: display the cancel button if the app was backgrounded

--- ts/features/itwallet/issuance/components/ItwIssuanceLoadingScreen.tsx
+++ ts/features/itwallet/issuance/components/ItwIssuanceLoadingScreen.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { Linking, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { AppStateStatus, Linking, View } from "react-native";
 import { ButtonOutline, IOStyles, VSpacer } from "@pagopa/io-app-design-system";
 import LoadingScreenContent from "../../../../components/screens/LoadingScreenContent";
 import I18n from "../../../../i18n";
 import { selectIsCieIdEidRequest } from "../../machine/eid/selectors";
 import { ItwEidIssuanceMachineContext } from "../../machine/provider";
+import { useIOSelector } from "../../../../store/hooks";
+import { appStateSelector } from "../../../../store/reducers/appState";
 
 /**
  * This loading screen component displays a cancel button during CieID identification
  * to allow the user to abort the flow after the external browser was opened.
  */
 export const ItwIssuanceLoadingScreen = () => {
+  const { appState } = useIOSelector(appStateSelector);
+
   const [callbackUrlReceived, setCallbackUrlReceived] = useState(false);
+  const prevAppState = useRef<AppStateStatus>();
 
   const machineRef = ItwEidIssuanceMachineContext.useActorRef();
   const isCieIdEidRequest = ItwEidIssuanceMachineContext.useSelector(
@@ -19,8 +24,14 @@ export const ItwIssuanceLoadingScreen = () => {
   );
 
   useEffect(() => {
-    // eslint-disable-next-line curly
-    if (!isCieIdEidRequest) return;
+    // eslint-disable-next-line functional/immutable-data
+    prevAppState.current = appState;
+  }, [appState]);
+
+  useEffect(() => {
+    if (!isCieIdEidRequest) {
+      return;
+    }
 
     const listener = Linking.addEventListener("url", ({ url }) =>
       setCallbackUrlReceived(!!url)
@@ -30,9 +41,18 @@ export const ItwIssuanceLoadingScreen = () => {
     };
   }, [isCieIdEidRequest]);
 
+  // The button is only shown if the app has been in the background,
+  // as aborting before the external browser is opened is not handled yet.
+  // This heuristic is not 100% accurate because the user could manually put the app in the background
+  // during the initial API calls in the flow, but they are usually quite fast.
+  const shouldShowCancelButton =
+    isCieIdEidRequest &&
+    prevAppState.current === "background" &&
+    !callbackUrlReceived;
+
   return (
     <LoadingScreenContent contentTitle={I18n.t("global.genericWaiting")}>
-      {isCieIdEidRequest && !callbackUrlReceived && (
+      {shouldShowCancelButton && (
         <View style={IOStyles.alignCenter}>
           <VSpacer size={16} />
           <ButtonOutline
