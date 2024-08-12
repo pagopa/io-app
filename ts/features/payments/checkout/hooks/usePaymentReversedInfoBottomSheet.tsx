aec9a9e5b298d9b7d1e8083fa41c1664e3d1fb28
Alessandro Izzo <alessandro.izzo998@gmail.com>
2024-08-12 12:03:16 +0200

    chore: added link to redirect

--- ts/features/payments/checkout/hooks/usePaymentReversedInfoBottomSheet.tsx
+++ ts/features/payments/checkout/hooks/usePaymentReversedInfoBottomSheet.tsx
@@ -1,19 +1,36 @@
 import React from "react";
-import { VSpacer } from "@pagopa/io-app-design-system";
+import { Body, LabelLink, VSpacer } from "@pagopa/io-app-design-system";
 import { useIOBottomSheetAutoresizableModal } from "../../../../utils/hooks/bottomSheet";
 import I18n from "../../../../i18n";
 import { ParagraphWithTitle } from "../../common/components/ParagraphWithTitle";
+import { openWebUrl } from "../../../../utils/url";
+import { WALLET_PAYMENT_SHOW_OTHER_CHANNELS_URL } from "../utils";
 
 export const usePaymentReversedInfoBottomSheet = () => {
+  const handleOnPressLink = () => {
+    openWebUrl(WALLET_PAYMENT_SHOW_OTHER_CHANNELS_URL);
+  };
+
   const getModalContent = () => (
     <>
       <ParagraphWithTitle
         title={I18n.t(
           "features.payments.checkout.bottomSheet.PAYMENT_REVERSED.payNotice.title"
         )}
-        body={I18n.t(
-          "features.payments.checkout.bottomSheet.PAYMENT_REVERSED.payNotice.content"
-        )}
+        body={
+          <>
+            <Body>
+              {I18n.t(
+                "features.payments.checkout.bottomSheet.PAYMENT_REVERSED.payNotice.content"
+              )}
+            </Body>{" "}
+            <LabelLink onPress={handleOnPressLink}>
+              {I18n.t(
+                "features.payments.checkout.bottomSheet.PAYMENT_REVERSED.payNotice.contentLink"
+              )}
+            </LabelLink>
+          </>
+        }
       />
       <VSpacer size={24} />
       <ParagraphWithTitle
