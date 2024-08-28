2f182a8cae05b6a37b6e7517915c9b120b03b604
Gianluca Spada <gianluca.spada@spindox.it>
2024-08-28 08:57:36 +0200

    refactor: generate DPoP key

--- ts/features/itwallet/common/utils/itwIssuanceUtils.ts
+++ ts/features/itwallet/common/utils/itwIssuanceUtils.ts
@@ -17,6 +17,11 @@ import {
 } from "../../../../config";
 import { type IdentificationContext } from "../../machine/eid/context";
 import { StoredCredential } from "./itwTypesUtils";
+import {
+  disposeEidCryptoKeys,
+  DPOP_EID_KEYTAG,
+  regenerateCryptoKey
+} from "./itwCryptoContextUtils";
 
 type AccessToken = Awaited<
   ReturnType<typeof Credential.Issuance.authorizeAccess>
@@ -124,20 +129,23 @@ const completeCieAuthFlow = async ({
   const query = Object.fromEntries(new URL(callbackUrl).searchParams);
   const { code } = Credential.Issuance.parseAuthroizationResponse(query);
 
-  const { accessToken, dPoPContext } =
-    await Credential.Issuance.authorizeAccess(
-      issuerConf,
-      code,
-      clientId,
-      CIE_L3_REDIRECT_URI,
-      codeVerifier,
-      {
-        walletInstanceAttestation: walletAttestation,
-        wiaCryptoContext
-      }
-    );
+  await regenerateCryptoKey(DPOP_EID_KEYTAG);
+  const dPopCryptoContext = createCryptoContextFor(DPOP_EID_KEYTAG);
 
-  return { accessToken, dPoPContext };
+  const { accessToken } = await Credential.Issuance.authorizeAccess(
+    issuerConf,
+    code,
+    clientId,
+    CIE_L3_REDIRECT_URI,
+    codeVerifier,
+    {
+      walletInstanceAttestation: walletAttestation,
+      wiaCryptoContext,
+      dPopCryptoContext
+    }
+  );
+
+  return { accessToken, dPoPContext: dPopCryptoContext };
 };
 
 type FullAuthFlowParams = {
@@ -200,22 +208,25 @@ const startAndCompleteFullAuthFlow = async ({
       authorizationContext
     );
 
-  const { accessToken, dPoPContext } =
-    await Credential.Issuance.authorizeAccess(
-      issuerConf,
-      code,
-      clientId,
-      redirectUri,
-      codeVerifier,
-      {
-        walletInstanceAttestation: walletAttestation,
-        wiaCryptoContext
-      }
-    );
+  await regenerateCryptoKey(DPOP_EID_KEYTAG);
+  const dPopCryptoContext = createCryptoContextFor(DPOP_EID_KEYTAG);
+
+  const { accessToken } = await Credential.Issuance.authorizeAccess(
+    issuerConf,
+    code,
+    clientId,
+    redirectUri,
+    codeVerifier,
+    {
+      walletInstanceAttestation: walletAttestation,
+      wiaCryptoContext,
+      dPopCryptoContext
+    }
+  );
 
   return {
     accessToken,
-    dPoPContext,
+    dPoPContext: dPopCryptoContext,
     credentialDefinition,
     clientId,
     issuerConf
@@ -253,8 +264,10 @@ const getPid = async ({
     accessToken,
     clientId,
     credentialDefinition,
-    dPoPContext,
-    { credentialCryptoContext }
+    {
+      credentialCryptoContext,
+      dPopCryptoContext: dPoPContext
+    }
   );
 
   const { parsedCredential } =
@@ -339,4 +352,8 @@ const getSpidProductionIdpHint = (spidIdpId: string) => {
     throw new Error(`Unknown idp ${spidIdpId}`);
   }
   return SPID_IDP_HINTS[spidIdpId];
+};
+
+export const disposeWallet = async () => {
+  await disposeEidCryptoKeys();
 };
