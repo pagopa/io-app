bff44fca066c523b9acc90175173a9d8ff6d5fbc
Federico Mastrini <federicomastrini93@gmail.com>
2024-08-31 10:19:04 +0200

    chore: remove xstate helpers

--- ts/xstate/utils/index.ts
+++
@@ -1,9 +0,0 @@
-const LOADING_TAG = "LOADING";
-const UPSERTING_TAG = "UPSERTING";
-const WAITING_USER_INPUT_TAG = "WAITING_USER_INPUT";
-
-export { LOADING_TAG, UPSERTING_TAG, WAITING_USER_INPUT_TAG };
-
-export const notImplementedStub = () => {
-  throw new Error("Not implemented");
-};
