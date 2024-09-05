5abad36b0787ad19b38d7fdd10f5af0a720f6ef7
Gianluca Spada <gianluca.spada@spindox.it>
2024-09-05 16:50:33 +0200

    fix(IT Wallet): [SIW-1535] Credential duplication after renewal (#6149)
    ## Short description
    This PR fixes a bug that happens when there is a credential in the
    wallet and that same credential is renewed. The bug is caused by:
    - Always appending new credentials to
    `features.itWallet.credentials.credentials` array
    - Using the unique key tag as the key for wallet cards
    
    ## List of changes proposed in this pull request
    - Overwrite credentials of the same type when dispatching
    `ITW_CREDENTIALS_STORE`
    - `ITW_{credential_type}` is used as key for wallet cards to avoid
    duplicates
    - Removed `ITW_CREDENTIALS_MULTIPLE_UPDATE` action: now it is safe to
    dispatch `ITW_CREDENTIALS_STORE` to store a credential or an array of
    credentials and overwrite previous instances
    
    ## How to test
    Currently one way to test this is to renew the driving license after
    it's been revoked, and then navigate to the wallet screen. Without the
    fix two driving license cards will appear, with the fix only one.

--- ts/features/itwallet/credentials/saga/handleItwCredentialsStoreSaga.ts
+++ ts/features/itwallet/credentials/saga/handleItwCredentialsStoreSaga.ts
@@ -1,6 +1,7 @@
 import { put } from "typed-redux-saga/macro";
-import { walletUpsertCard } from "../../../newWallet/store/actions/cards";
+import { walletAddCards } from "../../../newWallet/store/actions/cards";
 import { itwCredentialsStore } from "../store/actions";
+import { getCredentialStatus } from "../../common/utils/itwClaimsUtils";
 
 /**
  * This saga handles the credential store action and ensures the consistency between stored credentials and wallet state.
@@ -9,13 +10,15 @@ import { itwCredentialsStore } from "../store/actions";
 export function* handleItwCredentialsStoreSaga(
   itwCredentialsStoreAction: ReturnType<typeof itwCredentialsStore>
 ) {
-  const { keyTag, credentialType } = itwCredentialsStoreAction.payload;
   yield* put(
-    walletUpsertCard({
-      key: keyTag,
-      type: "itw",
-      category: "itw",
-      credentialType
-    })
+    walletAddCards(
+      itwCredentialsStoreAction.payload.map(c => ({
+        key: `ITW_${c.credentialType}`,
+        type: "itw",
+        category: "itw",
+        credentialType: c.credentialType,
+        status: getCredentialStatus(c)
+      }))
+    )
   );
 }
