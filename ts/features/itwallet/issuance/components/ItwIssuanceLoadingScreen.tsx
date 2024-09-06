faef1803084f5fafc56f25f98c70e197c3ce5f0f
Gianluca Spada <gianluca.spada@spindox.it>
2024-09-06 12:07:08 +0200

    chore: update copy when waiting for CieID identification

--- ts/features/itwallet/issuance/components/ItwIssuanceLoadingScreen.tsx
+++ ts/features/itwallet/issuance/components/ItwIssuanceLoadingScreen.tsx
@@ -1,32 +1,53 @@
-import React, { useEffect, useRef, useState } from "react";
-import { AppStateStatus, Linking, View } from "react-native";
-import { ButtonOutline, IOStyles, VSpacer } from "@pagopa/io-app-design-system";
+import React, { useEffect, useState } from "react";
+import { Linking, View } from "react-native";
+import {
+  Body,
+  ButtonLink,
+  ContentWrapper,
+  IOStyles,
+  VSpacer
+} from "@pagopa/io-app-design-system";
 import LoadingScreenContent from "../../../../components/screens/LoadingScreenContent";
 import I18n from "../../../../i18n";
 import { selectIsCieIdEidRequest } from "../../machine/eid/selectors";
 import { ItwEidIssuanceMachineContext } from "../../machine/provider";
-import { useIOSelector } from "../../../../store/hooks";
-import { appStateSelector } from "../../../../store/reducers/appState";
+
+const CieIdConnectionContent = () => {
+  const machineRef = ItwEidIssuanceMachineContext.useActorRef();
+  return (
+    <ContentWrapper>
+      <VSpacer size={8} />
+      <View style={IOStyles.centerJustified}>
+        <Body style={{ textAlign: "center" }}>
+          {I18n.t("features.itWallet.identification.loading.cieId.subtitle")}
+        </Body>
+        <VSpacer size={24} />
+        <View style={{ alignSelf: "center" }}>
+          <ButtonLink
+            accessibilityLabel={I18n.t(
+              "features.itWallet.identification.loading.cieId.cancel"
+            )}
+            label={I18n.t(
+              "features.itWallet.identification.loading.cieId.cancel"
+            )}
+            onPress={() => machineRef.send({ type: "abort" })}
+          />
+        </View>
+      </View>
+    </ContentWrapper>
+  );
+};
 
 /**
  * This loading screen component displays a cancel button during CieID identification
  * to allow the user to abort the flow after the external browser was opened.
  */
 export const ItwIssuanceLoadingScreen = () => {
-  const { appState } = useIOSelector(appStateSelector);
-
   const [callbackUrlReceived, setCallbackUrlReceived] = useState(false);
-  const prevAppState = useRef<AppStateStatus>();
 
-  const machineRef = ItwEidIssuanceMachineContext.useActorRef();
   const isCieIdEidRequest = ItwEidIssuanceMachineContext.useSelector(
     selectIsCieIdEidRequest
   );
-
-  useEffect(() => {
-    // eslint-disable-next-line functional/immutable-data
-    prevAppState.current = appState;
-  }, [appState]);
 
   useEffect(() => {
     if (!isCieIdEidRequest) {
@@ -41,28 +62,18 @@ export const ItwIssuanceLoadingScreen = () => {
     };
   }, [isCieIdEidRequest]);
 
-  // The button is only shown if the app has been in the background,
-  // as aborting before the external browser is opened is not handled yet.
-  // This heuristic is not 100% accurate because the user could manually put the app in the background
-  // during the initial API calls in the flow, but they are usually quite fast.
-  const shouldShowCancelButton =
-    isCieIdEidRequest &&
-    prevAppState.current === "background" &&
-    !callbackUrlReceived;
+  const isWaitingForCieIdIdentification =
+    isCieIdEidRequest && !callbackUrlReceived;
 
   return (
-    <LoadingScreenContent contentTitle={I18n.t("global.genericWaiting")}>
-      {shouldShowCancelButton && (
-        <View style={IOStyles.alignCenter}>
-          <VSpacer size={16} />
-          <ButtonOutline
-            fullWidth
-            accessibilityLabel={I18n.t("global.buttons.cancel")}
-            label={I18n.t("global.buttons.cancel")}
-            onPress={() => machineRef.send({ type: "abort" })}
-          />
-        </View>
-      )}
+    <LoadingScreenContent
+      contentTitle={
+        isWaitingForCieIdIdentification
+          ? I18n.t("features.itWallet.identification.loading.cieId.title")
+          : I18n.t("global.genericWaiting")
+      }
+    >
+      {isWaitingForCieIdIdentification && <CieIdConnectionContent />}
     </LoadingScreenContent>
   );
 };
