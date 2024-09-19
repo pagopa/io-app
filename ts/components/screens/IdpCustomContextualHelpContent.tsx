39d2169a4f2b09b159d91dcd14644a6a81bfa95e
Damiano Plebani <damiano.plebani@pagopa.it>
2024-09-19 10:33:38 +0200

    Update `Idp…` related screens

--- ts/components/screens/IdpCustomContextualHelpContent.tsx
+++ ts/components/screens/IdpCustomContextualHelpContent.tsx
@@ -7,7 +7,7 @@ import {
 import I18n from "../../i18n";
 import { Idp } from "../../../definitions/content/Idp";
 import { handleItemOnPress } from "../../utils/url";
-import LegacyMarkdown from "../ui/Markdown/LegacyMarkdown";
+import IOMarkdown from "../IOMarkdown";
 import EmailCallCTA from "./EmailCallCTA";
 
 type Props = Readonly<{
@@ -19,72 +19,56 @@ const IdpCustomContextualHelpContent = (idpTextData: Idp) => ({
   body: <IdpCustomContextualHelpBody idpTextData={idpTextData} />
 });
 
-const IdpCustomContextualHelpBody: React.FunctionComponent<Props> = props => {
-  const { idpTextData } = props;
-
-  const [isMarkdown1Loaded, setIsMarkdown1Loaded] = React.useState(false);
-  const [isMarkdown2Loaded, setIsMarkdown2Loaded] = React.useState(false);
-
-  return (
-    <React.Fragment>
-      {/** Recover credentials */}
-      <LegacyMarkdown onLoadEnd={() => setIsMarkdown1Loaded(true)}>
-        {idpTextData.recover_username
+const IdpCustomContextualHelpBody = ({ idpTextData }: Props) => (
+  <React.Fragment>
+    {/** Recover credentials */}
+    <IOMarkdown
+      content={
+        idpTextData.recover_username
           ? I18n.t("authentication.idp_login.dualRecoverDescription")
-          : I18n.t("authentication.idp_login.recoverDescription")}
-      </LegacyMarkdown>
+          : I18n.t("authentication.idp_login.recoverDescription")
+      }
+    />
 
-      <VSpacer size={16} />
-      {isMarkdown1Loaded && (
-        <React.Fragment>
-          {idpTextData.recover_username && (
-            <React.Fragment>
-              <ButtonSolid
-                label={I18n.t("authentication.idp_login.recoverUsername")}
-                accessibilityLabel={I18n.t(
-                  "authentication.idp_login.recoverUsername"
-                )}
-                onPress={handleItemOnPress(idpTextData.recover_username)}
-              />
-              <VSpacer size={16} />
-            </React.Fragment>
-          )}
-          <ButtonSolid
-            label={I18n.t("authentication.idp_login.recoverPassword")}
-            accessibilityLabel={I18n.t(
-              "authentication.idp_login.recoverUsername"
-            )}
-            onPress={handleItemOnPress(idpTextData.recover_password)}
-          />
-        </React.Fragment>
-      )}
+    <VSpacer size={16} />
 
-      {/** Idp contacts */}
-      <VSpacer size={16} />
-      <LegacyMarkdown onLoadEnd={() => setIsMarkdown2Loaded(true)}>
-        {idpTextData.description}
-      </LegacyMarkdown>
-      <VSpacer size={16} />
-      {isMarkdown2Loaded && (
-        <React.Fragment>
-          <EmailCallCTA phone={idpTextData.phone} email={idpTextData.email} />
-          <VSpacer size={16} />
-          {idpTextData.helpdesk_form && (
-            <React.Fragment>
-              <ButtonOutline
-                label={I18n.t("authentication.idp_login.openTicket")}
-                accessibilityLabel={I18n.t(
-                  "authentication.idp_login.recoverUsername"
-                )}
-                onPress={handleItemOnPress(idpTextData.helpdesk_form)}
-              />
-              <VSpacer size={16} />
-            </React.Fragment>
+    {idpTextData.recover_username && (
+      <React.Fragment>
+        <ButtonSolid
+          label={I18n.t("authentication.idp_login.recoverUsername")}
+          accessibilityLabel={I18n.t(
+            "authentication.idp_login.recoverUsername"
           )}
-        </React.Fragment>
-      )}
-    </React.Fragment>
-  );
-};
+          onPress={handleItemOnPress(idpTextData.recover_username)}
+        />
+        <VSpacer size={16} />
+      </React.Fragment>
+    )}
+    <ButtonSolid
+      label={I18n.t("authentication.idp_login.recoverPassword")}
+      accessibilityLabel={I18n.t("authentication.idp_login.recoverUsername")}
+      onPress={handleItemOnPress(idpTextData.recover_password)}
+    />
+
+    {/** Idp contacts */}
+    <VSpacer size={16} />
+    <IOMarkdown content={idpTextData.description} />
+    <VSpacer size={16} />
+    <EmailCallCTA phone={idpTextData.phone} email={idpTextData.email} />
+    <VSpacer size={16} />
+    {idpTextData.helpdesk_form && (
+      <React.Fragment>
+        <ButtonOutline
+          label={I18n.t("authentication.idp_login.openTicket")}
+          accessibilityLabel={I18n.t(
+            "authentication.idp_login.recoverUsername"
+          )}
+          onPress={handleItemOnPress(idpTextData.helpdesk_form)}
+        />
+        <VSpacer size={16} />
+      </React.Fragment>
+    )}
+  </React.Fragment>
+);
 
 export default IdpCustomContextualHelpContent;
