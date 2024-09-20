1b33ed47ba2f185c1edbfae0bccc0107ecf0ffb8
Alessandro <alessandro.izzo998@gmail.com>
2024-09-20 10:56:39 +0200

    chore: [IOBP-553] IDPay definitions adaption (#5926)
    ## Short description
    This PR adapts the IDPay specifications and definitions with the new
    openAPIs provided
    
    ## List of changes proposed in this pull request
    - Updated the openAPI definitions in the package.json file
    - Renamed the amount and and reward attribute with the new one
    implemented;

--- ts/features/idpay/onboarding/xstate/__tests__/services.test.ts
+++ ts/features/idpay/onboarding/xstate/__tests__/services.test.ts
@@ -7,8 +7,10 @@ import {
   StatusEnum
 } from "../../../../../../definitions/idpay/OnboardingStatusDTO";
 import { RequiredCriteriaDTO } from "../../../../../../definitions/idpay/RequiredCriteriaDTO";
-import { _typeEnum as BoolTypeEnum } from "../../../../../../definitions/idpay/SelfConsentBoolDTO";
-import { SelfConsentDTO } from "../../../../../../definitions/idpay/SelfConsentDTO";
+import {
+  _typeEnum as BoolTypeEnum,
+  SelfConsentBoolDTO
+} from "../../../../../../definitions/idpay/SelfConsentBoolDTO";
 import {
   SelfConsentMultiDTO,
   _typeEnum as MultiTypeEnum
@@ -90,14 +92,14 @@ const T_MULTI_CONSENTS_ANSWERS: Record<number, SelfConsentMultiDTO> = {
   }
 };
 
-const T_ACCEPTED_SELF_DECLARATION_LIST: Array<SelfConsentDTO> = [
+const T_ACCEPTED_SELF_DECLARATION_LIST: Array<SelfConsentBoolDTO> = [
   {
     _type: BoolTypeEnum.boolean,
     code: "3",
     accepted: true
   },
   ...Object.values(T_MULTI_CONSENTS_ANSWERS)
-] as Array<SelfConsentDTO>;
+] as Array<SelfConsentBoolDTO>;
 
 describe("IDPay Onboarding machine services", () => {
   const services = createServicesImplementation(
