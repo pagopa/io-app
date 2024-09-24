ec76d94e55626bf5d188ae740edaa5a1bd77d308
ChrisMattew <cristian.matteu@spindox.it>
2024-09-24 12:52:07 +0200

    feat(IngressScreen): improve tests

--- ts/features/ingress/__test__/IngressScreen.test.tsx
+++ ts/features/ingress/__test__/IngressScreen.test.tsx
@@ -2,10 +2,15 @@
 import { createStore } from "redux";
 import * as netInfo from "@react-native-community/netinfo";
 import { waitFor } from "@testing-library/react-native";
+import { act } from "react-test-renderer";
 import { IngressScreen } from "../screens/IngressScreen";
 import { renderScreenWithNavigationStoreContext } from "../../../utils/testWrapper";
 import { applicationChangeState } from "../../../store/actions/application";
 import { appReducer } from "../../../store/reducers";
+import I18n from "../../../i18n";
+import * as backendStatusSelectors from "../../../store/reducers/backendStatus";
+
+jest.useFakeTimers();
 
 describe(IngressScreen, () => {
   it("Should be defined", () => {
@@ -31,12 +36,11 @@ describe(IngressScreen, () => {
     });
   });
   describe("IngressScreen with device connection disabled", () => {
-    beforeAll(() =>
-      // @ts-ignore
-      jest.spyOn(netInfo, "fetch").mockResolvedValue({ isConnected: false })
-    );
-
     it("Should display OperationResultsScreenContent", async () => {
+      jest
+        .spyOn(netInfo, "fetch")
+        // @ts-ignore
+        .mockResolvedValueOnce({ isConnected: false });
       const { findByTestId, queryByTestId } = renderComponent();
       // Before the effect is resolved the loader should be displayed
       expect(queryByTestId("ingress-screen-loader-id")).toBeTruthy();
@@ -48,7 +52,88 @@ describe(IngressScreen, () => {
       expect(operationResults).toBeTruthy();
     });
   });
+  describe("IngressScreen when slowdowns occur", () => {
+    beforeEach(() => {
+      jest.clearAllTimers();
+      jest.clearAllMocks();
+    });
+    it("Should update LoadingScreenContent contentTitle after 5 sec and display the cdn unreachable blocking screen after 10", async () => {
+      const {
+        getDeviceBlockingScreen,
+        queryByText,
+        getFirstText,
+        getSecondText
+      } = await renderComponentWithSlowdowns();
+
+      await act(() => {
+        jest.advanceTimersByTime(5000);
+      });
+
+      expect(getDeviceBlockingScreen()).toBeTruthy();
+      expect(
+        queryByText(I18n.t("startup.cdn_unreachable_results_screen.title"))
+      ).toBeTruthy();
+      expect(
+        queryByText(I18n.t("startup.slowdowns_results_screen.title"))
+      ).toBeNull();
+      expect(getFirstText()).toBeNull();
+      expect(getSecondText()).toBeNull();
+    });
+    it("Should update LoadingScreenContent contentTitle after 5s and display the slowdowns blocking screen after 10s", async () => {
+      jest
+        .spyOn(backendStatusSelectors, "isBackendStatusLoadedSelector")
+        .mockImplementation(() => true);
+
+      const {
+        getDeviceBlockingScreen,
+        queryByText,
+        getFirstText,
+        getSecondText
+      } = await renderComponentWithSlowdowns();
+
+      await act(() => {
+        jest.advanceTimersByTime(5000);
+      });
+
+      expect(getDeviceBlockingScreen()).toBeTruthy();
+      expect(
+        queryByText(I18n.t("startup.cdn_unreachable_results_screen.title"))
+      ).toBeNull();
+      expect(
+        queryByText(I18n.t("startup.slowdowns_results_screen.title"))
+      ).toBeTruthy();
+      expect(getFirstText()).toBeNull();
+      expect(getSecondText()).toBeNull();
+    });
+  });
 });
+
+const renderComponentWithSlowdowns = async () => {
+  const { queryByText, queryByTestId } = renderComponent();
+  const getFirstText = () => queryByText(I18n.t("startup.title"));
+  const getSecondText = () => queryByText(I18n.t("startup.title2"));
+  const getDeviceBlockingScreen = () =>
+    queryByTestId("device-blocking-screen-id");
+
+  expect(getFirstText()).toBeTruthy();
+  expect(getSecondText()).toBeNull();
+
+  await act(() => {
+    jest.advanceTimersByTime(5000);
+  });
+
+  expect(getFirstText()).toBeNull();
+  expect(getSecondText()).toBeTruthy();
+  expect(getDeviceBlockingScreen()).toBeNull();
+
+  return {
+    queryByText,
+    queryByTestId,
+    getFirstText,
+    getSecondText,
+    getDeviceBlockingScreen
+  };
+};
 
 const renderComponent = () => {
   const initialState = appReducer(undefined, applicationChangeState("active"));
