f9a8b44ba101d3913735a6144370b4c318580c29
Gianluca Spada <gianluca.spada@spindox.it>
2024-10-03 12:58:53 +0200

    chore(IT Wallet): [SIW-1665] Hide eID card in wallet (#6214)
    > [!WARNING]  
    > Depends on https://github.com/pagopa/io-react-native-wallet/pull/145
    
    ## Short description
    This PR hides the eID card in the wallet screen. It also adds a bottom
    sheet to display information on the digital identity.
    
    ## List of changes proposed in this pull request
    - Removed the eID card from the wallet screen
    - Added a banner to inform the user the wallet is active
    - Added a bottom sheet component to display the eID claims and the
    issuance date
    - Added `expiration` and `issuedAt` properties to `StoredCredential`
    type, with Redux Persist migration
    
    ## How to test
    If you have an **existing wallet**, check the following:
    - The migration worked, and every credential in the Redux Store now has
    `issuedAt` and `expiration`
    - The eID card is not visible in the wallet screen, only other
    credentials
    - The bottom sheet that appears pressing on "Cos'è?" properly displays
    the issuance date
    
    Reset the wallet and get all credentials, then check the same points as
    above.
    
    ---------
    
    Co-authored-by: Federico Mastrini <federicomastrini93@gmail.com>

---
+++ ts/features/itwallet/common/components/ItwWalletReadyBanner.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Banner } from "@pagopa/io-app-design-system";
+import I18n from "../../../../i18n";
+import { useIONavigation } from "../../../../navigation/params/AppParamsList";
+import { ITW_ROUTES } from "../../navigation/routes";
+import { useIOSelector } from "../../../../store/hooks";
+import { itwIsWalletEmptySelector } from "../../credentials/store/selectors";
+import { itwLifecycleIsValidSelector } from "../../lifecycle/store/selectors";
+
+export const ItwWalletReadyBanner = () => {
+  const isItwValid = useIOSelector(itwLifecycleIsValidSelector);
+  const isWalletEmpty = useIOSelector(itwIsWalletEmptySelector);
+
+  const navigation = useIONavigation();
+
+  if (!isItwValid || !isWalletEmpty) {
+    return null;
+  }
+
+  const handleOnPress = () => {
+    navigation.navigate(ITW_ROUTES.MAIN, {
+      screen: ITW_ROUTES.ONBOARDING
+    });
+  };
+
+  return (
+    <Banner
+      testID="itwWalletReadyBannerTestID"
+      title={I18n.t("features.itWallet.issuance.eidResult.success.title")}
+      content={I18n.t("features.itWallet.issuance.eidResult.success.subtitle")}
+      action={I18n.t(
+        "features.itWallet.issuance.eidResult.success.actions.continueAlt"
+      )}
+      pictogramName="itWallet"
+      color="turquoise"
+      size="big"
+      onPress={handleOnPress}
+    />
+  );
+};
