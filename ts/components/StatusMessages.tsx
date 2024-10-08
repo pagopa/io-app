30e2a386900c043fb7bf26367c3a10175f089c1a
Cristiano Tofani <cri.tofani@gmail.com>
2024-10-08 10:25:21 +0200

    chore: [IOPLT-661] New status banner component (#6096)
    > [!note]
    > This PR depends on #6086
    ## Short description
    This PR introduces the new StatusMessages component to wrapp all the app
    reading the status messages available for the current route.
    
    ## List of changes proposed in this pull request
    - new StatusMessages component
    - Fixes on header space handling
    
    ## How to test
    Check the proper visualization of status messages on each route
    
    
    https://github.com/user-attachments/assets/e99af26d-9662-44af-a79d-cbba487a145b
    
    ---------
    
    Co-authored-by: Andrea <andrea.piai@pagopa.it>

---
+++ ts/components/StatusMessages.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  AlertEdgeToEdgeProps,
+  AlertEdgeToEdgeWrapper
+} from "@pagopa/io-app-design-system";
+import { useStatusAlertProps } from "../hooks/useStatusAlertProps";
+
+type StatusMessagesProps = React.PropsWithChildren;
+
+export const StatusMessages = ({ children }: StatusMessagesProps) => {
+  const alertProps: AlertEdgeToEdgeProps | undefined = useStatusAlertProps();
+
+  return (
+    <AlertEdgeToEdgeWrapper alertProps={alertProps}>
+      {children}
+    </AlertEdgeToEdgeWrapper>
+  );
+};
