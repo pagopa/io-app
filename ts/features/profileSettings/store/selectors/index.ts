96644d1fa019dcd0f5d872c76cb32821daa3aa2a
forrest57 <tomba808@gmail.com>
2024-10-08 15:52:46 +0200

    added banner UI and logic, tests and A11y coming in next PR

--- ts/features/profileSettings/store/selectors/index.ts
+++ ts/features/profileSettings/store/selectors/index.ts
@@ -1,7 +1,19 @@
 import { GlobalState } from "../../../../store/reducers/types";
+import { areNotificationPermissionsEnabled } from "../../../pushNotifications/store/reducers/environment";
 
 export const showProfileBannerSelector = (state: GlobalState) =>
   state.features.profileSettings.showProfileBanner;
+
+export const profileBannerToShowSelector = (state: GlobalState) => {
+  const isProfileBannerEnabled = showProfileBannerSelector(state);
+  const notificationsEnabled = areNotificationPermissionsEnabled(state);
+
+  return !notificationsEnabled
+    ? "NOTIFICATIONS"
+    : isProfileBannerEnabled
+    ? "PROFILE_BANNER"
+    : undefined;
+};
 
 export const hasUserAcknowledgedSettingsBannerSelector = (state: GlobalState) =>
   state.features.profileSettings.hasUserAcknowledgedSettingsBanner;
