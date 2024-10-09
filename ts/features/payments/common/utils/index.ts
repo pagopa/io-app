e43b336c188e559e1160eb9e86f5211f002f59aa
Alessandro Izzo <alessandro.izzo998@gmail.com>
2024-10-09 12:24:42 +0200

    chore: add ascending order when choosing "amount" sorting

--- ts/features/payments/common/utils/index.ts
+++ ts/features/payments/common/utils/index.ts
@@ -127,7 +127,11 @@ export const getSortedPspList = (
     case "name":
       return _.orderBy(pspList, psp => psp.pspBusinessName);
     case "amount":
-      return _.orderBy(pspList, psp => psp.taxPayerFee);
+      return _.orderBy(
+        pspList,
+        ["taxPayerFee", "pspBusinessName"],
+        ["asc", "asc"]
+      );
     case "default":
     default:
       return _.orderBy(
