bfe3a167dea80947e167604a8f1d402d2db2e28b
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2024-10-25 16:13:23 +0200

    refactor(payments): update navigation route

--- ts/features/payments/bizEventsTransaction/navigation/navigator.tsx
+++ ts/features/payments/bizEventsTransaction/navigation/navigator.tsx
@@ -5,9 +5,10 @@ import {
 } from "@react-navigation/stack";
 import React from "react";
 import { isGestureEnabled } from "../../../../utils/navigation";
+import PaymentsLoaderScreen from "../../common/components/PaymentsLoaderScreen";
+import WalletTransactionCartItemDetailsScreen from "../screens/PaymentsTransactionBizEventsCartItemDetailsScreen";
 import { PaymentsTransactionBizEventsDetailsScreen } from "../screens/PaymentsTransactionBizEventsDetailsScreen";
 import { PaymentsTransactionBizEventsListScreen } from "../screens/PaymentsTransactionBizEventsListScreen";
-import WalletTransactionCartItemDetailsScreen from "../screens/PaymentsTransactionBizEventsCartItemDetailsScreen";
 import PaymentsTransactionBizEventsPreviewScreen from "../screens/PaymentsTransactionBizEventsPreviewScreen";
 import { PaymentsTransactionBizEventsParamsList } from "./params";
 import { PaymentsTransactionBizEventsRoutes } from "./routes";
@@ -48,6 +49,13 @@ export const PaymentsTransactionBizEventsNavigator = () => (
       }
       component={PaymentsTransactionBizEventsPreviewScreen}
       options={{ gestureEnabled: isGestureEnabled }}
+    />
+    <Stack.Screen
+      name={
+        PaymentsTransactionBizEventsRoutes.PAYMENT_TRANSACTION_BIZ_EVENTS_LOADING_SCREEN
+      }
+      options={{ gestureEnabled: isGestureEnabled, headerShown: false }}
+      component={PaymentsLoaderScreen}
     />
   </Stack.Navigator>
 );
