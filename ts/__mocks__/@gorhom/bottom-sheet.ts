a8d92f8167a4f3f350ddf726f45d162495d8126e
ChrisMattew <cristian.matteu@spindox.it>
2024-11-06 11:19:27 +0100

    chore(bottom-sheet mock): add reference about the source of this file

--- ts/__mocks__/@gorhom/bottom-sheet.ts
+++ ts/__mocks__/@gorhom/bottom-sheet.ts
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable max-classes-per-file */
+/**
+ * This configuration was taken from the official library;
+ * it is not present in the current version in use
+ * but may be replaced with the first upgrade that introduces it.
+ *
+ * reference: https://github.com/gorhom/react-native-bottom-sheet/blob/master/mock.js
+ */
 import React, { PropsWithChildren } from "react";
 
 const NOOP = () => {};
