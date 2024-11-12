518c3fb0e40b020ebed1dda706101edf34365d2d
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2024-11-12 16:25:32 +0100

    refactor: add new outcome screen on pspList failed

--- ts/features/payments/checkout/components/WalletPaymentFailureDetail.tsx
+++ ts/features/payments/checkout/components/WalletPaymentFailureDetail.tsx
@@ -9,17 +9,19 @@ import {
   AppParamsList,
   IOStackNavigationProp
 } from "../../../../navigation/params/AppParamsList";
-import { usePaymentFailureSupportModal } from "../hooks/usePaymentFailureSupportModal";
-import { WalletPaymentFailure } from "../types/WalletPaymentFailure";
-import { useOnFirstRender } from "../../../../utils/hooks/useOnFirstRender";
-import { paymentCompletedSuccess } from "../store/actions/orchestration";
 import { useIODispatch, useIOSelector } from "../../../../store/hooks";
+import { useOnFirstRender } from "../../../../utils/hooks/useOnFirstRender";
 import {
-  selectOngoingPaymentHistory,
-  paymentAnalyticsDataSelector
+  paymentAnalyticsDataSelector,
+  selectOngoingPaymentHistory
 } from "../../history/store/selectors";
 import * as analytics from "../analytics";
+import { usePaymentFailureSupportModal } from "../hooks/usePaymentFailureSupportModal";
+import { PaymentsCheckoutRoutes } from "../navigation/routes";
+import { paymentsCalculatePaymentFeesAction } from "../store/actions/networking";
+import { paymentCompletedSuccess } from "../store/actions/orchestration";
 import { selectWalletPaymentCurrentStep } from "../store/selectors";
+import { WalletPaymentFailure } from "../types/WalletPaymentFailure";
 import { getPaymentPhaseFromStep } from "../utils";
 
 type Props = {
@@ -51,6 +53,13 @@ const WalletPaymentFailureDetail = ({ failure }: Props) => {
     supportModal.present();
   };
 
+  const handleChangePaymentMethod = () => {
+    dispatch(paymentsCalculatePaymentFeesAction.cancel());
+    navigation.replace(PaymentsCheckoutRoutes.PAYMENT_CHECKOUT_NAVIGATOR, {
+      screen: PaymentsCheckoutRoutes.PAYMENT_CHECKOUT_MAKE
+    });
+  };
+
   const closeAction: OperationResultScreenContentProps["action"] = {
     label: I18n.t("global.buttons.close"),
     testID: "wallet-payment-failure-close-button",
@@ -72,6 +81,18 @@ const WalletPaymentFailureDetail = ({ failure }: Props) => {
     action: closeAction,
     secondaryAction: contactSupportAction
   };
+
+  const selectOtherPaymentMethodAction: OperationResultScreenContentProps["action"] =
+    {
+      label: I18n.t(
+        "wallet.payment.failure.PAYMENT_METHOD_NOT_AVAILABLE_ERROR.action"
+      ),
+      testID: "wallet-payment-failure-go-back-button",
+      accessibilityLabel: I18n.t(
+        "wallet.payment.failure.PAYMENT_METHOD_NOT_AVAILABLE_ERROR.action"
+      ),
+      onPress: handleChangePaymentMethod
+    };
 
   const getPropsFromFailure = ({
     faultCodeCategory
@@ -147,6 +168,18 @@ const WalletPaymentFailureDetail = ({ failure }: Props) => {
           action: closeAction,
           secondaryAction: contactSupportAction
         };
+      case "PAYMENT_METHOD_NOT_AVAILABLE_ERROR":
+        return {
+          pictogram: "cardIssue",
+          title: I18n.t(
+            "wallet.payment.failure.PAYMENT_METHOD_NOT_AVAILABLE_ERROR.title"
+          ),
+          subtitle: I18n.t(
+            "wallet.payment.failure.PAYMENT_METHOD_NOT_AVAILABLE_ERROR.subtitle"
+          ),
+          action: selectOtherPaymentMethodAction
+        };
+
       default:
         return genericErrorProps;
     }
