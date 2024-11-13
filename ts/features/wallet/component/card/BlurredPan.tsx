bce129cbae3f4a6acfc7b549c15c8e80166ba6f5
Damiano Plebani <damiano.plebani@pagopa.it>
2024-11-13 15:07:09 +0100

    Improve React import as suggested by @ChrisMattew

--- ts/features/wallet/component/card/BlurredPan.tsx
+++ ts/features/wallet/component/card/BlurredPan.tsx
@@ -1,6 +1,5 @@
 import { BodyMonospace } from "@pagopa/io-app-design-system";
-import * as React from "react";
-import { ComponentProps } from "react";
+import React, { ComponentProps } from "react";
 
 /**
  * Represent a blurred pan using a consistent style within different components
