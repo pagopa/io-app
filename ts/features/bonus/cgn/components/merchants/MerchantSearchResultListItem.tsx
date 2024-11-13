dc3bcba89ee2276b63f9f94fca4c050deba1c96d
Frederik Batuna <gobi301@gmail.com>
2024-11-13 11:01:19 +0100

    feat: [PE-630,PE-758] CGN partner search screen (#6336)
    ## Short description
    Added a partner's search new screen.
    Refer to screens in https://pagopa.atlassian.net/browse/PE-630
    description.
    See https://pagopa.atlassian.net/browse/PE-758 for no search results
    screen
    
    ## List of changes proposed in this pull request
    - add labels
    - remove search functionality in discount list by operators
    - add new search page
    
    ## How to test
    
    To test with dev-api-server this PR is needed
    https://github.com/pagopa/io-dev-api-server/pull/432
    I did not made this PR depend on it since it is testable using other
    backend environments
    
    Going to the new search screen option 1:
    - login into the app
    - go to CGN card trough wallet (add it if necessary)
    - press discover opportunities
    - press magnifying icon in the top bar
    
    Going to the new search screen option 2:
    - login into the app
    - go to CGN card trough wallet (add it if necessary)
    - press discover opportunities
    - go to any category
    - press magnifying icon in the top bar
    
    Testing the search screen
    
    - Go to the new search screen
    - search for partner
    - check if you see the results
    - press on a result and check if it opens the correct screen
    
    | overview |
    | - |
    | <video
    src="https://github.com/user-attachments/assets/ded627b4-a068-43c3-8a15-c3b8af2ae998"/>
    |
    
    ---------
    
    Co-authored-by: Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
    Co-authored-by: Alessandro <alessandro.izzo998@gmail.com>
    Co-authored-by: mariateresaventura <90319761+mariateresaventura@users.noreply.github.com>

---
+++ ts/features/bonus/cgn/components/merchants/MerchantSearchResultListItem.tsx
@@ -0,0 +1,102 @@
+import {
+  Badge,
+  Body,
+  H6,
+  IOColors,
+  IOStyles,
+  ListItemNav
+} from "@pagopa/io-app-design-system";
+import React from "react";
+import { Keyboard, Text, View, StyleSheet } from "react-native";
+import I18n from "../../../../../i18n";
+import { useIONavigation } from "../../../../../navigation/params/AppParamsList";
+import CGN_ROUTES from "../../navigation/routes";
+import { SearchItem } from "../../../../../../definitions/cgn/merchants/SearchItem";
+import { highlightSearchText } from "../../../../../utils/highlightSearchText";
+
+export const TEXT_LEGNTH_WITH_BADGE = 60;
+export const TEXT_LEGNTH_WITHOUT_BADGE = 100;
+
+const styles = StyleSheet.create({
+  listItemTextContainer: {
+    flexGrow: 1,
+    flexShrink: 1
+  },
+  highlightYes: {
+    backgroundColor: IOColors["turquoise-150"]
+  }
+});
+
+export function MerchantSearchResultListItem({
+  item,
+  searchText
+}: {
+  item: SearchItem;
+  searchText: string;
+}) {
+  const navigation = useIONavigation();
+  return (
+    <View style={IOStyles.horizontalContentPadding}>
+      <ListItemNav
+        onPress={() => {
+          navigation.navigate(CGN_ROUTES.DETAILS.MAIN, {
+            screen: CGN_ROUTES.DETAILS.MERCHANTS.DETAIL,
+            params: {
+              merchantID: item.id
+            }
+          });
+          Keyboard.dismiss();
+        }}
+        value={
+          <View style={IOStyles.rowSpaceBetween}>
+            <View style={styles.listItemTextContainer}>
+              <H6>{highlightText({ text: item.name, searchText })}</H6>
+              <Body numberOfLines={2} ellipsizeMode="tail">
+                {highlightText({
+                  text: item.description,
+                  searchText,
+                  estimatedTextLengthToDisplay: item.newDiscounts
+                    ? TEXT_LEGNTH_WITH_BADGE
+                    : TEXT_LEGNTH_WITHOUT_BADGE
+                })}
+              </Body>
+            </View>
+            {item.newDiscounts && (
+              <View style={[IOStyles.rowSpaceBetween, IOStyles.alignCenter]}>
+                <Badge
+                  variant="purple"
+                  text={I18n.t("bonus.cgn.merchantsList.news")}
+                />
+              </View>
+            )}
+          </View>
+        }
+        accessibilityLabel={item.name}
+      />
+    </View>
+  );
+}
+
+function highlightText({
+  searchText,
+  text,
+  estimatedTextLengthToDisplay
+}: {
+  text: string;
+  searchText: string;
+  estimatedTextLengthToDisplay?: number;
+}) {
+  const chunks = highlightSearchText({
+    text,
+    searchText,
+    estimatedTextLengthToDisplay
+  });
+  return chunks.map((chunk, index) => (
+    <Text
+      key={index}
+      style={chunk.highlighted ? styles.highlightYes : undefined}
+    >
+      {chunk.text}
+    </Text>
+  ));
+}
