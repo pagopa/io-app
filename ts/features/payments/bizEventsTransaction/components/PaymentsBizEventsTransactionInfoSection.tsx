510392b9197651720d8abb62952540985e350849
Damiano Plebani <damiano.plebani@pagopa.it>
2024-11-21 15:06:05 +0100

    Add the new `WalletTransactionReceiptDivider` to avoid code repetition

--- ts/features/payments/bizEventsTransaction/components/PaymentsBizEventsTransactionInfoSection.tsx
+++ ts/features/payments/bizEventsTransaction/components/PaymentsBizEventsTransactionInfoSection.tsx
@@ -1,7 +1,3 @@
-import { capitalize } from "lodash";
-import * as React from "react";
-import { StyleSheet, View } from "react-native";
-import Placeholder from "rn-placeholder";
 import {
   Alert,
   Divider,
@@ -15,16 +11,21 @@ import {
   useIOTheme,
   VSpacer
 } from "@pagopa/io-app-design-system";
-import Svg, { Path } from "react-native-svg";
+import { capitalize } from "lodash";
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import Placeholder from "rn-placeholder";
+
+import { OriginEnum } from "../../../../../definitions/pagopa/biz-events/InfoNotice";
+import { NoticeDetailResponse } from "../../../../../definitions/pagopa/biz-events/NoticeDetailResponse";
+import { WalletInfo } from "../../../../../definitions/pagopa/biz-events/WalletInfo";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import I18n from "../../../../i18n";
-import { format } from "../../../../utils/dates";
 import { clipboardSetStringWithFeedback } from "../../../../utils/clipboard";
-import { WalletInfo } from "../../../../../definitions/pagopa/biz-events/WalletInfo";
-import { getPayerInfoLabel } from "../utils";
-import { NoticeDetailResponse } from "../../../../../definitions/pagopa/biz-events/NoticeDetailResponse";
-import { OriginEnum } from "../../../../../definitions/pagopa/biz-events/InfoNotice";
+import { format } from "../../../../utils/dates";
 import { capitalizeTextName } from "../../../../utils/strings";
+import { WalletTransactionReceiptDivider } from "../../transaction/components/WalletTransactionReceiptDivider";
+import { getPayerInfoLabel } from "../utils";
 
 type PaymentsBizEventsTransactionInfoSectionProps = {
   transaction?: NoticeDetailResponse;
@@ -57,19 +58,7 @@ const PaymentsBizEventsTransactionInfoSection = ({
 
   return (
     <>
-      {/* TransactionReceiptDivider */}
-      <Svg
-        width="100%"
-        height="24"
-        viewBox="0 0 360 24"
-        preserveAspectRatio="xMin slice"
-        fill="none"
-      >
-        <Path
-          d="M0 0H360V20.5645L348.75 24L337.5 20.5645L326.25 24L315 20.5645L303.75 24L292.5 20.5645L281.25 24L270 20.5645L258.75 24L247.5 20.5645L236.25 24L225 20.5645L213.75 24L202.5 20.5645L191.25 24L180 20.5645L168.75 24L157.5 20.5645L146.25 24L135 20.5645L123.75 24L112.5 20.5645L101.25 24L90 20.5645L78.75 24L67.5 20.5645L56.25 24L45 20.5645L33.75 24L22.5 20.5645L11.25 24L0 20.5645L0 0Z"
-          fill={IOColors[theme["appBackground-primary"]]}
-        />
-      </Svg>
+      <WalletTransactionReceiptDivider />
       <View style={styles.container}>
         <View style={[styles.contentCard, { backgroundColor }]}>
           <ListItemHeader
--- ts/features/payments/transaction/components/WalletTransactionInfoSection.tsx
+++ ts/features/payments/transaction/components/WalletTransactionInfoSection.tsx
@@ -12,7 +12,6 @@ import {
 } from "@pagopa/io-app-design-system";
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
-import Svg, { Path } from "react-native-svg";
 import Placeholder from "rn-placeholder";
 import { IOStyles } from "../../../../components/core/variables/IOStyles";
 import I18n from "../../../../i18n";
@@ -21,6 +20,7 @@ import { isDesignSystemEnabledSelector } from "../../../../store/reducers/persis
 import { Psp, Transaction } from "../../../../types/pagopa";
 import { clipboardSetStringWithFeedback } from "../../../../utils/clipboard";
 import { format } from "../../../../utils/dates";
+import { WalletTransactionReceiptDivider } from "./WalletTransactionReceiptDivider";
 
 type WalletTransactionInfoSectionProps = {
   transaction?: Transaction;
@@ -55,19 +55,7 @@ const WalletTransactionInfoSection = ({
 
   return (
     <>
-      {/* TransactionReceiptDivider */}
-      <Svg
-        width="100%"
-        height="24"
-        viewBox="0 0 360 24"
-        preserveAspectRatio="xMin slice"
-        fill="none"
-      >
-        <Path
-          d="M0 0H360V20.5645L348.75 24L337.5 20.5645L326.25 24L315 20.5645L303.75 24L292.5 20.5645L281.25 24L270 20.5645L258.75 24L247.5 20.5645L236.25 24L225 20.5645L213.75 24L202.5 20.5645L191.25 24L180 20.5645L168.75 24L157.5 20.5645L146.25 24L135 20.5645L123.75 24L112.5 20.5645L101.25 24L90 20.5645L78.75 24L67.5 20.5645L56.25 24L45 20.5645L33.75 24L22.5 20.5645L11.25 24L0 20.5645L0 0Z"
-          fill={IOColors[theme["appBackground-primary"]]}
-        />
-      </Svg>
+      <WalletTransactionReceiptDivider />
       <View style={styles.container}>
         <View style={[styles.contentCard, { backgroundColor }]}>
           <ListItemHeader
