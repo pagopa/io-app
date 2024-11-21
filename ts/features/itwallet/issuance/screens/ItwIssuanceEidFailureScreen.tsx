3cfa934fdf9cdf2dcd638fdc9135711454101dc2
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2024-11-21 10:50:40 +0100

    fix: refactor on press action

--- ts/features/itwallet/issuance/screens/ItwIssuanceEidFailureScreen.tsx
+++ ts/features/itwallet/issuance/screens/ItwIssuanceEidFailureScreen.tsx
@@ -1,7 +1,7 @@
 import * as O from "fp-ts/lib/Option";
 import { constNull, pipe } from "fp-ts/lib/function";
 import React, { useEffect } from "react";
-import { Linking } from "react-native";
+import { IOToast } from "@pagopa/io-app-design-system";
 import {
   OperationResultScreenContent,
   OperationResultScreenContentProps
@@ -27,6 +27,7 @@ import {
   trackItwUnsupportedDevice,
   trackWalletCreationFailed
 } from "../../analytics";
+import { openWebUrl } from "../../../../utils/url";
 
 export const ItwIssuanceEidFailureScreen = () => {
   const failureOption =
@@ -118,7 +119,9 @@ const ContentView = ({ failure }: ContentViewProps) => {
           "features.itWallet.unsupportedDevice.error.secondaryAction"
         ),
         onPress: () =>
-          Linking.openURL("https://io.italia.it/documenti-su-io/faq/#n1_12")
+          openWebUrl("https://io.italia.it/documenti-su-io/faq/#n1_12", () =>
+            IOToast.error(I18n.t("global.jserror.title"))
+          )
       }
     },
     [IssuanceFailureType.NOT_MATCHING_IDENTITY]: {
