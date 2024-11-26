7759895033a4d9db12823fd4d9d632c1d948b809
forrest57 <tomba808@gmail.com>
2024-11-26 16:42:33 +0100

    oversight

--- ts/features/pushNotifications/store/reducers/__tests__/environment.test.ts
+++ ts/features/pushNotifications/store/reducers/__tests__/environment.test.ts
@@ -104,9 +104,7 @@ describe("environmentReducer", () => {
       },
       setEngagementScreenShown()
     );
-    expect(userBehaviourState.pushNotificationPermissionsRequestDuration).toBe(
-      100
-    );
+    expect(userBehaviourState.engagementScreenShownThisSession).toBe(true);
   });
 });
 
