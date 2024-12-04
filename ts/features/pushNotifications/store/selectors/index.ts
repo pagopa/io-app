7228aaf204d6569fa0194d0bd1c0c698d2110159
forrest57 <tomba808@gmail.com>
2024-12-04 15:48:01 +0100

    addition of required logic and UI

--- ts/features/pushNotifications/store/selectors/index.ts
+++ ts/features/pushNotifications/store/selectors/index.ts
@@ -1,6 +1,10 @@
 import { GlobalState } from "../../../../store/reducers/types";
 import { userFromSuccessLoginSelector } from "../../../login/info/store/selectors";
+import { messageListForCategorySelector } from "../../../messages/store/reducers/allPaginated";
+import { UIMessage } from "../../../messages/types";
 import { areNotificationPermissionsEnabled } from "../reducers/environment";
+
+const NEW_MESSAGES_COUNT_TO_RESET_FORCE_DISMISS = 4;
 
 export const hasUserSeenSystemNotificationsPromptSelector = (
   state: GlobalState
@@ -27,10 +31,40 @@ export const hasUserSeenSystemNotificationsPromptSelector = (
   }
   return false;
 };
+export const timesPushNotificationBannerDismissedSelector = (
+  state: GlobalState
+) => state.notifications.userBehaviour.pushNotificationsBanner.timesDismissed;
+
+export const shouldResetNotificationBannerDismissStateSelector = (
+  state: GlobalState
+) => {
+  const forceDismissDate =
+    state.notifications.userBehaviour.pushNotificationsBanner
+      .forceDismissionDate;
+  const messagesList = messageListForCategorySelector(state, "INBOX");
+
+  if (messagesList === undefined || forceDismissDate === undefined) {
+    return false;
+  }
+
+  const newUnreadCount = messagesList.filter(
+    (message: UIMessage) =>
+      new Date(message.createdAt) > new Date(forceDismissDate) &&
+      !message.isRead
+  ).length;
+
+  return newUnreadCount >= NEW_MESSAGES_COUNT_TO_RESET_FORCE_DISMISS;
+};
 
 export const isPushNotificationsBannerRenderableSelector = (
   state: GlobalState
 ) => {
+  // the banner should not be renedered *only* when force dismissed and there are not enough new messages
+  const isForceDismissed =
+    state.notifications.userBehaviour.pushNotificationsBanner
+      .isForceDismissed &&
+    !shouldResetNotificationBannerDismissStateSelector(state);
+
   const notificationsEnabled = areNotificationPermissionsEnabled(state);
   // user has seen the full SPID/CIE login flow,
   // so is not logged with fasLogin during this session
@@ -40,6 +74,7 @@ export const isPushNotificationsBannerRenderableSelector = (
     hasUserSeenSystemNotificationsPromptSelector(state);
 
   return (
+    !isForceDismissed &&
     !isFullLogin &&
     !notificationsEnabled &&
     !hasUserSeenSystemNotificationsPrompt
