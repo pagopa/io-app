89e1a80bb7ca77e914d05d828d22882d08b5d1e4
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2024-12-16 10:21:11 +0100

    chore: [IOBP-1050] Rename biz events in receipt (#6540)
    ## Short description
    This pull request includes significant changes to the payments module,
    primarily related to the removal of the `bizEventsTransaction` feature
    and the introduction of a new `receipt` feature. The changes involve
    updates to the navigation, sagas, reducers, and various other files to
    reflect this transition.
    
    ## How to test
    Ensure that all `bizEvents` functionality remains unchanged
    
    Co-authored-by: Alessandro <alessandro.izzo998@gmail.com>

--- ts/features/payments/bizEventsTransaction/types/index.ts
+++
@@ -1,7 +0,0 @@
-export const paymentsBizEventsCategoryFilters = [
-  "all",
-  "payer",
-  "debtor"
-] as const;
-export type PaymentBizEventsCategoryFilter =
-  (typeof paymentsBizEventsCategoryFilters)[number];
