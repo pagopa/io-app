ef5145836c19cd0aeda93a1cde0955ad8c7dc881
Damiano Plebani <damiano.plebani@pagopa.it>
2024-12-18 14:53:54 +0100

    Remove big bottom margin after `FooterActions`

--- ts/features/zendesk/screens/ZendeskSupportHelpCenter.tsx
+++ ts/features/zendesk/screens/ZendeskSupportHelpCenter.tsx
@@ -398,6 +398,7 @@ const ZendeskSupportHelpCenter = () => {
       animatedRef={animatedScrollViewRef}
       testID={"ZendeskSupportHelpCenterScreen"}
       includeContentMargins={false}
+      excludeEndContentMargin={true}
     >
       <ContentWrapper>
         <FaqManager
