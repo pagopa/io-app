14db7aa563d460574b6d17efbf008ec60962603c
ChrisMattew <cristian.matteu@spindox.it>
2024-12-20 17:15:21 +0100

    perf: improve spidLogin selectors

--- ts/features/spidLogin/store/selectors/index.ts
+++ ts/features/spidLogin/store/selectors/index.ts
@@ -1,7 +1,15 @@
+import { createSelector } from "reselect";
 import { GlobalState } from "../../../../store/reducers/types";
 
-export const nativeLoginRequestInfoSelector = (state: GlobalState) =>
-  state.features.loginFeatures.spidLogin.nativeLogin.requestInfo;
+export const spidLoginSelector = (state: GlobalState) =>
+  state.features.loginFeatures.spidLogin;
 
-export const standardLoginRequestInfoSelector = (state: GlobalState) =>
-  state.features.loginFeatures.spidLogin.standardLogin.requestInfo;
+export const nativeLoginRequestInfoSelector = createSelector(
+  spidLoginSelector,
+  ({ nativeLogin }) => nativeLogin.requestInfo
+);
+
+export const standardLoginRequestInfoSelector = createSelector(
+  spidLoginSelector,
+  ({ standardLogin }) => standardLogin.requestInfo
+);
