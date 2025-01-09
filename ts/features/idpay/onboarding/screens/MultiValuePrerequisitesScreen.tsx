7d1891de3c623f1b7a85ae56876269a5f01f4af8
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-01-09 13:17:21 +0100

    refactor: replace custom radio with RadioGroup

--- ts/features/idpay/onboarding/screens/MultiValuePrerequisitesScreen.tsx
+++ ts/features/idpay/onboarding/screens/MultiValuePrerequisitesScreen.tsx
@@ -4,14 +4,12 @@ import {
   FooterActionsInline,
   H1,
   H6,
-  IOColors,
   IOStyles,
-  Icon,
-  PressableListItemBase,
+  RadioGroup,
   VSpacer
 } from "@pagopa/io-app-design-system";
 import { default as React } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import PagerView from "react-native-pager-view";
 import { SelfDeclarationMultiDTO } from "../../../../../definitions/idpay/SelfDeclarationMultiDTO";
 import { useHeaderSecondLevel } from "../../../../hooks/useHeaderSecondLevel";
@@ -21,12 +19,6 @@ import {
   multiRequiredCriteriaSelector,
   selectCurrentMultiSelfDeclarationPage
 } from "../machine/selectors";
-
-type ListItemProps = {
-  text: string;
-  checked: boolean;
-  onPress: () => void;
-};
 
 const MultiValuePrerequisitesScreen = () => {
   const pagerRef = React.useRef<PagerView>(null);
@@ -69,17 +61,17 @@ const MultiValuePrerequisiteItemScreenContent = ({
 }: MultiValuePrerequisiteItemScreenContentProps) => {
   const machine = IdPayOnboardingMachineContext.useActorRef();
 
-  const [selectedIndex, setSelectedIndex] = React.useState<number | undefined>(
+  const [selectedValue, setSelectedValue] = React.useState<string | undefined>(
     undefined
   );
 
   const handleContinuePress = () => {
-    if (selectedIndex !== undefined) {
+    if (selectedValue !== undefined) {
       machine.send({
         type: "select-multi-consent",
         data: {
           _type: selfDeclaration._type,
-          value: selfDeclaration.value[selectedIndex],
+          value: selectedValue,
           code: selfDeclaration.code
         }
       });
@@ -107,14 +99,15 @@ const MultiValuePrerequisiteItemScreenContent = ({
         <VSpacer size={24} />
         <H6>{selfDeclaration.description}</H6>
         <ScrollView>
-          {selfDeclaration.value.map((answer, index) => (
-            <CustomListItem
-              key={index}
-              text={answer}
-              checked={index === selectedIndex}
-              onPress={() => setSelectedIndex(index)}
-            />
-          ))}
+          <RadioGroup<string>
+            type="radioListItem"
+            items={selfDeclaration.value.map((answer, index) => ({
+              id: index.toString(),
+              value: answer
+            }))}
+            selectedItem={selectedValue}
+            onPress={value => setSelectedValue(value)}
+          />
         </ScrollView>
       </View>
       <FooterActionsInline
@@ -125,39 +118,12 @@ const MultiValuePrerequisiteItemScreenContent = ({
         }}
         endAction={{
           onPress: handleContinuePress,
-          disabled: selectedIndex === undefined,
+          disabled: selectedValue === undefined,
           label: I18n.t("global.buttons.continue")
         }}
       />
     </>
   );
 };
-
-const CustomListItem = ({ text, onPress, checked }: ListItemProps) => (
-  <View style={styles.outerListItem}>
-    <PressableListItemBase onPress={onPress}>
-      <View
-        style={[IOStyles.flex, IOStyles.rowSpaceBetween, styles.innerListItem]}
-      >
-        <H6 color={"bluegreyDark"}>{text}</H6>
-        <Icon
-          name={checked ? "legRadioOn" : "legRadioOff"}
-          size={24}
-          color={checked ? "blue" : "bluegrey"}
-        />
-      </View>
-    </PressableListItemBase>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  outerListItem: {
-    borderBottomWidth: 1,
-    borderBottomColor: IOColors["grey-100"]
-  },
-  innerListItem: {
-    paddingVertical: 4
-  }
-});
 
 export default MultiValuePrerequisitesScreen;
