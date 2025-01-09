b207270adeb6b14bee81ee1e5a02344ff1b98aca
Federico Mastrini <federicomastrini93@gmail.com>
2025-01-09 17:02:05 +0100

    chore(IT Wallet): [SIW-1933] Handle Trustmark QR code generation failures (#6558)
    ## Short description
    This PR adds the handling of Trustmark generation failures in the
    Trustmark QR Code screen
    
    ## List of changes proposed in this pull request
    - Added failure alert if QR Code generation fails
    - Added the ability to retry the QR Code generation with an exponential
    backoff timer
    - Updated tests
    
    ## How to test
    From the details screen of a credential, tap on the Trustmark button,
    check that everything works as expected.
    
    > [!TIP]
    > You can make the Trustmark generation fail by mocking the
    `getCredentialTrustmarkActor` function and make it throw
    
    Ensure that when the Trustmark generation fails, the screens shows an
    error alert with a "Retry" button.
    If you try to press the "Retry" button repeatedly, you should get an
    error toast telling you to wait a few seconds before trying to get the
    trustmark again.
    
    ## Preview
    <img
    src="https://github.com/user-attachments/assets/fd334d54-4d61-4d9f-9fd3-89d45cfdc4ee"
    width="200" />
    <img
    src="https://github.com/user-attachments/assets/8c5c729b-6368-4184-b4d8-ecf015056540"
    width="200" />

---
+++ ts/features/itwallet/trustmark/components/ItwTrustmarkQrCode.tsx
@@ -0,0 +1,79 @@
+import {
+  Body,
+  ButtonLink,
+  Icon,
+  IOColors,
+  IOVisualCostants
+} from "@pagopa/io-app-design-system";
+import React, { useCallback, useMemo } from "react";
+import { StyleSheet, View } from "react-native";
+import { QrCodeImage } from "../../../../components/QrCodeImage";
+import { useDebugInfo } from "../../../../hooks/useDebugInfo";
+import I18n from "../../../../i18n";
+import { ItwTrustmarkMachineContext } from "../machine/provider";
+import { selectFailure, selectTrustmarkUrl } from "../machine/selectors";
+
+/**
+ * Component that renders the QR code of the trustmark
+ */
+const ItwTrustmarkQrCode = () => {
+  const machineRef = ItwTrustmarkMachineContext.useActorRef();
+  const trustmarkUrl =
+    ItwTrustmarkMachineContext.useSelector(selectTrustmarkUrl);
+  const failure = ItwTrustmarkMachineContext.useSelector(selectFailure);
+
+  useDebugInfo({ trustmarkUrl, failure });
+
+  const handleOnRetry = useCallback(() => {
+    machineRef.send({ type: "retry" });
+  }, [machineRef]);
+
+  const content = useMemo(() => {
+    if (failure) {
+      return (
+        <View style={styles.alert}>
+          <Icon name="noticeFilled" size={24} color="grey-700" />
+          <Body textStyle={{ textAlign: "center" }}>
+            {I18n.t("features.itWallet.trustmark.failure.description")}
+          </Body>
+          <View>
+            <ButtonLink
+              label={I18n.t("features.itWallet.trustmark.failure.action")}
+              onPress={handleOnRetry}
+            />
+          </View>
+        </View>
+      );
+    }
+
+    return (
+      <QrCodeImage
+        size={"92%"}
+        value={trustmarkUrl}
+        correctionLevel="L"
+        accessibilityLabel={I18n.t("features.itWallet.trustmark.qrCode")}
+      />
+    );
+  }, [failure, trustmarkUrl, handleOnRetry]);
+
+  return <View style={styles.container}>{content}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: -IOVisualCostants.appMarginDefault,
+    alignItems: "center"
+  },
+  alert: {
+    backgroundColor: IOColors["grey-50"],
+    width: "92%",
+    alignItems: "center",
+    justifyContent: "center",
+    aspectRatio: 1,
+    padding: 16,
+    borderRadius: 16,
+    gap: 8
+  }
+});
+
+export { ItwTrustmarkQrCode };
