b833c9afe305a315a639ab427f2a021837d808ca
Gianluca Spada <gianluca.spada@spindox.it>
2025-01-10 15:38:27 +0100

    chore: change useFocusEffect instead of useEffect

--- ts/utils/hooks/__tests__/usePreventScreenCapture.test.tsx
+++ ts/utils/hooks/__tests__/usePreventScreenCapture.test.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import { render } from "@testing-library/react-native";
 import {
   enableSecureView,
   disableSecureView
 } from "react-native-screenshot-prevent";
+import { createStore } from "redux";
+import { appReducer } from "../../../store/reducers";
+import { applicationChangeState } from "../../../store/actions/application";
+import { renderScreenWithNavigationStoreContext } from "../../testWrapper";
 import { usePreventScreenCapture } from "../usePreventScreenCapture";
 
 jest.mock("react-native-screenshot-prevent", () => ({
@@ -11,19 +13,9 @@ jest.mock("react-native-screenshot-prevent", () => ({
   disableSecureView: jest.fn()
 }));
 
-type Params = {
-  key?: string;
-};
-
-const renderHook = ({ key }: Params = {}) => {
-  const Component = () => {
-    usePreventScreenCapture(key);
-    return null;
-  };
-  return render(<Component />);
-};
-
 describe("usePreventScreenCapture", () => {
+  jest.useFakeTimers();
+
   beforeEach(() => {
     jest.resetAllMocks();
   });
@@ -32,6 +24,7 @@ describe("usePreventScreenCapture", () => {
     const { unmount } = renderHook();
     expect(enableSecureView).toHaveBeenCalledTimes(1);
     unmount();
+    jest.advanceTimersByTime(500);
     expect(disableSecureView).toHaveBeenCalledTimes(1);
   });
 
@@ -39,7 +32,24 @@ describe("usePreventScreenCapture", () => {
     renderHook({ key: "A" });
     const hook2 = renderHook({ key: "B" });
     hook2.unmount();
+    jest.advanceTimersByTime(500);
     expect(enableSecureView).toHaveBeenCalledTimes(2);
     expect(disableSecureView).toHaveBeenCalledTimes(0);
   });
 });
+
+type Params = { key?: string };
+
+const renderHook = ({ key }: Params = {}) => {
+  const Component = () => {
+    usePreventScreenCapture(key);
+    return null;
+  };
+  const globalState = appReducer(undefined, applicationChangeState("active"));
+  return renderScreenWithNavigationStoreContext(
+    Component,
+    "TEST",
+    {},
+    createStore(appReducer, globalState as any)
+  );
+};
--- ts/utils/hooks/usePreventScreenCapture.ts
+++ ts/utils/hooks/usePreventScreenCapture.ts
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import { useCallback, useRef } from "react";
 import RNScreenshotPrevent from "react-native-screenshot-prevent";
 
 const activeTags: Set<string> = new Set();
@@ -18,18 +19,30 @@ const allowScreenCapture = (key: string) => {
 };
 
 /**
- * Hook that disables screen capture for as long as the component is mounted:
+ * Hook that disables screen capture for as long as the component is focused.
+ *
+ * In a stack navigator unfocused screens are not unmounted, so the `useEffect` cleanup function is not called.
+ *
+ * The native library implementation is the following:
  * - On Android, it enables `FLAG_SECURE`
  * - On iOS, uses a hidden secure text field as the platform does not expose an API to disable screenshots
  *
  * @param key An optional key to prevent conflicts when using multiple instances of this hook at the same time.
  */
 export function usePreventScreenCapture(key = "default") {
-  useEffect(() => {
-    preventScreenCapture(key);
+  const timeoutRef = useRef<number>();
 
-    return () => {
-      allowScreenCapture(key);
-    };
-  }, [key]);
+  useFocusEffect(
+    useCallback(() => {
+      clearTimeout(timeoutRef.current);
+
+      preventScreenCapture(key);
+
+      return () => {
+        // Here we wait a little after the blur event for navigation transition animations.
+        // eslint-disable-next-line functional/immutable-data
+        timeoutRef.current = setTimeout(() => allowScreenCapture(key), 500);
+      };
+    }, [key])
+  );
 }
