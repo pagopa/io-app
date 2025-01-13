38bcad1e5e8eb5569735cba8aa76995cbb191a3d
Gianluca Spada <gianluca.spada@spindox.it>
2025-01-13 11:24:50 +0100

    refactor: use autogenerated ID for default

--- ts/utils/hooks/usePreventScreenCapture.ts
+++ ts/utils/hooks/usePreventScreenCapture.ts
@@ -1,18 +1,19 @@
 import { useFocusEffect } from "@react-navigation/native";
-import { useCallback, useRef } from "react";
+import { useCallback, useMemo, useRef } from "react";
 import RNScreenshotPrevent from "react-native-screenshot-prevent";
+import uuid from "react-native-uuid";
 
 const activeTags: Set<string> = new Set();
 
-const preventScreenCapture = (key: string) => {
-  if (!activeTags.has(key)) {
-    activeTags.add(key);
+const preventScreenCapture = (tag: string) => {
+  if (!activeTags.has(tag)) {
+    activeTags.add(tag);
     RNScreenshotPrevent.enableSecureView();
   }
 };
 
-const allowScreenCapture = (key: string) => {
-  activeTags.delete(key);
+const allowScreenCapture = (tag: string) => {
+  activeTags.delete(tag);
   if (activeTags.size === 0) {
     RNScreenshotPrevent.disableSecureView();
   }
@@ -29,20 +30,22 @@ const allowScreenCapture = (key: string) => {
  *
  * @param key An optional key to prevent conflicts when using multiple instances of this hook at the same time.
  */
-export function usePreventScreenCapture(key = "default") {
+export function usePreventScreenCapture(key?: string) {
+  const tag = useMemo(() => key || uuid.v4().toString(), [key]);
+
   const timeoutRef = useRef<number>();
 
   useFocusEffect(
     useCallback(() => {
       clearTimeout(timeoutRef.current);
 
-      preventScreenCapture(key);
+      preventScreenCapture(tag);
 
       return () => {
         // Here we wait a little after the blur event for navigation transition animations.
         // eslint-disable-next-line functional/immutable-data
-        timeoutRef.current = setTimeout(() => allowScreenCapture(key), 500);
+        timeoutRef.current = setTimeout(() => allowScreenCapture(tag), 500);
       };
-    }, [key])
+    }, [tag])
   );
 }
