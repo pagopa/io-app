c3b2f952487771747ad9ae89a2613bd7a966fbdb
Alice Di Rico <83651704+Ladirico@users.noreply.github.com>
2025-01-16 09:39:21 +0100

    chore: [IOPID-2579] add management of remote portal urls (#6594)
    > [!Warning]
    > This PR depends on
    https://github.com/pagopa/io-services-metadata/pull/916
    https://github.com/pagopa/io-dev-api-server/pull/454
    https://github.com/pagopa/io-app/pull/6579
    https://github.com/pagopa/io-app/pull/6577
    
    > [!Note]
    > Need to edit `IO_SERVICES_METADATA_VERSION` ref on
    `scripts/generate-api-models.sh` after merge on `io-services-metadata`
    
    ## Short description
    Add logic to manage the remote urls
    
    ## List of changes proposed in this pull request
    - Add new ref  to metadata
    - Add selector to get the value of remote urls from the cdn
    `absolutePortalLinksSelector`
    - Add selector to manage the link building (base path from the cdn +
    body url) `replaceBaseUrlSelector`
    - Edit hardcoded references to `https://io.italia.it/` and
    `https://ioapp.it/` using selectors
    - Delete **unused** hardcoded references to `https://io.italia.it/` and
    `https://ioapp.it/` into the code
    
    ## Demo 
    
    
    https://github.com/user-attachments/assets/62888ce2-5e08-44b4-a521-e3c2c88b54a0
    
    
    ## How to test
    Run the application and check that all links are working properly (as-is
    operation)

--- ts/urls.ts
+++ ts/urls.ts
@@ -1,8 +1,12 @@
 // IO urls
-export const ioSuppliersUrl = "https://io.italia.it/app-content/fornitori";
-
 export const dpr28Dec2000Url =
   "https://www.normattiva.it/uri-res/N2Ls?urn:nir:stato:decreto.del.presidente.della.repubblica:2000-12-28;445";
 
-export const acceptedPaymentMethodsFaqUrl =
-  "https://io.italia.it/metodi-pagamento";
+export const PRIVACY_URL_BODY = "app-content/tos_privacy.html";
+export const IO_SUPPLIERS_URL_BODY = "app-content/fornitori";
+export const UNSUPPORTED_DEVICE_FAQ_URL_BODY = "faq/#n1_15";
+
+// ITW urls
+export const ITW_IPZS_PRIVACY_URL_BODY = "informativa-ipzs";
+export const DOCUMENTS_ON_IO_FAQ_12_URL_BODY = "documenti-su-io/faq/#n1_12";
+export const DOCUMENTS_ON_IO_FAQ_14_URL_BODY = "documenti-su-io/faq/#n1_14";
