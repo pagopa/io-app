d9d02c370fef48e96425449a2e3fb7c04d546a0f
Federico Mastrini <federicomastrini93@gmail.com>
2025-01-20 18:46:54 +0100

    chore: remove test

--- ts/features/itwallet/common/store/reducers/__tests__/index.test.ts
+++ ts/features/itwallet/common/store/reducers/__tests__/index.test.ts
@@ -1,12 +1,5 @@
 import { applicationChangeState } from "../../../../../../store/actions/application";
-import { logoutSuccess } from "../../../../../../store/actions/authentication";
-import { createRootReducer } from "../../../../../../store/reducers";
-import { itwCredentialsInitialState } from "../../../../credentials/store/reducers";
-import { itwIssuanceInitialState } from "../../../../issuance/store/reducers";
-import { itwWalletInstanceInitialState } from "../../../../walletInstance/store/reducers";
-import { itwLifecycleInitialState } from "../../../../lifecycle/store/reducers";
 import itWalletReducer from "../index";
-import { itwPreferencesInitialState } from "../preferences";
 
 describe("itWalletReducer", () => {
   it("should match snapshot [if this test fails, remember to add a migration to the store before updating the snapshot]", () => {
@@ -15,16 +8,5 @@ describe("itWalletReducer", () => {
       applicationChangeState("active")
     );
     expect(itWalletState).toMatchSnapshot();
-  });
-  it("should persist after a logout action", () => {
-    const newState = createRootReducer([])(undefined, logoutSuccess());
-
-    expect(newState.features.itWallet).toEqual({
-      issuance: itwIssuanceInitialState,
-      lifecycle: itwLifecycleInitialState,
-      credentials: itwCredentialsInitialState,
-      walletInstance: itwWalletInstanceInitialState,
-      preferences: itwPreferencesInitialState
-    });
   });
 });
