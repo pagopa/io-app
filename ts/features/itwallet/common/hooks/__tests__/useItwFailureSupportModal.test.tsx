5f90721a59c0cbfee547a9b1a0e3208bb2f40978
Gianluca Spada <gianluca.spada@spindox.it>
2025-01-20 16:49:49 +0100

    chore: update tests

--- ts/features/itwallet/common/hooks/__tests__/useItwFailureSupportModal.test.tsx
+++ ts/features/itwallet/common/hooks/__tests__/useItwFailureSupportModal.test.tsx
@@ -1,7 +1,8 @@
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
 import { render } from "@testing-library/react-native";
-import { ItwSupportModal } from "../useItwFailureSupportModal";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useItwFailureSupportModal } from "../useItwFailureSupportModal";
 import {
   IssuanceFailure,
   IssuanceFailureType
@@ -14,26 +15,31 @@ import { appReducer } from "../../../../../store/reducers";
 import { applicationChangeState } from "../../../../../store/actions/application";
 import { GlobalState } from "../../../../../store/reducers/types";
 
-describe("useItwFailureSupportModal", () => {
-  test.each([
-    IssuanceFailureType.WALLET_PROVIDER_GENERIC,
-    CredentialIssuanceFailureType.WALLET_PROVIDER_GENERIC
-  ])("renders support chat when the error is %s", failureType => {
-    const { queryByTestId } = renderHook({
-      failure: {
-        type: failureType,
-        reason: {}
-      } as CredentialIssuanceFailure
-    });
+jest.mock("../../../../../utils/hooks/bottomSheet", () => ({
+  // Mock the bottom sheet to immediately render the component
+  useIOBottomSheetAutoresizableModal: (params: { component: JSX.Element }) => ({
+    bottomSheet: params.component
+  })
+}));
 
+describe("useItwFailureSupportModal", () => {
+  it("renders support chat when it is enabled", () => {
+    const { queryByTestId } = renderHook({
+      supportChatEnabled: true,
+      failure: {
+        type: IssuanceFailureType.WALLET_PROVIDER_GENERIC,
+        reason: {}
+      } as IssuanceFailure
+    });
     expect(queryByTestId("contact-method-chat")).toBeTruthy();
     expect(queryByTestId("contact-method-mobile")).toBeNull();
     expect(queryByTestId("contact-method-landline")).toBeNull();
     expect(queryByTestId("contact-method-email")).toBeNull();
   });
 
-  it("renders all contact methods for MDL", () => {
+  it("renders contact methods for MDL", () => {
     const { queryByTestId } = renderHook({
+      supportChatEnabled: false,
       credentialType: "MDL",
       failure: {
         type: CredentialIssuanceFailureType.ISSUER_GENERIC,
@@ -46,21 +52,17 @@ describe("useItwFailureSupportModal", () => {
     expect(queryByTestId("contact-method-email")).toBeTruthy();
   });
 
-  test.each([
-    IssuanceFailureType.NOT_MATCHING_IDENTITY,
-    IssuanceFailureType.UNSUPPORTED_DEVICE,
-    IssuanceFailureType.WALLET_REVOCATION_ERROR,
-    IssuanceFailureType.ISSUER_GENERIC,
-    CredentialIssuanceFailureType.ASYNC_ISSUANCE
-  ])("does not render contact methods when the error is %s", failureType => {
+  it("renders contact methods for EuropeanHealthInsuranceCard", () => {
     const { queryByTestId } = renderHook({
+      supportChatEnabled: false,
+      credentialType: "EuropeanHealthInsuranceCard",
       failure: {
-        type: failureType,
+        type: CredentialIssuanceFailureType.ISSUER_GENERIC,
         reason: {}
       } as CredentialIssuanceFailure
     });
     expect(queryByTestId("contact-method-chat")).toBeNull();
-    expect(queryByTestId("contact-method-mobile")).toBeNull();
+    expect(queryByTestId("contact-method-mobile")).toBeTruthy();
     expect(queryByTestId("contact-method-landline")).toBeNull();
     expect(queryByTestId("contact-method-email")).toBeNull();
   });
@@ -69,14 +71,23 @@ describe("useItwFailureSupportModal", () => {
 type Params = {
   failure: IssuanceFailure | CredentialIssuanceFailure;
   credentialType?: string;
+  supportChatEnabled: boolean;
 };
 
 const renderHook = (params: Params) => {
   const initialState = appReducer(undefined, applicationChangeState("active"));
   const mockStore = configureMockStore<GlobalState>();
+  const Component = () => {
+    const { bottomSheet } = useItwFailureSupportModal(params);
+    return bottomSheet;
+  };
   return render(
     <Provider store={mockStore(initialState)}>
-      <ItwSupportModal {...params} dismissModal={jest.fn()} />
+      <SafeAreaProvider
+        initialSafeAreaInsets={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      >
+        <Component />
+      </SafeAreaProvider>
     </Provider>
   );
 };
