0db96092d297a5d06302e7c58f0357ea7d30e9d5
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2025-01-21 16:57:32 +0100

    chore: request review directly in wallet section

--- ts/features/itwallet/common/hooks/useItwReviewRequest.ts
+++ ts/features/itwallet/common/hooks/useItwReviewRequest.ts
@@ -1,16 +1,13 @@
 import { useCallback } from "react";
-import { Alert } from "react-native";
 import { useFocusEffect } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
-import I18n from "i18n-js";
 import { itwReviewRequestedSelector } from "../store/selectors/preferences";
 import { itwReviewRequested } from "../store/actions/preferences";
 import { requestAppReview } from "../../../../utils/storeReview";
-import { openWebUrl } from "../../../../utils/url";
 
 /**
- * Hook to monitor reviewRequested state and display an alert if needed.
- * If reviewRequested is true, show an alert with two options: one to leave a review on the store and the other to fill out a form on Qualtrics.
+ * Hook to monitor reviewRequested state and request an app review if needed.
+ * If reviewRequested is true, request an app review and then set reviewRequested to false.
  */
 export const useItwReviewRequest = () => {
   const reviewRequested = useSelector(itwReviewRequestedSelector);
@@ -19,24 +16,7 @@ export const useItwReviewRequest = () => {
   useFocusEffect(
     useCallback(() => {
       if (reviewRequested) {
-        Alert.alert(
-          I18n.t("features.itWallet.reviewRequested.alert.title"),
-          I18n.t("features.itWallet.reviewRequested.alert.content"),
-          [
-            {
-              text: I18n.t(
-                "features.itWallet.reviewRequested.alert.cancelButton"
-              ),
-              onPress: () => openWebUrl("https://google.com")
-            },
-            {
-              text: I18n.t(
-                "features.itWallet.reviewRequested.alert.confirmButton"
-              ),
-              onPress: () => requestAppReview()
-            }
-          ]
-        );
+        requestAppReview();
         dispatch(itwReviewRequested(false));
       }
     }, [reviewRequested, dispatch])
