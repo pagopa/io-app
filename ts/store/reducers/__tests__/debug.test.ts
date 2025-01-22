07c58a7181eba68728abaef441ebcf9383ddce9d
Federico Mastrini <federicomastrini93@gmail.com>
2025-01-22 19:25:49 +0100

    chore: remove types tests

--- ts/store/reducers/__tests__/debug.test.ts
+++ ts/store/reducers/__tests__/debug.test.ts
@@ -3,22 +3,6 @@ import { resetDebugData, setDebugData } from "../../actions/debug";
 import { debugDataSelector } from "../debug";
 
 describe("debug", () => {
-  it("should allow to set only object with string keys", () => {
-    // @ts-expect-error - Should not be allowed to set a string
-    setDebugData("string");
-    // @ts-expect-error - Should not be allowed to set a number
-    setDebugData(1);
-    // @ts-expect-error - Should not be allowed to set a boolean
-    setDebugData(true);
-    // @ts-expect-error - Should not be allowed to set a symbol
-    setDebugData(Symbol("symbol"));
-    // @ts-expect-error - Should not be allowed to set a function
-    setDebugData(() => null);
-    // @ts-expect-error - Should not be allowed to set a array
-    setDebugData(["string"]);
-    // @ts-expect-error - Should not be allowed to set an object
-    setDebugData(new Date());
-  });
   it("should return the debug data without the undefined values", () => {
     const state = appReducer(
       {
