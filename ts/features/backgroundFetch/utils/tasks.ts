fabfe6706ad0167d67f9d2b376884a715cbf23ee
Federico Mastrini <federicomastrini93@gmail.com>
2025-01-29 11:05:28 +0100

    Revert "chore: add ITW scheduled task"
    This reverts commit 2f6e69b5898b0a6035fd1b493a21c428eab6b96c.

--- ts/features/backgroundFetch/utils/tasks.ts
+++ ts/features/backgroundFetch/utils/tasks.ts
@@ -9,10 +9,7 @@ import { Prettify } from "../../../types/helpers";
 export enum BackgroundFetchTaskId {
   // Event dispatched automatically by react-native-background-fetch
   // Does not need to be scheduled or registered
-  REACT_NATIVE_BACKGROUND_FETCH = "react-native-background-fetch",
-  // Scheduled task to check if the Wallet Istance is valid
-  // aftter an offline session
-  ITW_WALLET_INSTANCE_CHECK = "it.pagopa.app.io.itw-wallet-instance-check"
+  REACT_NATIVE_BACKGROUND_FETCH = "react-native-background-fetch"
 }
 
 export type BackgroundFetchTaskConfig = Prettify<
