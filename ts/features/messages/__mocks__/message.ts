4adc87c3027de69725ea3453fe617e4ac80aa803
Andrea Piai <andrea.piai@pagopa.it>
2025-01-31 12:27:14 +0100

    No changes

--- ts/features/messages/__mocks__/message.ts
+++ ts/features/messages/__mocks__/message.ts
@@ -8,7 +8,7 @@ import { service_1 } from "./messages";
 export const messageId_1 = "FAT00001";
 
 /**
- * Generic message with due date.
+ * Generic message with due date
  */
 export const message_1: CreatedMessageWithContentAndAttachments = {
   id: messageId_1,
