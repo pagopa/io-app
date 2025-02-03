dd4abc00b3ae9fe83a13afebe5d9dce37d0dcb34
forrest57 <tomba808@gmail.com>
2025-02-03 15:34:16 +0100

    added missing tests

--- ts/features/fims/history/saga/__tests__/handleExportFimsHistorySaga.test.ts
+++ ts/features/fims/history/saga/__tests__/handleExportFimsHistorySaga.test.ts
@@ -1,35 +1,114 @@
+jest.mock("../../../common/analytics");
 import * as E from "fp-ts/Either";
-import { testSaga } from "redux-saga-test-plan";
+import { expectSaga, testSaga } from "redux-saga-test-plan";
 import { withRefreshApiCall } from "../../../../fastLogin/saga/utils";
 import { FimsHistoryClient } from "../../api/client";
 import { fimsHistoryExport } from "../../store/actions";
 import { handleExportFimsHistorySaga } from "../handleExportFimsHistorySaga";
+import { trackExportSucceeded } from "../../../common/analytics";
 
 type ResponseType = Awaited<ReturnType<FimsHistoryClient["requestExport"]>>;
 
 const mockBearerToken = "mockBearerToken";
 const mockAction = fimsHistoryExport.request();
-const decodedSuccessResponse = {
-  status: 200
-};
-
-const successResponse = E.right(decodedSuccessResponse) as ResponseType;
-const resultPromiseSuccess = Promise.resolve(successResponse);
-const mockSuccessClient = jest.fn(() => resultPromiseSuccess);
 
 describe("handleExportFimsHistorySaga", () => {
-  it("should dispatch success when status is 202", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it("should correctly pass the bearerToken to the client, dispatch success with payload 'SUCCESS' and track the export success when status is 202", () => {
+    const response = E.right({ status: 202 }) as ResponseType;
+
+    const resultPromise = Promise.resolve(response);
+    const mockClient = jest.fn(() => resultPromise);
     testSaga(
       handleExportFimsHistorySaga,
-      mockSuccessClient,
+      mockClient,
       mockBearerToken,
       mockAction
     )
       .next()
-      .call(withRefreshApiCall, resultPromiseSuccess, mockAction)
-      .next(E.right({ status: 202 }))
+      .call(withRefreshApiCall, resultPromise, mockAction)
+      .next(response)
       .put(fimsHistoryExport.success("SUCCESS"))
       .next()
       .isDone();
+
+    expect(mockClient).toHaveBeenCalledWith({
+      Bearer: `Bearer ${mockBearerToken}`
+    });
+    expect(trackExportSucceeded).toHaveBeenCalledTimes(1);
+  });
+  it('should dispatch success with payload "ALREADY_EXPORTING", but not track an export success when status is 409 ', () => {
+    const response = E.right({ status: 409 }) as ResponseType;
+
+    const resultPromise = Promise.resolve(response);
+    const mockClient = jest.fn(() => resultPromise);
+    testSaga(
+      handleExportFimsHistorySaga,
+      mockClient,
+      mockBearerToken,
+      mockAction
+    )
+      .next()
+      .call(withRefreshApiCall, resultPromise, mockAction)
+      .next(response)
+      .put(fimsHistoryExport.success("ALREADY_EXPORTING"))
+      .next()
+      .isDone();
+    expect(trackExportSucceeded).toHaveBeenCalledTimes(0);
+  });
+  it("should dispatch failure when status is anything else ", () => {
+    const response = E.right({ status: 418 }) as ResponseType;
+
+    const resultPromise = Promise.resolve(response);
+    const mockClient = jest.fn(() => resultPromise);
+    testSaga(
+      handleExportFimsHistorySaga,
+      mockClient,
+      mockBearerToken,
+      mockAction
+    )
+      .next()
+      .call(withRefreshApiCall, resultPromise, mockAction)
+      .next(response)
+      .put(fimsHistoryExport.failure())
+      .next()
+      .isDone();
+  });
+  it("should dispatch failure when decoding fails", () => {
+    const response = E.left({ status: 202 }) as ResponseType;
+
+    const resultPromise = Promise.resolve(response);
+    const mockClient = jest.fn(() => resultPromise);
+    testSaga(
+      handleExportFimsHistorySaga,
+      mockClient,
+      mockBearerToken,
+      mockAction
+    )
+      .next()
+      .call(withRefreshApiCall, resultPromise, mockAction)
+      .next(response)
+      .put(fimsHistoryExport.failure())
+      .next()
+      .isDone();
+  });
+  it("should dispatch failure when  promise is rejected", async () => {
+    const response = E.right({ status: 202 }) as ResponseType;
+
+    const resultPromise = Promise.reject(response);
+    const mockClient = jest.fn(() => resultPromise);
+
+    return expectSaga(
+      handleExportFimsHistorySaga,
+      mockClient,
+      mockBearerToken,
+      mockAction
+    )
+      .put(fimsHistoryExport.failure())
+      .run();
   });
 });
