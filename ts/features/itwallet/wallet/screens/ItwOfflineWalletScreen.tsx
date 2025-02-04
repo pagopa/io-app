8ea6828b19767de16f7bbf94d41a81e58b4ed265
Federico Mastrini <federicomastrini93@gmail.com>
2025-02-04 12:06:51 +0100

    chore: remove local `HeaderFirstLeveL` component

--- ts/features/itwallet/wallet/screens/ItwOfflineWalletScreen.tsx
+++ ts/features/itwallet/wallet/screens/ItwOfflineWalletScreen.tsx
@@ -1,9 +1,9 @@
+import { HeaderFirstLevel } from "@pagopa/io-app-design-system";
 import { useLayoutEffect } from "react";
 import { IOScrollView } from "../../../../components/ui/IOScrollView";
 import I18n from "../../../../i18n";
 import { useIONavigation } from "../../../../navigation/params/AppParamsList";
 import { ItwWalletCardsContainer } from "../../../wallet/components/WalletCardsContainer";
-import HeaderFirstLevel from "../../../../components/ui/HeaderFirstLevel";
 
 const ItwOfflineWalletScreen = () => {
   const navigation = useIONavigation();
