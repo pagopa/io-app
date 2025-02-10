4772cbb21ec3d2dae3839448ecca45fbb0952415
Alice Di Rico <alice.dirico@pagopa.it>
2025-02-10 12:32:47 +0100

    delete used migration and add tests on reducer

--- ts/features/appearanceSettings/store/reducers/index.ts
+++ ts/features/appearanceSettings/store/reducers/index.ts
@@ -1,15 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {
-  createMigrate,
-  MigrationManifest,
-  PersistConfig,
-  PersistPartial,
-  persistReducer
-} from "redux-persist";
+import { persistReducer, PersistConfig } from "redux-persist";
 import { getType } from "typesafe-actions";
 import { differentProfileLoggedIn } from "../../../../store/actions/crossSessions";
 import { Action } from "../../../../store/actions/types";
-import { isDevEnv } from "../../../../utils/environment";
 import { setShowAppearanceSettingsBanner } from "../actions";
 
 export type AppearanceSettingsState = {
@@ -39,26 +32,9 @@ const appearanceSettingsReducer = (
   }
 };
 
-const CURRENT_REDUX_PROFILE_SETTINGS_STORE_VERSION = 0;
-
-const migrations: MigrationManifest = {
-  // we changed the way we compute the installation ID
-  "0": (state): AppearanceSettingsState & PersistPartial => {
-    const prevState = state as AppearanceSettingsState & PersistPartial;
-
-    return {
-      ...prevState,
-      showAppearanceBanner: true
-    };
-  }
-};
-
 const persistConfig: PersistConfig = {
   key: "appearanceSettings",
-  storage: AsyncStorage,
-  migrate: createMigrate(migrations, { debug: isDevEnv }),
-  version: CURRENT_REDUX_PROFILE_SETTINGS_STORE_VERSION,
-  whitelist: ["showAppearanceBanner"]
+  storage: AsyncStorage
 };
 
 export const appearanceSettingsReducerPersistor = persistReducer(
