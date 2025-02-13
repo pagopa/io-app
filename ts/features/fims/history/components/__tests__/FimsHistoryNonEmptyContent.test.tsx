b289ef750986c9170b38a636b4ebe3e8f3b009e2
forrest57 <tomba808@gmail.com>
2025-02-13 16:50:43 +0100

    addition of required tests

---
+++ ts/features/fims/history/components/__tests__/FimsHistoryNonEmptyContent.test.tsx
@@ -0,0 +1,187 @@
+jest.mock("../../hooks/useFimsHistoryResultToasts.tsx");
+jest.mock("../FimsHistoryLoaders.tsx");
+jest.mock("../FimsHistoryListItemPicker.tsx");
+
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import { fireEvent } from "@testing-library/react-native";
+import { createStore } from "redux";
+import { AccessHistoryPage } from "../../../../../../definitions/fims_history/AccessHistoryPage";
+import {
+  remoteLoading,
+  remoteReady
+} from "../../../../../common/model/RemoteValue";
+import { applicationChangeState } from "../../../../../store/actions/application";
+import { appReducer } from "../../../../../store/reducers";
+import { GlobalState } from "../../../../../store/reducers/types";
+import { renderScreenWithNavigationStoreContext } from "../../../../../utils/testWrapper";
+import { FIMS_ROUTES } from "../../../common/navigation";
+import {
+  FimsHistoryNonEmptyContent,
+  FimsHistoryNonEmptyContentProps
+} from "../FimsHistoryNonEmptyContent";
+import * as HOOK from "../../hooks/useFimsHistoryResultToasts";
+
+const mockAccess = {
+  id: "TESTING",
+  redirect: { display_name: "TESTING", uri: "TESTING" },
+  service_id: "TESTING_SID",
+  timestamp: new Date(0)
+};
+
+const mockAccesses: AccessHistoryPage = {
+  data: [mockAccess]
+};
+const mockEmptyAccesses: AccessHistoryPage = {
+  data: []
+};
+
+// ------------------- UTILS
+
+const generateMockStoreForSelectors = (
+  isHistoryLoading: boolean,
+  isHistoryExporting: boolean
+) =>
+  ({
+    features: {
+      fims: {
+        history: {
+          historyExportState: isHistoryExporting
+            ? remoteLoading
+            : remoteReady("SUCCESS"),
+          consentsList: isHistoryLoading ? pot.someLoading({}) : pot.none
+        }
+      }
+    }
+  } as GlobalState);
+const renderComponent = (
+  props: FimsHistoryNonEmptyContentProps,
+  mockState: GlobalState
+) => {
+  const globalState = appReducer(mockState, applicationChangeState("active"));
+
+  return renderScreenWithNavigationStoreContext(
+    () => <FimsHistoryNonEmptyContent {...props} />,
+    FIMS_ROUTES.HISTORY,
+    {},
+    createStore(appReducer, globalState as any)
+  );
+};
+
+// --------------- END UTILS
+
+// eslint-disable-next-line sonarjs/cognitive-complexity
+describe("fimsHistoryNonEmptyContent", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  for (const historyLoading of [true, false]) {
+    for (const historyExporting of [true, false]) {
+      for (const hasAccesses of [true, false]) {
+        const fetchMore = jest.fn();
+        const shouldShowLoadingListFooter = historyLoading;
+        const shouldRenderPageFooter = !historyLoading || hasAccesses; // component logic is !( hLoading && !accesses )
+        const store = generateMockStoreForSelectors(
+          historyLoading,
+          historyExporting
+        );
+
+        const testString = `${+hasAccesses} accesses, historyLoading = ${historyLoading} and historyExporting = ${historyExporting}`;
+
+        it(`should fetch automatically to try and fill the list, and match snapshot for ${testString} `, () => {
+          const component = renderComponent(
+            {
+              accesses: hasAccesses ? mockAccesses : mockEmptyAccesses,
+              fetchMore
+            },
+            store
+          );
+          expect(component).toBeTruthy();
+          expect(component).toMatchSnapshot();
+          expect(fetchMore).toHaveBeenCalledTimes(1);
+        });
+
+        if (shouldShowLoadingListFooter) {
+          it(`should render list footer in case of ${testString} `, () => {
+            const component = renderComponent(
+              {
+                accesses: hasAccesses ? mockAccesses : mockEmptyAccesses,
+                fetchMore
+              },
+              store
+            );
+
+            expect(component.queryByTestId("testing-footer")).toBeTruthy();
+          });
+        } else {
+          it(`should not render list footer in case of ${testString} `, () => {
+            const component = renderComponent(
+              {
+                accesses: hasAccesses ? mockAccesses : mockEmptyAccesses,
+                fetchMore
+              },
+              store
+            );
+
+            expect(component.queryByTestId("testing-footer")).toBeNull();
+          });
+        }
+
+        if (shouldRenderPageFooter) {
+          it(`should render export footer in case of ${testString} `, () => {
+            const component = renderComponent(
+              {
+                accesses: hasAccesses ? mockAccesses : mockEmptyAccesses,
+                fetchMore
+              },
+              store
+            );
+
+            expect(component.queryByTestId("export-footer")).toBeTruthy();
+          });
+
+          const isPageFooterLoading = historyExporting;
+
+          it(`should ${
+            isPageFooterLoading ? "not" : ""
+          } dispatch the onPress if the user taps the primary action in case of ${testString}`, () => {
+            const mockFetchMore = jest.fn();
+            jest.spyOn(HOOK, "useFimsHistoryExport").mockReturnValue({
+              handleExportOnPress: mockFetchMore
+            });
+
+            const component = renderComponent(
+              {
+                accesses: hasAccesses ? mockAccesses : mockEmptyAccesses,
+                fetchMore
+              },
+              store
+            );
+
+            const renderedComponent = component.getByTestId("export-button");
+            expect(renderedComponent).toBeTruthy();
+
+            fireEvent.press(renderedComponent);
+
+            expect(mockFetchMore).toHaveBeenCalledTimes(
+              isPageFooterLoading ? 0 : 1
+            );
+          });
+        } else {
+          it(`should not render export footer in case of ${testString} `, () => {
+            const component = renderComponent(
+              {
+                accesses: hasAccesses ? mockAccesses : mockEmptyAccesses,
+                fetchMore
+              },
+              store
+            );
+
+            expect(component.queryByTestId("export-footer")).toBeNull();
+          });
+        }
+      }
+    }
+  }
+});
