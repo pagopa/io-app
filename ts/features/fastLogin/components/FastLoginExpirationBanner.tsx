315a27f34292421ff020ee3a0be7b88d2a9371fb
ChrisMattew <cristian.matteu@spindox.it>
2025-02-18 10:41:43 +0100

    feat(FastLoginExpirationBanner): update content copy

--- ts/features/fastLogin/components/FastLoginExpirationBanner.tsx
+++ ts/features/fastLogin/components/FastLoginExpirationBanner.tsx
@@ -5,10 +5,11 @@ import {
 } from "@pagopa/io-app-design-system";
 import { useCallback } from "react";
 import { StyleSheet, View } from "react-native";
-import { useIODispatch } from "../../../store/hooks";
+import { useIODispatch, useIOSelector } from "../../../store/hooks";
 import { openWebUrl } from "../../../utils/url";
 import I18n from "../../../i18n";
 import { closeSessionExpirationBanner } from "../store/actions/sessionExpirationActions";
+import { formattedExpirationDateSelector } from "../../../store/reducers/authentication";
 
 const SESSION_EXPIRED_EDUCATIONAL_URL =
   "https://assistenza.ioapp.it/hc/it/articles/32616176301713-Cosa-fare-quando-la-sessione-scade";
@@ -21,6 +22,7 @@ type Props = {
  * (see MultiBanner feature for the landing screen)
  */
 export const FastLoginExpirationBanner = ({ handleOnClose }: Props) => {
+  const expirationDate = useIOSelector(formattedExpirationDateSelector);
   const { error } = useIOToast();
   const dispatch = useIODispatch();
 
@@ -37,7 +39,9 @@ export const FastLoginExpirationBanner = ({ handleOnClose }: Props) => {
   return (
     <View style={styles.margins}>
       <Banner
-        content={I18n.t("fastLogin.expirationBanner.content")}
+        content={I18n.t("fastLogin.expirationBanner.content", {
+          date: expirationDate
+        })}
         action={I18n.t("fastLogin.expirationBanner.action.label")}
         pictogramName="identityCheck"
         color="neutral"
