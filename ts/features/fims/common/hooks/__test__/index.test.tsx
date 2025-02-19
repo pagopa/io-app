6cd4d09e9c54b46088b3bb85340b111f9b082651
Andrea Piai <andrea.piai@pagopa.it>
2025-02-19 12:30:00 +0100

    Tests for useFIMSFromServiceId

--- ts/features/fims/common/hooks/__test__/index.test.tsx
+++ ts/features/fims/common/hooks/__test__/index.test.tsx
@@ -1,6 +1,7 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
+import { ComponentType } from "react";
 import { createStore } from "redux";
-import { useAutoFetchingServiceByIdPot } from "..";
+import { useAutoFetchingServiceByIdPot, useFIMSFromServiceId } from "..";
 import { ServiceId } from "../../../../../../definitions/backend/ServiceId";
 import { applicationChangeState } from "../../../../../store/actions/application";
 import { appReducer } from "../../../../../store/reducers";
@@ -9,6 +10,7 @@ import { MESSAGES_ROUTES } from "../../../../messages/navigation/routes";
 import { GlobalState } from "../../../../../store/reducers/types";
 import { ServicePublic } from "../../../../../../definitions/backend/ServicePublic";
 import { loadServiceDetail } from "../../../../services/details/store/actions/details";
+import { FIMS_ROUTES } from "../../navigation";
 
 const mockDispatch = jest.fn();
 jest.mock("react-redux", () => ({
@@ -16,8 +18,31 @@ jest.mock("react-redux", () => ({
   useDispatch: () => mockDispatch
 }));
 
+const mockNavigation = jest.fn();
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      getState: () => ({
+        index: 0,
+        routes: [
+          {
+            name: "MESSAGE_DETAIL"
+          }
+        ]
+      }),
+      navigate: mockNavigation
+    })
+  };
+});
+
 // eslint-disable-next-line functional/no-let
-let serviceData: pot.Pot<ServicePublic, Error> | undefined;
+let serviceDataPot: pot.Pot<ServicePublic, Error> | undefined;
+// eslint-disable-next-line functional/no-let
+let serviceData: unknown;
+// eslint-disable-next-line functional/no-let
+let authenticationCallback: (label: string, url: string) => void | undefined;
 
 describe("index", () => {
   afterEach(() => {
@@ -45,9 +70,9 @@ describe("index", () => {
       }'`, () => {
         const serviceId = "01JMESJKA9HS28MEW12P7WPYVC" as ServiceId;
 
-        renderHook(serviceId, servicePot, serviceId);
+        renderAutoFetchHook(serviceId, servicePot, serviceId);
 
-        expect(serviceData).toEqual(servicePot);
+        expect(serviceDataPot).toEqual(servicePot);
         if (shouldHaveCalledDispatch) {
           expect(mockDispatch.mock.calls.length).toBe(1);
           expect(mockDispatch.mock.calls[0].length).toBe(1);
@@ -61,13 +86,14 @@ describe("index", () => {
     });
     it(`should have dispatched 'loadServiceDetail.request' and returned proper data for unmatching serviceId`, () => {
       const hookServiceId = "01JMESJKA9HS28MEW12P7WPYVC" as ServiceId;
-      renderHook(
+
+      renderAutoFetchHook(
         hookServiceId,
         pot.some({} as ServicePublic),
         "8MEW12P7WPYVC01JMESJKA9HS2" as ServiceId
       );
 
-      expect(serviceData).toEqual(pot.none);
+      expect(serviceDataPot).toEqual(pot.none);
 
       expect(mockDispatch.mock.calls.length).toBe(1);
       expect(mockDispatch.mock.calls[0].length).toBe(1);
@@ -76,37 +102,162 @@ describe("index", () => {
       );
     });
   });
+  describe("useFIMSFromServiceId", () => {
+    const serviceId = "01JMEWNY9BC3KVRCGTY1737J0S" as ServiceId;
+    const service = {
+      organization_fiscal_code: "01234567891",
+      organization_name: "An organization name",
+      service_id: serviceId,
+      service_name: "A service name"
+    } as ServicePublic;
+    [
+      pot.none,
+      pot.noneLoading,
+      pot.noneUpdating(service),
+      pot.noneError(Error("")),
+      pot.some(service),
+      pot.someLoading(service),
+      pot.someUpdating(service, service),
+      pot.someError(service, Error(""))
+    ].forEach(servicePot => {
+      it(`should call 'navigation.navigate' with proper paramters for '${servicePot.kind}' service and return proper service data for analytics`, () => {
+        const expectedServiceData = pot.isSome(servicePot)
+          ? {
+              serviceId,
+              organizationFiscalCode: service.organization_fiscal_code,
+              organizationName: service.organization_name,
+              serviceName: service.service_name
+            }
+          : {
+              serviceId
+            };
+
+        renderFromServiceIdHook(serviceId, servicePot, serviceId);
+
+        expect(serviceData).toEqual(expectedServiceData);
+        expect(authenticationCallback).toBeDefined();
+
+        const label = "A label";
+        const url = "iosso://https://relyingParty.url/login";
+        authenticationCallback(label, url);
+
+        expect(mockNavigation.mock.calls.length).toBe(1);
+        expect(mockNavigation.mock.calls[0].length).toBe(2);
+        expect(mockNavigation.mock.calls[0][0]).toBe(FIMS_ROUTES.MAIN);
+        expect(mockNavigation.mock.calls[0][1]).toEqual({
+          screen: FIMS_ROUTES.CONSENTS,
+          params: {
+            ctaUrl: "https://relyingParty.url/login",
+            ctaText: label,
+            organizationFiscalCode: pot.isSome(servicePot)
+              ? service.organization_fiscal_code
+              : undefined,
+            organizationName: pot.isSome(servicePot)
+              ? service.organization_name
+              : undefined,
+            serviceId: service.service_id,
+            serviceName: pot.isSome(servicePot)
+              ? service.service_name
+              : undefined,
+            source: MESSAGES_ROUTES.MESSAGE_DETAIL
+          }
+        });
+      });
+    });
+    it(`should call 'navigation.navigate' with proper paramters for unmatching service id service and return proper service data for analytics`, () => {
+      const hookServiceId = "01JMEZB6QNR7KKDEFRR6WZEH6F" as ServiceId;
+      const expectedServiceData = {
+        serviceId: hookServiceId
+      };
+
+      renderFromServiceIdHook(hookServiceId, pot.some(service), serviceId);
+
+      expect(serviceData).toEqual(expectedServiceData);
+      expect(authenticationCallback).toBeDefined();
+
+      const label = "A label";
+      const url = "iosso://https://relyingParty.url/login";
+      authenticationCallback(label, url);
+
+      expect(mockNavigation.mock.calls.length).toBe(1);
+      expect(mockNavigation.mock.calls[0].length).toBe(2);
+      expect(mockNavigation.mock.calls[0][0]).toBe(FIMS_ROUTES.MAIN);
+      expect(mockNavigation.mock.calls[0][1]).toEqual({
+        screen: FIMS_ROUTES.CONSENTS,
+        params: {
+          ctaUrl: "https://relyingParty.url/login",
+          ctaText: label,
+          organizationFiscalCode: undefined,
+          organizationName: undefined,
+          serviceId: hookServiceId,
+          serviceName: undefined,
+          source: MESSAGES_ROUTES.MESSAGE_DETAIL
+        }
+      });
+    });
+  });
 });
 
-const renderHook = (
+const renderAutoFetchHook = (
   hookServiceId: ServiceId,
   servicePot: pot.Pot<ServicePublic, Error>,
   storeServiceId: ServiceId
 ) => {
-  const initialState = appReducer(
-    {
-      features: {
-        services: {
-          details: {
-            byId: {
-              [storeServiceId]: servicePot
-            }
+  const appState = {
+    features: {
+      services: {
+        details: {
+          byId: {
+            [storeServiceId]: servicePot
           }
         }
       }
-    } as GlobalState,
-    applicationChangeState("active")
-  );
+    }
+  } as GlobalState;
+  return genericRender(() => AutoFetchHookWrapper(hookServiceId), appState);
+};
+
+const AutoFetchHookWrapper = (serviceId: ServiceId) => {
+  serviceDataPot = useAutoFetchingServiceByIdPot(serviceId);
+  return undefined;
+};
+
+const renderFromServiceIdHook = (
+  hookServiceId: ServiceId,
+  servicePot: pot.Pot<ServicePublic, Error>,
+  storeServiceId: ServiceId
+) => {
+  const appState = {
+    features: {
+      services: {
+        details: {
+          byId: {
+            [storeServiceId]: servicePot
+          }
+        }
+      }
+    }
+  } as GlobalState;
+  return genericRender(() => FromServiceIdHookWrapper(hookServiceId), appState);
+};
+
+const FromServiceIdHookWrapper = (serviceId: ServiceId) => {
+  const hookData = useFIMSFromServiceId(serviceId);
+  serviceData = hookData.serviceData;
+  authenticationCallback = hookData.startFIMSAuthenticationFlow;
+  return undefined;
+};
+
+const genericRender = (
+  hookWrapper: ComponentType<any>,
+  appState: GlobalState
+) => {
+  const initialState = appReducer(appState, applicationChangeState("active"));
   const store = createStore(appReducer, initialState as any);
   return renderScreenWithNavigationStoreContext(
-    () => HookWrapper(hookServiceId),
+    hookWrapper,
     MESSAGES_ROUTES.MESSAGE_DETAIL,
     {},
     store
   );
-};
-
-const HookWrapper = (serviceId: ServiceId) => {
-  serviceData = useAutoFetchingServiceByIdPot(serviceId);
-  return undefined;
 };
