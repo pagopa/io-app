5ac62d0e91a574cf3915f316fc8ecd1dc6ad71b2
Federico Mastrini <federicomastrini93@gmail.com>
2025-03-05 16:49:42 +0100

    chore: move `NoConnectivityScreen` to correct stack

--- ts/navigation/OfflineStackNavigator.tsx
+++ ts/navigation/OfflineStackNavigator.tsx
@@ -1,8 +1,10 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { NoConnectivityScreen } from "../features/connectivity/screen/NoConnectivityScreen";
 import { ItwStackNavigator } from "../features/itwallet/navigation/ItwStackNavigator";
 import { ITW_ROUTES } from "../features/itwallet/navigation/routes";
 import { isGestureEnabled } from "../utils/navigation";
 import { AppParamsList } from "./params/AppParamsList";
+import ROUTES from "./routes";
 
 const Stack = createStackNavigator<AppParamsList>();
 
@@ -18,6 +20,12 @@ const OfflineStackNavigator = () => (
       name={ITW_ROUTES.MAIN}
       component={ItwStackNavigator}
       options={{ gestureEnabled: isGestureEnabled, headerShown: false }}
+    />
+
+    <Stack.Screen
+      name={ROUTES.NO_CONNECTIVITY}
+      options={{ headerShown: false }}
+      component={NoConnectivityScreen}
     />
   </Stack.Navigator>
 );
