3d3be66fde3d96976baf4ebc4ea1d871bec52a7c
Alice Di Rico <alice.dirico@pagopa.it>
2025-03-17 17:15:16 +0100

    fix logic and add comments

--- ts/features/ingress/store/selectors/index.ts
+++ ts/features/ingress/store/selectors/index.ts
@@ -1,25 +1,7 @@
-import { createSelector } from "reselect";
 import { GlobalState } from "../../../../store/reducers/types";
-import { isConnectedSelector } from "../../../connectivity/store/selectors";
-import { itwLifecycleIsOperationalOrValid } from "../../../itwallet/lifecycle/store/selectors";
-import { isItwOfflineAccessEnabledSelector } from "../../../../store/reducers/persistedPreferences";
 
 export const isBlockingScreenSelector = (state: GlobalState) =>
   state.features.ingress.isBlockingScreen;
 
 export const offlineAccessReasonSelector = (state: GlobalState) =>
   state.features.ingress.offlineAccessReason;
-
-export const isDeviceOfflineWithWalletSelector = createSelector(
-  isConnectedSelector,
-  itwLifecycleIsOperationalOrValid,
-  isItwOfflineAccessEnabledSelector,
-  (
-    isConnected,
-    isOfflineAccessEnabled,
-    selectItwLifecycleIsOperationalOrValid
-  ) =>
-    !isConnected &&
-    isOfflineAccessEnabled &&
-    selectItwLifecycleIsOperationalOrValid
-);
--- ts/sagas/startup.ts
+++ ts/sagas/startup.ts
@@ -32,10 +32,7 @@ import {
 import { watchFciSaga } from "../features/fci/saga";
 import { watchFimsSaga } from "../features/fims/common/saga";
 import { watchIDPaySaga } from "../features/idpay/common/saga";
-import {
-  isBlockingScreenSelector,
-  isDeviceOfflineWithWalletSelector
-} from "../features/ingress/store/selectors";
+import { isBlockingScreenSelector } from "../features/ingress/store/selectors";
 import {
   watchItwSaga,
   watchItwOfflineSaga
@@ -102,7 +99,6 @@ import {
   profileSelector
 } from "../store/reducers/profile";
 import {
-  isStartupLoaded,
   StartupStatusEnum,
   startupTransientErrorInitialState
 } from "../store/reducers/startup";
@@ -111,7 +107,10 @@ import { ReduxSagaEffect, SagaCallReturnType } from "../types/utils";
 import { trackKeychainFailures } from "../utils/analytics";
 import { isTestEnv } from "../utils/environment";
 import { deletePin, getPin } from "../utils/keychain";
-import { watchSessionRefreshInOfflineSaga } from "../features/ingress/saga";
+import {
+  isDeviceOfflineWithWalletSaga,
+  watchSessionRefreshInOfflineSaga
+} from "../features/ingress/saga";
 import { startAndReturnIdentificationResult } from "./identification";
 import { previousInstallationDataDeleteSaga } from "./installation";
 import {
@@ -236,13 +235,33 @@ export function* initializeApplicationSaga(
   // Start watching for ITW sagas that do not require internet connection or a valid session
   yield* fork(watchItwOfflineSaga);
 
-  const isDeviceOfflineWithWallet = yield* select(
-    isDeviceOfflineWithWalletSelector
-  );
+  /**
+   * Prevents the saga from executing if the user opened the app while offline.
+   *
+   * - Calls `isDeviceOfflineWithWalletSaga` to determine if the device is offline,
+   *   the user has a valid IT Wallet instance, and offline access is enabled.
+   * - If this condition is met, it means the app started in offline mode,
+   *   so the function exits early (`return`), preventing unnecessary execution of subsequent logic.
+   * - This ensures that only relevant flows are triggered based on the app’s startup condition.
+   */
+  const isDeviceOfflineWithWallet = yield* call(isDeviceOfflineWithWalletSaga);
   if (isDeviceOfflineWithWallet) {
     return;
   }
 
+  /**
+   * Starts listening for session refresh failures while in offline mode.
+   *
+   * - `watchSessionRefreshInOfflineSaga` monitors when the offline access reason
+   *   is updated due to a session refresh failure.
+   * - If the session refresh process fails while offline, this saga will handle
+   *   transitioning the app to offline mode accordingly.
+   * - Forking (`yield* fork(...)`) allows this watcher to run in parallel
+   *   without blocking the rest of the execution.
+   *
+   * This ensures that if the device is online but session refresh fails,
+   * the app correctly transitions to offline mode when necessary.
+   */
   yield* fork(watchSessionRefreshInOfflineSaga);
 
   // Since the backend.json is done in parallel with the startup saga,
@@ -251,19 +270,6 @@ export function* initializeApplicationSaga(
   const remoteConfig = yield* select(remoteConfigSelector);
   if (O.isNone(remoteConfig)) {
     yield* take(backendStatusLoadSuccess);
-  }
-
-  /**
-   * To prevent cases where the user goes back online and the saga continues,
-   * we need to explicitly stop the flow at this point.
-   * If `backendStatusLoadSuccess` is dispatched, it means the user is back online,
-   * **BUT** they must continue navigating within the offline flow.
-   * The best way to ensure this is to exit the startup saga at this stage.
-   */
-  // TODO: delete
-  const startupStatus = yield* select(isStartupLoaded);
-  if (startupStatus === StartupStatusEnum.OFFLINE) {
-    return;
   }
 
   // Whether the user is currently logged in.
