cd7376a9c529b921b73dc9d370d2d6cc8b6df61f
Martino Cesari Tomba <60693085+forrest57@users.noreply.github.com>
2025-03-18 17:17:06 +0100

    feat:[IOCOM-2223] persisted dismiss logic for SEND banner (#6827)
    ## Short description
    addition of a persisted state entry for dismiss for the SEND engagement
    banner
    
    ## List of changes proposed in this pull request
    - required persisted reducer
    - required action
    - required selector, and update to the bannerMap
    - small refactor the the landingScreenBannerPicker 
    
    complete tests to come 
    
    ## How to test
    - existing tests should pass
    - on banner dismiss or success flow end, the banner should be dismissed
    - the dismissal should persist until the asyncStorage is cleared
    - the landingScreenBannerPicker should work as usual and intended
    - the landing_banners reducer's data should not be impacted other than
    the standard dismiss.

--- ts/features/pn/store/actions/index.ts
+++ ts/features/pn/store/actions/index.ts
@@ -20,8 +20,12 @@ export const startPNPaymentStatusTracking = createAction(
 export const cancelPNPaymentStatusTracking = createAction(
   "PN_CANCEL_PAYMENT_STATUS_TRACKING"
 );
+export const dismissPnActivationReminderBanner = createAction(
+  "DISMISS_PN_ACTIVATION_REMINDER_BANNER"
+);
 
 export type PnActions =
   | ActionType<typeof pnActivationUpsert>
   | ActionType<typeof startPNPaymentStatusTracking>
-  | ActionType<typeof cancelPNPaymentStatusTracking>;
+  | ActionType<typeof cancelPNPaymentStatusTracking>
+  | ActionType<typeof dismissPnActivationReminderBanner>;
