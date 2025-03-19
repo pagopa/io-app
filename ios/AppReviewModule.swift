7d71fb7b8ba882dbd34d6862b1208b03fa3671f5
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-03-19 16:40:36 +0100

    refactor:[IOPLT-928] Replaces app review npm package with native module code

---
+++ ios/AppReviewModule.swift
@@ -0,0 +1,33 @@
+//
+//  AppReviewModule.swift
+//  ItaliaApp
+//
+//  Created by Cristiano Tofani on 19/03/25.
+//
+
+import Foundation
+import UIKit
+import StoreKit
+
+
+/** `@objc` attribute exposes Swift methods to the Objective-C runtime**/
+@objc(AppReviewModule)
+class AppReviewModule: NSObject {
+  
+  @objc
+  func requestReview () -> Void {
+    if #available(iOS 14.0, *) {
+      let activeWindowScene = UIApplication.shared.connectedScenes.filter { scene in
+        return scene.activationState == .foregroundActive && scene is UIWindowScene
+      }.first
+      
+      if let scene = activeWindowScene as? UIWindowScene {
+        SKStoreReviewController.requestReview(in: scene)
+        return
+      }
+    } else if #available(iOS 10.3, *) {
+      SKStoreReviewController.requestReview()
+      return
+    }
+  }
+}
