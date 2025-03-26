4d77b218be36cd87c87171621df77beb8a4b819a
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-03-26 15:54:56 +0100

    Code refactor

--- ts/features/connectivity/saga/index.ts
+++ ts/features/connectivity/saga/index.ts
@@ -1,11 +1,10 @@
 import * as E from "fp-ts/lib/Either";
-import { call, fork, put, select, delay } from "typed-redux-saga/macro";
+import { call, fork, put } from "typed-redux-saga/macro";
 import { Millisecond } from "@pagopa/ts-commons/lib/units";
 import { configureNetInfo, fetchNetInfoState } from "../utils";
 import { startTimer } from "../../../utils/timer";
 import { setConnectionStatus } from "../store/actions";
 import { ReduxSagaEffect, SagaCallReturnType } from "../../../types/utils";
-import { isConnectedSelector } from "../store/selectors";
 
 const CONNECTIVITY_STATUS_LOAD_INTERVAL = (60 * 1000) as Millisecond;
 const CONNECTIVITY_STATUS_FAILURE_INTERVAL = (10 * 1000) as Millisecond;
@@ -18,67 +17,38 @@ export function* connectionStatusSaga(): Generator<
   boolean,
   SagaCallReturnType<typeof fetchNetInfoState>
 > {
-  try {
-    const response = yield* call(fetchNetInfoState());
-    if (E.isRight(response)) {
-      if (
-        response.right.isInternetReachable !== null &&
-        response.right.isInternetReachable !== undefined
-      ) {
-        const isAppConnected =
-          response.right.isConnected === true &&
-          response.right.isInternetReachable === true;
-
-        yield* put(setConnectionStatus(isAppConnected));
-        return isAppConnected;
-      }
-
-      yield* delay(500);
-
-      // on iOS the first call to netinfo returns null on the isInternetReachable field
-      // we need to wait for the next call to get the correct value
-      const retryResponse = yield* call(fetchNetInfoState());
-
-      if (E.isRight(retryResponse)) {
-        const retryIsAppConnected =
-          retryResponse.right.isConnected === true &&
-          retryResponse.right.isInternetReachable === true;
-
-        yield* put(setConnectionStatus(retryIsAppConnected));
-        return retryIsAppConnected;
-      }
-    }
-    return false;
-  } catch (e) {
-    // do nothing. it should be a library error
-    return false;
-  }
-}
-
-/**
- * this saga requests and checks in loop connection status
- * if the connection is off app could show a warning message or avoid
- * the whole usage.
- */
-export function* connectionStatusWatcherLoop() {
-  // check connectivity status periodically
   while (true) {
-    const response: SagaCallReturnType<typeof connectionStatusSaga> =
-      yield* call(connectionStatusSaga);
+    try {
+      const response = yield* call(fetchNetInfoState());
+      if (E.isRight(response)) {
+        if (
+          response.right.isInternetReachable !== null &&
+          response.right.isInternetReachable !== undefined
+        ) {
+          const isAppConnected =
+            response.right.isConnected === true &&
+            response.right.isInternetReachable === true;
 
-    // if we have no connection increase rate
-    if (response === false) {
+          // App is connected update the store and wait for the next check
+          yield* put(setConnectionStatus(isAppConnected));
+
+          if (isAppConnected) {
+            yield* call(startTimer, CONNECTIVITY_STATUS_LOAD_INTERVAL);
+            continue;
+          }
+          yield* call(startTimer, CONNECTIVITY_STATUS_FAILURE_INTERVAL);
+          continue;
+        }
+        // if the response is not valid, wait for the next check
+        // shortening the interval
+        yield* call(startTimer, 500);
+        continue;
+      }
       yield* call(startTimer, CONNECTIVITY_STATUS_FAILURE_INTERVAL);
       continue;
-    }
-
-    const isAppConnected = yield* select(isConnectedSelector);
-
-    // if connection is off increase rate
-    if (!isAppConnected) {
+    } catch (e) {
       yield* call(startTimer, CONNECTIVITY_STATUS_FAILURE_INTERVAL);
-    } else {
-      yield* call(startTimer, CONNECTIVITY_STATUS_LOAD_INTERVAL);
+      continue;
     }
   }
 }
@@ -86,5 +56,5 @@ export function* connectionStatusWatcherLoop() {
 export default function* root(): IterableIterator<ReduxSagaEffect> {
   // configure net info library to check status and fetch a specific url
   configureNetInfo();
-  yield* fork(connectionStatusWatcherLoop);
+  yield* fork(connectionStatusSaga);
 }
