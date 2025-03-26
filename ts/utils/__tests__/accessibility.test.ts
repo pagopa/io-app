79b651458e66ee8840d9033c0be0c9f422c62495
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-03-26 15:53:39 +0100

    chore: [IOBP-1303,IOBP-1411,IOBP-1412] Enhance CGN lists with ordinal and total element announcements for accessibility (#6824)
    ## Short description
    This PR includes modifications to the CGN lists to announce how many
    elements they contain and the ordinal number of the elements when
    intercepted
    
    ## List of changes proposed in this pull request
    - Updated the `CgnMerchantListViewRenderItem` function to include the
    item count in the accessibility label
    - Added a new utility function `getListItemAccessibilityLabelCount` to
    generate the accessibility label
    
    ## How to test
    ### `IOBP-1303`
    - Use a real device and navigate to `CGN_MERCHANTS_CATEGORIES` with
    TalkBack enabled
    - Verify that the screen reader announces the ordinal position and total
    number of elements for each list element
    - Repeat the same validation for `CGN_MERCHANTS_LIST_BY_CATEGORIES` to
    ensure consistent behavior
    
    ### `IOBP-1411 & IOBP-1412 `
    - Use a real device and navigate to `CGN_MERCHANTS_DETAIL` with TalkBack
    enabled
    - Verify that the screen reader is now not able to focus on badges
    - Ensure that it announce each information correctly
    
    ---------
    
    Co-authored-by: Alessandro <alessandro.izzo998@gmail.com>

---
+++ ts/utils/__tests__/accessibility.test.ts
@@ -0,0 +1,74 @@
+import { AccessibilityInfo } from "react-native";
+import {
+  getAccessibleAmountText,
+  getListItemAccessibilityLabelCount,
+  isScreenReaderEnabled
+} from "../accessibility";
+
+jest.mock("react-native", () => ({
+  AccessibilityInfo: {
+    setAccessibilityFocus: jest.fn(),
+    isScreenReaderEnabled: jest.fn()
+  },
+  findNodeHandle: jest.fn()
+}));
+
+describe("isScreenReaderEnabled", () => {
+  it("should return true if screen reader is enabled", async () => {
+    (AccessibilityInfo.isScreenReaderEnabled as jest.Mock).mockResolvedValue(
+      true
+    );
+
+    const result = await isScreenReaderEnabled();
+
+    expect(result).toBe(true);
+  });
+
+  it("should return false if screen reader is not enabled", async () => {
+    (AccessibilityInfo.isScreenReaderEnabled as jest.Mock).mockResolvedValue(
+      false
+    );
+
+    const result = await isScreenReaderEnabled();
+
+    expect(result).toBe(false);
+  });
+
+  it("should return false if an error occurs", async () => {
+    (AccessibilityInfo.isScreenReaderEnabled as jest.Mock).mockRejectedValue(
+      new Error("Error")
+    );
+
+    const result = await isScreenReaderEnabled();
+
+    expect(result).toBe(false);
+  });
+});
+
+describe("getAccessibleAmountText", () => {
+  it("should replace '-' with the localized minus symbol", () => {
+    const result = getAccessibleAmountText("-123");
+
+    expect(result).toBe("minus123");
+  });
+
+  it("should return undefined if amount is undefined", () => {
+    const result = getAccessibleAmountText();
+
+    expect(result).toBeUndefined();
+  });
+});
+
+describe("getListItemAccessibilityLabelCount", () => {
+  it("should return the correct accessibility label with comma", () => {
+    const result = getListItemAccessibilityLabelCount(10, 2);
+
+    expect(result).toBe(", Element 3 of 10");
+  });
+
+  it("should return the correct accessibility label without comma", () => {
+    const result = getListItemAccessibilityLabelCount(10, 2, true);
+
+    expect(result).toBe("Element 3 of 10");
+  });
+});
