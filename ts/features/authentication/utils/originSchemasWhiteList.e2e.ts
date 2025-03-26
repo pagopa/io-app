6f5e82dc6e91a2c917361177fcd9f0e5060cfeee
ChrisMattew <cristian.matteu@spindox.it>
2025-03-26 16:05:06 +0100

    chore: restore originSchemasWhiteList.e2e.ts file

---
+++ ts/features/authentication/utils/originSchemasWhiteList.e2e.ts
@@ -0,0 +1,7 @@
+// if the app is running in e2e env, add "http" to allow the dev-server usage
+export const originSchemasWhiteList = [
+  "https://*",
+  "intent://*",
+  "http://*",
+  "iologin://*"
+];
