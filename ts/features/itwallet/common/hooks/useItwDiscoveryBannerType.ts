deb7656baf7de6bb9bb2908d1e1b60b86ceb67af
Federico Mastrini <federicomastrini93@gmail.com>
2025-04-03 16:28:56 +0200

    chore: disable reactivation banner

--- ts/features/itwallet/common/hooks/useItwDiscoveryBannerType.ts
+++ ts/features/itwallet/common/hooks/useItwDiscoveryBannerType.ts
@@ -1,16 +1,9 @@
-import { useIOSelector } from "../../../../store/hooks.ts";
-import { itwIsWalletInstanceRemotelyActiveSelector } from "../store/selectors/preferences.ts";
-
 type DiscoveryBannerType = "onboarding" | "reactivating";
 
 /**
  * Hook to determine the type of the ITW discovery banner to display based on the wallet's state
  * @returns the type of the banner to display
  */
-export const useItwDiscoveryBannerType = (): DiscoveryBannerType => {
-  const isWalletRemotelyActive = useIOSelector(
-    itwIsWalletInstanceRemotelyActiveSelector
-  );
-
-  return isWalletRemotelyActive ? "reactivating" : "onboarding";
-};
+export const useItwDiscoveryBannerType = (): DiscoveryBannerType =>
+  // FIXME: (SIW-2048) we need to return the correct banner type based on the wallet's remote state
+  "onboarding";
