7916f7f69963f30905b7ff57cba369b0bf443a00
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-04-04 17:53:09 +0200

    adds the status message for offline to the specific global component

--- ts/components/StatusMessages.tsx
+++ ts/components/StatusMessages.tsx
@@ -1,17 +1,14 @@
-import {
-  AlertEdgeToEdgeProps,
-  AlertEdgeToEdgeWrapper
-} from "@pagopa/io-app-design-system";
+import { AlertEdgeToEdgeWrapper } from "@pagopa/io-app-design-system";
 import { PropsWithChildren } from "react";
 import { useStatusAlertProps } from "../hooks/useStatusAlertProps";
 
 type StatusMessagesProps = PropsWithChildren;
 
 export const StatusMessages = ({ children }: StatusMessagesProps) => {
-  const alertProps: AlertEdgeToEdgeProps | undefined = useStatusAlertProps();
+  const statusAlert = useStatusAlertProps();
 
   return (
-    <AlertEdgeToEdgeWrapper alertProps={alertProps}>
+    <AlertEdgeToEdgeWrapper alertProps={statusAlert?.alertProps}>
       {children}
     </AlertEdgeToEdgeWrapper>
   );
