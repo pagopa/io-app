6cca26ffc5464f9a099c8e70c30a2d7f5134c823
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-04-07 16:37:05 +0200

    fixes

--- ios/AppReviewModule.swift
+++ ios/AppReviewModule.swift
@@ -8,18 +8,29 @@
 import Foundation
 import UIKit
 import StoreKit
+import SwiftUICore
 
 
 /** `@objc` attribute exposes Swift methods to the Objective-C runtime**/
 @objc(AppReviewModule)
 class AppReviewModule: NSObject {
-  
-  @objc
+  @MainActor @objc
   func requestReview () -> Void {
-    if #available(iOS 14.0, *) {
-      let activeWindowScene = UIApplication.shared.connectedScenes.filter { scene in
+    if #available(iOS 16.0, *) {
+      let activeWindowScene = UIApplication.shared.connectedScenes.first { scene in
         return scene.activationState == .foregroundActive && scene is UIWindowScene
-      }.first
+      }
+      
+      if let scene = activeWindowScene as? UIWindowScene {
+        AppStore.requestReview(in: scene)
+        return
+      }
+    }
+    
+    if #available(iOS 14.0, *) {
+      let activeWindowScene = UIApplication.shared.connectedScenes.first { scene in
+        return scene.activationState == .foregroundActive && scene is UIWindowScene
+      }
       
       if let scene = activeWindowScene as? UIWindowScene {
         SKStoreReviewController.requestReview(in: scene)
