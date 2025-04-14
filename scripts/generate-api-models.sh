5fbecfbe96e316a9be3184f0d76342aad7212b74
Alessandro Izzo <alessandro.izzo998@gmail.com>
2025-04-14 09:39:19 +0200

    chore: upgrade services-metadata version

--- scripts/generate-api-models.sh
+++ scripts/generate-api-models.sh
@@ -2,7 +2,7 @@
 
 IO_BACKEND_VERSION=v16.12.1
 # need to change after merge on io-services-metadata
-IO_SERVICES_METADATA_VERSION=IOBP-1577-payment-flow-webview-feature-flag
+IO_SERVICES_METADATA_VERSION=1.0.68
 # Session manager version
 IO_SESSION_MANAGER_VERSION=1.4.0
 
