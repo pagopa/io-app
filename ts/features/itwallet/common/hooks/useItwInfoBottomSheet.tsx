59f790d8739a97b4f97f56c21da4c9cf54d04353
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2025-04-15 15:07:11 +0200

    feat(IT Wallet): Add L3 identification mode with CIE support and update UI components

--- ts/features/itwallet/common/hooks/useItwInfoBottomSheet.tsx
+++ ts/features/itwallet/common/hooks/useItwInfoBottomSheet.tsx
@@ -1,7 +1,8 @@
 import { H6, IOStyles, VSpacer, VStack } from "@pagopa/io-app-design-system";
-import { View } from "react-native";
+import { ImageSourcePropType, StyleSheet, View } from "react-native";
 import IOMarkdown from "../../../../components/IOMarkdown";
 import { useIOBottomSheetAutoresizableModal } from "../../../../utils/hooks/bottomSheet";
+import { AnimatedImage } from "../../../../components/AnimatedImage.tsx";
 
 /**
  * The type of the content of the bottom sheet.
@@ -17,20 +18,28 @@ type ItwInfoFlowContentType = {
 type ItwInfoFlowProps = {
   title: string;
   content: Array<ItwInfoFlowContentType>;
+  imageSrc?: ImageSourcePropType;
 };
 
 /**
  * A hook that returns a function to present an info bottom sheet.
  * @param title - the title of the bottom sheet.
  * @param content - the content of the bottom sheet. Consists of an array of objects with a title and a body.
+ * @param imageSrc - the source of the image to be displayed in the bottom sheet.
  */
-export const useItwInfoBottomSheet = ({ title, content }: ItwInfoFlowProps) => {
+export const useItwInfoBottomSheet = ({
+  title,
+  content,
+  imageSrc
+}: ItwInfoFlowProps) => {
   const BottomSheetBody = () => (
     <View style={IOStyles.flex}>
       {content.map((item, index) => (
         <VStack key={`${index}_${item.title}`} space={8}>
           {item.title && <H6>{item.title}</H6>}
           <IOMarkdown content={item.body} />
+          <VSpacer size={16} />
+          {imageSrc && <AnimatedImage source={imageSrc} style={styles.image} />}
         </VStack>
       ))}
       <VSpacer size={24} />
@@ -48,3 +57,7 @@ export const useItwInfoBottomSheet = ({ title, content }: ItwInfoFlowProps) => {
     bottomSheet
   };
 };
+
+const styles = StyleSheet.create({
+  image: { resizeMode: "cover", width: "100%" }
+});
--- ts/features/itwallet/identification/screens/ItwIdentificationModeSelectionScreen.tsx
+++ ts/features/itwallet/identification/screens/ItwIdentificationModeSelectionScreen.tsx
@@ -1,7 +1,11 @@
 import {
   ContentWrapper,
+  Divider,
   ListItemHeader,
+  ListItemInfo,
+  ListItemNav,
   ModuleNavigation,
+  VSpacer,
   VStack
 } from "@pagopa/io-app-design-system";
 import { useCallback, useMemo } from "react";
@@ -19,23 +23,151 @@ import { isCIEAuthenticationSupportedSelector } from "../../machine/eid/selector
 import { IOStackNavigationRouteProps } from "../../../../navigation/params/AppParamsList";
 import { ItwParamsList } from "../../navigation/ItwParamsList";
 import { itwDisabledIdentificationMethodsSelector } from "../../common/store/selectors/remoteConfig";
+import { itwIsL3EnabledSelector } from "../../common/store/selectors/preferences.ts";
+import { useItwInfoBottomSheet } from "../../common/hooks/useItwInfoBottomSheet.tsx";
 
-export type ItwIdentificationModeSelectionScreenNavigationParams = {
-  eidReissuing?: boolean;
+type L3IdentificationViewProps = {
+  handleCiePinPress: () => void;
+  handleCieIdPress: () => void;
 };
 
-type ScreenProps = IOStackNavigationRouteProps<
-  ItwParamsList,
-  "ITW_IDENTIFICATION_MODE_SELECTION"
->;
+const L3IdentificationView = ({
+  handleCiePinPress,
+  handleCieIdPress
+}: L3IdentificationViewProps) => {
+  const cieBottomSheet = useItwInfoBottomSheet({
+    title: I18n.t(
+      "features.itWallet.identification.l3.mode.bottomSheet.cie.title"
+    ),
+    content: [
+      {
+        body: I18n.t(
+          "features.itWallet.identification.l3.mode.bottomSheet.cie.content"
+        )
+      }
+    ],
+    // TODO: replace with the correct image when available
+    imageSrc: require("../../../../../img/features/itWallet/identification/itw_cie_placeholder.png")
+  });
 
-export const ItwIdentificationModeSelectionScreen = (params: ScreenProps) => {
-  const machineRef = ItwEidIssuanceMachineContext.useActorRef();
-  const isCieAuthenticationSupported = ItwEidIssuanceMachineContext.useSelector(
-    isCIEAuthenticationSupportedSelector
+  const pinBottomSheet = useItwInfoBottomSheet({
+    title: I18n.t(
+      "features.itWallet.identification.l3.mode.bottomSheet.pin.title"
+    ),
+    content: [
+      {
+        body: I18n.t(
+          "features.itWallet.identification.l3.mode.bottomSheet.pin.content"
+        )
+      }
+    ],
+    // TODO: replace with the correct image when available
+    imageSrc: require("../../../../../img/features/itWallet/identification/itw_cie_pin_placeholder.png")
+  });
+
+  return (
+    <IOScrollViewWithLargeHeader
+      title={{
+        label: I18n.t("features.itWallet.identification.l3.mode.title")
+      }}
+      description={I18n.t(
+        "features.itWallet.identification.l3.mode.description"
+      )}
+      headerActionsProp={{ showHelp: true }}
+      actions={{
+        type: "SingleButton",
+        primary: {
+          label: I18n.t("features.itWallet.identification.l3.mode.action"),
+          accessibilityLabel: I18n.t(
+            "features.itWallet.identification.l3.mode.action"
+          ),
+          onPress: handleCiePinPress,
+          testID: "l3-primary-action"
+        }
+      }}
+      testID="l3-identification-view"
+    >
+      <ContentWrapper>
+        <ListItemHeader
+          label={I18n.t(
+            "features.itWallet.identification.l3.mode.ciePin.header"
+          )}
+          testID="l3-cie-pin-header"
+        />
+        <VStack space={8}>
+          <ListItemInfo
+            value={I18n.t(
+              "features.itWallet.identification.l3.mode.ciePin.card"
+            )}
+            icon={"fiscalCodeIndividual"}
+            testID="l3-cie-card-info"
+            endElement={{
+              type: "iconButton",
+              componentProps: {
+                icon: "info",
+                onPress: () => cieBottomSheet.present(),
+                accessibilityLabel: I18n.t("global.buttons.info"),
+                testID: "l3-cie-info-button"
+              }
+            }}
+          />
+          <Divider />
+          <ListItemInfo
+            value={I18n.t(
+              "features.itWallet.identification.l3.mode.ciePin.pin"
+            )}
+            icon={"key"}
+            testID="l3-pin-info"
+            endElement={{
+              type: "iconButton",
+              componentProps: {
+                icon: "info",
+                onPress: () => pinBottomSheet.present(),
+                accessibilityLabel: I18n.t("global.buttons.info"),
+                testID: "l3-pin-info-button"
+              }
+            }}
+          />
+        </VStack>
+        <VSpacer size={24} />
+        <ListItemHeader
+          label={I18n.t(
+            "features.itWallet.identification.l3.mode.cieId.header"
+          )}
+          testID="l3-cie-id-header"
+        />
+        <ListItemNav
+          icon={"cie"}
+          value={I18n.t("features.itWallet.identification.l3.mode.cieId.title")}
+          description={I18n.t(
+            "features.itWallet.identification.l3.mode.cieId.subtitle"
+          )}
+          onPress={handleCieIdPress}
+          testID="l3-cie-id-nav"
+        />
+        {cieBottomSheet.bottomSheet}
+        {pinBottomSheet.bottomSheet}
+      </ContentWrapper>
+    </IOScrollViewWithLargeHeader>
   );
+};
+
+type DefaultIdentificationViewProps = {
+  onSpidPress: () => void;
+  onCiePinPress: () => void;
+  onCieIdPress: () => void;
+};
+
+const DefaultIdentificationView = ({
+  onSpidPress,
+  onCiePinPress,
+  onCieIdPress
+}: DefaultIdentificationViewProps) => {
   const disabledIdentificationMethods = useIOSelector(
     itwDisabledIdentificationMethodsSelector
+  );
+  const isCieAuthenticationSupported = ItwEidIssuanceMachineContext.useSelector(
+    isCIEAuthenticationSupportedSelector
   );
 
   const isSpidDisabled = useMemo(
@@ -50,38 +182,10 @@ export const ItwIdentificationModeSelectionScreen = (params: ScreenProps) => {
     () => disabledIdentificationMethods.includes("CiePin"),
     [disabledIdentificationMethods]
   );
-
   const isCieSupported = useMemo(
     () => cieFlowForDevServerEnabled || isCieAuthenticationSupported,
     [isCieAuthenticationSupported]
   );
-
-  const { eidReissuing } = params.route.params;
-
-  useFocusEffect(
-    useCallback(() => {
-      if (eidReissuing) {
-        machineRef.send({ type: "start-reissuing" });
-      }
-    }, [eidReissuing, machineRef])
-  );
-
-  useFocusEffect(trackItWalletIDMethod);
-
-  const handleSpidPress = useCallback(() => {
-    machineRef.send({ type: "select-identification-mode", mode: "spid" });
-    trackItWalletIDMethodSelected({ ITW_ID_method: "spid" });
-  }, [machineRef]);
-
-  const handleCiePinPress = useCallback(() => {
-    machineRef.send({ type: "select-identification-mode", mode: "ciePin" });
-    trackItWalletIDMethodSelected({ ITW_ID_method: "ciePin" });
-  }, [machineRef]);
-
-  const handleCieIdPress = useCallback(() => {
-    machineRef.send({ type: "select-identification-mode", mode: "cieId" });
-    trackItWalletIDMethodSelected({ ITW_ID_method: "cieId" });
-  }, [machineRef]);
 
   return (
     <IOScrollViewWithLargeHeader
@@ -104,7 +208,7 @@ export const ItwIdentificationModeSelectionScreen = (params: ScreenProps) => {
               )}
               testID="Spid"
               icon="spid"
-              onPress={handleSpidPress}
+              onPress={onSpidPress}
             />
           )}
           {isCieSupported && !isCiePinDisabled && (
@@ -117,7 +221,7 @@ export const ItwIdentificationModeSelectionScreen = (params: ScreenProps) => {
               )}
               testID="CiePin"
               icon="fiscalCodeIndividual"
-              onPress={handleCiePinPress}
+              onPress={onCiePinPress}
             />
           )}
           {!isCieIdDisabled && (
@@ -130,11 +234,64 @@ export const ItwIdentificationModeSelectionScreen = (params: ScreenProps) => {
               )}
               icon="device"
               testID="CieID"
-              onPress={handleCieIdPress}
+              onPress={onCieIdPress}
             />
           )}
         </VStack>
       </ContentWrapper>
     </IOScrollViewWithLargeHeader>
+  );
+};
+
+export type ItwIdentificationModeSelectionScreenNavigationParams = {
+  eidReissuing?: boolean;
+};
+
+type ScreenProps = IOStackNavigationRouteProps<
+  ItwParamsList,
+  "ITW_IDENTIFICATION_MODE_SELECTION"
+>;
+
+export const ItwIdentificationModeSelectionScreen = (params: ScreenProps) => {
+  const machineRef = ItwEidIssuanceMachineContext.useActorRef();
+  const isL3Enabled = useIOSelector(itwIsL3EnabledSelector);
+
+  const { eidReissuing } = params.route.params;
+
+  useFocusEffect(
+    useCallback(() => {
+      if (eidReissuing) {
+        machineRef.send({ type: "start-reissuing" });
+      }
+    }, [eidReissuing, machineRef])
+  );
+  useFocusEffect(trackItWalletIDMethod);
+
+  const handleSpidPress = useCallback(() => {
+    machineRef.send({ type: "select-identification-mode", mode: "spid" });
+    trackItWalletIDMethodSelected({ ITW_ID_method: "spid" });
+  }, [machineRef]);
+
+  const handleCiePinPress = useCallback(() => {
+    machineRef.send({ type: "select-identification-mode", mode: "ciePin" });
+    trackItWalletIDMethodSelected({ ITW_ID_method: "ciePin" });
+  }, [machineRef]);
+
+  const handleCieIdPress = useCallback(() => {
+    machineRef.send({ type: "select-identification-mode", mode: "cieId" });
+    trackItWalletIDMethodSelected({ ITW_ID_method: "cieId" });
+  }, [machineRef]);
+
+  return isL3Enabled ? (
+    <L3IdentificationView
+      handleCiePinPress={handleCiePinPress}
+      handleCieIdPress={handleCieIdPress}
+    />
+  ) : (
+    <DefaultIdentificationView
+      onSpidPress={handleSpidPress}
+      onCiePinPress={handleCiePinPress}
+      onCieIdPress={handleCieIdPress}
+    />
   );
 };
