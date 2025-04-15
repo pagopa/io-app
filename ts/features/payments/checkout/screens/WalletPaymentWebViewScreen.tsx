f29f367f9555c2aa413e7f2f33d03e5b4bc457c4
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-04-15 09:51:02 +0200

    fix: adjust originWhitelist for with dev entry

--- ts/features/payments/checkout/screens/WalletPaymentWebViewScreen.tsx
+++ ts/features/payments/checkout/screens/WalletPaymentWebViewScreen.tsx
@@ -2,13 +2,20 @@ import { WebView } from "react-native-webview";
 import { useIOSelector } from "../../../../store/hooks";
 import { WALLET_WEBVIEW_OUTCOME_SCHEMA } from "../../common/utils/const";
 import { walletPaymentWebViewPayloadSelector } from "../store/selectors";
+import { isDevEnv } from "../../../../utils/environment";
 
 const WalletPaymentWebViewScreen = () => {
   const payload = useIOSelector(walletPaymentWebViewPayloadSelector);
 
+  const originSchemasWhiteList = [
+    "https://*",
+    "iowallet://*",
+    ...(isDevEnv ? ["http://*"] : [])
+  ];
+
   return (
     <WebView
-      originWhitelist={["iowallet://*"]}
+      originWhitelist={originSchemasWhiteList}
       onShouldStartLoadWithRequest={event => {
         if (event.url.startsWith(WALLET_WEBVIEW_OUTCOME_SCHEMA)) {
           payload?.onSuccess?.(event.url);
