efc93c5cf174ee4bf06bde496064506e50664afb
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2025-04-18 14:54:32 +0200

    fix: test tracking actions by using mixpanelTrack directly in identification analytics

--- ts/features/identification/analytics/__tests__/index.test.ts
+++ ts/features/identification/analytics/__tests__/index.test.ts
@@ -1,5 +1,3 @@
-import { Mixpanel } from "mixpanel-react-native";
-import { mixpanelToken } from "../../../../config";
 import { trackIdentificationAction } from "../index";
 import {
   identificationCancel,
@@ -11,63 +9,62 @@ import {
   identificationSuccess
 } from "../../store/actions";
 import { PinString } from "../../../../types/PinString";
+import { mixpanelTrack } from "../../../../mixpanel";
 
-// Mock the Mixpanel class
-jest.mock("mixpanel-react-native", () => {
-  const mockMixpanel = {
-    track: jest.fn()
-  };
-
-  return {
-    Mixpanel: jest.fn(() => mockMixpanel)
-  };
-});
+jest.mock("../../../../mixpanel", () => ({
+  mixpanelTrack: jest.fn()
+}));
 
 jest.mock("../../../../config", () => ({
   mixpanelToken: "test-token"
 }));
 
 describe("trackIdentificationAction", () => {
-  const mockMixpanel = new Mixpanel(mixpanelToken, true);
-  const tracker = trackIdentificationAction(mockMixpanel);
-
-  it("should track identificationRequest action", () => {
-    tracker(identificationRequest(true));
-    expect(mockMixpanel.track).toHaveBeenCalledWith("IDENTIFICATION_REQUEST");
+  it("should track 'identificationRequest' action", () => {
+    trackIdentificationAction(identificationRequest(true));
+    expect(mixpanelTrack).toHaveBeenCalledWith("IDENTIFICATION_REQUEST");
   });
 
-  it("should track identificationStart action", () => {
-    tracker(identificationStart("123456" as PinString, true));
-    expect(mockMixpanel.track).toHaveBeenCalledWith("IDENTIFICATION_START");
+  it("should track 'identificationStart' action", () => {
+    trackIdentificationAction(identificationStart("123456" as PinString, true));
+    expect(mixpanelTrack).toHaveBeenCalledWith("IDENTIFICATION_START");
   });
 
-  it("should track identificationCancel action", () => {
-    tracker(identificationCancel());
-    expect(mockMixpanel.track).toHaveBeenCalledWith("IDENTIFICATION_CANCEL");
+  it("should track 'identificationCancel' action", () => {
+    trackIdentificationAction(identificationCancel());
+    expect(mixpanelTrack).toHaveBeenCalledWith("IDENTIFICATION_CANCEL");
   });
 
-  it("should track identificationSuccess action with payload", () => {
-    tracker(identificationSuccess({ isBiometric: true }));
-    expect(mockMixpanel.track).toHaveBeenCalledWith(
+  it("should track 'identificationSuccess' action with payload", () => {
+    trackIdentificationAction(identificationSuccess({ isBiometric: true }));
+    expect(mixpanelTrack).toHaveBeenCalledWith(
       "IDENTIFICATION_SUCCESS",
       expect.objectContaining({ identification_method: "bio" })
     );
   });
 
-  it("should track identificationFailure action", () => {
-    tracker(identificationFailure());
-    expect(mockMixpanel.track).toHaveBeenCalledWith("IDENTIFICATION_FAILURE");
+  it("should track 'identificationFailure' action", () => {
+    trackIdentificationAction(identificationFailure());
+    expect(mixpanelTrack).toHaveBeenCalledWith("IDENTIFICATION_FAILURE");
   });
 
-  it("should track identificationPinReset action", () => {
-    tracker(identificationPinReset());
-    expect(mockMixpanel.track).toHaveBeenCalledWith("IDENTIFICATION_PIN_RESET");
+  it("should track 'identificationPinReset' action", () => {
+    trackIdentificationAction(identificationPinReset());
+    expect(mixpanelTrack).toHaveBeenCalledWith("IDENTIFICATION_PIN_RESET");
   });
 
-  it("should track identificationForceLogout action", () => {
-    tracker(identificationForceLogout());
-    expect(mockMixpanel.track).toHaveBeenCalledWith(
-      "IDENTIFICATION_FORCE_LOGOUT"
-    );
+  it("should track 'identificationForceLogout' action", () => {
+    trackIdentificationAction(identificationForceLogout());
+    expect(mixpanelTrack).toHaveBeenCalledWith("IDENTIFICATION_FORCE_LOGOUT");
+  });
+
+  it("should track 'identificationSuccess' with proper parameters", () => {
+    trackIdentificationAction(identificationSuccess({ isBiometric: true }));
+    expect(mixpanelTrack).toHaveBeenCalledWith("IDENTIFICATION_SUCCESS", {
+      event_category: "UX",
+      event_type: "confirm",
+      flow: undefined,
+      identification_method: "bio"
+    });
   });
 });
--- ts/features/identification/analytics/index.ts
+++ ts/features/identification/analytics/index.ts
@@ -1,6 +1,5 @@
 // disabled in order to allows comments between the switch
 import { getType } from "typesafe-actions";
-import { mixpanel } from "../../../mixpanel";
 import { Action } from "../../../store/actions/types";
 import {
   identificationCancel,
@@ -12,29 +11,30 @@ import {
   identificationSuccess
 } from "../store/actions";
 import { buildEventProperties } from "../../../utils/analytics";
+import { mixpanelTrack } from "../../../mixpanel";
 
-export const trackIdentificationAction =
-  (mp: NonNullable<typeof mixpanel>) =>
-  (action: Action): void | ReadonlyArray<null> => {
-    switch (action.type) {
-      // identification
-      // identificationSuccess is handled separately
-      // because it has a payload.
-      case getType(identificationRequest):
-      case getType(identificationStart):
-      case getType(identificationCancel):
-      case getType(identificationFailure):
-      case getType(identificationPinReset):
-      case getType(identificationForceLogout):
-        return mp.track(action.type);
+export const trackIdentificationAction = (
+  action: Action
+): void | ReadonlyArray<null> => {
+  switch (action.type) {
+    // identification
+    // identificationSuccess is handled separately
+    // because it has a payload.
+    case getType(identificationRequest):
+    case getType(identificationStart):
+    case getType(identificationCancel):
+    case getType(identificationFailure):
+    case getType(identificationPinReset):
+    case getType(identificationForceLogout):
+      return mixpanelTrack(action.type);
 
-      // identification: identificationSuccess
-      case getType(identificationSuccess):
-        return mp.track(
-          action.type,
-          buildEventProperties("UX", "confirm", {
-            identification_method: action.payload.isBiometric ? "bio" : "pin"
-          })
-        );
-    }
-  };
+    // identification: identificationSuccess
+    case getType(identificationSuccess):
+      return mixpanelTrack(
+        action.type,
+        buildEventProperties("UX", "confirm", {
+          identification_method: action.payload.isBiometric ? "bio" : "pin"
+        })
+      );
+  }
+};
