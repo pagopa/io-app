8ffedf99a428af500c334c1e8a654669858b89ef
Federico Mastrini <federicomastrini93@gmail.com>
2025-05-05 19:08:36 +0200

    fix: tests

--- ts/boot/configureRectotron.ts
+++ ts/boot/configureRectotron.ts
@@ -2,15 +2,16 @@ import { NativeModules } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Reactotron from "reactotron-react-native";
 import { reactotronRedux } from "reactotron-redux";
+import { isDevEnv } from "../utils/environment";
 
 // add a regex to avoid tracing specific urls in Reactotron timeline
 const ignoredUrls: RegExp | undefined = /symbolicate/;
 
 // Automatically gets the machine's IP address to configure Reactotron
 const getScriptHostname = () => {
-  if (__DEV__) {
+  if (isDevEnv) {
     const scriptURL = NativeModules.SourceCode.scriptURL;
-    return scriptURL.split("://")[1].split(":")[0];
+    return scriptURL?.split("://")[1].split(":")[0];
   }
   return undefined;
 };
