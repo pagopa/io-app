715d639a1a42ed79764713ebf9d9598466d7ce92
Andrea <andrea.piai@pagopa.it>
2025-05-14 12:34:21 +0200

    chore: [IOCOM-2364] add `reason` to `PN_SERVICE_STATUS_CHANGE_ERROR` (#6975)
    ## Short description
    This PR adds the `reason` property to the mixpanel event
    `PN_SERVICE_STATUS_CHANGE_ERROR`
    
    ## List of changes proposed in this pull request
    - `reason` property to the mixpanel event
    `PN_SERVICE_STATUS_CHANGE_ERROR`
    
    ## How to test
    Using io-dev-api-server, simulate a left either, a 500 failure status
    code and a generic throw inside the pn activation saga and check that is
    properly reported.

--- ts/features/pn/store/sagas/watchPnSaga.ts
+++ ts/features/pn/store/sagas/watchPnSaga.ts
@@ -1,4 +1,5 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import * as E from "fp-ts/lib/Either";
 import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/function";
@@ -39,12 +40,13 @@ function* handlePnActivation(
       isPnTestEnabledSelector
     );
 
-    const result = yield* call(upsertPnActivation, {
+    const requestData = {
       body: {
         activation_status
       },
       isTest
-    });
+    };
+    const result = yield* call(upsertPnActivation, requestData);
 
     if (E.isRight(result) && result.right.status === 204) {
       yield* all([
@@ -53,12 +55,18 @@ function* handlePnActivation(
         call(loadPreferenceIfValidId, pnServiceId)
       ]);
       action.payload.onSuccess?.();
+    } else if (E.isLeft(result)) {
+      throw Error(readableReport(result.left));
     } else {
-      throw new Error();
+      throw Error(
+        `Status code: ${result.right.status} Request data: ${JSON.stringify(
+          requestData
+        )}`
+      );
     }
   } catch (e) {
     yield* all([
-      call(reportPNServiceStatusOnFailure, !activation_status),
+      call(reportPNServiceStatusOnFailure, !activation_status, `${e}`),
       put(pnActivationUpsert.failure()),
       call(loadPreferenceIfValidId, pnServiceId)
     ]);
@@ -66,7 +74,10 @@ function* handlePnActivation(
   }
 }
 
-function* reportPNServiceStatusOnFailure(predictedValue: boolean) {
+function* reportPNServiceStatusOnFailure(
+  predictedValue: boolean,
+  reason: string
+) {
   const pnServiceId = yield* select(pnMessagingServiceIdSelector);
   const pnServicePreferencesPot = yield* select(
     servicePreferencePotByIdSelector,
@@ -82,7 +93,7 @@ function* reportPNServiceStatusOnFailure(predictedValue: boolean) {
     ),
     O.getOrElse(() => predictedValue)
   );
-  trackPNServiceStatusChangeError(isServiceActive);
+  trackPNServiceStatusChangeError(isServiceActive, reason);
 }
 
 export function* watchPnSaga(bearerToken: SessionToken): SagaIterator {
