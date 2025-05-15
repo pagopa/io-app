b888e47f2ea894f070f38a772ecd6ab64355afa6
Alice Di Rico <alice.dirico@pagopa.it>
2025-05-15 16:12:57 +0200

    add MP tracking

--- ts/features/pageNotFound/screens/index.tsx
+++ ts/features/pageNotFound/screens/index.tsx
@@ -3,6 +3,11 @@ import I18n from "../../../i18n";
 import { OperationResultScreenContent } from "../../../components/screens/OperationResultScreenContent";
 import { useIONavigation } from "../../../navigation/params/AppParamsList";
 import { openAppStoreUrl } from "../../../utils/url";
+import { useOnFirstRender } from "../../../utils/hooks/useOnFirstRender";
+import {
+  track404ErrorScreen,
+  track404ErrorScreenUpdateAppButton
+} from "../analytics";
 
 export const BTN_UPDATE_TEST_ID = "pageNotFoundBtnUpdateId";
 export const BTN_CLOSE_TEST_ID = "pageNotFoundBtnCloseId";
@@ -10,6 +15,10 @@ export const BTN_CLOSE_TEST_ID = "pageNotFoundBtnCloseId";
 export const PageNotFound = () => {
   const { error } = useIOToast();
   const { popToTop } = useIONavigation();
+
+  useOnFirstRender(() => {
+    void track404ErrorScreen();
+  });
 
   return (
     <OperationResultScreenContent
@@ -20,6 +29,7 @@ export const PageNotFound = () => {
         testID: BTN_UPDATE_TEST_ID,
         label: I18n.t("btnUpdateApp"),
         onPress: async () => {
+          void track404ErrorScreenUpdateAppButton();
           void openAppStoreUrl(() => {
             error(I18n.t("msgErrorUpdateApp"));
           });
