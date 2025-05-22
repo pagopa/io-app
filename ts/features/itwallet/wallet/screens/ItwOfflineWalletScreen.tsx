579a323a0384766e413d725227ad6f01b460a7f7
Federico Mastrini <federicomastrini93@gmail.com>
2025-05-22 12:38:34 +0200

    fix: double headers in wallet's offline screen

--- ts/features/itwallet/wallet/screens/ItwOfflineWalletScreen.tsx
+++ ts/features/itwallet/wallet/screens/ItwOfflineWalletScreen.tsx
@@ -1,15 +1,27 @@
 import { HeaderFirstLevel } from "@pagopa/io-app-design-system";
+import { useEffect } from "react";
 import { IOScrollView } from "../../../../components/ui/IOScrollView";
 import I18n from "../../../../i18n";
 import { ItwWalletCardsContainer } from "../components/ItwWalletCardsContainer.tsx";
 import { withOfflineAlert } from "../utils/withOfflineAlert";
 import { useOnFirstRender } from "../../../../utils/hooks/useOnFirstRender.ts";
 import { trackItwOfflineWallet } from "../../analytics";
+import { useIONavigation } from "../../../../navigation/params/AppParamsList.ts";
 
 const ItwOfflineWalletScreen = () => {
+  const navigation = useIONavigation();
+
   useOnFirstRender(() => {
     trackItwOfflineWallet();
   });
+
+  useEffect(() => {
+    // Ensures that no hader is displayed other than the one mounted by the screen
+    navigation.setOptions({
+      headerShown: false,
+      header: undefined
+    });
+  }, [navigation]);
 
   return (
     <>
