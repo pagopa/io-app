b0a51a3229b7da6dec681f981f9ab47fb9dd9d65
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2025-05-26 16:51:26 +0200

    chore: show in playground the possibility to navigate to EHIC only if it is valid

--- ts/features/itwallet/playgrounds/components/ItwL3CredentialSection.tsx
+++ ts/features/itwallet/playgrounds/components/ItwL3CredentialSection.tsx
@@ -1,10 +1,13 @@
-import {ListItemHeader, ListItemNav} from "@pagopa/io-app-design-system";
+import { ListItemHeader, ListItemNav } from "@pagopa/io-app-design-system";
 import { useIONavigation } from "../../../../navigation/params/AppParamsList.ts";
 import { ITW_ROUTES } from "../../navigation/routes.ts";
 import { CredentialL3Key } from "../../common/utils/itwMocksUtils.ts";
+import { useIOSelector } from "../../../../store/hooks.ts";
+import { itwLifecycleIsValidSelector } from "../../lifecycle/store/selectors";
 
 export const ItwL3CredentialSection = () => {
   const navigation = useIONavigation();
+  const isItwValid = useIOSelector(itwLifecycleIsValidSelector);
 
   const handleCredentialPress = (credentialType: CredentialL3Key) => {
     navigation.navigate(ITW_ROUTES.MAIN, {
@@ -23,11 +26,13 @@ export const ItwL3CredentialSection = () => {
         description="Navigate to the Driving License detail screen"
         onPress={() => handleCredentialPress("mdl")}
       />
-      <ListItemNav
-        value="EU Health Insurance Card L3 (Need eID)"
-        description="Navigate to the EHIC detail screen"
-        onPress={() => handleCredentialPress("ts")}
-      />
+      {isItwValid && (
+        <ListItemNav
+          value="EU Health Insurance Card L3"
+          description="Navigate to the EHIC detail screen"
+          onPress={() => handleCredentialPress("ts")}
+        />
+      )}
       <ListItemNav
         value="Disability Card L3"
         description="Navigate to the Disability Card detail screen"
