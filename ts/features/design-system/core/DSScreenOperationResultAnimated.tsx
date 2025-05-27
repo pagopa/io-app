1f23fcfd88f6214ba4060b2141f55c9a6b674731
Damiano Plebani <damiano.plebani@pagopa.it>
2025-05-27 11:00:19 +0200

    Add the new DS page for the OperationResult with animation

---
+++ ts/features/design-system/core/DSScreenOperationResultAnimated.tsx
@@ -0,0 +1,50 @@
+import { BodyProps } from "@pagopa/io-app-design-system";
+import { useNavigation } from "@react-navigation/native";
+import { OperationResultScreenContent } from "../../../components/screens/OperationResultScreenContent";
+import I18n from "../../../i18n";
+
+const DSScreenOperationResultAnimated = () => {
+  const navigation = useNavigation();
+
+  const bodyPropsArray: Array<BodyProps> = [
+    {
+      text: I18n.t("email.cduScreens.emailAlreadyTaken.subtitleStart"),
+      style: {
+        textAlign: "center"
+      }
+    },
+    {
+      text: <> example@try.com </>,
+      style: {
+        textAlign: "center"
+      },
+      weight: "Semibold"
+    },
+    {
+      text: I18n.t("email.cduScreens.emailAlreadyTaken.subtitleEnd"),
+      style: {
+        textAlign: "center"
+      }
+    }
+  ];
+
+  return (
+    <OperationResultScreenContent
+      pictogram="umbrella"
+      title="C’è un problema temporaneo, riprova."
+      subtitle={bodyPropsArray}
+      action={{
+        label: I18n.t("global.buttons.close"),
+        accessibilityLabel: I18n.t("global.buttons.close"),
+        onPress: () => navigation.goBack()
+      }}
+      secondaryAction={{
+        label: I18n.t("global.buttons.retry"),
+        accessibilityLabel: I18n.t("global.buttons.retry"),
+        onPress: () => navigation.goBack()
+      }}
+    />
+  );
+};
+
+export { DSScreenOperationResultAnimated };
