a429baa0f09213b1dec9ca96207c43fcafb90f95
Federico Mastrini <federicomastrini93@gmail.com>
2025-06-11 17:56:17 +0200

    chore: remove store structure references from migrations

--- ts/features/itwallet/credentials/store/reducers/migrations.ts
+++ ts/features/itwallet/credentials/store/reducers/migrations.ts
@@ -1,38 +1,31 @@
 import { SdJwt } from "@pagopa/io-react-native-wallet";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
-import { MigrationManifest, PersistPartial } from "redux-persist";
-import { StoredCredential } from "../../../common/utils/itwTypesUtils";
-import { ItwCredentialsState } from ".";
+import { MigrationManifest } from "redux-persist";
 
 export const CURRENT_REDUX_ITW_CREDENTIALS_STORE_VERSION = 1;
 
 export const itwCredentialsStateMigrations: MigrationManifest = {
-  "0": (state): ItwCredentialsState & PersistPartial => {
-    // Version 0
-    // Add optional `storedStatusAttestation` field
-    const addStoredStatusAttestation = (
-      credential: StoredCredential
-    ): StoredCredential => ({
+  // Version 0
+  // Add optional `storedStatusAttestation` field
+  "0": (state: any) => {
+    const addStoredStatusAttestation = (credential: any) => ({
       ...credential,
       storedStatusAttestation: undefined
     });
-    const prevState = state as ItwCredentialsState & PersistPartial;
     return {
-      ...prevState,
-      eid: pipe(prevState.eid, O.map(addStoredStatusAttestation)),
-      credentials: prevState.credentials.map(credential =>
+      ...state,
+      eid: pipe(state.eid, O.map(addStoredStatusAttestation)),
+      credentials: state.credentials.map((credential: any) =>
         pipe(credential, O.map(addStoredStatusAttestation))
       )
     };
   },
 
-  "1": (state): ItwCredentialsState & PersistPartial => {
-    // Version 1
-    // Add issuance and expiration dates to stored credentials
-    const addIatExpProperties = (
-      credential: StoredCredential
-    ): StoredCredential => {
+  // Version 1
+  // Add issuance and expiration dates to stored credentials
+  "1": (state: any) => {
+    const addIatExpProperties = (credential: any) => {
       const { disclosures, sdJwt } = SdJwt.decode(credential.credential);
       const iatDisclosure = disclosures.find(
         ({ decoded }) => decoded[1] === "iat"
@@ -51,11 +44,10 @@ export const itwCredentialsStateMigrations: MigrationManifest = {
         }
       };
     };
-    const prevState = state as ItwCredentialsState & PersistPartial;
     return {
-      ...prevState,
-      eid: pipe(prevState.eid, O.map(addIatExpProperties)),
-      credentials: prevState.credentials.map(credential =>
+      ...state,
+      eid: pipe(state.eid, O.map(addIatExpProperties)),
+      credentials: state.credentials.map((credential: any) =>
         pipe(credential, O.map(addIatExpProperties))
       )
     };
