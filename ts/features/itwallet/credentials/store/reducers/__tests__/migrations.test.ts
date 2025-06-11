147f33fcb48ad0f5de61877b336534c31f67c31d
Federico Mastrini <federicomastrini93@gmail.com>
2025-06-11 17:35:39 +0200

    chore: add `credentials` reducer migrations tests

---
+++ ts/features/itwallet/credentials/store/reducers/__tests__/migrations.test.ts
@@ -0,0 +1,95 @@
+import _ from "lodash";
+import * as O from "fp-ts/lib/Option";
+import { SdJwt } from "@pagopa/io-react-native-wallet";
+import { itwCredentialsStateMigrations } from "../migrations";
+
+jest.mock("@pagopa/io-react-native-wallet", () => ({
+  ...jest.requireActual("@pagopa/io-react-native-wallet"),
+  SdJwt: {
+    ...jest.requireActual("@pagopa/io-react-native-wallet").SdJwt,
+    decode: jest.fn()
+  }
+}));
+
+describe("ITW credentials reducer migrations", () => {
+  it("should migrate from 0 to 1", () => {
+    const basePersistedGlobalStateAt0 = {
+      eid: O.some({}),
+      credentials: [O.some({}), O.some({}), O.none],
+      _persist: {
+        version: 0,
+        rehydrated: false
+      }
+    };
+    const persistedStateAt1 = _.merge(undefined, basePersistedGlobalStateAt0, {
+      eid: O.some({ storedStatusAttestation: undefined }),
+      credentials: [
+        O.some({ storedStatusAttestation: undefined }),
+        O.some({ storedStatusAttestation: undefined }),
+        O.none
+      ]
+    });
+
+    const from0To1Migration = itwCredentialsStateMigrations[0];
+    expect(from0To1Migration).toBeDefined();
+    const nextState = from0To1Migration(basePersistedGlobalStateAt0);
+
+    expect(nextState).toStrictEqual(persistedStateAt1);
+  });
+
+  it("should migrate from 1 to 2", () => {
+    jest.spyOn(SdJwt, "decode").mockReturnValue({
+      disclosures: [{ decoded: ["", "iat", 1718132000] }],
+      sdJwt: {
+        payload: {
+          exp: 1718192000
+        }
+      }
+    } as any);
+
+    const basePersistedGlobalStateAt1 = {
+      eid: O.some({ storedStatusAttestation: undefined }),
+      credentials: [
+        O.some({ storedStatusAttestation: undefined }),
+        O.some({ storedStatusAttestation: undefined }),
+        O.none
+      ],
+      _persist: {
+        version: 0,
+        rehydrated: false
+      }
+    };
+    const persistedStateAt2 = _.merge(undefined, basePersistedGlobalStateAt1, {
+      eid: O.some({
+        storedStatusAttestation: undefined,
+        jwt: {
+          expiration: "2024-06-12T11:33:20.000Z",
+          issuedAt: "2024-06-11T18:53:20.000Z"
+        }
+      }),
+      credentials: [
+        O.some({
+          storedStatusAttestation: undefined,
+          jwt: {
+            expiration: "2024-06-12T11:33:20.000Z",
+            issuedAt: "2024-06-11T18:53:20.000Z"
+          }
+        }),
+        O.some({
+          storedStatusAttestation: undefined,
+          jwt: {
+            expiration: "2024-06-12T11:33:20.000Z",
+            issuedAt: "2024-06-11T18:53:20.000Z"
+          }
+        }),
+        O.none
+      ]
+    });
+
+    const from0To1Migration = itwCredentialsStateMigrations[1];
+    expect(from0To1Migration).toBeDefined();
+    const nextState = from0To1Migration(basePersistedGlobalStateAt1);
+
+    expect(nextState).toStrictEqual(persistedStateAt2);
+  });
+});
