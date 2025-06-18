60fef311810e3a4cbbf028da37fab4236cce8747
Federico Mastrini <federicomastrini93@gmail.com>
2025-06-18 11:47:44 +0200

    chore: removed any

--- ts/features/itwallet/credentials/store/reducers/migrations.ts
+++ ts/features/itwallet/credentials/store/reducers/migrations.ts
@@ -1,31 +1,34 @@
 import { SdJwt } from "@pagopa/io-react-native-wallet";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
-import { MigrationManifest } from "redux-persist";
+import { MigrationManifest, PersistedState } from "redux-persist";
+
+type MigrationState = PersistedState & Record<string, any>;
 
 export const CURRENT_REDUX_ITW_CREDENTIALS_STORE_VERSION = 1;
 
 export const itwCredentialsStateMigrations: MigrationManifest = {
   // Version 0
   // Add optional `storedStatusAttestation` field
-  "0": (state: any) => {
-    const addStoredStatusAttestation = (credential: any) => ({
+  "0": (state: MigrationState) => {
+    const addStoredStatusAttestation = (credential: Record<string, any>) => ({
       ...credential,
       storedStatusAttestation: undefined
     });
     return {
       ...state,
       eid: pipe(state.eid, O.map(addStoredStatusAttestation)),
-      credentials: state.credentials.map((credential: any) =>
-        pipe(credential, O.map(addStoredStatusAttestation))
+      credentials: state.credentials.map(
+        (credential: O.Option<Record<string, any>>) =>
+          pipe(credential, O.map(addStoredStatusAttestation))
       )
     };
   },
 
   // Version 1
   // Add issuance and expiration dates to stored credentials
-  "1": (state: any) => {
-    const addIatExpProperties = (credential: any) => {
+  "1": (state: MigrationState) => {
+    const addIatExpProperties = (credential: Record<string, any>) => {
       const { disclosures, sdJwt } = SdJwt.decode(credential.credential);
       const iatDisclosure = disclosures.find(
         ({ decoded }) => decoded[1] === "iat"
@@ -47,8 +50,9 @@ export const itwCredentialsStateMigrations: MigrationManifest = {
     return {
       ...state,
       eid: pipe(state.eid, O.map(addIatExpProperties)),
-      credentials: state.credentials.map((credential: any) =>
-        pipe(credential, O.map(addIatExpProperties))
+      credentials: state.credentials.map(
+        (credential: O.Option<Record<string, any>>) =>
+          pipe(credential, O.map(addIatExpProperties))
       )
     };
   }
