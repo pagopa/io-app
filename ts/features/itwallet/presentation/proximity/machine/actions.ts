57d8018ce57c12301a6f9a9edaaf110303a17eea
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2025-06-19 14:32:59 +0200

    applied suggestion

--- ts/features/itwallet/presentation/proximity/machine/actions.ts
+++ ts/features/itwallet/presentation/proximity/machine/actions.ts
@@ -1,6 +1,8 @@
 import * as R from "fp-ts/lib/Record";
 import { identity, pipe } from "fp-ts/lib/function";
 import { assign } from "xstate";
+import { StackActions } from "@react-navigation/native";
+import NavigationService from "../../../../../navigation/NavigationService";
 import { useIONavigation } from "../../../../../navigation/params/AppParamsList";
 import { useIOStore } from "../../../../../store/hooks";
 import { ITW_ROUTES } from "../../../navigation/routes";
@@ -47,6 +49,6 @@ export const createProximityActionsImplementation = (
   },
 
   closeProximity: () => {
-    navigation.popToTop();
+    NavigationService.dispatchNavigationAction(StackActions.popToTop());
   }
 });
