edfd64eaad4ea530ca3e1dc18742634c424ef5b4
Federico Mastrini <federicomastrini93@gmail.com>
2025-06-26 12:51:35 +0200

    feat: skip `VALUE_NOT_FOUND` errors

--- ts/features/itwallet/common/store/storages/itwSecureStorage.ts
+++ ts/features/itwallet/common/store/storages/itwSecureStorage.ts
@@ -2,20 +2,34 @@ import * as SecureStorage from "@pagopa/io-react-native-secure-storage";
 import { type Storage } from "redux-persist";
 import * as Sentry from "@sentry/react-native";
 
+export const isValueNotFoundError = (e: unknown): boolean => {
+  if (!e || typeof e !== "object") {
+    return false;
+  }
+  const error = e as { message?: unknown };
+  return (
+    typeof error.message === "string" && error.message === "VALUE_NOT_FOUND"
+  );
+};
+
 export default function itwCreateSecureStorage(): Storage {
   return {
     getItem: async key => {
       try {
         return await SecureStorage.get(key);
       } catch (e) {
-        Sentry.captureException(e, {
-          tags: {
-            isRequired: true
-          }
-        });
-        Sentry.captureMessage(
-          `itwCreateSecureStorage.getItem: SecureStorage.get threw an exception on ${key} key`
-        );
+        if (!isValueNotFoundError(e)) {
+          // VALUE_NOT_FOUND it's a normal case when we try to get a value that is not set
+          // We should send to Sentry only unwanted exceptions
+          Sentry.captureException(e, {
+            tags: {
+              isRequired: true
+            }
+          });
+          Sentry.captureMessage(
+            `itwCreateSecureStorage.getItem: SecureStorage.get threw an exception on ${key} key`
+          );
+        }
         return undefined;
       }
     },
