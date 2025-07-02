dc0df6a3bd0885e151ce106b95856a1338ee2a23
Alessandro Izzo <alessandro.izzo998@gmail.com>
2025-07-02 09:29:29 +0200

    chore: Increase io-services-metadata tag version

--- scripts/generate-api-models.sh
+++ scripts/generate-api-models.sh
@@ -2,7 +2,7 @@
 
 IO_BACKEND_VERSION=v16.17.0
 # need to change after merge on io-services-metadata
-IO_SERVICES_METADATA_VERSION=IOBP-1805-add-idpay-onboarding-and-details-feature-flag
+IO_SERVICES_METADATA_VERSION=1.0.73
 # Session manager version
 IO_SESSION_MANAGER_VERSION=1.4.0
 # IO Wallet Backend version
