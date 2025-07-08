2502cb15316c92a697d782e5ba20a0e36cf1f294
Federico Mastrini <federicomastrini93@gmail.com>
2025-07-08 18:35:35 +0200

    feat(IT Wallet): [SIW-2670] New UX for CIE+PIN identification flow (#7157)
    ## Short description
    This PR aligns the CIE+PIN flow UX for both L2 and L3 levels with the
    [new design
    specifications](https://www.figma.com/design/CXv9wrNktnUtFGHUP5bkh3/Flusso-lettura-CIE---evolutiva?node-id=15901-26156&t=VsRRKtYUPh7YjQCH-4).
    It also refactors different components to improve readability and
    maintainability in both the eID issuance machine and components.
    
    ## List of changes proposed in this pull request
    - Refactored locale files and added new keys
    - Added required images assets
    - Added preparation steps for CIE+PIN in the L2 issuance flow
    - Refactored `useCieInfoAndPinBottomSheets` into
    `useCieInfoBottomSheet`: it now returns only one object based on the
    provided `type` parameter
    - Moved `useNoCieBottomSheet` to
    `ItwL3IdentificationModeSelectionScreen`
    - Renamed `ItwCiePinPreparationScreen` and `ItwCiePreparationScreen` to
    `ItwCiePreparationPinScreen` and `ItwCiePreparationCardScreen`
    - `CiePreparationScreenContent`: renamed to
    `ItwCiePreparationBaseScreenContent` and simplified logic
    - Refactored `UserIdentification` eID machine state
    
    ## How to test
    Try the different issuance flows (IT Wallet, IT Wallet upgrade and
    Documenti su IO) using CIE+PIN identification method and check that the
    UX represents correctly the one on the Figma design.
    Also check that no regressions were introduced in other identification
    methods.
    
    ## Demo
    
    | L2 | L3 |  L3 upgrade | L3 fallback
    | --- | --- | --- | --- |
    | <video
    src="https://github.com/user-attachments/assets/bc739d0a-663f-42e0-99cd-4b6cb545a781"
    /> | <video
    src="https://github.com/user-attachments/assets/256d64c9-d203-47db-83d3-29c2b49145f4"
    />|<video
    src="https://github.com/user-attachments/assets/0bb9d74e-0861-4bdc-97b8-ff3118f12b6f"
    /> | <video
    src="https://github.com/user-attachments/assets/d2793177-de8f-4e02-8eec-119e35fc9203"
    /> |

---
+++ ts/features/itwallet/identification/hooks/useCieInfoBottomSheet.tsx
@@ -0,0 +1,104 @@
+import { VStack } from "@pagopa/io-app-design-system";
+import { StyleSheet, View } from "react-native";
+import { useMemo } from "react";
+import { useIOBottomSheetModal } from "../../../../utils/hooks/bottomSheet";
+import I18n from "../../../../i18n";
+import { ItwEidIssuanceMachineContext } from "../../machine/provider";
+import IOMarkdown from "../../../../components/IOMarkdown";
+import { AnimatedImage } from "../../../../components/AnimatedImage";
+import { renderActionButtons } from "../../../../components/ui/IOScrollView";
+import { CiePreparationType } from "../components/cie/ItwCiePreparationBaseScreenContent";
+
+type Props = { type: CiePreparationType; showSecondaryAction?: boolean };
+
+/**
+ * Hook to display a bottom sheet with information about the CIE
+ * @param type - The type of info to display
+ * @param showSecondaryAction - Whether to show the secondary action button. The bottomsheet has different actions based
+ * on auth level or step in the flow.
+ * @returns The bottom sheet component
+ */
+export const useCieInfoBottomSheet = ({
+  type,
+  showSecondaryAction = true
+}: Props) => {
+  const machineRef = ItwEidIssuanceMachineContext.useActorRef();
+
+  const imageSrc = useMemo(() => {
+    switch (type) {
+      case "card":
+        return require("../../../../../img/features/itWallet/identification/cie_card.png");
+      case "pin":
+        return require("../../../../../img/features/itWallet/identification/cie_pin.png");
+      default:
+        return undefined;
+    }
+  }, [type]);
+
+  const bottomSheet = useIOBottomSheetModal({
+    title: I18n.t(
+      `features.itWallet.identification.cie.bottomSheet.${type}.title`
+    ),
+    component: (
+      <VStack space={24}>
+        <IOMarkdown
+          content={I18n.t(
+            `features.itWallet.identification.cie.bottomSheet.${type}.content`
+          )}
+        />
+        <AnimatedImage source={imageSrc} style={styles.image} />
+        <View>
+          {renderActionButtons(
+            showSecondaryAction
+              ? // If L3 (IT-Wallet), show both primary and secondary actions
+                {
+                  type: "TwoButtons",
+                  primary: {
+                    label: I18n.t(
+                      `features.itWallet.identification.cie.bottomSheet.${type}.primaryAction`
+                    ),
+                    onPress: () => {
+                      bottomSheet.dismiss();
+                    }
+                  },
+                  secondary: {
+                    label: I18n.t(
+                      `features.itWallet.identification.cie.bottomSheet.${type}.secondaryAction`
+                    ),
+                    onPress: () => {
+                      machineRef.send({
+                        type: "go-to-cie-warning",
+                        warning: type
+                      });
+                      bottomSheet.dismiss();
+                    }
+                  }
+                }
+              : // If not L3 (Documenti su IO), show only the primary action
+                {
+                  type: "SingleButton",
+                  primary: {
+                    label: I18n.t(
+                      `features.itWallet.identification.cie.bottomSheet.${type}.primaryAction`
+                    ),
+                    onPress: () => {
+                      bottomSheet.dismiss();
+                    }
+                  }
+                },
+            16
+          )}
+        </View>
+      </VStack>
+    )
+  });
+
+  return bottomSheet;
+};
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: "contain",
+    width: "100%"
+  }
+});
