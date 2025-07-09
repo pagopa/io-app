dc8a00440f92442ceaaa67a750bc43f6dd522021
Federico Mastrini <federicomastrini93@gmail.com>
2025-07-09 08:53:33 +0200

    fix: merge

--- ts/features/itwallet/machine/eid/machine.ts
+++ ts/features/itwallet/machine/eid/machine.ts
@@ -643,9 +643,6 @@ export const itwEidIssuanceMachine = setup({
                 "go-to-cie-warning": {
                   target: "CieWarning.PreparationCie"
                 },
-                "go-to-cie-warning": {
-                  target: "CieWarning.PreparationCie"
-                },
                 back: {
                   target: "PreparationPin"
                 },
