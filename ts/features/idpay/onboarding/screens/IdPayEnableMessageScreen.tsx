9faa165544441cbca27d3771076b78d51668eb2a
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-07-10 15:32:25 +0200

    feat: apply new machine logic to display new message screen

--- ts/features/idpay/onboarding/screens/IdPayEnableMessageScreen.tsx
+++ ts/features/idpay/onboarding/screens/IdPayEnableMessageScreen.tsx
@@ -1,13 +1,20 @@
+import { INonEmptyStringTag } from "@pagopa/ts-commons/lib/strings";
 import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
 import { OperationResultScreenContent } from "../../../../components/screens/OperationResultScreenContent";
 import I18n from "../../../../i18n";
+import { useIODispatch, useIOSelector } from "../../../../store/hooks";
+import { upsertServicePreference } from "../../../services/details/store/actions/preference";
+import { servicePreferenceResponseSuccessByIdSelector } from "../../../services/details/store/reducers";
 import { IdPayOnboardingMachineContext } from "../machine/provider";
 import { selectInitiative } from "../machine/selectors";
 
 const IdPayEnableMessageScreen = () => {
   const { useActorRef, useSelector } = IdPayOnboardingMachineContext;
   const machine = useActorRef();
+  const dispatch = useIODispatch();
+
+  const { serviceId } = useSelector(state => state.context);
 
   const initiative = useSelector(selectInitiative);
 
@@ -16,6 +23,34 @@ const IdPayEnableMessageScreen = () => {
     O.map(i => i.initiativeName),
     O.toUndefined
   );
+
+  const initiativeId = pipe(
+    initiative,
+    O.map(i => i.initiativeId),
+    O.toUndefined
+  );
+
+  const servicePreferenceResponseSuccess = useIOSelector(state =>
+    servicePreferenceResponseSuccessByIdSelector(
+      state,
+      serviceId as string & INonEmptyStringTag
+    )
+  );
+
+  const onSuccess = () => {
+    if (!initiativeId || !servicePreferenceResponseSuccess) {
+      return;
+    }
+
+    dispatch(
+      upsertServicePreference.request({
+        ...servicePreferenceResponseSuccess.value,
+        id: serviceId as string & INonEmptyStringTag,
+        inbox: true
+      })
+    );
+    machine.send({ type: "next" });
+  };
 
   return (
     <OperationResultScreenContent
@@ -26,7 +61,7 @@ const IdPayEnableMessageScreen = () => {
       })}
       action={{
         label: I18n.t("idpay.onboarding.enableMessages.confirmAction"),
-        onPress: () => machine.send({ type: "next" })
+        onPress: onSuccess
       }}
       secondaryAction={{
         label: I18n.t("idpay.onboarding.enableMessages.cancelAction"),
