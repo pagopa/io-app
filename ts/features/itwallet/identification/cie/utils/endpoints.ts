119ce5ff229d6279affedb37aacd5e3b376ddba6
Federico Mastrini <federicomastrini93@gmail.com>
2025-07-15 15:21:37 +0200

    fix: UAT IDP url on iOS

--- ts/features/itwallet/identification/cie/utils/endpoints.ts
+++ ts/features/itwallet/identification/cie/utils/endpoints.ts
@@ -3,6 +3,6 @@ import { Platform } from "react-native";
 const BASE_UAT_URL = "https://collaudo.idserver.servizicie.interno.gov.it/idp/";
 
 export const cieUatEndpoint = Platform.select({
-  ios: `${BASE_UAT_URL}Authn/SSL/Login2`,
+  ios: `${BASE_UAT_URL}Authn/SSL/Login2?`,
   default: BASE_UAT_URL
 });
