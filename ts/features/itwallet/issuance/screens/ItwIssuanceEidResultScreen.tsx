07f438545586797d83fe037079f041d0b83f6d26
Gianluca Spada <gianluca.spada@spindox.it>
2025-07-16 16:07:39 +0200

    chore: IT-Wallet activation success screen

--- ts/features/itwallet/issuance/screens/ItwIssuanceEidResultScreen.tsx
+++ ts/features/itwallet/issuance/screens/ItwIssuanceEidResultScreen.tsx
@@ -5,10 +5,13 @@ import { ItwEidIssuanceMachineContext } from "../../machine/provider";
 import { useItwDisableGestureNavigation } from "../../common/hooks/useItwDisableGestureNavigation";
 import { useAvoidHardwareBackButton } from "../../../../utils/useAvoidHardwareBackButton";
 import { trackAddFirstCredential, trackBackToWallet } from "../../analytics";
+import { useIOSelector } from "../../../../store/hooks";
+import { itwLifecycleIsITWalletValidSelector } from "../../lifecycle/store/selectors";
 
 export const ItwIssuanceEidResultScreen = () => {
   const route = useRoute();
 
+  const isItWalletValid = useIOSelector(itwLifecycleIsITWalletValidSelector);
   const machineRef = ItwEidIssuanceMachineContext.useActorRef();
 
   useItwDisableGestureNavigation();
@@ -19,11 +22,28 @@ export const ItwIssuanceEidResultScreen = () => {
     trackAddFirstCredential();
   };
 
-  const handleClose = async () => {
-    machineRef.send({ type: "go-to-wallet" });
-    trackBackToWallet({ exit_page: route.name, credential: "ITW_ID_V2" });
-  };
+  const handleBackToWallet = () => machineRef.send({ type: "go-to-wallet" });
 
+  // The user successfully activated IT-Wallet
+  if (isItWalletValid) {
+    return (
+      <OperationResultScreenContent
+        pictogram="success"
+        title={I18n.t("features.itWallet.issuance.eidResult.successL3.title")}
+        subtitle={I18n.t(
+          "features.itWallet.issuance.eidResult.successL3.subtitle"
+        )}
+        action={{
+          label: I18n.t(
+            "features.itWallet.issuance.eidResult.successL3.actions.continue"
+          ),
+          onPress: handleBackToWallet
+        }}
+      />
+    );
+  }
+
+  // The user successfully activated Documenti su IO
   return (
     <OperationResultScreenContent
       pictogram="success"
@@ -39,7 +59,10 @@ export const ItwIssuanceEidResultScreen = () => {
         label: I18n.t(
           "features.itWallet.issuance.eidResult.success.actions.close"
         ),
-        onPress: handleClose
+        onPress: () => {
+          handleBackToWallet();
+          trackBackToWallet({ exit_page: route.name, credential: "ITW_ID_V2" });
+        }
       }}
     />
   );
