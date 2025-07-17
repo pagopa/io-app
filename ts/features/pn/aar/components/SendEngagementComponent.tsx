608426dd405f0baabffecd53cf4b86fcab16bda3
Andrea Piai <andrea.piai@pagopa.it>
2025-07-17 18:54:10 +0200

    SendEngagementComponent

---
+++ ts/features/pn/aar/components/SendEngagementComponent.tsx
@@ -0,0 +1,134 @@
+import { useEffect } from "react";
+import { Body, FeatureInfo, H3, VSpacer } from "@pagopa/io-app-design-system";
+import { IOScrollView } from "../../../../components/ui/IOScrollView";
+import { useIOSelector } from "../../../../store/hooks";
+import { pnPrivacyUrlsSelector } from "../../../../store/reducers/backendStatus/remoteConfig";
+import { openWebUrl } from "../../../../utils/url";
+import { useIONavigation } from "../../../../navigation/params/AppParamsList";
+import I18n from "../../../../i18n";
+
+export type SendEngagmentComponentProps = {
+  isLoading: boolean;
+  onClose: () => void;
+  onPrimaryAction: () => void;
+};
+
+export const SendEngagmentComponent = ({
+  isLoading,
+  onClose,
+  onPrimaryAction
+}: SendEngagmentComponentProps) => {
+  const navigation = useIONavigation();
+  const { privacy: privacyUrl, tos: tosUrl } = useIOSelector(
+    pnPrivacyUrlsSelector
+  );
+  const openWebUrlIfNotLoading = (url: string) => {
+    if (!isLoading) {
+      openWebUrl(url);
+    }
+  };
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true
+    });
+  }, [navigation]);
+  return (
+    <IOScrollView
+      actions={{
+        type: "SingleButton",
+        primary: {
+          label: I18n.t("features.pn.aar.serviceActivation.primaryAction"),
+          onPress: onPrimaryAction,
+          loading: isLoading,
+          testID: "primary-action"
+        }
+      }}
+      headerConfig={{
+        title: "",
+        ignoreSafeAreaMargin: false,
+        type: "singleAction",
+        firstAction: {
+          icon: "closeMedium",
+          accessibilityLabel: I18n.t(
+            "features.pn.aar.serviceActivation.headerAction"
+          ),
+          onPress: onClose,
+          testID: "close-button"
+        }
+      }}
+    >
+      <H3 textStyle={{ textAlign: "center" }}>
+        {I18n.t("features.pn.aar.serviceActivation.title")}
+      </H3>
+      <VSpacer size={24} />
+      <FeatureInfo
+        pictogramProps={{ name: "emailDotNotif", pictogramStyle: "default" }}
+        body={
+          <Body>
+            <Body>
+              {I18n.t("features.pn.aar.serviceActivation.feature1.part1")}
+            </Body>
+            <Body weight="Semibold">
+              {I18n.t("features.pn.aar.serviceActivation.feature1.part2")}
+            </Body>
+            <Body>
+              {I18n.t("features.pn.aar.serviceActivation.feature1.part3")}
+            </Body>
+          </Body>
+        }
+      />
+      <VSpacer size={24} />
+      <FeatureInfo
+        pictogramProps={{ name: "savingMoney", pictogramStyle: "default" }}
+        body={
+          <Body>
+            <Body>
+              {I18n.t("features.pn.aar.serviceActivation.feature2.part1")}
+            </Body>
+            <Body weight="Semibold">
+              {I18n.t("features.pn.aar.serviceActivation.feature2.part2")}
+            </Body>
+          </Body>
+        }
+      />
+      <VSpacer size={24} />
+      <FeatureInfo
+        pictogramProps={{ name: "cardFavourite", pictogramStyle: "default" }}
+        body={
+          <Body>
+            <Body>
+              {I18n.t("features.pn.aar.serviceActivation.feature3.part1")}
+            </Body>
+            <Body weight="Semibold">
+              {I18n.t("features.pn.aar.serviceActivation.feature3.part2")}
+            </Body>
+          </Body>
+        }
+      />
+      <VSpacer size={32} />
+      <Body>
+        <Body>{I18n.t("features.pn.aar.serviceActivation.footer.part1")}</Body>
+        <Body weight="Semibold">
+          {I18n.t("features.pn.aar.serviceActivation.footer.part2")}
+        </Body>
+        <Body>{I18n.t("features.pn.aar.serviceActivation.footer.part3")}</Body>
+        <Body
+          asLink
+          weight="Semibold"
+          onPress={() => openWebUrlIfNotLoading(privacyUrl)}
+        >
+          {I18n.t("features.pn.aar.serviceActivation.footer.part4")}
+        </Body>
+        <Body>{I18n.t("features.pn.aar.serviceActivation.footer.part5")}</Body>
+        <Body
+          asLink
+          weight="Semibold"
+          onPress={() => openWebUrlIfNotLoading(tosUrl)}
+        >
+          {I18n.t("features.pn.aar.serviceActivation.footer.part6")}
+        </Body>
+        <Body>{I18n.t("features.pn.aar.serviceActivation.footer.part7")}</Body>
+      </Body>
+    </IOScrollView>
+  );
+};
