98b20feea6504114f3d0c30f2a656ac29286bc00
Gianluca Spada <gianluca.spada@spindox.it>
2025-07-21 09:28:05 +0200

    chore(IT Wallet): [SIW-2220] IT-Wallet activation success screen (#7191)
    ## Short description
    This PR modifies the Wallet activation screen to show that IT-Wallet was
    activated. The fallback to "Documenti su IO" remains.
    
    ## List of changes proposed in this pull request
    - Updated `ItwIssuanceEidResultScreen`
    
    ## How to test
    Activate IT-Wallet and Documenti su IO and ensure the success screens
    are coherent.
    
    |Documenti su IO|IT-Wallet|
    |-|-|
    |<img width="260" alt="doc_io"
    src="https://github.com/user-attachments/assets/8f68eda6-90b8-45e9-9f2a-ecc96ffad254"
    />|<img width="260" alt="itwallet"
    src="https://github.com/user-attachments/assets/de016f49-b76b-46b6-a715-ebcbe33fbc53"
    />|

--- ts/features/itwallet/issuance/screens/ItwIssuanceEidResultScreen.tsx
+++ ts/features/itwallet/issuance/screens/ItwIssuanceEidResultScreen.tsx
@@ -5,10 +5,13 @@ import { ItwEidIssuanceMachineContext } from "../../machine/eid/provider";
 import { useItwDisableGestureNavigation } from "../../common/hooks/useItwDisableGestureNavigation";
 import { useAvoidHardwareBackButton } from "../../../../utils/useAvoidHardwareBackButton";
 import { trackAddFirstCredential, trackBackToWallet } from "../../analytics";
+import { useIOSelector } from "../../../../store/hooks";
+import { itwLifecycleIsITWalletValidSelector } from "../../lifecycle/store/selectors";
 
 export const ItwIssuanceEidResultScreen = () => {
   const route = useRoute();
 
+  const isItWalletValid = useIOSelector(itwLifecycleIsITWalletValidSelector);
   const machineRef = ItwEidIssuanceMachineContext.useActorRef();
 
   useItwDisableGestureNavigation();
@@ -19,27 +22,49 @@ export const ItwIssuanceEidResultScreen = () => {
     trackAddFirstCredential();
   };
 
-  const handleClose = async () => {
-    machineRef.send({ type: "go-to-wallet" });
-    trackBackToWallet({ exit_page: route.name, credential: "ITW_ID_V2" });
-  };
+  const handleBackToWallet = () => machineRef.send({ type: "go-to-wallet" });
 
+  // The user successfully activated IT-Wallet
+  if (isItWalletValid) {
+    return (
+      <OperationResultScreenContent
+        pictogram="success"
+        title={I18n.t("features.itWallet.issuance.eidResult.success.title")}
+        subtitle={I18n.t(
+          "features.itWallet.issuance.eidResult.success.subtitle"
+        )}
+        action={{
+          label: I18n.t(
+            "features.itWallet.issuance.eidResult.success.actions.continue"
+          ),
+          onPress: handleBackToWallet
+        }}
+      />
+    );
+  }
+
+  // The user successfully activated Documenti su IO
   return (
     <OperationResultScreenContent
       pictogram="success"
-      title={I18n.t("features.itWallet.issuance.eidResult.success.title")}
-      subtitle={I18n.t("features.itWallet.issuance.eidResult.success.subtitle")}
+      title={I18n.t("features.itWallet.issuance.eidResult.successL2.title")}
+      subtitle={I18n.t(
+        "features.itWallet.issuance.eidResult.successL2.subtitle"
+      )}
       action={{
         label: I18n.t(
-          "features.itWallet.issuance.eidResult.success.actions.continue"
+          "features.itWallet.issuance.eidResult.successL2.actions.continue"
         ),
         onPress: handleContinue
       }}
       secondaryAction={{
         label: I18n.t(
-          "features.itWallet.issuance.eidResult.success.actions.close"
+          "features.itWallet.issuance.eidResult.successL2.actions.close"
         ),
-        onPress: handleClose
+        onPress: () => {
+          handleBackToWallet();
+          trackBackToWallet({ exit_page: route.name, credential: "ITW_ID_V2" });
+        }
       }}
     />
   );
