pagopa/io-app

commit 98b20feea6504114f3d0c30f2a656ac29286bc00

chore(IT Wallet): [SIW-2220] IT-Wallet activation success screen (#7191)
## Short description
This PR modifies the Wallet activation screen to show that IT-Wallet was
activated. The fallback to "Documenti su IO" remains.

## List of changes proposed in this pull request
- Updated `ItwIssuanceEidResultScreen`

## How to test
Activate IT-Wallet and Documenti su IO and ensure the success screens
are coherent.

|Documenti su IO|IT-Wallet|
|-|-|
|<img width="260" alt="doc_io"
src="https://github.com/user-attachments/assets/8f68eda6-90b8-45e9-9f2a-ecc96ffad254"
/>|<img width="260" alt="itwallet"
src="https://github.com/user-attachments/assets/de016f49-b76b-46b6-a715-ebcbe33fbc53"
/>|

ts/features/itwallet/common/components/ItwWalletReadyBanner.tsx
@@ -36,7 +36,7 @@ export const ItwWalletReadyBanner = () => {
           : "features.itWallet.issuance.eidResult.success.subtitle"
       )}
       action={I18n.t(
-        "features.itWallet.issuance.eidResult.success.actions.continueAlt"
+        "features.itWallet.issuance.eidResult.successL2.actions.continueAlt"
       )}
       color="turquoise"
       onPress={handleOnPress}

ts/features/itwallet/issuance/screens/ItwIssuanceEidResultScreen.tsx
@@ -5,10 +5,13 @@ import { ItwEidIssuanceMachineContext } from "../../machine/eid/provider";
 import { useItwDisableGestureNavigation } from "../../common/hooks/useItwDisableGestureNavigation";
 import { useAvoidHardwareBackButton } from "../../../../utils/useAvoidHardwareBackButton";
 import { trackAddFirstCredential, trackBackToWallet } from "../../analytics";
+import { useIOSelector } from "../../../../store/hooks";
+import { itwLifecycleIsITWalletValidSelector } from "../../lifecycle/store/selectors";
 
 export const ItwIssuanceEidResultScreen = () => {
   const route = useRoute();
 
+  const isItWalletValid = useIOSelector(itwLifecycleIsITWalletValidSelector);
   const machineRef = ItwEidIssuanceMachineContext.useActorRef();
 
   useItwDisableGestureNavigation();
@@ -19,27 +22,49 @@ export const ItwIssuanceEidResultScreen = () => {
     trackAddFirstCredential();
   };
 
-  const handleClose = async () => {
-    machineRef.send({ type: "go-to-wallet" });
-    trackBackToWallet({ exit_page: route.name, credential: "ITW_ID_V2" });
-  };
+  const handleBackToWallet = () => machineRef.send({ type: "go-to-wallet" });
 
+  // The user successfully activated IT-Wallet
+  if (isItWalletValid) {
+    return (
+      <OperationResultScreenContent
+        pictogram="success"
+        title={I18n.t("features.itWallet.issuance.eidResult.success.title")}
+        subtitle={I18n.t(
+          "features.itWallet.issuance.eidResult.success.subtitle"
+        )}
+        action={{
+          label: I18n.t(
+            "features.itWallet.issuance.eidResult.success.actions.continue"
+          ),
+          onPress: handleBackToWallet
+        }}
+      />
+    );
+  }
+
+  // The user successfully activated Documenti su IO
   return (
     <OperationResultScreenContent
       pictogram="success"
-      title={I18n.t("features.itWallet.issuance.eidResult.success.title")}
-      subtitle={I18n.t("features.itWallet.issuance.eidResult.success.subtitle")}
+      title={I18n.t("features.itWallet.issuance.eidResult.successL2.title")}
+      subtitle={I18n.t(
+        "features.itWallet.issuance.eidResult.successL2.subtitle"
+      )}
       action={{
         label: I18n.t(
-          "features.itWallet.issuance.eidResult.success.actions.continue"
+          "features.itWallet.issuance.eidResult.successL2.actions.continue"
         ),
         onPress: handleContinue
       }}
       secondaryAction={{
         label: I18n.t(
-          "features.itWallet.issuance.eidResult.success.actions.close"
+          "features.itWallet.issuance.eidResult.successL2.actions.close"
         ),
-        onPress: handleClose
+        onPress: () => {
+          handleBackToWallet();
+          trackBackToWallet({ exit_page: route.name, credential: "ITW_ID_V2" });
+        }
       }}
     />
   );

ts/features/itwallet/machine/eid/actions.ts
@@ -104,7 +104,9 @@ export const createEidIssuanceActionsImplementation = (
   },
 
   navigateToWallet: () => {
-    toast.success(I18n.t("features.itWallet.issuance.eidResult.success.toast"));
+    toast.success(
+      I18n.t("features.itWallet.issuance.eidResult.successL2.toast")
+    );
     navigation.reset({
       index: 1,
       routes: [
@@ -230,7 +232,9 @@ export const createEidIssuanceActionsImplementation = (
   resetWalletInstance: () => {
     store.dispatch(itwLifecycleWalletReset());
     store.dispatch(itwSetAuthLevel(undefined));
-    toast.success(I18n.t("features.itWallet.issuance.eidResult.success.toast"));
+    toast.success(
+      I18n.t("features.itWallet.issuance.eidResult.successL2.toast")
+    );
   },
 
   trackWalletInstanceCreation: () => {

commit a5dc4a8c4aee9d2ebb47dacf6673ea401f87a92b

fix(IT Wallet): [SIW-2767] Copy regression in "Documenti su IO" ready banner (#7204)
## Short description
This PR fixes a regression introduced by #7191, that displays copy
related to IT-Wallet in Documenti su IO ready banner.

## List of changes proposed in this pull request
- Fix i18n keys

## How to test
Activate Documenti su IO (disable L3 flag): check that the ready banner
mentions Documenti su IO

|Before|After|
|------|-----|
|<img width="320" alt="before"
src="https://github.com/user-attachments/assets/f1fd7df4-21fa-4c83-b14f-edad3cad3268"
/>|<img width="320" alt="after"
src="https://github.com/user-attachments/assets/e9cd35b5-f967-447a-b1f6-8d030eb290e3"
/>|

ts/features/itwallet/common/components/ItwWalletReadyBanner.tsx
@@ -24,7 +24,7 @@ export const ItwWalletReadyBanner = () => {
   };
 
   const bannerTitle = !isNewItwRenderable
-    ? I18n.t("features.itWallet.issuance.eidResult.success.title")
+    ? I18n.t("features.itWallet.issuance.eidResult.successL2.title")
     : undefined;
 
   return (
@@ -33,7 +33,7 @@ export const ItwWalletReadyBanner = () => {
       content={I18n.t(
         isNewItwRenderable
           ? "features.itWallet.issuance.emptyWallet.itwReadyBanner.content"
-          : "features.itWallet.issuance.eidResult.success.subtitle"
+          : "features.itWallet.issuance.eidResult.successL2.subtitle"
       )}
       action={I18n.t(
         "features.itWallet.issuance.eidResult.successL2.actions.continueAlt"