6093ac7b57131aa4dac16a561ba4fbad69a27ee7
Federico Mastrini <federicomastrini93@gmail.com>
2025-07-29 18:19:18 +0200

    chore: remove blocking

--- ts/features/itwallet/wallet/components/ItwCredentialWalletCard.tsx
+++ ts/features/itwallet/wallet/components/ItwCredentialWalletCard.tsx
@@ -3,8 +3,6 @@ import { ItwCredentialCard } from "../../common/components/ItwCredentialCard";
 import { WalletCardPressableBase } from "../../../wallet/components/WalletCardPressableBase";
 import { useIONavigation } from "../../../../navigation/params/AppParamsList";
 import { ITW_ROUTES } from "../../navigation/routes";
-import { useIOSelector } from "../../../../store/hooks";
-import { itwShouldRenderNewItWalletSelector } from "../../common/store/selectors";
 
 export type ItwCredentialWalletCardProps = ItwCredentialCard & {
   isPreview?: false; // Cards in wallet cannot be in preview mode
@@ -12,15 +10,8 @@ export type ItwCredentialWalletCardProps = ItwCredentialCard & {
 
 const WrappedItwCredentialCard = (props: ItwCredentialWalletCardProps) => {
   const navigation = useIONavigation();
-  const isNewItwRenderable = useIOSelector(itwShouldRenderNewItWalletSelector);
-  const needsItwUpgrade = isNewItwRenderable && props.level !== "L3";
 
   const handleOnPress = () => {
-    if (needsItwUpgrade) {
-      // TODO add credential upgrade flow (SIW-2814)
-      return;
-    }
-
     navigation.navigate(ITW_ROUTES.MAIN, {
       screen: ITW_ROUTES.PRESENTATION.CREDENTIAL_DETAIL,
       params: {
