602bd436b5706cd730dd81db82ffaff9df3729c4
Federico Mastrini <federicomastrini93@gmail.com>
2025-07-31 15:11:49 +0200

    refactor: replace `curriedAppReducer` with `reproduceSequence`

--- ts/features/itwallet/common/store/reducers/__tests__/securePreferences.test.ts
+++ ts/features/itwallet/common/store/reducers/__tests__/securePreferences.test.ts
@@ -1,6 +1,4 @@
-import { pipe } from "fp-ts/lib/function";
 import { applicationChangeState } from "../../../../../../store/actions/application";
-import { Action } from "../../../../../../store/actions/types";
 import { appReducer } from "../../../../../../store/reducers";
 import { GlobalState } from "../../../../../../store/reducers/types";
 import {
@@ -11,10 +9,7 @@ import {
   itwOfflineAccessCounterReset,
   itwOfflineAccessCounterUp
 } from "../../actions/securePreferences";
-
-const curriedAppReducer =
-  (action: Action) => (state: GlobalState | undefined) =>
-    appReducer(state, action);
+import { reproduceSequence } from "../../../../../../utils/tests";
 
 describe("IT Wallet secure preferences reducer", () => {
   it("should return the initial state", () => {
@@ -22,49 +17,43 @@ describe("IT Wallet secure preferences reducer", () => {
       offlineAccessCounter: 0
     };
 
-    const targetSate = pipe(
-      undefined,
-      curriedAppReducer(applicationChangeState("active"))
-    );
+    const targetSate = appReducer(undefined, applicationChangeState("active"));
 
     expect(targetSate.features.itWallet.securePreferences).toEqual(
       INITIAL_STATE
     );
   });
   it("should increment the offline access counter", () => {
-    const targetSate = pipe(
-      undefined,
-      curriedAppReducer(applicationChangeState("active")),
-      curriedAppReducer(itwOfflineAccessCounterUp())
-    );
+    const targetSate = reproduceSequence({} as GlobalState, appReducer, [
+      applicationChangeState("active"),
+      itwOfflineAccessCounterUp()
+    ]);
 
     expect(
       targetSate.features.itWallet.securePreferences.offlineAccessCounter
     ).toEqual(1);
   });
   it("should not increment the offline access counter beyond the maximum limit", () => {
-    const targetSate = pipe(
-      undefined,
-      curriedAppReducer(applicationChangeState("active")),
-      curriedAppReducer(itwOfflineAccessCounterUp()),
-      curriedAppReducer(itwOfflineAccessCounterUp()),
-      curriedAppReducer(itwOfflineAccessCounterUp()),
-      curriedAppReducer(itwOfflineAccessCounterUp()),
-      curriedAppReducer(itwOfflineAccessCounterUp()),
-      curriedAppReducer(itwOfflineAccessCounterUp())
-    );
+    const targetSate = reproduceSequence({} as GlobalState, appReducer, [
+      applicationChangeState("active"),
+      itwOfflineAccessCounterUp(),
+      itwOfflineAccessCounterUp(),
+      itwOfflineAccessCounterUp(),
+      itwOfflineAccessCounterUp(),
+      itwOfflineAccessCounterUp(),
+      itwOfflineAccessCounterUp()
+    ]);
     expect(
       targetSate.features.itWallet.securePreferences.offlineAccessCounter
     ).toEqual(ITW_MAX_OFFLINE_ACCESS_COUNTER);
   });
   it("should reset the offline access counter", () => {
-    const targetSate = pipe(
-      undefined,
-      curriedAppReducer(applicationChangeState("active")),
-      curriedAppReducer(itwOfflineAccessCounterUp()),
-      curriedAppReducer(itwOfflineAccessCounterUp()),
-      curriedAppReducer(itwOfflineAccessCounterReset())
-    );
+    const targetSate = reproduceSequence({} as GlobalState, appReducer, [
+      applicationChangeState("active"),
+      itwOfflineAccessCounterUp(),
+      itwOfflineAccessCounterUp(),
+      itwOfflineAccessCounterReset()
+    ]);
 
     expect(
       targetSate.features.itWallet.securePreferences.offlineAccessCounter
