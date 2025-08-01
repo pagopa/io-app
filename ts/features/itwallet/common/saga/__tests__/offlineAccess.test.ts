9c24e8e0f510ddb8cb0d1342f951ee739a89c713
Federico Mastrini <federicomastrini93@gmail.com>
2025-08-01 12:41:15 +0200

    chore: fix counter logic

--- ts/features/itwallet/common/saga/__tests__/offlineAccess.test.ts
+++ ts/features/itwallet/common/saga/__tests__/offlineAccess.test.ts
@@ -6,82 +6,108 @@ import {
   itwOfflineAccessCounterReset,
   itwOfflineAccessCounterUp
 } from "../../store/actions/securePreferences";
-import {
-  handleItwOfflineAccessCounterReset,
-  handleItwOfflineAccessCounterUpOnIdentificationSuccess,
-  handleItwOfflineAccessCounterUpOnReasonSet
-} from "../offlineAccess";
-import { setOfflineAccessReason } from "../../../../ingress/store/actions";
+import { watchItwOfflineAccess } from "../offlineAccess";
+import { progressSelector as identificationStatusSelector } from "../../../../identification/store/selectors";
+import { itwUpdateWalletInstanceStatus } from "../../../walletInstance/store/actions";
+import { identificationSuccess } from "../../../../identification/store/actions";
 
-describe("handleItwOfflineAccessCounterReset", () => {
+describe("watchItwOfflineAccess", () => {
   it("should handle offline access counter reset on wallet instance status update", async () => {
-    await expectSaga(handleItwOfflineAccessCounterReset)
+    await expectSaga(watchItwOfflineAccess)
+      .provide([
+        [
+          select(identificationStatusSelector),
+          {
+            kind: "identified"
+          }
+        ],
+        [select(offlineAccessReasonSelector), undefined]
+      ])
+      .dispatch(itwUpdateWalletInstanceStatus.success({} as any))
       .put(itwOfflineAccessCounterReset())
-      .run();
-  });
-});
-
-describe("handleItwOfflineAccessCounterUpOnIdentificationSuccess", () => {
-  it("should not increase counter on identification success without an offline access reason", async () => {
-    await expectSaga(handleItwOfflineAccessCounterUpOnIdentificationSuccess)
-      .provide([[select(offlineAccessReasonSelector), undefined]])
-      .not.put(itwOfflineAccessCounterReset())
-      .not.put(itwOfflineAccessCounterUp())
-      .run();
-  });
-
-  it("should not increase counter on identification success with an wrong offline access reason", async () => {
-    await expectSaga(handleItwOfflineAccessCounterUpOnIdentificationSuccess)
-      .provide([
-        [
-          select(offlineAccessReasonSelector),
-          OfflineAccessReasonEnum.SESSION_REFRESH
-        ]
-      ])
-      .not.put(itwOfflineAccessCounterReset())
-      .not.put(itwOfflineAccessCounterUp())
-      .run();
-  });
-
-  it("should increase counter on identification success with offline acces reason", async () => {
-    await expectSaga(handleItwOfflineAccessCounterUpOnIdentificationSuccess)
-      .provide([
-        [
-          select(offlineAccessReasonSelector),
-          OfflineAccessReasonEnum.DEVICE_OFFLINE
-        ]
-      ])
-      .not.put(itwOfflineAccessCounterReset())
-      .put(itwOfflineAccessCounterUp())
-      .run();
-  });
-});
-
-describe("handleItwOfflineAccessCounterUpOnReasonSet", () => {
-  it("should not increase counter on DEVICE_OFFLINE access reason dispatch", async () => {
-    await expectSaga(
-      handleItwOfflineAccessCounterUpOnReasonSet,
-      setOfflineAccessReason(OfflineAccessReasonEnum.DEVICE_OFFLINE)
-    )
-      .not.put(itwOfflineAccessCounterReset())
-      .not.put(itwOfflineAccessCounterUp())
       .run();
   });
 
   test.each([
-    OfflineAccessReasonEnum.SESSION_REFRESH,
-    OfflineAccessReasonEnum.SESSION_EXPIRED
-  ])("should increase counter on %s access reason dispatch", async reason => {
-    await expectSaga(
-      handleItwOfflineAccessCounterUpOnReasonSet,
-      setOfflineAccessReason(reason)
-    )
+    ["unidentified", undefined],
+    ["unidentified", OfflineAccessReasonEnum.DEVICE_OFFLINE],
+    ["unidentified", OfflineAccessReasonEnum.SESSION_EXPIRED],
+    ["unidentified", OfflineAccessReasonEnum.SESSION_REFRESH],
+    ["identified", undefined]
+  ])(
+    "should not increase counter if user is %s and access reason is %s",
+    async (identificationKind, offlineAccessReason) => {
+      await expectSaga(watchItwOfflineAccess)
+        .provide([
+          [
+            select(identificationStatusSelector),
+            {
+              kind: identificationKind
+            }
+          ],
+          [select(offlineAccessReasonSelector), offlineAccessReason]
+        ])
+        .not.put(itwOfflineAccessCounterReset())
+        .not.put(itwOfflineAccessCounterUp())
+        .run();
+    }
+  );
+
+  test.each([
+    ["identified", OfflineAccessReasonEnum.DEVICE_OFFLINE],
+    ["identified", OfflineAccessReasonEnum.SESSION_EXPIRED],
+    ["identified", OfflineAccessReasonEnum.SESSION_REFRESH]
+  ])(
+    "should increase counter if user is %s and access reason is %s",
+    async (identificationKind, offlineAccessReason) => {
+      await expectSaga(watchItwOfflineAccess)
+        .provide([
+          [
+            select(identificationStatusSelector),
+            {
+              kind: identificationKind
+            }
+          ],
+          [select(offlineAccessReasonSelector), offlineAccessReason]
+        ])
+        .not.put(itwOfflineAccessCounterReset())
+        .put(itwOfflineAccessCounterUp())
+        .run();
+    }
+  );
+
+  it("should not increase counter on identification success without an offline access reason set", async () => {
+    await expectSaga(watchItwOfflineAccess)
       .provide([
+        [
+          select(identificationStatusSelector),
+          {
+            kind: "unidentified"
+          }
+        ],
+        [select(offlineAccessReasonSelector), undefined]
+      ])
+      .dispatch(identificationSuccess({} as any))
+      .not.put(itwOfflineAccessCounterReset())
+      .not.put(itwOfflineAccessCounterUp())
+      .run();
+  });
+
+  it("should increase counter on identification success with an offline access reason set", async () => {
+    await expectSaga(watchItwOfflineAccess)
+      .provide([
+        [
+          select(identificationStatusSelector),
+          {
+            kind: "unidentified"
+          }
+        ],
         [
           select(offlineAccessReasonSelector),
           OfflineAccessReasonEnum.DEVICE_OFFLINE
         ]
       ])
+      .dispatch(identificationSuccess({} as any))
       .not.put(itwOfflineAccessCounterReset())
       .put(itwOfflineAccessCounterUp())
       .run();
--- ts/features/itwallet/common/saga/offlineAccess.ts
+++ ts/features/itwallet/common/saga/offlineAccess.ts
@@ -1,14 +1,13 @@
 import { SagaIterator } from "redux-saga";
-import { put, select, takeLatest } from "typed-redux-saga/macro";
+import { fork, put, select, takeLatest } from "typed-redux-saga/macro";
 import { identificationSuccess } from "../../../identification/store/actions";
+import { progressSelector as identificationStatusSelector } from "../../../identification/store/selectors";
 import { offlineAccessReasonSelector } from "../../../ingress/store/selectors";
 import { itwUpdateWalletInstanceStatus } from "../../walletInstance/store/actions";
 import {
   itwOfflineAccessCounterReset,
   itwOfflineAccessCounterUp
 } from "../store/actions/securePreferences";
-import { setOfflineAccessReason } from "../../../ingress/store/actions";
-import { OfflineAccessReasonEnum } from "../../../ingress/store/reducer";
 
 /**
  * Handles the offline access counter reset by listening for the wallet
@@ -18,36 +17,37 @@ import { OfflineAccessReasonEnum } from "../../../ingress/store/reducer";
  * successfully, indicating that the user has returned online and the wallet instance
  * stattus is refreshed.
  */
-export function* handleItwOfflineAccessCounterReset(): SagaIterator {
+function* handleItwOfflineAccessCounterReset(): SagaIterator {
   yield* put(itwOfflineAccessCounterReset());
+}
+
+/**
+ * Handles the increment of the offline access counter if an offline access reason
+ * is set
+ */
+function* incrementOfflineAccessCounter() {
+  const offlineAccessReason = yield* select(offlineAccessReasonSelector);
+  if (offlineAccessReason !== undefined) {
+    yield* put(itwOfflineAccessCounterUp());
+  }
 }
 
 /**
  * Handles the increment of the offline access counter when the app start in
  * offline mode and user completes the identification successfully.
  */
-export function* handleItwOfflineAccessCounterUpOnIdentificationSuccess(): SagaIterator {
-  const offlineAccessReason = yield* select(offlineAccessReasonSelector);
-  if (offlineAccessReason === OfflineAccessReasonEnum.DEVICE_OFFLINE) {
-    yield* put(itwOfflineAccessCounterUp());
-  }
-}
+function* handleItwOfflineAccessCounterUpOnIdentificationSuccess(): SagaIterator {
+  // Get the current identification status to determine if the user has already completed
+  // the identification.
+  const identificationStatus = yield* select(identificationStatusSelector);
 
-/**
- * Handles the increment of the offline access counter when:
- * - the user completes the identification successfully, but the session refresh fails.
- * - the user opens the app without a valid session and the app is in offline mode.
- *
- * This is required because in these cases the identification is made (or not made at all)
- * before the access  reason is set.
- */
-export function* handleItwOfflineAccessCounterUpOnReasonSet(
-  action: ReturnType<typeof setOfflineAccessReason>
-): SagaIterator {
-  if (action.payload !== OfflineAccessReasonEnum.DEVICE_OFFLINE) {
-    // SESSION_REFRESH reason is dispatched after the user identification
-    // SESSION_EXPIRED reason is dispatched without an user identification
-    yield* put(itwOfflineAccessCounterUp());
+  // If the user already completed the identification successfully
+  // call the increment function directly.
+  if (identificationStatus.kind === "identified") {
+    yield* incrementOfflineAccessCounter();
+  } else {
+    // Otherwise, wait for the identification success action to be dispatched.
+    yield* takeLatest(identificationSuccess, incrementOfflineAccessCounter);
   }
 }
 
@@ -55,16 +55,9 @@ export function* handleItwOfflineAccessCounterUpOnReasonSet(
  * Watch actions that trigger the offline access counter reset or increment.
  */
 export function* watchItwOfflineAccess(): SagaIterator {
+  yield* fork(handleItwOfflineAccessCounterUpOnIdentificationSuccess);
   yield* takeLatest(
     itwUpdateWalletInstanceStatus.success,
     handleItwOfflineAccessCounterReset
-  );
-  yield* takeLatest(
-    identificationSuccess,
-    handleItwOfflineAccessCounterUpOnIdentificationSuccess
-  );
-  yield* takeLatest(
-    setOfflineAccessReason,
-    handleItwOfflineAccessCounterUpOnReasonSet
   );
 }
