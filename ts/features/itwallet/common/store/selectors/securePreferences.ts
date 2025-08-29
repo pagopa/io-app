d9133b5249440260d4d6409f42c72cb13c2b6733
Cristian Matteu <94987118+ChrisMattew@users.noreply.github.com>
2025-08-29 10:44:38 +0200

    chore: [SIW-2824] Track offline counter screen views (#7284)
    ## Short description
    This PR integrates event tracking into the warning and blocking screens
    of the offline mini app
    
    ## List of changes proposed in this pull request
    - Implemented the dispatch of the `ITW_OFFLINE_ACCESS_EXPIRING` event on
    the screen that informs the user this is the last available offline
    access
    - Implemented the dispatch of the `ITW_OFFLINE_ACCESS_EXPIRED` event on
    the screen prompting the user to reconnect and reload the app
    
    ## How to test
    After removing the connection from the device, close and reopen the app
    until the relevant screens are displayed, and ensure that the events
    associated with them are correctly sent to MixPanel once the connection
    is restored
    
    | ITW_OFFLINE_ACCESS_EXPIRING | ITW_OFFLINE_ACCESS_EXPIRED |
    |--------|--------|
    |<img width="300" alt="20250826_163558"
    src="https://github.com/user-attachments/assets/e32b4bf7-241d-464b-957a-440ff25e3d05"
    /> | <img width="300" alt="20250826_163639"
    src="https://github.com/user-attachments/assets/dd070b69-021d-448e-be75-7909cf00215d"
    /> |

--- ts/features/itwallet/common/store/selectors/securePreferences.ts
+++ ts/features/itwallet/common/store/selectors/securePreferences.ts
@@ -1,6 +1,7 @@
 import { createSelector } from "reselect";
 import { GlobalState } from "../../../../../store/reducers/types";
 import { ItwSecurePreferencesState } from "../reducers/securePreferences";
+import { progressSelector } from "../../../../identification/store/selectors";
 
 // 5 means that the fifth time the user accesses the app offline, they will be required to return online.
 export const ITW_MAX_OFFLINE_ACCESS_COUNTER = 5;
@@ -29,9 +30,18 @@ export const itwIsOfflineAccessLimitReached = createSelector(
 /**
  * Returns `true` if the user should be warned about the offline access limit
  * before reaching it.
+ *
+ * TODO: [SIW-2892] Revise this workaround
+ * The check on the `identificationProgress` status was added to mitigate the issue
+ * caused by the delay between the update of `startupStatus` (whose value, when offline, renders the `OfflineStackNavigator`)
+ * and that of the underlying counter (which also responds to the `startupLoadSuccess` action).
+ * This delay gives rise to an edge case during the transition from 1 remaining attempt to no attempts left,
+ * causing — in the render just before the counter update — the incorrect dispatch of the `ITW_OFFLINE_ACCESS_EXPIRING` event
  */
 export const itwShouldDisplayOfflineAccessLimitWarning = createSelector(
   itwOfflineAccessCounterSelector,
-  offlineAccessCounter =>
-    offlineAccessCounter === ITW_MAX_OFFLINE_ACCESS_COUNTER - 1
+  progressSelector,
+  (offlineAccessCounter, identificationProgress) =>
+    offlineAccessCounter === ITW_MAX_OFFLINE_ACCESS_COUNTER - 1 &&
+    identificationProgress.kind === "identified"
 );
