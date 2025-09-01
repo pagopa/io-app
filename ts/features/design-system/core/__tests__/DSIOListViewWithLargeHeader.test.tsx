de230e6552651b5cd954156002d326f0f76e89b8
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-09-01 10:27:20 +0200

    test: update assertion, removed snapshot

--- ts/features/design-system/core/__tests__/DSIOListViewWithLargeHeader.test.tsx
+++ ts/features/design-system/core/__tests__/DSIOListViewWithLargeHeader.test.tsx
@@ -6,65 +6,6 @@ import { renderScreenWithNavigationStoreContext } from "../../../../utils/testWr
 import DESIGN_SYSTEM_ROUTES from "../../navigation/routes";
 import { DSIOListViewWithLargeHeader } from "../DSIOListViewWithLargeHeader";
 
-jest.mock("react-native", () => ({
-  AccessibilityInfo: {
-    setAccessibilityFocus: jest.fn()
-  }
-}));
-
-jest.mock("@react-navigation/native-stack", () => ({
-  createNativeStackNavigator: () => ({
-    Navigator: ({ children }: any) => <>{children}</>,
-    Screen: () => null
-  })
-}));
-
-jest.mock("@react-navigation/stack", () => ({
-  createStackNavigator: () => ({
-    Navigator: ({ children }: any) => <>{children}</>,
-    Screen: () => null
-  })
-}));
-
-jest.mock("@react-navigation/bottom-tabs", () => ({
-  createBottomTabNavigator: () => ({
-    Navigator: ({ children }: any) => <>{children}</>,
-    Screen: () => null
-  })
-}));
-
-jest.mock("@react-navigation/native", () => ({
-  useNavigation: () => ({
-    setOptions: jest.fn
-  }),
-  useRoute: jest.fn,
-  useFocusEffect: jest.fn,
-  useIsFocused: jest.fn,
-  NavigationContainer: ({ children }: any) => <>{children}</>,
-  DarkTheme: {
-    dark: true,
-    colors: {
-      primary: "#000",
-      background: "#000",
-      card: "#111",
-      text: "#fff",
-      border: "#222",
-      notification: "#333"
-    }
-  },
-  DefaultTheme: {
-    dark: false,
-    colors: {
-      primary: "#fff",
-      background: "#fff",
-      card: "#eee",
-      text: "#000",
-      border: "#ccc",
-      notification: "#ddd"
-    }
-  }
-}));
-
 const renderComponent = () => {
   const globalState = appReducer(undefined, applicationChangeState("active"));
   return renderScreenWithNavigationStoreContext<GlobalState>(
@@ -77,7 +18,7 @@ const renderComponent = () => {
 
 describe("DSIOListViewWithLargeHeader", () => {
   it("should render list items correctly", () => {
-    const { toJSON } = renderComponent();
-    expect(toJSON()).toMatchSnapshot();
+    const { getByTestId } = renderComponent();
+    expect(getByTestId("io-list-view-large-header")).toBeTruthy();
   });
 });
