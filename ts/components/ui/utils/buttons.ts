83735ce6a58ebb3f094dab166fcb7f7216199ac1
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-09-03 09:42:07 +0200

    refactor: move button config into utils

---
+++ ts/components/ui/utils/buttons.ts
@@ -0,0 +1,9 @@
+import { IOToast } from "@pagopa/io-app-design-system";
+import I18n from "../../../i18n";
+import { openWebUrl } from "../../../utils/url";
+
+export const getWhatCanYouDoButtonConfig = (url: string) => ({
+  icon: "instruction" as const,
+  label: I18n.t("global.buttons.whatCanYouDo"),
+  onPress: () => openWebUrl(url, () => IOToast.error(I18n.t("genericError")))
+});
