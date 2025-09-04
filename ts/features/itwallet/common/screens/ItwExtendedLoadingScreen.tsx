0f3811f2d046a43a4e4945cab11033f18c8bfbdb
MonicaRungi-EY <monica.rungi@itauros.com>
2025-09-04 16:22:38 +0200

    prettier

--- ts/features/itwallet/common/screens/ItwExtendedLoadingScreen.tsx
+++ ts/features/itwallet/common/screens/ItwExtendedLoadingScreen.tsx
@@ -2,8 +2,8 @@ import LoadingScreenContent from "../../../../components/screens/LoadingScreenCo
 import I18n from "../../../../i18n";
 
 export const ItwExtendedLoadingScreen = () => (
-    <LoadingScreenContent
-      testID="loader"
-      contentTitle={I18n.t(`features.itWallet.loadingScreen.title`)}
-    />
-  );
+  <LoadingScreenContent
+    testID="loader"
+    contentTitle={I18n.t(`features.itWallet.loadingScreen.title`)}
+  />
+);
