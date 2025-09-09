7e99e5a07c1fac18ae3941df0c2419fd2fccfdaa
Federico Mastrini <federicomastrini93@gmail.com>
2025-09-09 18:47:17 +0200

    chore: remove useMemo

--- ts/features/itwallet/common/hooks/useItwStatusIconColor.ts
+++ ts/features/itwallet/common/hooks/useItwStatusIconColor.ts
@@ -1,11 +1,6 @@
 import { IOColors, useIOTheme } from "@pagopa/io-app-design-system";
-import { useMemo } from "react";
 
 export const useItwStatusIconColor = (expired: boolean): IOColors => {
   const theme = useIOTheme();
-  return useMemo(
-    () =>
-      expired ? theme["icon-decorative"] : theme["interactiveElem-default"],
-    [expired, theme]
-  );
+  return expired ? theme["icon-decorative"] : theme["interactiveElem-default"];
 };
