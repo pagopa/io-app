83f1154eb054113dd3399dc61d36334e0b53f03f
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2025-09-10 16:23:45 +0200

    feat(IT Wallet): Add documentation for useNotAvailableToastGuard hook

--- ts/features/itwallet/common/hooks/useNotAvailableToastGuard.ts
+++ ts/features/itwallet/common/hooks/useNotAvailableToastGuard.ts
@@ -4,6 +4,14 @@ import I18n from "i18next";
 import { isItwCredential } from "../utils/itwCredentialUtils.ts";
 import { StoredCredential } from "../utils/itwTypesUtils.ts";
 
+/**
+ * A hook that wraps a function to show a "feature not available" toast
+ * if the provided credential is an ITW credential.
+ * This is useful for features that are not yet available for ITW credentials such as
+ * support request.
+ * @param fn The function to be wrapped
+ * @param storedCredential The credential to check
+ */
 export const useNotAvailableToastGuard = (
   fn: (...args: Array<any>) => any,
   storedCredential: StoredCredential
