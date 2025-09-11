d45fe1361c5248330b42df19a479bf33f9e8c36c
Alice Di Rico <alice.dirico@pagopa.it>
2025-09-11 15:06:22 +0200

    chore: change name on header, change spid/cie error name and fix cieid login error (not correct path)

--- ts/features/authentication/activeSessionLogin/screens/ActiveSessionCieIdLoginScreen.tsx
+++ ts/features/authentication/activeSessionLogin/screens/ActiveSessionCieIdLoginScreen.tsx
@@ -113,7 +113,7 @@ const ActiveSessionCieIdLoginWebView = ({
 
   const handleLoginFailure = useCallback(
     (code?: string, message?: string) => {
-      if (code !== AUTH_ERRORS.NOT_SAME_CF) {
+      if (code !== AUTH_ERRORS.ERROR_1004) {
         dispatch(activeSessionLoginFailure());
       }
 
