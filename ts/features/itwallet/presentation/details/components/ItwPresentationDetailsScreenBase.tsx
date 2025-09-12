86ef067f102e16d57e2b7ef35f9957f2d8244dc5
Damiano Plebani <damiano.plebani@pagopa.it>
2025-09-12 16:05:32 +0200

    Remove footer gradient from `ItwPresentationDetailsScreenBase`

--- ts/features/itwallet/presentation/details/components/ItwPresentationDetailsScreenBase.tsx
+++ ts/features/itwallet/presentation/details/components/ItwPresentationDetailsScreenBase.tsx
@@ -1,25 +1,18 @@
 import {
   ButtonSolidProps,
-  GradientBottomActions,
-  IOSpacer,
-  IOSpacingScale,
-  IOVisualCostants,
-  buttonSolidHeight
+  FooterActions,
+  useFooterActionsMeasurements
 } from "@pagopa/io-app-design-system";
-import { Fragment, ReactNode, useMemo } from "react";
+import { Fragment, ReactNode } from "react";
 import Animated, {
-  Easing,
   useAnimatedRef,
   useAnimatedScrollHandler,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming
+  useSharedValue
 } from "react-native-reanimated";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useHeaderSecondLevel } from "../../../../../hooks/useHeaderSecondLevel.tsx";
+import { useItwFeaturesEnabled } from "../../../common/hooks/useItwFeaturesEnabled.ts";
 import { getHeaderPropsByCredentialType } from "../../../common/utils/itwStyleUtils.ts";
 import { StoredCredential } from "../../../common/utils/itwTypesUtils.ts";
-import { useItwFeaturesEnabled } from "../../../common/hooks/useItwFeaturesEnabled.ts";
 
 export type CredentialCtaProps = Omit<ButtonSolidProps, "fullWidth">;
 
@@ -31,39 +24,18 @@ export type ItwPresentationDetailsScreenBaseProps = {
 
 const scrollTriggerOffsetValue: number = 88;
 
-const gradientSafeArea: IOSpacingScale = 80;
-const contentEndMargin: IOSpacingScale = 32;
-const spaceBetweenActions: IOSpacer = 24;
-
 const ItwPresentationDetailsScreenBase = ({
   credential,
   children,
   ctaProps
 }: ItwPresentationDetailsScreenBaseProps) => {
   const animatedScrollViewRef = useAnimatedRef<Animated.ScrollView>();
-  const safeAreaInsets = useSafeAreaInsets();
   const itwFeaturesEnabled = useItwFeaturesEnabled(credential);
 
-  const gradientOpacity = useSharedValue(1);
   const scrollTranslationY = useSharedValue(0);
 
-  const bottomMargin: number = useMemo(
-    () =>
-      safeAreaInsets.bottom === 0
-        ? IOVisualCostants.appMarginDefault
-        : safeAreaInsets.bottom,
-    [safeAreaInsets]
-  );
-
-  const safeBottomAreaHeight: number = useMemo(
-    () => bottomMargin + buttonSolidHeight + contentEndMargin,
-    [bottomMargin]
-  );
-
-  const gradientAreaHeight: number = useMemo(
-    () => bottomMargin + buttonSolidHeight + gradientSafeArea,
-    [bottomMargin]
-  );
+  const { footerActionsMeasurements, handleFooterActionsMeasurements } =
+    useFooterActionsMeasurements();
 
   const headerProps = getHeaderPropsByCredentialType(
     credential.credentialType,
@@ -81,57 +53,35 @@ const ItwPresentationDetailsScreenBase = ({
     ...headerProps
   });
 
-  const scrollHandler = useAnimatedScrollHandler(
-    ({ contentOffset, layoutMeasurement, contentSize }) => {
-      // eslint-disable-next-line functional/immutable-data
-      scrollTranslationY.value = contentOffset.y;
-
-      const isEndReached =
-        Math.floor(layoutMeasurement.height + contentOffset.y) >=
-        Math.floor(contentSize.height);
-
-      // eslint-disable-next-line functional/immutable-data
-      gradientOpacity.value = isEndReached ? 0 : 1;
-    }
-  );
-
-  const footerGradientOpacityTransition = useAnimatedStyle(() => ({
-    opacity: withTiming(gradientOpacity.value, {
-      duration: 200,
-      easing: Easing.ease
-    })
-  }));
-
-  const footerComponent = ctaProps && (
-    <GradientBottomActions
-      primaryActionProps={ctaProps}
-      transitionAnimStyle={footerGradientOpacityTransition}
-      dimensions={{
-        bottomMargin,
-        extraBottomMargin: 0,
-        gradientAreaHeight,
-        spaceBetweenActions,
-        safeBackgroundHeight: bottomMargin
-      }}
-    />
-  );
+  const scrollHandler = useAnimatedScrollHandler(({ contentOffset }) => {
+    // eslint-disable-next-line functional/immutable-data
+    scrollTranslationY.value = contentOffset.y;
+  });
 
   return (
     <Fragment>
       <Animated.ScrollView
         ref={animatedScrollViewRef}
         contentContainerStyle={{
-          paddingBottom: safeBottomAreaHeight
+          paddingBottom: footerActionsMeasurements.safeBottomAreaHeight
         }}
         onScroll={scrollHandler}
-        scrollEventThrottle={16}
+        scrollEventThrottle={8}
         snapToOffsets={[0, scrollTriggerOffsetValue]}
         snapToEnd={false}
         decelerationRate="normal"
       >
         {children}
       </Animated.ScrollView>
-      {footerComponent}
+      {ctaProps && (
+        <FooterActions
+          onMeasure={handleFooterActionsMeasurements}
+          actions={{
+            type: "SingleButton",
+            primary: ctaProps
+          }}
+        />
+      )}
     </Fragment>
   );
 };
