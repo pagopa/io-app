3e8c9ffa3dfdd411e5a7cf2823675db1368dd4f4
Alessandro Mazzon <alessandro.mazzon@pagopa.it>
2025-09-15 15:26:10 +0200

    feat(IT Wallet): Add itw metadata

--- ts/features/itwallet/common/components/ItwIssuanceMetadata.tsx
+++ ts/features/itwallet/common/components/ItwIssuanceMetadata.tsx
@@ -1,7 +1,5 @@
 import { Divider, ListItemInfo } from "@pagopa/io-app-design-system";
 import { useMemo } from "react";
-import { pipe } from "fp-ts/lib/function";
-import * as O from "fp-ts/lib/Option";
 import I18n from "i18next";
 import { useItwInfoBottomSheet } from "../hooks/useItwInfoBottomSheet";
 import { StoredCredential } from "../utils/itwTypesUtils";
@@ -13,6 +11,8 @@ import {
 import { ITW_IPZS_PRIVACY_URL_BODY } from "../../../../urls";
 import { useIOSelector } from "../../../../store/hooks";
 import { generateDynamicUrlSelector } from "../../../../store/reducers/backendStatus/remoteConfig";
+import { getAuthSource, getItwAuthSource } from "../utils/itwMetadataUtils.ts";
+import { isItwCredential } from "../utils/itwCredentialUtils.ts";
 
 type ItwIssuanceMetadataProps = {
   credential: StoredCredential;
@@ -77,15 +77,6 @@ const ItwMetadataIssuanceListItem = ({
   );
 };
 
-const getAuthSource = (credential: StoredCredential) =>
-  pipe(
-    credential.issuerConf.openid_credential_issuer
-      .credential_configurations_supported?.[credential.credentialId],
-    O.fromNullable,
-    O.map(config => config.authentic_source),
-    O.toUndefined
-  );
-
 /**
  * Renders additional issuance-related metadata, i.e. releaser and auth source.
  * They are not part of the claims list, thus they're rendered separately.
@@ -99,10 +90,13 @@ export const ItwIssuanceMetadata = ({
 }: ItwIssuanceMetadataProps) => {
   const releaserName =
     credential.issuerConf.federation_entity.organization_name;
-  const authSource = getAuthSource(credential);
+  const itwCredential = isItwCredential(credential.credential);
   const privacyUrl = useIOSelector(state =>
     generateDynamicUrlSelector(state, "io_showcase", ITW_IPZS_PRIVACY_URL_BODY)
   );
+  const authSource = itwCredential
+    ? getItwAuthSource(credential)
+    : getAuthSource(credential);
 
   const releaserNameBottomSheet: ItwMetadataIssuanceListItemProps["bottomSheet"] =
     useMemo(
