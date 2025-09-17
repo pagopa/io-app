a7bac6eed24520e7e21c45c673c2cd23b6b2a709
forrest57 <tomba808@gmail.com>
2025-09-17 15:49:22 +0200

    missing i18n

--- ts/features/pn/aar/components/SendAARLoadingComponent.tsx
+++ ts/features/pn/aar/components/SendAARLoadingComponent.tsx
@@ -1,5 +1,9 @@
+import i18n from "i18next";
 import LoadingScreenContent from "../../../../components/screens/LoadingScreenContent";
 
 export const SendAARLoadingComponent = () => (
-  <LoadingScreenContent contentTitle="" testID="LoadingScreenContent" />
+  <LoadingScreenContent
+    contentTitle={i18n.t("features.pn.aar.flow.fetchingQrData.loadingText")}
+    testID="LoadingScreenContent"
+  />
 );
