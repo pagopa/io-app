c19974d2ae5c3ddd1615dec987f9b81693724a85
Federico Mastrini <federicomastrini93@gmail.com>
2025-09-25 10:29:24 +0200

    chore: change sizes

--- ts/features/itwallet/identification/cie/screens/ItwCiePreparationCanScreen.tsx
+++ ts/features/itwallet/identification/cie/screens/ItwCiePreparationCanScreen.tsx
@@ -1,7 +1,7 @@
-import { ContentWrapper } from "@pagopa/io-app-design-system";
+import { ContentWrapper, VSpacer } from "@pagopa/io-app-design-system";
 import { constNull } from "fp-ts/lib/function";
 import I18n from "i18next";
-import { Dimensions, Image, StyleSheet, View } from "react-native";
+import { Image } from "react-native";
 import { IOScrollViewWithLargeHeader } from "../../../../../components/ui/IOScrollViewWithLargeHeader";
 
 export const ItwCiePreparationCanScreen = () => (
@@ -22,29 +22,12 @@ export const ItwCiePreparationCanScreen = () => (
     }}
   >
     <ContentWrapper>
-      <View style={[styles.imageContainer, { height: screenHeight * 0.5 }]}>
-        <Image
-          accessibilityIgnoresInvertColors
-          source={require("../../../../../../img/features/itWallet/identification/cie_can.png")}
-          resizeMode="contain"
-          style={styles.image}
-        />
-      </View>
+      <VSpacer size={8} />
+      <Image
+        accessibilityIgnoresInvertColors
+        source={require("../../../../../../img/features/itWallet/identification/cie_can.png")}
+        resizeMode="contain"
+      />
     </ContentWrapper>
   </IOScrollViewWithLargeHeader>
 );
-
-// Get the screen height to calculate a responsive image container height
-const screenHeight = Dimensions.get("window").height;
-
-const styles = StyleSheet.create({
-  imageContainer: {
-    width: "100%",
-    marginTop: 15,
-    height: screenHeight * 0.5
-  },
-  image: {
-    width: "100%",
-    height: "100%"
-  }
-});
