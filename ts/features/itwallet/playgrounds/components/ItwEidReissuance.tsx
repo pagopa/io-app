f26d09cff614ea0c5145bfb1e1049b01397f5835
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2025-09-29 09:38:30 +0200

    chore: passing through ItwL2IdentificationModeSelectionScreen reissuing eID in ItwPlayground

--- ts/features/itwallet/playgrounds/components/ItwEidReissuance.tsx
+++ ts/features/itwallet/playgrounds/components/ItwEidReissuance.tsx
@@ -1,17 +1,18 @@
 import { Alert, View } from "react-native";
 import I18n from "i18next";
 import { IOButton, ListItemHeader } from "@pagopa/io-app-design-system";
-import { ItwEidIssuanceMachineContext } from "../../machine/eid/provider";
 import {
   itwLifecycleIsITWalletValidSelector,
   itwLifecycleIsValidSelector
 } from "../../lifecycle/store/selectors";
 import { useIOSelector } from "../../../../store/hooks";
+import { useIONavigation } from "../../../../navigation/params/AppParamsList";
+import { ITW_ROUTES } from "../../navigation/routes";
 
 export const ItwReissuanceSection = () => {
-  const machineRef = ItwEidIssuanceMachineContext.useActorRef();
   const isWalletValid = useIOSelector(itwLifecycleIsValidSelector);
   const isItwL3 = useIOSelector(itwLifecycleIsITWalletValidSelector);
+  const navigation = useIONavigation();
 
   if (!isWalletValid || isItwL3) {
     return null;
@@ -27,7 +28,12 @@ export const ItwReissuanceSection = () => {
           text: I18n.t("global.buttons.confirm"),
           style: "destructive",
           onPress: () => {
-            machineRef.send({ type: "start", mode: "reissuance" });
+            navigation.navigate(ITW_ROUTES.MAIN, {
+              screen: ITW_ROUTES.IDENTIFICATION.MODE_SELECTION.L2,
+              params: {
+                eidReissuing: true
+              }
+            });
           }
         }
       ],
