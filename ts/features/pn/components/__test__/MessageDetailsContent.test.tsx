7aa73c63b33e217e04dfc906182507892272a1bd
forrest57 <tomba808@gmail.com>
2025-10-15 12:02:51 +0200

    GH PR comments

--- ts/features/pn/components/__test__/MessageDetailsContent.test.tsx
+++ ts/features/pn/components/__test__/MessageDetailsContent.test.tsx
@@ -1,5 +1,4 @@
 import * as O from "fp-ts/Option";
-import { pipe } from "fp-ts/lib/function";
 import _ from "lodash";
 import { createStore } from "redux";
 import { applicationChangeState } from "../../../../store/actions/application";
@@ -23,7 +22,7 @@ jest.mock("../../aar/store/selectors", () => ({
   isAarMessageDelegatedSelector: jest.fn().mockReturnValue(true)
 }));
 
-const mockMessage = pipe(thirdPartyMessage, toPNMessage, O.toUndefined)!;
+const mockMessage = O.toUndefined(toPNMessage(thirdPartyMessage))!;
 const mockMessageWithoutDenomination = _.omit(
   mockMessage,
   "senderDenomination"
