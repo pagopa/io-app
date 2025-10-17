76231483a03a079887b0159aa19ca84917793a8a
pawelPrzywara <pawel.przywara.1991@gmail.com>
2025-10-17 08:21:34 +0200

    step 2

--- ts/features/toyProfile/screens/ProfileHomeScreen.tsx
+++ ts/features/toyProfile/screens/ProfileHomeScreen.tsx
@@ -1,10 +1,11 @@
 import {
   ContentWrapper,
   Divider,
-  ListItemInfo
+  ListItemInfo,
+  ListItemSwitch
 } from "@pagopa/io-app-design-system";
 import I18n from "i18next";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import { useEffect } from "react";
 import { emptyContextualHelp } from "../../../utils/emptyContextualHelp.tsx";
@@ -12,18 +13,44 @@ import { IOScrollViewWithLargeHeader } from "../../../components/ui/IOScrollView
 import { getToyProfileDetailsAction } from "../store/actions";
 import { toyProfileSelector } from "../store/selectors";
 import { useIOSelector } from "../../../store/hooks.ts";
+import { loadUserDataProcessing } from "../../settings/common/store/actions/userDataProcessing.ts";
+import { UserDataProcessingChoiceEnum } from "../../../../definitions/backend/UserDataProcessingChoice.ts";
+import { userDataProcessingSelector } from "../../settings/common/store/selectors/userDataProcessing.ts";
+import LoadingSpinnerOverlay from "../../../components/LoadingSpinnerOverlay.tsx";
+import { UserDataProcessingStatusEnum } from "../../../../definitions/backend/UserDataProcessingStatus.ts";
 
 const ProfileHomeScreen = () => {
   const dispatch = useDispatch();
-  const toyProfilePot = useSelector(toyProfileSelector);
+
+  const toyProfilePot = useIOSelector(toyProfileSelector);
+  const deletePot = useIOSelector(
+    s => userDataProcessingSelector(s)[UserDataProcessingChoiceEnum.DELETE]
+  );
 
   useEffect(() => {
     dispatch(getToyProfileDetailsAction.request());
+    dispatch(
+      loadUserDataProcessing.request(UserDataProcessingChoiceEnum.DELETE)
+    );
   }, [dispatch]);
 
-  const isLoading = pot.isLoading(toyProfilePot);
-  const isError = pot.isError(toyProfilePot);
-  const isSome = pot.isSome(toyProfilePot);
+  const profile = pot.isSome(toyProfilePot) ? toyProfilePot.value : undefined;
+
+  const delIsLoading = pot.isLoading(deletePot);
+  const delIsSome = pot.isSome(deletePot);
+  const delStatus = delIsSome ? deletePot.value?.status : undefined;
+  const delDisabled = delIsLoading || pot.isNone(deletePot);
+  const delValue =
+    !!delStatus &&
+    [
+      UserDataProcessingStatusEnum.PENDING,
+      UserDataProcessingStatusEnum.WIP,
+      UserDataProcessingStatusEnum.CLOSED
+    ].includes(delStatus);
+
+  const onDeleteSwitchChange = (checked: boolean) => {
+    // TODO
+  };
 
   return (
     <IOScrollViewWithLargeHeader
@@ -34,59 +61,75 @@ const ProfileHomeScreen = () => {
       faqCategories={["profile"]}
       headerActionsProp={{ showHelp: true }}
     >
-      <ContentWrapper>
-        {isError && (
-          <ListItemInfo value={toyProfilePot.error.message} />
-        )}
-        {isSome && (
-          <>
-            {toyProfilePot.value.name && toyProfilePot.value.family_name && (
-              <>
-                <ListItemInfo
-                  label={I18n.t("profile.data.list.nameSurname")}
-                  icon={"profile"}
-                  value={
-                    toyProfilePot.value.name +
-                    " " +
-                    toyProfilePot.value.family_name
-                  }
+      <LoadingSpinnerOverlay
+        isLoading={pot.isNone(toyProfilePot) || pot.isLoading(toyProfilePot)}
+        loadingOpacity={0.9}
+        loadingCaption={I18n.t("profile.main.privacy.loading")}
+      >
+        <ContentWrapper>
+          {pot.isError(toyProfilePot) && (
+            <ListItemInfo value={toyProfilePot.error.message} />
+          )}
+
+          {profile && (
+            <>
+              {profile.name && profile.family_name && (
+                <>
+                  <ListItemInfo
+                    label={I18n.t("profile.data.list.nameSurname")}
+                    icon="profile"
+                    value={`${profile.name} ${profile.family_name}`}
+                  />
+                  <Divider />
+                </>
+              )}
+
+              {profile.fiscal_code && (
+                <>
+                  <ListItemInfo
+                    label={I18n.t("profile.data.list.fiscalCode")}
+                    icon="fiscalCodeIndividual"
+                    value={profile.fiscal_code}
+                  />
+                  <Divider />
+                </>
+              )}
+
+              {profile.email && (
+                <>
+                  <ListItemInfo
+                    label={I18n.t("profile.data.list.email")}
+                    icon="email"
+                    value={profile.email}
+                  />
+                  <Divider />
+                </>
+              )}
+
+              {profile.date_of_birth && (
+                <>
+                  <ListItemInfo
+                    label={I18n.t("profile.data.list.birthDate")}
+                    icon="calendar"
+                    value={profile.date_of_birth.toLocaleDateString()}
+                  />
+                  <Divider />
+                </>
+              )}
+              {delIsSome && (
+                <ListItemSwitch
+                  disabled={delDisabled}
+                  icon="message"
+                  onSwitchValueChange={onDeleteSwitchChange}
+                  isLoading={delIsLoading || !delIsSome}
+                  label={I18n.t("profile.data.deleteState")}
+                  value={delValue}
                 />
-                <Divider />
-              </>
-            )}
-            {toyProfilePot.value.fiscal_code && (
-              <>
-                <ListItemInfo
-                  label={I18n.t("profile.data.list.fiscalCode")}
-                  icon={"fiscalCodeIndividual"}
-                  value={toyProfilePot.value.fiscal_code}
-                />
-                <Divider />
-              </>
-            )}
-            {toyProfilePot.value.email && (
-              <>
-                <ListItemInfo
-                  label={I18n.t("profile.data.list.email")}
-                  icon={"email"}
-                  value={toyProfilePot.value.email}
-                />
-                <Divider />
-              </>
-            )}
-            {toyProfilePot.value.date_of_birth && (
-              <>
-                <ListItemInfo
-                  label={I18n.t("profile.data.list.birthDate")}
-                  icon={"calendar"}
-                  value={toyProfilePot.value.date_of_birth.toLocaleDateString()}
-                />
-                <Divider />
-              </>
-            )}
-          </>
-        )}
-      </ContentWrapper>
+              )}
+            </>
+          )}
+        </ContentWrapper>
+      </LoadingSpinnerOverlay>
     </IOScrollViewWithLargeHeader>
   );
 };
