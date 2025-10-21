2fc9f5e8d436744568ec1b1f4466646669590472
Alessandro Izzo <alessandro.izzo998@gmail.com>
2025-10-21 12:47:39 +0200

    refactor: Changed loading screen with spinner loading component

--- ts/features/idpay/common/analytics/index.ts
+++ ts/features/idpay/common/analytics/index.ts
@@ -6,21 +6,9 @@ type EventProperties = {
   featureKey: IdPayFeatureKey;
 };
 
-const mapErrorByFeatureKey: Record<IdPayFeatureKey, string> = {
-  "idpay.onboarding": "IDPAY_ONBOARDING_INGRESS_TIMEOUT",
-  "idpay.initiative_details": "IDPAY_DETAILS_INGRESS_TIMEOUT"
-};
-
 const mapIngressScreenByFeatureKey: Record<IdPayFeatureKey, string> = {
   "idpay.onboarding": "IDPAY_ONBOARDING_INGRESS_SCREEN",
   "idpay.initiative_details": "IDPAY_DETAILS_INGRESS_SCREEN"
-};
-
-export const trackIDPayDisabledFeatureError = (props: EventProperties) => {
-  mixpanelTrack(
-    mapErrorByFeatureKey[props.featureKey],
-    buildEventProperties("KO", "screen_view", props)
-  );
 };
 
 export const trackIDPayDisabledFeatureIngressScreen = (
--- ts/features/idpay/common/components/IdPayDisabledFeatureAlert.tsx
+++ ts/features/idpay/common/components/IdPayDisabledFeatureAlert.tsx
@@ -1,39 +1,17 @@
 import I18n from "i18next";
-import { useEffect, useState } from "react";
 import { useIONavigation } from "../../../../navigation/params/AppParamsList";
 import { useOnFirstRender } from "../../../../utils/hooks/useOnFirstRender";
 import { useHeaderSecondLevel } from "../../../../hooks/useHeaderSecondLevel";
 import { OperationResultScreenContent } from "../../../../components/screens/OperationResultScreenContent";
-import LoadingScreenContent from "../../../../components/screens/LoadingScreenContent";
-import {
-  trackIDPayDisabledFeatureError,
-  trackIDPayDisabledFeatureIngressScreen
-} from "../analytics";
+import { trackIDPayDisabledFeatureIngressScreen } from "../analytics";
 import { IdPayFeatureKey } from "./IdPayEnabledFeatureFlagGuard";
 
 type Props = {
   featureKey: IdPayFeatureKey;
 };
 
-export const TIMEOUT_CHANGE_LABEL = 30000;
-
-const timeoutTentativesConfig = [
-  {
-    contentTitle: I18n.t("idpay.onboarding.failToRetry.contentTitle"),
-    timeout: TIMEOUT_CHANGE_LABEL
-  },
-  {
-    contentTitle: I18n.t("startup.title2"),
-    timeout: TIMEOUT_CHANGE_LABEL
-  }
-];
-
 export const IdPayDisabledFeatureAlert = ({ featureKey }: Props) => {
   const navigation = useIONavigation();
-  const [contentTitle, setContentTitle] = useState(
-    I18n.t("idpay.onboarding.failToRetry.contentTitle")
-  );
-  const [currentTentative, setCurrentTentative] = useState(0);
 
   useOnFirstRender(() => {
     trackIDPayDisabledFeatureIngressScreen({ featureKey });
@@ -44,56 +22,21 @@ export const IdPayDisabledFeatureAlert = ({ featureKey }: Props) => {
     title: ""
   });
 
-  useEffect(() => {
-    const tentativeConfig = timeoutTentativesConfig[currentTentative];
-    if (!tentativeConfig) {
-      return;
-    }
-    setContentTitle(tentativeConfig?.contentTitle);
-    const timeout = setTimeout(() => {
-      setCurrentTentative(prev => prev + 1);
-    }, tentativeConfig?.timeout);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [currentTentative]);
-
-  const showBlockingScreen =
-    currentTentative === timeoutTentativesConfig.length;
-
   const handleOnPressBack = () => {
     navigation.popToTop();
   };
 
-  useEffect(() => {
-    if (showBlockingScreen) {
-      trackIDPayDisabledFeatureError({
-        featureKey
-      });
-    }
-  }, [showBlockingScreen, featureKey]);
-
-  if (showBlockingScreen) {
-    return (
-      <OperationResultScreenContent
-        testID="device-blocking-screen-id"
-        pictogram="time"
-        title={I18n.t("idpay.slowdowns_results_screen.title")}
-        subtitle={I18n.t("idpay.slowdowns_results_screen.subtitle")}
-        action={{
-          label: I18n.t("global.buttons.back"),
-          onPress: handleOnPressBack
-        }}
-      />
-    );
-  }
-
   return (
-    <LoadingScreenContent
-      testID="ingress-screen-loader-id"
-      contentTitle={contentTitle}
-      animatedPictogramSource="waiting"
+    <OperationResultScreenContent
+      testID="device-blocking-screen-id"
+      pictogram="spinner"
+      enableAnimatedPictogram
+      title={I18n.t("idpay.onboarding.disabledScreen.title")}
+      subtitle={I18n.t("idpay.onboarding.disabledScreen.subtitle")}
+      secondaryAction={{
+        label: I18n.t("global.buttons.cancel"),
+        onPress: handleOnPressBack
+      }}
     />
   );
 };
