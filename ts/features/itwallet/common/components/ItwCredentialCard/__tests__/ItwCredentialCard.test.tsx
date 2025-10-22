27065a3027daeaafc39f046251616ef47b2062e8
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2025-10-22 16:32:47 +0200

    test: add new scenario cases in ItwCredentialCard test

--- ts/features/itwallet/common/components/ItwCredentialCard/__tests__/ItwCredentialCard.test.tsx
+++ ts/features/itwallet/common/components/ItwCredentialCard/__tests__/ItwCredentialCard.test.tsx
@@ -4,9 +4,13 @@ import configureMockStore from "redux-mock-store";
 import { applicationChangeState } from "../../../../../../store/actions/application";
 import { appReducer } from "../../../../../../store/reducers";
 import { GlobalState } from "../../../../../../store/reducers/types";
-import { ItwCredentialStatus } from "../../../utils/itwTypesUtils";
+import {
+  ItwCredentialStatus,
+  ItwJwtCredentialStatus
+} from "../../../utils/itwTypesUtils";
 import { ItwCredentialCard } from "../ItwCredentialCard";
 import * as lifecycleSelectors from "../../../../lifecycle/store/selectors";
+import * as credentials from "../../../../credentials/store/selectors";
 
 describe("ItwCredentialCard", () => {
   it.each([
@@ -80,4 +84,62 @@ describe("ItwCredentialCard", () => {
     );
     expect(component).toMatchSnapshot();
   });
+
+  it.each([
+    "jwtExpired",
+    "jwtExpiring"
+  ] as ReadonlyArray<ItwJwtCredentialStatus>)(
+    "should match snapshot when eID is expired and credential is %p (credential status overridden to 'valid')",
+    credentialStatus => {
+      jest
+        .spyOn(credentials, "itwCredentialsEidStatusSelector")
+        .mockReturnValue("jwtExpired");
+
+      const mockStore = configureMockStore<GlobalState>();
+      const store = mockStore(
+        appReducer(undefined, applicationChangeState("active")) as GlobalState
+      );
+
+      const component = render(
+        <Provider store={store}>
+          <ItwCredentialCard
+            credentialType="mDL"
+            credentialStatus={credentialStatus}
+          />
+        </Provider>
+      );
+
+      expect(component).toMatchSnapshot();
+    }
+  );
+
+  it.each([
+    "expired",
+    "expiring",
+    "invalid",
+    "unknown"
+  ] as ReadonlyArray<ItwCredentialStatus>)(
+    "should match snapshot when eID is expired and credential is %p (credential status not overridden)",
+    credentialStatus => {
+      jest
+        .spyOn(credentials, "itwCredentialsEidStatusSelector")
+        .mockReturnValue("jwtExpired");
+
+      const mockStore = configureMockStore<GlobalState>();
+      const store = mockStore(
+        appReducer(undefined, applicationChangeState("active")) as GlobalState
+      );
+
+      const component = render(
+        <Provider store={store}>
+          <ItwCredentialCard
+            credentialType="mDL"
+            credentialStatus={credentialStatus}
+          />
+        </Provider>
+      );
+
+      expect(component).toMatchSnapshot();
+    }
+  );
 });
