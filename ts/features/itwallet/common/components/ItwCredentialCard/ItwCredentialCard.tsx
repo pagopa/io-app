4209eb0e7079092465921789d9337db58b435ef4
RiccardoMolinari95 <98079356+RiccardoMolinari95@users.noreply.github.com>
2025-10-23 16:54:43 +0200

    chore(IT Wallet): [SIW-3187,SIW-3236] eID new reactivation flow (Wallet section) and ITW upgrade banner copy (#7491)
    ## Short description
    This PR manages the display of alerts and credential card in the Wallet
    section for eID and credentials during the eID reactivation flow.
    Changed the copy of ITW upgrade banner based on the presence of
    Documenti su IO.
    
    ## List of changes proposed in this pull request
    - Updated copy for `jwtExpired` and `jwtExpiring` cases in
    `ItwEidLifecycleAlert`
    - Ensured that in the Wallet section, credential cards are displayed as
    `valid` when the eID status is `jwtExpiring` or `jwtExpired` and the
    credential status is not `invalid`, `expired`, or `expiring`.
    - Removed `ItwPresentationCredentialVerificationExpired` screen.
    Previously, credentials with `jwtExpired` would redirect users to a
    reissuing screen; now, users can access the credential detail screen
    directly.
    - Added an alert, in `ItwEidLifecycleAlert`, for the offline scenario
    when the eID is expired and IT Wallet is not active.
    - Moved `ItwEidLifecycleAlert` into `ItwEidInfoBottomSheetContent` so it
    appears above the claims section.
    - Changed the `ItwUpgradeBanner` copy based on the presence of Documenti
    su IO.
    
    ## How to test
    Verify that all implemented scenarios match the Figma designs for eID
    states and banners.
    
    Possible scenarios: 
    
    
    |**Expiring eID and digital credential nearing expiration or valid** |
    **Valid eID and one digital credential nearing expiration and one
    expired** | **Expired eID and digital credential valid, nearing
    expiration or expired** | **Expired eID and physical credential nearing
    expiration or expired** |
    | ------------- | ------------- | ------------- | ------------- |
    | <img width="395" height="893" alt="Screenshot 2025-10-21 at 11 23 37"
    src="https://github.com/user-attachments/assets/981ea2db-5e01-4239-b946-dd6f4af5511d"
    />| <img width="395" height="893" alt="Screenshot 2025-10-21 at 11 30
    32"
    src="https://github.com/user-attachments/assets/a580ab21-704e-45b4-9084-163b1fe10d52"
    /> | <img width="395" height="893" alt="Screenshot 2025-10-21 at 11 41
    14"
    src="https://github.com/user-attachments/assets/2556eaf6-b27b-416d-847c-c731f7312e72"
    /> | <img width="395" height="893" alt="Screenshot 2025-10-21 at 11 44
    10"
    src="https://github.com/user-attachments/assets/de081702-a3d5-4032-b557-ae5f86735770"
    /> |
    
    |**Offline, expiring eID**  | **Offline, expired eID** |
    | ------------- | ------------- |
    | <img width="395" height="893" alt="Screenshot 2025-10-21 at 11 19 23"
    src="https://github.com/user-attachments/assets/25cf4615-663c-4c5f-821d-5e6a278e23d1"
    /> |<img width="395" height="893" alt="Screenshot 2025-10-21 at 11 16
    18"
    src="https://github.com/user-attachments/assets/a231e66d-75ad-4461-a9c8-3ae6f082eb58"
    /> |
    
    |**ITW banner with expiring eID** | **ITW banner with expired eID** |
    **Issuing a new credential with an expired eID** |
    | ------------- | ------------- | ------------- |
    | <video
    src="https://github.com/user-attachments/assets/467e2a1b-dc8b-4f52-83b4-732fb64d61c4">
    | <video
    src="https://github.com/user-attachments/assets/f3ef4960-c029-4d3d-967c-7e2b63f285d3">
    | <video
    src="https://github.com/user-attachments/assets/e1ac7816-00a1-4b4c-ac74-7b31797514cc">
    |

--- ts/features/itwallet/common/components/ItwCredentialCard/ItwCredentialCard.tsx
+++ ts/features/itwallet/common/components/ItwCredentialCard/ItwCredentialCard.tsx
@@ -13,7 +13,9 @@ import {
 } from "../../utils/itwCredentialUtils";
 import { getThemeColorByCredentialType } from "../../utils/itwStyleUtils";
 import { ItwCredentialStatus } from "../../utils/itwTypesUtils";
+import { itwCredentialsEidStatusSelector } from "../../../credentials/store/selectors";
 import { itwLifecycleIsITWalletValidSelector } from "../../../lifecycle/store/selectors";
+import { offlineAccessReasonSelector } from "../../../../ingress/store/selectors";
 import { CardBackground } from "./CardBackground";
 import { DigitalVersionBadge } from "./DigitalVersionBadge";
 import { CardColorScheme } from "./types";
@@ -58,6 +60,40 @@ export const ItwCredentialCard = ({
   const typefacePreference = useIOSelector(fontPreferenceSelector);
   const isItwPid = useIOSelector(itwLifecycleIsITWalletValidSelector);
   const needsItwUpgrade = isItwPid && !isItwCredential;
+  const maybeEidStatus = useIOSelector(itwCredentialsEidStatusSelector);
+  const offlineAccessReason = useIOSelector(offlineAccessReasonSelector);
+
+  /**
+   * The credential's expire UI should be displayed only when:
+   * - the eID status is "valid"
+   * - the eID status is not "valid" and the document associated to the
+   *   credential is "expiring", "expired", "invalid" or "unknown"
+   */
+  const status = useMemo<ItwCredentialStatus>(() => {
+    const excludedCredentialStatuses: ReadonlyArray<ItwCredentialStatus> = [
+      "expired",
+      "expiring",
+      "invalid",
+      "unknown"
+    ];
+
+    // In offline mode show digital credential nearing expiration and expired as valid
+    if (
+      offlineAccessReason !== undefined &&
+      !excludedCredentialStatuses.includes(credentialStatus)
+    ) {
+      return "valid";
+    }
+
+    if (
+      maybeEidStatus === "valid" ||
+      excludedCredentialStatuses.includes(credentialStatus)
+    ) {
+      return credentialStatus;
+    }
+
+    return "valid";
+  }, [credentialStatus, maybeEidStatus, offlineAccessReason]);
 
   const borderColorMap = useBorderColorByStatus();
 
@@ -69,11 +105,13 @@ export const ItwCredentialCard = ({
       };
     }
 
-    return tagPropsByStatus[credentialStatus];
-  }, [credentialStatus, needsItwUpgrade]);
+    return tagPropsByStatus[status];
+  }, [status, needsItwUpgrade]);
 
   const { titleColor, titleOpacity, colorScheme } = useMemo<StyleProps>(() => {
-    const isValid = validCredentialStatuses.includes(credentialStatus);
+    // Include "jwtExpired" as a valid status because credentials with this state
+    // should not appear faded. Only the "expired" status should be displayed with reduced opacity.
+    const isValid = [...validCredentialStatuses, "jwtExpired"].includes(status);
     const theme = getThemeColorByCredentialType(credentialType);
 
     if (needsItwUpgrade) {
@@ -84,7 +122,7 @@ export const ItwCredentialCard = ({
       };
     }
 
-    if (credentialStatus === "unknown") {
+    if (status === "unknown") {
       return {
         titleColor: Color(theme.textColor).grayscale().hex(),
         titleOpacity: 0.5,
@@ -105,7 +143,7 @@ export const ItwCredentialCard = ({
       titleOpacity: 0.5,
       colorScheme: "faded"
     };
-  }, [credentialType, credentialStatus, needsItwUpgrade]);
+  }, [credentialType, status, needsItwUpgrade]);
 
   return (
     <View style={styles.cardContainer}>
@@ -145,12 +183,7 @@ export const ItwCredentialCard = ({
         credentialType={credentialType}
         colorScheme={colorScheme}
       />
-      <View
-        style={[
-          styles.border,
-          { borderColor: borderColorMap[credentialStatus] }
-        ]}
-      />
+      <View style={[styles.border, { borderColor: borderColorMap[status] }]} />
     </View>
   );
 };
