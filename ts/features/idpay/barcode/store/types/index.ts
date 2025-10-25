6eb84455ec8a47b04d1344974c5ebadfe9dbf5e8
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-10-25 17:22:53 +0200

    chore: common types file

---
+++ ts/features/idpay/barcode/store/types/index.ts
@@ -0,0 +1,16 @@
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import { TransactionBarCodeResponse } from "../../../../../../definitions/idpay/TransactionBarCodeResponse";
+import { TransactionErrorDTO } from "../../../../../../definitions/idpay/TransactionErrorDTO";
+import { NetworkError } from "../../../../../utils/errors";
+
+export type IdPayBarcodeState = {
+  [initiativeId: string]: pot.Pot<
+    TransactionBarCodeResponse,
+    TransactionErrorDTO | NetworkError
+  >;
+};
+
+export type IdPayStaticCodeState = pot.Pot<
+  TransactionBarCodeResponse,
+  TransactionErrorDTO | NetworkError
+>;
