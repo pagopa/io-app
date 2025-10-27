4c72ee1be07f8577d76e91c764e4ea73418591cd
Alessandro Izzo <alessandro.izzo998@gmail.com>
2025-10-27 10:42:44 +0100

    feat: Added IDPay disabled feature flag  screen if the entire IDPay feature is disabled

---
+++ ts/features/idpay/common/screens/IdPayDisabledScreen.tsx
@@ -0,0 +1,41 @@
+import I18n from "i18next";
+import { useIONavigation } from "../../../../navigation/params/AppParamsList";
+import { useOnFirstRender } from "../../../../utils/hooks/useOnFirstRender";
+import { useHeaderSecondLevel } from "../../../../hooks/useHeaderSecondLevel";
+import { OperationResultScreenContent } from "../../../../components/screens/OperationResultScreenContent";
+
+/**
+ * Screen displayed when the IDPay feature is disabled via feature flag.
+ * This screen shows a message to the user informing them that the functionality
+ * is no longer available.
+ */
+export const IdPayDisabledScreen = () => {
+  const navigation = useIONavigation();
+
+  useOnFirstRender(() => {
+    // TODO: Add analytics tracking if needed
+    // trackIDPayFeatureDisabledScreen();
+  });
+
+  useHeaderSecondLevel({
+    headerShown: false,
+    title: ""
+  });
+
+  const handleOnPressClose = () => {
+    navigation.popToTop();
+  };
+
+  return (
+    <OperationResultScreenContent
+      testID="idpay-feature-disabled-screen"
+      pictogram="umbrella"
+      title={I18n.t("idpay.onboarding.featureDisabled.title")}
+      subtitle={I18n.t("idpay.onboarding.featureDisabled.subtitle")}
+      action={{
+        label: I18n.t("global.buttons.close"),
+        onPress: handleOnPressClose
+      }}
+    />
+  );
+};
