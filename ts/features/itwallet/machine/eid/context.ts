1c1ddc0a040ac550a6d771ae8ade275e010616f0
Federico Mastrini <federicomastrini93@gmail.com>
2025-10-28 08:31:34 +0100

    chore: rename `l2-plus` to `l3-next`

--- ts/features/itwallet/machine/eid/context.ts
+++ ts/features/itwallet/machine/eid/context.ts
@@ -46,10 +46,12 @@ export type EidIssuanceMode = "issuance" | "reissuance" | "upgrade";
  * determines which authentication method are allowed:
  * - "l2": Documenti su IO issuance using CIE+PIN, CIEID, or SPID
  * - "l2-fallback": Documenti su IO issuance using CIEID or SPID (fallback mode)
- * - "l2-plus": IT Wallet issuance using CIE+PIN, CIEID, or SPID plus an additional CIE card authentication
- * - "l3" IT Wallet issuance using CIE+PIN or CIEID
+ * - "l3": IT Wallet issuance using CIE+PIN or CIEID
+ * - "l3-next" IT Wallet issuance using CIE+PIN, CIEID, or SPID plus an additional CIE card authentication
+ *
+ * Note: "l3" and "l3-next" will be merged once the new L2+/L3 flow will be available.
  */
-export type EidIssuanceLevel = "l2" | "l2-fallback" | "l2-plus" | "l3";
+export type EidIssuanceLevel = "l2" | "l2-fallback" | "l3" | "l3-next";
 
 export type Context = {
   /**
--- ts/features/itwallet/machine/eid/utils.ts
+++ ts/features/itwallet/machine/eid/utils.ts
@@ -2,7 +2,7 @@ import { EidIssuanceLevel } from "./context";
 
 /**
  * Utility function to determine if L3 features are enabled based on the issuance level.
+ * L3 features are enabled for levels "l3" and "l3-legacy".
  */
 export const isL3IssuanceFeaturesEnabled = (level?: EidIssuanceLevel) =>
-  level === "l3" || // L3 PID issuance
-  level === "l2-plus"; // L3 PID issuance with L2+ plus flow
+  level?.startsWith("l3") || false;
