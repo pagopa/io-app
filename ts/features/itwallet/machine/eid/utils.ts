09467a680dfef01ec1345959b3ff125def9f2dfa
Federico Mastrini <federicomastrini93@gmail.com>
2025-10-28 16:30:07 +0100

    fix: isL3IssuanceFeaturesEnabled

--- ts/features/itwallet/machine/eid/utils.ts
+++ ts/features/itwallet/machine/eid/utils.ts
@@ -2,7 +2,7 @@ import { EidIssuanceLevel } from "./context";
 
 /**
  * Utility function to determine if L3 features are enabled based on the issuance level.
- * L3 features are enabled for levels "l3" and "l3-legacy".
+ * L3 features are enabled for levels "l3" and "l3-next".
  */
 export const isL3IssuanceFeaturesEnabled = (level?: EidIssuanceLevel) =>
-  level?.startsWith("l3") || false;
+  level === "l3" || level === "l3-next";
