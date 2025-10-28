427ce4075920729b6b30fbbb8f54dad5f0b84ed3
Gianluca Spada <gianluca.spada@spindox.it>
2025-10-28 08:29:57 +0100

    chore: update tests

--- ts/features/itwallet/issuance/screens/__tests__/ItwIssuanceCredentialAsyncContinuationScreen.test.tsx
+++ ts/features/itwallet/issuance/screens/__tests__/ItwIssuanceCredentialAsyncContinuationScreen.test.tsx
@@ -74,7 +74,7 @@ const renderComponent = (
 
   return renderScreenWithNavigationStoreContext<GlobalState>(
     ItwIssuanceCredentialAsyncContinuationScreen,
-    ITW_ROUTES.ISSUANCE.CREDENTIAL_ASYNC_FLOW_CONTINUATION,
+    ITW_ROUTES.LANDING_SCREEN.CREDENTIAL_ASYNC_FLOW_CONTINUATION,
     routeParams ?? {},
     store
   );
