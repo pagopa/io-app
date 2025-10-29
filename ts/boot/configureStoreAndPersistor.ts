ef74f17d2467a65cf944d714a5e53d445cc37eed
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-10-29 16:33:47 +0100

    chore: [IOPLT-1370] Upgrades react-native to 0.81

--- ts/boot/configureStoreAndPersistor.ts
+++ ts/boot/configureStoreAndPersistor.ts
@@ -57,7 +57,7 @@ import { isDevEnv, isTestEnv } from "../utils/environment";
 import { PersistedPreferencesState } from "../store/reducers/persistedPreferences";
 import { SpidIdps } from "../../definitions/content/SpidIdps";
 import { fromGeneratedToLocalSpidIdp } from "../utils/idps";
-import { configureReactotron } from "./configureRectotron";
+// import { configureReactotron } from "./configureRectotron";
 
 /**
  * Redux persist will migrate the store to the current version
@@ -652,7 +652,7 @@ const logger = createLogger({
 });
 
 // configure Reactotron if the app is running in dev mode
-export const RTron = isDevEnv ? configureReactotron() : undefined;
+// export const RTron = isDevEnv ? configureReactotron() : undefined;
 const sagaMiddleware = createSagaMiddleware();
 
 function configureStoreAndPersistor(): {
@@ -673,9 +673,10 @@ function configureStoreAndPersistor(): {
 
   // add Reactotron enhancer if the app is running in dev mode
   const enhancer: StoreEnhancer =
-    RTron && RTron.createEnhancer
-      ? composeEnhancers(middlewares, RTron.createEnhancer())
-      : composeEnhancers(middlewares);
+    // RTron && RTron.createEnhancer
+    //   ? composeEnhancers(middlewares, RTron.createEnhancer())
+    // :
+    composeEnhancers(middlewares);
 
   const store: Store = createStore<
     PersistedGlobalState,
