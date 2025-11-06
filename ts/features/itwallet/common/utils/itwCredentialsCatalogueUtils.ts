88e627dc68c385b7df6a55cd7b34d9d51adcdc2e
Gianluca Spada <gianluca.spada@spindox.it>
2025-11-06 08:46:09 +0100

    chore: remove defaultRetryingFetch

--- ts/features/itwallet/common/utils/itwCredentialsCatalogueUtils.ts
+++ ts/features/itwallet/common/utils/itwCredentialsCatalogueUtils.ts
@@ -1,12 +1,15 @@
 import { CredentialsCatalogue } from "@pagopa/io-react-native-wallet";
-import { defaultRetryingFetch } from "../../../../utils/fetch";
 import { Env } from "./environment";
 
 export type DigitalCredentialsCatalogue = Awaited<
   ReturnType<typeof CredentialsCatalogue.fetchAndParseCatalogue>
 >;
 
+/**
+ * Fetch the Digital Credentials Catalogue.
+ *
+ * @param env The environment to use for the Trust Anchor's base URL
+ * @returns The credentials catalogue parsed JWT
+ */
 export const fetchCredentialsCatalogue = (env: Env) =>
-  CredentialsCatalogue.fetchAndParseCatalogue(env.WALLET_TA_BASE_URL, {
-    appFetch: defaultRetryingFetch()
-  });
+  CredentialsCatalogue.fetchAndParseCatalogue(env.WALLET_TA_BASE_URL);
