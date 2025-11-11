fea488c9aa944c62203e52502131660b17e378ef
MonicaRungi-EY <Monica.Rungi@it.ey.com>
2025-11-11 10:47:32 +0100

    alert

--- ts/features/itwallet/presentation/details/components/ItwPresentationCredentialStatusAlert.tsx
+++ ts/features/itwallet/presentation/details/components/ItwPresentationCredentialStatusAlert.tsx
@@ -46,7 +46,9 @@ const excludedCredentialTypes = [
   CredentialType.PID,
   CredentialType.EDUCATION_DEGREE,
   CredentialType.EDUCATION_ENROLLMENT,
-  CredentialType.RESIDENCY
+  CredentialType.RESIDENCY,
+  CredentialType.EDUCATION_DIPLOMA,
+  CredentialType.SCHOOL_ENROLLMENT
 ] as const;
 
 type ExcludedCredentialTypes = (typeof excludedCredentialTypes)[number];
