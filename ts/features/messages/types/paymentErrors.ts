822b5e7f377dd83726f04e1addb286cb5eddbe91
ChrisMattew <cristian.matteu@spindox.it>
2025-11-11 12:50:43 +0100

    style(paymentErrors): rename types

--- ts/features/messages/types/paymentErrors.ts
+++ ts/features/messages/types/paymentErrors.ts
@@ -3,22 +3,25 @@ import { isError, RemoteValue } from "../../../common/model/RemoteValue";
 import { isOngoingPaymentFromDetailV2Enum } from "../../../utils/payment";
 
 export type MessagePaymentError =
-  | MessageGenericError
-  | MessageSpecificError
-  | MessageTimeoutError;
-export type MessageGenericError = { type: "generic"; message: string };
-export type MessageSpecificError = { type: "specific"; details: Detail_v2Enum };
-export type MessageTimeoutError = { type: "timeout" };
+  | MessagePaymentGenericError
+  | MessagePaymentSpecificError
+  | MessagePaymentTimeoutError;
+export type MessagePaymentGenericError = { type: "generic"; message: string };
+export type MessagePaymentSpecificError = {
+  type: "specific";
+  details: Detail_v2Enum;
+};
+export type MessagePaymentTimeoutError = { type: "timeout" };
 
 export const isMessagePaymentGenericError = (
   error: MessagePaymentError
-): error is MessageGenericError => error.type === "generic";
+): error is MessagePaymentGenericError => error.type === "generic";
 export const isMessagePaymentSpecificError = (
   error: MessagePaymentError
-): error is MessageSpecificError => error.type === "specific";
+): error is MessagePaymentSpecificError => error.type === "specific";
 export const isMessagePaymentTimeoutError = (
   error: MessagePaymentError
-): error is MessageTimeoutError => error.type === "timeout";
+): error is MessagePaymentTimeoutError => error.type === "timeout";
 
 export const isTimeoutOrGenericOrOngoingPaymentError = (
   input: RemoteValue<unknown, MessagePaymentError>
