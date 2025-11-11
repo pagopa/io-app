ba9b4034b592a2b2b68eecbeead472bc226e486e
Gianluca Spada <gianluca.spada@spindox.it>
2025-11-11 14:37:48 +0100

    refactor: itwIsCredentialsCatalogueStale

--- ts/features/itwallet/credentialsCatalogue/store/selectors/index.ts
+++ ts/features/itwallet/credentialsCatalogue/store/selectors/index.ts
@@ -1,4 +1,6 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
+import * as O from "fp-ts/lib/Option";
+import { constTrue, pipe } from "fp-ts/lib/function";
 import { isAfter } from "date-fns";
 import { GlobalState } from "../../../../../store/reducers/types";
 
@@ -15,7 +17,10 @@ export const itwCredentialsCatalogueSelector = (state: GlobalState) =>
  * Normally, the catalogue is fetched every 24 hours according to the `expires` HTTP header.
  * If the fetch fails, it is still possible to select the persisted catalogue, but it may be stale.
  */
-export const itwIsCredentialsCatalogueStale = (state: GlobalState) => {
-  const catalogue = itwCredentialsCatalogueSelector(state);
-  return catalogue ? isAfter(new Date(), new Date(catalogue.exp * 1000)) : true;
-};
+export const itwIsCredentialsCatalogueStale = (state: GlobalState) =>
+  pipe(
+    itwCredentialsCatalogueSelector(state),
+    O.fromNullable,
+    O.map(catalogue => isAfter(new Date(), new Date(catalogue.exp * 1000))),
+    O.getOrElse(constTrue)
+  );
