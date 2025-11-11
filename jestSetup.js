c7a3be9f7e3fe38edef4ea395cf96e2a1d1507c2
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-11-11 11:48:37 +0100

    apply suggestion from review

--- jestSetup.js
+++ jestSetup.js
@@ -268,16 +268,14 @@ jest.mock("react-native-bluetooth-state-manager", () => ({
   getState: jest.fn().mockResolvedValue(true)
 }));
 
-jest.mock("@pagopa/io-react-native-iso18013", () => {
-  return {
-    CBOR: {
-      decodeIssuerSigned: jest.fn(() => Promise.resolve("test"))
-    },
-    COSE: {
-      verify: jest.fn(() => Promise.resolve(true))
-    }
-  };
-});
+jest.mock("@pagopa/io-react-native-iso18013", () => ({
+  CBOR: {
+    decodeIssuerSigned: jest.fn(() => Promise.resolve("test"))
+  },
+  COSE: {
+    verify: jest.fn(() => Promise.resolve(true))
+  }
+}));
 
 jest.mock("@pagopa/io-react-native-cie", () => ({
   CieManager: jest.fn()
