2df62791653203ea694de5e7549ba80f33792e0a
Pawel <33898715+soixdev91@users.noreply.github.com>
2025-11-12 10:11:52 +0100

    chore: [IOBP-2178] App check with npx knip - IDPay (#7600)
    ## Short description
    Remove duplicated or unused exports for IDPay
    
    ## How to test
    run npx knip again
    
    ---------
    
    Co-authored-by: Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>

--- ts/features/idpay/onboarding/machine/events.ts
+++ ts/features/idpay/onboarding/machine/events.ts
@@ -3,49 +3,49 @@ import { SelfConsentMultiDTO } from "../../../../../definitions/idpay/SelfConsen
 import { SelfConsentMultiTypeDTO } from "../../../../../definitions/idpay/SelfConsentMultiTypeDTO";
 import { SelfConsentTextDTO } from "../../../../../definitions/idpay/SelfConsentTextDTO";
 
-export type StartOnboarding = {
+type StartOnboarding = {
   readonly type: "start-onboarding";
   readonly serviceId: string;
   readonly hasInbox: boolean;
 };
 
-export type ToggleBoolCriteria = {
+type ToggleBoolCriteria = {
   readonly type: "toggle-bool-criteria";
   readonly criteria: SelfConsentBoolDTO;
 };
 
-export type SelectMultiConsent = {
+type SelectMultiConsent = {
   readonly type: "select-multi-consent";
   readonly data: SelfConsentMultiDTO | SelfConsentMultiTypeDTO;
 };
 
-export type InputTextCriteria = {
+type InputTextCriteria = {
   readonly type: "input-text-criteria";
   readonly criteria: SelfConsentTextDTO;
 };
 
-export type Next = {
+type Next = {
   readonly type: "next";
 };
 
-export type Back = {
+type Back = {
   readonly type: "back";
 };
 
-export type Close = {
+type Close = {
   readonly type: "close";
 };
 
-export type UpdatePushNotificationSetting = {
+type UpdatePushNotificationSetting = {
   readonly type: "update-notification-status";
   readonly isPushNotificationEnabled: boolean;
 };
 
-export type CheckDetails = {
+type CheckDetails = {
   readonly type: "check-details";
 };
 
-export type RetryConnectionEvent = {
+type RetryConnectionEvent = {
   readonly type: "retryConnection";
 };
 
