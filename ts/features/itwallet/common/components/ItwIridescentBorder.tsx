4ea93336e734bba80c047cab7fd146b1df17209c
Federico Mastrini <federicomastrini93@gmail.com>
2025-11-14 08:42:13 +0100

    chore: add light effect

--- ts/features/itwallet/common/components/ItwIridescentBorder.tsx
+++ ts/features/itwallet/common/components/ItwIridescentBorder.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable functional/immutable-data */
+import { useIOThemeContext } from "@pagopa/io-app-design-system";
 import {
   Canvas,
   ColorMatrix,
@@ -8,8 +9,10 @@ import {
   Paint,
   Rect,
   RoundedRect,
+  Circle as SkiaCircle,
   Group as SkiaGroup,
   LinearGradient as SkiaLinearGradient,
+  RadialGradient as SkiaRadialGradient,
   vec
 } from "@shopify/react-native-skia";
 import { LayoutRectangle } from "react-native";
@@ -36,11 +39,25 @@ type ItwIridescentBorderProps = {
   variant?: ItwIridescentBorderVariant;
 };
 
+/* Light */
+const lightScaleMultiplier = 1;
+const lightSize: LayoutRectangle["width"] = 250;
+const visibleLightPercentage = 0.25; // Visible light when it's near box boundaries
+
 export const ItwIridescentBorder = ({
   width,
   height,
   variant = "default"
 }: ItwIridescentBorderProps) => {
+  const { themeType } = useIOThemeContext();
+  const isLightMode = themeType === "light";
+
+  /* Styles */
+  const gradientTickOpacity = isLightMode ? 1 : 0.8;
+  const lightSkiaOpacity = isLightMode ? 0.4 : 0.05;
+  const gradientBorderOpacity = isLightMode ? 1.0 : 0.5;
+
+  /* Sensors */
   const rotationSensor = useAnimatedSensor(SensorType.ROTATION);
   const currentRoll = useSharedValue(0);
   const initialRoll = useSharedValue(0);
@@ -63,9 +80,23 @@ export const ItwIridescentBorder = ({
     () => initialRoll.value - currentRoll.value
   );
 
+  /* Set translate boundaries for the main light */
+  const maxTranslateX = (width - (lightSize ?? 0) * visibleLightPercentage) / 2;
+
   /* We don't need to look at the whole quaternion range,
       just a very small part of it. */
   const quaternionRange: number = 0.5;
+
+  const skiaLightTranslateX = useDerivedValue(() => {
+    const translateX = interpolate(
+      relativeRoll.value,
+      [quaternionRange, -quaternionRange],
+      [maxTranslateX, -maxTranslateX],
+      Extrapolation.CLAMP
+    );
+
+    return [{ translateX }, { scale: lightScaleMultiplier }];
+  });
 
   const skiaGradientTranslateY = useDerivedValue(() => [
     {
@@ -109,6 +140,45 @@ export const ItwIridescentBorder = ({
     </SkiaGroup>
   );
 
+  const BoxLight = () => (
+    <SkiaGroup opacity={lightSkiaOpacity} origin={vec(width / 2, height / 2)}>
+      <SkiaCircle
+        cx={(width ?? 0) / 2}
+        cy={(height ?? 0) / 2}
+        r={lightSize / 2}
+        transform={skiaLightTranslateX}
+      >
+        <SkiaRadialGradient
+          c={vec((width ?? 0) / 2, (height ?? 0) / 2)}
+          r={lightSize / 2}
+          /* There are many stops because it's an easing gradient. */
+          positions={[
+            0, 0.081, 0.155, 0.225, 0.29, 0.353, 0.412, 0.471, 0.529, 0.588,
+            0.647, 0.71, 0.775, 0.845, 0.919, 1
+          ]}
+          colors={[
+            "rgba(255,255,255,1)",
+            "rgba(255,255,255,0.987)",
+            "rgba(255,255,255,0.95)",
+            "rgba(255,255,255,0.89)",
+            "rgba(255,255,255,0.825)",
+            "rgba(255,255,255,0.74)",
+            "rgba(255,255,255,0.65)",
+            "rgba(255,255,255,0.55)",
+            "rgba(255,255,255,0.45)",
+            "rgba(255,255,255,0.35)",
+            "rgba(255,255,255,0.26)",
+            "rgba(255,255,255,0.175)",
+            "rgba(255,255,255,0.1)",
+            "rgba(255,255,255,0.05)",
+            "rgba(255,255,255,0.01)",
+            "rgba(255,255,255,0)"
+          ]}
+        />
+      </SkiaCircle>
+    </SkiaGroup>
+  );
+
   return (
     <Canvas
       style={{
@@ -117,14 +187,16 @@ export const ItwIridescentBorder = ({
         width
       }}
     >
-      <SkiaGroup blendMode={"colorBurn"} opacity={0}>
+      <BoxLight />
+
+      <SkiaGroup blendMode={"colorBurn"} opacity={0.05}>
         <BrandedGradient />
       </SkiaGroup>
 
       <Mask
         mode="alpha"
         mask={
-          <SkiaGroup blendMode={"colorDodge"} opacity={1}>
+          <SkiaGroup blendMode={"colorDodge"} opacity={gradientBorderOpacity}>
             <BrandedGradient />
           </SkiaGroup>
         }
@@ -132,7 +204,7 @@ export const ItwIridescentBorder = ({
         <Group
           layer={
             <Paint>
-              <ColorMatrix matrix={OpacityMatrix(1)} />
+              <ColorMatrix matrix={OpacityMatrix(gradientTickOpacity)} />
             </Paint>
           }
         >
@@ -145,7 +217,6 @@ export const ItwIridescentBorder = ({
             strokeWidth={3}
             strokeJoin={"round"}
             style={"stroke"}
-            color={"red"}
           />
         </Group>
       </Mask>
