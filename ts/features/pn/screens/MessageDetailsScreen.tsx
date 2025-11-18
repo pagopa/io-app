79b2f141eae989bc352e6b4af8a6f5db8200b4a2
forrest57 <tomba808@gmail.com>
2025-11-18 12:21:32 +0100

    small fix

--- ts/features/pn/screens/MessageDetailsScreen.tsx
+++ ts/features/pn/screens/MessageDetailsScreen.tsx
@@ -1,7 +1,7 @@
 import { HeaderSecondLevel } from "@pagopa/io-app-design-system";
 import { useFocusEffect } from "@react-navigation/native";
 import I18n from "i18next";
-import { RefObject, useCallback, useEffect, useRef } from "react";
+import { RefObject, useCallback, useEffect, useMemo, useRef } from "react";
 import { ServiceId } from "../../../../definitions/backend/ServiceId";
 import { OperationResultScreenContent } from "../../../components/screens/OperationResultScreenContent";
 import { useHardwareBackButton } from "../../../hooks/useHardwareBackButton";
@@ -127,9 +127,11 @@ export const MessageDetailsScreen = ({ route }: MessageDetailsRouteProps) => {
   const aarBottomSheetRef = useRef<() => void>(undefined);
 
   const currentFiscalCode = useIOSelector(profileFiscalCodeSelector);
-  const sendMessageOrUndefined = useIOSelector(
-    curriedSendMessageFromIdSelector(messageId)
+  const sendMessageFromIdSelector = useMemo(
+    () => curriedSendMessageFromIdSelector(messageId),
+    [messageId]
   );
+  const sendMessageOrUndefined = useIOSelector(sendMessageFromIdSelector);
 
   const isAarMessage = openingSourceIsAarMessage(sendOpeningSource);
   const fiscalCodeOrUndefined = isAarMessage ? undefined : currentFiscalCode;
