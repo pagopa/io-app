1d5358bf4229319a60a908684a2dbfa6e0935dee
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-11-25 10:35:34 +0100

    chore: removed wrong path

--- ts/utils/deepLinkUtils.ts
+++ ts/utils/deepLinkUtils.ts
@@ -1,5 +1,3 @@
-import { IO_UNIVERSAL_LINK_PREFIX } from "./navigation";
-
 // Internal deep link paths that require wallet update
 const WALLET_UPDATE_PATHS = ["ioit://cgn-details/detail", "ioit://main/wallet"];
 
@@ -8,12 +6,5 @@ const WALLET_UPDATE_PATHS = ["ioit://cgn-details/detail", "ioit://main/wallet"];
  * - External Universal Links from web
  * - Specific internal paths that need wallet refresh
  */
-export const shouldTriggerWalletUpdate = (url: string): boolean => {
-  // External Universal Links always trigger wallet update
-  if (url.startsWith(IO_UNIVERSAL_LINK_PREFIX)) {
-    return true;
-  }
-
-  // Check if internal path requires wallet update
-  return WALLET_UPDATE_PATHS.some(path => url.startsWith(path));
-};
+export const shouldTriggerWalletUpdate = (url: string): boolean =>
+  WALLET_UPDATE_PATHS.some(path => url.startsWith(path));
