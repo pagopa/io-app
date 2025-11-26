8e7d1e5664baa56f0d2d9fcc7412cb9a79974af2
Damiano Plebani <damiano.plebani@pagopa.it>
2025-11-26 10:17:44 +0100

    Refactor `CieLoginConfigScreen`

--- ts/features/authentication/login/cie/components/CieLoginConfigScreenContent.tsx
+++ ts/features/authentication/login/cie/components/CieLoginConfigScreenContent.tsx
@@ -8,25 +8,22 @@ const CieLoginConfigScreenContent = () => {
   const dispatch = useIODispatch();
   const useCieUat = useIOSelector(isCieLoginUatEnabledSelector);
   return (
-    <>
-      <ListItemCheckbox
-        icon="warningFilled"
-        selected={useCieUat}
-        onValueChange={newValue => {
-          if (newValue) {
-            dispatch(cieLoginEnableUat());
-          } else {
-            dispatch(cieLoginDisableUat());
-          }
-        }}
-        value={`Abilita endpoint di collaudo (${CieEntityIds.DEV})`}
-        description={
-          "Questa opzione serve agli sviluppatori, per testare la login con CIE."
+    <ListItemCheckbox
+      icon="warningFilled"
+      selected={useCieUat}
+      onValueChange={newValue => {
+        if (newValue) {
+          dispatch(cieLoginEnableUat());
+        } else {
+          dispatch(cieLoginDisableUat());
         }
-        accessibilityLabel={""}
-      />
-      <Divider />
-    </>
+      }}
+      value={`Abilita endpoint di collaudo (${CieEntityIds.DEV})`}
+      description={
+        "Questa opzione serve agli sviluppatori, per testare la login con CIE."
+      }
+      accessibilityLabel={""}
+    />
   );
 };
 
