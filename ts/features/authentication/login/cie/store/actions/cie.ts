1dd7949a4ddd94411ac50d46aff07add26d0c970
Andrea Piai <andrea.piai@pagopa.it>
2025-11-26 16:15:49 +0100

    Removes unused hasApiLevelSupport, hasNFCFeature and readingEvent properties

--- ts/features/authentication/login/cie/store/actions/cie.ts
+++ ts/features/authentication/login/cie/store/actions/cie.ts
@@ -9,18 +9,6 @@ import {
   createStandardAction
 } from "typesafe-actions";
 
-export const hasApiLevelSupport = createAsyncAction(
-  "CIE_HAS_API_LEVEL_REQUEST",
-  "CIE_HAS_API_LEVEL_SUCCESS",
-  "CIE_HAS_API_LEVEL_FAILURE"
-)<void, boolean, Error>();
-
-export const hasNFCFeature = createAsyncAction(
-  "CIE_HAS_NFC_FEATURE_REQUEST",
-  "CIE_HAS_NFC_FEATURE_SUCCESS",
-  "CIE_HAS_NFC_FEATURE_FAILURE"
-)<void, boolean, Error>();
-
 export const cieIsSupported = createAsyncAction(
   "CIE_IS_SUPPORTED_REQUEST",
   "CIE_IS_SUPPORTED_SUCCESS",
@@ -32,12 +20,6 @@ export const nfcIsEnabled = createAsyncAction(
   "NFC_IS_ENABLED_SUCCESS",
   "NFC_IS_ENABLED_FAILURE"
 )<void, boolean, Error>();
-
-export const updateReadingState = createAsyncAction(
-  "UPDATE_READING_STATE_REQUEST",
-  "UPDATE_READING_STATE_SUCCESS",
-  "UPDATE_READING_STATE_FAILURE"
-)<void, string, Error>();
 
 export type CieAuthenticationErrorReason = CEvent["event"] | "GENERIC";
 
@@ -51,9 +33,6 @@ export const cieAuthenticationError = createStandardAction(
 )<CieAuthenticationErrorPayload>();
 
 export type CieAuthenticationActions =
-  | ActionType<typeof hasApiLevelSupport>
-  | ActionType<typeof hasNFCFeature>
   | ActionType<typeof cieIsSupported>
   | ActionType<typeof nfcIsEnabled>
-  | ActionType<typeof updateReadingState>
   | ActionType<typeof cieAuthenticationError>;
