6faafd536a3d88f960059c47eb37648095e74354
MonicaRungi-EY <Monica.Rungi@it.ey.com>
2025-11-27 14:07:17 +0100

    removed unused file

--- ts/features/wallet/store/actions/bottomSheet.ts
+++
@@ -1,9 +0,0 @@
-import { ActionType, createStandardAction } from "typesafe-actions";
-
-export const walletSetBottomSheetSurveyVisible = createStandardAction(
-  "WALLET_SET_BOTTOM_SHEET_SURVEY_VISIBLE"
-)<boolean>();
-
-export type WalletBottomSheetActions = ActionType<
-  typeof walletSetBottomSheetSurveyVisible
->;
