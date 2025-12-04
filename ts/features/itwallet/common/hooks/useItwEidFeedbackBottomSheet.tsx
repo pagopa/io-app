9dae463d38fd9fa36ca4bfaff1a258640383c0c0
RiccardoMolinari95 <riccardo95molinari@gmail.com>
2025-12-04 17:38:13 +0100

    feat: add tracking for qualtrics survey

--- ts/features/itwallet/common/hooks/useItwEidFeedbackBottomSheet.tsx
+++ ts/features/itwallet/common/hooks/useItwEidFeedbackBottomSheet.tsx
@@ -1,11 +1,19 @@
 import { Body, IOButton, VStack } from "@pagopa/io-app-design-system";
 import I18n from "i18next";
+import { useMemo, useRef } from "react";
 import { View } from "react-native";
+import { useRoute } from "@react-navigation/native";
 import { useIOBottomSheetModal } from "../../../../utils/hooks/bottomSheet.tsx";
 import { openWebUrl } from "../../../../utils/url.ts";
 import { IT_WALLET_SURVEY_EID_REISSUANCE_FAILURE } from "../utils/constants.ts";
+import {
+  TrackQualtricsSurvey,
+  trackItwSurveyRequestAccepted,
+  trackItwSurveyRequestDeclined
+} from "../../analytics";
 
 type ItwEidFeedbackBottomSheetProps = {
+  surveyPage?: string;
   onPrimaryAction?: () => void;
   onSecondaryAction?: () => void;
 };
@@ -19,6 +27,18 @@ export const useItwEidFeedbackBottomSheet = ({
   onPrimaryAction,
   onSecondaryAction
 }: ItwEidFeedbackBottomSheetProps = {}) => {
+  const { name: routeName } = useRoute();
+  // Prevents a Declined event when the bottom sheet is dismissed after an Accepted action.
+  const skipDeclinedEvent = useRef(false);
+
+  const trackingProps: TrackQualtricsSurvey = useMemo(
+    () => ({
+      survey_id: "confirm_eid_flow_exit",
+      survey_page: routeName
+    }),
+    [routeName]
+  );
+
   const { bottomSheet, present, dismiss } = useIOBottomSheetModal({
     title: I18n.t("features.itWallet.feedback.reissuance.bottomSheet.title"),
     component: (
@@ -38,6 +58,9 @@ export const useItwEidFeedbackBottomSheet = ({
                   "features.itWallet.feedback.reissuance.bottomSheet.primaryAction"
                 )}
                 onPress={() => {
+                  // eslint-disable-next-line functional/immutable-data
+                  skipDeclinedEvent.current = true;
+                  trackItwSurveyRequestAccepted(trackingProps);
                   openWebUrl(IT_WALLET_SURVEY_EID_REISSUANCE_FAILURE);
                   onPrimaryAction?.();
                   dismiss();
@@ -60,7 +83,14 @@ export const useItwEidFeedbackBottomSheet = ({
           </View>
         </VStack>
       </>
-    )
+    ),
+    onDismiss: () => {
+      if (!skipDeclinedEvent.current) {
+        trackItwSurveyRequestDeclined(trackingProps);
+      }
+      // eslint-disable-next-line functional/immutable-data
+      skipDeclinedEvent.current = false;
+    }
   });
 
   return { bottomSheet, present, dismiss };
