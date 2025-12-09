06b58564d1b088b49131e32f3c80de8e711a019a
ChrisMattew <cristian.matteu@spindox.it>
2025-12-09 16:59:58 +0100

    test(validateMandateSaga): add comprehensive tests for mandate validation logic

---
+++ ts/features/pn/aar/saga/__tests__/validateMandateSaga.test.ts
@@ -0,0 +1,260 @@
+import * as E from "fp-ts/lib/Either";
+import { testSaga } from "redux-saga-test-plan";
+import { validateMandateSaga } from "../validateMandateSaga";
+import { AARFlowState, sendAARFlowStates } from "../../utils/stateUtils";
+import {
+  sendAarMockStateFactory,
+  sendAarMockStates
+} from "../../utils/testUtils";
+import { SessionToken } from "../../../../../types/SessionToken";
+import { setAarFlowState } from "../../store/actions";
+import { trackSendAARFailure } from "../../analytics";
+import { isPnTestEnabledSelector } from "../../../../../store/reducers/persistedPreferences";
+import { withRefreshApiCall } from "../../../../authentication/fastLogin/saga/utils";
+import { AARProblemJson } from "../../../../../../definitions/pn/aar/AARProblemJson";
+
+const mockValidatingMandateState = sendAarMockStateFactory.validatingMandate();
+const mockValidatingMandateAction = setAarFlowState(mockValidatingMandateState);
+const aarStatesWithoutValidatingMandate = sendAarMockStates.filter(
+  ({ type }) => type !== mockValidatingMandateState.type
+);
+const sessionToken = "test-session-token" as SessionToken;
+const sessionTokenWithBearer = `Bearer ${sessionToken}` as SessionToken;
+
+const mockAcceptIOMandate = jest.fn();
+
+const getMockKoState = (
+  prevState: AARFlowState,
+  error: AARProblemJson | undefined,
+  reason: string
+): AARFlowState => ({
+  type: "ko",
+  previousState: { ...prevState },
+  ...(error != null && { error }),
+  debugData: {
+    phase: "Validate Mandate",
+    reason
+  }
+});
+
+describe("validateMandateSaga", () => {
+  afterEach(jest.clearAllMocks);
+
+  it.each([false, true])(
+    'should call "acceptIOMandate" with the right values (isSendUATEnvironment: %s)',
+    isSendUATEnvironment => {
+      testSaga(
+        validateMandateSaga,
+        mockAcceptIOMandate,
+        sessionToken,
+        mockValidatingMandateAction
+      )
+        .next()
+        .select(isPnTestEnabledSelector)
+        .next(isSendUATEnvironment);
+
+      const { mandateId, nisData, mrtdData, signedVerificationCode } =
+        mockValidatingMandateState;
+
+      expect(mockAcceptIOMandate).toHaveBeenCalledWith({
+        Bearer: sessionTokenWithBearer,
+        body: {
+          nisData: {
+            nis: nisData.nis,
+            pub_key: nisData.publicKey,
+            sod: nisData.sod
+          },
+          signedNonce: signedVerificationCode,
+          mrtdData
+        },
+        isTest: isSendUATEnvironment,
+        mandateId
+      });
+    }
+  );
+
+  it.each(aarStatesWithoutValidatingMandate)(
+    'should exit early when called with state "$type"',
+    payload => {
+      testSaga(
+        validateMandateSaga,
+        mockAcceptIOMandate,
+        sessionToken,
+        setAarFlowState(payload)
+      )
+        .next()
+        .call(
+          trackSendAARFailure,
+          "Validate Mandate",
+          `Called in wrong state (${payload.type})`
+        )
+        .next()
+        .isDone();
+    }
+  );
+
+  it(`should dispatch "setAarFlowState" with type: "${sendAARFlowStates.fetchingNotificationData}" on a 204`, () => {
+    const successState: AARFlowState = {
+      type: sendAARFlowStates.fetchingNotificationData,
+      iun: mockValidatingMandateState.iun,
+      recipientInfo: { ...mockValidatingMandateState.recipientInfo },
+      mandateId: mockValidatingMandateState.mandateId
+    };
+
+    testSaga(
+      validateMandateSaga,
+      mockAcceptIOMandate,
+      sessionToken,
+      mockValidatingMandateAction
+    )
+      .next()
+      .select(isPnTestEnabledSelector)
+      .next(true)
+      .call(
+        withRefreshApiCall,
+        mockAcceptIOMandate(),
+        mockValidatingMandateAction
+      )
+      .next(
+        E.right({
+          headers: {},
+          status: 204
+        })
+      )
+      .put(setAarFlowState(successState))
+      .next()
+      .isDone();
+  });
+
+  it.each([
+    E.right({
+      status: 500,
+      value: { status: 500, detail: "A detail" } as AARProblemJson
+    }),
+    E.right({
+      status: 404,
+      value: { status: 404, detail: "A detail" } as AARProblemJson
+    }),
+    E.right({
+      status: 418,
+      value: { status: 418, detail: "A detail" } as AARProblemJson
+    })
+  ])("should dispatch a KO state when the response is %o", res => {
+    const error = E.isRight(res) ? res.right.value : undefined;
+    const reason = `HTTP request failed (${
+      E.isRight(res) ? res.right.status : ""
+    } ${E.isRight(res) ? res.right.value.status : ""} A detail)`;
+    testSaga(
+      validateMandateSaga,
+      mockAcceptIOMandate,
+      sessionToken,
+      mockValidatingMandateAction
+    )
+      .next()
+      .select(isPnTestEnabledSelector)
+      .next(true)
+      .call(
+        withRefreshApiCall,
+        mockAcceptIOMandate(),
+        mockValidatingMandateAction
+      )
+      .next(res)
+      .call(trackSendAARFailure, "Validate Mandate", reason)
+      .next()
+      .put(
+        setAarFlowState(
+          getMockKoState(mockValidatingMandateState, error, reason)
+        )
+      )
+      .next()
+      .isDone();
+  });
+
+  it('should call "trackSendAARFailure" with "Fast login expiration" and stop on 401', () => {
+    testSaga(
+      validateMandateSaga,
+      mockAcceptIOMandate,
+      sessionToken,
+      mockValidatingMandateAction
+    )
+      .next()
+      .select(isPnTestEnabledSelector)
+      .next(true)
+      .call(
+        withRefreshApiCall,
+        mockAcceptIOMandate(),
+        mockValidatingMandateAction
+      )
+      .next(
+        E.right({
+          headers: {},
+          status: 401,
+          value: {}
+        })
+      )
+      .call(trackSendAARFailure, "Validate Mandate", "Fast login expiration")
+      .next()
+      .isDone();
+  });
+
+  it("should dispatch KO state on exception throw", () => {
+    testSaga(
+      validateMandateSaga,
+      mockAcceptIOMandate,
+      sessionToken,
+      mockValidatingMandateAction
+    )
+      .next()
+      .select(isPnTestEnabledSelector)
+      .next(true)
+      .call(
+        withRefreshApiCall,
+        mockAcceptIOMandate(),
+        mockValidatingMandateAction
+      )
+      .throw(new Error())
+      .call(trackSendAARFailure, "Validate Mandate", "An error was thrown ()")
+      .next()
+      .put(
+        setAarFlowState(
+          getMockKoState(
+            mockValidatingMandateState,
+            undefined,
+            `An error was thrown ()`
+          )
+        )
+      )
+      .next()
+      .isDone();
+  });
+  it("should dispatch KO state on a decoding failure", () => {
+    testSaga(
+      validateMandateSaga,
+      mockAcceptIOMandate,
+      sessionToken,
+      mockValidatingMandateAction
+    )
+      .next()
+      .select(isPnTestEnabledSelector)
+      .next(true)
+      .call(
+        withRefreshApiCall,
+        mockAcceptIOMandate(),
+        mockValidatingMandateAction
+      )
+      .next(E.left([]))
+      .call(trackSendAARFailure, "Validate Mandate", "Decoding failure ()")
+      .next()
+      .put(
+        setAarFlowState(
+          getMockKoState(
+            mockValidatingMandateState,
+            undefined,
+            `Decoding failure ()`
+          )
+        )
+      )
+      .next()
+      .isDone();
+  });
+});
