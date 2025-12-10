7a0622b7ed0d289fad0cd03e496a92c59a3e1585
Damiano Plebani <damiano.plebani@pagopa.it>
2025-12-10 17:42:29 +0100

    Remove `SendAARLoadingComponent` component

--- ts/features/pn/aar/components/SendAARLoadingComponent.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingScreenContent from "../../../../components/screens/LoadingScreenContent";
-
-type Props = {
-  contentTitle: string;
-};
-
-export const SendAARLoadingComponent = ({ contentTitle }: Props) => (
-  <LoadingScreenContent title={contentTitle} testID="LoadingScreenContent" />
-);
