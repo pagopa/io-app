cdf5ba7560d7a836414c64cb4cf852caff9088dc
Frederik Batuna <gobi301@gmail.com>
2025-12-10 10:06:37 +0100

    rm: unused type

--- ts/features/services/common/navigation/params.ts
+++ ts/features/services/common/navigation/params.ts
@@ -1,10 +1,9 @@
 import { ServiceDetailsScreenRouteParams } from "../../details/screens/ServiceDetailsScreen";
-import { ServiceFavouritesScreenRouteParams } from "../../serviceFavourites/screens/ServiceFavouritesScreen";
 import { InstitutionServicesScreenRouteParams } from "../../institution/screens/InstitutionServicesScreen";
 import { SERVICES_ROUTES } from "./routes";
 
 export type ServicesParamsList = {
   [SERVICES_ROUTES.SERVICE_DETAIL]: ServiceDetailsScreenRouteParams;
   [SERVICES_ROUTES.INSTITUTION_SERVICES]: InstitutionServicesScreenRouteParams;
-  [SERVICES_ROUTES.FAVOURITE_SERVICES]: ServiceFavouritesScreenRouteParams;
+  [SERVICES_ROUTES.FAVOURITE_SERVICES]: undefined;
 };
--- ts/features/services/serviceFavourites/screens/ServiceFavouritesScreen.tsx
+++ ts/features/services/serviceFavourites/screens/ServiceFavouritesScreen.tsx
@@ -2,8 +2,6 @@ import I18n from "i18next";
 import { ContentWrapper, H3, useIOTheme } from "@pagopa/io-app-design-system";
 import { useHeaderSecondLevel } from "../../../../hooks/useHeaderSecondLevel";
 
-export type ServiceFavouritesScreenRouteParams = undefined;
-
 export function ServiceFavouritesScreen() {
   const theme = useIOTheme();
   useHeaderSecondLevel({
