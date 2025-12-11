0042e5a7ff15cf3ed5f140a51174ead9633da294
Frederik Batuna <gobi301@gmail.com>
2025-12-11 10:02:26 +0100

    mv: ListEmptyComponent

--- ts/features/services/favouriteServices/screens/FavouriteServicesScreen.tsx
+++ ts/features/services/favouriteServices/screens/FavouriteServicesScreen.tsx
@@ -1,5 +1,5 @@
 import I18n from "i18next";
-import { Fragment, useMemo } from "react";
+import { Fragment, useCallback, useMemo } from "react";
 import { IOListViewWithLargeHeader } from "../../../../components/ui/IOListViewWithLargeHeader";
 import { OperationResultScreenContent } from "../../../../components/screens/OperationResultScreenContent";
 import { useIONavigation } from "../../../../navigation/params/AppParamsList";
@@ -8,17 +8,35 @@ import { useIOSelector } from "../../../../store/hooks";
 import { favouriteServicesSelector } from "../store/selectors";
 
 export function FavouriteServicesScreen() {
+  const navigation = useIONavigation();
   const favouriteServices = useIOSelector(favouriteServicesSelector);
   const favouriteServicesList = useMemo(
     () => Object.values(favouriteServices),
     [favouriteServices]
   );
 
+  const ListEmptyComponent = useCallback(
+    () => (
+      <OperationResultScreenContent
+        pictogram="empty"
+        title={I18n.t("services.favouriteServices.emptyList.title")}
+        subtitle={I18n.t("services.favouriteServices.emptyList.subtitle")}
+        action={{
+          label: I18n.t("services.favouriteServices.emptyList.searchAction"),
+          icon: "search",
+          onPress() {
+            navigation.navigate(SERVICES_ROUTES.SEARCH);
+          }
+        }}
+      />
+    ),
+    [navigation]
+  );
+
   return (
     <IOListViewWithLargeHeader
       headerActionsProp={{
         showHelp: true
-        // TODO QUESTION, should show options icon button? if yes, to what screen it should navigate?
       }}
       title={{
         label: I18n.t("services.favouriteServices.title")
@@ -27,25 +45,6 @@ export function FavouriteServicesScreen() {
       data={favouriteServicesList}
       renderItem={() => <Fragment />}
       ListEmptyComponent={ListEmptyComponent}
-    />
-  );
-}
-
-// TODO QUESTION, should go in own file?
-function ListEmptyComponent() {
-  const navigation = useIONavigation();
-  return (
-    <OperationResultScreenContent
-      pictogram="empty"
-      title={I18n.t("services.favouriteServices.emptyList.title")}
-      subtitle={I18n.t("services.favouriteServices.emptyList.subtitle")}
-      action={{
-        label: I18n.t("services.favouriteServices.emptyList.searchAction"),
-        icon: "search", // TODO QUESTION, icon appears on the left with this code, it is on the right in the design
-        onPress() {
-          navigation.navigate(SERVICES_ROUTES.SEARCH);
-        }
-      }}
     />
   );
 }
