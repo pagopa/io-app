1896933f2cfc915f25a801f5f35bea733e8ac8c0
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2025-12-16 12:20:04 +0100

    update dependency

--- ts/features/services/favouriteServices/screens/FavouriteServicesScreen.tsx
+++ ts/features/services/favouriteServices/screens/FavouriteServicesScreen.tsx
@@ -48,7 +48,7 @@ export const FavouriteServicesScreen = () => {
         }}
       />
     );
-  }, [present, sortedServices]);
+  }, [present, sortedServices.length]);
 
   const ListEmptyComponent = useCallback(
     () => (
