3e80be54cd4798af2770d9149e76d1329f86bfb0
ChrisMattew <cristian.matteu@spindox.it>
2025-12-17 12:53:29 +0100

    feat: add tests for useIsNfcFeatureEnabled hook and enhance NFC feature checks

--- ts/features/pn/aar/hooks/useIsNfcFeatureEnabled.ts
+++ ts/features/pn/aar/hooks/useIsNfcFeatureEnabled.ts
@@ -1,23 +1,35 @@
 import { useCallback, useState } from "react";
-import * as cieUtils from "../../../authentication/login/cie/utils/cie";
+import cieSdk from "@pagopa/react-native-cie";
+import { constNull } from "fp-ts/lib/function";
+import { Platform } from "react-native";
 
 export const useIsNfcFeatureEnabled = () => {
   const [isChecking, setIsChecking] = useState(false);
 
-  const isNfcEnabled = useCallback(async () => {
-    setIsChecking(true);
+  const isNfcEnabled = useCallback(async (): Promise<boolean> => {
+    if (Platform.OS === "android") {
+      setIsChecking(true);
 
-    return cieUtils
-      .isNfcEnabled()
-      .then(res => res)
-      .finally(() => {
-        setIsChecking(false);
-      });
+      return cieSdk
+        .isNFCEnabled()
+        .catch(() => false)
+        .finally(() => {
+          setIsChecking(false);
+        });
+    }
+
+    return Promise.resolve(true);
+  }, []);
+
+  const openNFCSettings = useCallback(() => {
+    if (Platform.OS === "android") {
+      void cieSdk.openNFCSettings().catch(constNull);
+    }
   }, []);
 
   return {
     isChecking,
     isNfcEnabled,
-    openNFCSettings: cieUtils.openNFCSettings
+    openNFCSettings
   };
 };
