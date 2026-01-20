31ce80acd938a2aa716f1e42721660896fc36b95
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-01-20 15:46:33 +0100

    refactor: [IOBP-000] Replaced deprecated loader components (#7779)
    ## Short description
    This pull request updates loading screens in the EYCA activation and
    IDPay onboarding flows to use a new, standardized `LoadingScreenContent`
    component instead of the older `LoadingComponent`.
    
    ## List of changes proposed in this pull request
    - Replaced `LoadingComponent` with `LoadingScreenContent` and
    `IdPayLoadingScreen`
    
    ## How to test
    Ensure that now available spacing is aligned with app spacing
    
    ## Preview
    | Master | IOBP-000 |
    |--------|--------|
    | <img width="485" height="955" alt="Screenshot 2026-01-19 at 12 23 43"
    src="https://github.com/user-attachments/assets/24b3275b-c94e-4458-bce8-a24c805c790d"
    /> | <img width="474" height="953" alt="Screenshot 2026-01-19 at 12 23
    04"
    src="https://github.com/user-attachments/assets/d3e90f6c-8dbb-4ad5-8508-780d236b8aa8"
    /> |
    
    Co-authored-by: Pawel <33898715+soixdev91@users.noreply.github.com>

--- ts/features/idpay/onboarding/screens/IdPayLoadingScreen.tsx
+++ ts/features/idpay/onboarding/screens/IdPayLoadingScreen.tsx
@@ -2,8 +2,8 @@ import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/function";
 import I18n from "i18next";
 import { useEffect } from "react";
+import LoadingScreenContent from "../../../../components/screens/LoadingScreenContent";
 import { useOnFirstRender } from "../../../../utils/hooks/useOnFirstRender";
-import LoadingComponent from "../../../fci/components/LoadingComponent";
 import { isLoadingSelector } from "../../common/machine/selectors";
 import { trackIDPayOnboardingConversionRate } from "../analytics";
 import { IdPayOnboardingMachineContext } from "../machine/provider";
@@ -42,9 +42,9 @@ const IdPayLoadingScreen = () => {
   );
 
   return (
-    <LoadingComponent
-      captionTitle={I18n.t("idpay.onboarding.loading.title")}
-      captionSubtitle={I18n.t("idpay.onboarding.loading.subtitle")}
+    <LoadingScreenContent
+      title={I18n.t("idpay.onboarding.loading.title")}
+      subtitle={I18n.t("idpay.onboarding.loading.subtitle")}
     />
   );
 };
