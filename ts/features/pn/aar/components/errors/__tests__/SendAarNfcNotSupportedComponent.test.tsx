856c816b75016e2fd998f2f5fa354eb947d37f3e
ChrisMattew <cristian.matteu@spindox.it>
2026-01-21 15:36:03 +0100

    feat(SendAarNfcNotSupportedComponent): add events tracking strategy and update unit tests accordingly

--- ts/features/pn/aar/components/errors/__tests__/SendAarNfcNotSupportedComponent.test.tsx
+++ ts/features/pn/aar/components/errors/__tests__/SendAarNfcNotSupportedComponent.test.tsx
@@ -8,9 +8,20 @@ import { SendAarNfcNotSupportedComponent } from "../SendAarNfcNotSupportedCompon
 import * as FLOW_MANAGER from "../../../hooks/useSendAarFlowManager";
 import * as IO_NAV from "../../../../../../navigation/params/AppParamsList";
 import * as URL_UTILS from "../../../../../../utils/url";
+import {
+  trackSendAarNotificationOpeningNfcNotSupported,
+  trackSendAarNotificationOpeningNfcNotSupportedClosure,
+  trackSendAarNotificationOpeningNfcNotSupportedInfo
+} from "../../../analytics";
 
 const terminateFlowMock = jest.fn();
 const setOptionsMock = jest.fn();
+
+jest.mock("../../../analytics", () => ({
+  trackSendAarNotificationOpeningNfcNotSupported: jest.fn(),
+  trackSendAarNotificationOpeningNfcNotSupportedClosure: jest.fn(),
+  trackSendAarNotificationOpeningNfcNotSupportedInfo: jest.fn()
+}));
 
 describe("SendAarNfcNotSupportedComponent", () => {
   beforeEach(() => {
@@ -33,6 +44,14 @@ describe("SendAarNfcNotSupportedComponent", () => {
     expect(setOptionsMock).toHaveBeenCalledWith({ headerShown: true });
   });
 
+  it('should call "trackSendAarNotificationOpeningNfcNotSupported" on component mount', () => {
+    renderComponent();
+
+    expect(
+      trackSendAarNotificationOpeningNfcNotSupported
+    ).toHaveBeenCalledTimes(1);
+  });
+
   it("calls terminateFlow when close icon is pressed", () => {
     jest.spyOn(FLOW_MANAGER, "useSendAarFlowManager").mockImplementation(
       () =>
@@ -42,17 +61,35 @@ describe("SendAarNfcNotSupportedComponent", () => {
     );
     const { getByTestId } = renderComponent();
     const closeButton = getByTestId("close-x");
+
     expect(terminateFlowMock).toHaveBeenCalledTimes(0);
+    expect(
+      trackSendAarNotificationOpeningNfcNotSupportedClosure
+    ).toHaveBeenCalledTimes(0);
+
     fireEvent.press(closeButton);
+
     expect(terminateFlowMock).toHaveBeenCalledTimes(1);
+    expect(
+      trackSendAarNotificationOpeningNfcNotSupportedClosure
+    ).toHaveBeenCalledTimes(1);
   });
   it("opens help center url when help center cta is pressed", () => {
     const webUrlSpy = jest.spyOn(URL_UTILS, "openWebUrl");
     const { getByTestId } = renderComponent();
     const helpCenterCta = getByTestId("help-center-cta");
+
     expect(webUrlSpy).toHaveBeenCalledTimes(0);
+    expect(
+      trackSendAarNotificationOpeningNfcNotSupportedInfo
+    ).toHaveBeenCalledTimes(0);
+
     fireEvent.press(helpCenterCta);
+
     expect(webUrlSpy).toHaveBeenCalledTimes(1);
+    expect(
+      trackSendAarNotificationOpeningNfcNotSupportedInfo
+    ).toHaveBeenCalledTimes(1);
   });
 });
 
