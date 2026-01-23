80beadf3946b3b5137f1ebdd9baf00158aacbfaa
ChrisMattew <cristian.matteu@spindox.it>
2026-01-23 11:15:29 +0100

    feat: add tracking functions for CIE reading closure alerts and related unit tests

--- ts/features/pn/aar/analytics/index.ts
+++ ts/features/pn/aar/analytics/index.ts
@@ -4,6 +4,8 @@ import { buildEventProperties } from "../../../../utils/analytics";
 import { SendUserType } from "../../../pushNotifications/analytics";
 import { SendAARFailurePhase } from "../utils/stateUtils";
 
+export type SendAarScreen = "CIE_PREPARATION" | "NFC_ACTIVATION";
+
 export const trackSendQRCodeScanRedirect = () => {
   const eventName = "SEND_QRCODE_SCAN_REDIRECT";
   const properties = buildEventProperties("UX", "screen_view");
@@ -212,5 +214,29 @@ export const trackSendAarMandateCiePreparation = () => {
 export const trackSendAarMandateCiePreparationContinue = () => {
   const eventName = "SEND_MANDATE_CIE_PREPARATION_CONTINUE";
   const eventProps = buildEventProperties("UX", "action");
+  void mixpanelTrack(eventName, eventProps);
+};
+
+export const trackSendAarMandateCieReadingClosureAlert = (
+  screen: SendAarScreen
+) => {
+  const eventName = "SEND_MANDATE_CIE_READING_CLOSURE_ALERT";
+  const eventProps = buildEventProperties("UX", "screen_view", { screen });
+  void mixpanelTrack(eventName, eventProps);
+};
+
+export const trackSendAarMandateCieReadingClosureAlertAccepted = (
+  screen: SendAarScreen
+) => {
+  const eventName = "SEND_MANDATE_CIE_READING_CLOSURE_ALERT_ACCEPTED";
+  const eventProps = buildEventProperties("UX", "exit", { screen });
+  void mixpanelTrack(eventName, eventProps);
+};
+
+export const trackSendAarMandateCieReadingClosureAlertContinue = (
+  screen: SendAarScreen
+) => {
+  const eventName = "SEND_MANDATE_CIE_READING_CLOSURE_ALERT_CONTINUE";
+  const eventProps = buildEventProperties("UX", "action", { screen });
   void mixpanelTrack(eventName, eventProps);
 };
