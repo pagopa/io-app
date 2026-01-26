0d10816b1a13ebbf7d69c78ec94d65796c484d1b
Federico Mastrini <federicomastrini93@gmail.com>
2026-01-26 12:09:35 +0100

    add mixpanel super properties

--- ts/features/itwallet/analytics/saga/index.ts
+++ ts/features/itwallet/analytics/saga/index.ts
@@ -1,4 +1,4 @@
-import { fork, select, takeEvery } from "typed-redux-saga/macro";
+import { call, fork, select, takeEvery } from "typed-redux-saga/macro";
 import { SagaIterator } from "redux-saga";
 import { GlobalState } from "../../../../store/reducers/types";
 import { updateItwAnalyticsProperties } from "../properties/propertyUpdaters";
@@ -6,6 +6,8 @@ import {
   itwCredentialsStore,
   itwCredentialsRemove
 } from "../../credentials/store/actions";
+import { registerSuperProperties } from "../../../../mixpanel.ts";
+import { getNfcAntennaInfo } from "../../../../utils/nfc.ts";
 import {
   handleCredentialStoredAnalytics,
   handleCredentialRemovedAnalytics
@@ -26,9 +28,30 @@ export function* watchItwAnalyticsSaga(): SagaIterator {
 export function* syncItwAnalyticsProperties() {
   const state: GlobalState = yield* select();
   updateItwAnalyticsProperties(state);
+
+  // TODO remove this fork when NFC antenna info tracking is not needed anymore
+  yield* fork(handleNfcAntennaInfoTracking);
 }
 
 export function* watchItwCredentialsAnalyticsSaga(): SagaIterator {
   yield* takeEvery(itwCredentialsStore, handleCredentialStoredAnalytics);
   yield* takeEvery(itwCredentialsRemove, handleCredentialRemovedAnalytics);
+}
+
+/**
+ * Tracks NFC antenna information for discovery and debugging purposes.
+ * TODO remove this function when NFC antenna info tracking is not needed anymore
+ */
+export function* handleNfcAntennaInfoTracking() {
+  try {
+    const { deviceHeight, deviceWidth, availableNfcAntennas } = yield* call(
+      getNfcAntennaInfo
+    );
+    registerSuperProperties({
+      NFC_ANTENNA_HAS_DEVICE_INFO: deviceHeight !== 0 && deviceWidth !== 0,
+      NFC_AVAILABLE_ANTENNAS: availableNfcAntennas.length
+    });
+  } catch (failure) {
+    registerSuperProperties({ NFC_ANTENNA_READ_FAILURE: failure });
+  }
 }
