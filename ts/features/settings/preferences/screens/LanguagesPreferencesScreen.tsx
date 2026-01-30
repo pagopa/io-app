a543ecf6ddfde9b2a3219971b92636144c692f4a
Damiano Plebani <damiano.plebani@pagopa.it>
2026-01-30 11:36:55 +0100

    Remove banner

--- ts/features/settings/preferences/screens/LanguagesPreferencesScreen.tsx
+++ ts/features/settings/preferences/screens/LanguagesPreferencesScreen.tsx
@@ -1,10 +1,8 @@
 import {
-  Banner,
   ListItemHeader,
   RadioGroup,
   RadioItem,
   useIOToast,
-  VSpacer,
   VStack
 } from "@pagopa/io-app-design-system";
 import * as pot from "@pagopa/ts-commons/lib/pot";
@@ -13,7 +11,6 @@ import { pipe } from "fp-ts/lib/function";
 import I18n from "i18next";
 import _ from "lodash";
 import {
-  createRef,
   useCallback,
   useContext,
   useEffect,
@@ -34,16 +31,13 @@ import {
 } from "../../../../store/actions/persistedPreferences";
 
 import { useIODispatch, useIOSelector } from "../../../../store/hooks";
-import { sectionStatusByKeySelector } from "../../../../store/reducers/backendStatus/sectionStatus";
 import { preferredLanguageSelector } from "../../../../store/reducers/persistedPreferences";
 import { ContextualHelpPropsMarkdown } from "../../../../utils/contextualHelp";
 import { usePrevious } from "../../../../utils/hooks/usePrevious";
 import {
   fromLocaleToPreferredLanguage,
-  fromPreferredLanguageToLocale,
-  getFullLocale
+  fromPreferredLanguageToLocale
 } from "../../../../utils/locale";
-import { openWebUrl } from "../../../../utils/url";
 import { profileUpsert } from "../../common/store/actions";
 import { profileSelector } from "../../common/store/selectors";
 
@@ -59,7 +53,6 @@ const contextualHelpMarkdown: ContextualHelpPropsMarkdown = {
 type AppLocaleId = `app-locale-${AppLocale}`;
 
 const LanguagesPreferencesScreen = () => {
-  const viewRef = createRef<View>();
   const dispatch = useIODispatch();
   const toast = useIOToast();
   const selectedLanguage = useRef<string | undefined>(undefined);
@@ -67,10 +60,6 @@ const LanguagesPreferencesScreen = () => {
   const { showModal } = useContext(LightModalContext);
   const profile = useIOSelector(profileSelector, _.isEqual);
   const prevProfile = usePrevious(profile);
-  const bannerInfoSelector = useIOSelector(
-    sectionStatusByKeySelector("favourite_language")
-  );
-  const isBannerVisible = bannerInfoSelector && bannerInfoSelector.is_visible;
   const preferredLanguageSelect = useIOSelector(
     preferredLanguageSelector,
     _.isEqual
@@ -290,25 +279,6 @@ const LanguagesPreferencesScreen = () => {
               selectedItem={selectedItem}
               onPress={onLanguageSelected}
             />
-            {isBannerVisible && (
-              <>
-                <VSpacer />
-                <Banner
-                  ref={viewRef}
-                  color="neutral"
-                  content={bannerInfoSelector.message[getFullLocale()]}
-                  pictogramName="charity"
-                  action={I18n.t(
-                    "profile.preferences.list.preferred_language.banner.button"
-                  )}
-                  onPress={() =>
-                    openWebUrl(
-                      bannerInfoSelector.web_url?.[getFullLocale()] || ""
-                    )
-                  }
-                />
-              </>
-            )}
           </View>
 
           <View>
