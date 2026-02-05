bbdee5461be1e3e0a51869b125bcb20d26d1ce5e
Cristiano Tofani <cristiano.tofani@pagopa.it>
2026-02-05 10:50:11 +0100

    forces disable autolink on local env for cie package

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,14 @@
 // "rnpm" is deprecated and support for it will be removed in next major version of the CLI.
 // Migration guide https://github.com/react-native-community/cli/blob/master/docs/configuration.md
+const shouldExcludeInternalModule = process.env.NO_INTERNAL_MODULE === '1';
 
 // eslint-disable-next-line functional/immutable-data
 module.exports = {
   dependencies: {
     "@pagopa/react-native-cie": {
       platforms: {
-        android: null // disable Android platform, other platforms will still autolink if provided
+        android: null, // disable Android platform, other platforms will still autolink if provided
+        ios: shouldExcludeInternalModule ? null : {}
       }
     }
   },
