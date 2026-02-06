5f93b0bb15a8e846d973aadc0b5723e93c9369cd
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-02-06 11:36:30 +0100

    chore: removed spread operation

--- ts/features/payments/details/screens/PaymentsMethodDetailsScreen.tsx
+++ ts/features/payments/details/screens/PaymentsMethodDetailsScreen.tsx
@@ -73,7 +73,7 @@ const PaymentsMethodDetailsScreen = () => {
     return (
       <>
         <PaymentsMethodDetailsBaseScreenComponent
-          card={{ ...cardProps }}
+          card={cardProps}
           headerTitle={headerTitle}
         >
           <WalletDetailsPaymentMethodFeatures paymentMethod={paymentMethod} />
