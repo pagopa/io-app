815ba1c36e79b46a12ada7dd2b2efad70890708e
Federico Mastrini <federicomastrini93@gmail.com>
2026-02-20 11:03:03 +0100

    chore: remove unused saga

--- ts/features/tour/sagas/index.ts
+++ ts/features/tour/sagas/index.ts
@@ -1,10 +1,6 @@
 import { put, select, takeEvery } from "typed-redux-saga/macro";
 import { getType } from "typesafe-actions";
-import {
-  completeTourAction,
-  nextTourStepAction,
-  startTourAction
-} from "../store/actions";
+import { completeTourAction, nextTourStepAction } from "../store/actions";
 import {
   activeGroupIdSelector,
   activeStepIndexSelector,
@@ -21,18 +17,6 @@ function* handleNextStep() {
   }
 }
 
-function* handleStartTour(action: ReturnType<typeof startTourAction>) {
-  const completed: ReadonlyArray<string> = yield* select(
-    state => state.features.tour.completed
-  );
-  if (completed.includes(action.payload.groupId)) {
-    // Already completed, the reducer already no-ops but this
-    // prevents any downstream effects
-    return;
-  }
-}
-
 export function* watchTourSaga() {
   yield* takeEvery(getType(nextTourStepAction), handleNextStep);
-  yield* takeEvery(getType(startTourAction), handleStartTour);
 }
