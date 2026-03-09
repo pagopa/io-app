069879b6704a65537b5b585429d8cd0ad96ac071
Gianluca Spada <gianluca.spada@spindox.it>
2026-03-09 11:56:50 +0100

    chore: add more comments for IoWallet

--- ts/features/itwallet/common/utils/itwIoWallet.ts
+++ ts/features/itwallet/common/utils/itwIoWallet.ts
@@ -2,13 +2,21 @@ import { IoWallet, ItwVersion } from "@pagopa/io-react-native-wallet";
 import { useIOSelector } from "../../../../store/hooks";
 import { selectItwSpecsVersion } from "../store/selectors/environment";
 
+/**
+ * This object contains all IoWallet instances that have been created, one per specs version.
+ * It works as a registry of singleton instances, that are lazily created once and then reused.
+ *
+ * To access one of the instances it is required to use {@link getIoWallet} or {@link useIoWallet}.
+ */
 const instances: {
   [V in ItwVersion]?: IoWallet;
 } = {};
 
 /**
  * Convenience hook to get the {@link IoWallet} instance matching the current IT-Wallet specs version.
- * @returns IoWallet
+ * @returns IoWallet instance
+ * @example
+ * const ioWallet = useIoWallet()
  */
 export const useIoWallet = () => {
   const itwSpecsVersion = useIOSelector(selectItwSpecsVersion);
@@ -18,7 +26,9 @@ export const useIoWallet = () => {
 /**
  * Convenience function to get the {@link IoWallet} instance matching the provided IT-Wallet specs version.
  * @param version IT-Wallet version
- * @returns IoWallet
+ * @returns IoWallet instance
+ * @example
+ * const ioWallet = getIoWallet("1.3.3")
  */
 export function getIoWallet(version: ItwVersion) {
   // Lazy initialization
