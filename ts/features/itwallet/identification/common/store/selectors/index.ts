b86063dc7159f9e988c54dfe11147f644464e405
MonicaRungi-EY <Monica.Rungi@it.ey.com>
2026-03-11 11:33:17 +0100

    remove comment

--- ts/features/itwallet/identification/common/store/selectors/index.ts
+++ ts/features/itwallet/identification/common/store/selectors/index.ts
@@ -1,6 +1,5 @@
 import * as pot from "@pagopa/ts-commons/lib/pot";
 import { GlobalState } from "../../../../../../store/reducers/types";
 
-// TODO: comment this for test purposes
 export const itwHasNfcFeatureSelector = (state: GlobalState) =>
   pot.getOrElse(state.features.itWallet.identification.hasNfcFeature, false);
