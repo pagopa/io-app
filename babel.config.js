91e3aff214935da4ddbe90f899fd64023940235c
Cristiano Tofani <cristiano.tofani@pagopa.it>
2026-03-25 10:06:25 +0100

    fixes babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable functional/immutable-data */
 module.exports = {
-  presets: ["module:@react-native/babel-preset"],
+  presets: ['babel-preset-expo'],
   plugins: [
     [
       "module-resolver",
