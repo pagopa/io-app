0f244e15daa26eada5e156450017d42949a3ec6d
Alice Di Rico <alice.dirico@pagopa.it>
2026-04-01 17:50:06 +0200

    refactor: Enhance link tracking logic to include HTTP links in analytics

--- ts/features/linking/analytics/index.ts
+++ ts/features/linking/analytics/index.ts
@@ -1,12 +1,15 @@
-import { isHttpsLink } from "../../../components/ui/Markdown/handlers/link";
+import {
+  isHttpLink,
+  isHttpsLink
+} from "../../../components/ui/Markdown/handlers/link";
 import { mixpanelTrack } from "../../../mixpanel";
 import { buildEventProperties } from "../../../utils/analytics";
 import { getUrlBasepath } from "../../../utils/url";
 
 export function trackIOOpenedFromUniversalAppLink(link_id: string) {
   // check if is universal app link (if it is not, it means that it is an
-  // internal link that should not be tracked as universal app link opening)
-  if (isHttpsLink(link_id)) {
+  // deeplink that should not be tracked as universal app link opening)
+  if (isHttpsLink(link_id) || isHttpLink(link_id)) {
     void mixpanelTrack(
       "IO_UNIVERSAL_APP_LINK",
       buildEventProperties("TECH", undefined, {
