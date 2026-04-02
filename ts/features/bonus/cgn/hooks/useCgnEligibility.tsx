6822ab87e3f48c50a48baa630d6b636310cb0987
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-04-02 08:55:14 +0200

    chore: add useCgnEligibility hook

---
+++ ts/features/bonus/cgn/hooks/useCgnEligibility.tsx
@@ -0,0 +1,12 @@
+import * as pot from "@pagopa/ts-commons/lib/pot";
+import { useIOSelector } from "../../../../store/hooks";
+import { profileSelector } from "../../../settings/common/store/selectors";
+import { canAccessCgn } from "../utils/dates";
+
+const useCgnEligibility = () => {
+  const profile = pot.toUndefined(useIOSelector(profileSelector));
+
+  return canAccessCgn(profile?.date_of_birth);
+};
+
+export default useCgnEligibility;
