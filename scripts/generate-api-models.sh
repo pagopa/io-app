decf6b7b3c2a60f480335edcea65aca82caf5065
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-04-02 08:52:29 +0200

    chore: update definition tag

--- scripts/generate-api-models.sh
+++ scripts/generate-api-models.sh
@@ -2,7 +2,7 @@
 
 IO_BACKEND_VERSION=v17.5.2
 # need to change after merge on io-services-metadata
-IO_SERVICES_METADATA_VERSION=1.0.98
+IO_SERVICES_METADATA_VERSION=IEG-2715-cgn-engagement-banner
 # Session manager version
 IO_SESSION_MANAGER_VERSION=1.23.1
 # IO Wallet user function version
