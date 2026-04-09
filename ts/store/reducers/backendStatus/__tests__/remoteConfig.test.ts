1c9f58a544bd8556426374f5697867552afd903c
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-04-09 11:19:04 +0200

    test: cgn discovery banner remote config

--- ts/store/reducers/backendStatus/__tests__/remoteConfig.test.ts
+++ ts/store/reducers/backendStatus/__tests__/remoteConfig.test.ts
@@ -25,7 +25,8 @@ import {
   sendEstimateTimelinesUrlSelector,
   sendShowAbstractSelector,
   sendVisitTheWebsiteUrlSelector,
-  isSendLollipopPlaygroundEnabledSelector
+  isSendLollipopPlaygroundEnabledSelector,
+  isCGNDiscoveryBannerEnabledSelector
 } from "../remoteConfig";
 
 describe("remoteConfig", () => {
@@ -1314,5 +1315,63 @@ describe("isSendLollipopPlaygroundEnabledSelector", () => {
     } as GlobalState;
     const output = isSendLollipopPlaygroundEnabledSelector(state);
     expect(output).toBe(true);
+  });
+});
+
+describe("isCGNDiscoveryBannerEnabledSelector", () => {
+  it("should return false if remoteConfig is not set", () => {
+    const state = {
+      remoteConfig: O.none
+    } as GlobalState;
+    expect(isCGNDiscoveryBannerEnabledSelector(state)).toBe(false);
+  });
+
+  it("should return false if remote version is higher than App version", () => {
+    const state = {
+      remoteConfig: O.some({
+        cgn: {
+          show_cgn_engagement_banner: {
+            min_app_version: {
+              android: "2.0.0.0",
+              ios: "2.0.0.0"
+            }
+          }
+        }
+      })
+    } as GlobalState;
+    jest.spyOn(appVersion, "getAppVersion").mockImplementation(() => "1.0.0.0");
+    expect(isCGNDiscoveryBannerEnabledSelector(state)).toBe(false);
+  });
+
+  it("should return true if remote version is lower or equals than App version", () => {
+    const state = {
+      remoteConfig: O.some({
+        cgn: {
+          show_cgn_engagement_banner: {
+            min_app_version: {
+              android: "1.0.0.0",
+              ios: "1.0.0.0"
+            }
+          }
+        }
+      })
+    } as GlobalState;
+
+    const equalsVersionState = {
+      remoteConfig: O.some({
+        cgn: {
+          show_cgn_engagement_banner: {
+            min_app_version: {
+              android: "2.0.0.0",
+              ios: "2.0.0.0"
+            }
+          }
+        }
+      })
+    } as GlobalState;
+
+    jest.spyOn(appVersion, "getAppVersion").mockImplementation(() => "2.0.0.0");
+    expect(isCGNDiscoveryBannerEnabledSelector(state)).toBe(true);
+    expect(isCGNDiscoveryBannerEnabledSelector(equalsVersionState)).toBe(true);
   });
 });
