d5c3f2ce0c7ac67be0671c39f0509b5ba1041548
Riccardo Sacco <riccardo.sacco@it.ey.com>
2026-04-13 17:31:47 +0200

    chore(IT-Wallet): Update ItwStatus type to include additional credential statuses

--- ts/features/itwallet/analytics/utils/types.ts
+++ ts/features/itwallet/analytics/utils/types.ts
@@ -92,7 +92,14 @@ export type ItwCredentialMixpanelStatus =
   | "verification_expired"
   | "unknown";
 
-export type ItwStatus = "not_active" | "L2" | "L3";
+export type ItwStatus =
+  | "not_active"
+  | "L2"
+  | "L3"
+  | "L2+ (spid_can)"
+  | "L3 (cieid_can)"
+  | "L3 (cieid_pin)"
+  | "L3 (cie_pin)";
 
 // Assuming that the eID status is the same as the PID status
 export type ItwPIDStatus = Extract<
