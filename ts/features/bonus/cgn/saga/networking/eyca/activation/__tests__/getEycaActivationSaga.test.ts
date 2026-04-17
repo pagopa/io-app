6029094e1bff3794960948262b8653b2b455f966
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-04-17 14:43:27 +0200

    test: coverage update

--- ts/features/bonus/cgn/saga/networking/eyca/activation/__tests__/getEycaActivationSaga.test.ts
+++ ts/features/bonus/cgn/saga/networking/eyca/activation/__tests__/getEycaActivationSaga.test.ts
@@ -1,11 +1,15 @@
 import * as E from "fp-ts/lib/Either";
 import { testSaga } from "redux-saga-test-plan";
-import { getGenericError } from "../../../../../../../../utils/errors";
+import {
+  getGenericError
+} from "../../../../../../../../utils/errors";
 import { startTimer } from "../../../../../../../../utils/timer";
+import { withRefreshApiCall } from "../../../../../../../authentication/fastLogin/saga/utils";
 import { cgnEycaActivation } from "../../../../../store/actions/eyca/activation";
 import {
   getActivation,
-  handleEycaActivationSaga
+  handleEycaActivationSaga,
+  handleStartActivation
 } from "../getEycaActivationSaga";
 
 describe("handleEycaActivationSaga", () => {
@@ -66,5 +70,157 @@ describe("handleEycaActivationSaga", () => {
       .put(cgnEycaActivation.success("COMPLETED"))
       .next()
       .isDone();
+  });
+});
+
+describe("getActivation", () => {
+  const data = [
+    {
+      value: "COMPLETED",
+      returnValue: "COMPLETED"
+    },
+    {
+      value: "ERROR",
+      returnValue: "ERROR"
+    },
+    {
+      value: "RUNNING",
+      returnValue: "PROCESSING"
+    },
+    {
+      value: "PENDING",
+      returnValue: "PROCESSING"
+    }
+  ];
+
+  const getEycaActivation = jest.fn();
+  data.forEach(status => {
+    it(`should return right with status ${status.value} on success response`, async () => {
+      const mockResponse = E.right({
+        status: 200,
+        value: {
+          status: status.value
+        }
+      });
+
+      testSaga(getActivation, getEycaActivation)
+        .next()
+        .call(
+          withRefreshApiCall,
+          getEycaActivation({}),
+          cgnEycaActivation.request()
+        )
+        .next(mockResponse)
+        .returns(E.right(status.returnValue))
+        .next()
+        .isDone();
+    });
+  });
+
+  it("should return error on decoding failure", async () => {
+    const wrongResponse = E.right({
+      status: 200,
+      value: {
+        status: "UNKNOWN"
+      }
+    });
+
+    testSaga(getActivation, getEycaActivation)
+      .next()
+      .call(
+        withRefreshApiCall,
+        getEycaActivation({}),
+        cgnEycaActivation.request()
+      )
+      .next(wrongResponse)
+      .returns(
+        E.left(getGenericError(new Error("unexpected status result UNKNOWN")))
+      )
+      .next()
+      .isDone();
+  });
+
+  it("should return right with NOT_FOUND on 404 response", async () => {
+    const mockResponse = E.right({
+      status: 404,
+      value: {}
+    });
+
+    testSaga(getActivation, getEycaActivation)
+      .next()
+      .call(
+        withRefreshApiCall,
+        getEycaActivation({}),
+        cgnEycaActivation.request()
+      )
+      .next(mockResponse)
+      .returns(E.right("NOT_FOUND"))
+      .next()
+      .isDone();
+  });
+
+  it("should return left with error on non-200/404 response", async () => {
+    const mockResponse = E.right({
+      status: 500,
+      value: {}
+    });
+
+    testSaga(getActivation, getEycaActivation)
+      .next()
+      .call(
+        withRefreshApiCall,
+        getEycaActivation({}),
+        cgnEycaActivation.request()
+      )
+      .next(mockResponse)
+      .returns(E.left(getGenericError(new Error("response status 500"))))
+      .next()
+      .isDone();
+  });
+
+  it("should throw an error if API returns a network error", () => {
+    const networkError = new Error("Network error");
+    testSaga(getActivation, getEycaActivation)
+      .next()
+      .call(
+        withRefreshApiCall,
+        getEycaActivation({}),
+        cgnEycaActivation.request()
+      )
+      .throw(networkError)
+      .returns(E.left(getGenericError(networkError)))
+      .next()
+      .isDone();
+  });
+});
+
+describe("handleStartActivation", () => {
+  const startEycaActivation = jest.fn();
+
+  const mapStatus = new Map([
+    [201, "PROCESSING"],
+    [202, "PROCESSING"],
+    [403, "INELIGIBLE"],
+    [409, "ALREADY_ACTIVE"]
+  ]);
+
+  mapStatus.forEach((mappedStatus, backendStatus) => {
+    it(`should return right with ${mappedStatus} when response status is ${backendStatus}`, async () => {
+      const mockResponse = E.right({
+        status: backendStatus
+      });
+
+      testSaga(handleStartActivation, startEycaActivation)
+        .next()
+        .call(
+          withRefreshApiCall,
+          startEycaActivation({}),
+          cgnEycaActivation.request()
+        )
+        .next(mockResponse)
+        .returns(E.right(mappedStatus))
+        .next()
+        .isDone();
+    });
   });
 });
