550d282675038c6a0c39ed079bdc76ade0721f80
Damiano Plebani <damiano.plebani@pagopa.it>
2026-04-17 09:50:00 +0200

    chore: [IOPLT-1758] Update `jest` to `30.x` (#8001)
    ## Short description
    Upgrade `jest` to `30.x` version
    
    ## List of changes proposed in this pull request
    - Remove deprecated packages:
    - `@testing-library/jest-native`, now integrated into React Native
    Testing Library
      - `abortcontroller-polyfill`, already available in Node 22
    -  Upgrade `@testing-library/react-native`
    - Update tests with renamed methods, after their deprecation
    - Update `jestSetup` with the new `crypto` mock, remove redundant mock
    about `device-info`
    - Upgrade all the `jest` packages to the `30.x` version
    - Remove  `--maxWorkers=4` from `test:ci` command
    - CI improvement is negligible, but on local development we went from
    around `235s` to around `122s` for the full test suite
    
    ## How to test
    - Ignore any snapshots that only include an updated URL
    - Check that the updated tests and `jestSetup` do not have any
    regressions
    
    ---------
    
    Co-authored-by: Cristiano Tofani <cristiano.tofani@pagopa.it>

--- ts/features/itwallet/machine/eid/__tests__/machine.test.ts
+++ ts/features/itwallet/machine/eid/__tests__/machine.test.ts
@@ -1123,7 +1123,9 @@ describe("itwEidIssuanceMachine", () => {
 
     // Wallet instance creation failed
 
-    expect(actor.getSnapshot().value).toStrictEqual("TosAcceptance");
+    await waitFor(() =>
+      expect(actor.getSnapshot().value).toStrictEqual("TosAcceptance")
+    );
   });
 
   it("Should fail when creating Wallet Instance", async () => {
@@ -1639,10 +1641,14 @@ describe("itwEidIssuanceMachine", () => {
     await waitFor(() => expect(getWalletAttestation).toHaveBeenCalledTimes(1));
 
     // Wallet Instance Attestation failure triggers cleanupIntegrityKeyTag
-    expect(cleanupIntegrityKeyTag).toHaveBeenCalledTimes(1);
+    await waitFor(() =>
+      expect(cleanupIntegrityKeyTag).toHaveBeenCalledTimes(1)
+    );
 
     // Check that the machine transitions to Failure state
-    expect(actor.getSnapshot().value).toStrictEqual("Failure");
+    await waitFor(() =>
+      expect(actor.getSnapshot().value).toStrictEqual("Failure")
+    );
     expect(actor.getSnapshot().tags).toStrictEqual(new Set());
   });
 
@@ -1761,7 +1767,9 @@ describe("itwEidIssuanceMachine", () => {
     // Wallet instance creation and attestation obtainment success
 
     // Navigate to ipzs privacy screen
-    expect(actor.getSnapshot().value).toStrictEqual("IpzsPrivacyAcceptance");
+    await waitFor(() =>
+      expect(actor.getSnapshot().value).toStrictEqual("IpzsPrivacyAcceptance")
+    );
   });
 
   it("Should navigate to CieWarning screen when 'go-to-cie-warning' event is received", async () => {
