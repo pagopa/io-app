dbf6d53ec617745e5d35f8cee72e07643e325a3e
LazyAfternoons <lazyafternoons@mailbox.org>
2026-04-22 11:37:57 +0200

    fix: typescript issue

--- ts/utils/markdown.ts
+++ ts/utils/markdown.ts
@@ -18,7 +18,7 @@ function customPlugin() {
       ) {
         // eslint-disable-next-line functional/immutable-data
         const data = node.data || (node.data = {});
-        const hast = h(node.name, node.attributes);
+        const hast = h(node.name as string, node.attributes); // Casting required as after migrating to pnpm the Root type gives error as it doesn't containg either tagName or properties.
 
         // eslint-disable-next-line functional/immutable-data
         data.hName = hast.tagName;
