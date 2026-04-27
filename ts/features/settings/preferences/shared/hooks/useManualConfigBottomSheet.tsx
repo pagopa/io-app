bd32793247615fcbca21e7954898f63d199dfb2d
Damiano Plebani <damiano.plebani@pagopa.it>
2026-04-27 08:16:53 +0200

    Replace `IOMarkdownLite` with `Body`

--- ts/features/settings/preferences/shared/hooks/useManualConfigBottomSheet.tsx
+++ ts/features/settings/preferences/shared/hooks/useManualConfigBottomSheet.tsx
@@ -1,14 +1,12 @@
-import {
-  FooterActionsInline,
-  IOMarkdownLite
-} from "@pagopa/io-app-design-system";
+import { Body, FooterActionsInline } from "@pagopa/io-app-design-system";
 import I18n from "i18next";
+import { ReactElement } from "react";
 import { useIOBottomSheetModal } from "../../../../../utils/hooks/bottomSheet";
 
-const ManualConfigConfirm = (): React.ReactElement => (
-  <IOMarkdownLite
-    content={I18n.t("services.optIn.preferences.manualConfig.bottomSheet.body")}
-  />
+const ManualConfigConfirm = (): ReactElement => (
+  <Body>
+    {I18n.t("services.optIn.preferences.manualConfig.bottomSheet.body")}
+  </Body>
 );
 
 export const useManualConfigBottomSheet = (onConfirm: () => void) => {
