d0095d0dba95398864222d3cdbee211dae042713
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-04-29 15:40:58 +0200

    chore: ci testing

--- ts/utils/assert.ts
+++ ts/utils/assert.ts
@@ -1,9 +1,10 @@
+const DEFAULT_ASSERTION_MESSAGE = "Assertion failed";
+
 export function assert(
   condition: unknown,
-  msg: string = "Assertion failed"
+  msg: string = DEFAULT_ASSERTION_MESSAGE
 ): asserts condition {
   if (!condition) {
     throw new Error(msg);
   }
 }
-
