ce4493af491111374f3d15dead838483f3744b37
Riccardo Sacco <riccardo.sacco@it.ey.com>
2026-05-05 17:44:09 +0200

    feat(IT-Wallet): add ItwThirdPartyCredentials type for third-party credential status

--- ts/features/itwallet/analytics/utils/types.ts
+++ ts/features/itwallet/analytics/utils/types.ts
@@ -98,6 +98,8 @@ export type ItwCredentialMixpanelStatus =
   | "verification_expired"
   | "unknown";
 
+export type ItwThirdPartyCredentials = "not_available" | "valid" | "not_valid";
+
 export type ItwCredentialActionPosition = "screen" | "bottom_sheet";
 
 export type ItwStatus =
