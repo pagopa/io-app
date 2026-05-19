532ceff32bb60634147fdcb16b69bee972f3627a
Federico Mastrini <federicomastrini93@gmail.com>
2026-05-19 09:27:45 +0200

    chore: add confirmation for store action

--- ts/features/itwallet/presentation/proximity/screens/ItwProximityStoreConsentScreen.tsx
+++ ts/features/itwallet/presentation/proximity/screens/ItwProximityStoreConsentScreen.tsx
@@ -1,9 +1,28 @@
 import I18n from "i18next";
 import { OperationResultScreenContent } from "../../../../../components/screens/OperationResultScreenContent";
+import { useIODispatch } from "../../../../../store/hooks";
+import { identificationRequest } from "../../../../identification/store/actions";
 import { ItwProximityMachineContext } from "../machine/provider";
 
 export const ItwProximityStoreConsentScreen = () => {
+  const dispatch = useIODispatch();
   const machineRef = ItwProximityMachineContext.useActorRef();
+
+  const confirmConsentStore = () =>
+    dispatch(
+      identificationRequest(
+        false,
+        true,
+        undefined,
+        {
+          label: I18n.t("global.buttons.cancel"),
+          onCancel: () => undefined
+        },
+        {
+          onSuccess: () => machineRef.send({ type: "store-consent" })
+        }
+      )
+    );
 
   return (
     <OperationResultScreenContent
@@ -18,7 +37,7 @@ export const ItwProximityStoreConsentScreen = () => {
         label: I18n.t(
           "features.itWallet.presentation.proximity.storeConsent.action"
         ),
-        onPress: () => machineRef.send({ type: "store-consent" })
+        onPress: confirmConsentStore
       }}
       secondaryAction={{
         label: I18n.t(
