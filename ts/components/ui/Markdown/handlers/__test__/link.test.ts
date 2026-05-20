0f4dad7be73d0595a64378a1f4f6d9045f37e0ad
forrest57 <tomba808@gmail.com>
2026-05-20 12:03:09 +0200

    refactored said function to not use fp-ts

--- ts/components/ui/Markdown/handlers/__test__/link.test.ts
+++ ts/components/ui/Markdown/handlers/__test__/link.test.ts
@@ -1,4 +1,3 @@
-import * as E from "fp-ts/lib/Either";
 import {
   deriveCustomHandledLink,
   isHttpLink,
@@ -9,66 +8,73 @@ import {
 const loadingCases: ReadonlyArray<
   [input: string, expectedResult: ReturnType<typeof deriveCustomHandledLink>]
 > = [
-  ["", E.left(new Error(`"" is not recognized as a valid handled link`))],
-  [
-    "some text",
-    E.left(new Error(`"some text" is not recognized as a valid handled link`))
-  ],
-  [
-    "iohandledlink://noprotocol:somevalue",
-    E.left(
-      new Error(
-        `"iohandledlink://noprotocol:somevalue" is not recognized as a valid handled link`
-      )
-    )
-  ],
+  ["", undefined],
+  ["some text", undefined],
+  ["iohandledlink://noprotocol:somevalue", undefined],
+  ["mailto:somevalue", undefined],
   [
     "iohandledlink://http://www.google.com",
-    E.right({
+    {
       schema: "http",
       url: "http://www.google.com",
       value: "//www.google.com"
-    })
+    }
+  ],
+  [
+    "IOHANDLEDLINK://HTTP://WWW.GOOGLE.COM",
+    {
+      schema: "HTTP",
+      url: "HTTP://WWW.GOOGLE.COM",
+      value: "//WWW.GOOGLE.COM"
+    }
+  ],
+  [
+    "iohandledlink://iohandledLink://http://www.google.com",
+    {
+      schema: "http",
+      url: "http://www.google.com",
+      value: "//www.google.com"
+    }
   ],
   [
     "iohandledlink://https://www.google.com",
-    E.right({
+    {
       schema: "https",
       url: "https://www.google.com",
       value: "//www.google.com"
-    })
+    }
   ],
   [
     "iohandledlink://copy:123text456",
-    E.right({
+    {
       schema: "copy",
       url: "copy:123text456",
       value: "123text456"
-    })
+    }
   ],
   [
     "iohandledlink://sms:123456",
-    E.right({
+    {
       schema: "sms",
       url: "sms:123456",
       value: "123456"
-    })
+    }
   ],
   [
     "iohandledlink://tel:123456",
-    E.right({
+    {
       schema: "tel",
       url: "tel:123456",
       value: "123456"
-    })
+    }
   ],
   [
     "iohandledlink://mailto:name.surname@email.com",
-    E.right({
+    {
       schema: "mailto",
       url: "mailto:name.surname@email.com",
       value: "name.surname@email.com"
-    })
+    }
   ]
 ];
 
