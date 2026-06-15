947836180a4afceaa3c2090870d6d7c359a1e3db
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-06-15 11:34:36 +0200

    refactor: collapse header on scroll

--- apps/main-app/ts/features/bonus/cgn/screens/merchants/CgnMerchantsCategoriesSelectionScreen.tsx
+++ apps/main-app/ts/features/bonus/cgn/screens/merchants/CgnMerchantsCategoriesSelectionScreen.tsx
@@ -8,7 +8,7 @@ import {
   VSpacer
 } from "@pagopa/io-app-design-system";
 import I18n from "i18next";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { View } from "react-native";
 import { RefreshControl } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -71,42 +71,7 @@ const CgnMerchantsCategoriesSelectionScreen = () => {
 
   const tabRoutesKeys = Object.keys(CgnMerchantsHomeTabRoutes);
 
-  const ListHeaderComponent = (
-    <>
-      <TabNavigation
-        includeContentMargins={true}
-        tabAlignment="start"
-        selectedIndex={tabRoutesKeys.indexOf(selectedTab)}
-      >
-        {tabRoutesKeys.map((routeKey, index) => {
-          const route = routeKey as keyof CgnMerchantsHomeTabParamsList;
-          const onPress = () => setSelectedTab(route);
-
-          const label = tabOptions[route].title;
-          const accessibilityLabel = I18n.t(
-            "bonus.cgn.merchantsList.tabs.a11y",
-            {
-              label,
-              index: index + 1,
-              total: tabRoutesKeys.length
-            }
-          );
-          return (
-            <TabItem
-              testID={`cgn-merchants-tab-${route}`}
-              icon={tabOptions[route].icon}
-              label={label}
-              accessibilityLabel={accessibilityLabel}
-              key={route}
-              onPress={onPress}
-            />
-          );
-        })}
-      </TabNavigation>
-      <VSpacer size={16} />
-    </>
-  );
-  const animatedFlatListRef = useAnimatedRef<any>();
+  const animatedFlatListRef = useAnimatedRef<Animated.FlatList<unknown>>();
 
   useHeaderSecondLevel({
     title: I18n.t("bonus.cgn.merchantsList.screenTitle"),
@@ -129,12 +94,53 @@ const CgnMerchantsCategoriesSelectionScreen = () => {
     }
   });
 
+  const ListHeaderComponent = useMemo(
+    () => (
+      <>
+        <CgnAnimatedHeader />
+        <View style={{ flexGrow: 0, flexShrink: 0 }}>
+          <TabNavigation
+            includeContentMargins={true}
+            tabAlignment="start"
+            selectedIndex={tabRoutesKeys.indexOf(selectedTab)}
+          >
+            {tabRoutesKeys.map((routeKey, index) => {
+              const route = routeKey as keyof CgnMerchantsHomeTabParamsList;
+              const onPress = () => setSelectedTab(route);
+
+              const label = tabOptions[route].title;
+              const accessibilityLabel = I18n.t(
+                "bonus.cgn.merchantsList.tabs.a11y",
+                {
+                  label,
+                  index: index + 1,
+                  total: tabRoutesKeys.length
+                }
+              );
+              return (
+                <TabItem
+                  testID={`cgn-merchants-tab-${route}`}
+                  icon={tabOptions[route].icon}
+                  label={label}
+                  accessibilityLabel={accessibilityLabel}
+                  key={route}
+                  onPress={onPress}
+                />
+              );
+            })}
+          </TabNavigation>
+        </View>
+        <VSpacer size={16} />
+      </>
+    ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [selectedTab]
+  );
+
   return (
     <View style={{ flex: 1 }}>
-      <CgnAnimatedHeader ref={animatedFlatListRef}>
-        {ListHeaderComponent}
-      </CgnAnimatedHeader>
       <Animated.FlatList
+        ref={animatedFlatListRef}
         scrollEventThrottle={8}
         snapToEnd={false}
         data={[...data]}
@@ -148,6 +154,7 @@ const CgnMerchantsCategoriesSelectionScreen = () => {
             />
           )
         }
+        ListHeaderComponent={ListHeaderComponent}
         ListFooterComponent={ListFooterComponent}
         ListEmptyComponent={ListEmptyComponent}
         ItemSeparatorComponent={() => <Divider />}
