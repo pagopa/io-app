b558adc0b6cf997105b38c73fbba1e21490fc4c0
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-07-09 15:24:06 +0200

    refactor: hooks for debug printer

--- apps/main-app/ts/components/debug/DebugView.tsx
+++ apps/main-app/ts/components/debug/DebugView.tsx
@@ -6,7 +6,7 @@ import {
 
 import { PropsWithChildren } from "react";
 import { View } from "react-native";
-import { withDebugEnabled } from "./withDebugEnabled";
+import { useDebugEnabled } from "./withDebugEnabled";
 
 export type DebugViewProps = {
   title?: string;
@@ -16,12 +16,16 @@ export type DebugViewProps = {
 /**
  * This component renders its content only if the debug mode is enabled, otherwise return null (nothing)
  */
-export const DebugView = withDebugEnabled(
-  ({
-    children,
-    title = "Debug",
-    ignoreHorizontalMargins = false
-  }: PropsWithChildren<DebugViewProps>) => (
+export const DebugView = ({
+  children,
+  title = "Debug",
+  ignoreHorizontalMargins = false
+}: PropsWithChildren<DebugViewProps>) => {
+  const isDebug = useDebugEnabled();
+  if (!isDebug) {
+    return null;
+  }
+  return (
     <View
       testID="DebugViewTestID"
       style={{
@@ -36,5 +40,5 @@ export const DebugView = withDebugEnabled(
       <ListItemHeader label={title} iconName="ladybug" />
       {children}
     </View>
-  )
-);
+  );
+};
--- apps/main-app/ts/components/debug/withDebugEnabled.tsx
+++ apps/main-app/ts/components/debug/withDebugEnabled.tsx
@@ -1,16 +1,5 @@
-import { ComponentType } from "react";
 import { useIOSelector } from "../../store/hooks";
 import { isDebugModeEnabledSelector } from "../../store/reducers/debug";
 
-/**
- * This HOC allows to render the wrapped component only if the debug mode is enabled, otherwise returns null (nothing)
- */
-export const withDebugEnabled =
-  <P extends Record<string, unknown>>(WrappedComponent: ComponentType<P>) =>
-  (props: P) => {
-    const isDebug = useIOSelector(isDebugModeEnabledSelector);
-    if (!isDebug) {
-      return null;
-    }
-    return <WrappedComponent {...props} />;
-  };
+/** Returns true when debug mode is enabled. */
+export const useDebugEnabled = () => useIOSelector(isDebugModeEnabledSelector);
