413c2c59699f0ee72627886103642bf1884a7c9d
Davide Rafaele Onano <davide.onano@spindox.it>
2026-07-15 17:17:46 +0200

    chore: reorder import statements in ItwPresentationPidDetailScreen test file

--- apps/main-app/ts/features/itwallet/presentation/details/screens/__tests__/ItwPresentationPidDetailScreen.test.tsx
+++ apps/main-app/ts/features/itwallet/presentation/details/screens/__tests__/ItwPresentationPidDetailScreen.test.tsx
@@ -1,5 +1,6 @@
-import { createStore } from "redux";
 import I18n from "i18next";
+import { createStore } from "redux";
+
 import { applicationChangeState } from "../../../../../../store/actions/application";
 import { appReducer } from "../../../../../../store/reducers";
 import { GlobalState } from "../../../../../../store/reducers/types";
