b6f35399f7ac1af13f42e63eeabeb8e8c342839d
Damiano Plebani <damiano.plebani@pagopa.it>
2026-08-03 16:18:25 +0200

    Remove temporary references for testing purposes

--- apps/main-app/ts/features/idpay/barcode/saga/index.ts
+++ apps/main-app/ts/features/idpay/barcode/saga/index.ts
@@ -2,8 +2,12 @@ import { SagaIterator } from "redux-saga";
 import { takeLatest } from "typed-redux-saga/macro";
 
 import { IDPayClient } from "../../common/api/client";
-import { idPayGenerateBarcode } from "../store/actions";
+import {
+  idPayGenerateBarcode,
+  idPayGenerateStaticCode
+} from "../store/actions";
 import { handleGenerateBarcode } from "./handleGenerateBarcode";
+import { handleGenerateStaticCode } from "./handleGenerateStaticCode";
 
 export function* watchIDPayBarcodeSaga(
   idPayClient: IDPayClient,
@@ -13,6 +17,13 @@ export function* watchIDPayBarcodeSaga(
     idPayGenerateBarcode.request,
     handleGenerateBarcode,
     idPayClient.createBarCodeTransaction,
+    bearerToken
+  );
+
+  yield* takeLatest(
+    idPayGenerateStaticCode.request,
+    handleGenerateStaticCode,
+    idPayClient.retrievectiveBarCodeTransaction,
     bearerToken
   );
 }
