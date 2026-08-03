07ae751a3396d192c64e0f4a3f1a549cec6ddf39
Federico Mastrini <federicomastrini93@gmail.com>
2026-08-03 17:18:19 +0200

    refactor sagas

--- apps/main-app/ts/features/itwallet/statusList/saga/index.ts
+++ apps/main-app/ts/features/itwallet/statusList/saga/index.ts
@@ -1,23 +1,33 @@
 import { SagaIterator } from "redux-saga";
 import { call, fork, select } from "typed-redux-saga/macro";
 
-import { itwIsL3EnabledSelector } from "../../common/store/selectors";
+import { selectItwSpecsVersion } from "../../common/store/selectors/environment";
+import { getIoWallet } from "../../common/utils/itwIoWallet";
 import { registerStatusListProperties } from "../analytics";
 import { checkStatusListCoherenceSaga } from "./checkStatusListCoherenceSaga";
 import { refreshStaleStatusListsSaga } from "./refreshStaleStatusListsSaga";
 import { registerStatusListFetchTaskSaga } from "./registerStatusListFetchTaskSaga";
 
-export function* watchItwStatusListSaga(): SagaIterator {
-  const isWhitelisted = yield* select(itwIsL3EnabledSelector);
-  if (!isWhitelisted) {
-    // If the user is not whitelisted for L3 features, we can skip background
-    // task sagas as they won't have access to IT Wallet features that require
-    // status list checks.
+export function* watchItwStatusListAuthenticatedSaga(): SagaIterator {
+  const itwSpecsVersion = yield* select(selectItwSpecsVersion);
+  const ioWallet = getIoWallet(itwSpecsVersion);
+
+  if (!ioWallet.CredentialStatus.statusList.isSupported) {
     return;
   }
 
   // Register the background task for Status List fetch only for active wallet instances
   yield* fork(registerStatusListFetchTaskSaga);
+}
+
+export function* watchItwStatusListSaga(): SagaIterator {
+  const itwSpecsVersion = yield* select(selectItwSpecsVersion);
+  const ioWallet = getIoWallet(itwSpecsVersion);
+
+  if (!ioWallet.CredentialStatus.statusList.isSupported) {
+    return;
+  }
+
   // Run startup coherence for the Status List Token cache
   yield* call(checkStatusListCoherenceSaga);
   // Check for stale Status List Tokens and refresh them in the background
