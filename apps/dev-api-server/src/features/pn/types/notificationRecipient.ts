a7169feef6b1a924e29c372c7e9c74ab85392b40
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-11 16:19:01 +0200

    chore(knip): removed unused files from report

--- apps/dev-api-server/src/features/pn/types/notificationRecipient.ts
+++
@@ -1,8 +0,0 @@
-import { NotificationPaymentInfo } from "./notificationPaymentInfo";
-
-export type NotificationRecipient = {
-  denomination: string;
-  payment: NotificationPaymentInfo;
-  recipientType: string;
-  taxId: string;
-};
