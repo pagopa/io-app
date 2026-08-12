03ee29009121f600f4fc5488fa28eb05a22ec9bc
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-12 12:03:07 +0200

    chore(knip): removed unused file

--- apps/dev-api-server/src/features/pn/types/notificationPaymentInfo.ts
+++
@@ -1,7 +0,0 @@
-import { OrganizationFiscalCode } from "../../../../generated/definitions/communication/OrganizationFiscalCode";
-import { PaymentNoticeNumber } from "../../../../generated/definitions/communication/PaymentNoticeNumber";
-
-export type NotificationPaymentInfo = {
-  creditorTaxId: OrganizationFiscalCode;
-  noticeCode: PaymentNoticeNumber;
-};
