5cf9249ef7a967ad9f6aabb7a49d5129bc439c71
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-14 11:05:18 +0200

    refactor: replaced currentRouteSelector with passed prop

--- apps/main-app/ts/store/reducers/backendStatus/statusMessages.ts
+++ apps/main-app/ts/store/reducers/backendStatus/statusMessages.ts
@@ -7,7 +7,6 @@ import { getType } from "typesafe-actions";
 
 import { backendStatusLoadSuccess } from "../../actions/backendStatus";
 import { Action } from "../../actions/types";
-import { currentRouteSelector } from "../navigation";
 import { GlobalState } from "../types";
 
 export type StatusMessagesState = O.Option<BackendStatus["statusMessages"]>;
@@ -31,18 +30,19 @@ const EMPTY_ARRAY: ReadonlyArray<StatusMessage> = [];
 
 // Since the return of the selector comes from an array.filter function it is important to cache the result
 // to avoid unintended rerender on components.
-export const statusMessageByRouteSelector = createSelector(
-  [statusMessagesSelector, currentRouteSelector],
-  (statusMessages, currentRoute): ReadonlyArray<StatusMessage> | undefined =>
-    pipe(
-      statusMessages,
-      O.fromNullable,
-      O.map(({ items }) => {
-        const messages = items.filter(message =>
-          message.routes.includes(currentRoute)
-        );
-        return messages.length > 0 ? messages : EMPTY_ARRAY;
-      }),
-      O.toUndefined
-    )
-);
+export const statusMessageByRouteSelector = (currentRoute: string) =>
+  createSelector(
+    [statusMessagesSelector],
+    (statusMessages): ReadonlyArray<StatusMessage> | undefined =>
+      pipe(
+        statusMessages,
+        O.fromNullable,
+        O.map(({ items }) => {
+          const messages = items.filter(message =>
+            message.routes.includes(currentRoute)
+          );
+          return messages.length > 0 ? messages : EMPTY_ARRAY;
+        }),
+        O.toUndefined
+      )
+  );
