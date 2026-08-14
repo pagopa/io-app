00b9004a02d55a7e70a7a55f69e508d07443a57e
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-14 11:10:56 +0200

    test: update imports

--- apps/main-app/ts/utils/__tests__/content.test.ts
+++ apps/main-app/ts/utils/__tests__/content.test.ts
@@ -4,8 +4,7 @@ import * as O from "fp-ts/lib/Option";
 import { Locales, setLocale } from "../../i18n";
 import {
   getContextualHelpDataFromRouteSelector,
-  idpContextualHelpDataFromIdSelector,
-  screenContextualHelpDataSelector
+  idpContextualHelpDataFromIdSelector
 } from "../../store/reducers/content";
 
 const chData = {
@@ -156,50 +155,6 @@ describe("idpContextualHelpDataFromIdSelector", () => {
       expect(idpData.value.phone).toEqual(phone);
     }
   };
-});
-
-describe("screenContextualHelpDataSelector", () => {
-  it("should return no data if navigation state is empty", async () => {
-    const screenData = screenContextualHelpDataSelector.resultFunc(
-      pot.some(chData),
-      ""
-    );
-    expect(pot.isSome(screenData) && O.isNone(screenData.value)).toBeTruthy();
-  });
-
-  it("should return data (italian) if the current screen is present as key", async () => {
-    setLocale("it" as Locales);
-    assertScreenValues("title IT", "**content IT**");
-  });
-
-  it("should return data (english) if the current screen is present as key", async () => {
-    setLocale("en" as Locales);
-    assertScreenValues("title EN", "**content EN**");
-  });
-
-  it("should return data (italian) if the current screen is present as key and the set language is not supported", async () => {
-    setLocale("br" as Locales);
-    assertScreenValues("title IT", "**content IT**");
-  });
-
-  const assertScreenValues = (title: string, content: string) => {
-    const screenData = screenContextualHelpDataSelector.resultFunc(
-      pot.some(chData),
-      "AUTHENTICATION_IDP_LOGIN"
-    );
-    if (pot.isSome(screenData) && O.isSome(screenData.value)) {
-      expect(screenData.value.value.title).toEqual(title);
-      expect(screenData.value.value.content).toEqual(content);
-    }
-  };
-
-  it("should return no data if the current screen is not present as key", async () => {
-    const screenData = screenContextualHelpDataSelector.resultFunc(
-      pot.some(chData),
-      "NO_KEY"
-    );
-    expect(pot.isSome(screenData) && O.isNone(screenData.value)).toBeTruthy();
-  });
 });
 
 describe("getContextualHelpDataFromRouteSelector", () => {
