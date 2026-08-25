03773872108890031422fb8d02e3bc313880459c
Federico Mastrini <federicomastrini93@gmail.com>
2026-08-25 17:54:19 +0200

    fix: migrations

--- apps/main-app/ts/features/itwallet/walletInstance/store/reducers/index.ts
+++ apps/main-app/ts/features/itwallet/walletInstance/store/reducers/index.ts
@@ -70,7 +70,7 @@ export const itwWalletInstanceInitialState: ItwWalletInstanceState = {
 
 type MigrationState = PersistedState & Record<string, any>;
 
-const CURRENT_REDUX_ITW_WALLET_INSTANCE_STORE_VERSION = 6;
+const CURRENT_REDUX_ITW_WALLET_INSTANCE_STORE_VERSION = 7;
 
 export const migrations: MigrationManifest = {
   // Convert status into a pot for better async handling
@@ -100,7 +100,7 @@ export const migrations: MigrationManifest = {
     ...state,
     isRemotelyActive: undefined
   }),
-  // Add statusList
+  // Reset isRemotelyActive after changing its semantics
   "5": (state: MigrationState) => ({
     ...state,
     isRemotelyActive: undefined
@@ -110,6 +110,11 @@ export const migrations: MigrationManifest = {
     ...state,
     keyAttestations: state.walletUnitAttestations ?? {},
     walletUnitAttestations: undefined
+  }),
+  // Add statusList
+  "7": (state: MigrationState) => ({
+    ...state,
+    statusList: undefined
   })
 };
 
