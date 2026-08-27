72750f796b42417ba1da633458a5b26e4f5976e7
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-27 15:19:25 +0200

    chore: removed custom app review module

--- apps/main-app/ts/features/appReviews/native/AppReview.ts
+++
@@ -1,7 +0,0 @@
-import { NativeModules } from "react-native";
-
-export interface AppReviewModule {
-  requestReview: () => void;
-}
-
-export default NativeModules.AppReviewModule as AppReviewModule;
