11dd527cae7b46b2b957276bac45ebaccbaf7735
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-31 08:26:31 +0200

    fix: ios runtime crash

--- libs/expo-navigation-bar-manager/index.ts
+++ libs/expo-navigation-bar-manager/index.ts
@@ -1,4 +1,5 @@
 import { requireNativeModule } from "expo-modules-core";
+import { Platform } from "react-native";
 
 export type NavigationBarTheme = "dark" | "light";
 
@@ -10,7 +11,11 @@ interface NavigationBarManagerModuleType {
 }
 
 const NavigationBarManagerModule =
-  requireNativeModule<NavigationBarManagerModuleType>("NavigationBarManager");
+  Platform.OS === "android"
+    ? requireNativeModule<NavigationBarManagerModuleType>(
+        "NavigationBarManager"
+      )
+    : undefined;
 
 /**
  * Sets the Android navigation bar color and icon contrast for the given theme.
@@ -19,4 +24,5 @@ export const setNavigationBarColor = (
   theme: NavigationBarTheme,
   backgroundColor: string
 ): Promise<boolean> =>
-  NavigationBarManagerModule.setNavigationBarColor(theme, backgroundColor);
+  NavigationBarManagerModule?.setNavigationBarColor(theme, backgroundColor) ??
+  Promise.resolve(false);
