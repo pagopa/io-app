1dcb222addc7118784f38cb90b2f5c9bc4cec4e0
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-08-31 08:38:29 +0200

    fix: ios runtime crash

--- libs/expo-nfc-antenna-info/index.ts
+++ libs/expo-nfc-antenna-info/index.ts
@@ -1,4 +1,5 @@
 import { requireNativeModule } from "expo-modules-core";
+import { Platform } from "react-native";
 
 export type AvailableNfcAntenna = {
   locationX: number;
@@ -18,16 +19,32 @@ interface NfcAntennaInfoModuleType {
 }
 
 const NfcAntennaInfoModule =
-  requireNativeModule<NfcAntennaInfoModuleType>("NfcAntennaInfo");
+  Platform.OS === "android"
+    ? requireNativeModule<NfcAntennaInfoModuleType>("NfcAntennaInfo")
+    : undefined;
 
 /**
  * Retrieves NFC antenna information on Android devices with API level 34+.
+ * @throws If invoked on a platform other than Android.
  */
-export const getNfcAntennaInfo = (): Promise<NfcAntennaInfoResult> =>
-  NfcAntennaInfoModule.getNfcAntennaInfo();
+export const getNfcAntennaInfo = (): Promise<NfcAntennaInfoResult> => {
+  if (!NfcAntennaInfoModule) {
+    return Promise.reject(
+      new Error("NfcAntennaInfo is only available on Android devices.")
+    );
+  }
+  return NfcAntennaInfoModule.getNfcAntennaInfo();
+};
 
 /**
  * Checks if Host Card Emulation (HCE) is supported on the device.
+ * @throws If invoked on a platform other than Android.
  */
-export const isHceSupported = (): Promise<boolean> =>
-  NfcAntennaInfoModule.isHceSupported();
+export const isHceSupported = (): Promise<boolean> => {
+  if (!NfcAntennaInfoModule) {
+    return Promise.reject(
+      new Error("NfcAntennaInfo is only available on Android devices.")
+    );
+  }
+  return NfcAntennaInfoModule.isHceSupported();
+};
