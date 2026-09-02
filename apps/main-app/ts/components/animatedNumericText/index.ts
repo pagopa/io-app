a0a4d9f45a35c8eefc4172eb115fc99e35f8814f
Damiano Plebani <damiano.plebani@pagopa.it>
2026-09-02 11:06:52 +0200

    Add specific `knip` conf to ignore `*.ios` pattern

--- apps/main-app/ts/components/animatedNumericText/index.ts
+++ apps/main-app/ts/components/animatedNumericText/index.ts
@@ -1,3 +1,2 @@
 export { AnimatedNumericText } from "./AnimatedNumericText";
 export { DEFAULT_FONT_SIZE, DEFAULT_FONT_WEIGHT } from "./constants";
-export type { AnimatedNumericTextProps } from "./types";
