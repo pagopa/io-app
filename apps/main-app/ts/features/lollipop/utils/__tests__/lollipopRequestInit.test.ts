1b7ea18057b9649335d68095d8c7d5209ab80d10
Alessandra Lerteri <alessandralerteri@live.it>
2026-09-04 09:27:47 +0200

    test: add unit tests for customContentToSignPromises

--- apps/main-app/ts/features/lollipop/utils/__tests__/lollipopRequestInit.test.ts
+++ apps/main-app/ts/features/lollipop/utils/__tests__/lollipopRequestInit.test.ts
@@ -7,6 +7,7 @@ import { getUnixTimestamp } from "../../httpSignature/signature";
 import { KeyInfo } from "../crypto";
 import {
   customContentSignatureBases,
+  customContentToSignPromises,
   CutsomContentToSignInput,
   lollipopRequestInit
 } from "../fetch";
@@ -71,27 +72,29 @@ describe("Check lollipopRequestInit mocks", () => {
   });
 });
 
+const signatureConfigForgeInput = {
+  publicKey,
+  keyTag: keyInfo.keyTag as string,
+  lollipopConfig,
+  method: "GET",
+  inputUrl: new URLParse(fullUrl)
+};
+
+const baseCustomContentInput: CutsomContentToSignInput = {
+  customContentToSign: undefined,
+  keyInfo,
+  keyTag: keyInfo.keyTag as string,
+  signatureConfigForgeInput
+};
+
+const expectedSignatureInput = (headerName: string, ordinal: number) =>
+  `sig${ordinal}=("${headerName}");created=${mockTimestamp};nonce="nonce";alg="ecdsa-p256-sha256";keyid="${publicKeyThumbprint}"`;
+
+const expectedSignatureBase = (headerName: string, headerValue: string) =>
+  `"${headerName}": ${headerValue}\n"@signature-params": ("${headerName}");created=${mockTimestamp};nonce="nonce";alg="ecdsa-p256-sha256";keyid="${publicKeyThumbprint}"`;
+
 describe("customContentSignatureBases", () => {
-  const signatureConfigForgeInput = {
-    publicKey,
-    keyTag: keyInfo.keyTag as string,
-    lollipopConfig,
-    method: "GET",
-    inputUrl: new URLParse(fullUrl)
-  };
-
-  const baseInput: CutsomContentToSignInput = {
-    customContentToSign: undefined,
-    keyInfo,
-    keyTag: keyInfo.keyTag as string,
-    signatureConfigForgeInput
-  };
-
-  const expectedSignatureInput = (headerName: string, ordinal: number) =>
-    `sig${ordinal}=("${headerName}");created=${mockTimestamp};nonce="nonce";alg="ecdsa-p256-sha256";keyid="${publicKeyThumbprint}"`;
-
-  const expectedSignatureBase = (headerName: string, headerValue: string) =>
-    `"${headerName}": ${headerValue}\n"@signature-params": ("${headerName}");created=${mockTimestamp};nonce="nonce";alg="ecdsa-p256-sha256";keyid="${publicKeyThumbprint}"`;
+  const baseInput = baseCustomContentInput;
 
   it("should return an empty array if customContentToSign is undefined", () => {
     expect(customContentSignatureBases(baseInput)).toStrictEqual([]);
@@ -167,6 +170,76 @@ describe("customContentSignatureBases", () => {
         headerValue: anotherValue
       }
     ]);
+  });
+});
+
+describe("customContentToSignPromises", () => {
+  const baseInput = baseCustomContentInput;
+
+  const { sign } = jest.requireMock("@pagopa/io-react-native-crypto");
+
+  beforeEach(() => {
+    sign.mockReset().mockResolvedValue("MockSignature");
+  });
+
+  it("should return an empty array if there is no custom content to sign", async () => {
+    const result = await customContentToSignPromises(baseInput);
+    expect(result).toStrictEqual([]);
+    expect(sign).not.toHaveBeenCalled();
+  });
+
+  it("should sign every custom content and return the results in order", async () => {
+    const externalMessageId = "00000000000000000005";
+    const anotherValue = "another-value";
+    const result = await customContentToSignPromises({
+      ...baseInput,
+      customContentToSign: {
+        externalMessageId,
+        anotherKey: anotherValue
+      }
+    });
+
+    expect(result).toStrictEqual([
+      {
+        headerIndex: 2,
+        headerPrefix: "externalMessageId",
+        headerName: "x-pagopa-lollipop-custom-externalMessageId",
+        headerValue: externalMessageId,
+        signature: "sig2=:MockSignature:",
+        signatureInput: expectedSignatureInput(
+          "x-pagopa-lollipop-custom-externalmessageid",
+          2
+        )
+      },
+      {
+        headerIndex: 3,
+        headerPrefix: "anotherKey",
+        headerName: "x-pagopa-lollipop-custom-anotherKey",
+        headerValue: anotherValue,
+        signature: "sig3=:MockSignature:",
+        signatureInput: expectedSignatureInput(
+          "x-pagopa-lollipop-custom-anotherkey",
+          3
+        )
+      }
+    ]);
+    expect(sign).toHaveBeenCalledTimes(2);
+  });
+
+  it("should return an empty array if signing any custom content fails", async () => {
+    sign
+      .mockResolvedValueOnce("MockSignature")
+      .mockRejectedValueOnce(new Error("Failure"));
+
+    const result = await customContentToSignPromises({
+      ...baseInput,
+      customContentToSign: {
+        externalMessageId: "00000000000000000005",
+        anotherKey: "another-value"
+      }
+    });
+
+    expect(result).toStrictEqual([]);
   });
 });
 
