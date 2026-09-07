b0f7d0cca8f2fd39a4d4ef3deca521ea9bb54893
Gianluca Spada <gianluca.spada@spindox.it>
2026-09-07 12:14:52 +0000

    chore(IT-Wallet): [SIW-4552] Update IT-Wallet specification to version 1.4 LTS (#8487)
    ## Short description
    This PR updates the **IT-Wallet technical specification** to version
    [1.4.6](https://italia.github.io/eid-wallet-it-docs/releases/1.4.6/en/).
    Since the implementation details are handled by `io-react-native-wallet`
    and IO Wallet SDK, the bulk of this PR covers the renaming of Wallet
    Unit Attestation to Key Attestation.
    
    ## List of changes proposed in this pull request
    - Updated `io-react-native-wallet` to version 3.7.0
    - Set IT-Wallet specification version to 1.4.6
    (`CURRENT_ITW_SPECS_VERSION`)
    - Renamed Wallet Unit Attestation to Key Attestation
    - Minor changes to align to the new specification
    
    ## How to test
    Test the usual IT-Wallet flows and ensure everything works as expected.
    Test Documenti su IO for regressions.

--- apps/main-app/ts/features/itwallet/common/store/selectors/environment.ts
+++ apps/main-app/ts/features/itwallet/common/store/selectors/environment.ts
@@ -4,6 +4,7 @@ import { createSelector } from "reselect";
 
 import { GlobalState } from "../../../../../store/reducers/types";
 import { itwCredentialsEidSelector } from "../../../credentials/store/selectors";
+import { CURRENT_ITW_SPECS_VERSION } from "../../utils/constants";
 import { itwIsL3EnabledSelector } from "./index";
 
 export const selectItwEnv = (state: GlobalState) =>
@@ -43,6 +44,6 @@ export const selectItwSpecsVersion = createSelector(
       return eid.spec_version as ItwVersion;
     }
     // Users that are whitelisted and don't have an EID use the latest version by default
-    return "1.3.3";
+    return CURRENT_ITW_SPECS_VERSION;
   }
 );
