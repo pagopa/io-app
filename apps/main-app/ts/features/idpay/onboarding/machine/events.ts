a21fbb7bae46b2e0b5a0cb46912e2ac9cfa95203
Alice Di Rico <alice.dirico@pagopa.it>
2026-09-11 17:23:36 +0200

    feat: Adaptation to the new PARI APIs

--- apps/main-app/ts/features/idpay/onboarding/machine/events.ts
+++ apps/main-app/ts/features/idpay/onboarding/machine/events.ts
@@ -1,5 +1,4 @@
 import { SelfConsentBoolDTO } from "@io-app/api-types/generated/definitions/idpay/SelfConsentBoolDTO";
-import { SelfConsentMultiDTO } from "@io-app/api-types/generated/definitions/idpay/SelfConsentMultiDTO";
 import { SelfConsentMultiTypeDTO } from "@io-app/api-types/generated/definitions/idpay/SelfConsentMultiTypeDTO";
 import { SelfConsentTextDTO } from "@io-app/api-types/generated/definitions/idpay/SelfConsentTextDTO";
 
@@ -41,7 +40,7 @@ type RetryConnectionEvent = {
 };
 
 type SelectMultiConsent = {
-  readonly data: SelfConsentMultiDTO | SelfConsentMultiTypeDTO;
+  readonly data: SelfConsentMultiTypeDTO;
   readonly type: "select-multi-consent";
 };
 
