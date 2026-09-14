6b052c2de0bd33c3544f6d4986c5e55e3a8151bd
Alessandra Lerteri <alessandralerteri@live.it>
2026-09-14 16:12:44 +0200

    refactor: [IOPID-4207] read CIE ID security level and UAT flag from selectors

--- apps/main-app/ts/features/authentication/login/optIn/screens/OptInScreen.tsx
+++ apps/main-app/ts/features/authentication/login/optIn/screens/OptInScreen.tsx
@@ -47,14 +47,9 @@ export enum Identifier {
   TEST = "TEST"
 }
 
-export type ChosenIdentifier =
-  | {
-      identifier: Identifier.CIE_ID;
-      params: CieIdLoginProps;
-    }
-  | {
-      identifier: Identifier.CIE | Identifier.SPID | Identifier.TEST;
-    };
+export type ChosenIdentifier = {
+  identifier: Identifier;
+};
 
 const OptInScreen = () => {
   useHeaderSecondLevel({
@@ -93,16 +88,9 @@ const OptInScreen = () => {
   useFocusEffect(() => setAccessibilityFocus(accessibilityFirstFocuseViewRef));
 
   const getNavigationParams =
-    (): NavigatorScreenParams<AuthenticationParamsList> => {
-      if (params.identifier === "CIE_ID") {
-        return {
-          screen: authScreensMap[params.identifier],
-          params: params.params
-        };
-      }
-
-      return { screen: authScreensMap[params.identifier] };
-    };
+    (): NavigatorScreenParams<AuthenticationParamsList> => ({
+      screen: authScreensMap[params.identifier]
+    });
 
   const navigateToIdpPage = (isLV: boolean) => {
     if (isLV) {
