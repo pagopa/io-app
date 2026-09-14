7288804a9400060ea420d1b6549d8f0f0100f77b
Alessandra Lerteri <alessandralerteri@live.it>
2026-09-14 12:47:52 +0200

    refactor: rename errorCode prop to failure in idp selection failure content

--- apps/main-app/ts/features/authentication/login/idp/screens/OneIdentityIdpSelectionScreen.tsx
+++ apps/main-app/ts/features/authentication/login/idp/screens/OneIdentityIdpSelectionScreen.tsx
@@ -118,7 +118,7 @@ export const OneIdentityIdpSelectionScreen = ({
   if (state.status === "failure") {
     return (
       <OneIdentityIdpSelectionFailureContent
-        errorCode={state.error}
+        failure={state.error}
         isActiveSessionLogin={isActiveSessionLogin}
       />
     );
