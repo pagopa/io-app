4ed2a0a1f11d56eaaffccf6dce0ec6ad2f6a8835
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2026-09-24 14:35:12 +0200

    chore: removed unused function

--- apps/main-app/ts/store/actions/navigation.ts
+++ apps/main-app/ts/store/actions/navigation.ts
@@ -83,11 +83,3 @@ export const navigateToPrivacyScreen = () =>
 /**
  * CIE
  */
-
-/**
- * @deprecated
- */
-export const navigateToWorkunitGenericFailureScreen = () =>
-  NavigationService.dispatchNavigationAction(
-    CommonActions.navigate(ROUTES.WORKUNIT_GENERIC_FAILURE)
-  );
