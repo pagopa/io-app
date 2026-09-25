8c562b9458c42303a593f0e425c79d6f1b8cfe9a
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2026-09-25 15:43:58 +0200

    applied suggestion on similar code

--- apps/main-app/ts/features/authentication/common/components/CieIdWebViewLogin.tsx
+++ apps/main-app/ts/features/authentication/common/components/CieIdWebViewLogin.tsx
@@ -64,12 +64,7 @@ export const CieIdWebViewLogin = memo(
 
     const handleLoginFailure = useCallback(
       (code?: string, message?: string) => {
-        const reason = code
-          ? `login failed with code ${code}`
-          : message
-            ? `login failed with message ${message}`
-            : "login failed with no error code or message available";
-
+        const reason = `Login failed with code (${code}) and message (${message})`;
         onEvent({
           type: "LOGIN_FAILURE",
           payload: { code, message, reason }
--- apps/main-app/ts/features/authentication/common/components/CieWebViewLogin.tsx
+++ apps/main-app/ts/features/authentication/common/components/CieWebViewLogin.tsx
@@ -45,12 +45,7 @@ export const CieWebViewLogin = memo(
 
     const handleLoginFailure = useCallback(
       (code?: string, message?: string) => {
-        const reason = code
-          ? `login failed with code ${code}`
-          : message
-            ? `login failed with message ${message}`
-            : "login failed with no error code or message available";
-
+        const reason = `Login failed with code (${code}) and message (${message})`;
         onEvent({
           type: "LOGIN_FAILURE",
           payload: { code, message, reason }
